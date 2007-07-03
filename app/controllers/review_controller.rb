b5d0a0a2d0b7f24e5e71130eed09addfa87494b4
mkpatel2 <mkpatel2@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2007-07-03 21:06:07 +0000

    Work on Reviews
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@33 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -21,19 +21,13 @@ class ReviewController < ApplicationController
   end
   
   def view_review
-    @questions = Question.find(:all,:conditions => ["rubric_id = ?",(Assignment.find((ReviewMapping.find(params[:id])).assignment_id)).review_rubric_id])
-    @review = Review.find_by_review_mapping_id(params[:id])
-    @rubric = Rubric.find((Assignment.find((ReviewMapping.find(params[:id])).assignment_id)).review_rubric_id)
-    @valuemap = Hash.new
-    for i in @rubric.min_question_score..@rubric.max_question_score
-      @valuemap[i] = i
-    end
-    if(@review)
-     @found = true
-     
-    else
-      
-    end
+    @review = Review.new
+    @mapping = ReviewMapping.find(params[:id])
+    @assgt = Assignment.find(@mapping.assignment_id)
+    @questions = Question.find(:all,:conditions => ["rubric_id = ?", @assgt.review_rubric_id]) 
+    @rubric = Rubric.find(@assgt.review_rubric_id)
+    @max = @rubric.max_question_score
+    @min = @rubric.min_question_score
   end
   
   def edit_review
@@ -42,13 +36,48 @@ class ReviewController < ApplicationController
   
   def new_review
     
+    
   end
+  
+  def create_review
+     params.each do |elem|
+       puts "#{elem[0]}, #{elem[1]}" 
+     end
+     @review = Review.new
+     @review.review_mapping_id = params[:mapping_id]
+     
+     if params[:new_review_score]
+      # The new_question array contains all the new questions
+      # that should be saved to the database
+      for review_key in params[:new_review_score].keys
+        rs = ReviewScore.new(params[:new_review_score][review_key])
+        rs.question_id = params[:new_question][review_key]
+        rs.score = params[:new_score][review_key]
+        @review.review_scores << rs
+      end      
+    end
+    if @review.save
+      flash[:notice] = 'Rubric was successfully saved.'
+      redirect_to :action => 'list_reviews', :id => params[:assgt_id]
+    else # If something goes wrong, stay at same page
+      render :action => 'view_review'
+    end
+  end
+  
+ 
   
   def feedback
     @reviewer_id = session[:user].id
     @assignment_id = params[:id]
     @questions = Question.find(:all,:conditions => ["rubric_id = ?", Assignment.find(@assignment_id).review_rubric_id])
     @review_mapping = ReviewMapping.find(:all,:conditions => ["reviewer_id = ? and assignment_id = ?", @reviewer_id, @assignment_id])   
+  end
+  
+  def list_reviews
+    @reviewer_id = session[:user].id
+    @assignment_id = params[:id]
+    @questions = Question.find(:all,:conditions => ["rubric_id = ?", Assignment.find(@assignment_id).review_rubric_id])
+    @review_mapping = ReviewMapping.find(:all,:conditions => ["reviewer_id = ? and assignment_id = ?", @reviewer_id, @assignment_id])     
   end
   
 end

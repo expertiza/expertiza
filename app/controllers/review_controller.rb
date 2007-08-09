4125054b3e8017265fb7012998899b0df6796102
riwesley <riwesley@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2007-08-09 04:32:22 +0000

    wiki links now have <ul> tags
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@64 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -23,9 +23,39 @@ class ReviewController < ApplicationController
   
   def view_review
     @review = Review.find(params[:id])
+    @mapping_id = params[:id]
     @review_scores = @review.review_scores
     @mapping = ReviewMapping.find(@review.review_mapping_id)
     @assgt = Assignment.find(@mapping.assignment_id)    
+    @author = Participant.find(:first,:conditions => ["user_id = ? AND assignment_id = ?", @mapping.author_id, @assgt.id])
+    @questions = Question.find(:all,:conditions => ["rubric_id = ?", @assgt.review_rubric_id]) 
+    @rubric = Rubric.find(@assgt.review_rubric_id)
+    
+    if @assgt.team_assignment 
+      @author_first_user_id = TeamsUser.find(:first,:conditions => ["team_id=?", @mapping.team_id]).user_id
+      @team_members = TeamsUser.find(:all,:conditions => ["team_id=?", @mapping.team_id])
+      @author_name = User.find(@author_first_user_id).name;
+      @author = Participant.find(:first,:conditions => ["user_id = ? AND assignment_id = ?", @author_first_user_id, @mapping.assignment_id])
+    else
+      @author_name = User.find(@mapping.author_id).name
+      @author = Participant.find(:first,:conditions => ["user_id = ? AND assignment_id = ?", @mapping.author_id, @mapping.assignment_id])
+    end
+    
+    @max = @rubric.max_question_score
+    @min = @rubric.min_question_score 
+    
+    @current_folder = DisplayOption.new
+    @current_folder.name = "/"
+    if params[:current_folder]
+      @current_folder.name = StudentAssignmentHelper::sanitize_folder(params[:current_folder][:name])
+    end
+    
+    @files = Array.new
+    @files = get_submitted_file_list(@direc, @author, @files)
+    
+    if params['fname']
+      view_submitted_file(@current_folder,@author)
+    end   
   end
   
   def edit_review
--- app/models/review_mapping.rb
+++ app/models/review_mapping.rb
@@ -1,7 +1,7 @@
 class ReviewMapping < ActiveRecord::Base
   belongs_to :assignment
   has_many :reviews
-
+  
   def self.assign_reviewers(assignment_id, num_reviews, num_review_of_reviews)
     @authors = Participant.find(:all, :conditions => ['assignment_id = ? and submit_allowed=1', assignment_id])
     @reviewers = Participant.find(:all, :conditions => ['assignment_id = ? and review_allowed=1', assignment_id])
@@ -16,6 +16,27 @@ class ReviewMapping < ActiveRecord::Base
         current_author_candidate = (current_author_candidate + stride) % @authors.size
         ReviewMapping.create(:author_id => @authors[current_author_candidate].user_id, :reviewer_id => @reviewers[i].user_id, :assignment_id => assignment_id)
       end
+    end
+    for i in 0 .. @reviewers.size - 1
+      # The review mapping is such that ...
+      #   0  reviews 1, 2, 3
+      #   1  reviews 2, 3, 4
+      #   etc.
+      # The RoR mapping will be such that ...
+      #   0  reviews 1's review of 2
+      #   1  reviews 2's review of 3
+      #   etc.
+      #  To keep it simple, this code only creates 1 review of review; it ignores the 
+      # of reviews of reviews specified on the Create Review Assignment page!
+      current_reviewer_of_review_candidate = i
+      current_reviewer_candidate = (i + 1) % @reviewers.size
+      current_reviewee_candidate = (i + 2) % @reviewers.size
+      review_map = ReviewMapping.find(:first, :conditions =>['assignment_id = ? and reviewer_id = ? and author_id = ?', assignment_id, @reviewers[i].user_id, @reviewers[current_reviewee_candidate].user_id])
+      
+      ReviewOfReviewMapping.create(:reviewer_id => 
+                                   @reviewers[current_reviewer_of_review_candidate].user_id,
+                                   :review_mapping_id => review_map.id,
+                                   :assignment_id => assignment_id);
     end
   end
 end

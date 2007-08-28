12dcb8f69ef7b74ad01b3d3c22b1d4d8e0d6f419
ajbudlon <ajbudlon@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2007-08-28 23:01:12 +0000

    backend functionality for user profile updates. need to update Goldberg on production system to surface functionality in app.
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@75 392d7b7b-3f31-0410-9dc3-c95a9635ea79

---
+++ app/controllers/profile_controller.rb
@@ -0,0 +1,40 @@
+#Allows a user to update their own profile information
+class ProfileController < ApplicationController
+
+#load the view with the current fields
+#only valid if user is logged in
+ def edit 
+    @user = session[:user]
+    if @user == nil 
+      flash[:error]="Login required for this action."
+      redirect_to '/'
+    else
+       @user.confirm_password = ''
+    end
+ 
+ end
+  
+ #store parameters to user object
+ def update
+    @user = session[:user]
+    if params[:user][:clear_password] == ''
+      params[:user].delete('clear_password')
+    end
+
+    if params[:user][:clear_password] and
+        params[:user][:clear_password].length > 0 and
+        params[:user][:confirm_password] != params[:user][:clear_password]
+      flash[:error] = 'Password do not match!'
+      render :action => 'edit'
+    else
+      if @user.update_attributes(params[:user])
+        flash[:note] = 'Profile was successfully updated.'
+        redirect_to :action => 'edit', :id => @user
+      else
+        flash[:note] = 'Profile was not updated.'
+        render :action => 'edit'
+      end
+    end
+  end
+
+end

c54f00c82d3b321f7dee2492f8f2089f73b80a74
ajbudlon <ajbudlon@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2008-02-12 04:19:37 +0000

    Realized that assignment_id is not required since a participant_id is supplied and Participant model has a reference to assignment.
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@123 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- db/migrate/030_create_submission_weblinks.rb
+++ db/migrate/030_create_submission_weblinks.rb
@@ -1,14 +1,10 @@
 class CreateSubmissionWeblinks < ActiveRecord::Migration
   def self.up
     create_table :submission_weblinks do |t|
-      t.column :assignment_id, :integer # the assignment to which this link belongs
       t.column :participant_id, :integer # the participant which submitted this link
       t.column :link, :text # the URL which the participant provided
     end
     
-    execute "alter table submission_weblinks 
-             add constraint fk_assignments_submission_weblinks
-             foreign key (assignment_id) references assignments(id)"
     execute "alter table submission_weblinks 
              add constraint fk_participants_submission_weblinks
              foreign key (participant_id) references participants(id)"

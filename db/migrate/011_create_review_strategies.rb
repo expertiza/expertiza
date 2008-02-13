a85f8405114bfe06361722320e4c534b1c248602
rschmid <rschmid@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2008-02-13 03:27:13 +0000

    added create_review_strategies
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@126 392d7b7b-3f31-0410-9dc3-c95a9635ea79

---
+++ db/migrate/011_create_review_strategies.rb
@@ -0,0 +1,11 @@
+ class CreateReviewStrategies < ActiveRecord::Migration
+  def self.up
+    create_table :review_strategies do |t|
+      t.column :name, :string  # the name of the strategy, e.g., "rubric", "ranking"
+    end
+  end
+
+  def self.down
+    drop_table :review_strategies
+  end
+end

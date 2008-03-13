b8f055d52d097b211c1eec0b38fedca92dd28825
aveerav <aveerav@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2008-03-13 19:24:15 +0000

    survey response controller modified for displaying charts properly
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@136 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- app/controllers/survey_response_controller.rb
+++ app/controllers/survey_response_controller.rb
@@ -48,10 +48,10 @@ class SurveyResponseController < ApplicationController
         end
       end
       
-      if params[:count].to_i == @assigned_surveys.length
-        redirect_to(:action =>'submit')
-        return
-      end
+#      if params[:count].to_i == @assigned_surveys.length
+#        redirect_to(:action =>'submit', :survey_id=>params[:survey_id], :score=>@scores, :assignment_id=>params[:id])
+#        return
+#      end
       
       @survey = @assigned_surveys[params[:count].to_i]
       @questions = @survey.questions
@@ -63,12 +63,49 @@ class SurveyResponseController < ApplicationController
   end
 
   def submit
- 
+  @submitted = true;
+    @survey_id = params[:survey_id]
+    @survey = Questionnaire.find(@survey_id)
+    @questions = @survey.questions
+    @scores = params[:score]
+    @comments = params[:comments]
+    @assignment_id = params[:assignment_id]
+    for question in @questions
+      @new = SurveyResponse.new
+      @new.survey_id = @survey_id
+      @new.question_id = question.id
+      @new.assignment_id = @assignment_id
+      @new.survey_deployment_id=params[:survey_deployment_id]
+      @new.email = params[:email]
+      @new.score = @scores[question.id.to_s]
+      @new.comments = @comments[question.id.to_s]
+      @new.save
+      end
+    
+    if params[:survey_deployment_id] # Remove participant for course evaluations once he is done with the survey
+       @surveys=nil
+       SurveyParticipant.find_all_by_user_id_and_survey_deployment_id(session[:user].id,params[:survey_deployment_id]).each do |sp|
+         sp.destroy
+       end
+    else
+      @surveys = SurveyHelper::get_assigned_surveys(@assignment_id)
+    end
   end
   
   def view_responses
-    @assignment = Assignment.find(params[:id])
-    @surveys = SurveyHelper::get_all_available_surveys(params[:id], session[:user].role_id)
+   
+    if params[:course_eval] # Check if this is a course evaluation
+       survey_id=SurveyDeployment.find(params[:id]).course_evaluation_id
+       @surveys = [Questionnaire.find(survey_id)]
+       #Temprorary Assignment object
+       @assignment=Assignment.new
+       @assignment.name="Course Evaluation"
+       @assignment.id=params[:id]
+       @course_eval=params[:course_eval]
+    else
+      @assignment = Assignment.find(params[:id])
+      @surveys = SurveyHelper::get_all_available_surveys(params[:id], session[:user].role_id)
+    end
     @responses = Array.new
     @empty = true
     for survey in @surveys
@@ -98,6 +135,7 @@ class SurveyResponseController < ApplicationController
         for i in min..max
           if !question.true_false? || i == min || i == max
             list = SurveyResponse.find(:all, :conditions => ["assignment_id = ? and survey_id = ? and question_id = ? and score = ?", params[:id], survey.id, question.id, i])
+            
             if question.true_false?
               if i == min
                 this_response_question[:labels] << "False"
@@ -107,15 +145,18 @@ class SurveyResponseController < ApplicationController
             else
               this_response_question[:labels] << i
             end
-            this_response_question[:values] << list.length
-            this_response_question[:count] += list.length
+            this_response_question[:values] << list.length.to_s  
             this_response_question[:average] += i*list.length
-          end
+           elsif params[:course_eval]
+            list = SurveyResponse.find(:all, :conditions => ["survey_deployment_id = ? and survey_id = ? and question_id = ? and score = ?", params[:id], survey.id, question.id, i]);
+          end  
         end
+        no_of_question = SurveyResponse.find(:all, :conditions => ["assignment_id = ? and survey_id = ? and question_id = ?", params[:id], survey.id, question.id])
+        this_response_question[:count] = no_of_question.length
         if this_response_question[:count] > 0 
           @empty = false
           this_response_survey[:empty] = false
-          this_response_question[:average] /= this_response_question[:count].to_f      
+          this_response_question[:average] /= this_response_question[:count].to_f  
           this_response_survey[:avg_labels] << this_response_question[:name][0..50] 
           this_response_survey[:avg_values] << this_response_question[:average]
         end
@@ -126,9 +167,22 @@ class SurveyResponseController < ApplicationController
   end
   
   def comments
-    @responses = SurveyResponse.find(:all, :conditions => ["assignment_id = ? and survey_id = ? and question_id = ? and comments != ?", params[:assignment_id], params[:survey_id], params[:question_id], ""], :order => "score");
+    unless params[:course_eval] # Check if survey is a course evaluation
+      @responses = SurveyResponse.find(:all, :conditions => ["assignment_id = ? and survey_id = ? and question_id = ?", params[:assignment_id], params[:survey_id], params[:question_id]], :order => "score"); 
+    else
+      @responses = SurveyResponse.find(:all, :conditions => ["survey_deployment_id = ? and survey_id = ? and question_id = ?", params[:assignment_id], params[:survey_id], params[:question_id]], :order => "score");
+      @course_eval="1"   
+    end
+    
     @question = Question.find(params[:question_id])
     @survey = Questionnaire.find(params[:survey_id])
+   
+   unless params[:course_eval] 
     @assignment = Assignment.find(params[:assignment_id])
+   else
+    @assignment=Assignment.new
+    @assignment.name="Course Evaluation"
+   end
+   
   end
 end

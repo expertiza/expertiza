5e6e5e1c3c5917238591de54915a7e1123d47442
aveerav <aveerav@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2008-03-13 19:23:22 +0000

    author feedback functionality added
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@135 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- app/controllers/review_feedback_controller.rb
+++ app/controllers/review_feedback_controller.rb
@@ -48,4 +48,132 @@ class ReviewFeedbackController < ApplicationController
     ReviewFeedback.find(params[:id]).destroy
     redirect_to :action => 'list'
   end
+  
+  # Action implemented for editing the feedback rubric already entered
+  def edit_feedback
+  @a = (params[:id3])
+    @b = (params[:id2])
+    # Find entry in ReviewFeedback table with passed review id and author id
+    @reviewfeedback = ReviewFeedback.find(:first, :conditions =>["review_id =? AND author_id = ?", @a, @b])
+  
+  #@reviewfeedback = ReviewFeedback.find_by_review_id(params[:id3])
+    @assgt_id = params[:id1]
+    @author_id = params[:id2]
+    @review_id = params[:id3]
+    @assignment = Assignment.find(@assgt_id)
+    @questions = Question.find(:all,:conditions => ["questionnaire_id = ?", @assignment.author_feedback_rubric_id]) 
+    @rubric = Rubric.find(@assignment.author_feedback_rubric_id)
+    @max = @rubric.max_question_score
+    @min = @rubric.min_question_score  
+    
+  end
+
+  #Action for entering a new feedback
+  #Find the questions for particular feedback from Questions table and display those questions
+  def new_feedback
+
+    @review_feedback = ReviewFeedback.new
+    @assgt_id = params[:id1]
+    @author_id = params[:id2]
+    @review_id = params[:id3]
+    @assignment = Assignment.find(@assgt_id)
+    @questions = Question.find(:all,:conditions => ["questionnaire_id = ?", @assignment.author_feedback_rubric_id]) 
+    @rubric = Rubric.find(@assignment.author_feedback_rubric_id)
+    @max = @rubric.max_question_score
+    @min = @rubric.min_question_score  
+      
+    
+  end
+  #Action for creating a new feedback record in the ReviewFeedback table.
+  #Save the comments of Feedback in review scores table and the additional cooment in ReviewFeedback table
+  def create_feedback
+    params.each do |elem|
+    puts "#{elem[0]}, #{elem[1]}" 
+    end
+    
+    @review_feedback = ReviewFeedback.new
+    @assgt_id = params[:assgt_id]
+    @author_id = params[:author_id]
+    @review_id = params[:review_id]
+    
+    @review_feedback.additional_comment = params[:new_feedback][:comments]
+    @review_feedback.assignment_id = @assgt_id
+    @review_feedback.author_id = @author_id
+    @review_feedback.review_id = @review_id
+
+    if params[:new_review_score]
+        for review_key in params[:new_review_score].keys
+        rs = ReviewScore.new(params[:new_review_score][review_key])
+        rs.review_id = @review_id
+        rs.question_id = params[:new_question][review_key]
+        rs.score = params[:new_score][review_key]
+        rs.questionnaire_type_id = 4        
+        @review_feedback.review_scores<< rs
+      end      
+    end
+    if @review_feedback.save
+      flash[:notice] = 'ReviewFeedback was successfully created.'
+      redirect_to :action=> 'view_feedback', :id1 =>params[:assgt_id], :id2 =>params[:author_id], :id3=>params[:review_id]
+    end
+    @review_scores = ReviewScore.find(:all,:conditions =>["review_id = ? and questionnaire_type_id = ?", @review_id, '4'])
+    for review_score in @review_scores
+    review_score.review_id = @review_feedback.id
+    review_score.update
+    end
+  end
+ 
+  #Action for updating a previous feedback and inserting new values in the ReviewFeedback and ReviewScores table
+  def update_feedback
+    @a = (params[:review_id])
+    @b = (params[:author_id])
+    @reviewfeedback = ReviewFeedback.find(:first, :conditions =>["review_id =? AND author_id = ?", @a, @b])
+    @reviewfeedback.additional_comment = params[:new_reviewfeedback][:comments]
+    @rev_id = @reviewfeedback.id
+    @author_id = params[:author_id]
+    @assgt_id = params[:assgt_id]
+    if params[:new_review_score]
+      # The new_question array contains all the new questions
+      # that should be saved to the database
+      for review_key in params[:new_review_score].keys
+        question_id = params[:new_question][review_key]
+        rs = ReviewScore.find(:first,:conditions => ["review_id = ? AND question_id = ?", @rev_id, question_id])
+        rs.comments = params[:new_review_score][review_key][:comments]
+        rs.score = params[:new_score][review_key]
+        rs.questionnaire_type_id = "4"
+        rs.update
+      end      
+    end
+    if @reviewfeedback.update
+      flash[:notice] = 'Review was successfully updated.'
+      redirect_to :action=> 'view_feedback', :id1 =>params[:assgt_id], :id2 =>params[:author_id], :id3=>params[:review_id]
+    end    
+  end
+  
+  # Action for Viewing the Feedback previously entered.
+  def view_feedback
+    @a = (params[:id3])
+    @b = (params[:id2])
+    @reviewfeedback = ReviewFeedback.find(:first, :conditions =>["review_id =? AND author_id = ?", @a, @b])
+    #@reviewfeedback = ReviewFeedback.find_by_review_id(params[:id3]) 
+    @review_id = @reviewfeedback.id
+    puts @review_id
+    @review_scores = ReviewScore.find(:all,:conditions =>["review_id =? AND questionnaire_type_id = ?", @review_id, '4'])
+    @assgt_id = params[:id1]
+    @author_id = params[:id2]
+    @assgt = Assignment.find(@assgt_id)
+    @questions = Question.find(:all,:conditions => ["questionnaire_id = ?", @assgt.author_feedback_rubric_id])
+  end
+  
+  
+  # Action for Instructor to view a review given by the reviwer to an author. The author Feedback will also be available through this action
+  def view_feedback_instructor 
+    @reviewfeedback = ReviewFeedback.find(:all, :conditions =>["review_id =? AND author_id = ?", (params[:id3]), (params[:id2])]) 
+    @review_id = @reviewfeedback.id
+    @review_scores = ReviewScore.find(:all,:conditions =>["review_id =? AND questionnaire_type_id = ?", @review_id, '4'])
+    @assgt_id = params[:id1]
+    @author_id = params[:id2]
+    @assgt = Assignment.find(@assgt_id)
+    @questions = Question.find(:all,:conditions => ["questionnaire_id = ?", @assgt.author_feedback_rubric_id])
+  end  
+  
 end

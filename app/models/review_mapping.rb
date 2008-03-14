031e08cd1a85245b144a0c2b0ca6616ac8f3710e
aveerav <aveerav@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2008-03-14 12:15:17 +0000

    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@148 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- app/models/review_mapping.rb
+++ app/models/review_mapping.rb
@@ -5,22 +5,42 @@ class ReviewMapping < ActiveRecord::Base
   has_many :reviews
   has_many :review_of_review_mappings
   
-  def self.assign_reviewers(assignment_id, num_reviews, num_review_of_reviews)
+  ##feedback added
+  def self.assign_reviewers(assignment_id, num_reviews, num_review_of_reviews, mapping_strategy)
     @authors = Participant.find(:all, :conditions => ['assignment_id = ? and submit_allowed=1', assignment_id])
     @reviewers = Participant.find(:all, :conditions => ['assignment_id = ? and review_allowed=1', assignment_id])
+    @assignments = Assignment.find_by_id(assignment_id)
     puts 'authors.size = ', @authors.size
     puts 'reviewers.size = ', @reviewers.size
     
-    stride = 1 # get_rel_prime(num_reviews, @reviewers.size)
-    for i in 0 .. @reviewers.size - 1
-      current_reviewer_candidate = i
-      current_author_candidate = current_reviewer_candidate
-      for j in 0 .. (@reviewers.size * num_reviews / @authors.size) - 1  # This method potentially assigns authors different #s of reviews, if limit is non-integer
-        current_author_candidate = (current_author_candidate + stride) % @authors.size
-        ReviewMapping.create(:author_id => @authors[current_author_candidate].user_id, :reviewer_id => @reviewers[i].user_id, :assignment_id => assignment_id)
+    due_date = DueDate.find(:all,:conditions => ["assignment_id = ?",assignment_id], :order => "round DESC", :limit =>1)
+    @round = 1
+    if (due_date[0] && !due_date[0].round.nil?)
+      @round = due_date[0].round - 1
+    end
+    puts "rounds = ",@round
+    for round_num in 1..@round
+      puts "round# ",round_num
+      stride = 1 # get_rel_prime(num_reviews, @reviewers.size)
+      for i in 0 .. @reviewers.size - 1
+        current_reviewer_candidate = i
+        current_author_candidate = current_reviewer_candidate
+        for j in 0 .. (@reviewers.size * num_reviews / @authors.size) - 1  # This method potentially assigns authors different #s of reviews, if limit is non-integer
+          current_author_candidate = (current_author_candidate + stride) % @authors.size
+            if (@assignments.team_assignment != true)
+              ReviewMapping.create(:author_id => @authors[current_author_candidate].user_id, :reviewer_id => @reviewers[i].user_id, :assignment_id => assignment_id, :round => round_num)
+            else
+              team = TeamsUser.find(:first,:conditions=>["user_id =? and team_id in (select id from teams where assignment_id=?)", @authors[current_author_candidate].user_id, assignment_id]).team_id
+              ReviewMapping.create(:author_id => @authors[current_author_candidate].user_id, :reviewer_id => @reviewers[i].user_id, :assignment_id => assignment_id, :round => round_num, :team_id=>team)
+            end  
+            ##
+            puts 'Review Mapping created'
+            ##
+        end
       end
     end
   end
+  ##
   
   def self.import_reviewers(file,assignment)
     File.open(file, "r") do |infile|
@@ -44,7 +64,7 @@ class ReviewMapping < ActiveRecord::Base
   def get_author_ids
     author_ids = Array.new
     if (self.team_id)
-      team_users = TeamUser.find_by_sql("select * from team_users where team_id = " + self.team_id.to_s)
+      team_users = TeamsUser.find_by_sql("select * from teams_users where team_id = " + self.team_id.to_s)
       for member in team_users
         author_id << member.user_id
       end

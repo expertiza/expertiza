49ad2f29ad6695531b3db3747ba4a8cbdbb61f4d
ajbudlon <ajbudlon@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2008-03-21 22:19:27 +0000

    Additional Migrate files
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@179 392d7b7b-3f31-0410-9dc3-c95a9635ea79

---
+++ db/migrate/011_create_review_strategies.rb
@@ -0,0 +1,12 @@
+class CreateReviewStrategies < ActiveRecord::Migration
+  def self.up
+  create_table "review_strategies", :force => true do |t|
+    t.column "name", :string
+  end
+  
+  execute "INSERT INTO `review_strategies` VALUES (1,'questionnaire');"
+  end
+
+  def self.down
+  end
+end

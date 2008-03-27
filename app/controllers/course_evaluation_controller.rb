ec2084dd9eeaa1a42fab49531a906422d6d288a1
aveerav <aveerav@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2008-03-27 21:18:25 +0000

    controller to administer and track course evaluations
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@191 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- app/controllers/course_evaluation_controller.rb
+++ app/controllers/course_evaluation_controller.rb
@@ -4,14 +4,13 @@ class CourseEvaluationController < ApplicationController
     unless session[:user] #Check for a valid user
       redirect_to '/'
       return 
-    end
-    
+    end    
     deployments=SurveyParticipant.find_all_by_user_id(session[:user].id)
     @surveys=Array.new
     deployments.each do |sd|
       survey_deployment=SurveyDeployment.find(sd.survey_deployment_id)
       if(Time.now>survey_deployment.start_date && Time.now<survey_deployment.end_date)
-        @surveys<<[Rubric.find(survey_deployment.course_evaluation_id),sd.survey_deployment_id,survey_deployment.end_date]
+        @surveys<<[Questionnaire.find(survey_deployment.course_evaluation_id),sd.survey_deployment_id,survey_deployment.end_date, survey_deployment.course_id]
       end
      end
    end

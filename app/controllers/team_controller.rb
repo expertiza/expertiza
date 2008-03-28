78f9e0a8e1bf2d2a53a7e806b08ac126282d2600
aveerav <aveerav@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2008-03-28 04:12:27 +0000

    changes made to list team assignments
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@198 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -1,5 +1,4 @@
 require 'dl'
-
 class TeamController < ApplicationController
    auto_complete_for :user, :name
    
@@ -12,32 +11,38 @@ class TeamController < ApplicationController
         end       
         flash[:note] = str
      end
-     if (params[:assignment_id] == nil)
-       assignment_id = session[:assignment_id]
-     else
-       session[:assignment_id] = params[:assignment_id]
-       assignment_id = params[:assignment_id]
-     end
-     @assignment = Assignment.find(assignment_id)    
-     @team_pages, @teams = paginate :teams, :conditions => ["assignment_id = ?",assignment_id], :per_page => 10
+     session[:assignment_id] = params[:assignment_id]
+     
+     @assignment = Assignment.find(session[:assignment_id])    
+     @team_pages, @teams = paginate :teams, :conditions => ["assignment_id = ?",session[:assignment_id]], :per_page => 10
   end
   
   def edit
     @team = Team.find(params[:id])
   end
+  
+  def destroy
+    @team = Team.find(params[:id])
+    for teamsuser in TeamsUser.find(:all, :conditions => ["team_id =?", @team.id])
+       teamsuser.destroy
+    end    
+    @team.destroy
+    redirect_to :action => 'list', :assignment_id=> params[:assignment_id]
+  end
 
   def new
-    @assignment = Assignment.find(session[:assignment_id])
+    @assignment = Assignment.find(params[:assignment_id])
+    
     @team = Team.new 
   end
 
   def create
-    check = Team.find(:all, :conditions => ["name =? and assignment_id =?", params[:team][:name], session[:assignment_id]])        
+    check = Team.find(:all, :conditions => ["name =? and assignment_id =?", params[:team][:name], params[:assignment_id]])        
     @team = Team.new(params[:team])
-    @team.assignment_id = session[:assignment_id]
+    @team.assignment_id = params[:assignment_id]
     if (check.length == 0)      
       @team.save
-      redirect_to :action => 'list'
+      redirect_to :action => 'list', :assignment_id=> params[:assignment_id]
     else
       flash[:error] = 'Team name is already in use.'        
       render :action => 'new'
@@ -49,8 +54,10 @@ class TeamController < ApplicationController
     check = Team.find(:all, :conditions => ["name =? and assignment_id =?", params[:team][:name], @team.assignment_id])    
     if (check.length == 0)
        if @team.update_attributes(params[:team])
-          redirect_to :action => 'list'
+          redirect_to :action => 'list', :assignment_id=> params[:assignment_id]
        end
+    elsif (check.length == 1 && check[0].name = params[:team][:name])
+      redirect_to :action => 'list', :assignment_id=> params[:assignment_id]
     else
       flash[:error] = 'Team name is already in use.'        
       render :action => 'edit'
@@ -62,12 +69,17 @@ class TeamController < ApplicationController
       file = params['uploaded_file']
       unknown = TeamHelper::upload_teams(file,params[:assignment_id],params[:options],logger)           
     end  
-    redirect_to :action => 'list', :unknown => unknown
+    redirect_to :action => 'list', :unknown => unknown, :assignment_id=> params[:assignment_id]
   end  
   
   def list_assignments
-    user_id = session[:user].id    
-    @assignment_pages, @assignments = paginate :assignments, :order => 'name',:conditions => ["instructor_id = ? and team_assignment =?", session[:user].id, 1], :per_page => 10
+    user_id = session[:user].id
+    if (User.find_by_id(session[:user].id).role_id!=1)
+      @assignments = Assignment.find(:all, :order => 'name',:conditions => ["instructor_id = ? and team_assignment =?", session[:user].id, 1])
+    else
+      @assignments = Assignment.find(:all, :order => 'name',:conditions => ["team_assignment =?", 1])
+    end 
+    
   end
   
   def delete_team

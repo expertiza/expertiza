507e7630864808a667a64eee897274ec42cf6d3e
rschmid <rschmid@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2008-05-05 19:51:28 +0000

    renamed txt column to additional_comments and user_id column to author_id and updated all references to them
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@257 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- app/models/review_feedback.rb
+++ app/models/review_feedback.rb
@@ -1,3 +1,6 @@
 class ReviewFeedback < ActiveRecord::Base
     has_many :review_scores
+    belongs_to :review
+    belongs_to :assignment
+    belongs_to :author, :class_name => "User", :foreign_key => "author_id"
 end

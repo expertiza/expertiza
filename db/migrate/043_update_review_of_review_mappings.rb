b720f582afa4b5cfb72346650f2728bb2d07d88e
ajbudlon <ajbudlon@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2008-07-17 18:30:59 +0000

    Needed to remove key for column before removing column.
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@311 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- db/migrate/043_update_review_of_review_mappings.rb
+++ db/migrate/043_update_review_of_review_mappings.rb
@@ -1,6 +1,11 @@
 class UpdateReviewOfReviewMappings < ActiveRecord::Migration
   def self.up
-    remove_column "review_of_review_mappings", :assignment_id, :reviewer_id, :review_id    
+    execute "ALTER TABLE review_of_review_mappings 
+             DROP FOREIGN KEY fk_review_of_review_mapping_reviews"
+             
+    remove_column "review_of_review_mappings", "assignment_id"
+    remove_column "review_of_review_mappings", "reviewer_id"
+    remove_column "review_of_review_mappings", "review_id"    
   end
 
   def self.down

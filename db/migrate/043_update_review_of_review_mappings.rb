e2c5ee0ca100f03a91036b13de9fe477735b698e
ajbudlon <ajbudlon@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2008-07-18 22:40:12 +0000

    Invalid index removal - corrected.
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@315 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- db/migrate/043_update_review_of_review_mappings.rb
+++ db/migrate/043_update_review_of_review_mappings.rb
@@ -1,10 +1,9 @@
 class UpdateReviewOfReviewMappings < ActiveRecord::Migration
   def self.up
-    execute "ALTER TABLE review_of_review_mappings 
-             DROP FOREIGN KEY fk_review_of_review_mapping_reviews"
-             
+    execute "ALTER TABLE `review_of_review_mappings` 
+    DROP INDEX `fk_review_of_review_mapping_reviews`"
+
     remove_column "review_of_review_mappings", "assignment_id"
-    remove_column "review_of_review_mappings", "reviewer_id"
     remove_column "review_of_review_mappings", "review_id"    
   end
 

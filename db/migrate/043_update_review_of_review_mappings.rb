fbb2d0d958a2b5ba5b052fcd1db2781770354ecd
ajbudlon <ajbudlon@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2008-07-24 23:34:29 +0000

    Migrate file changes
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@317 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- db/migrate/043_update_review_of_review_mappings.rb
+++ db/migrate/043_update_review_of_review_mappings.rb
@@ -1,8 +1,16 @@
 class UpdateReviewOfReviewMappings < ActiveRecord::Migration
   def self.up
+    begin
     execute "ALTER TABLE `review_of_review_mappings` 
-    DROP INDEX `fk_review_of_review_mapping_reviews`"
-
+             DROP INDEX `fk_review_of_review_mapping_reviews`"
+    rescue
+    end
+   
+    begin
+    execute "ALTER TABLE `review_of_review_mappings` 
+             DROP FOREIGN KEY  `fk_review_of_review_mapping_reviews`"      
+    rescue      
+    end
     remove_column "review_of_review_mappings", "assignment_id"
     remove_column "review_of_review_mappings", "review_id"    
   end

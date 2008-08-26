0236ef071a9c86ee1e6516c82a6818df6ef6ad41
aveerav <aveerav@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2008-08-26 02:02:25 +0000

    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@333 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -3,10 +3,15 @@
 # Author: unknown
 class Administrator < User
   
-  QUESTIONNAIRE = [["My instructors' questionnaires",'list_instructors'],
+  QUESTIONNAIRE = [["My instructor's questionnaires",'list_instructors'],
             ['My questionnaires','list_mine'],
             ['All public questionnaires','list_all']]
-  ASSIGNMENT = [["My instructors' assignments",'list_instructors'],
+            
+  SIGNUPSHEET = [["My instructor's signups",'list_instructors'],
+            ['My signups','list_mine'],
+            ['All public signups','list_all']]          
+            
+  ASSIGNMENT = [["My instructor's assignments",'list_instructors'],
                 ['My assignments','list_mine'],
                 ['All public assignments','list_all']]
     

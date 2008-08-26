162c8d9499a4642571b4828cf5855adb9ed52cca
aveerav <aveerav@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2008-08-26 02:04:38 +0000

    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@335 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,7 +3,13 @@ class Question < ActiveRecord::Base
     belongs_to :review_score  # each review_score pertains to a particular question
     belongs_to :review_of_review_score  # ditto
     has_many :question_advices # for each question, there is separate advice about each possible score
-
+    has_many :signup_choices
+    
+    validates_presence_of :txt
+    validates_presence_of :weight
+    validates_uniqueness_of :txt
+    validates_numericality_of :weight
+    
     NUMERIC = 'Numeric'
     TRUE_FALSE = 'True/False'
 

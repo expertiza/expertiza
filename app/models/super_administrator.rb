08ca3f9a74130fb0f65706ad0bbf202cf02262c0
aveerav <aveerav@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2008-08-26 01:54:26 +0000

    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@332 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- app/models/super_administrator.rb
+++ app/models/super_administrator.rb
@@ -1,14 +1,20 @@
 
 class SuperAdministrator < User
   
-  QUESTIONNAIRE = [["My instructors' questionnaires",'list_instructors'],
-            ["My admins' questionnaires",'list_admins'],
-            ['My questionnaires','list_mine'],
+  QUESTIONNAIRE = [["My instructor's questionnaires",'list_instructors'],
+            ["My admin's questionnaires",'list_admins'],
+            ["My questionnaires",'list_mine'],
             ['All public questionnaires','list_all'],
             ['All private questionnaires','list_all_private']]
+            
+  SIGNUPSHEET = [["My instructor's signups",'list_instructors'],
+            ["My admin's signups",'list_admins'],
+            ['My signups','list_mine'],
+            ['All public signups','list_all'],
+            ['All private signups','list_all_private']]          
  
-  ASSIGNMENT = [["My instructors' assignments",'list_instructors'],
-                ["My admins' assignments",'list_admins'],
+  ASSIGNMENT = [["My instructor's assignments",'list_instructors'],
+                ["My admin's assignments",'list_admins'],
                 ['My assignments','list_mine'],
                 ['All public assignments','list_all'],
                 ['All private assignments','list_all_private']]
@@ -26,13 +32,23 @@ class SuperAdministrator < User
   end
    
   def list_admins(object_type, user_id)
-    object_type.find(:all,
-                     :joins => "inner join users on " + object_type.to_s.pluralize + ".instructor_id = users.id AND users.parent_id = " + user_id.to_s)
+    if (object_type != SignupSheet)
+      object_type.find(:all,
+                       :joins => "inner join users on " + object_type.to_s.pluralize + ".instructor_id = users.id AND users.parent_id = " + user_id.to_s)
+    else 
+      object_type.find(:all,
+                       :joins => "inner join users on instructor_id = users.id AND users.parent_id = " + user_id.to_s)  
+    end                 
   end
 
   def list_instructors(object_type, user_id)
-    object_type.find(:all,
-                     :joins => "inner join users on " + object_type.to_s.pluralize + ".instructor_id = users.id AND users.parent_id = " + user_id.to_s)
+    if (object_type != SignupSheet)
+      object_type.find(:all,
+                       :joins => "inner join users on " + object_type.to_s.pluralize + ".instructor_id = users.id AND users.parent_id = " + user_id.to_s)
+    else
+      object_type.find(:all,
+                       :joins => "inner join users on instructor_id = users.id AND users.parent_id = " + user_id.to_s)
+    end                   
   end
 
   def get(object_type, id, user_id)

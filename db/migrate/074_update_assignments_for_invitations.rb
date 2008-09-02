1b1869d7732269eb6af002d9233d606faf7a8ee6
ajbudlon <ajbudlon@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2008-09-02 15:09:36 +0000

    Added rescue conditions to avoid errors when migrating against partially updated databases
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@352 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- db/migrate/074_update_assignments_for_invitations.rb
+++ db/migrate/074_update_assignments_for_invitations.rb
@@ -1,7 +1,14 @@
 class UpdateAssignmentsForInvitations < ActiveRecord::Migration
-  def self.up      
-    add_column :assignments, :team_count, :integer, :null => false, :default => 0
-    remove_column :assignments, :max_team_count
+  def self.up   
+    begin
+      add_column :assignments, :team_count, :integer, :null => false, :default => 0
+    rescue
+    end
+  
+    begin
+      remove_column :assignments, :max_team_count
+    rescue
+    end  
   end
 
   def self.down

f7221620093317207829a3769dc3352212335b2b
aveerav <aveerav@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2008-10-09 14:48:39 +0000

    fixed typo from temmate to teammate
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@421 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- db/migrate/078_rename_peer_review_questionnaire_id_to_teammate_questionnaire_id_in_assignments.rb
+++ db/migrate/078_rename_peer_review_questionnaire_id_to_teammate_questionnaire_id_in_assignments.rb
@@ -1,6 +1,6 @@
 class RenamePeerReviewQuestionnaireIdToTeammateQuestionnaireIdInAssignments < ActiveRecord::Migration
   def self.up
-    execute "ALTER TABLE `assignments` CHANGE `peer_review_questionnaire_id` `temmate_review_questionnaire_id` INT( 10 ) NULL DEFAULT NULL"
+    execute "ALTER TABLE `assignments` CHANGE `peer_review_questionnaire_id` `teammate_review_questionnaire_id` INT( 10 ) NULL DEFAULT NULL"
   end
 
   def self.down

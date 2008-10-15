34841eb1ad3d7d89a93431d0f745107ad04bd0a5
ajbudlon <ajbudlon@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2008-10-15 17:28:27 +0000

    I just realized the current database migration is 80 - this means the changes Ashwin introduced are not valid in this migration. I've updated it and migration 82 to support the name change from Review Rubric to Review.
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@431 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- db/migrate/080_correct_questionnaire_type_in_review_scores.rb
+++ db/migrate/080_correct_questionnaire_type_in_review_scores.rb
@@ -1,13 +1,9 @@
 class CorrectQuestionnaireTypeInReviewScores < ActiveRecord::Migration
   def self.up
-    type = QuestionnaireType.find_by_name('Review Rubric')
-    type.name = 'Review'
-    type.save
-    
     ReviewScore.find(:all).each{
        |entry|
        if entry.questionnaire_type_id.nil?
-         entry.questionnaire_type_id = QuestionnaireType.find_by_name('Review').id
+         entry.questionnaire_type_id = QuestionnaireType.find_by_name('Review Rubric').id
        elsif entry.questionnaire_type_id == 4
          entry.questionnaire_type_id = QuestionnaireType.find_by_name('Author Feedback').id
        end

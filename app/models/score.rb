c1f20baffebe014df4f637d5294bb9892e928ddd
ajbudlon <ajbudlon@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2008-10-23 20:33:11 +0000

    Update to instructor grading view for Teammate Reviews.
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@464 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,2 +1,3 @@
 class Score < ActiveRecord::Base
+  belongs_to :question
 end
--- app/models/teammate_review.rb
+++ app/models/teammate_review.rb
@@ -5,28 +5,12 @@ class TeammateReview < ActiveRecord::Base
   
   # Computes the total score awarded for a  teammate review
   def get_total_score
-    scores = Score.find_by_sql("select * from scores where instance_id = "+self.id.to_s+" and questionnaire_type_id= "+ QuestionnaireType.find_by_name("Teammate Review").id)
+    scores = Score.find_by_sql("select * from scores where instance_id = "+self.id.to_s+" and questionnaire_type_id= "+ QuestionnaireType.find_by_name("Teammate Review").id.to_s)
     total_score = 0
     scores.each{
       |item|
       total_score += item.score
     }   
     return total_score
-  end
-  
-  def display_as_html  
-    code = "<B>Reviewer:</B> "+self.reviewer.fullname+'&nbsp;&nbsp;&nbsp;<a href="#" name= "review'+self.id.to_s+'Link" onClick="toggleElement('+"'review"+self.id.to_s+"','review'"+');return false;">hide teammate review</a>'
-    code = code + '<div id="review'+self.id.to_s+'" style="">'   
-    code = code + '<BR/><BR/>'
-    scores = Score.find_by_sql("select * from scores where instance_id = "+self.id.to_s+" and questionnaire_type_id= "+ QuestionnaireType.find_by_name("Teammate Review").id)
-    scores.each{
-      | teammateReviewScore |      
-      code = code + "<I>"+teammateReviewScore.question.txt+"</I><BR/><BR/>"
-      code = code + '(<FONT style="BACKGROUND-COLOR:gold">'+teammateReviewScore.score.to_s+"</FONT> out of <B>"+teammateReviewScore.question.questionnaire.max_question_score.to_s+"</B>): "+teammateReviewScore.comments+"<BR/><BR/>"
-    }          
-    comment = self.additional_comment.gsub('^p','').gsub(/\n/,'<BR/>&nbsp;&nbsp;&nbsp;')    
-    code = code + "<B>Additional Comment:</B><BR/>"+comment+""
-    code = code + "</div>"
-    return code
-  end
+  end 
 end

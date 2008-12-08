16c6478ede7676a47dcfc03896b9428d6f08cd45
ajbudlon <ajbudlon@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2008-12-08 18:58:40 +0000

    Corrections for problems found during MAE277 assignment creation:   1) Wrong message was being returned for review mapping deletion   2) User deletion was expecting a courses_users table, which is not present in our system.      Corrected this by removing the has_and_belongs_to_many :courses line   3) AssignmentParticipants will automatically be added when a reviewer is added to a mapping   4) Import of assignmentteams and courseteams corrected.
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@478 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- app/models/assignment_team.rb
+++ app/models/assignment_team.rb
@@ -1,5 +1,67 @@
 class AssignmentTeam < Team
 
+  def self.import(row,session,id,options)
+    if row.length < 2
+       raise ArgumentError, "Not enough items" 
+    end
+    
+    assignment = Assignment.find(id)
+    if assignment == nil
+      raise ImportError, "The assignment with id \""+id.to_s+"\" was not found. <a href='/assignment/new'>Create</a> this assignment?"
+    end
+    
+    if options[:has_column_names] == "true"
+        name = row[0].to_s.strip
+        index = 1
+    else
+        name = generate_team_name()
+        index = 0
+    end 
+    
+    currTeam = AssignmentTeam.find(:first, :conditions => ["name =? and parent_id =?",name,assignment.id])
+    
+    if options[:handle_dups] == "ignore" && currTeam != nil
+      return
+    end
+    
+    if currTeam != nil && options[:handle_dups] == "rename"
+       name = generate_team_name()
+       currTeam = nil
+    end
+    if options[:handle_dups] == "replace" && teams.first != nil        
+       for teamsuser in TeamsUser.find(:all, :conditions => ["team_id =?", currTeam.id])
+           teamsuser.destroy
+       end    
+       currTeam.destroy
+       currTeam = nil
+    end     
+    
+    if currTeam == nil
+       currTeam = Team.new
+       currTeam.name = name
+       currTeam.assignment_id = assignment.id
+       currTeam.save
+    end
+      
+    while(index < row.length) 
+        user = User.find_by_name(row[index].to_s.strip)
+        if user == nil
+          raise ImportError, "The user \""+row[index].to_s.strip+"\" was not found. <a href='/users/new'>Create</a> this user?"                           
+        elsif currTeam != nil         
+          currUser = TeamsUser.find(:first, :conditions => ["team_id =? and user_id =?", currTeam.id,user.id])          
+          if currUser == nil
+            currUser = TeamsUser.new
+            currUser.team_id = currTeam.id
+            currUser.user_id = user.id
+            currUser.save   
+            
+            AssignmentParticipant.create(:parent_id => assignment.id, :user_id => user.id, :permission_granted => true)
+          end                      
+        end
+        index = index+1      
+    end                
+  end
+
   def email
     self.get_team_users.first.email    
   end
--- app/models/team.rb
+++ app/models/team.rb
@@ -18,68 +18,6 @@ class Team < ActiveRecord::Base
     return self.name
   end
   
-  def self.import(row,session,id,options)
-    if row.length < 2
-       raise ArgumentError, "Not enough items" 
-    end
-    
-    assignment = Assignment.find(id)
-    if assignment == nil
-      raise ImportError, "The assignment with id \""+id.to_s+"\" was not found. <a href='/assignment/new'>Create</a> this assignment?"
-    end
-    
-    if options[:has_column_names] == "true"
-        name = row[0].to_s.strip
-        index = 1
-    else
-        name = generate_team_name()
-        index = 0
-    end 
-    
-    currTeam = Team.find(:first, :conditions => ["name =? and assignment_id =?",name,assignment.id])
-    
-    if options[:handle_dups] == "ignore" && currTeam != nil
-      return
-    end
-    
-    if currTeam != nil && options[:handle_dups] == "rename"
-       name = generate_team_name()
-       currTeam = nil
-    end
-    if options[:handle_dups] == "replace" && teams.first != nil        
-       for teamsuser in TeamsUser.find(:all, :conditions => ["team_id =?", currTeam.id])
-           teamsuser.destroy
-       end    
-       currTeam.destroy
-       currTeam = nil
-    end     
-    
-    if currTeam == nil
-       currTeam = Team.new
-       currTeam.name = name
-       currTeam.assignment_id = assignment.id
-       currTeam.save
-    end
-      
-    while(index < row.length) 
-        user = User.find_by_name(row[index].to_s.strip)
-        if user == nil
-          raise ImportError, "The user \""+row[index].to_s.strip+"\" was not found. <a href='/users/new'>Create</a> this user?"                           
-        elsif currTeam != nil         
-          currUser = TeamsUser.find(:first, :conditions => ["team_id =? and user_id =?", currTeam.id,user.id])          
-          if currUser == nil
-            currUser = TeamsUser.new
-            currUser.team_id = currTeam.id
-            currUser.user_id = user.id
-            currUser.save   
-            
-            AssignmentParticipant.create(:assignment_id => assignment.id, :user_id => user.id, :permission_granted => true)
-          end                      
-        end
-        index = index+1      
-    end                
-  end
-  
   def self.generate_team_name()
     counter = 0    
     while (true)

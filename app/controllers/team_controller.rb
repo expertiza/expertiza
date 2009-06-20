a1dc173409826d891370e351fce3a092df0cb5f8
ukataka <ukataka@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2009-06-20 01:24:56 +0000

    Added code to create teams randomly.
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@538 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -1,6 +1,194 @@
 class TeamController < ApplicationController
  auto_complete_for :user, :name  
+
+   ############## These methods created by mahesh############
+ def And(number1, number2)
+   #produces bitwise and of two integers
+   return number1&number2
+ end
+ 
+ def score(team)
+   #for each student in team 
+   #result += And(studnet, nextstudnet)
+   #dec2bin(result)
+   #return sum of 1's in dec2bin(result) 
+ end
+ 
+ def min(a,b)
+   if a<b
+     return a
+   end
+ else
+   return b
+ end
+ 
+def create_teams_view
+ @parent = Object.const_get(session[:team_type]).find(params[:id])
+ 
+end
+
+def delete_all
+  
+#  parent = Object.const_get(session[:team_type]).find(params[:id])
+#  redirect_to :action => 'list' , :id => parent.id
+  
+#  participants = Participant.find_all(["parent_id=?",parent.id])
+#  no_of_teams = (participants.length)/(parent.team_count)
+#  diff = (participants.length)%(parent.team_count)
+  
+
+    #no_of_teams ++
+
+  
+#
+#   for i in 1..no_of_teams
+#   team = Team.find_by_name("Team #{i}")
+ #  team.delete
+#   end
+
+  parent = Object.const_get(session[:team_type]).find(params[:id])  
+  teams = Team.find_all(["parent_id=?",parent.id])
+  
+  for team in teams
+    team.delete
+  end
+redirect_to :action => 'list', :id => parent.id
+
+end
+
+
+
+def randomize_teams
+  
+   parent = Object.const_get(session[:team_type]).find(params[:id])  
+   participants = Participant.find_all(["parent_id=?",parent.id])
    
+   participants = participants.sort{rand(3) - 1 }
+   
+      
+   #participants = Participant.find_by_sql("SELECT user_id from participants where parent_id = ", parent.id)
+    for participant in participants 
+      puts participant.user_id
+      user = User.find_by_id(participant.user_id)
+      puts user.name
+    end
+  
+   puts "***********" 
+   puts participants.length
+   puts parent.team_count
+   
+   no_of_teams = (participants.length)/(parent.team_count)
+   diff = (participants.length)%(parent.team_count)
+   
+   i=0
+   j=0
+   k=0
+   
+   
+         
+  for i in 1..no_of_teams
+      #formteam("Team"+ i+1)
+      #puts i
+    #team = Object.const_get(session[:team_type]+'Team').create(:name => "#{parent.name} Team #{i}", :parent_id => parent.id)
+   begin
+     check_for_existing_team_name(parent,"Team #{i}")
+     
+   rescue TeamExistsError
+   team = Team.find_by_name("Team #{i}")
+   team.delete
+     
+   end
+     team = Object.const_get(session[:team_type]+'Team').create(:name => "Team #{i}", :parent_id => parent.id)
+     TeamNode.create(:parent_id => parent.id, :node_object_id => team.id)
+ 
+     
+    j=i*parent.team_count
+    
+    for k in 1..parent.team_count
+    #for k in 0..(parent.team_count -1)
+    user = User.find_by_id(participants[j-k].user_id)
+    team.add_member(user)
+    #puts parent.team_count
+    end
+    #puts parent.team_count
+  end
+
+    
+  if diff != 0
+    begin
+     check_for_existing_team_name(parent,"Team #{no_of_teams+1}")
+     
+   rescue TeamExistsError
+   team = Team.find_by_name("Team #{no_of_teams+1}")
+   team.delete
+     
+   end
+    
+  team = Object.const_get(session[:team_type]+'Team').create(:name => "Team #{no_of_teams+1}", :parent_id => parent.id)
+  TeamNode.create(:parent_id => parent.id, :node_object_id => team.id)
+  
+  for indx in 1..diff
+    user = User.find_by_id(participants[participants.length-indx].user_id)
+    team.add_member(user)
+  end
+    
+    
+  end
+ 
+ #for each member of the user-assignment table create random teams
+ # and add them to the database 
+  
+end
+
+
+
+
+def create_teams
+   parent = Object.const_get(session[:team_type]).find(params[:id])
+      
+   #call randomize
+   randomize_teams
+   #teams = Team.find_all(["parent_id=?",parent.id])
+   #for tm in teams
+    # puts "##########"
+     #puts tm.id
+   #end
+   
+   #for tm in teams
+   #team_users = TeamsUser.find_all(["team_id=?",tm.id])
+   #puts "###############"
+   #puts team_users.user_id
+   #end
+      
+   
+  #The actual team maker algorithm
+  #for i in 1 .. 20
+    # for each random team A in teamset
+        #for each random team B in teamset - {team A}
+            #for each studentA in team A
+                #for each studentB in team B
+                    #oldscore = min(score(teamA),score(teamB))
+                    #swap studnetA and studentB
+                    #newscore = min(score(teamA),score(teamB))
+                    #if newscore < Thresholdscore
+                      #revert
+                    #end if
+                #end
+            #end
+        #end
+    #end  
+  #i++
+  #end
+
+  #if all team scores >= Thresholdscore
+  #add each student into the table
+  #add each team to the table 
+   
+  redirect_to :action => 'list', :id => parent.id
+ end
+
+
+##############                                       ############
  def list
    if params[:type]
     session[:team_type] = params[:type]

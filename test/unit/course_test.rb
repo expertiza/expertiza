f538b6fd968caa54641d56dd776905119075c6db
efg <efg@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2009-08-14 13:32:34 +0000

    Repair course_test.rb and course_controller_test.rb so that the tests succeed. This necessitate a change to course_controller, which simply uncommented out a rescue clause that had been commented out for no apparent reason.
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@547 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- test/unit/course_test.rb
+++ test/unit/course_test.rb
@@ -7,18 +7,22 @@ class CourseTest < Test::Unit::TestCase
     @course = Course.find(1)
   end
   
-  def test_create
+  def test_retrieval
     assert_kind_of Course, @course
+    assert_equal "My Course", @course.name
     assert_equal 1, @course.id
-    assert_equal "Object-Oriented Programming", @course.title
+    assert_equal 1, @course.instructor_id
+    assert_equal 'abc123', @course.directory_path
+    assert_equal 'This is info', @course.info
+    assert @course.private
   end
-  
+ 
   def test_update
-    assert_equal "Object-Oriented Programming", @course.title
-    @course.title = "Object-Oriented"
+    assert_equal "My Course", @course.name
+    @course.name = "Object-Oriented"
     @course.save
     @course.reload
-    assert_equal "Object-Oriented", @course.title
+    assert_equal "Object-Oriented", @course.name
   end
   
   def test_destroy

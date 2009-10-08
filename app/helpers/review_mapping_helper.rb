4aa09907adb6de78aa89364efc5b968dbcc142cf
ajbudlon <ajbudlon@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2009-10-08 22:14:09 +0000

    Resolved:   Team import without column names should only require a single field. The logic was requiring two fields in all cases.
      Review Mapping "delete all" actions were not functioning due to an assumption that the .class call on a params value
      would provide the correct class. Instead it was returning as a String class.
       
      Added the ability to delete all reviewer and metareviewer mappings from a single menu option.
    
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@573 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- app/helpers/review_mapping_helper.rb
+++ app/helpers/review_mapping_helper.rb
@@ -1,2 +1,24 @@
 module ReviewMappingHelper
+  
+  def self.delete_mappings(mappings,flash,contributor = nil)  
+    title = "A delete action failed:<br/>"
+    msg = ""    
+    mappings.each{ 
+       |mapping|
+       begin
+         mapping.delete
+       rescue
+         msg += "&nbsp;&nbsp;&nbsp;" + $! + "<a href='/review_mapping/delete_review/"+mapping.id.to_s+"'>Delete these reviews</a>?<br/>"
+       end
+    }
+    if msg.length > 0
+      title += msg
+      flash[:error] = title      
+    elsif contributor.nil?
+      flash[:note] = "All review and metareview mappings have been deleted."
+    else
+      flash[:note] = "All review mappings for \""+contributor.name+"\" have been deleted."      
+    end
+      
+  end  
 end

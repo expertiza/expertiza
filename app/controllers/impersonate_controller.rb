441a549f4b03e7cf42e5b9226209308b950b719c
ajbudlon <ajbudlon@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2009-10-13 21:07:41 +0000

    Update menus after impersonating a user
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@591 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- app/controllers/impersonate_controller.rb
+++ app/controllers/impersonate_controller.rb
@@ -1,9 +1,8 @@
 class ImpersonateController < ApplicationController
   #auto_complete_for :user, :name
   
-  def auto_complete_for_user_name       
-     current_user = User.find(session[:user].id)
-     @users = current_user.getAvailableUsers(params[:user][:name])        
+  def auto_complete_for_user_name     
+     @users = session[:user].getAvailableUsers(params[:user][:name])        
      render :inline => "<%= auto_complete_result @users, 'name' %>", :layout => false
   end
 
@@ -11,14 +10,16 @@ class ImpersonateController < ApplicationController
      
   end
  
-  def impersonate   
+  def impersonate 
      user = User.find_by_name(params[:user][:name])
      if user
         if session[:super_user] == nil
           session[:super_user] = session[:user]
         end
-        session[:user] = user 
-        ImpersonateHelper::display_user_view(session,logger)     
+        
+        AuthController.clear_user_info(session, nil)
+        session[:user] = user
+        AuthController.set_current_role(user.role_id, session)   
         redirect_to :action => AuthHelper::get_home_action(session[:user]), 
                     :controller => AuthHelper::get_home_controller(session[:user])
      else 
@@ -30,9 +31,10 @@ class ImpersonateController < ApplicationController
   
   def restore
       if session[:super_user] != nil
+        AuthController.clear_user_info(session, nil)
         session[:user] = session[:super_user]
-        session[:super_user] = nil
-        ImpersonateHelper::display_user_view(session,logger)
+        session[:super_user] = nil       
+        AuthController.set_current_role(session[:user].role_id, session)   
         redirect_to :action => AuthHelper::get_home_action(session[:user]), 
                     :controller => AuthHelper::get_home_controller(session[:user])
      else

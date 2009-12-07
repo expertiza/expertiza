d321428116ad566b1ff15adf31e13c561b5c728f
ajbudlon <ajbudlon@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2009-12-07 21:24:43 +0000

    Resolving problem reported by student  - Hardcoded reference to database  - Use of SQL instead of Ruby commands
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@624 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- db/migrate/079_rename_peer_reviews.rb
+++ db/migrate/079_rename_peer_reviews.rb
@@ -1,72 +1,71 @@
 class RenamePeerReviews < ActiveRecord::Migration
   def self.up
+   
     begin
-       execute "RENAME TABLE `pg_development`.`peer_reviews`  TO `pg_development`.`teammate_reviews`;"
-     rescue
-       end
-     begin
-       execute "RENAME TABLE `pg_development`.`peer_review_scores`  TO `pg_development`.`teammate_review_scores`;"
+     rename_table :peer_reviews, :teammate_reviews
     rescue
-       end
+    end
+    
     begin
-       execute "alter table pg_development.teammate_reviews drop foreign key fk_peer_reviews_assignments"
-       rescue
-     end
-     begin
-       execute "alter table pg_development.teammate_reviews drop index fk_peer_reviews_assignments"
-       rescue
-     end
-     begin
-       add_index "teammate_reviews", ["assignment_id"], :name => "fk_teammate_reviews_assignments"
-     rescue
-   end
-   begin
-       execute "alter table teammate_reviews
-                add constraint fk_teammate_reviews_assignments
-                foreign key (assignment_id) references assignments(id)"
-                rescue
-       end
-       begin
-              
-       execute "alter table pg_development.teammate_review_scores drop foreign key fk_peer_review_score_peer_reviews"
-       rescue
-     end
-     begin
-       execute "alter table pg_development.teammate_review_scores drop index fk_peer_review_score_peer_reviews"
-rescue
-end
-      begin
-       execute "alter table pg_development.teammate_review_scores drop foreign key fk_peer_review_score_questions"
-       rescue
-     end
-     begin
-       execute "alter table pg_development.teammate_review_scores drop index fk_peer_review_score_questions"
-       rescue
-       end
-       begin
-       execute " ALTER TABLE `teammate_review_scores` CHANGE `peer_review_id` `teammate_review_id` INT( 11 ) NULL DEFAULT NULL"
-       rescue
-       end
-       begin
-       add_index "teammate_review_scores", ["teammate_review_id"], :name => "fk_teamate_review_score_teammate_reviews"
-       rescue
-       end
+     rename_table :peer_review_scores, :teammate_review_scores
+    rescue
+    end
+
+    begin
+       execute "alter table teammate_reviews drop foreign key fk_peer_reviews_assignments"
+    rescue
+    end
+    
+    begin
+       execute "alter table teammate_reviews drop index fk_peer_reviews_assignments"
+    rescue
+    end
+  
+    begin
+       execute 'ALTER TABLE `teammate_reviews`
+             ADD CONSTRAINT fk_teammate_reviews_assignments
+             FOREIGN KEY (assignment_id) REFERENCES assignments(id)'        
+    rescue
+    end
+  
+    begin             
+       execute "alter table teammate_review_scores drop foreign key fk_peer_review_score_peer_reviews"
+    rescue
+    end
+  
+    begin
+       execute "alter table teammate_review_scores drop index fk_peer_review_score_peer_reviews"
+    rescue
+    end
+  
+    begin
+       execute "alter table teammate_review_scores drop foreign key fk_peer_review_score_questions"
+    rescue
+    end
+    
+    begin
+       execute "alter table teammate_review_scores drop index fk_peer_review_score_questions"
+    rescue
+    end
+      
+    begin
+       change_column :teammate_review_scores, :peer_review_id, :teammate_review_id, :integer, :null => true
+    rescue
+    end
+
     begin
        execute "alter table teammate_review_scores 
                 add constraint fk_teamate_review_score_teammate_reviews
                 foreign key (teammate_review_id) references teammate_reviews(id)"
-     rescue
+   rescue
    end
+
    begin
-       add_index "teammate_review_scores", ["question_id"], :name => "fk_teammate_review_score_questions"
-  rescue
-end
-    begin
        execute "alter table teammate_review_scores 
                 add constraint fk_teammate_review_score_questions
                 foreign key (question_id) references questions(id)"
-      rescue
-    end        
+   rescue
+   end        
   end
 
   def self.down

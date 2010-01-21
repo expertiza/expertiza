a4e17626f316960925bce60a8b36b961ea50dea5
ajbudlon <ajbudlon@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2010-01-21 13:05:33 +0000

    Overlooked several models in the previous upload.
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@637 392d7b7b-3f31-0410-9dc3-c95a9635ea79

---
+++ app/models/review_of_review.rb
@@ -0,0 +1,151 @@
+class ReviewOfReview < ActiveRecord::Base
+    belongs_to :mapping, :class_name => 'ReviewOfReviewMapping', :foreign_key => 'mapping_id'
+    
+  def display_as_html(prefix = nil, count = nil) 
+    if prefix
+      identifier = "<B>Metareviewer:</B> "+self.mapping.reviewer.fullname
+      str = prefix+"_"+self.id.to_s
+    else
+      identifier = '<B>Metareview '+count.to_s+'</B>'
+      str = self.id.to_s
+    end    
+    code = identifier+'&nbsp;&nbsp;&nbsp;<a href="#" name= "metareview_'+str+'Link" onClick="toggleElement('+"'metareview_"+str+"','metareview'"+');return false;">hide metareview</a>'    
+    code += "<BR/><B>Last updated:</B> "
+    if self.updated_at.nil?
+      code += "Not available"
+    else
+      code += self.updated_at.strftime('%A %B %d %Y, %I:%M%p')
+    end   
+    code += '<div id="metareview_'+str+'" style=""><BR/><BR/>'         
+    questionnaire = self.mapping.assignment.questionnaires.find_by_type('MetareviewQuestionnaire')
+    questions = questionnaire.questions
+    scores = Array.new
+    questions.each{
+       | question |
+       score = Score.find_by_question_id_and_instance_id(question.id, self.id)
+       if score
+         scores << score
+       end
+    }
+    
+    scores.each{
+      | reviewScore |      
+      code = code + "<I>"+Question.find_by_id(reviewScore.question_id).txt+"</I><BR/><BR/>"
+      code = code + '(<FONT style="BACKGROUND-COLOR:gold">'+reviewScore.score.to_s+"</FONT> out of <B>"+Question.find_by_id(reviewScore.question_id).questionnaire.max_question_score.to_s+"</B>): "+reviewScore.comments+"<BR/><BR/>"
+    }  
+    if self.additional_comment != nil
+      comment = self.additional_comment.gsub('^p','').gsub(/\n/,'<BR/>&nbsp;&nbsp;&nbsp;')
+    else
+      comment = ''
+    end
+    code += "<B>Additional Comment:</B><BR/>"+comment+"</div>"
+    return code    
+  end
+  
+  def self.get_assessments_for(participant)
+    assessments = find(:all, :include => :mapping, :conditions => ['reviewee_id = ?',participant.id])
+    return assessments.sort {|a,b| a.mapping.reviewer.fullname <=> b.mapping.reviewer.fullname }    
+  end  
+  
+  # Computes the total score awarded for a metareview
+  def get_total_score
+    questionnaire = self.mapping.assignment.questionnaires.find_by_type('MetareviewQuestionnaire')
+    questions = questionnaire.questions
+    
+    total_score = 0
+    
+    questions.each{
+      | question |
+      item = Score.find_by_instance_id_and_question_id(self.id, question.id)
+      total_score += item.score      
+    }    
+    return total_score        
+  end
+
+  def self.get_metareivew_mapping
+    ReviewOfReviewMapping.find_by_id(self.review_of_review_mapping_id)
+  end
+
+  def delete
+    type_id = QuestionnaireType.find_by_name("Metareview").id
+    scores = Score.find_all_by_instance_id_and_questionnaire_type_id(self.id,type_id)
+    scores.each {|score| score.destroy}
+    self.destroy
+  end
+
+  # Generate emails for reviewers when a new review of their work is made
+  #ajbudlon, sept 07, 2007        
+  def email
+   review_of_review_mapping = ReviewOfReviewMapping.find_by_id(self.review_of_review_mapping_id)
+   review_mapping = ReviewMapping.find_by_id(review_of_review_mapping.review_mapping_id)
+      
+   if User.find_by_id(review_mapping.reviewer_id).email_on_review_of_review
+     review_id = review_of_review_mapping.review_id
+     review = Review.find(review_id)
+     
+     user = User.find(review_mapping.reviewer_id)
+     recipient = User.find_by_id(review_mapping.reviewer_id).email
+     Mailer.deliver_message(
+         {:recipients => recipient,
+          :subject => "A new review of review is available for #{self.name}",
+          :body => {
+           :obj_name => Assignment.find_by_id(review_mapping.assignment_id).name,
+           :type => "review of review",
+           :location => "Review "+get_review_number(review_of_review_mapping).to_s,
+           :review_scores => Score.find(:all, :conditions=>["instance_id=? and questionnaire_type_id=?",review.id, QuestionnaireType.find_by_name("Review").id]),
+           :ror_review_scores => self.review_of_review_scores,
+           :user_name => ApplicationHelper::get_user_first_name(user),
+           :partial_name => "update"
+          }
+         }
+        )         
+   end  
+  end
+  
+  # Get all review mappings for this assignment & reviewer
+  # required to give reviewer location of new submission content
+  # link can not be provided as it might give user ability to access data not
+  # available to them.  
+  #ajbudlon, sept 07, 2007       
+  def get_review_number(mapping)
+    reviewer_mappings = ReviewMapping.find_by_sql(
+      "select * from review_of_review_mappings where assignment_id = " +self.id.to_s +
+      " and review_id = " + mapping.review_id.to_s
+    )
+    review_num = 1
+    for rm in reviewer_mappings
+      if rm.reviewer_id != mapping.reviewer_id
+        review_num += 1
+      else
+        break
+      end
+    end  
+    return review_num
+  end  
+  
+ #Generate an email to the instructor when a new review exceeds the allowed difference
+ #ajbudlon, nov 18, 2008
+ def notify_on_difference(new_pct,avg_pct,limit)
+   instructor = User.find(self.mapping.assignment.instructor_id)  
+   Mailer.deliver_message(
+     {:recipients => instructor.email,
+      :subject => "Expertiza Notification: A metareview score is outside the acceptable range",
+      :body => {
+        :first_name => ApplicationHelper::get_user_first_name(instructor),
+        :reviewer_name => self.mapping.reviewer.fullname,
+        :type => "metareview",
+        :reviewee_name => self.mapping.reviewee.fullname,
+        :limit => limit,
+        :new_pct => new_pct,
+        :avg_pct => avg_pct,
+        :types => "metareviews",
+        :performer => "metareviewer",
+        :assignment => self.mapping.assignment,              
+        :partial_name => 'limit_notify'
+      }
+     }
+   )
+          
+ end   
+  
+end
---
+++ app/models/teammate_review.rb
@@ -0,0 +1,106 @@
+class TeammateReview < ActiveRecord::Base
+  has_many :teammate_review_scores
+  belongs_to :mapping, :class_name => "TeammateReviewMapping", :foreign_key => "mapping_id"
+  
+  # Computes the total score awarded for a  teammate review
+  def get_total_score
+    questionnaire = self.mapping.assignment.questionnaires.find_by_type('TeammateReviewQuestionnaire')
+    questions = questionnaire.questions
+    
+    total_score = 0
+    
+    questions.each{
+      | question |
+      item = Score.find_by_instance_id_and_question_id(self.id, question.id)
+      total_score += item.score      
+    }    
+    return total_score        
+  end 
+  
+  def self.get_assessments_for(participant)
+    assessments = find(:all, :include => :mapping, :conditions => ['reviewee_id = ?',participant.id])
+    return assessments.sort {|a,b| a.mapping.reviewer.fullname <=> b.mapping.reviewer.fullname }    
+  end  
+  
+  def display_as_html(prefix = nil, count = nil)
+    if prefix
+      identifier = "<B>Reviewer:</B> "+self.mapping.reviewer.fullname
+      str = prefix+"_"+self.id.to_s
+    else
+      identifier = '<B>Teammate Review '+count.to_s+'</B>'
+      str = self.id.to_s
+    end    
+    code = identifier+'&nbsp;&nbsp;&nbsp;<a href="#" name= "teammate_review_'+str+'Link" onClick="toggleElement('+"'teammate_review_"+str+"','teammate_review'"+');return false;">hide review</a><BR/>'           
+    code += "<BR/><B>Last updated:</B> "
+    if self.updated_at.nil?
+      code += "Not available"
+    else
+      code += self.updated_at.strftime('%A %B %d %Y, %I:%M%p')
+    end     
+        
+    code += '<div id="teammate_review_'+str+'" style=""><BR/><BR/>'
+    questionnaire = self.mapping.assignment.questionnaires.find_by_type('TeammateReviewQuestionnaire')
+    questions = questionnaire.questions
+    scores = Array.new
+    questions.each{
+       | question |
+       score = Score.find_by_question_id_and_instance_id(question.id, self.id)
+       if score
+         scores << score
+       end
+    }    
+
+    count = 0
+    scores.each{
+      | reviewScore |
+      count += 1
+      code += "<B>Question "+count.to_s+": </B><I>"+Question.find_by_id(reviewScore.question_id).txt+"</I><BR/><BR/>"
+      code += '&nbsp;&nbsp;&nbsp;(<FONT style="BACKGROUND-COLOR:gold">'+reviewScore.score.to_s+"</FONT> out of <B>"+Question.find_by_id(reviewScore.question_id).questionnaire.max_question_score.to_s+"</B>): "+reviewScore.comments.gsub("<","&lt;").gsub(">","&gt;")+"<BR/><BR/>"
+    }     
+    if self.additional_comment != nil
+      comment = self.additional_comment.gsub('^p','').gsub(/\n/,'<BR/>&nbsp;&nbsp;&nbsp;')
+    else
+      comment = ''
+    end
+    code += "<B>Additional Comment:</B><BR/>"+comment+"</div>"
+    return code
+  end   
+  
+  #return the reviewer for this review
+  def reviewer
+    self.mapping.reviewer.user
+  end
+  
+  #return the reviewee for this review
+  def reviewee
+    self.mapping.reviewee.user
+  end
+  
+ #Generate an email to the instructor when a new review exceeds the allowed difference
+ #ajbudlon, nov 18, 2008
+ def notify_on_difference(new_pct,avg_pct,limit)
+   mapping = TeammateReviewMapping.find(self.mapping_id)
+   instructor = User.find(mapping.assignment.instructor_id)  
+   Mailer.deliver_message(
+     {:recipients => instructor.email,
+      :subject => "Expertiza Notification: A review score is outside the acceptable range",
+      :body => {        
+        :first_name => ApplicationHelper::get_user_first_name(instructor),
+        :reviewer_name => mapping.reviewer.fullname,
+        :type => "review",
+        :reviewee_name => mapping.reviewee.fullname,
+        :limit => limit,
+        :new_pct => new_pct,
+        :avg_pct => avg_pct,
+        :types => "reviews",
+        :performer => "reviewer",
+        :assignment => mapping.assignment,    
+        :partial_name => 'limit_notify'
+      }
+     }
+   )
+          
+ end  
+  
+  
+end

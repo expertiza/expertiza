70f5fbb8bc37f2b6073b20fdb7ea54989d73cf8b
rkadanj <rkadanj@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2010-02-14 08:22:23 +0000

    Adding models related to signup sheet.
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@656 392d7b7b-3f31-0410-9dc3-c95a9635ea79

---
+++ app/models/sign_up_topic.rb
@@ -0,0 +1,74 @@
+class SignUpTopic < ActiveRecord::Base
+  
+  
+  def self.import(row,session,id = nil)
+
+      if row.length != 4
+          raise ArgumentError, "CSV File expects the format: Topic identifier, Topic name, Max choosers, Topic Category"
+      end      
+
+      topic = SignUpTopic.find_by_topic_name_and_assignment_id(row[1],session[:assignment_id])
+      
+      if topic == nil        
+        attributes = ImportTopicsHelper::define_attributes(row)
+        ImportTopicsHelper::create_new_sign_up_topic(attributes,session)
+      else
+        topic.max_choosers = row[2]
+        topic.topic_identifier = row[0]
+        #topic.assignment_id = session[:assignment_id]  
+        topic.save
+      end
+  end
+
+  def self.find_slots_filled(assignment_id)
+    SignUpTopic.find_by_sql("SELECT topic_id as topic_id, COUNT(t.max_choosers) as count FROM sign_up_topics t JOIN signed_up_users u ON t.id = u.topic_id WHERE t.assignment_id =" + assignment_id+  " and u.is_waitlisted = false GROUP BY t.id")    
+  end
+
+  def self.find_slots_waitlisted(assignment_id)
+    SignUpTopic.find_by_sql("SELECT topic_id as topic_id, COUNT(t.max_choosers) as count FROM sign_up_topics t JOIN signed_up_users u ON t.id = u.topic_id WHERE t.assignment_id =" + assignment_id +  " and u.is_waitlisted = true GROUP BY t.id")
+  end
+
+  def self.find_waitlisted_topics(assignment_id,creator_id)
+    SignedUpUser.find_by_sql("SELECT u.id FROM sign_up_topics t, signed_up_users u WHERE t.id = u.topic_id and u.is_waitlisted = true and t.assignment_id = " + assignment_id.to_s + " and u.creator_id = " + creator_id.to_s)
+  end
+
+  def self.slotExist?(topic_id)
+    topic = SignUpTopic.find(topic_id)
+    no_of_students_who_selected_the_topic = SignedUpUser.find_all_by_topic_id_and_is_waitlisted(topic_id, false)
+
+    if !no_of_students_who_selected_the_topic.nil?
+      if topic.max_choosers > no_of_students_who_selected_the_topic.size
+        return true
+      else
+        return false
+      end
+    else
+      return true
+    end
+  end
+
+
+  def self.cancel_all_waitlists(creator_id, assignment_id)
+    waitlisted_topics = SignUpTopic.find_waitlisted_topics(assignment_id,creator_id)
+    if !waitlisted_topics.nil?
+      for waitlisted_topic in waitlisted_topics
+        entry = SignedUpUser.find(waitlisted_topic.id)
+        entry.destroy
+      end
+    end
+
+  end
+
+  def update_waitlisted_users(max_choosers)
+    num_of_users_promotable = max_choosers.to_i - self.max_choosers.to_i
+
+    num_of_users_promotable.times {
+      next_waitlisted_user = SignedUpUser.find(:first, :conditions => {:topic_id => self.id, :is_waitlisted => true})
+      if !next_waitlisted_user.nil?
+        next_waitlisted_user.is_waitlisted = false
+        next_waitlisted_user.save
+      end
+    }
+  end
+
+end

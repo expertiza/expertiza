066581dc208302d8072d139bcf2b8de9e7ca7e8d
rkadanj <rkadanj@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2010-02-28 21:02:34 +0000

    Adding suggest and approve functionality.
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@680 392d7b7b-3f31-0410-9dc3-c95a9635ea79

---
+++ db/migrate/122_create_suggestion_comments.rb
@@ -0,0 +1,16 @@
+class CreateSuggestionComments < ActiveRecord::Migration
+  def self.up
+    create_table :suggestion_comments do |t|
+       t.column :id,            :int
+       t.column :comments,      :text
+       t.column :commenter,     :string
+       t.column :vote,          :string
+       t.column :suggestion_id, :int
+       t.column :created_at,    :datetime
+    end
+  end
+
+  def self.down
+    drop_table :suggestion_comments
+  end
+end

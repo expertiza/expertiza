6939a261f9d103ef36151b31d2623fd2ce8ae4ff
agummad <agummad@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2010-04-15 17:39:59 +0000

    leaderboard and review reports (modified).
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@692 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- app/controllers/response_controller.rb
+++ app/controllers/response_controller.rb
@@ -50,15 +50,14 @@ class ResponseController < ApplicationController
         score.update_attribute('score',v[:score])
         score.update_attribute('comments',v[:comment])
       end    
-   update_cache(@myid)
     rescue
       msg = "#{@map.get_title} was not saved."
     end
 
     begin
        ResponseHelper.compare_scores(@response, @questionnaire)
-   #   ScoreCache.update_cache(@response.id)
-       update_cache(@myid)
+    ScoreCache.update_cache(@response.id)
+    #  update_cache(@myid)
       msg = "#{@map.get_title} was successfully saved -- #{@myid}."
     rescue
       msg = "An error occurred while saving the response: "+$!
@@ -109,7 +108,7 @@ class ResponseController < ApplicationController
     
     #begin
       ResponseHelper.compare_scores(@response, @questionnaire)
-      update_cache(@res)
+     ScoreCache.update_cache(@res)
       msg = "#{@map.get_title} was successfully saved."
     #rescue
     #  @response.delete
@@ -148,158 +147,6 @@ class ResponseController < ApplicationController
     @min = @questionnaire.min_question_score
     @max = @questionnaire.max_question_score     
   end
-def update_cache(rid)
- 
-  presenceflag = 0
-   @ass_id = 0
-   @userset = []
-   @team = 0
-   @team_number = 0
-   @teamass = 0
-   @reviewmap = Response.find(rid).map_id
-   @rm = ResponseMap.find(@reviewmap)
-   @participant1 = AssignmentParticipant.new
-    @the_object_id = 90
-    @map_type = @rm.type.to_s
-    @t_score = 0
-    @t_min = 0
-    @teammember = TeamsUser.new
-    @t_max = 0
-    @myfirst = "before"
-    
-   
-    
-    #if (@map_type == "ParticipantReviewResponseMap")
-      if(@map_type == "TeamReviewResponseMap")
-                @ass_id = @rm.reviewed_object_id
-                @assignment1 = Assignment.find(@ass_id)
-                @teammember =  TeamsUser.find(:first, :conditions => ["team_id = ?",@rm.reviewee_id])
-                @participant1 = AssignmentParticipant.find(:first, :conditions =>["user_id = ? and parent_id = ?", @teammember.user_id, @ass_id])
-                @the_object_id = @teammember.team_id
-       
-     else
-                @participant1 = AssignmentParticipant.find(@rm.reviewee_id)
-                @the_object_id = @participant1.id
-                @assignment1 = Assignment.find(@participant1.parent_id)
-                @ass_id = @assignment1.id
-
- 
-     end
-        
-            
-            
-    
-    
-            @questions = Hash.new    
-            questionnaires = @assignment1.questionnaires
-            questionnaires.each{
-                     |questionnaire|
-                     @questions[questionnaire.symbol] = questionnaire.questions
-              } 
-           @allscores = @participant1.get_scores( @questions)
-             
-            @scorehash = get_my_scores(@allscores, @map_type) 
-          
-            
-                @p_score = @scorehash[:avg]               
-                @p_min = @scorehash[:min]
-                @p_max = @scorehash[:max]
-            
-            sc = ScoreCache.find(:first,:conditions =>["assignment_id = ? and object_id = ? and object_type = ?", @ass_id , @the_object_id, @map_type ])
-          if ( sc == nil)
-               presenceflag = 1
-               @msgs = "first entry"
-                sc = ScoreCache.new
-                sc.object_id = @the_object_id
-                sc.assignment_id = @ass_id
-                sc.range = @p_min.to_s + "-" + @p_max.to_s
-                 sc.score = @p_score
-                if @thiscourse != nil
-                  sc.course_id = @thiscourse.id
-                end
-                sc.object_type = @map_type                        
-                
-                sc.save
-            # make another new tuple for new score
-            else
-              if @thiscourse != nil
-                  sc.course_id = @thiscourse.id
-               end
-                sc.range = @p_min.to_s + "-" + @p_max.to_s
-                sc.score = @p_score
-                presenceflag = 2
-                sc.update
-            #look for a consolidated score and change
-            end               
- 
-    
-    
-    
-    
-    #########################
-    end
-    
-    
- 
-    
-    
-  
-  
-  def get_my_scores( scorehash, map_type)
-    
-     @p_score = 0
-     @p_min = 0  
-     @p_max = 0
-     
-#  ParticipantReviewResponseMap - Review mappings for single user assignments
-#  TeamReviewResponseMap - Review mappings for team based assignments
-#  MetareviewResponseMap - Metareview mappings
-#  TeammateReviewResponseMap - Review mapping between teammates
-#  FeedbackResponseMap - Feedback from author to reviewer
- 
- 
-     if(map_type == "ParticipantReviewResponseMap")
-            
-            if (scorehash[:review])
-                @p_score = scorehash[:review][:scores][:avg]               
-                @p_min = scorehash[:review][:scores][:min]
-                @p_max = scorehash[:review][:scores][:max]
-            end
-      elsif (map_type == "TeamReviewResponseMap")
-           if (scorehash[:review])
-                @p_score = scorehash[:review][:scores][:avg]               
-                @p_min = scorehash[:review][:scores][:min]
-                @p_max = scorehash[:review][:scores][:max]
-            end
- 
-        elsif (map_type == "TeammateReviewResponseMap")
-           if (scorehash[:review])
-                @p_score = scorehash[:teammate][:scores][:avg]               
-                @p_min = scorehash[:teammate][:scores][:min]
-                @p_max = scorehash[:teammate][:scores][:max]
-            end
-    
-      elsif (map_type == "MetareviewResponseMap")
-            if (scorehash[:metareview])
-                @p_score = scorehash[:metareview][:scores][:avg]               
-                @p_min = scorehash[:metareview][:scores][:min]
-                @p_max = scorehash[:metareview][:scores][:max]
-            end
-      elsif (map_type == "FeedbackResponseMap")
-         if (scorehash[:feedback])
-                @p_score = scorehash[:feedback][:scores][:avg]               
-                @p_min = scorehash[:feedback][:scores][:min]
-                @p_max = scorehash[:feedback][:scores][:max]
-            end
-       end 
-     @scoreset = Hash.new
-     @scoreset[:avg] = @p_score
-     @scoreset[:min] = @p_min
-     @scoreset[:max] = @p_max
-     return @scoreset
-  end
-  
-   
     
   
     

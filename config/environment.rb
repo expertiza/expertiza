53279fc6471994b80c51d73c8bebd943f5b0dfdf
nilbus <nilbus@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2010-10-05 16:58:17 +0000

    Keep windows in mind for graphviz dependency checking
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/trunk@787 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- config/environment.rb
+++ config/environment.rb
@@ -21,7 +21,7 @@ Rails::Initializer.run do |config|
   config.gem 'RedCloth'
   config.gem 'rgl', :lib => 'rgl/adjacency'
   config.gem 'rubyzip', :lib => "zip/zip"
-  raise 'graphviz dependency is not installed - missing dot executable' unless %x(which dot).any?
+  raise 'graphviz dependency is not installed - missing dot executable' unless RUBY_PLATFORM =~ /mswin/ or %x(which dot).to_s.any?
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named

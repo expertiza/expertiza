12b8eabd74f947cceb04ee35f552ca89b7d75916
albierin <albierin@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2010-10-17 16:17:41 +0000

    Team: OSS project_Team1 Project #3
    Edit suggestions implemented for students and instructors.
    View comments implemented for students.
    
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/projectA/trunk@804 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -4,5 +4,28 @@ class Suggestion < ActiveRecord::Base
    
    def find_all_by_assignment_id(assignment_id)
       find(:all, :conditions => ["assignment_id = ?", assignment_id])
-   end
+  end
+  
+    # Generate emails for reviewers when new content is available for review
+  #ajbudlon, sept 07, 2007   
+  def email(user_id, editor) 
+  
+    # Get all review mappings for this assignment & author
+    user = User.find(user_id)
+  
+     if user.email_on_review
+        Mailer.deliver_message(
+          {:recipients => user.email,
+           :subject => "Suggested topic has been edited by #{editor}",
+           :body => {
+            :obj_name => self.title,
+            :type => "suggestion",
+            :location => self.id,
+            :first_name => ApplicationHelper::get_user_first_name(user),
+            :partial_name => "update"
+           }
+          }
+        )
+     end
+  end 
 end

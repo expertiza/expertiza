84eab24be87fc53b1d190ab8df6d51e299d6fc0c
mafashin <mafashin@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2010-10-21 17:45:49 +0000

    E6: Merged trunk changes r800:839 into projectA/trunk
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/projectA/trunk@840 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- vendor/plugins/goldberg_filters/lib/goldberg_filters.rb
+++ vendor/plugins/goldberg_filters/lib/goldberg_filters.rb
@@ -39,7 +39,9 @@ module GoldbergFilters
         public_role = Role.find(@settings.public_role_id)
         if not public_role or not public_role.cache or 
             not public_role.cache[:credentials] or 
-            not public_role.cache[:menu]
+            not public_role.cache[:menu] or
+            public_role.cache[:credentials].is_a?(YAML::Object) or
+            public_role.cache[:menu].is_a?(YAML::Object)
           Role.rebuild_cache
           public_role = Role.find(@settings.public_role_id)
         end

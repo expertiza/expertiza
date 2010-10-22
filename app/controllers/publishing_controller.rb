0ee446e29cf35fa893f0f35571c49dfae4cbbf1b
sdnellen <sdnellen@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2010-10-22 15:23:24 +0000

    E13 publishing rights and digital signature checkin
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/projectB/trunk@860 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- app/controllers/publishing_controller.rb
+++ app/controllers/publishing_controller.rb
@@ -1,26 +1,79 @@
+require 'digital_sig'
+
 class PublishingController < ApplicationController
   
   def view   
     @participants = AssignmentParticipant.find_all_by_user_id(session[:user].id)
   end
   
-  def set_master_publish_permission
-    session[:user].update_attribute('master_permission_granted',params[:id])    
+  def set_master_publish_permission        
+    # if digital sig is verified then change settings
+    if session[:dsig] 
+      session[:user].update_attribute('master_permission_granted',params[:set_master])    
+      session[:user].update_attribute('permission_updated_at',DateTime.now)  
+      session[:user].update_attribute('digital_signature',session[:dsig])  
+    else
+      flash[:error] = 'You need to digitally sign before changing rights.'      
+    end
     redirect_to :action => 'view'
   end
   
   def set_publish_permission
-    participant = AssignmentParticipant.find(params[:id])
-    participant.update_attribute('permission_granted',params[:allow])  
+    # if digital sig is verified then change settings
+    if session[:dsig] 
+      participant = AssignmentParticipant.find(params[:id])
+      participant.update_attribute('permission_granted',params[:allow])  
+      participant.update_attribute('permission_updated_at',DateTime.now)    
+      participant.update_attribute('digital_signature',session[:dsig])  
+    else
+      flash[:error] = 'You need to digitally sign before changing rights.'      
+    end
     redirect_to :action => 'view'
   end  
   
   def update_publish_permissions
-    participants = AssignmentParticipant.find_all_by_user_id(session[:user].id)
-    participants.each{
-      | participant |
-      participant.update_attribute('permission_granted',params[:allow])  
-    }    
+    # if digital sig is verified then change settings
+    if session[:dsig] 
+      participants = AssignmentParticipant.find_all_by_user_id(session[:user].id)
+      participants.each{
+        | participant |
+        participant.update_attribute('permission_granted',params[:allow])  
+        participant.update_attribute('permission_updated_at',DateTime.now)    
+        participant.update_attribute('digital_signature',session[:dsig])  
+      }    
+    else
+      flash[:error] = 'You need to digitally sign before changing rights.'      
+    end
     redirect_to :action => 'view'
   end
+  
+  def get_private_key
+     @username = session[:user].name
+  end
+  
+  def validate_key
+    ck_user = User.find_by_id(session[:user].id)
+    
+    # if no certificate set for user then generate
+    if ck_user.certificate.nil?
+      ck_user.gen_keys_and_certificate
+    end 
+    #puts "#{params[:private_key]}"
+    # generate dig signature and validate using certificate
+    pkey1 = params[:private_key]
+    pkey1.gsub!(/\r/,"")
+    
+    begin
+      # allow bypass of key entry for debug
+      if pkey1 =~ /test bypass/
+        session[:dsig] = "DEBUG: SIGNATURE TEST BYPASSED"
+      else  
+        session[:dsig]= DigitalSig.gen_digital_signature("MASTER", ck_user.name, pkey1, ck_user.certificate)
+      end  
+    rescue
+      session[:dsig]= nil
+    end  
+    redirect_to :action => 'view'
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -104,6 +104,9 @@ class UsersController < ApplicationController
         if @user.role.name == "Instructor" or @user.role.name == "Administrator"
           AssignmentQuestionnaires.create(:user_id => @user.id)
         end
+        # user sucessfully created, so generate info for digital signing
+        @user.gen_keys_and_certificate
+        
         flash[:notice] = 'User was successfully created.'
         redirect_to :action => 'list'
       else

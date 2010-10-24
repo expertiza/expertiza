e088a193693eec1415164e461d897249d29aa145
gchandr <gchandr@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2010-10-24 19:07:56 +0000

    E11: db migration for code review
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/projectA/trunk@939 392d7b7b-3f31-0410-9dc3-c95a9635ea79

---
+++ db/migrate/20101016233655_create_code_reviews.rb
@@ -0,0 +1,19 @@
+class CreateCodeReviews < ActiveRecord::Migration
+  def self.up
+    create_table :code_reviews do |t|
+      t.text :title
+      t.text :changes
+      t.integer :files_uploaded
+      t.timestamps :last_modified
+      t.timestamps
+    end
+    
+    add_column :participants, :code_review_id, :integer
+    execute "INSERT INTO site_controllers(name, permission_id, builtin) values('code_review', 8, 0);"
+
+  end
+
+  def self.down
+    drop_table :code_reviews
+  end
+end

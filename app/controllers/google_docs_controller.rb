98f148bfbc6c4c0488b65f0d2cbbdee145ec24b1
gpascar <gpascar@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2010-11-01 22:11:35 +0000

    E12: Added comments to related files (gpascar)
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/projectA/trunk@1016 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- app/controllers/google_docs_controller.rb
+++ app/controllers/google_docs_controller.rb
@@ -1,28 +1,51 @@
 class GoogleDocsController < ApplicationController
-    helper :google_docs
-    include GoogleDocsHelper
-    helper_method :gdocs_authenticated
+  helper :google_docs
+  include GoogleDocsHelper
+  helper_method :gdocs_authenticated
 
+  # Indicates whether a user is authenticated to use Google Docs APIs
+  # or not.  This is accomplished by checking the session for specific
+  # Google Docs tokens.
   def gdocs_authenticated
     if ! session[:google_docs_token] && params[:token]
-      puts "No session token, but there was a param token #{params[:token]}"
+      #puts "No session token, but there was a param token #{params[:token]}"
+
+      # Create a new client
       client = GData::Client::DocList.new
-      client.authsub_token = params[:token] # extract the single-use token from the URL query params
+
+      # Set the token to the one from the session
+      # extract the single-use token from the URL query params
+      client.authsub_token = params[:token]
+
+      # Upgrade the single user token to a full session one
       session[:google_docs_token] = client.auth_handler.upgrade()
-      puts "Upgraded token to a session token #{session[:google_docs_token]}"
+
+      #puts "Upgraded token to a session token #{session[:google_docs_token]}"
     end
 
-    puts "Returning session token #{session[:google_docs_token]}"
+    #puts "Returning session token #{session[:google_docs_token]}"
     return session[:google_docs_token] != nil
   end
 
+  # Return a list of documents via the Google Docs APIs.
   def documents
+    # Check that we are authenticated
     if ( gdocs_authenticated )
+      # Create a new client
       client = GData::Client::DocList.new
+
+      # Set the token from the session
       client.authsub_token = session[:google_docs_token]
+
+      # Point the client at the document feed
       feed = client.get('http://docs.google.com/feeds/documents/private/full').to_xml
+
+      # Create a new Array to return
       docs = Array.new
+
+      # Iterate over each element within the result set
       feed.elements.each('entry') do |entry|
+
         # Extract the href value from each <atom:link>
         links = {}
         entry.elements.each('link') do |link|
@@ -33,14 +56,23 @@ class GoogleDocsController < ApplicationController
             links[link.attribute('rel').value] = link.attribute('href').value
           end
         end
+
+        # This is future code for when the Google Docs APIs promotes v3
+        # outside of Google Labs (eg out of beta).
         revisions = Array.new
         #revisions = get_revisions(entry.elements['gd:resourceId'].text)
+        
+        # Set the default document type to generic, just in case we can't
+        # figure it out.  After that iterate over each category element
+        # looking for the 'kind' scheme.
         document_type = 'generic'
         entry.elements.each('category') do |category|
           if category.attribute('scheme').value.eql? "http://schemas.google.com/g/2005#kind"
             document_type = category.attribute('label').value
           end
         end
+
+        # Append the current document structure to the result
         docs << {
           :id => entry.elements['id'].text,
           :title => entry.elements['title'].text, 
@@ -67,13 +99,13 @@ class GoogleDocsController < ApplicationController
 
   # Probably shouldn't be in the controller...
   # This method will only work with Google Docs Protocol 3.0 or higher.  This
-  # is currently in Google Labs and not available.
+  # is currently in Google Labs and not generally available.
   def get_revisions(resourceId)
     revisions = Array.new
     if ( gdocs_authenticated )
       client = GData::Client::DocList.new
       client.authsub_token = session[:google_docs_token]
-	  puts "Requesting: http://docs.google.com/feeds/default/private/full/#{resourceId}/revisions"
+      puts "Requesting: http://docs.google.com/feeds/default/private/full/#{resourceId}/revisions"
       feed = client.get("http://docs.google.com/feeds/default/private/full/#{resourceId}/revisions").to_xml
       feed.elements.each('entry') do |entry|
         revisions << {
--- app/helpers/google_docs_helper.rb
+++ app/helpers/google_docs_helper.rb
@@ -1,25 +1,42 @@
 module GoogleDocsHelper
   require 'gdata'
 
+  # Indicates whether a user is authenticated to use Google Docs APIs
+  # or not.  This is accomplished by checking the session for specific
+  # Google Docs tokens.
   def gdocs_authenticated
     if ! session[:google_docs_token] && params[:token]
-	  puts "No session token, but there was a param token #{params[:token]}"
+      #puts "No session token, but there was a param token #{params[:token]}"
+
+      # Create a new client
       client = GData::Client::DocList.new
-      client.authsub_token = params[:token] # extract the single-use token from the URL query params
+
+      # Set the token to the one from the session
+      # extract the single-use token from the URL query params
+      client.authsub_token = params[:token]
+
+      # Upgrade the single user token to a full session one
       session[:google_docs_token] = client.auth_handler.upgrade()
-	  puts "Upgraded token to a session token #{session[:google_docs_token]}"
+
+      #puts "Upgraded token to a session token #{session[:google_docs_token]}"
     end
 
-	puts "Returning session token #{session[:google_docs_token]}"
+    #puts "Returning session token #{session[:google_docs_token]}"
     return session[:google_docs_token] != nil
   end
 
+  # Generate a link a view can use to authenticate Expertiza with the
+  # Google Docs API
   def gdocs_authentication_link
+    # Create a new client
     client = GData::Client::DocList.new
+
     #domain = 'example.com'  # force users to login to a Google Apps hosted domain
     next_url = request.url
     secure = false  # set secure = true for signed AuthSub requests
     sess = true
+
+    # Generate the link
     authsub_link = client.authsub_url(next_url, secure, sess)
 
     return authsub_link

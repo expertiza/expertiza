7fb405fe91ee17b8c9aa5f42869e557733abbee6
rsjohns3 <rsjohns3@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2010-11-02 02:37:20 +0000

    E3: Add comments to db:migration add_topic_description_to_sign_up_topics.rb
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/projectA/trunk@1020 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- db/migrate/20101022024052_add_topic_description_to_sign_up_topics.rb
+++ db/migrate/20101022024052_add_topic_description_to_sign_up_topics.rb
@@ -1,4 +1,9 @@
 class AddTopicDescriptionToSignUpTopics < ActiveRecord::Migration
+# OSS project_Team1 (rsjohns3) CSC517 Fall 2010
+# This migration adds topic_description column to sign_up_topics table
+# to allow topic description from suggestions to be preserved when
+# suggestions are approved and turned into official signup topics
+#
   def self.up
     add_column :sign_up_topics, :topic_description, :string
   end

bcbf9ddeb265bcd9d831b8070c26dd62d5f026c8
rsjohns3 <rsjohns3@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2010-11-02 02:35:45 +0000

    Add comments to db:migration add_created_date_to_suggestion.rb
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/projectA/trunk@1019 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- db/migrate/20101019022324_add_created_date_to_suggestion.rb
+++ db/migrate/20101019022324_add_created_date_to_suggestion.rb
@@ -1,4 +1,8 @@
 class AddCreatedDateToSuggestion < ActiveRecord::Migration
+# OSS project_Team1 (rsjohns3) CSC517 Fall 2010
+# This migration adds a createdDate column to the Suggestions table
+# to allow student suggestions to be sorted by createdDate
+#  
   def self.up
     add_column :suggestions, :createdDate, :datetime
   end

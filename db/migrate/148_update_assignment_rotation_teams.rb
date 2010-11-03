6348c95e12f01af93f95484b46edf555bba55900
narumug3 <narumug3@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2010-11-03 00:09:15 +0000

    narumug3, lagrawa-Change for Team Rotation Constraint (CSC517-2010) -Database Update Table script to update assignment table to accommodate new fields for team rotation constraints
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/projectB/trunk@1026 392d7b7b-3f31-0410-9dc3-c95a9635ea79

---
+++ db/migrate/148_update_assignment_rotation_teams.rb
@@ -0,0 +1,26 @@
+class UpdateAssignmentRotationTeams < ActiveRecord::Migration
+  def self.up
+    #rotationCondition 0=> rotation not required, 1 => course wise, 2 => course + category wise
+    execute "ALTER TABLE assignments
+              ADD COLUMN rotation_condition INT"
+    execute "ALTER TABLE assignments
+              ADD COLUMN category_id INT"
+    execute "ALTER TABLE assignments
+              ADD COLUMN max_allowed_rotation INT"
+    execute "ALTER TABLE assignments
+              ADD CONSTRAINT fk_category_id
+              FOREIGN KEY (category_id) REFERENCES categories(id)"  
+  end  
+
+  def self.down
+    execute "ALTER TABLE assignments
+              DROP FOREIGN KEY fk_category_id"
+    execute "ALTER TABLE assignments
+              REMOVE COLUMN max_allowed_rotation"
+    
+    execute "ALTER TABLE assignments
+              REMOVE COLUMN rotation_condition"
+    execute "ALTER TABLE assignments
+              REMOVE COLUMN category_id"
+  end
+end

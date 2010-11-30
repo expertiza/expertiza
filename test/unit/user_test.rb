0648d32f0b1011fddcd23612bab0cb0260c49de8
rsjohns3 <rsjohns3@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2010-11-30 06:12:21 +0000

    E09: Correct Unit tests in user_test.rb (CSC517-601 rsjohns3 11/30/2010)
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/projectA/trunk@1186 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -3,32 +3,35 @@ require File.dirname(__FILE__) + '/../test_helper'
 class UserTest < Test::Unit::TestCase
   
   # Test user retrieval by email
-  def test_get_by_login_email
-    user = User.get_by_login('ajbudlon@ncsu.edu')    
-    assert_equal 'ajbudlon', user.name
+  def test_find_by_login_email
+    user = User.find_by_login('student1@foo.edu')    
+    assert_equal 'student1', user.name
   end
   
   # Test user retrieval by name
-  def test_get_by_login_name
-    user = User.get_by_login('ajbudlon@ncsu.edu')    
-    assert_equal 'ajbudlon', user.name
+  def test_find_by_login_name
+    user = User.find_by_login('student1')    
+    assert_equal 'student1', user.name
   end
 
   # 101 add a new user 
   def test_add_user
-    user = User.new(:name => "testStudent1",
-                    :password => Digest::SHA1.hexdigest("test"),
-                    :fullname => "test Student 1",
-                    :role_id => "1")
+    user = User.new
+    user.name = "testStudent1"
+    user.fullname = "test_Student_1"
+    user.password = Digest::SHA1.hexdigest("testStudent1")
+    user.email = "testStudent1@foo.edu"
+    user.role_id = "1"
     assert user.save
   end 
   
   # 102 Add a user with existing name 
   def test_add_user_with_exist_name
-    user = User.new(:name => users(:admin1).name,
-                    :password => Digest::SHA1.hexdigest("test"),
-                    :fullname => "test admin 1",
-                    :role_id => "3")
+    user = User.new
+    user.name = 'student1'
+    user.password = Digest::SHA1.hexdigest("student1")
+    user.fullname = "student1_fullname",
+    user.role_id = "3"
     assert !user.save
     assert_equal ActiveRecord::Errors.default_error_messages[:taken], user.errors.on(:name)
   end
@@ -42,14 +45,14 @@ class UserTest < Test::Unit::TestCase
   end
   # 202 edit a user name to an invalid name (e.g. blank)
   def test_update_user_with_invalid_name
-    user = users(:test1)
+    user = User.find_by_login('student1')
     user.name = "";
     assert !user.valid?
   end
   # 203 Change a user name to an existing name.
   def test_update_user_with_existing_name
-    user = users(:test1)
-    user.name = users(:admin1).name;
+    user = User.find_by_login('student1')
+    user.name = "student2"
     assert !user.valid?
   end  
 end

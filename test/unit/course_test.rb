280a52d54b788e01e617fdecd3732acf9479416f
jmfoste2 <jmfoste2@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2010-12-01 22:48:44 +0000

    E09: Unit test changes for E09
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/projectA/trunk@1226 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- test/unit/course_test.rb
+++ test/unit/course_test.rb
@@ -4,21 +4,21 @@ class CourseTest < Test::Unit::TestCase
   fixtures :courses
 
   def setup
-    @course = Course.find(1)
+    @course = courses(:course1)
   end
   
   def test_retrieval
     assert_kind_of Course, @course
-    assert_equal "My Course", @course.name
-    assert_equal 1, @course.id
-    assert_equal 1, @course.instructor_id
-    assert_equal 'abc123', @course.directory_path
-    assert_equal 'This is info', @course.info
+    assert_equal "CSC111", @course.name
+    assert_equal courses(:course1).id, @course.id 
+    assert_equal courses(:course1).instructor_id, @course.instructor_id
+    assert_equal 'csc111', @course.directory_path
+    assert_equal 'CSC111 Programming Class', @course.info
     assert @course.private
   end
  
   def test_update
-    assert_equal "My Course", @course.name
+    assert_equal "CSC111", @course.name
     @course.name = "Object-Oriented"
     @course.save
     @course.reload

e2838023a6606b4df5771d1e7b97a20b1bc56e7b
jmfoste2 <jmfoste2@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2010-12-01 22:48:29 +0000

    E09: Functional test changes for E09
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/projectA/trunk@1225 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- test/functional/admin_controller_test.rb
+++ test/functional/admin_controller_test.rb
@@ -1,5 +1,6 @@
-require File.dirname(__FILE__) + '/../test_helper'
-require 'admin_controller'
+#require File.dirname(__FILE__) + '/../test_helper'
+#require 'admin_controller'
+require 'test_helper'
 
 # Re-raise errors caught by the controller.
 class AdminController; def rescue_action(e) raise e end; end

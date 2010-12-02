86e64f396733ec48b4878880c0c53a0db1e7d58e
rsjohns3 <rsjohns3@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2010-12-02 02:04:20 +0000

    E09: Remove unneeded assert tests. Leave assert test for field 'name' to allow validate_presence_of test to be performed (csc517-601 rsjohns3 12/01/2010)
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/projectA/trunk@1230 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- test/unit/assignment_test.rb
+++ test/unit/assignment_test.rb
@@ -13,32 +13,12 @@ class AssignmentTest < Test::Unit::TestCase
     assignment = Assignment.new
     # Assignment should not be valid, because some fields have not been created.
     assert !assignment.valid?
-    # These two fields have been created, so they should be invalid.
-    assert assignment.errors.invalid?(:name)
-    assert assignment.errors.invalid?(:directory_path)
+    # Intentionally set name to nothing to test validates_presence_of rsjohns3
+    assignment.name = ""
+    assert !assignment.valid?    
     
     # Submitter count is initialized to 0 by the controller.
     assert_equal assignment.submitter_count, 0
-    # assert_equal assignment.instructor_id, (session[:user]).id
-
-    # The following fields have not been set yet.
-    assert assignment.errors.invalid?(:review_questionnaire_id)
-    assert assignment.errors.invalid?(:review_of_review_questionnaire_id)
-    assert assignment.errors.invalid?(:review_weight)
-    assert assignment.errors.invalid?(:reviews_visible_to_all)
-    assert assignment.errors.invalid?(:team_assignment)
-    assert assignment.errors.invalid?(:wiki_assignment_id)
-    assert assignment.errors.invalid?(:require_signup)
   end
-
-    # Instructor_id is initialized to the current user by the controller ... needs to be checked by a functional test.
-    
-    #assert assignment.errors.invalid?(:course_id)
-    #assert assignment.errors.invalid?(:private)
-    #assert assignment.errors.invalid?(:num_reviewers)
-    #assert assignment.errors.invalid?(:num_review_of_reviewers)
-    #assert assignment.errors.invalid?(:review_strategy_id)
-    #assert assignment.errors.invalid?(:mapping_strategy_id)
-
 
 end

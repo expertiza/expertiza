bc7c6b6167d8fdbb70de36f2ee7288b2e6d549df
Edward Anderson <nilbus@nilbus.com>
2011-01-21 22:19:57 -0500

    Remove User.signed_up_users association: creator_id can be a team_id or a user_id; not always a user_id

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ActiveRecord::Base
   has_many :participants, :class_name => 'Participant', :foreign_key => 'user_id', :dependent => :destroy
   # FIXME:          :class_name should be AssignmentParticipant, probably. In most cases it's used that way. But all?
   has_many :assignments, :through => :participants
-  has_many :signed_up_users, :foreign_key => 'creator_id', :dependent => :destroy
   
   belongs_to :parent, :class_name => 'User', :foreign_key => 'parent_id'
   

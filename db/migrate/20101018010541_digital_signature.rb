99306ba1b1fdeb5b2a4b2f849616869bc0ddba4d
Edward Anderson <nilbus@nilbus.com>
2011-01-21 19:38:21 -0500

    Use rails migration helpers instead of executing SQL in 20101018010541_digital_signature migration

--- db/migrate/20101018010541_digital_signature.rb
+++ db/migrate/20101018010541_digital_signature.rb
@@ -1,20 +1,13 @@
 class DigitalSignature < ActiveRecord::Migration
   def self.up
-    begin
-    execute "ALTER TABLE users
-              ADD COLUMN digital_certificate VARCHAR(1000)"
-    execute "ALTER TABLE participants
-              ADD COLUMN time_stamp DATETIME"  
-    execute "ALTER TABLE participants
-              ADD COLUMN digital_signature LONGTEXT"  
-    rescue
-      put $!
-    end    
+    add_column :users, :digital_certificate, :text
+    add_column :participants, :time_stamp, :datetime
+    add_column :participants, :digital_signature, :text
   end
 
   def self.down
-      remove_column :users, :digital_certificate
-      remove_column :participants, :time_stamp
-      remove_column :participants, :digital_signature
+    remove_column :users, :digital_certificate
+    remove_column :participants, :time_stamp
+    remove_column :participants, :digital_signature
   end
 end

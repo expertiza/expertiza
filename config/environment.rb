0343695dfffce861dfb47fa8f643e99e5141cfaa
Edward Anderson <edward.anderson@scimedsolutions.com>
2011-07-02 20:15:07 -0400

    Switch to Rails 2.3.11

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.10'
+RAILS_GEM_VERSION = '2.3.11'
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')

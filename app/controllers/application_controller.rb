6fabf5779db24eb2d5a774e9f2f92a71b96363ca
Edward Anderson <edward.anderson@scimedsolutions.com>
2011-08-20 13:27:01 -0400

    Update tests for 2.3.14 and Authlogic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@
 class ApplicationController < ActionController::Base
 
   helper_method :current_user_session, :current_user
-  protect_from_forgery
+  protect_from_forgery unless Rails.env.test?
   filter_parameter_logging :password, :password_confirmation, :clear_password, :clear_password_confirmation
 
   def authorize 

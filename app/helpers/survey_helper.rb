99b6bb8bc0454ead8ab163bd05ba96cd60a93891
Ed Gehringer <efg@ncsu.edu>
2011-09-17 13:05:34 -0400

    Fixed SurveyHelper to use current questionnaire typenames. This does not fix Expertiza surveys, which have not been used in a few years, but it does prevent getting an error when trying to view survey results for an assignment that has no surveys.

--- app/helpers/survey_helper.rb
+++ app/helpers/survey_helper.rb
@@ -5,13 +5,13 @@ module SurveyHelper
       assigned_surveys = []
       for joiner in joiners
         survey = Questionnaire.find(joiner.questionnaire_id)
-        assigned_surveys << survey if survey.type_id == 2
+        assigned_surveys << survey if survey.type == 'SurveyQuestionnaire'
       end
       assigned_surveys.sort!{|a,b| a.name <=> b.name} 
     end
 
   def self.get_global_surveys
-      global_surveys = Questionnaire.find(:all, :conditions => ["type_id = ? and private = ?", 3, false])
+      global_surveys = Questionnaire.find(:all, :conditions => ["type = ? and private = ?", 'GlobalSurveyQuestionnaire', false])
       global_surveys.sort!{|a,b| a.name <=> b.name} 
    end
 

f833d6c73d8fe3b3dce04d4247edd765126edaa1
Matthew Banz <matt.banz@gmail.com>
2011-10-25 23:48:11 -0600

    Moved code to the model and removed unused code

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -1,139 +1,24 @@
 class TeamController < ApplicationController
- auto_complete_for :user, :name  
-
-   ############## These methods created by mahesh############
- def And(number1, number2)
-   #produces bitwise and of two integers
-   return number1&number2
- end
- 
- def score(team)
-   #for each student in team 
-   #result += And(studnet, nextstudnet)
-   #dec2bin(result)
-   #return sum of 1's in dec2bin(result) 
- end
- 
- def min(a,b)
-   if a<b
-     return a   
-   else
-     return b
-   end
- end
+ auto_complete_for :user, :name
  
 def create_teams_view
  @parent = Object.const_get(session[:team_type]).find(params[:id])
- 
 end
 
 def delete_all
-  
-#  parent = Object.const_get(session[:team_type]).find(params[:id])
-#  redirect_to :action => 'list' , :id => parent.id
-  
-#  participants = Participant.find(:all, :conditions => ["parent_id=?",parent.id])
-#  no_of_teams = (participants.length)/(parent.team_count)
-#  diff = (participants.length)%(parent.team_count)
-  
-
-    #no_of_teams ++
-
-  
-#
-#   for i in 1..no_of_teams
-#   team = Team.find_by_name("Team #{i}")
- #  team.delete
-#   end
-
   parent = Object.const_get(session[:team_type]).find(params[:id])  
-  teams = Team.find(:all, :conditions => ["parent_id=?",parent.id])
+  teams = Team.find(:all, :conditions => ["parent_id=?", parent.id])
   
   for team in teams
     team.delete
   end
-redirect_to :action => 'list', :id => parent.id
 
+  redirect_to :action => 'list', :id => parent.id
 end
-
-
-
-def randomize_teams
-  
-   parent = Object.const_get(session[:team_type]).find(params[:id])  
-   participants = Participant.find(:all, :conditions => ["parent_id=?",parent.id])
-   
-   participants = participants.sort{rand(3) - 1 }
-   
-   no_of_teams = (participants.length)/(parent.team_count)
-   diff = (participants.length)%(parent.team_count)
-   
-   i=0
-   j=0
-   k=0
-   
-   
-         
-  for i in 1..no_of_teams
-   begin
-     check_for_existing_team_name(parent,"Team #{i}")
-     
-   rescue TeamExistsError
-   team = Team.find_by_name("Team #{i}")
-   team.delete
-     
-   end
-     team = Object.const_get(session[:team_type]+'Team').create(:name => "Team #{i}", :parent_id => parent.id)
-     TeamNode.create(:parent_id => parent.id, :node_object_id => team.id)
- 
-     
-    j=i*parent.team_count
-    
-    for k in 1..parent.team_count
-    #for k in 0..(parent.team_count -1)
-    user = User.find_by_id(participants[j-k].user_id)
-    team.add_member(user)    
-    end    
-  end
-
-    
-  if diff != 0
-    begin
-     check_for_existing_team_name(parent,"Team #{no_of_teams+1}")
-     
-   rescue TeamExistsError
-   team = Team.find_by_name("Team #{no_of_teams+1}")
-   team.delete
-     
-   end
-    
-  team = Object.const_get(session[:team_type]+'Team').create(:name => "Team #{no_of_teams+1}", :parent_id => parent.id)
-  TeamNode.create(:parent_id => parent.id, :node_object_id => team.id)
-  
-  for indx in 1..diff
-    user = User.find_by_id(participants[participants.length-indx].user_id)
-    team.add_member(user)
-  end
-    
-    
-  end
- 
- #for each member of the user-assignment table create random teams
- # and add them to the database 
-  
-end
-
-
-
 
 def create_teams
-   parent = Object.const_get(session[:team_type]).find(params[:id])
-      
-   #call randomize
-   randomize_teams   
-      
-  
-   
+  parent = Object.const_get(session[:team_type]).find(params[:id])
+  Team.randomize_all_by_parent(parent)
   redirect_to :action => 'list', :id => parent.id
  end
 
@@ -141,7 +26,7 @@ def create_teams
    if params[:type]
     session[:team_type] = params[:type]
    end
-   @root_node = Object.const_get(session[:team_type]+"Node").find_by_node_object_id(params[:id])   
+   @root_node = Object.const_get(session[:team_type] + "Node").find_by_node_object_id(params[:id])
    @child_nodes = @root_node.get_teams()
  end
  
@@ -152,7 +37,7 @@ def create_teams
  def create
    parent = Object.const_get(session[:team_type]).find(params[:id])
    begin
-    check_for_existing_team_name(parent,params[:team][:name])
+    Team.check_for_existing(parent, params[:team][:name])
     team = Object.const_get(session[:team_type]+'Team').create(:name => params[:team][:name], :parent_id => parent.id)
     TeamNode.create(:parent_id => parent.id, :node_object_id => team.id)
     redirect_to :action => 'list', :id => parent.id
@@ -166,7 +51,7 @@ def create_teams
    team = Team.find(params[:id])
    parent = Object.const_get(session[:team_type]).find(team.parent_id)
    begin
-    check_for_existing_team_name(parent,params[:team][:name])
+    Team.check_for_existing(parent, params[:team][:name])
     team.name = params[:team][:name]
     team.save
     redirect_to :action => 'list', :id => parent.id
@@ -222,15 +107,5 @@ def create_teams
    end      
    redirect_to :controller => 'team', :action => 'list', :id => assignment.id
  end
- 
- protected
- 
- def check_for_existing_team_name(parent,name)    
-    list = Object.const_get(session[:team_type]+'Team').find(:all, :conditions => ['parent_id = ? and name = ?',parent.id,name])
-    if list.length > 0     
-      raise TeamExistsError, 'Team name, "'+name+'", is already in use.'
-    end
- end
-
  
 end

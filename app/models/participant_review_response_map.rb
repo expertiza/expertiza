9cd9dc70c830cfd62c8c8fa0d0fb05e2e12c70da
ssgujar <ssgujar@ncsu.edu>
2011-10-31 15:09:24 -0400

    Moved 'get_review_questionnaire_id_for_assignment' from ReviewMappingController to Assignment Moved 'compute_hash_review' from ReviewMappingController to Assignment
    Refactored 'add_self_reviewer' method in ReviewMappingController. For this, defined 'assign_reviewer' method in TeamReviewResponseMap and ParticipantReviewResponseMap each

--- app/models/participant_review_response_map.rb
+++ app/models/participant_review_response_map.rb
@@ -1,4 +1,16 @@
 class ParticipantReviewResponseMap < ReviewResponseMap
   belongs_to :reviewee, :class_name => 'Participant', :foreign_key => 'reviewee_id'
-  belongs_to :contributor, :class_name => 'Participant', :foreign_key => 'reviewee_id' 
+  belongs_to :contributor, :class_name => 'Participant', :foreign_key => 'reviewee_id'
+
+  def self.assign_reviewer submission_id, assignment_id, reviewer_id
+     contributor = AssignmentParticipant.find_by_id_and_parent_id(submission_id, assignment_id)
+      if ParticipantReviewResponseMap.find(:first, :conditions => ['reviewee_id = ? and reviewer_id = ?',contributor.id ,reviewer_id]).nil?
+        ParticipantReviewResponseMap.create(:reviewee_id => contributor.id,
+                                            :reviewer_id => reviewer_id,
+                                            :reviewed_object_id => assignment_id)
+      else
+        flash[:error] = "There are no more submissions for you to review at this time."
+        raise "The reviewer, \""+reviewer.name+"\", is already assigned to this contributor."
+      end
+  end
 end

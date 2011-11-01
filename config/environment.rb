c4e2d0fbac0f3a67b5158befaa9037c3edfa32bc
Vartika <vartikasingh07@gmail.com>
2011-11-01 17:40:29 -0400

    Conflict resolved in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -15,17 +15,7 @@ Rails::Initializer.run do |config|
 
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
-
-<<<<<<< HEAD
-  # Specify gems that this application depends on and have them installed with rake gems:install
-  # config.gem "hpricot", :lib, :version => '0.6', :source => "http://code.whytheluckystiff.net"
-  config.gem 'fastercsv'
-  config.gem 'mysql'
-  config.gem 'RedCloth'
-  config.gem 'rgl', :lib => 'rgl/adjacency'
-  config.gem 'rubyzip', :lib => "zip/zip"
-  #raise 'graphviz dependency is not installed - missing dot executable' unless RUBY_PLATFORM =~ /mswin/ or %x(which dot).to_s.any?
-||||||| merged common ancestors
+  
   # Specify gems that this application depends on and have them installed with rake gems:install
   # config.gem "hpricot", :lib, :version => '0.6', :source => "http://code.whytheluckystiff.net"
   config.gem 'fastercsv'
@@ -34,11 +24,9 @@ Rails::Initializer.run do |config|
   config.gem 'rgl', :lib => 'rgl/adjacency'
   config.gem 'rubyzip', :lib => "zip/zip"
   raise 'graphviz dependency is not installed - missing dot executable' unless RUBY_PLATFORM =~ /mswin/ or %x(which dot).to_s.any?
-=======
   if RAILS_ENV == 'production' and RUBY_PLATFORM !~ /mswin|mingw/ # Don't check on Windows, because there's no "which" command to check
     raise 'dot executable missing - install graphviz' if %x(which dot).to_s.empty?
   end
->>>>>>> master
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named

1d8acb13c3ab9fd2ca1c2a8b4318c058ea36064a
Andrew Davis <ajdavis7@ncsu.edu>
2011-11-01 21:40:03 -0500

    E9: Added skeleton for non-trivial SignUpSheetController functional tests.

---
+++ test/functional/sign_up_sheet_controller_test.rb
@@ -0,0 +1,87 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require 'sign_up_sheet_controller'
+
+# Re-raise errors caught by the controller.
+class SignUpSheetController; def rescue_action(e) raise e end; end
+
+class SignUpSheetControllerTest < ActionController::TestCase
+
+  def test_add_signup_topics_staggered
+  end
+  
+  def test_add_signup_topics
+  end
+  
+  def test_view_publishing_rights
+  end
+  
+  def test_load_add_signup_topics
+  end
+  
+  def test_create
+  end
+  
+  def test_redirect_to_sign_up
+  end
+  
+  def test_delete
+  end
+  
+  def test_edit
+  end
+  
+  def test_update
+  end
+  
+  def test_signup_topics
+  end
+  
+  def test_delete_signup
+  end
+  
+  def test_delete_signup_for_topic
+  end
+  
+  def test_signup
+  end
+  
+  def test_slotAvailable?
+  end
+  
+  def test_otherConfirmedTopicforUser
+  end
+  
+  def test_confirmTopic
+  end
+  
+  def test_create_team
+  end
+  
+  def test_generate_team_name
+  end
+  
+  def test_create_team_users
+  end
+  
+  def test_has_user
+  end
+  
+  def test_save_topic_dependencies
+  end
+  
+  def test_save_topic_deadlines
+  end
+  
+  def test_build_dependency_graph
+  end
+  
+  def test_create_common_start_time_topics
+  end
+  
+  def test_create_topic_deadline
+  end
+  
+  def test_set_start_due_date
+  end
+  
+end

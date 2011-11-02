4a3f6381d028892ce3fcd8ab7bd937742240bc0c
Andrew Davis <ajdavis7@ncsu.edu>
2011-11-01 20:48:36 -0500

    E9: Added skeletons for unit tests of non-trivial functions.

---
+++ test/unit/signed_up_user_test.rb
@@ -0,0 +1,13 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class SignedUpUserTest < ActiveSupport::TestCase
+  
+  def test_cancel_waitlists_of_users
+  
+  end
+  
+  def test_find_team_participants
+  
+  end
+
+end

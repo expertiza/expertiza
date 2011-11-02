26a99efe4a39934dde8efa4a69bd6e7a21d6a2ee
Vartika <vartikasingh07@gmail.com>
2011-11-02 16:25:05 -0400

    unit test added

--- test/functional/notification_controller_test.rb
+++ test/functional/notification_controller_test.rb
@@ -1,8 +1,21 @@
 require 'test_helper'
 
-class NotificationControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+# class NotificationControllerTest < ActionController::TestCase
+#   # Replace this with your real tests.
+#   test "the truth" do
+#     assert true
+#   end
+# end
+
+class UserControllerTest < ActionController::TestCase
+  test "invite friend" do
+    assert_difference 'ActionMailer::Base.deliveries.size', +1 do
+      post :invite_friend, :email => 'friend@example.com'
+    end
+    invite_email = ActionMailer::Base.deliveries.first
+ 
+    assert_equal "You have been invited by me@example.com", invite_email.subject
+    assert_equal 'friend@example.com', invite_email.to[0]
+    assert_match(/Hi friend@example.com/, invite_email.body)
   end
 end
--- test/unit/notification_test.rb
+++ test/unit/notification_test.rb
@@ -2,7 +2,38 @@ require 'test_helper'
 
 class NotificationTest < ActiveSupport::TestCase
   # Replace this with your real tests.
+  
+  fixtures :users
+  
   test "the truth" do
     assert true
   end
+  
+  # assert_select_email do
+  #     assert_select "h1", "Email alert"
+  #   end
+  
+  test "required fields" do                 
+      user1 = users(:student1)
+      assert(!user1.valid?)
+      user2 =users(:student2)
+      assert(!user2.valid?)
+  end
+  
 end
+   
+   # class UserMailerTest < ActionMailer::TestCase
+   #    tests UserMailer
+   #    test "invite" do
+   #      @expected.from    = 'me@example.com'
+   #      @expected.to      = 'friend@example.com'
+   #      @expected.subject = "You have been invited by #{@expected.from}"
+   #      @expected.body    = read_fixture('invite')
+   #      @expected.date    = Time.now
+   # 
+   #      assert_equal @expected.encoded, UserMailer.create_invite('me@example.com', 'friend@example.com', @expected.date).encoded
+   #    end
+   # 
+   #  end
+   
+#end

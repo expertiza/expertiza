271904df57cb6d5a8f88cf9274df5d69b6fcdf27
ssgujar <ssgujar@ncsu.edu>
2011-11-03 18:14:03 -0400

    Pulled up 'assign_reviewer' method from TeamReviewResponseMap and ParticipantReviewResponseMap to their superclass ReviewResponseMap

--- app/models/team_review_response_map.rb
+++ app/models/team_review_response_map.rb
@@ -2,14 +2,4 @@ class TeamReviewResponseMap < ReviewResponseMap
   belongs_to :reviewee, :class_name => 'Team', :foreign_key => 'reviewee_id'
   belongs_to :contributor, :class_name => 'Team', :foreign_key => 'reviewee_id'
 
-   def self.assign_reviewer contributor_id, reviewer_id, assignment_id
-      if TeamReviewResponseMap.find(:first, :conditions => ['reviewee_id = ? and reviewer_id = ?', contributor_id, reviewer_id]).nil?
-      TeamReviewResponseMap.create(:reviewee_id => contributor_id,
-                                   :reviewer_id => reviewer_id,
-                                   :reviewed_object_id => assignment_id)
-    else
-      raise "The reviewer, \""+reviewer.name+"\", is already assigned to this contributor."
-    end
-  end
-
 end

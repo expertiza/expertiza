3b34a0b053ee088b57ff47d491f774236b921065
Edward Anderson <edward.anderson@scimedsolutions.com>
2011-11-04 23:00:16 -0400

    Load seed data for cucumber tests

--- features/support/env.rb
+++ features/support/env.rb
@@ -47,10 +47,15 @@ ActionController::Base.allow_rescue = false
 # block that will explicitly put your database in a known state.
 Cucumber::Rails::World.use_transactional_fixtures = true
 
-Fixtures.reset_cache  
+# Get fixtures ready for use
+Fixtures.reset_cache
 fixtures_folder = File.join(RAILS_ROOT, 'spec', 'fixtures')
 fixtures = Dir[File.join(fixtures_folder, '*.yml')].map {|f| File.basename(f, '.yml') }
 Fixtures.create_fixtures(fixtures_folder, fixtures)
+
+# Load seed data
+seed_file = File.join(Rails.root, "db", "seeds.rb")
+load(seed_file)
 
 class ActiveRecord::Base
   mattr_accessor :shared_connection

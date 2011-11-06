bf9aa257436458c931a5ba27d781b51be1078143
Edward Anderson <edward.anderson@scimedsolutions.com>
2011-11-05 23:09:25 -0400

    Clear the database when running cucumber directly
    Also don't load fixtures for cucumber

--- features/support/env.rb
+++ features/support/env.rb
@@ -47,11 +47,14 @@ ActionController::Base.allow_rescue = false
 # block that will explicitly put your database in a known state.
 Cucumber::Rails::World.use_transactional_fixtures = true
 
-# Get fixtures ready for use
-Fixtures.reset_cache
-fixtures_folder = File.join(RAILS_ROOT, 'spec', 'fixtures')
-fixtures = Dir[File.join(fixtures_folder, '*.yml')].map {|f| File.basename(f, '.yml') }
-Fixtures.create_fixtures(fixtures_folder, fixtures)
+# Empty out the test database. This will only run if it hasn't already.
+`rake db:test:prepare` if User.count > 0
+
+# # Get fixtures ready for use
+# Fixtures.reset_cache
+# fixtures_folder = File.join(RAILS_ROOT, 'spec', 'fixtures')
+# fixtures = Dir[File.join(fixtures_folder, '*.yml')].map {|f| File.basename(f, '.yml') }
+# Fixtures.create_fixtures(fixtures_folder, fixtures)
 
 # Load seed data
 seed_file = File.join(Rails.root, "db", "seeds.rb")
@@ -69,5 +72,3 @@ end
 # Forces all threads to share the same connection. This works on
 # Capybara because it starts the web server in a thread.
 ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
-
-

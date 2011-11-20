27e6fccbf2b35927485fe364a94e270b22e1769c
ssgujar <ssgujar@ncsu.edu>
2011-11-19 22:26:13 -0500

    Remove this file.

--- db/migrate/20111119210421_create_quiz_deadline_types.rb
+++
@@ -1,9 +0,0 @@
-class CreateQuizDeadlineTypes < ActiveRecord::Migration
-  def self.up
-    DeadlineType.create :name => "quiz"
-  end
-
-  def self.down
-    DeadlineType.find_by_name("quiz").destroy
-  end
-end

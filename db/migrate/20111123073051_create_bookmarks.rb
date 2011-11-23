82f67011cf32458f51130713f7b99b2f7a29840c
Chandan Apsangi <chandan.jc@gmail.com>
2011-11-23 04:31:49 -0500

    Re-implemented add feature

---
+++ db/migrate/20111123073051_create_bookmarks.rb
@@ -0,0 +1,16 @@
+class CreateBookmarks < ActiveRecord::Migration
+
+  def self.up
+
+    create_table :bookmarks do |t|
+      t.column "url", :string, :null => false
+      t.column "discoverer_user_id", :integer, :null=> false
+      t.column "user_count", :integer, :null => false
+    end
+  end
+
+  def self.down
+    drop_table :bookmarks
+  end
+
+end

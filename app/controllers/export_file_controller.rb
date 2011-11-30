d96d2e2bcbd92c3be34d5bcbca9924973960b3be
Raghu Gowda <rgowda@ncsu.edu>
2011-11-30 09:26:59 -0500

    Changes related to exporting grades

--- app/controllers/export_file_controller.rb
+++ app/controllers/export_file_controller.rb
@@ -1,16 +1,25 @@
 class ExportFileController < ApplicationController
   require 'fastercsv'
-  
-  def start    
+
+  def start
+    @model = params[:model]
+    if(@model == 'Assignment')
+      @title = 'Grades'
+    end
+    @id = params[:id]
+
+  end
+
+  def export
     filename = "out.csv"
     csv_data = FasterCSV.generate do |csv|
-        csv << Object.const_get(params[:model]).get_export_fields()       
-               
-        Object.const_get(params[:model]).export(csv,params[:id])       
+      csv << Object.const_get(params[:model]).get_export_fields(params[:options])
+
+      Object.const_get(params[:model]).export(csv, params[:id],params[:options])
     end
-       
-    send_data csv_data, 
-      :type => 'text/csv; charset=iso-8859-1; header=present',
-      :disposition => "attachment; filename=#{filename}"              
+
+    send_data csv_data,
+              :type => 'text/csv; charset=iso-8859-1; header=present',
+              :disposition => "attachment; filename=#{filename}"
   end
 end

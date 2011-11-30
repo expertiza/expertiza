f70d05c30c327739ee427f2b36b8255aa426af52
Shyam <sshyamr@ncsu.edu>
2011-11-30 13:24:21 -0500

    Diff is final. Show code not included

--- app/helpers/review_comments_helper.rb
+++ app/helpers/review_comments_helper.rb
@@ -6,7 +6,7 @@ module ReviewCommentsHelper
 
     comment_window_html = "<table>"
     for i in 0..(comment_array.length-1) do
-      comment_window_html += "<tr><td><b>Comment #{i+1}:</b><br/>" + comment_array[i].to_s +
+      comment_window_html += "<tr><td><b><u>Comment #{i+1}</u>:</b><br/>" + comment_array[i].to_s +
           "</td></tr>"
     end
     comment_window_html += "</table>"

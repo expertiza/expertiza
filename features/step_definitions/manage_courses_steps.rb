5233cddaf0341130c9fe6813ad5adc0075578346
Graham Malmgren <gtmalmgr@ncsu.edu>
2011-12-01 12:58:08 -0500

    created a workaround for the Manage... link which has spaces

--- features/step_definitions/manage_courses_steps.rb
+++ features/step_definitions/manage_courses_steps.rb
@@ -1,40 +1,8 @@
-Given /^I am signed in as an instructor "([^"]*)"$/ do |name|
-  Users.find_by_name(name)
-end
-
-Then /^I am able to view the newly created course in "([^"]*)"\.$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+When /^I follow the "Manage..." link as an "([^"]*)"$/ do |role|
+  visit "/menu/manage%20#{role}%20content"
 end
 
 Given /^I have a course named "([^"]*)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
-
-Then /^I am able to view the newly created course in "([^"]*)" with the name of "([^"]*)"\.$/ do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
-end
-
-Then /^I see the list of courses does not include the deleted course "([^"]*)"\.$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
-
-Then /^I see the list of courses includes the course named "([^"]*)"\.$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
-
-Then /^I see the list of courses includes the course named "([^"]*)" which is private\.$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
-
-Then /^I am able to  view a list of courses\.$/ do
-  pending # express the regexp above with the code you wish you had
-end
-
-Given /^I am signed in as a student "([^"]*)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
-
-Then /^I should be redirected to "([^"]*)"$/ do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 

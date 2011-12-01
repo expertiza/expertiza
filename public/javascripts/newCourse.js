d7a5ea342cf67fabbcd9d113baca324c8c583646
Matthew Banz <matt.banz@gmail.com>
2011-12-01 01:52:03 -0700

    You can now delete and somewhat edit assignments made on the new course page

--- public/javascripts/newCourse.js
+++ public/javascripts/newCourse.js
@@ -6,8 +6,16 @@ jQuery(function() {
      * Handles when the user chooses to add restrictions on partners
      * by showing the text boxes for max_duplicate_pairings and min_unique_pairings.
      */
-    jQuery("#restrictPartnersCheckbox").change(function() {
-        jQuery("#partnerRestrictionsWrapper").toggle(jQuery(this).is(":checked"));
+    jQuery("#restrictPartners").click(function() {
+        var checked = jQuery(this).is(":checked");
+        jQuery("#partnerRestrictionsWrapper").toggle(checked);
+
+        if(!checked) {
+            jQuery("#course_max_duplicate_pairings").val("");
+            jQuery("#course_min_unique_pairings").val("");
+            jQuery("#assignmentsWrapper").empty();
+            jQuery("#addAssignmentsWrapper").hide();
+        }
     });
 
     /**
@@ -28,10 +36,11 @@ jQuery(function() {
 
         pTag.nextAll().each(function() {
             jQuery(this).find("input[name^=assignment]").each(function() {
+                this.name = this.name.replace(/\d+/, index);
+
                 if(this.id.length > 0) {
-                    this.id = replaceAssignmentNumber(this.id, index);
+                    this.id = this.id.replace(/\d+/, index);
                 }
-                this.name = replaceAssignmentNumber(this.name, index);
             });
             ++index;
         });
@@ -46,23 +55,13 @@ jQuery(function() {
      * that each assignment has an index in its name. (e.g. assignment0, assignment1, etc)
      */
     jQuery("#plus1Assignment").click(function() {
-        var id = "assignment" + jQuery("#assignmentsWrapper > p").length;
+        var i = jQuery("#assignmentsWrapper > p").length;
 
-        var name = '<label>Name: <input type="text" id="' + id + '_name" name="' + id + '[name]" /></label>',
-            teamCount = '<label>Team Size: <input type="text" id="' + id + '_team_count" name="' + id + '[team_count]" /></label>',
-            isTeam = '<input type="hidden" name="' + id + '[team_assignment]" value="true" />',
-            wikiType = '<input type="hidden" name="' + id + '[wiki_type_id]" value="1" />';
-
-        jQuery("#assignmentsWrapper").append('<p>' + name + teamCount + isTeam + wikiType + '<a href="#" class="deleteAssignment"><img src="/images/delete.png" /></a></p>')
+        jQuery("#assignmentsWrapper").append(
+            '<p>' +
+                '<label>Name: <input type="text" id="assignment' + i + '_name" name="assignment' + i + '[name]" /></label>' +
+                '<label>Team Size: <input type="text" id="assignment' + i + '_team_count" name="assignment' + i + '[team_count]" /></label>' +
+                '<a href="#" class="deleteAssignment"><img src="/images/delete.png" /></a>' +
+            '</p>');
     });
 });
-
-/**
- * Helper function to re-index the inputs for an assignment.
- * @param identifier
- * @param number
- */
-function replaceAssignmentNumber(identifier, number) {
-    var i = identifier.search(/[^a-zA-Z0-9]/);
-    return "assignment" + number + identifier.substr(i);
-}

30e1066e1018f05b8908f287d57781825ac0a48e
themightytruk <rkhasian@ncsu.edu>
2011-12-02 12:48:38 -0500

    Changing from reserved word type to q_type

--- app/models/question_type.rb
+++ app/models/question_type.rb
@@ -2,5 +2,5 @@ class QuestionType < ActiveRecord::Base
 
     belongs_to :question, :class_name => "Question", :foreign_key => "question_id" #the question the type is for
 
-    validates_presence_of :type # user must define type for the custom question
+    validates_presence_of :q_type # user must define type for the custom question
 end

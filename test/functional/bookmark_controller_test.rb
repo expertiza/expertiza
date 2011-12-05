7a3ddc476692dc12954ec459b9512343c75d8a38
Chandan Apsangi <chandan.jc@gmail.com>
2011-12-05 00:06:02 -0500

    Funtional tests updated

--- test/functional/bookmark_controller_test.rb
+++ test/functional/bookmark_controller_test.rb
@@ -1,71 +1,81 @@
-require File.dirname(__FILE__) + '/../test_helper'
-require 'review_controller'
+require 'test_helper'
+require 'bookmarks_controller'
 
 
-class BookmarkControllerTest < Test::Unit::TestCase
+class BookmarksControllerTest < ActionController::TestCase
+
+fixtures :users, :roles, :participants,:bmappings, :bmappings_tags, :bmapping_ratings, :bookmark_rating_rubrics,:bookmarks, :sign_up_topics
+  fixtures :users
+  fixtures :assignments
+  fixtures :questionnaires
+  fixtures :courses
+  set_fixture_class :system_settings => 'SystemSettings'
+  fixtures :system_settings
+  fixtures :content_pages
+  @settings = SystemSettings.find(:first)
+
+ def setup
+    @controller = BookmarksController.new
+    @request    = ActionController::TestRequest.new
+    @response   = ActionController::TestResponse.new
+    #@request.host = 'test.host.com'
+
+    @request.session[:user] = User.find(users(:admin).id )
+    roleid = User.find(users(:admin).id).role_id
+    Role.rebuild_cache
+
+    Role.find(roleid).cache[:credentials]
+    @request.session[:credentials] = Role.find(roleid).cache[:credentials]
+    # Work around a bug that causes session[:credentials] to become a YAML Object
+    @request.session[:credentials] = nil if @request.session[:credentials].is_a? YAML::Object
+    @settings = SystemSettings.find(:first)
+    AuthController.set_current_role(roleid,@request.session)
+    #   @request.session[:user] = User.find_by_name("suadmin")
+  end
 
 
+def test_view_bookmarks 
 
-def test_view_bookmarks
-
-get :view_bookmarks 
-assert_response :success
+get :view_bookmarks
+assert_response 302
 
 end
 
 
-def test_view_bookmarks_most_recent
+def test_view_bookmarks_most_recent 
 
-get :view_bookmarks :order_by => "most recent"
-assert_response :success
+get :view_bookmarks, :order_by => "most recent"
+assert_response 302
 
 end
 
 
 def test_view_bookmarks_most_popular
-
-get :view_bookmarks :order_by => "most popular"
-assert_response :success
-
+get :view_bookmarks, :order_by => "most popular"
+assert_response 302
 end
 
 
 def test_manage_bookmarks
-
-get :manage_bookmarks, :user => Users(:student1)
-assert_response :success 
-
+get :manage_bookmarks, :user => users(:student1)
+assert_response 302 
 end
 
 
 def test_search_bookmarks
 
-
-
 end
 
-def test_create_bookmark
-bookmark = Bookmark.new{:b_url => "www.google.com",
-                                 :b_title          => "google",
-                                 :b_tags_text      => "google it",
-                                 :b_description    => "it's google",                               
-                                 :user => Users(:student1), }
-assert bookmark.save
 
-end
-
-def test_add_bookmark_with_id
-
-get :add_bookmark, :b_url => "www.google.com",
-                                 :b_title          => "google",
-                                 :b_tags_text      => "google it",
-                                 :b_description    => "it's google",                               
-                                 :user => Users(:student1), 
-				:topicid => Switch_topics(:two).topic_id
-				
-
+def test_add_bookmark_with_id 
+	get(:add_bookmark, {:b_url => "www.google.com",
+		                         :b_title          => "google",
+		                         :b_tags_text      => "google it",
+		                         :b_description    => "its google",                               
+		                         :user => users(:admin), 
+					:topicid => 243819443 },session_for(users(:admin)))
 			
-assert_redirected_to  :action => 'view_topic_bookmarks', :id => :topicid
+	assert_redirected_to  :action => 'view_topic_bookmarks', :id => :topicid
 
 end
 
@@ -76,8 +86,8 @@ def test_add_bookmark_without_id
 get :add_bookmark, :b_url => "www.google.com",
                                  :b_title          => "google",
                                  :b_tags_text      => "google it",
-                                 :b_description    => "it's google",                               
-                                 :user => Users(:student1), 
+                                 :b_description    => "its google",                               
+                                 :user => "student1", 
 				:topicid => 'nil'
 				
 
@@ -91,7 +101,7 @@ get :add_bookmark, :b_url => "www.wikipedia.com",
                                  :b_title          => "wikipedia",
                                  :b_tags_text      => "wiki it",
                                  :b_description    => "it's wiki",                               
-                                 :user => Users(:student2), 
+                                 :user => users(:student2), 
 				:topicid => 'nil'
 				
 
@@ -106,12 +116,37 @@ get :add_bookmark, :b_url => "www.google.com",
                                  :b_title          => "google",
                                  :b_tags_text      => "google it",
                                  :b_description    => "it's google",                               
-                                 :user => Users(:student3), 
+                                 :user => users(:student3), 
 				:topicid => 'nil'
 				
 
 			
 assert_redirected_to  :action => 'manage_bookmarks'
+end
+
+def test_view_review_bookmarks
+get :view_review_bookmarks, :id =>participants(:par1).id
+
+assert_response 302
+
+end
+
+def test_view
+get :view, {:id => bmappings(:bmapping1).id}
+assert_response 302
+end
+
+def test_create_rating_rubric_not_empty
+post :create_rating_rubric, :params => {:display_text => "Did you like the bookmark?", :minimum_rating => 1, :maximum_rating => 1 }
+
+assert_template :view_rating_rubric_form
+
+end
+
+def test_create_rating_rubric_empty
+post :create_rating_rubric, :display_text => "", :minimum_rating => "", :maximum_rating => "" 
+
+assert_redirected_to :action => :view_rating_rubrics
 end
 
 

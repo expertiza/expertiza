a0d2d22b7d91aa821b32ba1ad6c37d3b47342c5a
Graham Malmgren <gtmalmgr@ncsu.edu>
2011-12-05 22:47:40 -0500

    all instructor/manage_courses.feature(s) are working! yeee haw

--- features/step_definitions/manage_courses_steps.rb
+++ features/step_definitions/manage_courses_steps.rb
@@ -2,7 +2,13 @@ When /^I follow the "Manage..." link as an "([^"]*)"$/ do |role|
   visit "/menu/manage%20#{role}%20content"
 end
 
-Given /^I have a course named "([^"]*)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+Given /^I have a (public|private) course named "([^"]*)"$/ do |public_or_private,course_name|
+    When "I follow the \"Manage...\" link as an \"instructor\""
+      And "I follow \"Create #{public_or_private.capitalize} Course\""
+      And "I fill in \"#{course_name}\" for \"Course Name\""
+      And "I fill in \"#{course_name}-directory\" for \"Course Directory\""
+      And "I fill in \"A very informational course about information\" for \"Course Information\""
+      And "I press \"Create\""
+    Then "I should see \"Course1\""
 end
 

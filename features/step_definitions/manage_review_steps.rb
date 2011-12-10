b2e167ceea4d7606ef8a1ff16761a19fa8e0e2f2
opevans <opevans@ncsu.edu>
2011-12-10 01:40:01 -0500

    modified:   app/views/tree_display/actions/_assignments_actions.html.erb 	modified:   app/views/tree_display/actions/_questionnaire_types_actions.html.erb 	new file:   features/instructor/manage_assignments.feature 	modified:   features/step_definitions/authentication_steps.rb 	new file:   features/step_definitions/manage_assignments_steps.rb 	modified:   features/step_definitions/manage_review_steps.rb

--- features/step_definitions/manage_review_steps.rb
+++ features/step_definitions/manage_review_steps.rb
@@ -1,3 +1,11 @@
+Given /^I have a (public|private) review named "([^"]*)"$/ do |public_or_private,review_name|
+  When "I follow the \"Manage...\" link as an \"instructor\""
+    And "I follow \"Create #{public_or_private.capitalize} Review\""
+    And "I fill in \"#{review_name}\" for \"questionnaire[name]\""
+    And "I press \"Create review\""
+  Then "I should see \"#{review_name}\""
+end
+
 Then /^I should see the details of submitted teammate review$/ do
   should have_content "Teammate Review"
 end

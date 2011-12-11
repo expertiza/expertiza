b755d1d6c0c2cc524fa5f0e1ea12e64b649c19f5
Owen Evans <opevans@ncsu.edu>
2011-12-10 21:15:56 -0500

    Committing final update to assignments cucumber tests.

--- features/step_definitions/manage_review_steps.rb
+++ features/step_definitions/manage_review_steps.rb
@@ -1,8 +1,8 @@
-Given /^I have a (public|private) review named "([^"]*)"$/ do |public_or_private,review_name|
+Given /^I have a (public|private) (review|metareview|author feedback|teammate review|survey|global survey|course evaluation) named "([^"]*)"$/ do |public_or_private,review_type,review_name|
   When "I follow the \"Manage...\" link as an \"instructor\""
-    And "I follow \"Create #{public_or_private.capitalize} Review\""
+    And "I follow \"Create #{public_or_private.capitalize} #{review_type.split.map {|w| w.capitalize}.join ' '}\""
     And "I fill in \"#{review_name}\" for \"questionnaire[name]\""
-    And "I press \"Create review\""
+    And "I press \"Create #{review_type}\""
   Then "I should see \"#{review_name}\""
 end
 

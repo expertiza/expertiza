ba802f3c1e216ca97eb94a604fe7d3b4a8dbfcd6
Mohan Kantrapati <mvkantra@ncsu.edu>
2011-12-11 01:03:20 -0500

    post presentation commit

---
+++ db/migrate/20111118044951_add_version_to_responses.rb
@@ -0,0 +1,9 @@
+class AddVersionToResponses < ActiveRecord::Migration
+  def self.up
+    add_column :responses, :version_num, :integer
+  end
+
+  def self.down
+    remove_column :responses, :version_num
+  end
+end

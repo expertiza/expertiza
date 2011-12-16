fefc7922a796d202245ba9238086355a22bc57ec
Adam Alford <agalford@ncsu.edu>
2011-12-15 22:51:42 -0500

    Committing unit tests for Judith

--- test/unit/question_type_test.rb
+++ test/unit/question_type_test.rb
@@ -1,8 +1,15 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
+#test question type
 
 class QuestionTypeTest < ActiveSupport::TestCase
-  
-  test "the truth" do
-    assert true
+  fixtures :questions
+
+  def test_boolean_question
+    q4 = Question.new
+    q4.questionnaire_id = @questionnaire1.id
+    q4.true_false = true
+    q4.txt = "4"
+    @questionnaire1.questions << q4
+    assert @questionnaire1.true_false_questions?
   end
 end

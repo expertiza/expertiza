486e2d1ce80ab0771124d2bad8385401a43f8d6c
Melroy Dsouza <melroyndsouza@gmail.com>
2011-12-16 12:53:02 -0500

    Adding new test cases

--- app/models/interaction.rb
+++ app/models/interaction.rb
@@ -2,9 +2,10 @@ class Interaction < ActiveRecord::Base
   belongs_to  :participant, :class_name => 'AssignmentParticipant', :foreign_key => 'assignment_id'
   belongs_to  :team, :class_name => 'Team', :foreign_key => 'team_id'
 
-
-  validates_presence_of :comments, :message => "Please enter comments.\n"
+  validates_presence_of :number_of_minutes, :message =>'Number of minutes is blank.'
+  validates_presence_of :comments, :message => "Please enter comments."
   validates_presence_of :interaction_datetime, :message => "Please enter date and time."
+
   validates_numericality_of :number_of_minutes, :minimum => 0, :message => "Number of minutes is blank or non-numeric."
 
 end

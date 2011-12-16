4502f6b5759ad502ab0aaaa1023d012a0976261f
uojha <uojha@ncsu.edu>
2011-12-16 11:44:40 -0500

    Update test/functional/question_types_controller_test.rb

--- test/functional/question_types_controller_test.rb
+++ test/functional/question_types_controller_test.rb
@@ -1,18 +1,20 @@
-require 'test_helper'
-#test
+require File.dirname(__FILE__) + '/../test_helper'
+require 'question_types_controller'
+
 class QuestionTypesControllerTest < ActionController::TestCase
-  test "should get index" do
+
+  test "index" do
     get :index
     assert_response :success
     assert_not_nil assigns(:question_types)
   end
 
-  test "should get new" do
+  test "new" do
     get :new
     assert_response :success
   end
 
-  test "should create question_type" do
+  test "create question_type" do
     assert_difference('QuestionType.count') do
       post :create, :question_type => { }
     end
@@ -20,22 +22,22 @@ class QuestionTypesControllerTest < ActionController::TestCase
     assert_redirected_to question_type_path(assigns(:question_type))
   end
 
-  test "should show question_type" do
+  test "show question_type" do
     get :show, :id => question_types(:one).to_param
     assert_response :success
   end
 
-  test "should get edit" do
+  test "edit" do
     get :edit, :id => question_types(:one).to_param
     assert_response :success
   end
 
-  test "should update question_type" do
+  test "update question_type" do
     put :update, :id => question_types(:one).to_param, :question_type => { }
     assert_redirected_to question_type_path(assigns(:question_type))
   end
 
-  test "should destroy question_type" do
+  test "destroy question_type" do
     assert_difference('QuestionType.count', -1) do
       delete :destroy, :id => question_types(:one).to_param
     end

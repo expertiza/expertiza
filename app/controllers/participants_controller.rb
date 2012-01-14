8c048a0c165e0edf576942045a1f850d77e09dff
Andrew Kofink <ajkofink@gmail.com>
2012-01-13 22:00:56 -0500

    updated error message when user does not exist when trying to add him or her to a course

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -13,7 +13,7 @@ class ParticipantsController < ApplicationController
     begin
       curr_object.add_participant(params[:user][:name])
     rescue
-      flash[:error] = "User #{params[:user][:name]} does not exist. Would you like to " + link_to 'create this user?', :controller => 'users', :action => 'new'}
+      flash[:error] = "User #{params[:user][:name]} does not exist. Would you like to " + link_to('create this user?', :controller => 'users', :action => 'new')
     end
     redirect_to :action => 'list', :id => curr_object.id, :model => params[:model]
   end

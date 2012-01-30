b4afc4d589e411175e30dc8dd9fe9b6b531ee313
Adam Alford <agalford@ncsu.edu>
2012-01-29 23:23:00 -0500

    Updated link to custom rubric guide and schema file that somehow didn't get migrated

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110512155258) do
+ActiveRecord::Schema.define(:version => 20111201222103) do
 
   create_table "assignment_questionnaires", :force => true do |t|
     t.integer "assignment_id"
@@ -230,12 +230,12 @@ ActiveRecord::Schema.define(:version => 20110512155258) do
   add_index "question_advices", ["question_id"], :name => "fk_question_question_advices"
 
   create_table "question_types", :force => true do |t|
-    t.string  "type",                       :null => false
+    t.string  "q_type",                     :null => false
     t.string  "parameters"
     t.integer "question_id", :default => 1, :null => false
   end
 
-  add_index "question_types", ["question_id"], :name => "fk_question_type_questions"
+  add_index "question_types", ["question_id"], :name => "fk_question_type_question"
 
   create_table "questionnaires", :force => true do |t|
     t.string   "name",                :limit => 64
@@ -248,8 +248,8 @@ ActiveRecord::Schema.define(:version => 20110512155258) do
     t.integer  "default_num_choices"
     t.string   "type"
     t.string   "display_type"
+    t.string   "section"
     t.text     "instruction_loc"
-	t.string   "section"
   end
 
   create_table "questions", :force => true do |t|

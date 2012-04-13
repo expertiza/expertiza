39436139e8f265abe14d1f799076533391d614c6
Curious Fiddler <vaibhav.cse@gmail.com>
2012-04-12 23:51:01 -0400

    Wrong method of retrieving team id was used. Fixed that now

--- app/controllers/advertise_for_partner_controller.rb
+++ app/controllers/advertise_for_partner_controller.rb
@@ -3,12 +3,12 @@ class AdvertiseForPartnerController < ApplicationController
   #adds a new advertise for partners entry in team table...
   def new
     puts "team #{params[:team_id]}"
-    Team.update_all("advertise_for_partner=true",:id=>params[:team_id])
+    Team.update_all({:advertise_for_partner => true}, ["id = ?", params[:team_id]])
   end
 
   #removes an entry from team table for corresponding team who requested to remove their advertisement for partner request
   def remove
-    Team.update_all("advertise_for_partner=false",:id=>params[:team_id])
+    Team.update_all({:advertise_for_partner => false}, ["id = ?", params[:team_id]])
     assignment=Assignment.find(Team.find(params[:team_id]).parent_id)
     participant=Participant.find_by_parent_id_and_user_id(assignment.id,session[:user].id)
     redirect_to :controller => 'student_team', :action => 'view' , :id => participant.id

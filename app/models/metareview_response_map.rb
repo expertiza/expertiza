73864c1fdfed8278f8d150ea460705f483a11df4
Curious Fiddler <vaibhav.cse@gmail.com>
2012-05-03 00:22:05 -0400

    Part bug fix: the metareview now shows a link for viewing previous reviews, but plays odd with custom review code

--- app/models/metareview_response_map.rb
+++ app/models/metareview_response_map.rb
@@ -1,17 +1,17 @@
 class MetareviewResponseMap < ResponseMap
   belongs_to :reviewee, :class_name => 'Participant', :foreign_key => 'reviewee_id'
-  belongs_to :review_mapping, :class_name => 'ResponseMap', :foreign_key => 'reviewed_object_id'
-
+  belongs_to :review_mapping, :class_name => 'ResponseMap', :foreign_key => 'reviewed_object_id' 
+  
   #return all the versions available for a response map.
   #a person who is doing meta review has to be able to see all the versions of review.
-  def show_review()
+  def get_all_versions()
     if self.review_mapping.response
       @sorted_array=Array.new
       @prev=Response.all
       for element in @prev
         if(element.map_id==self.review_mapping.id)
           array_not_empty=1
-          @sorted_array<<element
+          @sorted_array << element
         end
       end
       @sorted=@sorted_array.sort { |m1,m2|(m1.version_num and m2.version_num) ? m1.version_num <=> m2.version_num : (m1.version_num ? -1 : 1)}

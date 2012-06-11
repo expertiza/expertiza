66841111d489924aa8b5edf9f353539977f994ff
Curious Fiddler <vaibhav.cse@gmail.com>
2012-06-11 01:12:19 -0700

    The review bug now seems to have been fixed. A reviewer can attempt a new review now.

--- app/models/review_response_map.rb
+++ app/models/review_response_map.rb
@@ -1,6 +1,6 @@
 class ReviewResponseMap < ResponseMap
   belongs_to :assignment, :class_name => 'Assignment', :foreign_key => 'reviewed_object_id'
- 
+  
   def questionnaire
     self.assignment.questionnaires.find_by_type('ReviewQuestionnaire')
   end 

1043d9abe73ea7faa57546b9222ec4a9547e3023
Amarja <amarjavaidya21@@gmail.com>
2012-10-19 14:46:37 -0400

    Initial Implementation for penalties

--- app/helpers/penalty_helper.rb
+++ app/helpers/penalty_helper.rb
@@ -86,4 +86,54 @@ module PenaltyHelper
 
     penalty
   end
+
+  def calculate_meta_review_penalty()
+
+    # Check number of reviews required for the assignment
+    # TODO : check how to set num_reviews in assignments table
+    num_of_meta_reviews_required = 2
+    if (num_of_meta_reviews_required > 0)
+
+      #reviews
+      meta_review_mappings = MetareviewResponseMap.find_all_by_reviewer_id(@participant.id)
+
+      meta_review_due_date = DueDate.find_by_deadline_type_id_and_assignment_id(@meta_review_deadline_type_id, @assignment.id).due_at
+
+      meta_review_map_created_at_after_due_date_list = Array.new
+
+      # Calculate the number of reviews that the user has completed so far.
+      num_of_meta_reviews_completed_before_deadline = 0
+      #num_of_reviews_completed_after_deadline = 0
+      num_of_reviews_completed = 0
+      review_mappings.each do |map|
+        created_at = Response.find_by_map_id(map.id).created_at
+        num_of_reviews_completed += 1 if map.response
+        if(created_at < review_due_date)
+          num_of_reviews_completed_before_deadline += 1
+        else
+          review_map_created_at_after_due_date_list <<  created_at
+        end
+      end
+
+      review_map_created_at_after_due_date_list.sort!
+
+      penalty = 0
+      # assign max penalty for all the reviews which are not completed
+      if(num_of_reviews_completed_before_deadline >= num_of_reviews_required)
+        # no need to calculate the penalty
+      else
+        # assign maximum penalty for uncompleted reviews
+        if(num_of_reviews_required > num_of_reviews_completed)
+          penalty += (num_of_reviews_required - num_of_reviews_completed) * @max_penalty_per_review
+        else
+          # calculate the penalty for reviews completed after deadline
+          for i in 0..(num_of_reviews_required - num_of_reviews_completed_before_deadline)
+            (review_map_created_at_after_due_date_list[i] - review_due_date) * @penalty_per_unit
+          end
+        end
+      end
+    end
+
+    penalty
+  end
 end

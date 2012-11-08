765e63b8b665a4e0ae29a53aeb5be4b10c63355f
Prajakta Hegde <pshegde@ncsu.edu>
2012-11-07 22:14:34 -0500

    controllers for late policy

---
+++ app/controllers/late_policies_controller.rb
@@ -0,0 +1,83 @@
+class LatePoliciesController < ApplicationController
+  # GET /late_policies
+  # GET /late_policies.xml
+  def index
+    @penalty_policies = PenaltyPolicy.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @penalty_policies }
+    end
+  end
+
+  # GET /late_policies/1
+  # GET /late_policies/1.xml
+  def show
+    @penalty_policy = PenaltyPolicy.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @penalty_policy }
+    end
+  end
+
+  # GET /late_policies/new
+  # GET /late_policies/new.xml
+  def new
+    @penalty_policy = PenaltyPolicy.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @penalty_policy }
+    end
+  end
+
+  # GET /late_policies/1/edit
+  def edit
+    @penalty_policy = PenaltyPolicy.find(params[:id])
+  end
+
+  # POST /late_policies
+  # POST /late_policies.xml
+  def create
+    @penalty_policy = PenaltyPolicy.new(params[:penalty_policy])
+
+    respond_to do |format|
+      if @penalty_policy.save
+        format.html { redirect_to(@penalty_policy, :notice => 'PenaltyPolicy was successfully created.') }
+        format.xml  { render :xml => @penalty_policy, :status => :created, :location => @penalty_policy }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @penalty_policy.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /late_policies/1
+  # PUT /late_policies/1.xml
+  def update
+    @penalty_policy = PenaltyPolicy.find(params[:id])
+
+    respond_to do |format|
+      if @penalty_policy.update_attributes(params[:penalty_policy])
+        format.html { redirect_to(@penalty_policy, :notice => 'PenaltyPolicy was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @penalty_policy.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /late_policies/1
+  # DELETE /late_policies/1.xml
+  def destroy
+    @penalty_policy = PenaltyPolicy.find(params[:id])
+    @penalty_policy.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(penalty_policies_url) }
+      format.xml  { head :ok }
+    end
+  end
+end

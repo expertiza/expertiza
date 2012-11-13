8314ca9ddf43015fa90e9b05668a4400fcc9331b
Pamela Ocampo <pamela.ocampo@gmail.com>
2012-11-12 23:02:50 -0500

    topic, assignment, and team id parameters

--- app/controllers/sign_up_sheet_controller.rb
+++ app/controllers/sign_up_sheet_controller.rb
@@ -670,6 +670,14 @@ class SignUpSheetController < ApplicationController
   # Submit a bid for a team and a specific topic
   def submit_bid
     # Should get team_id and sign_up_topic_id as parameters
+
+    team = SignedUpUser.find_team_users(params[:assignment_id], (session[:user].id))
+    team_id = users_team[0].t_id
+    topic_id = params[:id]
+    assignment_id = params[:assignment_id]
+    puts "team id is #{team_id} and topic id is #{topic_id} on assignment id #{assignment_id}"
+
+    #redirect_to :action => 'signup_topics', :id => params[:assignment_id]
   end
 
   # Delete a bid for a team and a specific topic

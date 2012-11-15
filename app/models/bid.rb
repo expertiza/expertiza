0c2b10b922a5ec241bfbcf8641ae6b1bba1eb9da
Pamela Ocampo <pamela.ocampo@gmail.com>
2012-11-15 14:39:42 -0600

    Create new bid object in create and save

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -3,8 +3,14 @@ class Bid < ActiveRecord::Base
   belongs_to :team
 
   # Create a bid for a team and topic
-  def create
+  def create(team_id, topic_id)
+    @bid = Bid.new
+    @bid.team_id = team_id
+    @bid.topic_id = topic_id
+    @bid.save
+
      # Should get a team_id and sign_up_topic_id as parameters
+    puts "create new bid for team #{@bid.team_id} and topic #{@bid.topic_id}"
   end
 
   # Delete a bid for a team and topic

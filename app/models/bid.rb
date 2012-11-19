ea96c8165b8124006182d5a66d8b14b5dd7931dc
Pamela Ocampo <pamela.ocampo@gmail.com>
2012-11-19 16:07:49 -0500

    removed bid creation and destruction in model

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -2,16 +2,4 @@ class Bid < ActiveRecord::Base
   belongs_to :topic, :class_name => 'SignUpTopic'
   belongs_to :team
   attr_accessible :topic_id, :team_id
-
-  # Create a bid for a team and topic
-  def create
-
-     # Should get a team_id and sign_up_topic_id as parameters
-    puts "create new bid for team #{self.team_id} and topic #{self.topic_id}"
-  end
-
-  # Delete a bid for a team and topic
-  def destroy
-    # Should get a team_id and sign_up_topic_id as parameters
-  end
 end

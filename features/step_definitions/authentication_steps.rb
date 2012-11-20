75227182d479ef689fb1d1e3ce9cc5066e10bfd6
Chris Barile <crbarile@ncsu.edu>
2012-11-19 22:37:44 -0500

    Fixed step definition for "Given a student with the username __ exists"
    Fixed step definition for "Given a student with the username __
    exists". It was implemented by manipulating the GUI to add a new user.
    This should be done directly using the model code to reduce the steps
    reliance on other features in the software.

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -10,17 +10,19 @@ Given 'I am logged in as a student' do
 end
 
 Given /^a student with the username "(\S+)" exists$/ do |username|
-  if(find_button('Logout').nil?)
-     And 'I am logged in as admin'
+  user = User.find_by_name(username)
+
+  if(user.nil?)
+    User.create({
+                    :name => username,
+                    :fullname => username,
+                    :clear_password => 'password',
+                    :clear_password_confirmation => 'password',
+                    :role => Role.find_by_name!('student'),
+                    :email => "#{username}@mailinator.com",
+                    :is_new_user => false
+                })
   end
-  find(:xpath, "//a[contains(.,'Users')]").click
-  click_link 'New User'
-  fill_in 'user_name', :with => username
-  fill_in 'user_fullname', :with => username
-  fill_in 'user_email', :with => "#{username}@mailinator.com"
-  fill_in 'user_clear_password', :with => 'password'
-  fill_in 'user_clear_password_confirmation', :with => 'password'
-  click_button 'Create'
 end
 
 Then /I should be logged in as "(\S+)"/ do |username|

dcfaa4d6f4aa6cffd7c23d2e45c7a45b02cc50af
Pamela Ocampo <pamela.ocampo@gmail.com>
2012-11-27 18:14:16 -0500

    Decision to move bid model testing to sign_up_sheet_controller test

--- test/unit/bid_test.rb
+++ test/unit/bid_test.rb
@@ -1,19 +1,4 @@
 require 'test_helper'
 
 class BidTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def setup
-    @bid = Bid.new
-  end
-
-  test "save bid with valid data" do
-  end
-
-  test "save bid with invalid data" do
-  end
-
-  test "delete bid" do
-    @bid.delete
-    assert @bid.nil?
-  end
 end

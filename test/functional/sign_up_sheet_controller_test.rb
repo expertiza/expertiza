6d4773ee74d9ce5dcca167be5cb56b61722c7baa
Pamela Ocampo <pamela.ocampo@gmail.com>
2012-11-27 20:35:11 -0500

    sign up sheet controller test stubs

---
+++ test/functional/sign_up_sheet_controller_test.rb
@@ -0,0 +1,37 @@
+require "test_helper"
+
+class SignUpSheetControllerTest < ActionController::TestCase
+  fixtures :users, :roles, :teams, :assignments, :nodes, :system_settings, :content_pages, :permissions, :participants
+  fixtures :roles_permissions, :controller_actions, :site_controllers, :menu_items, :bids, :sign_up_topics, :teams_users
+
+  def setup
+    @controller = SignUpSheetController.new
+  end
+
+  test "submit valid bid" do
+    student = users(:student1)
+
+  end
+
+  test "submit invalid bid" do
+
+  end
+
+  test "delete bid" do
+
+  end
+
+
+  # Called after every test method runs. Can be used to tear
+  # down fixture information.
+  def teardown
+    # Do nothing
+  end
+
+  # Fake test
+  def test_fail
+
+    # To change this template use File | Settings | File Templates.
+    fail("Not implemented")
+  end
+end

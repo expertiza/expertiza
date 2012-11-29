99ef081411f6615ffa35e52d43ea8997ef735fcd
Pamela Ocampo <pamela.ocampo@gmail.com>
2012-11-28 19:06:26 -0500

    Integration testing for bid actions
    Added additional fixtures for sign up topics
    Manually assigned bid ids to bid fixtures
    Tested submitting, deleting, and submitting more than max

--- test/functional/sign_up_sheet_controller_test.rb
+++ test/functional/sign_up_sheet_controller_test.rb
@@ -8,39 +8,86 @@ class SignUpSheetControllerTest < ActionController::TestCase
     @controller = SignUpSheetController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
-    @student = users(:student1)
-    @request.session[:user] = @student
+    @student1 = users(:student1)
+    @student2 = users(:student1)
+    @request.session[:user] = @student1
 
-    @team = teams(:team0)
-    @assignment = assignments(:assignment0)
+    @team = teams(:lottery_team1)
+    @assignment = assignments(:lottery_assignment)
     @topic = sign_up_topics(:LotteryTopic1)
     #go to the topic view
     post :sign_up_topics, {:id => @assignment.id}
 
   end
 
-  test "submit valid bid" do
-    puts "Submit bid by current user #{@student.name}"
-    puts "on assignment #{@assignment.name}, #{@assignment.id}"
-    puts "for topic #{@topic.topic_name}, #{@topic.id}"
-
+  test "should submit bid" do
     post :submit_bid, {:assignment_id => @assignment.id, :id => @topic.id }
     assert_not_nil Bid.find_by_team_id_and_topic_id(@team.id, @topic.id)
-  end
-
-  test "delete my bid" do
+    assert_redirected_to :action =>  :signup_topics, :id => @assignment.id
 
   end
 
-  test "delete a teammates bid" do
+  test "should not submit bid twice" do
+    post :submit_bid, {:assignment_id => @assignment.id, :id => @topic.id }
+    post :submit_bid, {:assignment_id => @assignment.id, :id => @topic.id }
+    assert_equal "Your team has already bid for topic #{SignUpTopic.find(@topic.id).topic_name}", flash[:notice]
+    assert_redirected_to :action =>  :signup_topics, :id => @assignment.id
 
   end
 
-  test "submit more than 3 bids" do
+  test "should delete my bid" do
+    @bid = bids(:bid1)
+    assert_difference('Bid.count', -1) do
+      post :delete_bid, {:id => @bid.id, :assignment_id => @assignment.id }
+    end
+    assert_redirected_to :action =>  :signup_topics, :id => @assignment.id
 
   end
 
-  test "submit more than 3 bids as other teammate" do
+  test "should delete a team bid" do
+    post :submit_bid, {:assignment_id => @assignment.id, :id => @topic.id }
+    @request.session[:user] = @student2
+    post :sign_up_topics, {:id => @assignment.id}
+    @bid = Bid.find_by_team_id_and_topic_id(@team.id, @topic.id)
 
+    assert_difference('Bid.count', -1) do
+      post :delete_bid, {:id => @bid.id, :assignment_id => @assignment.id }
+    end
+
+    assert_redirected_to :action =>  :signup_topics, :id => @assignment.id
+
+  end
+
+  test "should not have more than 3 bids" do
+    @topic2 = sign_up_topics(:LotteryTopic2)
+    @topic3 = sign_up_topics(:LotteryTopic3)
+    @topic4 = sign_up_topics(:LotteryTopic4)
+
+    post :submit_bid, {:assignment_id => @assignment.id, :id => @topic.id }
+    post :submit_bid, {:assignment_id => @assignment.id, :id => @topic2.id }
+    post :submit_bid, {:assignment_id => @assignment.id, :id => @topic3.id }
+    post :submit_bid, {:assignment_id => @assignment.id, :id => @topic4.id }
+
+    assert_equal "Your team has bid the maximum amount of bids", flash[:notice]
+    assert_redirected_to :action =>  :signup_topics, :id => @assignment.id
+
+
+
+  end
+
+  test "should not have 3 bids as other teammate" do
+    @topic2 = sign_up_topics(:LotteryTopic2)
+    @topic3 = sign_up_topics(:LotteryTopic3)
+    @topic4 = sign_up_topics(:LotteryTopic4)
+
+    post :submit_bid, {:assignment_id => @assignment.id, :id => @topic.id }
+    post :submit_bid, {:assignment_id => @assignment.id, :id => @topic2.id }
+    @request.session[:user] = @student2
+    post :sign_up_topics, {:id => @assignment.id}
+    post :submit_bid, {:assignment_id => @assignment.id, :id => @topic3.id }
+    post :submit_bid, {:assignment_id => @assignment.id, :id => @topic4.id }
+
+    assert_equal "Your team has bid the maximum amount of bids", flash[:notice]
+    assert_redirected_to :action =>  :signup_topics, :id => @assignment.id
   end
 end

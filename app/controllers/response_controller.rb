bbe5f49c0348640948b94988955adff60eb6463d
Lakshmi Ramachandran <lramach@ncsu.edu>
2012-11-29 17:52:20 -0500

    Adding the id for the assignment to test the automated metareview feature on.

--- app/controllers/response_controller.rb
+++ app/controllers/response_controller.rb
@@ -379,7 +379,7 @@ class ResponseController < ApplicationController
     @return = params[:return]
     @map.notification_accepted = false
     @map.save
-    if(@map.assignment.id == 561 or @map.assignment.id == 559) #Making the automated metareview feature available for one 'ethical analysis 6' assignment only.
+    if(@map.assignment.id == 561 or @map.assignment.id == 559 or @map.assignment.id == 562) #Making the automated metareview feature available for one 'ethical analysis 6' assignment only.
       #puts("*** saving for me:: #{params[:id]} and metareview selection :save_options - #{params["save_options"]}")
       if(params["save_options"].nil? or params["save_options"].empty?)#default it to with metareviews
         params["save_options"] = "WithMeta"

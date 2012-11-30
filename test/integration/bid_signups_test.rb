f38a31e6983a7f98f74d4f0a61142b4987d2dea8
apsherid <apsherid@ncsu.edu>
2012-11-30 18:24:54 -0500

    Integration Test File Added

---
+++ test/integration/bid_signups_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class BidSignupsTest < ActionController::IntegrationTest
+  fixtures :participants, :deadline_types, :due_dates, :assignments, :roles
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+end

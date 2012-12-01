9c1c33f7d4ce4616c68e95ca140bee7300401335
apsherid <apsherid@ncsu.edu>
2012-11-30 20:51:16 -0500

    Integration test_updates

--- test/integration/bid_signups_test.rb
+++ test/integration/bid_signups_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+require 'sign_up_sheet_controller'
 
 class BidSignupsTest < ActionController::IntegrationTest
   fixtures :users, :roles, :teams, :assignments, :nodes, :system_settings, :content_pages, :permissions, :participants
@@ -15,7 +16,6 @@ class BidSignupsTest < ActionController::IntegrationTest
     @team = teams(:lottery_team1)
     @assignment = assignments(:lottery_assignment)
     @topic = sign_up_topics(:LotteryTopic3)
-    post :controller=>'sign_up_sheet', :action => :signup_topics, :id => @assignment.id
   end
 
   # Given I am on the sign up topics page
@@ -24,28 +24,48 @@ class BidSignupsTest < ActionController::IntegrationTest
   # And I should see the topic in my teams bids
   # And I should see a delete icon for that topic
   test "sign up for bid" do
-    post :controller=>'sign_up_sheet', :action => :signup_topics, :id => @assignment.id
+    get :controller=>'sign_up_sheet', :action => :signup_topics, :id => @assignment.id
     assert_response :success
     @bid = Bid.find_by_team_id_and_topic_id(@team.id, @topic.id)
     assert_nil @bid
     post  :controller=>'sign_up_sheet', :action => 'submit_bid', :id => :LotteryTopic3, :assignment_id => :lottery_assignment
-    assert_redirected_to :action => "signup_topics", :id=>assignment_id
+    assert_redirected_to :action => :signup_topics, :id=>assignment_id
     @bid = Bid.find_by_team_id_and_topic_id(@team.id, @topic.id)
     assert_not_nil @bid
 
     assert true
   end
+
   # Given I am on the sign up topics page
   # And my team has 3 bids
   # Then I should see the 3 topics my team has bid on
+  test "check for 3 bids" do
+
+    @bid = Bid.find_by_team_id_and_topic_id(@team.id, @topic.id)
+    assert_nil @bid
+    post  :controller=>'sign_up_sheet', :action => 'submit_bid', :id => :LotteryTopic3, :assignment_id => :lottery_assignment
+    assert_redirected_to :action => :signup_topics, :id=>assignment_id
+    @bid = Bid.find_by_team_id_and_topic_id(@team.id, @topic.id)
+    assert_not_nil @bid
+
+    assert true
+  end
 
   # Given I am on the sign up topics page
   # And my team has 3 bids
   # When I press bid
   # Then I should should see a notice that says my team has reached max bids
   # And I should see no change in bid topics
-  # Replace this with your real tests.
-  test "the truth" do
+  test "stop 4 bids" do
+    post :controller=>'sign_up_sheet', :action => :sign_up_topics, :id => @assignment.id
+    assert_response :success
+    @bid = Bid.find_by_team_id_and_topic_id(@team.id, @topic.id)
+    assert_nil @bid
+    post  :controller=>'sign_up_sheet', :action => 'submit_bid', :id => :LotteryTopic3, :assignment_id => :lottery_assignment
+    assert_redirected_to :action => :signup_topics, :id=>assignment_id
+    @bid = Bid.find_by_team_id_and_topic_id(@team.id, @topic.id)
+    assert_not_nil @bid
+
     assert true
   end
 end

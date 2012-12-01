3a378c2a68ffeb4a847e9d4728a97b3f2fa76d2e
Vandit Khamker <vanditkhamker@gmail.com>
2012-11-30 22:33:18 -0500

    all Changes for E712 - Improvements to code review dashboard.

--- db/migrate/20121130200351_add_initial_and_final_line_number_to_review_comments.rb
+++ db/migrate/20121130200351_add_initial_and_final_line_number_to_review_comments.rb
@@ -1,11 +1,11 @@
-class Add < ActiveRecord::Migration
+class AddInitialAndFinalLineNumberToReviewComments < ActiveRecord::Migration
   def self.up
-    add_column :review_comments, :inital_line_number, :integer
+    add_column :review_comments, :initial_line_number, :integer
     add_column :review_comments, :last_line_number, :integer
   end
 
   def self.down
-    remove :review_comments, :inital_line_number
+    remove :review_comments, :initial_line_number
     remove :review_comments, :last_line_number
   end
 end

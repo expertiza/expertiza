e62c9f933ea7841b4d5f6eaa5d02e0683956f6e0
Chris Barile <crbarile@ncsu.edu>
2012-12-03 01:19:38 -0500

    Added step definitions to edit_instructor.rb and create_instructor.rb, and added the rewritten version of features/admin/edit_instructor.feature.
    These fix the Edit an Instructor feature.

--- features/step_definitions/edit_instructor.rb
+++ features/step_definitions/edit_instructor.rb
@@ -1,5 +1,6 @@
 And /^I edit "(\S+)" to have the name "(\S+)"$/ do |old_name,new_name|
   click_link 'Edit'
   fill_in 'user_name', :with => new_name
-  click_link 'Edit'
+  click_button 'Edit'
 end
+

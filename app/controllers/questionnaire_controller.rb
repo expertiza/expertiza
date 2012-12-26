9a2c40014ea45f33ae07322a408f0991d4cb7afb
Andrew Kofink <ajkofink@gmail.com>
2012-12-26 07:46:13 -0500

    Questionnaires can now be toggled between public and private.
    The toggle_access method was missing from the questionnaire controller.

--- app/controllers/questionnaire_controller.rb
+++ app/controllers/questionnaire_controller.rb
@@ -199,6 +199,15 @@ class QuestionnaireController < ApplicationController
       render :action => 'edit_advice'
     end
   end
+
+  # Toggle the access permission for this assignment from public to private, or vice versa
+  def toggle_access
+    questionnaire = Questionnaire.find(params[:id])
+    questionnaire.private = !questionnaire.private
+    questionnaire.save
+    
+    redirect_to :controller => 'tree_display', :action => 'list'
+  end
   
   private
   #save questionnaire object after create or edit

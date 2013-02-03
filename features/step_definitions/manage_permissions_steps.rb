8b1ba336340464cbc0561adfb4e277d59c97f514
Pamela Ocampo <pamela.ocampo@gmail.com>
2013-02-03 01:39:18 -0500

    consolidated permission step definitions

--- features/step_definitions/manage_permissions_steps.rb
+++ features/step_definitions/manage_permissions_steps.rb
@@ -30,3 +30,11 @@ end
 Given /^a permission "(\S+)" exists$/ do |permission|
   Permission.create!(:name => permission)
 end
+
+When /^I delete "(\S+)" for the role$/ do |perm_name|
+  click_link ' Remove'
+end
+
+Then /^I should not see "(\S+)" in the list of permissions$/ do |perm_name|
+  should have_no_content perm_name
+end

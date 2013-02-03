7002396e5c7602685af1a34d46a62c9b6eea9b9d
Pamela Ocampo <pamela.ocampo@gmail.com>
2013-02-03 01:54:01 -0500

    consolidated manage role step definitions

--- features/step_definitions/manage_roles_steps.rb
+++ features/step_definitions/manage_roles_steps.rb
@@ -16,3 +16,23 @@ end
 When /^a role "(\S+)" exists$/ do |role|
   Role.create!(:name => role, :description => role)
 end
+
+When /^I open the "(\S+)" page$/ do |rolename|
+  click_link rolename
+end
+
+And /^I edit the role to have name as "(\S+)"$/ do |rolename|
+  click_link 'Edit'
+  fill_in 'role_name', :with => rolename
+  click_button 'Edit'
+  click_link 'Back'
+end
+
+Then /^I delete "(\S+)"$/ do |rolename|
+  click_link rolename
+  click_link 'Delete'
+end
+
+Then /^I should not see "(\S+)" in the list of roles$/ do |rolename|
+  should have_no_content rolename
+end

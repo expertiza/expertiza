0d042cc685151a8fdfa00be4240ee7d9b8e75a5f
Andrew Kofink <ajkofink@gmail.com>
2013-02-06 14:07:40 -0500

    Updated travis and the grant_expertiza sql script to clean up unnecessary commands

--- db/grant_expertiza.sql
+++ db/grant_expertiza.sql
@@ -1,5 +1,6 @@
 drop database if exists pg_development;
 drop database if exists pg_test;
+drop user expertiza@localhost;
 
 create database if not exists pg_development CHARACTER SET = 'utf8' COLLATE = 'utf8_unicode_ci';
 create database if not exists pg_test CHARACTER SET = 'utf8' COLLATE = 'utf8_unicode_ci';

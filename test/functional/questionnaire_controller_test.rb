6ce683af4f016421ad21870994f51e762f4f794b
Chun Sing Tsui <ctsui@ncsu.edu>
2013-03-07 12:35:52 -0800

    Added two test. Also updated code so now test can be run correctly:  - Updated initial setup method with the correct authorization code.  - Added "section" to questionnaire1 in fixture.  - Added "roles" and "question_types" fixtures.

--- test/functional/questionnaire_controller_test.rb
+++ test/functional/questionnaire_controller_test.rb
@@ -11,19 +11,25 @@ class QuestionnaireControllerTest < ActionController::TestCase
   fixtures :users
   fixtures :question_advices
   fixtures :questions
-  
-  def initial
-    @controller = QuestionnaireController.new
-    @request    = ActionController::TestRequest.new
-    @response   = ActionController::TestResponse.new
-    @Questionnaire = questionnaires(:questionnaire1).id
-    @request.session[:user] = User.find( users(:superadmin).id )
+  fixtures :roles
+  fixtures :question_types
 
-     rankinfo = User.find(users(:superadmin).id).rankinfo
-    Rank.rebuild_cache
-    Rank.find(rankinfo).cache[:credentials]
-    @request.session[:credentials] = Rank.find(rankinfo).cache[:credentials]
-    AuthController.set_current_role(rankinfo,@request.session)
+  def setup
+    @controller = QuestionnaireController.new
+    @request = ActionController::TestRequest.new
+    @response = ActionController::TestResponse.new
+    @Questionnaire = questionnaires(:questionnaire1).id
+
+    @request.session[:user] = User.find(users(:superadmin).id )
+    roleid = User.find(users(:superadmin).id).role_id
+    Role.rebuild_cache
+
+    Role.find(roleid).cache[:credentials]
+    @request.session[:credentials] = Role.find(roleid).cache[:credentials]
+    # Work around a bug that causes session[:credentials] to become a YAML Object
+    @request.session[:credentials] = nil if @request.session[:credentials].is_a? YAML::Object
+    @settings = SystemSettings.find(:first)
+    AuthController.set_current_role(roleid,@request.session)
   end
   
   def test_questionnaire_edit
@@ -58,7 +64,7 @@ class QuestionnaireControllerTest < ActionController::TestCase
     post :save_advice, :id => @Questionnaire, :advice =>  { "#{Fixtures.identify(:advice0)}"=> { :advice => "test" } }   
     
     assert_response :redirect
-    assert_equal "The  Question advice in Questionnaire has been  saved", flash[:notice]
+    assert_equal "The questionnaire's question advice was successfully saved", flash[:notice]
     assert_redirected_to :action => 'list'
   end
 
@@ -71,5 +77,39 @@ class QuestionnaireControllerTest < ActionController::TestCase
                                     :instruction_loc => "http://www.expertiza.ncsu.edu"}}
     assert_response(:success)
     assert_not_nil(Questionnaire.find(:first, :conditions => ["name = ?", "test edit name"]))
+  end
+
+  # Test the toggle access action
+  def test_toggle_access
+
+    # Get its access and put it in old_access
+    questionnaire1 = Questionnaire.find_by_id(@Questionnaire)
+    old_access = questionnaire1.private
+
+    # call controller method
+    post :toggle_access, :id=>@Questionnaire
+
+    # Get its access again and put it in new_access
+    questionnaire1 = Questionnaire.find_by_id(@Questionnaire)
+    new_access = questionnaire1.private
+   
+    # test assertion
+    assert_equal(old_access, !new_access)
+
+    assert_redirected_to :controller => 'tree_display', :action => 'list'
+
+  end
+
+  # Should change count after deleting question type
+  def test_delete_question_type
+
+    # get qt_1's question id from fixtures
+    qt_1_question_id = question_types(:qt_1).question_id
+
+    # Test deleting question type by id (instance_eval to test private method)
+    assert_difference('QuestionType.count', -1) do
+      @controller.instance_eval{delete_question_type(qt_1_question_id) }
+    end
+
   end
 end

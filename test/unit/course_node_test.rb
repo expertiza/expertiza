b347faae4f7af8b6e6bfcd07c49fe3f5e78d315c
Kenny Yarboro <kbyarbor@ncsu.edu>
2013-03-10 14:05:31 -0400

    add course_node_test.rb and course_node.rb

--- test/unit/course_node_test.rb
+++ test/unit/course_node_test.rb
@@ -1,53 +1,34 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class CourseTest < ActiveSupport::TestCase
-  fixtures :courses, :users, :nodes, :assignments
+  fixtures :nodes
 
   def setup
-    @course = courses(:course1)
-    @user = users(:admin)
+    @course_node = nodes(:node23)
   end
 
   def test_get_children
-    cnode = CourseNode.new
-    cnode.get_children('name', 'ASC', 'admin', true)
-
-    assert true
+    assert(@course_node.get_children('name', 'ASC', 'admin', true) != nil, 'Should be true')
   end
 
   def test_get_name
-    cnode = CourseNode.new
-    assert(cnode.get_name != '', 'Should be true')
+    assert_equal("CSC110", @course_node.get_name)
   end
 
   def test_get_directory
-    cnode = CourseNode.new
-    assert(cnode.get_directory != '', 'Should be true')
+    assert_equal("csc110", @course_node.get_directory)
   end
 
   def test_get_creation_date
-    cnode = CourseNode.new
-    assert(cnode.get_creation_date != '', 'Should be true')
+    assert(@course_node.get_creation_date != nil, 'Should be true')
   end
 
   def test_get_modified_date
-    cnode = CourseNode.new
-    assert(cnode.get_modified_date != '', 'Should be true')
+    assert(@course_node.get_modified_date != nil, 'Should be true')
   end
 
   def test_get_teams
-    cnode = CourseNode.new
-    assert(cnode.get_teams != nil, 'Should be true')
-  end
-
-  def test_get_survey_distribution_id
-    cnode = CourseNode.new
-    assert(cnode.get_survey_distribution_id != nil, 'Should be true')
-  end
-
-  def test_get_creation_date
-    cnode = CourseNode.new
-    assert(cnode.get_survey_distribution_id != nil, 'Should be true')
+    assert(@course_node.get_teams != nil, 'Should be true')
   end
 
 end

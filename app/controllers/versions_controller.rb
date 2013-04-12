0c37681e2ade90570819efa0f2a6ad214b328916
meng <sirejulius@gmail.com>
2013-04-12 12:50:53 -0400

    undo user conflict

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -4,7 +4,7 @@ class VersionsController < ApplicationController
     @version = Version.find_by_id(params[:id])
     @versions = Version.find(:all,:conditions => ["whodunnit = ? AND created_at = ?", @version.whodunnit,@version.created_at])
     @versions.each do |v|
-      if v.whodunnit.to_i != session[:user].id
+      if v.item.versions.last.whodunnit.to_i != session[:user].id
         flash[:note] = "User #{User.find(v.whodunnit).name} already edited this item."
         redirect_to :back
       end

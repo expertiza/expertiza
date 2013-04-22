8889957f8aafea50d4524a3d139e8bf4f807de30
Hao Liu <hliu11@ncsu.edu>
2013-04-21 23:26:26 -0400

    start working on the ReviewAnalyticController

--- app/controllers/review_analytic_controller.rb
+++ app/controllers/review_analytic_controller.rb
@@ -1,3 +1,37 @@
-class ReviewAnalyticController
+class ReviewAnalyticController < ApplicationController
+  include CourseHelper
+
+  def index
+
+  end
+
+
+  def course_list
+    courses = associated_courses(session[:user])
+    course_list = Array.new
+    courses.each do |course|
+      course_list << [course.name, course.id]
+    end
+    course_list
+  end
+
+  def assignment_list(course)
+    assignments = course.assignments
+    assignment_list = Array.new
+    assignments.each do |assignment|
+      assignment_list << [assignment.name, assignment.id]
+    end
+    assignment_list
+  end
+
+  def team_list(assignment)
+    teams = assignment.teams
+    team_list = Array.new
+    teams.each do |team|
+      team_list << [team.name, team.id]
+    end
+    team_list
+  end
+
 
 end

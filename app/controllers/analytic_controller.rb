89b43577f6f93c8e1f68c47c6664a48de11e7766
Hao Liu <hliu11@ncsu.edu>
2013-04-23 02:42:25 -0400

    merge chart

--- app/controllers/analytic_controller.rb
+++ app/controllers/analytic_controller.rb
@@ -2,7 +2,9 @@ class AnalyticController < ApplicationController
   include CourseHelper
   include AnalyticHelper
 
-  def index
+  before_filter :init
+
+  def init
     #all internal not use by the page
     @available_scope_types = [:courses, :assignments, :teams]
     @selected_scope_type = nil
@@ -13,17 +15,62 @@ class AnalyticController < ApplicationController
     @available_courses = associated_courses(session[:user])
     @selected_courses = Array.new
 
-    @available_graph_data = Hash.new
-    @available_graph_data[:course] = CourseAnalytic.instance_methods
-    @available_graph_data[:assignment] = AssignmentAnalytic.instance_methods
-    @available_graph_data[:team] = AssignmentTeamAnalytic.instance_methods
+    @analytic_module = Hash.new
+    @analytic_module[:course] = CourseAnalytic
+    @analytic_module[:assignment] = AssignmentAnalytic
+    @analytic_module[:team] = AssignmentTeamAnalytic
+
     @selected_graph_data = Array.new
 
   end
 
+  def index
 
-  def possible_comparison_types
 
+
+  end
+
+  def graph_data_type_list
+    respond_to do |format|
+      format.json { render :json => @analytic_module[params[:scope].to_sym].instance_methods}
+    end
+  end
+
+  #dataPoint = [
+  #    {:name => 'review 1', :data => [9.9, 7.5, 6.4, 9.2, 4.0, 6.0, 5.6, 8.5, 6.4, 4.1, 5.6, 4.4]},
+  #    {:name => 'review 2', :data =>  [3.6, 8.8, 8.5, 3.4, 6.0, 4.5, 5.0, 4.3, 9.2, 8.5, 6.6, 9.3]},
+  #    {:name => 'review 3', :data => [8.9, 8.8, 9.3, 4.4, 7.0, 8.3, 9.0, 9.6, 5.4, 6.2, 9.3, 5.2]}
+  #]
+  #options2 = {
+  #    :title => "this is title2",
+  #    :x_axis_categories => [ 'Problem1', 'Problem2', 'Problem3', 'Problem4', 'Problem5', 'Problem6', 'Problem7', 'Problem8', 'Problem9', 'Problem10', 'Problem11','Problem12']
+  #}
+  #Chart.new(:bar, dataPoint, options2)
+  def get_graph_data_bundle
+    data_point = Array.new
+    params[:id].each do |object_id|
+      object = Object.const_get(params[:scope].capitalize).find(object_id)
+      object_data = Hash.new
+      object_data[:name] = object.name
+      object_data[:data] = gather_data(object, params[:data_type])
+      data_point << object_data
+    end
+    option = Hash.new
+    option[:x_axis_categories] = params[:data_type]
+
+    respond_to do |format|
+      format.json { render :json => Chart.new(:bar, data_point, option).data }
+    end
+  end
+
+
+
+  def gather_data(object, data_type_array)
+    data_array = Array.new
+    data_type_array.each do |data_method|
+      data_array << object.send(data_method)
+    end
+    data_array
   end
 
   def course_list

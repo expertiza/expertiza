b30e9e76422d7f3dbdcd35edd88ed4d1bb3936dc
meng <sirejulius@gmail.com>
2013-04-23 20:51:19 -0400

    user conflict message updated

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -7,7 +7,7 @@ class VersionsController < ApplicationController
     @versions.each do |v|
       if v.item
         if v.item.versions.last.whodunnit.to_i != session[:user].id
-          flash[:note] = "User #{User.find(v.item.versions.last.whodunnit).name} already edited this item."
+          flash[:note] = "User #{User.find(v.item.versions.last.whodunnit).name} edited this item since your last edit. "
           redirect_to :back
         end
       end

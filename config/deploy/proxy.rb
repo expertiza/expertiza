c91cb176b21fb35ed818628ae1099aee0ace51d6
Andrew Kofink <ajkofink@gmail.com>
2013-05-19 10:19:14 -0400

    Ignored sql dumps, and fixed the proxy cap script

--- config/deploy/proxy.rb
+++ config/deploy/proxy.rb
@@ -1,7 +1,9 @@
 puts "*** \033[1;41mUsing a PROXY to connect to the server!\033[0m"
-set :proxy_server { Capistrano::CLI.ui.ask "Proxy server: " }
-set :port         { Capistrano::CLI.ui.ask "Port: " }
+
+set :proxy_server do Capistrano::CLI.ui.ask "Proxy server: " end
+set :port         do Capistrano::CLI.ui.ask "Port: " end
 set :default_branch, 'staging'
+
 role :web, proxy_server
 role :app, proxy_server
 role :cron, proxy_server

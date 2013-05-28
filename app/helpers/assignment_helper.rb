6b31aafe4b1dfc8b5c66f56a86b9a1357ad8b5ab
Hao Liu <hliu11@ncsu.edu>
2013-05-28 15:54:55 -0400

    new assignment controller

--- app/helpers/assignment_helper.rb
+++ app/helpers/assignment_helper.rb
@@ -1,2 +1,99 @@
 module AssignmentHelper
+
+  def setup_questionnaire
+    @selected_questionnaires = Hash.new
+
+    @selected_questionnaire[:review] = @assignment.questionnaires.find_by_type('ReviewQuestionnaire')
+    @selected_questionnaire[:metareview] = @assignment.questionnaires.find_by_type('MetareviewQuestionnaire')
+    @selected_questionnaire[:feedback] = @assignment.questionnaires.find_by_type('AuthorFeedbackQuestionnaire')
+    @selected_questionnaire[:teammate] = @assignment.questionnaires.find_by_type('TeammateReviewQuestionnaire')
+
+    @available_questionnaires[:review] = ReviewQuestionnaires.find_all_by_instructor_id(@owner.id)
+    @available_questionnaires[:metareview] = MetareviewQuestionnaire.find_all_by_instructor_id(@owner.id)
+    @available_questionnaires[:feedback] = AuthorFeedbackQuestionnaire.find_all_by_instructor_id(@owner.id)
+    @available_questionnaires[:teammate] = TeammateReviewQuestionnaire.find_all_by_instructor_id(@owner.id)
+
+    if @selected_questionnaire[:review].nil?
+      @weights[:review] = @default_weights[:review]
+      @limits[:review] = @default_notification_limit
+    else
+      assignment_questionnaire = AssignmentQuestionnaire.find_by_assignment_id_and_questionnaire_id(@assignment.id, @selected_questionnaire[:review].id)
+      @weights[:review] = assignment_questionnaire.questionnaire_weight
+      @limits[:review] = assignment_questionnaire.notification_limit
+    end
+
+    if @selected_questionnaire[:metareview].nil?
+      @weights[:metareview] = @default_weights[:metareview]
+      @limits[:metareview] = @default_notification_limit
+    else
+      assignment_questionnaire = AssignmentQuestionnaire.find_by_assignment_id_and_questionnaire_id(@assignment.id, @selected_questionnaire[:metareview].id)
+      @weights[:metareview] = assignment_questionnaire.questionnaire_weight
+      @limits[:metareview] = assignment_questionnaire.notification_limit
+    end
+
+    if @selected_questionnaire[:feedback].nil?
+      @weights[:feedback] = @default_weights[:feedback]
+      @limits[:feedback] = @default_notification_limit
+    else
+      assignment_questionnaire = AssignmentQuestionnaire.find_by_assignment_id_and_questionnaire_id(@assignment.id, @selected_questionnaire[:feedback].id)
+      @weights[:feedback] = assignment_questionnaire.questionnaire_weight
+      @limits[:feedback] = assignment_questionnaire.notification_limit
+    end
+
+    if @selected_questionnaire[:teammate].nil?
+      @weights[:teammate] = @default_weights[:teammate]
+      @limits[:teammate] = @default_notification_limit
+    else
+      assignment_questionnaire = AssignmentQuestionnaire.find_by_assignment_id_and_questionnaire_id(@assignment.id, @selected_questionnaire[:teammate].id)
+      @weights[:teammate] = assignment_questionnaire.questionnaire_weight
+      @limits[:teammate] = assignment_questionnaire.notification_limit
+    end
+  end
+  def get_rubric_weights
+    @weights = Hash.new
+
+    @weights[:review] = 100
+    @weights[:metareview] = 0
+    @weights[:feedback] = 0
+    @weights[:teammate] = 0
+
+    @assignment.questionnaires.each do |questionnaire|
+      @weights[questionnaire.symbol] = questionnaire.questionnaire_weight
+    end
+  end
+  def get_notification_limits
+    @limits = Hash.new
+
+    @limits[:review] = @default_notification_limit
+    @limits[:metareview] = @default_notification_limit
+    @limits[:feedback] = @default_notification_limit
+    @limits[:teammate] = @default_notification_limit
+
+    @assignment.questionnaires.each do |questionnaire|
+      @limits[questionnaire.symbol] = questionnaire.notification_limit
+    end
+  end
+  def set_limits_and_weights
+    if session[:user].role.name == "Teaching Assistant"
+      user_id = TA.get_my_instructor(session[:user]).id
+    else
+      user_id = session[:user].id
+    end
+
+    default = AssignmentQuestionnaire.find_by_user_id_and_assignment_id_and_questionnaire_id(user_id, nil, nil)
+
+    @assignment.questionnaires.each {
+        |questionnaire|
+
+      aq = AssignmentQuestionnaire.find_by_assignment_id_and_questionnaire_id(@assignment.id, questionnaire.id)
+      if params[:limits][questionnaire.symbol].length > 0
+        aq.update_attribute('notification_limit', params[:limits][questionnaire.symbol])
+      else
+        aq.update_attribute('notification_limit', default.notification_limit)
+      end
+      aq.update_attribute('questionnaire_weight', params[:weights][questionnaire.symbol])
+      aq.update_attribute('user_id', user_id)
+    }
+  end
+
 end
--- app/models/due_date.rb
+++ app/models/due_date.rb
@@ -2,11 +2,14 @@ class DueDate < ActiveRecord::Base
   NO = 1
   LATE = 2
   OK = 3
-  
+
   belongs_to :assignment
   belongs_to :deadline_type
   validate :due_at_is_valid_datetime
 
+  def type
+    self.deadline_type.name
+  end
 
   def due_at_is_valid_datetime
     unless due_at.blank?
@@ -14,28 +17,29 @@ class DueDate < ActiveRecord::Base
     end
   end
 
-  def self.copy(old_assignment_id, new_assignment_id)    
-    duedates = find(:all, :conditions => ['assignment_id = ?',old_assignment_id])
-    duedates.each{
-      |orig_due_date|
+  def self.copy(old_assignment_id, new_assignment_id)
+    duedates = find(:all, :conditions => ['assignment_id = ?', old_assignment_id])
+    duedates.each {
+        |orig_due_date|
       new_due_date = orig_due_date.clone
       new_due_date.assignment_id = new_assignment_id
-      new_due_date.save       
-    }    
+      new_due_date.save
+    }
   end
-  
-  def self.set_duedate (duedate,deadline, assign_id, max_round)
+
+  def self.set_duedate (duedate, deadline, assign_id, max_round)
     submit_duedate=DueDate.new(duedate);
     submit_duedate.deadline_type_id = deadline;
     submit_duedate.assignment_id = assign_id
-    submit_duedate.round = max_round; 
+    submit_duedate.round = max_round;
     submit_duedate.save;
   end
-  
+
   def setFlag()
-     #puts"~~~~~~~~~enter setFlag"
-      self.flag = true
-      self.save
-     #puts"~~~~~~~~~#{self.flag.to_s}"
-    end
+    #puts"~~~~~~~~~enter setFlag"
+    self.flag = true
+    self.save
+    #puts"~~~~~~~~~#{self.flag.to_s}"
+  end
+
 end

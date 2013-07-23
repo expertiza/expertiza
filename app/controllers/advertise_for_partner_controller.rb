3e279801eb9b41b033ee285be1e9cddd5cb07e10
Ed Gehringer <efg@ncsu.edu>
2013-07-23 17:08:16 -0400

    Changed advertise_for_partners_controller to use common code for new and edit.  Created a new partial to assist in this.

--- app/controllers/advertise_for_partner_controller.rb
+++ app/controllers/advertise_for_partner_controller.rb
@@ -3,20 +3,27 @@ class AdvertiseForPartnerController < ApplicationController
   #adds a new advertise for partners entry in team table...
   def new
     puts "team #{params[:team_id]}"
-    Team.update_all({:advertise_for_partner => true}, ["id = ?", params[:team_id]])
   end
 
   #removes an entry from team table for corresponding team who requested to remove their advertisement for partner request
   def remove
-    Team.update_all({:advertise_for_partner => false}, ["id = ?", params[:team_id]])
+    team = Team.find(params[:team_id])
+    team.advertise_for_partner = false
+    team.comments_for_advertisement = nil
+    team.save
+
     assignment=Assignment.find(Team.find(params[:team_id]).parent_id)
     participant=Participant.find_by_parent_id_and_user_id(assignment.id,session[:user].id)
     redirect_to :controller => 'student_team', :action => 'view' , :id => participant.id
   end
 
   #update the team table with newly created advertise for partner request for the corresponding team
-  def add_advertise_comment
-    Team.update(params[:id],:comments_for_advertisement => params[:comment].to_s)
+  def create
+    team = Team.find(params[:id])
+    team.advertise_for_partner = true
+    team.comments_for_advertisement = params[:comments_for_advertisement]
+    team.save
+
     assignment=Assignment.find(Team.find(params[:id]).parent_id)
     participant=Participant.find_by_parent_id_and_user_id(assignment.id,session[:user].id)
     redirect_to :controller => 'student_team', :action => 'view' , :id => participant.id
@@ -25,7 +32,8 @@ class AdvertiseForPartnerController < ApplicationController
   #update the advertisement when done with editing #####This should be edit rather than update....
   def update
     @team=Team.find(params[:id])
-    @team.comments_for_advertisement = params[:team][:comments_for_advertisement]
+    #@team.comments_for_advertisement = params[:comments_for_advertisement]
+    Team.update(params[:id], :comments_for_advertisement => params[:comments_for_advertisement])
     assignment=Assignment.find(Team.find(params[:id]).parent_id)
     participant=Participant.find_by_parent_id_and_user_id(assignment.id,session[:user].id)
     if @team.save

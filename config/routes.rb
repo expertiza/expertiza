98a8bb8817320d919d91d221d11cfed7e5032a42
Andrew Kofink <andrew.kofink@scimedsolutions.com>
2013-07-22 23:45:08 -0400

    The home page shows

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,21 @@
 Expertiza::Application.routes.draw do
+  resources :pages do
+    collection do
+      get :home
+    end
+  end
+
+  resources :auth do
+    collection do
+      get :login
+    end
+  end
+
+  resources :password_retrieval do
+    collection do
+      get :forgotten
+    end
+  end
+
+  root to: 'pages#home'
 end

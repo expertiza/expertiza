25456175caf2a2847bc0340f0b0afede5e282692
Andrew Kofink <andrew.kofink@scimedsolutions.com>
2013-08-10 22:11:14 -0400

    Remove syntax error from the merged assignment model

--- db/schema.rb
+++ db/schema.rb
@@ -1,15 +1,17 @@
-# This file is auto-generated from the current state of the database. Instead of editing this file, 
-# please use the migrations feature of Active Record to incrementally modify your database, and
-# then regenerate this schema definition.
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your database schema. If you need
-# to create the application database on another system, you should be using db:schema:load, not running
-# all the migrations from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130606204315) do
+ActiveRecord::Schema.define(:version => 20130730143615) do
 
   create_table "assignment_questionnaires", :force => true do |t|
     t.integer "assignment_id"
@@ -547,7 +549,7 @@ ActiveRecord::Schema.define(:version => 20130606204315) do
 
   create_table "users", :force => true do |t|
     t.string  "name",                                     :default => "",    :null => false
-    t.string  "password",                  :limit => 40,  :default => "",    :null => false
+    t.string  "crypted_password",          :limit => 40,  :default => "",    :null => false
     t.integer "role_id",                                  :default => 0,     :null => false
     t.string  "password_salt"
     t.string  "fullname"

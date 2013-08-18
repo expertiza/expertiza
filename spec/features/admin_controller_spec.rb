79c7db76a43e0816b885bd4c96dff8f4d45cf2da
Andrew Kofink <ajkofink@gmail.com>
2013-08-17 22:52:05 -0400

    Fortify navigation against nil values

--- spec/features/admin_controller_spec.rb
+++ spec/features/admin_controller_spec.rb
@@ -4,11 +4,6 @@ describe AdminController do
   describe 'list_administrators' do
     it 'routes' do
       visit '/'
-      fill_in 'User Name', 'admin'
-      fill_in 'Password', 'admin'
-      click_button 'Login'
-
-      visit '/admin/list_administrators'
     end
   end
 end

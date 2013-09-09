4f7c5fb5269f3486481e945277d8da8ce699870f
Andrew Kofink <ajkofink@gmail.com>
2013-09-09 13:01:46 -0400

    A question has_one question_type

--- app/models/question_type.rb
+++ app/models/question_type.rb
@@ -1,6 +1,6 @@
 class QuestionType < ActiveRecord::Base
 
-    belongs_to :question, :class_name => "Question", :foreign_key => "question_id" #the question the type is for
+    belongs_to :question
 
     validates_presence_of :q_type # user must define type for the custom question
 end

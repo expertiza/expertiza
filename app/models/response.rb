1e0eab106a940d0080d4cc14d8fdebbf49673fd9
Andrew Kofink <ajkofink@gmail.com>
2013-09-27 22:14:21 -0400

    Add communication between Response and relative oo classes

--- app/models/response.rb
+++ app/models/response.rb
@@ -2,6 +2,13 @@ class Response < ActiveRecord::Base
   belongs_to :map, :class_name => 'ResponseMap', :foreign_key => 'map_id'
   has_many :scores, :class_name => 'Score', :foreign_key => 'response_id', :dependent => :destroy
 
+  delegate :questionnaire, :reviewee, :reviewer,
+    :to => :map
+
+  def team_has_user?(user)
+    reviewer.team.has_user user
+  end
+
   def display_as_html(prefix = nil, count = nil, file_url = nil)
     identifier = ""
     # The following three lines print out the type of rubric before displaying

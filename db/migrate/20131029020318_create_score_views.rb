5bf2e1cbf63d32cb3a8d86d82f556e4f5d0ffe81
Bhumika Dhotre <bvdhotre@ncsu.edu>
2013-10-28 22:53:57 -0400

    .

--- db/migrate/20131029020318_create_score_views.rb
+++ db/migrate/20131029020318_create_score_views.rb
@@ -2,7 +2,7 @@ class CreateScoreViews < ActiveRecord::Migration
 
     def self.up
       execute <<-SQL
-      CREATE VIEW scoreViews AS SELECT ques.weight question_weight,q.id AS q_id,q.q_type AS q_type,q.parameters AS q_parameters,q.question_id AS q_question_id,
+      CREATE VIEW score_views AS SELECT ques.weight question_weight,q.id AS q_id,q.q_type AS q_type,q.parameters AS q_parameters,q.question_id AS q_question_id,
       q1.id "q1_id",q1.NAME AS q1_name,q1.instructor_id AS q1_instructor_id,q1.private AS q1_private,
       q1.min_question_score AS q1_min_question_score,q1.max_question_score AS q1_max_question_score,
       q1.created_at AS q1_created_at,q1.updated_at AS q1_updated_at,q1.default_num_choices AS q1_default_num_choices,
@@ -15,7 +15,7 @@ class CreateScoreViews < ActiveRecord::Migration
     end
     def self.down
       execute <<-SQL
-      DROP VIEW scoreViews
+      DROP VIEW score_views
       SQL
     end
 

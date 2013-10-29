78153e3136c1b3d0054c9e6ec6702a09d41346c5
Bhumika Dhotre <bvdhotre@ncsu.edu>
2013-10-28 22:12:31 -0400

    changed method signature for get_total_score and return type for compute_scores and implemented views for increasing speed in get_total_score

--- app/models/score.rb
+++ app/models/score.rb
@@ -5,29 +5,32 @@ class Score < ActiveRecord::Base
   # parameters
   #  assessments - a list of assessments of some type (e.g., author feedback, teammate review)
   #  questions - the list of questions that was filled out in the process of doing those assessments
+
+
   def self.compute_scores(assessments, questions)
-    scores = Hash.new
+    scores = ParticipantScore.new
     if assessments.length > 0
-      scores[:max] = -999999999
-      scores[:min] = 999999999
+      scores.max = -999999999
+      scores.min = 999999999
       total_score = 0
       length_of_assessments=assessments.length.to_f
       q_types = Array.new
       questions.each {
           |question|
         q_types << QuestionType.find_by_question_id(question.id)
+
       }
       assessments.each {
           |assessment|
         #questionnaire = Questionnaire.find(assessment.)
 
-        curr_score = get_total_score(:response => assessment, :questions => questions, :q_types => q_types)
+        curr_score = get_total_score(assessment, questions, q_types)
 
-        if curr_score > scores[:max]
-          scores[:max] = curr_score
+        if curr_score > scores.max
+          scores.max = curr_score
         end
-        if curr_score < scores[:min]
-          scores[:min] = curr_score
+        if curr_score < scores.min
+          scores.min = curr_score
         end
 
         # Check if the review is invalid. If is not valid do not include in score calculation
@@ -38,14 +41,11 @@ class Score < ActiveRecord::Base
         total_score += curr_score
       }
       if (length_of_assessments!=0)
-        scores[:avg] = total_score.to_f / length_of_assessments
+        scores.avg = total_score.to_f / length_of_assessments
       else
-        scores[:avg]=0
+        scores.avg=0
       end
-    else
-      scores[:max] = nil
-      scores[:min] = nil
-      scores[:avg] = nil
+
     end
     return scores
   end
@@ -55,32 +55,29 @@ class Score < ActiveRecord::Base
   #  assessment - specifies the assessment for which the total score is being calculated
   #  questions  - specifies the list of questions being evaluated in the assessment
 
-  def self.get_total_score(params)
-    @response = params[:response]
-    @questions = params[:questions]
-    @q_types = params[:q_types]
+  def self.get_total_score(response, questions, q_types)
+    #@response = params[:response]
+    #@questions = params[:questions]
+    #@q_types = params[:q_types]
 
     weighted_score = 0
     sum_of_weights = 0
-    @invalid=0
+
     #Check for invalid reviews.
     #Check if the latest review done by the reviewer falls into the latest review stage
-    map=ResponseMap.find(@response.map_id)
-
-    @questionnaire = Questionnaire.find(@questions[0].questionnaire_id)
+    @questionnaire = Questionnaire.find(questions[0].questionnaire_id)
 
     x = 0
-    y = 0
     if @questionnaire.section == "Custom"
-      @questions.each {
+      questions .each {
           |question|
-        item = Score.find_by_response_id_and_question_id(@response.id, question.id)
-        if @q_types.length <= x
-          @q_types[x] = QuestionType.find_by_question_id(question.id)
+        item = Score.find_by_response_id_and_question_id(response .id, question.id)
+        if q_types .length <= x
+          q_types[x] = QuestionType.find_by_question_id(question.id)
         end
 
-        if @q_types[x].q_type == "Rating"
-          ratingPart = @q_types[x].parameters.split("::").last
+        if q_types [x].q_type == "Rating"
+          ratingPart = q_types[x].parameters.split("::").last
           if ratingPart.split("|")[0] == "1"
             if (!item.nil?)
               weighted_score += item.comments.to_i * question.weight
@@ -89,26 +86,35 @@ class Score < ActiveRecord::Base
           end
         end
         x = x + 1
+        max_question_score = @questionnaire.max_question_score
       }
     else
-      @questions.each {
-          |question|
-        item = Score.find_by_response_id_and_question_id(@response.id, question.id)
-        if item != nil
-          weighted_score += item.score * question.weight
-        end
-        sum_of_weights += question.weight
-      }
+
+      #questionnaireData = MyView.where('q1_id = ? AND s_response_id = ?',questions[0].questionnaire_id,response .id).select('q1_max_question_score ,SUM(question_weight) as sum_of_weights,SUM(question_weight * s_score) as weighted_score')
+      #questionnaireData = MyView.where('q1_id = ? AND s_response_id = ?',questions[0].questionnaire_id,response .id).select('q1_max_question_score ,SUM(question_weight) as sum_of_weights,SUM(question_weight * s_score) as weighted_score').first
+      questionnaireData = MyView.find_by_sql ["SELECT q1_max_question_score ,SUM(question_weight) as sum_of_weights,SUM(question_weight * s_score) as weighted_score FROM my_views WHERE q1_id = ? AND s_response_id = ?",questions[0].questionnaire_id,response .id]
+      weighted_score = questionnaireData[0].weighted_score.to_f
+      sum_of_weights = questionnaireData[0].sum_of_weights.to_f
+      max_question_score = questionnaireData[0].q1_max_question_score.to_f
     end
 
-    #assignment_participant = Participant.find(:all, :conditions => ["id = ?", map.reviewee_id])
+    submission_valid?(response)
 
-    due_dates = DueDate.find(:all, :conditions => ["assignment_id = ?", map.reviewed_object_id])
+    if (sum_of_weights > 0)
+      return (weighted_score / (sum_of_weights * max_question_score)) * 100
+    else
+      return -1 #indicating no score
+    end
+  end
+
+  def self.submission_valid?(response)
+    map=ResponseMap.find(response.map_id)
+    #assignment_participant = Participant.find(:all, :conditions => ["id = ?", map.reviewee_id])
+    @sorted_deadlines = nil
+    @sorted_deadlines = DueDate.find_all_by_assignment_id(map.reviewed_object_id).order('due_at DESC')
 
     # to check the validity of the response
-    if due_dates.size!=0
-      @sorted_deadlines=Array.new
-      @sorted_deadlines=due_dates.sort { |m1, m2| (m1.due_at and m2.due_at) ? m2.due_at <=> m1.due_at : (m1.due_at ? -1 : 1) }
+    if @sorted_deadlines.nil?
 
       #find the latest review deadline
       #less than current time
@@ -133,25 +139,13 @@ class Score < ActiveRecord::Base
         end
       end
 
-      resubmission_times = ResubmissionTime.find(:all, :conditions => ["participant_id = ?", map.reviewee_id], :order => "resubmitted_at DESC")
-
-      if @response.is_valid_for_score_calculation?(resubmission_times, latest_review_phase_start_time)
+      resubmission_times =   ResubmissionTime.find_all_by_participant_id(map.reviewee_id).order('resubmitted_at DESC')
+      if response .is_valid_for_score_calculation?(resubmission_times, latest_review_phase_start_time)
         @invalid = 0
       else
         @invalid = 1
       end
-
-      #if(@response.created_at < next_ele.due_at)
-      # @invalid=0
-      #else
-      # @invalid = 1
-      #end
-    end
-
-    if (sum_of_weights > 0)
-      return (weighted_score.to_f / (sum_of_weights.to_f * @questionnaire.max_question_score.to_f)) * 100
-    else
-      return -1 #indicating no score
+      return @invalid
     end
   end
 

27821ec68e31832527cec1a66f2fd1da0290691b
vshesha <vinays.sheshadri@gmail.com>
2013-10-28 22:59:29 -0400

    testing files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,9 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 
+require 'capybara/rspec'
+require 'capybara/rails'
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
@@ -35,4 +38,6 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+  config.include Capybara::DSL
+  Capybara.javascript_driver = :webkit
 end

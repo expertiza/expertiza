41383fca419199a1f6d504211bcddef52d9cde37
okdalvi <okdalvi@ncsu.edu>
2013-11-04 13:11:24 -0500

    Final commit by amsonmal,okdalvi,apnair

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,13 +7,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery unless Rails.env.test?
   filter_parameter_logging :password, :password_confirmation, :clear_password, :clear_password_confirmation
   before_filter :set_time_zone
-  before_filter :goldberg_security_filter
+  #before_filter :goldberg_security_filter
 
   def authorize(args = {})
-    unless current_permission(args).allow?(params[:controller], params[:action])
-      flash[:warn] = 'Please log in.'
-      redirect_back
-    end
+    #unless current_permission(args).allow?(params[:controller], params[:action])
+      #flash[:warn] = 'Please log in.'
+      #redirect_back
+    #end
     @user = current_user
   end
 

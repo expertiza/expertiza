ac343045ca1aa0a2fc1212e7cc1073b52cb02a57
Bhumika Dhotre <bvdhotre@ncsu.edu>
2013-11-07 00:18:07 -0500

    comments added

--- app/support/participant_scores.rb
+++ app/support/participant_scores.rb
@@ -4,14 +4,15 @@ class ParticipantScores
     @participant =  participant
   end
   def score_summary
- result = ParticipantScoreView.find_by_sql(['SELECT temp.questionaire_type,AVG(score) avg_score, MIN(score) min_score, MAX(score) max_score FROM
+    #get the score summary by averaging the scores according to question weight
+ result = ParticipantScoreView.find_by_sql(['SELECT temp_scores.questionaire_type,AVG(score) avg_score, MIN(score) min_score, MAX(score) max_score FROM
 (SELECT (SUM(score * weight)*100/ (SUM(weight) * max_question_score))score,questionaire_type
- FROM participant_score_views WHERE participant_id = ? GROUP BY response_id) temp GROUP BY questionaire_type',@participant.id] )
+ FROM participant_score_views WHERE participant_id = ? GROUP BY response_id) temp_scores GROUP BY questionaire_type',@participant.id] )
  return  result
   end
 
   def score_for_review(response_type)
-
+      #get scores per question for a particular review type
       result =  ParticipantScoreView.find_by_sql(['SELECT * FROM participant_score_views WHERE questionaire_type = ? AND participant_id = ?',response_type,@participant.id])
       return result
   end

adf3bcf16f3e81c499c47f605b01c191845329ef
Bhumika Dhotre <bvdhotre@ncsu.edu>
2013-11-07 00:20:22 -0500

    comments added

--- app/support/assignment_scores.rb
+++ app/support/assignment_scores.rb
@@ -4,9 +4,10 @@ class Assignment_scores
   end
 
   def scores
-    result = ParticipantScoreView.find_by_sql(['SELECT temp.team_id,temp.questionaire_type,AVG(score) avg_score, MIN(score) min_score, MAX(score) max_score FROM
+    #get score summary for a particular assignment
+    result = ParticipantScoreView.find_by_sql(['SELECT temp_scores.team_id,temp.questionaire_type,AVG(score) avg_score, MIN(score) min_score, MAX(score) max_score FROM
     (SELECT (SUM(score * weight)*100/ (SUM(weight) * max_question_score))score,questionaire_type,team_id
-    FROM participant_score_views WHERE assignment_id = ? GROUP BY response_id) temp GROUP BY team_id,questionaire_type',@assignment.id]);
+    FROM participant_score_views WHERE assignment_id = ? GROUP BY response_id) temp_scores GROUP BY team_id,questionaire_type',@assignment.id]);
     return result
   end
 

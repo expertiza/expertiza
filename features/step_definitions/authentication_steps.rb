547811b2d51cd39f01ec69431bb2a79e9f09e364
Daniel Kernicky <daniel.kernicky@gmail.com>
2013-11-17 15:08:05 -0500

    fixed authentication step
    3 more scenarios pass, 111 more steps, >1% more coverage

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -58,7 +58,7 @@ Given /an? (Student|Teaching Assistant|Instructor|Administrator|Super-Administra
     
   User.create({
     :name => name,
-    :fullname => name,
+    :fullname => name + ", " + name,
     :password => 'password',
     :password_confirmation => 'password',
     :role => Role.find_by_name!(user_type),

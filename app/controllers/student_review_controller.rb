15164bf13034aa1b23e3ea073fcdf89c6383ae56
Ankit <ankit.bhutani7@gmail.com>
2013-12-04 03:46:26 -0500

    debugging for e914

--- app/controllers/student_review_controller.rb
+++ app/controllers/student_review_controller.rb
@@ -44,7 +44,7 @@ class StudentReviewController < ApplicationController
       deadline_type_id = DeadlineType.find_by_name('review').id
 
       @metareview_responses.each do |metareview_response|
-        review_resp = ResponseMap.find_by_id(metareview_response.reviewed_object_id)
+        review_resp = Response.find_by_id(metareview_response.reviewed_object_id)
         if review_resp
           #ACS Removed the if condition(and corressponding else) which differentiate assignments as team and individual assignments
           # to treat all assignments as team assignments

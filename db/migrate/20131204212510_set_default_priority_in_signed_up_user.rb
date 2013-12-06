31f37fa57b239513d6c9204831a8bb2cb54e1d0c
Bhumika Dhotre <bvdhotre@ncsu.edu>
2013-12-05 21:22:44 -0500

    code merged from last years project

--- db/migrate/20131204212510_set_default_priority_in_signed_up_user.rb
+++
@@ -1,9 +0,0 @@
-class SetDefaultPriorityInSignedUpUser < ActiveRecord::Migration
-  def self.up
-    change_column :signed_up_users, :preference_priority_number, :integer, :default => 0
-  end
-
-  def self.down
-    change_column :signed_up_users, :preference_priority_number, :integer
-  end
-end

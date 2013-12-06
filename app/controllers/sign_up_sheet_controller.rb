32a7b4f8407f1f3010941790dae6d7ceb3950308
cececec <ganqhuang@gmail.com>
2013-12-06 17:41:04 -0500

    Sign_up_sheet revamped

--- app/controllers/sign_up_sheet_controller.rb
+++ app/controllers/sign_up_sheet_controller.rb
@@ -241,7 +241,7 @@ class SignUpSheetController < ApplicationController
     assignment=Assignment.find(params[:id])
 
     if assignment.due_dates.find_by_deadline_type_id(1)!= nil
-      unless assignment.staggered_deadline? and assignment.due_dates.find_by_deadline_type_id(1).due_at < Time.now
+      if !assignment.staggered_deadline? and assignment.due_dates.find_by_deadline_type_id(1).due_at < Time.now
         @show_actions = false
       end
     end
@@ -274,8 +274,93 @@ class SignUpSheetController < ApplicationController
     #Always use team_id ACS
 
     #check whether the user already has a team for this assignment
-    SignupSheet.signup_team(@assignment, @user_id, params[:id])
+    signup_team(@assignment.id, @user_id, params[:id])
     redirect_to :action => 'list', :id => params[:assignment_id]
+  end
+
+  def signup_team(assignment_id, user_id, topic_id)
+    puts   assignment_id
+    puts   user_id
+    puts   topic_id
+    users_team = SignedUpUser.find_team_users(assignment_id, user_id)
+    if users_team.size == 0
+      #if team is not yet created, create new team.
+      team = AssignmentTeam.create_team_and_node(assignment_id)
+      user = User.find(user_id)
+      teamuser = create_team_users(user, team.id)
+      confirmationStatus = confirmTopic(team.id, topic_id, assignment_id)
+    else
+      confirmationStatus = confirmTopic(users_team[0].t_id, topic_id, assignment_id)
+    end
+  end
+
+  def confirmTopic(creator_id, topic_id, assignment_id)
+    #check whether user has signed up already
+    user_signup = otherConfirmedTopicforUser(assignment_id, creator_id)
+
+    sign_up = SignedUpUser.new
+    sign_up.topic_id = params[:id]
+    sign_up.creator_id = creator_id
+    result = false
+    if user_signup.size == 0
+
+      # Using a DB transaction to ensure atomic inserts
+      ActiveRecord::Base.transaction do
+        #check whether slots exist (params[:id] = topic_id) or has the user selected another topic
+        if slotAvailable?(topic_id)
+          sign_up.is_waitlisted = false
+
+          #Update topic_id in participant table with the topic_id
+          participant = Participant.find_by_user_id_and_parent_id(session[:user].id, assignment_id)
+
+          participant.update_topic_id(topic_id)
+        else
+          sign_up.is_waitlisted = true
+        end
+        if sign_up.save
+          result = true
+        end
+      end
+    else
+      #If all the topics choosen by the user are waitlisted,
+      for user_signup_topic in user_signup
+        if user_signup_topic.is_waitlisted == false
+          flash[:error] = "You have already signed up for a topic."
+          return false
+        end
+      end
+
+      # Using a DB transaction to ensure atomic inserts
+      ActiveRecord::Base.transaction do
+        #check whether user is clicking on a topic which is not going to place him in the waitlist
+        if !slotAvailable?(topic_id)
+          sign_up.is_waitlisted = true
+          if sign_up.save
+            result = true
+          end
+        else
+          #if slot exist, then confirm the topic for the user and delete all the waitlist for this user
+          SignUpTopic.cancel_all_waitlists(creator_id, assignment_id)
+          sign_up.is_waitlisted = false
+          sign_up.save
+          participant = Participant.find_by_user_id_and_parent_id(session[:user].id, assignment_id)
+          participant.update_topic_id(topic_id)
+          result = true
+        end
+      end
+    end
+
+    result
+  end
+
+  def otherConfirmedTopicforUser(assignment_id, creator_id)
+    user_signup = SignedUpUser.find_user_signup_topics(assignment_id, creator_id)
+    user_signup
+  end
+
+  # When using this method when creating fields, update race conditions by using db transactions
+  def slotAvailable?(topic_id)
+    SignUpTopic.slotAvailable?(topic_id)
   end
 
   # When using this method when creating fields, update race conditions by using db transactions

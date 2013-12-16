f1c77cf9bf5497dbb8cc7e89b51cda9dfbad1eda
Andrew Kofink <ajkofink@gmail.com>
2013-12-16 17:35:26 -0500

    Add a 'down' method for this migration

--- db/migrate/20131122223434_rename_is_lottery.rb
+++ db/migrate/20131122223434_rename_is_lottery.rb
@@ -1,8 +1,9 @@
-class ChangeColumnName < ActiveRecord::Migration
+class RenameIsLottery < ActiveRecord::Migration
   def self.up
     rename_column :assignments, :is_lottery, :is_intelligent
   end
 
   def self.down
+    rename_column :assignments, :is_intelligent, :is_lottery
   end
 end

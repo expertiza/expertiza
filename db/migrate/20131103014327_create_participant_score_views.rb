e98f9c312f66e22ed1cd5dcd9e310946b8e56c41
Andrew Kofink <ajkofink@gmail.com>
2013-12-16 22:49:31 -0500

    Revert "Merge branch 'e806' of git://github.com/akofink/expertiza into akofink-e806"
    This reverts commit 0c6d99daae9fc89207934a5f351be5a8532dc4df.

--- db/migrate/20131103014327_create_participant_score_views.rb
+++ db/migrate/20131103014327_create_participant_score_views.rb
@@ -3,7 +3,7 @@ class CreateParticipantScoreViews < ActiveRecord::Migration
     execute <<-SQL
       CREATE VIEW participant_score_views AS SELECT r.id response_id,s.score,q.weight,qs.name questionaire_type,qs.max_question_score,t.id as team_id,user_id as participant_id , t.parent_id as assignment_id
  FROM scores s ,responses r, questions q, questionnaires qs , teams_users tu , teams t
- WHERE r.id=s.response_id AND q.id = s.question_id AND qs.id = q.questionnaire_id    AND tu.team_id = r.reviewee_id   AND tu.team_id = t.id
+ WHERE  r.id=s.response_id AND q.id = s.question_id AND qs.id = q.questionnaire_id    AND tu.team_id = r.reviewee_id   AND tu.team_id = t.id
     SQL
   end
   def self.down

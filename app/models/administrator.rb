9475d864d9c8714be3bdb1fd16ce5d5ba095207a
Andrew Kofink <ajkofink@gmail.com>
2014-02-17 16:12:20 -0500

    Update deprecated ActiveRecord calls

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -16,19 +16,15 @@ class Administrator < User
                 ['All public assignments','list_all']]
 
   def list_instructors(object_type, user_id)
-    object_type.find(:all,
-                     :joins => "inner join users on " + object_type.to_s.pluralize + ".instructor_id = users.id AND users.parent_id = " + user_id.to_s)
+    object_type.joins( "inner join users on " + object_type.to_s.pluralize + ".instructor_id = users.id AND users.parent_id = " + user_id.to_s)
   end
 
   def list_all(object_type, user_id)
-    object_type.find(:all,
-                     :conditions => ["instructor_id = ? OR private = 0", user_id])
+    object_type.where( ["instructor_id = ? OR private = 0", user_id])
   end
 
   # This method gets a questionnaire or an assignment, making sure that current user is allowed to see it.
   def get(object_type, id, user_id)
-    object_type.find(:first,
-                     :conditions => ["id = ? AND (instructor_id = ? OR private = 0)",
-                                     id, user_id]) # You are allowed to get it if it is public, or if your id is the one that created it.
+    object_type.where( ["id = ? AND (instructor_id = ? OR private = 0)", id, user_id]) # You are allowed to get it if it is public, or if your id is the one that created it.
   end
 end

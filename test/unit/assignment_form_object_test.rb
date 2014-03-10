1c18447ae7b05a744db96e296d08d3f255e68b6c
losmescaleros <mdnevill@ncsu.edu>
2014-03-10 19:54:01 -0400

    Added unit test for assignment form object.

---
+++ test/unit/assignment_form_object_test.rb
@@ -0,0 +1,24 @@
+require 'test/unit'
+require 'test_helper'
+
+class AssignmentFormObjectTest < Test::Unit::TestCase
+
+  # Called before every test method runs. Can be used
+  # to set up fixture information.
+  def setup
+    # Do nothing
+  end
+
+  # Called after every test method runs. Can be used to tear
+  # down fixture information.
+
+  def teardown
+    # Do nothing
+  end
+
+  # Fake test
+  def test_fail
+
+    fail('Not implemented')
+  end
+end

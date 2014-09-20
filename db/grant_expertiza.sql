a06cb2b96d680bc6e45902ef8ea8cc747db8435e
Yatish Mehta <yatishmehta27@gmail.com>
2014-09-20 11:41:39 -0400

    Modified name changes in .sql

--- db/grant_expertiza.sql
+++ db/grant_expertiza.sql
@@ -1,10 +1,10 @@
-drop database if exists pg_development;
-drop database if exists pg_test;
+drop database if exists expertiza_development;
+drop database if exists expertiza_test;
 
-create database if not exists pg_development CHARACTER SET = 'utf8' COLLATE = 'utf8_unicode_ci';
-create database if not exists pg_test CHARACTER SET = 'utf8' COLLATE = 'utf8_unicode_ci';
+create database if not exists expertiza_development CHARACTER SET = 'utf8' COLLATE = 'utf8_unicode_ci';
+create database if not exists expertiza_test CHARACTER SET = 'utf8' COLLATE = 'utf8_unicode_ci';
 
 create user expertiza@localhost;
 
-grant all on pg_development.* to expertiza@localhost;
-grant all on pg_test.* to expertiza@localhost;
+grant all on expertiza_development.* to expertiza@localhost;
+grant all on expertiza_test.* to expertiza@localhost;

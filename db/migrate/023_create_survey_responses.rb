821d67f4f5297a05cee5e18be5d6a360cca3967e
Karthik Bhupalam <kbhupal@ncsu.edu>
2014-10-22 02:40:25 -0400

    setup-changes

--- db/migrate/023_create_survey_responses.rb
+++ db/migrate/023_create_survey_responses.rb
@@ -1,14 +1,15 @@
 class CreateSurveyResponses < ActiveRecord::Migration
   def self.up
   create_table "survey_responses", :force => true do |t|
-    t.column "score", :integer, :limit => 10
+    t.column "score", :integer, :limit => 8
     t.column "comments", :text
-    t.column "assignment_id", :integer, :limit => 10, :default => 0, :null => false
-    t.column "question_id", :integer, :limit => 10, :default => 0, :null => false
-    t.column "survey_id", :integer, :limit => 10, :default => 0, :null => false
+    t.column "assignment_id", :integer, :limit => 8, :default => 0, :null => false
+    t.column "question_id", :integer, :limit => 8, :default => 0, :null => false
+    t.column "survey_id", :integer, :limit => 8, :default => 0, :null => false
     t.column "email", :string
   end
-  
+=begin
+
   add_index "survey_responses", ["assignment_id"], :name => "fk_survey_assignments"
 
   execute "alter table survey_responses
@@ -26,7 +27,8 @@ class CreateSurveyResponses < ActiveRecord::Migration
   execute "alter table survey_responses 
              add constraint fk_survey_questionnaires
              foreign key (survey_id) references questionnaires(id)" 
-  
+
+=end
   end
 
   def self.down
--- db/migrate/045_menu_update_impersonate.rb
+++ db/migrate/045_menu_update_impersonate.rb
@@ -6,12 +6,14 @@ class MenuUpdateImpersonate < ActiveRecord::Migration
     if site_controller == nil
        site_controller = SiteController.create(:name => 'impersonate', :permission_id => permission1.id, :builtin => 0)
     end
-    action = ControllerAction.find(:first, :conditions => ['name = "start" and site_controller_id = ?',site_controller.id])
+    #action = ControllerAction.find(:first, :conditions => ['name = "start" and site_controller_id = ?',site_controller.id])
+    action = ControllerAction.where(:site_controller_id=>site_controller.id, :name=>'start').first
     if action == nil
       action = ControllerAction.create(:name => 'start', :site_controller_id => site_controller.id)
     end
     
-    action2 = ControllerAction.find(:first, :conditions => ['name = "restore" and site_controller_id = ?',site_controller.id])
+    #action2 = ControllerAction.find(:first, :conditions => ['name = "restore" and site_controller_id = ?',site_controller.id])
+    action2 = ControllerAction.where(:site_controller_id=>site_controller.id, :name=>'restore').first
     if action2 == nil
       action2 = ControllerAction.create(:name => 'restore', :site_controller_id => site_controller.id, :permission_id => permission2.id)
     end
@@ -24,7 +26,8 @@ class MenuUpdateImpersonate < ActiveRecord::Migration
   def self.down
     site_controller = SiteController.find_by_name('impersonate')
     if site_controller 
-    controllers = ControllerAction.find(:all, :conditions => ['site_controller_id = ?',site_controller.id])
+    #controllers = ControllerAction.find(:all, :conditions => ['site_controller_id = ?',site_controller.id])
+    controllers = ControllerAction.where(:site_controller_id=>site_controller.id).all
     controllers.each {|controller|
        if controller 
         menuItem = MenuItem.find_by_controller_action_id(controller.id)

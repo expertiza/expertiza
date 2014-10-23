ed3e9f87c7abcd2879678ed5ba2160bde80a4e03
Kevin Schneiderman <kevin.schneiderman@gmail.com>
2014-10-22 20:39:35 -0400

    Finished the reverted changes

--- db/migrate/20140808212437_add_categories.rb
+++ db/migrate/20140808212437_add_categories.rb
@@ -1,27 +1,15 @@
 class AddCategories < ActiveRecord::Migration
   def self.up
-    -    add_column :nodes, :name, :string
-    -    add_column :nodes, :lft, :integer
-    -    add_column :nodes, :rgt, :integer
-    -    add_column :nodes, :depth, :integer
-    +    create_table :categories do |t|
-    Andrew KofinkCollaborator
-    akofink added a note 2 days ago
-    Revert this change. It looks like you haven't pulled from the rails4 branch. This will only complicate things for you. Please pull often.
-Add a line note
-+      t.string :name
-+      t.integer :parent_id
-+      t.integer :lft
-+      t.integer :rgt
-+      t.integer :depth # this is optional.
-+    end
-   end
+        add_column :nodes, :name, :string
+        add_column :nodes, :lft, :integer
+        add_column :nodes, :rgt, :integer
+        add_column :nodes, :depth, :integer
+       end
  
    def self.down
--    remove_column :nodes, :name
--    remove_column :nodes, :lft
--    remove_column :nodes, :rgt
--    remove_column :nodes, :depth
-+    drop_table :categories
+    remove_column :nodes, :name
+    remove_column :nodes, :lft
+    remove_column :nodes, :rgt
+    remove_column :nodes, :depth
    end
  end

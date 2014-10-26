7a5c3d0b2e00b878a37c87e19f9c8c9d9e6d6ae0
Ankit Agrawal <aagrawa5@ncsu.edu>
2014-10-25 20:59:06 -0400

    Renamed 'list' method StudentQuizzesController to 'index' as per RESTful guidlines.
    Code-cleanup : removed unnecessary instance variables and if-blocks from 'index' method.

--- app/controllers/student_quizzes_controller.rb
+++ app/controllers/student_quizzes_controller.rb
@@ -1,37 +1,10 @@
 class StudentQuizzesController < ApplicationController
-  def list
-    @participant = AssignmentParticipant.find(params[:id])
-    return unless current_user_id?(@participant.user_id)
 
-    @assignment = Assignment.find(@participant.parent_id)
-
-    # Find the current phase that the assignment is in.
-    @quiz_phase = @assignment.get_current_stage(AssignmentParticipant.find(params[:id]).topic_id)
-
-    @quiz_mappings = QuizResponseMap.where(reviewer_id: @participant.id)
-
-    # Calculate the number of quizzes that the user has completed so far.
-    @num_quizzes_total = @quiz_mappings.size
-
-    @num_quizzes_completed = 0
-    @quiz_mappings.each do |map|
-      @num_quizzes_completed += 1 if map.response
-    end
-
-    if @assignment.staggered_deadline?
-      @quiz_mappings.each { |quiz_mapping|
-        if @assignment.team_assignment?
-          participant = AssignmentTeam.get_first_member(quiz_mapping.reviewee_id)
-        else
-          participant = quiz_mapping.reviewee
-        end
-
-        if !participant.nil? and !participant.topic_id.nil?
-          quiz_due_date = TopicDeadline.where(topic_id: participant.topic_id, deadline_type_id: 1).first
-        end
-      }
-      deadline_type_id = DeadlineType.find_by_name('quiz').id
-    end
+  def index
+    participant = AssignmentParticipant.find(params[:id])
+    return unless current_user_id?(participant.user_id)
+    @assignment = Assignment.find(participant.parent_id)
+    @quiz_mappings = QuizResponseMap.where(reviewer_id: participant.id)
   end
 
   def finished_quiz

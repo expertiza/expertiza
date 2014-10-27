f4b593593523417b516d6ed1a376347cd3ad6165
Kevin Schneiderman <kevin.schneiderman@gmail.com>
2014-10-27 19:21:13 -0400

    Added one test for set student

--- spec/student_teams_controller_spec.rb
+++ spec/student_teams_controller_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe StudentTeamsController do
-  describe 'student should be set when view, create, edit, and leave are called'
-    StudentTeamsController.should_recieve
+  let (:student_teams_controller) {StudentTeamsController.new}
+  describe 'set student should be called when view is called'
+    Student.should_receive(:find).with('12345')
+    student_teams_controller.stub(:view)
+    post :view, {student_id: 12345}
 end

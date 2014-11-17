03636cf32abde6d766af6426dd2262430a73d16d
jmkubasc <jmkubasc@ncsu.edu>
2014-11-16 19:52:57 -0500

    Able to add a student to an assignment and have the student navigate to the assignment.

--- spec/factories/student_factory.rb
+++ spec/factories/student_factory.rb
@@ -1,12 +1,42 @@
 FactoryGirl.define do
-  factory :student, class: User do
-    sequence(:name)       { |n| "student#{n}" }
-    sequence(:email)      { |n| "student#{n}@mailinator.com"}
+  factory :user do
     password              "password"
     password_confirmation "password"
-    role                  { Role.find_by_name('student') }
     is_new_user           0
 
-  #  association :role, factory: :student_role
+    factory :instructor do
+      name  "instructor"
+      email "instructor@mailinator.com"
+      role  { Role.find_by_name('instructor') }
+    end
+
+    factory :student do
+      sequence(:name)  { |n| "student#{n}" }
+      sequence(:email) { |n| "student#{n}@mailinator.com"}
+      role             { Role.find_by_name('student') }
+    end
+  end
+
+  factory :assignment do
+    sequence(:name)           { |n| "assignment#{n}" }
+    sequence(:directory_path) { |n| "assignment#{n}_path" }
+    submitter_count           0
+    course_id                 0
+    instructor
+    private                   0
+    num_reviews               0
+    num_review_of_reviews     0
+    num_review_of_reviewers   0
+    wiki_type                 { WikiType.find_by_name('No') }
+    num_reviewers             0
+    max_team_size             3
+    staggered_deadline        0
+    review_topic_threshold    0
+    copy_flag                 0
+    rounds_of_reviews         1
+    microtask                 0
+    num_quiz_questions        0
+    calculate_penalty         0
+    is_penalty_calculated     0
   end
 end
--- spec/features/student_add_someone_to_team_spec.rb
+++ spec/features/student_add_someone_to_team_spec.rb
@@ -2,12 +2,17 @@ describe 'Student adds someone to team', :type => :feature do
   it 'sanity check' do
     student = FactoryGirl.create :student
 
+    assignment = FactoryGirl.create :assignment
+    assignment.add_participant(student.name)
+
     visit root_path
 
     fill_in 'User Name', with: student.name
     fill_in 'Password', with: student.password
     click_on 'Login'
 
-    expect(page).to have_content('Assignments')
+    click_link assignment.name
+
+    expect(page).to have_content('Submit or Review work for')
   end
 end

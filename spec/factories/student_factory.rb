f485a2c12f2942f1d9f39d4e254de86ed3fc740a
jmkubasc <jmkubasc@ncsu.edu>
2014-11-16 21:13:00 -0500

    Able to create team and send invite to a student

--- spec/factories/student_factory.rb
+++ spec/factories/student_factory.rb
@@ -6,37 +6,50 @@ FactoryGirl.define do
 
     factory :instructor do
       name  "instructor"
-      email "instructor@mailinator.com"
+      email { "#{name}@mailinator.com" }
       role  { Role.find_by_name('instructor') }
     end
 
+    # Can create many unique students
     factory :student do
-      sequence(:name)  { |n| "student#{n}" }
-      sequence(:email) { |n| "student#{n}@mailinator.com"}
-      role             { Role.find_by_name('student') }
+      sequence(:name) { |n| "student#{n}" }
+      email           { "#{name}@mailinator.com" }
+      role            { Role.find_by_name('student') }
     end
   end
 
   factory :assignment do
-    sequence(:name)           { |n| "assignment#{n}" }
-    sequence(:directory_path) { |n| "assignment#{n}_path" }
-    submitter_count           0
-    course_id                 0
+    name                    "assignment"
+    directory_path          { "#{name}_path" }
+    submitter_count         0
+    course_id               0
     instructor
-    private                   0
-    num_reviews               0
-    num_review_of_reviews     0
-    num_review_of_reviewers   0
-    wiki_type                 { WikiType.find_by_name('No') }
-    num_reviewers             0
-    max_team_size             3
-    staggered_deadline        0
-    review_topic_threshold    0
-    copy_flag                 0
-    rounds_of_reviews         1
-    microtask                 0
-    num_quiz_questions        0
-    calculate_penalty         0
-    is_penalty_calculated     0
+    private                 0
+    num_reviews             0
+    num_review_of_reviews   0
+    num_review_of_reviewers 0
+    wiki_type               { WikiType.find_by_name('No') }
+    num_reviewers           0
+    max_team_size           3
+    staggered_deadline      0
+    review_topic_threshold  0
+    copy_flag               0
+    rounds_of_reviews       1
+    microtask               0
+    num_quiz_questions      0
+    calculate_penalty       0
+    is_penalty_calculated   0
+
+    # Need to create a node after creating an assignment
+    after(:create) { |assignment| assignment.create_node  }
+  end
+
+  factory :sign_up_topic do
+    sequence(:topic_name) { |n| "topic#{n}" }
+    assignment
+    max_choosers          3
+    category              { "#{topic_name}_category" }
+    topic_identifier      1
+    micropayment          0
   end
 end
--- spec/features/student_add_someone_to_team_spec.rb
+++ spec/features/student_add_someone_to_team_spec.rb
@@ -1,18 +1,37 @@
 describe 'Student adds someone to team', :type => :feature do
-  it 'sanity check' do
-    student = FactoryGirl.create :student
+  it 'send invite to student' do
+    student1 = FactoryGirl.create :student
+    student2 = FactoryGirl.create :student
 
     assignment = FactoryGirl.create :assignment
-    assignment.add_participant(student.name)
+    assignment.add_participant student1.name
+    assignment.add_participant student2.name
+
+    topic = FactoryGirl.create :sign_up_topic, assignment: assignment
 
     visit root_path
 
-    fill_in 'User Name', with: student.name
-    fill_in 'Password', with: student.password
+    # Log in as student1
+    fill_in 'login_name', with: student1.name
+    fill_in 'login_password', with: student1.password
     click_on 'Login'
 
+    # Navigate to the assignment
     click_link assignment.name
+    click_link 'Your team'
 
-    expect(page).to have_content('Submit or Review work for')
+    # Create a team
+    fill_in 'team_name', with: 'TestTeamName'
+    click_on 'Create Team'
+
+    # Expect team name to be displayed
+    expect(page).to have_content('TestTeamName')
+
+    # Invite student2 to the team
+    fill_in 'user_name', with: student2.name
+    click_on 'Invite'
+
+    # Expect student2 to show up under 'Sent Invitations'
+    expect(page).to have_content(student2.name)
   end
 end

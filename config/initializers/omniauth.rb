fba022b07e00450607812c6f913a5e9c702db767
Karthik Bhupalam <kbhupal@ncsu.edu>
2014-12-02 20:15:05 -0500

    Added google plus login image and few comments

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
+# Secret client and secret key configuration for google app
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, '1025716012590-2nlv84m8uo3pled9bide8442r2020gc3.apps.googleusercontent.com', 'kWjKq314ywRKAF5s5A0F8Wxh',
            {client_options: {ssl: {verify: false}}}

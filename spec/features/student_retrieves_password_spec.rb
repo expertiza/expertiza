2a3476af4c9fedb402da7f4ace9fe8f8f8093fac
jmgoldsw <jmgoldsw@ncsu.edu>
2014-12-08 21:59:16 -0500

    Condensed code by removing white space.
     On branch rails4
     Your branch is up-to-date with 'origin/rails4'.
    
     Changes to be committed:
    	modified:   student_retrieves_password_spec.rb

--- spec/features/student_retrieves_password_spec.rb
+++ spec/features/student_retrieves_password_spec.rb
@@ -5,7 +5,6 @@ describe 'Student retrieves password', :type => :feature do
 
     # Traverse to the password retrieval page by way of the link.
     click_link 'Forgotten your password?'
-
     expect(page).to have_content('Forgotten Your Password?')
   end
 
@@ -31,45 +30,34 @@ describe 'Student retrieves password', :type => :feature do
     student1 = FactoryGirl.create :student
 
     visit forgotten_password_retrieval_index_path
-
     expect(page).to have_content('Forgotten Your Password?')
 
     fill_in 'user_email', with: student1.email
-
     click_on 'Request password'
-
     expect(page).to have_content('A new password has been sent to your e-mail address.')
   end
 
 
   scenario 'with invalid e-mail' do
     visit forgotten_password_retrieval_index_path
-
     expect(page).to have_content('Forgotten Your Password?')
-
     bogus_email = 'bogus'
 
     # NOTE: The behavior of an improperly formatted and a non-existent
     # e-mail address is the same.
     fill_in 'user_email', with: bogus_email
-
     click_on 'Request password'
-
     expect(page).to have_content('No account is associated with the address, "' + bogus_email + '". Please try again.')
   end
 
 
   scenario 'with blank e-mail' do
     visit forgotten_password_retrieval_index_path
-
     expect(page).to have_content('Forgotten Your Password?')
 
     blank_email = ''
-
     fill_in 'user_email', with: blank_email
-
     click_on 'Request password'
-
     expect(page).to have_content('Please enter an e-mail address')
   end
 end

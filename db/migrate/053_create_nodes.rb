1170ac0aea3b6ed13086a9f44b8dc1733136aa44
xshao2 <xshao2@ncsu.edu>
2014-12-09 23:10:15 -0500

    try to fix db migrate bug but failed

--- db/migrate/053_create_nodes.rb
+++ db/migrate/053_create_nodes.rb
@@ -1,13 +1,32 @@
+# class CreateNodes < ActiveRecord::Migration
+#   def self.up
+#     create_table :nodes do |t|
+#       t.column :parent_id, :integer
+#       t.column :node_object_id, :integer
+#       t.column :type, :string
+#     end
+#   end
+#
+#   def self.down
+#     drop_table :nodes
+#   end
+# end
+
+
+
+
 class CreateNodes < ActiveRecord::Migration
-  def self.up
-    create_table :nodes do |t|
+def self.up
+create_table :nodes do |t|
       t.column :parent_id, :integer
       t.column :node_object_id, :integer
-      t.column :type, :string            
-    end           
+      t.column :type, :string
+      t.column :lft, :integer
+      t.column :rgt, :integer
+    end
   end
 
-  def self.down
-    drop_table :nodes
+def self.down
+	    drop_table :nodes
   end
 end

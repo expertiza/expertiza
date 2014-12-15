9f6b2132d73378db809513f3adab07b6ddc363fa
Andrew Kofink <ajkofink@gmail.com>
2014-12-15 10:45:08 -0500

    Update config, gems and db to the rails4 branch

--- db/migrate/114_update_controllers_for_response.rb
+++ db/migrate/114_update_controllers_for_response.rb
@@ -2,7 +2,7 @@ class UpdateControllersForResponse < ActiveRecord::Migration
   def self.up
     perm = Permission.find_by_name("do assignments")
     
-    controller = SiteController.find_or_create_by_name("response")
+    controller = SiteController.find_or_create_by(name: "response")
     if controller
       controller.permission_id = perm.id
       controller.save
@@ -10,31 +10,31 @@ class UpdateControllersForResponse < ActiveRecord::Migration
     
     controller = SiteController.find_by_name("review")
     if controller 
-      ControllerAction.find_all_by_site_controller_id(controller.id).each{|action| action.destroy}
+      ControllerAction.where(site_controller_id: controller.id).each{|action| action.destroy}
       controller.destroy
     end
     
     controller = SiteController.find_by_name("reviewing")
     if controller
-      ControllerAction.find_all_by_site_controller_id(controller.id).each{|action| action.destroy}
+      ControllerAction.where(site_controller_id: controller.id).each{|action| action.destroy}
       controller.destroy
     end
     
     controller = SiteController.find_by_name("review_feedback")
     if controller
-      ControllerAction.find_all_by_site_controller_id(controller.id).each{|action| action.destroy}
+      ControllerAction.where(site_controller_id: controller.id).each{|action| action.destroy}
       controller.destroy
     end
     
     controller = SiteController.find_by_name("review_of_review")
     if controller
-      ControllerAction.find_all_by_site_controller_id(controller.id).each{|action| action.destroy}
+      ControllerAction.where(site_controller_id: controller.id).each{|action| action.destroy}
       controller.destroy    
     end
     
     controller = SiteController.find_by_name("teammate_review")
     if controller
-      ControllerAction.find_all_by_site_controller_id(controller.id).each{|action| action.destroy}
+      ControllerAction.where(site_controller_id: controller.id).each{|action| action.destroy}
       controller.destroy
     end
     

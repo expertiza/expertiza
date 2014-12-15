5a69809ac7c5ec551abf61ad65e21a8bef66ee83
Andrew Kofink <ajkofink@gmail.com>
2014-12-15 10:45:08 -0500

    Update config, gems and db to the rails4 branch

--- db/migrate/20130418215246_update_controller_actions_for_versions_revert.rb
+++ db/migrate/20130418215246_update_controller_actions_for_versions_revert.rb
@@ -2,7 +2,7 @@ class UpdateControllerActionsForVersionsRevert < ActiveRecord::Migration
   def self.up
     @permission = Permission.find_by_name('public actions - execute')
     @controller = SiteController.find_by_name('versions')
-    @action = ControllerAction.find_or_create_by_name('revert')
+    @action = ControllerAction.find_or_create_by(name: 'revert')
     @action.site_controller_id = @controller.id
     @action.permission_id = @permission.id
     @action.save

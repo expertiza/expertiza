7b66db8f917f31ff1446705a89259142d06c4f14
Yatish Mehta <yatishmehta27@gmail.com>
2015-02-28 03:17:25 -0500

    Fixes spec file structure an few indentation

---
+++ spec/controllers/student_teams_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe StudentTeamsController do
+  
+  let (:student_teams_controller) {StudentTeamsController.new}
+  let(:student)                   {double "student"}
+  
+  describe '#view' do
+    it 'sets the student' do
+      expect(AssignmentParticipant).to receive(:find).with('12345').and_return student
+      
+      student_teams_controller.stub(:current_user_id?)
+      student_teams_controller.stub(:params).and_return({student_id: '12345'})
+      student.stub(:user_id)
+      student_teams_controller.view
+    end
+  end
+end

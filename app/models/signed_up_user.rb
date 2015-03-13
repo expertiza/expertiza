971b324c829a8c5b1aeefd63fc493bcc360c37dd
MaggieDong <tianqing705@gmail.com>
2015-03-13 17:10:01 -0400

    fix some bugs according to the global rules.

--- app/models/signed_up_user.rb
+++ app/models/signed_up_user.rb
@@ -1,5 +1,5 @@
 class SignedUpUser < ActiveRecord::Base
-  belongs_to :topic, :class_name => 'SignUpTopic'
+  belongs_to :topic, class_name: 'SignUpTopic'
 
   #the below has been added to make is consistent with the database schema
   validates_presence_of :topic_id, :creator_id
@@ -11,11 +11,9 @@ class SignedUpUser < ActiveRecord::Base
   end
 
   def self.find_team_participants(assignment_id)
-    #@participants = SignedUpUser.find_by_sql("SELECT s.id as id, t.id as topic_id, t.topic_name as name , s.is_waitlisted as is_waitlisted, s.creator_id, s.creator_id as team_id FROM signed_up_users s, sign_up_topics t where s.topic_id = t.id and t.assignment_id = " + assignment_id)
     @participants = SignedUpUser.find_by_sql(["SELECT s.id as id, t.id as topic_id, t.topic_name as name , s.is_waitlisted as is_waitlisted, s.creator_id, s.creator_id as team_id FROM signed_up_users s, sign_up_topics t where s.topic_id = t.id and t.assignment_id = ? ",assignment_id])
     i=0
     for participant in @participants
-      #participant_names = SignedUpUser.find_by_sql("SELECT s.name as u_name, t.name as team_name FROM users s, teams t, teams_users u WHERE t.id = u.team_id and u.user_id = s.id and t.id = " + participant.team_id)
       participant_names = SignedUpUser.find_by_sql(["SELECT s.name as u_name, t.name as team_name FROM users s, teams t, teams_users u WHERE t.id = u.team_id and u.user_id = s.id and t.id = ?", participant.team_id])
       team_name_added = false
       names = '(missing team)'
@@ -38,18 +36,15 @@ class SignedUpUser < ActiveRecord::Base
   end
 
   def self.find_participants(assignment_id)
-    #SignedUpUser.find_by_sql("SELECT t.id as topic_id,u.name as name, s.is_waitlisted as is_waitlisted FROM signed_up_users s, users u, sign_up_topics t where s.creator_id = u.id and s.topic_id = t.id and t.assignment_id = " + assignment_id)
     SignedUpUser.find_by_sql(["SELECT t.id as topic_id,u.name as name, s.is_waitlisted as is_waitlisted FROM signed_up_users s, users u, sign_up_topics t where s.creator_id = u.id and s.topic_id = t.id and t.assignment_id = ?", assignment_id])
   end
 
   def self.find_team_users(assignment_id,user_id)
-    #TeamsUser.find_by_sql("SELECT t.id as t_id FROM teams_users u, teams t WHERE u.team_id = t.id and t.parent_id =" + assignment_id.to_s + " and user_id =" + user_id.to_s)
     a =  TeamsUser.find_by_sql(["SELECT t.id as t_id FROM teams_users u, teams t WHERE u.team_id = t.id and t.parent_id = ? and user_id = ?", assignment_id, user_id])
     return a
   end
 
   def self.find_user_signup_topics(assignment_id,creator_id)
-    #SignedUpUser.find_by_sql("SELECT t.id as topic_id, t.topic_name as topic_name, u.is_waitlisted as is_waitlisted FROM sign_up_topics t, signed_up_users u WHERE t.id = u.topic_id and t.assignment_id = " + assignment_id.to_s + " and u.creator_id =" + creator_id.to_s)
     SignedUpUser.find_by_sql(["SELECT t.id as topic_id, t.topic_name as topic_name, u.is_waitlisted as is_waitlisted, u.preference_priority_number as preference_priority_number FROM sign_up_topics t, signed_up_users u WHERE t.id = u.topic_id and t.assignment_id = ? and u.creator_id = ?", assignment_id.to_s, creator_id.to_s])
   end
 

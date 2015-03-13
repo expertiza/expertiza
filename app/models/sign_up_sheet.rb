5ddf7e92bba24548cb0815c3760a19498e152b25
MaggieDong <tianqing705@gmail.com>
2015-03-13 17:11:15 -0400

    fix some bugs according to the global rules.

--- app/models/sign_up_sheet.rb
+++ app/models/sign_up_sheet.rb
@@ -1,7 +1,7 @@
 class SignUpSheet < ActiveRecord::Base
   def signup_team ( assignment_id, user_id, topic_id )
     users_team = SignedUpUser.find_team_users(assignment_id, user_id)
-    if users_team.size == 0
+    if users_team.empty?
       #if team is not yet created, create new team.
       team = AssignmentTeam.create_team_and_node(assignment_id)
       user = User.find(user_id)

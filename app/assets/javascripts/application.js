a3d2c1f0ca9fff2581b8c2664e4411e0491eba57
Winbobob <zhu6@ncsu.edu>
2015-03-12 23:04:04 -0400

    multiple data confirmations to delete user

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -41,21 +41,59 @@ $.rails.confirmed = function(link) {
 $.rails.showConfirmDialog = function(link) {
   var html, message;
   message = link.attr('data-confirm');
-  html = "<div class=\"modal\" id=\"dialog-confirm\" title=\"Delete This User?\">\n  <div class=\"modal-body\">\n    <p>" + message + "</p>\n";
-  $(html).modal();
+  html1 = "<div class=\"modal\" id=\"dialog-confirm1\" title=\"Delete This User?\">\n  <div class=\"modal-body\">\n    <p>" + message + "</p>\n";
+
+  html2 = "<div class=\"modal\" id=\"dialog-confirm2\" title=\"System Information\">\n  <div class=\"modal-body\">\n    <p>This user cannot be deleted. Do you want to rename the user account to <current_account_name>_hidden?</p>\n"; 
+
+  html3 = "<div class=\"modal\" id=\"dialog-confirm3\" title=\"System Information\">\n  <div class=\"modal-body\">\n    <p>Rename successfully!!</p>\n";
+
+  html4 = "<div class=\"modal\" id=\"dialog-confirm4\" title=\"System Information\">\n  <div class=\"modal-body\">\n    <p>You cannot delete this user!!</p>\n";
+
   //confirmation box style
   $(function() {
-    $("#dialog-confirm").dialog({
-      resizable: false,
-      height:240,
+    $(html1).modal();
+    $("#dialog-confirm1").dialog({
       width: 340,
-      modal: true,
       buttons: {
         "Cancel": function() {
           $( this ).dialog( "close" );
         },
-        "OK": function() {
-          return $.rails.confirmed(link);
+        "Yes": function() {
+          //return $.rails.confirmed(link);
+          $( this ).dialog( "close" );
+          $(html2).modal();
+          $("#dialog-confirm2").dialog({
+            width: 340,
+            buttons: {
+              "Cancel": function() {
+                $( this ).dialog( "close" );
+              },
+              "Yes": function() {
+                  $( this ).dialog( "close" );
+                  $(html3).modal();
+                  $("#dialog-confirm3").dialog({
+                    width: 340,
+                    buttons: {
+                      "Close": function() {
+                        $( this ).dialog( "close" );
+                      }
+                    }
+                  });
+              },
+              "No, delete any way!": function() {
+                  $( this ).dialog( "close" );
+                  $(html4).modal();
+                  $("#dialog-confirm4").dialog({
+                      width: 340,
+                      buttons: {
+                        "Close": function() {
+                          $( this ).dialog( "close" );
+                        }
+                      }
+                  });
+              }
+            }
+          });
         }
       }
     });

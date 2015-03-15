97d30247b4f68300dc82311668d6c7618b6bcd1f
Ivan Kopas <ikopas@ncsu.edu>
2015-03-14 20:16:48 -0400

    Began integration tests

--- test/integration/bookmarks_test.rb
+++ test/integration/bookmarks_test.rb
@@ -4,13 +4,16 @@ class BookmarksTest < ActionDispatch::IntegrationTest
 
   # setup a user through fixtures first (check out fixtures/users.yml) then run the test
   def setup
-    @user = users(:user2) # get a value from fixture named 'users' which has a symbol 'michael' defined
+    @user = users(:user2) # get a value from fixture named 'users' which has a symbol 'user2' defined
   end
 
-
-  test "accessing bookmarks/managing_bookmarks" do
-    get 'bookmarks/managing_bookmarks'
-    assert_template 'bookmarks/managing_bookmarks'
+  test "login and access bookmarks/managing_bookmarks" do
+    get 'home'
+    assert_select "a[href=?]", '/password_retrieval/forgotten' # ensure you're on home page
+    post '/auth/login', login: { name: @user.name, password: "123"} # log in user2
+    #assert_template 'tree_display/list'
+    # get 'bookmarks/managing_bookmarks'
+    # assert_template 'bookmarks/managing_bookmarks'
   end
 
 

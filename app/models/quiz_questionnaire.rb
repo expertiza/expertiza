e9cce64ead8d0ba86f567f9b14a6a0260a2d35d5
NorthCarolinaWolf <ywang95@ncsu.edu>
2015-03-15 22:48:31 -0400

    refactor the questionnair_controller, move some methods into models/questionnaire.rb and models/quiz_questionnaire.rb

--- app/models/quiz_questionnaire.rb
+++ app/models/quiz_questionnaire.rb
@@ -22,7 +22,53 @@ class QuizQuestionnaire < Questionnaire
       return scores[:quiz][:scores][:avg] * 100  / 100.to_f
       else
         return 0
+    end
+  end
+
+  def self.valid_quiz(aid,new_question,question_type,new_choices)
+    num_quiz_questions = Assignment.find(aid).num_quiz_questions
+    valid = "valid"
+
+    (1..num_quiz_questions).each do |i|
+      if new_question[i.to_s][:txt] == ''
+        #One of the questions text is not filled out
+        valid = "Please make sure all questions have text"
+        break
+      elsif question_type[i.to_s][:type] == nil
+        #A type isnt selected for a question
+        valid = "Please select a type for each question"
+        break
+      else
+        type = question_type[i.to_s][:type]
+        if type == 'MCC' or type == 'MCR'
+          correct_selected = false
+          (1..4).each do |x|
+            if new_choices[i.to_s][type][x.to_s][:txt] == ''
+              #Text isnt provided for an option
+              valid = "Please make sure every question has text for all options"
+              break
+            elsif type == 'MCR' and not new_choices[i.to_s][type][x.to_s][:iscorrect] == nil
+              correct_selected = true
+            elsif type == 'MCC' and not new_choices[i.to_s][type][x.to_s][:iscorrect] == 0.to_s
+              correct_selected = true
+            end
+          end
+          unless correct_selected == true
+            #A correct option isnt selected for a check box or radio question
+            valid = "Please select a correct answer for all questions"
+            break
+          end
+        elsif type == 'TF'
+          if new_choices[i.to_s]["TF"] == nil
+            #A correct option isnt selected for a true/false question
+            valid = "Please select a correct answer for all questions"
+            break
+          end
+        end
       end
     end
 
+    return valid
   end
+
+end

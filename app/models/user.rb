4e9e5822b7304da4d7723bd6558af5bb534b063a
Yang Song <ysong8@ncsu.edu>
2015-04-02 13:01:20 -0400

    Syntax fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -118,7 +118,7 @@ class User < ActiveRecord::Base
 
   def valid_password?(password)
     Authlogic::CryptoProviders::Sha1.stretches = 1
-    Authlogic::CryptoProviders::Sha1.matches?(crypted_password, *[self.password_salt.to_s + password
+    Authlogic::CryptoProviders::Sha1.matches?(crypted_password, *[self.password_salt.to_s + password])
   end
 
   # Resets the password to be mailed to the user

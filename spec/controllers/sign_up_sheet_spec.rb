28dc27a79682302fba6562a426bc111ad64be634
tastynoodle <szhang29@ncsu.edu>
2015-04-20 21:39:54 -0400

    modify rspec tests

--- spec/controllers/sign_up_sheet_spec.rb
+++ spec/controllers/sign_up_sheet_spec.rb
@@ -2,17 +2,16 @@ require 'rails_helper'
 
 describe SignUpSheetController do
   before(:each) do
-    @assignment = Assignment.where(name: 'assignment1').first || Assignment.new({
-                                                                                  "id"=> "1",
-                                                                                  "name"=>"My assignment",
-                                                                                  "is_intelligent"=>1
+    @assignment = Assignment.where(name: 'My assignment').first || Assignment.new({
+                                                                                  "name"=>"My assignment"
                                                                               })
     @assignment.save
 
     @topic = SignUpTopic.new({
                                  topic_name: "Normal Topic",
-                                 id: 1,
-                                 assignment_id: nil
+                                 topic_identifier: "Ch10",
+                                 assignment_id: @assignment.id,
+                                 max_choosers: 2
                              })
     @topic.save
 
@@ -22,19 +21,17 @@ describe SignUpSheetController do
   end
 
   it "should be able to create topic for assignment" do
-    get :new, id: 1
-    expect(response).to be_success
-    get :create, id: 1, topic: {topic_name: "New Topic", max_choosers: 2, topic_identifier: "Ch1", category: "Programming"}
-    expect(response).should redirect_to(edit_assignment_path(1) + "#tabs-5")
+    get :create, id: @assignment.id, topic: {topic_name: "New Topic", max_choosers: 2, topic_identifier: "Ch1", category: "Programming"}
+    expect(response).should redirect_to(edit_assignment_path(@assignment.id) + "#tabs-5")
   end
 
   it "should be able to edit topic" do
-    get :edit, id: 1
+    get :edit, id: @topic.id
     expect(response).to be_success
   end
 
   it "should be able to delete topic" do
-    delete :destroy, id: 1, assignment_id: 1
-    expect(response).should redirect_to edit_assignment_path(1) + "#tabs-5"
+    delete :destroy, id: @topic.id, assignment_id: @assignment.id
+    expect(response).should redirect_to edit_assignment_path(@assignment.id) + "#tabs-5"
   end
 end

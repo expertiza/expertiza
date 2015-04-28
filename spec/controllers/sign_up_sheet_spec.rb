5fdf9b23f7b25b0558243cb06a8b122b020e8079
tastynoodle <szhang29@ncsu.edu>
2015-04-28 00:31:12 -0400

    add test for detecting dependency cycle

--- spec/controllers/sign_up_sheet_spec.rb
+++ spec/controllers/sign_up_sheet_spec.rb
@@ -16,13 +16,21 @@ describe SignUpSheetController do
                                                                               })
     @assignment.save
 
-    @topic = SignUpTopic.new({
-                                 topic_name: "Normal Topic",
+    @topic1 = SignUpTopic.new({
+                                 topic_name: "Topic1",
                                  topic_identifier: "Ch10",
                                  assignment_id: @assignment.id,
                                  max_choosers: 2
                              })
-    @topic.save
+    @topic1.save
+
+    @topic2 = SignUpTopic.new({
+                                  topic_name: "Topic2",
+                                  topic_identifier: "Ch10",
+                                  assignment_id: @assignment.id,
+                                  max_choosers: 2
+                              })
+    @topic2.save
 
     # simulate authorized session
     ApplicationController.any_instance.stub(:current_role_name).and_return('Instructor')
@@ -35,17 +43,24 @@ describe SignUpSheetController do
   end
 
   it "should be able to edit topic" do
-    get :edit, id: @topic.id
+    get :edit, id: @topic1.id
     expect(response).to be_success
   end
 
   it "should be able to delete topic" do
-    delete :destroy, id: @topic.id, assignment_id: @assignment.id
+    delete :destroy, id: @topic1.id, assignment_id: @assignment.id
     expect(response).should redirect_to edit_assignment_path(@assignment.id) + "#tabs-5"
   end
 
   it "should be able to generate topic dependency" do
     post :save_topic_dependencies, assignment_id: @assignment.id
     expect(File).to exist("public/assets/staggered_deadline_assignment_graph/graph_#{@assignment.id}.jpg")
+  end
+
+  it "should be able to detect cycles" do
+    post :save_topic_dependencies, assignment_id: @assignment.id,
+         ('topic_dependencies_' + @topic1.id.to_s)=>{"dependent_on"=>[@topic2.id.to_s]},
+         ('topic_dependencies_' + @topic2.id.to_s)=>{"dependent_on"=>[@topic1.id.to_s]}
+    expect(flash[:error]).to eq("There may be one or more cycles in the dependencies. Please correct them")
   end
 end

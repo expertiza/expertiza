164a8334970929f02f5ab9e387dcf77360681653
Asish Prabhakar Kottala <akottal@ncsu.edu>
2015-05-05 12:56:54 -0400

    assessment 360 refactored

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1128,4 +1128,77 @@ class Assignment < ActiveRecord::Base
           "from teams t, signed_up_users s "+
           "where s.topic_id='"+id.to_s+"' and s.creator_id = t.id and t.advertise_for_partner = 1").size > 0
     end
+
+  def review_progress_pie_chart
+    reviewed = self.get_percentage_reviews_completed
+    pending = 100 - reviewed
+    reviewed_msg = reviewed.to_s + "% reviewed"
+    pending_msg = pending.to_s + "% pending"
+
+    GoogleChart::PieChart.new('160x100'," ",false) do |pc|
+      pc.data_encoding = :extended
+      pc.data reviewed_msg, reviewed, '228b22' # want to write '20' responed
+      pc.data pending_msg, pending, 'ff0000' # rest of the class
+
+      # Pie Chart with labels
+      pc.show_labels = false
+      pc.show_legend = true
+      @pie_chart = pc.to_url
+    end
+    @pie_chart
+  end
+
+  def review_progress_bar_chart
+    bar_1_data = Array.new
+    dates = Array.new
+    date = self.created_at.to_datetime.to_date
+
+    while ((date <=> Date.today) <= 0)
+      if self.get_total_reviews_completed_by_date(date) != 0 then
+        bar_1_data.push(self.get_total_reviews_completed_by_date(date))
+        dates.push(date.month.to_s + "-" + date.day.to_s)
+      end
+
+      date = (date.to_datetime.advance(:months => 1)).to_date
+    end
+
+    color_1 = 'c53711'
+    min=0
+    #max= assignment.get_total_reviews_assigned
+    max = self.get_total_reviews_assigned
+
+    GoogleChart::BarChart.new("600x80", " ", :vertical, false) do |bc|
+      bc.data "Review", bar_1_data, color_1
+      bc.axis :y, :positions => [min, max], :range => [min,max]
+      bc.axis :x, :labels => dates
+      bc.show_legend = false
+      bc.stacked = false
+      bc.data_encoding = :extended
+      bc.params.merge!({:chl => "Nov"})
+      @bar_chart = (bc.to_url)
+    end
+    @bar_chart
+  end
+
+  def review_grade_distribution_histogram
+    bar_2_data = self.get_score_distribution
+    color_2 = '4D89F9'
+    min = 0
+    max = 100
+
+    p '======================='
+    p bar_2_data
+    GoogleChart::BarChart.new("130x100", " ", :vertical, false) do |bc|
+      bc.data "Review", bar_2_data, color_2
+      bc.axis :y, :positions => [0, bar_2_data.max], :range => [0, bar_2_data.max]
+      bc.axis :x, :positions => [min, max], :range => [min,max]
+      bc.width_spacing_options :bar_width => 1, :bar_spacing => 0, :group_spacing => 0
+      bc.show_legend = false
+      bc.stacked = false
+      bc.data_encoding = :extended
+      bc.params.merge!({:chl => "Nov"})
+      @histogram = (bc.to_url)
+    end
+    @histogram
+  end
   end

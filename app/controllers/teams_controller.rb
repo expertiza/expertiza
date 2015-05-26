ba3709826c5dbd6fa545383778c22b0629962597
Winbobob <zhu6@ncsu.edu>
2015-05-26 14:32:20 -0400

    fix bugs related to team creation by instructors

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -22,6 +22,7 @@ class TeamsController < ApplicationController
     redirect_to :action => 'list', :id => parent.id
   end
 
+  #This function is used to create teams with random names.
   def create_teams
     parent = Object.const_get(session[:team_type]).find(params[:id])
     Team.randomize_all_by_parent(parent, session[:team_type], params[:team][:size].to_i)

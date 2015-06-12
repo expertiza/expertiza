fedb151c974f88e51f758ebc3aa4259fdc1fab85
Winbobob <zhu6@ncsu.edu>
2015-06-12 11:36:08 -0400

    If one team does not involve any response mappings as reviewee, it can be deleted.

--- app/models/assignment_team.rb
+++ app/models/assignment_team.rb
@@ -2,8 +2,8 @@ class AssignmentTeam < Team
 
   belongs_to  :assignment, :class_name => 'Assignment', :foreign_key => 'parent_id'
   has_many    :review_mappings, :class_name => 'ReviewResponseMap', :foreign_key => 'reviewee_id'
-  has_many :response_maps, foreign_key: :reviewee_id
-  has_many :responses, through: :response_maps, foreign_key: :map_id
+  has_many :review_response_maps, foreign_key: :reviewee_id
+  has_many :responses, through: :review_response_maps, foreign_key: :map_id
 
     # START of contributor methods, shared with AssignmentParticipant
 
@@ -87,7 +87,7 @@ class AssignmentTeam < Team
   end
 
   def destroy
-    response_maps.each(&:destroy)
+    review_response_maps.each(&:destroy)
     super
   end
 

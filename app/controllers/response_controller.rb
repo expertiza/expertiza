855153b743d6090eb4f49e942bf1f2cf8d0003fc
yangsong8 <ysong8@ncsu.edu>
2015-07-15 13:43:48 -0400

    Get rid of the 1st view method in response_controller.rb.

--- app/controllers/response_controller.rb
+++ app/controllers/response_controller.rb
@@ -217,19 +217,6 @@ class ResponseController < ApplicationController
     redirect_to :controller => 'response', :action => 'saving', :id => @map.map_id, :return => params[:return], :msg => msg, :save_options => params[:save_options]
   end
 
-  def view
-    @response = Response.find(params[:id])
-    return if redirect_when_disallowed(@response)
-    @map = @response.map
-    get_content
-    @review_scores = Array.new
-    @question_type = Array.new
-    @questions.each do |question|
-      @review_scores << Score.where(response_id: @map.response_id, question_id:  question.id).first
-      @question_type << QuestionType.find_by_question_id(question.id)
-    end
-  end
-
   def new
 
     @header = "New"

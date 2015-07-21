24fd6aafd197a7ffbe34d0144024b9e8502634c5
yangsong8 <ysong8@ncsu.edu>
2015-07-21 19:35:17 -0400

    Fix quiz question update for TF and MCR type.

--- app/models/question_type.rb
+++ app/models/question_type.rb
@@ -6,14 +6,22 @@ class QuestionType < ActiveRecord::Base
   def self.get_formatted_question_type question_id
     type = QuestionType.find_by_question_id(question_id).q_type
 
+    # for quiz questions, we store 'TF', 'MCC', 'MCR' in the DB, and the full names are returned below
     if type == 'TF'
       return 'True/False'
-    elsif type == 'Essay'
-      return 'Essay'
     elsif type == 'MCC'
       return 'Multiple Choice - Checked'
     elsif type == 'MCR'
       return 'Multiple Choice - Radio'
+    end
+  end
+
+  def self.find_by_question_id question_id
+    question_type_records = QuestionType.where(question_id: question_id)
+    if question_type_records.nil?
+      nil
+    else
+      question_type_records.first
     end
   end
 end

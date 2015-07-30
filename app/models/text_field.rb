ab803085a6eb968cb5aa77b23749de089fe6ecfe
Winbobob <zhu6@ncsu.edu>
2015-07-30 15:00:35 -0400

    Complete model files for text_response question

--- app/models/text_field.rb
+++ app/models/text_field.rb
@@ -1,13 +1,29 @@
 class TextField < TextResponse
   def edit
+  	html = "<form accept-charset="UTF-8" action="/questions/create" method="post">"
+  	html += "Type: <input id="question_type" name="question[type]" type="text" value="Scale" size="3" disabled="true" />"
+  	html += "Txt: <input id="question_txt" name="question[txt]" size="70" type="text" />"
+  	html += "TextField size: <input id="question_size" name="question[size]" size="5" type="text" />"
+  	html += "Weight: <input id="question_weight" name="question[weight]" size="1" type="text" />"
+  	html += "<input name="commit" type="submit" value="Create/Edit" />"
+  	html += "</form>"
   end
 
   def view_question_text
-  end
-
-  def view_completed_question
+  	html = "Type: <input id="question_type" name="question[type]" type="text" value="Scale" size="3" disabled="true" />"
+  	html += "Txt: <input id="question_txt" name="question[txt]" size="70" type="text" disabled="true" />"
+  	html += "TextField size: <input id="question_size" name="question[size]" size="5" type="text" disabled="true" />"
+  	html += "Weight: <input id="question_weight" name="question[weight]" size="1" type="text" disabled="true" />"
   end
 
   def complete
+  	html = self.txt
+  	html += "<input id="answer_comments" name="answer[comments]" size=" +self.size+ " type="text"/>"
+  end
+
+  def view_completed_question(response_id)
+  	answer = Answer.where(question_id: self.id, response_id: response_id).first
+  	html = self.txt
+  	html += "<input id="answer_comments" name="answer[comments]" size=" +self.size+ " type="text" value=" +answer.comments+ "/>"
   end
 end

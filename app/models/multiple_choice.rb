792c8b69e380ca268ae34811e9f57e012f8e3a2f
Winbobob <zhu6@ncsu.edu>
2015-07-30 14:40:12 -0400

    Complete model files for unscored_question

--- app/models/multiple_choice.rb
+++ app/models/multiple_choice.rb
@@ -1,13 +1,38 @@
 class MultipleChoice < UnscoredQuestion
   def edit
+  	html = "<form accept-charset="UTF-8" action="/questions/create" method="post">"
+  	html += "Type: <input id="question_type" name="question[type]" type="text" value="Scale" size="3" disabled="true" />"
+  	html += "Txt: <input id="question_txt" name="question[txt]" size="70" type="text" />"
+  	html += "Alternatives: <input id="question_alternatives" name="question[alternatives]" size="5" type="text" />"
+  	html += "Weight: <input id="question_weight" name="question[weight]" size="1" type="text" />"
+  	html += "<input name="commit" type="submit" value="Create/Edit" />"
+  	html += "</form>"
   end
 
   def view_question_text
-  end
-
-  def view_completed_question
+  	html = "Type: <input id="question_type" name="question[type]" type="text" value="Scale" size="3" disabled="true" />"
+  	html += "Txt: <input id="question_txt" name="question[txt]" size="70" type="text" disabled="true"/>"
+  	html += "Alternatives: <input id="question_alternatives" name="question[alternatives]" size="5" type="text" disabled="true"/>"
+  	html += "Weight: <input id="question_weight" name="question[weight]" size="1" type="text" disabled="true"/>"
   end
 
   def complete
+  	html = "Txt: <input id="question_txt" name="question[txt]" size="70" type="text" disabled="true"/>"
+  	alternatives = self.alternatives.splict('|')
+  	alternatives.each_with_index do |alternative, index|
+  		html += "<input type="checkbox" id="multiple_choice"" +index_to_s+ "name=" +alternative+ ">"
+  		html += alternative
+  	end
+  end
+
+  def view_completed_question(response_id)
+  	answer = Answer.where(question_id: self.id, response_id: response_id).first
+  	html = "Txt: <input id="question_txt" name="question[txt]" size="70" type="text" disabled="true"/>"
+  	alternatives = self.alternatives.splict('|')
+  	alternatives.each_with_index do |alternative, index|
+  		html += "<input type="checkbox" id="multiple_choice"" +index_to_s+ "name=" +alternative
+  		html += "checked="checked"" if answer.answer == index
+  		html += ">" + alternative
+  	end
   end
 end

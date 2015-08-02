a47303a061b08683a930b366144035229d6e4002
yangsong8 <ysong8@ncsu.edu>
2015-08-02 15:14:51 -0400

    Change migration: change->self.up.

--- db/migrate/20150802171710_change_default_values_of_questions_table.rb
+++ db/migrate/20150802171710_change_default_values_of_questions_table.rb
@@ -1,5 +1,5 @@
 class ChangeDefaultValuesOfQuestionsTable < ActiveRecord::Migration
-  def change
+  def self.up
     change_column :questions, :size, :string, :default =>""
     change_column :questions, :max_label, :string, :default =>""
     change_column :questions, :min_label, :string, :default =>""

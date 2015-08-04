d23f5df61b781d98bd7b4a85ccfd7e5fdbefb2c2
Winbobob <zhu6@ncsu.edu>
2015-08-04 13:49:34 -0400

    Finish 'edit' method in TextArea, TextField, Checkbox

--- app/models/questionnaire_header.rb
+++ app/models/questionnaire_header.rb
@@ -1,5 +1,5 @@
-class Checkbox < UnscoredQuestion
-  #This method returns what to display if an instructor (etc.) is creating or editing a questionnaire (questionnaires_controller.rb)
+class QuestionnaireHeader < Question
+	#This method returns what to display if an instructor (etc.) is creating or editing a questionnaire (questionnaires_controller.rb)
   def edit(count)
     html ='<tr>'
     html+='<td align="center"><input id="question_chk' +count.to_s+ '" type="checkbox"></td>'
@@ -17,27 +17,15 @@ class Checkbox < UnscoredQuestion
     html = '<TR><TD align="left"> '+self.txt+' </TD>'
     html += '<TD align="left">'+self.type+'</TD>'
     html += '<td align="center">'+self.weight.to_s+'</TD>'
-    html += '<TD align="center">True/False</TD>'
+    html += '<TD align="center">-</TD>'
     html += '</TR>'
     html.html_safe
   end
 
   def complete
-    ''
+  	self.txt
   end
 
-  #This method returns what to display if a student is viewing a filled-out questionnaire
-  def view_completed_question(count, answer)
-    if answer.answer == 1
-      html = '<li><p><img src="/images/Check-icon.png">' +self.txt+ '<br></p></li>'
-    else
-      html = '<li><p><img src="/images/delete-icon.png">' +self.txt+ '<br></p></li>'
-    end
-    html.html_safe
-  end
-
-  def self.checked?(response_id)
-    answer = Answer.where(question_id: self.id, response_id: response_id).first
-    return answer.comment == '0'
+  def view_completed_question
   end
 end
--- app/models/text_area.rb
+++ app/models/text_area.rb
@@ -1,18 +1,4 @@
 class TextArea < TextResponse
-  def edit
-  	''
-  end
-
-  #This method returns what to display if an instructor (etc.) is viewing a questionnaire
-  def view_question_text
-    html = '<TR><TD align="left"> '+self.txt+' </TD>'
-    html += '<TD align="left">'+self.type+'</TD>'
-    html += '<td align="center">'+self.weight.to_s+'</TD>'
-    html += '<TD align="center">-</TD>'
-    html += '</TR>'
-    html.html_safe
-  end
-
   def complete
   	''
   end

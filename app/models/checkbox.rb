dba48ea813ad03a551fab43095020eef338a3eab
Winbobob <zhu6@ncsu.edu>
2015-08-04 12:52:06 -0400

    1.Finish 'view_question_txt' method in TextArea, TextField, Checkbox; 2.Create model files for Section_header, Table_header, Column_header.

--- app/models/checkbox.rb
+++ app/models/checkbox.rb
@@ -3,12 +3,12 @@ class Checkbox < UnscoredQuestion
     ''
   end
 
+  #This method returns what to display if an instructor (etc.) is viewing a questionnaire
   def view_question_text
     html = '<TR><TD align="left"> '+self.txt+' </TD>'
     html += '<TD align="left">'+self.type+'</TD>'
     html += '<td align="center">'+self.weight.to_s+'</TD>'
-    questionnaire = self.questionnaire
-    html += '<TD align="center">0 to 1</TD>'
+    html += '<TD align="center">True/False</TD>'
     html += '</TR>'
     html.html_safe
   end
---
+++ app/models/table_header.rb
@@ -0,0 +1,13 @@
+class TableHeader < Question
+  def edit
+  end
+
+  def view_question_text
+  end
+
+  def complete
+  end
+
+  def view_completed_question
+  end
+end

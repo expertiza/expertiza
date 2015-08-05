02aa680e322ee6879dca2e5584d6ca51a342d441
Winbobob <zhu6@ncsu.edu>
2015-08-04 23:10:44 -0400

    Partially finish 'Complete' method in TextArea, TextField, Checkbox

--- app/models/text_field.rb
+++ app/models/text_field.rb
@@ -1,6 +1,11 @@
 class TextField < TextResponse
-  def complete
-  	''
+  def complete(count, answer=nil)
+    html = '<label for="responses_' +count.to_s+ '">' +self.txt+ '</label>'
+    html += '<p><input name="responses[' +count.to_s+ '][score]" type="hidden">'
+    html += '<input id="responses_' +count.to_s+ '_comments" label=' +self.txt+ ' name="responses[' +count.to_s+ '][comment]" size=' +self.size.to_s+ ' type="text"'
+    html += 'value=' + answer.answer if !answer.nil?
+    html += '>'
+    html.html_safe
   end
 
   def view_completed_question(count, answer)

7412be053664cee2082b46177e82e7fe58058674
yangsong8 <ysong8@ncsu.edu>
2015-08-24 21:28:52 -0400

    Fix view_question_text for quiz questions.

--- app/models/true_false.rb
+++ app/models/true_false.rb
@@ -1,9 +1,7 @@
 class TrueFalse < QuizQuestion
+  has_many :quiz_question_choices, :class_name => 'QuizQuestionChoice', :foreign_key => 'question_id'
+
   def edit(count)
-
-  end
-
-  def view_question_text
 
   end
 

1c6b0497fba49d651abd64101524699f79f9a390
Yang Song <ysong8@ncsu.edu>
2015-08-28 17:13:18 -0400

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,10 +42,10 @@ set :repo_url, 'https://github.com/expertiza/expertiza.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml log pg_data}
+set :linked_files, %w{config/database.yml }
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log pg_data}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }

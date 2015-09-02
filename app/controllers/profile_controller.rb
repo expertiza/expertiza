dcfd1236ff10b4af650255bae2c41b981475adda
yangsong8 <ysong8@ncsu.edu>
2015-09-02 12:38:01 -0400

    Fix update password on students' end

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -9,6 +9,7 @@ class ProfileController < ApplicationController
   end
 
   def update
+    params.permit!
     @user = session[:user]
 
     unless params[:assignment_questionnaire].nil? or params[:assignment_questionnaire][:notification_limit].blank?
@@ -16,7 +17,7 @@ class ProfileController < ApplicationController
       aq.update_attribute('notification_limit',params[:assignment_questionnaire][:notification_limit])
     end
 
-    if @user.update_attributes(user_params)
+    if @user.update_attributes(params[:user])
       flash[:success] = 'Profile was successfully updated.'
     else
       flash[:error] = 'Profile was not updated.'
@@ -26,6 +27,7 @@ class ProfileController < ApplicationController
   end
 
   private
+  # this is for strong parameters, but it is not used now. If you are planning to fix this, you can remove this line: "params.permit!"
   def user_params
     params.require(:user).permit(:name, :crypted_password, :role_id, :password_salt, :fullname, :email, :parent_id, :private_by_default, :mru_directory_path, :email_on_review, :email_on_submission, :email_on_review_of_review, :is_new_user, :master_permission_granted, :handle, :leaderboard_privacy, :digital_certificate, :persistence_token, :timezonepref, :public_key, :copy_of_emails)
   end

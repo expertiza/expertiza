adab5da8b9cdc5e364f87a30fb7408ffc08bf9eb
Expertiza Developer <sjolly@ncsu.edu>
2015-10-17 21:59:50 -0400

    updated assignment factory

--- spec/factories/assignments.rb
+++ spec/factories/assignments.rb
@@ -1,13 +1,37 @@
 require 'factory_girl_rails'
 
 FactoryGirl.define do
-    factory :assignment do
+  #instructor for assignment
+  factory :user do
+    name "test"
+    fullname "test_user"
+    email "sjolly@ncsu.edu"
+    parent_id =1
+    is_new_user=true
+  end
+
+  #team for assignment
+  
+  factory :team do
+      name 'Team1'
+      parent_id 2
+      type 'g'
+      comments_for_advertisement 'good'
+      advertise_for_partner true
+  end
+
+  factory :wiki_type do
+      name 'wiki_1a'
+  end
+  factory :assignment do
           name 'OSS'
           submitter_count 3
           course_id 1
-          instructor_id 2
           private false
           num_reviews 2
-
-    end
-end
+          association :instructor, factory: :user
+          association :wiki_type, factory: :wiki_type
+          #after(:create) {|assignment| assignment.add_user(:user)}
+          #after(:create) {|assignment| assignment.add_team(:team)}¬    end
+  end
+  end
--- spec/factories/teams.rb
+++ spec/factories/teams.rb
@@ -1,12 +1,12 @@
-require 'factory_girl_rails'
+#require 'factory_girl_rails'
 
-FactoryGirl.define do
-    factory :team do
-    name 'Team1'
-    parent_id 2
-    type 'g'
-    comments_for_advertisement 'good'
-    advertise_for_partner true
- end
-end
+#FactoryGirl.define do
+ #   factory :team do
+  #  name 'Team1'
+   # parent_id 2
+    #type 'g'
+    #comments_for_advertisement 'good'
+    #advertise_for_partner true
+ #end
+#end
 

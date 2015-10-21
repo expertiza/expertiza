61342a076b7516207f341751eaaedad7896a0593
shrenujgandhi <sgandhi4@ncsu.edu>
2015-10-20 22:22:59 -0400

    Login test successful
    Rails_helper: has database cleaner code
    student_factory: test student data
    student_submission_spec: login feature test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,7 +25,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false                               # Changed to false to disable ActiveRecord
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -41,4 +41,23 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+
+  # Used database cleaner to truncate existing test data
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation,:only => ['users'])
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, :js => true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
 end

1d7e5793a597883372ba3adaf2f5126bafc23cec
Max Schweizer <meschwei@ncsu.edu>
2015-10-22 00:46:29 -0400

    Added additional add_signup_topic testing

--- spec/models/sign_up_sheet_spec.rb
+++ spec/models/sign_up_sheet_spec.rb
@@ -14,5 +14,27 @@ describe SignUpSheet do
       expect(SignUpSheet.add_signup_topic(2)).to eql({})
     end
 
+    it 'will return a SignUpSheet with one topic and no due dates' do
+      assignment = double(Assignment)
+      allow(assignment).to receive(:get_review_rounds) { 0 }
+      allow(Assignment).to receive(:find) { assignment }
+
+      topic1 = SignUpTopic.new
+      topic1.id = 'id'
+      topic1.topic_identifier = 'topic_identifier'
+      topic1.topic_name = 'topic_name'
+      allow(SignUpTopic).to receive(:where) { [topic1] }
+
+      topicDeadline = double(TopicDeadline)
+      allow(TopicDeadline).to receive(:where) { topicDeadline }
+      allow(topicDeadline).to receive(:first) { nil }
+
+      deadlineType = double(DeadlineType)
+      allow(DeadlineType).to receive(:find_by_name) { deadlineType }
+      allow(deadlineType).to receive(:id) { nil }
+
+      expect(SignUpSheet.add_signup_topic(2)).to eql({0 => {"id" => 0, "topic_identifier" => "topic_identifier", "topic_name" => "topic_name", "submission_1" => nil}})
+    end
+
   end
 end

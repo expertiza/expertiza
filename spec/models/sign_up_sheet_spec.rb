68dc05fa5f1031117df9ee935e5e6b46df23d0b8
Max Schweizer <meschwei@ncsu.edu>
2015-10-22 00:35:56 -0400

    Removed accidental duplicate code

--- spec/models/sign_up_sheet_spec.rb
+++ spec/models/sign_up_sheet_spec.rb
@@ -14,15 +14,5 @@ describe SignUpSheet do
       expect(SignUpSheet.add_signup_topic(2)).to eql({})
     end
 
-    it 'will return an empty Hash when there are no topics' do
-      assignment = double(Assignment)
-      allow(assignment).to receive(:get_review_rounds) { nil }
-      allow(Assignment).to receive(:find) { assignment }
-
-      allow(SignUpTopic).to receive(:where) { nil }
-
-      expect(SignUpSheet.add_signup_topic(2)).to eql({})
-    end
-
   end
 end

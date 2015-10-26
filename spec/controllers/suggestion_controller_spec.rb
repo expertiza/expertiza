796036fde0a485bd233d614c090e3338b138a582
drinkbeer <chenjb90@gmail.com>
2015-10-26 17:20:14 -0400

    merge all branches

--- spec/controllers/suggestion_controller_spec.rb
+++ spec/controllers/suggestion_controller_spec.rb
@@ -7,6 +7,7 @@ require File.join('./app/controllers/suggestion_controller')
 
 
 describe "SuggestionController" do
+
   describe "#Test1:approve_suggestion_in_waitlist" do
   
     before(:each) do
@@ -133,7 +134,118 @@ describe "SuggestionController" do
       expect(page).to have_content('Your topic(s): RSpect')
       
     end
-
-
   end
+  
+  # Test by Hma
+  describe "test3" do
+    it "test" do
+      # login as student and submit a suggestion
+      visit 'content_pages/view'
+      fill_in "User Name", with: "student5404"
+      fill_in "Password", with: "password"
+      click_button "SIGN IN"
+  
+      visit '/suggestion/new?id=711'
+      fill_in 'suggestion_title',  with: 'test title'
+      fill_in 'suggestion_description',  with: 'test description'
+      click_button "Submit"
+  
+      click_link "Logout"
+  
+      #login as a professor and approve the suggestion
+      visit 'content_pages/view'
+      fill_in "User Name", with: 'instructor6'
+      fill_in "Password", with: 'password'
+      click_button "SIGN IN"
+  
+      visit "/suggestion/" + Suggestion.last.id.to_s
+      click_button "Approve suggestion"
+  
+      click_link "Logout"
+  
+      #login again as a student, check if the topic is changed
+      visit 'content_pages/view'
+      fill_in "User Name", with: 'student5404'
+      fill_in "Password", with: 'password'
+      click_button "SIGN IN"
+  
+      visit '/sign_up_sheet/list?assignment_id=711'
+      expect(page).to have_content('Your topic(s): Amazon S3 and Rails')
+  
+    end
+  end
+  
+  describe "test2"  do
+    it 'should switch to suggested topic after it got approved' do
+      @newtopic = 'Violet and Zoe'
+
+
+      #sign in as student5404:
+      visit 'content_pages/view'
+      fill_in "User Name", with: 'student5404'
+      fill_in "Password", with: 'password'
+      click_button "SIGN IN"
+      expect(page).to have_content('Assignments')
+
+      #suggest a topic:
+      # signup_preference default to be Y
+      visit "/student_task/view?id=28634"
+      expect(page).to have_content('Submit or Review work')
+      visit "/suggestion/new?id=711"
+      expect(page).to have_content('New suggestion')
+      fill_in 'Title',with: @newtopic
+      expect{click_button "Submit"}.to change(Suggestion, :count).by(1)
+
+      #logout
+      click_link "Logout"
+      current_path.should == "/"
+      visit '/suggestion/new?id=711'
+      expect(page).to have_content('This is not allowed')
+      expect(page).to have_content('Welcome')
+      expect(page).to have_no_content('User: student5404')
+
+      #sign in as instructor6
+      visit 'content_pages/view'
+      expect(page).to have_content('Welcome.')
+      fill_in "User Name", with: 'instructor6'
+      fill_in "Password", with: 'password'
+      click_button "SIGN IN"
+      expect(page).to have_content('Manage content')
+
+      #approve the suggestion
+      visit '/suggestion/list?id=711&type=Assignment'
+      expect(page).to have_content('Suggested topics for Writing assignment 1a')
+      num = Suggestion.last.id.to_s
+      visit "/suggestion/"+num
+      expect(page).to have_content('Suggestion')
+      expect(page).to have_content('Title: '+@newtopic)
+      click_button "Approve suggestion"
+      visit "/suggestion/"+num.to_s
+      expect(page).to have_content('status:	Approved')
+
+      #logout as instructor6
+      click_link "Logout"
+      current_path.should == "/"
+      visit '/suggestion/new?id=711'
+      expect(page).to have_content('This is not allowed')
+      expect(page).to have_content('Welcome')
+
+      #sign in as student5404:
+      visit 'content_pages/view'
+      fill_in "User Name", with: 'student5404'
+      fill_in "Password", with: 'password'
+      click_button "SIGN IN"
+      expect(page).to have_content('Assignments')
+
+      #check the approved suggestion in topics list
+      visit "/sign_up_sheet/list?assignment_id=711"
+      expect(page).to have_content("Your approved suggested topic")
+
+      # switch to the new topic
+      num2 = SignUpTopic.last.id.to_s
+      visit "/sign_up_sheet/switch_original_topic_to_approved_suggested_topic/"+num2+"?assignment_id=711"
+      expect(page).to have_content("Your topic(s): "+@newtopic)
+    end
+  end
+
 end 

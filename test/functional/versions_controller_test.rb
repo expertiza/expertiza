b221f69a7bbb9ced4b536115acec71a018da9802
Anshita Sayal <asayal@ncsu.edu>
2015-10-26 16:17:37 -0400

    checking in users.yml

--- test/functional/versions_controller_test.rb
+++ test/functional/versions_controller_test.rb
@@ -1,8 +1,10 @@
 require 'test_helper'
 
+
 class VersionsControllerTest < ActionController::TestCase
 
   test 'should get index redirect to versions_search' do
+    users(:user_one)
     get :index
     assert_redirected_to versions_search_path
   end

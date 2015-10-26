34c8ab5278e3d674047649df41341f0654a5bccd
Anshita Sayal <asayal@ncsu.edu>
2015-10-26 12:45:48 -0400

    functional tests for versions_controller.rb

--- test/functional/versions_controller_test.rb
+++ test/functional/versions_controller_test.rb
@@ -7,4 +7,18 @@ class VersionsControllerTest < ActionController::TestCase
     assert_redirected_to versions_search_path
   end
 
+  test 'should delete destroy redirect to versions path' do
+    delete :destroy, id: versions(:version_one).id
+    assert_redirected_to versions_path
+  end
+
+  test 'should delete destroy_all redirect to versions path' do
+    delete :destroy_all
+    assert_redirected_to versions_path
+  end
+
+  test 'should redirect to show' do
+    get :show, id: versions(:version_one).id
+  end
+
 end

7476aa2371ee51557567fa902fee25d470acda5c
anushb <abonam@ncsu.edu>
2015-10-30 17:34:45 -0400

    Added rspec tests

--- spec/controllers/tree_display_controller_spec.rb
+++ spec/controllers/tree_display_controller_spec.rb
@@ -37,4 +37,55 @@ describe TreeDisplayController do
     end
   end
 
+  describe "#go_to_menu_items" do
+    before do
+      allow(nil).to receive(:find_by_node_object_id).and_return(nil)
+      allow(nil).to receive(:id).and_return(nil)
+      allow(nil).to receive(:name).and_return(true)
+    end
+    it "should receive Review Rubrics and redirect to list" do
+      allow(nil).to receive(:find_by_name).with("Review").and_return(nil)
+        get "go_to_menu_items", params1: "Review Rubrics"
+        expect(response).to redirect_to(list_tree_display_index_path)
+    end
+    it "should receive Teammate review rubrics and redirect to list" do
+      allow(nil).to receive(:find_by_name).with("Teammate review").and_return(nil)
+      get "go_to_menu_items", params1: "Teammate review rubrics"
+      expect(response).to redirect_to(list_tree_display_index_path)
+    end
+    it "should receive Metareview rubrics and redirect to list" do
+      allow(nil).to receive(:find_by_name).with("Metareview").and_return(nil)
+      get "go_to_menu_items", params1: "Metareview rubrics"
+      expect(response).to redirect_to(list_tree_display_index_path)
+    end
+
+    it "should receive Author feedbacks and redirect to list" do
+      allow(nil).to receive(:find_by_name).with("Author feedbacks").and_return(nil)
+      get "go_to_menu_items", params1: "Author Feedback"
+      expect(response).to redirect_to(list_tree_display_index_path)
+    end
+    it "should receive Global surveys and redirect to list" do
+      allow(nil).to receive(:find_by_name).with("Global surveys").and_return(nil)
+      get "go_to_menu_items", params1: "Global Survey"
+      expect(response).to redirect_to(list_tree_display_index_path)
+    end
+    it "should receive Course evaluations and redirect to list" do
+      allow(nil).to receive(:find_by_name).with("Course evaluations").and_return(nil)
+      get "go_to_menu_items", params1: "Course Evaluation"
+      expect(response).to redirect_to(list_tree_display_index_path)
+    end
+    it "should receive Surveys and redirect to list" do
+      allow(nil).to receive(:find_by_name).with("Surveys").and_return(nil)
+      get "go_to_menu_items", params1: "Survey"
+      expect(response).to redirect_to(list_tree_display_index_path)
+    end
+    it "should redirect to root_url if request parameter is invalid" do
+      allow(nil).to receive(:find_by_name).with(nil).and_return(nil)
+      get "go_to_menu_items"
+      expect(response).to redirect_to(root_path)
+    end
+  end
+
+
+
 end

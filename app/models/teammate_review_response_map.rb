60cd21ec98691b047bd810267c1d84d0f8a078d8
citizen-kane <kagarwa@ncsu.edu>
2015-10-30 22:12:56 -0400

    Update teammate_review_response_map.rb

--- app/models/teammate_review_response_map.rb
+++ app/models/teammate_review_response_map.rb
@@ -13,4 +13,26 @@ class TeammateReviewResponseMap < ResponseMap
   def get_title
     return "Teammate Review"
   end
+  def email4(map_id, partial="new_submission")
+    defn = Hash.new
+    defn[:body] = Hash.new
+    defn[:body][:partial_name] = partial
+    response_map = ResponseMap.find map_id
+    assignment=nil
+
+    reviewer_participant_id =  response_map.reviewer_id
+    participant = Participant.find(reviewer_participant_id)
+    assignment = Assignment.find(participant.parent_id)
+
+    defn[:subject] = "A new submission is available for "+assignment.name
+    defn[:body][:type] = "Teammate Review"
+    participant = AssignmentParticipant.find(response_map.reviewee_id)
+    topic_id = SignedUpTeam.topic_id(participant.parent_id, participant.user_id)
+    defn[:body][:obj_name] = SignUpTopic.find(topic_id).topic_name
+    user = User.find(participant.user_id)
+    defn[:body][:first_name] = user.fullname
+    defn[:to] = user.email
+    Mailer.sync_message(defn).deliver
+
+  end
 end

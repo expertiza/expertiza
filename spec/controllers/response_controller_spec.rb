5bea66dee1ed1d5b5a01f51d7136355e3a63c79e
viswa <viswaraavi@yahoo.in>
2015-10-31 23:01:55 -0400

    Tests modified

--- spec/controllers/response_controller_spec.rb
+++ spec/controllers/response_controller_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 
 RSpec.describe ResponseController, type: :controller do
   before(:each)
-  response = create(:Response)
-  feedresposemap=create(:FeedbackResponseMap)
+  response = FactoryGirl.create(:Response)
+  feedresposemap=FactoryGirl.create(:FeedbackResponseMap)
 
   describe "GET #new_feedback" do
 
-    it "Should call find method"
-    Response.should_recieve(:find).with("Additional comments").and_return(:response)
+    it "Should call find method" do
+    Response.should_receive(:find).with("Additional comments").and_return(:response)
     end
-    it "should find response in feedresponsemap"
-    FeedbackResponseMap.should_recieve(:where).and_return(:feedresponsemap)
+    it "should find response in feedresponsemap" do
+    FeedbackResponseMap.should_receive(:where).and_return(:feedresponsemap)
     end
     it "returns http success" do
       
@@ -22,8 +22,8 @@ RSpec.describe ResponseController, type: :controller do
   end
 
   describe "GET #view" do
-     it "Should call find method"
-    Response.should_recieve(:find).with("Additional comments").and_return(:response)
+     it "Should call find method" do
+    Response.should_receive(:find).with("Additional comments")
     end
     it "returns http success" do
       get :view
@@ -32,8 +32,8 @@ RSpec.describe ResponseController, type: :controller do
   end
 
   describe "POST #delete" do
-    it "Should call find method"
-    Response.should_recieve(:find).with("Additional comments").and_return(:response)
+    it "Should call find method" do
+    Response.should_receive(:find).with("Additional comments").and_return(:response)
     end
     it "returns http success" do
 
@@ -45,8 +45,8 @@ RSpec.describe ResponseController, type: :controller do
 
 
   describe "GET #saving" do
-     it "Should call find method"
-    Response.should_recieve(:find).with("Additional comments").and_return(:response)
+     it "Should call find method" do
+    Response.should_receive(:find).with("Additional comments").and_return(:response)
     end
     it "returns http success" do
       get :saving
@@ -55,8 +55,8 @@ RSpec.describe ResponseController, type: :controller do
   end
 
   describe "GET #redirection" do
-     it "Should call find by map method"
-    Response.should_recieve(:find_by_map).with("Additional comments").and_return(:response)
+     it "Should call find by map method" do
+    Response.should_receive(:find_by_map).with("Additional comments").and_return(:response)
     end
 
     it "returns http success" do

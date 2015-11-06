5058b72e8c1de812ce7422ef3ce9dc83b72ee6d8
cnbrown4 <cnbrown4@ncsu.edu>
2015-11-06 14:41:01 -0500

    Updated get_metareview_map test
    This change was made to match the update made in the code

--- test/unit/review_response_map_test.rb
+++ test/unit/review_response_map_test.rb
@@ -12,7 +12,7 @@ class ReviewResponseMapTest < ActiveSupport::TestCase
 	test "method_get_metareview_response_maps" do
         	review_response_map_test = ReviewResponseMap.new
 		review_response_map_test.id = 1
-		assert_equal review_response_map_test.get_metareview_response_maps.count, 2
+		assert_equal review_response_map_test.get_metareview_response_maps.count, 1
 	end
 
 	test "method_get_team_response_for_round" do

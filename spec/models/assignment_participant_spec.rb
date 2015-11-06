ebea9d9ab21bf7b930fbcff056be8533c33b96bc
Akash Agrawal <aagrawa7@ncsu.edu>
2015-11-06 17:32:02 -0500

    Update assignment_participant_spec.rb

--- spec/models/assignment_participant_spec.rb
+++ spec/models/assignment_participant_spec.rb
@@ -2,39 +2,6 @@ require 'rails_helper'
 
 describe AssignmentParticipant do
 
-=begin
-  describe "review_score" do
-    it { ReviewQuestionnaire.should respond_to :review_score }
-  end
-=end
-
-
-  describe "calculate_Scores" do
-    it { is_expected.not_to respond_to(:between?).with(7).arguments }
-  end
-
-=begin
-  describe "a string" do
-    it { is_expected.to respond_to(:length) }
-  end
-=end
-=begin
-  it "should know about associated Projects" do
-    @user.should respond_to(:Scores)
-  end
-=end
-
-
-   # it "should do" do
-  #    should validate_presence_of(:assignment)
-   # end
-   # it {should validate_presence_of(:made)}
-
-=begin
-    it "should have date_till only if it has date_from"
-    its "date_till should be >= date_from"
-=end
-
   describe "calculateScores" do
 
     it "should_not.be less than 0" do

621c60a784d59aa1addff09c0af4a6304a31d51e
Shivam Gulati <shivamgulati1991@gmail.com>
2015-11-06 18:27:34 -0500

    fix

--- spec/models/assignment_participant_spec.rb
+++ spec/models/assignment_participant_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 describe AssignmentParticipant do
 
-  describe "calculateScores" do
+ # describe "calculate_scores" do
 #it "should do" do
 #    should validate_presence_of(:assignment)
 # end
 # it {should validate_presence_of(:made)}
     
-  end
+#  end
 
-  describe "Calculate_Scores" do
+  describe "calculate_scores" do
     it "should_not.be greater than 100" do
       100.should eq(100.0)
     end

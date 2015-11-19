bc6aa7f56f6cdad59055b35498396a6565a15a59
Rahul P Shah <rshah5@ncsu.edu>
2015-11-19 02:14:47 -0500

    password reset link

--- app/controllers/password_retrieval_controller.rb
+++ app/controllers/password_retrieval_controller.rb
@@ -7,19 +7,31 @@ class PasswordRetrievalController < ApplicationController
   def forgotten
   end
 
-  def send_password
+  def send_link
     if params[:user][:email].nil? || params[:user][:email].strip.length == 0
       flash[:error] = "Please enter an e-mail address"
     else
       user = User.find_by_email(params[:user][:email])
       if user
-        password = user.reset_password         # the password is reset
-        MailerHelper::send_mail_to_user(user, "Your Expertiza password has been reset", "send_password", password).deliver
-        flash[:success] = "A new password has been sent to your e-mail address."
+        link = ExpiryLink.generate_link(params[:user][:email])
+        MailerHelper::send_mail_to_user(user, "Reset your Expertiza Password", "send_link", link).deliver_later
+        flash[:success] = "The reset link has been sent to your e-mail address."
       else
         flash[:error] = "No account is associated with the address, \""+params[:user][:email]+"\". Please try again."
       end
     end
     redirect_to :action => 'forgotten'
   end
+  def reset_password
+    @expiry_link = ExpiryLink.where(link:params[:link]).first
+    if @expiry_link and @expiry_link.is_valid?
+      @user = User.find_by_login(@expiry_link.email)
+    else
+      
+      flash[:error] = "Link is expired or already used. Generate a new link."
+      redirect_to action: 'forgotten'
+    end
+  end
+ 
+
 end

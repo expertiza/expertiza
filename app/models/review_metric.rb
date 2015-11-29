c03797aefdf45f9af515d23f81a6b459448ef8f2
Expertiza Developer <sjolly@ncsu.edu>
2015-11-29 18:31:36 -0500

    Iteration2ReviewMetricCalculationCode

--- app/models/review_metric.rb
+++ app/models/review_metric.rb
@@ -1,7 +1,13 @@
 class ReviewMetric < ActiveRecord::Base
 
   def calulate_metric
-	self.total_word_count = 0
+	
+	@answer = Answer.where ("response_id = #{self.response_id}")
+	@count = 0 	
+	(0..@answer.count-1).each do |i|
+	@count += @answer[i][:comments].split.count
+	end
+	self.total_word_count = @count
 	self.diff_word_count = 0
 	self.suggestion_count = 0
 	self.error_count = 0

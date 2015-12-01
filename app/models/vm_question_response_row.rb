d0a92abd492c3f12dbd0335bb2ecbcdebf93f224
Matt Witte <mjwitte32@gmail.com>
2015-11-30 21:17:02 -0500

    Woops. I extracted VmQuestionResponseRow and put it in its own file. In doing so, I forgot to add it to be tracked by git. Should be golden now
    quit
    exit

---
+++ app/models/vm_question_response_row.rb
@@ -0,0 +1,35 @@
+class VmQuestionResponseRow
+
+  def initialize(questionText, question_id, weight)
+    @questionText = questionText
+    @question_id = question_id
+    @weight = weight
+    @score_row = Array.new
+  end
+
+  def questionText
+    @questionText
+  end
+
+  def question_id
+     @question_id
+  end
+
+  def score_row
+    @score_row
+  end
+
+  def weight
+    @weight
+  end
+  
+  
+  class VmQuestionResponseScoreCell
+
+    def initialize(score_value, color_code)
+      @score_value = score_value
+      @color_code = color_code
+    end
+  end
+
+end

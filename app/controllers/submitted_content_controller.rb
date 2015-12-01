2238f1c8f0859ce9e6602c3c9fc12db7d1ee9a2c
Adeesh <adeeshag@gmail.com>
2015-12-01 11:51:20 -0500

    Fixed a small bug in students view to list events across teams.

--- app/controllers/submitted_content_controller.rb
+++ app/controllers/submitted_content_controller.rb
@@ -29,8 +29,8 @@ class SubmittedContentController < ApplicationController
     @can_submit=true
 
     @stage = @assignment.get_current_stage(SignedUpTeam.topic_id(@participant.parent_id, @participant.user_id))
-    team_id = TeamsUser.team_id(@participant.parent_id, @participant.user_id)
-    @events = SubmissionHistory.where( ["team_id = ? AND assignment_id = ?", team_id, @participant.parent_id ]).page(params[:page]).order('id').per_page(10).all
+    @team_id = TeamsUser.team_id(@participant.parent_id, @participant.user_id)
+    @events = SubmissionHistory.where( ["team_id = ? AND assignment_id = ?", @team_id, @participant.parent_id ]).page(params[:page]).order('id').per_page(10).all
   end
 
   #view is called when @assignment.submission_allowed(topic_id) is false

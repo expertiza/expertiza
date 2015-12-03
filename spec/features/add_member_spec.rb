8745c24fbdff259cd251cdcecfe0d012f455d73e
hxia3 <hxia3@ncsu.edu>
2015-12-03 15:39:58 -0500

    fixed receiving invitation problem

--- spec/features/add_member_spec.rb
+++ spec/features/add_member_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 require_relative './helpers/login_helper'
 include LogInHelper
 
-def send_invi
+def send_inv
 
   click_link "Ethical analysis 2"
   click_link "Your team"
@@ -12,15 +12,27 @@ def send_invi
 end
 
 feature 'Add someone to a team' do
-  scenario 'send the invitation as sender' do
+  scenario 'send the invitation as sender', :js=>true do
 
     # sign in
-
     log_in('student4346', 'password')
-
+    page.driver.browser.manage.window.maximize
     # send invitation
-    send_invi
-    expect(page). to have_content('Waitings for reply')
+    send_inv
+    expect(page). to have_content('Waiting for reply')
+
+    # log out
+    click_link 'Logout'
+
+    # sign in as the receiver
+    log_in('student4347','password')
+    click_link "Ethical analysis 2"
+    click_link "Your team"
+
+    click_link 'Accept'
+    page.driver.browser.switch_to.alert.accept
+    expect(page). to have_content('student4346')
+
   end
 
 end

6ca1f0e238890896a94c9c04927878aa27df75f3
Junjie Shen <jshen6@ncsu.edu>
2015-12-03 00:58:22 -0500

    Done on the last two senarios, but need to clean up assignment database after each time

--- spec/features/E1582_create_two_round_review_assignments_spec.rb
+++ spec/features/E1582_create_two_round_review_assignments_spec.rb
@@ -1,9 +1,8 @@
 require 'spec_helper'
+require "selenium-webdriver"
 
 feature 'Create a two-round review assignment' do
-  scenario 'without a topic' do
-    #FIXME
-    #Capybara.default_max_wait_time = 5
+  scenario 'without a topic', :js => true do
     login_with 'instructor6', 'password'
     expect(page).to have_content('Manage content')
     click_link( 'Assignments', match: :first)
@@ -11,15 +10,10 @@ feature 'Create a two-round review assignment' do
     #find(:xpath, "(//a[text()='Assignments'])[2]").click
 
     visit new_assignment_path
-    #expect(page).to have_content('WCAE 2015')
-    #expect(page).to have_no_content('Select an assignment from the list or set')
-#AssignmentNode_1446_1
-    #click_button 'assignment_newfalse.1'
     expect(page).to have_content('New Assignment')
     fill_in 'Assignment name:', with: 'E1583_test_1'
     fill_in 'Submission directory', with: 'E1583_test_1'
     select "CSC/ECE 517, Spring 2015", :from => "assignment_form_assignment_course_id"
-    check 'assignment_form_assignment_staggered_deadline'
 
     click_button 'Create'
     expect(page).to have_content('You did not specify all necessary rubrics:')
@@ -27,31 +21,71 @@ feature 'Create a two-round review assignment' do
     fill_in 'Number of review rounds:', with: '2'
     click_button 'Set'
 
-    click_on 'Rubrics'
-    
-    expect(page).to have_content('Review rubric varies by round?')
-    #require 'pry'
-    #binding.pry
-    within('#questionnaire_table_ReviewQuestionnaire', visible: false) do
-      find("option[value='115']").click
-    end
-#questionnaire_table_ReviewQuestionnaire > td:nth-child(3) > select:nth-child(1) > option:nth-child(2)
-    #select "Chapter Review", from: "#questionnaire_table_ReviewQuestionnaire > td:nth-child(3) > select:nth-child(1)", visible: false
-    #page.all("wiki", match: :first, visible: false).last.click  
-#find("#questionnaire_table_ReviewQuestionnaire > td:nth-child(3) > select:nth-child(1)", :visible=>false)
-    #click_button 'Save'
-    #expect(page).to have_content('Assignment was successfully saved')
+    click_button 'Save'
+
+    # Potential bug of expertiza, changes of the 'number of review rounds' 
+    # will only take effect after click both the Set and Save button
+    click_on 'Due dates'
+ 
+    fill_in 'datetimepicker_submission_round_1', with: '2015/12/03 00:00'
+    fill_in 'datetimepicker_review_round_1', with: '2015/12/06 00:00'
+    fill_in 'datetimepicker_submission_round_2', with: '2015/12/11 00:00'
+    fill_in 'datetimepicker_review_round_2', with: '2015/12/14 00:00'
+
+    click_button 'Save'
+    expect(page).to have_content('Assignment was successfully saved')
   end
 
-  scenario 'with a topic' do
-    #FIXME
-    login_with 'instructor6', 'passwordrowssap'
-    expect(page).to have_content('Incorrect Name/Password')
+  scenario 'with a topic', :js => true do
+    login_with 'instructor6', 'password'
+    expect(page).to have_content('Manage content')
+    click_link( 'Assignments', match: :first)
+    expect(page).to have_content('Manage content')
+    #find(:xpath, "(//a[text()='Assignments'])[2]").click
 
+    visit new_assignment_path
+    expect(page).to have_content('New Assignment')
+    fill_in 'Assignment name:', with: 'E1583_test_2'
+    fill_in 'Submission directory', with: 'E1583_test_2'
+    select "CSC/ECE 517, Spring 2015", :from => "assignment_form_assignment_course_id"
+    #check 'assignment_form_assignment_staggered_deadline'
 
+    click_button 'Create'
+    expect(page).to have_content('You did not specify all necessary rubrics:')
+    
+    click_on 'Topics'
+    expect(page).to have_content('Signup topics have not yet been created.')
 
-    #within find('tr', text: 'My title') { click_link 'edit' }
-    #select 'No', from: "suggestion_signup_preference"
+    click_on 'New topic'
+
+    expect(page).to have_content('You are adding a topic to this assignment.')
+    click_button('OK')
+    expect(page).to have_content('Number of slots')
+    fill_in 'topic_topic_identifier', with: '999'
+    fill_in 'topic_topic_name', with: 'E1583_test_2_topic'
+    fill_in 'topic_category', with: 'E1583'
+    fill_in 'topic_max_choosers', with: '3'
+    click_button 'Create'
+
+    click_on 'Due dates'
+    fill_in 'Number of review rounds:', with: '2'
+    click_button 'Set'
+
+    click_button 'Save'
+    #expect(page).to have_content('Assignment was successfully saved')
+
+    
+    # Potential bug of expertiza, changes of the 'number of review rounds' 
+    # will only take effect after click both the Set and Save button
+    click_on 'Due dates'
+ 
+    fill_in 'datetimepicker_submission_round_1', with: '2015/12/03 00:00'
+    fill_in 'datetimepicker_review_round_1', with: '2015/12/06 00:00'
+    fill_in 'datetimepicker_submission_round_2', with: '2015/12/11 00:00'
+    fill_in 'datetimepicker_review_round_2', with: '2015/12/14 00:00'
+
+    click_button 'Save'
+    expect(page).to have_content('Assignment was successfully saved')
   end
 
   def login_with(username, password)

232f36c796e4e38229be3d4a1a1f69fe7bd8e109
kmjos <kmjos@ncsu.edu>
2015-12-04 02:52:49 -0500

    trying to get review strategy working

--- app/assets/javascripts/review_strategy.jsx
+++ app/assets/javascripts/review_strategy.jsx
@@ -7,18 +7,20 @@ jQuery(document).ready(function() {
             var _strategyDetails = [];
             var strategyType;
 
-            moreContent.push(
+//            moreContent.push(
+            return(
                 <table>
+                    <tbody>
                     <tr id='assignment_review_assignment_strategy_row'>
                         <td>
                             "Review Strategy: "
-                            <select id="dropdownlist" class="form-control">
+                            <select id="dropdownlist" className="form-control">
                                 <option><a href="#">Auto-Selected</a></option>
                                 <option><a href="#">Instructor-Selected</a></option>
                             </select>
 
                             <img src="/assets/info.png" title='- Instructor-Selected: Instructor decides who reviews whom
-                            - Auto-Selected: When a student is about to begin a review, Expertiza assigns that student a particular review.'></img>
+                            - Auto-Selected: When a student is about to begin a review, Expertiza assigns that student a particular review.'/>
                         </td>
                     </tr>
                     <tr>
@@ -26,20 +28,25 @@ jQuery(document).ready(function() {
                             //{_strategyDetails}
                             <tr id="assignment_review_topic_threshold_row">
                                 <td>
-                                    <img src="/assets/info.png" title='A topic is reviewable if the minimum number of reviews already done for the submissions on that topic is within k of the minimum number of reviews done on the least-reviewed submission on any topic.'></img>
-                                'Maximum number of reviews per submission:'
-                                <input type="text" class="form-control" value={this.props.assignment_form.assignment.max_reviews_per_submission} size: "1">
+                                    'Review topic threshold (k):'
+                                    //<input type="text" className="form-control" defaultValue={this.props.assignment_form.assignment.review_topic_threshold} size="1"/>
+                                    <img src="/assets/info.png" title='A topic is reviewable if the minimum number of reviews already done for the submissions on that topic is within k of the minimum number of reviews done on the least-reviewed submission on any topic.'/>
+                                    'Maximum number of reviews per submission:'
+                                    <br/>
+                                <input type="text" className="form-control" defaultValue={this.props.assignment_form.assignment.max_reviews_per_submission} size="1"/>
                                 </td>
                             </tr>
                         </td>
                     </tr>
+                    </tbody>
                 </table>
             );
             $('#dropdownlist option').on('click', function(){
                 strategyType = $(this).text();
             });
+            //return ({moreContent})
         }
-    });
+    })
     
     if (document.getElementById("review_strategy")) {
         React.render(
@@ -47,4 +54,4 @@ jQuery(document).ready(function() {
             document.getElementById("review_strategy")
         )
     }
-});
+})

28b3471cb73125cad3cf5515e1326f1e28ae979b
jcinma <jcinma@gmail.com>
2015-12-04 16:16:59 -0500

    just creating the stub. will need to fill in

---
+++ app/assets/javascripts/topic_edit_assignment.jsx
@@ -0,0 +1,41 @@
+jQuery(document).ready(function() {
+    var topicTab = React.createClass() {
+
+    }
+})
+
+/* Source
+<h1>Topics for <%= @assignment_form.assignment.name %> assignment</h1>
+        <input name="assignment_form[assignment][allow_suggestions]" type="hidden" value="false"/>
+        <%= check_box_tag('assignment_form[assignment][allow_suggestions]', 'true', @assignment_form.assignment.allow_suggestions) %>
+        <%= label_tag('assignment_form[assignment][allow_suggestions]', 'Allow topic suggestions from students?') %>
+        <br>
+        <input name="assignment_form[assignment][is_intelligent]" type="hidden" value="false"/>
+        <%= check_box_tag('assignment_form[assignment][is_intelligent]', 'true', @assignment_form.assignment.is_intelligent?)%>
+        <%= label_tag('assignment_form[assignment][is_intelligent]', 'Enable bidding for topics?') %>
+        <img src="/assets/info.png" title="This feature allow students to &quot;bid&quot; for topics.
+      Instructor must specify when topics are assigned, by going to the Due Dates tab and
+      entering a due date for &quot;signup&quot;."/>
+        <br>
+        <input name="assignment_form[assignment][can_review_same_topic]" type="hidden" value="false"/>
+        <%= check_box_tag('assignment_form[assignment][can_review_same_topic]', 'true', @assignment_form.assignment.can_review_same_topic?)%>
+        <%= label_tag('assignment_form[assignment][can_review_same_topic]', 'Enable authors to review others working on same topic?') %>
+        <img src="/assets/info.png" title="If checked, it is possible that the auhtors review another artifact on the same topic "/>
+        <br>
+
+        <input name="assignment_form[assignment][can_choose_topic_to_review]" type="hidden" value="false"/>
+        <%= check_box_tag('assignment_form[assignment][can_choose_topic_to_review]', 'true', @assignment_form.assignment.can_choose_topic_to_review?)%>
+        <%= label_tag('assignment_form[assignment][can_choose_topic_to_review]', 'Allow reviewer to choose which topic to review?') %>
+
+        <br>
+        <input name="assignment_form[assignment][use_bookmark]" type="hidden" value="false"/>
+        <%= check_box_tag('assignment_form[assignment][use_bookmark]', 'true', @assignment_form.assignment.use_bookmark, {:onChange => 'useBookmarkChanged()'})%>
+        <%= label_tag('assignment_form[assignment][use_bookmark]', 'Allow participants to create bookmarks?') %>
+        <br><br>
+        <% if @assignment_form.assignment.staggered_deadline == true %>
+            <%session[:duedates] = SignUpSheet.add_signup_topic(@assignment_form.assignment.id)%>
+            <%= render '/sign_up_sheet/add_signup_topics_staggered.html' %>
+        <% else %>
+            <%= render '/sign_up_sheet/add_signup_topics.html' %>
+        <% end %>
+        */

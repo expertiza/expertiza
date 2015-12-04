cf63f7499e70d7357f05cddd6e28185041331f6f
kmjos <kmjos@ncsu.edu>
2015-12-04 00:53:23 -0500

    Working on review_strategy

--- app/assets/javascripts/assignments.jsx
+++ app/assets/javascripts/assignments.jsx
@@ -3,55 +3,103 @@ jQuery(document).ready(function() {
     var TabSystem = React.createClass({
         getInitialState: function() {
             return {
-                tableContent: {
-                    General: {},
-                    Topics: {},
-                    Rubrics: {},
-                    ReviewStrategy: {},
-                    DueDates: {}
-                },
-                activeTab: "1"
-            }
-        },
-        componentWillMount: function() {
-            var _this = this;
-
-            jQuery.get("/assignments/get_session_last_open_tab", function(data) {
-                _this.setState({
-                    activeTab: data
-                })
-            })
-
-        },
-        handleTabChange: function(tabIndex) {
-            jQuery.get("/assignments/set_session_last_open_tab?tab="+tabIndex.toString())
+                tabs: [
+                    {title: 'General', content: <script src="assignments/edit/general"></script>},
+                    {title: 'Topics', content: 'Content 2'},
+                    {title: 'Rubrics', content: 'Content 1'},
+                    {title: 'ReviewStrategy', content: 'Content 2'},
+                    {title: 'DueDates', content: 'Content 2'}
+                ],
+                active: 1
+            };
         },
         render: function() {
-            return (
-                <ReactSimpleTabs
-                    className="tab-system"
-                    tabActive={parseInt(this.state.activeTab)}
-                    onAfterChange={this.handleTabChange}
-                >
-                    <ReactSimpleTabs.Panel title="General">
-                        <script src="assignments/edit/general"></script>
-                    </ReactSimpleTabs.Panel>
-                    <ReactSimpleTabs.Panel title="Topics">
-
-                    </ReactSimpleTabs.Panel>
-                    <ReactSimpleTabs.Panel title="Rubrics">
-                        <script src="assignments/edit/rubrics"></script>
-                    </ReactSimpleTabs.Panel>
-                    <ReactSimpleTabs.Panel title="Review Strategy">
-                        <script src="assignments/edit/review_strategy"></script>
-                    </ReactSimpleTabs.Panel>
-                    <ReactSimpleTabs.Panel title="DueDates">
-                        <script src="assignments/edit/due_dates"></script>
-                    </ReactSimpleTabs.Panel>
-                </ReactSimpleTabs>
-            )
+            return <div>
+                <TabsSwitcher items={this.state.tabs} active={this.state.active} onTabClick={this.handleTabClick}/>
+                <TabsContent items={this.state.tabs} active={this.state.active}/>
+            </div>;
+        },
+        handleTabClick: function(index) {
+            this.setState({active: index})
         }
-    })
+    });
+
+    var TabsSwitcher = React.createClass({
+        render: function() {
+            var active = this.props.active;
+            var items = this.props.items.map(function(item, index) {
+                return <a href="#" className={'tab ' + (active === index ? 'tab_selected' : '')} onClick={this.onClick.bind(this, index)}>
+				{item.title}
+                </a>;
+            }.bind(this));
+            return <div>{items}</div>;
+        },
+        onClick: function(index) {
+            this.props.onTabClick(index);
+        }
+    });
+
+    var TabsContent = React.createClass({
+        render: function() {
+            var active = this.props.active;
+            var items = this.props.items.map(function(item, index) {
+                return <div className={'tabs-panel ' + (active === index ? 'tabs-panel_selected' : '')}>{item.content}</div>;
+            });
+            return <div>{items}</div>;
+        }
+    });
+
+    //    getInitialState: function() {
+    //        return {
+    //            tableContent: {
+    //                General: {},
+    //                Topics: {},
+    //                Rubrics: {},
+    //                ReviewStrategy: {},
+    //                DueDates: {}
+    //            },
+    //            activeTab: "1"
+    //        }
+    //    },
+    //    componentWillMount: function() {
+    //        var _this = this;
+    //
+    //        jQuery.get("/assignments/get_session_last_open_tab", function(data) {
+    //            _this.setState({
+    //                activeTab: data
+    //            })
+    //        })
+    //
+    //    },
+    //    handleTabChange: function(tabIndex) {
+    //        jQuery.get("/assignments/set_session_last_open_tab?tab="+tabIndex.toString())
+    //    },
+    //    render: function() {
+    //        return (
+    //            <ReactSimpleTabs
+    //                className="tab-system"
+    //                tabActive={parseInt(this.state.activeTab)}
+    //                onAfterChange={this.handleTabChange}
+    //            >
+    //                <ReactSimpleTabs.Panel title="General">
+    //                    <script src="assignments/edit/general"></script>
+    //                </ReactSimpleTabs.Panel>
+    //                <ReactSimpleTabs.Panel title="Topics">
+    //
+    //                </ReactSimpleTabs.Panel>
+    //                <ReactSimpleTabs.Panel title="Rubrics">
+    //                    <script src="assignments/edit/rubrics"></script>
+    //                </ReactSimpleTabs.Panel>
+    //                <ReactSimpleTabs.Panel title="Review Strategy">
+    //                    <script src="assignments/edit/review_strategy"></script>
+    //                </ReactSimpleTabs.Panel>
+    //                <ReactSimpleTabs.Panel title="DueDates">
+    //                    <script src="assignments/edit/due_dates"></script>
+    //                </ReactSimpleTabs.Panel>
+    //            </ReactSimpleTabs>
+    //        )
+    //    }
+    //})
     
     if (document.getElementById("assignments")) {
         React.render(
---
+++ app/assets/javascripts/review_strategy.jsx
@@ -0,0 +1,51 @@
+jQuery(document).ready(function() {
+
+
+    var StrategyTable = React.createClass({
+        render: function() {
+            var moreContent = [];
+            var _strategyDetails = [];
+            var strategyType;
+
+            moreContent.push(
+                <table>
+                    <tr id='assignment_review_assignment_strategy_row'>
+                        <td>
+                            "Review Strategy: "
+                            <select id="dropdownlist" class="form-control">
+                                <option><a href="#">Auto-Selected</a></option>
+                                <option><a href="#">Instructor-Selected</a></option>
+                            </select>
+
+                            <img src="/assets/info.png" title='- Instructor-Selected: Instructor decides who reviews whom
+                            - Auto-Selected: When a student is about to begin a review, Expertiza assigns that student a particular review.'></img>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            //{_strategyDetails}
+                            <tr id="assignment_review_topic_threshold_row">
+                                <td>
+                                    <img src="/assets/info.png" title='A topic is reviewable if the minimum number of reviews already done for the submissions on that topic is within k of the minimum number of reviews done on the least-reviewed submission on any topic.'></img>
+                                </br>
+                                'Maximum number of reviews per submission:'
+                                <input type="text" class="form-control" value={this.props.assignment_form.assignment.max_reviews_per_submission} size: "1">
+                                </td>
+                            </tr>
+                        </td>
+                    </tr>
+                </table>
+            );
+            $('#dropdownlist option').on('click', function(){
+                strategyType = $(this).text();
+            });
+        }
+    });
+    
+    if (document.getElementById("review_strategy")) {
+        React.render(
+            React.createElement(StrategyTable),
+            document.getElementById("review_strategy")
+        )
+    }
+});

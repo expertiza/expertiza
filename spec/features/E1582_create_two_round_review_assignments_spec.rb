b2cc6376cfd19399dfb8014f021df4646b9fa6d1
ginlee <liyu19931023@gmail.com>
2015-12-03 19:28:50 -0500

    make two round review test more DRY

--- spec/features/E1582_create_two_round_review_assignments_spec.rb
+++ spec/features/E1582_create_two_round_review_assignments_spec.rb
@@ -11,28 +11,9 @@ feature 'Create a two-round review assignment' do
 
     visit new_assignment_path
     expect(page).to have_content('New Assignment')
-    fill_in 'Assignment name:', with: 'E1583_test_1'
-    fill_in 'Submission directory', with: 'E1583_test_1'
-    select "CSC/ECE 517, Spring 2015", :from => "assignment_form_assignment_course_id"
-
-    click_button 'Create'
+    create_assignment('E1583_test_1')
     expect(page).to have_content('You did not specify all necessary rubrics:')
-    click_on 'Due dates'
-    fill_in 'Number of review rounds:', with: '2'
-    click_button 'Set'
-
-    click_button 'Save'
-
-    # Potential bug of expertiza, changes of the 'number of review rounds' 
-    # will only take effect after click both the Set and Save button
-    click_on 'Due dates'
- 
-    fill_in 'datetimepicker_submission_round_1', with: '2015/12/03 00:00'
-    fill_in 'datetimepicker_review_round_1', with: '2015/12/06 00:00'
-    fill_in 'datetimepicker_submission_round_2', with: '2015/12/11 00:00'
-    fill_in 'datetimepicker_review_round_2', with: '2015/12/14 00:00'
-
-    click_button 'Save'
+    set_due_dates
     expect(page).to have_content('Assignment was successfully saved')
     visit '/tree_display/list'
     click_link( 'Delete', match: :first)
@@ -47,12 +28,7 @@ feature 'Create a two-round review assignment' do
 
     visit new_assignment_path
     expect(page).to have_content('New Assignment')
-    fill_in 'Assignment name:', with: 'E1583_test_2'
-    fill_in 'Submission directory', with: 'E1583_test_2'
-    select "CSC/ECE 517, Spring 2015", :from => "assignment_form_assignment_course_id"
-    #check 'assignment_form_assignment_staggered_deadline'
-
-    click_button 'Create'
+    create_assignment('E1583_test_2')
     expect(page).to have_content('You did not specify all necessary rubrics:')
     
     click_on 'Topics'
@@ -68,28 +44,34 @@ feature 'Create a two-round review assignment' do
     fill_in 'topic_category', with: 'E1583'
     fill_in 'topic_max_choosers', with: '3'
     click_button 'Create'
+    set_due_dates
+    #expect(page).to have_content('Assignment was successfully saved')
+    expect(page).to have_content('Assignment was successfully saved')
+    visit '/tree_display/list'
+    click_link( 'Delete', match: :first)
+  end
 
+  def set_due_dates
     click_on 'Due dates'
     fill_in 'Number of review rounds:', with: '2'
     click_button 'Set'
-
     click_button 'Save'
-    #expect(page).to have_content('Assignment was successfully saved')
-
-    
-    # Potential bug of expertiza, changes of the 'number of review rounds' 
+    # Potential bug of expertiza, changes of the 'number of review rounds'
     # will only take effect after click both the Set and Save button
     click_on 'Due dates'
- 
+    #set the due dates time
     fill_in 'datetimepicker_submission_round_1', with: '2015/12/03 00:00'
     fill_in 'datetimepicker_review_round_1', with: '2015/12/06 00:00'
     fill_in 'datetimepicker_submission_round_2', with: '2015/12/11 00:00'
     fill_in 'datetimepicker_review_round_2', with: '2015/12/14 00:00'
-
     click_button 'Save'
-    expect(page).to have_content('Assignment was successfully saved')
-    visit '/tree_display/list'
-    click_link( 'Delete', match: :first)
+  end
+
+  def create_assignment(assignment_name)
+    fill_in 'Assignment name:', with: assignment_name
+    fill_in 'Submission directory', with: assignment_name
+    select "CSC/ECE 517, Spring 2015", :from => "assignment_form_assignment_course_id"
+    click_button 'Create'
   end
 
   def login_with(username, password)

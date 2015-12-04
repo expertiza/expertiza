a7139019555144e7445c0185d26a734a519624c0
kmjos <kmjos@ncsu.edu>
2015-12-04 18:22:22 -0500

    Partially completed addition of the topic_edit_assignment.jsx page

--- app/assets/javascripts/components/topic_edit_assignment.jsx
+++ app/assets/javascripts/components/topic_edit_assignment.jsx
@@ -1,6 +1,51 @@
-jQuery(document).ready(function() {
 
-})
+    var TopicTab = React.createClass({
+        render: function(){
+            var header = "Topics for " + this.props.assignment_form.assignment.name + " assignment";
+            return(
+                <span>
+                <h1><script type="text/javascript">document.write(header);</script></h1>
+
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" value={this.props.assignment_form.assignment.allow_suggestions}/>
+                                Allow topic suggestions from students
+                        </label>
+                    </div>
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" value={this.props.assignment_form.assignment.is_intelligent}/>
+                            Enable bidding for topics
+                        </label>
+                        <img src="/assets/info.png" title="This feature allow students to &quot;bid&quot; for topics.
+                        Instructor must specify when topics are assigned, by going to the Due Dates tab and
+                        entering a due date for &quot;signup&quot;."/>
+                    </div>
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" value={this.props.assignment_form.assignment.can_review_same_topic}/>
+                            Enable authors to review others working on same topic
+                        </label>
+                        <img src="/assets/info.png" title="If checked, it is possible that the authors review another artifact on the same topic "/>
+                    </div>
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" value={this.props.assignment_form.assignment.can_choose_topic_to_review}/>
+                            Allow reviewer to choose which topic to review
+                        </label>
+                    </div>
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" value={this.props.assignment_form.assignment.use_bookmark}/>
+                            Allow participants to create bookmarks
+                        </label>
+                    </div>
+                    <br></br>
+                </span>
+            );
+        }
+    })
+
 
 /* Source
 <h1>Topics for <%= @assignment_form.assignment.name %> assignment</h1>

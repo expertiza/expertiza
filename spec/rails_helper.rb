a6be53ceead6434958418111badb36b994b87ced
viswa teja <viswaraavi@yahoo.in>
2015-12-04 11:34:45 -0500

    Db cleaner config moddifed

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,7 +30,16 @@ RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
   end
-  config.before(:each)
+   config.before(:each) do |example|
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and

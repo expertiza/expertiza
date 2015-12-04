14928df75aa9051fedbdcb657e29bb84f6d5d0ef
Moharnab Saikia <moharnab123saikia@gmail.com>
2015-12-04 03:36:07 -0500

    added test for review_metric model

--- test/models/review_metric_test.rb
+++ test/models/review_metric_test.rb
@@ -4,4 +4,22 @@ class ReviewMetricTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @valid_review = ReviewMetric.new(response_id: Response.last.id)
+    @invalid_review = ReviewMetric.new
+  end
+
+  test "object should be valid " do
+    assert @valid_review.valid?
+  end
+  test "should calculate metrics for valid response_id" do
+    assert @valid_review.calulate_metric
+  end
+  test "should not calculate metrics for invalid response_id" do
+    assert_not @invalid_review.calulate_metric
+  end
+
+
+
 end

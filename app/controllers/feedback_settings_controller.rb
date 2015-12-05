8eb502b1cda9d1f002ba2b38e8f9dd49e5f3f9ea
kunal bhandari <kunal.bhandari6@gmail.com>
2015-12-04 22:08:18 -0500

    made changes to user email instead of id

---
+++ app/controllers/feedback_settings_controller.rb
@@ -0,0 +1,105 @@
+class FeedbackSettingsController < ApplicationController
+  before_action :set_feedback_setting, only: [:show, :edit, :update, :destroy]
+  before_action :set_file_types, only:[:show, :edit]
+  # GET /feedback_settings
+  def index
+    @feedback_settings = FeedbackSetting.all
+    @feedback_statuses = FeedbackStatus.all
+  end
+def action_allowed?
+    @settings = FeedbackSetting.find(1)
+    #if params[:action] == 'edit' or params[:action] == 'update'
+
+
+      if @current_user.present?
+        return true if @settings.support_team.include? @current_user.email
+        return false
+      end
+
+end
+  # GET /feedback_settings/1
+  def show
+  end
+
+  # GET /feedback_settings/new
+  def new
+    @feedback_setting = FeedbackSetting.new
+  end
+
+  # GET /feedback_settings/1/edit
+  def edit
+  end
+def add_attachment_type
+  @feedback_setting = FeedbackAttachmentSetting.new(feedback_attachment_params)
+  @feedback_setting.save
+  redirect_to :back, notice: 'Added File Type'
+end
+  def add_status
+    @feedback_status = FeedbackStatus.new(feedback_status_params)
+    @feedback_status.save
+    redirect_to :back, notice: 'Added File Type'
+  end
+  # POST /feedback_settings
+  def create
+    @feedback_setting = FeedbackSetting.new(feedback_setting_params)
+
+    if @feedback_setting.save
+      redirect_to @feedback_setting, notice: 'Feedback setting was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /feedback_settings/1
+  def update
+    if @feedback_setting.update(feedback_setting_params)
+      redirect_to @feedback_setting, notice: 'Feedback setting was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /feedback_settings/1
+  def destroy
+    @feedback_setting.destroy
+    redirect_to feedback_settings_url, notice: 'Feedback setting was successfully destroyed.'
+  end
+
+  def delete_attachment_type
+    @file_type= FeedbackAttachmentSetting.find(params[:id])
+    @file_type.destroy
+    redirect_to :back, notice: 'Attachment type successfully deleted.'
+
+  end
+  def delete_status
+    @status= FeedbackStatus.find(params[:id])
+    @status.destroy
+    redirect_to :back, notice: 'Attachment type successfully deleted.'
+
+  end
+
+  def set_file_types
+    @file_types = FeedbackAttachmentSetting.all
+    @file_type=FeedbackAttachmentSetting.new
+    @feedback_statuses = FeedbackStatus.all
+    @feedback_status = FeedbackStatus.new
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_feedback_setting
+      @feedback_setting = FeedbackSetting.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def feedback_setting_params
+      params.require(:feedback_setting).permit(:support_mail, :max_attachments, :max_attachment_size, :wrong_retries, :wait_duration, :wait_duration_increment, :support_team)
+    end
+
+    def feedback_attachment_params
+      params.require(:feedback_attachment_setting).permit(:file_type)
+    end
+  def feedback_status_params
+    params.require(:feedback_status).permit(:status)
+  end
+end
--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -2,13 +2,6 @@ class FeedbacksController < ApplicationController
   before_action :set_feedback, only: [:show, :edit, :update, :destroy]
   before_action :verify_captcha, only: [:create]
 
-  def verify_captcha
-    if !verify_recaptcha
-      redirect_to :back, notice: 'Wrong captcha'
-    else
-      @settings = FeedbackSetting.find(1)
-    end
-  end
 
   # GET /feedbacks
   def index
@@ -28,12 +21,7 @@ class FeedbacksController < ApplicationController
 
   # GET /feedbacks/new
   def new
-    user = session[:user]
-    if user.present?
-      @user_email = user.email
-    else
-      @user_email = nil
-    end
+
     @feedback = Feedback.new
   end
 
@@ -43,42 +31,48 @@ class FeedbacksController < ApplicationController
 
   end
 
+  def verify_captcha
+    if !verify_recaptcha
+      redirect_to :back, notice: 'Wrong captcha'
+    else
+      @settings = FeedbackSetting.find(1)
+    end
+  end
+
+  def verify_user
+    if @current_user.present?
+      @feedback.user_email = @current_user.email
+      return true
+    else
+      if User.find_by_email(params[:feedback][:user_email]).nil?
+        return false
+      end
+      return true
+    end
+  end
+
+
   # POST /feedbacks
   def create
     @feedback = Feedback.new(feedback_params)
     @feedback.status = "New"
 
-    user = session[:user]
-    if user.present?
-      @user_email = user.email
-    else
-      @user_email = nil
-    end
-
-    if @user_email.nil?
-      @user = User.find_by_email(params[:feedback][:user_id])
-    else
-      @user = User.find_by_email(@user_email)
-    end
-
-    if @user.present?
-      @feedback.user_id = @user.id
-
+    if verify_user
       if @feedback.save
         if params[:feedback][:attachment]
           create_attachment
         else
+          send_support_email
           redirect_to @feedback, notice: 'Feedback was successfully created.'
         end
       else
         redirect_to @feedback, notice: 'Feedback not created.'
       end
     else
-      wrong_retries_calculator
       redirect_to :back, notice: 'Please enter your registered email to Expertiza'
     end
-
   end
+
   def wrong_retries_calculator
     if session[:wrong_email_attempts]==nil
       session[:wrong_email_attempts]={:value => 0}
@@ -90,8 +84,23 @@ class FeedbacksController < ApplicationController
            session[:wrong_email_attempts][:expires] =  (@settings.wait_duration * wrong_retries_incrementer).minutes.from_now
          end
     end
-
   end
+
+  def send_support_email
+    # Get support email from feedback_setting table
+    @feedback_setting = FeedbackSetting.find(1)
+    Mailer.new_feedback_generated_message(
+        { to: @feedback_setting.support_mail,
+          subject: "A new feedback has been submitted",
+          body: {
+              email: @feedback.user_email,
+              title: @feedback.title,
+              description: @feedback.description
+          }
+        }
+    ).deliver
+  end
+
   def create_attachment
 
     if params[:feedback][:attachment].size > @settings.max_attachment_size.kilobytes
@@ -101,6 +110,7 @@ class FeedbacksController < ApplicationController
       @attachment.uploaded_file = params[:feedback][:attachment]
       @attachment.feedback_id = @feedback.id
       if @attachment.save
+        send_support_email
         redirect_to @feedback, notice: 'Feedback was successfully created.'
       else
         @feedback.destroy
@@ -147,6 +157,6 @@ class FeedbacksController < ApplicationController
 
   # Only allow a trusted parameter "white list" through.
   def feedback_params
-    params.require(:feedback).permit(:user_id, :title, :description, :status)
+    params.require(:feedback).permit(:user_email, :title, :description, :status)
   end
 end

6a0447b745f1b69e1b0dc97e5ea5a15db50ba11f
yangsong8 <ysong8@ncsu.edu>
2015-12-06 01:34:04 -0500

    Naming change: institutions_id->institution_id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -121,7 +121,7 @@ class UsersController < ApplicationController
       end
 
       @user = User.new(user_params)
-      @user.institutions_id=params[:users][:institutions_id]
+      @user.institution_id=params[:users][:institution_id]
       # record the person who created this new user
       @user.parent_id = (session[:user]).id
       # set the user's timezone to its parent's
@@ -206,7 +206,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :crypted_password, :role_id, :password_salt, :fullname, :email, :parent_id, :private_by_default, :mru_directory_path, :email_on_review, :email_on_submission, :email_on_review_of_review, :is_new_user, :master_permission_granted, :handle, :leaderboard_privacy, :digital_certificate, :persistence_token, :timezonepref, :public_key, :copy_of_emails,:institutions_id)
+    params.require(:user).permit(:name, :crypted_password, :role_id, :password_salt, :fullname, :email, :parent_id, :private_by_default, :mru_directory_path, :email_on_review, :email_on_submission, :email_on_review_of_review, :is_new_user, :master_permission_granted, :handle, :leaderboard_privacy, :digital_certificate, :persistence_token, :timezonepref, :public_key, :copy_of_emails,:institution_id)
   end
 
   def get_role

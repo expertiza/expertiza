64b6fc57de86e7fcc367d704edef63e4f3fe36da
Ankit <gohilankit@gmail.com>
2015-12-16 22:43:50 -0500

    modified tests to incoporate migrations on review_chats

--- test/models/review_chat_test.rb
+++ test/models/review_chat_test.rb
@@ -3,22 +3,12 @@ require 'test_helper'
 class ReviewChatTest < ActiveSupport::TestCase
 
  def setup
-	@review_chat = ReviewChat.new(assignment_id: '726', reviewer_id: '29103', team_id: '1000', type_flag: 'Q', content: 'This is test content.')
+	@review_chat = ReviewChat.new(response_map_id: '90207', type_flag: 'Q', content: 'This is test content.')
 	end
 	
-	test "assignment_id should be present" do
-	@review_chat.assignment_id = " "
-	assert_not @review_chat.assignment_id?
-	end
-
-	test "reviewer_id should be present" do
-	@review_chat.reviewer_id = " "
-	assert_not @review_chat.reviewer_id?
-	end
-
-	test "team_id should be present" do
-	@review_chat.team_id = " "
-	assert_not @review_chat.team_id?
+	test "respone_map_id should be present" do
+	@review_chat.response_map_id = " "
+	assert_not @review_chat.response_map_id?
 	end
 
 	test "content should have a maximum length of 255" do

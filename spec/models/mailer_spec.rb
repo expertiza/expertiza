8786139ca14584f17cb4a7d4a740dc83ec6af7c2
Winbobob <zhu6@ncsu.edu>
2015-12-29 04:38:05 -0500

    Refact unit tests: 1.Change the code to latest RSpec syntax; 2.Remove the depreciated warning; 3.Make the code follow DRY principle better.

--- spec/models/mailer_spec.rb
+++ spec/models/mailer_spec.rb
@@ -1,9 +1,9 @@
-require_relative '../rails_helper'
+require 'rails_helper'
 
 describe 'Tests mailer' do
   it 'should send email to required email address with proper content ' do
     # Send the email, then test that it got queued
-    email = Mailer.sync_message(
+    puts email = Mailer.sync_message(
         {:to => 'tluo@ncsu.edu',
          :subject => "Test",
          :body => {
@@ -14,11 +14,10 @@ describe 'Tests mailer' do
              :partial_name => 'update'
          }
         }
-    ).deliver
+    ).deliver_now
 
-    expect(email.from[0]).to eql("expertiza.development@gmail.com")
-    expect(email.to[0]).to eql('expertiza.development@gmail.com')
-    expect(email.subject).to eql('Test')
-    #assert_equal read_fixture('invite').join, email.body.to_s
+    expect(email.from[0]).to eq("expertiza.development@gmail.com")
+    expect(email.to[0]).to eq('expertiza.development@gmail.com')
+    expect(email.subject).to eq('Test')
   end
 end

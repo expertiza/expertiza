1238bb4c79c993f03b9d573444ee304ee199913f
yangsong8 <ysong8@ncsu.edu>
2015-12-30 16:25:24 -0500

    Merge E1577 (heat map)

---
+++ app/models/vm_question_response.rb
@@ -0,0 +1,192 @@
+#This is a new model create by E1577 (heat map)
+#represents each table in the view_team view.
+#the important piece to note is that the @listofrows is a  list of type VmQuestionResponse_Row, which represents a row of the heatgrid table.
+class VmQuestionResponse
+
+  def initialize(questionnaire, round,rounds)
+    @listofrows = []
+    @listofreviewers = []
+    @listofreviews = []
+    @listofteamparticipants = []
+    @max_score = questionnaire.max_question_score
+    @questionnaire_type = questionnaire.type
+    @questionnaire_display_type = questionnaire.display_type
+    @rounds = rounds
+    @round = round
+    @name  = questionnaire.name
+  end
+
+  def name
+    @name
+  end
+
+  def addQuestions(questions)
+    questions.each do |question|
+      # Get the maximum score for this question. For some unknown, godforsaken reason, the max
+      # score for the question is stored not on the question, but on the questionnaire. Neat.
+      corresponding_questionnaire = Questionnaire.find_by(id: question.questionnaire.id)
+      question_max_score = corresponding_questionnaire.max_question_score
+      row = VmQuestionResponseRow.new(question.txt, question.id, question.weight, question_max_score,question.seq)
+      @listofrows << row
+    end
+  end
+
+  def addReviews(participant,team,vary)
+
+    if @questionnaire_type == "ReviewQuestionnaire"
+      if @rounds == 1
+        reviews = participant.reviews()
+      else
+        reviews = ReviewResponseMap.get_assessments_round_for(team ,@round)
+      end
+      reviews.each do |review|
+        review_mapping = ReviewResponseMap.where(id: review.map_id).first
+        if review_mapping.present?
+          participant = Participant.find(review_mapping.reviewer_id)
+          @listofreviewers << participant
+          @listofreviews << review
+        end
+      end
+    elsif @questionnaire_type == "AuthorFeedbackQuestionnaire"
+      reviews = participant.feedback()     #feedback reviews
+      reviews.each do |review|
+        review_mapping = FeedbackResponseMap.where(id: review.map_id).first
+        participant = Participant.find(review_mapping.reviewer_id)
+        @listofreviewers << participant
+        @listofreviews << review
+      end
+    elsif @questionnaire_type == "TeammateReviewQuestionnaire"
+      reviews = participant.teammate_reviews()
+      reviews.each do |review|
+        review_mapping = TeammateReviewResponseMap.where(id: review.map_id).first
+        participant = Participant.find(review_mapping.reviewer_id)
+        #commenting out teamreviews. I just realized that teammate reviews are hidden during the current semester,
+        #and I don't know how to implement the logic, so I'm being safe.
+        #@listofreviewers << participant
+        #@listofreviews << review
+      end
+    elsif @questionnaire_type == "MetareviewQuestionnaire"
+      reviews = participant.metareviews()
+      reviews.each do |review|
+        review_mapping = MetareviewResponseMap.where(id: review.map_id).first
+        participant = Participant.find(review_mapping.reviewer_id)
+        @listofreviewers << participant
+        @listofreviews << review
+      end
+    end
+
+    reviews.each do |review|
+      answers = Answer.where(response_id: review.response_id)
+      answers.each do |answer|
+        addAnswer(answer)
+      end
+    end
+
+  end
+
+  def displayTeamMembers
+    @output = ""
+    if @questionnaire_type == "MetareviewQuestionnaire"  ||      @questionnaire_type == "ReviewQuestionnaire"
+      @output = "Team members:"
+      @listofteamparticipants.each do |participant|
+        @output = @output  +  " (" + participant.fullname + ") "
+      end
+
+    end
+
+    @output
+
+  end
+
+  def addTeamMembers(team)
+    @listofteamparticipants = team.participants
+  end
+
+  def listofteamparticipants
+    @listofteamparticipants
+  end
+
+  def max_score
+    @max_score
+  end
+
+  def max_score_for_questionnaire
+    @max_score * @listofrows.length
+  end
+
+  def max_score_for_questionnaire
+    @max_score * @listofrows.length
+  end
+
+  def rounds
+    @rounds
+  end
+
+  def round
+    @round
+  end
+
+  def questionnaire_type
+    @questionnaire_type
+  end
+
+  def questionnaire_display_type
+    @questionnaire_display_type
+  end
+
+  def listofreviews
+    @listofreviews
+  end
+  def listofrows
+    @listofrows
+
+  end
+
+  def listofreviewers
+    @listofreviewers
+  end
+
+  def addAnswer(answer)
+    # We want to add each response score from this review (answer) to its corresponding
+    # question row.
+    @listofrows.each do |row|
+      if row.question_id == answer.question_id
+        # Go ahead and calculate what the color code for this score should be.
+        question_max_score = row.question_max_score
+
+        # This calculation is a little tricky. We're going to find the percentage for this score,
+        # multiply it by 5, and then take the ceiling of that value to get the color code. This
+        # should work for any point value except 0 (which we'll handle separately).
+        color_code_number = 0
+        if answer.answer.is_a? Numeric
+          color_code_number = ((answer.answer.to_f / question_max_score.to_f) * 5.0).ceil
+          # Color code c0 is reserved for null spaces in the table which will be gray.
+          if color_code_number == 0
+            color_code_number = 1
+          end
+        end
+
+        # Now construct the color code and we're good to go!
+        color_code = "c#{color_code_number}"
+        row.score_row.push(VmQuestionResponseScoreCell.new(answer.answer, color_code, answer.comments))
+      end
+    end
+  end
+
+  def get_number_of_comments_greater_than_10_words()
+
+    first_time = true
+
+    @listofreviews.each do |review|
+      answers = Answer.where(response_id: review.response_id)
+      questionnaire = review.questionnaire_by_answer(answers.first)
+      answers.each do |answer|
+        @listofrows.each do |row|
+          if row.question_id == answer.question_id && answer.comments.to_s.length >10
+            row.countofcomments =  row.countofcomments + 1
+          end
+        end
+      end
+    end
+  end
+end

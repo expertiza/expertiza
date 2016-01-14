a1a68fb59372a981dddbf021cab64a0f687aca8e
Winbobob <zhu6@ncsu.edu>
2016-01-14 11:03:26 -0500

    Refactor the pending tests in assignment_spec.rb file.

--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -1,99 +1,98 @@
 require 'rails_helper'
 
-xdescribe "validations" do
-  it "assignment should exist" do
-    expect(FactoryGirl.create(:assignment)).to be_valid
+describe "validations" do
+
+  before(:each) do
+    @assignment = build(:assignment)
+  end
+
+  it "assignment is valid" do
+    expect(@assignment).to be_valid
   end
 	
-  it "assignment without name should not exist" do
-    expect(FactoryGirl.build(:assignment_without_name)).not_to be_valid
+  it "assignment without name is not valid" do
+    @assignment.name = nil
+    @assignment.save
+    expect(@assignment).not_to be_valid
   end
 
   it "checks whether Assignment Team is created or not" do
-     expect(FactoryGirl.create(:assignmentTeam)).to be_valid
+     expect(create(:assignment_team)).to be_valid
   end
 	
   it "checks whether signed up topic is created or not" do
-    expect(FactoryGirl.create(:signed_up_topic)).to be_valid
-  end
-
-end
-
-xdescribe "#team_assignment" do
-  it "checks team assignment should be true" do
-    assign = FactoryGirl.create(:assignment)
-    res = assign.team_assignment
-    expect(res).to be true
+    expect(create(:topic)).to be_valid
   end
 end
 
-xdescribe "#has_teams?" do
-  it "checks assignment should have a team" do
-    assign = FactoryGirl.build(:assignment)
-    assign_team = FactoryGirl.create(:assignmentTeam)
-    assign.teams << assign_team
-    assign.save!
-    res = assign.has_teams?
-    expect(res).to be true
+describe "#team_assignment" do
+  it "checks an assignment has team" do
+    assignment = build(:assignment)
+    expect(assignment.team_assignment).to be true
   end
 end
 
-xdescribe "#has_topics?" do
-  it "checks assignment should have a topic" do
-    assign_signed_up_topic = FactoryGirl.create(:signed_up_topic)
-    assign_topic = FactoryGirl.build(:assignment)
-    assign_topic.sign_up_topics << assign_signed_up_topic
-    assign_topic.save!
-    res = assign_topic.has_topics?
-    expect(res).to be true
+describe "#has_teams?" do
+  it "checks an assignment has a team" do
+    assignment = build(:assignment)
+    assign_team = build(:assignment_team)
+    assignment.teams << assign_team
+    expect(assignment.has_teams?).to be true
   end
 end
 
-xdescribe "#is_google_doc" do
+describe "#has_topics?" do
+  it "checks an assignment has a topic" do
+    assignment = build(:assignment)
+    topic = build(:topic)
+    assignment.sign_up_topics << topic
+    expect(assignment.has_topics?).to be true
+  end
+end
+
+describe "#is_google_doc" do
   it "checks whether assignment is a google doc" do
     skip('#is_google_doc no longer exists in assignment.rb file.')
-    assign = FactoryGirl.create(:assignment)
-    res = assign.is_google_doc
+    assignment = build(:assignment)
+    res = assignment.is_google_doc
     expect(res).to be false
   end
 end
 
-xdescribe "#is_microtask?" do
+describe "#is_microtask?" do
   it "checks whether assignment is a micro task" do
-    assign = FactoryGirl.create(:assignment)
-    id = assign.is_microtask?
-    expect(id).to be true
+    assignment = build(:assignment, microtask: true)
+    expect(assignment.is_microtask?).to be true
   end
 end
 
-xdescribe "#dynamic_reviewer_assignment?" do
+describe "#dynamic_reviewer_assignment?" do
   it "checks the Review Strategy Assignment" do
-    assign = FactoryGirl.create(:assignment)
-    id = assign.dynamic_reviewer_assignment?
-    expect(id).to be true
+    assignment = build(:assignment)
+    expect(assignment.dynamic_reviewer_assignment?).to be true
   end
 end
 
-xdescribe "#is_coding_assignment?" do
-  it "checks assignment should be coding assignment" do
-    expect(FactoryGirl.create(:assignment)).to be_valid
+describe "#is_coding_assignment?" do
+  it "checks assignment is coding assignment or not" do
+    assignment = build(:assignment)
+    expect(assignment.is_coding_assignment?).to be false
    end
 end
 
-xdescribe "#candidate_assignment_teams_to_review" do
+describe "#candidate_assignment_teams_to_review" do
   it "returns nil if if there are no contributors" do
-    assign = FactoryGirl.create(:assignment)
-    reviewer = FactoryGirl.create(:user)
-    cand_team = assign.candidate_assignment_teams_to_review(reviewer)
+    assignment = build(:assignment)
+    reviewer = build(:participant)
+    cand_team = assignment.candidate_assignment_teams_to_review(reviewer)
     expect(cand_team).to be_empty
   end
-
 end
 
-xdescribe "#candidate_topics_for_quiz" do
+describe "#candidate_topics_for_quiz" do
   it "returns nil if sign up topic is empty" do
-    assign = FactoryGirl.create(:assignment)
-    cand_team = assign.candidate_topics_for_quiz
-    expect(cand_team).to be_nil
+    assignment = build(:assignment)
+    cand_topic = assignment.candidate_topics_for_quiz
+    expect(cand_topic).to be_nil
   end
 end

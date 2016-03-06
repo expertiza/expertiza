5062df226cf14154d7992780edb1071af099bbcd
EricHorton <ewhorton@ncsu.edu>
2016-03-06 18:09:24 -0500

    Finishing submission tests.

--- spec/features/calibration_spec.rb
+++ spec/features/calibration_spec.rb
@@ -137,16 +137,79 @@ describe 'Submitter' do
 
   # Set up for testing
   before :each do
-    # TODO set up any needed state
+    # Create an instructor and student
+    @instructor = create :instructor
+    @student = create :student
+    @submitter = create :student
+
+    # Create an assignment with calibration
+    # Either course: nil is required or an AssignmentNode must also be created.
+    # The page will not load if the assignment has a course but no mapping node.
+    @assignment = create :assignment, is_calibrated: true, instructor: @instructor, course: nil
+
+    # Create an assignment due date
+    create(:deadline_type,name:"submission")
+    create(:deadline_type,name:"review")
+    create(:deadline_type,name:"resubmission")
+    create(:deadline_type,name:"rereview")
+    create(:deadline_type,name:"metareview")
+    create(:deadline_type,name:"drop_topic")
+    create(:deadline_type,name:"signup")
+    create(:deadline_type,name:"team_formation")
+    create(:deadline_right)
+    create(:deadline_right, name: 'Late')
+    create(:deadline_right, name: 'OK')
+    create :due_date, due_at: (DateTime.now + 1)
+
+    # Create and map a questionnaire (rubric) to the assignment
+    @questionnaire = create :questionnaire
+    create :assignment_questionnaire, assignment: @assignment, questionnaire: @questionnaire
+
+    # Create a team linked to the calibrated assignment
+    @team = create :assignment_team, assignment: @assignment
+
+    # Create an assignment participant linked to the assignment
+    @participant = create :participant, assignment: @assignment, user: @submitter
+
+    # Create a mapping between the assignment team and the
+    # participant object's user (the submitter).
+    create :team_user, team: @team, user: @submitter
   end
 
   # Verify submitters can be added to the assignment
   it 'can be added to the assignment by login' do
-    fail 'not yet implemented'
+    # Log in as the instructor
+    login_as @instructor.name
+
+    # Visit the add participant page
+    visit "/participants/list?id=#{@assignment.id}&model=Assignment"
+
+    # Student is not already a participant
+    expect(page).to have_no_link @student.name
+
+    # Add student as a submitter
+    fill_in 'user_name', with: @student.name
+    choose 'user_role_submitter'
+    click_on 'Add'
+
+    # Verify the submitter is listed
+    expect(page).to have_link @student.name
   end
 
   # Verify submitters can submit artifacts
   it 'can submit artifacts for calibration' do
-    fail 'not yet implemented'
+    # Log in as student
+    login_as @submitter.name
+
+    # Click on the assignment link, and navigate to work view
+    click_link @assignment.name
+    click_link 'Your work'
+
+    # Fill in submission with a url and submit
+    fill_in 'submission', with: 'https://google.com'
+    click_on 'Upload link'
+
+    # Verify presense of link on page
+    expect(page).to have_link 'https://google.com'
   end
 end

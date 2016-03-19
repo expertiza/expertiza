d8495cb46e335516c3a663cdf346ee441aa91dca
schen35 <schen35@ncsu.edu>
2016-03-19 17:23:04 -0400

    edit criterion

--- app/models/criterion.rb
+++ app/models/criterion.rb
@@ -11,15 +11,11 @@ class Criterion < ScoredQuestion
     html+='<td><input size="10" disabled="disabled" value="'+self.type+'" name="question['+self.id.to_s+'][type]" id="question_'+self.id.to_s+'_type" type="text">''</td>'
     html+='<td><input size="2" value="'+self.weight.to_s+'" name="question['+self.id.to_s+'][weight]" id="question_'+self.id.to_s+'_weight" type="text">''</td>'
     html+='<td>text area size <input size="3" value="'+self.size.to_s+'" name="question['+self.id.to_s+'][size]" id="question_'+self.id.to_s+'_size" type="text"></td>'
-<<<<<<< HEAD
+
     # html+='<td> max_label <input size="10" value="'+self.max_label.to_s+'" name="question['+self.id.to_s+'][max_label]" id="question_'+self.id.to_s+'_max_label" type="text">  min_label <input size="12" value="'+self.min_label.to_s+'" name="question['+self.id.to_s+'][min_label]" id="question_'+self.id.to_s+'_min_label" type="text"></td>'
     # html+='</tr>'
     html = edit_plus_html(html)
-=======
-    html+='<td> max_label <input size="10" value="'+self.max_label.to_s+'" name="question['+self.id.to_s+'][max_label]" id="question_'+self.id.to_s+'_max_label" type="text">  min_label <input size="10" value="'+self.min_label.to_s+'" name="question['+self.id.to_s+'][min_label]" id="question_'+self.id.to_s+'_min_label" type="text"></td>'
-    html+='</tr>'
 
->>>>>>> 7e5ef278cd5803a254e60254a6ef84aa288c9c88
     html.html_safe
   end
 

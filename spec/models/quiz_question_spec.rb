a72775c4622f32993b28aec87c021a80055c2569
Joseph Sankar <jesankar@ncsu.edu>
2016-03-19 17:54:24 -0400

    Refactored variable names and explicitly set flag for incorrect choices

--- spec/models/quiz_question_spec.rb
+++ spec/models/quiz_question_spec.rb
@@ -2,64 +2,67 @@ require 'rails_helper'
 
 describe QuizQuestion do
 
-  #let(:quiz_question){QuizQuestion.new id: 1, txt: "In which city is NCSU located?"}
+  #Create a multiple-choice radio question and assign it four choices
   before(:each) do
-    @q = QuizQuestion.new
-    @q.id = 1
-    @q.txt = "In which city is NCSU located?"
+    @question = MultipleChoiceRadio.new
+    @question.id = 1
+    @question.txt = "In which city is NCSU located?"
 
-    @q_c1 = QuizQuestionChoice.new
-    @q_c1.txt = "Raleigh"
-    @q_c1.iscorrect = true
+    @question_choice1 = QuizQuestionChoice.new
+    @question_choice1.txt = "Raleigh"
+    @question_choice1.iscorrect = true
 
-    @q_c2 = QuizQuestionChoice.new
-    @q_c2.txt = "Atlanta"
+    @question_choice2 = QuizQuestionChoice.new
+    @question_choice2.txt = "Atlanta"
+    @question_choice2.iscorrect = false
 
-    @q_c3 = QuizQuestionChoice.new
-    @q_c3.txt = "Charlotte"
+    @question_choice3 = QuizQuestionChoice.new
+    @question_choice3.txt = "Charlotte"
+    @question_choice3.iscorrect = false
 
-    @q_c4 = QuizQuestionChoice.new
-    @q_c4.txt = "North Carolina"
+    @question_choice4 = QuizQuestionChoice.new
+    @question_choice4.txt = "North Carolina"
+    @question_choice4.iscorrect = false
 
-    @q.quiz_question_choices << @q_c1
-    @q.quiz_question_choices << @q_c2
-    @q.quiz_question_choices << @q_c3
-    @q.quiz_question_choices << @q_c4
+    @question.quiz_question_choices << @question_choice1
+    @question.quiz_question_choices << @question_choice2
+    @question.quiz_question_choices << @question_choice3
+    @question.quiz_question_choices << @question_choice4
 
   end
 
   describe "#new" do
-    it "creates a new instance of QuizQuestion" do
-      expect(@q.class).to be(QuizQuestion)
+    it "creates a new instance of MultipleChoiceRadio" do
+      expect(@question.class).to be(MultipleChoiceRadio)
     end
   end
 
   describe "#txt" do
     it "returns the text of the question" do
-      expect(@q.txt).to eq("In which city is NCSU located?")
+      expect(@question.txt).to eq("In which city is NCSU located?")
     end
   end
 
   describe "view_question_text" do
     it "contains the text of the question" do
-      expect(@q.view_question_text.html_safe).to include("In which city is NCSU located?")
+      expect(@question.view_question_text.html_safe).to include("In which city is NCSU located?")
     end
 
     it "contains each of the choices" do
-      expect(@q.view_question_text.html_safe).to include("Raleigh")
-      expect(@q.view_question_text.html_safe).to include("Atlanta")
-      expect(@q.view_question_text.html_safe).to include("Charlotte")
-      expect(@q.view_question_text.html_safe).to include("North Carolina")
+      expect(@question.view_question_text.html_safe).to include("Raleigh")
+      expect(@question.view_question_text.html_safe).to include("Atlanta")
+      expect(@question.view_question_text.html_safe).to include("Charlotte")
+      expect(@question.view_question_text.html_safe).to include("North Carolina")
     end
 
     it "bolds the correct answer" do
-      expect(@q.view_question_text.html_safe).to include("<b>Raleigh</b>")
+      expect(@question.view_question_text.html_safe).to include("<b>Raleigh</b>")
     end
 
     it "does not bold incorrect answers" do
-      expect(@q.view_question_text.html_safe).not_to include("<b>Atlanta</b>")
-      expect(@q.view_question_text.html_safe).not_to include("<b>Charlotte</b>")
-      expect(@q.view_question_text.html_safe).not_to include("<b>North Carolina</b>")
+      expect(@question.view_question_text.html_safe).not_to include("<b>Atlanta</b>")
+      expect(@question.view_question_text.html_safe).not_to include("<b>Charlotte</b>")
+      expect(@question.view_question_text.html_safe).not_to include("<b>North Carolina</b>")
     end
   end
 end

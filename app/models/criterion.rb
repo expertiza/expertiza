213a5706a5f51cc5049c512cbc161cab49519208
schen35 <schen35@ncsu.edu>
2016-03-19 17:06:17 -0400

    edit criterion

--- app/models/criterion.rb
+++ app/models/criterion.rb
@@ -11,27 +11,30 @@ class Criterion < ScoredQuestion
     html+='<td><input size="10" disabled="disabled" value="'+self.type+'" name="question['+self.id.to_s+'][type]" id="question_'+self.id.to_s+'_type" type="text">''</td>'
     html+='<td><input size="2" value="'+self.weight.to_s+'" name="question['+self.id.to_s+'][weight]" id="question_'+self.id.to_s+'_weight" type="text">''</td>'
     html+='<td>text area size <input size="3" value="'+self.size.to_s+'" name="question['+self.id.to_s+'][size]" id="question_'+self.id.to_s+'_size" type="text"></td>'
-    html+='<td> max_label <input size="10" value="'+self.max_label.to_s+'" name="question['+self.id.to_s+'][max_label]" id="question_'+self.id.to_s+'_max_label" type="text">  min_label <input size="12" value="'+self.min_label.to_s+'" name="question['+self.id.to_s+'][min_label]" id="question_'+self.id.to_s+'_min_label" type="text"></td>'
-    html+='</tr>'
-
+    # html+='<td> max_label <input size="10" value="'+self.max_label.to_s+'" name="question['+self.id.to_s+'][max_label]" id="question_'+self.id.to_s+'_max_label" type="text">  min_label <input size="12" value="'+self.min_label.to_s+'" name="question['+self.id.to_s+'][min_label]" id="question_'+self.id.to_s+'_min_label" type="text"></td>'
+    # html+='</tr>'
+    html = edit_plus_html(html)
     html.html_safe
   end
 
+
+  ## This method has been refactor into the superclass scored_question.rb
+  ## code added  to remove duplicated code
   #This method returns what to display if an instructor (etc.) is viewing a questionnaire
-  def view_question_text
-    html = '<TR><TD align="left"> '+self.txt+' </TD>'
-    html += '<TD align="left">'+self.type+'</TD>'
-    html += '<td align="center">'+self.weight.to_s+'</TD>'
-    questionnaire = self.questionnaire
-    if !self.max_label.nil? && !self.min_label.nil?
-      html += '<TD align="center"> ('+self.min_label+') '+questionnaire.min_question_score.to_s+' to '+ questionnaire.max_question_score.to_s + ' ('+self.max_label+')</TD>'
-    else
-      html += '<TD align="center">'+questionnaire.min_question_score.to_s+' to '+ questionnaire.max_question_score.to_s + '</TD>'
-    end
-
-    html += '</TR>'
-    html.html_safe
-  end
+  # def view_question_text
+  #   html = '<TR><TD align="left"> '+self.txt+' </TD>'
+  #   html += '<TD align="left">'+self.type+'</TD>'
+  #   html += '<td align="center">'+self.weight.to_s+'</TD>'
+  #   questionnaire = self.questionnaire
+  #   if !self.max_label.nil? && !self.min_label.nil?
+  #     html += '<TD align="center"> ('+self.min_label+') '+questionnaire.min_question_score.to_s+' to '+ questionnaire.max_question_score.to_s + ' ('+self.max_label+')</TD>'
+  #   else
+  #     html += '<TD align="center">'+questionnaire.min_question_score.to_s+' to '+ questionnaire.max_question_score.to_s + '</TD>'
+  #   end
+  #
+  #   html += '</TR>'
+  #   html.html_safe
+  # end
 
   def complete(count, answer=nil, questionnaire_min, questionnaire_max, dropdown_or_scale)
   	if self.size.nil?
@@ -128,26 +131,36 @@ class Criterion < ScoredQuestion
       end
       html += '<td width="10%"></td></tr><tr>'
 
-      if !self.min_label.nil?
-        html += '<td width="10%">' +self.min_label+ '</td>'
-      else
-        html += '<td width="10%"></td>'
-      end
-      for j in questionnaire_min..questionnaire_max
-        html += '<td width="10%"><input type="radio" id="' +j.to_s+ '" value="' +j.to_s+ '" name="Radio_' +self.id.to_s+ '"'
-        html += 'checked="checked"' if (!answer.nil? and answer.answer == j) or (answer.nil? and questionnaire_min == j)
-        html += '></td>'
-      end
+      # if !self.min_label.nil?
+      #   html += '<td width="10%">' +self.min_label+ '</td>'
+      # else
+      #   html += '<td width="10%"></td>'
+      # end
+      ## code added  to remove duplicated code
+      html = complete_min_label_condition(html)
+
+
+      ##
+      # for j in questionnaire_min..questionnaire_max
+      #   html += '<td width="10%"><input type="radio" id="' +j.to_s+ '" value="' +j.to_s+ '" name="Radio_' +self.id.to_s+ '"'
+      #   html += 'checked="checked"' if (!answer.nil? and answer.answer == j) or (answer.nil? and questionnaire_min == j)
+      #   html += '></td>'
+      # end
+      ## code added  to remove duplicated code
+      html = complete_questionnaire_min_to_questionnaire_max(html, answer, questionnaire_min, questionnaire_max)
+
       html += '<script>jQuery("input[name=Radio_' +self.id.to_s+ ']:radio").change(function() {'
       html += 'var response_score = jQuery("#responses_' +count.to_s+ '_score");'
       html += 'var checked_value = jQuery("input[name=Radio_' +self.id.to_s+ ']:checked").val();'
       html += 'response_score.val(checked_value);});</script>'
 
-      if !self.max_label.nil?
-        html += '<td width="10%">' +self.max_label+ '</td>'
-      else
-        html += '<td width="10%"></td>'
-      end
+      # if !self.max_label.nil?
+      #   html += '<td width="10%">' +self.max_label+ '</td>'
+      # else
+      #   html += '<td width="10%"></td>'
+      # end
+      ## code added  to remove duplicated code
+      html = complete_max_label_condition(html)
 
       html += '<td width="10%"></td></tr></table>'
       html += '<textarea cols=' +cols+ ' rows=' +rows+ ' id="responses_' +count.to_s+ '_comments" name="responses[' +count.to_s+ '][comment]" style="overflow:hidden;">'

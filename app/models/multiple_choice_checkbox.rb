8957ec410a7c6c986a730573b696cf0b5c50e401
Joseph Sankar <jesankar@ncsu.edu>
2016-03-20 16:04:57 -0400

    Added tests as well as modified 'edit' method to work with saved choices

--- app/models/multiple_choice_checkbox.rb
+++ app/models/multiple_choice_checkbox.rb
@@ -1,6 +1,35 @@
 class MultipleChoiceCheckbox < QuizQuestion
-  def edit(count)
+  include ActionView::Helpers::FormHelper
 
+  def edit(count)
+    @question = self
+    questionnum = @question.id
+
+    html = ""
+
+    #QuizQuestion needs to do some generic stuff before,
+    #so we call it with a block.
+    super(count) do |common_html|
+      html += common_html
+
+      #@quiz_question_choices = QuizQuestionChoice.where(question_id: @question.id)
+      @quiz_question_choices = @question.quiz_question_choices
+
+      i = 1
+      for @quiz_question_choice in @quiz_question_choices
+        html += "<tr><td>&nbsp;&nbsp;&nbsp;"
+
+        html += hidden_field_tag("quiz_question_choices[#{questionnum}][MultipleChoiceCheckbox][#{i}][iscorrect]",'0')
+        html += check_box_tag("quiz_question_choices[#{questionnum}][MultipleChoiceCheckbox][#{i}][iscorrect]",'1', @quiz_question_choice.iscorrect)
+        html += "&nbsp;" + text_field_tag("quiz_question_choices[#{questionnum}][MultipleChoiceCheckbox][#{i}][txt]", @quiz_question_choice.txt, :size=>40)
+
+        html += "</td></tr>"
+
+        i += 1
+      end
+    end
+
+    html.html_safe
   end
 
   def complete(count, answer=nil)
--- app/models/quiz_question.rb
+++ app/models/quiz_question.rb
@@ -1,6 +1,19 @@
 class QuizQuestion < Question
   has_many :quiz_question_choices, :class_name => 'QuizQuestionChoice', :foreign_key => 'question_id'
-  def edit
+  
+  def edit(count)
+    common_html = "<tr>"
+    @question = self
+
+    #Display the text (common for all questions)
+    if $disp_flag != 1
+      common_html += "<td>" + text_area("question[]", 'txt', cols: 100) + "</td>"
+    end
+
+    common_html += "</tr>"
+
+    #Delegate specific functionality to type of question
+    yield common_html
   end
 
   def view_question_text

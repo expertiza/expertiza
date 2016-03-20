88479b64a28415080e122abb6addf66ed8970171
hawkwoodye <hawkwoodye@gmail.com>
2016-03-19 20:14:56 -0400

    new specs

--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -1,3 +1,4 @@
+
 require 'rails_helper'
 
 describe "validations" do
---
+++ spec/models/checkbox_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+#YJ
+describe 'validations' do
+
+
+  it 'should do something' do
+
+    true.should == false
+  end
+end

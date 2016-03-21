a35ff528b38a83f5946ac3a800b0ce7339bc6c5f
guanh01 <guanh01@gmail.com>
2016-03-21 14:49:08 -0400

    a little material added to scale_spec.rb

---
+++ spec/models/scale_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "scale" do
+  before(:each) do
+    @scale = build(:scale)
+  end
+  # it "scale is valid" do
+  #   expect(@scale).to be_valid
+  # end
+  it "returns a html if an instructor is creating or editing a questionnaire "
+  it "returns what to display if an instructor is viewing a questionnaire"
+  it "return what to display when students do peer reviews"
+  it "return what to diaplay when student view existed peer reviews"
+
+end

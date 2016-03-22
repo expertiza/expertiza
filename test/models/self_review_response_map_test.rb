db3555d4a638309dfd697e89fa8bbbd5523c25f2
sujithktkm <skataka@ncsu.edu>
2016-03-22 01:14:07 -0400

    Added fixtures and one new test

--- test/models/self_review_response_map_test.rb
+++ test/models/self_review_response_map_test.rb
@@ -4,4 +4,21 @@ class SelfReviewResponseMapTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  fixtures :questionnaires, :assignments
+
+  # Replace this with your real tests.
+
+  test "method_questionnaire" do
+    @questionnaire = questionnaires(:questionnaire0)
+    @assignment = assignments(:assignment1)
+    reviewrespmap = SelfReviewResponseMap.new
+    reviewrespmap.assignment = @assignment
+    reviewrespmap.questionnaire 1
+    assert_equal reviewrespmap.assignment.questionnaires[0].type, "ReviewQuestionnaire"
+  end
+
+  test "method_get_title" do
+    p = SelfReviewResponseMap.new
+    assert_equal p.get_title, "Self Review"
+  end
 end

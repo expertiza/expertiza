7f2dadbf0c6bccd874932ebc52a468a3d322f06d
Narasimha Reddy <narasimhadhanireddy@gmail.com>
2016-03-23 02:24:27 -0400

    Testing for Hash without Key

--- app/controllers/review_mapping_controller.rb
+++ app/controllers/review_mapping_controller.rb
@@ -529,13 +529,13 @@ class ReviewMappingController < ApplicationController
           unsorted_teams_hash[response_map.reviewee_id] += 1
         end
       end 
-      teams_hash = unsorted_teams_hash.sort_by{|k, v| v}.to_h
+      teams_hash = unsorted_teams_hash.sort_by{|_, v| v}.to_h
       participants_with_insufficient_review_num.each do |participant_id|
         teams_hash.each do |team_id, num_review_received|
           unless TeamsUser.exists?(team_id: team_id, user_id: Participant.find(participant_id).user_id)
             ReviewResponseMap.where(:reviewee_id => team_id, :reviewer_id => participant_id, :reviewed_object_id => assignment_id).first_or_create
             teams_hash[team_id] += 1
-            teams_hash = teams_hash.sort_by{|k, v| v}.to_h
+            teams_hash = teams_hash.sort_by{|_, v| v}.to_h
             break
           end
         end

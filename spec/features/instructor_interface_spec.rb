7d6f0ac8b83dd2e8c706dc69a29542fe43dfa631
Vijay Kumar Badugu <badugu.vijay@gmail.com>
2016-03-23 20:44:51 -0400

    Added test cases for import of assignment topics

--- spec/features/instructor_interface_spec.rb
+++ spec/features/instructor_interface_spec.rb
@@ -63,6 +63,59 @@ describe "Integration tests for instructor interface" do
     end
   end
 
+  describe "Import tests for assignment topics" do
+    it 'should be valid file with 3 columns' do
+      login_as("instructor6")
+      visit '/assignments/1/edit'
+      click_link "Topics"
+      click_link "Import topics"
+      file_path=Rails.root+"spec/features/assignment_topic_csvs/3-col-valid_topics_import.csv"
+      attach_file('file',file_path)
+      click_button "Import"
+      click_link "Topics"
+      expect(page).to have_content('expertiza')
+      expect(page).to have_content('mozilla')
+    end
+
+    it 'should be a valid file with 3 or more columns' do
+      login_as("instructor6")
+      visit '/assignments/1/edit'
+      click_link "Topics"
+      click_link "Import topics"
+      file_path=Rails.root+"spec/features/assignment_topic_csvs/3or4-col-valid_topics_import.csv"
+      attach_file('file',file_path)
+      click_button "Import"
+      click_link "Topics"
+      expect(page).to have_content('capybara')
+      expect(page).to have_content('cucumber')
+    end
+
+    it 'should be a invalid csv file' do
+      login_as("instructor6")
+      visit '/assignments/1/edit'
+      click_link "Topics"
+      click_link "Import topics"
+      file_path=Rails.root+"spec/features/assignment_topic_csvs/invalid_topics_import.csv"
+      attach_file('file',file_path)
+      click_button "Import"
+      click_link "Topics"
+      expect(page).should_not have_content('airtable')
+      expect(page).should_not have_content('devise')
+    end
+
+    it 'should be an random text file' do
+      login_as("instructor6")
+      visit '/assignments/1/edit'
+      click_link "Topics"
+      click_link "Import topics"
+      file_path=Rails.root+"spec/features/assignment_topic_csvs/random.txt"
+      attach_file('file',file_path)
+      click_button "Import"
+      click_link "Topics"
+      expect(page).should_not have_content('this is a random file which should fail')
+    end
+  end
+
   describe "View assignment scores" do
     it 'is able to view scores' do
       login_as("instructor6")

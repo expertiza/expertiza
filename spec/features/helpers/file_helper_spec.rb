b80cdc8ee00db35d46ffb2b698e00a4343b06e12
pranavkulkarni <PranavKulkarni@Pranavs-MacBook-Pro-2.local>
2016-03-29 18:42:04 -0400

    added a test case on file helper

--- spec/features/helpers/file_helper_spec.rb
+++ spec/features/helpers/file_helper_spec.rb
@@ -14,14 +14,8 @@ describe FileHelper do
 
   describe "#files" do
     it 'return files present in a dir' do
-      #x = Dir.entries("/")
-      #exp_array = x.map &->(i){File.expand_path(i)}
-      #expected_array = ["/Users/PranavKulkarni/Documents/zip car/Drivers+license.compressed.pdf", "/Users/PranavKulkarni/Documents/zip car/zipcar+declaration.compressed.pdf"]
-      #expect(@dummy_class.files("/Users/PranavKulkarni/Documents/zip car")).to match_array(expected_array)
-      #puts @dummy_class.files("/")
-
-      #expect(exp_array).to include(@dummy_class.files("/"))
-      #true.should == false
+      expected_array = ["/tmp/expertiza1603/1.txt", "/tmp/expertiza1603/2.txt"]
+      expect(@dummy_class.files("/tmp/expertiza1603")).to match_array(expected_array)
     end
   end
 

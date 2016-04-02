f5243fee9a2005b56b50279e87349c2a1ae54132
EricHorton <ewhorton@ncsu.edu>
2016-04-02 12:19:41 -0400

    Finishing test for validate_quiz.

--- spec/controllers/questionnaires_controller_spec.rb
+++ spec/controllers/questionnaires_controller_spec.rb
@@ -9,6 +9,14 @@ describe QuestionnairesController do
 
     # Create a new controller instance for testing.
     before :each do
+
+      # Create an instructor
+      @instructor = create(:instructor)
+
+      #Create an assignment with quiz
+      @assignment = create :assignment, require_quiz: true, instructor: @instructor, course: nil, num_quiz_questions: 1
+
+      # Create a params stub for the controller to work with
       @params = {
           :utf8 => '✓',
           :questionnaire => {
@@ -62,21 +70,55 @@ describe QuestionnairesController do
               }
           },
           :save => 'Create Quiz',
-          :aid => '1',
+          :aid => @assignment.id,
           :pid => '1',
           :controller => 'questionnaires',
           :action => 'create_quiz_questionnaire'
       }
 
-      # Create an instructor
-      @instructor = create(:instructor)
-
-      #Create an assignment with quiz
-      @assignment = create :assignment, require_quiz: true, instructor: @instructor, course: nil, num_quiz_questions: 1
-
-      # Initialize controller
+      # Initialize controller and stub params
       @controller = QuestionnairesController.new
       allow(@controller).to receive(:params).and_return(@params)
+    end
+
+    it 'does not validate if the quiz does not have a name' do
+      # Remove quiz name from params
+      @params[:questionnaire][:name] = ''
+
+      # Verify the quiz does not validate
+      expect(@controller.validate_quiz).to eq 'Please specify quiz name (please do not use your name or id).'
+    end
+
+    it 'does not validate if a question does not have a type' do
+      # Remove a question's type
+      @params[:question_type]['1'][:type] = nil
+
+      # Verify the quiz does not validate
+      expect(@controller.validate_quiz).to eq 'Please select a type for each question'
+    end
+
+    it 'does not validate if a question does not have text' do
+      # Delete a question's text
+      @params[:new_question]['1'] = ''
+
+      # Verify the quiz does not validate
+      expect(@controller.validate_quiz).to eq 'Please make sure all questions have text'
+    end
+
+    it 'does not validate if a question options does not have text' do
+      # Delete a question options's text
+      @params[:new_choices]['1']['MultipleChoiceRadio']['1'][:txt] = ''
+
+      # Verify the quiz does not validate
+      expect(@controller.validate_quiz).to eq 'Please make sure every question has text for all options'
+    end
+
+    it 'does not validate if a question does not have a correct option' do
+      # Remove the correct option
+      @params[:new_choices]['1']['MultipleChoiceRadio']['1'][:iscorrect] = nil
+
+      # Verify the quiz does not validate
+      expect(@controller.validate_quiz).to eq 'Please select a correct answer for all questions'
     end
 
     # Verify a correct params hash representing a quiz

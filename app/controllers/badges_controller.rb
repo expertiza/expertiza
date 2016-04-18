3aa5577b2576247b490b971a21e7b7330aee81c7
sujithktkm <skataka@ncsu.edu>
2016-04-18 01:18:55 -0400

    Grid view of badges while creating assignment groups

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -4,9 +4,40 @@ class BadgesController < ApplicationController
     true
   end
 
-  def new i=0
+  def new
     course_id = params[:course_id]
     @assignments = Assignment.where("course_id = ?", course_id)
+    @badge_url = Array.new
+    @badge_name = Array.new
+    @badge_id = Array.new
+
+    response = get_badges_created(session[:user].id)
+    parsed_response = JSON.parse(response.body)
+    user_data = nil
+
+    if response.code == '200' && !parsed_response['data'].nil?
+      parsed_response['data'].each do |badge|
+        @badge_url.push badge.image_url
+        @badge_name.push badge.title
+        @badge_id.push badge.id
+      end
+    else
+      user_data = parsed_response['meta']
+    end
+
+    # response = method_name(expertiza_admin_user_id)
+    parsed_response = JSON.parse(response.body)
+
+    if response.code == '200' && !parsed_response['data'].nil?
+      user_data = parsed_response['data']
+      user_data.each do |badge|
+        @badge_url.push badge.image_url
+        @badge_name.push badge.title
+        @badge_id.push badge.id
+      end
+    else
+      user_data = parsed_response['meta']
+    end
   end
 
   def create
@@ -22,4 +53,19 @@ class BadgesController < ApplicationController
 
   def configuration
   end
+
+  private
+
+  def get_badges_created(user_id)
+    uri = URI.parse("https://api.credly.com")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    token = User.select('credly_accesstoken').where("id = ?", user_id)
+    request = Net::HTTP::Get.new("/v1.1/me/badges/created?order_direction=ASC&access_token=" + token[0].credly_accesstoken)
+    request["X-Api-Key"] = "f14c0138c043c3159420f297276eab61"
+    request["X-Api-Secret"] = "6qmzTxOQZJfF5K1ExH80K+umX9gfU5lmtswycO9TycswGbKEIPwuoXxcIohF4d6go0FeLMRv9uV+MD0jmeQsHBDaTNKa+blumqcd+cfK1y5lqTbLiLZsxdue9vth3Lh9U6Juy1rvy2VGYo8EOqh46PMjOmmOTUIZan9vvaf8Z0I="
+    response = http.request(request)
+    response
+  end
+
 end

d68a03a819ceab6afd605ca8bf781dfb3ea9023c
saiteja09 <saiteja09@gmail.com>
2016-04-24 01:09:11 -0400

    added code to enlist users eligible for a badge in a assignment

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -39,30 +39,174 @@ class LeaderboardController < ApplicationController
       end
     end
 
+    student_badges = Hash.new()
 
     #GetBadgeGroups
     @badge_groups = BadgeGroup.where('course_id = ? and is_course_level_group = ?', params[:course_id], 0)
     @badge_groups.each do |badge_group|
-      @assignment_groups = AssignmentGroup.where('badge_group_id = ?', badge_group.id)
+      @assignment_groups = AssignmentGroup.where('badge_group_id = ?', badge_group.id).first
+      participant_scores = Hash.new()
+      if( assignment_stage[@assignment_groups.assignment_id] == 'Finished')
+        participant_assignment = Participant.where('parent_id = ?', @assignment_groups.assignment_id)
+        participant_assignment.each do |p|
+          score = get_scores p.id
+          begin
+            if score != nil and score.key?(:total_score)
+              participant_scores[p.user_id] = score[:total_score]
+            end
+          rescue
+            if score.is_a? Float
+              participant_scores[p.user_id] = score
+            end
+          end
+        end
+      end
+      sorted_scores = Hash[participant_scores.sort_by{|k, v| v}.reverse!]
+      get_eligible_users_for_badge badge_group, sorted_scores, @assignment_groups.assignment_id
+    end
+  end
 
 
+  def get_eligible_users_for_badge badge_group, sorted_scores, assignment_id
+    strategy = badge_group.strategy
+    threshold = badge_group.threshold
+    count_teams = Team.where('parent_id = ?', assignment_id )
+    count_participants = Participant.where('parent_id = ?', assignment_id)
+    final_users = nil
+
+
+    if(count_teams.count == count_participants.count)
+      if strategy == 'Top Scores'
+        final_users = get_users_top_scores_team_of_one sorted_scores , threshold
+      elsif strategy == 'Score Threshold'
+        final_users = get_users_threshold_team_of_one sorted_scores , threshold
+      end
+    else
+      if strategy == 'Top Scores'
+        final_users = get_users_top_scores_team_of_multiple sorted_scores , threshold, assignment_id
+      elsif strategy == 'Score Threshold'
+        final_users = get_users_threshold_team_of_multiple sorted_scores , threshold, assignment_id
+      end
     end
 
 
 
-    #GetScoresForAssignmentLevelBadges
+  end
 
-    #@participant = AssignmentParticipant.find(params[:id])
-    #@team_id = TeamsUser.team_id(@participant.parent_id, @participant.user_id)
-    #@assignment = @participant.assignment
-    ##@questions = {} # A hash containing all the questions in all the questionnaires used in this assignment
-    #questionnaires = @assignment.questionnaires
-    #retrieve_questions (questionnaires)
+
+  def get_users_top_scores_team_of_one sorted_scores,threshold
+    prev_value = 0
+    final_users = Array.new
+    rank =0
+    sorted_scores.each_with_index do |(k, v), i|
+      if i == 0
+        prev_value = v
+        rank = 1
+      end
+
+      if( rank < threshold)
+        final_users.push(k)
+      else
+        break
+      end
+      if(v < prev_value)
+        rank = rank + 1
+        prev_value = v
+      end
+    end
+    final_users
+  end
+
+  def get_users_threshold_team_of_one sorted_scores, threshold
+    final_users = Array.new
+    sorted_scores.each_with_index do |(k, v), i|
+      if v >= threshold
+        final_users.push(k)
+        break
+      end
+    end
+    final_users
+  end
+
+  def get_users_top_scores_team_of_multiple sorted_scores, threshold, assignment_id
+    final_users = Array.new
+    prev_value = 0
+    rank = 0
+    sorted_scores.each_with_index do |(k, v), i|
+      if i == 0
+        prev_value = v
+        rank = 1
+      end
+
+      if( rank < threshold)
+        if !final_users.include?(k)
+          final_users.push(k)
+        end
+        results = Team.joins(:teams_users).where('teams.parent_id= ? and teams_users.user_id=?', assignment_id, k)
+        team_id = results[0].id
+        team_users = TeamsUser.where('team_id = ?', team_id)
+        team_users.each do |tu|
+          if(!final_users.include?(k))
+            final_users.push(tu.user_id)
+          end
+        end
+      else
+        break
+      end
+      if(v < prev_value)
+        rank = rank + 1
+        prev_value = v
+      end
+    end
+    final_users
+  end
+
+  def get_users_threshold_team_of_multiple sorted_scores, threshold, assignment_id
+    final_users = Array.new
+    sorted_scores.each_with_index do |(k, v), i|
+      if v >= threshold
+        if !final_users.include?(k)
+          final_users.push(k)
+        end
+        results = Team.joins(:teams_users).where('teams.parent_id= ? and teams_users.user_id=?', assignment_id, k)
+        team_id = results[0].id
+        team_users = TeamsUser.where('team_id = ?', team_id)
+        team_users.each do |tu|
+          if(!final_users.include?(k))
+            final_users.push(tu.user_id)
+          end
+        end
+        break
+      end
+    end
+    final_users
+  end
+
+  #GetScoresForAssignmentLevelBadges
+  def get_scores participant_id
+
+    @participant = AssignmentParticipant.find(participant_id)
+    @team_id = TeamsUser.team_id(@participant.parent_id, @participant.user_id)
+    @assignment = @participant.assignment
+    @questions = {} # A hash containing all the questions in all the questionnaires used in this assignment
+    questionnaires = @assignment.questionnaires
+    retrieve_questions(questionnaires)
 
     #@pscore has the newest versions of response for each response map, and only one for each response map (unless it is vary rubric by round)
-    #@pscore = @participant.scores(@questions)
+    @pscore = @participant.scores(@questions)
 
-    j = 0
+  end
+
+  def retrieve_questions (questionnaires)
+    questionnaires.each do |questionnaire|
+      round = AssignmentQuestionnaire.where(assignment_id: @assignment.id, questionnaire_id:questionnaire.id).first.used_in_round
+      if(round!=nil)
+        questionnaire_symbol = (questionnaire.symbol.to_s+round.to_s).to_sym
+      else
+        questionnaire_symbol = questionnaire.symbol
+      end
+      @questions[questionnaire_symbol] = questionnaire.questions
+    end
   end
 
 end

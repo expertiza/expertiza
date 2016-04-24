f2528d69045193a81eebc7cccb7dbc68093a8cd9
sujithktkm <skataka@ncsu.edu>
2016-04-24 17:40:36 -0400

    Bug fixes - See comment for more details

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,4 +1,5 @@
 # E1626
 class Badge < ActiveRecord::Base
   has_many :badge_groups, :class_name => 'BadgeGroup', :foreign_key => 'badge_id'
+
 end
--- app/models/badge_group.rb
+++ app/models/badge_group.rb
@@ -2,4 +2,5 @@
 class BadgeGroup < ActiveRecord::Base
   belongs_to :badge
   belongs_to :course
+  has_many :assignment_groups
 end

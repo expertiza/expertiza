b4ec253e1698755dedcda3cd6f573f689732adeb
rroycho <rroycho@ncsu.edu>
2016-04-25 23:03:03 -0400

    Changes for coursr survey response

--- app/controllers/survey_response_controller.rb
+++ app/controllers/survey_response_controller.rb
@@ -11,11 +11,24 @@ class SurveyResponseController < ResponseController
       end
     elsif @type =="course"
       @course = Course.find(params[:id])
+      survey_deployment = SurveyDeployment.where('course_id = ?', @course.id)
+      if @survey_deployment.nil?
+        @empty = true
+      else
+        puts survey_deployment[:course_id]
+        @responses = SurveyResponse.where('survey_deployment_id = ?', survey_deployment[:id])
+      end
+
+
     end
   end
   def new_course_survey_response
     @survey_response_questionnaire = SurveyQuestionnaire.new
     @questionnaire = Questionnaire.find(params[:questionnaire_id])
     @questions = Question.where('questionnaire_id = ?',params[:questionnaire_id])
+  end
+
+  def submit
+
   end
 end

e08ed84b36bf0a300c66f15a66c49d2c15ad2455
yangsong8 <ysong8@ncsu.edu>
2016-04-28 12:32:36 -0400

    Fix a further issue found in fixing #682.

--- app/controllers/response_controller.rb
+++ app/controllers/response_controller.rb
@@ -199,7 +199,7 @@ class ResponseController < ApplicationController
 
     #@map.save
     msg = "Your response was successfully saved."
-    error_msg="Error"
+    error_msg=""
     @response.email();
     redirect_to :controller => 'response', :action => 'saving', :id => @map.map_id, :return => params[:return], :msg => msg, :error_msg => error_msg, :save_options => params[:save_options]
   end

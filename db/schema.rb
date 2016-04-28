01eb6b24726030e787af6ae5b52a811d7443c5d8
Expertiza Developer <dhaval0603@gmail.com>
2016-04-27 23:22:00 -0400

    Adding DB Schema file to address Tidy build fail

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160326205445) do
+ActiveRecord::Schema.define(version: 20160422010851) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "question_id", limit: 4,     default: 0, null: false
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 20160326205445) do
     t.boolean  "can_choose_topic_to_review", limit: 1,     default: true
     t.boolean  "is_calibrated",              limit: 1,     default: false
     t.boolean  "is_selfreview_enabled",      limit: 1
+    t.boolean  "reviewer_is_team",           limit: 1,     default: false
   end
 
   add_index "assignments", ["course_id"], name: "fk_assignments_courses", using: :btree
@@ -372,6 +373,7 @@ ActiveRecord::Schema.define(version: 20160326205445) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "calibrate_to",       limit: 1,   default: false
+    t.boolean  "reviewer_is_team",   limit: 1,   default: false
   end
 
   add_index "response_maps", ["reviewer_id"], name: "fk_response_map_reviewer", using: :btree

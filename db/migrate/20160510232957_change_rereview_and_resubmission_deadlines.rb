d6635ad773439cde813ae828f60a7010955cee18
yangsong8 <ysong8@ncsu.edu>
2016-05-10 19:56:08 -0400

    Change resubmission and rereview due dates to submission and review due dates

---
+++ db/migrate/20160510232957_change_rereview_and_resubmission_deadlines.rb
@@ -0,0 +1,17 @@
+class ChangeRereviewAndResubmissionDeadlines < ActiveRecord::Migration
+  def change
+    resubmission_deadlines = DueDate.where(:deadline_type_id=>3)
+    resubmission_deadlines.each do |resubmission_deadline|
+      resubmission_deadline.deadline_type = 1
+      resubmission_deadline.submission_allowed_id=3
+      resubmission_deadline.save
+    end
+
+    rereview_deadlines = DueDate.where(:deadline_type_id=>4)
+    rereview_deadlines.each do |resubmission_deadline|
+      rereview_deadline.deadline_type = 1
+      rereview_deadline.submission_allowed_id=3
+      rereview_deadline.save
+    end
+  end
+end

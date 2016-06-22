66182b4d0c587eab75a3c0bb6a32055fffd26a0f
yangsong8 <ysong8@ncsu.edu>
2016-06-22 11:40:37 -0400

    Refactor assignment.rb-move summarization code to answer.rb

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -139,6 +139,41 @@ class Answer < ActiveRecord::Base
       end
     end
 
+  #start added by ferry, required for the summarization (refactored by Yang on June 22, 2016)
+  def self.answers_by_question_for_reviewee_in_round(assignment_id, reviewee_id, q_id, round)
+    #  get all answers to this question
+    question_answer = Answer.select(:answer, :comments)
+                          .joins("join responses on responses.id = answers.response_id")
+                          .joins("join response_maps on responses.map_id = response_maps.id")
+                          .joins("join questions on questions.id = answers.question_id")
+                          .where("response_maps.reviewed_object_id = ? and
+                                           response_maps.reviewee_id = ? and
+                                           answers.question_id = ? and
+                                           responses.round = ?", assignment_id, reviewee_id, q_id, round)
+    return question_answer
+  end
+
+  def self.answers_by_question(assignment_id, q_id)
+    question_answer = Answer.select("DISTINCT answers.comments,  answers.answer")
+                          .joins("JOIN questions ON answers.question_id = questions.id")
+                          .joins("JOIN responses ON responses.id = answers.response_id")
+                          .joins("JOIN response_maps ON responses.map_id = response_maps.id")
+                          .where("answers.question_id = ? and response_maps.reviewed_object_id = ?", q_id, assignment_id)
+    return question_answer
+  end
+
+  def self.answers_by_question_for_reviewee(assignment_id, reviewee_id, q_id)
+    question_answers = Answer.select(:answer, :comments)
+                           .joins("join responses on responses.id = answers.response_id")
+                           .joins("join response_maps on responses.map_id = response_maps.id")
+                           .joins("join questions on questions.id = answers.question_id")
+                           .where("response_maps.reviewed_object_id = ? and
+                                                 response_maps.reviewee_id = ? and
+                                                 answers.question_id = ? ", assignment_id, reviewee_id, q_id )
+    return question_answers
+  end
+  # end added by ferry, required for the summarization
+
     require 'analytic/score_analytic'
     include ScoreAnalytic
   end

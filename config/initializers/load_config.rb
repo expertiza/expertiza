193b87d8476a05b1f6b3c27936ca838680e9a28a
yangsong8 <ysong8@ncsu.edu>
2016-07-15 10:09:39 -0400

    Revert "Merge branch 'rkyadav-ncsu-master'"
    This reverts commit 4a455501b8e591f951db0b7a5b7dc23570dedfdb, reversing
    changes made to dd3a08042003bb1534703f5492046d10407efacd.
    
    Conflicts:
    	app/controllers/response_controller.rb
    	config/webservices.yml

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,3 +1,1 @@
 GOOGLE_CONFIG = YAML.load_file("#{Rails.root }/config/google_auth.yml")[Rails.env]
-
-WEBSERVICE_CONFIG=YAML.load_file("#{Rails.root}/config/webservices.yml")[Rails.env]
--- test/models/metareview_score_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class MetareviewScoreTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end

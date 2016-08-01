e91532527151115bb7565a5c5a31549df66f0168
Winbobob <zhu6@ncsu.edu>
2016-08-01 13:12:26 -0400

    Update SECRET_KEY_BASE with Rails 4 style.

--- config/initializers/secret_token.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Expertiza::Application.config.secret_token = 'bde42e81831307228075769730f359975a518fbbe3e673bf4beacc377a1cf159b240d2c01f77e60f2999d6e24e05e908665c5c2c9cc4ddca4f2573710cb83f25'
-Expertiza::Application.config.secret_key_base = '8dc3f7e3f38da95f5dd0465a9de9625dab519f1329c0427fa8aa2161d059294baa11db1fab6c8d0993fa3f86c7f767c7baafbe904576948c6fa6a88c7d2e8761'

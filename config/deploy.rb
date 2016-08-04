6d640dc641a325a73e303e45e2d19171adcd0715
Zhewei Hu <zhu6@ncsu.edu>
2016-08-04 14:54:03 -0500

    Update deploy.rb
    Update `keep_releases` from 5 to 10

--- config/deploy.rb
+++ config/deploy.rb
@@ -56,7 +56,7 @@ set :linked_dirs, %w{log pg_data}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 10
 
 namespace :deploy do
 

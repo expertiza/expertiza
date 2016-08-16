e4373bec925b07ceac1a177ab025e42ade455832
yangsong8 <ysong8@ncsu.edu>
2016-08-16 10:59:19 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -354,7 +354,6 @@ Expertiza::Application.routes.draw do
       get :intelligent_sign_up
       get :intelligent_save
       post :save_topic_deadlines
-      post :save_topic_dependencies
     end
   end
 

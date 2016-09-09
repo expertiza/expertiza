20b14b15df941d0c00e803a41604b73179e55bea
yangsong8 <ysong8@ncsu.edu>
2016-09-09 17:12:31 -0400

    Add comment to assignment.rb

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,6 +1,6 @@
 ###
 ###
-### This class needs refactoring
+### We have spent a lot of time on refactoring this file, PLEASE consult with Expertiza development team before putting code in.
 ###
 ###
 class Assignment < ActiveRecord::Base

95818226449cbeac55aa14f2e1fd7b5bc30bcbc4
Winbobob <zhu6@ncsu.edu>
2016-09-30 16:05:36 -0400

    Show review grades and comments to students.

--- app/helpers/student_task_helper.rb
+++ app/helpers/student_task_helper.rb
@@ -1,2 +1,14 @@
 module StudentTaskHelper
+	def get_review_grade_info(participant)
+		info = ''
+		if participant.try(:grade_for_reviewer).nil? and participant.try(:comment_for_reviewer).empty?
+            result = "N/A"
+        else
+			info = "Score: " + participant.try(:grade_for_reviewer).to_s + "\n"
+			info += "Comment: " + participant.try(:comment_for_reviewer).to_s
+			info = truncate(info, length: 1500, omission: '...')
+			result = "<img src = '/assets/info.png' title = '" + info + "'>"
+		end
+		result.html_safe
+	end
 end

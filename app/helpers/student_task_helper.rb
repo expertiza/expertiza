0f76695a98fe7261467ced31bc58497c6ef82a31
Winbobob <zhu6@ncsu.edu>
2016-10-08 00:50:57 -0400

    Other's work enable condition: professor mentioned that a submission is reviewable if the submission is in a stage where reviews can be done. It does not depend on the stage of the topic the reviewer is writing on.

--- app/helpers/student_task_helper.rb
+++ app/helpers/student_task_helper.rb
@@ -1,14 +1,20 @@
 module StudentTaskHelper
-	def get_review_grade_info(participant)
-		info = ''
-		if participant.try(:grade_for_reviewer).nil? or participant.try(:comment_for_reviewer).nil?
-            result = "N/A"
-        else
-			info = "Score: " + participant.try(:grade_for_reviewer).to_s + "\n"
-			info += "Comment: " + participant.try(:comment_for_reviewer).to_s
-			info = truncate(info, length: 1500, omission: '...')
-			result = "<img src = '/assets/info.png' title = '" + info + "'>"
-		end
-		result.html_safe
-	end
+  def get_review_grade_info(participant)
+    info = ''
+    if participant.try(:grade_for_reviewer).nil? or participant.try(:comment_for_reviewer).nil?
+      result = "N/A"
+    else
+      info = "Score: " + participant.try(:grade_for_reviewer).to_s + "\n"
+      info += "Comment: " + participant.try(:comment_for_reviewer).to_s
+      info = truncate(info, length: 1500, omission: '...')
+      result = "<img src = '/assets/info.png' title = '" + info + "'>"
+    end
+    result.html_safe
+  end
+
+  def check_reviewable_topics(assignment)
+    sign_up_topics = SignUpTopic.where(assignment_id: assignment.id)
+    sign_up_topics.each {|topic| return true if assignment.can_review(topic.id) }
+    false
+  end
 end

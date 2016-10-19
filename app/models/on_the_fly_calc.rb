3cdc58debe66967a6ea59384ee4fb1c627d414e3
Aayush Agarwal <aayush@Aayushs-MacBook-Pro.local>
2016-10-19 00:58:43 -0400

    minor refactor for on_the_fly

--- app/models/on_the_fly_calc.rb
+++ app/models/on_the_fly_calc.rb
@@ -11,7 +11,6 @@ module OnTheFlyCalc
   def compute_reviews_hash
     @review_scores = {}
     @response_type = 'ReviewResponseMap'
-    # @myreviewers = ResponseMap.select('DISTINCT reviewer_id').where(['reviewed_object_id = ? && type = ? ', self.id, @response_type])
 
     # if this assignment uses vary rubric by rounds feature, load @questions for each round
     if self.varying_rubrics_by_round? # [reviewer_id][round][reviewee_id] = score
@@ -23,7 +22,6 @@ module OnTheFlyCalc
         @questions = Question.where(['questionnaire_id = ?', review_questionnaire_id])
 
         @response_maps.each do |response_map|
-          # Check if response is there
           @corresponding_response = Response.where(['map_id = ?', response_map.id])
           unless @corresponding_response.empty?
             @corresponding_response = @corresponding_response.reject {|response| response.round != round }
@@ -32,7 +30,6 @@ module OnTheFlyCalc
           @respective_scores = @review_scores[response_map.reviewer_id][round] if !@review_scores[response_map.reviewer_id].nil? && !@review_scores[response_map.reviewer_id][round].nil?
 
           if !@corresponding_response.empty?
-            # @corresponding_response is an array, Answer.get_total_score calculate the score for the last one
             @this_review_score_raw = Answer.get_total_score(response: @corresponding_response, questions: @questions)
             if @this_review_score_raw
               @this_review_score = ((@this_review_score_raw * 100) / 100.0).round if @this_review_score_raw >= 0.0
@@ -47,20 +44,18 @@ module OnTheFlyCalc
           @review_scores[response_map.reviewer_id][round] = @respective_scores
         end
       end
-    else # [reviewer_id][reviewee_id] = score
+    else
       @response_maps = ResponseMap.where(['reviewed_object_id = ? && type = ?', self.id, @response_type])
       review_questionnaire_id = review_questionnaire_id()
 
       @questions = Question.where(['questionnaire_id = ?', review_questionnaire_id])
 
       @response_maps.each do |response_map|
-        # Check if response is there
         @corresponding_response = Response.where(['map_id = ?', response_map.id])
         @respective_scores = {}
         @respective_scores = @review_scores[response_map.reviewer_id] unless @review_scores[response_map.reviewer_id].nil?
 
         if !@corresponding_response.empty?
-          # @corresponding_response is an array, Answer.get_total_score calculate the score for the last one
           @this_review_score_raw = Answer.get_total_score(response: @corresponding_response, questions: @questions)
           if @this_review_score_raw
             @this_review_score = ((@this_review_score_raw * 100) / 100.0).round if @this_review_score_raw >= 0.0
@@ -129,9 +124,7 @@ module OnTheFlyCalc
           round_sym = ("review" + i.to_s).to_sym
           grades_by_rounds[round_sym] = Answer.compute_scores(assessments, questions[round_sym])
           total_num_of_assessments += assessments.size
-          unless grades_by_rounds[round_sym][:avg].nil?
-            total_score += grades_by_rounds[round_sym][:avg] * assessments.size.to_f
-          end
+          total_score += grades_by_rounds[round_sym][:avg] * assessments.size.to_f unless grades_by_rounds[round_sym][:avg].nil?
         end
 
         # merge the grades from multiple rounds
@@ -144,9 +137,7 @@ module OnTheFlyCalc
           if !grades_by_rounds[round_sym][:max].nil? && scores[:teams][index.to_s.to_sym][:scores][:max] < grades_by_rounds[round_sym][:max]
             scores[:teams][index.to_s.to_sym][:scores][:max] = grades_by_rounds[round_sym][:max]
           end
-          if !grades_by_rounds[round_sym][:min].nil? && scores[:teams][index.to_s.to_sym][:scores][:min] > grades_by_rounds[round_sym][:min]
-            scores[:teams][index.to_s.to_sym][:scores][:min] = grades_by_rounds[round_sym][:min]
-          end
+          scores[:teams][index.to_s.to_sym][:scores][:min] = grades_by_rounds[round_sym][:min] if !grades_by_rounds[round_sym][:min].nil? && scores[:teams][index.to_s.to_sym][:scores][:min] > grades_by_rounds[round_sym][:min]
         end
 
         if total_num_of_assessments != 0

fd586415860da39ad0e993aae374eae65d2381af
radoshi7 <rushabh097@gmail.com>
2016-10-22 16:59:54 -0400

    line 22 bidss

--- app/controllers/lottery_controller.rb
+++ app/controllers/lottery_controller.rb
@@ -19,7 +19,7 @@ class LotteryController < ApplicationController
       bids = []
       topic_ids.each do |topic_id|
         bid_record = Bid.where(user_id: user_id, topic_id: topic_id).first rescue nil
-        bid_record.nil? ? bids << 0 : bids << bid_record.priority ||= 0
+        bids << bid_record.nil? ? 0 : bid_record.priority ||= 0
       end
       if bids.uniq != [0] and ![6864, 6865, 6866, 6855].include? user_id
         priority_info << {pid: user_id, ranks: bids}

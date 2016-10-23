a7bf2521ac9eb057e34903d0bd268b9c705298f4
svenka15 <svenka15@ncsu.edu>
2016-10-23 16:59:21 -0400

    common code removed

--- spec/features/instructor_interface_spec.rb
+++ spec/features/instructor_interface_spec.rb
@@ -1,23 +1,6 @@
 require 'rails_helper'
 
-describe "Integration tests for instructor interface" do
-  before(:each) do
-    create(:assignment)
-    create_list(:participant, 3)
-    create(:assignment_node)
-    create(:deadline_type, name: "submission")
-    create(:deadline_type, name: "review")
-    create(:deadline_type, name: "metareview")
-    create(:deadline_type, name: "drop_topic")
-    create(:deadline_type, name: "signup")
-    create(:deadline_type, name: "team_formation")
-    create(:deadline_right)
-    create(:deadline_right, name: 'Late')
-    create(:deadline_right, name: 'OK')
-    create(:assignment_due_date)
-    create(:assignment_due_date, deadline_type: DeadlineType.where(name: 'review').first, due_at: Time.now + (100 * 24 * 60 * 60))
-  end
-
+  common
   describe "Instructor login" do
     it "with valid username and password" do
       login_as("instructor6")

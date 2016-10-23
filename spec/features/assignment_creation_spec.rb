4443add3bb4ff0e9c58b9ee5b89c95a36a42c8b0
cvvishal <cvvishal@gmail.com>
2016-10-23 15:48:21 -0400

    indentation correction

--- spec/features/assignment_creation_spec.rb
+++ spec/features/assignment_creation_spec.rb
@@ -26,7 +26,7 @@ def get_selected_id(finder_var)
     TeammateReviewQuestionnaire.find_by_name(finder_var)[:id]
   end
 end
-def test()
+def test
       it "is able to create with teams" do
       login_as("instructor6")
       visit '/assignments/new?private=1'
@@ -45,8 +45,7 @@ def test()
         max_team_size: 3,
         show_teammate_reviews: true
       )
-    end
-
+      end
 end
 describe "assignment function" do
   before(:each) do
@@ -117,27 +116,24 @@ describe "assignment function" do
         spec_location: 'testLocation'
       )
     end
-    test()
+    test
     #it "is able to create with teams" do
-     # login_as("instructor6")
-      #visit '/assignments/new?private=1'
-
-      #fill_in 'assignment_form_assignment_name', with: 'private assignment for test'
-      #select('Course 2', from: 'assignment_form_assignment_course_id')
-      #fill_in 'assignment_form_assignment_directory_path', with: 'testDirectory'
-      #check("team_assignment")
-      #check("assignment_form_assignment_show_teammate_reviews")
-      #fill_in 'assignment_form_assignment_max_team_size', with: 3
-
-      #click_button 'Create'
-
-      #assignment = Assignment.where(name: 'private assignment for test').first
-      #expect(assignment).to have_attributes(
-       # max_team_size: 3,
-        #show_teammate_reviews: true
-      #)
+    # login_as("instructor6")
+    #visit '/assignments/new?private=1'
+    #fill_in 'assignment_form_assignment_name', with: 'private assignment for test'
+    #select('Course 2', from: 'assignment_form_assignment_course_id')
+    #fill_in 'assignment_form_assignment_directory_path', with: 'testDirectory'
+    #check("team_assignment")
+    #check("assignment_form_assignment_show_teammate_reviews")
+    #fill_in 'assignment_form_assignment_max_team_size', with: 3
+    #click_button 'Create'
+    #assignment = Assignment.where(name: 'private assignment for test').first
+    #expect(assignment).to have_attributes(
+    # max_team_size: 3,
+    #show_teammate_reviews: true
+    #)
     #end
-    test()
+    test
     it "is able to create with staggered deadline" do
       skip('skip test on staggered deadline temporarily')
       login_as("instructor6")

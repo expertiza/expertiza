7f14e9f9abc4786598a8345c09c56abb4e6e7a42
Kunal Kapoor <kkapoor@ncsu.edu>
2016-10-23 19:51:55 -0400

    RSpec tests created for due_date.rb

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -191,9 +191,9 @@ FactoryGirl.define do
     due_at "2015-12-30 23:30:12"
     deadline_type { DeadlineType.first || association(:deadline_type) }
     assignment { Assignment.first || association(:assignment) }
-    submission_allowed_id 3
-    review_allowed_id 3
-    review_of_review_allowed_id 3
+    submission_allowed_id {DeadlineRight.first || association(:deadline_right)}
+    review_allowed_id {DeadlineRight.first || association(:deadline_right)}
+    review_of_review_allowed_id {DeadlineRight.first || association(:deadline_right)}
     round 1
     flag false
     threshold 1
@@ -209,9 +209,9 @@ FactoryGirl.define do
     due_at "2015-12-30 23:30:12"
     deadline_type { DeadlineType.first || association(:deadline_type) }
     topic { SignUpTopic.first || association(:topic) }
-    submission_allowed_id 3
-    review_allowed_id 3
-    review_of_review_allowed_id 3
+    submission_allowed_id {DeadlineRight.first || association(:deadline_right)}
+    review_allowed_id {DeadlineRight.first || association(:deadline_right)}
+    review_of_review_allowed_id {DeadlineRight.first || association(:deadline_right)}
     round 1
     flag false
     threshold 1
@@ -317,7 +317,7 @@ FactoryGirl.define do
   end
 
   factory :response, class: Response do
-    review_response_map { ReviewResponseMap.first || association(:review_response_map) }
+    map_id { ReviewResponseMap.first || association(:review_response_map) }
     additional_comment nil
     version_num nil
     round nil

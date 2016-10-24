e5a323283b17a736be6013849c28bfde1329bf6f
LeoJY <leojy1993@gmail.com>
2016-10-23 20:10:35 -0400

    add should_not test to teammate_review_response_map

--- spec/models/teammate_review_response_map_spec.rb
+++ spec/models/teammate_review_response_map_spec.rb
@@ -7,16 +7,36 @@ describe TeammateReviewResponseMap do
     end
   end
   describe "id" do
+  #test all the id are stored correctly
+  	it "should be our exact teammatereviewresponsemap's id" do
+      expect(teammatereviewresponsemap.id).to eq(6)
+    end
+    it "should not be any other teammatereviewresponsemap's id" do
+      expect(teammatereviewresponsemap.id).not_to eq(7)
+    end
     it "should be our exact reviewer's id" do
       expect(teammatereviewresponsemap.reviewer_id).to eq(2)
+    end
+    it "should not be any other reviewer's id" do
+      expect(teammatereviewresponsemap.reviewer_id).not_to eq(3)
     end
     it "should be our exact reviewee's id" do
       expect(teammatereviewresponsemap.reviewee_id).to eq(1)
     end
+    it "should not be any other reviewee's id" do
+      expect(teammatereviewresponsemap.reviewee_id).not_to eq(2)
+    end
   end
   describe "title" do
-    it "should be Review" do
+  #test the title to be stored correctly
+    it "should be Teammate Review" do
       expect(teammatereviewresponsemap.get_title).to eq('Teammate Review')
+    end
+    it "should not be Review" do
+      expect(teammatereviewresponsemap.get_title).not_to eq('Review')
+    end
+    it "should not be Feedback Review" do
+      expect(teammatereviewresponsemap.get_title).not_to eq('Feedback Review')
     end
   end
 end

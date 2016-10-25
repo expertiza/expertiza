cd2f0726fc8fc41753ea0360797b29eb7a761349
anudeepvrm <anudeepvrm@gmail.com>
2016-10-25 19:14:35 -0400

    adding check for submission_valid? method call

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -3,39 +3,48 @@ require 'rails_helper'
 describe Answer do
 
 	describe "#test get total score" do
-			let!(:questionnaire) {create(:questionnaire)}
-			let!(:question1) {create(:question,:questionnaire => questionnaire,:weight=>1)}
-			let!(:question2) {create(:question,:questionnaire => questionnaire,:weight=>2)}
-			let!(:response_record) {create(:response_record,:id => 1)}
+		let!(:questionnaire) {create(:questionnaire)}
+		let!(:question1) {create(:question,:questionnaire => questionnaire,:weight=>1)}
+		let!(:question2) {create(:question,:questionnaire => questionnaire,:weight=>2)}
+		let!(:response_record) {create(:response_record,:id => 1)}
 			
-			it "returns total score when required conditions are met" do
-				# stub for ScoreView.find_by_sql to revent prevent unit testing sql db queries
-				ScoreView.stub(:find_by_sql).and_return([double("scoreview",weighted_score: 20,sum_of_weights: 5,q1_max_question_score: 4)])
-				Answer.stub(:where).and_return([double("row1",question_id: 1,answer: "1")])
-				expect(Answer.get_total_score(response: [response_record], questions: [question1])).to be 100.0
-				#calculation = (weighted_score / (sum_of_weights * max_question_score)) * 100
-				# 4.0
-			end
+		it "returns total score when required conditions are met" do
+			# stub for ScoreView.find_by_sql to revent prevent unit testing sql db queries
+			ScoreView.stub(:find_by_sql).and_return([double("scoreview",weighted_score: 20,sum_of_weights: 5,q1_max_question_score: 4)])
+			Answer.stub(:where).and_return([double("row1",question_id: 1,answer: "1")])
+			expect(Answer.get_total_score(response: [response_record], questions: [question1])).to be 100.0
+			#calculation = (weighted_score / (sum_of_weights * max_question_score)) * 100
+			# 4.0
+		end
 
-			it "returns total score when one answer is nil for scored question and its weight gets removed from sum_of_weights" do
-				ScoreView.stub(:find_by_sql).and_return([double("scoreview",weighted_score: 20,sum_of_weights: 5,q1_max_question_score: 4)])
-				Answer.stub(:where).and_return([double("row1",question_id: 1,answer: nil)])
-				expect(Answer.get_total_score(response: [response_record], questions: [question1])).to be_within(0.01).of(125.0)
-			end			
+		it "returns total score when one answer is nil for scored question and its weight gets removed from sum_of_weights" do
+			ScoreView.stub(:find_by_sql).and_return([double("scoreview",weighted_score: 20,sum_of_weights: 5,q1_max_question_score: 4)])
+			Answer.stub(:where).and_return([double("row1",question_id: 1,answer: nil)])
+			expect(Answer.get_total_score(response: [response_record], questions: [question1])).to be_within(0.01).of(125.0)
+		end			
 
-			it "returns -1 when answer is nil for scored question which makes sum of weights = 0" do
-				ScoreView.stub(:find_by_sql).and_return([double("scoreview",weighted_score: 20,sum_of_weights: 1,q1_max_question_score: 5)])
-				Answer.stub(:where).and_return([double("row1",question_id: 1,answer: nil)])
-				expect(Answer.get_total_score(response: [response_record], questions: [question1])).to be -1.0
-			end
+		it "returns -1 when answer is nil for scored question which makes sum of weights = 0" do
+			ScoreView.stub(:find_by_sql).and_return([double("scoreview",weighted_score: 20,sum_of_weights: 1,q1_max_question_score: 5)])
+			Answer.stub(:where).and_return([double("row1",question_id: 1,answer: nil)])
+			expect(Answer.get_total_score(response: [response_record], questions: [question1])).to be -1.0
+		end
 
-			it "returns -1 when weighted_score of questionnaireData is nil" do
-				ScoreView.stub(:find_by_sql).and_return([double("scoreview",weighted_score: nil,sum_of_weights: 5,q1_max_question_score: 5)])
-				Answer.stub(:where).and_return([double("row1",question_id: 1,answer: nil)])
-				expect(Answer.get_total_score(response: [response_record], questions: [question1])).to be -1.0
-			end
+		it "returns -1 when weighted_score of questionnaireData is nil" do
+			ScoreView.stub(:find_by_sql).and_return([double("scoreview",weighted_score: nil,sum_of_weights: 5,q1_max_question_score: 5)])
+			Answer.stub(:where).and_return([double("row1",question_id: 1,answer: nil)])
+			expect(Answer.get_total_score(response: [response_record], questions: [question1])).to be -1.0
+		end
 
+		it "checks if submission_valid? is called" do
+			ScoreView.stub(:find_by_sql).and_return([double("scoreview",weighted_score: nil,sum_of_weights: 5,q1_max_question_score: 5)])
+			Answer.stub(:where).and_return([double("row1",question_id: 1,answer: nil)])
+			expect(Answer).to receive(:submission_valid?)			
+			Answer.get_total_score(response: [response_record], questions: [question1])
 			
+		end
 	end
+
+	describe "#test" do
 	
+	end
 end

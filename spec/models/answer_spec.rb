6d7391fefe070e8768a023bf42db4a7b3df5dec3
anudeepvrm <anudeepvrm@gmail.com>
2016-10-25 20:53:29 -0400

    adding test cases for answer.compute_scores for invalid response

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -81,5 +81,17 @@ describe Answer do
 			expect(scores[:min]).to be total_score2
 			expect(scores[:avg]).to be (total_score1+total_score2)/2
 		end
+		
+		it "returns scores when an invalid assessments is given" do
+			response1 = double("respons1")
+			assessments=[response1]
+			Answer.instance_variable_set(:@invalid,1)
+			total_score=100.0
+			Answer.stub(:get_total_score).and_return(total_score)
+			scores=Answer.compute_scores(assessments, [:question1])
+			expect(scores[:max]).to be total_score
+			expect(scores[:min]).to be total_score
+			expect(scores[:avg]).to be 0
+		end
 	end
 end

a827e1fc86c974e3dde9362c1f136417dcf1d42d
canhsiao <canhsiao@gmail.com>
2016-10-25 21:11:25 -0400

    refactor join_team_requests

--- app/controllers/join_team_requests_controller.rb
+++ app/controllers/join_team_requests_controller.rb
@@ -1,31 +1,23 @@
 class JoinTeamRequestsController < ApplicationController
+	# decide if the controller is accessisable to the user
   def action_allowed?
     current_role_name.eql?("Student")
   end
 
   def index
     @join_team_requests = JoinTeamRequest.all
+    render
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render xml: @join_team_requests }
-    end
   end
 
   def show
     @join_team_request = JoinTeamRequest.find(params[:id])
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render xml: @join_team_request }
-    end
+    render
   end
 
   def new
     @join_team_request = JoinTeamRequest.new
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render xml: @join_team_request }
-    end
+    render
   end
 
   def edit
@@ -66,33 +58,41 @@ class JoinTeamRequestsController < ApplicationController
 
   # update join team request entry for join_team_request table and add it to the table
   def update
-	  @join_team_request = JoinTeamRequest.find(params[:id])
-	  respond_to do |format|
-		  if @join_team_request.update_attribute(:comments, params[:join_team_request][:comments])
-			  format.html { redirect_to(@join_team_request, notice: 'JoinTeamRequest was successfully updated.') }
-			  format.xml  { head :ok }
-		  else
-			  format.html { render action: "edit" }
-			  format.xml  { render xml: @join_team_request.errors, status: :unprocessable_entity }
-		  end
-	  end
+    @join_team_request = JoinTeamRequest.find(params[:id])
+    respond_to do |format|
+      if @join_team_request.update_attribute(:comments, params[:join_team_request][:comments])
+        format.html { redirect_to(@join_team_request, notice: 'JoinTeamRequest was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render action: "edit" }
+        format.xml  { render xml: @join_team_request.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
-	  @join_team_request = JoinTeamRequest.find(params[:id])
-	  @join_team_request.destroy
+    @join_team_request = JoinTeamRequest.find(params[:id])
+    @join_team_request.destroy
 
-	  respond_to do |format|
-		  format.html { redirect_to(join_team_requests_url) }
-		  format.xml  { head :ok }
-	  end
+    respond_to do |format|
+      format.html { redirect_to(join_team_requests_url) }
+      format.xml  { head :ok }
+    end
   end
 
   # decline request to join the team...
   def decline
-	  @join_team_request = JoinTeamRequest.find(params[:id])
-	  @join_team_request.status = 'D'
-	  @join_team_request.save
-	  redirect_to view_student_teams_path student_id: params[:teams_user_id]
+    @join_team_request = JoinTeamRequest.find(params[:id])
+    @join_team_request.status = 'D'
+    @join_team_request.save
+    redirect_to view_student_teams_path student_id: params[:teams_user_id]
+  end
+  private
+  def render
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render xml: @join_team_requests }
+    end
   end
 end

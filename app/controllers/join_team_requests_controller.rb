aa92a36f816887e4a7ef93b9fbc92f34dd3a2262
canhsiao <canhsiao@gmail.com>
2016-10-26 20:11:24 -0400

    refactor more

--- app/controllers/join_team_requests_controller.rb
+++ app/controllers/join_team_requests_controller.rb
@@ -32,65 +32,65 @@ class JoinTeamRequestsController < ApplicationController
     if team.full?
       flash[:note] = "This team is full."
     elsif !team_member.empty?
-        flash[:note] = "You are already a member of this team."
+	    flash[:note] = "You are already a member of this team."
     else
 
-        @join_team_request = JoinTeamRequest.new
-        @join_team_request.comments = params[:comments]
-        @join_team_request.status = 'P'
-        @join_team_request.team_id = params[:team_id]
+	    @join_team_request = JoinTeamRequest.new
+	    @join_team_request.comments = params[:comments]
+	    @join_team_request.status = 'P'
+	    @join_team_request.team_id = params[:team_id]
 
-        participant = Participant.where(user_id: session[:user][:id], parent_id: params[:assignment_id]).first
-        @join_team_request.participant_id = participant.id
-        respond_to do |format|
-          if @join_team_request.save
-            format.html { redirect_to(@join_team_request, notice: 'JoinTeamRequest was successfully created.') }
-            format.xml  { render xml: @join_team_request, status: :created, location: @join_team_request }
-          else
-            format.html { render action: "new" }
-            format.xml  { render xml: @join_team_request.errors, status: :unprocessable_entity }
-          end
-        end
+	    participant = Participant.where(user_id: session[:user][:id], parent_id: params[:assignment_id]).first
+	    @join_team_request.participant_id = participant.id
+	    respond_to do |format|
+		    if @join_team_request.save
+			    format.html { redirect_to(@join_team_request, notice: 'JoinTeamRequest was successfully created.') }
+			    format.xml  { render xml: @join_team_request, status: :created, location: @join_team_request }
+		    else
+			    format.html { render action: "new" }
+			    format.xml  { render xml: @join_team_request.errors, status: :unprocessable_entity }
+		    end
+	    end
     end
   end
 
   # update join team request entry for join_team_request table and add it to the table
   def update
-    @join_team_request = JoinTeamRequest.find(params[:id])
-    respond_to do |format|
-      if @join_team_request.update_attribute(:comments, params[:join_team_request][:comments])
-        format.html { redirect_to(@join_team_request, notice: 'JoinTeamRequest was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.xml  { render xml: @join_team_request.errors, status: :unprocessable_entity }
-      end
-    end
+	  @join_team_request = JoinTeamRequest.find(params[:id])
+	  respond_to do |format|
+		  if @join_team_request.update_attribute(:comments, params[:join_team_request][:comments])
+			  format.html { redirect_to(@join_team_request, notice: 'JoinTeamRequest was successfully updated.') }
+			  format.xml  { head :ok }
+		  else
+			  format.html { render action: "edit" }
+			  format.xml  { render xml: @join_team_request.errors, status: :unprocessable_entity }
+		  end
+	  end
   end
 
   def destroy
-    @join_team_request = JoinTeamRequest.find(params[:id])
-    @join_team_request.destroy
+	  @join_team_request = JoinTeamRequest.find(params[:id])
+	  @join_team_request.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(join_team_requests_url) }
-      format.xml  { head :ok }
-    end
+	  respond_to do |format|
+		  format.html { redirect_to(join_team_requests_url) }
+		  format.xml  { head :ok }
+	  end
   end
 
   # decline request to join the team...
   def decline
-    @join_team_request = JoinTeamRequest.find(params[:id])
-    @join_team_request.status = 'D'
-    @join_team_request.save
-    redirect_to view_student_teams_path student_id: params[:teams_user_id]
+	  @join_team_request = JoinTeamRequest.find(params[:id])
+	  @join_team_request.status = 'D'
+	  @join_team_request.save
+	  redirect_to view_student_teams_path student_id: params[:teams_user_id]
   end
   private
   def render
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render xml: @join_team_requests }
-    end
+	  respond_to do |format|
+		  format.html # index.html.erb
+		  format.xml  { render xml: @join_team_requests }
+	  end
   end
 end

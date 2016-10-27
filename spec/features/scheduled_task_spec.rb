2d7e69cb070c37340ed95044b55ef19df62b9987
niv2395 <nnatara2@ncsu.edu>
2016-10-27 02:47:43 -0400

    Update scheduled_task_spec.rb

--- spec/features/scheduled_task_spec.rb
+++ spec/features/scheduled_task_spec.rb
@@ -14,26 +14,6 @@ def expect_deadline_check(deadline_condition)
     display_condition = "metareview"
   end
   
-describe 'Metareview deadline reminder email' do
-  it 'is able to send reminder email for Metareview deadline to reviewers ' do
-    id = 2
-    @name = "user"
-    due_at = Time.zone.now.advance(minutes: +2)
-
-    due_at1 = Time.zone.parse(due_at.to_s(:db))
-    curr_time = Time.zone.now.to_s(:db)
-    curr_time = Time.zone.parse(curr_time)
-    time_in_min = ((due_at1 - curr_time).to_i / 60) * 60
-    Delayed::Job.delete_all
-    expect(Delayed::Job.count).to eq(0)
-
-    dj = Delayed::Job.enqueue(payload_object: ScheduledTask.new(id, "metareview", due_at), priority: 1, run_at: time_in_min)
-
-    expect(Delayed::Job.count).to eq(1)
-    expect(Delayed::Job.last.handler).to include("deadline_type: metareview")
-  end
-end
-
 describe 'Drop Topic deadline reminder email' do
   it 'is able to send reminder email for drop topic deadline to reviewers ' do
     id = 2

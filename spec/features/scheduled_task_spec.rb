25226a8019f537e0daccd3a2e445842e24bfa2f5
niv2395 <nnatara2@ncsu.edu>
2016-10-27 02:51:24 -0400

    Added the common function

--- spec/features/scheduled_task_spec.rb
+++ spec/features/scheduled_task_spec.rb
@@ -25,4 +25,23 @@ def expect_deadline_check(deadline_condition)
     send_reminder_condition = 'is able to send reminder email for team formation deadline to reviewers '
     display_condition = "team_formation"
   end
- 
+ describe deadline_condition do
+    it send_reminder_condition do
+      @name = "user"
+      # due_at = DateTime.now.getlocal.advance(minutes: +2)
+      # due_at1 = Time.parse.getlocal(due_at.to_s(:db))
+      # curr_time = DateTime.now.getlocal.to_s(:db)
+      # curr_time = Time.parse.getlocal(curr_time)
+      Delayed::Job.delete_all
+      expect(Delayed::Job.count).to eq(0)
+      expect(Delayed::Job.count).to eq(1)
+      expect(Delayed::Job.last.handler).to include("deadline_type: " + display_condition)
+    end
+  end
+end
+expect_deadline_check('Submission deadline reminder email')
+expect_deadline_check('Review deadline reminder email')
+expect_deadline_check('Metareview deadline reminder email')
+expect_deadline_check('Drop Topic deadline reminder email')
+expect_deadline_check('Signup deadline reminder email')
+expect_deadline_check('Team formation deadline reminder email')

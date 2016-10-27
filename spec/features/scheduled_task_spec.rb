4ac0b46b17ece21b1072dd352ce06d32aefe8119
niv2395 <nnatara2@ncsu.edu>
2016-10-27 02:46:34 -0400

    Removed submission deadline block

--- spec/features/scheduled_task_spec.rb
+++ spec/features/scheduled_task_spec.rb
@@ -13,33 +13,6 @@ def expect_deadline_check(deadline_condition)
     send_reminder_condition = 'is able to send reminder email for Metareview deadline to reviewers '
     display_condition = "metareview"
   end
-describe 'Submission deadline reminder email' do
-  it 'is able to send reminder email for submission deadline to signed-up users ' do
-    # Delayed::Worker.delay_jobs = false
-    id = 2
-    @name = "user"
-
-    # due_at = DateTime.now + 120
-    # seconds_until_due = due_at - Time.now
-    # minutes_until_due = seconds_until_due / 60
-    due_at = Time.zone.now.advance(minutes: +2)
-
-    # puts DateTime.now
-    # puts due_at
-    due_at1 = Time.zone.parse(due_at.to_s(:db))
-    curr_time = Time.zone.now.to_s(:db)
-    curr_time = Time.zone.parse(curr_time)
-    time_in_min = ((due_at1 - curr_time).to_i / 60) * 60
-    Delayed::Job.delete_all
-    expect(Delayed::Job.count).to eq(0)
-
-    dj = Delayed::Job.enqueue(payload_object: ScheduledTask.new(id, "submission", due_at), priority: 1, run_at: time_in_min)
-
-    expect(Delayed::Job.count).to eq(1)
-
-    expect(Delayed::Job.last.handler).to include("deadline_type: submission")
-  end
-end
 
 describe 'Review deadline reminder email' do
   it 'is able to send reminder email for review deadline to reviewers ' do

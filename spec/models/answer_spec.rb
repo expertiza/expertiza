78f3cf6bd27a38a11acad174f4e43c7a84ac66ef
anudeepvrm <anudeepvrm@gmail.com>
2016-10-28 02:05:15 -0400

    bug fixes in answer_spec

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Answer do
-  let(:questionnaire) {create(:questionnaire)}
+  let(:questionnaire) {create(:questionnaire,:id => 1)}
   let(:question1) {create(:question,:questionnaire => questionnaire,:weight=>1,:id =>1)}
   let(:question2) {create(:question,:questionnaire => questionnaire,:weight=>2,:id =>2)}
   let(:response_map) {create(:review_response_map,:id=>1,:reviewed_object_id => 1)}

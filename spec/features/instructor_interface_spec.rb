ab1043d29adb025aaab1c5a9bcac535fa694fbbd
svenka15 <svenka15@ncsu.edu>
2016-10-28 03:49:17 -0400

    functions added

--- spec/features/instructor_interface_spec.rb
+++ spec/features/instructor_interface_spec.rb
@@ -1,38 +1,7 @@
 require 'rails_helper'
+integration_tests_for_instructor_interface
+instructor_login
 
-describe "Integration tests for instructor interface" do
-  before(:each) do
-    create(:assignment)
-    create_list(:participant, 3)
-    create(:assignment_node)
-    create(:deadline_type, name: "submission")
-    create(:deadline_type, name: "review")
-    create(:deadline_type, name: "metareview")
-    create(:deadline_type, name: "drop_topic")
-    create(:deadline_type, name: "signup")
-    create(:deadline_type, name: "team_formation")
-    create(:deadline_right)
-    create(:deadline_right, name: 'Late')
-    create(:deadline_right, name: 'OK')
-    create(:assignment_due_date)
-    create(:assignment_due_date, deadline_type: DeadlineType.where(name: 'review').first, due_at: Time.now.getlocal + (100 * 24 * 60 * 60))
-  end
-
-  describe "Instructor login" do
-    it "with valid username and password" do
-      login_as("instructor6")
-      visit '/tree_display/list'
-      expect(page).to have_content("Manage content")
-    end
-
-    it "with invalid username and password" do
-      visit root_path
-      fill_in 'login_name', with: 'instructor6'
-      fill_in 'login_password', with: 'something'
-      click_button 'SIGN IN'
-      expect(page).to have_content('Your username or password is incorrect.')
-    end
-  end
 
   describe "Create a course", type: :controller do
     it "is able to create a public course or a private course" do

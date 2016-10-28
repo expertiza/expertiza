07d97a2617a3b85a659517840a46bcae891eb90e
abhinand-lingareddy <abhinand.lingareddy@gmail.com>
2016-10-27 23:01:45 -0400

    changes as part of adding test for final_versions_from_reviewer

--- spec/models/review_response_map_spec.rb
+++ spec/models/review_response_map_spec.rb
@@ -3,12 +3,21 @@ require 'rails_helper'
 describe 'ReviewResponseMap' do
 
   before(:each) do
-    @review_response= build(:review_response_map)
+    @participant=create(:participant)
+    @assignment_questionnaire= create(:assignment_questionnaire)
+    @review_response=create(:review_response_map)
+
+
   end
 
   describe "#validity" do
     it "should have a valid reviewee_id" do
       expect(@review_response.reviewee_id).to be_instance_of(Fixnum)
     end
+    it "should return review_map with questionnaire id" do
+      map=ReviewResponseMap.final_versions_from_reviewer(1)
+      expect(map).to be_truthy
+      expect(map[:review][:questionnaire_id]).to be(1)
     end
+  end
 end

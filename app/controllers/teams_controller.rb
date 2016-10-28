8ea83fb27d8f0bfd71bbbc1332ec8a1c080cd2ca
Tanmay Goel <goeltanmay@gmail.com>
2016-10-28 16:39:58 -0400

    removing puts

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -97,7 +97,6 @@ class TeamsController < ApplicationController
   # The team and team members are all copied.
   def inherit
     assignment = Assignment.find(params[:id])
-    puts "Inside #{assignment.id}"
     if assignment.course_id >= 0
       course = Course.find(assignment.course_id)
       teams = course.get_teams

8097eabf7902776221e3235d34e9e1da97c6a1eb
svenka15 <svenka15@ncsu.edu>
2016-10-28 04:16:41 -0400

    instructor_login modified

--- spec/features/instructor_interface_spec.rb
+++ spec/features/instructor_interface_spec.rb
@@ -1,20 +1,4 @@
 require 'rails_helper'
-  def instructor_login
-    describe "Instructor login" do
-      it "with valid username and password" do
-      login_as("instructor6")
-      visit '/tree_display/list'
-      expect(page).to have_content("Manage content")
-  end
-
-    it "with invalid username and password" do
-      visit root_path
-      fill_in 'login_name', with: 'instructor6'
-      fill_in 'login_password', with: 'something'
-      click_button 'SIGN IN'
-      expect(page).to have_content('Your username or password is incorrect.')
-    end
-  end
 
 
 

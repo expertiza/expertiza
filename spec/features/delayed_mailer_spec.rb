aaf1bfa985b79a4aeaec215cb86e0986e968f26d
cvvishal <cvvishal@gmail.com>
2016-10-28 02:36:22 -0400

    review_deadline_reminder duplicate demoved

--- spec/features/delayed_mailer_spec.rb
+++ spec/features/delayed_mailer_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-def expect_deadline_check(deadline_condition,send_reminder_condition,display_condition)
+def expect_deadline_check(deadline_condition,send_reminder_condition,display_condition,dj_condition)
   describe deadline_condition do
     it send_reminder_condition do
       # Delayed::Worker.delay_jobs = false
@@ -21,7 +21,7 @@ def expect_deadline_check(deadline_condition,send_reminder_condition,display_con
       Delayed::Job.delete_all
       expect(Delayed::Job.count).to eq(0)
   
-      dj = Delayed::Job.enqueue(payload_object: DelayedMailer.new(id, "submission", due_at), priority: 1, run_at: time_in_min)
+      dj = Delayed::Job.enqueue(payload_object: DelayedMailer.new(id, dj_condition, due_at), priority: 1, run_at: time_in_min)
   
       expect(Delayed::Job.count).to eq(1)
   
@@ -30,27 +30,8 @@ def expect_deadline_check(deadline_condition,send_reminder_condition,display_con
   end
 end
 
-expect_deadline_check('Submission deadline reminder email','is able to send reminder email for submission deadline to signed-up users ',"deadline_type: submission")
-
-describe 'Review deadline reminder email' do
-  it 'is able to send reminder email for review deadline to reviewers ' do
-    id = 2
-    @name = "user"
-    due_at = DateTime.now.advance(minutes: +2)
-
-    due_at1 = Time.parse(due_at.to_s(:db))
-    curr_time = DateTime.now.to_s(:db)
-    curr_time = Time.parse(curr_time)
-    time_in_min = ((due_at1 - curr_time).to_i / 60) * 60
-    Delayed::Job.delete_all
-    expect(Delayed::Job.count).to eq(0)
-
-    dj = Delayed::Job.enqueue(payload_object: DelayedMailer.new(id, "review", due_at), priority: 1, run_at: time_in_min)
-
-    expect(Delayed::Job.count).to eq(1)
-    expect(Delayed::Job.last.handler).to include("deadline_type: review")
-  end
-end
+expect_deadline_check('Submission deadline reminder email','is able to send reminder email for submission deadline to signed-up users ',"deadline_type: submission","submission")
+expect_deadline_check('Review deadline reminder email','is able to send reminder email for review deadline to reviewers ',"deadline_type: review","review")
 
 describe 'Metareview deadline reminder email' do
   it 'is able to send reminder email for Metareview deadline to reviewers ' do

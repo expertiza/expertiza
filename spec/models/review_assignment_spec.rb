75f7399703cbdbcf015ccabebc7f5e21dcec3aaa
rnambis <rnambis@ncsu.edu>
2016-10-28 11:27:00 -0400

    cleared errors in review_assignment_spec.rb

--- spec/models/review_assignment_spec.rb
+++ spec/models/review_assignment_spec.rb
@@ -1,13 +1,14 @@
 require 'rails_helper'
 
-before(:all) do
+
+describe ReviewAssignment do
+  before(:all) do
 	
 	@assign = Assignment.new
 	@assign.extend(ReviewAssignment)
 	@review = AssignmentParticipant.new
 end
-describe ReviewAssignment do
-it "give the list of contributors" do
- expect(@test.candidate_assignment_teams_to_review(@review)) should be_kind_of "Array"
-end 
-
+	it "give the list of contributors" do
+	 expect(@test.candidate_assignment_teams_to_review(@review)).should be_kind_of "Array"
+	end 
+end

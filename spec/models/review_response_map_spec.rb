78176bc582d0b4cbc884692f7550ab959b1aef3d
abhinand-lingareddy <abhinand.lingareddy@gmail.com>
2016-10-27 23:40:34 -0400

    changes as part of adding test for export_fields

--- spec/models/review_response_map_spec.rb
+++ spec/models/review_response_map_spec.rb
@@ -11,21 +11,25 @@ describe 'ReviewResponseMap' do
   end
 
   describe "#validity" do
-    xit "should have a valid reviewee_id" do
+    it "should have a valid reviewee_id" do
       expect(@review_response.reviewee_id).to be_instance_of(Fixnum)
     end
-    xit "should return review_map with questionnaire id" do
+    it "should return review_map with questionnaire id" do
       map=ReviewResponseMap.final_versions_from_reviewer(1)
       expect(map).to be_truthy
       expect(map[:review][:questionnaire_id]).to be(1)
     end
-    xit "should return the valid questionnaire" do
+    it "should return the valid questionnaire" do
       review_questionnaire=@review_response.questionnaire 1
       expect(review_questionnaire).to be_instance_of(ReviewQuestionnaire)
       expect(review_questionnaire.id).to be(1)
     end
     it "should return title" do
       expect(@review_response.title).to eql "Review"
+    end
+    it "should return export_fields" do
+      export_fields=ReviewResponseMap.export_fields nil
+      expect(export_fields.length).to be(2)
     end
   end
 end

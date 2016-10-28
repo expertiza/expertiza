8af450f86e00b25169c3daebbfaedea8a97ebcb7
maheshmasale <masale123@hotmail.com>
2016-10-28 00:58:30 -0400

    Changed code to add the reviews for each user

--- spec/features/heat_map_spec.rb
+++ spec/features/heat_map_spec.rb
@@ -15,6 +15,7 @@ describe "HeatMapTest", type: :feature do
     create(:deadline_right, name: 'Late')
     create(:deadline_right, name: 'OK')
     create(:assignment_due_date, deadline_type: DeadlineType.where(name: 'review').first, due_at: Time.now + (100 * 24 * 60 * 60))
+
     create(:topic)
     create(:topic, topic_name: "TestReview")
     create(:team_user, user: User.where(role_id: 2).first)
@@ -25,11 +26,41 @@ describe "HeatMapTest", type: :feature do
     create(:signed_up_team, team_id: 2, topic: SignUpTopic.second)
     create(:assignment_questionnaire)
     create(:question)
-    create(:review_response_map, reviewer_id: User.where(role_id: 2).third.id)
-    create(:review_response_map, reviewer_id: User.where(role_id: 2).second.id, reviewee: AssignmentTeam.second)
+    #create(:review_response_map, reviewer_id: User.where(role_id: 2).third.id)
+    #create(:review_response_map, reviewer_id: User.where(role_id: 2).second.id, reviewee: AssignmentTeam.second)
+    #create(:response, additional_comment:'Round 1 - Good job', round:1)
+    #create(:response, additional_comment:'Round 2 - not so good job', round:2)
   end
 
+
+  def createReviews
+    login_as('student2065')
+    expect(page).to have_content "User: student2065"
+    expect(page).to have_content "TestAssignment"
+
+    click_link "TestAssignment"
+    expect(page).to have_content "Submit or Review work for TestAssignment"
+    expect(page).to have_content "Others' work"
+
+    click_link "Others' work"
+    expect(page).to have_content 'Reviews for "TestAssignment"'
+
+    choose "topic_id"
+    click_button "Request a new submission to review"
+
+    click_link "Begin"
+
+    fill_in "responses[0][comment]", with: "HelloWorld"
+    select 5, from: "responses[0][score]"
+    fill_in "review[comments]", with: "Excellent work done!"
+    click_button "Submit Review"
+    expect(page).to have_content 'Your response was successfully saved.'
+
+  end
+
+
   def load_questionnaire
+
     login_as('student2064')
     expect(page).to have_content "User: student2064"
     expect(page).to have_content "TestAssignment"
@@ -42,22 +73,29 @@ describe "HeatMapTest", type: :feature do
 
   end
 
-  it "Loads Heat Map page" do
+
+  it "See reviews" do
+    createReviews
+    #expect(page).to have_content "Review 1"
+    expect(page).to have_content "Your response was successfully saved."
+    # 1. click on the button of "Toggles question list"
+    # 2. expect to have content of the list of questions
+  end
+
+  xit "Loads Heat Map page" do
     # Load data
     load_questionnaire
 
     expect(page).to have_content "Summary Report for assignment"
   end
 
-  xit "Toggles question list" do
+  it "Sorts by total review score" do
+    createReviews
+    click_link "Logout"
     load_questionnaire
-    # 1. click on the button of "Toggles question list"
-    # 2. expect to have content of the list of questions
-  end
 
+    expect(page).to have_content "Review 1"
 
-  xit "Sorts by total review score" do
-    load_questionnaire
     # 1. click on the button of "Sorts by total review score"
     # 2. calculate or load the total score for each review
     # 3. check if the total score is sorted and toggled between low-to-high and high-to-low

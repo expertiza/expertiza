170d1371794e0dc1f147b0700d9020c7e2c87d7a
niv2395 <nnatara2@ncsu.edu>
2016-10-28 18:08:37 -0400

    Update questionnaire_spec.rb

--- spec/features/questionnaire_spec.rb
+++ spec/features/questionnaire_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
+def create_public_review
+    it "is able to create a public review questionnaire" do
+      login_as("instructor6")
 
+      visit '/questionnaires/new?model=ReviewQuestionnaire&private=0'
+
+      fill_in('questionnaire_name', with: 'Review 1')
+
+      fill_in('questionnaire_min_question_score', with: '0')
+
+      fill_in('questionnaire_max_question_score', with: '5')
+
+      select('no', from: 'questionnaire_private')
+
+      click_button "Create"
+
+      expect(Questionnaire.where(name: "Review 1")).to exist
+    end
+  end
+end
 describe "Questionnaire tests for instructor interface" do
   before(:each) do
     create(:assignment)
@@ -33,29 +52,11 @@ describe "Questionnaire tests for instructor interface" do
       expect(page).to have_content('Your username or password is incorrect.')
     end
   end
-  def create_public_review
-      it "is able to create a public review questionnaire" do
-        login_as("instructor6")
-
-        visit '/questionnaires/new?model=ReviewQuestionnaire&private=0'
-
-        fill_in('questionnaire_name', with: 'Review 1')
-
-        fill_in('questionnaire_min_question_score', with: '0')
-
-        fill_in('questionnaire_max_question_score', with: '5')
-
-        select('no', from: 'questionnaire_private')
-
-        click_button "Create"
-
-        expect(Questionnaire.where(name: "Review 1")).to exist
-      end
-  end
+  
   describe "Create a public review questionnaire", type: :controller do
     create_public_review
   end
-  create_public_review
+    
 
   describe "Create a private review questionnaire", type: :controller do
     it "is able to create a private review questionnaire" do

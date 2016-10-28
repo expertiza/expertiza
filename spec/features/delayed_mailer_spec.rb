68cc23c118a8f0f07c864ee2dd394b187b04367d
cvvishal <cvvishal@gmail.com>
2016-10-28 02:58:20 -0400

    space indentation changed

--- spec/features/delayed_mailer_spec.rb
+++ spec/features/delayed_mailer_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-def expect_deadline_check(deadline_condition,send_reminder_condition,display_condition,dj_condition)
+def expect_deadline_check(deadline_condition, send_reminder_condition, display_condition, dj_condition)
   describe deadline_condition do
     it send_reminder_condition do
       # Delayed::Worker.delay_jobs = false
@@ -30,9 +30,9 @@ def expect_deadline_check(deadline_condition,send_reminder_condition,display_con
   end
 end
 
-expect_deadline_check('Submission deadline reminder email','is able to send reminder email for submission deadline to signed-up users ',"deadline_type: submission","submission")
-expect_deadline_check('Review deadline reminder email','is able to send reminder email for review deadline to reviewers ',"deadline_type: review","review")
-expect_deadline_check('Metareview deadline reminder email','is able to send reminder email for Metareview deadline to reviewers ',"deadline_type: metareview","metareview")
-expect_deadline_check('Drop Topic deadline reminder email','is able to send reminder email for drop topic deadline to reviewers ',"deadline_type: drop_topic","drop_topic")
-expect_deadline_check('Signup deadline reminder email','is able to send reminder email for signup deadline to reviewers ',"deadline_type: signup","signup")
-expect_deadline_check('Team formation deadline reminder email','is able to send reminder email for team formation deadline to reviewers ',"deadline_type: team_formation","team_formation"
+expect_deadline_check('Submission deadline reminder email', 'is able to send reminder email for submission deadline to signed-up users ', "deadline_type: submission", "submission")
+expect_deadline_check('Review deadline reminder email', 'is able to send reminder email for review deadline to reviewers ', "deadline_type: review", "review")
+expect_deadline_check('Metareview deadline reminder email', 'is able to send reminder email for Metareview deadline to reviewers ', "deadline_type: metareview", "metareview")
+expect_deadline_check('Drop Topic deadline reminder email', 'is able to send reminder email for drop topic deadline to reviewers ', "deadline_type: drop_topic", "drop_topic")
+expect_deadline_check('Signup deadline reminder email', 'is able to send reminder email for signup deadline to reviewers ', "deadline_type: signup", "signup")
+expect_deadline_check('Team formation deadline reminder email', 'is able to send reminder email for team formation deadline to reviewers ', "deadline_type: team_formation", "team_formation")

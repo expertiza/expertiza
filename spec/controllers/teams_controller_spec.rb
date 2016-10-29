0600fd797e5c114f28092cc4f979a62a64672c14
Tanmay Goel <goeltanmay@gmail.com>
2016-10-28 20:19:15 -0400

    trying test.

--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -1,33 +1,35 @@
 require 'rails_helper'
 
 describe TeamsController do
-  it "test for inherit" do
-    assignment = build(Assignment)
-    course = Course.new
-    course.name = "newCourse"
-    course.save
-    assignment.course_id = course.id
-    assignment.save
+    describe "Testing copy functionality - " do
+        it "test for inherit" do
+            assignment = build(Assignment)
+            course = Course.new
+            course.name = "newCourse"
+            course.save
+            assignment.course_id = course.id
+            assignment.save
 
-    course_team = CourseTeam.new
-    course_team.name = "course_team_1"
-    course_team.parent_id = course.id
-    course_team.save!
+            course_team = CourseTeam.new
+            course_team.name = "course_team_1"
+            course_team.parent_id = course.id
+            course_team.save!
 
-    course_team = CourseTeam.new
-    course_team.name = "course_team_2"
-    course_team.parent_id = course.id
-    course_team.save!
+            course_team = CourseTeam.new
+            course_team.name = "course_team_2"
+            course_team.parent_id = course.id
+            course_team.save!
 
-    course_team = CourseTeam.new
-    course_team.name = "course_team_3"
-    course_team.parent_id = course.id
-    course_team.save!
+            course_team = CourseTeam.new
+            course_team.name = "course_team_3"
+            course_team.parent_id = course.id
+            course_team.save!
 
-    puts "assignment #{assignment.id}"
-    post :inherit, id: assignment.id
-
-    assignment_teams = AssignmentTeam.all
-    puts assignment_teams.count
-  end
+            # puts "assignment #{assignment.id}"
+            post :inherit, id: assignment.id
+            expect(response).to have_http_status(200)
+            # assignment_teams = AssignmentTeam.all
+            # puts assignment_teams.count
+        end
+    end
 end

c3926fa61a37d621f5085041bfdf01d50354266f
hsy12321 <shuang7@ncsu.edu>
2016-10-29 00:49:01 -0400

    minor changes

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -353,7 +353,7 @@ FactoryGirl.define do
     permission_granted nil
     penalty_accumulated 0
     grade nil
-    type "AssignmentParticipant"
+    type "CourseParticipant"
     handle "handle"
     time_stamp nil
     digital_signature nil
@@ -364,3 +364,4 @@ FactoryGirl.define do
 
 
 end
+

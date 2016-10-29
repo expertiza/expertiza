0be6dfb2cde9f946dfa10e7c4fcc803cd4c6c8ab
Ajay Chandra Pendyala <ajaychandrapendyala@gmail.com>
2016-10-28 22:33:51 -0400

    added tests for delete method

--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -1,16 +1,13 @@
 require 'rails_helper'
 
 describe TeamsController do
-  describe "POST #create" do
+
+    describe "POST #create" do
     context "with an assignment team" do
       it "increases count by 1" do	
         expect{create :assignment_team, assignment: @assignment}.to change(Team,:count).by(1)
       end
-      it "saves in database" do
-        assignment_team = create(:assignment_team)
-        post :create, :team=>assignment_team
-        
-      end
+
       it "redirects to the list page" do
       end
     end
@@ -19,10 +16,20 @@ describe TeamsController do
       it "increases the count by 1" do
         expect{create :course_team, course: @course}.to change(Team,:count).by(1)
       end
-      it "saves the new team in the database" do
-      end
-      it "redirects to the new page" do
+
+    end
+
+
+    context "with an assignment team " do
+      it "deletes an assignment team" do
+        @assignment = create(:assignment)
+        @a_team = create(:assignment_team)
+
+        expect{ @a_team.delete }.to change(Team, :count).by(-1)
       end
     end
+
+    
+
   end
 end

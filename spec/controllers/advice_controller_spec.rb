5c5a2e74af6d9a19f691cd8507dd100f541f7660
shriya <ssharm25@ncsu.edu>
2016-10-28 22:46:50 -0400

    commiting

---
+++ spec/controllers/advice_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+require 'rails_helper'
+describe AdviceController do
+
+  describe '#edit_advice' do
+  it 'should' do
+   @question = Question.new
+   @questionnaire = Questionnaire.new
+
+    true.should == true
+  end
+  end
+  end

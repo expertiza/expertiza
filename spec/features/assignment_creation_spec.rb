e1b0a40f639bd803e4cb5f5eaea56df1d810bfa5
niv2395 <nnatara2@ncsu.edu>
2016-10-28 21:46:03 -0400

    Added function_to_create_with_teams

--- spec/features/assignment_creation_spec.rb
+++ spec/features/assignment_creation_spec.rb
@@ -28,6 +28,22 @@ def edit_topics_properties
   end
 end
 
+def function_to_create_with_teams
+  it "is able to create with teams" do
+    login_as("instructor6")
+    visit '/assignments/new?private=1'
+    fill_in 'assignment_form_assignment_name', with: 'private assignment for test'
+    select('Course 2', from: 'assignment_form_assignment_course_id')
+    fill_in 'assignment_form_assignment_directory_path', with: 'testDirectory'
+    check("team_assignment")
+    check("assignment_form_assignment_show_teammate_reviews")
+    fill_in 'assignment_form_assignment_max_team_size', with: 3
+    click_button 'Create'
+    assignment = Assignment.where(name: 'private assignment for test').first
+    expect(assignment).to have_attributes(max_team_size: 3, show_teammate_reviews: true)
+  end
+end
+
 def update_scored_question(tr_variable,select_variable)
   it "should update scored question dropdown" do
     find_link('Rubrics').click

a886b79f164d6435e5e5c6ace38e5b86959e23a8
canhsiao <canhsiao@gmail.com>
2016-10-30 14:03:46 -0400

    conflicts resolve

--- app/controllers/join_team_requests_controller.rb
+++ app/controllers/join_team_requests_controller.rb
@@ -4,24 +4,12 @@ class JoinTeamRequestsController < ApplicationController
     current_role_name.eql?("Student")
   end
 
-	def index
-		@join_team_requests = JoinTeamRequest.all
-		rendermethod
   def index
     @join_team_requests = JoinTeamRequest.all
     render
 
   end
 
-	def show
-		@join_team_request = JoinTeamRequest.find(params[:id])
-		rendermethod
-	end
-
-	def new
-		@join_team_request = JoinTeamRequest.new
-		rendermethod
-	end
   def show
     @join_team_request = JoinTeamRequest.find(params[:id])
     render
@@ -76,49 +64,6 @@ class JoinTeamRequestsController < ApplicationController
 	end
 
 
-	# update join team request entry for join_team_request table and add it to the table
-	def update
-		@join_team_request = JoinTeamRequest.find(params[:id])
-		respond_to do |format|
-			if @join_team_request.update_attribute(:comments, params[:join_team_request][:comments])
-				format.html { redirect_to(@join_team_request, notice: 'JoinTeamRequest was successfully updated.') }
-				format.xml  { head :ok }
-			else
-				format.html { render action: "edit" }
-				format.xml  { render xml: @join_team_request.errors, status: :unprocessable_entity }
-			end
-		end
-	end
-  # create a new join team request entry for join_team_request table and add it to the table
-  def create
-    # check if the advertisement is from a team member and if so disallow requesting invitations
-    team_member = TeamsUser.where(['team_id =? and user_id =?', params[:team_id], session[:user][:id]])
-    team = Team.find(params[:team_id])
-    if team.full?
-      flash[:note] = "This team is full."
-    elsif !team_member.empty?
-	    flash[:note] = "You are already a member of this team."
-    else
-
-	    @join_team_request = JoinTeamRequest.new
-	    @join_team_request.comments = params[:comments]
-	    @join_team_request.status = 'P'
-	    @join_team_request.team_id = params[:team_id]
-
-	    participant = Participant.where(user_id: session[:user][:id], parent_id: params[:assignment_id]).first
-	    @join_team_request.participant_id = participant.id
-	    respond_to do |format|
-		    if @join_team_request.save
-			    format.html { redirect_to(@join_team_request, notice: 'JoinTeamRequest was successfully created.') }
-			    format.xml  { render xml: @join_team_request, status: :created, location: @join_team_request }
-		    else
-			    format.html { render action: "new" }
-			    format.xml  { render xml: @join_team_request.errors, status: :unprocessable_entity }
-		    end
-	    end
-    end
-  end
-
   # update join team request entry for join_team_request table and add it to the table
   def update
 	  @join_team_request = JoinTeamRequest.find(params[:id])
@@ -143,15 +88,6 @@ class JoinTeamRequestsController < ApplicationController
 	  end
   end
 
-	# decline request to join the team...
-	def decline
-		@join_team_request = JoinTeamRequest.find(params[:id])
-		@join_team_request.status = 'D'
-		@join_team_request.save
-		redirect_to view_student_teams_path student_id: params[:teams_user_id]
-	end
-	private
-	def rendermethod
   # decline request to join the team...
   def decline
 	  @join_team_request = JoinTeamRequest.find(params[:id])

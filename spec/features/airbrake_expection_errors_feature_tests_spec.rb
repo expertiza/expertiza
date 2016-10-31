0de74afe85df8fc8ee3c985cedc3f1501ad4373f
Winbobob <zhu6@ncsu.edu>
2016-10-30 23:22:51 -0400

    Fix Airbrake-1517247902792549741 and write corresponding feature test.

---
+++ spec/features/airbrake_expection_errors_feature_tests_spec.rb
@@ -0,0 +1,86 @@
+require 'rails_helper'
+
+describe "Airbrake expection errors" do
+	before(:each) do
+	    create(:assignment, name: "TestAssignment", directory_path: 'test_assignment')
+	    create_list(:participant, 3)
+	    create(:assignment_node)
+	    create(:deadline_type, name: "submission")
+	    create(:deadline_type, name: "review")
+	    create(:deadline_type, name: "metareview")
+	    create(:deadline_type, name: "drop_topic")
+	    create(:deadline_type, name: "signup")
+	    create(:deadline_type, name: "team_formation")
+	    create(:deadline_right)
+	    create(:deadline_right, name: 'Late')
+	    create(:deadline_right, name: 'OK')
+	    create(:assignment_due_date, due_at: (DateTime.now + 1))
+	    create(:assignment_due_date, deadline_type: DeadlineType.where(name: 'review').first, due_at: (DateTime.now + 5))
+	    create(:topic)
+	    create(:topic, topic_name: "TestReview")
+	    create(:team_user, user: User.where(role_id: 2).first)
+	    create(:team_user, user: User.where(role_id: 2).second)
+	    create(:assignment_team)
+	    create(:team_user, user: User.where(role_id: 2).third, team: AssignmentTeam.second)
+	    create(:signed_up_team)
+	    create(:signed_up_team, team_id: 2, topic: SignUpTopic.second)
+	    create(:assignment_questionnaire)
+	    create(:question)
+	end
+
+	# Airbrake-1806782678925052472
+	it "can list sign_up_topics by using 'assignment_id' or 'id' as parameter", js: true do
+    	login_as 'student2066'
+    	
+    	visit '/sign_up_sheet/list?assignment_id=1'
+    	expect(page).to have_content('Signup sheet for')
+    	expect(page).to have_content('Hello world!')
+    	expect(page).to have_content('TestReview')
+    	
+    	visit '/sign_up_sheet/list?id=1'
+    	expect(page).to have_content('Signup sheet for')
+    	expect(page).to have_content('Hello world!')
+    	expect(page).to have_content('TestReview')
+	end
+end
+
+describe "airbrake-1517247902792549741" do
+	it "cannot access to '/tree_display/list' if not login" do 
+		visit '/tree_display/list'
+		expect(page).to have_current_path('/auth/failure')
+		expect(page).not_to have_content('Manage content')
+		expect(page).to have_content('Welcome!')
+		expect(page).to have_content('User Name')
+		expect(page).to have_content('Password')
+	end
+
+	it "can access to '/tree_display/list' after login as an admin/instructor/TA" do
+		create(:instructor)
+		login_as 'instructor6'
+		visit '/tree_display/list'
+		expect(page).to have_current_path('/tree_display/list')
+		expect(page).to have_content('Manage content')
+		expect(page).to have_content('Courses')
+		expect(page).to have_content('Assignments')
+		expect(page).to have_content('Questionnaires')
+		expect(page).not_to have_content('Welcome!')
+		expect(page).not_to have_content('User Name')
+		expect(page).not_to have_content('Password')
+	end
+
+	it "can access to '/student_task/list' after login as a student" do
+		stu = create(:student)
+		login_as stu.name
+		visit '/tree_display/list'
+		expect(page).to have_current_path('/student_task/list')
+		expect(page).to have_content('Assignments')
+		expect(page).to have_content('Tasks not yet started')
+		expect(page).to have_content('Students who have teamed with you')
+		expect(page).to have_content('Review Grade')
+		expect(page).to have_content('Publishing Rights')
+		expect(page).not_to have_content('Welcome!')
+		expect(page).not_to have_content('User Name')
+		expect(page).not_to have_content('Password')
+		expect(page).not_to have_content('SIGN IN')
+	end
+end

c1232ee1aa28fe36c0084dfba4ed8ae4c284ac26
samir <sjha4@ncsu.edu>
2016-11-02 19:21:49 -0400

    Changing naming convention of variable

--- app/models/team_formation.rb
+++ app/models/team_formation.rb
@@ -9,14 +9,14 @@ class TeamFormation < DeadlineType
    emails
 end
   def get_one_member_team
-    mail_list = []
+    email_list = []
     teams = TeamsUser.all.group(:team_id).count(:team_id)
     for team_id in teams.keys
       next unless teams[team_id] == 1
       user_id = TeamsUser.where(team_id: team_id).first.user_id
       email = User.find(user_id).email
-      mail_list << email
+      email_list << email
     end
-    mail_list
+    email_list
   end
 end

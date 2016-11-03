4a7e606fd9f648f92218be891a1ac33d0a03055d
Prathamesh Prabhudesai <prathprabhudesai@yahoo.com>
2016-11-03 19:38:39 -0400

    Updated the comments

--- app/controllers/sign_up_sheet_controller.rb
+++ app/controllers/sign_up_sheet_controller.rb
@@ -108,29 +108,31 @@ class SignUpSheetController < ApplicationController
     redirect_to edit_assignment_path(params[:assignment_id]) + "#tabs-5"
   end
 
+  # This function works in conjunction with Add Team
   def assign_topic # renders a view for a team to be added
-    @topic = SignUpTopic.find(params[:id])
-    @assignment = Assignment.find(params[:assignment_id])
+    @topic = SignUpTopic.find(params[:id]) # Identify the topic to which you want to add team
+    @assignment = Assignment.find(params[:assignment_id]) 
   end
 
+  # This function works in conjunction with Remove Team
   def remove_topic # renders a view for the team to be removed
     @topic = SignUpTopic.find(params[:id])
-    @assignment = Assignment.find(params[:assignment_id])
+    @assignment = Assignment.find(params[:assignment_id]) # Identify the assignment 
   end
 
-  def remove_team # method for remove the topic assigned to the team
+  def remove_team # method for removing the team assigned to the topic
     @topic = SignUpTopic.find(params[:id]) #Find all the details about the topic
     @assignment = Assignment.find(params[:assignment_id]) # Find all the details about the assignment
     @user = User.find_by name: params[:user][:name] # Find user
 
-    if @user.blank?
+    if @user.blank? # Check if the user exists
       flash[:error] = "User does not exist"
-      redirect_to edit_assignment_path(params[:assignment_id]) + "#tabs-2"
-    else
+      redirect_to edit_assignment_path(params[:assignment_id]) + "#tabs-2" 
+    else 
       @team = TeamsUser.find_by_sql("select T1.team_id from teams_users T1, teams T2 where T1.user_id = "+ @user.id.to_s+" AND T1.team_id = T2.id AND T2.parent_id = "+ @assignment.id.to_s) # find a team associated with this user having particular assignment and topic
       @isTopicTaken = SignedUpTeam.find_by_sql("select * from signed_up_teams where topic_id = "+ @topic.id.to_s) # List of all teams having the same topic
 
-      if(@isTopicTaken.any?)
+      if(@isTopicTaken.any?) 
         if(@team.any?)
           @userTeam = SignedUpTeam.find_by_sql("select * from signed_up_teams where team_id = "+ @team[0].team_id.to_s) # Previously assigned topic
           if(@userTeam.any? and @userTeam[0].topic_id == @topic.id)
@@ -165,7 +167,7 @@ class SignUpSheetController < ApplicationController
     else
       @team = TeamsUser.find_by_sql("select T1.team_id from teams_users T1, teams T2 where T1.user_id = "+ @user.id.to_s+" AND T1.team_id = T2.id AND T2.parent_id = "+ @assignment.id.to_s)
       @isTopicTaken = SignedUpTeam.find_by_sql("select * from signed_up_teams where topic_id = "+ @topic.id.to_s)
-      @disp_flag = 0
+      @disp_flag = 0 # Checks if user has the same topic
 
       if(@team.any?)
         @userTeam = SignedUpTeam.find_by_sql("select * from signed_up_teams where team_id = "+ @team[0].team_id.to_s)
@@ -180,6 +182,7 @@ class SignUpSheetController < ApplicationController
               end
             end
             if @disp_flag != 1
+              # Update the topic assigned to the team
               @userTeam[0].update(topic_id: @topic.id)
               puts @topic.max_choosers
               if (@isTopicTaken.size >= @topic.max_choosers)
@@ -190,13 +193,14 @@ class SignUpSheetController < ApplicationController
                 flash[:success] = "The topic has been assigned to the team"
               end
               @oldTopicTeams = SignedUpTeam.find_by_sql("select * from signed_up_teams where topic_id= "+ @oldTopic.to_s+" and is_waitlisted=1")
-              if(@oldTopicTeams.any?)
+              # Once the user's team has been reassigned to the new topic, the waitlist for the user's old topic must be updated
+              if(@oldTopicTeams.any?) #Check if the user's old topic had any waitlisted teams
                 @oldTopicTeams[0].update(is_waitlisted: 0)
               end
               redirect_to edit_assignment_path(params[:assignment_id]) + "#tabs-2"
             end
 
-          else #@userTeam.any
+          else #if the team does not have a topic, create a table enrty and assign a topic to the team
             @sign_up = SignedUpTeam.new
             @sign_up.topic_id = @topic.id
             @sign_up.team_id = @team[0].team_id
@@ -214,7 +218,7 @@ class SignUpSheetController < ApplicationController
             redirect_to edit_assignment_path(params[:assignment_id]) + "#tabs-2"
           end
 
-        else #isTopicTaken
+        else # if this topic has not been assigned to any team
           if @userTeam.any?
             @oldTopic = @userTeam[0].topic_id;
             @userTeam[0].update(topic_id: @topic.id)

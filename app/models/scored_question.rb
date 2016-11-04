a614a9ce8a5862c610707e30a8c48fee55fdc6b0
Lei Zhang <lzhang45@ncsu.edu>
2016-11-04 09:39:31 -0400

    edit files

--- app/models/scored_question.rb
+++ app/models/scored_question.rb
@@ -18,4 +18,14 @@ class ScoredQuestion < ChoiceQuestion
     answer = Answer.where(question_id: self.id, response_id: response_id).first
     self.weight * answer.answer
   end
+
+  def question_min_to_max(answer = nil, questionnaire_min, questionnaire_max)
+    (questionnaire_min..questionnaire_max).each do |j|
+      html = '<td width="10%"><input type="radio" id="' + j.to_s + '" value="' + j.to_s + '" name="Radio_' + self.id.to_s + '"'
+      html += 'checked="checked"' if (!answer.nil? and answer.answer == j) or (answer.nil? and questionnaire_min == j)
+      html += '></td>'
+      html
+    end
+  end
 end
+

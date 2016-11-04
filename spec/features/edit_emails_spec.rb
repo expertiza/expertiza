39b174242a2ae5afdd075fc7322235273c79bfd9
shufankang <skang7@ncsu.edu>
2016-11-04 19:29:52 +0000

    Second round change according to reviews

--- spec/features/edit_emails_spec.rb
+++ spec/features/edit_emails_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'should edit profile email options' do
+feature 'Edit profile email options' do
 
   before(:each) do
     create(:assignment)
@@ -13,13 +13,10 @@ describe 'should edit profile email options' do
       expect(page).to have_current_path('/profile/edit')
       expect(page).to have_content("E-mail address")
       expect(page).to have_content("E-mail options")
-
       fill_in 'E-mail address', with: 'ychen75@ncsu.edu'
       expect(find_field('E-mail address').value).to eq 'ychen75@ncsu.edu'
     end
   end
-
-
 
   describe 'email preferences options'  do
 
@@ -30,9 +27,8 @@ describe 'should edit profile email options' do
         expect(page).to have_current_path('/profile/edit')
         expect(page).to have_content("E-mail address")
         expect(page).to have_content("E-mail options")
-
+        #find and check the box for when someone else reviews my work
         find(:css, "#user_email_on_review").set(true)
-
         review_box = find('#user_email_on_review')
         expect(review_box).to be_checked
       end
@@ -41,9 +37,8 @@ describe 'should edit profile email options' do
         login_as "instructor6"
         visit '/profile/edit'
         expect(page).to have_current_path('/profile/edit')
-
+        #find and uncheck the box for when someone else reviews my work
         find(:css, "#user_email_on_review").set(false)
-
         review_box = find('#user_email_on_review')
         expect(review_box).not_to be_checked
       end
@@ -56,9 +51,8 @@ describe 'should edit profile email options' do
         expect(page).to have_current_path('/profile/edit')
         expect(page).to have_content("E-mail address")
         expect(page).to have_content("E-mail options")
-
+        #find and check the box for When someone else submits work I am assigned to review
         find(:css, "#user_email_on_submission").set(true)
-
         review_box = find('#user_email_on_submission')
         expect(review_box).to be_checked
       end
@@ -67,9 +61,8 @@ describe 'should edit profile email options' do
         login_as "instructor6"
         visit '/profile/edit'
         expect(page).to have_current_path('/profile/edit')
-
+        #find and uncheck the box for When someone else submits work I am assigned to review
         find(:css, "#user_email_on_submission").set(false)
-
         review_box = find('#user_email_on_submission')
         expect(review_box).not_to be_checked
       end
@@ -82,9 +75,8 @@ describe 'should edit profile email options' do
         expect(page).to have_current_path('/profile/edit')
         expect(page).to have_content("E-mail address")
         expect(page).to have_content("E-mail options")
-
+        #find and check the box for When someone else reviews one of my reviews (metareviews my work)
         find(:css, "#user_email_on_review_of_review").set(true)
-
         review_box = find('#user_email_on_review_of_review')
         expect(review_box).to be_checked
       end
@@ -93,13 +85,11 @@ describe 'should edit profile email options' do
         login_as "instructor6"
         visit '/profile/edit'
         expect(page).to have_current_path('/profile/edit')
-
+        #find and uncheck the box for When someone else reviews one of my reviews (metareviews my work)
         find(:css, "#user_email_on_review_of_review").set(false)
-
         review_box = find('#user_email_on_review_of_review')
         expect(review_box).not_to be_checked
       end
     end
-
   end
 end
--- spec/features/email_notification_spec.rb
+++ spec/features/email_notification_spec.rb
@@ -16,7 +16,6 @@ feature 'Reset password notification' do
 
   scenario 'testing for content' do
     expect(page).to have_current_path('/password_retrieval/forgotten')
-
     expect(current_email).to have_content 'Your Expertiza password has been reset. We strongly recommend that you change the password the next time you access the application.'
     expect(current_email).to have_content 'User Name'
     expect(current_email).to have_content 'New password'
@@ -38,14 +37,12 @@ feature 'Welcome notification' do
     find('input#user_email', visible: false).set('expertiza@test.com')
 
     click_button 'Create'
-
     # Will find an email sent to expertiza.development@gmail.com and set `current_email`
     open_email('expertiza.development@gmail.com')
   end
 
   scenario 'testing for content' do
     expect(page).to have_current_path('/users/list')
-
     expect(current_email).to have_content 'Your Expertiza account has been created at http://expertiza.ncsu.edu. We strongly recommend that you change the password the first time you access the application.'
     expect(current_email).to have_content 'User Name'
     expect(current_email).to have_content 'E-mail address'
@@ -71,6 +68,8 @@ feature 'New submission notification' do
 
   scenario 'testing for content' do
     expect(current_email).to have_content 'assignment has just been entered'
+    expect(current_email).to have_content 'You may log into Expertiza and view it now'
+    expect(current_email).to have_content 'Hi'
   end
 end
 
@@ -117,6 +116,7 @@ feature 'Register notification' do
 
   scenario 'testing for content' do
     expect(current_email).to have_content "You have been registered to use the Expertiza system."
+    expect(current_email).to have_content "You can access the system using the following URL"
+    expect(current_email).to have_content "Please use the following information to log into Expertiza:"
   end
-
 end

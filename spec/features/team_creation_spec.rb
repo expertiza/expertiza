e5011763486549b0474c157b83a2f6e200825fe5
sndesai92 <sndesai@ncsu.edu>
2016-11-04 18:42:15 -0400

    added test 2  for issue 312

--- spec/features/team_creation_spec.rb
+++ spec/features/team_creation_spec.rb
@@ -127,6 +127,14 @@ Hello world! ')
       expect(page).to have_content('Team Name: final2_Team1')
     end
 
+    it 'Student should accept the invitation sent by other student and both have topics and no longer has any pending invitations' do
+      visit '/invitation/accept?inv_id=1&student_id=1&team_id=2'
+      visit '/student_teams/view?student_id=1'
+      expect(page).to have_content('Team Name: final2_Team1')
+
+      expect(page).not_to have_content('pending')
+    end
+
     it 'student should reject the invitation sent by the other student and both gave topics' do
       visit '/invitation/decline?inv_id=1&student_id=1'
       visit '/student_teams/view?student_id=1'
@@ -248,7 +256,9 @@ Hello world! ')
       expect(page).not_to have_content('pending')
     end
 
-    it "Student should reject the inviattion sent by the other student who has a topic" do
+
+
+    it "Student should reject the invitation sent by the other student who has a topic" do
       visit '/invitation/decline?inv_id=1&student_id=1'
       visit '/student_teams/view?student_id=1'
       expect(page).to have_content('Team Name: final2_Team1')

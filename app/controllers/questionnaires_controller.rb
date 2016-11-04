b577b0c876628a41830170618ac5cf48c8bc0969
navisingh14 <navjotgogna@gmail.com>
2016-11-04 00:14:45 -0400

    added the functionality to import the file from any local machine

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -609,9 +609,13 @@ class QuestionnairesController < ApplicationController
 
   def import
 
+      uploaded_io = params[:csv]
+      File.open(Rails.root.join('spec', 'features/import_export_csv_oss/', uploaded_io.original_filename), 'wb') do |file|
+        file.write(uploaded_io.read)
+      end
     questionnaire_id = (params[:id])
     begin
-        file_data = File.read(Rails.root.join('spec/features/import_export_csv_oss/'+params[:csv]))
+        file_data = File.read(Rails.root.join('spec/features/import_export_csv_oss/'+uploaded_io.original_filename))
         a = QuestionnaireHelper.get_questions_from_csv(file_data,params[:id])
         redirect_to edit_questionnaire_path(questionnaire_id.to_sym), notice: "All questions have been successfully imported!"
        rescue

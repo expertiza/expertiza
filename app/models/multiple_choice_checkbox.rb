57b010f6c19f6638aff782b3baec09c257034a1a
Parag Nakhwa <psnakhwa@ncsu.edu>
2016-11-04 20:36:47 -0400

    completed method for view_question_text

--- app/models/multiple_choice_checkbox.rb
+++ app/models/multiple_choice_checkbox.rb
@@ -32,6 +32,20 @@ class MultipleChoiceCheckbox < QuizQuestion
   end
 
   def view_question_text
+    html = "<b>" + self.txt + '</b><br />'
+    html += "Question Type: " + self.type + '<br />'
+    if self.quiz_question_choices
+      self.quiz_question_choices.each do |choices|
+        html += if choices.iscorrect?
+                  "  - <b>" + choices.txt + "</b><br /> "
+                else
+                  "  - " + choices.txt + "<br /> "
+                end
+      end
+      html += '<br />'
+    end
+
+    html.html_safe
 
   end
 
--- app/models/multiple_choice_radio.rb
+++ app/models/multiple_choice_radio.rb
@@ -29,7 +29,22 @@ class MultipleChoiceRadio < QuizQuestion
 
   def view_completed_question(count, answer)
   end
-  def view_question_text
 
+  def view_question_text
+    html = "<b>" + self.txt + '</b><br />'
+    html += "Question Type: " + self.type + '<br />'
+    if self.quiz_question_choices
+      self.quiz_question_choices.each do |choices|
+        html += if choices.iscorrect?
+                  "  - <b>" + choices.txt + "</b><br /> "
+                else
+                  "  - " + choices.txt + "<br /> "
+                end
+      end
+      html += '<br />'
+    end
+
+    html.html_safe
   end
+
 end

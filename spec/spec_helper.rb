12fd5fd6e0256b2f2454623d5774b281a611ff9f
Winbobob <zhu6@ncsu.edu>
2016-11-05 09:23:04 -0400

    Fix tests in /controllers/sign_up_sheet_spec.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -94,7 +94,7 @@ RSpec.configure do |config|
       # before a spec run, as is the case in a typical spec helper. It will
       # cause any verifying double instantiation for a class that does not
       # exist to raise, protecting against incorrectly spelt names.
-      mocks.verify_doubled_constant_names = true
+      mocks.verify_doubled_constant_names = false
     end
 
   Dir["./spec/features/helpers/*.rb"].each do |filename|

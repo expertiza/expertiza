1ab214f0332ae8b82df36008fe1a722e4c984ce7
anudeepvrm <anudeepvrm@gmail.com>
2016-11-08 14:47:28 -0500

    removing stub as curretn expertiza code not allowing

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -15,34 +15,34 @@ describe Answer do
    				
     it "returns total score when required conditions are met" do
       # stub for ScoreView.find_by_sql to revent prevent unit testing sql db queries
-      ScoreView.stub(:find_by_sql).and_return([double("scoreview",weighted_score: 20,sum_of_weights: 5,q1_max_question_score: 4)])
-      Answer.stub(:where).and_return([double("row1",question_id: 1,answer: "1")])
+      allow(ScoreView).to receive(:find_by_sql).and_return([double("scoreview",weighted_score: 20,sum_of_weights: 5,q1_max_question_score: 4)])
+      allow(Answer).to receive(:where).and_return([double("row1",question_id: 1,answer: "1")])
       expect(Answer.get_total_score(response: [response_record], questions: [question1])).to eq 100.0
       #output calculation is (weighted_score / (sum_of_weights * max_question_score)) * 100
       # 4.0
     end
 
     it "returns total score when one answer is nil for scored question and its weight gets removed from sum_of_weights" do
-      ScoreView.stub(:find_by_sql).and_return([double("scoreview",weighted_score: 20,sum_of_weights: 5,q1_max_question_score: 4)])
-      Answer.stub(:where).and_return([double("row1",question_id: 1,answer: nil)])
+      allow(ScoreView).to receive(:find_by_sql).and_return([double("scoreview",weighted_score: 20,sum_of_weights: 5,q1_max_question_score: 4)])
+      allow(Answer).to receive(:where).and_return([double("row1",question_id: 1,answer: nil)])
       expect(Answer.get_total_score(response: [response_record], questions: [question1])).to be_within(0.01).of(125.0)
     end			
 
     it "returns -1 when answer is nil for scored question which makes sum of weights = 0" do
-      ScoreView.stub(:find_by_sql).and_return([double("scoreview",weighted_score: 20,sum_of_weights: 1,q1_max_question_score: 5)])
-      Answer.stub(:where).and_return([double("row1",question_id: 1,answer: nil)])
+      allow(ScoreView).to receive(:find_by_sql).and_return([double("scoreview",weighted_score: 20,sum_of_weights: 1,q1_max_question_score: 5)])
+      allow(Answer).to receive(:where).and_return([double("row1",question_id: 1,answer: nil)])
       expect(Answer.get_total_score(response: [response_record], questions: [question1])).to eq -1.0
     end
 
     it "returns -1 when weighted_score of questionnaireData is nil" do
-      ScoreView.stub(:find_by_sql).and_return([double("scoreview",weighted_score: nil,sum_of_weights: 5,q1_max_question_score: 5)])
-      Answer.stub(:where).and_return([double("row1",question_id: 1,answer: nil)])
+      allow(ScoreView).to receive(:find_by_sql).and_return([double("scoreview",weighted_score: nil,sum_of_weights: 5,q1_max_question_score: 5)])
+      allow(Answer).to receive(:where).and_return([double("row1",question_id: 1,answer: nil)])
       expect(Answer.get_total_score(response: [response_record], questions: [question1])).to eq -1.0
     end
 
     it "checks if submission_valid? is called" do
-      ScoreView.stub(:find_by_sql).and_return([double("scoreview",weighted_score: nil,sum_of_weights: 5,q1_max_question_score: 5)])
-      Answer.stub(:where).and_return([double("row1",question_id: 1,answer: nil)])
+      allow(ScoreView).to receive(:find_by_sql).and_return([double("scoreview",weighted_score: nil,sum_of_weights: 5,q1_max_question_score: 5)])
+      allow(Answer).to receive(:where).and_return([double("row1",question_id: 1,answer: nil)])
       expect(Answer).to receive(:submission_valid?)			
       Answer.get_total_score(response: [response_record], questions: [question1])			
     end
@@ -54,7 +54,7 @@ describe Answer do
 
     it "returns nil if list of assessments is empty" do
       assessments=[]
-      Answer.stub(:get_total_score).and_return(100.0)
+      allow(Answer).to receive(:get_total_score).and_return(100.0)
       scores=Answer.compute_scores(assessments, [question1])
       expect(scores[:max]).to eq nil
       expect(scores[:min]).to eq nil
@@ -65,7 +65,7 @@ describe Answer do
       assessments=[response1]
       Answer.instance_variable_set(:@invalid,0)
       total_score=100.0
-      Answer.stub(:get_total_score).and_return(total_score)
+      allow(Answer).to receive(:get_total_score).and_return(total_score)
       scores=Answer.compute_scores(assessments, [question1])
       expect(scores[:max]).to eq total_score
       expect(scores[:min]).to eq total_score
@@ -77,7 +77,7 @@ describe Answer do
       Answer.instance_variable_set(:@invalid,0)
       total_score1=100.0
       total_score2=80.0
-      Answer.stub(:get_total_score).and_return(total_score1,total_score2)
+      allow(Answer).to receive(:get_total_score).and_return(total_score1,total_score2)
       scores=Answer.compute_scores(assessments, [question1])
       expect(scores[:max]).to eq total_score1
       expect(scores[:min]).to eq total_score2
@@ -88,7 +88,7 @@ describe Answer do
       assessments=[response1]
       Answer.instance_variable_set(:@invalid,1)
       total_score=100.0
-      Answer.stub(:get_total_score).and_return(total_score)
+      allow(Answer).to receive(:get_total_score).and_return(total_score)
       scores=Answer.compute_scores(assessments, [question1])
       expect(scores[:max]).to eq total_score
       expect(scores[:min]).to eq total_score
@@ -99,7 +99,7 @@ describe Answer do
       assessments=[response1]
       Answer.instance_variable_set(:@invalid,nil)
       total_score=100.0
-      Answer.stub(:get_total_score).and_return(total_score)
+      allow(Answer).to receive(:get_total_score).and_return(total_score)
       scores=Answer.compute_scores(assessments, [question1])
       expect(scores[:max]).to eq total_score
       expect(scores[:min]).to eq total_score
@@ -150,8 +150,6 @@ describe Answer do
       due_date1.deadline_type_id = 4
       due_date2.deadline_type_id = 2
       ResubmissionTime1, ResubmissionTime2 = Time.new-24, Time.new-48
-      AssignmentDueDate.stub_chain(:where, :order).and_return(due_date1, due_date2)
-      ResubmissionTime.stub_chain(:where, :order).and_return(ResubmissionTime1, ResubmissionTime2)
       expect(Answer.submission_valid?(response_record)).to eq nil
     end
 
@@ -159,7 +157,8 @@ describe Answer do
     it "Checking when no due date objects are passed back to @sorted_deadlines" do
       response_record.id = 1
       response_record.additional_comment = "Test"
-      AssignmentDueDate.stub_chain(:where, :order).and_return(nil)
+      allow(AssignmentDueDate).to receive(:where).and_return(nil)
+      allow(AssignmentDueDate).to receive(:order).and_return(nil)
       expect{Answer.submission_valid?(response_record)}.to raise_error
     end
   end

d6506f5167a9ce8dfa77fac596a1dd30a47f4fab
navisingh14 <navjotgogna@gmail.com>
2016-11-09 17:27:30 -0500

    removed edit.xls, removed variable a

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -613,7 +613,7 @@ class QuestionnairesController < ApplicationController
         file.write(uploaded_io.read)
       end
         file_data = File.read(Rails.root.join('spec/features/import_export_csv_oss/'+uploaded_io.original_filename))
-        a = QuestionnaireHelper.get_questions_from_csv(file_data,params[:id])
+        QuestionnaireHelper.get_questions_from_csv(file_data,params[:id])
         redirect_to edit_questionnaire_path(questionnaire_id.to_sym), notice: "All questions have been successfully imported!"
        rescue
       redirect_to edit_questionnaire_path(questionnaire_id.to_sym), notice: $ERROR_INFO

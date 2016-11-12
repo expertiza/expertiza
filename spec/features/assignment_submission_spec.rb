d1706154b5729e1b3bcacfeb976ad3c28f8101d8
popoosl <flypopoo@gmail.com>
2016-11-12 13:40:22 -0500

    Initial create assignment&topic and build test case 1

---
+++ spec/features/assignment_submission_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe "assignment submisstion test" do
+  before(:each) do
+    #create assignment and topic
+    create(:assignment, name: "Assignment1684", directory_path: "Assignment1684")
+    create_list(:participant, 3)
+    create(:topic, topic_name: "Topic")
+    create(:deadline_type, name: "submission")
+    create(:deadline_type, name: "review")
+    create(:deadline_type, name: "metareview")
+    create(:deadline_type, name: "drop_topic")
+    create(:deadline_type, name: "signup")
+    create(:deadline_type, name: "team_formation")
+    create(:deadline_right)
+    create(:deadline_right, name: 'Late')
+    create(:deadline_right, name: 'OK')
+    create(:assignment_due_date, deadline_type: DeadlineType.where(name: "submission").first, due_at: DateTime.now + 1)
+  end
+
+  def signup_topic
+    user = User.find_by_name("student2064")
+    stub_current_user(user, user.role.name, user.role)
+    visit '/student_task/list'
+    visit '/sign_up_sheet/sign_up?id=1&topic_id=1' #signup topic
+    visit '/student_task/list'
+    click_link "Assignment1684"
+    click_link "Your work"
+  end
+
+  it "test1: submit single link" do
+    signup_topic
+    fill_in 'submission', with: "https://google.com"
+    click_on 'Upload link'
+    expect(page).to have_content "https://google.com"
+  end
+end

5d114b4a7df6960aae41ed9e9b45c5072120fac8
yangsong8 <ysong8@ncsu.edu>
2016-11-13 23:34:09 -0500

    Wrap the long lines in review_response_map.rb (E1642)

--- app/models/review_response_map.rb
+++ app/models/review_response_map.rb
@@ -5,7 +5,8 @@ class ReviewResponseMap < ResponseMap
 
   # In if this assignment uses "varying rubrics" feature, the sls
   # "used_in_round" field should not be nil
-  # so find the round # based on current time and the due date times, and use that round # to find corresponding questionnaire_id from assignment_questionnaires table
+  # so find the round # based on current time and the due date times, and use that round # to find corresponding
+  # questionnaire_id from assignment_questionnaires table
   # otherwise this assignment does not use the "varying rubrics", so in assignment_questionnaires table there should
   # be only 1 questionnaire with type 'ReviewQuestionnaire'.    -Yang
   def questionnaire(round)
@@ -132,7 +133,8 @@ class ReviewResponseMap < ResponseMap
   def self.review_response_report(id, assignment, type, review_user)
     if review_user.nil?
       # This is not a search, so find all reviewers for this assignment
-      response_maps_with_distinct_participant_id = ResponseMap.select("DISTINCT reviewer_id").where(["reviewed_object_id = ? and type = ? and calibrate_to = ?", id, type, 0])
+      response_maps_with_distinct_participant_id =
+          ResponseMap.select("DISTINCT reviewer_id").where(["reviewed_object_id = ? and type = ? and calibrate_to = ?", id, type, 0])
       @reviewers = []
       response_maps_with_distinct_participant_id.each do |reviewer_id_from_response_map|
         @reviewers << AssignmentParticipant.find(reviewer_id_from_response_map.reviewer_id)

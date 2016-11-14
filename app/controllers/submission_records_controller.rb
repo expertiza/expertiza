18c30f1cf23cb5642e5dd70c473b4333bd6e99bb
yangsong8 <ysong8@ncsu.edu>
2016-11-14 16:40:01 -0500

    Clean up submission_records_controller.rb (E1675)

--- app/controllers/submission_records_controller.rb
+++ app/controllers/submission_records_controller.rb
@@ -2,82 +2,20 @@ class SubmissionRecordsController < ApplicationController
   before_action :set_submission_record, only: [:show, :edit, :update, :destroy]
   
   def action_allowed?
-    if params[:action] == 'edit' || params[:action] == 'update'
-      assignment = Assignment.find(params[:id])
-      return true if ['Super-Administrator', 'Administrator'].include? current_role_name
-      return true if assignment.instructor_id == current_user.id
-      return true if TaMapping.exists?(ta_id: current_user.id, course_id: assignment.course_id) && (TaMapping.where(course_id: assignment.course_id).include?TaMapping.where(ta_id: current_user.id, course_id: assignment.course_id).first)
-      return true if assignment.course_id && Course.find(assignment.course_id).instructor_id == current_user.id
-      return false
-    else
-      ['Super-Administrator',
-       'Administrator',
-       'Instructor',
-       'Teaching Assistant'].include? current_role_name
-    end
+    # currently we only have a index method which shows all the submission records given a team_id
+    assignment_team = AssignmentTeam.find(params[:team_id])
+    assignment = Assignment.find(assignment_team.parent_id)
+    return true if ['Super-Administrator', 'Administrator'].include? current_role_name
+    return true if assignment.instructor_id == current_user.id
+    return true if TaMapping.exists?(ta_id: current_user.id, course_id: assignment.course_id) && (TaMapping.where(course_id: assignment.course_id).include?TaMapping.where(ta_id: current_user.id, course_id: assignment.course_id).first)
+    return true if assignment.course_id && Course.find(assignment.course_id).instructor_id == current_user.id
+    return false
   end
 
-  # GET /submission_records
+  # Show submission records.
+  # expects to get team_id from params
   def index
     @submission_records = SubmissionRecord.where(team_id: params[:team_id])
   end
 
-  # GET /submission_records/1
-  def show()
-
-    @submission_records = SubmissionRecord.where("team_id = ? AND assignment_id = ?",params['team_id'],params['assignment_id'])
-
-  end
-
-# This query will basically find the record using WHERE statement, it searches
-# for the partucular team, and then the corresponding assignment.
-  def findRecords()
-    @submission_records = SubmissionRecord.where("team_id = ? AND assignment_id = ?",params['team_id'],params['assignment_id'])
-
-  end
-  # GET /submission_records/new
-  def new
-    @submission_record = SubmissionRecord.new
-  end
-
-  # GET /submission_records/1/edit
-  def edit
-  end
-
-  # POST /submission_records
-  def create
-    @submission_record = SubmissionRecord.new(submission_record_params)
-
-    if @submission_record.save
-      redirect_to @submission_record, notice: 'Submission record was successfully created.'
-    else
-      render :new
-    end
-  end
-
-  # PATCH/PUT /submission_records/1
-  def update
-    if @submission_record.update(submission_record_params)
-      redirect_to @submission_record, notice: 'Submission record was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # DELETE /submission_records/1
-  def destroy
-    @submission_record.destroy
-    redirect_to submission_records_url, notice: 'Submission record was successfully destroyed.'
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_submission_record
-      @submission_record = SubmissionRecord.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def submission_record_params
-      params[:submission_record]
-    end
 end

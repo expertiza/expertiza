98fd0b921ce611600e69ff3873496a0d2319e4ab
yangsong8 <ysong8@ncsu.edu>
2016-11-14 16:11:54 -0500

    Add db/schema.rb back.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161028111459) do
+ActiveRecord::Schema.define(version: 20161114210745) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "question_id", limit: 4,     default: 0, null: false
@@ -492,7 +492,6 @@ ActiveRecord::Schema.define(version: 20161028111459) do
     t.datetime "updated_at",                  null: false
     t.text     "type",          limit: 65535
     t.string   "content",       limit: 255
-    t.datetime "createdat"
     t.string   "operation",     limit: 255
     t.integer  "team_id",       limit: 4
     t.string   "user",          limit: 255

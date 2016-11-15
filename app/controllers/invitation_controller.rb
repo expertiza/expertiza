163e0061c7f670573a39ff4d34ad624f82ae06b3
canhsiao <canhsiao@gmail.com>
2016-11-14 21:33:07 -0500

    update according to review

--- app/controllers/invitation_controller.rb
+++ app/controllers/invitation_controller.rb
@@ -1,4 +1,4 @@
-class InvitationController < ApplicationController
+class InvitationsController < ApplicationController
 	# decide if the controller is accessisable to the user
   def action_allowed?
     ['Instructor', 'Teaching Assistant', 'Administrator', 'Super-Administrator', 'Student'].include? current_role_name
@@ -9,26 +9,26 @@ class InvitationController < ApplicationController
   end
 
   def create
-    user = User.find_by_name(params[:user][:name].strip)
+    user_invitee = User.find_by_name(params[:user][:name].strip)
     team = AssignmentTeam.find(params[:team_id])
     student = AssignmentParticipant.find(params[:student_id])
     return unless current_user_id?(student.user_id)
 
     # check if the invited user is valid
-    if user
+    if user_invitee
       # check if the user is a participant of the assignment
-      participant = AssignmentParticipant.where('user_id =? and parent_id =?', user.id, student.parent_id).first
+      participant = AssignmentParticipant.where('user_id =? and parent_id =?', user_invitee.id, student.parent_id).first
       if participant && !team.full?
-        team_member = TeamsUser.where(['team_id =? and user_id =?', team.id, user.id])
+        team_member = TeamsUser.where(['team_id =? and user_id =?', team.id, user_invitee.id])
         # check if invited user is already in the team
-        if team_member.empty? && Invitation.is_invited?(student.user_id, user.id, student.parent_id)
+        if team_member.empty? && Invitation.is_invited?(student.user_id, user_invitee.id, student.parent_id)
           # check if the invited user is already invited (i.e. awaiting reply)
           # reply_status is the current replying status
           # W means there's a invitation reply waiting to be sent
           # A means the invitation is accepted
           # D means the invitaion is declined
           @invitation = Invitation.new
-          @invitation.to_id = user.id
+          @invitation.to_id = user_invitee.id
           @invitation.from_id = student.user_id
           @invitation.assignment_id = student.parent_id
           @invitation.reply_status = 'W'
@@ -36,29 +36,30 @@ class InvitationController < ApplicationController
         end
       end
     end
-    if !user
-      flash[:note] = "The user \"#{params[:user][:name].strip}\" does not exist. Please make sure the name entered is correct."
+    if !user_invitee
+      flash[:note] = "The invited user \"#{params[:user][:name].strip}\" does not exist. Please make sure the name entered is correct."
     elsif !participant
-      flash[:note] = "The user \"#{params[:user][:name].strip}\" is not a participant of this assignment."
+      flash[:note] = "The invited user \"#{params[:user][:name].strip}\" is not a participant of this assignment."
     elsif team.full?
       flash[:error] = "Your team already has the maximum number members."
     elsif !team_member.empty?
-      flash[:note] = "The user \"#{user.name}\" is already a member of the team."
-    elsif Invitation.is_invited?(student.user_id, user.id, student.parent_id)
-    else
-      flash[:note] = "You have already sent an invitation to \"#{user.name}\"."
+      flash[:note] = "The invited user \"#{user_invitee.name}\" is already a member of the team."
+    elsif Invitation.is_invited?(student.user_id, user_invitee.id, student.parent_id)
+      flash[:note] = "You have already sent an invitation to \"#{user_invitee.name}\"."
     end
 
-    update_join_team_request user, student
+    update_join_team_request user_invitee, student
 
     redirect_to view_student_teams_path student_id: student.id
   end
 
   # update the request to join team 
-  def update_join_team_request(user, student)
+  # if join team request exist, handle invitaion automatically
+  # used in above case at line 51 when the condition is fulfiled
+  def update_join_team_request(user_invitee, student)
     # update the status in the join_team_request to A
-    if user && student
-      participant = AssignmentParticipant.where(['user_id =? and parent_id =?', user.id, student.parent_id]).first
+    if user_invitee && student
+      participant = AssignmentParticipant.where(['user_id =? and parent_id =?', user_invitee.id, student.parent_id]).first
       if participant
         old_entry = JoinTeamRequest.where(['participant_id =? and team_id =?', participant.id, params[:team_id]]).first
         old_entry.update_attribute("status", 'A') if old_entry
@@ -84,18 +85,18 @@ class InvitationController < ApplicationController
 
     
     if ready_to_join?(inviter_participant)
-	    @inv.reply_status = 'A'
-	    @inv.save
+      @inv.reply_status = 'A'
+      @inv.save
 
-	    # Remove the users previous team since they are accepting an invite for possibly a new team.
-	    TeamsUser.remove_team(student.user_id, params[:team_id])
+      # Remove the users previous team since they are accepting an invite for possibly a new team.
+      TeamsUser.remove_team(student.user_id, params[:team_id])
 
-	    # Accept the invite and return boolean on whether the add was successful
-	    add_successful = Invitation.accept_invite(params[:team_id], @inv.from_id, @inv.to_id, student.parent_id)
+      # Accept the invite and return boolean on whether the add was successful
+      add_successful = Invitation.accept_invite(params[:team_id], @inv.from_id, @inv.to_id, student.parent_id)
 
-	    unless add_successful
-		    flash[:error] = "The system failed to add you to the team that invited you."
-	    end
+      unless add_successful
+        flash[:error] = "The system failed to add you to the team that invited you."
+      end
     end
 
     redirect_to view_student_teams_path student_id: params[:student_id]
@@ -103,32 +104,32 @@ class InvitationController < ApplicationController
 
   # decline invitation
   def decline
-	  @inv = Invitation.find(params[:inv_id])
-	  @inv.reply_status = 'D'
-	  @inv.save
-	  student = Participant.find(params[:student_id])
-	  redirect_to view_student_teams_path student_id: student.id
+    @inv = Invitation.find(params[:inv_id])
+    @inv.reply_status = 'D'
+    @inv.save
+    student = Participant.find(params[:student_id])
+    redirect_to view_student_teams_path student_id: student.id
   end
 
   # cancel invitation
   def cancel
-	  Invitation.find(params[:inv_id]).destroy
-	  redirect_to view_student_teams_path student_id: params[:student_id]
+    Invitation.find(params[:inv_id]).destroy
+    redirect_to view_student_teams_path student_id: params[:student_id]
   end
   
   private 
   def ready_to_join?(inviter_participant)
-	  ready_to_join = false
-	  # check if the inviter's team is still existing, and have available slot to add the invitee
+    ready_to_join = false
+    # check if the inviter's team is still existing, and have available slot to add the invitee
 	  
     inviter_assignment_team = AssignmentTeam.team(inviter_participant)
-	  if inviter_assignment_team.nil?
-		  flash[:error] = "The team that invited you does not exist anymore."
-	  elsif inviter_assignment_team.full?
-		  flash[:error] = "The team that invited you is full now."
-	  else
-		  ready_to_join = true
-	  end
-	  return ready_to_join
+    if inviter_assignment_team.nil?
+      flash[:error] = "The team that invited you does not exist anymore."
+    elsif inviter_assignment_team.full?
+      flash[:error] = "The team that invited you is full now."
+    else
+      ready_to_join = true
+    end
+      return ready_to_join
   end
 end

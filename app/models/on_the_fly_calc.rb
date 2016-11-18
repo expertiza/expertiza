06a14ec27473eee407aebab7e9a81d8baf01bd62
aayushagarwal7 <s.aayushagarwal@gmail.com>
2016-11-18 03:41:59 -0500

    minor

--- app/models/on_the_fly_calc.rb
+++ app/models/on_the_fly_calc.rb
@@ -60,7 +60,16 @@ module OnTheFlyCalc
       score_team = {}
       score_team[:team] = team
       if self.varying_rubrics_by_round?
-        assess
+        self.num_review_rounds.each do |i|
+          total_score = 0
+          total_num_of_assessments = 0 # calculate grades for each rounds
+          grades_by_rounds = {}
+          assessments = ReviewResponseMap.get_assessments_round_for(team, i)
+          round_sym = ("review" + i.to_s).to_sym
+          grades_by_rounds[round_sym] = Answer.compute_scores(assessments, questions[round_sym])
+          total_num_of_assessments += assessments.size
+          total_score += grades_by_rounds[round_sym][:avg] * assessments.size.to_f unless grades_by_rounds[round_sym][:avg].nil?
+        end
         calculate_score
         calculate_assessment
       else
@@ -74,19 +83,6 @@ module OnTheFlyCalc
 end
 
 private
-
-def assess
-  self.num_review_rounds.each do |i|
-    total_score = 0
-    total_num_of_assessments = 0 # calculate grades for each rounds
-    grades_by_rounds = {}
-    assessments = ReviewResponseMap.get_assessments_round_for(team, i)
-    round_sym = ("review" + i.to_s).to_sym
-    grades_by_rounds[round_sym] = Answer.compute_scores(assessments, questions[round_sym])
-    total_num_of_assessments += assessments.size
-    total_score += grades_by_rounds[round_sym][:avg] * assessments.size.to_f unless grades_by_rounds[round_sym][:avg].nil?
-  end
-end
 
 def calculate_score
   score = {}

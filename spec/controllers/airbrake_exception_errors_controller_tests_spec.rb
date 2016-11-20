88b0f78a1b31d9b062de185a4c2d380cebf6fcf8
Zhewei Hu <zhu6@ncsu.edu>
2016-11-20 16:11:07 -0500

    Update airbrake_exception_errors_controller_tests_spec.rb

--- spec/controllers/airbrake_exception_errors_controller_tests_spec.rb
+++ spec/controllers/airbrake_exception_errors_controller_tests_spec.rb
@@ -1,22 +1,7 @@
 require 'rails_helper'
 require 'pry'
 
-describe TeamsController do
-  # Airbrake-1804043391875943089
-  describe '#new', type: :controller do
-    it 'will set the default team parent as Assignment' do
-      allow(Assignment).to receive(:find).with(1).and_return(instance_double('Assignment'))
-      get :new
-      # get :new, params: { id: 1 }, session: { team_type: nil }
-      controller.params[:id] = 1
-      controller.session[:team_type] = nil
-      expect(response).to have_http_status(302)
-
-      expect { Object.const_get(session[:team_type]).find(params[:id]) }.to raise_error(TypeError)
-      expect { Object.const_get(session[:team_type] ||= 'Assignment').find(params[:id]) }.not_to raise_error(TypeError)
-    end
-  end
- 
+describe TeamsController do 
   # Airbrake-1807465099223895248
   describe '#delete', type: :controller do
     before(:each) do

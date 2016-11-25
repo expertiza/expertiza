90e2b9eaecac10be55e7210cb0b682b18ff91bae
bhaveshkasliwal30 <bkasliw@ncsu.edu>
2016-11-25 15:27:43 -0500

    Changes in groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,58 +1,130 @@
 class GroupsController < ApplicationController
-  before_action :set_group, only: [:show, :edit, :update, :destroy]
+  autocomplete :user, :name
 
-  # GET /groups
-  def index
-    @groups = Group.all
+  def action_allowed?
+    ['Instructor',
+     'Teaching Assistant',
+     'Administrator'].include? current_role_name
   end
 
-  # GET /groups/1
-  def show
+  # This function is used to create groups with random names.
+  # Instructors can call by clicking "Create temas" icon anc then click "Create groups" at the bottom.
+  def create_groups
+    parent = Object.const_get(session[:group_type]).find(params[:id])
+    Group.randomize_all_by_parent(parent, session[:group_type], params[:group_size].to_i)
+    undo_link("Random groups have been successfully created.")
+    redirect_to action: 'list', id: parent.id
   end
 
-  # GET /groups/new
+  def list
+    allowed_types = ['Assignment', 'Course']
+    session[:group_type] = params[:type] if params[:type] && allowed_types.include?(params[:type])
+    begin
+      @root_node = Object.const_get(session[:group_type] + "Node").find_by_node_object_id(params[:id])
+      @child_nodes = @root_node.get_groups
+    rescue
+      flash[:error] = $!
+    end
+  end
+
   def new
-    @group = Group.new
+    @parent = Object.const_get(session[:group_type] ||= 'Assignment').find(params[:id])
   end
 
-  # GET /groups/1/edit
-  def edit
-  end
-
-  # POST /groups
+  # called when a instructor tries to create an empty namually.
   def create
-    @group = Group.new(group_params)
-
-    if @group.save
-      redirect_to @group, notice: 'Group was successfully created.'
-    else
-      render :new
+    parent = Object.const_get(session[:group_type]).find(params[:id])
+    begin
+      Group.check_for_existing(parent, params[:group][:name], session[:group_type])
+      @group = Object.const_get(session[:group_type] + 'Group').create(name: params[:group][:name], parent_id: parent.id)
+      GroupNode.create(parent_id: parent.id, node_object_id: @group.id)
+      undo_link("The group \"#{@group.name}\" has been successfully created.")
+      redirect_to action: 'list', id: parent.id
+    rescue GroupExistsError
+      flash[:error] = $ERROR_INFO
+      redirect_to action: 'new', id: parent.id
     end
   end
 
-  # PATCH/PUT /groups/1
   def update
-    if @group.update(group_params)
-      redirect_to @group, notice: 'Group was successfully updated.'
+    @group = Group.find(params[:id])
+    parent = Object.const_get(session[:group_type]).find(@group.parent_id)
+    begin
+      Group.check_for_existing(parent, params[:group][:name], session[:group_type])
+      @group.name = params[:group][:name]
+      @group.save
+      flash[:success] = "The group \"#{@group.name}\" has been successfully updated."
+      undo_link("")
+      redirect_to action: 'list', id: parent.id
+    rescue GroupExistsError
+      flash[:error] = $ERROR_INFO
+      redirect_to action: 'edit', id: @group.id
+    end
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def delete
+    # delete records in group, groups_users, signed_up_groups table
+    @group = Group.find(params[:id])
+    course = Object.const_get(session[:group_type]).find(@group.parent_id)
+
+    @sign_ups = SignedUpGroup.where(group_id: @group.id)
+
+    @groups_users = GroupsUser.where(group_id: @group.id)
+
+    if @sign_ups.size == 1 and @sign_ups.first.is_waitlisted == false # this group hold a topic
+      # if there is another group in waitlist, make this group hold this topic
+      topic_id = @sign_ups.first.topic_id
+      next_wait_listed_group = SignedUpGroup.where(topic_id: topic_id, is_waitlisted: true).first
+      # if slot exist, then confirm the topic for this group and delete all waitlists for this group
+      if next_wait_listed_group
+        SignUpTopic.assign_to_first_waiting_group(next_wait_listed_group)
+      end
+    end
+
+    @sign_ups.destroy_all if @sign_ups
+    @groups_users.destroy_all if @groups_users
+    @group.destroy if @group
+
+    undo_link("The group \"#{@group.name}\" has been successfully deleted.")
+    redirect_to action: 'list', id: course.id
+  end
+
+  # Copies existing groups from a course down to an assignment
+  # The group and group members are all copied.
+  def inherit
+    assignment = Assignment.find(params[:id])
+    if assignment.course_id >= 0
+      course = Course.find(assignment.course_id)
+      groups = course.get_groups
+      if !groups.empty?
+        groups.each do |group|
+          group.copy(assignment.id)
+        end
+      else
+        flash[:note] = "No groups were found when trying to inherit."
+      end
     else
-      render :edit
+      flash[:error] = "No course was found for this assignment."
     end
+    redirect_to controller: 'groups', action: 'list', id: assignment.id
   end
 
-  # DELETE /groups/1
-  def destroy
-    @group.destroy
-    redirect_to groups_url, notice: 'Group was successfully destroyed.'
+  # Copies existing groups from an assignment up to a course
+  # The group and group members are all copied.
+  def bequeath
+    group = AssignmentGroup.find(params[:id])
+    assignment = Assignment.find(group.parent_id)
+    if assignment.course_id >= 0
+      course = Course.find(assignment.course_id)
+      group.copy(course.id)
+      flash[:note] = "The group \"" + group.name + "\" was successfully copied to \"" + course.name + "\""
+    else
+      flash[:error] = "This assignment is not #{url_for(controller: 'assignment', action: 'assign', id: assignment.id)} with a course."
+    end
+    redirect_to controller: 'groups', action: 'list', id: assignment.id
   end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_group
-      @group = Group.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def group_params
-      params.require(:group).permit(:isAnonymous, :name, :parent_id, :type, :comments_for_advertisement, :advertise_for_partners)
-    end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,157 @@
-class Group < Team
+class Group < ActiveRecord::Base
+  before_action :set_group, only: [:show, :edit, :update, :destroy]
   has_many :groups_users, dependent: :destroy
   has_many :users, through: :groups_users
   has_many :join_group_requests
+
+  # Get the participants of the given group
+  def participants
+    users.where(parent_id: parent_id || current_user_id).flat_map(&:participants)
+  end
+  alias get_participants participants
+
+  # Get the response review map
+  def responses
+    participants.flat_map(&:responses)
+  end
+
+  # Delete the given group
+  def delete
+    for groupsuser in GroupsUser.where(["group_id =?", self.id])
+      groupsuser.delete
+    end
+    self.destroy
+  end
+
+  # Get the names of the users
+  def get_reviewer_names
+    names = []
+    users.each do |user|
+      names << user.fullname
+    end
+    names
+  end
+
+  # Check if the user exist
+  def has_user(user)
+    users.include? user
+  end
+
+  # Check if the current group is full?
+  def full?
+    return false if self.parent_id == nil #course group, does not group_size
+    max_group_members = Assignment.find(self.parent_id).group_size
+    curr_group_size = Group.size(self.id)
+    (curr_group_size >= max_group_members)
+  end
+
+  # Add memeber to the group
+  def add_member(user, _assignment_id)
+    if has_user(user)
+      raise "The user \"" + user.name + "\" is already a member of the group, \"" + self.name + "\""
+    end
+
+    if can_add_member = !full?
+      t_user = GroupsUser.create(user_id: user.id, group_id: self.id)
+      add_participant(self.parent_id, user)
+    end
+
+    can_add_member
+  end
+
+  # Define the size of the group
+  def self.size(group_id)
+    GroupsUser.where(["group_id = ?", group_id]).count
+  end
+
+  # Copy method to copy this group
+  def copy_members(new_group)
+    members = GroupsUser.where(group_id: self.id)
+    members.each do |member|
+      t_user = GroupsUser.create(group_id: new_group.id, user_id: member.user_id)
+      parent = Object.const_get(self.parent_model).find(self.parent_id)
+    end
+  end
+
+  # Algorithm
+  # Start by adding single members to groups that are one member too small.
+  # Add two-member groups to groups that two members too small. etc.
+  def self.randomize_all_by_parent(parent, group_type, min_group_size)
+    participants = Participant.where(["parent_id = ?", parent.id + "Participant"])
+    participants = participants.sort { rand(3) - 1 }
+    users = participants.map {|p| User.find(p.user_id) }.to_a
+    # find groups still need group members and users who are not in any group
+    groups = Group.where(parent_id: parent.id).to_a
+    groups_num = groups.size
+    i = 0
+    groups_num.times do
+      groups_users = GroupsUser.where(group_id: groups[i].id)
+      groups_users.each do |groups_user|
+        users.delete(User.find(groups_user.user_id))
+      end
+      if Group.size(groups.first.id) >= min_group_size
+        groups.delete(groups.first)
+      else
+        i += 1
+      end
+    end
+    # sort groups by decreasing group size
+    groups.sort_by {|group| Group.size(group.id) }.reverse!
+    # insert users who are not in any group to groups still need group members
+    if !users.empty? and !groups.empty?
+      groups.each do |group|
+        curr_group_size = Group.size(group.id)
+        member_num_difference = min_group_size - curr_group_size
+        for i in (1..member_num_difference).to_a
+          group.add_member(users.first, parent.id)
+          users.delete(users.first)
+          break if users.empty?
+        end
+        break if users.empty?
+      end
+    end
+    # If all the existing groups are fill to the min_group_size and we still have more users, create groups for them.
+    if !users.empty?
+      num_of_groups = users.length.fdiv(min_group_size).ceil
+      nextGroupMemberIndex = 0
+      for i in (1..num_of_groups).to_a
+        group = Object.const_get(group_type + 'Group').create(name: "Group" + i.to_s, parent_id: parent.id)
+        # GroupNode.create(parent_id: parent.id, node_object_id: group.id)
+        min_group_size.times do
+          break if nextGroupMemberIndex >= users.length
+          user = users[nextGroupMemberIndex]
+          group.add_member(user, parent.id)
+          nextGroupMemberIndex += 1
+        end
+      end
+    end
+  end
+
+  # Generate the group name
+  def self.generate_group_name(groupnameprefix)
+    counter = 1
+    while true
+      groupname = groupnameprefix + "_Group#{counter}"
+      return groupname if !Group.find_by_name(groupname)
+      counter += 1
+    end
+  end
+
+  # Extract group members from the csv and push to DB
+  def import_group_members(starting_index, row)
+    index = starting_index
+    while index < row.length
+      user = User.find_by_name(row[index].to_s.strip)
+      if user.nil?
+        raise ImportError, "The user \"" + row[index].to_s.strip + "\" was not found. <a href='/users/new'>Create</a> this user?"
+      else
+        if GroupsUser.where(["group_id =? and user_id =?", id, user.id]).first.nil?
+          add_member(user, nil)
+        end
+      end
+      index += 1
+    end
+  end
+
+
 end

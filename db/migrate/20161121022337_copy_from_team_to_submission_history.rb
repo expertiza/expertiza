e2472e2912e30c362ed52ad55096ad11f35a8499
Prerit Bhandari <preritbhandari@gmail.com>
2016-11-26 13:01:03 -0500

    Incomplete chnages

--- db/migrate/20161121022337_copy_from_team_to_submission_history.rb
+++ db/migrate/20161121022337_copy_from_team_to_submission_history.rb
@@ -1,4 +1,13 @@
 class CopyFromTeamToSubmissionHistory < ActiveRecord::Migration
   def change
   end
+  def up
+    Team.for_each do |t|
+      SubmissionHistory.create()
+      sh.team_id
+    end
+  end
+  def down
+
+  end
 end

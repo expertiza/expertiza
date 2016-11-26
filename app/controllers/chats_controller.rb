1f35f336c89cfcab733fbce2b5aff6efb52a2a00
saratkavuru <kavurusarat@gmail.com>
2016-11-26 18:41:42 -0500

    controllers for messages and chats

---
+++ app/controllers/chats_controller.rb
@@ -0,0 +1,2 @@
+class ChatsController < ApplicationController
+end
---
+++ test/controllers/messages_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MessagesControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end

5c896da9b9aaa702afac88df2c3f4783339f6b16
abhinand-lingareddy <abhinand.lingareddy@gmail.com>
2016-11-26 18:47:47 -0500

    added messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,27 @@
 class MessagesController < ApplicationController
+  respond_to :html, :js
+
+  def index
+    @chat = Chat.find(params[:chat_id])
+    @messages = @chat.messages
+    @new_message = @chat.messages.build
+  end
+
+  def create
+    @chat = Chat.find(params[:chat_id])
+    @message = @chat.messages.build(message_params)
+    @message.user=current_user
+
+    if @message.save
+      sync_new @message , scope: Message.by_chat(@chat)
+    end
+
+    respond_with { @message }
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body,:user)
+  end
 end

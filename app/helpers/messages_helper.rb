5c90cb00a4a0c33ae48cec387656736a21e6387e
saratkavuru <kavurusarat@gmail.com>
2016-11-28 14:39:37 -0500

    Added another helper method to figure out reviewee/reviewer

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -2,4 +2,8 @@ module MessagesHelper
 def self_or_other(message)
     message.user == current_user ? "self" : "other"
   end
+def reviewee_or_reviewer(message)
+	response = ReviewResponseMap.find(message.chat.review_response_map_id)
+	message.user ==response.reviewer.user ? "Reviewer" : "Reviewee"
+end
 end

390e811253cfcfc5603464189000c9186ad39bb2
saratkavuru <kavurusarat@gmail.com>
2016-11-28 04:40:50 -0500

    Added model validation to prevent blank messages.

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,5 @@ class Message < ActiveRecord::Base
   belongs_to :user
   sync :all
   sync_scope :by_chat, ->(chat) { where(chat_id: chat.id) }
-
+  validates_presence_of :body, :chat_id, :user_id
 end

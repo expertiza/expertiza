f8e01535363072ae1af32672e31f6916dc70fffb
bhaveshkasliwal30 <bkasliw@ncsu.edu>
2016-11-28 16:14:14 -0500

    Groups adding node

---
+++ app/models/group_node.rb
@@ -0,0 +1,25 @@
+class GroupNode < Node
+  belongs_to :node_object, class_name: 'Group'
+  attr_accessible :parent_id, :node_object_id
+  def self.table
+    "groups"
+  end
+
+  def self.get(parent_id)
+    query = "select nodes.* from nodes, " + self.table
+    query = query + " where nodes.node_object_id = " + self.table + ".id"
+    query = query + " and nodes.type = '" + self.to_s + "'"
+    if parent_id
+      query = query + " and " + self.table + ".parent_id = " + parent_id.to_s
+    end
+    find_by_sql(query)
+  end
+
+  def get_name
+    Group.find(self.node_object_id).name
+  end
+
+  def get_children(_sortvar = nil, _sortorder = nil, _user_id = nil, _parent_id = nil, _search = nil)
+    GroupUserNode.get(self.node_object_id)
+  end
+end

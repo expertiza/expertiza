4eae9b7096be2256155e6e1f74f85a54b15f5373
Tanmay goel <goeltanmay@gmail.com>
2016-11-28 16:54:11 -0500

    Include

--- app/models/link_submission_history.rb
+++ app/models/link_submission_history.rb
@@ -2,11 +2,11 @@ class LinkSubmissionHistory < SubmissionHistory
 
   def self.create(link, team, action)
     # lets assume link of type http://docs.google.com  or   http://wiki.expertiza.ncsu.edu  or http://github.com/goeltanmay/
-    if link.includes? "docs.google.com"
+    if link.include? "docs.google.com"
       history_obj = GoogledocSubmissionHistory.create(link, team, action)
-    elsif link.includes? "wiki.expertiza.ncsu.edu"
+    elsif link.include? "wiki.expertiza.ncsu.edu"
       history_obj = WikipediaSubmissionHistory.create(link, team, action)
-    elsif link.includes? "github.com"
+    elsif link.include? "github.com"
       history_obj = GithubSubmissionHistory.create(link, team, action)
     else
       history_obj = LinkSubmissionHistory.new

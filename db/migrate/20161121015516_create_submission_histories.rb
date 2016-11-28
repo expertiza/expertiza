8454162b9d66eb48b9d5fe8346a1b10b4fc30259
Tanmay goel <goeltanmay@gmail.com>
2016-11-28 17:05:10 -0500

    Submitted detail needed more space on db

--- db/migrate/20161121015516_create_submission_histories.rb
+++ db/migrate/20161121015516_create_submission_histories.rb
@@ -1,9 +1,9 @@
 class CreateSubmissionHistories < ActiveRecord::Migration
   def change
     create_table :submission_histories do |t|
-      t.string :submitted_detail
+      t.text :submitted_detail
       t.datetime :submitted_at
-       t.string :type
+      t.string :type
       t.string :action
       t.references :team, index: true
       t.timestamps null: false

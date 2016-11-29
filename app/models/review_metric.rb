f0841f80141832a82fd9b3a0a46c7517b7016faf
Lahari Kommi <lkommi@ncsu.edu>
2016-11-28 22:03:05 -0500

    Changed the type from integer to boolean

--- app/models/review_metric.rb
+++ app/models/review_metric.rb
@@ -1,8 +1,8 @@
 class ReviewMetric < ActiveRecord::Base
 	belongs_to :review_response_map, class_name: 'ReviewResponseMap', foreign_key: 'response_id'
     attr_accessor :volume, :integer
-    attr_accessor :suggestion, :integer
-    attr_accessor :problem, :integer
-    attr_accessor :offensive_term, :integer
+    attr_accessor :suggestion, :boolean
+    attr_accessor :problem, :boolean
+    attr_accessor :offensive_term, :boolean
 
 end

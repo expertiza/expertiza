6256ec4ea7da3a4b5b6d12909cbb578643f73d99
rishabh2693 <rishab2693@gmail.com>
2016-11-30 12:18:04 -0500

    Added delete functionalities

--- app/controllers/groups_users_controller.rb
+++ app/controllers/groups_users_controller.rb
@@ -1,58 +1,83 @@
 class GroupsUsersController < ApplicationController
-  before_action :set_groups_user, only: [:show, :edit, :update, :destroy]
-
-  # GET /groups_users
-  def index
-    @groups_users = GroupsUser.all
+  def action_allowed?
+    ['Instructor',
+     'Teaching Assistant',
+     'Administrator'].include? current_role_name
   end
 
-  # GET /groups_users/1
-  def show
+  def auto_complete_for_user_name
+    group = Group.find(session[:group_id])
+    @users = group.get_possible_group_members(params[:user][:name])
+    render inline: "<%= auto_complete_result @users, 'name' %>", layout: false
   end
 
-  # GET /groups_users/new
+  def list
+    @group = Group.find(params[:id])
+    @assignment = Assignment.find(@group.assignment_id)
+    @groups_users = GroupsUser.page(params[:page]).per_page(10).where(["group_id = ?", params[:id]])
+  end
+
   def new
-    @groups_user = GroupsUser.new
+    @group = Group.find(params[:id])
   end
 
-  # GET /groups_users/1/edit
-  def edit
-  end
-
-  # POST /groups_users
   def create
-    @groups_user = GroupsUser.new(groups_user_params)
-
-    if @groups_user.save
-      redirect_to @groups_user, notice: 'Groups user was successfully created.'
-    else
-      render :new
+    user = User.find_by_name(params[:user][:name].strip)
+    unless user
+      urlCreate = url_for controller: 'users', action: 'new'
+      flash[:error] = "\"#{params[:user][:name].strip}\" is not defined. Please <a href=\"#{urlCreate}\">create</a> this user before continuing."
     end
+
+    group = Group.find(params[:id])
+
+    if group.is_a?(AssignmentGroup)
+      assignment = Assignment.find(group.parent_id)
+      if AssignmentParticipant.find_by_user_id_and_assignment_id(user.id, assignment.id).nil?
+        urlAssignmentParticipantList = url_for controller: 'participants', action: 'list', id: assignment.id, model: 'Assignment', authorization: 'participant'
+        flash[:error] = "\"#{user.name}\" is not a participant of the current assignment. Please <a href=\"#{urlAssignmentParticipantList}\">add</a> this user before continuing."
+      else
+        add_member_return = group.add_member(user, group.parent_id)
+        if add_member_return == false
+          flash[:error] = "This group already has the maximum number of members."
+        end
+
+        @groups_user = GroupsUser.last
+        undo_link("The group user \"#{user.name}\" has been successfully added to \"#{group.name}\".")
+      end
+    else #CourseTeam
+      course = Course.find(group.parent_id)
+      if CourseParticipant.find_by_user_id_and_parent_id(user.id, course.id).nil?
+        urlCourseParticipantList = url_for controller: 'participants', action: 'list', id: course.id, model: 'Course', authorization: 'participant'
+        flash[:error] = "\"#{user.name}\" is not a participant of the current course. Please <a href=\"#{urlCourseParticipantList}\">add</a> this user before continuing."
+      else
+        add_member_return = group.add_member(user)
+        if add_member_return == false
+          flash[:error] = "This group already has the maximum number of members."
+        end
+
+        @groups_user = GroupsUser.last
+        undo_link("The group user \"#{user.name}\" has been successfully added to \"#{group.name}\".")
+      end
+    end
+
+    redirect_to controller: 'groups', action: 'list', id: group.parent_id
   end
 
-  # PATCH/PUT /groups_users/1
-  def update
-    if @groups_user.update(groups_user_params)
-      redirect_to @groups_user, notice: 'Groups user was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # DELETE /groups_users/1
-  def destroy
+  def delete
+    @groups_user = GroupsUser.find(params[:id])
+    parent_id = Group.find(@groups_user.group_id).parent_id
+    @user = User.find(@groups_user.user_id)
     @groups_user.destroy
-    redirect_to groups_users_url, notice: 'Groups user was successfully destroyed.'
+    undo_link("The group user \"#{@user.name}\" has been successfully removed. ")
+    redirect_to controller: 'groups', action: 'list', id: parent_id
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_groups_user
-      @groups_user = GroupsUser.find(params[:id])
+  def delete_selected
+    params[:item].each do |item_id|
+      group_user = GroupsUser.find(item_id).first
+      group_user.destroy
     end
 
-    # Only allow a trusted parameter "white list" through.
-    def groups_user_params
-      params.require(:groups_user).permit(:group_id, :user_id)
-    end
+    redirect_to action: 'list', id: params[:id]
+  end
 end

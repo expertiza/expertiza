07e8ad9b5a6300fbbd70abd28e7fe48b31b9df8f
akashag26 <akashag26@gmail.com>
2016-11-30 19:53:33 -0500

    New Views and Controller

---
+++ app/controllers/class_performance_controller.rb
@@ -0,0 +1,7 @@
+class ClassPerformanceController < ApplicationController
+  def select_rubrics
+  end
+
+  def show_class_performance
+  end
+end
---
+++ test/controllers/class_performance_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ClassPerformanceControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end

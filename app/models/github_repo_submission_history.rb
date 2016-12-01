c08d85d48da2ebc51ae42c56c1994accec6b9252
Prerit Bhandari <preritbhandari@gmail.com>
2016-12-01 18:19:01 -0500

    Added api call for getting timestamp of github repo

--- app/models/github_repo_submission_history.rb
+++ app/models/github_repo_submission_history.rb
@@ -5,4 +5,13 @@ class GithubRepoSubmissionHistory < GithubSubmissionHistory
     history_obj.team = team
     history_obj.action = action
   end
+
+  def get_submitted_at_time(link)
+    uri = URI(link)
+    link_path = uri.path
+    git_user_details = link_path.split("/")
+    github = Github.new
+    a = github.repos git_user_details[1], git_user_details[2]
+    return a.pushed_at
+  end
 end

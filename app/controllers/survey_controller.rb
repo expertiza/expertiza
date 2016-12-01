fbf53af5e236f922800f3d44e98c4e48a26e978c
DJaySathe <dssathe@ncsu.edu>
2016-11-30 22:49:10 -0500

    changed get_surveys to assign and removed old code

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -5,58 +5,8 @@ class SurveyController < ApplicationController
      'Administrator'].include? current_role_name
   end
   #E1680. Improve survey functionality commit by dssathe
-  def get_surveys
+  def assign
     @my_surveys=Questionnaire.where([instructor_id = ?", session[:user].id])
     @global_surveys=Questionnaire.all;
-  end
-  
-  def assign
-    @assignment = Assignment.find(params[:id])
-    @assigned_surveys = SurveyHelper.get_assigned_surveys(@assignment.id)
-    @surveys = []
-
-    @surveys = if params['subset'] == "mine"
-                 Questionnaire.where(["type_id = 2 and instructor_id = ?", session[:user].id])
-               elsif params['subset'] == "public"
-                 Questionnaire.where(["type_id = 2 and private = 0"])
-               else
-                 @assigned_surveys
-               end
-
-    if params['update']
-      if params[:surveys]
-        @checked = params[:surveys]
-
-        @submit_surveys = if params['submit_subset'] == "mine"
-                            Questionnaire.where(["type_id = 2 and instructor_id = ?", session[:user].id])
-                          elsif params['submit_subset'] == "public"
-                            Questionnaire.where(["type_id = 2 and private = 0"])
-                          else
-                            @assigned_surveys
-                          end
-
-        for survey in @submit_surveys
-          unless @checked.include? survey.id
-            AssignmentQuestionnaire.delete_all(["questionnaire_id = ? and assignment_id = ?", survey.id, @assignment.id])
-            @assigned_surveys.delete(survey)
-          end
-        end
-
-        for checked_survey in @checked
-          @current = Questionnaire.find(checked_survey)
-          next if @assigned_surveys.include? @current
-          @new = AssignmentQuestionnaire.new(questionnaire_id: checked_survey, assignment_id: @assignment.id)
-          @new.save
-          @assigned_surveys << @current
-        end
-      else
-        for survey in @submit_surveys
-          AssignmentQuestionnaire.delete_all(["questionnaire_id = ? and assignment_id = ?", survey.id, @assignment.id])
-          @assigned_surveys.delete(survey)
-          @surveys.delete(survey)
-        end
-      end
-    end
-    @surveys.sort! {|a, b| a.name <=> b.name }
   end
 end

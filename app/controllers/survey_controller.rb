50c15324768f5a4d6bfa962c3abbfcaf1de1dc18
pranavnawathe <pranav.nawathe92@gmail.com>
2016-12-01 01:10:44 -0500

    trying redirection

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -25,6 +25,6 @@ class SurveyController < ApplicationController
     # get the questionnaire object from the id
     @selected_survey_questionnaire = Questionnaire.find(selected_survey_id)
     
-    redirect_to survey_path
+    redirect_to assign_path
   end
 end

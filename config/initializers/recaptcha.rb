dc19e57318354c1f581e31f595677dbedeed6d67
Vivek Bhat <vivekbhat@live.com>
2016-11-30 21:45:33 -0500

    Recaptcha added

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,7 @@
+# config/initializers/recaptcha.rb
+Recaptcha.configure do |config|
+  config.site_key  = '6LdQig0UAAAAAHx0HjYG_QHY9hsGQMiA0if1UDnd'
+  config.secret_key = '6LdQig0UAAAAAGFKGgQAf5YDHyacRxFUC8whtwCo'
+  # Uncomment the following line if you are using a proxy server:
+  # config.proxy = 'http://myproxy.com.au:8080'
+end

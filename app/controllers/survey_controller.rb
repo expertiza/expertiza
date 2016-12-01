6299cf5503eb60252d99080ba39438fb2642e6aa
DJaySathe <dssathe@ncsu.edu>
2016-12-01 00:23:44 -0500

    Update survey_controller.rb

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -8,7 +8,10 @@ class SurveyController < ApplicationController
   #E1680. Improve survey functionality commit by dssathe
   def assign
     @assignment = Assignment.find(params[:id])
+    @first_row=Questionnaire.new;
+    @first_row.name="Select a Global Survey";
     @my_surveys=Questionnaire.where(["instructor_id=? and type = 'SurveyQuestionnaire'", session[:user].id]);
-    @global_surveys=Questionnaire.where(["type = 'GlobalSurveyQuestionnaire'"]);
+    @global_surveys << @first_row
+    @global_surveys << Questionnaire.where(["type = 'GlobalSurveyQuestionnaire'"]);
   end
 end

abb42b6f6141fdcc23484f89cc3656c008bf1919
Kunal Kapoor <kunalkapoor.com@gmail.com>
2016-12-01 23:29:33 -0500

    added gruff resources

--- app/controllers/class_performance_controller.rb
+++ app/controllers/class_performance_controller.rb
@@ -1,3 +1,5 @@
+require 'gruff'
+
 class ClassPerformanceController < ApplicationController
   def action_allowed?
     true
@@ -48,5 +50,16 @@ class ClassPerformanceController < ApplicationController
     puts params[:class_performance][:criterias]
     puts params[:result]
     puts params[:id]
+
+    line_chart = Gruff::Bar.new()
+    line_chart.labels = {0=>'Value (USD)'}
+    line_chart.title = "My Guitar Collection"
+
+    {"'70 Strat"=>2500, 
+     "'69 Tele"=>2000,
+     "'02 Modded Mexi Strat Squier"=>400}.each do |guitar, value| 
+        line_chart.data(guitar, value )
+    end
+    line_chart.write("app/assets/images/class_performance/chart.png")
   end
 end

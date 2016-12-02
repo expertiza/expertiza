b6b72f2f16d61e522dff78aad9e9f8ef2f1fbb1e
Tanmay goel <goeltanmay@gmail.com>
2016-12-01 23:28:30 -0500

    Bugfix

--- app/models/github_submission_history.rb
+++ app/models/github_submission_history.rb
@@ -3,9 +3,8 @@ class GithubSubmissionHistory < LinkSubmissionHistory
     if link.include? "pull"
       history_obj = GithubPullRequestSubmissionHistory.create(link, team, action)
     else
-      history_objGithubRepoSubmissionHistory.create(link, team, action)
+      history_obj = GithubRepoSubmissionHistory.create(link, team, action)
     return history_obj
   end
   end
 end
-

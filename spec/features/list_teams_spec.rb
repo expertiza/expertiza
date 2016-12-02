b75700c5e56b8dc74ae571a92092e051b73d8293
zli36 <zli36@ncsu.edu>
2016-12-01 19:59:29 -0500

    Update list_teams_spec.rb

--- spec/features/list_teams_spec.rb
+++ spec/features/list_teams_spec.rb
@@ -3,15 +3,13 @@ require 'rails_helper'
 describe 'List Team' do
 
   it 'should list all team nodes' do
-   # create(:assignment,name:"List_team")
-   # @list1=Assignment.find_by(name: 'List_team')
-   # create(:assignment_node,node_object_id: @list1.id)
-   # assignment_team = create(:assignment_team,name:'List_team1',assignment:Assignment.find_by(name:'List_team'))
-   # team_user = create(:team_user,User.where(role_id: 2).first)
+    create(:assignment)
+    create(:assignment_node)
+    assignment_team = create(:assignment_team)
+    team_user = create(:team_user)
 
     login_as("instructor6")
-    @list_team=Assignment.find_by(name:'List_team')
-    visit "/teams/list?id=#{@list_team.id}&type=Assignment"
+    visit '/teams/list?id=1&type=Assignment'
 
     page.all('#theTable tr').each do |tr|
       expect(tr).to have_content?(assignment_team.name)

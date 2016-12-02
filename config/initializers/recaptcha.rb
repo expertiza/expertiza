2cf0c484671bda67e6dabb4f0d45420d3996f128
Srikar Potta <spotta@ncsu.edu>
2016-12-01 19:30:45 -0500

    Working captcha

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,7 @@
+# config/initializers/recaptcha.rb
+Recaptcha.configure do |config|
+  config.site_key  = '6LenZg0UAAAAAJY0aRjqylrMluyC6q3GPM2z3cvT'
+  config.secret_key = '6LenZg0UAAAAAP16CgdaxIy-kTFpJWKKSG_fXHAA'
+  # Uncomment the following line if you are using a proxy server:
+  # config.proxy = 'http://myproxy.com.au:8080'
+end

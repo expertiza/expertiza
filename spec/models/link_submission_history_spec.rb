bbd14081079995ab96d54137faad6db9e1b775bf
Tanmay goel <goeltanmay@gmail.com>
2016-12-02 20:44:42 -0500

    New expectation

--- spec/models/link_submission_history_spec.rb
+++ spec/models/link_submission_history_spec.rb
@@ -9,7 +9,7 @@ describe LinkSubmissionHistory do
       assignment_team.parent_id = assignment.id
       assignment_team.submit_hyperlink("https://github.com/prerit2803/expertiza")
       assignment_team.save
-      expect_any_instance_of(SubmissionHistory).to receive(:get_submitted_at_time)
+      expect_any_instance_of(GithubRepoSubmissionHistory).to receive(:get_submitted_at_time)
       LinkSubmissionHistory.add_submission(assignment.id)
     end
   end

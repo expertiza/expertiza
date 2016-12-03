1a9e83d4b0d496e2e0baece899b00bba23a7b640
navisingh14 <navjotgogna@gmail.com>
2016-12-02 20:24:17 -0500

    changed formatting

--- spec/controllers/response_time_controller_spec.rb
+++ spec/controllers/response_time_controller_spec.rb
@@ -22,28 +22,27 @@ context 'checking that the user creates valid post request' do
   end
 
   describe 'POST #record_start_time' do
+    it 'returns with an HTTP status of 200' do
 
-  it 'returns with an HTTP status of 200' do
+      allow(ResponseTime).to receive(:where).and_return(responsetime)
+      allow(responsetime).to receive(:each).and_return(1)
+      allow_any_instance_of(ResponseTime).to receive_message_chain(:new).and_return(1)
+      allow(responsetime).to receive(:save).and_return(true)
 
-    allow(ResponseTime).to receive(:where).and_return(responsetime)
-    allow(responsetime).to receive(:each).and_return(1)
-    allow_any_instance_of(ResponseTime).to receive_message_chain(:new).and_return(1)
-    allow(responsetime).to receive(:save).and_return(true)
+      post "record_start_time", :response_time => {map_id: 1, round: 1, link: 'hello' , start: date3}
+      expect(response).to have_http_status(200)
 
-    post "record_start_time", :response_time => {map_id: 1, round: 1, link: 'hello' , start: date3}
-    expect(response).to have_http_status(200)
+    end
 
-  end
 
   describe 'POST #record_end_time', js: true do
+    it 'respond with an HTTP status of 200'  do
+      allow(ResponseTime).to receive(:where).and_return(response_time)
+      allow(response_time).to receive(:each).and_return(1)
+      post "record_end_time", :response_time => {map_id: 2, round: 2, end: date3}, format: :json
+      expect(response).to have_http_status(200)
 
-  it 'respond with an HTTP status of 200'  do
-    allow(ResponseTime).to receive(:where).and_return(response_time)
-    allow(response_time).to receive(:each).and_return(1)
-    post "record_end_time", :response_time => {map_id: 2, round: 2, end: date3}, format: :json
-    expect(response).to have_http_status(200)
-
-  end
+    end
   end
 
 end

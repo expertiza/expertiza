5a803bd58871b89ef4f43909964c3b15dd376f39
Tanmay goel <goeltanmay@gmail.com>
2016-12-02 20:29:25 -0500

    try

--- spec/models/link_submission_history_spec.rb
+++ spec/models/link_submission_history_spec.rb
@@ -9,16 +9,16 @@ describe LinkSubmissionHistory do
       assignment_team.assignment = assignment
       assignment_team.submit_hyperlink("https://github.com/prerit2803/expertiza")
       assignment_team.save
-      # puts assignment_team.hyperlinks
-      # puts assignment_team.parent_id
-      # # puts assignment_team.assignment.id
-      # expect(LinkSubmissionHistory).to receive(:add_submission)
-      # expect_any_instance_of(AssignmentTeam).to receive(:hyperlinks)
-      # expect(SubmissionHistory).to receive(:create)
-      # expect(LinkSubmissionHistory).to receive(:create)
-      # expect(GithubSubmissionHistory).to receive(:create)
-      # expect(GithubRepoSubmissionHistory).to receive(:create)
-      # expect_any_instance_of(SubmissionHistory).to receive(:get_submitted_at_time)
+      puts assignment_team.hyperlinks
+      puts assignment_team.parent_id
+      # puts assignment_team.assignment.id
+      expect(LinkSubmissionHistory).to receive(:add_submission)
+      expect_any_instance_of(AssignmentTeam).to receive(:hyperlinks)
+      expect(SubmissionHistory).to receive(:create)
+      expect(LinkSubmissionHistory).to receive(:create)
+      expect(GithubSubmissionHistory).to receive(:create)
+      expect(GithubRepoSubmissionHistory).to receive(:create)
+      expect_any_instance_of(SubmissionHistory).to receive(:get_submitted_at_time)
       LinkSubmissionHistory.add_submission(assignment.id)
       puts SubmissionHistory.all.size
     end

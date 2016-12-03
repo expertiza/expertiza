a122c170bc6b5cd75fe9f68978780f022588a730
aanilku <aanilku@ncsu.edu>
2016-12-02 22:43:22 -0500

    corrected loger object name

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -22,7 +22,7 @@ class AuthController < ApplicationController
       if user and user.valid_password?(params[:login][:password])
         after_login(user)
       else
-        logger.warn "Failed login attempt by #{session[:user_id]}."
+        @@event_logger.warn "Failed login attempt by #{session[:user_id]}."
         flash[:error] = "Your username or password is incorrect."
         redirect_to controller: 'password_retrieval', action: 'forgotten'
       end
@@ -58,7 +58,7 @@ class AuthController < ApplicationController
   end
 
   def logout
-    logger.warn "Logout attempt by #{session[:user_id]}."
+    @@event_logger.warn "Logout attempt by #{session[:user_id]}."
     AuthController.logout(session)
     
     redirect_to '/'

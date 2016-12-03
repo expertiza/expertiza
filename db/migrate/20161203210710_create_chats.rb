b71c14c8e5d2c562f7d994faa57305c90de62caf
saratkavuru <kavurusarat@gmail.com>
2016-12-03 16:11:06 -0500

    New migration files

--- db/migrate/20161203210710_create_chats.rb
+++ db/migrate/20161203210710_create_chats.rb
@@ -1,7 +1,7 @@
 class CreateChats < ActiveRecord::Migration
   def change
     create_table :chats do |t|
-
+    	
       t.integer :assignment_team_id, :null => false,:references=>[:teams, :id]
 
       t.timestamps null: false
--- db/migrate/20161203210928_create_messages.rb
+++ db/migrate/20161203210928_create_messages.rb
@@ -1,7 +1,7 @@
 class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
-      
+
       t.text :body 
 
       t.references :chat, index: true, foreign_key: true

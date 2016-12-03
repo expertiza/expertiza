7ef846dc5b178d5d1040343001441cb31521a7c4
Tanmay goel <goeltanmay@gmail.com>
2016-12-02 21:28:40 -0500

    Undoing changes for travis cl

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,9 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema.define(version: 20161121015516) do
+
   create_table "answers", force: :cascade do |t|
     t.integer "question_id", limit: 4,     default: 0, null: false
     t.integer "answer",      limit: 4

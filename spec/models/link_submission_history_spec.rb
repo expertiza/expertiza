f683839100856376ba58b0ce70c8fd29e6856500
Tanmay goel <goeltanmay@gmail.com>
2016-12-02 20:41:15 -0500

    good test

--- spec/models/link_submission_history_spec.rb
+++ spec/models/link_submission_history_spec.rb
@@ -9,14 +9,8 @@ describe LinkSubmissionHistory do
       assignment_team.parent_id = assignment.id
       assignment_team.submit_hyperlink("https://github.com/prerit2803/expertiza")
       assignment_team.save
-      puts assignment.id
-      puts assignment_team.parent_id
-      puts assignment_team.hyperlinks
-      puts assignment.teams[0].hyperlinks
-      # puts assignment_team.assignment.id
       expect_any_instance_of(SubmissionHistory).to receive(:get_submitted_at_time)
       LinkSubmissionHistory.add_submission(assignment.id)
-      puts SubmissionHistory.all.size
     end
   end
 end

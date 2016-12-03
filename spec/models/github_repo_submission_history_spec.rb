becb085ead1f5be237715dfb4ee6ee4e95d17324
Tanmay goel <goeltanmay@gmail.com>
2016-12-02 22:14:02 -0500

    tests

--- spec/models/github_repo_submission_history_spec.rb
+++ spec/models/github_repo_submission_history_spec.rb
@@ -3,7 +3,9 @@ require "rails_helper"
 describe GithubRepoSubmissionHistory do
   describe "get_submitted_at_time" do
     it "should return the last time a github repo was pushed" do
-
+      history_object = GithubRepoSubmissionHistory.new
+      time_stamp = history_object.get_submitted_at_time("https://github.com/prerit2803/expertiza/")
+      expect(time_stamp).not_to be_empty
     end
   end
 end

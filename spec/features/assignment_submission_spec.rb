0a122d053626bd4d47bd4e46ce9d34cb6c9e7e3f
popoosl <flypopoo@gmail.com>
2016-12-03 17:54:27 -0500

    test CI

--- spec/features/assignment_submission_spec.rb
+++ spec/features/assignment_submission_spec.rb
@@ -28,20 +28,19 @@ describe "assignment submisstion test" do
     click_link "Your work"
   end
 
-#  describe "submit hyperlink" do
-    it "is able to submit a single valid link" do
-      signup_topic
-      fill_in 'submission', with: "https://www.ncsu.edu"
-      click_on 'Upload link'
-      expect(page).to have_content "https://www.ncsu.edu"
-      #open the link and check content
-      ##click_on "https://www.ncsu.edu"
-      ##expect(page).to have_http_status(200)
-      #new_window = page.driver.browser.window_handles.last
-      #page.within_window new_window do
-        #expect(page).to have_content "NC STATE NEWS"
-      #end
-    end
+  it "is able to submit a single valid link" do
+    signup_topic
+    fill_in 'submission', with: "https://www.ncsu.edu"
+    click_on 'Upload link'
+    expect(page).to have_content "https://www.ncsu.edu"
+    #open the link and check content
+    click_on "https://www.ncsu.edu"
+    expect(page).to have_http_status(200)
+    #new_window = page.driver.browser.window_handles.last
+    #page.within_window new_window do
+      #expect(page).to have_content "NC STATE NEWS"
+    #end
+  end
 =begin
     it "should not submit invalid link" do
       signup_topic
@@ -61,105 +60,100 @@ describe "assignment submisstion test" do
       expect(page).to have_content "The URL or URI is not valid"
 
     end
-
-    it "is able to submit multiple valid links" do
-      signup_topic
-      fill_in 'submission', with: "https://www.ncsu.edu"
-      click_on 'Upload link'
-      fill_in 'submission', with: "https://www.google.com"
-      click_on 'Upload link'
-      fill_in 'submission', with: "https://bing.com"
-      click_on 'Upload link'
-      expect(page).to have_content "https://www.ncsu.edu"
-      expect(page).to have_content "https://www.google.com"
-      expect(page).to have_content "https://bing.com"
-    end
-
-    it "should not submit duplicated link" do
-      signup_topic
-      fill_in 'submission', with: "https://google.com"
-      click_on 'Upload link'
-      expect(page).to have_content "https://google.com"
-      fill_in 'submission', with: "https://google.com"
-      click_on 'Upload link'
-      expect(page).to have_content "You or your teammate(s) have already submitted the same hyperlink."
-    end
-
-    it "submit empty link" do
-      signup_topic
-      #hyperlink is empty
-      fill_in 'submission', with: ""
-      click_on 'Upload link'
-      expect(page).to have_content "The URL or URI is not valid. Reason: The hyperlink cannot be empty!"
-      #hyperlink is "http://"
-      fill_in 'submission', with: "http://"
-      click_on 'Upload link'
-      expect(page).to have_content "The URL or URI is not valid. Reason: bad URI(absolute but no path): http://"
-    end
-
+=end
+  it "is able to submit multiple valid links" do
+    signup_topic
+    fill_in 'submission', with: "https://www.ncsu.edu"
+    click_on 'Upload link'
+    fill_in 'submission', with: "https://www.google.com"
+    click_on 'Upload link'
+    fill_in 'submission', with: "https://bing.com"
+    click_on 'Upload link'
+    expect(page).to have_content "https://www.ncsu.edu"
+    expect(page).to have_content "https://www.google.com"
+    expect(page).to have_content "https://bing.com"
   end
 
-  describe "submit file" do
-    it "is able to submit single valid file" do
-      signup_topic
-      file_path = Rails.root + "spec/features/assignment_submission_txts/valid_assignment_file.txt"
-      attach_file('uploaded_file', file_path)
-      click_on 'Upload file'
-      expect(page).to have_content "valid_assignment_file.txt"
+  it "should not submit duplicated link" do
+    signup_topic
+    fill_in 'submission', with: "https://google.com"
+    click_on 'Upload link'
+    expect(page).to have_content "https://google.com"
+    fill_in 'submission', with: "https://google.com"
+    click_on 'Upload link'
+    expect(page).to have_content "You or your teammate(s) have already submitted the same hyperlink."
+  end
 
-      #check content of the uploaded file
-      file_upload_path = Rails.root + "pg_data/instructor6/csc517/test/Assignment1684/0/valid_assignment_file.txt"
-      expect(File).to exist(file_upload_path)
-      expect(File.read(file_upload_path)).to have_content "valid_assignment_file: This is a .txt file to test assignment submission."
-    end
+  it "submit empty link" do
+    signup_topic
+    #hyperlink is empty
+    fill_in 'submission', with: ""
+    click_on 'Upload link'
+    expect(page).to have_content "The URL or URI is not valid. Reason: The hyperlink cannot be empty!"
+    #hyperlink is "http://"
+    fill_in 'submission', with: "http://"
+    click_on 'Upload link'
+    expect(page).to have_content "The URL or URI is not valid. Reason: bad URI(absolute but no path): http://"
+  end
 
-    it "is able to submit multiple valid files" do
-      signup_topic
-      #upload file1
-      file_path = Rails.root + "spec/features/assignment_submission_txts/valid_assignment_file.txt"
-      attach_file('uploaded_file', file_path)
-      click_on 'Upload file'
-      #upload file2
-      file_path = Rails.root + "spec/features/assignment_submission_txts/valid_assignment_file2.txt"
-      attach_file('uploaded_file', file_path)
-      click_on 'Upload file'
-      expect(page).to have_content "valid_assignment_file.txt"
-      expect(page).to have_content "valid_assignment_file2.txt"
+  it "is able to submit single valid file" do
+    signup_topic
+    file_path = Rails.root + "spec/features/assignment_submission_txts/valid_assignment_file.txt"
+    attach_file('uploaded_file', file_path)
+    click_on 'Upload file'
+    expect(page).to have_content "valid_assignment_file.txt"
 
-      #check content of the uploaded files
-      #file1
-      file_upload_path = Rails.root + "pg_data/instructor6/csc517/test/Assignment1684/0/valid_assignment_file.txt"
-      expect(File).to exist(file_upload_path)
-      expect(File.read(file_upload_path)).to have_content "valid_assignment_file: This is a .txt file to test assignment submission."
-      #file2
-      file_upload_path = Rails.root + "pg_data/instructor6/csc517/test/Assignment1684/0/valid_assignment_file2.txt"
-      expect(File).to exist(file_upload_path)
-      expect(File.read(file_upload_path)).to have_content "valid_assignment_file2: This is a .txt file to test assignment submission."
-    end
+    #check content of the uploaded file
+    file_upload_path = Rails.root + "pg_data/instructor6/csc517/test/Assignment1684/0/valid_assignment_file.txt"
+    expect(File).to exist(file_upload_path)
+    expect(File.read(file_upload_path)).to have_content "valid_assignment_file: This is a .txt file to test assignment submission."
+  end
 
-    it "is able to update already uploaded file" do
-      signup_topic
-      #upload file
-      file_path = Rails.root + "spec/features/assignment_submission_txts/valid_assignment_file3.txt"
-      file_write = File.open(file_path, 'w')
-      file_write.puts "This is the original file."
-      file_write.close
-      attach_file('uploaded_file', file_path)
-      click_on 'Upload file'
-      file_upload_path = Rails.root + "pg_data/instructor6/csc517/test/Assignment1684/0/valid_assignment_file3.txt"
-      expect(File).to exist(file_upload_path)
-      expect(File.read(file_upload_path)).to have_content "This is the original file."
+  it "is able to submit multiple valid files" do
+    signup_topic
+    #upload file1
+    file_path = Rails.root + "spec/features/assignment_submission_txts/valid_assignment_file.txt"
+    attach_file('uploaded_file', file_path)
+    click_on 'Upload file'
+    #upload file2
+    file_path = Rails.root + "spec/features/assignment_submission_txts/valid_assignment_file2.txt"
+    attach_file('uploaded_file', file_path)
+    click_on 'Upload file'
+    expect(page).to have_content "valid_assignment_file.txt"
+    expect(page).to have_content "valid_assignment_file2.txt"
 
-      #update file
-      file_write = File.open(file_path, 'w')
-      file_write.puts "This is the updated file."
-      file_write.close
-      attach_file('uploaded_file', file_path)
-      click_on 'Upload file'
-      expect(File).to exist(file_upload_path)
-      expect(File.read(file_upload_path)).to have_content "This is the updated file."
-    end
-=end
-#  end
+    #check content of the uploaded files
+    #file1
+    file_upload_path = Rails.root + "pg_data/instructor6/csc517/test/Assignment1684/0/valid_assignment_file.txt"
+    expect(File).to exist(file_upload_path)
+    expect(File.read(file_upload_path)).to have_content "valid_assignment_file: This is a .txt file to test assignment submission."
+    #file2
+    file_upload_path = Rails.root + "pg_data/instructor6/csc517/test/Assignment1684/0/valid_assignment_file2.txt"
+    expect(File).to exist(file_upload_path)
+    expect(File.read(file_upload_path)).to have_content "valid_assignment_file2: This is a .txt file to test assignment submission."
+  end
+
+  it "is able to update already uploaded file" do
+    signup_topic
+    #upload file
+    file_path = Rails.root + "spec/features/assignment_submission_txts/valid_assignment_file3.txt"
+    file_write = File.open(file_path, 'w')
+    file_write.puts "This is the original file."
+    file_write.close
+    attach_file('uploaded_file', file_path)
+    click_on 'Upload file'
+    file_upload_path = Rails.root + "pg_data/instructor6/csc517/test/Assignment1684/0/valid_assignment_file3.txt"
+    expect(File).to exist(file_upload_path)
+    expect(File.read(file_upload_path)).to have_content "This is the original file."
+
+    #update file
+    file_write = File.open(file_path, 'w')
+    file_write.puts "This is the updated file."
+    file_write.close
+    attach_file('uploaded_file', file_path)
+    click_on 'Upload file'
+    expect(File).to exist(file_upload_path)
+    expect(File.read(file_upload_path)).to have_content "This is the updated file."
+  end
 
 end

5725039f07870c37469bbcf3689b16064246dd76
ssharma25 <ssharm25@ncsu.edu>
2016-12-02 22:53:33 -0500

    Update auth_controller.rb

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -22,7 +22,7 @@ class AuthController < ApplicationController
       if user and user.valid_password?(params[:login][:password])
         after_login(user)
       else
-        @@event_logger.warn "Failed login attempt by #{session[:user.user_id]}."
+        @@event_logger.warn "Failed login attempt by #{session[:user_id]}."
         flash[:error] = "Your username or password is incorrect."
         redirect_to controller: 'password_retrieval', action: 'forgotten'
       end
@@ -32,7 +32,7 @@ class AuthController < ApplicationController
   # function to handle common functionality for conventional user login and google login
   def after_login(user)
     session[:user] = user
-    @@event_logger.warn "user_id: #{session[:user_id]} logged in."
+    @@event_logger.warn "user_id: #{user.user_id} logged in."
     AuthController.set_current_role(user.role_id, session)
     
     redirect_to controller: AuthHelper.get_home_controller(session[:user]),

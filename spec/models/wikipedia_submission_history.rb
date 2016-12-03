33c9b6af3ef1ddf48a3359070bc68d40cd9508f6
Tanmay goel <goeltanmay@gmail.com>
2016-12-02 22:20:29 -0500

    Update of text

--- spec/models/wikipedia_submission_history.rb
+++ spec/models/wikipedia_submission_history.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe WikipediaSubmissionHistory do
   describe "get_submitted_at_time" do
-    it "should return the last time a github pull request was updated" do
+    it "should return the last time a wiki page was updated" do
       history_object = WikipediaSubmissionHistory.new
       time_stamp = history_object.get_submitted_at_time("http://wiki.expertiza.ncsu.edu/index.php/CSC/ECE_517_Fall_2016/oss_E1663")
       expect(time_stamp).not_to be_empty

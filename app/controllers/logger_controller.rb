f0bf5d59e958d6e8869cbf4b6e4b39d7ee5c6f32
samir <sjha4@ncsu.edu>
2016-12-05 01:30:39 -0500

    Adding user_id to logger

--- app/controllers/logger_controller.rb
+++ app/controllers/logger_controller.rb
@@ -7,7 +7,7 @@ class LoggerController < ApplicationController
 
   def view_logs
 
-  	@@event_logger.debug @user_id+"Entered view action in log manager"
+  	@@event_logger.debug "#{current_user} Entered view action in log manager"
   	filePath = "#{Rails.root}/log/events.log"
   	@logArray = Array.new
 

f4e00fb26a8b8730128e289da20b48001659ff26
DJaySathe <dssathe@ncsu.edu>
2016-12-06 13:57:38 -0500

    Migration Updated

--- db/migrate/20161206185054_add_survey_idand_global_survey_i_dto_courses.rb
+++ db/migrate/20161206185054_add_survey_idand_global_survey_i_dto_courses.rb
@@ -1,4 +1,9 @@
 class AddSurveyIdandGlobalSurveyIDtoCourses < ActiveRecord::Migration
-  def change
+  def self.up
+    add_column :assignments, :survey_id, :integer, :null => true
+    add_column :assignments, :global_survey_id, :integer, :null => true
+  end
+
+  def self.down
   end
 end

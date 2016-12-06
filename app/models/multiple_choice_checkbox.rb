187636b91a22f619b69ac135cd9c55881a05e894
Lei Zhang <lzhang45@ncsu.edu>
2016-12-05 23:19:53 -0500

    Refactor edit method of MCR, MCC, TF

--- app/models/multiple_choice_checkbox.rb
+++ app/models/multiple_choice_checkbox.rb
@@ -1,5 +1,31 @@
 class MultipleChoiceCheckbox < QuizQuestion
-  def edit(count)
+  def edit
+    quiz_question_choices = QuizQuestionChoice.where(question_id: self.id)
+
+    html = '<tr><td>'
+    html += '<textarea cols="100" name="question[' + self.id.to_s + '][txt]" '
+    html += 'id="question_' + self.id.to_s + '_txt">' + self.txt + '</textarea>'
+    html += '</td></tr>'
+
+    for i in 0..3
+      html += "<tr><td>"
+
+      html += '<input type="hidden" name="quiz_question_choices[' + self.id.to_s + '][MultipleChoiceCheckbox][' + (i + 1).to_s + '][iscorrect]" '
+      html += 'id="quiz_question_choices_' + self.id.to_s + '_MultipleChoiceCheckbox_' + (i + 1).to_s + '_iscorrect" value="0" />'
+
+      html += '<input type="checkbox" name="quiz_question_choices[' + self.id.to_s + '][MultipleChoiceCheckbox][' + (i + 1).to_s + '][iscorrect]" '
+      html += 'id="quiz_question_choices_' + self.id.to_s + '_MultipleChoiceCheckbox_' + (i + 1).to_s + '_iscorrect" value="1" '
+      html += 'checked="checked" ' if quiz_question_choices[i].iscorrect
+      html += '/>'
+
+      html += '<input type="text" name="quiz_question_choices[' + self.id.to_s + '][MultipleChoiceCheckbox][' + (i + 1).to_s + '][txt]" '
+      html += 'id="quiz_question_choices_' + self.id.to_s + '_MultipleChoiceCheckbox_' + (i + 1).to_s + '_txt" '
+      html += 'value="' + quiz_question_choices[i].txt + '" size="40" />'
+
+      html += '</td></tr>'
+    end
+
+    html.html_safe
   end
 
   def complete
--- app/models/true_false.rb
+++ app/models/true_false.rb
@@ -1,5 +1,27 @@
 class TrueFalse < QuizQuestion
-  def edit(count)
+  def edit
+    quiz_question_choices = QuizQuestionChoice.where(question_id: self.id)
+
+    html = '<tr><td>'
+    html += '<textarea cols="100" name="question[' + self.id.to_s + '][txt]" '
+    html += 'id="question_' + self.id.to_s + '_txt">' + self.txt + '</textarea>'
+    html += '</td></tr>'
+
+    html += '<tr><td>'
+    html += '<input type="radio" name="quiz_question_choices[' + self.id.to_s + '][TrueFalse][1][iscorrect]" '
+    html += 'id="quiz_question_choices_' + self.id.to_s + '_TrueFalse_1_iscorrect_True" value="True" '
+    html += 'checked="checked" ' if quiz_question_choices[0].iscorrect
+    html += '/>True'
+    html += '</td></tr>'
+
+    html += '<tr><td>'
+    html += '<input type="radio" name="quiz_question_choices[' + self.id.to_s + '][TrueFalse][1][iscorrect]" '
+    html += 'id="quiz_question_choices_' + self.id.to_s + '_TrueFalse_1_iscorrect_True" value="False" '
+    html += 'checked="checked" ' if quiz_question_choices[1].iscorrect
+    html += '/>False'
+    html += '</td></tr>'
+
+    html.html_safe
   end
 
   def complete

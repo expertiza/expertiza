88c126f7271b872f57c0783bf4c8e9585fdd0bbd
pranavnawathe <pranav.nawathe92@gmail.com>
2016-12-06 15:08:20 -0500

    Added code to assign survey

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -11,6 +11,14 @@ class SurveyController < ApplicationController
   def assign
     @assignment = Assignment.find(params[:id])
     #Dummy row created which will be selected by default
+
+    # If survey is already assigned to the assignment, 
+    # flash warning to the user that he/she is attempting 
+    # to overwrite it
+    if !@assignment.survey_id.nil?
+      flash[:error] = "You are attempting to overwrite existing assignment survey"
+    end
+
     @first_row=Questionnaire.new;
     @first_row.name="Select a Global Survey";
     @first_row.id=0;
@@ -21,15 +29,27 @@ class SurveyController < ApplicationController
   end
   
   def assign_survey
-     assignment_id = params[:assignment_id]
+    assignment_id = params[:assignment_id]
     # get the survey id from select tag
     selected_survey_id = params[:my_survey]
     # get the questionnaire object from the id
-    @selected_survey_questionnaire = Questionnaire.find(selected_survey_id)
+    # @selected_survey_questionnaire = Questionnaire.find(selected_survey_id)
     
-    #create an entry for the questionnaire in assignment queationnaire table
-    @new = AssignmentQuestionnaire.new(questionnaire_id: selected_survey_id, assignment_id: assignment_id, user_id: session[:user].id)
-    @new.save
+    selected_global_survey_id = params[:global_survey]
+
+    @assignment = Assignment.find(assignment_id)
+
+    #Update the Survey Id with the selected Survey Id
+    @assignment[:survey_id] = selected_survey_id
+    @assignment.update!(@assignment_params)
+    
+    #Update the Global Survey Id with the selected Global Survey Id
+    @assignment[:global_survey_id] = selected_global_survey_id
+    @assignment.update!(@assignment_params)
+
+    # create an entry for the questionnaire in assignment queationnaire table
+    # @new = AssignmentQuestionnaire.new(questionnaire_id: selected_survey_id, assignment_id: assignment_id, user_id: session[:user].id)
+    # @new.save
     # flash success message
     flash[:success] = 'Survey has been successfuly assigned'
     # redirect back to the same page with same assignment id

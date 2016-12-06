3862c9ad269b762f71d1e4999031e34d907ffcbb
pranavnawathe <pranav.nawathe92@gmail.com>
2016-12-06 17:25:36 -0500

    Added migration code for changing type name

--- db/migrate/20161206222024_update_tree_folders.rb
+++ db/migrate/20161206222024_update_tree_folders.rb
@@ -1,4 +1,7 @@
 class UpdateTreeFolders < ActiveRecord::Migration
   def change
+    surveyTree = TreeFolder.find_by_name('Survey')
+    surveyTree.name = 'Assignment Survey'
+    surveyTree.save!
   end
 end

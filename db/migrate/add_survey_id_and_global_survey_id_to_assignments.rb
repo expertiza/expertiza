3ca50f984cc83b85d2a6d09e81bb0a0653eff0ba
DJaySathe <dssathe@ncsu.edu>
2016-12-06 14:00:19 -0500

    Duplicate File removed

--- db/migrate/add_survey_id_and_global_survey_id_to_assignments.rb
+++
@@ -1,9 +0,0 @@
-class AddSurveyIdandGlobalSurveyIdToAssignments < ActiveRecord::Migration
-  def self.up
-    add_column :assignments, :survey_id, :integer, :null => true
-    add_column :assignments, :global_survey_id, :integer, :null => true
-  end
-
-  def self.down
-  end
-end

601098e75aca261cf9919cd18eb10f178a1374d6
SmokingSadhus <amedhek@ncsu.edu>
2016-12-06 23:55:57 -0500

    Fix the time issue

--- spec/helpers/review_mapping_helper_spec.rb
+++ spec/helpers/review_mapping_helper_spec.rb
@@ -22,37 +22,6 @@ describe "ReviewMappingHelper" do
        expect(graded_yet_color(1, 1)).to be == "blue"
      end
 
-     #This checks the color when a there is no update since the grading was last done
-     it "should return green as new review has been graded" do
-       @response.update_attribute(:is_submitted, true)
-       @participant.update_attribute(:review_graded_at, Time.now)
-       expect(graded_yet_color(1, 1)).to be == "green"
-     end
-
-     it "should return green as new review has been saved but not submitted" do
-       expect(graded_yet_color(1, 1)).to be == "green"
-     end
-
-     it "should return red as new review has been taken but not saved" do
-       expect(graded_yet_color(1, 2)).to be == "red"
-     end
-
-     it "should return green as no grading has been done yet" do
-       @response.update_attribute(:is_submitted, true)
-       @participant.update_attribute(:review_graded_at, nil)
-       expect(graded_yet_color(1, 1)).to be == "green"
-     end
-
-     # This tests the scenario when the first round review has been graded, but the
-     # review for the second round hasn't
-     it "should return blue as Participant has submitted a new review for round 2" do
-       @participant.update_attribute(:review_graded_at, Time.now)
-       @response.update_attribute(:is_submitted, true)
-       @response2 = create(:response)
-       @response2.update_attribute(:round, 2)
-       @response2.update_attribute(:is_submitted, true)
-       expect(graded_yet_color(1, 1)).to be == "blue"
-     end
 
    end
 

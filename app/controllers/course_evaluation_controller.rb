d00977dfbd8f573e65e2de3688fed06ae0ae2adf
pranavnawathe <pranav.nawathe92@gmail.com>
2016-12-06 20:47:31 -0500

    added creation of surveys list code

--- app/controllers/course_evaluation_controller.rb
+++ app/controllers/course_evaluation_controller.rb
@@ -8,13 +8,38 @@ class CourseEvaluationController < ApplicationController
       redirect_to '/'
       return
     end
-    deployments = SurveyParticipant.where(user_id: session[:user].id)
+    deployments = Participants.where(user_id: session[:user].id)
     @surveys = []
     deployments.each do |sd|
-      survey_deployment = SurveyDeployment.find(sd.survey_deployment_id)
-      if Time.now > survey_deployment.start_date && Time.now < survey_deployment.end_date
-        @surveys << [Questionnaire.find(survey_deployment.course_evaluation_id), sd.survey_deployment_id, survey_deployment.end_date, survey_deployment.course_id]
+      if sd.type == 'AssignmentParticipant'
+        @assignment = Assignment.find(sd.parent_id)
+       
+        if !@assignment.survey_id.nil?
+           @surveys << [Questionnaire.find(@assignment.survey_id)]
+        end
+        
+        if !@assignment.global_survey_id.nil?
+           @surveys << [Questionnaire.find(@assignment.global_survey_id)]
+        end
       end
+      
+      if sd.type == 'AssignmentParticipant'
+        @course = Course.find(sd.parent_id)
+       
+        if !@course.survey_id.nil?
+           @surveys << [Questionnaire.find(@course.survey_id)]
+        end
+        
+        if !@course.global_survey_id.nil?
+           @surveys << [Questionnaire.find(@course.global_survey_id)]
+        end
+      end
+      
+      # survey_deployment = SurveyDeployment.find(sd.survey_deployment_id)
+      # if Time.now > survey_deployment.start_date && Time.now < survey_deployment.end_date
+       # @surveys << [Questionnaire.find(survey_deployment.course_evaluation_id), sd.survey_deployment_id, survey_deployment.end_date, survey_deployment.course_id]
+      #end
+        
     end
   end
 end

74d58df17d21835bf60728a01223b1767d93c003
saratkavuru <kavurusarat@gmail.com>
2016-12-07 17:16:55 -0500

    Minor fixes

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,12 +1,12 @@
 module MessagesHelper
 
-def reviewee_or_reviewer(message)
+def is_reviewer(message)
 	team = AssignmentTeam.find(message.chat.assignment_team_id)
   for user in team.users
     if message.user==user
-      return "Reviewee"
+      return "Author"
     end
-      end
-	return "Reviewer"
+  end
+ return "Reviewer"
 end
 end

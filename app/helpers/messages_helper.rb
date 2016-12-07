485c36a7dac031e752a9a9158deecc467e72df0b
saratkavuru <kavurusarat@gmail.com>
2016-12-06 19:00:39 -0500

    Fixed bug in view with reviewers and reviewees on different sides.

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,7 +1,5 @@
 module MessagesHelper
-def self_or_other(message)
-    message.user == current_user ? "self" : "other"
-  end
+
 def reviewee_or_reviewer(message)
 	team = AssignmentTeam.find(message.chat.assignment_team_id)
   for user in team.users

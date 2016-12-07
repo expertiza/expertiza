e9fb47c573483ce5614f83c02f2e6589b6317e38
saratkavuru <kavurusarat@gmail.com>
2016-12-07 17:26:21 -0500

    Update messages_helper.rb

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,7 +1,7 @@
 module MessagesHelper
 
 def is_reviewer(message)
-team = AssignmentTeam.find(message.chat.assignment_team_id)
+ team = AssignmentTeam.find(message.chat.assignment_team_id)
   for user in team.users
     if message.user==user
       return "Author"

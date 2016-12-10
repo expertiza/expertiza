c7bb2718eae4894663e9ea27b71f4e93babdff4b
aanilku <aanilku@ncsu.edu>
2016-12-10 16:56:13 -0500

    test

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -42,10 +42,10 @@ class AssignmentsController < ApplicationController
       @@event_logger.warn ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
       @@event_logger.warn "to_s: "+assignment_form_params.to_s
       @@event_logger.warn "inspect: "+assignment_form_params.inspect
-      @@event_logger.warn "assignment name: "+assignment_form_params.name
+      #@@event_logger.warn "assignment name: "+assignment_form_params.name
       @@event_logger.warn ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
-      @@event_logger.warn "&assignments_controller|create|#{session[:user].role_id}|#{session[:user].id}|create assignment|#{assignment_form_params[:name]}"
+      @@event_logger.warn "&assignments_controller|create|#{session[:user].role_id}|#{session[:user].id}|create assignment|#{assignment_form_params[:assignment][:name]}"
       redirect_to edit_assignment_path @assignment_form.assignment.id
       undo_link("Assignment \"#{@assignment_form.assignment.name}\" has been created successfully. ")
     else

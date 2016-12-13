ac0eae6450ec93f5bf162ddb699b1cd0c36edad5
pranavnawathe <pranav.nawathe92@gmail.com>
2016-12-13 18:54:46 -0500

    debugging

--- app/controllers/response_controller.rb
+++ app/controllers/response_controller.rb
@@ -135,7 +135,8 @@ class ResponseController < ApplicationController
     @type = @map.type
     
     assignment_id = @map.reviewed_object_id
-    survey_id = Assignment.find(assignment_id).survey_id
+    assignemnt = Assignment.find(assignment_id)
+    survey_id = assignment.survey_id
     @questionnaire = Questionnaire.find(survey_id)
     
     @questions = sort_questions(@questionnaire.questions)

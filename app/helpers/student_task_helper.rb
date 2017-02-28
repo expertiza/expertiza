0d1d6557421204c6276888a940767f749288d366
Winbobob <zhu6@ncsu.edu>
2017-02-28 14:57:50 -0500

    Student should see the review grades and comments now.

--- app/helpers/student_task_helper.rb
+++ app/helpers/student_task_helper.rb
@@ -1,11 +1,12 @@
 module StudentTaskHelper
   def get_review_grade_info(participant)
     info = ''
-    if participant.try(:grade_for_reviewer).nil? or participant.try(:comment_for_reviewer).nil?
+    if participant.try(:review_grade).try(:grade_for_reviewer).nil? || 
+       participant.try(:review_grade).try(:comment_for_reviewer).nil?
       result = "N/A"
     else
-      info = "Score: " + participant.try(:grade_for_reviewer).to_s + "/100\n"
-      info += "Comment: " + participant.try(:comment_for_reviewer).to_s
+      info = "Score: " + participant.try(:review_grade).try(:grade_for_reviewer).to_s + "/100\n"
+      info += "Comment: " + participant.try(:review_grade).try(:comment_for_reviewer).to_s
       info = truncate(info, length: 1500, omission: '...')
       result = "<img src = '/assets/info.png' title = '" + info + "'>"
     end

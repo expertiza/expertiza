f72cbc56dafc0774f3bc07d8b46565b9e9aec386
myelava <myelava@ncsu.edu>
2017-03-14 12:17:28 -0400

    changes in team mailing

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -31,7 +31,7 @@ class TeamsController < ApplicationController
     @parent = Object.const_get(session[:team_type] ||= 'Assignment').find(params[:id])
   end
 
-  # called when a instructor tries to create an empty namually.
+  # called when a instructor tries to create an empty manually.
   def create
     parent = Object.const_get(session[:team_type]).find(params[:id])
     begin

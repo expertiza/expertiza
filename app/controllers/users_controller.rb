597972bd5f75ff6805dcd74b5de0ada14180a2e4
RagaRavali <ragaravalip93@gmail.com>
2017-03-15 18:29:28 -0400

    user creation mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -95,7 +95,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
     @rolename = Role.find_by_name(params[:role])
-    ##here add a mailer to - created accounnt
+    ##here add a mailer to - created account
     foreign
   end
 

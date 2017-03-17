bf16192eb2f605e8d869b2e2a139f3d21bfdfb52
Aravind Anantha <aananth3@ncsu.edu>
2017-03-17 18:51:23 -0400

    Reduced ABC size of create in JoinTeamRequestsController

--- app/controllers/join_team_requests_controller.rb
+++ app/controllers/join_team_requests_controller.rb
@@ -1,4 +1,5 @@
 class JoinTeamRequestsController < ApplicationController
+  before_action :check_team, only: [:create]
   def action_allowed?
     current_role_name.eql?("Student")
   end
@@ -34,34 +35,25 @@ class JoinTeamRequestsController < ApplicationController
 
   # create a new join team request entry for join_team_request table and add it to the table
   def create
-    # check if the advertisement is from a team member and if so disallow requesting invitations
-    team_member = TeamsUser.where(['team_id =? and user_id =?', params[:team_id], session[:user][:id]])
-    team = Team.find(params[:team_id])
-    if team.full?
-      flash[:note] = "This team is full."
-    else
-      if !team_member.empty?
-        flash[:note] = "You are already a member of this team."
+
+    @join_team_request = JoinTeamRequest.new
+    @join_team_request.comments = params[:comments]
+    @join_team_request.status = 'P'
+    @join_team_request.team_id = params[:team_id]
+
+    participant = Participant.where(user_id: session[:user][:id], parent_id: params[:assignment_id]).first
+    @join_team_request.participant_id = participant.id
+    respond_to do |format|
+      if @join_team_request.save
+        format.html { redirect_to(@join_team_request, notice: 'JoinTeamRequest was successfully created.') }
+        format.xml  { render xml: @join_team_request, status: :created, location: @join_team_request }
+        flash[:note] = "Noob shit team, noob shit asshole!"
       else
-
-        @join_team_request = JoinTeamRequest.new
-        @join_team_request.comments = params[:comments]
-        @join_team_request.status = 'P'
-        @join_team_request.team_id = params[:team_id]
-
-        participant = Participant.where(user_id: session[:user][:id], parent_id: params[:assignment_id]).first
-        @join_team_request.participant_id = participant.id
-        respond_to do |format|
-          if @join_team_request.save
-            format.html { redirect_to(@join_team_request, notice: 'JoinTeamRequest was successfully created.') }
-            format.xml  { render xml: @join_team_request, status: :created, location: @join_team_request }
-          else
-            format.html { render action: "new" }
-            format.xml  { render xml: @join_team_request.errors, status: :unprocessable_entity }
-          end
-        end
+        format.html { render action: "new" }
+        format.xml  { render xml: @join_team_request.errors, status: :unprocessable_entity }
       end
     end
+
   end
 
   # update join team request entry for join_team_request table and add it to the table
@@ -95,4 +87,22 @@ class JoinTeamRequestsController < ApplicationController
     @join_team_request.save
     redirect_to view_student_teams_path student_id: params[:teams_user_id]
   end
+
+  private
+    def check_team
+      # check if the advertisement is from a team member and if so disallow requesting invitations
+      team_member = TeamsUser.where(['team_id =? and user_id =?', params[:team_id], session[:user][:id]])
+      team = Team.find(params[:team_id])
+
+      if team.full?
+        flash[:note] = "This team is full."
+        return
+      end
+
+      if !team_member.empty?
+        flash[:note] = "You are already a member of this team."
+        return
+      end
+
+    end
 end

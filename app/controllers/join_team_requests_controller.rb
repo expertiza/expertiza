9d117eef29b3969be1a1971e347146dca0aa5a4f
Aravind Anantha <aananth3@ncsu.edu>
2017-03-17 23:53:50 -0400

    Revert "Adding comments about users students participants in invitations controller"
    This reverts commit 6fed278bc07137a07fb7f213cd1581f1c51d39e5.

--- app/controllers/join_team_requests_controller.rb
+++ app/controllers/join_team_requests_controller.rb
@@ -1,167 +1,104 @@
-class InvitationController < ApplicationController
-  before_action :check_user, only: [:create]
+class JoinTeamRequestsController < ApplicationController
+  before_action :check_team, only: [:create]
   def action_allowed?
-    ['Instructor', 'Teaching Assistant', 'Administrator', 'Super-Administrator', 'Student'].include? current_role_name
+    current_role_name.eql?("Student")
+  end
+
+  def index
+    @join_team_requests = JoinTeamRequest.all
+    respond_after @join_team_requests
+  end
+
+  def show
+    @join_team_request = JoinTeamRequest.find(params[:id])
+    respond_after @join_team_request
   end
 
   def new
-    @invitation = Invitation.new
+    @join_team_request = JoinTeamRequest.new
+    respond_after @join_team_request
   end
 
+  def edit
+    @join_team_request = JoinTeamRequest.find(params[:id])
+  end
+
+  # create a new join team request entry for join_team_request table and add it to the table
   def create
-    #user is the student you are inviting to your team
-    user = User.find_by(name: params[:user][:name].strip)
-    #team has information about the team
-    team = AssignmentTeam.find(params[:team_id])
-    #student has information about the participant
-    student = AssignmentParticipant.find(params[:student_id])
 
-    #participant information about student you are trying to invite to the team
-    team_member = TeamsUser.where(['team_id =? and user_id =?', team.id, user.id])
-    # check if invited user is already in the team
-    if !team_member.empty?
-      flash[:note] = "The user \"#{user.name}\" is already a member of the team."
-    else
-      # check if the invited user is already invited (i.e. awaiting reply)
-      if Invitation.is_invited?(student.user_id, user.id, student.parent_id)
-        @invitation = Invitation.new
-        @invitation.to_id = user.id
-        @invitation.from_id = student.user_id
-        @invitation.assignment_id = student.parent_id
-        @invitation.reply_status = 'W'
-        @invitation.save
+    @join_team_request = JoinTeamRequest.new
+    @join_team_request.comments = params[:comments]
+    @join_team_request.status = 'P'
+    @join_team_request.team_id = params[:team_id]
+
+    participant = Participant.where(user_id: session[:user][:id], parent_id: params[:assignment_id]).first
+    @join_team_request.participant_id = participant.id
+    respond_to do |format|
+      if @join_team_request.save
+        format.html { redirect_to(@join_team_request, notice: 'JoinTeamRequest was successfully created.') }
+        format.xml  { render xml: @join_team_request, status: :created, location: @join_team_request }
       else
-        flash[:note] = "You have already sent an invitation to \"#{user.name}\"."
-        team_member = TeamsUser.where(['team_id =? and user_id =?', team.id, user.id])
-        # check if invited user is already in the team
-        if !team_member.empty?
-          flash[:note] = "The user \"#{user.name}\" is already a member of the team."
-        else
-          # check if the invited user is already invited (i.e. awaiting reply)
-          if Invitation.is_invited?(student.user_id, user.id, student.parent_id)
-            @invitation = Invitation.new
-            @invitation.to_id = user.id
-            @invitation.from_id = student.user_id
-            @invitation.assignment_id = student.parent_id
-            #Status code W for waiting for reply
-            @invitation.reply_status = 'W'
-            @invitation.save
-          else
-            flash[:note] = "You have already sent an invitation to \"#{user.name}\"."
-          end
-        end
-      end
-
-    end
-
-    update_join_team_request user, student
-
-    redirect_to view_student_teams_path student_id: student.id
-  end
-
-  def update_join_team_request(user, student)
-    # update the status in the join_team_request to A
-    if user && student
-      return
-    end
-    #participant information of invitee and assignment
-    participant = AssignmentParticipant.where(['user_id =? and parent_id =?', user.id, student.parent_id]).first
-    if participant
-      old_entry = JoinTeamRequest.where(['participant_id =? and team_id =?', participant.id, params[:team_id]]).first
-      #Status code A for accepted
-      old_entry.update_attribute("status", 'A') if old_entry
-    end
-
-  end
-
-  def auto_complete_for_user_name
-    search = params[:user][:name].to_s
-    @users = User.where("LOWER(name) LIKE ?", "%#{search}%") unless search.blank?
-  end
-
-  def accept
-    @inv = Invitation.find(params[:inv_id])
-
-    student = Participant.find(params[:student_id])
-
-    assignment_id = @inv.assignment_id
-    inviter_user_id = @inv.from_id
-    inviter_participant = AssignmentParticipant.find_by(user_id: inviter_user_id, parent_id: assignment_id)
-
-    ready_to_join = false
-    # check if the inviter's team is still existing, and have available slot to add the invitee
-    inviter_assignment_team = AssignmentTeam.team(inviter_participant)
-    if inviter_assignment_team.nil?
-      flash[:error] = "The team that invited you does not exist anymore."
-    elsif inviter_assignment_team.full?
-      flash[:error] = "The team that invited you is full now."
-    else
-      ready_to_join = true
-    end
-
-
-    if ready_to_join
-      #Status code A for accepted
-      @inv.reply_status = 'A'
-      @inv.save
-
-      # Remove the users previous team since they are accepting an invite for possibly a new team.
-      TeamsUser.remove_team(student.user_id, params[:team_id])
-
-      # Accept the invite and return boolean on whether the add was successful
-      add_successful = Invitation.accept_invite(params[:team_id], @inv.from_id, @inv.to_id, student.parent_id)
-
-      unless add_successful
-        flash[:error] = "The system failed to add you to the team that invited you."
+        format.html { render action: "new" }
+        format.xml  { render xml: @join_team_request.errors, status: :unprocessable_entity }
       end
     end
 
-    redirect_to view_student_teams_path student_id: params[:student_id]
   end
 
+  # update join team request entry for join_team_request table and add it to the table
+  def update
+    @join_team_request = JoinTeamRequest.find(params[:id])
+    respond_to do |format|
+      if @join_team_request.update_attribute(:comments, params[:join_team_request][:comments])
+        format.html { redirect_to(@join_team_request, notice: 'JoinTeamRequest was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render action: "edit" }
+        format.xml  { render xml: @join_team_request.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @join_team_request = JoinTeamRequest.find(params[:id])
+    @join_team_request.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(join_team_requests_url) }
+      format.xml  { head :ok }
+    end
+  end
+
+  # decline request to join the team...
   def decline
-    @inv = Invitation.find(params[:inv_id])
-    #Status code D for declined
-    @inv.reply_status = 'D'
-    @inv.save
-    student = Participant.find(params[:student_id])
-    redirect_to view_student_teams_path student_id: student.id
+    @join_team_request = JoinTeamRequest.find(params[:id])
+    @join_team_request.status = 'D'
+    @join_team_request.save
+    redirect_to view_student_teams_path student_id: params[:teams_user_id]
   end
 
-  def cancel
-    Invitation.find(params[:inv_id]).destroy
-    redirect_to view_student_teams_path student_id: params[:student_id]
-  end
   private
-  def check_user
-    #user is the student you are inviting to your team
-    user = User.find_by(name: params[:user][:name].strip)
-    #team has information about the team
-    team = AssignmentTeam.find(params[:team_id])
-    #student has information about the participant
-    student = AssignmentParticipant.find(params[:student_id])
+    def check_team
+      # check if the advertisement is from a team member and if so disallow requesting invitations
+      team_member = TeamsUser.where(['team_id =? and user_id =?', params[:team_id], session[:user][:id]])
+      team = Team.find(params[:team_id])
 
-    return unless current_user_id?(student.user_id)
+      if team.full?
+        flash[:note] = "This team is full."
+        return
+      end
 
-    # check if the invited user is valid
-    if !user
-      flash[:note] = "The user \"#{params[:user][:name].strip}\" does not exist. Please make sure the name entered is correct."
-      redirect_to view_student_teams_path student_id: student.id
-      return
-    end
-    participant = AssignmentParticipant.where('user_id =? and parent_id =?', user.id, student.parent_id).first
-    # check if the user is a participant of the assignment
-    if !participant
-      flash[:note] = "The user \"#{params[:user][:name].strip}\" is not a participant of this assignment."
-      redirect_to view_student_teams_path student_id: student.id
-      return
+      if !team_member.empty?
+        flash[:note] = "You are already a member of this team."
+        return
+      end
+
     end
 
-    if team.full?
-      flash[:error] = "Your team already has the maximum number members."
-      redirect_to view_student_teams_path student_id: student.id
-      return
+    def respond_after(request)
+      respond_to do |format|
+        format.html
+        format.xml  { render xml: request }
+      end
     end
-
-  end
 end

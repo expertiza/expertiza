33521306a3a42d23421c592b5ef38eee77c7dd5c
Michael <mamoran@ncsu.edu>
2017-03-19 22:06:38 +0000

    Added tests for new calculate_penalty_minutes method in penalty_helper.rb

--- spec/helpers/penalty_helper_spec.rb
+++ spec/helpers/penalty_helper_spec.rb
@@ -25,13 +25,36 @@ describe "PenaltyHelper" do
   end
   
   describe "#update_calculated_penalty_objects" do
-    it "increase calculated penalty for day late assignment to policy maximum when changing policy time unit from Hour to Minute" do
+    it "should increase calculated penalty for day late assignment to policy maximum when changing policy time unit from Hour to Minute" do
       @late_policy.update_attributes(:penalty_unit => 'Minute')
       PenaltyHelper.update_calculated_penalty_objects(@late_policy)
       penalty_points = @calculated_penalty.penalty_points
       expect(penalty_points).to be == @late_policy.max_penalty
     end
   end
+  
+  describe "#calculate_penalty_minutes" do
+    it "should return 1 when passed a 1 minute time difference and a penalty unit of 'Minute'" do
+      @penalty_unit = 'Minute'
+      time_difference = ((DateTime.now.in_time_zone) - (DateTime.now.in_time_zone - 1.minute)) 
+      penalty_minutes = PenaltyHelper.calculate_penalty_minutes(time_difference)
+      expect(penalty_minutes.round).to be == 1
+    end
+    
+    it "should return 1 when passed a 1 hour time difference and a penalty unit of 'Hour'" do
+      @penalty_unit = 'Hour'
+      time_difference = ((DateTime.now.in_time_zone) - (DateTime.now.in_time_zone - 1.hour)) 
+      penalty_minutes = PenaltyHelper.calculate_penalty_minutes(time_difference)
+      expect(penalty_minutes.round).to be == 1
+    end
+    
+    it "should return 1 when passed a 1 day time difference and a penalty unit of 'Day'" do
+      @penalty_unit = 'Day'
+      time_difference = ((DateTime.now.in_time_zone) - (DateTime.now.in_time_zone - 1.day)) 
+      penalty_minutes = PenaltyHelper.calculate_penalty_minutes(time_difference)
+      expect(penalty_minutes.round).to be == 1
+    end
+  end 
   
   # Add tests to validate calculate_penalty
 end

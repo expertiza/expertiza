d1f7cf951b71d47209e3e0ae147ad0b2bd3600be
Alexander Valkovsky <avalkovsky@gmail.com>
2017-03-20 18:31:56 -0400

    moved logical to grades_helper.rb

--- app/helpers/grades_helper.rb
+++ app/helpers/grades_helper.rb
@@ -36,6 +36,67 @@ module GradesHelper
     {has_team: has_team, has_metareview: has_metareview, true_num: true_num}
   end
 
+  def participant
+    @participant = Participant.find(params[:id])
+  end
+
+  def rscore_review
+     @participant = Participant.find(params[:id])
+     @pscore = @participant.scores(@questions)
+     if @pscore[:review]
+         @rscore_review=Rscore.new(@pscore,:review)
+         end
+     @rscore_review
+  end
+
+  def rscore_metareview
+     @participant = Participant.find(params[:id])
+     @pscore = @participant.scores(@questions)
+     if @pscore[:metareview]
+         @rscore_metareview=Rscore.new(@pscore,:metareview)
+         end
+     @rscore_metareview
+  end
+
+  def rscore_feedback
+     @participant = Participant.find(params[:id])
+     @pscore = @participant.scores(@questions)
+     if @pscore[:feedback]
+         @rscore_feedback=Rscore.new(@pscore,:feedback)
+         end
+     @rscore_feedback
+  end
+
+  def rscore_teammate
+     @participant = Participant.find(params[:id])
+     @pscore = @participant.scores(@questions)
+     if @pscore[:teammate]
+         @rscore_teammate=Rscore.new(@pscore,:teammate)
+         end
+     @rscore_teammate
+  end
+
+  def p_total_score
+     @participant = Participant.find(params[:id])
+     @pscore = @participant.scores(@questions)
+     if @participant.grade
+         @total_score = participant.grade
+         else
+             @total_score = @pscore[:total_score]
+             end
+     @total_score
+  end
+
+  def p_title
+     @participant = Participant.find(params[:id])
+     if @participant.grade
+         @title = "A score in blue indicates that the value was overwritten by the instructor or teaching assistant."
+         else
+             @title = nil
+             end
+     @title
+  end
+
   def get_css_style_for_hamer_reputation(reputation_value)
     css_class = if reputation_value < 0.5
                   'c1'

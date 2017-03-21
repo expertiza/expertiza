dea089609cbee23c14df72e5b460cfa168e79e2c
Yuanchen Lu <ylu36@ncsu.edu>
2017-03-21 19:01:43 -0400

    moved @name

--- spec/features/scheduled_task_spec.rb
+++ spec/features/scheduled_task_spec.rb
@@ -3,6 +3,7 @@ def enqueue_scheduled_tasks(stage)
   # due_at = DateTime.now.in_time_zone + 120
   # seconds_until_due = due_at - Time.now
   # minutes_until_due = seconds_until_due / 60
+  @name = "user"
   due_at = DateTime.now.in_time_zone + 1.day
   # puts DateTime.now.in_time_zone
   # puts due_at
@@ -19,7 +20,7 @@ describe 'Submission deadline reminder email' do
   it 'is able to send reminder email for submission deadline to signed-up users ' do
     # Delayed::Worker.delay_jobs = false
     # id = 2
-    @name = "user"
+    # @name = "user"
     enqueue_scheduled_tasks("submission")
     expect(Delayed::Job.count).to eq(1)
     expect(Delayed::Job.last.handler).to include("deadline_type: submission")
@@ -29,7 +30,7 @@ end
 describe 'Review deadline reminder email' do
   it 'is able to send reminder email for review deadline to reviewers ' do
     # id = 2
-    @name = "user"
+    # @name = "user"
     enqueue_scheduled_tasks("drop_outstanding_reviews")
     expect(Delayed::Job.count).to eq(2)
     expect(Delayed::Job.last.handler).to include("deadline_type: drop_outstanding_reviews")
@@ -39,7 +40,7 @@ end
 describe 'Metareview deadline reminder email' do
   it 'is able to send reminder email for Metareview deadline to reviewers ' do
     # id = 2
-    @name = "user"
+    # @name = "user"
     enqueue_scheduled_tasks("metareview")
     expect(Delayed::Job.count).to eq(1)
     expect(Delayed::Job.last.handler).to include("deadline_type: metareview")
@@ -49,7 +50,7 @@ end
 describe 'Drop Topic deadline reminder email' do
   it 'is able to send reminder email for drop topic deadline to reviewers ' do
     # id = 2
-    @name = "user"
+    # @name = "user"
     enqueue_scheduled_tasks("drop_topic")
     expect(Delayed::Job.count).to eq(1)
     expect(Delayed::Job.last.handler).to include("deadline_type: drop_topic")
@@ -59,7 +60,7 @@ end
 describe 'Signup deadline reminder email' do
   it 'is able to send reminder email for signup deadline to reviewers ' do
     # id = 2
-    @name = "user"
+    # @name = "user"
     enqueue_scheduled_tasks("signup")
     expect(Delayed::Job.count).to eq(1)
     expect(Delayed::Job.last.handler).to include("deadline_type: signup")
@@ -69,7 +70,7 @@ end
 describe 'Team formation deadline reminder email' do
   it 'is able to send reminder email for team formation deadline to reviewers ' do
     # id = 2
-    @name = "user"
+    # @name = "user"
     enqueue_scheduled_tasks("team_formation")
     expect(Delayed::Job.count).to eq(1)
     expect(Delayed::Job.last.handler).to include("deadline_type: team_formation")

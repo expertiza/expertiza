86ed284ad6306c31dd34f45563d8a42c72bca243
balesj <jabales@ncsu.edu>
2017-03-21 08:31:56 -0400

    More layout, about to start implementing use of factories to get the testing environment set up.

--- spec/features/heat_map_spec.rb
+++ spec/features/heat_map_spec.rb
@@ -1,9 +1,19 @@
 require 'rspec'
 
-describe 'display a heat map of scores', js: true do
-  
+describe 'Student can view review scores in a heat map distribution', js: true do
+
   before(:each) do
     #Include setup here for pre-test stuff
+
+    # Create a student
+    @student = create(:student)
+
+    #Create an assignment
+
+    #Create reviewers
+
+    #Create reviews for the assignment from reviewers
+    
   end
 
   it 'Views a heat map of review scores' do

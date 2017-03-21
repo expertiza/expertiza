6259f21ce3ca6680299a0be54a581a94848ec2f9
alokozai <qalokoz@ncsu.edu>
2017-03-21 01:07:44 -0400

    unit test got get sentiment

--- spec/helpers/review_mapping_helper_spec.rb
+++ spec/helpers/review_mapping_helper_spec.rb
@@ -6,4 +6,14 @@ describe 'ReviewMappingHelper', :type => :helper do
       expect(helper.construct_sentiment_query(1,"Text")).not_to eq(nil)
     end
   end
+
+  describe "#get_sentiment" do
+    it "should not return nil" do
+      review = helper.construct_sentiment_query(1,"Test Review")
+      # Test first try to get sentiment from the sentiment analysis web service
+      expect(helper.get_sentiment(review, true)).not_to eq(nil)
+      # Test a retry to get sentiment from the sentiment analysis web service
+      expect(helper.get_sentiment(review, false)).not_to eq(nil)
+    end
+  end
 end

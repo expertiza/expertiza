7dbf5a70a172cd7f1347f0717d766fb9b5ba770b
balesj <jabales@ncsu.edu>
2017-03-21 09:39:14 -0400

    Added two more tests, still need to finish up factory implementation.

--- spec/features/heat_map_spec.rb
+++ spec/features/heat_map_spec.rb
@@ -16,7 +16,7 @@ describe 'Student can view review scores in a heat map distribution', js: true d
 
   end
 
-  it 'should be able to views a heat map of review scores' do
+  it 'should be able to view a heat map of review scores' do
     #Log in as the student with an assignment and reviews
     login_as @student.name
 
@@ -25,6 +25,28 @@ describe 'Student can view review scores in a heat map distribution', js: true d
     click_link 'Alternate View'
 
     expect(page).to have_content('Summary Report for Assignment')
+  end
+
+  it 'should be able to follow the link to a specific review' do
+    #Log in as the student with an assignment and reviews
+    login_as @student.name
+
+    #Select the assignment and follow the link to the heat map
+    click_link @assignment.name
+    click_link 'Review 1'
+
+    expect(page).to have_content('Review for')
+  end
+
+  it 'should be able to toggle the question list' do
+    #Log in as the student with an assignment and reviews
+    login_as @student.name
+
+    #Select the assignment and follow the link to the heat map
+    click_link @assignment.name
+    click_link 'toggle question list' #Is this a link or a button or what? Make sure this works.
+
+    expect(page).to have_content('Question')
   end
 
 end

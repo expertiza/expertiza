9d71135079f285cd114dada13e89c43d009594d0
balesj <jabales@ncsu.edu>
2017-03-21 08:45:42 -0400

    Set up a basic test to verify that the heat map page is accessible to the test user.

--- spec/features/heat_map_spec.rb
+++ spec/features/heat_map_spec.rb
@@ -13,12 +13,18 @@ describe 'Student can view review scores in a heat map distribution', js: true d
     #Create reviewers
 
     #Create reviews for the assignment from reviewers
-    
+
   end
 
-  it 'Views a heat map of review scores' do
-    #Fill in steps to execute this
-    true.should == false
+  it 'should be able to views a heat map of review scores' do
+    #Log in as the student with an assignment and reviews
+    login_as @student.name
+
+    #Select the assignment and follow the link to the heat map
+    click_link @assignment.name
+    click_link 'Alternate View'
+
+    expect(page).to have_content('Summary Report for Assignment')
   end
 
 end

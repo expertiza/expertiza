212d7495717350ec7f9cbdb146a41e39db0e89ba
fuzzymee <rpmee@ncsu.edu>
2017-03-21 00:05:18 -0400

    don't care box true test pass

--- spec/features/review_assignment_spec.rb
+++ spec/features/review_assignment_spec.rb
@@ -1,52 +1,56 @@
-#set up student
-#set up assignment
-#set up topics
-#set up topic submissions
-
-def login_and_request_review def
-	#fill instance_variable_get
-	login_as(user)
-	find_link(assignment name).click #find the link that allows user to request randomized topic submission
-	find_link('Others\' Work').click  #find the link for others' work
-	find_link(request review link name).click #find the link that requests a review 
-end
-
 require 'rails_helper'
 
-describe "review assignment", js: true do
-	before(:each) do
-		@assignment = create(:assignment, name: "automatic review assignment test", max_team_size: 4)
-		create(:assignment_node)
-		create(:deadline_type, name: "submission")
-		create(:deadline_type, name: "review")
-		create(:deadline_type, name: "metareview")
-		create(:deadline_type, name: "drop_topic")
-		create(:deadline_type, name: "signup")
-		create(:deadline_type, name: "team_formation")
-		create(:deadline_right)
-		create(:deadline_right, name: 'Late')
-		create(:deadline_right, name: 'OK')
+describe "peer review testing", type: :feature do
 
-		(1..10).each do |i|
-			student = create :student, name: 'student' + i.to_s
-			create :participant, assignment: @assignment, user: student
-			if i % 3 == 1 and i != 10
-				instance_variable_set('@team' + (i / 3 + 1).to_s, create(:assignment_team, name: 'team' + i.to_s))
-				@team = instance_variable_get('@team' + (i / 3 + 1).to_s)
-			end
-			create :team_user, user: student, team: @team
-		end
-	end
-	
-	it "can get review" do
-		@student_reviewer = create :student, name: 'test_student'
-		@participant_reviewer = create :participant, assignment: @assignment, user: @student_reviewer	
-	end
-	
-	it "show error when student's submission is only available for topic" do
-		expect(page).to have_content("There are no more submissions to review on this #{work}.")
-	end
-	
-	
+  before(:each) do
+    # create assignment and topic
+    create(:assignment, name: "TestAssignment", directory_path: "TestAssignment")
+    create_list(:participant, 3)
+    create(:topic, topic_name: "Topic")
+    create(:deadline_type, name: "submission")
+    create(:deadline_type, name: "review")
+    create(:deadline_type, name: "metareview")
+    create(:deadline_type, name: "drop_topic")
+    create(:deadline_type, name: "signup")
+    create(:deadline_type, name: "team_formation")
+    create(:deadline_right)
+    create(:deadline_right, name: 'Late')
+    create(:deadline_right, name: 'OK')
+    create(:assignment_due_date, deadline_type: DeadlineType.where(name: "submission").first, due_at: DateTime.now.in_time_zone + 1.day)
+  end
+  
+  def signup_topic
+    user = User.find_by_name("student2064")
+    stub_current_user(user, user.role.name, user.role)
+    visit '/student_task/list'
+    visit '/sign_up_sheet/sign_up?id=1&topic_id=1' # signup topic
+    visit '/student_task/list'
+    click_link "TestAssignment"
+    click_link "Your work"
+  end
+  
+  def submit_to_topic
+    signup_topic
+    fill_in 'submission', with: "https://www.ncsu.edu"
+    click_on 'Upload link'
+    expect(page).to have_content "https://www.ncsu.edu"
+  end
+  
+  it "is able to submit a single valid link" do
+    submit_to_topic
+    # open the link and check content
+    click_on "https://www.ncsu.edu"
+    expect(page).to have_http_status(200)
+  end
+  
+  it "submit and try to review own topic" do
+	submit_to_topic
+	visit '/student_task/list'
+	click_link "TestAssignment"
+	click_link "Others' work"
+        find(:css, "#i_dont_care").set(true)
+	click_button "Request a new submission to review"
+        expect(page).to have_content "No topics are available to review at this time. Please try later."
+  end
+  
 end
-:w

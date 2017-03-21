9043d2b3c46412804e4789160112095a89ebfa2a
Kushagra Mishra <kmishra@ncsu.edu>
2017-03-21 19:21:05 -0400

    test new query

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -498,7 +498,7 @@ class Assignment < ActiveRecord::Base
 
           reviewee = Team.find_by_id(ans.reviewee_id)
           if reviewee.nil?
-            reviewee = Participant.find_by_id(ans.reviewee_id).user
+            reviewee = User.find_by_id(ans.reviewee_id)
             puts reviewee.nil?
           end
 

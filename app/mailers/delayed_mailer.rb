29948d8e0d7fe65794eea0f3c427eff6361daa3c
Prateek Pramanik <ppraman2@ncsu.edu>
2017-03-20 22:55:49 -0400

    fixes based on Rubocop suggestions

--- app/mailers/delayed_mailer.rb
+++ app/mailers/delayed_mailer.rb
@@ -19,8 +19,8 @@ class DelayedMailer
       if (self.deadline_type == "metareview")
         mail_metareviewers
         if assignment.team_assignment?
-          teamMails = getTeamMembersMail
-          email_reminder(teamMails, "teammate review")
+          team_mails = get_team_members_mail
+          email_reminder(team_mails, "teammate review")
         end
       end
 
@@ -57,11 +57,11 @@ class DelayedMailer
     end
   end
 
-
+  # Added by Prateek as part of Spring 2017 E1711
   def get_emails_signed_up_users(assignment, sign_up_topics)
 
     emails = []
-    for topic in sign_up_topic
+    for topic in sign_up_topics
 
       # A signed up team is a single person when it is not a team assignment
       signed_up_teams = SignedUpTeam.where(['topic_id = ?', topic.id])
@@ -70,7 +70,7 @@ class DelayedMailer
 
         # Since this is a team assignment, we need to first find each team and then
         # each user to find all the emails
-        for team in signed_up_team
+        for team in signed_up_teams
           team_members = TeamsUser.where(team_id: team.team_id)
           for team_member in team_members
             user = User.find(team_member.user_id)
@@ -79,7 +79,7 @@ class DelayedMailer
         end
 
       else
-        for signed_up_user in signed_up_team
+        for signed_up_user in signed_up_teams
 
           # The team_id field is really a user ID because this is not a team assignment
           user_id = signed_up_user.team_id
@@ -88,25 +88,24 @@ class DelayedMailer
         end # end for signed_up_user
       end # end else
     end # end for topic
-    return emails
+    emails
   end
-    
+
+  # Last refactored by Prateek as part of Spring 2017 E1711
   # Find the signed_up_teams of the assignment and send mails to them
   def mail_signed_up_users
     emails = []
     assignment = Assignment.find(self.assignment_id)
     sign_up_topics = SignUpTopic.where(['assignment_id = ?', self.assignment_id])
 
-    # If there are sign_up topics for an assignement then send a mail toonly signed_up_teams else send a mail to all participants
-    if (sign_up_topics.nil? || sign_up_topics.count == 0)
+    # If there are sign_up topics for an assignement then send a mail to only signed_up_teams else send a mail to all participants
+    if sign_up_topics.nil? || sign_up_topics.count.zero?
       if assignment.team_assignment?
-        emails = getTeamMembersMail
+        emails = get_team_members_mail
       else
-        
         # mail_assignment_participants sends out email so
         # return immediately after so email is not sent twice
         mail_assignment_participants
-        return
       end
     else
       emails = get_emails_signed_up_users(assignement, sign_up_topics)
@@ -114,18 +113,18 @@ class DelayedMailer
     email_reminder(emails, self.deadline_type)
   end
 
-  def getTeamMembersMail
-    teamMembersMailList = []
-    assignment = Assignment.find(self.assignment_id)
+  # Function name changed by Prateek as part of Spring 2017 E1711
+  def get_team_members_mail
+    emails = []
     teams = Team.where(['parent_id = ?', self.assignment_id])
     for team in teams
       team_participants = TeamsUser.where(['team_id = ?', team.id])
       for team_participant in team_participants
         user = User.find(team_participant.user_id)
-        teamMembersMailList << user.email
+        emails << user.email
       end
     end
-    teamMembersMailList
+    emails
   end
 
   def get_one_member_team

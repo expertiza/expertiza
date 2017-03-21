8f3b6814cdb96deaf498bc08a6b831c8cfea2997
balesj <jabales@ncsu.edu>
2017-03-21 10:09:11 -0400

    Configured some basic factory operations. I still need to set up the actual reviews.

--- spec/features/heat_map_spec.rb
+++ spec/features/heat_map_spec.rb
@@ -3,24 +3,30 @@ require 'rspec'
 describe 'Student can view review scores in a heat map distribution', js: true do
 
   before(:each) do
-    #Include setup here for pre-test stuff
+    # Include setup here for pre-test stuff
 
     # Create a student
     @student = create(:student)
 
-    #Create an assignment
+    # Create an instructor
+    @instructor = create(:instructor)
 
-    #Create reviewers
+    # Create an assignment
+    @assignment = create :assignment, instructor: @instructor, course: nil  # Does this need to be tweaked?
 
-    #Create reviews for the assignment from reviewers
+    # Create reviewers
+
+    # Create reviews for the assignment from reviewers
+    # I'm not quite sure what the best way is to set these up. Do I need to make actual reviewers? Which factory type
+    # makes the reviews I need to display in the heat map?
 
   end
 
   it 'should be able to view a heat map of review scores' do
-    #Log in as the student with an assignment and reviews
+    # Log in as the student with an assignment and reviews
     login_as @student.name
 
-    #Select the assignment and follow the link to the heat map
+    # Select the assignment and follow the link to the heat map
     click_link @assignment.name
     click_link 'Alternate View'
 
@@ -28,10 +34,10 @@ describe 'Student can view review scores in a heat map distribution', js: true d
   end
 
   it 'should be able to follow the link to a specific review' do
-    #Log in as the student with an assignment and reviews
+    # Log in as the student with an assignment and reviews
     login_as @student.name
 
-    #Select the assignment and follow the link to the heat map
+    # Select the assignment and follow the link to the heat map
     click_link @assignment.name
     click_link 'Review 1'
 
@@ -39,10 +45,10 @@ describe 'Student can view review scores in a heat map distribution', js: true d
   end
 
   it 'should be able to toggle the question list' do
-    #Log in as the student with an assignment and reviews
+    # Log in as the student with an assignment and reviews
     login_as @student.name
 
-    #Select the assignment and follow the link to the heat map
+    # Select the assignment and follow the link to the heat map
     click_link @assignment.name
     click_link 'toggle question list' #Is this a link or a button or what? Make sure this works.
 

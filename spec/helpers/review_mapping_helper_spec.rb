cc610dbb915f43cb620d6dd526f03ade6eebd7b5
Siddharth Singh <sid5788@gmail.com>
2017-03-22 01:56:43 -0400

    Updating Tests

--- spec/helpers/review_mapping_helper_spec.rb
+++ spec/helpers/review_mapping_helper_spec.rb
@@ -15,6 +15,23 @@ describe 'ReviewMappingHelper', :type => :helper do
       # Test a retry to get sentiment from the sentiment analysis web service
       expect(helper.get_sentiment(review, false)).not_to eq(nil)
     end
+
+    it "should not get web response with 404 error" do
+      review = helper.construct_sentiment_query(1,"Test Reviews to check if our Rest Client is able to reach the sentiment analysis service.")
+      # Test first try to get sentiment from the sentiment analysis web service
+      expect(helper.get_sentiment(review, true).code).not_to eq(404)
+      # Test a retry to get sentiment from the sentiment analysis web service
+      expect(helper.get_sentiment(review, true).code).not_to eq(404)
+    end
+
+    it "should not get web response with 500 error" do
+      review = helper.construct_sentiment_query(1,"Test Reviews to check if our Rest Client is able to reach the sentiment analysis service.")
+      # Test first try to get sentiment from the sentiment analysis web service
+      expect(helper.get_sentiment(review, true).code).not_to eq(500)
+      # Test a retry to get sentiment from the sentiment analysis web service
+      expect(helper.get_sentiment(review, true).code).not_to eq(500)
+    end
+
   end
 
   describe "#get_sentiment_list" do
@@ -24,21 +41,5 @@ describe 'ReviewMappingHelper', :type => :helper do
       @reviewers = ReviewResponseMap.review_response_report(@id, @assignment, "ReviewResponseMap", nil)
       expect(helper.get_sentiment_list).not_to eq(nil)
     end
-  end
-
-  it "should not get web response with 404 error" do
-    review = helper.construct_sentiment_query(1,"Test Reviews to check if our Rest Client is able to reach the sentiment analysis service.")
-    # Test first try to get sentiment from the sentiment analysis web service
-    expect(helper.get_sentiment(review, true).code).not_to eq(404)
-    # Test a retry to get sentiment from the sentiment analysis web service
-    expect(helper.get_sentiment(review, true).code).not_to eq(404)
-  end
-
-  it "should not get web response with 500 error" do
-    review = helper.construct_sentiment_query(1,"Test Reviews to check if our Rest Client is able to reach the sentiment analysis service.")
-    # Test first try to get sentiment from the sentiment analysis web service
-    expect(helper.get_sentiment(review, true).code).not_to eq(500)
-    # Test a retry to get sentiment from the sentiment analysis web service
-    expect(helper.get_sentiment(review, true).code).not_to eq(500)
   end
 end

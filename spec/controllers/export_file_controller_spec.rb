5d06a866f71e310d6debc7cb7bb12d4e27aa4286
aapatel8 <aapatel8@ncsu.edu>
2017-03-22 19:44:47 -0400

    Fixing controller test case

--- spec/controllers/export_file_controller_spec.rb
+++ spec/controllers/export_file_controller_spec.rb
@@ -19,13 +19,14 @@ describe ExportFileController do
       expected_csv = File.read('spec/features/assignment_export_details/expected_details_csv.txt')
       # generated_csv = post :exportdetails, id: assignment.id, delim_type2: ",", model: "Assignment", details: options
       # expect(generated_csv).to eq(expected_csv)
-      controller.params = {
+      params = {
           id: assignment.id,
           delim_type2: ",",
           model: 'Assignment',
           details: options
       }
-      generated_csv = controller.send(:exportdetails)
+      # generated_csv = controller.send(:exportdetails)
+      generated_csv = get :exportdetails, {:params => params}
       expect (generated_csv).to eq(expected_csv)
     end
   end

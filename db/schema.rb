7a41bbef64fc3247af15886defb3a7912b517890
Vidyashree Hegde <Vidyashree Hegde>
2017-03-22 11:31:28 -0400

    increase threshold

--- db/schema.rb
+++ db/schema.rb
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 20170222161826) do
     t.integer  "days_between_submissions",   limit: 4
     t.string   "review_assignment_strategy", limit: 255
     t.integer  "max_reviews_per_submission", limit: 4
-    t.integer  "review_topic_threshold",     limit: 4,     default: 0
+    t.integer  "review_topic_threshold",     limit: 10,     default: 0
     t.boolean  "copy_flag",                                default: false
     t.integer  "rounds_of_reviews",          limit: 4,     default: 1
     t.boolean  "microtask",                                default: false

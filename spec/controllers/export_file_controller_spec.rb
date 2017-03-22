6c2f931552dd8cdf869f2e5f9ee5d23f8227e957
aapatel8 <aapatel8@ncsu.edu>
2017-03-22 18:54:55 -0400

    Fixing controller test case

--- spec/controllers/export_file_controller_spec.rb
+++ spec/controllers/export_file_controller_spec.rb
@@ -11,8 +11,12 @@ describe ExportFileController do
     create(:review_response_map)
     create(:response)
     create(:answer, comments: "Test comment")
+    options = {"team_id"=>"true", "team_name"=>"true",
+               "reviewer"=>"true", "question"=>"true",
+               "question_id"=>"true", "comment_id"=>"true",
+               "comments"=>"true", "score"=>"true"}
     expected_csv = File.read('spec/features/assignment_export_details/expected_details_csv.txt')
-    generated_csv = post :exportdetails, id: assignment.id, delim_type2: ",", model: "Assignment"
+    generated_csv = post :exportdetails, id: assignment.id, delim_type2: ",", model: "Assignment", details: options
     expect(generated_csv).to eq(expected_csv)
   end
 end

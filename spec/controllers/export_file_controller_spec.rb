043756041d9c05ab79bdef1c886868911a460e4a
aapatel8 <aapatel8@ncsu.edu>
2017-03-22 18:57:34 -0400

    Fixing controller test case

--- spec/controllers/export_file_controller_spec.rb
+++ spec/controllers/export_file_controller_spec.rb
@@ -2,21 +2,23 @@ require 'rails_helper'
 
 describe ExportFileController do
   describe "check export to detailed csv" do
-    assignment = create(:assignment)
-    create(:assignment_team, name: "team1")
-    student = create(:student, name: "student1")
-    create(:participant, user: student)
-    create(:questionnaire)
-    create(:question)
-    create(:review_response_map)
-    create(:response)
-    create(:answer, comments: "Test comment")
-    options = {"team_id"=>"true", "team_name"=>"true",
-               "reviewer"=>"true", "question"=>"true",
-               "question_id"=>"true", "comment_id"=>"true",
-               "comments"=>"true", "score"=>"true"}
-    expected_csv = File.read('spec/features/assignment_export_details/expected_details_csv.txt')
-    generated_csv = post :exportdetails, id: assignment.id, delim_type2: ",", model: "Assignment", details: options
-    expect(generated_csv).to eq(expected_csv)
+    it "checks if returned csv is same as expected" do
+      assignment = create(:assignment)
+      create(:assignment_team, name: "team1")
+      student = create(:student, name: "student1")
+      create(:participant, user: student)
+      create(:questionnaire)
+      create(:question)
+      create(:review_response_map)
+      create(:response)
+      create(:answer, comments: "Test comment")
+      options = {"team_id"=>"true", "team_name"=>"true",
+                 "reviewer"=>"true", "question"=>"true",
+                 "question_id"=>"true", "comment_id"=>"true",
+                 "comments"=>"true", "score"=>"true"}
+      expected_csv = File.read('spec/features/assignment_export_details/expected_details_csv.txt')
+      generated_csv = post :exportdetails, id: assignment.id, delim_type2: ",", model: "Assignment", details: options
+      expect(generated_csv).to eq(expected_csv)
+    end
   end
 end

45d8c0e29aa329e931b4543adeea107141a374a4
aapatel8 <aapatel8@ncsu.edu>
2017-03-22 20:22:48 -0400

    Fixing model test case

--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -166,4 +166,29 @@ describe "has correct csv values?" do
     # puts "Expected CSV - " + expected_csv.to_s
     expect(generated_csv).to eq(expected_csv)
   end
+
+  it "checks csv with no data" do
+    assignment = create(:assignment)
+    create(:assignment_team, name: "team1")
+    student = create(:student, name: "student1")
+    create(:participant, user: student)
+    create(:questionnaire)
+    create(:question)
+    create(:review_response_map)
+    create(:response)
+    delimiter = ","
+    options = {"team_id"=>"true", "team_name"=>"true",
+               "reviewer"=>"true", "question"=>"true",
+               "question_id"=>"true", "comment_id"=>"true",
+               "comments"=>"true", "score"=>"true"}
+    expected_csv = File.read('spec/features/assignment_export_details/expected_details_no_data_csv.txt')
+    generated_csv = CSV.generate(col_sep: delimiter) do |csv|
+      csv << Assignment.export_Headers(assignment.id)
+      csv << Assignment.export_details_fields(options)
+      Assignment.export_details(csv, assignment.id, options)
+    end
+    # puts "Generated CSV - " + generated_csv
+    # puts "Expected CSV - " + expected_csv.to_s
+    expect(generated_csv).to eq(expected_csv)
+  end
 end

378c45e05c6494a4bc9db5be93b42d45dd772dc6
ruthvik1994 <mruthvik1994@gmail.com>
2017-03-25 17:38:57 -0400

    Failed Decline Testing

--- spec/controllers/invitations_controller_spec.rb
+++ spec/controllers/invitations_controller_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe InvitationsController, type: :controller do
 
+  describe "Invitaton decline" do
+    it "should change status to D" do
+      @inv = Invitation.new(reply_status: 'W')
+      @inv.save
+      get :decline, params = {inv_id: @inv.id}
+      expect(@inv.reply_status).to eq('D')
+    end
+  end
 end

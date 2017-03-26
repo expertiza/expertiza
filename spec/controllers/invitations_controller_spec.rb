20b08080621c426e2cd524b1d09dbd364c1b54df
Aravind Anantha <aananth3@ncsu.edu>
2017-03-26 03:17:48 -0400

    Added invitation factory

--- spec/controllers/invitations_controller_spec.rb
+++ spec/controllers/invitations_controller_spec.rb
@@ -9,16 +9,28 @@ RSpec.describe InvitationsController, type: :controller do
     @assignment_team = create(:assignment_team, assignment: @assignment)
     @team_user = create(:team_user)
     @participant = create(:participant)
+    @invitation = create(:invitation)
   end
 
-  describe "decline_invitation" do
+
+
+  describe "GET /decline" do
     it "must decline invitation with invitation_id" do
-      @inv = Invitation.new(assignment_id: @assignment.id, reply_status: 'W')
+      @inv = Invitation.new
+      @inv.assignment_id = @assignment.id
+      @inv.reply_status = 'W'
       @inv.save!
-      get :decline, :inv_id => @inv.id, :student_id => @student.id
-      temp = Invitation.find(@inv.id)
-      expect(temp.reply_status).to eq('D')
+      get :decline, {:inv_id => @inv.id, :student_id => @student.id}
+      expect(Invitation.find_by(assignment_id: @assignment.id).reload.reply_status).to eq('D')
     end
   end
+
+  describe "check_decline_call" do
+    it "must check something!" do
+      expect(Invitation).to receive(:decline)
+    end
+  end
+
+
 
 end

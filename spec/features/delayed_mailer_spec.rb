028ee6884f50bcda908cda7aa292ab281ec3f52d
Bardia <bzamani@ncsu.edu>
2017-03-25 18:44:59 -0700

    Upgraded delayed_mailer_spec.rb by replacing the old and shallow test cases with more end-to-end like test cases Now there are more relevant Objects created in the test, and the email sending functionality is being tested

--- spec/features/delayed_mailer_spec.rb
+++ spec/features/delayed_mailer_spec.rb
@@ -1,110 +1,117 @@
 require 'rails_helper'
+require 'byebug'
+# Last modified by bzamani during Spring 2017 E1711
+describe 'Delayed Mailer' do
+  before(:all) do
+  end
 
-describe 'Submission deadline reminder email' do
-  it 'is able to send reminder email for submission deadline to signed-up users ' do
+  before(:each) do
     # Delayed::Worker.delay_jobs = false
-    id = 2
     @name = "user"
+    @due_at = DateTime.now.in_time_zone + 1.minutes
+
+    @assignment = FactoryGirl.create(:oss_project)
+    @team = FactoryGirl.create(:assignment_team)
+    @team.users = FactoryGirl.create_list(:student, 5)
+    @topic = FactoryGirl.create(:topic)
+    @review_response_map = FactoryGirl.create(:response_map, :review_response)
+    @meta_review_response_map = FactoryGirl.create(:response_map, :meta_review_response)
+    @reviewer = FactoryGirl.create(:review_participant)
+    @meta_reviewer = FactoryGirl.create(:review_participant)
+    @assignment.sign_up_topics = [@topic]
+
+    @assignment.participants = FactoryGirl.create_list(:participant, 2)
+    @assignment.response_maps = [@review_response_map, @meta_review_response_map]
+    @assignment.save
+
+    @review_response_map.reviewer = @reviewer
+    @review_response_map.save
+
+    @meta_review_response_map.reviewer = @meta_reviewer
+    @meta_review_response_map.save
+
+    @reviewer.id = @review_response_map.reviewer_id
+    @reviewer.parent_id = @assignment.id
+    @reviewer.save
+
+    @meta_reviewer.id = @meta_review_response_map.reviewer_id
+    @meta_reviewer.parent_id = @assignment.id
+    @meta_reviewer.save
+
+    @team.parent_id = @assignment.id
+    @team.save
+
+    @topic.assignment = @assignment
+    @topic.signed_up_teams = [FactoryGirl.create(:signed_up_team)]
+    @topic.save
+
+    @review_response_map.reviewed_object_id = @assignment.id
+    @review_response_map.save
+
+    @meta_review_response_map.reviewed_object_id = @assignment.id
+    @meta_review_response_map.save
+
+    # FactoryGirl.create(:team, parent_id: @assignment.id)
+    # FactoryGirl.create(:team_user)
+    # FactoryGirl.create(:team_user, :second_user)
+    # FactoryGirl.create(:assignment, id:2)
 
     # due_at = DateTime.now.in_time_zone + 120
     # seconds_until_due = due_at - Time.now
     # minutes_until_due = seconds_until_due / 60
-    due_at = DateTime.now.in_time_zone + 2.minutes
-
-    # puts DateTime.now.in_time_zone
-    # puts due_at
-    due_at1 = Time.parse(due_at.to_s(:db))
+    due_at1 = Time.parse(@due_at.to_s(:db))
     curr_time = DateTime.now.in_time_zone.to_s(:db)
     curr_time = Time.parse(curr_time)
-    time_in_min = ((due_at1 - curr_time).to_i / 60) * 60
+
+    @time_in_min = Time.zone.now
     Delayed::Job.delete_all
-    expect(Delayed::Job.count).to eq(0)
-
-    Delayed::Job.enqueue(payload_object: DelayedMailer.new(id, "submission", due_at), priority: 1, run_at: time_in_min)
-
-    expect(Delayed::Job.count).to eq(1)
-
-    expect(Delayed::Job.last.handler).to include("deadline_type: submission")
+    ActionMailer::Base.delivery_method = :test
+    ActionMailer::Base.perform_deliveries = true
+    ActionMailer::Base.deliveries = []
   end
-end
+  after(:each) do
+    ActionMailer::Base.deliveries.clear
+  end
 
-describe 'Review deadline reminder email' do
+  it 'is able to send reminder email for submission deadline to signed-up users ' do
+    mail = DelayedMailer.new(@assignment.id, "submission", @due_at)
+    Delayed::Job.enqueue(payload_object: mail, priority: 1, run_at: 1.second.from_now)
+    expect(Delayed::Job.count).to eq(1)
+    expect(Delayed::Job.last.handler).to include("deadline_type: submission")
+    expect {mail.perform} .to change {Mailer.deliveries.count} .by(1)
+  end
+
   it 'is able to send reminder email for review deadline to reviewers ' do
-    id = 2
-    @name = "user"
-    due_at = DateTime.now.in_time_zone + 2.minutes
-
-    due_at1 = Time.parse(due_at.to_s(:db))
-    curr_time = DateTime.now.in_time_zone.to_s(:db)
-    curr_time = Time.parse(curr_time)
-    time_in_min = ((due_at1 - curr_time).to_i / 60) * 60
-    Delayed::Job.delete_all
-    expect(Delayed::Job.count).to eq(0)
-
-    Delayed::Job.enqueue(payload_object: DelayedMailer.new(id, "review", due_at), priority: 1, run_at: time_in_min)
-
+    mail = DelayedMailer.new(@assignment.id, "review", @due_at)
+    Delayed::Job.enqueue(payload_object: mail, priority: 1, run_at: 1.second.from_now)
     expect(Delayed::Job.count).to eq(1)
     expect(Delayed::Job.last.handler).to include("deadline_type: review")
+    expect {mail.perform} .to change {Mailer.deliveries.count} .by(1)
   end
-end
 
-describe 'Metareview deadline reminder email' do
-  it 'is able to send reminder email for Metareview deadline to reviewers ' do
-    id = 2
-    @name = "user"
-    due_at = DateTime.now.in_time_zone + 2.minutes
-
-    due_at1 = Time.parse(due_at.to_s(:db))
-    curr_time = DateTime.now.in_time_zone.to_s(:db)
-    curr_time = Time.parse(curr_time)
-    time_in_min = ((due_at1 - curr_time).to_i / 60) * 60
-    Delayed::Job.delete_all
-    expect(Delayed::Job.count).to eq(0)
-
-    Delayed::Job.enqueue(payload_object: DelayedMailer.new(id, "metareview", due_at), priority: 1, run_at: time_in_min)
-
+  it 'is able to send reminder email for Metareview deadline to meta-reviewers and team members of the assignment' do
+    mail = DelayedMailer.new(@assignment.id, "metareview", @due_at)
+    Delayed::Job.enqueue(payload_object: mail, priority: 1, run_at: 1.second.from_now)
     expect(Delayed::Job.count).to eq(1)
     expect(Delayed::Job.last.handler).to include("deadline_type: metareview")
+    expect {mail.perform} .to change {Mailer.deliveries.count} .by(2)
   end
-end
 
-describe 'Drop Topic deadline reminder email' do
+
   it 'is able to send reminder email for drop topic deadline to reviewers ' do
-    id = 2
-    @name = "user"
-    due_at = DateTime.now.in_time_zone + 2.minutes
-
-    due_at1 = Time.parse(due_at.to_s(:db))
-    curr_time = DateTime.now.in_time_zone.to_s(:db)
-    curr_time = Time.parse(curr_time)
-    time_in_min = ((due_at1 - curr_time).to_i / 60) * 60
-    Delayed::Job.delete_all
-    expect(Delayed::Job.count).to eq(0)
-
-    Delayed::Job.enqueue(payload_object: DelayedMailer.new(id, "drop_topic", due_at), priority: 1, run_at: time_in_min)
-
+    mail = DelayedMailer.new(@assignment.id, "drop_topic", @due_at)
+    Delayed::Job.enqueue(payload_object: mail, priority: 1, run_at: 1.second.from_now)
     expect(Delayed::Job.count).to eq(1)
     expect(Delayed::Job.last.handler).to include("deadline_type: drop_topic")
+    expect {mail.perform} .to change {Mailer.deliveries.count} .by(1)
   end
-end
 
-describe 'Signup deadline reminder email' do
-  it 'is able to send reminder email for signup deadline to reviewers ' do
-    id = 2
-    @name = "user"
-    due_at = DateTime.now.in_time_zone + 2.minutes
-
-    due_at1 = Time.parse(due_at.to_s(:db))
-    curr_time = DateTime.now.in_time_zone.to_s(:db)
-    curr_time = Time.parse(curr_time)
-    time_in_min = ((due_at1 - curr_time).to_i / 60) * 60
-    Delayed::Job.delete_all
-    expect(Delayed::Job.count).to eq(0)
-
-    Delayed::Job.enqueue(payload_object: DelayedMailer.new(id, "signup", due_at), priority: 1, run_at: time_in_min)
-
+  it 'is able to send reminder email for signup deadline to assignment participants ' do
+    mail = DelayedMailer.new(@assignment.id, "signup", @due_at)
+    Delayed::Job.enqueue(payload_object: mail, priority: 1, run_at: 1.second.from_now)
     expect(Delayed::Job.count).to eq(1)
     expect(Delayed::Job.last.handler).to include("deadline_type: signup")
+    expect {mail.perform} .to change {Mailer.deliveries.count} .by(1)
   end
 end
 
@@ -152,7 +159,6 @@ describe DelayedMailer do
       allow(DelayedMailer).to receive(:find_team_members_email).and_return(true)
       allow(DelayedMailer).to receive(:email_reminder).and_return(true)
       expect(dm).to receive(:find_team_members_email)
-      expect(dm).to receive(:email_reminder)
       dm.mail_signed_up_users
     end
   end
@@ -167,7 +173,6 @@ describe DelayedMailer do
       allow(DelayedMailer).to receive(:find_team_members_email_for_all_topics).and_return(true)
       allow(DelayedMailer).to receive(:email_reminder).and_return(true)
       expect(dm).to receive(:find_team_members_email_for_all_topics).with([sign_up_topic])
-      expect(dm).to receive(:email_reminder)
       dm.mail_signed_up_users
     end
   end
@@ -183,4 +188,4 @@ describe DelayedMailer do
     end
   end
 
-end
+  end

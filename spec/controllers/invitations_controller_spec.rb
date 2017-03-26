5ade33f3c3587f291e491f6f68b242a9ca505dc0
Aravind Anantha <aananth3@ncsu.edu>
2017-03-25 22:37:05 -0400

    invitations spec before each added

--- spec/controllers/invitations_controller_spec.rb
+++ spec/controllers/invitations_controller_spec.rb
@@ -2,12 +2,23 @@ require 'rails_helper'
 
 RSpec.describe InvitationsController, type: :controller do
 
-  describe "Invitaton decline" do
-    it "should change status to D" do
-      @inv = Invitation.new(reply_status: 'W')
-      @inv.save
-      get :decline, params = {inv_id: @inv.id}
-      expect(@inv.reply_status).to eq('D')
+  before(:each) do
+    @instructor = create(:instructor)
+    @student = create(:student)
+    @assignment = create(:assignment)
+    @assignment_team = create(:assignment_team, assignment: @assignment)
+    @team_user = create(:team_user)
+    @participant = create(:participant)
+  end
+
+  describe "decline_invitation" do
+    it "must decline invitation with invitation_id" do
+      @inv = Invitation.new(assignment_id: @assignment.id, reply_status: 'W')
+      @inv.save!
+      get :decline, :inv_id => @inv.id, :student_id => @student.id
+      temp = Invitation.find(@inv.id)
+      expect(temp.reply_status).to eq('D')
     end
   end
+
 end

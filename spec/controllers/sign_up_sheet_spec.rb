46a5972766586ad78313da4a9aca27dfec407ac5
Shahryar Rashid <srashid3@ncsu.edu>
2017-03-30 16:54:31 -0400

    Updated tests for signup sheet controller

--- spec/controllers/sign_up_sheet_spec.rb
+++ spec/controllers/sign_up_sheet_spec.rb
@@ -151,46 +151,73 @@ end
 
 describe SignUpSheetController do
   before(:each) do
+    @assignment = create(:assignment)
+    create_list(:participant, 3)
+    @student1 = User.where(role_id: 2).first
+    @student2 = User.where(role_id: 2).second
+    @student3 = User.where(role_id: 2).third
+
+    @topic1 = create(:topic, topic_name: "Parallel Architecture")
+    @topic2 = create(:topic, topic_name: "MVC Framework")
+    @topic3 = create(:topic, topic_name: "Design Patterns")
+    create(:deadline_type, name: "signup")
+    create(:deadline_type, name: "team_formation")
+    create(:deadline_type, name: "submission")
+    create(:deadline_type, name: "review")
+    create(:deadline_type, name: "metareview")
+    create(:deadline_type, name: "drop_topic") # Must Have an ID of 6
+    create(:deadline_right)
+    create(:deadline_right, name: 'Late')
+    create(:deadline_right, name: 'OK')
+    create(:assignment_due_date, deadline_type: DeadlineType.where(name: "drop_topic").first, due_at: DateTime.now.in_time_zone - 1.day)
+
+    create(:assignment_team, name: "Team1")
+    create(:assignment_team, name: "Team2")
+    create(:assignment_team, name: "Team3", submitted_hyperlinks: nil)
+    create(:team_user, user: User.where(role_id: 2).first, team: AssignmentTeam.first)
+    create(:team_user, user: User.where(role_id: 2).second, team: AssignmentTeam.second)
+    create(:team_user, user: User.where(role_id: 2).third, team: AssignmentTeam.third)
+
+    create(:signed_up_team, team_id: 2 , topic: SignUpTopic.second)
+    create(:signed_up_team, team_id: 3 , topic: SignUpTopic.third)
+
+    # Simulate Authorized Session
     instructor.save
     @user = User.find_by_name("instructor")
-
-    @assignment = Assignment.where(name: 'My assignment').first || Assignment.new("name" => "My assignment",
-                                                                                  "instructor_id" => @user.id)
-    @assignment.save
-
-    @topic1 = SignUpTopic.new(topic_name: "Topic1",
-                              topic_identifier: "Ch10",
-                              assignment_id: @assignment.id,
-                              max_choosers: 2)
-    @topic1.save
-
-    @topic2 = SignUpTopic.new(topic_name: "Topic2",
-                              topic_identifier: "Ch10",
-                              assignment_id: @assignment.id,
-                              max_choosers: 2)
-    @topic2.save
-
-    student.save
-    @user = User.find_by(name: "student")
-    @participant = @assignment.add_participant(@user.name, true, true, true)
-
-    # simulate authorized session
     allow_any_instance_of(ApplicationController).to receive(:current_role_name).and_return('Instructor')
     allow_any_instance_of(ApplicationController).to receive(:undo_link).and_return(TRUE)
   end
 
   describe "Instructor singup user" do
     it "adds user to topic with no signed up team" do
-      post :signup_as_instructor_action, username: @user.name, assignment_id: @assignment.id, topic_id: @topic1.id
-      num_teams = @topic1.signed_up_teams.length
-      expect(num_teams).to eq(1)
+      post :signup_as_instructor_action, username: @student1.name, assignment_id: @assignment.id, topic_id: @topic1.id
+      expect(flash[:success]).to eq('You have successfully signed up the student for the topic!')
+    end
+    it "checks that a user already has a topic" do
+      post :signup_as_instructor_action, username: @student2.name, assignment_id: @assignment.id, topic_id: @topic1.id
+      expect(flash[:error]).to eq('The student has already signed up for a topic!')
     end
     it "checks to make sure the user exists" do
       post :signup_as_instructor_action, username: "asifljasdlf", assignment_id: @assignment.id, topic_id: @topic1.id
       expect(flash[:error]).to eq('That student does not exist!')
     end
     it "redirects back to topics page" do
-      post :signup_as_instructor_action, username: @user.name, assignment_id: @assignment.id, topic_id: @topic1.id
+      post :signup_as_instructor_action, username: @student1.name, assignment_id: @assignment.id, topic_id: @topic1.id
+      expect(response).to redirect_to edit_assignment_url(id: @assignment.id)
+    end
+  end
+
+  describe "Instructor delete signup" do
+    it "checks to see if the user has already submitted their work" do
+      post :delete_signup_as_instructor, id: 2, topic_id: @topic2.id
+      expect(flash[:error]).to eq('The student has already submitted their work, so you are not allowed to remove them.')
+    end
+    it "checks to see if the deadline has passed" do
+      post :delete_signup_as_instructor, id: 3, topic_id: @topic3.id
+      expect(flash[:error]).to eq('You cannot drop a student after the drop topic deadline!')
+    end
+    it "redirects back to topics page" do
+      post :delete_signup_as_instructor, id: 3, topic_id: @topic3.id
       expect(response).to redirect_to edit_assignment_url(id: @assignment.id)
     end
   end

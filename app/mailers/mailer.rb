7eb258c570044239daddb1b11e11a425f6b490a5
RagaRavali <ragaravalip93@gmail.com>
2017-03-31 01:51:30 -0400

    correction in view msg

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,6 +1,7 @@
 class Mailer < ActionMailer::Base
   if Rails.env.development? || Rails.env.test?
     default from: 'expertiza.development@gmail.com'
+    helper ApplicationHelper
   else
     default from: 'expertiza-support@lists.ncsu.edu'
   end
@@ -14,16 +15,23 @@ class Mailer < ActionMailer::Base
     @avg_pct = defn[:body][:avg_pct]
     @assignment = defn[:body][:assignment]
 
-    emails = User.where(["role_id = ?",2], ["copy_of_all_emails = ?", true]).select("email")
-    ins_email = []
-    ins_email << emails
+    #emails = User.where(["role_id = ?",2], ["copy_of_all_emails = ?", true]).select("email")
+    #ins_email = []
+    #ins_email << emails
 
     if Rails.env.development? || Rails.env.test?
       defn[:to] = 'expertiza.development@gmail.com'
     end
+    condition = User.where(["role_id = ?", 2]).select("copy_of_all_emails = ?")
+    if condition == true
+      defn[:cc] = User.where(["role_id = ? and copy_of_all_emails = ?", 2,true]).select("email")
+    end
+    else
+      defn[:cc] = 'expertiza.development@gmail.com'
+
     mail(subject: defn[:subject],
          to: defn[:to],
-         cc: ins_email,
+         cc: defn[:cc],
          bcc: defn[:bcc])
   end
 
@@ -39,8 +47,10 @@ class Mailer < ActionMailer::Base
     if Rails.env.development? || Rails.env.test?
       defn[:to] = 'expertiza.development@gmail.com'
     end
+
     mail(subject: defn[:subject],
          to: defn[:to],
+         cc: defn[:cc],
          bcc: defn[:bcc])
   end
 
@@ -60,13 +70,16 @@ class Mailer < ActionMailer::Base
   end
 
   def delayed_message(defn)
-    emails = User.where(["role_id = ?",2], ["copy_of_all_emails = ?", true]).select("email")
-    ins_email = []
-    ins_email << emails
+    condition = User.where(["role_id = ?", 2]).select("copy_of_all_emails = ?")
+    if condition == true
+      defn[:cc] = User.where(["role_id = ? and copy_of_all_emails = ?", 2,true]).select("email")
+    end
+    else
+      defn[:cc] = 'expertiza.development@gmail.com'
     ret = mail(subject: defn[:subject],
                body: defn[:body],
                content_type: "text/html",
-               cc: ins_email,
+               cc: defn[:cc],
                bcc: defn[:bcc])
     CUSTOM_LOGGER.info(ret.encoded.to_s)
   end

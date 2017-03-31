9caa9a7fbfedd6e69f8dde128b3e8e668efcd5e5
balesj <jabales@ncsu.edu>
2017-03-31 19:03:40 -0400

    Cleaned up some duplicated code and a useless assignment.

--- spec/features/heat_map_spec.rb
+++ spec/features/heat_map_spec.rb
@@ -3,7 +3,6 @@ require 'selenium-webdriver'
 
 describe 'Student can view review scores in a heat map distribution', js: true do
   before(:each) do
-    # Include setup here for pre-test stuff
     create(:assignment, name: "TestAssignment", directory_path: 'test_assignment')
     create_list(:participant, 3)
     create(:assignment_node)
@@ -31,7 +30,6 @@ describe 'Student can view review scores in a heat map distribution', js: true d
 
   def create_review
     # NOTE: this function does not work since it stubs: login_as('student2064')
-    user = User.find_by_name('student2064')
     visit root_path
     fill_in 'login_name', with: 'student2064'
     fill_in 'login_password', with: 'password'
@@ -106,34 +104,6 @@ describe 'Student can view review scores in a heat map distribution', js: true d
     # expect(page).to have_content('Question')
 
     expect(page).to have_content('toggle question list')
-  end
-end
-
-describe 'Student does not have scores to show in a heat map distribution', js: true do
-  before(:each) do
-    create(:assignment, name: "TestAssignment", directory_path: 'test_assignment')
-    create_list(:participant, 3)
-    create(:assignment_node)
-    create(:deadline_type, name: "submission")
-    create(:deadline_type, name: "review")
-    create(:deadline_type, name: "metareview")
-    create(:deadline_type, name: "drop_topic")
-    create(:deadline_type, name: "signup")
-    create(:deadline_type, name: "team_formation")
-    create(:deadline_right)
-    create(:deadline_right, name: 'Late')
-    create(:deadline_right, name: 'OK')
-    create(:assignment_due_date, deadline_type: DeadlineType.where(name: 'review').first, due_at: Time.now.in_time_zone + 1.day)
-    create(:topic)
-    create(:topic, topic_name: "TestReview")
-    create(:team_user, user: User.where(role_id: 2).first)
-    create(:team_user, user: User.where(role_id: 2).second)
-    create(:assignment_team)
-    create(:team_user, user: User.where(role_id: 2).third, team: AssignmentTeam.second)
-    create(:signed_up_team)
-    create(:signed_up_team, team_id: 2, topic: SignUpTopic.second)
-    create(:assignment_questionnaire)
-    create(:question)
   end
 
   it 'should show an empty table with no reviews' do

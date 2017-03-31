f91b0452a4dc006fa729c03f0fb8a07bb35da223
myelava <myelava@ncsu.edu>
2017-03-30 22:51:33 -0400

    import file helper fix

--- app/helpers/import_file_helper.rb
+++ app/helpers/import_file_helper.rb
@@ -19,5 +19,19 @@ module ImportFileHelper
     user.timezonepref = User.find(user.parent_id).timezonepref
     user.save!
     user
+    instructor_email = User.where(["role_id = ?", 2]).select("email").first
+    send_email_to_instructor(instructor_email)
   end
+
+  def send_email_to_instructor(instructor_email)
+    Mailer.suggested_topic(
+        to: instructor_email,
+        #cc: cc_mail_list,
+        subject: "A new topic named '#{@suggestion.title}' has been suggested",
+        body: {
+            suggested_topic_name: @suggestion.title,
+            proposer: @user_id
+        }
+    ).deliver_now!
+    end
 end

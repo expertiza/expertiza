f7048c9a480718c47ad7d8ee4d17d91df7419982
Matthew Dunlap <mdunlap@ncsu.edu>
2017-03-31 21:00:59 -0400

    Refactor scheduled_task_spec and delayed_mailed_spec

--- spec/features/delayed_mailer_spec.rb
+++ spec/features/delayed_mailer_spec.rb
@@ -1,79 +1,58 @@
 require 'rails_helper'
-def enqueue_delayed_job(stage)
-  # due_at = DateTime.now.in_time_zone + 120
-  # seconds_until_due = due_at - Time.now
-  # minutes_until_due = seconds_until_due / 60
-  # @name = "user"
-  # id = 2
+
+def get_time_to_run()
   due_at = DateTime.now.in_time_zone + 2.minutes
-  # puts DateTime.now.in_time_zone
-  # puts due_at
   due_at1 = Time.zone.parse(due_at.to_s(:db))
   curr_time = DateTime.now.in_time_zone.to_s(:db)
   curr_time = Time.zone.parse(curr_time)
-  time_in_min = ((due_at1 - curr_time).to_i / 60) * 60
+  ((due_at1 - curr_time).to_i / 60) * 60
+end
+
+def enqueue_delayed_job(stage)
+  id = 2
   Delayed::Job.delete_all
   expect(Delayed::Job.count).to eq(0)
-  Delayed::Job.enqueue(payload_object: DelayedMailer.new(id, stage, due_at), priority: 1, run_at: time_in_min)
+  Delayed::Job.enqueue(payload_object: DelayedMailer.new(id, stage, DateTime.now.in_time_zone + 2.minutes), priority: 1, run_at: get_time_to_run)
+end
+
+def send_reminder_email(email_type)
+  enqueue_delayed_job(email_type)
+  expect(Delayed::Job.count).to eq(1)
+  expect(Delayed::Job.last.handler).to include("deadline_type: " + email_type)
 end
 
 describe 'Submission deadline reminder email' do
   it 'is able to send reminder email for submission deadline to signed-up users ' do
-    # Delayed::Worker.delay_jobs = false
-    # id = 2
-    # @name = "user"
-    enqueue_delayed_job("submission")
-    expect(Delayed::Job.count).to eq(1)
-    expect(Delayed::Job.last.handler).to include("deadline_type: submission")
+    send_reminder_email("submission")
   end
 end
 
 describe 'Review deadline reminder email' do
   it 'is able to send reminder email for review deadline to reviewers ' do
-    # id = 2
-    # @name = "user"
-    enqueue_delayed_job("review")
-    expect(Delayed::Job.count).to eq(1)
-    expect(Delayed::Job.last.handler).to include("deadline_type: review")
+    send_reminder_email("review")
   end
 end
 
 describe 'Metareview deadline reminder email' do
   it 'is able to send reminder email for Metareview deadline to reviewers ' do
-    # id = 2
-    @name = "user"
-    enqueue_delayed_job("metareview")
-    expect(Delayed::Job.count).to eq(1)
-    expect(Delayed::Job.last.handler).to include("deadline_type: metareview")
+    send_reminder_email("metareview")
   end
 end
 
 describe 'Drop Topic deadline reminder email' do
   it 'is able to send reminder email for drop topic deadline to reviewers ' do
-    # id = 2
-    # @name = "user"
-    enqueue_delayed_job("drop_topic")
-    expect(Delayed::Job.count).to eq(1)
-    expect(Delayed::Job.last.handler).to include("deadline_type: drop_topic")
+    send_reminder_email("drop_topic")
   end
 end
 
 describe 'Signup deadline reminder email' do
   it 'is able to send reminder email for signup deadline to reviewers ' do
-    # id = 2
-    # @name = "user"
-    enqueue_delayed_job("signup")
-    expect(Delayed::Job.count).to eq(1)
-    expect(Delayed::Job.last.handler).to include("deadline_type: signup")
+    send_reminder_email("signup")
   end
 end
 
 describe 'Team formation deadline reminder email' do
   it 'is able to send reminder email for team formation deadline to reviewers ' do
-    # id = 2
-    # @name = "user"
-    enqueue_delayed_job("team_formation")
-    expect(Delayed::Job.count).to eq(1)
-    expect(Delayed::Job.last.handler).to include("deadline_type: team_formation")
+    send_reminder_email("team_formation")
   end
 end
--- spec/features/scheduled_task_spec.rb
+++ spec/features/scheduled_task_spec.rb
@@ -1,86 +1,61 @@
 require 'rails_helper'
-def enqueue_scheduled_tasks(stage)
-  # due_at = DateTime.now.in_time_zone + 120
-  # seconds_until_due = due_at - Time.now
-  # minutes_until_due = seconds_until_due / 60
-  # id = 2
-  # @name = "user"
+
+def get_time_to_run()
   due_at = DateTime.now.in_time_zone + 1.day
-  # puts DateTime.now.in_time_zone
-  # puts due_at
   due_at1 = Time.zone.parse(due_at.to_s(:db))
   curr_time = DateTime.now.in_time_zone.to_s(:db)
   curr_time = Time.zone.parse(curr_time)
-  time_in_min = ((due_at1 - curr_time).to_i / 60) * 60
+  ((due_at1 - curr_time).to_i / 60) * 60
+end
+
+def enqueue_scheduled_tasks(stage)
+  id = 2
   Delayed::Job.delete_all
   expect(Delayed::Job.count).to eq(0)
-  Delayed::Job.enqueue(payload_object: ScheduledTask.new(id, stage, due_at), priority: 1, run_at: time_in_min)
+  Delayed::Job.enqueue(payload_object: ScheduledTask.new(id, stage, DateTime.now.in_time_zone + 1.day), priority: 1, run_at: get_time_to_run)
+end
+
+def send_reminder_email(email_type)
+  enqueue_scheduled_tasks(email_type)
+  expect(Delayed::Job.count).to eq(1)
+  expect(Delayed::Job.last.handler).to include("deadline_type: " + email_type)
 end
 
 describe 'Submission deadline reminder email' do
   it 'is able to send reminder email for submission deadline to signed-up users ' do
-    # Delayed::Worker.delay_jobs = false
-    # id = 2
-    # @name = "user"
-    enqueue_scheduled_tasks("submission")
-    expect(Delayed::Job.count).to eq(1)
-    expect(Delayed::Job.last.handler).to include("deadline_type: submission")
+    send_reminder_email("submission")
   end
 end
 
 describe 'Review deadline reminder email' do
   it 'is able to send reminder email for review deadline to reviewers ' do
-    # id = 2
-    # @name = "user"
-    enqueue_scheduled_tasks("drop_outstanding_reviews")
-    expect(Delayed::Job.count).to eq(1)
-    expect(Delayed::Job.last.handler).to include("deadline_type: drop_outstanding_reviews")
+    send_reminder_email("drop_outstanding_reviews")
   end
 end
 
 describe 'Metareview deadline reminder email' do
   it 'is able to send reminder email for Metareview deadline to reviewers ' do
-    # id = 2
-    # @name = "user"
-    enqueue_scheduled_tasks("metareview")
-    expect(Delayed::Job.count).to eq(1)
-    expect(Delayed::Job.last.handler).to include("deadline_type: metareview")
+    send_reminder_email("metareview")
   end
 end
 
 describe 'Drop Topic deadline reminder email' do
   it 'is able to send reminder email for drop topic deadline to reviewers ' do
-    # id = 2
-    # @name = "user"
-    enqueue_scheduled_tasks("drop_topic")
-    expect(Delayed::Job.count).to eq(1)
-    expect(Delayed::Job.last.handler).to include("deadline_type: drop_topic")
+    send_reminder_email("drop_topic")
   end
 end
 
 describe 'Signup deadline reminder email' do
   it 'is able to send reminder email for signup deadline to reviewers ' do
-    # id = 2
-    # @name = "user"
-    enqueue_scheduled_tasks("signup")
-    expect(Delayed::Job.count).to eq(1)
-    expect(Delayed::Job.last.handler).to include("deadline_type: signup")
+    send_reminder_email("signup")
   end
 end
 
 describe 'Team formation deadline reminder email' do
   it 'is able to send reminder email for team formation deadline to reviewers ' do
-    # id = 2
-    # @name = "user"
-    enqueue_scheduled_tasks("team_formation")
-    expect(Delayed::Job.count).to eq(1)
-    expect(Delayed::Job.last.handler).to include("deadline_type: team_formation")
+    send_reminder_email("team_formation")
     due_at = DateTime.now.in_time_zone + 1.day
-    due_at1 = Time.zone.parse(due_at.to_s(:db))
-    curr_time = DateTime.now.in_time_zone.to_s(:db)
-    curr_time = Time.zone.parse(curr_time)
-    time_in_min = ((due_at1 - curr_time).to_i / 60) * 60
-    Delayed::Job.enqueue(payload_object: ScheduledTask.new(2, "drop_one_member_topics", due_at), priority: 1, run_at: time_in_min)
+    Delayed::Job.enqueue(payload_object: ScheduledTask.new(2, "drop_one_member_topics", due_at), priority: 1, run_at: get_time_to_run)
     expect(Delayed::Job.count).to eq(2)
     expect(Delayed::Job.last.handler).to include("deadline_type: drop_one_member_topics")
   end

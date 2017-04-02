d72e9c1edba3a28a4e5da3cc88f6ff10d9a38c07
rmyers <rmmaily@gmail.com>
2017-04-01 23:20:32 -0400

    Fixed specs

--- spec/controllers/notifications_controller_spec.rb
+++ spec/controllers/notifications_controller_spec.rb
@@ -1,41 +1,37 @@
 require 'rails_helper'
 
-
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
- describe NotificationsController do
-  test "should get index" do
-    get notification_url
-    assert_response :success
-  end
-  
-
-  test "should create notification" do
-    assert_difference('Notification.count') do
-      post notification_url
+  describe NotificationsController do
+    test "should get index" do
+      get notification_url
+      assert_response :success
     end
- 
-    assert_redirected_to notification_path(Article.last)
-    assert_equal 'Notification was successfully created.', flash[:notice]
-  end
 
-  
-  test "should update notification" do
-    notification = notification(:one)
- 
-    patch notification_url(notification), params: { article: { title: "updated" } }
- 
-    assert_redirected_to notification_path(notification)
-    notification.reload
-    assert_equal 'Notification was successfully created.', flash[:notice]
-  end
+    test "should create notification" do
+      assert_difference('Notification.count') do
+        post notification_url
+      end
 
-  
-  test "should destroy notification" do
+      assert_redirected_to notification_path(Article.last)
+      assert_equal 'Notification was successfully created.', flash[:notice]
+    end
+
+    test "should update notification" do
+      notification = notification(:one)
+
+      patch notification_url(notification), params: {article: {title: "updated"}}
+ 
+      assert_redirected_to notification_path(notification)
+      notification.reload
+      assert_equal 'Notification was successfully created.', flash[:notice]
+    end
+
+    test "should destroy notification" do
       assert_difference('notification.count', -1) do
         delete notification_url(@notification)
       end
- 
+
       assert_redirected_to notification_path
     end
-   end
- end
+  end
+end

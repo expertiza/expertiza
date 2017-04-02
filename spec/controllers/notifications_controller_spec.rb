0118456351582e3577f059725b3d57737ca8f203
rmyers <rmmaily@gmail.com>
2017-04-02 05:45:27 -0400

    Fixing specs

--- spec/controllers/notifications_controller_spec.rb
+++ spec/controllers/notifications_controller_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 describe NotificationsController do
   it "#run_get_notification" do
-    user = build(:student)
-    stub_current_user(user, user.role.name, user.role)
     get "run_get_notification"
     expect(response).to redirect_to('/')
   end

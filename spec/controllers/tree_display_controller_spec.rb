6a0e7dcab666921e4493b29e13250e14180bc7f6
rmyers <rmmaily@gmail.com>
2017-04-02 04:10:55 -0400

    Fixing specs

--- spec/controllers/tree_display_controller_spec.rb
+++ spec/controllers/tree_display_controller_spec.rb
@@ -23,114 +23,110 @@ describe TreeDisplayController do
     end
   end
 
-  decribe "#confirm" do
-    it "should confirm user type"
-    post :confirm, id: {nodeType: 'instructor'}
-  end
-end
-describe "#ta_for_current_mappings?" do
-  it "should return true if current user is a TA for current course" do
-    allow(session[:user]).to receive("ta?").and_return(true)
-  end
-end
-
-describe "#populate_rows" do
-  let(:dbl) { double }
-  before { expect(dbl).to receive(:populate_rows).with(Hash, String) }
-  it "passes when the arguments match" do
-    dbl.populate_rows({}, "")
-  end
-end
-
-describe "#populate_1_row" do
-  let(:dbl) { double }
-  before { expect(dbl).to receive(:populate_1_row).with(Node) }
-  it "passes when the arguments match" do
-    dbl.populate_1_row(Node.new)
-  end
-end
-
-describe "#drill" do
-  it "redirect to list action" do
-    get "drill", root: 1
-    expect(session[:root]).to eq('1')
-    expect(response).to redirect_to(list_tree_display_index_path)
-  end
-end
-
-describe "GET #folder_node_ng_getter" do
-  before(:each) do
-    @treefolder = TreeFolder.new
-    @treefolder.parent_id = nil
-    @treefolder.name = "Courses"
-    @treefolder.child_type = "CourseNode"
-    @treefolder.save
-    @foldernode = FolderNode.new
-    @foldernode.parent_id = nil
-    @foldernode.type = "FolderNode"
-  end
-  it "populates a list of FolderNodes when there is a match" do
-    @foldernode.node_object_id = 1
-    @foldernode.save
-    get :folder_node_ng_getter
-    expect(response.body).to match [@foldernode].to_json
-  end
-  it "populates an empty list when there is no match" do
-    @foldernode.node_object_id = 2
-    @foldernode.save
-    get :folder_node_ng_getter
-    expect(response.body).to eq "[]"
-  end
-end
-it { is_expected.to respond_to(:folder_node_ng_getter) }
-it { is_expected.to respond_to(:children_node_ng) }
-it { is_expected.to respond_to(:children_node_2_ng) }
-
-describe "GET #session_last_open_tab" do
-  it "returns HTTP status 200" do
-    get :session_last_open_tab
-    expect(response).to have_http_status(200)
-  end
-end
-
-describe "POST #children_node_ng" do
-  before(:each) do
-    @treefolder = TreeFolder.new
-    @treefolder.parent_id = nil
-    @treefolder.name = "Courses"
-    @treefolder.child_type = "CourseNode"
-    @treefolder.save
-    @foldernode = FolderNode.new
-    @foldernode.parent_id = nil
-    @foldernode.type = "FolderNode"
-    @foldernode.node_object_id = 1
-    @foldernode.save
-    @course = create(:course)
-    create(:assignment)
-    create(:assignment_node)
-    create(:course_node)
-    @instructor = User.where(role_id: 1).first
+  describe "#ta_for_current_mappings?" do
+    it "should return true if current user is a TA for current course" do
+      allow(session[:user]).to receive("ta?").and_return(true)
+    end
   end
 
-  it "returns a list of course objects(private) as json" do
-    params = FolderNode.all
-    post :children_node_ng, {reactParams: {child_nodes: params.to_json, nodeType: "FolderNode"}}, user: @instructor
-    expect(response.body).to match /csc517\/test/
+  describe "#populate_rows" do
+    let(:dbl) { double }
+    before { expect(dbl).to receive(:populate_rows).with(Hash, String) }
+    it "passes when the arguments match" do
+      dbl.populate_rows({}, "")
+    end
   end
 
-  it "returns an empty list when there are no private or public courses" do
-    params = FolderNode.all
-    Assignment.delete(1)
-    Course.delete(1)
-    post :children_node_ng, {reactParams: {child_nodes: params.to_json, nodeType: "FolderNode"}}, user: @instructor
-    expect(response.body).to eq "{\"Courses\":[]}"
+  describe "#populate_1_row" do
+    let(:dbl) { double }
+    before { expect(dbl).to receive(:populate_1_row).with(Node) }
+    it "passes when the arguments match" do
+      dbl.populate_1_row(Node.new)
+    end
   end
 
-  it "returns a list of course objects(public) as json" do
-    params = FolderNode.all
-    @course.private = false
-    @course.save
-    post :children_node_ng, {reactParams: {child_nodes: params.to_json, nodeType: "FolderNode"}}, user: @instructor
-    expect(response.body).to match /csc517\/test/
+  describe "#drill" do
+    it "redirect to list action" do
+      get "drill", root: 1
+      expect(session[:root]).to eq('1')
+      expect(response).to redirect_to(list_tree_display_index_path)
+    end
+  end
+
+  describe "GET #folder_node_ng_getter" do
+    before(:each) do
+      @treefolder = TreeFolder.new
+      @treefolder.parent_id = nil
+      @treefolder.name = "Courses"
+      @treefolder.child_type = "CourseNode"
+      @treefolder.save
+      @foldernode = FolderNode.new
+      @foldernode.parent_id = nil
+      @foldernode.type = "FolderNode"
+    end
+    it "populates a list of FolderNodes when there is a match" do
+      @foldernode.node_object_id = 1
+      @foldernode.save
+      get :folder_node_ng_getter
+      expect(response.body).to match [@foldernode].to_json
+    end
+    it "populates an empty list when there is no match" do
+      @foldernode.node_object_id = 2
+      @foldernode.save
+      get :folder_node_ng_getter
+      expect(response.body).to eq "[]"
+    end
+  end
+  it { is_expected.to respond_to(:folder_node_ng_getter) }
+  it { is_expected.to respond_to(:children_node_ng) }
+  it { is_expected.to respond_to(:children_node_2_ng) }
+
+  describe "GET #session_last_open_tab" do
+    it "returns HTTP status 200" do
+      get :session_last_open_tab
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "POST #children_node_ng" do
+    before(:each) do
+      @treefolder = TreeFolder.new
+      @treefolder.parent_id = nil
+      @treefolder.name = "Courses"
+      @treefolder.child_type = "CourseNode"
+      @treefolder.save
+      @foldernode = FolderNode.new
+      @foldernode.parent_id = nil
+      @foldernode.type = "FolderNode"
+      @foldernode.node_object_id = 1
+      @foldernode.save
+      @course = create(:course)
+      create(:assignment)
+      create(:assignment_node)
+      create(:course_node)
+      @instructor = User.where(role_id: 1).first
+    end
+
+    it "returns a list of course objects(private) as json" do
+      params = FolderNode.all
+      post :children_node_ng, {reactParams: {child_nodes: params.to_json, nodeType: "FolderNode"}}, user: @instructor
+      expect(response.body).to match /csc517\/test/
+    end
+
+    it "returns an empty list when there are no private or public courses" do
+      params = FolderNode.all
+      Assignment.delete(1)
+      Course.delete(1)
+      post :children_node_ng, {reactParams: {child_nodes: params.to_json, nodeType: "FolderNode"}}, user: @instructor
+      expect(response.body).to eq "{\"Courses\":[]}"
+    end
+
+    it "returns a list of course objects(public) as json" do
+      params = FolderNode.all
+      @course.private = false
+      @course.save
+      post :children_node_ng, {reactParams: {child_nodes: params.to_json, nodeType: "FolderNode"}}, user: @instructor
+      expect(response.body).to match /csc517\/test/
+    end
   end
 end

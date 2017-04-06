949b6c257d68c00769525dfa547fc6e38c3d2d67
ferry <ferryxo@yahoo.com>
2017-04-06 01:24:36 -0400

    revert changes to schema.rb (somehow simicheck table was included in the schema).

--- db/schema.rb
+++ db/schema.rb
@@ -317,6 +317,12 @@ ActiveRecord::Schema.define(version: 20170404122028) do
 
   add_index "participants", ["user_id"], name: "fk_participant_users", using: :btree
 
+  create_table "password_resets", force: :cascade do |t|
+    t.string   "user_email", limit: 255
+    t.string   "token",      limit: 255
+    t.datetime "updated_at"
+  end
+
   create_table "permissions", force: :cascade do |t|
     t.string "name", limit: 255, default: "", null: false
   end
@@ -514,16 +520,6 @@ ActiveRecord::Schema.define(version: 20170404122028) do
 
   add_index "signed_up_teams", ["topic_id"], name: "fk_signed_up_users_sign_up_topics", using: :btree
 
-  create_table "simicheck_comparisons", force: :cascade do |t|
-    t.string   "comparison_key", limit: 255
-    t.string   "file_type",      limit: 255
-    t.integer  "assignment_id",  limit: 4
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-  end
-
-  add_index "simicheck_comparisons", ["assignment_id"], name: "fk_rails_2e87b4a9ae", using: :btree
-
   create_table "site_controllers", force: :cascade do |t|
     t.string  "name",          limit: 255, default: "", null: false
     t.integer "permission_id", limit: 4,   default: 0,  null: false
@@ -706,7 +702,6 @@ ActiveRecord::Schema.define(version: 20170404122028) do
   add_foreign_key "review_grades", "participants"
   add_foreign_key "sign_up_topics", "assignments", name: "fk_sign_up_topics_assignments"
   add_foreign_key "signed_up_teams", "sign_up_topics", column: "topic_id", name: "fk_signed_up_users_sign_up_topics"
-  add_foreign_key "simicheck_comparisons", "assignments", on_delete: :cascade
   add_foreign_key "ta_mappings", "courses", name: "fk_ta_mappings_course_id"
   add_foreign_key "ta_mappings", "users", column: "ta_id", name: "fk_ta_mappings_ta_id"
   add_foreign_key "teams_users", "teams", name: "fk_users_teams"

47230d5dc1d6b76440a0bbe513c994e3d828e60b
rmyers <rmmaily@gmail.com>
2017-04-11 07:14:59 -0400

    Added missing heat_map_spec.rb

--- spec/features/heat_map_spec.rb
+++ spec/features/heat_map_spec.rb
@@ -28,6 +28,27 @@ describe 'Student can view review scores in a heat map distribution', js: true d
     create(:question)
   end
 
+  def create_review
+    # NOTE: this function does not work since it stubs: login_as('student2064')
+    visit root_path
+    fill_in 'login_name', with: 'student559'
+    fill_in 'login_password', with: 'password'
+    click_button 'SIGN IN'
+    click_link "NewAssignment"
+    click_link "Others' work"
+    choose "topic_id"
+    click_button "Request a new submission to review"
+    click_link "Begin"
+    fill_in 'responses[0][comment]', with: 'HelloWorld'
+    select 5, from: "responses[0][score]"
+    click_button 'Submit Review'
+    # click ok on the pop-up box that warns you that responses can not be edited
+    page.driver.browser.switch_to.alert.accept
+    user = User.find_by name: 'student2064'
+    stub_current_user(user, user.role.name, user.role)
+    visit '/student_task/list'
+  end
+
   # it 'should be able to sort by total review score' do
   # This would require us to create several reviews
   # end

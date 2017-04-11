c446d43db70ed45fbc12350fe093cd82795f7cfd
rmyers <rmmaily@gmail.com>
2017-04-11 08:40:19 -0400

    Returning to what is in master

--- spec/features/heat_map_spec.rb
+++ spec/features/heat_map_spec.rb
@@ -3,8 +3,8 @@ require 'selenium-webdriver'
 
 describe 'Student can view review scores in a heat map distribution', js: true do
   before(:each) do
-    create_list(:participant, 3)
     create(:assignment, name: "NewAssignment", directory_path: 'new_assignment')
+    create_list(:participant, 3)
     create(:assignment_node)
     create(:deadline_type, name: "submission")
     create(:deadline_type, name: "review")
@@ -26,6 +26,27 @@ describe 'Student can view review scores in a heat map distribution', js: true d
     create(:signed_up_team, team_id: 2, topic: SignUpTopic.second)
     create(:assignment_questionnaire)
     create(:question)
+  end
+
+  def create_review
+    # NOTE: this function does not work since it stubs: login_as('student2064')
+    visit root_path
+    fill_in 'login_name', with: 'student2064'
+    fill_in 'login_password', with: 'password'
+    click_button 'SIGN IN'
+    click_link "NewAssignment"
+    click_link "Others' work"
+    choose "topic_id"
+    click_button "Request a new submission to review"
+    click_link "Begin"
+    fill_in 'responses[0][comment]', with: 'HelloWorld'
+    select 5, from: "responses[0][score]"
+    click_button 'Submit Review'
+    # click ok on the pop-up box that warns you that responses can not be edited
+    page.driver.browser.switch_to.alert.accept
+    user = User.find_by name: 'student2066'
+    stub_current_user(user, user.role.name, user.role)
+    visit '/student_task/list'
   end
 
   # it 'should be able to sort by total review score' do

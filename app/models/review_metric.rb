ddb4756e13359b604d694add8d550fee9bf4f4f6
rmyers <rmmaily@gmail.com>
2017-04-22 17:51:30 -0400

    Fixing code environment

--- app/models/review_metric.rb
+++ app/models/review_metric.rb
@@ -1,3 +1,4 @@
 class ReviewMetric < ActiveRecord::Base
+  attr_accessible :id, :metric
   has_many :review_metrics_mappings
 end
--- app/models/review_metrics_mapping.rb
+++ app/models/review_metrics_mapping.rb
@@ -1,4 +1,5 @@
 class ReviewMetricsMapping < ActiveRecord::Base
+  attr_accessible :id, :response_id, :metric_id, :value
   has_many :review_metrics
   has_many :responses
 end

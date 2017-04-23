dd18ac4da4d2b970e7c7f73d957456acb3ffd150
Pranav Vaidya <psvaidya@ncsu.edu>
2017-04-23 13:16:25 -0400

    Added compute_total_score method in LocalDbCalc

--- app/models/local_db_calc.rb
+++ app/models/local_db_calc.rb
@@ -1,18 +1,44 @@
 # E1731 changes: New file. Not yet finished with the implementation
 module LocalDbCalc
-  def fetch_total_scores
-    reference_id = ResponseMap.where(reviewee_id: self.id).pluck(:id)
-    total = LocalDbScore.where(response_map_id: reference_id).pluck(:score)
+  def self.compute_total_score(assignment, scores)
+    total = 0
+
+    teams = AssignmentTeam.where(parent_id: assignment.id)
+    teams.each { |team|
+      response_maps = team.review_mappings
+      response_maps.each { |map| total += LocalDbScore.where(response_map_id: map.id).pluck(:score) }
+    }
+
+    total
   end
 
-  def store_total_scores(scores)
-    total = 0
-    self.questionnaires.each { |questionnaire|
-      #round = AssignmentQuestionnaire.find_by_assignment_id_and_questionnaire_id(self.id, questionnaire.id).used_in_round
-       total += questionnaire.get_weighted_score(self, scores) }
+  # To be modified
+  def self.store_total_scores(assignment, scores)
 
-    reference_id = ResponseMap.where(reviewee_id: self.id).pluck(:id)
-    round = AssignmentQuestionnaire.find_by_assignment_id_and_questionnaire_id(self.id, self.questionnaires[0].id).used_in_round
-    LocalDbScore.create(review_type: "ReviewLocalDBScore", round: round, score: total, response_map_id: reference_id)
+#    teams = AssignmentTeam.where(parent_id: assignment.id)
+
+#    assignment.questionnaires.each { |questionnaire|
+#      round = AssignmentQuestionnaire.find_by_assignment_id_and_questionnaire_id(assignment.id, questionnaire.id).used_in_round
+#      questionnaire_symbol = if !round.nil?
+#                               (questionnaire.symbol.to_s + round.to_s).to_sym
+#                             else
+#                               questionnaire.symbol
+#                             end
+
+#      aq = questionnaire.assignment_questionnaires.find_by_assignment_id(assignment.id)
+#      score = 0
+#      if !scores[symbol][:scores][:avg].nil?
+#        score = scores[symbol][:scores][:avg] * aq.questionnaire_weight / 100.0
+#      end
+
+#      response_map_id = ResponseMap.where(reviewee_id: assignment.id).pluck(:id)
+
+#      if(LocalDbScore.where(response_map_id: response_map_id, round: round).nil?)
+#        LocalDbScore.create(review_type: "ReviewLocalDBScore", round: round, score: score, response_map_id: reference_id)
+#      else
+#        LocalDbScore.update # add score
+#      end
+#    }
+
   end
 end

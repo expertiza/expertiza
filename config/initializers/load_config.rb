bbf63e2d4d27d0d791da0fd8ef1eeb8da2535e3b
rmyers <rmmaily@gmail.com>
2017-04-24 17:13:51 -0400

    Updating yml linkage

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,2 +1,3 @@
 GOOGLE_CONFIG = YAML.load_file("#{Rails.root}/config/google_auth.yml")[Rails.env]
 WEBSERVICE_CONFIG = YAML.load_file("#{Rails.root}/config/webservices.yml")[Rails.env]
+TEXT_METRICS_KEYWORDS = YAML.load_file("#{Rails.root}/config/text_metrics_keywords.yml")[Rails.env]

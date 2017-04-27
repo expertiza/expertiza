a765eee970ed6bc5097bb0ac14fd232fb7f648ff
Expertiza Developer <bradford.ingersoll@gmail.com>
2017-04-26 22:09:32 -0400

    create_new_assignment_submission method - needs testing

--- app/models/plagiarism_checker_helper.rb
+++ app/models/plagiarism_checker_helper.rb
@@ -3,11 +3,15 @@ class PlagiarismCheckerHelper
   # PlagiarismCheckerHelper acts as the integration point between all services and models
   # related to PlagiarismChecker
 
-  # Returns ID of new submission?
+  # Create a new PlagiarismCheckerAssignmentSubmission
   def self.create_new_assignment_submission(submission_name = '')
+    # Start by creating a new assignment submission
     response = SimiCheckWebService.new_comparison(submission_name)
     json_response = JSON.parse(response.body)
-    return json_response["id"]
+    submission_name = json_response["name"]
+    submission_id = json_response["id"]
+    assignment_submission = PlagiarismCheckerAssignmentSubmission.new(name: submission_name, simicheck_id: submission_id)
+    assignment_submission.save!
   end
 
 end

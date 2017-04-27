ff4d417fea8c12808d776c6992a5e856081977a3
Expertiza Developer <bradford.ingersoll@gmail.com>
2017-04-26 21:53:35 -0400

    Added comment to PlagiarismCheckerHelper

--- app/models/plagiarism_checker_helper.rb
+++ app/models/plagiarism_checker_helper.rb
@@ -1,5 +1,8 @@
 class PlagiarismCheckerHelper
 
+  # PlagiarismCheckerHelper acts as the integration point between all services and models
+  # related to PlagiarismChecker
+
   # Returns ID of new submission?
   def self.create_new_assignment_submission(submission_name = '')
     response = SimiCheckWebService.new_comparison(submission_name)

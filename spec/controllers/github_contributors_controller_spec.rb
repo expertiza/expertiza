2fd696bfc72957a7bf750b1aa328b4c5198f1653
George Mathew <george.meg91@gmail.com>
2017-04-28 21:07:25 -0400

    DRY test files

--- spec/controllers/github_contributors_controller_spec.rb
+++ spec/controllers/github_contributors_controller_spec.rb
@@ -24,48 +24,37 @@ describe GithubContributorsController do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@instructor)
     allow_any_instance_of(ApplicationController).to receive(:current_role_name).and_return('Instructor')
     allow_any_instance_of(ApplicationController).to receive(:current_role).and_return(@role)
+    allow(AssignmentTeam).to receive(:find).with(@assignment_team.id).and_return @assignment_team
+    allow(Assignment).to receive(:find).with(@assignment.id).and_return @assignment
   end
 
   describe '#show_github_contributor' do
     let(:github_contributors_controller) { GithubContributorsController.new }
     it 'valid_submission' do
-      @sub_record.content = 'https://github.com/mozilla/geckodriver'
-      @sub_record.operation = 'Submit Hyperlink'
-      @sub_record.save
-      allow(AssignmentTeam).to receive(:find).with(@assignment_team.id).and_return @assignment_team
-      allow(Assignment).to receive(:find).with(@assignment.id).and_return @assignment
-      get 'show', {id: @sub_record.id}
+      update_submission_record(@sub_record, 'https://github.com/mozilla/geckodriver', 'Submit Hyperlink')
       expect(response).to have_http_status(200)
     end
 
     it 'invalid_submission#1' do
-      @sub_record.content = 'Some Random File.pdf'
-      @sub_record.operation = 'Submit File'
-      @sub_record.save
-      allow(AssignmentTeam).to receive(:find).with(@assignment_team.id).and_return @assignment_team
-      allow(Assignment).to receive(:find).with(@assignment.id).and_return @assignment
-      get 'show', {id: @sub_record.id}
+      update_submission_record(@sub_record, 'Some Random File.pdf', 'Submit File')
       expect(response).to redirect_to('/')
     end
 
     it 'invalid_submission#2' do
-      @sub_record.content = 'https://wikipedia.org'
-      @sub_record.operation = 'Submit Hyperlink'
-      @sub_record.save
-      allow(AssignmentTeam).to receive(:find).with(@assignment_team.id).and_return @assignment_team
-      allow(Assignment).to receive(:find).with(@assignment.id).and_return @assignment
-      get 'show', {id: @sub_record.id}
+      update_submission_record(@sub_record, 'https://wikipedia.org', 'Submit Hyperlink')
       expect(response).to render_template('github_contributors/not_found')
     end
 
     it 'invalid_submission#3' do
-      @sub_record.content = 'https://github.com/ai-se/citemap'
-      @sub_record.operation = 'Submit Hyperlink'
-      @sub_record.save
-      allow(AssignmentTeam).to receive(:find).with(@assignment_team.id).and_return @assignment_team
-      allow(Assignment).to receive(:find).with(@assignment.id).and_return @assignment
-      get 'show', {id: @sub_record.id}
+      update_submission_record(@sub_record, 'https://github.com/ai-se/citemap', 'Submit Hyperlink')
       expect(response).to render_template('github_contributors/not_found')
     end
   end
+end
+
+def update_submission_record(submission_record, content, operation)
+  submission_record.content = content
+  submission_record.operation = operation
+  submission_record.save
+  get 'show', {id: @sub_record.id}
 end

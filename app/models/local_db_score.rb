e88d965667bff28a205b8a5ce10a94f9f84d772c
Pranav Vaidya <psvaidya@ncsu.edu>
2017-04-28 21:15:32 -0400

    Fixing codeclimate issue

--- app/models/local_db_score.rb
+++ app/models/local_db_score.rb
@@ -1,4 +1,5 @@
 # E1731 changes: New table local_db_scores created
 class LocalDbScore < ActiveRecord::Base
   belongs_to :response_map
+  attr_accessible :score_type, :round, :score, :response_map_id
 end

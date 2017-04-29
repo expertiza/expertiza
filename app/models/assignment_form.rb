22598e5b7dab15cf1884cbd3ebca9aede4386449
ruthvik1994 <mruthvik1994@gmail.com>
2017-04-29 18:22:28 -0400

    minor edit

--- app/models/assignment_form.rb
+++ app/models/assignment_form.rb
@@ -5,10 +5,7 @@ class AssignmentForm
   attr_accessor :errors
 
   DEFAULT_MAX_TEAM_SIZE = 1
-
-  include PublicActivity::Model
-  tracked owner: ->(controller, model) { controller && controller.current_user }
-
+  
   def initialize(args = {})
     @assignment = Assignment.new(args[:assignment])
     if args[:assignment].nil?

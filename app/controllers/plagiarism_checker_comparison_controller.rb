f028a051b1fb468b455aea105870d89c68373766
misseill <misseill@gmail.com>
2017-04-29 01:29:51 -0400

    add team.find to ui

--- app/controllers/plagiarism_checker_comparison_controller.rb
+++ app/controllers/plagiarism_checker_comparison_controller.rb
@@ -1,7 +1,5 @@
 class PlagiarismCheckerComparisonController < ApplicationController
 
   def index
-    plagiarism_checker_assignment_submission_id = session[:plagiarism_checker_assignment_submission_id]
-    @PlagiarismCheckerComparisons = PlagiarismCheckerComparison.where(plagiarism_checker_assignment_submission_id: plagiarism_checker_assignment_submission_id)
   end
 end

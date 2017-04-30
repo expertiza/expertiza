a77c0afe6685cfa0e7822445400ba0dac8d50a17
Nephi Grant <nephdog@gmail.com>
2017-04-30 02:38:45 -0400

    added semicolon

--- app/controllers/plagiarism_checker_comparison_controller.rb
+++ app/controllers/plagiarism_checker_comparison_controller.rb
@@ -2,7 +2,7 @@ class PlagiarismCheckerComparisonController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:save_results]
   skip_before_action :authorize, only: [:save_results]
 
-  def index end
+  def index; end
 
   def save_results
     assignment_submission_id = params[:id]

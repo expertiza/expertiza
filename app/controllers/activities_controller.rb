f8b9c88bbbb9ff38157ea3bfbc66f3a5bdb04789
ruthvik1994 <mruthvik1994@gmail.com>
2017-05-01 00:54:38 -0400

    PublicActivity: Search functionality added

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,10 +1,29 @@
 class ActivitiesController < ApplicationController
+
   def action_allowed?
     ['Administrator',
      'Instructor',
      'Teaching Assistant'].include? current_user.role.name
   end
+
   def index
+    user = params[:user]
+    activity_time = nil
+    if params[:activity_time]
+      activity_time = params[:activity_time][0]
+    end
     @activities = PublicActivity::Activity.order("created_at desc")
+    unless user.to_s.strip.empty?
+      users = User.where("name like ?", "%#{params[:user]}%")
+      @activities = @activities.where(owner_id: users)
+    end
+    if activity_time
+      @activities = @activities.where("Date(created_at) = ?", activity_time)
+    end
   end
+
+  def search_by_time
+
+  end
+
 end

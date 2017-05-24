7d5a65f806b0b09239772673583e4a563ac6bc92
yangsong8 <ysong8@ncsu.edu>
2017-05-23 21:29:41 -0400

    Refactor E1734: Add foreign keys to AssignmentSurveyDeployment and CourseSurveyDeployment

--- app/models/assignment_survey_deployment.rb
+++ app/models/assignment_survey_deployment.rb
@@ -1,2 +1,7 @@
 class AssignmentSurveyDeployment < SurveyDeployment
+  belongs_to :assignment, class_name: 'Assignment', foreign_key: 'parent_id'
+
+  def parent_name
+    assignment.name
+  end
 end
--- app/models/course_survey_deployment.rb
+++ app/models/course_survey_deployment.rb
@@ -1,2 +1,7 @@
 class CourseSurveyDeployment < SurveyDeployment
+  belongs_to :course, class_name: 'Course', foreign_key: 'parent_id'
+
+  def parent_name
+    course.name
+  end
 end

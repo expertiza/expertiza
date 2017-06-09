dcb64b9121697cebddc345cd8c548109bdf1f002
yangsong8 <ysong8@ncsu.edu>
2017-06-08 20:22:00 -0400

    Fix some pending migrations (mainly to please postgresql)

--- db/migrate/20170428233048_update_survey_questionnaire_to_assignment_survey_questionnaire.rb
+++ db/migrate/20170428233048_update_survey_questionnaire_to_assignment_survey_questionnaire.rb
@@ -1,5 +1,5 @@
 class UpdateSurveyQuestionnaireToAssignmentSurveyQuestionnaire < ActiveRecord::Migration
   def change
-    execute "UPDATE `questionnaires` set type = 'AssignmentSurveyQuestionnaire' where type in ('SurveyQuestionnaire')"
+    execute "UPDATE questionnaires set type = 'AssignmentSurveyQuestionnaire' where type in ('SurveyQuestionnaire')"
   end
 end

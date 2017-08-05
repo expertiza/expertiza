15e281129c9afa439e95f6fb355d35e3496e579b
joydeepm02 <joydeepm02@gmail.com>
2017-08-05 17:31:13 -0400

    Update instructor.rb

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -15,6 +15,7 @@ class Instructor < User
       flash[:error] = "Illegal parameter."
     else
       object_type.where(["instructor_id = ? OR private = 0", user_id])
+    end
   end
 
   def list_mine(object_type, user_id)
@@ -22,6 +23,7 @@ class Instructor < User
       flash[:error] = "Illegal parameter."
     else
       object_type.where(["instructor_id = ?", user_id])
+    end
   end
 
   def get(object_type, id, user_id)

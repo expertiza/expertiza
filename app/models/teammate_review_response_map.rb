fa2b735b3c4989091e11e5c279ee0a2ff88fd89b
joydeepm02 <joydeepm02@gmail.com>
2017-08-06 20:10:14 -0400

    Added type check for security update
    Line 20:
    
    if !id.is_a? Integer
          flash[:error] = "Illegal parameter."

--- app/models/teammate_review_response_map.rb
+++ app/models/teammate_review_response_map.rb
@@ -17,7 +17,11 @@ class TeammateReviewResponseMap < ResponseMap
   def self.teammate_response_report(id)
     # Example query
     # SELECT distinct reviewer_id FROM response_maps where type = 'TeammateReviewResponseMap' and reviewed_object_id = 711
-    @reviewers = ResponseMap.select("DISTINCT reviewer_id").where(["reviewed_object_id = ? and type = ?", id, 'TeammateReviewResponseMap'])
+    if !id.is_a? Integer
+      flash[:error] = "Illegal parameter."
+    else
+      @reviewers = ResponseMap.select("DISTINCT reviewer_id").where(["reviewed_object_id = ? and type = ?", id, 'TeammateReviewResponseMap'])
+    end
   end
 
   # Send Teammate Review Emails

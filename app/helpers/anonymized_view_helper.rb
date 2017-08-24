2fefad37d0bb98fe99439bb28e6590ca0bbb6e4b
saratkavuru <kavurusarat@gmail.com>
2017-08-23 21:48:08 -0400

    Removing minor Code Climate issues

--- app/helpers/anonymized_view_helper.rb
+++ app/helpers/anonymized_view_helper.rb
@@ -1,26 +1,34 @@
 module AnonymizedViewHelper
+  
   def is_anonymous_mode?
     session[:anonymous_mode]
   end
+  
   def anonymize(name)
     ((name.sum)%50)+1
   end
+  
+  #All the below functions will return anonmized names if mode is sset or  else just return name as is.
   def display_anon_fullname(name)
     name_array=name.split(',')
     return AnonymizedLastName.find(anonymize(name_array[-1])).name + ", "+ AnonymizedFirstName.find(anonymize(name_array[0])).name if is_anonymous_mode?
     return name
   end
+  
   def display_anon_name(name)
     return AnonymizedFirstName.find(anonymize(name)).name if is_anonymous_mode?
     return name
   end
+  
   def display_anon_mail(name)
     name_array=name.split('@')
-    return display_anon_handle(name_array[0]) + "@address.com" if is_anonymous_mode?
+    return display_anon_handle(name_array[0]) + "@mailinator.com" if is_anonymous_mode?
     return name
   end
+  
   def display_anon_handle(name)
     return AnonymizedFirstName.find(anonymize(name)).name.downcase if is_anonymous_mode?
     return name
   end
+
 end

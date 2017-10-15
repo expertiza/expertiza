6779a824bb9081f83c54153f8b3f36f838cf77fa
KunmiaoYang <kyang12@ncsu.edu>
2017-10-14 21:42:05 -0400

    Add strong parameter in questions_controller.rb

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -40,7 +40,7 @@ class QuestionsController < ApplicationController
   # Save a question created by the user
   # follows from new
   def create
-    @question = Question.new(params[:question])
+    @question = Question.new(question_params)
     if @question.save
       flash[:notice] = 'The question was successfully created.'
       redirect_to action: 'list'
@@ -58,7 +58,7 @@ class QuestionsController < ApplicationController
   # follows from edit
   def update
     @question = Question.find(params[:id])
-    if @question.update_attributes(params[:question])
+    if @question.update_attributes(question_params)
       flash[:notice] = 'The question was successfully updated.'
       redirect_to action: 'show', id: @question
     else
@@ -78,5 +78,12 @@ class QuestionsController < ApplicationController
       flash[:error] = $ERROR_INFO
     end
     redirect_to edit_questionnaire_path(questionnaire_id.to_s.to_sym)
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:txt, :weight, :questionnaire_id, :seq, :type, :size,
+                                     :alternatives, :break_before, :max_label, :min_label)
   end
 end

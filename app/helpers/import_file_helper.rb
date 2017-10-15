e72d68084b3ac2b9748d19737c4804d6f6059e1e
KunmiaoYang <kyang12@ncsu.edu>
2017-10-15 12:32:08 -0400

    Add strong parameter in import_file_helper.rb

--- app/helpers/import_file_helper.rb
+++ app/helpers/import_file_helper.rb
@@ -14,10 +14,38 @@ module ImportFileHelper
   end
 
   def self.create_new_user(attributes, session)
-    user = User.new(attributes)
+    user = User.new(user_params(attributes))
     user.parent_id = (session[:user]).id
     user.timezonepref = User.find(user.parent_id).timezonepref
     user.save!
     user
+  end
+
+  private
+
+  def user_params(params_hash)
+    params_local = params
+    params_local[:user] = params_hash
+    params_local.require(:user).permit(:name,
+                                       :crypted_password,
+                                       :role_id,
+                                       :password_salt,
+                                       :fullname,
+                                       :email,
+                                       :parent_id,
+                                       :private_by_default,
+                                       :mru_directory_path,
+                                       :email_on_review,
+                                       :email_on_submission,
+                                       :email_on_review_of_review,
+                                       :is_new_user,
+                                       :master_permission_granted,
+                                       :handle,
+                                       :digital_certificate,
+                                       :persistence_token,
+                                       :timezonepref,
+                                       :public_key,
+                                       :copy_of_emails,
+                                       :institution_id)
   end
 end

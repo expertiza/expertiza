5fa73fb1c7c9ad3db68f9ff935c3c6c09cc66d31
Kunal Kulkarni <kunalkul1@gmail.com>
2017-10-15 17:41:30 -0400

    adding topic (not complete)

--- spec/features/team_functionalities_spec.rb
+++ spec/features/team_functionalities_spec.rb
@@ -1,4 +1,24 @@
-describe "create group assignment" do
+require 'rails_helper'
+def create_new_assignment
+  login_as("instructor6")
+  visit "/assignments/new?private=0"
+
+  fill_in 'assignment_form_assignment_name', with: 'public assignment for test'
+  select('Course 2', from: 'assignment_form_assignment_course_id')
+  fill_in 'assignment_form_assignment_directory_path', with: 'testDirectory'
+  fill_in 'assignment_form_assignment_spec_location', with: 'testLocation'
+  check("assignment_form_assignment_microtask")
+  check("team_assignment")
+  fill_in 'assignment_form_assignment_max_team_size', with: '3', visible: false
+  check("assignment_form_assignment_reviews_visible_to_all")
+  check("assignment_form_assignment_is_calibrated")
+  check("assignment_form_assignment_availability_flag")
+  expect(page).to have_select("assignment_form[assignment][reputation_algorithm]", options: ['--', 'Hamer', 'Lauw'])
+
+  click_button 'Create'
+end
+
+describe "create group assignment"  do
   before(:each) do
     create(:deadline_type, name: "submission")
     create(:deadline_type, name: "review")
@@ -15,22 +35,7 @@ describe "create group assignment" do
   end
 
   it "is able to create a public group assignment" do
-    login_as("instructor6")
-    visit "/assignments/new?private=0"
-
-    fill_in 'assignment_form_assignment_name', with: 'public assignment for test'
-    select('Course 2', from: 'assignment_form_assignment_course_id')
-    fill_in 'assignment_form_assignment_directory_path', with: 'testDirectory'
-    fill_in 'assignment_form_assignment_spec_location', with: 'testLocation'
-    check("assignment_form_assignment_microtask")
-    check("team_assignment")
-    fill_in 'assignment_form_assignment_max_team_size', with: '3', visible: false
-    check("assignment_form_assignment_reviews_visible_to_all")
-    check("assignment_form_assignment_is_calibrated")
-    uncheck("assignment_form_assignment_availability_flag")
-    expect(page).to have_select("assignment_form[assignment][reputation_algorithm]", options: ['--', 'Hamer', 'Lauw'])
-
-    click_button 'Create'
+    create_new_assignment
     assignment = Assignment.where(name: 'public assignment for test').first
     expect(assignment).to have_attributes(
                               name: 'public assignment for test',
@@ -39,7 +44,43 @@ describe "create group assignment" do
                               spec_location: 'testLocation',
                               microtask: true,
                               is_calibrated: true,
-                              availability_flag: false
+                              availability_flag: true
                           )
+    visit "/assignments/#{assignment.id}/edit"
+    click_link 'Topics'
+    click_link 'New topic'
+    fill_in 'topic_topic_identifier', with: '112'
+    fill_in 'topic_topic_name', with: 'test_topic_1'
+    fill_in 'topic_category', with: 'test_topic_1'
+    fill_in 'topic_max_choosers', with: 3
+    # fill_in 'topic_link', with: 'test_topic_1'
+    # fill_in 'topic_description', with: 'test_topic_1'
+    click_button 'Create'
+    sign_up_topics = SignUpTopic.where(topic_name: 'test_topic_1').first
+    expect(sign_up_topics).to have_attributes(
+                                  topic_name: 'test_topic_1',
+                                  assignment_id: 1,
+                                  max_choosers: 3,
+                                  topic_identifier: '112',
+                                  category: 'test_topic_1'
+                              )
+    create(:assignment_due_date)
+    # assignment = Assignment.where(name: 'public assignment for test').first
+
+    # expect(page).to have_current_path("/assignments/#{assignment.id}/edit#tabs-5", url: true)
+    # visit "/assignments/#{assignment.id}/edit#tabs-5"
+    # puts current_url
+    # fill_in 'assignment_form_assignment_rounds_of_reviews', with: '1'
+    # fill_in 'datetimepicker_submission_round_1', with: (Time.now.in_time_zone + 1.day).strftime("%Y/%m/%d %H:%M")
+    # fill_in 'datetimepicker_review_round_1', with: (Time.now.in_time_zone + 10.days).strftime("%Y/%m/%d %H:%M")
+    # click_button 'Save'
+    # expect(page).to have_content("The assignment was successfully saved....")
+
   end
+
+  # it "adds topic to assignment" do
+  #   assignment = Assignment.where(name: 'public assignment for test').first
+  #   visit "/assignments/#{assignment.id}/edit"
+  #   puts "/assignments/#{assignment.id}/edit"
+  # end
 end

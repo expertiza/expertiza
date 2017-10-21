01fbd767e60100bf680d103e18db15231afa073c
xzhang72 <xzhang72@ncsu.edu>
2017-10-21 12:57:46 -0400

    Modify find_by_login test 8

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -169,7 +169,7 @@ describe User do
       it 'finds user by email if the local part of email is the same as username' do
         allow(User).to receive(:find_by_email).and_return(nil)
         allow(User).to receive(:where).and_return([{name: 'abc', fullname: 'abc bbc'}])
-        expect(User.find_by_login('abcxyz@gmail.com')).to eq ([name: 'abc', fullname: 'abc bbc'])
+        expect(User.find_by_login('abcxyz@gmail.com')).to eq ({:name=>"abc", :fullname=>"abc bbc"})
       end
     end
   end

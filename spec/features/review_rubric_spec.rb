2e4db1e0cd393f08b6ccf5c56622df7f61a05601
Rahul Oswald Coutinho <rcoutin@ncsu.edu>
2017-10-22 02:56:42 -0400

    Adding questionnaire and dummy question

--- spec/features/review_rubric_spec.rb
+++ spec/features/review_rubric_spec.rb
@@ -1,19 +1,45 @@
 include InstructorInterfaceHelperSpec
 
+# Feature test of rubric advice
 describe "Edit rubric advice" do
   before(:each) do
     assignment_setup
   end
+
+  # Login test
   describe "Instructor login" do
     it "with valid username and password" do
       login_as("instructor6")
       visit '/tree_display/list'
       expect(page).to have_content("Manage content")
     end
-    # # it "accepts changes to rubric advice" do
-    #   login_as("instructor6")
-    #   visit '/questionnaires/115/edit'
-    #   expect(page).to have_content "Edit Review"
-    # end
+
+    it "with invalid username and password" do
+      visit root_path
+      fill_in 'login_name', with: 'instructor6'
+      fill_in 'login_password', with: 'wrongpassword'
+      click_button 'SIGN IN'
+      expect(page).to have_text('Your username or password is incorrect.')
+    end
   end
+
+  def load_questionnaire
+    login_as("instructor6")
+    visit '/questionnaires/new?model=ReviewQuestionnaire&private=0'
+    fill_in('questionnaire_name', with: 'DummyReview')
+    fill_in('questionnaire_min_question_score', with: '0')
+    fill_in('questionnaire_max_question_score', with: '5')
+    select('no', from: 'questionnaire_private')
+    click_button "Create"
+  end
+
+  def load_question question_type
+    load_questionnaire
+    fill_in('question_total_num', with: '1')
+    select(question_type, from: 'question_type')
+    click_button "Add"
+  end
+
+
+
 end

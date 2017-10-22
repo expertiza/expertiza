4a97c63c95edfdf66d5151776a5fa33885a70ffb
xzhang72 <xzhang72@ncsu.edu>
2017-10-22 16:18:48 -0400

    modify get_user_list 8

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -132,7 +132,7 @@ describe User do
         allow(user).to receive_message_chain("role.instructor?") { false }
         allow(user).to receive_message_chain("role.ta?") { false }
         allow(user).to receive_message_chain("role.super_admin?") { false }
-        allow(User).to receive_message_chain("all.find_each").and_yield(user1).and_yield(user2)
+        allow(User).to receive_message_chain("all.find_each").and_yield(user1)
         #user_list = double
         #allow(user_list).to receive(uniq).and_return(user1)
         expect(user.get_user_list()).to eq ([user1,user2])

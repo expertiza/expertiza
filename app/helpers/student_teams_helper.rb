225735eb1fcc296e2d53599e9ea388b28e19ba20
Suvodeep90 <Suvodeep.majumder90@gmail.com>
2017-10-24 00:37:27 -0400

    Changes for bugrandy project

--- app/helpers/student_teams_helper.rb
+++ app/helpers/student_teams_helper.rb
@@ -6,8 +6,8 @@ module StudentTeamsHelper
 
   def user_has_topic?(user_id, assignment_id)
     user_topic = SignUpTopic.joins("INNER JOIN signed_up_teams ON signed_up_teams.topic_id = sign_up_topics.id").
-        joins("INNER JOIN teams_users ON teams_users.team_id = signed_up_teams.team_id").
-        where("sign_up_topics.assignment_id = ? and teams_users.user_id = ?", assignment_id, user_id).uniq
+                              joins("INNER JOIN teams_users ON teams_users.team_id = signed_up_teams.team_id").
+                              where("sign_up_topics.assignment_id = ? and teams_users.user_id = ?", assignment_id, user_id).uniq
     !user_topic.empty?
   end
 end

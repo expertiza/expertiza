e614ec4a056b4ba2c5671dbf3badc96e5839b949
tiansss <tianshi.st1020@gmail.com>
2017-10-24 21:17:51 -0400

    test for #list, #sign_up, #signup_as_instructor_action

--- spec/controllers/sign_up_sheet_controller_spec.rb
+++ spec/controllers/sign_up_sheet_controller_spec.rb
@@ -74,39 +74,102 @@ describe SignUpSheetController do
   end
 
   describe '#list' do
+    let(:params) { { id: '1' } }
+    let(:bids) { [bid] }
+    let(:session){{user:student}}
     context 'when current assignment is intelligent assignment and has submission duedate (deadline_type_id 1)' do
-      it 'renders sign_up_sheet#intelligent_topic_selection page'
+      it 'renders sign_up_sheet#intelligent_topic_selection page' do
+        allow(SignUpTopic).to receive(:find_slots_filled).with(1).and_return(topic)
+        allow(SignUpTopic).to receive(:find_slots_waitlisted).with(1).and_return(topic)
+        allow(SignUpTopic).to receive(:where).with(assignment_id: assignment.id, private_to:nil).and_return([topic])
+        allow(assignment).to receive(:max_team_size).and_return(1)
+        allow(participant).to receive(:team).and_return(team)
+
+        allow(assignment).to receive(:is_intelligent).and_return(true)
+        allow(Bid).to receive(:where).with(team_id: team.try(:id)).and_return(bids)
+        allow(bids).to receive(:order).with(:priority).and_return(bids)
+        allow(SignUpTopic).to receive(:find_by).with(id: bid.topic_id).and_return(topic)
+
+        allow(assignment.due_dates).to receive(:find_by_deadline_type_id).with(7).and_return(nil)
+        allow(assignment.due_dates).to receive(:find_by_deadline_type_id).with(6).and_return(nil)
+        allow(assignment.due_dates).to receive(:find_by_deadline_type_id).with(1).and_return(due_date)
+
+        allow(assignment).to receive(:staggered_deadline).and_return(true)
+        allow(SignedUpTeam).to receive(:find_team_users).with(1,student.id).and_return([])
+
+        get :list, params,session
+        expect(response).to render_template(:intelligent_topic_selection)
+      end
     end
 
-    context 'when current assignment is intelligent assignment and has submission duedate (deadline_type_id 1)' do
-      it 'renders sign_up_sheet#list page'
+    context 'when current assignment is not intelligent assignment and has submission duedate (deadline_type_id 1)' do
+      it 'renders sign_up_sheet#list page' do
+        allow(assignment).to receive(:is_intelligent).and_return(false)
+
+        get :list, params,session
+        expect(response).to render_template(:list)
+      end
     end
   end
 
   describe '#sign_up' do
+    let(:params) { { id: '1' } }
+    let(:session){{user:student}}
     context 'when SignUpSheet.signup_team method return nil' do
-      it 'shows an error flash message and redirects to sign_up_sheet#list page'
+      it 'shows an error flash message and redirects to sign_up_sheet#list page' do
+        allow(SignUpSheet).to receive(:signup_team).with(any_args).and_return(nil)
+        get :sign_up, params,session
+        expect(response).to redirect_to action: 'list', id: params[:id]
+        expect(flash.now[:error]).to eq("You've already signed up for a topic!")
+      end
     end
   end
 
   describe '#signup_as_instructor_action' do
+    let(:params) { { username: '1' } }
     context 'when user cannot be found' do
-      it 'shows an flash error message and redirects to assignment#edit page'
+      it 'shows an flash error message and redirects to assignment#edit page' do
+        allow(User).to receive(:find_by).with(any_args).and_return(nil)
+        get :signup_as_instructor_action, params
+        expect(flash.now[:error]).to eq("That student does not exist!")
+      end
     end
 
-    context 'when user cannot be found' do
+    context 'when user can be found' do
       context 'when an assignment_participant can be found' do
         context 'when creating team related objects successfully' do
-          it 'shows a flash success message and redirects to assignment#edit page'
+          it 'shows a flash success message and redirects to assignment#edit page' do
+            allow(User).to receive(:find_by).with(any_args).and_return(student)
+            allow(AssignmentParticipant).to receive(:exists?).with(any_args).and_return(true)
+            allow(SignUpSheet).to receive(:signup_team).with(any_args).and_return(signed_up_team)
+
+            get :signup_as_instructor_action
+            expect(flash.now[:success]).to eq("You have successfully signed up the student for the topic!")
+          end
         end
 
         context 'when creating team related objects unsuccessfully' do
-          it 'shows a flash error message and redirects to assignment#edit page'
+          it 'shows a flash error message and redirects to assignment#edit page' do
+            allow(User).to receive(:find_by).with(any_args).and_return(student)
+            allow(AssignmentParticipant).to receive(:exists?).with(any_args).and_return(true)
+            allow(SignUpSheet).to receive(:signup_team).with(any_args).and_return(nil)
+
+            get :signup_as_instructor_action
+            expect(flash.now[:error]).to eq("The student has already signed up for a topic!")
+            expect(response).to redirect_to controller: 'assignments', action: 'edit', id: params[:assignment_id]
+          end
         end
       end
 
-      context 'when an assignment_participant can be found' do
-        it 'shows a flash error message and redirects to assignment#edit page'
+      context 'when an assignment_participant cannot be found' do
+        it 'shows a flash error message and redirects to assignment#edit page' do
+          allow(User).to receive(:find_by).with(any_args).and_return(student)
+          allow(AssignmentParticipant).to receive(:exists?).with(any_args).and_return(false)
+          get :signup_as_instructor_action
+          expect(flash.now[:error]).to eq("The student is not registered for the assignment!")
+          expect(response).to redirect_to controller: 'assignments', action: 'edit', id: params[:assignment_id]
+
+        end
       end
     end
   end

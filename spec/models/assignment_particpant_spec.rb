71d7198cb2d6c986edfdf107e8864dafb78887d5
Expertiza Developer <you@example.com>
2017-10-24 23:23:32 -0400

    final2

--- spec/models/assignment_particpant_spec.rb
+++ spec/models/assignment_particpant_spec.rb
@@ -202,19 +202,31 @@ describe AssignmentParticipant do
   end
 
   describe '#quizzes_taken' do
-    it 'returns corrsponding quiz responses given by current participant'
+    it 'returns corrsponding quiz responses given by current participant' do
+      allow(QuizResponseMap).to receive(:get_assessments_for).with(any_args).and_return([response])
+      expect(participant.quizzes_taken).to eq([response])
+    end
   end
 
   describe '#metareviews' do
-    it 'returns corrsponding metareview responses given by current participant'
+    it 'returns corrsponding metareview responses given by current participant' do
+      allow(MetareviewResponseMap).to receive(:get_assessments_for).with(any_args).and_return([response])
+      expect(participant.metareviews).to eq([response])
+    end
   end
 
   describe '#teammate_reviews' do
-    it 'returns corrsponding teammate review responses given by current participant'
+    it 'returns corrsponding teammate review responses given by current participant' do
+      allow(TeammateReviewResponseMap).to receive(:get_assessments_for).with(any_args).and_return([response])
+      expect(participant.teammate_reviews).to eq([response])
+    end
   end
 
   describe '#bookmark_reviews' do
-    it 'returns corrsponding bookmark review responses given by current participant'
+    it 'returns corrsponding bookmark review responses given by current participant' do
+      allow(BookmarkRatingResponseMap).to receive(:get_assessments_for).with(any_args).and_return([response])
+      expect(participant.bookmark_reviews).to eq([response])
+    end
   end
 
   describe '#files' do

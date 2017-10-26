6e7a6bb62c8aa8780cf29965e1df856e6cbf89d9
Chengcheng Shao <cshao@ncsu.edu>
2017-10-25 23:04:48 -0400

    delete dup code

--- spec/models/assignment_particpant_spec.rb
+++ spec/models/assignment_particpant_spec.rb
@@ -17,6 +17,7 @@ describe AssignmentParticipant do
     allow(assignment).to receive(:questionnaires).and_return([review_questionnaire])
     allow(participant).to receive(:team).and_return(team)
     allow(participant).to receive(:user).and_return(student)
+    allow(ResponseMap).to receive(:get_assessments_for).with(any_args).and_return([response])
   end
 
   describe '#dir_path' do
@@ -188,14 +189,12 @@ describe AssignmentParticipant do
 
   describe '#feedback' do
     it 'returns corrsponding author feedback responses given by current participant' do
-      allow(FeedbackResponseMap).to receive(:get_assessments_for).with(any_args).and_return([response])
       expect(participant.feedback).to eq([response])
     end
   end
 
   describe '#reviews' do
     it 'returns corrsponding peer review responses given by current team' do
-      allow(ReviewResponseMap).to receive(:get_assessments_for).with(team).and_return([response])
       expect(participant.reviews).to eq([response])
     end
   end
@@ -209,28 +208,24 @@ describe AssignmentParticipant do
 
   describe '#quizzes_taken' do
     it 'returns corrsponding quiz responses given by current participant' do
-      allow(QuizResponseMap).to receive(:get_assessments_for).with(any_args).and_return([response])
       expect(participant.quizzes_taken).to eq([response])
     end
   end
 
   describe '#metareviews' do
     it 'returns corrsponding metareview responses given by current participant' do
-      allow(MetareviewResponseMap).to receive(:get_assessments_for).with(any_args).and_return([response])
       expect(participant.metareviews).to eq([response])
     end
   end
 
   describe '#teammate_reviews' do
     it 'returns corrsponding teammate review responses given by current participant' do
-      allow(TeammateReviewResponseMap).to receive(:get_assessments_for).with(any_args).and_return([response])
       expect(participant.teammate_reviews).to eq([response])
     end
   end
 
   describe '#bookmark_reviews' do
     it 'returns corrsponding bookmark review responses given by current participant' do
-      allow(BookmarkRatingResponseMap).to receive(:get_assessments_for).with(any_args).and_return([response])
       expect(participant.bookmark_reviews).to eq([response])
     end
   end

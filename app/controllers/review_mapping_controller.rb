381bb01235f5cf58f8398e24e652e9b305ef0bc9
srinivas parasa <sparasa@ncsu.edu>
2017-10-26 21:54:24 -0400

    Fixed error in review_mapping_controller.rb

--- app/controllers/review_mapping_controller.rb
+++ app/controllers/review_mapping_controller.rb
@@ -292,7 +292,7 @@ class ReviewMappingController < ApplicationController
       end
     end
     student_review_num = params[:num_reviews_per_student].to_i
-    submission_review_num = params[:num_reviews_per_submission].to
+    submission_review_num = params[:num_reviews_per_submission].to_i
     calibrated_artifacts_num = params[:num_calibrated_artifacts].to_i
     uncalibrated_artifacts_num = params[:num_uncalibrated_artifacts].to_i
 

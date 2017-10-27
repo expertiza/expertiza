829f1162a4797064fe8824573765e359571ac526
Darshil <darshil0193@gmail.com>
2017-10-27 15:04:13 -0400

    refactor(team.rb): Updating tests
    Updating the tests in team_spec.rb

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -12,12 +12,16 @@ describe Team do
   before(:each) do
     allow(TeamsUser).to receive(:where).with(team_id: 1).and_return([team_user])
   end
-  describe '#participant' do
-    it 'gets the participants of current team, by default returns an empty array'
+  describe '#participants' do
+    it 'gets the participants of current team, by default returns an empty array' do
+      expect(team.participants).to eq []
+    end
   end
 
   describe '#responses' do
-    it 'gets the response done by participants in current team, by default returns an empty array'
+    it 'gets the response done by participants in current team, by default returns an empty array' do
+      expect(team.responses).to eq []
+    end
   end
 
   describe '#delete' do
@@ -25,42 +29,60 @@ describe Team do
   end
 
   describe '#node_type' do
-    it 'always returns TeamNode'
+    it 'always returns TeamNode' do
+      expect(team.node_type).to eq 'TeamNode'
+    end
   end
 
   describe '#author_names' do
-    it 'returns an array of author\'s name'
+    it 'returns an array of author\'s name' do
+      expect(team.author_names).to eq ['no one']
+    end
   end
 
   describe '#user?' do
     context 'when users in current team includes the parameterized user' do
-      it 'returns true'
+      it 'returns true' do
+        expect(team.user? user).to be true
+      end
     end
 
     context 'when users in current team does not include the parameterized user' do
-      it 'returns false'
+      it 'returns false' do
+        expect(team.user? user2).to be false
+      end
     end
   end
 
   describe '#full?' do
     context 'when the parent_id of current team is nil' do
-      it 'returns false'
+      it 'returns false' do
+        allow(TeamsUser).to receive(:where).with(team_id: 1).and_return([])
+        expect(team.full?).to eq false
+      end
     end
 
     context 'when the parent_id of current team is not nil' do
       context 'when the current team size is bigger than or equal to max team members' do
-        it 'returns true'
+        it 'returns true' do
+          allow(TeamsUser).to receive(:where).with(team_id: 1).and_return([team_user, team_user, team_user])
+          expect(team.full?).to eq true
+        end
       end
 
       context 'when the current team size is smaller than max team members' do
-        it 'returns false'
+        it 'returns false' do
+          expect(team.full?).to eq false
+        end
       end
     end
   end
 
   describe '#add_member' do
     context 'when parameterized user has already joined in current team' do
-      it 'raise an error'
+      it 'raise an error' do
+        expect { team.add_member(user) }.to raise_error(RuntimeError, "The user #{user.name} is already a member of the team #{team.name}")
+      end
     end
 
     context 'when parameterized user did join in current team yet' do
@@ -71,7 +93,11 @@ describe Team do
   end
 
   describe '.size' do
-    it 'returns the size of current team'
+    it 'returns the size of current team' do
+      team_user.team_id = team.id
+      allow(TeamsUser).to receive(:where).with(team_id: 1).and_return([team_user])
+      expect(Team.size(team.id)).to eq 1
+    end
   end
 
   describe '#copy_members' do
@@ -93,12 +119,16 @@ describe Team do
   end
 
   describe '.generate_team_name' do
-    it 'generates the unused team name'
+    it 'generates the unused team name' do
+      expect(Team.generate_team_name('Test')).to eq 'Test_Team1'
+    end
   end
 
   describe '.import_team_members' do
     context 'when cannot find a user by name' do
-      it 'raises an ImportError'
+      it 'raises an ImportError' do
+        expect { team.import_team_members(0, ['TestUser']) }.to raise_error(ImportError, "The user TestUser was not found. <a href='/users/new'>Create</a> this user?")
+      end
     end
 
     context 'when can find certain user' do
@@ -108,7 +138,9 @@ describe Team do
 
   describe '.import' do
     context 'when row is empty and has_column_names option is not true' do
-      it 'raises an ArgumentError'
+      it 'raises an ArgumentError' do
+        expect { Team.import([], 1, { has_column_names: true}, AssignmentTeam) }.to raise_error(ArgumentError, 'Not enough fields on this line.')
+      end
     end
 
     context 'when has_column_names option is true' do

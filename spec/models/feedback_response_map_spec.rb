0281035bb95d72fe04e54e54359886f87a19b8aa
hhimani@ncsu.edu <hhimani@ncsu.edu>
2017-10-27 23:00:51 -0400

    feedback response map

--- spec/models/feedback_response_map_spec.rb
+++ spec/models/feedback_response_map_spec.rb
@@ -1,8 +1,24 @@
 describe 'FeedbackResponseMap' do
   before(:each) do
     @feedback_response_map = build(:feedback_response_map,id: 1)
+    @assignment=build(:assignment,)
   end
   it "should be vaild" do
     expect(@feedback_response_map).to be_valid
+  end
+
+  #it "should be valid assignment" do
+   # expect(@feedback_response_map.assignment).to must_be_empty
+ # end
+
+  it "should return title" do
+    #expect (@feedback_response_map.get_title).to eql"Feedback"
+    expect @feedback_response_map.get_title.should == "Feedback"
+  end
+
+
+  it "should return test show review" do
+    #expect (@feedback_response_map.get_title).to eql"Feedback"
+    expect @feedback_response_map.show_review.should == "No review was performed"
   end
 end

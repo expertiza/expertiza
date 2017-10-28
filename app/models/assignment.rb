c8a5847923886959b74158e52b3ed0e7dd5fcbac
Dax Amin <daxkamin@gmail.com>
2017-10-28 00:40:22 -0400

    Update assignment.rb

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -38,7 +38,7 @@ class Assignment < ActiveRecord::Base
   def check_directory_path
     @assignments = Assignment.all
     @assignments.each do |assignment|
-      if (assignment.directory_path != nil && self.directory_path != nil && assignment.directory_path.include?(self.directory_path))
+      if !assignment.directory_path.nil? && !self.directory_path.nil? && assignment.directory_path.include?(self.directory_path)
         errors.add(:directory_path, "The directory is already in use")
         return false
       end

176c21ac5d1799f0f99f7811380021cd593543bb
Aditya Shah <ashah22@ncsu.edu>
2017-10-31 01:10:16 -0400

    code_climate bug fix

--- app/controllers/sign_up_sheet_controller.rb
+++ app/controllers/sign_up_sheet_controller.rb
@@ -104,10 +104,7 @@ class SignUpSheetController < ApplicationController
   end
 
   def display_error(available_slots, topic_identifier)
-    if available_slots.zero?
-      flash[:error] = "Try duplicating the topic " + topic_identifier + " after increasing the number of slots since all slots are currently filled."
-    else
-      flash[:error]="Instead of duplicating the topic " + topic_identifier + " extend the due date since the topic has all slots available."
+    flash[:error] = available_slots.zero? ? "Try duplicating the topic " + topic_identifier + " after increasing the number of slots since all slots are currently filled." : "Instead of duplicating the topic " + topic_identifier + " extend the due date since the topic has all slots available."
   end
 
   def display_success(topic_identifier)

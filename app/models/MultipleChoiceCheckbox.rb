3070c1a5cd21bed10f538d3d1cd8f30d479da503
Shreyas Zagade <shreyaszagade@gmail.com>
2017-10-31 02:33:58 -0400

    Resolved wrong variable name issue

--- app/models/MultipleChoiceCheckbox.rb
+++ app/models/MultipleChoiceCheckbox.rb
@@ -5,9 +5,9 @@ class MultipleChoiceCheckbox
 	def update_option_type(quiz_question_choice,parameters,question_id,option_number)
 		#parameters[:quiz_question_choices][@question.id.to_s][@question.type][i.to_s]
 		if parameters[:quiz_question_choices][question_id.to_s][@question_type][option_number.to_s]
-            quiz_question_choice.update_attributes(iscorrect: params[:quiz_question_choices][question_id.to_s][@question_type][option_number.to_s][:iscorrect], txt: params[:quiz_question_choices][question_id.to_s][@question_type][option_number.to_s][:txt])
+            quiz_question_choice.update_attributes(iscorrect: parameters[:quiz_question_choices][question_id.to_s][@question_type][option_number.to_s][:iscorrect], txt: parameters[:quiz_question_choices][question_id.to_s][@question_type][option_number.to_s][:txt])
         else
-            quiz_question_choice.update_attributes(iscorrect: '0', txt: params[:quiz_question_choices][quiz_question_choice.id.to_s][:txt])
+            quiz_question_choice.update_attributes(iscorrect: '0', txt: parameters[:quiz_question_choices][quiz_question_choice.id.to_s][:txt])
         end
 	end
 

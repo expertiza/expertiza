a0b1d8a6df0b157fbb6f47c347e49e8cebf4f25f
Suvodeep90 <Suvodeep.majumder90@gmail.com>
2017-10-31 21:04:03 -0400

    Changes done for allowing user to delete the req he/she sends

--- app/helpers/student_teams_helper.rb
+++ app/helpers/student_teams_helper.rb
@@ -5,6 +5,8 @@ module StudentTeamsHelper
   end
 
   def user_has_topic?(user_id, assignment_id)
+    #This query takes the user_id and assignment_id as input then does a inner join between the Sign_up_Topicsm Signed_up_teams,
+    #and teams_users table to find the if the user has selected any topic for that perticular assignmnet or not.
     user_topic = SignUpTopic.joins("INNER JOIN signed_up_teams ON signed_up_teams.topic_id =
                   sign_up_topics.id").joins("INNER JOIN teams_users ON teams_users.team_id =
                   signed_up_teams.team_id").where("sign_up_topics.assignment_id = ? and teams_users.user_id = ?", assignment_id, user_id).uniq

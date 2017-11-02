0bb9620a386f78465e7eeeda577afaba2e323eb7
Shreyas Zagade <shreyaszagade@gmail.com>
2017-11-02 00:25:15 -0400

    Removed unnecessary print statements

--- app/models/multiple_choice_checkbox_type.rb
+++ app/models/multiple_choice_checkbox_type.rb
@@ -5,7 +5,6 @@ class MultipleChoiceCheckboxType
   	end
 
 	def update_option_type(quiz_question_choice,parameters,question_id,option_number)
-		#parameters[:quiz_question_choices][@question.id.to_s][@question.type][i.to_s]
 		if parameters[:quiz_question_choices][question_id.to_s][@question_type][option_number.to_s]
             quiz_question_choice.update_attributes(iscorrect: parameters[:quiz_question_choices][question_id.to_s][@question_type][option_number.to_s][:iscorrect], txt: parameters[:quiz_question_choices][question_id.to_s][@question_type][option_number.to_s][:txt])
         else
--- app/models/multiple_choice_radio_type.rb
+++ app/models/multiple_choice_radio_type.rb
@@ -5,13 +5,6 @@ class MultipleChoiceRadioType
   	end
 
 	def update_option_type(quiz_question_choice,parameters,question_id,option_number)
-		puts "SSZ 1: #{parameters}"
-		puts "SSZ 1: #{parameters[:quiz_question_choices]}"
-		puts "#{@question_type}"
-		puts "SSZ 1: #{parameters[:quiz_question_choices][question_id.to_s]}"
-		puts "SSZ 1: #{parameters[:quiz_question_choices][question_id.to_s][@question_type]}"
-		puts "SSZ 1: #{parameters[:quiz_question_choices][question_id.to_s][@question_type][:correctindex]}"
-
 		if parameters[:quiz_question_choices][question_id.to_s][@question_type][:correctindex] == option_number.to_s
             quiz_question_choice.update_attributes(iscorrect: '1', txt: parameters[:quiz_question_choices][question_id.to_s][@question_type][option_number.to_s][:txt])
         else

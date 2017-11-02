2c6334616429f1a7874d81d32cbff826ac9fb7b0
Shreyas Zagade <shreyaszagade@gmail.com>
2017-11-01 22:51:41 -0400

    Debugging

--- app/models/multiple_choice_radio_type.rb
+++ app/models/multiple_choice_radio_type.rb
@@ -3,11 +3,12 @@ class MultipleChoiceRadioType
 	@question_type = "MultipleChoiceRadio"
 
 	def update_option_type(quiz_question_choice,parameters,question_id,option_number)
-		puts "SSZ 1: #{quiz_question_choice}"
 		puts "SSZ 1: #{parameters}"
-		puts "SSZ 1: #{question_id}"
-		puts "SSZ 1: #{option_number}"
-		
+		puts "SSZ 1: #{parameters[:quiz_question_choices]}"
+		puts "SSZ 1: #{parameters[:quiz_question_choices][question_id.to_s]}"
+		puts "SSZ 1: #{parameters[:quiz_question_choices][question_id.to_s][@question_type]}"
+		puts "SSZ 1: #{parameters[:quiz_question_choices][question_id.to_s][@question_type][:correctindex]}"
+
 		if parameters[:quiz_question_choices][question_id.to_s][@question_type][:correctindex] == option_number.to_s
             quiz_question_choice.update_attributes(iscorrect: '1', txt: parameters[:quiz_question_choices][question_id.to_s][@question_type][option_number.to_s][:txt])
         else

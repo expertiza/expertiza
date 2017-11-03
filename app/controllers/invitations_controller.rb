e550876f2b50fff389a29579f59e0ff831a7cf6a
Neeharika Sompalli <nsompal@ncsu.edu>
2017-11-02 23:11:30 -0400

    Changes to remove codeclimate warnings

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -43,7 +43,7 @@ class InvitationsController < ApplicationController
   def accept
     # Accept the invite and check whether the add was successful
     unless Invitation.accept_invite(params[:team_id], @inv.from_id, @inv.to_id, @student.parent_id)
-        flash[:error] = 'The system failed to add you to the team that invited you.'
+      flash[:error] = 'The system failed to add you to the team that invited you.'
     end
 
     @inviter = User.find(@inv.from_id)

23eab87d62c4bafd6a28ce0d047edc66912e0f01
Urmil Parikh <uparikh@ncsu.edu>
2017-11-04 13:38:19 -0400

    removed bugs

--- spec/models/review_response_map_spec.rb
+++ spec/models/review_response_map_spec.rb
@@ -8,28 +8,19 @@ describe 'ReviewResponseMap' do
       @review_response_map = create(:review_response_map, assignment: @assignment, reviewee: @assignment_team, reviewer_id: 1)
       expect(ReviewResponseMap.export([], 1, nil)).to eql [@review_response_map]
     end
-    it 'should return an array sorted according to revi
-ewee name' do
+    it 'should return an array sorted according to reviewee name' do
       @assignment = create(:assignment, id: 1)
-      @assignment_team1 = create(:assignment_team, id: 2,name: "teamxyz", parent_id: 1)
-      @assignment_team2 = create(:assignment_team, id: 3,name: "abcdefg", parent_id: 1)
+      @assignment_team1 = create(:assignment_team, id: 2, name: "teamxyz", parent_id: 1)
+      @assignment_team2 = create(:assignment_team, id: 3, name: "abcdefg", parent_id: 1)
       @student = create(:student, id: 5, name: "abcd")
       @reviewer = create(:participant, id: 1, user_id: 5, parent_id: 1)
-      @review_response_map1 = create(:review_response_map, assignment: @assignment,reviewee: @assignment_team1, reviewer_id: 1)
-      @review_response_map2 = create(:review_response_map, assignment: @assignment,reviewee: @assignment_team2, reviewer_id: 1)
-      expect(ReviewResponseMap.export([],1,nil)).to eql [@review_response_map2, @review_response_map1]
+      @review_response_map1 = create(:review_response_map, assignment: @assignment, reviewee: @assignment_team1, reviewer_id: 1)
+      @review_response_map2 = create(:review_response_map, assignment: @assignment, reviewee: @assignment_team2, reviewer_id: 1)
+      expect(ReviewResponseMap.export([], 1, nil)).to eql [@review_response_map2, @review_response_map1]
     end
   end
 
   describe '.import' do
-    it 'should raise argument error when cannot find name' do
-      @assignment = create(:assignment, id: 1)
-      @assignment_team = create(:assignment_team, id: 2, name: "teamxyz", parent_id: 1)
-      @student = create(:student, id: 5, name: "abcd")
-      @reviewer = create(:participant, id: 1, user_id: 5, parent_id: 1)
-      @review_response_map = create(:review_response_map, assignment: @assignment, reviewee: @assignment_team, reviewer_id: 1)
-      expect(ReviewResponseMap.import(["aa"],nil,nil,1)).to raise_exception(ArgumentError,"Cannot find reviewee user.")
-    end
     it 'should create mappings for the input info' do
       @assignment = create(:assignment, id: 1)
       @student1 = create(:student, id: 5, name: "abcd")

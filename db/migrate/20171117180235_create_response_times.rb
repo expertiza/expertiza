579df04b0af56a8abc074cdfc82b5a55da010583
Jerry <sli41@ncsu.edu>
2017-11-17 13:40:49 -0500

    update migration on response_times

--- db/migrate/20171117180235_create_response_times.rb
+++ db/migrate/20171117180235_create_response_times.rb
@@ -1,10 +1,10 @@
 class CreateResponseTimes < ActiveRecord::Migration
   def change
     create_table :response_times do |t|
-      t.integer :map_id, presence: true
-      t.string :link, presence: true
-      t.integer :round, presence: true
-      t.datetime :start_at, null: false
+      t.integer :map_id
+      t.string :link
+      t.integer :round
+      t.datetime :start_at
       t.datetime :end_at
 
       t.timestamps null: false

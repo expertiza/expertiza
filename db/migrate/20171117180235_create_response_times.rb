d81f071906b9561698264c370bb26c6427426336
Jerry <sli41@ncsu.edu>
2017-11-17 13:32:22 -0500

    add columns to response_times db schema

--- db/migrate/20171117180235_create_response_times.rb
+++ db/migrate/20171117180235_create_response_times.rb
@@ -1,6 +1,11 @@
 class CreateResponseTimes < ActiveRecord::Migration
   def change
     create_table :response_times do |t|
+      t.integer :map_id, presence: true
+      t.string :link, presence: true
+      t.integer :round, presence: true
+      t.datetime :start_at, null: false
+      t.datetime :end_at
 
       t.timestamps null: false
     end

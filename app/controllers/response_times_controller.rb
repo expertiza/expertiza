7cf9b87e2e71f38dd7159b2c964d625d6dd37fb9
Shijie <sli41@ncsu.edu>
2017-11-23 00:43:51 -0500

    add js code to open link in new window and record time

--- app/controllers/response_times_controller.rb
+++ app/controllers/response_times_controller.rb
@@ -1,56 +1,40 @@
 class ResponseTimesController < ApplicationController
-  before_action :set_response_time, only: [:show, :edit, :update, :destroy]
-
-  # GET /response_times
-  def index
-    @response_times = ResponseTime.all
+  def action_allowed?
+    true
   end
 
-  # GET /response_times/1
-  def show
-  end
-
-  # GET /response_times/new
-  def new
-    @response_time = ResponseTime.new
-  end
-
-  # GET /response_times/1/edit
-  def edit
-  end
-
-  # POST /response_times
-  def create
+  def response_start_time
+    map_id = params[:response_time][:map_id]
+    round = params[:response_time][:round]
+    link = params[:response_time][:link]
+    start_at = params[:response_time][:start_at]
+    # check if this link is already opened and timed
+    @response_time_records = ResponseTime.where(map_id: map_id, round: round, link: link)
+    # if opened, end these records with current time
+    if @response_time_records
+      @response_time_records.each do |time_record|
+        if time_record.end_at.nil?
+          time_record.update_attribute('end_at', start_at)
+        end
+      end
+    end
+    # create new response time record for current link
     @response_time = ResponseTime.new(response_time_params)
-
-    if @response_time.save
-      redirect_to @response_time, notice: 'Response time was successfully created.'
-    else
-      render :new
-    end
+    @response_time.save
   end
 
-  # PATCH/PUT /response_times/1
-  def update
-    if @response_time.update(response_time_params)
-      redirect_to @response_time, notice: 'Response time was successfully updated.'
-    else
-      render :edit
+  def response_end_time
+    @data = params.require(:response_time)
+    @response_time_records = ResponseTime.where(map_id: @data[:map_id], round: @data[:round], link: @data[:link])
+    @response_time_records.each do |time_record|
+      if time_record.end_at.nil?
+        time_record.update_attribute('end', @data[:end_at])
+        break
+      end
     end
-  end
-
-  # DELETE /response_times/1
-  def destroy
-    @response_time.destroy
-    redirect_to response_times_url, notice: 'Response time was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_response_time
-      @response_time = ResponseTime.find(params[:id])
-    end
-
     # Only allow a trusted parameter "white list" through.
     def response_time_params
       params.require(:response_time).permit(:map_id, :link, :round, :start_at, :end_at)

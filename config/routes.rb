590ec821e0437d282824c5d9dddb12c100e1f50f
ajaypendyala <ajaychandrapendyala@gmail.com>
2017-11-25 01:15:14 -0500

    automated metareview metrics

--- config/routes.rb
+++ config/routes.rb
@@ -311,6 +311,7 @@ Expertiza::Application.routes.draw do
       get :show_calibration_results_for_student
       post :custom_create
       get :pending_surveys
+      get :autometareview
     end
   end
 

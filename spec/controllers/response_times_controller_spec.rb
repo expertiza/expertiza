6b4130a3e66ee07d2447c36201d5ccd4219278f0
darshanbhandari123 <darshanbhandari78@gmail.com>
2017-11-30 11:51:56 -0500

    Basic structure of response time controller spec file

---
+++ spec/controllers/response_times_controller_spec.rb
@@ -0,0 +1,26 @@
+describe ResponseTimesController do
+  describe '#action_allowed?' do
+    context 'when called' do
+      it 'always return true' do
+
+      end
+    end
+  end
+
+  describe '#response_start_time' do
+    context 'when the link is opened and timed' do
+      it 'should update time record with end time as current time' do
+
+      end
+    end
+  end
+
+  describe '#response_end_time' do
+    context 'when response does not have a end time' do
+      it 'should update time record with end time as current time' do
+
+      end
+    end
+  end
+
+end

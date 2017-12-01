2158a63aa2a048c8fcb272498b08f5300b8b6696
KunmiaoYang <kyang12@ncsu.edu>
2017-12-01 11:17:55 -0500

    add enum source in metric model

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -1,5 +1,5 @@
 class Metric < ActiveRecord::Base
-  enum source: [ :github, :trello ]
+  enum source: [ :github, :trello, :wiki ]
   belongs_to :team
   belongs_to :assignment
   has_many :metric_data_points, dependent: :destroy, foreign_key: 'metric_id'

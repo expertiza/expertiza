83b7294532524a19fc745a8faa172a83fdcc2517
Aishwarya Sundararajan <asundar2@ncsu.edu>
2017-11-30 20:54:18 -0500

    Modified response_end_time spec

--- spec/controllers/response_times_controller_spec.rb
+++ spec/controllers/response_times_controller_spec.rb
@@ -28,6 +28,7 @@ describe ResponseTimesController do
         allow(ResponseTime).to receive(:end_at).and_return(dummy)
         allow(dummy).to receive(:nil?).and_return(true)
         allow(ResponseTime).to receive(:update_attributes).with(:end_at,Time.now.to_date).and_return(response_time_records)
+        expect(response.body).to be_blank
       end
     end
   end

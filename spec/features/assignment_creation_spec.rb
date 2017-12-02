341c1b785ebc10c2f72f8a3b924888a11bffab03
ssudame0412 <32787842+ssudame0412@users.noreply.github.com>
2017-12-02 17:41:10 -0500

    Update assignment_creation_spec.rb
    fixed test case

--- spec/features/assignment_creation_spec.rb
+++ spec/features/assignment_creation_spec.rb
@@ -227,7 +227,7 @@ describe "assignment function" do
       fill_in 'assignment_form_assignment_directory_path', with: 'testDirectory'
 
       find_link('ReviewStrategy').click
-      expect(page).to have_content("Deadline type")
+      expect(page).to have_content("Review Strategy")
     end
 
     it "is able show tab due deadlines" do
@@ -238,7 +238,35 @@ describe "assignment function" do
       fill_in 'assignment_form_assignment_directory_path', with: 'testDirectory'
 
       find_link('Due date').click
-      expect(page).to have_content("Review Strategy")
+      expect(page).to have_content("Deadline type")
+    end
+    
+    it "set the deadline for an assignment review" do
+      login_as("instructor6")
+      visit '/assignments/new?private=0'
+      fill_in 'assignment_form_assignment_name', with: 'public assignment for test'
+      select('Course 2', from: 'assignment_form_assignment_course_id')
+      fill_in 'assignment_form_assignment_directory_path', with: 'testDirectory'
+      click_link 'Due date'
+      fill_in 'assignment_form_assignment_rounds_of_reviews', with: '1'
+      fill_in 'datetimepicker_submission_round_1', with: (Time.now.in_time_zone + 1.day).strftime("%Y/%m/%d %H:%M")
+      fill_in 'datetimepicker_review_round_1', with: (Time.now.in_time_zone + 10.days).strftime("%Y/%m/%d %H:%M")
+      click_button 'submit_btn'
+
+      submission_type_id = DeadlineType.where(name: 'submission')[0].id
+      review_type_id = DeadlineType.where(name: 'review')[0].id
+
+      submission_due_date = DueDate.find(1)
+      review_due_date = DueDate.find(2)
+      expect(submission_due_date).to have_attributes(
+                                         deadline_type_id: submission_type_id,
+                                         type: 'AssignmentDueDate'
+                                     )
+
+      expect(review_due_date).to have_attributes(
+                                     deadline_type_id: review_type_id,
+                                     type: 'AssignmentDueDate'
+                                 )
     end
 
     it "is able show tab rubrics" do
@@ -297,41 +325,7 @@ describe "assignment function" do
       fill_in 'num_reviews_per_student', with: 5
     end
   end
-  
-  describe "Due dates tab for new form", js: true do
-    before(:each) do
-      login_as("instructor6")
-      visit '/assignments/new?private=0'
 
-      fill_in 'assignment_form_assignment_name', with: 'public assignment for test'
-      select('Course 2', from: 'assignment_form_assignment_course_id')
-      fill_in 'assignment_form_assignment_directory_path', with: 'testDirectory'
-      click_link 'Due date'
-    end
-
-    # able to set deadlines for a single round of reviews
-    it "set the deadline for an assignment review" do
-      fill_in 'assignment_form_assignment_rounds_of_reviews', with: '1'
-      fill_in 'datetimepicker_submission_round_1', with: (Time.now.in_time_zone + 1.day).strftime("%Y/%m/%d %H:%M")
-      fill_in 'datetimepicker_review_round_1', with: (Time.now.in_time_zone + 10.days).strftime("%Y/%m/%d %H:%M")
-      click_button 'submit_btn'
-
-      submission_type_id = DeadlineType.where(name: 'submission')[0].id
-      review_type_id = DeadlineType.where(name: 'review')[0].id
-
-      submission_due_date = DueDate.find(1)
-      review_due_date = DueDate.find(2)
-      expect(submission_due_date).to have_attributes(
-                                         deadline_type_id: submission_type_id,
-                                         type: 'AssignmentDueDate'
-                                     )
-
-      expect(review_due_date).to have_attributes(
-                                     deadline_type_id: review_type_id,
-                                     type: 'AssignmentDueDate'
-                                 )
-    end
-  end
   # instructor can set in which deadline can student reviewers take the quizzes
   describe "deadlines", js: true do
     before(:each) do

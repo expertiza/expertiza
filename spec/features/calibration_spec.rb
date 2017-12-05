d020a76058dc52d807a7a0bb8cb474d7532fd1a1
Na <nwen@ncsu.edu>
2017-12-05 13:36:15 -0500

    added testcases

--- spec/features/calibration_spec.rb
+++ spec/features/calibration_spec.rb
@@ -25,16 +25,28 @@ describe 'expert review' do
     create(:deadline_right, name: 'OK')
     create(:assignment_due_date, deadline_type: DeadlineType.where(name: "submission").first, due_at: DateTime.now.in_time_zone + 1.day)
     create(:assignment, name: "test")
+    create(:topic)
+    create(:topic, topic_name: "TestReview")
+    create(:team_user, user: User.where(role_id: 2).first)
+    create(:team_user, user: User.where(role_id: 2).second)
+    create(:assignment_team)
+    create(:team_user, user: User.where(role_id: 2).third, team: AssignmentTeam.second)
+    create(:signed_up_team)
+    create(:signed_up_team, team_id: 2, topic: SignUpTopic.second)
+    create(:assignment_questionnaire)
+    create(:question)
+    create(:assignment_node)
+
 
   end
 
   context 'in assignments#edit page' do
     it 'has a checkbox with title \'Calibration for training?\' on \'General\' tab' do
-      assignment_id = Assignment.where(name: "test").first
+      assignment = Assignment.where(name: "test").first
       instructor = User.find_by(name: "instructor6")
       login_as("instructor6")
       stub_current_user(instructor, instructor.role.name, instructor.role)
-      visit "/assignments/#{assignment_id.id}/edit"
+      visit "/assignments/#{assignment.id}/edit"
       click_link 'General'
       expect(page).to have_field('Calibration for training?')
     end
@@ -42,11 +54,11 @@ describe 'expert review' do
       context '\'Calibration\' due date' do
         it 'works correctly'do
           # displays a new tab named \'Calibration\' and adds a calibration due date in \'Due dates\' tab'
-          assignment_id = Assignment.where(name: "test").first
+          assignment = Assignment.where(name: "test").first
           instructor = User.find_by(name: "instructor6")
           login_as("instructor6")
           stub_current_user(instructor, instructor.role.name, instructor.role)
-          visit "/assignments/#{assignment_id.id}/edit"
+          visit "/assignments/#{assignment.id}/edit"
           click_link 'General'
           expect(page).to have_field('Calibration for training?')
           page.check 'Calibration for training?'
@@ -64,20 +76,38 @@ describe 'expert review' do
 
   context 'when current assignment is in calibration stage' do
     context 'calibration feature' do
-      it 'works correctly'
+      it 'works correctly' do
+        login_as('student2064')
       # shows current stage of this assignment to be 'Calibration' on student_task#view page
-
+        assignment = Assignment.where(name: "test").first
+        visit "/student_task/view?id=#{assignment.id}"
+        expect(assignment.current_stage_name).to eql 'Calibration'
+        click_link "Others' work "
       # shows 'Calibration review 1, 2, 3...' instead of 'Review 1, 2, 3...' on student_review#list page
 
+        expect(page).to have_content("Calibration review")
+        expect(page).to have_no_content("Calibration review")
       # allows students to do calibration review and the data can be saved successfully
 
+
       # the student is able to compare the results of expert review by clicking 'show calibration results' link
+        click_link "show calibration results"
+      end
     end
   end
 
   context 'when current assignment is in review stage' do
-    it 'excludes calibration reviews from outstanding review restriction and total review restriction'
+    login_as('student2064')
+    assignment = Assignment.where(name: "test").first
+    it 'excludes calibration reviews from outstanding review restriction and total review restriction' do
 
-    it 'shows \'Review 1, 2, 3...\' instead of \'Calibration review 1, 2, 3...\' on student_review#list page'
+
+    end
+    it 'shows \'Review 1, 2, 3...\' and \'Calibration review 1, 2, 3...\' on student_review#list page' do
+      visit "/student_task/list"
+      expect(page).to have_content("Calibration review")
+      expect(page).to have_content("Review")
+
+    end
   end
 end

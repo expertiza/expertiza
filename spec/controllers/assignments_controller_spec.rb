4d33243f298799e423bd69e3deff9a007d952856
apatial <apatial@ncsu.edu>
2017-12-06 01:03:41 -0500

    allowed method to be called in spec

--- spec/controllers/assignments_controller_spec.rb
+++ spec/controllers/assignments_controller_spec.rb
@@ -159,8 +159,12 @@ describe AssignmentsController do
           .and_return([double('AssignmentQuestionnaire', questionnaire_id: 666, used_in_round: 1)])
         allow(Questionnaire).to receive(:where).with(id: 666).and_return([double('Questionnaire', type: 'ReviewQuestionnaire')])
         assignment_due_date = build(:assignment_due_date)
-        allow(AssignmentDueDate).to receive(:where).with(parent_id: 1).and_return([assignment_due_date])
-        allow(AssignmentDueDate).to receive(:where).with(parent_id: 1, deadline_type_id: 2).and_return([assignment_due_date])
+        assignment_array = [assignment_due_date]
+        allow(assignment_array).to receive(:order).and_return(assignment_array)
+        allow(AssignmentDueDate).to receive(:where).with(parent_id: "1").and_return(assignment_array)
+        allow(AssignmentDueDate).to receive(:where).with(parent_id: 1).and_return(assignment_array)
+        allow(AssignmentDueDate).to receive(:where).with(parent_id: 1, deadline_type_id: 2).and_return(assignment_array)
+
         allow(assignment).to receive(:num_review_rounds).and_return(1)
         params = {id: 1}
         get :edit, params

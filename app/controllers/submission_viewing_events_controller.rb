ac8eb4ce0829bcb9ead0a110e755202c8eaa0a14
Jerry <sli41@ncsu.edu>
2017-12-14 23:01:15 -0500

    refactor helper and controller

--- app/controllers/submission_viewing_events_controller.rb
+++ app/controllers/submission_viewing_events_controller.rb
@@ -1,17 +1,16 @@
 class SubmissionViewingEventsController < ApplicationController
-
-  def action_allowed?
-    return true
-  end
+  # def action_allowed?
+  #   return true
+  # end
 
   # record time when link or file is opened in new window
   def record_start_time
     param_args = params[:submission_viewing_event]
     # check if this link is already opened and timed
-    @submission_viewing_event_records = SubmissionViewingEvent.where(map_id: param_args[:map_id], round: param_args[:round], link: param_args[:link])
+    submission_viewing_event_records = SubmissionViewingEvent.where(map_id: param_args[:map_id], round: param_args[:round], link: param_args[:link])
     # if opened, end these records with current time
-    if @submission_viewing_event_records
-      @submission_viewing_event_records.each do |time_record|
+    if submission_viewing_event_records
+      submission_viewing_event_records.each do |time_record|
         if time_record.end_at.nil?
           # time_record.update_attribute('end_at', start_at)
           time_record.destroy
@@ -19,16 +18,16 @@ class SubmissionViewingEventsController < ApplicationController
       end
     end
     # create new response time record for current link
-    @submission_viewing_event = SubmissionViewingEvent.new(submission_viewing_event_params)
-    @submission_viewing_event.save
+    submission_viewing_event = SubmissionViewingEvent.new(submission_viewing_event_params)
+    submission_viewing_event.save
     render nothing: true
   end
 
   # record time when link or file window is closed
   def record_end_time
     @data = params.require(:submission_viewing_event)
-    @submission_viewing_event_records = SubmissionViewingEvent.where(map_id: @data[:map_id], round: @data[:round], link: @data[:link])
-    @submission_viewing_event_records.each do |time_record|
+    submission_viewing_event_records = SubmissionViewingEvent.where(map_id: @data[:map_id], round: @data[:round], link: @data[:link])
+    submission_viewing_event_records.each do |time_record|
       if time_record.end_at.nil?
         time_record.update_attribute('end_at', @data[:end_at])
         break
@@ -43,7 +42,8 @@ class SubmissionViewingEventsController < ApplicationController
   def mark_end_time
     @data = params.require(:submission_viewing_event)
     @link_array = []
-    SubmissionViewingEvent.where(map_id: @data[:map_id], round: @data[:round]).each do |submissionviewingevent_entry|
+    submission_viewing_event_records  = SubmissionViewingEvent.where(map_id: @data[:map_id], round: @data[:round])
+    submission_viewing_event_records.each do |submissionviewingevent_entry|
       if submissionviewingevent_entry.end_at.nil?
         @link_array.push(submissionviewingevent_entry.link)
         submissionviewingevent_entry.update_attribute('end_at', @data[:end_at])
--- app/helpers/review_mapping_helper.rb
+++ app/helpers/review_mapping_helper.rb
@@ -188,7 +188,7 @@ module ReviewMappingHelper
   end
 
   def get_time_spent_on_review_for_certain_team_for_each_round(map_id)
-    (1..@assignment.num_review_rounds).each {|round| instance_variable_set("@time_spent_round_" + round.to_s,0) }
+    (1..@assignment.num_review_rounds).each {|round| instance_variable_set("@time_spent_round_" + round.to_s, 0) }
     (1..@assignment.num_review_rounds).each do |round|
       sum_time = 0
       @submission_viewing_events = SubmissionViewingEvent.where(map_id: map_id, round: round)

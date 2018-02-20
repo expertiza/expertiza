4d41d2a1e08c7c815a813bae0ba7977f2a95d055
ferry <ferryxo@gmail.com>
2018-02-19 22:25:02 -0500

    deactivate auto save by default and set the timer to 90 seconds

--- app/assets/javascripts/response.js
+++ app/assets/javascripts/response.js
@@ -3,22 +3,44 @@
 *
 **/
 
+var vis = (function(){
+    var stateKey, eventKey, keys = {
+        hidden: "visibilitychange",
+        webkitHidden: "webkitvisibilitychange",
+        mozHidden: "mozvisibilitychange",
+        msHidden: "msvisibilitychange"
+    };
+    for (stateKey in keys) {
+        if (stateKey in document) {
+            eventKey = keys[stateKey];
+            break;
+        }
+    }
+    return function(c) {
+        if (c) document.addEventListener(eventKey, c);
+        return !document[stateKey];
+    }
+})();
 
 var autoSavePost;
 
-// triggers in every 10 seconds to save the draft version of the review
+// triggers in every 90 seconds to save the draft version of the review
 autoSavePost = function() {
     var dt, time;
     dt = new Date;
     time = dt.getHours() + ':' + dt.getMinutes() + ':' + dt.getSeconds();
-    if ($('.review_form').length > 0) {
-        $('form').attr('data-remote', 'true');
-        document.getElementById('save_review').click();
-        if($('input[name=saved]').value="0")$('input[name=saved]').val("1");
-        $('form').removeAttr('data-remote');
+
+    //if it's was in the background, don't autosae to save bandwidth
+    if(vis() && document.getElementById('autosave_cbx').checked) {
+        if ($('.review_form').length > 0) {
+            $('form').attr('data-remote', 'true');
+            document.getElementById('save_review').click();
+            if ($('input[name=saved]').value = "0")$('input[name=saved]').val("1");
+            $('form').removeAttr('data-remote');
+            $('#save_progress').html('<span id="tick"> &#10004; </span>' + 'Draft Autosaved at ' + time);
+        }
     }
-    $('#save_progress').html('<span id="tick"> &#10004; </span>' + 'Draft Autosaved at ' + time);
-    setTimeout(autoSavePost, 10000);
+    setTimeout(autoSavePost, 90000);
 };
 
 // add star rating option to each dropdown box in review form

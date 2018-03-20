65098aa525b0b1a642d87bc0f9bd9084b2b2f8ed
Dileep <dileep.badveli@gmail.com>
2018-03-20 11:45:14 -0400

    Added Some contexts

--- spec/models/on_the_fly_calc_spec.rb
+++ spec/models/on_the_fly_calc_spec.rb
@@ -2,10 +2,31 @@ describe OnTheFlyCalc do
   describe "#compute total score" do
   end
   describe "#compute reviews hash" do
+
+    context "Has multiple review phases with different review rubrics" do
+    end
+
+    context "Does not have multiple review phases with different review rubrics" do
+    end
+
   end
   describe "#compute avg and ranges hash" do
+
+    context "Has multiple review phases with different review rubrics" do
+    end
+
+    context "Does not have multiple review phases with different review rubrics" do
+    end
+
   end
   descirbe"#scores" do
+
+    context "Has multiple review phases with different review rubrics" do
+    end
+
+    context "Does not have multiple review phases with different review rubrics" do
+    end
+
   end
 
 end

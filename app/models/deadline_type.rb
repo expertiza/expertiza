2578cca957aea3dcef0ffb4593be9ad59d21edce
xzhang72 <xzhang72@ncsu.edu>
2018-03-20 18:11:29 -0400

    fix #5321

--- app/models/deadline_type.rb
+++ app/models/deadline_type.rb
@@ -1,5 +1,5 @@
 class DeadlineType < ActiveRecord::Base
   has_many :assignment_due_dates, class_name: 'AssignmentDueDate', foreign_key: 'deadline_type_id'
   has_many :topic_due_dates, class_name: 'TopicDueDate', foreign_key: 'deadline_type_id'
-  attr_accessible :name
+  attr_accessible :name, :id
 end

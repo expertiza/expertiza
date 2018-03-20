246a2831dd961df332151ae5f86ce23302a41c05
Dileep <dileep.badveli@gmail.com>
2018-03-20 11:25:54 -0400

    Added the base examples

---
+++ spec/models/on_the_fly_calc_spec.rb
@@ -0,0 +1,11 @@
+describe OnTheFlyCalc do
+  describe "#compute total score" do
+  end
+  describe "#compute reviews hash" do
+  end
+  describe "#compute avg and ranges hash" do
+  end
+  descirbe"#scores" do
+  end
+
+end

8c7faae6546bff957fef47b11cb7ea84f5e2cc15
xzhang72 <xzhang72@ncsu.edu>
2018-03-20 20:14:35 -0400

    add :review_assignment_strategy, :review_topic_threshold, :max_reviews_per_submission to attr_accessible in assignments.rb, fix 5329

--- app/models/assignment_team.rb
+++ app/models/assignment_team.rb
@@ -4,7 +4,7 @@ class AssignmentTeam < Team
   has_many :review_response_maps, foreign_key: 'reviewee_id'
   has_many :responses, through: :review_response_maps, foreign_key: 'map_id'
 
-  attr_accessible :name, :parent_id, :type, :name, :directory_path, :require_quiz, :course_id, :max_team_size, :review_assignment_strategy, :review_topic_threshold, :max_reviews_per_submission
+  attr_accessible :name, :parent_id, :type, :name, :directory_path, :require_quiz, :course_id, :max_team_size
 
   # START of contributor methods, shared with AssignmentParticipant
 

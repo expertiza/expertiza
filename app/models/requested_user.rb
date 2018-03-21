d5351f080d968224f2d17969719e49ad66417954
xxxxavier <haooline@hotmail.com>
2018-03-21 14:54:46 -0400

    Update requested_user.rb

--- app/models/requested_user.rb
+++ app/models/requested_user.rb
@@ -1,7 +1,6 @@
 class RequestedUser < ActiveRecord::Base
   before_save { self.email = email.downcase }
   before_save { self.name }
-  attr_accessible :name, :role_id, :fullname, :institution_id, :email, :status, :introduction, :self_introduction
   validates :name, presence: true, length: {maximum: 50, message: "is too long"}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255, message: "is too long"},

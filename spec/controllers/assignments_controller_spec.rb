636e1e12b772f97a0a1bbbd0de48e086cc42034d
Daniel Burcal <djburcal@ncsu.edu>
2018-03-26 23:17:11 -0400

    Changing tests

--- spec/controllers/assignments_controller_spec.rb
+++ spec/controllers/assignments_controller_spec.rb
@@ -195,7 +195,7 @@ describe AssignmentsController do
 
       it 'allows a topic to be deleted' do
         params = {id: 1, anchor: 'tabs-2'}
-        get :edit, params, xhr: false
+        get :edit, params, xhr: true
         expect(response).to respond_to(:destroy)
       end
 
@@ -224,25 +224,26 @@ describe AssignmentsController do
       end
 
       it 'does not allow a new topic to be added' do
-        params = {id: 1, anchor: 'tabs-2'}
-        get :edit, params, xhr: true
+        params = {id: 1, anchor: '#tabs-2'}
+        get :edit, params
+        expect(response).to render_template(:partial => 'sign_up_topics/add_signup_topics')
         expect(response).not_to respond_to(:new)
       end
     end
 
-    context 'team has ad', js: true do
-      it 'displays the ad horn in the manage topics table' do
-        login_as('instructor6')
-        visit "/assignments/1/edit#tabs-2"
-        @team = Team.new(id: 1, advertise_for_partner: 1)
-        @signed_up_team = SignedUpTeam.new(id: 1, topic_id: 1)
-        allow(SignUpTopic).to receive(:find).with('1').and_return(topic)
-        # params = {id: 1}
-        # get :edit, params, xhr: true, with: '/#tabs-2'
-        # expect(response).to have_css('img', text: 'ad_horn.png')
-        expect(response).to have_content("ad_horn.png")
-      end
-    end
+    # context 'team has ad', js: true do
+    #   it 'displays the ad horn in the manage topics table' do
+    #     login_as('instructor6')
+    #     visit "/assignments/1/edit#tabs-2"
+    #     @team = Team.new(id: 1, advertise_for_partner: 1)
+    #     @signed_up_team = SignedUpTeam.new(id: 1, topic_id: 1)
+    #     allow(SignUpTopic).to receive(:find).with('1').and_return(topic)
+    #     # params = {id: 1}
+    #     # get :edit, params, xhr: true, with: '/#tabs-2'
+    #     # expect(response).to have_css('img', text: 'ad_horn.png')
+    #     expect(response).to have_content("ad_horn.png")
+    #   end
+    # end
   end
 
   describe '#update' do

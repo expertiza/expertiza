ad755ca6dd116d266ca9aa5abcef57a374114223
ryan.potocnik <ryanpotocnik@yahoo.com>
2018-03-27 22:01:59 -0400

    Added commit information broken up by date

--- app/controllers/github_data_controller.rb
+++ app/controllers/github_data_controller.rb
@@ -62,44 +62,39 @@ class GithubDataController < ApplicationController
   GITHUB_PULL_REGEX = %r(https?:\/\/(?:[w]{3}\.)?github\.com\/([A-Z0-9_\-]+)\/([A-Z0-9_\-]+)\/pull\/([0-9]+)[\S]*)i
 
   def show
+    # assignment = Assignment.find(@submission_record.assignment_id)
+    # @assignment_start_date = assignment.created_at
+    # puts "Assignment start: #{@assignment_start_date}"
+    # @assignment_end_date = @assignment_start_date
+    # assignment.due_dates.each do |due_date|
+    #   @assignment_end_date = due_date.due_at if due_date.deadline_type.name == "submission" and
+    #       due_date.due_at > @assignment_end_date
+    # end
+    # puts "Deadline: #{@assignment_end_date}"
     owner, repo, pull_number = retrieve_github_url(@submission_record)
-    assignment = Assignment.find(@submission_record.assignment_id)
-    @assignment_start_date = assignment.created_at
-    puts "Assignment start: #{@assignment_start_date}"
-    @assignment_end_date = @assignment_start_date
-    assignment.due_dates.each do |due_date|
-      @assignment_end_date = due_date.due_at if due_date.deadline_type.name == "submission" and
-          due_date.due_at > @assignment_end_date
-    end
-    puts "Deadline: #{@assignment_end_date}"
     unless pull_number.nil?
       retrieve_graphql_data(owner, repo, pull_number)
     end
   end
 
   def retrieve_graphql_data(owner, repo, pull)
+    #Run GraphQL query
     data = query IndexQuery, owner: owner, name: repo, pull: pull.to_i
-    github_commits = data.repository.pull_request.commits
-    puts "TotalCommits: #{github_commits.total_count}"
     @commits = Array.new
     github_commits.nodes.each do |node|
+      #Add all commits except for merge commits because large and no work actually done in this step
       @commits.push(node.commit) unless node.commit.message.start_with? "Merge"
-      # puts "SHA: #{node.commit.oid}"
-      # puts "Name: #{node.commit.committer.name}"
-      # puts "Committed Date: #{DateTime.parse(node.commit.committed_date)}"
-      # puts "Additions: #{node.commit.additions}"
-      # puts "Deletions: #{node.commit.deletions}"
-      # puts "Message: #{node.commit.message}"
-      # puts "Files changed: #{node.commit.changed_files}",""
     end
+    #New hash with default value 0
     @commits_by_user = Hash.new(0)
-    @commits_by_date = Hash.new(0)
+    #New nested hash with nested values defaulting to 0
+    @changes_by_date = Hash.new { |h,k| h[k] = Hash.new(0) }
     @commits.each do |commit|
       @commits_by_user[commit.committer.name] += 1
-      @commits_by_date[DateTime.parse(commit.committed_date).strftime('%Y-%m-%d')] += 1
+      @changes_by_date[DateTime.parse(commit.committed_date).strftime('%Y-%m-%d')][:commits] += 1
+      @changes_by_date[DateTime.parse(commit.committed_date).strftime('%Y-%m-%d')][:additions] += commit.additions
+      @changes_by_date[DateTime.parse(commit.committed_date).strftime('%Y-%m-%d')][:deletions] += commit.deletions
     end
-    puts @commits_by_user
-    puts @commits_by_date
   end
 
   def retrieve_github_url(submission)

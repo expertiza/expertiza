c7355476ccd75c0151be178729767469cba2c5a1
Satvik Shetty <satvikshetty04@gmail.com>
2018-04-01 04:18:54 -0400

    Fixed sample submissions when due date not set

--- app/controllers/sample_submissions_controller.rb
+++ app/controllers/sample_submissions_controller.rb
@@ -10,7 +10,10 @@ class SampleSubmissionsController < ApplicationController
     @assignment_teams = AssignmentTeam.where(:parent_id => sample_submission_params[:id], :make_public=>true)
     @assignment = Assignment.where(:id => sample_submission_params[:id]).first
     @assignment_teams_professor = AssignmentTeam.where(:parent_id => @assignment.sample_assignment_id, :make_public=>true)
-    @assignment_due_date = DueDate.where(:parent_id => @assignment.id).last.due_at
+    @assignment_due_date = DueDate.where(:parent_id => @assignment.id).last
+    if @assignment_due_date != nil
+      @assignment_due_date = @assignment_due_date.due_at
+    end
   end
 
   private

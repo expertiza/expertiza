0fc728c8b6f6d0d720e17529627e516063e346f4
Dileep <dileep.badveli@gmail.com>
2018-04-01 19:51:52 -0400

    some tests now work

--- spec/models/on_the_fly_calc_spec.rb
+++ spec/models/on_the_fly_calc_spec.rb
@@ -4,10 +4,10 @@ describe OnTheFlyCalc do
   let(:team) { build(:assignment_team, id: 1, name: 'no team') }
 
   describe OnTheFlyCalc do
-    let(:assignment) { create(:assignment, id: 1,rounds_of_reviews: 1,teams: [team],participants: [participant])}
+    let(:assignment1) { create(:assignment,name: "uni1",rounds_of_reviews: 1,teams: [team],participants: [participant])}
     let(:questionnaire) { create(:questionnaire, id: 1)}
     let(:assignment_questionnaire) {create(:assignment_questionnaire, assignment: assignment, used_in_round: 0)}
-    let(:assignment2) { create(:assignment, id: 2,rounds_of_reviews: 2,teams: [team2], participants: [participant2])}
+    let(:assignment2) { create(:assignment,name: "uni2",rounds_of_reviews: 2,teams: [team2], participants: [participant2])}
     let(:assignment_questionnaire2) { create(:assignment_questionnaire, assignment: assignment2, used_in_round: 2, questionnaire: questionnaire)}
     let(:response) {create(:response)}
     let(:response2) {create(:response)}
@@ -16,13 +16,12 @@ describe OnTheFlyCalc do
     let(:question){create(:question, questionnaire: questionnaire)}
     let(:team){create(:assignment_team, id:1)}
     let(:team2){create(:assignment_team, id:2)}
-    let(:participant) {create(:assignment_participant, id=1)}
-    let(:participant2) {create(:assignment_participant, id=2)}
-    #let(:participant2) {Assignment_participant.new.id}
+    let(:participant) {create(:participant, id:1)}
+    let(:participant2) {create(:participant, id:2)}
     describe '#compute_total_score' do
       it 'return total scores' do
         @score={"2"=>{:scores=>{:avg=>4.5}}}
-        allow(Questionnaire).to receive(:get_weighted_score).with(assignment,@score).and_return(4.5)
+        allow(Questionnaire).to receive(:get_weighted_score).with(assignment1,@score).and_return(4.5)
         expect(questionnaire.get_weighted_score(assignment2, @score)).to eq(4.5)
       end
     end
@@ -30,7 +29,7 @@ describe OnTheFlyCalc do
       before(:each) do
         allow(Assignment).to receive(:find).with('2').and_return(assignment2)
         allow(AssignmentQuestionnaire).to receive(:where).with(assignment_id: 2, used_in_round: 2).and_return([assignment_questionnaire2])
-        allow(Assignment).to receive(:find).with('1').and_return(assignment)
+        allow(Assignment).to receive(:find).with('1').and_return(assignment1)
         allow(AssignmentQuestionnaire).to receive(:where).with(assignment_id: 1, used_in_round: 0).and_return([assignment_questionnaire])
       end
       it 'for varying_rubrics_by_round case' do
@@ -40,33 +39,30 @@ describe OnTheFlyCalc do
       end
       it 'for non-varying_rubrics_by_round case' do
         allow(Assignment).to receive(:varying_rubrics_by_round?).and_return(false)
-        expect(assignment.varying_rubrics_by_round?).to eq(false)
-        allow(ResponseMap).to receive(:where).with('reviewed_object_id = ? && type = ?', assignment.id, 'ReviewResponseMap')
+        expect(assignment1.varying_rubrics_by_round?).to eq(false)
+        allow(ResponseMap).to receive(:where).with('reviewed_object_id = ? && type = ?', assignment1.id, 'ReviewResponseMap')
       end
     end
     describe '#compute_avg_and_ranges_hash' do
-      before(:each) do
-        allow(ReviewResponseMap).to receive(:get_assessments_for).with(team)
-        allow(Answer).to receive(:compute_scores)
-      end
       it 'for varying_rubrics_by_round case' do
-        expect(assignment.contributors).to eq(team)
-        allow(Assignment).to receive(:review_questionnaire_id)
-        allow(Question).to receive(:where).with('questionnaire_id = ?', [assignment_questionnaire])
+        allow(Assignment).to receive(:varying_rubrics_by_round?).and_return(true)
+        expect(assignment1.contributors).to eq([team])
 
       end
       it 'for non-varying_rubrics_by_round case' do
-        expect(assignment2.contributors).to eq(team2)
+        allow(Assignment).to receive(:varying_rubrics_by_round?).and_return(false)
+        expect(assignment2.contributors).to eq([team2])
       end
     end
     describe '#scores' do
       it 'for varying_rubrics_by_round case' do
-        allow(Assignment).to receive(:calculate_score)
+        allow(Assignment).to receive(:varying_rubrics_by_round?).and_return(true)
+        expect(Assignment).to receive(:calculate_score)
       end
       it 'for non-varying_rubrics_by_round case' do
         allow(Assignment).to receive(:varying_rubrics_by_round?).and_return(false)
       end
-      allow(ReviewResponseMap).to receive(:get_assessments_for).with(team)
+      #allow(ReviewResponseMap).to receive(:get_assessments_for).with(team)
     end
 
   end

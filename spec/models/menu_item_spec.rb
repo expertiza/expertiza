49dfa36482cf8bdbd91b1ef2937c42ab195df282
Kalyan Ghosh <24x7kalyan@gmail.com>
2018-04-02 10:56:36 -0400

    Update menu_item_spec.rb

--- spec/models/menu_item_spec.rb
+++ spec/models/menu_item_spec.rb
@@ -1,6 +1,5 @@
 describe MenuItem do
   ###
-  # Please do not share this file with other teams.
   # Use factories to `build` necessary objects.
   # Please avoid duplicated code as much as you can by moving the code to `before(:each)` block or separated methods.
   # RSpec tutorial video (until 9:32): https://youtu.be/dzkVfaKChSU?t=35s
@@ -37,14 +36,12 @@ describe MenuItem do
 
   describe '.find_or_create_by_name' do
     it 'returns a menu item with corresponding name' do
-    # Write your test here!
       expect(MenuItem.find_or_create_by_name("home").name).to eq("home")
     end
   end
 
   describe '#delete' do
     it 'deletes current menu items and all child menu items' do
-    # Write your test here!
             expect{@test1.delete}.to change{MenuItem.count}.by(-4)
     end
   end
@@ -52,14 +49,12 @@ describe MenuItem do
   describe '#above' do
     context 'when current menu item has parent_id' do
       it 'returns the first parent menu item by querying the parent_id and current sequence number minus one' do
-      # Write your test here!
         expect(@test4.above).to eq(@test3)
       end
     end
 
     context 'when current menu item does not have parent_id' do
       it 'returns the first parent menu item by querying the current sequence number minus one' do
-      # Write your test here!
         expect(@test5.above).to eq(@test1) 
       end
     end
@@ -68,14 +63,12 @@ describe MenuItem do
   describe '#below' do
     context 'when current menu item has parent_id' do
       it 'returns the first parent menu item by querying the parent_id and current sequence number plus one' do
-      # Write your test here!
         expect(@test3.below).to eq(@test4)
       end
     end
 
     context 'when current menu item does not have parent_id' do
       it 'returns the first parent menu item by querying the current sequence number plus one' do
-      # Write your test here!
         expect(@test1.below).to eq(@test5) 
       end
     end
@@ -84,7 +77,6 @@ describe MenuItem do
   describe '.repack' do
     context 'when current menu item has repack_id' do
       it 'finds all menus items with parent_id equal to repack_id and repacks the sequence number' do
-      # Write your test here!
 	@temp = MenuItem.repack(1)
 	@test2.reload
 	@test3.reload
@@ -96,7 +88,6 @@ describe MenuItem do
 
     context 'when current menu item does not have repack_id' do
       it 'finds all menus items with parent_id null and repacks the sequence number' do
-      # Write your test here!
 	@temp = MenuItem.repack(nil)
 	@test1.reload
 	@test5.reload
@@ -110,14 +101,12 @@ describe MenuItem do
   describe '.next_seq' do
     context 'when parent_id is bigger than 0' do
       it 'selects corresponding menu items with inputted parent_id and returns the next sequence number' do
-      # Write your test here!
         expect(MenuItem.next_seq(1)).to eq(5)
       end
     end
 
     context 'when parent_id is smaller than or equal to 0' do
       it 'selects corresponding menu items with parent_id null and returns the next sequence number' do
-      # Write your test here!
         expect(MenuItem.next_seq(nil)).to eq(6)
       end
     end
@@ -128,7 +117,6 @@ describe MenuItem do
       context 'when the controller_action_id of current item is bigger than 0' do
         context 'when perms does not exist' do
           it 'returns corresponding items' do
-          # Write your test here!
             @test7 = MenuItem.new(name: "home7", parent_id: nil, seq: 7, controller_action_id: 1, content_page_id: nil, label: "newlabel")
             @test7.save
             @test8 = MenuItem.new(name: "home8", parent_id: 1, seq: 8, controller_action_id: 2, content_page_id: nil, label: "newlabel")
@@ -145,7 +133,6 @@ describe MenuItem do
       context 'when the controller_action_id of current item is smaller than or equal to 0 and the content_page_id of current item is bigger than 0' do
         context 'when perms does not exist' do
           it 'returns corresponding items' do
-          # Write your test here!
             @test7 = MenuItem.new(name: "home7", parent_id: nil, seq: 7, controller_action_id: nil, content_page_id: 1, label: "newlabel")
             @test7.save
             @test8 = MenuItem.new(name: "home8", parent_id: 1, seq: 8, controller_action_id: nil, content_page_id: 2, label: "newlabel")
@@ -161,7 +148,6 @@ describe MenuItem do
 
       context 'when the controller_action_id and content_page_id of current item is smaller than or equal to 0' do
         it 'returns corresponding items' do
-        # Write your test here!
 	  expect(MenuItem.items_for_permissions).to be_empty
 	end
       end
@@ -171,7 +157,6 @@ describe MenuItem do
       context 'when the controller_action_id of current item is bigger than 0' do
         context 'when perms exists' do
           it 'returns corresponding items' do
-          # Write your test here!
             @test7 = MenuItem.new(name: "home7", parent_id: nil, seq: 7, controller_action_id: 1, content_page_id: nil, label: "newlabel")
             @test7.save
             @test8 = MenuItem.new(name: "home8", parent_id: 1, seq: 8, controller_action_id: 2, content_page_id: nil, label: "newlabel")
@@ -188,7 +173,6 @@ describe MenuItem do
       context 'when the controller_action_id of current item is smaller than or equal to 0 and the content_page_id of current item is bigger than 0' do
         context 'when perms exists' do
           it 'returns corresponding items' do
-          # Write your test here!
             @test7 = MenuItem.new(name: "home7", parent_id: nil, seq: 7, controller_action_id: nil, content_page_id: 1, label: "newlabel")
             @test7.save
             @test8 = MenuItem.new(name: "home8", parent_id: 1, seq: 8, controller_action_id: nil, content_page_id: 2, label: "newlabel")
@@ -204,7 +188,6 @@ describe MenuItem do
 
       context 'when the controller_action_id and content_page_id of current item is smaller than or equal to 0' do
         it 'returns corresponding items' do
-        # Write your test here!
 	  expect(MenuItem.items_for_permissions([1])).to be_empty
 	end
       end

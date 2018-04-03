beb13f5448c19ee22dcff3819df44366cb2be147
Ashis Kumar Sahoo <ashiskumar123@gmail.com>
2018-04-03 08:57:07 -0400

    Code Climate fix.

--- spec/features/sample_submission_spec.rb
+++ spec/features/sample_submission_spec.rb
@@ -1,21 +1,29 @@
-describe "sample submission test" do
-  def create_assignment_team(assignment_name, parent_id)
-    assignment_team = AssignmentTeam.new
-    assignment_team.name = assignment_name
-    assignment_team.parent_id = parent_id
-    assignment_team.save!
-  end
-  before(:each) do
-    # create assignment and topic
-    assignment = build(Assignment)
-    course = Course.new
-    course.name = "SampleSubmissionTestCourse"
-    course.save
-    assignment.course_id = course.id
-    assignment.save
+def create_assignment_team(assignment_name, parent_id)
+  assignment_team = AssignmentTeam.new
+  assignment_team.name = assignment_name
+  assignment_team.parent_id = parent_id
+  assignment_team.save!
+end
+def init_test
+  # create assignment and topic
+  assignment = build(Assignment)
+  course = Course.new
+  course.name = "SampleSubmissionTestCourse"
+  course.save
+  assignment.course_id = course.id
+  assignment.save
 
-    create_assignemnt_team("ss_assignment_team_1", assignment.id)
-    create_assignemnt_team("ss_assignment_team_2", assignment.id)
+  create_assignemnt_team("ss_assignment_team_1", assignment.id)
+  create_assignemnt_team("ss_assignment_team_2", assignment.id)
+end
+def visit_sample_submissions_page
+  visit '/student_task/list'
+  click_on "Example Assignment"
+  click_on "Sample Submissions"
+end
+describe "sample submission test" do
+  before(:each) do
+    init_test
   end
 
   it "is able to make an assignment public" do
@@ -25,40 +33,30 @@ describe "sample submission test" do
   end
 
   it "is able to view sample submissions page" do
-    visit '/student_task/list'
-    click_on "Example Assignment"
-    click_on "Sample Submissions"
+    visit_sample_submissions_page
     expect(page).to have_http_status(200)
   end
 
   it "should not see current assignment submissions if deadline is not met" do
     # Set deadline after current time.
-    visit '/student_task/list'
-    click_on "Example Assignment"
-    click_on "Sample Submissions"
+    visit_sample_submissions_page
     expect(page).to have_content "No sample submissions from current assignment made public yet"
   end
 
   it "should see current assignment submissions if deadline is met" do
     # Set deadline before current time.
-    visit '/student_task/list'
-    click_on "Example Assignment"
-    click_on "Sample Submissions"
+    visit_sample_submissions_page
     expect(page).to_not have_content "No sample submissions from current assignment made public yet"
   end
 
   it "should not see instructor selected submissions if instructor has not selected them" do
-    visit '/student_task/list'
-    click_on "Example Assignment"
-    click_on "Sample Submissions"
+    visit_sample_submissions_page
     expect(page).to have_content "No sample submissions from previous assignment made available yet"
   end
 
   it "should see instructor selected submissions if instructor has selected them" do
     # Instructor makes submission available.
-    visit '/student_task/list'
-    click_on "Example Assignment"
-    click_on "Sample Submissions"
+    visit_sample_submissions_page
     expect(page).to_not have_content "No sample submissions from previous assignment made available yet"
   end
 end

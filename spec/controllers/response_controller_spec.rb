6cb8c137e3a5fb13abdb37e86bc261f5edd52735
ferry <ferryxo@gmail.com>
2018-04-05 17:12:47 -0400

    assignment>review_questionnaire_id if round is nil, check due date first to get the round.

--- spec/controllers/response_controller_spec.rb
+++ spec/controllers/response_controller_spec.rb
@@ -138,6 +138,7 @@ describe ResponseController do
       allow(AssignmentQuestionnaire).to receive(:where).with(assignment_id: 1).and_return([assignment_questionnaire])
       # set_dropdown_or_scale
       allow(AssignmentQuestionnaire).to receive(:where).with(assignment_id: 1, questionnaire_id: 1).and_return([assignment_questionnaire])
+      allow(AssignmentQuestionnaire).to receive(:where).with(assignment_id: 1, used_in_round: 1).and_return([assignment_questionnaire])
       allow(Questionnaire).to receive(:find).with(any_args).and_return(questionnaire)
       allow(Questionnaire).to receive(:questions).and_return(question)
       allow(Answer).to receive(:create).and_return(answer)

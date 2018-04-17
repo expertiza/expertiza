0981992798d1f82b4cb2ef531719a4588705aba2
Xinhao Zhang <haooline@hotmail.com>
2018-04-17 17:07:56 -0400

    Add space after comma

--- app/controllers/answer_controller.rb
+++ app/controllers/answer_controller.rb
@@ -17,8 +17,8 @@ class AnswerController < ApplicationController
     response_id = params[:response_id] if params.key?(:response_id)
     questionnaire_id = params[:questionnaire_id] if params.key?(:questionnaire_id)
     # get all answers given the questionaire and response id
-    join_query = sanitize_sql_array(["LEFT JOIN answers ON answers.question_id = questions.id AND answers.response_id = '?'",response_id])
-    where_query = sanitize_sql_array(["questions.questionnaire_id = '?'",questionnaire_id])
+    join_query = sanitize_sql_array(["LEFT JOIN answers ON answers.question_id = questions.id AND answers.response_id = '?'", response_id])
+    where_query = sanitize_sql_array(["questions.questionnaire_id = '?'", questionnaire_id])
     question_answers = Question.joins(join_query)
                            .select('answers.*, questions.txt as qtxt, questions.type as qtype, questions.seq as qseq')
                            .where(where_query)

936b87451846ab9f893a65653ec0068f6e75dd64
Saurabh Labde <saurabhlabde@Saurabhs-MacBook-Pro.local>
2018-04-27 16:43:47 -0400

    working changes

--- app/models/assignment_duty_questionnaire_mapping.rb
+++ app/models/assignment_duty_questionnaire_mapping.rb
@@ -3,5 +3,9 @@ class AssignmentDutyQuestionnaireMapping < ActiveRecord::Base
 		assignment_questionnaire_obj = AssignmentQuestionnaire.where(assignment_id: assignment_id).pluck(:questionnaire_id)
 		assignment_questionnaire_obj.last
 	end
+
+	def self.get_duty_id(assignment_id)
+		assignment_questionnaire = AssignmentDutyQuestionnaireMapping.where(assignment_id: assignment_id).pluck(:duty_id)
+	end
 end
 

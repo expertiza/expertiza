200c538bb2711a36a3b212189d121c0aa353963b
Saurabh Labde <saurabhlabde@Saurabhs-MacBook-Pro.local>
2018-04-26 20:32:13 -0400

    Questionnaire Id saved in Assignment Duty Questionnaire Mapping Table

--- app/models/assignment_duty_questionnaire_mapping.rb
+++ app/models/assignment_duty_questionnaire_mapping.rb
@@ -1,7 +1,7 @@
 class AssignmentDutyQuestionnaireMapping < ActiveRecord::Base
 	def self.get_questionnaire_id(assignment_id)
 		assignment_questionnaire_obj = AssignmentQuestionnaire.where(assignment_id: assignment_id).pluck(:questionnaire_id)
-		
+		assignment_questionnaire_obj[2]
 	end
 end
 

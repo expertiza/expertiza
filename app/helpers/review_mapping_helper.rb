8e9d2c78ee9ee89f91c58b369447249bbb0e54e7
ysgeer <ysgeer@gmail.com>
2018-04-28 23:59:39 -0400

    climate fixes

--- app/helpers/review_mapping_helper.rb
+++ app/helpers/review_mapping_helper.rb
@@ -24,7 +24,7 @@ module ReviewMappingHelper
   def get_data_for_review_report(reviewed_object_id, reviewer_id, type)
     rspan = 0
 
-    (1..@assignment.num_review_rounds).each { |round| instance_variable_set("@review_in_round_" + round.to_s, 0)}
+    (1..@assignment.num_review_rounds).each { |round| instance_variable_set("@review_in_round_" + round.to_s, 0) }
 
     response_maps = ResponseMap.where(["reviewed_object_id = ? AND reviewer_id = ? AND type = ?", reviewed_object_id, reviewer_id, type])
     response_maps.each do |ri|
@@ -55,7 +55,7 @@ module ReviewMappingHelper
             color.push 'purple'
           else
             link = submitted_hyperlink(round, response_map, assignment_created, assignment_due_dates)
-            if link.nil? or (link !~ /https*:\/\/wiki(.*)/) # can be extended for github links in future
+            if link.nil? or (link !~ %r{https*:\/\/wiki(.*)}) # can be extended for github links in future
               color.push 'green'
             else
               link_updated_at = get_link_updated_at(link)
@@ -97,7 +97,7 @@ module ReviewMappingHelper
     submission_due_date = assignment_due_dates.where(round: round, deadline_type_id: 1).try(:first).try(:due_at)
     subm_hyperlink = SubmissionRecord.where(team_id: response_map.reviewee_id, operation: 'Submit Hyperlink')
     submitted_h = subm_hyperlink.where(created_at: assignment_created..submission_due_date)
-    link = submitted_h.try(:last).try(:content)
+    submitted_h.try(:last).try(:content)
   end
 
   # returns last modified header date
@@ -112,7 +112,7 @@ module ReviewMappingHelper
   def link_updated_since_last?(round, due_dates, link_updated_at)
     submission_due_date = due_dates.where(round: round, deadline_type_id: 1).try(:first).try(:due_at)
     submission_due_last_round = due_dates.where(round: round - 1, deadline_type_id: 1).try(:first).try(:due_at)
-    updated = (link_updated_at < submission_due_date) && (link_updated_at > submission_due_last_round)
+    (link_updated_at < submission_due_date) && (link_updated_at > submission_due_last_round)
   end
 
   def get_team_reviewed_link_name(max_team_size, response, reviewee_id)
@@ -134,7 +134,7 @@ module ReviewMappingHelper
 
   # varying rubric by round
   def get_each_round_score_awarded_for_review_report(reviewer_id, team_id)
-    (1..@assignment.num_review_rounds).each { |round| instance_variable_set("@score_awarded_round_" + round.to_s, '-----')}
+    (1..@assignment.num_review_rounds).each { |round| instance_variable_set("@score_awarded_round_" + round.to_s, '-----') }
     (1..@assignment.num_review_rounds).each do |round|
       if @review_scores[reviewer_id] && @review_scores[reviewer_id][round] && @review_scores[reviewer_id][round][team_id] && @review_scores[reviewer_id][round][team_id] != -1.0
         instance_variable_set("@score_awarded_round_" + round.to_s, @review_scores[reviewer_id][round][team_id].inspect + '%')
@@ -143,7 +143,7 @@ module ReviewMappingHelper
   end
 
   def get_min_max_avg_value_for_review_report(round, team_id)
-    %i[max min avg].each { |metric| instance_variable_set('@' + metric.to_s, '-----')}
+    %i[max min avg].each { |metric| instance_variable_set('@' + metric.to_s, '-----') }
     if @avg_and_ranges[team_id] && @avg_and_ranges[team_id][round] && %i[max min avg].all? { |k| @avg_and_ranges[team_id][round].key? k}
       %i[max min avg].each do |metric|
         metric_value = @avg_and_ranges[team_id][round][metric].nil? ? '-----' : @avg_and_ranges[team_id][round][metric].round(0).to_s + '%'
@@ -159,11 +159,11 @@ module ReviewMappingHelper
           r.avg_vol_in_round_2,
           r.avg_vol_in_round_3 = Response.get_volume_of_review_comments(@assignment.id, r.id)
     end
-    @all_reviewers_overall_avg_vol = @reviewers.inject(0) { |sum, r| sum += r.overall_avg_vol} / @reviewers.length
-    @all_reviewers_avg_vol_in_round_1 = @reviewers.inject(0) { |sum, r| sum += r.avg_vol_in_round_1} / @reviewers.length
-    @all_reviewers_avg_vol_in_round_2 = @reviewers.inject(0) { |sum, r| sum += r.avg_vol_in_round_2} / @reviewers.length
-    @all_reviewers_avg_vol_in_round_3 = @reviewers.inject(0) { |sum, r| sum += r.avg_vol_in_round_3} / @reviewers.length
-    @reviewers.sort! { |r1, r2| r2.overall_avg_vol <=> r1.overall_avg_vol}
+    @all_reviewers_overall_avg_vol = @reviewers.inject(0) { |sum, r| sum += r.overall_avg_vol } / @reviewers.length
+    @all_reviewers_avg_vol_in_round_1 = @reviewers.inject(0) { |sum, r| sum += r.avg_vol_in_round_1 } / @reviewers.length
+    @all_reviewers_avg_vol_in_round_2 = @reviewers.inject(0) { |sum, r| sum += r.avg_vol_in_round_2 } / @reviewers.length
+    @all_reviewers_avg_vol_in_round_3 = @reviewers.inject(0) { |sum, r| sum += r.avg_vol_in_round_3 } / @reviewers.length
+    @reviewers.sort! { |r1, r2| r2.overall_avg_vol <=> r1.overall_avg_vol }
   end
 
   def display_volume_metric(overall_avg_vol, avg_vol_in_round_1, avg_vol_in_round_2, avg_vol_in_round_3)

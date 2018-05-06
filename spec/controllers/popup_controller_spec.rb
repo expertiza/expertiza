90441d28bb7b008b21e3c7e303e6540a1e564a8b
Expertiza Developer <pnshore@ncsu.edu>
2018-05-06 18:09:00 -0400

    added framework for tests and begin writing tests for tone analysis functions

--- spec/controllers/popup_controller_spec.rb
+++ spec/controllers/popup_controller_spec.rb
@@ -1,7 +1,24 @@
 describe PopupController do
-  before(:each) do
+  let(:team) { build(:assignment_team, id:1, name: "team1", assignment: assignment)}
+  let(:student) { build(:student, id: 1, name: "student")}
+  let(:student2) { build(:student, id: 2, name: "student2")}
+  let(:participant){build(:participant, id:1, user: student, assignment: assignment)}
+  let(:participant2){build(:participant, id:2, user: student2, assignment: assignment)}
+  let(:response) {build(:response, id: 1)}
+  let(:assignment) {build(:assignment, id: 1)}
+  let(:response_map){
+    build(:review_response_map,
+          id: 1,
+          reviewee_id: team.id,
+          reviewer_id: participant2.id,
+          response:[response],
+          assignment: assignment)
+  }
+  final_versions = {:"review round1"=>{:questionnaire_id=>1, :response_ids=>[77024]}, :"review round2"=>{:questionnaire_id=>2, :response_ids=>[]}, :"review round3"=>{:questionnaire_id=>3, :response_ids=>[]}}
+  test_url = "http://testurl"
+  mocked_comments1 = OpenStruct.new(:comments => "test comment")
+  mocked_comments2 = OpenStruct.new( :comments => "test comment2")
 
-  end
 
   describe '#action_allowed?' do
 
@@ -20,20 +37,43 @@ describe PopupController do
   end
 
   ######### Tone Analysis Tests ##########
-  describe '#tone_analysis_chart_popup' do
+  describe "tone analysis tests"do
+    before(:each) do
+      allow(ReviewResponseMap).to receive(:where).with('reviewee_id = ?', team.id).and_return([response_map])
+      allow(Assignment).to receive(:find).with('reviewee_id = ?', team.id).and_return(assignment)
+      allow(ReviewResponseMap).to receive(:final_versions_from_reviewer).with(1).and_return(final_versions)
+      allow(Answer).to receive(:where).with(any_args).and_return(mocked_comments1)
+    end
+    describe '#tone_analysis_chart_popup' do
+      context 'when tone analysis page is loaded, review tone analysis is calculated' do
+        it 'builds a tone analysis report for both the summery and tone analysis pages and returns an array of heat map URLs' do
+          puts mocked_comments1.comments
+          result = get :tone_analysis_chart_popup
+          expect(result.to_a[0]).to eq(test_url)
+        end
+      end
+    end
 
-  end
+    describe '#view_review_scores_popup' do
+      context 'when popup page loads, review tone analysis is calculated' do
 
-  describe '#view_review_scores_popup' do
+      end
+    end
 
-  end
+    describe '#build_tone_analysis_report' do
+      context 'uppon selecting summery, the tone analysis for review comments is calculated and applied to the page' do
+        it 'builds a tone analysis report and returns the heat map URLs' do
+          result = get :build_tone_analysis_report
+          expect(result).to eq(3)
+        end
+      end
+    end
 
-  describe '#build_tone_analysis_report' do
+    describe '#build_tone_analysis_heatmap' do
+      context 'uppon selecting tone analyis, the tone analysis for each reviewers comments is calculate and displayed' do
 
-  end
-
-  describe '#build_tone_analysis_heatmap' do
-
+      end
+    end
   end
   ##########################################
 

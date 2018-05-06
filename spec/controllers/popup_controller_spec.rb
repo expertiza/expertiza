a4674605eea7802e1905d3701237de2206a5040c
Expertiza Developer <pnshore@ncsu.edu>
2018-05-06 14:56:18 -0400

    created template rspec file for popup_controller.rb

---
+++ spec/controllers/popup_controller_spec.rb
@@ -0,0 +1,48 @@
+describe PopupController do
+  before(:each) do
+
+  end
+
+  describe '#action_allowed?' do
+
+  end
+
+  describe '#author_feedback_popup' do
+
+  end
+
+  describe '#team_users_popup' do
+
+  end
+
+  describe '#participants_popup' do
+
+  end
+
+  ######### Tone Analysis Tests ##########
+  describe '#tone_analysis_chart_popup' do
+
+  end
+
+  describe '#view_review_scores_popup' do
+
+  end
+
+  describe '#build_tone_analysis_report' do
+
+  end
+
+  describe '#build_tone_analysis_heatmap' do
+
+  end
+  ##########################################
+
+  describe '#reviewer_details_popup' do
+
+  end
+
+  describe '#self_review_popup' do
+
+  end
+
+end

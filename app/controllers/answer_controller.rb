93bf8e7af274159f2d7f01081ac59f378edf8b8a
Winbobob <zhu6@ncsu.edu>
2018-05-24 16:55:57 -0400

    Fix an issue related to square brackets

--- app/controllers/answer_controller.rb
+++ app/controllers/answer_controller.rb
@@ -16,9 +16,9 @@ class AnswerController < ApplicationController
   # reference: https://stackoverflow.com/questions/35639507/parametrized-join-in-rails-4
   def index
     if params.key?(:response_id)
-        join_query = sanitize_sql_array(["LEFT JOIN answers ON answers.question_id = questions.id AND answers.response_id = '?'", params[:response_id])
+        join_query = sanitize_sql_array(["LEFT JOIN answers ON answers.question_id = questions.id AND answers.response_id = '?'", params[:response_id]])
     end
-    where_query = sanitize_sql_array(["questions.questionnaire_id = '?'", params[:questionnaire_id]) if params.key?(:questionnaire_id)
+    where_query = sanitize_sql_array(["questions.questionnaire_id = '?'", params[:questionnaire_id]]) if params.key?(:questionnaire_id)
     # get all answers given the questionaire and response id
     question_answers = Question.joins(join_query)
                            .select('answers.*, questions.txt as qtxt, questions.type as qtype, questions.seq as qseq')

1ef379047cc83580e3bb0718ed5ab4b24dc79f69
Ed Gehringer <efg@ncsu.edu>
2018-06-07 16:49:04 -0400

    Remove unused methods in admin_controller.rb

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -12,14 +12,6 @@ class AdminController < ApplicationController
     @users = User.where(["role_id = ?", Role.superadministrator.id])
   end
 
-  def new_super_administrator
-    @user = User.new
-  end
-
-  def save_super_administrator
-    PgUsersController.create(Role.superadministrator.id, :admin_controller, :list_super_administrators, :new_super_administrator)
-  end
-
   def show_super_administrator
     @user = User.find(params[:id])
     @role = Role.find(@user.role_id)
@@ -29,28 +21,9 @@ class AdminController < ApplicationController
     @users = User.admins.order(:name).where("parent_id = ?", current_user.id).paginate(page: params[:page], per_page: 50)
   end
 
-  def add_administrator
-    @user = User.new
-    redirect_to 'users#new'
-  end
-
-  def create_administrator
-    save_administrator
-    redirect_to "users#new"
-  end
-
-  def save_administrator # saves newly created administrator to database
-    PgUsersController.create(Role.administrator.id, :admin_controller, :list_administrators, :add_administrator)
-  end
-
   def show_administrator
     @user = User.find(params[:id])
     @role = Role.find(@user.role_id)
-  end
-
-  def remove_administrator
-    User.find(params[:id]).destroy
-    redirect_to action: 'list_administrators'
   end
 
   def list_instructors
@@ -60,14 +33,5 @@ class AdminController < ApplicationController
   def show_instructor
     @user = User.find(params[:id])
     @role = Role.find(@user.role_id)
-  end
-
-  def remove_instructor
-    User.find(params[:id]).destroy
-    redirect_to action: 'list_instructors'
-  end
-
-  def list_users(conditions)
-    @users = User.order(:name).where(conditions).paginate(page: params[:page], per_page: 50)
   end
 end

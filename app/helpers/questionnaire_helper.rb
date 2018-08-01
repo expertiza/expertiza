f13cb83134ba92ff90a9d20fcf208725c579646c
Expertiza Developer <mohits19j95@gmail.com>
2018-08-01 03:09:40 -0400

    refactor questionnaire_helper

--- app/helpers/questionnaire_helper.rb
+++ app/helpers/questionnaire_helper.rb
@@ -4,30 +4,13 @@
 # require 'csv'
 
 module QuestionnaireHelper
-  CSV_QUESTION = 0
-  CSV_TYPE = 1
-  CSV_PARAM = 2
-  CSV_WEIGHT = 3
 
   def self.create_questionnaire_csv(questionnaire, _user_name)
     csv_data = CSV.generate do |csv|
       questionnaire.questions.each do |question|
         # Each row is formatted as follows
         # Question, question advice (from high score to low), type, weight
-        row = []
-        row << question.txt
-        row << question.type
-
-        row << question.alternatives || ''
-        row << question.size || ''
-
-        row << question.weight
-
-        # if questionnaire.section == "Custom"
-        #  row << QuestionType.find_by_question_id(question.id).parameters
-        # else
-        #  row << ""
-        # end
+        row = create_row_from_question(question)
 
         # loop through all the question advice from highest score to lowest score
         adjust_advice_size(questionnaire, question)
@@ -42,52 +25,43 @@ module QuestionnaireHelper
     csv_data
   end
 
+  def self.create_row_from_question(question)
+    row = []
+    row << question.txt
+    row << question.type
+    row << question.alternatives || ''
+    row << question.size || ''
+    row << question.weight
+    row
+  end
+
   def self.get_questions_from_csv(questionnaire, file)
     questions = []
-    custom_rubric = questionnaire.section == "Custom"
 
     CSV::Reader.parse(file) do |row|
       unless row.empty?
-        i = 0
-        score = questionnaire.max_question_score
-        q = Question.new
+        question = Question.new
+        question.true_false = false
 
-        q_type = QuestionType.new if custom_rubric
+        question.txt = row[0].strip unless row[0].nil?
+        question.true_false = row[1].downcase.strip == Question::TRUE_FALSE.downcase unless row[1].nil?
+        question.weight = row[3].strip.to_i unless row[3].nil?
 
-        q.true_false = false
-
-        row.each do |cell|
-          case i
-          when CSV_QUESTION
-            q.txt = cell.strip unless cell.nil?
-          when CSV_TYPE
-            unless cell.nil?
-              q.true_false = cell.downcase.strip == Question::TRUE_FALSE.downcase
-              q_type.q_type = cell.strip if custom_rubric
-            end
-          when CSV_PARAM
-            if custom_rubric
-              q_type.parameters = cell.strip if cell
-            end
-          when CSV_WEIGHT
-            q.weight = cell.strip.to_i if cell
-          else
-            if score >= questionnaire.min_question_score and !cell.nil?
-              a = QuestionAdvice.new(score: questionnaire.min_question_score + i - 4, advice: cell.strip)
-              score -= 1
-              q.question_advices << a
-            end
-          end
-
-          i += 1
+        (questionnaire.min_question_score..questionnaire.max_question_score).each do |i|
+          question.question_advices << QuestionAdvice.new(score: i, advice: row[4 + i].strip) unless row[4 + i].nil?
         end
 
-        q.save
+        question.save
 
-        q_type.question = q if custom_rubric
-        q_type.save if custom_rubric
+        if questionnaire.section == "Custom"
+          question_type = QuestionType.new
+          question_type.q_type = row[1].strip
+          question_type.parameters = row[2].strip
+          question_type.question
+          question_type.save
+        end
 
-        questions << q
+        questions << question
       end
     end
 
@@ -97,11 +71,9 @@ module QuestionnaireHelper
   def self.adjust_advice_size(questionnaire, question)
     # now we only support question advices for scored questions
     return unless question.is_a?(ScoredQuestion)
+    return if questionnaire.max_question_score.nil? || questionnaire.min_question_score.nil?
 
-    max = questionnaire.max_question_score
-    min = questionnaire.min_question_score
-
-    QuestionAdvice.delete_all(["question_id = ? AND (score > ? OR score < ?)", question.id, max, min]) if !max.nil? && !min.nil?
+    QuestionAdvice.delete_all(["question_id = ? AND (score > ? OR score < ?)", question.id, questionnaire.max_question_score, questionnaire.min_question_score])
 
     (questionnaire.min_question_score..questionnaire.max_question_score).each do |i|
       qas = QuestionAdvice.where("question_id = ? AND score = ?", question.id, i)

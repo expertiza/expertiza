148ce4f8728e74491add8127a38f37fff4bd123c
ajcpendyala <ajcpendyala@gmail.com>
2018-08-14 21:12:47 -0400

    removed the duplicated code in toggle access

--- app/helpers/toggle_access_helper.rb
+++ app/helpers/toggle_access_helper.rb
@@ -1,27 +1,22 @@
 module ToggleAccessHelper
   def toggle_access
-    if !Assignment.find_by(id: params[:id]).nil?
-      assignment = Assignment.find_by(id: params[:id])
-      assignment.private = !assignment.private
-      assignment.save
-    elsif !Questionnaire.find_by(id: params[:id]).nil?
-      @questionnaire = Questionnaire.find_by(id: params[:id])
-      @questionnaire.private = !@questionnaire.private
-      @questionnaire.save
-      @access = @questionnaire.private == true ? "private" : "public"
-      undo_link("the questionnaire \"#{@questionnaire.name}\" has been successfully made #{@access}.")
-    elsif !Course.find_by(id: params[:id]).nil?
-      @course = Course.find_by(id: params[:id])
-      @course.private = !@course.private
-      begin
-        @course.save!
-      rescue StandardError
-        flash[:error] = $ERROR_INFO
-      end
-      @access = @course.private == true ? "private" : "public"
-      undo_link("The course \"#{@course.name}\" has been successfully made #{@access}.")
-    end
 
+    @object = params[:controller].classify.constantize.find(params[:id])
+    @object.private = !@object.private
+
+    begin
+      @object.save!
+    rescue StandardError
+      flash[:error] = $ERROR_INFO
+    end
+    @access = @object.private == true ? "private" : "public"
+
+    case params[:controller]
+    when 'QuestionnairesController'
+      undo_link("The questionnaire \"#{@object.name}\" has been successfully made #{@access}.")
+    when 'CourseController'
+      undo_link("The course \"#{@object.name}\" has been successfully made #{@access}.")
+    end
     redirect_to controller: 'tree_display', action: 'list'
   end
 end

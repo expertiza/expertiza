7dee9195557987ee20d27dc0e5588a745400fdce
Winbobob <zhu6@ncsu.edu>
2018-10-05 12:00:53 -0400

    Further refactor assignment_team.rb

--- app/models/assignment_team.rb
+++ app/models/assignment_team.rb
@@ -1,4 +1,7 @@
 class AssignmentTeam < Team
+  require File.dirname(__FILE__) + '/analytic/assignment_team_analytic'
+  include AssignmentTeamAnalytic
+
   belongs_to :assignment, class_name: 'Assignment', foreign_key: 'parent_id'
   has_many :review_mappings, class_name: 'ReviewResponseMap', foreign_key: 'reviewee_id'
   has_many :review_response_maps, foreign_key: 'reviewee_id'
@@ -38,9 +41,7 @@ class AssignmentTeam < Team
   def assign_reviewer(reviewer)
     assignment = Assignment.find(self.parent_id)
     raise "The assignment cannot be found." if assignment.nil?
-
-    ReviewResponseMap.create(reviewee_id: self.id, reviewer_id: reviewer.id,
-                             reviewed_object_id: assignment.id)
+    ReviewResponseMap.create(reviewee_id: self.id, reviewer_id: reviewer.id, reviewed_object_id: assignment.id)
   end
 
   # Evaluates whether any contribution by this team was reviewed by reviewer
@@ -53,8 +54,8 @@ class AssignmentTeam < Team
   # This method needs refactoring: it sounds like it returns a topic object but in fact it returns an id
   def topic
     team_topic = nil
-    participants.each do |participant|
-      team_topic = SignedUpTeam.topic_id(participant.parent_id, participant.user_id)
+    participants.each do |p|
+      team_topic = SignedUpTeam.topic_id(p.parent_id, p.user_id)
       break if team_topic
     end
     team_topic
@@ -62,7 +63,7 @@ class AssignmentTeam < Team
 
   # Whether the team has submitted work or not
   def has_submissions?
-    !self.submitted_files.empty? or self.submitted_hyperlinks.present?
+    self.submitted_files.any? or self.submitted_hyperlinks.present?
   end
 
   # Get Participants of the team
@@ -75,7 +76,6 @@ class AssignmentTeam < Team
     end
     participants
   end
-
   alias get_participants participants
 
   # Delete the team
@@ -95,7 +95,7 @@ class AssignmentTeam < Team
 
   # Get the first member of the team
   def self.get_first_member(team_id)
-    find(team_id).participants.first
+    find_by(id: team_id).try(:participants).try(:first)
   end
 
   # Return the files residing in the directory of team submissions
@@ -109,7 +109,9 @@ class AssignmentTeam < Team
   # REFACTOR BEGIN:: functionality of import,export, handle_duplicate shifted to team.rb
   # Import csv file to form teams directly
   def self.import(row, assignment_id, options)
-    raise ImportError, "The assignment with the id \"" + id.to_s + "\" was not found. <a href='/assignment/new'>Create</a> this assignment?" if Assignment.find(assignment_id).nil?
+    unless Assignment.find_by(id: assignment_id)
+      raise ImportError, "The assignment with the id \"" + id.to_s + "\" was not found. <a href='/assignment/new'>Create</a> this assignment?"
+    end
     @assignment_team = prototype
     Team.import(row, assignment_id, options, @assignment_team)
   end
@@ -132,9 +134,8 @@ class AssignmentTeam < Team
 
   # Add Participants to the current Assignment Team
   def add_participant(assignment_id, user)
-    if AssignmentParticipant.find_by(parent_id: assignment_id, user_id: user.id).nil?
-      AssignmentParticipant.create(parent_id: assignment_id, user_id: user.id, permission_granted: user.master_permission_granted)
-    end
+    return if AssignmentParticipant.find_by(parent_id: assignment_id, user_id: user.id)
+    AssignmentParticipant.create(parent_id: assignment_id, user_id: user.id, permission_granted: user.master_permission_granted)
   end
 
   # return a hash of scores that the team has received for the questions
@@ -171,7 +172,7 @@ class AssignmentTeam < Team
     end
     files
   end
-  
+
   def submit_hyperlink(hyperlink)
     hyperlink.strip!
     raise 'The hyperlink cannot be empty!' if hyperlink.empty?
@@ -229,19 +230,13 @@ class AssignmentTeam < Team
 
   # Set the directory num for this team
   def set_student_directory_num
-    if self.directory_num.nil? || self.directory_num < 0
-      max_num = AssignmentTeam.where(parent_id: self.parent_id).order('directory_num desc').first.directory_num
-      dir_num = max_num ? max_num + 1 : 0
-      self.update_attribute('directory_num', dir_num)
-      # ACS Get participants irrespective of the number of participants in the team
-      # removed check to see if it is a team assignment
-    end
+    return if self.directory_num and self.directory_num >= 0
+    max_num = AssignmentTeam.where(parent_id: self.parent_id).order('directory_num desc').first.directory_num
+    dir_num = max_num ? max_num + 1 : 0
+    self.update_attributes(directory_num: dir_num)
   end
 
   def received_any_peer_review?
-    !ResponseMap.where(reviewee_id: self.id, reviewed_object_id: self.parent_id).empty?
+    ResponseMap.where(reviewee_id: self.id, reviewed_object_id: self.parent_id).any?
   end
-
-  require File.dirname(__FILE__) + '/analytic/assignment_team_analytic'
-  include AssignmentTeamAnalytic
 end

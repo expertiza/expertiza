473a7c568bdb6e01ec94ffb127f9616fab69cf86
Kevin Acken <kevacken@gmail.com>
2018-10-19 12:26:25 -0400

    adds failing assignment team test

--- spec/models/assignment_team_spec.rb
+++ spec/models/assignment_team_spec.rb
@@ -1,6 +1,7 @@
 describe 'AssignmentTeam' do
   let(:assignment) { build(:assignment) }
   let(:team) { build(:assignment_team) }
+  let(:participant) { build(:participant) }
   let(:team_without_submitted_hyperlinks) { build(:assignment_team, submitted_hyperlinks: "") }
 
   describe "#hyperlinks" do
@@ -13,6 +14,16 @@ describe 'AssignmentTeam' do
     context "when current teams did not submit hyperlinks" do
       it "returns an empty array" do
         expect(team_without_submitted_hyperlinks.hyperlinks).to eq([])
+      end
+    end
+  end
+
+  describe "#include?" do
+    context "when an assignment team has one participant" do
+      it "includes one participant" do
+        participant.parent_id = team.parent_id
+        Assignment.find(team.parent_id).add_participant(User.find(participant.user_id), true , true , true)
+        expect(team.includes?(participant)).to eq true
       end
     end
   end

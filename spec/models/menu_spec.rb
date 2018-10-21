e88f08ba6904bbe740bb67522cbad295bcc253d0
Barrett Bryson <bryson.barrett@gmail.com>
2018-10-21 17:38:14 -0400

    Corrected menu_spec file type, added basic framework for testing

---
+++ spec/models/menu_spec.rb
@@ -0,0 +1,21 @@
+describe Menu do
+
+	describe ".select" do
+	end
+
+	describe ".get_item" do
+	end
+
+	describe ".get_menu" do
+	end
+
+	describe ".selected" do
+	end
+
+	describe ".selected?" do
+	end
+
+	describe ".crumbs" do
+	end
+
+end

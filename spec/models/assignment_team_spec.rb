d2e5a7e193f86c7bf023883cf7de6701a806c461
wwang33 <wwang33@ncsu.edu>
2018-10-22 10:57:47 -0400

    top 10 tests

--- spec/models/assignment_team_spec.rb
+++ spec/models/assignment_team_spec.rb
@@ -1,5 +1,6 @@
 describe AssignmentTeam do
 
+
   # let(:assignment_team1) { AssignmentTeam.new }
   let(:participant1) { build(:participant, id: 1, user: build(:student, name: 'no name', fullname: 'no one')) }
   let(:participant2) { build(:participant, id: 2) }
@@ -141,6 +142,7 @@ describe AssignmentTeam do
       end
     end
   end
+
   #?
   describe "#topic" do
     it "returns the topic id chosen by this team" do
@@ -160,175 +162,13 @@ describe AssignmentTeam do
     end
 
     context "when current assignment team did not submit files but submitted hyperlinks" do
-      it "returns true"
-      # Write your test here!
-    # end
-      # Write your test here!
-    end
+         it "returns true"
+         # Write your test here!
+       end
 
-    context "when current assignment team did not submit either files or hyperlinks" do
-      it "returns false"
-      # Write your test here!
-    end
-  end
-  #
-  # describe "#participants" do
-  #   it "returns participants of the current assignment team"
-  #   # Write your test here!
-  # end
-  #
-  # describe "#delete" do
-  #   context "when the current team is an assignment team" do
-  #     it "deletes topic sign up record, team users, team node and the team itself"
-  #     # Write your test here!
-  #   end
-  #
-  #   context "when the current team is not an assignment team" do
-  #     it "deletes team users, team node and the team itself"
-  #     # Write your test here!
-  #   end
-  # end
-  #
-  # describe "#destroy" do
-  #   it "deletes review response map records"
-  #   # Write your test here!
-  # end
-  #
-  # describe ".get_first_member" do
-  #   it "returns the first participant of current assignment team"
-  #   # Write your test here!
-  # end
-  #
-  # describe "#submitted_files" do
-  #   it "returns the submitted files of current assignment team"
-  #   # Write your test here!
-  # end
-  #
-  # describe ".import" do
-  #   context "when there is no assignment with this assignment id" do
-  #     it "raises an ImportError"
-  #     # Write your test here!
-  #   end
-  #
-  #   context "when there exists an assignment with this assignment id" do
-  #     it "imports a csv file to form assignment teams"
-  #     # Write your test here!
-  #   end
-  # end
-  #
-  # describe ".export" do
-  #   it "exports assignment teams to an array"
-  #   # Write your test here!
-  # end
-  #
-  # describe "#copy" do
-  #   it "copies the current assignment team and team members to a new course team"
-  #   # Write your test here!
-  # end
-  #
-  # describe "#add_participant" do
-  #   context "when there is no assignment participant mapping" do
-  #     it "adds this user to the assignment"
-  #     # Write your test here!
-  #   end
-  # end
-  #
-  # describe "#scores" do
-  #   it "returns a hash of scores that current assignment team has received for the questions"
-  #   # Write your test here!
-  # end
-  #
-  # describe "#hyperlinks" do
-  #   context "when current teams submitted hyperlinks" do
-  #     it "returns the hyperlinks submitted by the team" do
-  #       expect(team.hyperlinks).to eq(["https://www.expertiza.ncsu.edu"])
-  #     end
-  #   end
-  #
-  #   context "when current teams did not submit hyperlinks" do
-  #     it "returns an empty array" do
-  #       expect(team_without_submitted_hyperlinks.hyperlinks).to eq([])
-  #     end
-  #   end
-  # end
-  #
-  # describe "#files" do
-  #   it "returns all files in certain directory"
-  #   # Write your test here!
-  # end
-  #
-  # describe "#submit_hyperlink" do
-  #   context "when the hyperlink is empty" do
-  #     it "raises an exception saying 'The hyperlink cannot be empty'"
-  #     # Write your test here!
-  #   end
-  #
-  #   context "when the hyperlink is not empty" do
-  #     context "when Expertiza is unable to get the response from pinging the hyperlink" do
-  #       it "raises an exception with corresponding HTTP status code"
-  #       # Write your test here!
-  #     end
-  #
-  #     context "when Expertiza is able to get the response from the hyperlink" do
-  #       it "saves the hyperlink to submitted_hyperlinks field"
-  #       # Write your test here!
-  #     end
-  #   end
-  # end
-  #
-  # describe "#remove_hyperlink" do
-  #   it "removes the hyperlink from the submitted_hyperlinks field"
-  #   # Write your test here!
-  # end
-  #
-  # describe ".team" do
-  #   context "when the participant is nil" do
-  #     it "returns nil"
-  #     # Write your test here!
-  #   end
-  #
-  #   context "when there are not team users records" do
-  #     it "returns nil"
-  #     # Write your test here!
-  #   end
-  #
-  #   context "when the participant is not nil and there exist team users records" do
-  #     it "returns the team given the participant"
-  #     # Write your test here!
-  #   end
-  # end
-  #
-  # describe ".export_fields" do
-  #   it "exports the fields of the csv file"
-  #   # Write your test here!
-  # end
-  #
-  # describe ".remove_team_by_id" do
-  #   it "deletes a team geiven the team id"
-  #   # Write your test here!
-  # end
-  #
-  # describe "#path" do
-  #   it "returns the directory path of the assignment team"
-  #   # Write your test here!
-  # end
-  #
-  # describe "#set_student_directory_num" do
-  #   context "when there is no directory number for the assignment team" do
-  #     it "sets a directory number for the assignment team"
-  #     # Write your test here!
-  #   end
-  # end
-  #
-  # describe "#received_any_peer_review?" do
-  #   context "when there exist corresponding response maps" do
-  #     it "returns true"
-  #     # Write your test here!
-  #   end
-  #
-  #   context "when there does not exist corresponding response maps" do
-  #     it "returns false"
-  #     # Write your test here!
-  #   end
-  # end
+       context "when current assignment team did not submit either files or hyperlinks" do
+         it "returns false"
+         # Write your test here!
+       end
+     end
 end

99e0be892547ae5e00c2286ae1ce7cb3f32d569d
BarrettJB <bryson.barrett@gmail.com>
2018-10-23 14:18:57 -0400

    added some prototype functions

--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -1,6 +1,22 @@
 describe Menu do
-
+	
+	describe Node do
+		
+	end
+	
+	it "can be initialized" do
+		menu = Menu.new
+		expect(menu).not_to be_nil
+	end
+	
+	
 	describe ".select" do
+		it "can find an existing menu item by name" do
+			expect()
+		end
+		it "returns nil if no menu item matches the given name" do
+			expect(menu.select("dne")).to be_nil
+		end
 	end
 
 	describe ".get_item" do

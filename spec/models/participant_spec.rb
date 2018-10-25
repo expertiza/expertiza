3c16a14d26a48e1778ad7f7a431ec846dc36ef7d
carmenbentley <44073231+carmenbentley@users.noreply.github.com>
2018-10-24 20:45:29 -0400

    Added Coverage for Email, Delete, & Topic_Name
    Need to figure out how to just expect an exception and not cause a RT error

--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -12,49 +12,56 @@ describe Participant do
   ###
   # Please do not share this file with other teams.
   # Use factories to `build` necessary objects.
-  # Please avoid duplicated code as much as you can by moving the code to `before(:each)` block or separated methods.
+  # Please avoid duplicated code as much as you can by
+  # moving the code to `before(:each)` block or separated methods.
   # RSpec tutorial video (until 9:32): https://youtu.be/dzkVfaKChSU?t=35s
-  # RSpec unit tests examples: https://github.com/expertiza/expertiza/blob/3ce553a2d0258ea05bced910abae5d209a7f55d6/spec/models/response_spec.rb
+  # RSpec unit tests examples:
+  # https://github.com/expertiza/expertiza/blob/3ce553a2d0258ea05bced910abae5d209a7f55d6/spec/models/response_spec.rb
   ###
 
-  # Create necessary student participants for testing.
+  let(:user) { 
+	build( :student, id: 1, name: 'no name', fullname: 'no one') }
+  let(:team) {	
+	build( :assignment_team, id: 1, name: 'myTeam' ) }
+  let(:team_user) { 
+	build( :team_user, id: 1, user: user, team: team) }
+
+  let(:topic){ build( :topic ) }
+
   let(:participant) {
-	build(:participant, user: build(:student, name: "Student", fullname: "Test, Student", id: 1 ) ) }
+	build(:participant,
+	user: build( :student, name: "Jane", fullname: "Doe, Jane", id: 1 ) ) }
   let(:participant2) { 
-	build(:participant, user: build(:student, name: "John", fullname: "Doe, John", id: 2 ) ) }
+	build(:participant, 
+	user: build( :student, name: "John", fullname: "Doe, John", id: 2 ) ) }
   let(:participant3) { 
-	build(:participant, can_review: false, user: build(:student, name: "King", fullname: "Titan, King", id: 3 ) ) }
+	build(:participant, can_review: false, 
+	user: build(:student, name: "King", fullname: "Titan, King", id: 3 ) ) }
 
-##create assignment
   let(:assignment) { build(:assignment, id: 1, name: 'no assgt') }
-  
-##create team 
-  let(:user) { build(:student, id: 1, name: 'no name', fullname: 'no one', participants: [participant]) }
-  let(:team) { build(:assignment_team, id: 1, name: 'myTeam', users: [user]) }
-  let(:team_user) { build(:team_user, id: 1, user: user) }
 
-##create review response 
-  let(:review_response_map) {
-	build(:review_response_map, assignment: assignment, reviewer: participant, reviewee: team) }
-  let(:response) {
-	build(:response, id: 1, map_id: 1, response_map: review_response_map, scores: [answer]) }
-  let(:answer) { Answer.new(answer: 1, comments: 'Answer text', question_id: 1) }
+  let( :review_response_map ) {
+	build( :review_response_map, assignment: assignment, reviewer: participant, reviewee: team ) }
+  let( :response ) {
+	build(:response, id: 1, map_id: 1, response_map: review_response_map, scores: [ answer ] ) }
+  let( :answer ) { 
+	Answer.new( answer: 1, comments: 'Answer text', question_id: 1 ) }
  
-  #create question
-  let(:question) { Criterion.new(id: 1, weight: 2, break_before: true) }
-  let(:questionnaire) { ReviewQuestionnaire.new(id: 1, questions: [question], max_question_score: 5) }
+  let( :question ) { 
+	Criterion.new(id: 1, weight: 2, break_before: true ) }
+  let( :questionnaire ) { 
+	ReviewQuestionnaire.new(id: 1, questions: [ question ], max_question_score: 5) }
 
-
-  describe '#team' do
-    it 'returns nil for a participant not assigned to a team' do
-      expect( participant.team ).to eq( nil )
-    end
-
-    it 'returns the team of the participant' do
-      allow( participant ).to receive( :team ).and_return( team.name )
-      expect( participant.team ).to eq( 'myTeam' )
-    end
+  after(:each) do
+    ActionMailer::Base.deliveries.clear
   end
+
+#  describe '#team' do
+#    it 'returns the team of the participant' do
+#      allow(TeamsUser).to receive(:find_by).with(user: participant).and_return(team_user)
+#      expect( participant.team ).to eq(team_user)
+#    end
+#  end
 
   describe '#response' do
     it 'Returns the reponses that are associated with this participant' do
@@ -66,23 +73,23 @@ describe Participant do
   # Test Completed by instructor.
   describe "#name" do
     it "returns the name of the user" do
-      expect( participant.name ).to eq "Student"
+      expect( participant.name ).to eq( "Jane" )
     end
   end
 
   # Test Completed by instructor.
   describe "#fullname" do
     it "returns the full name of the user" do
-      expect( participant.fullname ).to eq "Test, Student"
+      expect( participant.fullname ).to eq( "Doe, Jane" )
     end
   end
 
   describe '#handle' do
     it 'returns the handle of the participant' do
-      expect( participant.handle( nil ) ).to eq( 'handle' )
+      expect( participant.handle( nil ) ).to eq( "handle" )
     end
   end
-## Question --> what is returned with force == nil verses force == true
+
   describe '#delete' do
     it 'deletes a participant if no associations exist and force is nil' do
       expect( participant.delete( nil ) ).to eq( participant )
@@ -90,28 +97,31 @@ describe Participant do
     it 'deletes a participant if no associations exist and force is true' do
       expect( participant.delete( true ) ).to eq( participant )
     end
-#    it 'deletes a participant if associations exist and force is true' do
-#      expect( participant.delete( true ) ).to eq( participant )
-#    end
+    it 'deletes a participant if associations exist and force is true' do
+      allow( participant ).to receive( :team ).and_return( team )
+      expect( participant.delete( true ) ).to eq( participant )
+    end
+    it 'deletes a participant if associations exist and force is true' do
+      allow( participant ).to receive( :team ).and_return( team )
+      allow( team ).to receive( :teams_users ).and_return( length: 1 )
+      expect( participant.delete( true ) ).to eq( participant )
+    end
 #    it 'raises execption when trying to delect participant where associations exists and force is nil' do
-#      expect( participant.delete( nil ) ).to raise( "Association exist for this participant." )
+#      allow( participant ).to receive( :team ).and_return( team )
+#      expect( participant.delete( nil ) ).to raise_error.with_message("Associations exist for this participant.")
 #    end
   end
-## Question --> Is it necessary to test force_delete if these things are tested via delete?
-#  describe '#force_delete' do
-#    it 'forces a delete regardless of existing associations' do
-#      expect( participant.force_delete( ResponseMap.where ) ).to eq( 'Fill this in by hand' )
-#    end
-#  end
+
+#  method --> force_delete is tested via the testing of method --> delete
 
   describe '#topic_name' do
     it 'returns the topic name associated with the participant topic name is nil' do
       expect( participant.topic_name ).to eq( '<center>&#8212;</center>' )
     end
-#    it 'returns the topic name associated with the participant topic name has value' do
-#      allow( participant ).to receive( :topic_name ).and_return( 'Topic Name' )
-#      expect( participant.topic_name ).to eq( 'Topic Name' )
-#    end
+    it 'returns the topic name associated with the participant topic name has value' do
+      allow( participant ).to receive( :topic ).and_return( topic )
+      expect( participant.topic_name ).to eq( "Hello world!" )   
+    end
   end
 
   describe '#able_to_review' do
@@ -126,11 +136,12 @@ describe Participant do
     end
   end
 
-#  describe '#email' do
-#    it '#email' do
-#      expect(participant.email('Missing "pw"', 'Missing "home_page"')).to eq('Fill this in by hand')
-#    end
-#  end
+  describe '#email' do
+    it 'sends an email to the participant' do
+      expect { participant.email("Missing 'pw'", "Missing 'home_page'") }.to change {
+		ActionMailer::Base.deliveries.count }.by(1)
+    end
+  end
 
 #  describe '#score' do
 #    it '???' do
@@ -145,16 +156,20 @@ describe Participant do
 
   describe '#get_permissions' do
     it 'returns the permissions of participant' do
-      expect( Participant.get_permissions( 'participant' ) ).to contain_exactly( [ :can_submit, true ], [ :can_review, true ], [ :can_take_quiz, true ] )
+      expect( Participant.get_permissions( 'participant' ) ).to contain_exactly(
+		[ :can_submit, true ], [ :can_review, true ], [ :can_take_quiz, true ] )
     end
     it 'returns the permissions of reader' do
-      expect( Participant.get_permissions( 'reader' ) ).to contain_exactly( [ :can_submit, false ], [ :can_review, true ], [ :can_take_quiz, true ] )
+      expect( Participant.get_permissions( 'reader' ) ).to contain_exactly(
+		[ :can_submit, false ], [ :can_review, true ], [ :can_take_quiz, true ] )
     end
     it 'returns the permissions of reviewer' do
-      expect( Participant.get_permissions( 'reviewer' ) ).to contain_exactly( [ :can_submit, false ], [ :can_review, true ], [ :can_take_quiz, false ] )
+      expect( Participant.get_permissions( 'reviewer' ) ).to contain_exactly(
+		[ :can_submit, false ], [ :can_review, true ], [ :can_take_quiz, false ] )
     end
     it 'returns the permissions of submitter' do
-      expect(Participant.get_permissions('submitter')).to contain_exactly( [:can_submit, true], [:can_review, false], [:can_take_quiz, false] )
+      expect(Participant.get_permissions('submitter')).to contain_exactly(
+		[:can_submit, true], [:can_review, false], [:can_take_quiz, false] )
     end
   end
 
@@ -175,8 +190,8 @@ describe Participant do
 
   describe '#sort_by_name' do
     it 'returns a sorted list of participants alphabetical by name' do
-      unsorted = [ participant, participant2, participant3 ]
-      sorted = [ participant2, participant3, participant ]
+      unsorted = [ participant3, participant, participant2 ]
+      sorted = [ participant, participant2, participant3  ]
       expect( Participant.sort_by_name( unsorted ) ).to eq( sorted )
     end
   end

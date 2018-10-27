52a90eaaf3a1ee8634178d9ecad62ab84fa3daa3
Kevin Acken <kevacken@gmail.com>
2018-10-27 17:54:04 -0400

    adds unit tests for assignment team

--- spec/models/assignment_team_spec.rb
+++ spec/models/assignment_team_spec.rb
@@ -1,9 +1,12 @@
 describe 'AssignmentTeam' do
   let(:team_without_submitted_hyperlinks) { build(:assignment_team, submitted_hyperlinks: "") }
-  let(:team) { build(:assignment_team, parent_id: 1) }
+  let(:team) { build(:assignment_team, id: 1, parent_id: 1) }
   let(:assignment) { build(:assignment, id: 1) }
-  let(:participant) { build(:participant) }
-  let(:user) { build(:student, id: 2) }
+  let(:participant1) { build(:participant, id: 1) }
+  let(:participant2) { build(:participant, id: 2) }
+  let(:user1) { build(:student, id: 2) }
+  let(:user2) { build(:student, id: 3) }
+  let(:review_response_map) { build(:review_response_map, reviewed_object_id: 1, reviewer_id: 1, reviewee_id: 1) }
 
   describe "#hyperlinks" do
     context "when current teams submitted hyperlinks" do
@@ -22,17 +25,16 @@ describe 'AssignmentTeam' do
   describe "#includes?" do
     context "when an assignment team has one participant" do
       it "includes one participant" do
-        allow(team).to receive(:users).with(no_args).and_return([user])
-        allow(AssignmentParticipant).to receive(:find_by).with(user_id: user.id, parent_id: team.parent_id).and_return(participant)
-        expect(team.includes?(participant)).to eq true
+        allow(team).to receive(:users).with(no_args).and_return([user1])
+        allow(AssignmentParticipant).to receive(:find_by).with(user_id: user1.id, parent_id: team.parent_id).and_return(participant1)
+        expect(team.includes?(participant1)).to eq true
       end
     end
     
-    context "when an assignment team has a user but no participants" do
+    context "when an assignment team has no users" do
       it "includes no participants" do
         allow(team).to receive(:users).with(no_args).and_return([])
-        allow(AssignmentParticipant).to receive(:find_by).with(user_id: user.id, parent_id: team.parent_id).and_return(nil)
-        expect(team.includes?(participant)).to eq false
+        expect(team.includes?(participant1)).to eq false
       end
     end
   end
@@ -86,8 +88,6 @@ describe 'AssignmentTeam' do
     end
   end
 
-
-
   describe "#review_map_type" do
     it "provides the review map type" do
       expect(team.review_map_type).to eq "ReviewResponseMap"
@@ -100,4 +100,43 @@ describe 'AssignmentTeam' do
       AssignmentTeam.prototype
     end
   end
+
+  describe "#reviewed_by?" do
+    context "when a team has a reviewer" do
+      it "has been reviewed by this reviewer" do
+         template = 'reviewee_id = ? && reviewer_id = ? && reviewed_object_id = ?'
+         allow(ReviewResponseMap).to receive(:where).with(template, team.id, participant1.id, team.assignment.id).and_return([review_response_map])
+         expect(team.reviewed_by?(participant1)).to eq true
+      end
+    end
+
+    context "when a team does not have any reviewers" do
+      it "has not been reviewed by this reviewer" do
+         template = 'reviewee_id = ? && reviewer_id = ? && reviewed_object_id = ?'
+         allow(ReviewResponseMap).to receive(:where).with(template, team.id, participant1.id, team.assignment.id).and_return([])
+         expect(team.reviewed_by?(participant1)).to eq false
+      end
+    end
+  end
+
+  describe "#participants" do
+    context "when an assignment team has two participants" do
+      it "has those two participants" do
+        allow(team).to receive(:users).with(no_args).and_return([user1, user2])
+        allow(AssignmentParticipant).to receive(:find_by).with(user_id: user1.id, parent_id: team.parent_id).and_return(participant1)
+        allow(AssignmentParticipant).to receive(:find_by).with(user_id: user2.id, parent_id: team.parent_id).and_return(participant2)
+        expect(team.participants).to eq [participant1, participant2]
+      end
+    end
+    
+    context "when an assignment team has a user but no participants" do
+      it "includes no participants" do
+        allow(team).to receive(:users).with(no_args).and_return([user1])
+        allow(AssignmentParticipant).to receive(:find_by).with(user_id: user1.id, parent_id: team.parent_id).and_return(nil)
+        expect(team.participants).to eq []
+      end
+    end
+  end
+
+  
 end

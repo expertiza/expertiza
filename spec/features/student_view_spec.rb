605935f8a8f79a1db8ca903bb2d59ca59479ccdc
Harsh Agrawal <hrsh.agrwl1@gmail.com>
2018-10-28 18:17:00 -0400

    Add Tests for the added features

--- spec/features/student_view_spec.rb
+++ spec/features/student_view_spec.rb
@@ -1,16 +1,24 @@
 require 'rspec'
 
 describe 'My behaviour' do
-
-
-  it " can display relevant menu items after login as an admin/instructor/TA", js: true do
+  it "can display relevant menu items after login as an admin/instructor/TA", js: true do
     create(:instructor)
     login_as 'instructor6'
     visit '/tree_display/list'
     expect(page).to have_current_path('/tree_display/list')
-    expect(page).to have_content('Manage content')
-    expect(page).to have_content('Survey Deployments')
+    expect(page).to have_content('Open Student View')
     expect(page).not_to have_content('Assignments')
-    expect(page).not_to have_content('Course Evaluation')
+    click_button "Manage..."
+    expect(page).to have_content('Student View')
+  end
+
+  it "can display relevant menu items when in student view", js: true do
+    create(:instructor)
+    login_as 'instructor6'
+    visit '/tree_display/list'
+    click_link 'Open Student View'
+    expect(page).to have_current_path('/student_task/list')
+    expect(page).to have_content('Assignments')
+    expect(page).to have_content('Close Student View')
   end
 end

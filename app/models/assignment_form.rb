7bde0ab70d5c52f9bef1e6fc47ab45f26ab526da
Priyankha <pri12dec@gmail.com>
2018-10-30 19:34:27 -0400

    Changed the enqueue_simicheck_task method

--- app/models/assignment_form.rb
+++ app/models/assignment_form.rb
@@ -289,8 +289,7 @@ class AssignmentForm
   end
 
   def enqueue_simicheck_task(due_date, simicheck_delay)
-    DelayedJob.enqueue(DelayedMailer.new(@assignment.id, "compare_files_with_simicheck", due_date.due_at.to_s(:db)),
-                       1, find_min_from_now(Time.parse(due_date.due_at.to_s(:db)) + simicheck_delay.to_i.hours).minutes.from_now)
+    MailWorker.perform_in((find_min_from_now(Time.parse(due_date.due_at.to_s(:db)) + simicheck_delay.to_i.hours).minutes.from_now)*60, @assignment.id, "compare_files_with_simicheck", due_date.due_at.to_s(:db))
   end
 
   # Copies the inputted assignment into new one and returns the new assignment id

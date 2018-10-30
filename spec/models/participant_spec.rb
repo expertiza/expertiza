1d5b484a70aeafea528109bf6f7c19156ad02179
zhan23 <zhan23@ncsu.edu>
2018-10-30 16:52:59 -0400

    can_review

--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -1,6 +1,6 @@
 describe Participant do
   let(:student) { build(:student, name: "Student", fullname: "Test, Student") }
-  let(:participant) { build(:participant, user: student) }
+  let(:participant) { build(:participant, user: student, can_review: false) }
 
   describe "#name" do
     it "returns the name of the user" do
@@ -13,4 +13,12 @@ describe Participant do
       expect(participant.fullname).to eq "Test, Student"
     end
   end
+
+  describe "#able_to_review" do
+    it "returns whether the current participant has permission to review others' work" do
+      #allow(participant).to receive(:can_review).with(nil).and_return(true)
+      expect(participant.able_to_review).to be false
+    end
+  end
+
 end

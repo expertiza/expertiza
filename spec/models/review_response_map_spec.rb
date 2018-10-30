bc02e1fe6a638e358961663bf2486f758df1bb29
raveenaaa <raveenadcosta19@gmail.com>
2018-10-29 21:09:20 -0400

    E1850-raveenaaa Tested questionnaire method

--- spec/models/review_response_map_spec.rb
+++ spec/models/review_response_map_spec.rb
@@ -6,7 +6,10 @@ describe 'ReviewResponseMap' do
    let(:participant) { build(:participant, id: 1, user: build(:student, name: 'no name', fullname: 'no one')) }
    let(:team) { build(:assignment_team) }
    let(:assignment) { build(:assignment, id: 1, name: 'Test Assgt') }
+   let(:question) { Criterion.new(id: 1, weight: 2, break_before: true) }
+   let(:questionnaire) { ReviewQuestionnaire.new(id: 1, questions: [question], max_question_score: 5) }
    let(:review_response_map) { build(:review_response_map, assignment: assignment, reviewer: participant, reviewee: team) }
+
 
   describe '#get_title' do
 
@@ -17,7 +20,14 @@ describe 'ReviewResponseMap' do
   end
 
   describe '#questionnaire' do
-	
+
+    it 'returns questionnaire' do
+	allow(assignment).to receive(:review_questionnaire_id).and_return(1)
+	allow(Questionnaire).to receive(:find_by).with(id: 1).and_return(questionnaire)  
+	expect(review_response_map.questionnaire.id).to eq(1)
+    end
+  end
 end
+    
 
         

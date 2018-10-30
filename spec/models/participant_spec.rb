db711f00e5ac72b60b89e87f07b59bbf5721ceb5
carmenbentley <44073231+carmenbentley@users.noreply.github.com>
2018-10-30 18:25:04 -0400

    100% coverage

--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -10,6 +10,7 @@
 ##
 describe Participant do
   ###
+  # Note from Project Mentor:  
   # Please do not share this file with other teams.
   # Use factories to `build` necessary objects.
   # Please avoid duplicated code as much as you can by
@@ -18,16 +19,15 @@ describe Participant do
   # RSpec unit tests examples:
   # https://github.com/expertiza/expertiza/blob/3ce553a2d0258ea05bced910abae5d209a7f55d6/spec/models/response_spec.rb
   ###
-
   let(:user) { 
 	build( :student, id: 1, name: 'no name', fullname: 'no one') }
+ let(:user4) { 
+	build( :student, id: 4, name: 'no name', fullname: 'no four') }
   let(:team) {	
 	build( :assignment_team, id: 1, name: 'myTeam' ) }
   let(:team_user) { 
 	build( :team_user, id: 1, user: user, team: team) }
-
   let(:topic){ build( :topic ) }
-
   let(:participant) {
 	build(:participant,
 	user: build( :student, name: "Jane", fullname: "Doe, Jane", id: 1 ) ) }
@@ -37,195 +37,191 @@ describe Participant do
   let(:participant3) { 
 	build(:participant, can_review: false, 
 	user: build(:student, name: "King", fullname: "Titan, King", id: 3 ) ) }
-
   let(:assignment) { build(:assignment, id: 1, name: 'no assgt') }
-
   let( :review_response_map ) {
 	build( :review_response_map, assignment: assignment, reviewer: participant, reviewee: team ) }
   let( :response ) {
 	build(:response, id: 1, map_id: 1, response_map: review_response_map, scores: [ answer ] ) }
   let( :answer ) { 
 	Answer.new( answer: 1, comments: 'Answer text', question_id: 1 ) }
- 
   let( :question ) { 
 	Criterion.new(id: 1, weight: 2, break_before: true ) }
   let( :questionnaire ) { 
 	ReviewQuestionnaire.new(id: 1, questions: [ question ], max_question_score: 5) }
-let(:questionnaire1){
-	build(:questionnaire, id:2,name: 'q1',type: "Assignmentquestionnaire")
-}
-let(:assignment_questionnaire){
-	build(:assignment_questionnaire , used_in_round: 0, assignment_id: assignment.id, questionnaire_id: questionnaire1.id)
-}
+ let(:participant4) { 
+	build(:participantReal, can_review: false, 
+	user: user4 ) }
+ 
   after(:each) do
     ActionMailer::Base.deliveries.clear
   end
-describe '#grade' do
-	it 'validates the type of grade (numericality)' do
-		participant.grade='a'
-		expect(participant).not_to be_valid
-	end
-	it 'allow grade to be nil' do
-		participant.grade=nil
-		expect(participant).to be_valid
-	end
-end
 
-#  describe '#team' do
-#    it 'returns the team of the participant' do
-#      allow(TeamsUser).to receive(:find_by).with(user: participant).and_return(team_user)
-#      expect( participant.team ).to eq(team_user)
-#    end
-#  end
+#  Unable to test method due to method content. Project Mentor said to leave it un-covered.
+  describe '#team' do
+    it 'returns the team of the participant' do
+      allow(TeamsUser).to receive(:find_by).and_return(team_user)
+      expect( participant4.team ).to eq(team)
+    end
+  end
 
   describe '#response' do
-    it 'Returns the reponses that are associated with this participant' do
-      allow( participant ).to receive( :responses ).and_return( response )
-      expect( participant.responses ).to eq( response )
+    it 'Returns the participant responses' do
+      allow(participant.response_maps).to receive(:map).and_return(response)
+      expect(participant.responses).to eq(response)
     end
   end
 
   # Test Completed by instructor.
   describe "#name" do
     it "returns the name of the user" do
-      expect( participant.name ).to eq( "Jane" )
+      expect(participant.name).to eq("Jane")
     end
   end
 
   # Test Completed by instructor.
   describe "#fullname" do
     it "returns the full name of the user" do
-      expect( participant.fullname ).to eq( "Doe, Jane" )
+      expect(participant.fullname).to eq("Doe, Jane")
     end
   end
 
   describe '#handle' do
     it 'returns the handle of the participant' do
-      expect( participant.handle( nil ) ).to eq( "handle" )
+      expect(participant.handle(nil)).to eq("handle")
     end
   end
 
   describe '#delete' do
     it 'deletes a participant if no associations exist and force is nil' do
-      expect( participant.delete( nil ) ).to eq( participant )
+      expect(participant.delete(nil)).to eq(participant)
     end
     it 'deletes a participant if no associations exist and force is true' do
-      expect( participant.delete( true ) ).to eq( participant )
+      expect(participant.delete(true)).to eq(participant)
     end
-    it 'deletes a participant if associations exist and force is true' do
-      allow( participant ).to receive( :team ).and_return( team )
-      expect( participant.delete( true ) ).to eq( participant )
+    it 'delete a participant with associations and force is true and multiple team_users' do
+      allow(participant).to receive(:team).and_return(team)
+      expect(participant.delete(true)).to eq(participant)
     end
-    it 'deletes a participant if associations exist and force is true' do
-      allow( participant ).to receive( :team ).and_return( team )
-      allow( team ).to receive( :teams_users ).and_return( length: 1 )
-      expect( participant.delete( true ) ).to eq( participant )
+    it 'delete participant with associations and force is true and single team_user' do
+      allow(participant).to receive(:team).and_return(team)
+      allow(team).to receive(:teams_users).and_return(length: 1)
+      expect(participant.delete(true)).to eq(participant)
     end
-#    it 'raises execption when trying to delect participant where associations exists and force is nil' do
-#      allow( participant ).to receive( :team ).and_return( team )
-#      expect( participant.delete( nil ) ).to raise_error.with_message("Associations exist for this participant.")
-#    end
+    it 'raises error, delete participant with associations and force is nil' do
+      allow(participant).to receive(:team).and_return(team)
+      expect{participant.delete(nil)}.to raise_error.with_message("Associations exist for this participant.")
+    end
   end
 
 #  method --> force_delete is tested via the testing of method --> delete
 
   describe '#topic_name' do
-    it 'returns the topic name associated with the participant topic name is nil' do
-      expect( participant.topic_name ).to eq( '<center>&#8212;</center>' )
+    it 'returns the participant topic name when nil' do
+      expect(participant.topic_name).to eq('<center>&#8212;</center>')
     end
-    it 'returns the topic name associated with the participant topic name has value' do
-      allow( participant ).to receive( :topic ).and_return( topic )
-      expect( participant.topic_name ).to eq( "Hello world!" )   
+    it 'returns the participant topic name when not nil' do
+      allow(participant).to receive(:topic).and_return(topic)
+      expect(participant.topic_name).to eq("Hello world!")   
     end
   end
 
   describe '#able_to_review' do
-    it '#able_to_review when can_review is true' do
+    it 'returns true when can_review is true' do
       expect(participant.able_to_review).to eq(true)
     end
-  end
-
-  describe '#able_to_review' do
-    it '#able_to_review when can_review is false' do
+    it '#returns false when can_review is false' do
       expect(participant3.able_to_review).to eq(false)
     end
   end
 
   describe '#email' do
     it 'sends an email to the participant' do
-      expect { participant.email("Missing 'pw'", "Missing 'home_page'") }.to change {
-		ActionMailer::Base.deliveries.count }.by(1)
+      expect {participant.email("Missing 'pw'", "Missing 'home_page'")}.to change{
+		ActionMailer::Base.deliveries.count}.by(1)
     end
   end
 
- # describe '#score' do
-  #  it 'return scores' do
-   #   question1 = double( 'Question', weight: 2 ,questionnaire_id: questionnaire1.id)
-#	questions=[question]
-#	questionnairenew=double('questionnaire')
-#	p questionnairenew.symbol
- #     scores={}
-#	scores[:participant]=participant
-#	allow(participant).to receive(:assignment).and_return(assignment)
 
-#	allow(assignment).to receive(:questionnaire).and_return(questionnaire1)
-#	p assignment.questionnaire
-	#allow(AssignmentQuestionnaire).to receive(:find_by).with(assignment_id: participant.assignment.id, questionnaire_id: questionnaire1.id).and_return(assignment_questionnaire)
-#round=assignment_questionnaire.used_in_round	
-#	questionnaire_symbol=questionnaire1.symbol
-#	scores[questionnaire_symbol]={}
-#	p scores
-#scores[questionnaire_symbol][:assesments]=allow(questionnaire1).to receive(:get_assessments_for).with(participant).and_return(a_value)
-#	p scores      
-#scores[questionnaire_symbol][:scores]=allow(Answer).to receive(:compute_scores).with(scores[questionnaire_symbol][:assesments],questions[questionnaire_symbol]).and_return(a_value)
-#	p scores
-
-#	scores[:total_score]=0
-
-	#p scores
-#	expect( participant.scores( [question1] ) ).to eq( scores )
- #   end 
- #end
 
   describe '#get_permissions' do
     it 'returns the permissions of participant' do
-      expect( Participant.get_permissions( 'participant' ) ).to contain_exactly(
-		[ :can_submit, true ], [ :can_review, true ], [ :can_take_quiz, true ] )
+      expect(Participant.get_permissions('participant')).to contain_exactly(
+		[:can_submit, true], [:can_review, true], [:can_take_quiz, true])
     end
     it 'returns the permissions of reader' do
-      expect( Participant.get_permissions( 'reader' ) ).to contain_exactly(
-		[ :can_submit, false ], [ :can_review, true ], [ :can_take_quiz, true ] )
+      expect(Participant.get_permissions('reader')).to contain_exactly(
+		[:can_submit, false], [:can_review, true], [:can_take_quiz, true])
     end
     it 'returns the permissions of reviewer' do
-      expect( Participant.get_permissions( 'reviewer' ) ).to contain_exactly(
-		[ :can_submit, false ], [ :can_review, true ], [ :can_take_quiz, false ] )
+      expect( Participant.get_permissions('reviewer')).to contain_exactly(
+		[:can_submit, false], [:can_review, true], [:can_take_quiz, false])
     end
     it 'returns the permissions of submitter' do
       expect(Participant.get_permissions('submitter')).to contain_exactly(
-		[:can_submit, true], [:can_review, false], [:can_take_quiz, false] )
+		[:can_submit, true], [:can_review, false], [:can_take_quiz, false])
     end
   end
 
   describe '#get_authorization' do
     it 'returns participant when no arguments are pasted' do
-      expect( Participant.get_authorization( nil, nil, nil ) ).to eq( 'participant' )
+      expect(Participant.get_authorization(nil, nil, nil)).to eq('participant')
     end
     it 'returns reader when no arguments are pasted' do
-      expect( Participant.get_authorization( false, true, true ) ).to eq( 'reader' )
+      expect(Participant.get_authorization(false, true, true)).to eq('reader')
     end
     it 'returns submitter when no arguments are pasted' do
-      expect( Participant.get_authorization( true, false, false ) ).to eq( 'submitter' )
+      expect(Participant.get_authorization(true, false, false)).to eq('submitter')
     end
     it 'returns reviewer when no arguments are pasted' do
-      expect( Participant.get_authorization( false, true, false ) ).to eq( 'reviewer' )
+      expect(Participant.get_authorization(false, true, false)).to eq('reviewer')
     end
   end
 
   describe '#sort_by_name' do
     it 'returns a sorted list of participants alphabetical by name' do
-      unsorted = [ participant3, participant, participant2 ]
-      sorted = [ participant, participant2, participant3  ]
-      expect( Participant.sort_by_name( unsorted ) ).to eq( sorted )
+      unsorted = [participant3, participant, participant2]
+      sorted = [participant, participant2, participant3 ]
+      expect(Participant.sort_by_name(unsorted)).to eq(sorted)
     end
   end
+
+
+  describe '#score' do
+    it '???' do
+      questions = {:question=>:question1}
+     test = [questionnaire]
+     assessments = ["test"]
+     scores = {}
+
+      allow(participant.assignment).to receive(:questionnaires).and_return(test)
+      allow(AssignmentQuestionnaire).to receive(:find_by).with(assignment_id: 1, questionnaire_id: 1).and_return(questionnaire)
+      allow(questionnaire).to receive(:get_assessments_for).with(participant).and_return(a_value)
+      allow(questionnaire).to receive(:used_in_round).and_return(1)
+      allow(Answer).to receive(:compute_scores).with(any_args).and_return(100)
+      allow(participant.assignment).to receive(:compute_total_score).with(any_args).and_return(5)
+
+     check = participant.scores(questions)
+     expect(check).to have_key(:participant)
+     #expect(check).to have_key(:questionnaire_symbol)
+     expect(check).to have_key(:total_score)
+   end
+    it 'svvfs' do
+      questions = {:question=>:question1}
+     test = [questionnaire]
+     assessments = ["test"]
+     scores = {}
+
+      allow(participant.assignment).to receive(:questionnaires).and_return(test)
+      allow(AssignmentQuestionnaire).to receive(:find_by).with(assignment_id: 1, questionnaire_id: 1).and_return(questionnaire)
+      allow(questionnaire).to receive(:get_assessments_for).with(participant).and_return(a_value)
+      allow(questionnaire).to receive(:used_in_round).and_return(nil)
+      allow(Answer).to receive(:compute_scores).with(any_args).and_return(100)
+      allow(participant.assignment).to receive(:compute_total_score).with(any_args).and_return(5)
+
+     check = participant.scores(questions)
+     expect(check).to include(:participant => participant)
+     expect(check).to include(:review)
+     expect(check).to include(:total_score => 5)
+   end
+ end
 end

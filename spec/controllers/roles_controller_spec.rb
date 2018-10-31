c188761a1c420b4310deeb7c764f1d58a681bfda
Adharsh Srivats R <adharshsrivats@gmail.com>
2018-10-30 21:26:57 -0400

    Added test to check 'prevention of new role addition'

---
+++ spec/controllers/roles_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+require 'spec_helper'
+RSpec.describe RolesController, :type => :controller do
+  describe 'Create a role' do
+    include Capybara::DSL
+    it 'should not create a new role' do
+      visit roles_path
+      expect(page).not_to have_button ("New Role")
+    end
+    it 'should not create a new role from list' do
+      visit roles_list_path
+      expect(page).not_to have_button ("New Role")
+    end
+  end
+end

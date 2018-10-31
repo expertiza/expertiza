8bc34c05b0d64a0e197b18e7df4b049e646a3df1
ajcpendyala <ajcpendyala@gmail.com>
2018-10-31 13:13:21 -0400

    added more tests to course controller

--- spec/controllers/course_controller_spec.rb
+++ spec/controllers/course_controller_spec.rb
@@ -1,6 +1,7 @@
 describe CourseController do
   let(:instructor) { build(:instructor, id: 6) }
-  let(:student) { build(:student)}
+  let(:student) { build(:student) }
+  let(:course) { double('Course', instructor_id: 6, path: '/cscs', name: 'abc') }
   describe '#action_allowed?' do
     context 'when current user is student' do
       it 'disallows all actions' do
@@ -19,18 +20,22 @@ describe CourseController do
 
   describe '#create' do
     it 'redirects to tree_display#list page after creating a new course' do
-      params = {
-                course: {
-                        name: 'CSC999',
-                        private: false,
-                        institutions_id: 1,
-                        directory_path: '/cscs',
-                        info: 'hello',
-                        instructor_id: instructor.id
-                }
-      }
+      allow(Course).to receive(:create).with(name: 'CSC999', private: false, institutions_id: 1, directory_path: '/cscs', info: 'hello', instructor_id: 6).and_return(double('Course', instructor_id: 6))
+      allow(CourseNode).to receive(:create).with('1').and_return(double('CourseNode', node_object_id: 1, parent_id: 1))
       session = {user: instructor}
+      params = {
+        course: {
+          name: 'CSC999',
+          private: false,
+          institutions_id: 1,
+          directory_path: '/cscs',
+          info: 'hello',
+          instructor_id: session[:user].id
+        }
+      }
+
       post :create, params, session
+      expect(flash[:error]).to eq 'error'
       expect(response).to redirect_to('/tree_display/list')
     end
   end
@@ -44,6 +49,26 @@ describe CourseController do
         get :edit, params, session
         expect(response).to render_template(:edit)
       end
+    end
+  end
+
+  describe '#delete' do
+    it 'deletes the course and redirects to tree_display#list page' do
+      allow(Course).to receive(:find).with('1').and_return(course)
+      allow(course).to receive(:destroy).and_return(true)
+      params = {id: 1}
+      session = {user: instructor}
+      post :delete, params, session
+      expect(response).to redirect_to('/tree_display/list')
+    end
+  end
+
+  describe '#new' do
+    it 'sets the private instance variable' do
+      params = {private:  1}
+      session = {user: instructor}
+      get :new, params, session
+      expect(controller.instance_variable_get(:@private)).to eq('1')
     end
   end
 end

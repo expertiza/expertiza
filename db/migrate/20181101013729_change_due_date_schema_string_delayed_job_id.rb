921d446b428f92626636e6d23ce748307a296c00
Pratik Singh <psingh22@ncsu.edu>
2018-10-31 21:43:49 -0400

    Enabled Sidekiq job deletion

--- db/migrate/20181101013729_change_due_date_schema_string_delayed_job_id.rb
+++ db/migrate/20181101013729_change_due_date_schema_string_delayed_job_id.rb
@@ -1,6 +1,6 @@
 class ChangeDueDateSchemaStringDelayedJobId < ActiveRecord::Migration
   def change
-    change_table :due_date do |t|
+    change_table :due_dates do |t|
       t.change :delayed_job_id, :string
     end
   end

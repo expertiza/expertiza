1c1f6920219224f974d11418eaf2c265628f2dd8
Piyush Mishra <piyushmishra0894@gmail.com>
2018-10-31 22:21:20 -0400

    E1850-pmishra: Removing PR check errors

--- spec/models/review_response_map_spec.rb
+++ spec/models/review_response_map_spec.rb
@@ -1,12 +1,10 @@
 require 'rspec'
 require 'spec_helper'
-
-describe 'ReviewResponseMap' do 
-   let(:participant) { build(:participant, id: 1, user: build(:student, name: 'no name', fullname: 'no one')) }
-   let(:team) { build(:assignment_team) }
-   let(:assignment) { build(:assignment, id: 1, name: 'Test Assgt') }
-   let(:review_response_map) { build(:review_response_map, assignment: assignment, reviewer: participant, reviewee: team) }
-
+describe 'ReviewResponseMap' do
+  let(:participant) { build(:participant, id: 1, user: build(:student, name: 'no name', fullname: 'no one')) }
+  let(:team) { build(:assignment_team) }
+  let(:assignment) { build(:assignment, id: 1, name: 'Test Assgt') }
+  let(:review_response_map) { build(:review_response_map, assignment: assignment, reviewer: participant, reviewee: team) }
   describe '#get_title' do
     it 'returns the title' do
       expect(review_response_map.get_title).to eql("Review")

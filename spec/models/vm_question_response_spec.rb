68edb20781eb2065c051a10f039c7e0ce9f51785
mfleader42 <mfleader42@gmail.com>
2018-11-02 11:29:42 -0400

    added 2nd test

--- spec/models/vm_question_response_spec.rb
+++ spec/models/vm_question_response_spec.rb
@@ -1,11 +1,33 @@
 describe VmQuestionResponse do
-  let(:assignment) { build(:assignment, id: 1, questionnaires: [review_questionnaire], is_penalty_calculated: true) }
-  let(:assignment_questionnaire) { build(:assignment_questionnaire, used_in_round: 1, assignment: assignment) }
-  let(:review_questionnaire) { build(:questionnaire, id: 1, questions: [question]) }
-  questionnaire = Questionnaire.new
-  let(:response){ VmQuestionResponse.new(questionnaire, assignment)}
 
-  it 'adds a question that is not a QuestionnaireHeader' do
-    response.add_questions 'q1'
-  end  
+  context 'when invalid questionnaire' do
+    let(:rq) { create(:questionnaire) }
+    let(:aq) { create(:assignment_questionnaire) }
+    let(:asmt) { create(:assignment) }
+    let(:rsp) { VmQuestionResponse.new(rq, asmt) }
+      it 'has no round value given a generic questionnaire' do
+        # expect(rsp.round).to be_nil
+        # expect response from assignment?
+        # expect(asmt).to exist?
+      end
+  end
+
+  context 'when initialized with a valid assignment questionnaire' do
+    let(:rq) { create(:questionnaire) }
+    let(:aq) { create(:assignment_questionnaire) }
+    let(:asmt) { create(:assignment) }
+    let(:rsp) { VmQuestionResponse.new(rq, asmt, 7) }
+    it 'has a round value of the questionnaire given' do
+      expect(rsp.round).to eq(7)
+    end
+
+    it 'adds a question that is not a QuestionnaireHeader' do
+      # response.add_questions 'q1'
+    end  
+
+    it 'displays the members of the team' do
+      rsp.display_team_members
+    end
+  end
+
 end

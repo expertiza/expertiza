51082cb868d937e392c5c3b41cddc6d329c2f202
mfleader42 <mfleader42@gmail.com>
2018-11-02 12:38:21 -0400

    up to 40%

--- spec/models/vm_question_response_spec.rb
+++ spec/models/vm_question_response_spec.rb
@@ -17,17 +17,33 @@ describe VmQuestionResponse do
     let(:aq) { create(:assignment_questionnaire) }
     let(:asmt) { create(:assignment) }
     let(:rsp) { VmQuestionResponse.new(rq, asmt, 7) }
+    let(:q0) { create(:question) }
+    
+
     it 'has a round value of the questionnaire given' do
       expect(rsp.round).to eq(7)
     end
 
-    it 'adds a question that is not a QuestionnaireHeader' do
-      # response.add_questions 'q1'
-    end  
-
-    it 'displays the members of the team' do
-      rsp.display_team_members
+    context 'when given a list of valid questions' do
+      it 'adds valid questions' do
+        qs = Array.new(1) { q0 }
+        expect(rsp.list_of_rows.size).to eq(0)
+        rsp.add_questions qs
+        expect(rsp.list_of_rows.size).to eq(1)
+      end
     end
+    
+    context 'when given a participant, team, and vary' do
+      let(:teem) { create(:team) }
+      let(:ppnt) { create(:participant) }
+
+      it 'displays the members of the team' do
+        rsp.display_team_members
+      end
+
+
+    end
+
   end
 
 end

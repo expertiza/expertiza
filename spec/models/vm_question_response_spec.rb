154fa4dbb2af23cb41dced5d82285c16e375d136
mfleader42 <mfleader42@gmail.com>
2018-11-02 23:48:14 -0400

    stuck on vm.add_reviews

--- spec/models/vm_question_response_spec.rb
+++ spec/models/vm_question_response_spec.rb
@@ -3,14 +3,33 @@ describe VmQuestionResponse do
     let(:rq) { create(:questionnaire) }
     let(:aq) { create(:assignment_questionnaire) }
     let(:asmt) { create(:assignment) }
-    let(:rsp) { VmQuestionResponse.new(rq, asmt, 7) }
+    let(:rsp) { VmQuestionResponse.new(rq, asmt, 1) }
     let(:q0) { create(:question) }
     let(:header_q) { QuestionnaireHeader(q0) }
+    let(:rvw_rsp_map) { create(:review_response_map) }
+
+
     
-    
+    context 'when given a team' do
+      let(:team) { create(:assignment_team) }
+      let(:ppnt) { create(:participant) }
+
+      it 'displays the members of the team' do
+        # ppnt.user.fullname = 'Voldemort'
+        # team.add_participant(1, ppnt)
+        out = 'Team members:'
+        rsp.add_team_members(team)
+        rsp.listofteamparticipants.each do |participant|
+          out = out + " (" + participant.fullname + ") "
+        end
+        expect(rsp.display_team_members).to eq out
+      end
+
+      
+    end
 
     it 'has the round value of the given questionnaire' do
-      expect(rsp.round).to eq(7)
+      expect(rsp.round).to eq 1
     end
 
     context 'when given a list of valid questions' do
@@ -19,32 +38,11 @@ describe VmQuestionResponse do
       it 'can calculate the max score for the questionnaire' do
         rsp.add_questions qs
         expect(rsp.max_score).to eq 5
-        expect(rsp.list_of_rows.size).to eq(1)
+        expect(rsp.list_of_rows.size).to eq 1
         expect(rsp.max_score_for_questionnaire()).to eq qs.size * rq.max_question_score
       end
     end
-    
-    context 'when given a participant, team, and vary' do
-      let(:team) { create(:assignment_team) }
-      let(:ppnt) { create(:participant) }
 
-      it 'displays the members of the team' do
-        out = 'Team members:'
-        rsp.add_team_members(team)
-        rsp.listofteamparticipants.each do |participant|
-          out = out + " (" + participant.fullname + ") "
-        end
-        expect(rsp.display_team_members).to eq out
-      end
-    end
-
-    context 'and when given answers' do
-      let(:ans) { create(:answer) }
-      let(:anss) { [ans, ans] }
-      it 'adds each row score (answer) to its corresponding row' do
-
-      end    
-    end
   end
 
 end

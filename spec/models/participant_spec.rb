3ba67547559f24dcf1c0ee9b804bf654a39d837a
reetu08 <shakyareetu8@gmail.com>
2018-11-04 21:08:09 -0500

    Added test for #email, coverage 79.76%

--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -1,18 +1,25 @@
 describe Participant do
   let(:student) { build(:student, name: "Student", fullname: "Test, Student") }
+  let(:student2) { build(:student, name: "Student2") }
   let(:participant) { build(:participant, user: student) }
-  let(:response_map1) { build(:review_response_map, reviewer: participant2, id: 10) }
-  let(:response_map2) { build(:review_response_map, reviewer: participant2, id: 20) }
-  let(:response1) { build(:response, id: 1, map_id: 10, response_map: response_map1) }
-  let(:response2) { build(:response, id: 2, map_id: 20, response_map: response_map2) }
-  let(:participant3) { build(:participant) }
-  #let (:team_user1) {TeamsUser.new user_id, team_id}
-  #let (:team_user1) { build(:team_user, student: participant)}
-
+  let(:assignment) { build(:assignment) }
+  let(:participant2) { build(:participant, assignment: assignment) }
+  let(:assignment_team) { build(:assignment_team) }
+  let(:team_user) { build(:team_user, user: student, team: assignment_team)}
+  let(:response) { build(:response, response_map: response_map)}
+  let(:response_map) { build(:review_response_map, assignment: assignment, reviewer: participant2, reviewee: assignment_team)}
+  let(:question) { build(:question) }
+  let(:review_questionnaire) { build(:questionnaire, id: 2) }
+  #let(:response_map1) { build(:review_response_map, reviewer: participant2, id: 10) }
+  #et(:response_map2) { build(:review_response_map, reviewer: participant2, id: 20) }
+  #let(:response1) { build(:response, id: 1, map_id: 10, response_map: response_map1) }
+  #let(:response2) { build(:response, id: 2, map_id: 20, response_map: response_map2) }
+  #let(:team_user1) {TeamsUser.new user_id, team_id}
   
   describe "#team" do
    it "returns the team id for the user" do
-     expect(participant.team).to eq(nil)
+    allow(TeamsUser).to receive(:find_by).with(user: student).and_return(team_user)
+    expect(participant.team).to eq(nil)
    end   
   end
   
@@ -37,25 +44,15 @@ describe Participant do
   
    describe '#scores' do
     it 'returns scores obtained by the participant for given questions' do
-      assignment = build(:assignment, id: 1) 
-      p = build(:participant, assignment: assignment)
-      review_questionnaire = build(:questionnaire, id: 1)
-      question = double('Question')
-      response = build(:response)
-      reponse_map = build(:review_response_map)
-      
       allow(assignment).to receive(:questionnaires).and_return([review_questionnaire])
-      allow(AssignmentQuestionnaire).to receive(:find_by).with(assignment_id: 1, questionnaire_id: 1)
-                                                         .and_return(double('AssignmentQuestionnaire', used_in_round: nil))
-       allow(review_questionnaire).to receive(:symbol).and_return(:review)
-       allow(review_questionnaire).to receive(:get_assessments_for).with(p).and_return([response])
-       allow(Answer).to receive(:compute_scores).with([response], [question]).and_return(max: 95, min: 88, avg: 90)
+      allow(AssignmentQuestionnaire).to receive_message_chain(:find_by, :used_in_round).with(assignment_id: 1, questionnaire_id: 2)\
+          .with(no_args).and_return(2)
+       allow(review_questionnaire).to receive(:get_assessments_for).with(participant2).and_return([response])
+       allow(Answer).to receive(:compute_scores).and_return(max: 95, min: 88, avg: 90)
        allow(assignment).to receive(:compute_total_score).with(any_args).and_return(100)
-       
-       
-       expect(p.assignment.compute_total_score(:test)).to eql(100)
-       #expect(p.scores(:review [question])).to eql(100)
-       
+       expect(participant2.assignment.compute_total_score(:test)).to eql(100)
+       puts participant2.scores(question)
+       expect(participant2.scores(question).inspect).to eq("{:participant=>#<AssignmentParticipant id: nil, can_submit: true, can_review: true, user_id: 2, parent_id: nil, submitted_at: nil, permission_granted: nil, penalty_accumulated: 0, grade: nil, type: \"AssignmentParticipant\", handle: \"handle\", time_stamp: nil, digital_signature: nil, duty: nil, can_take_quiz: true, Hamer: 1.0, Lauw: 0.0>, :review2=>{:assessments=>nil, :scores=>{:max=>95, :min=>88, :avg=>90}}, :total_score=>100}")
      end
    end
    
@@ -69,17 +66,17 @@ describe Participant do
      context 'when the participant has an assignment with an unnamed topic' do
        it 'returns error message' do
          topic = build(:topic, topic_name: '')
-         allow(participant3).to receive(:topic).and_return(topic)
-         expect(participant3.topic_name).to eql('<center>&#8212;</center>')
+         allow(participant2).to receive(:topic).and_return(topic)
+         expect(participant2.topic_name).to eql('<center>&#8212;</center>')
        end
      end
      
      context 'when the participant has an assignment with a named topic' do
        it 'returns the name of the topic associated to the assignment of the participant' do
          topic = build(:topic, topic_name: 'Test topic name')
-         allow(participant3).to receive(:topic).and_return(topic)
+         allow(participant2).to receive(:topic).and_return(topic)
          expect(topic.topic_name).to eql('Test topic name')
-         expect(participant3.topic_name).to eql('Test topic name')
+         expect(participant2.topic_name).to eql('Test topic name')
        end
      end   
    end
@@ -89,6 +86,16 @@ describe Participant do
        expect(participant.able_to_review).to eql(true)
      end
    end
+   
+   describe '#email' do
+    it 'sends email to the current user' do
+      allow(User).to receive(:find_by).with(id: nil).and_return(student)
+      allow(participant).to receive(:assignment_id).and_return(nil)
+      allow(Assignment).to receive(:find_by).with(id: nil).and_return(assignment)
+      expect(participant.email('password', 'home_page').subject).to eq("You have been registered as a participant in the Assignment final2")
+      expect(participant.email('password', 'home_page').to[0]).to eq ("expertiza.development@gmail.com")
+    end
+  end
    
    describe '.get_permissions' do
      context 'when current participant is authorized as reader' do
@@ -110,16 +117,24 @@ describe Participant do
      end  
    end
   
-  describe ".get_authorization"  do
-        it "returns authorization as reader when input is true for can_review and can_take quiz" do
-                expect(Participant.get_authorization(false, true, true )).to eq('reader')
-        end
-        it  "returns authorization submitter when input is true only for can_submit" do
-                expect(Participant.get_authorization(true, false, false)).to eq('submitter')
-        end
-        it  "returns authorization as reviewer when the input is true for only for can_review" do
-                expect(Participant.get_authorization(false, true, false)).to eq('reviewer')
-        end
+  describe '.get_authorization'  do
+    context 'when input is true for can_review and can_take quiz' do
+      it 'returns authorization as reader' do
+        expect(Participant.get_authorization(false, true, true )).to eq('reader')
+      end
+    end
+    
+    context 'when input is true only for can_submit' do
+      it 'returns authorization submitter' do
+        expect(Participant.get_authorization(true, false, false)).to eq('submitter')
+      end
+    end
+    
+    context 'when the input is true for only for can_review' do
+      it 'returns authorization as reviewer' do
+        expect(Participant.get_authorization(false, true, false)).to eq('reviewer')
+      end
+    end
    end
    
    describe '.sort_by_name' do

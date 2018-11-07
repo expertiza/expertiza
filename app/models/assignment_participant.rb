941ac9509fcb95c3373546a2a39a30d134afe09f
carmenbentley <44073231+carmenbentley@users.noreply.github.com>
2018-11-07 12:12:55 -0500

    code climate space requirement

--- app/models/assignment_participant.rb
+++ app/models/assignment_participant.rb
@@ -57,7 +57,7 @@ class AssignmentParticipant < Participant
   #   scores[:total_score] = self.assignment.compute_total_score(scores)
   #   merge scores[review#] (for each round) to score[review]  -Yang
   #   merge_scores(scores) if self.assignment.varying_rubrics_by_round?
-  # In the event that this is a microtask, we need to scale the score accordingly and record the total possible points
+    # In the event that this is a microtask, we need to scale the score accordingly and record the total possible points
     # PS: I don't like the fact that we are doing this here but it is difficult to make it work anywhere else
     # topic_total_scores(scores) if self.assignment.microtask?
 
@@ -70,9 +70,9 @@ class AssignmentParticipant < Participant
     # scores[:quiz] = Hash.new
     # scores[:quiz][:assessments] = quiz_responses
     # scores[:quiz][:scores] = Answer.compute_quiz_scores(scores[:quiz][:assessments])
-    # scores[:total_score] = assignment.compute_total_score(scores)
+   # scores[:total_score] = assignment.compute_total_score(scores)
    # scores[:total_score] += compute_quiz_scores(scores)
- # move lots of calculation from view(_participant.html.erb) to model
+  # move lots of calculation from view(_participant.html.erb) to model
   # calculate_scores(scores)
   # end
 

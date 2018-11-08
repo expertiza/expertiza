05433815462544c3d3f5578b2881907c69c6b520
Varun Shah <varun.shah196@gmail.com>
2018-11-07 20:54:17 -0500

    Updated Rspec test to remove depreciated syntax

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe ApplicationController, type: :controller do
     it 'should not remove utf8 characters' do
       controller.params[:key]='abc漢字123!@$'
       controller.filter_utf8
-      controller.params[:key].should eql 'abc漢字123!@$'
+      expect(controller.params[:key]).to eql 'abc漢字123!@$'
     end
     it 'should remove non utf8 characters' do
       controller.params[:key]="a\xC2a"
       controller.filter_utf8
-      controller.params[:key].should eql 'aa'
+      expect(controller.params[:key]).to eql 'aa'
     end
   end
 end

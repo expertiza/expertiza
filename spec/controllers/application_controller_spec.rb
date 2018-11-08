6f56092ccbd0158d7e018d716a6dce1291dd3c93
Varun Shah <varun.shah196@gmail.com>
2018-11-07 21:09:24 -0500

    Resolved code_climate errors

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 RSpec.describe ApplicationController, type: :controller do
   describe 'remove_non_utf8' do
     it 'should not remove utf8 characters' do
-      controller.params[:key]='abc漢字123!@$'
+      controller.params[:key] = 'abc漢字123!@$'
       controller.filter_utf8
       expect(controller.params[:key]).to eql 'abc漢字123!@$'
     end
     it 'should remove non utf8 characters' do
-      controller.params[:key]="a\xC2a"
+      controller.params[:key] = "a\xC2a"
       controller.filter_utf8
       expect(controller.params[:key]).to eql 'aa'
     end

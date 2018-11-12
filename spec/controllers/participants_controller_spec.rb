0c15d65ea207b4c405e2243e1abcf0d92d9bfffd
ajcpendyala <ajcpendyala@gmail.com>
2018-11-12 12:51:37 -0500

    added more tests

--- spec/controllers/participants_controller_spec.rb
+++ spec/controllers/participants_controller_spec.rb
@@ -3,6 +3,8 @@ describe ParticipantsController do
   let(:student) { build(:student) }
   let(:course_participant) { build(:course_participant) }
   let(:participant) { build(:participant) }
+  let(:assignment_node) { build(:assignment_node) }
+  let(:assignment) { build(:assignment) }
   describe '#action_allowed?' do
     context 'when current user is student' do
       it 'allows update_duties action' do
@@ -49,7 +51,7 @@ describe ParticipantsController do
       allow(participant).to receive(:destroy).and_return(true)
       params = {id: 1}
       session = {user: instructor}
-      post :delete_assignment_participant, params, session
+      get :delete_assignment_participant, params, session
       expect(response).to redirect_to('/review_mapping/list_mappings?id=1')
     end
   end
@@ -59,8 +61,70 @@ describe ParticipantsController do
       allow(Participant).to receive(:find).with('1').and_return(participant)
       params = {student_id: 1}
       session = {user: instructor}
-      post :update_duties, params, session
+      get :update_duties, params, session
       expect(response).to redirect_to('/student_teams/view?student_id=1')
+    end
+  end
+
+  describe '#update_authorizations' do
+    it 'updates the authorizations for the participant' do
+      allow(Participant).to receive(:find).with('1').and_return(participant)
+      params = {authorization: 'participant', id: 1}
+      session = {user: instructor}
+      get :update_authorizations, params, session
+      expect(response).to redirect_to('/participants/list?id=1&model=Assignment')
+    end
+  end
+
+  describe '#list' do
+    it 'lists the participants' do
+      allow(AssignmentNode).to receive(:find_by).with(node_object_id: '1').and_return(assignment_node)
+      allow(Assignment).to receive(:find).with('1').and_return(assignment)
+      params = {model: 'Assignment', authorization: 'participant', id: 1}
+      session = {user: instructor}
+      get :list, params, session
+      expect(controller.instance_variable_get(:@participants)).to be_empty
+    end
+  end
+
+  describe '#add' do
+    it 'adds a participant' do
+      allow(Assignment).to receive(:find).with('1').and_return(assignment)
+      allow(User).to receive(:find_by).with(name: student.name).and_return(student)
+      params = {model: 'Assignment', authorization: 'participant', id: 1, user: {name: student.name}}
+      session = {user: instructor}
+      xhr :get, :add, params, session
+      expect(response).to render_template('add.js.erb')
+    end
+    it 'does not add a participant for a non-existing user' do
+      allow(Assignment).to receive(:find).with('1').and_return(assignment)
+      params = {model: 'Assignment', authorization: 'participant', id: 1, user: {name: 'Aaa'}}
+      session = {user: instructor}
+      xhr :get, :add, params, session
+      expect(flash[:error]).to eq 'The user <b>Aaa</b> does not exist or has already been added.'
+      expect(response).to render_template('add.js.erb')
+    end
+  end
+
+  describe '#inherit' do
+    it 'inherits the participant list' do
+      allow(Assignment).to receive(:find).with('1').and_return(assignment)
+      params = {id: 1}
+      session = {user: instructor}
+      get :inherit, params, session
+      expect(flash[:note]).to eq 'No participants were found to inherit this assignment.'
+      expect(response).to redirect_to('/participants/list?model=Assignment')
+    end
+  end
+
+  describe '#bequeath_all' do
+    it 'bequeaths the participant list' do
+      allow(Assignment).to receive(:find).with('1').and_return(assignment)
+      params = {id: 1}
+      session = {user: instructor}
+      get :bequeath_all, params, session
+      expect(flash[:note]).to eq 'All assignment participants are already part of the course'
+      expect(response).to redirect_to('/participants/list?model=Assignment')
     end
   end
 end

bdb7e9c91f7b00b9e306aa24bdf534e46a1c1040
root <root@vm19-49.vcl.ncsu.edu>
2018-11-11 22:43:23 -0500

    Completed .export method

--- spec/models/review_response_map_spec.rb
+++ spec/models/review_response_map_spec.rb
@@ -7,17 +7,14 @@ describe 'ReviewResponseMap' do
   let(:student){ build(:participant, id: 3, parent_id: 1, user: build(:student, name: 'no name', fullname: 'first user')) }
   let(:student2){ build(:participant, id: 4, parent_id: 1, user: build(:student, name: 'no name', fullname: 'second one')) }
   let(:team) { build(:assignment_team) }
-  let(:assignment) { build(:assignment, id: 1, name: 'Test Assgt') }
+  let(:assignment) { build(:assignment, id: 1, name: 'Test Assgt', rounds_of_reviews: 1 )}
   let(:assignment2) { build(:assignment, id: 2, name: 'Test Assgt') }
   let(:response_map) { build(:review_response_map, reviewer: student, response: [response], reviewee_id: 1, type: "ReviewResponseMap") }
   let(:response_map2) { build(:review_response_map, reviewer: student2, response:[response2, response3],reviewee_id: 1, type: "ReviewResponseMap") }
-  # let(:response_map){build(:review_response_map)}
-  # let(:response_map2){build(:review_response_map)}
   let(:question) { Criterion.new(id: 1, weight: 2, break_before: true) }
   let(:questionnaire) { ReviewQuestionnaire.new(id: 1, questions: [question], max_question_score: 5) }
   let(:answer) { Answer.new(answer: 1, comments: 'Answer text', question_id: 1) }
   let(:response) { build(:response, id: 1, round: 1, is_submitted: true,map_id: 1, scores: [answer]) }
-  # let(:response){build(:response)}
   let(:null_response){double(:response)}
   let(:response2) { build(:response, id: 2, round: 1, is_submitted: true, map_id: 1, scores: [answer]) }
   let(:response3) { build(:response, id: 3, round: 1, is_submitted: false, map_id: 1, scores: [answer]) }
@@ -25,7 +22,6 @@ describe 'ReviewResponseMap' do
   let(:review_response_map2) { build(:review_response_map, assignment: assignment2, reviewer: participant2, reviewee: team) }
   let(:meta_review_response_map) { build(:meta_review_response_map, review_mapping: review_response_map, reviewee: participant)}
   let(:feedback_response_map){ build(:review_response_map, response:[response2, response3], type:'FeedbackResponseMap')}
-  # let(:feedback_response_map){build(:review_response_map)}
   
   describe '#get_title' do
     it 'returns the title' do
@@ -78,10 +74,16 @@ describe 'ReviewResponseMap' do
     end
   end
 
+  describe '.export' do
+    it 'adds reviewer and reviewee names to array' do
+      allow(ReviewResponseMap).to receive(:where).with(reviewed_object_id: 1).and_return([review_response_map])
+      expect(ReviewResponseMap.export([], 1, 'test')).to eql([review_response_map])
+    end
+  end
+  
   describe '#show_feedback' do
     context 'when no response is present or response is nil' do
-      it 'returns nil' do
-        # db1 = instance_double("Response").as_null_object  
+      it 'returns nil' do  
         allow(review_response_map).to receive(:response).and_return([])     
         expect(review_response_map.show_feedback null_response).to be(nil)        
       end
@@ -104,6 +106,17 @@ describe 'ReviewResponseMap' do
         allow(Assignment).to receive(:find).with(1).and_return(assignment)
         allow(ReviewResponseMap).to receive(:prepare_final_review_versions).with(assignment, [review_response_map, review_response_map2]).and_return("prepare_final_review_versions")
         expect(ReviewResponseMap.final_versions_from_reviewer 1).to eql("prepare_final_review_versions")
+    end
+  end
+
+  describe '.prepare_final_review_versions' do
+    context 'when rounds_of_reviews <=1' do
+      xit 'calls prepare_review_response with round=nil' do
+        maps = [feedback_response_map]
+        allow(ReviewResponseMap).to receive(:prepare_review_response).with(any_args).and_return({something: "testing"})
+        expect(ReviewResponseMap).to receive(:prepare_review_response).with(any_args).once
+        expect(ReviewResponseMap.prepare_final_review_versions assignment, maps).to eql("test")
+      end
     end
   end
 end

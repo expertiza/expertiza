cda448c88f408a0d166248319e760d93b9e6d932
rahuliyer95 <rahuliyer573@gmail.com>
2018-11-13 21:03:08 -0500

    Improved average score computation for bookmark

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -9,16 +9,18 @@ class Bookmark < ActiveRecord::Base
   # Computes the average rating when the ratings are provided via rubrics
   def average_based_on_rubric
     assignment = SignUpTopic.find(topic_id).assignment
-    questions = assignment.questionnaires.where(type: 'BookmarkRatingQuestionnaire').flat_map(&:questions)
     responses = BookmarkRatingResponseMap.where(
       reviewed_object_id: assignment.id,
       reviewee_id: id
     ).flat_map {|r| Response.where(map_id: r.id) }
+    questions = assignment.questionnaires.where(type: 'BookmarkRatingQuestionnaire').flat_map(&:questions)
     scores = Answer.compute_scores(responses, questions)
-    if scores[:avg].nil?
-      0
-    else
-      (scores[:avg] * 5.0 / 100.0).round(2)
-    end
+    normalize_average_scores(scores)
+  end
+
+  # Normalize the average scores based on responses to `BookmarkRatingQuestionnaire` rubric
+  def normalize_average_scores(scores)
+    scores[:avg] = 0 if scores[:avg].nil?
+    (scores[:avg] * 5.0 / 100.0).round(2)
   end
 end

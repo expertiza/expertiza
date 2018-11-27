d5d27f384570458d7be50de2e506e5582c5bd119
Maxxxyu <ysk950603@gmail.com>
2018-11-27 01:15:57 -0500

    Rename the index file and add code that can create a grading history

--- app/controllers/grading_histories_controller.rb
+++ app/controllers/grading_histories_controller.rb
@@ -1,8 +1,12 @@
 class GradingHistoriesController < ApplicationController
   before_action :set_grading_history, only: [:show, :edit, :update, :destroy]
 
+  def action_allowed?
+    true
+  end
+
   # GET /grading_histories
   def index
-    @grading_histories = GradingHistory.where(grade_receiver_id: params[:grade_receiver_id])
+    @grading_histories = GradingHistory.all
   end
 end

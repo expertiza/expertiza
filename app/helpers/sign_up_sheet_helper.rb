e95a5e87b8ecd17feaba3c0324d12ec700d476b4
vivin <vivinya25@gmail.com>
2018-12-03 23:19:16 -0500

    Issue 1 - Topic Signup by Instructor and TA

--- app/helpers/sign_up_sheet_helper.rb
+++ app/helpers/sign_up_sheet_helper.rb
@@ -75,4 +75,18 @@ module SignUpSheetHelper
     end
     html.html_safe
   end
+#  student7535 student7553 teaching_assistant7517
+  def display_instructor_signup?()
+    participant = AssignmentParticipant.find(params[:id].to_i)
+    assignment_data = participant.assignment
+    assignment = Assignment.find(assignment_data.id)
+    puts @assignment.id
+    puts TaMapping.exists?(ta_id: current_user.try(:id), course_id: assignment.course_id)
+    (%w[Super-Administrator Administrator ].include? session[:user].role.name) ||
+        (assignment.instructor_id == current_user.try(:id)) ||
+        TaMapping.exists?(ta_id: current_user.try(:id), course_id: assignment.course_id) ||
+        (assignment.course_id && Course.find(assignment.course_id).instructor_id == current_user.try(:id))
+
+
+  end
 end

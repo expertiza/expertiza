ec6d3ef6a6361fb130bad6abba8a06c5da79c92b
khanshariquem <sharique101322cse@gmail.com>
2018-12-03 23:11:01 -0500

    Contain changes related to the issue with color coding when an instructor or TA views the submitted assignments.

--- app/helpers/assignment_helper.rb
+++ app/helpers/assignment_helper.rb
@@ -136,6 +136,7 @@ module AssignmentHelper
     topic = SignedUpTeam.where(team_id: team.id).first.try :topic
     topic_identifier = topic.try :topic_identifier
     topic_name = topic.try :topic_name
+    topic_due_date = DueDate.where(parent_id: topic ? topic.id : @assignment.id).order(due_at: :desc).first
     users_for_curr_team = []
     participants = []
     teams_users.each do |teams_user|
@@ -143,14 +144,16 @@ module AssignmentHelper
       users_for_curr_team << user
       participants << Participant.where(["parent_id = ? AND user_id = ?", @assignment.id, user.id]).first
     end
-    [topic_identifier ||= "", topic_name ||= "", users_for_curr_team, participants]
+    [topic_identifier ||= "", topic_name ||= "", topic_due_date ||= "", users_for_curr_team, participants]
   end
 
-  def get_team_name_color_in_list_submission(team)
+  def get_team_name_color_in_list_submission(team , topic_due_date)
     if team.try(:grade_for_submission) && team.try(:comment_for_submission)
-      '#cd6133' # brown. submission grade has been assigned.
+      'brown' # brown. submission grade has been assigned.
+    elsif Time.now > topic_due_date.due_at
+      'blue' # BLUE GRADE CAN BE ASSIGNED , REVIEW DEADLINE IS OVER
     else
-      '#0984e3' # submission grade is not assigned yet.
+      'red' # RED GRADE CAN NOT BE ASSIGNED.
     end
   end
 end

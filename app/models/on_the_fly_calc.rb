5d45ea0fa938870893c18428de5b844b9e9bf64f
Kevin Acken <kevacken@gmail.com>
2018-12-07 12:12:49 -0500

    refactors average author feedback score logic

--- app/models/on_the_fly_calc.rb
+++ app/models/on_the_fly_calc.rb
@@ -51,48 +51,49 @@ module OnTheFlyCalc
 
   def compute_author_feedback_scores
     @author_feedback_scores = {}
-    @response_type = 'ReviewResponseMap'
-    @response_maps = ResponseMap.where('reviewed_object_id = ? && type = ?', self.id, @response_type)
+    @response_maps = ResponseMap.where('reviewed_object_id = ? && type = ?', self.id, 'ReviewResponseMap')
     rounds = self.rounds_of_reviews
     (1..rounds).each do |round|
       @response_maps.each do |response_map|
         # The reviewer is the metareviewee whose review the authors or teammates are reviewing.
-        reviewer = @author_feedback_scores[response_map.reviewer_id] if !@author_feedback_scores[response_map.reviewer_id].nil?
         response = Response.where('map_id = ?', response_map.id)
         response = response.select {|response| response.round == round }
-        next if response.empty?
-        # The author feedback response maps for the teammates reviewing the reviewer.
-        author_feedback_response_maps = ResponseMap.where('reviewed_object_id = ? && type = ?', response.first.id, "FeedbackResponseMap")
-        author_feedback_response_maps.each do |author_feedback_response_map|
-          @corresponding_response = Response.where('map_id = ?', author_feedback_response_map.id)
-          next if @corresponding_response.empty?
-          # @corresponding_response = @corresponding_response.select {|response| response.round == round } unless @corresponding_response.empty?
-          @respective_scores = {}
-          @respective_scores = reviewer[round] if !reviewer.nil? && !reviewer[round].nil?
-          author_feedback_questionnaire_id = feedback_questionnaire_id(@corresponding_response)
-          @questions = Question.where('questionnaire_id = ?', author_feedback_questionnaire_id)
-          # The score of the author feedback review
-          calc_review_score 
-          # Compute the sum of the author feedbacks for this review. 
-          @respective_scores[response_map.reviewee_id] = 0 if @respective_scores[response_map.reviewee_id].nil?
-          @respective_scores[response_map.reviewee_id] += @this_review_score
-          reviewer = {} if reviewer.nil?
-          reviewer[round] = {} if reviewer[round].nil?
-          reviewer[round] = @respective_scores
-        end 
-        # Divide the sum of the author feedback scores for this review by their number to get the
-        # average.
-        if (reviewer != nil && reviewer[round] != nil && response_map != nil && response_map.reviewee_id != nil && reviewer[round][response_map.reviewee_id] != nil && !author_feedback_response_maps.empty?)
-          reviewer[round][response_map.reviewee_id] /= author_feedback_response_maps.count
-        end
-        @author_feedback_scores[response_map.reviewer_id] = reviewer
+        calc_avg_feedback_score(response) unless response.empty?
       end
     end
     @author_feedback_scores
   end
 end
 
+
+
 private
+
+def calc_avg_feedback_score(response)
+  # The author feedback response maps for the teammates reviewing the review of their work.
+  author_feedback_response_maps = ResponseMap.where('reviewed_object_id = ? && type = ?', response.first.id, 'FeedbackResponseMap')
+  author_feedback_response_maps.each do |author_feedback_response_map|
+    @author_feedback_response = Response.where('map_id = ?', author_feedback_response_map.id)
+    next if @author_feedback_response.empty?
+    @respective_scores = {}
+    @respective_scores = @author_feedback_scores[response_map.reviewer_id][round] if !@author_feedback_scores[response_map.reviewer_id].nil? && !@author_feedback_scores[response_map.reviewer_id][round].nil?
+    author_feedback_questionnaire_id = feedback_questionnaire_id(@author_feedback_response)
+    @questions = Question.where('questionnaire_id = ?', author_feedback_questionnaire_id)
+    # The score of the author feedback review
+    calc_review_score 
+    # Compute the sum of the author feedbacks for this review. 
+    @respective_scores[response_map.reviewee_id] = 0 if @respective_scores[response_map.reviewee_id].nil?
+    @respective_scores[response_map.reviewee_id] += @this_review_score
+    @author_feedback_scores[response_map.reviewer_id] = {} if @author_feedback_scores[response_map.reviewer_id].nil?
+    @author_feedback_scores[response_map.reviewer_id][round] = {} if @author_feedback_scores[response_map.reviewer_id][round].nil?
+    @author_feedback_scores[response_map.reviewer_id][round] = @respective_scores
+  end 
+  # Divide the sum of the author feedback scores for this review by their number to get the
+  # average.
+  if (@author_feedback_scores[response_map.@author_feedback_scores[response_map.reviewer_id]_id] != nil && @author_feedback_scores[response_map.reviewer_id][round] != nil && response_map != nil && response_map.reviewee_id != nil && @author_feedback_scores[response_map.reviewer_id][round][response_map.reviewee_id] != nil && !author_feedback_response_maps.empty?)
+    @author_feedback_scores[response_map.@author_feedback_scores[response_map.reviewer_id]_id][round][response_map.reviewee_id] /= author_feedback_response_maps.count
+  end
+end
 
 def calc_review_score
   if !@corresponding_response.empty?

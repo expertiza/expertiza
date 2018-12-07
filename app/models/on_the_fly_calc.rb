1352a10e9018df44081b73f1cd032e0b23ecfb09
Mohit Jain <mohit12345jain@gmail.com>
2018-12-07 16:00:16 -0500

    Refactoring onthefly model

--- app/models/on_the_fly_calc.rb
+++ app/models/on_the_fly_calc.rb
@@ -84,11 +84,11 @@ def calc_avg_feedback_score(response)
   # Divide the sum of the author feedback scores for this review by their number to get the
   # average.
 
-  if (!@author_feedback_scores[@response_map.reviewer_id].nil? &&
+  if !@author_feedback_scores[@response_map.reviewer_id].nil? &&
       !@author_feedback_scores[@response_map.reviewer_id][@round].nil? &&
       !@response_map.nil? && !@response_map.reviewee_id.nil? &&
       !@author_feedback_scores[@response_map.reviewer_id][@round][@response_map.reviewee_id].nil? &&
-      !author_feedback_response_maps.empty?)
+      !author_feedback_response_maps.empty?
     @author_feedback_scores[@response_map.reviewer_id][@round][@response_map.reviewee_id] /= author_feedback_response_maps.count
   end
 end
@@ -104,7 +104,7 @@ def calc_feedback_scores_sum
   @questions = Question.where('questionnaire_id = ?', author_feedback_questionnaire_id)
   # Calculate the score of the author feedback review.
   calc_review_score
-  # Compute the sum of the author feedback scores for this review. 
+  # Compute the sum of the author feedback scores for this review.
   @respective_scores[@response_map.reviewee_id] = 0 if @respective_scores[@response_map.reviewee_id].nil?
   @respective_scores[@response_map.reviewee_id] += @this_review_score
   # The reviewer is the metareviewee whose review the authors or teammates are reviewing.

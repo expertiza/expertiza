0bda15a6e95ee7f344d89f67c3bbdaecd0837dc5
unknown <karan.kanchan96@gmail.com>
2018-12-07 13:16:29 -0500

    Rspec

--- spec/mailers/response_spec.rb
+++ spec/mailers/response_spec.rb
@@ -2,11 +2,19 @@ require "rails_helper"
 
 RSpec.describe Mailer, :type => :mailer do
   describe "notify_grade_conflict_message" do
+  
+	let(:mail) { Mailer.notify_grade_conflict_message }
 
-    it "renders the headers" do
+    it "renders the subject" do
       expect(mail.subject).to eq("Expertiza Notification: A review score is outside the acceptable range")
+	end
+	
+	it "renders the receiver email" do
       expect(mail.to).to eq(["cshinde57@gmail.com"])
-      expect(mail.from).to eq(["expertiza-support@lists.ncsu.edu"])
+	end
+
+	it "renders the sender email" do
+      expect(mail.from).to eq(["expertiza.development@gmail.com"])
     end
 
   end

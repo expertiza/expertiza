c4939a9f8db064e8f11d53939a97a9e8511270e0
cshinde57 <43565493+cshinde57@users.noreply.github.com>
2018-12-07 22:34:51 -0500

    Update review_response_map.rb

--- app/models/review_response_map.rb
+++ app/models/review_response_map.rb
@@ -150,8 +150,6 @@ class ReviewResponseMap < ResponseMap
       defn[:body][:obj_name] = assignment.name
       defn[:body][:first_name] = User.find(user.id).fullname
       defn[:to] = User.find(user.id).email
-      p User.find(user.id).email
-      p "review_response"
       Mailer.sync_message(defn).deliver_now
     end
   end

eb24536385cd8a0c416e5bcb597379650171f6b3
Mingkang Zhuang <mzhuang@ncsu.edu>
2018-12-07 23:01:11 -0500

    code climate fix

--- spec/controllers/grading_histories_controller_test.rb
+++ spec/controllers/grading_histories_controller_test.rb
@@ -1,12 +1,12 @@
 describe GradingHistoriesController do
-	describe '#index' do
-		context 'when show review grading history for paticipant' do
-			it 'gets history for the paticipant and renders grading_history#index page' do
-				allow(GradingHistory).to receive(:where).with(grade_receiver_id: 1)
-				params = {grade_receiver_id: 1}
-				get :index, params
-				expect(response).to redirect_to('/')
-			end
-		end
-	end
+  describe '#index' do
+    context 'when show review grading history for paticipant' do
+      it 'gets history for the paticipant and renders grading_history#index page' do
+        allow(GradingHistory).to receive(:where).with(grade_receiver_id: 1)
+        params = {grade_receiver_id: 1}
+        get :index, params
+        expect(response).to redirect_to('/')
+      end
+    end
+  end
 end

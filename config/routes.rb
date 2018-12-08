4b71ff10dc0a5a96da3fb6e08c810eb3c97790c4
sauve-moi <yzzczy@gmail.com>
2018-12-07 22:39:57 -0500

    fix code

--- config/routes.rb
+++ config/routes.rb
@@ -266,8 +266,8 @@ resources :institution, except: [:destroy] do
       get :new_feedback
       get :view
       get :remove_hyperlink
-      get :saving
-      get :redirection
+      get :save
+      get :redirect
       get :show_calibration_results_for_student
       post :custom_create
       get :pending_surveys

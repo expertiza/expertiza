c0675b7f5eb00fcacd117a4f3810a44c5675a273
Mingkang Zhuang <mzhuang@ncsu.edu>
2018-12-13 22:08:59 -0500

    add access control for grading history

--- app/controllers/grading_histories_controller.rb
+++ app/controllers/grading_histories_controller.rb
@@ -2,11 +2,24 @@ class GradingHistoriesController < ApplicationController
   before_action :set_grading_history, only: %i[show]
 
   def action_allowed?
-    return true if ['Instructor', 'Teaching Assistant', 'Super-Administrator', 'Administrator'].include? current_role_name
+    return true if ['Super-Administrator', 'Administrator'].include? current_role_name
+    type = params[:grade_type]
+    if type.eql? "Submission"
+      assignment_team = AssignmentTeam.find(params[:grade_receiver_id])
+      assignment = Assignment.find(assignment_team.parent_id)
+    end
+    if type.eql? "Review"
+      participant_id = params[:participant_id]
+      grade_receiver = AssignmentParticipant.find(participant_id)
+      assignment = Assignment.find(grade_receiver.parent_id)
+    end
+    return true if assignment.instructor_id == current_user.id
+    return true if TaMapping.exists?(ta_id: current_user.id, course_id: assignment.course_id) && (TaMapping.where(course_id: assignment.course_id).include? TaMapping.where(ta_id: current_user.id, course_id: assignment.course_id).first)
+    return true if assignment.course_id && Course.find(assignment.course_id).instructor_id == current_user.id
   end
 
   # GET /grading_histories
   def index
-    @grading_histories = GradingHistory.where(grade_receiver_id: params[:grade_receiver_id])
+    @grading_histories = GradingHistory.where(grade_receiver_id: params[:grade_receiver_id], grading_type: params[:grade_type]).reverse_order
   end
 end

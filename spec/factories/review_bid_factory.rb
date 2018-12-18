4221184ceb615629db8304a15e363615813c5dc5
kuangwanjing <gpkuang2017@gmail.com>
2018-12-18 00:41:58 -0500

    fix style

--- spec/factories/review_bid_factory.rb
+++ spec/factories/review_bid_factory.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :review_bid, class: ReviewBid do
     team_id 1
     participant { Participant.first || association(:participant) }
-		priority 1
+    priority 1
   end
 end

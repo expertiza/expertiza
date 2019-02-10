c2cff94a39721eb9ffef5f13fb0b72cbe2458be9
Winbobob <zhu6@ncsu.edu>
2019-02-10 15:04:30 -0500

    Update Capistrano deploy code

--- config/deploy.rb
+++ config/deploy.rb
@@ -55,7 +55,8 @@ set :linked_dirs, %w{log pg_data vendor/assets/components}
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :default_env, { 
-  "PASSENGER_INSTANCE_REGISTRY_DIR" => "/var/run/passenger-instreg"
+  "PASSENGER_INSTANCE_REGISTRY_DIR" => "/var/run/passenger-instreg",
+  "JAVA_HOME" => "/usr/lib/jvm/java-1.8.0-openjdk.x86_64"
 }
 
 # Default value for local_user is ENV['USER']

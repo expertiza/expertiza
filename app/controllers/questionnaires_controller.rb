e40008a8b2cd929a5c07c13de65cf955c877bb18
Winbobob <zhu6@ncsu.edu>
2019-02-15 10:11:17 -0500

    Refactor pull request #1345

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -49,47 +49,48 @@ class QuestionnairesController < ApplicationController
     if params[:questionnaire][:name].blank?
       flash[:error] = 'A rubric or survey must have a title.'
       redirect_to controller: 'questionnaires', action: 'new', model: params[:questionnaire][:type], private: params[:questionnaire][:private]
-    end
-    questionnaire_private = params[:questionnaire][:private] == 'true'
-    display_type = params[:questionnaire][:type].split('Questionnaire')[0]
-    begin
-      @questionnaire = Object.const_get(params[:questionnaire][:type]).new if Questionnaire::QUESTIONNAIRE_TYPES.include? params[:questionnaire][:type]
-    rescue StandardError
-      flash[:error] = $ERROR_INFO
-    end
-    begin
-      @questionnaire.private = questionnaire_private
-      @questionnaire.name = params[:questionnaire][:name]
-      @questionnaire.instructor_id = session[:user].id
-      @questionnaire.min_question_score = params[:questionnaire][:min_question_score]
-      @questionnaire.max_question_score = params[:questionnaire][:max_question_score]
-      @questionnaire.type = params[:questionnaire][:type]
-      # Zhewei: Right now, the display_type in 'questionnaires' table and name in 'tree_folders' table are not consistent.
-      # In the future, we need to write migration files to make them consistency.
-      case display_type
-      when 'AuthorFeedback'
-        display_type = 'Author%Feedback'
-      when 'CourseSurvey'
-        display_type = 'Course%Survey'
-      when 'TeammateReview'
-        display_type = 'Teammate%Review'
-      when 'GlobalSurvey'
-        display_type = 'Global%Survey'
-      when 'AssignmentSurvey'
-        display_type = 'Assignment%Survey'
+    else
+      questionnaire_private = params[:questionnaire][:private] == 'true'
+      display_type = params[:questionnaire][:type].split('Questionnaire')[0]
+      begin
+        @questionnaire = Object.const_get(params[:questionnaire][:type]).new if Questionnaire::QUESTIONNAIRE_TYPES.include? params[:questionnaire][:type]
+      rescue StandardError
+        flash[:error] = $ERROR_INFO
       end
-      @questionnaire.display_type = display_type
-      @questionnaire.instruction_loc = Questionnaire::DEFAULT_QUESTIONNAIRE_URL
-      @questionnaire.save
-      # Create node
-      tree_folder = TreeFolder.where(['name like ?', @questionnaire.display_type]).first
-      parent = FolderNode.find_by(node_object_id: tree_folder.id)
-      QuestionnaireNode.create(parent_id: parent.id, node_object_id: @questionnaire.id, type: 'QuestionnaireNode')
-      flash[:success] = 'You have successfully created a questionnaire!'
-    rescue StandardError
-      flash[:error] = $ERROR_INFO
+      begin
+        @questionnaire.private = questionnaire_private
+        @questionnaire.name = params[:questionnaire][:name]
+        @questionnaire.instructor_id = session[:user].id
+        @questionnaire.min_question_score = params[:questionnaire][:min_question_score]
+        @questionnaire.max_question_score = params[:questionnaire][:max_question_score]
+        @questionnaire.type = params[:questionnaire][:type]
+        # Zhewei: Right now, the display_type in 'questionnaires' table and name in 'tree_folders' table are not consistent.
+        # In the future, we need to write migration files to make them consistency.
+        case display_type
+        when 'AuthorFeedback'
+          display_type = 'Author%Feedback'
+        when 'CourseSurvey'
+          display_type = 'Course%Survey'
+        when 'TeammateReview'
+          display_type = 'Teammate%Review'
+        when 'GlobalSurvey'
+          display_type = 'Global%Survey'
+        when 'AssignmentSurvey'
+          display_type = 'Assignment%Survey'
+        end
+        @questionnaire.display_type = display_type
+        @questionnaire.instruction_loc = Questionnaire::DEFAULT_QUESTIONNAIRE_URL
+        @questionnaire.save
+        # Create node
+        tree_folder = TreeFolder.where(['name like ?', @questionnaire.display_type]).first
+        parent = FolderNode.find_by(node_object_id: tree_folder.id)
+        QuestionnaireNode.create(parent_id: parent.id, node_object_id: @questionnaire.id, type: 'QuestionnaireNode')
+        flash[:success] = 'You have successfully created a questionnaire!'
+      rescue StandardError
+        flash[:error] = $ERROR_INFO
+      end
+      redirect_to controller: 'questionnaires', action: 'edit', id: @questionnaire.id
     end
-    redirect_to controller: 'questionnaires', action: 'edit', id: @questionnaire.id
   end
 
   def create_questionnaire

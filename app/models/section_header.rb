bd3feff6cdf6930e24698e00b2ee29c087e4f4e0
gshugh <gshugh@ncsu.edu>
2019-03-03 18:18:53 -0500

    Fix second html_safe and refactor.

--- app/models/section_header.rb
+++ app/models/section_header.rb
@@ -3,20 +3,18 @@
 # https://github.com/presidentbeef/brakeman/issues/579
 
 class SectionHeader < QuestionnaireHeader
-  # https://makandracards.com/makandra/2579-everything-you-know-about-html_safe-is-wrong
+  # Code Climate suggestion
   def complete(_count, _answer = nil)
-#    html = ''.html_safe
-    safe_join(['<b style="color: #986633; font-size: x-large">',
-               self.txt,
-               '</b><br/><br/>'])
-#    html
+    safe_join(make_header << '<br/><br/>')
   end
 
   def view_completed_question(_count, _answer)
-    html = ''.html_safe
-    html << '<b style="color: #986633; font-size: x-large">'.html_safe
-    html << self.txt
-    html << '</b>'.html_safe
-    html
+    safe_join(make_header)
+  end
+
+  def private make_header
+    ['<b style="color: #986633; font-size: x-large">',
+     self.txt,
+     '</b>']
   end
 end

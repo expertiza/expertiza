4a98fa7d5540710e42fcb4692c32db316f1727cd
gshugh <gshugh@ncsu.edu>
2019-03-02 20:54:15 -0500

    Test code climate

--- app/models/section_header.rb
+++ app/models/section_header.rb
@@ -1,4 +1,6 @@
 class SectionHeader < QuestionnaireHeader
+  :attr_accessible
+  
   def complete(_count, _answer = nil)
     html = '<br><br><div><b style="color: #986633; font-size: x-large">' + self.txt + '</b></div>'
     html.html_safe

679d250e8b2bf68c68b4ad547991154e96436e34
gshugh <gshugh@ncsu.edu>
2019-03-03 11:16:13 -0500

    Document false positive for Mass assignment ... Restructure code for html_safe.

--- app/models/section_header.rb
+++ app/models/section_header.rb
@@ -1,13 +1,23 @@
+# Code Climate mistakenly reports
+# "Mass assignment is not restricted using attr_accessible"
+# https://github.com/presidentbeef/brakeman/issues/579
+
 class SectionHeader < QuestionnaireHeader
-  :attr_accessible
-  
+
+  # https://makandracards.com/makandra/2579-everything-you-know-about-html_safe-is-wrong
   def complete(_count, _answer = nil)
-    html = '<b style="color: #986633; font-size: x-large">' + self.txt + '</b><br/><br/>'
-    html.html_safe
+    html = ''.html_safe
+    html << '<b style="color: #986633; font-size: x-large">'.html_safe
+    html << self.txt
+    html << '</b><br/><br/>'.html_safe
+    html
   end
 
   def view_completed_question(_count, _answer)
-    html = '<b style="color: #986633; font-size: x-large">' + self.txt + '</b>'
-    html.html_safe
+    html = ''.html_safe
+    html << '<b style="color: #986633; font-size: x-large">'.html_safe
+    html << self.txt
+    html << '</b>'.html_safe
+    html
   end
 end

45337993d4b0e7331ce1f8c800e2b041e6481c0f
Aurora Tiffany-Davis <aurora.tdavis@gmail.com>
2019-03-06 11:16:59 -0800

    Add (blank) authorization helper and associated spec file

---
+++ app/helpers/authorization_helper.rb
@@ -0,0 +1,36 @@
+module AccessHelper
+  def authorize
+    unless all_actions_allowed?
+      flash_msg
+      redirect_back
+    end
+  end
+
+  def flash_msg
+    flash[:error] = if current_role && current_role.name.try(:downcase).start_with?('a', 'e', 'i', 'o', 'u')
+                      if params[:action] == 'new'
+                        "An #{current_role_name.try(:downcase)} is not allowed to create this/these #{params[:controller]}"
+                      else
+                        "An #{current_role_name.try(:downcase)} is not allowed to #{params[:action]} this/these #{params[:controller]}"
+                                      end
+                    else
+                      if params[:action] == 'new'
+                        "A #{current_role_name.try(:downcase)} is not allowed to create this/these #{params[:controller]}"
+                      else
+                        "A #{current_role_name.try(:downcase)} is not allowed to #{params[:action]} this/these #{params[:controller]}"
+                                      end
+                    end
+  end
+
+  def all_actions_allowed?
+    if current_user && current_role.super_admin?
+      true
+    else
+      action_allowed?
+    end
+  end
+
+  def action_allowed?
+    # default action_allowed is nil. So to allow any action, we need to override this in the controller.
+  end
+end

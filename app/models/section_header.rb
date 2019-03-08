eaeea8dd4aa3ccf5c50e91c2b0ef27f818c0b9ec
gshugh <gshugh@ncsu.edu>
2019-03-08 17:08:42 -0500

    Fix Code Climate issues. Remove html_safe. DRY code. Mass assignmnet false positive still exists.

--- app/models/section_header.rb
+++ app/models/section_header.rb
@@ -1,11 +1,24 @@
+# E1920
+# Code Climate mistakenly reports
+# "Mass assignment is not restricted using attr_accessible"
+# https://github.com/presidentbeef/brakeman/issues/579
+#
+# Remove html_safe
+# DRY code
+
 class SectionHeader < QuestionnaireHeader
+  # Code Climate suggestion to use safe_join instead of html_safe
   def complete(_count, _answer = nil)
-    html = '<b style="color: #986633; font-size: x-large">' + self.txt + '</b><br/><br/>'
-    html.html_safe
+    safe_join(make_header << '<br/><br/>')
   end
 
   def view_completed_question(_count, _answer)
-    html = '<b style="color: #986633; font-size: x-large">' + self.txt + '</b>'
-    html.html_safe
+    safe_join(make_header)
+  end
+
+  private def make_header
+    ['<b style="color: #986633; font-size: x-large">',
+     self.txt,
+     '</b>']
   end
 end

ce31c47e5e46a216d1addd72103b044c682cd5b1
Aurora Tiffany-Davis <aurora.tdavis@gmail.com>
2019-03-11 08:06:24 -0700

    E1915.  Add new private method to authorization helper

--- app/helpers/authorization_helper.rb
+++ app/helpers/authorization_helper.rb
@@ -8,19 +8,17 @@ module AuthorizationHelper
   # We use session directly instead of current_role_name and the like
   # Because helpers do not seem to have access to the methods defined in app/controllers/application_controller.rb
 
-  # Determine if the currently logged-in user has the privileges of a TA
-  # Let the Role model define this logic for the sake of DRY
-  # If there is no currently logged-in user simply return false
+  # PUBLIC METHODS
+
+  # Determine if the currently logged-in user has the privileges of a TA (or higher)
   def current_user_has_ta_privileges?
-    session[:user] ? session[:user].role.hasAllPrivilegesOf(Role.find_by(name: 'Teaching Assistant')) : false
+    current_user_has_privileges_of?('Teaching Assistant')
   end
 
-  # Determine if the currently logged-in user has any one of the defined roles in the Role model
-  # These roles are: Student, Teaching Assistant, Instructor, Administrator, Super-Administrator
-  # Student has the 'lowest' role, so we only check for that role. Any role that Student or 'higher' will pass the check
-  # If there is no currently logged in user, return false
+  # Determine if the currently logged-in user has the privileges of a Student (or higher)
+  # As student is the "lowest" role, any student (or higher) has at least some privileges
   def current_user_has_any_privileges?
-    session[:user] ? session[:user].role.hasAllPrivilegesOf(Role.find_by(name: 'Student')) : false
+    current_user_has_privileges_of?('Student')
   end
 
   # Determine if the currently logged-in user is participating in an Assignment based on the passed in AssignmentTeam ID.
@@ -30,6 +28,16 @@ module AuthorizationHelper
     team = AssignmentTeam.find_by(id: assignment_team_id)
     participant = AssignmentParticipant.find_by(parent_id: team.assignment.id, user_id: session[:user].id)
     participant ? true : false
+  end
+
+  # PRIVATE METHODS
+  private
+
+  # Determine if the currently logged-in user has the privileges of the given role name (or higher privileges)
+  # Let the Role model define this logic for the sake of DRY
+  # If there is no currently logged-in user simply return false
+  def current_user_has_privileges_of?(role_name)
+    session[:user] ? session[:user].role.hasAllPrivilegesOf(Role.find_by(name: role_name)) : false
   end
 
 end

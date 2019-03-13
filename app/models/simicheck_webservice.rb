f2a31afa96d68691965398946dc9a00269513f78
George Hugh <gshugh@ncsu.edu>
2019-03-13 15:22:40 -0400

    Move RestClient::Request.execute to private method.

--- app/models/simicheck_webservice.rb
+++ app/models/simicheck_webservice.rb
@@ -10,11 +10,23 @@ class SimiCheckWebService
   # Returns:
   #   response = RestClient::Response
   def self.all_comparisons
-    RestClient::Request.execute(method: :get,
-                                url: full_url('/comparisons'),
-                                headers: make_short_header,
+    request_execute(:get, '/comparisons', make_short_header)
+  end
+
+  private def request_execute(method_sym, url, header_func)
+    RestClient::Request.execute(method: method_sym,
+                                url: full_url(url),
+                                headers: header_func,
                                 verify_ssl: false)
   end
+  private def request_execute_payload(method_sym, url, json_body, header_func)
+    RestClient::Request.execute(method: method_sym,
+                                url: full_url(url),
+                                payload: json_body,
+                                headers: header_func,
+                                verify_ssl: false)
+  end
+
 
   # Creates a new comparison
   # Parameters:
@@ -25,12 +37,8 @@ class SimiCheckWebService
   # DRY headers
   def self.new_comparison(comparison_name = '')
     # full_url = @base_uri + '/comparison'
-    # json_body = {comparison_name: comparison_name}.to_json
-    RestClient::Request.execute(method: :put,
-                                url: full_url('/comparison'),
-                                payload: comparison_json_body(comparison_name),
-                                headers: make_header,
-                                verify_ssl: false)
+    json_body = {comparison_name: comparison_name}.to_json
+    request_execute_payload(:put, '/comparison', json_body, make_header)
   end
 
   # Deletes a comparison
@@ -40,10 +48,7 @@ class SimiCheckWebService
   #   response = RestClient::Response (NO BODY)
   def self.delete_comparison(comparison_id)
     # full_url = @base_uri + '/comparison/' + comparison_id
-    RestClient::Request.execute(method: :delete,
-                                url: full_url('/comparison/' + comparison_id),
-                                headers: make_short_header,
-                                verify_ssl: false)
+    request_execute(:delete, '/comparison/' + comparison_id, make_short_header)
   end
 
   # Gets the details about a comparison
@@ -53,10 +58,11 @@ class SimiCheckWebService
   #   response = RestClient::Response
   def self.get_comparison_details(comparison_id)
     # full_url = @base_uri + '/comparison/' + comparison_id
-    RestClient::Request.execute(method: :get,
-                                url: full_url('/comparison/' + comparison_id),
-                                headers: make_short_header,
-                                verify_ssl: false)
+    request_execute(:get, '/comparison/' + comparison_id, make_short_header)
+    # RestClient::Request.execute(method: :get,
+    #                             url: full_url('/comparison/' + comparison_id),
+    #                             headers: make_short_header,
+    #                             verify_ssl: false)
   end
 
   # Updates a comparison (currently only the name)
@@ -68,12 +74,13 @@ class SimiCheckWebService
   # DRY headers
   def self.update_comparison(comparison_id, new_comparison_name)
     # full_url = @base_uri + '/comparison/' + comparison_id
-    # json_body = {comparison_name: new_comparison_name}.to_json
-    RestClient::Request.execute(method: :post,
-                                url: full_url('/comparison/' + comparison_id),
-                                payload: comparison_json_body(new_comparison_name),
-                                headers: make_header,
-                                verify_ssl: false)
+    json_body = {comparison_name: new_comparison_name}.to_json
+    request_execute_payload(:post, '/comparison/' + comparison_id, json_body, make_header)
+    # RestClient::Request.execute(method: :post,
+    #                             url: full_url('/comparison/' + comparison_id),
+    #                             payload: comparison_json_body(new_comparison_name),
+    #                             headers: make_header,
+    #                             verify_ssl: false)
   end
 
   ############################################
@@ -90,11 +97,12 @@ class SimiCheckWebService
     # full_url = @base_uri + '/upload_file/' + comparison_id
     file_to_upload = File.new(path_to_file, 'rb')
     json_body = {"file" => file_to_upload} # don't use .to_json!
-    RestClient::Request.execute(method: :put,
-                                url: full_url('/upload_file/' + comparison_id),
-                                payload: json_body,
-                                headers: make_header('multipart/form-data'),
-                                verify_ssl: false)
+    request_execute_payload(:put, '/upload_file/' + comparison_id, json_body, make_header('multipart/form-data'))
+    # RestClient::Request.execute(method: :put,
+    #                             url: full_url('/upload_file/' + comparison_id),
+    #                             payload: json_body,
+    #                             headers: make_header('multipart/form-data'),
+    #                             verify_ssl: false)
   end
 
   # Deletes files from a comparison
@@ -107,11 +115,12 @@ class SimiCheckWebService
   def self.delete_files(comparison_id, filenames_to_delete)
     # full_url = @base_uri + '/delete_files/' + comparison_id
     json_body = {"filenames" => filenames_to_delete}.to_json
-    RestClient::Request.execute(method: :post,
-                                url: full_url('/delete_files/' + comparison_id),
-                                payload: json_body,
-                                headers: make_header,
-                                verify_ssl: false)
+    request_execute_payload(:post, '/delete_files/' + comparison_id, json_body, make_header)
+    # RestClient::Request.execute(method: :post,
+    #                             url: full_url('/delete_files/' + comparison_id),
+    #                             payload: json_body,
+    #                             headers: make_header,
+    #                             verify_ssl: false)
   end
 
   ############################################
@@ -125,10 +134,11 @@ class SimiCheckWebService
   #   response = RestClient::Response
   def self.get_similarity_nxn(comparison_id)
     # full_url = @base_uri + '/similarity_nxn/' + comparison_id
-    RestClient::Request.execute(method: :get,
-                                url: full_url('/similarity_nxn/' + comparison_id),
-                                headers: make_short_header,
-                                verify_ssl: false)
+    request_execute(:get, '/similarity_nxn/' + comparison_id, make_short_header)
+    # RestClient::Request.execute(method: :get,
+    #                             url: full_url('/similarity_nxn/' + comparison_id),
+    #                             headers: make_short_header,
+    #                             verify_ssl: false)
   end
 
   # Starts the computation of file similarity for a comparison
@@ -140,11 +150,12 @@ class SimiCheckWebService
   def self.post_similarity_nxn(comparison_id, callback_url = '')
     # full_url = @base_uri + '/similarity_nxn/' + comparison_id
     json_body = callback_url.empty? ? {}.to_json : {"callback_url" => callback_url}.to_json
-    RestClient::Request.execute(method: :post,
-                                url: full_url('/similarity_nxn/' + comparison_id),
-                                payload: json_body,
-                                headers: make_header,
-                                verify_ssl: false)
+    request_execute_payload(:post, '/similarity_nxn/' + comparison_id, json_body, make_header)
+    # RestClient::Request.execute(method: :post,
+    #                             url: full_url('/similarity_nxn/' + comparison_id),
+    #                             payload: json_body,
+    #                             headers: make_header,
+    #                             verify_ssl: false)
   end
 
   # # Checks where a NxN comparison has terminated - NOT WORKING?
@@ -166,11 +177,12 @@ class SimiCheckWebService
   def self.get_similarity_1xn(comparison_id, filename)
     # full_url = @base_uri + '/similarity_1xn/' + comparison_id
     json_body = {"filename" => filename}.to_json
-    RestClient::Request.execute(method: :get,
-                                url: full_url('/similarity_1xn/' + comparison_id),
-                                payload: json_body,
-                                headers: make_header,
-                                verify_ssl: false)
+    request_execute_payload(:get, '/similarity_1xn/' + comparison_id, json_body, make_header)
+    # RestClient::Request.execute(method: :get,
+    #                             url: full_url('/similarity_1xn/' + comparison_id),
+    #                             payload: json_body,
+    #                             headers: make_header,
+    #                             verify_ssl: false)
   end
 
   # Finds the files in a comparison that are most similar to a given file
@@ -178,11 +190,12 @@ class SimiCheckWebService
   def self.post_similarity_1xn(comparison_id, filename, callback_url = '')
     # full_url = @base_uri + '/similarity_1xn/' + comparison_id
     json_body = callback_url.empty? ? {"filename" => filename}.to_json : {"filename" => filename, "callback_url" => callback_url}.to_json
-    RestClient::Request.execute(method: :post,
-                                url: full_url('/similarity_1xn/' + comparison_id),
-                                payload: json_body,
-                                headers: make_header,
-                                verify_ssl: false)
+    request_execute_payload(:post, '/similarity_1xn/' + comparison_id, json_body, make_header)
+    # RestClient::Request.execute(method: :post,
+    #                             url: full_url('/similarity_1xn/' + comparison_id),
+    #                             payload: json_body,
+    #                             headers: make_header,
+    #                             verify_ssl: false)
   end
 
   ############################################
@@ -192,23 +205,21 @@ class SimiCheckWebService
   # Gets the results of the similarity comparison
   def self.visualize_similarity(comparison_id)
     # full_url = @base_uri + '/visualize_similarity/' + comparison_id
-    RestClient::Request.execute(method: :get,
-                                url: full_url('/visualize_similarity/' + comparison_id),
-                                headers: make_short_header,
-                                verify_ssl: false)
+    request_execute(:get, '/visualize_similarity/' + comparison_id, make_short_header)
+    # RestClient::Request.execute(method: :get,
+    #                             url: full_url('/visualize_similarity/' + comparison_id),
+    #                             headers: make_short_header,
+    #                             verify_ssl: false)
   end
 
   # Gets the results of similarity comparison
   def self.visualize_comparison(comparison_id, file_id1, file_id2)
     # full_url = @base_uri + '/visualize_comparison/' + comparison_id + '/' + file_id1 + '/' + file_id2
-    RestClient::Request.execute(method: :get,
-                                url: full_url('/visualize_comparison/' + comparison_id + '/' + file_id1 + '/' + file_id2),
-                                headers: make_short_header,
-                                verify_ssl: false)
-  end
-
-  private def comparison_json_body(value)
-    {comparison_name: value}.to_json
+    request_execute(:get, '/visualize_comparison/' + comparison_id + '/' + file_id1 + '/' + file_id2, make_short_header)
+    # RestClient::Request.execute(method: :get,
+    #                             url: full_url('/visualize_comparison/' + comparison_id + '/' + file_id1 + '/' + file_id2),
+    #                             headers: make_short_header,
+    #                             verify_ssl: false)
   end
 
   private def full_url(dir)

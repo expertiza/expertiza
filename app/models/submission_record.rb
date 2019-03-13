f49a32ad2ff9fb1ffcb9ab9a7203afbb3f0479b2
cladkins <cladkins@ncsu.edu>
2019-03-12 22:18:41 -0400

    fixed code smells for submission_record.rb

--- app/models/submission_record.rb
+++ app/models/submission_record.rb
@@ -1,4 +1,5 @@
 class SubmissionRecord < ActiveRecord::Base
+  attr_accessible # TODO: this may break mass assignment upstream. If mass assignment is needed, the fields need to be whiteliste here.
   validates :content, presence: true
   validates :operation, presence: true
   validates :team_id, presence: true

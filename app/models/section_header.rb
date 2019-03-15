1c5584d7f8d418a979194d708e3232c8e0652674
George Hugh <gshugh@ncsu.edu>
2019-03-15 14:34:25 -0400

    Fix comments.

--- app/models/section_header.rb
+++ app/models/section_header.rb
@@ -1,13 +1,4 @@
-# E1920
-# Code Climate mistakenly reports
-# "Mass assignment is not restricted using attr_accessible"
-# https://github.com/presidentbeef/brakeman/issues/579
-#
-# Remove html_safe
-# DRY code
-
 class SectionHeader < QuestionnaireHeader
-  # Code Climate suggestion to use safe_join instead of html_safe
   def complete(_count, _answer = nil)
     safe_join(make_header << '<br/><br/>')
   end

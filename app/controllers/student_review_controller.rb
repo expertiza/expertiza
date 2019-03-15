a013194d511f850b12e62de0f4636e26af3f75cd
Ziweiwww <30943778+Ziweiwww@users.noreply.github.com>
2019-03-15 18:19:00 -0400

    Update student_review_controller.rb
    Fix omit parentheses for ternary conditions.

--- app/controllers/student_review_controller.rb
+++ app/controllers/student_review_controller.rb
@@ -5,7 +5,7 @@ class StudentReviewController < ApplicationController
      'Administrator',
      'Super-Administrator',
      'Student'].include? current_role_name and
-    ((%w[list].include? action_name) ? are_needed_authorizations_present?(params[:id], "submitter") : true)
+    %w[list].include? action_name ? are_needed_authorizations_present?(params[:id], "submitter") : true
   end
 
   def list

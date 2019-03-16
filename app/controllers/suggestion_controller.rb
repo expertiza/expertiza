66391933f911749e43ca51fa3819758ce90f2035
Ziweiwww <30943778+Ziweiwww@users.noreply.github.com>
2019-03-16 14:48:21 -0400

    Update suggestion_controller.rb
    Fix some climate issues

--- app/controllers/suggestion_controller.rb
+++ app/controllers/suggestion_controller.rb
@@ -75,9 +75,7 @@ class SuggestionController < ApplicationController
     @suggestion.status = 'Initiated'
     @suggestion.unityID = if params[:suggestion_anonymous].nil?
                             session[:user].name
-                          else
-                            ""
-    end
+                          else "" end
 
     if @suggestion.save
       flash[:success] = 'Thank you for your suggestion!' if @suggestion.unityID != ''
@@ -111,22 +109,21 @@ class SuggestionController < ApplicationController
   # If user submits a suggestion anonymously and it gets approved -> DOES NOT get an email
   def send_email
     proposer = User.find_by(id: @user_id)
-    if proposer
-      teams_users = TeamsUser.where(team_id: @team_id)
-      cc_mail_list = []
-      teams_users.each do |teams_user|
-        cc_mail_list << User.find(teams_user.user_id).email if teams_user.user_id != proposer.id
-      end
-      Mailer.suggested_topic_approved_message(
-        to: proposer.email,
-        cc: cc_mail_list,
-        subject: "Suggested topic '#{@suggestion.title}' has been approved",
-        body: {
-          approved_topic_name: @suggestion.title,
-          proposer: proposer.name
-        }
-      ).deliver_now!
+    return unless proposer
+    teams_users = TeamsUser.where(team_id: @team_id)
+    cc_mail_list = []
+    teams_users.each do |teams_user|
+      cc_mail_list << User.find(teams_user.user_id).email if teams_user.user_id != proposer.id
     end
+    Mailer.suggested_topic_approved_message(
+      to: proposer.email,
+      cc: cc_mail_list,
+      subject: "Suggested topic '#{@suggestion.title}' has been approved",
+      body: {
+        approved_topic_name: @suggestion.title,
+        proposer: proposer.name
+      }
+    ).deliver_now!
   end
 
   def notification

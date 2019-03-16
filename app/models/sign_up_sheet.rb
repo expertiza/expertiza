8f13bdae7bb509b6e387260af0f3d6b6cc958e09
George Hugh <gshugh@ncsu.edu>
2019-03-16 01:10:09 -0400

    Remove unneeded comments. Ensure sign_up_sheet_spec.rb passes.

--- app/models/sign_up_sheet.rb
+++ app/models/sign_up_sheet.rb
@@ -1,25 +1,12 @@
-# E1920
-# Code Climate mistakenly reports
-# "Mass assignment is not restricted using attr_accessible"
-# https://github.com/presidentbeef/brakeman/issues/579
-#
-# Change variable names to snake_case; confirmation_status, sign_up
-# Change method names to snake case; confirm_topic, create_sign_up_team,
-#   other_confirmed_topic_for_user, slot_available?, teammate_ads?
-# Rubyify code
-
 class SignUpSheet < ActiveRecord::Base
   # Team lazy initialization method [zhewei, 06/27/2015]
-  # Comment out teamuser line per Code climate
   def self.signup_team(assignment_id, user_id, topic_id = nil)
     users_team = SignedUpTeam.find_team_users(assignment_id, user_id)
     if users_team.empty?
       # if team is not yet created, create new team.
       # create Team and TeamNode
       team = AssignmentTeam.create_team_and_node(assignment_id)
-      # user = User.find(user_id)
       # create TeamsUser and TeamUserNode
-      # teamuser = ApplicationController.helpers.create_team_users(user, team.id)
       # create SignedUpTeam
       confirmation_status = SignUpSheet.confirm_topic(user_id, team.id, topic_id, assignment_id) if topic_id
     elsif topic_id
@@ -29,8 +16,6 @@ class SignUpSheet < ActiveRecord::Base
     confirmation_status
   end
 
-  # Change name to confirm_topic per Code Climate
-  # Cognitive Complexity and Assignment Branch Condition still exist
   def self.confirm_topic(user_id, team_id, topic_id, assignment_id)
     # check whether user has signed up already
     user_signup = SignUpSheet.other_confirmed_topic_for_user(assignment_id, team_id)
@@ -49,7 +34,6 @@ class SignUpSheet < ActiveRecord::Base
       end
     else
       # If all the topics choosen by the user are waitlisted,
-      # Chnage loop per Code Climate
       user_signup.each do |user_signup_topic|
         return false if user_signup_topic.is_waitlisted == false
       end
@@ -64,8 +48,6 @@ class SignUpSheet < ActiveRecord::Base
     result
   end
 
-  # Change method name to sign_up_waitlisted
-  # Remove user_id from method signature
   def self.sign_up_waitlisted(assignment_id, sign_up, team_id, topic_id)
     if !slot_available?(topic_id)
       sign_up.is_waitlisted = true
@@ -78,26 +60,18 @@ class SignUpSheet < ActiveRecord::Base
     result
   end
 
-  # Change method name to cancel_all_waitlists
-  # Change variable name to signed_up_team per Code Climate
   # Change where().first to where() per Code Climate. Using find_by() causes test failures.
   #      Assumes that SignedUpTeam has 1 record per topic_id
-  # Change result=true to true
-  # Comment out team_id line
-  # Change method signature, remove user_id
-  # Change to update_attributes per Code Climate
   def self.cancel_all_waitlists(assignment_id, sign_up, team_id, topic_id)
     Waitlist.cancel_all_waitlists(team_id, assignment_id)
     sign_up.is_waitlisted = false
     sign_up.save
     # Update topic_id in signed_up_teams table with the topic_id
-    # team_id = SignedUpTeam.find_team_users(assignment_id, user_id)
     signed_up_team = SignedUpTeam.where(topic_id: topic_id)
-    signed_up_team.update_attributes('topic_id', topic_id)
+    signed_up_team.update_attributes(topic_id: topic_id)
     true
   end
 
-  # Change name to create_sign_up_team per Code Climate
   def self.create_sign_up_team(assignment_id, sign_up, topic_id, user_id)
     if slot_available?(topic_id)
       sign_up.is_waitlisted = false
@@ -112,22 +86,15 @@ class SignUpSheet < ActiveRecord::Base
     [team_id, topic_id]
   end
 
-  # Change name to other_confirmed_topic_for_user per Code Climate
-  # Rubify code
   def self.other_confirmed_topic_for_user(assignment_id, team_id)
     SignedUpTeam.find_user_signup_topics(assignment_id, team_id)
   end
 
   # When using this method when creating fields, update race conditions by using db transactions
-  # Change name to slot_available? per Code Climate
   def self.slot_available?(topic_id)
     SignUpTopic.slot_available?(topic_id)
   end
 
-  # Assignment Branch Condition still exists
-  # Change for loop per Code Climate
-  # Change local variabel to due_date
-  # Change DateTime.parse to Time.zone.parse
   def self.add_signup_topic(assignment_id)
     @review_rounds = Assignment.find(assignment_id).num_review_rounds
     @topics = SignUpTopic.where(assignment_id: assignment_id)
@@ -139,7 +106,6 @@ class SignUpSheet < ActiveRecord::Base
       due_date['topic_identifier'] = topic.topic_identifier
       due_date['topic_name'] = topic.topic_name
 
-      # for round in 1..@review_rounds
       1.upto(@review_rounds) do |r|
         process_review_round(assignment_id, due_date, r, topic)
       end
@@ -152,7 +118,6 @@ class SignUpSheet < ActiveRecord::Base
     @duedates
   end
 
-  # Change name to teammate_ads? per Code Climate
   def self.teammate_ads?(topic_id)
     teams = Team.joins('INNER JOIN signed_up_teams ON signed_up_teams.team_id = teams.id')
                 .select('teams.*')
@@ -164,10 +129,6 @@ class SignUpSheet < ActiveRecord::Base
   class << self
     private
 
-    # Assignment Branch Condition still exists
-    # Change duedate* to due_date*
-    # Change loop variable to d
-    # Change DateTime.parse to Time.zone.parse
     def process_review_round(assignment_id, due_date, round, topic)
       due_date_rev, due_date_subm = find_topic_due_dates(round, topic)
 
@@ -184,9 +145,6 @@ class SignUpSheet < ActiveRecord::Base
       due_date['review_' + round.to_s] = Time.zone.parse(due_date_rev['due_at'].to_s).strftime("%Y-%m-%d %H:%M:%S")
     end
 
-    # Change name to find_topic_due_dates
-    # Change variables to due_date*
-    # Change where().first to find_by()
     def find_topic_due_dates(round, topic)
       deadline_type_subm = DeadlineType.find_by(name: 'submission').id
       due_date_subm = TopicDueDate.find_by(parent_id: topic.id, deadline_type_id: deadline_type_subm, round: round)
@@ -196,8 +154,6 @@ class SignUpSheet < ActiveRecord::Base
     end
   end
 
-  # Assignment Branch Condition and Cognitive Complexity still exists
-  # Change where().first to find_by() per Code Climate
   def self.import(row_hash, session, _id = nil)
     raise 'Not enough items: expect 2 or more columns: Topic Identifier, User Name 1, User Name 2, ...' if row_hash.length < 2
 

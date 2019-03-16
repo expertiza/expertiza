811d1ed9fae05ae879fd5362350cbcef82c34532
cladkins <cladkins@ncsu.edu>
2019-03-16 16:36:44 -0400

    fixed code smells for teammate_review_response_map.rb

--- app/models/teammate_review_response_map.rb
+++ app/models/teammate_review_response_map.rb
@@ -22,8 +22,6 @@ class TeammateReviewResponseMap < ResponseMap
   # Refactored from email method in response.rb
   def email(defn, participant, assignment)
     defn[:body][:type] = "Teammate Review"
-    participant = AssignmentParticipant.find(reviewee_id)
-    topic_id = SignedUpTeam.topic_id(participant.parent_id, participant.user_id)
     defn[:body][:obj_name] = assignment.name
     user = User.find(participant.user_id)
     defn[:body][:first_name] = user.fullname

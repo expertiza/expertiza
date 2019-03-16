f22449798005273218fb65e1ae950013105b2665
George Hugh <gshugh@ncsu.edu>
2019-03-16 10:40:42 -0400

    Extract block to satisfy Code Climate.

--- app/models/waitlist.rb
+++ app/models/waitlist.rb
@@ -2,10 +2,14 @@ class Waitlist < ActiveRecord::Base
   def self.cancel_all_waitlists(team_id, assignment_id)
     waitlisted_topics = SignUpTopic.find_waitlisted_topics(assignment_id, team_id)
 
+    destroy_topics(waitlisted_topics) unless waitlisted_topics.nil?
+  end
+
+  private def self.destroy_topics(waitlisted_topics)
     waitlisted_topics.each do |waitlisted_topic|
       entry = SignedUpTeam.find(waitlisted_topic.id)
       entry.destroy
       ExpertizaLogger.info LoggerMessage.new('Waitlist', '', "Waitlisted topic deleted with id: #{waitlisted_topic.id}")
-    end unless waitlisted_topics.nil?
+    end
   end
 end

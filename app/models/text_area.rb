8ce18af8f5e986c19d3f49b27562b2a395209048
George Hugh <gshugh@ncsu.edu>
2019-03-16 20:12:23 -0400

    Fix Code Climate issues except for html_safe.

--- app/models/text_area.rb
+++ app/models/text_area.rb
@@ -3,13 +3,14 @@ class TextArea < TextResponse
     if self.size.nil?
       cols = '70'
       rows = '1'
-    elsif
+    else
       cols = self.size.split(',')[0]
       rows = self.size.split(',')[1]
     end
     html = '<p><label for="responses_' + count.to_s + '">' + self.txt + '</label></p>'
     html += '<input id="responses_' + count.to_s + '_score" name="responses[' + count.to_s + '][score]" type="hidden" value="">'
-    html += '<p><textarea cols="' + cols + '" rows="' + rows + '" id="responses_' + count.to_s + '_comments" name="responses[' + count.to_s + '][comment]" class="tinymce">'
+    html += '<p><textarea cols="' + cols + '" rows="' + rows +
+            '" id="responses_' + count.to_s + '_comments" name="responses[' + count.to_s + '][comment]" class="tinymce">'
     html += answer.comments unless answer.nil?
     html += '</textarea>'
     html += '</p>'

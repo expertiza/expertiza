84c7a61c05fabc56731084f9dc0aa5552d210e7b
uschatto <47371094+uschatto@users.noreply.github.com>
2019-03-18 01:41:46 -0400

    E1917 : profile_controller.rb updated
    Removed unprotected mass assignment issue, redundant code issue

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -9,12 +9,11 @@ class ProfileController < ApplicationController
   end
 
   def update
-    params.permit!
     @user = session[:user]
 
     unless params[:assignment_questionnaire].nil? or params[:assignment_questionnaire][:notification_limit].blank?
       aq = AssignmentQuestionnaire.where(['user_id = ? and assignment_id is null and questionnaire_id is null', @user.id]).first
-      aq.update_attributes('notification_limit', params[:assignment_questionnaire][:notification_limit])
+      aq.update_attributes(notification_limit: params[:assignment_questionnaire][:notification_limit])
     end
     if @user.update_attributes(params[:user])
       ExpertizaLogger.info LoggerMessage.new(controller_name, @user.name, "Your profile was successfully updated.", request)
@@ -25,31 +24,5 @@ class ProfileController < ApplicationController
     end
 
     redirect_to controller: :profile, action: :edit
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name,
-                                 :crypted_password,
-                                 :role_id,
-                                 :password_salt,
-                                 :fullname,
-                                 :email,
-                                 :parent_id,
-                                 :private_by_default,
-                                 :mru_directory_path,
-                                 :email_on_review,
-                                 :email_on_submission,
-                                 :email_on_review_of_review,
-                                 :is_new_user,
-                                 :master_permission_granted,
-                                 :handle,
-                                 :digital_certificate,
-                                 :persistence_token,
-                                 :timezonepref,
-                                 :public_key,
-                                 :copy_of_emails,
-                                 :institution_id)
   end
 end

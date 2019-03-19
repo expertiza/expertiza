13113a92af516cbf6974e79733af1db6ebd56096
Expertiza Developer <jfwang2@ncsu.edu>
2019-03-18 23:42:36 -0400

    tried to start figuring some testing out

--- spec/controllers/assessment360_controller_spec.rb
+++ spec/controllers/assessment360_controller_spec.rb
@@ -4,6 +4,29 @@
 # If you are a student writing that functionality, feel free to remove this comment!
 
 describe Assessment360Controller do
+  let(:course) { double('Course', instructor_id: 6, path: '/cscs', name: 'abc') }
+
+  describe '#all_students_all_reviews' do
+    context 'when course does not have participants' do
+      before(:each) do
+        request.env['HTTP_REFERER'] = 'http://example.com'
+        get "all_students_all_reviews"
+      end
+
+      it 'redirects to back' do
+        expect(response).to redirect_to(:back)
+      end
+
+      it 'flashes an error' do
+        expect(flash[:error]).to be_present
+      end
+    end
+
+    context 'when course has participants' do
+      # allow(Course).to receive(:find).with('1').and_return(course)
+    end
+  end
+
   describe '#course_student_grade_summary' do
     context 'when course does not have participants' do
       before(:each) do
@@ -19,8 +42,10 @@ describe Assessment360Controller do
         expect(flash[:error]).to be_present
       end
     end
+
+    context 'when course has participants' do
+      # allow(Course).to receive(:find).with('1').and_return(course)
+    end
   end
 end
 
-
-# adding a comment to do initial commit

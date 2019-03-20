bd58e19fe7b7bf49f81e6454dfe3210323d91b31
Expertiza Developer <jfwang2@ncsu.edu>
2019-03-20 16:11:41 -0400

    handling cases in both methods where there are no participants

--- spec/controllers/assessment360_controller_spec.rb
+++ spec/controllers/assessment360_controller_spec.rb
@@ -6,6 +6,8 @@
 describe Assessment360Controller do
   let(:instructor) { build(:instructor, id: 6) }
   let(:course) { double('Course', instructor_id: 6, path: '/cscs', name: 'abc') }
+  let(:assignment) { build(:assignment, id: 1, instructor_id: 6, due_dates: [due_date], microtask: true, staggered_deadline: true) }
+  let(:due_date) { build(:assignment_due_date, deadline_type_id: 1) }
 
   describe '#all_students_all_reviews' do
     context 'when course does not have participants' do
@@ -23,13 +25,25 @@ describe Assessment360Controller do
       end
     end
 
-    context 'when course has participants' do
-      it 'does not divide by zero' do
+    context 'method is called' do
+      before(:each) do
+        request.env['HTTP_REFERER'] = 'http://example.com'
+        get "course_student_grade_summary"
+      end
+
+      it 'redirects to back and flashes error as there are no participants' do
+        # allow(Course).to receive(:find).with(1).and_return(course)
+        # THE LINE ABOVE DOES NOT WORK AND I DON'T KNOW WHY
+        allow(Course).to receive(:find).and_return(course)
+        allow(course).to receive(:assignments).and_return([assignment])
+        allow(assignment).to receive(:reject).and_return(assignment)
+        allow(course).to receive(:get_participants).and_return([]) #no participants
         params = {id: 1}
         session = {user: instructor}
-        allow(Course).to receive(:find).with(1).and_return(course)
         get :all_students_all_reviews, params, session
         expect(controller.send(:action_allowed?)).to be true
+        expect(response).to redirect_to(:back)
+        expect(flash[:error]).to be_present
       end
     end
   end
@@ -50,8 +64,24 @@ describe Assessment360Controller do
       end
     end
 
-    context 'when course has participants' do
-      # allow(Course).to receive(:find).with('1').and_return(course)
+    context 'method is called' do
+      before(:each) do
+        request.env['HTTP_REFERER'] = 'http://example.com'
+        get "course_student_grade_summary"
+      end
+
+      it 'redirects to back and flashes error as there are no participants' do
+        allow(Course).to receive(:find).and_return(course)
+        allow(course).to receive(:assignments).and_return([assignment])
+        allow(assignment).to receive(:reject).and_return(assignment)
+        allow(course).to receive(:get_participants).and_return([]) #no participants
+        params = {id: 1}
+        session = {user: instructor}
+        get :course_student_grade_summary, params, session
+        expect(controller.send(:action_allowed?)).to be true
+        expect(response).to redirect_to(:back)
+        expect(flash[:error]).to be_present
+      end
     end
   end
 end

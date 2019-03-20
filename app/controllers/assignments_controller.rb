d7508984d17912cd2cd0d3f686221e92ce1212e8
Veeha <vkhanna@ncsu.edu>
2019-03-20 16:00:02 -0400

    Added %w to array declaration

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -9,7 +9,8 @@ class AssignmentsController < ApplicationController
       assignment = Assignment.find(params[:id])
       user_id = current_user.try(:id)
       # either current_user is an super/admin or instructor for the assignment or a TA for the course exists or owner of the Course
-      (['Super-Administrator', 'Administrator'].include? current_role_name) ||
+
+      (%w[Super-Administrator Administrator].include? current_role_name) ||
 
       (assignment.instructor_id == user_id) ||
 

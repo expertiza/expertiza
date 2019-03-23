635c8c1783737ccf4a3b0890787b152230ae3d15
Expertiza Developer <jfwang2@ncsu.edu>
2019-03-23 14:44:31 -0400

    added more tests for first method, adding tests for second method

--- spec/controllers/assessment360_controller_spec.rb
+++ spec/controllers/assessment360_controller_spec.rb
@@ -9,8 +9,13 @@ describe Assessment360Controller do
   let(:assignment) { build(:assignment, id: 1, instructor_id: 6, due_dates: [due_date], microtask: true, staggered_deadline: true) }
   let(:assignment_list) { [assignment] }
   let(:due_date) { build(:assignment_due_date, deadline_type_id: 1) }
-  let(:course_participant) { build(:course_participant) }
+  let(:course_participant) { build(:course_participant, user_id: 1) }
   let(:student1) { build(:student, id: 1, name: :lily) }
+  let(:assignment_with_participants) {build(:assignment, id: 1, name: "test_assignment", instructor_id: 2,
+                                            participants: [build(:participant, id: 1, user_id: 1, assignment: assignment)], course_id: 1)}
+  let(:assignment_with_participants_list) { [assignment_with_participants] }
+  let(:teammate_review) { build(:review_response_map) }
+  let(:meta_review) { build(:meta_review_response_map) }
 
   describe '#all_students_all_reviews' do
     context 'when course does not have participants' do
@@ -59,6 +64,24 @@ describe Assessment360Controller do
         expect(controller.send(:action_allowed?)).to be true
         expect(response.status).to eq(200)
       end
+
+      it 'has participants, next assignment participant exists, but there are no reviews' do
+        allow(Course).to receive(:find).with("1").and_return(course)
+        allow(course).to receive(:assignments).and_return(assignment_with_participants_list)
+        allow(assignment_with_participants_list).to receive(:reject).and_return(assignment_with_participants_list)
+        allow(course).to receive(:get_participants).and_return([course_participant]) #has participants
+        allow(StudentTask).to receive(:teamed_students).with(course_participant.user).and_return(student1)
+        allow(assignment_with_participants.participants).to receive(:find_by).with({:user_id=>course_participant.user_id}).and_return(course_participant)
+        allow(course_participant).to receive(:teammate_reviews).and_return(teammate_review)
+        allow(course_participant).to receive(:metareviews).and_return(meta_review)
+        allow(teammate_review).to receive(:count).and_return(0)
+        allow(meta_review).to receive(:count).and_return(0)
+        params = {course_id: 1}
+        session = {user: instructor}
+        get :all_students_all_reviews, params, session
+        expect(controller.send(:action_allowed?)).to be true
+        expect(response.status).to eq(200)
+      end
     end
   end
 
@@ -89,6 +112,19 @@ describe Assessment360Controller do
         allow(course).to receive(:assignments).and_return([assignment])
         allow(assignment).to receive(:reject).and_return(assignment)
         allow(course).to receive(:get_participants).and_return([]) #no participants
+        params = {course_id: 1}
+        session = {user: instructor}
+        get :course_student_grade_summary, params, session
+        expect(controller.send(:action_allowed?)).to be true
+        expect(response).to redirect_to(:back)
+        expect(flash[:error]).to be_present
+      end
+
+      it 'redirects to back and flashes error as there are no participants' do
+        allow(Course).to receive(:find).with("1").and_return(course)
+        allow(course).to receive(:assignments).and_return([assignment])
+        allow(assignment).to receive(:reject).and_return(assignment)
+        allow(course).to receive(:get_participants).and_return([course_participant]) #has participants
         params = {course_id: 1}
         session = {user: instructor}
         get :course_student_grade_summary, params, session

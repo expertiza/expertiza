6cfb3e785d8f79faeb47d6baa7ccbd846596db8b
Expertiza Developer <jfwang2@ncsu.edu>
2019-03-23 13:34:54 -0400

    increased coverage in first method

--- spec/controllers/assessment360_controller_spec.rb
+++ spec/controllers/assessment360_controller_spec.rb
@@ -5,10 +5,12 @@
 
 describe Assessment360Controller do
   let(:instructor) { build(:instructor, id: 6) }
-  let(:course) { double('Course', instructor_id: 6, path: '/cscs', name: 'abc') }
+  let(:course) { double('Course', instructor_id: 6, path: '/cscs', name: 'abc', id: 1) }
   let(:assignment) { build(:assignment, id: 1, instructor_id: 6, due_dates: [due_date], microtask: true, staggered_deadline: true) }
+  let(:assignment_list) { [assignment] }
   let(:due_date) { build(:assignment_due_date, deadline_type_id: 1) }
   let(:course_participant) { build(:course_participant) }
+  let(:student1) { build(:student, id: 1, name: :lily) }
 
   describe '#all_students_all_reviews' do
     context 'when course does not have participants' do
@@ -34,8 +36,8 @@ describe Assessment360Controller do
 
       it 'redirects to back and flashes error as there are no participants' do
         allow(Course).to receive(:find).with("1").and_return(course)
-        allow(course).to receive(:assignments).and_return([assignment])
-        allow(assignment).to receive(:reject).and_return(assignment)
+        allow(course).to receive(:assignments).and_return(assignment_list)
+        allow(assignment_list).to receive(:reject).and_return(assignment_list)
         allow(course).to receive(:get_participants).and_return([]) #no participants
         params = {course_id: 1}
         session = {user: instructor}
@@ -45,17 +47,17 @@ describe Assessment360Controller do
         expect(flash[:error]).to be_present
       end
 
-      it 'has participants and avoids dividing by zero' do
+      it 'has participants, next assignment participant does not exist, and avoids divide by zero' do
         allow(Course).to receive(:find).with("1").and_return(course)
-        allow(course).to receive(:assignments).and_return([assignment])
-        allow(assignment).to receive(:reject).and_return(assignment)
-        allow(course).to receive(:get_participants).and_return([]) #no participants
+        allow(course).to receive(:assignments).and_return(assignment_list)
+        allow(assignment_list).to receive(:reject).and_return(assignment_list)
+        allow(course).to receive(:get_participants).and_return([course_participant]) #has participants
+        allow(StudentTask).to receive(:teamed_students).with(course_participant.user).and_return(student1)
         params = {course_id: 1}
         session = {user: instructor}
         get :all_students_all_reviews, params, session
         expect(controller.send(:action_allowed?)).to be true
-        expect(response).to redirect_to(:back)
-        expect(flash[:error]).to be_present
+        expect(response.status).to eq(200)
       end
     end
   end

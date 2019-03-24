7d5b1f5ebddec50a9fe345db3c1467e5b41eb54e
George Hugh <gshugh@ncsu.edu>
2019-03-24 19:37:36 -0400

    Finish up Code Climate issues.

--- app/models/text_response.rb
+++ app/models/text_response.rb
@@ -18,7 +18,7 @@ class TextResponse < Question
     #
     # html.html_safe
     content_tag(:tr,
-                content_tag(:td, '<a rel="nofollow" data-method="delete" href="/questions/' + self.id.to_s + '">Remove</a>', {:align => "center"}, false) +
+                content_tag(:td, '<a rel="nofollow" data-method="delete" href="/questions/' + self.id.to_s + '">Remove</a>', {align: "center"}, false) +
                     content_tag(:td, '<input size="6" value="' + self.seq.to_s + '" name="question[' + self.id.to_s + '][seq]" id="question_' +
                         self.id.to_s + '_seq" type="text">', {}, false) +
                     content_tag(:td, '<textarea cols="50" rows="1" name="question[' + self.id.to_s + '][txt]" id="question_' + self.id.to_s +
@@ -27,7 +27,7 @@ class TextResponse < Question
                         '" name="question[' + self.id.to_s + '][type]" id="question_' + self.id.to_s + '_type" type="text">', {}, false) +
                     content_tag(:td, '<!--placeholder (TextRsponse does not need weight)-->', {}, false) +
                     content_tar(:td, 'text area size <input size="6" value="' + self.size.to_s +
-                        '" name="question[' + self.id.to_s + '][size]" id="question_' + self.id.to_s + '_size" type="text">', {}, false),{}, false)
+                        '" name="question[' + self.id.to_s + '][size]" id="question_' + self.id.to_s + '_size" type="text">', {}, false), {}, false)
   end
 
   # This method returns what to display if an instructor (etc.) is viewing a questionnaire

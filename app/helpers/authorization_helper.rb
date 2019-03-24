235e29311974aded39108ffd53b0caba32af050f
gabalmat <gabalmat@ncsu.edu>
2019-03-24 18:22:35 +0900

    E1915. Modified find_assignment_from_response_id based on results from testing

--- app/helpers/authorization_helper.rb
+++ app/helpers/authorization_helper.rb
@@ -130,13 +130,18 @@ module AuthorizationHelper
   # Recursively find an assignment given the passed in Response id. Because a ResponseMap
   # can either point to an Assignment or another Response, recursively search until the
   # ResponseMap object's reviewed_object_id points to an Assignment.
+  # Returns false if the Response with the passed in response_id does not map to
+  # either an Assignment or another Response object.
   def find_assignment_from_response_id(response_id)
     response = Response.find(response_id.to_i)
-    response_map = ResponseMap.find(response.map_id)
-    assignment = Assignment.find(response_map.reviewed_object_id)
-    return assignment unless assignment.nil?
-    return false unless Response.exists?(id: response_map.reviewed_object_id)
-    find_assignment_from_response_id(response_map.reviewed_object_id)
+    response_map = response.response_map
+    if response_map.assignment
+      return response_map.assignment
+    elsif response_map.review_mapping
+      find_assignment_from_response_id(response_map.reviewed_object_id)
+    else
+      false
+    end
   end
 
   def find_assignment_instructor(assignment)

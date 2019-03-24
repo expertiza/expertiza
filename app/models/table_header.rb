8ebab6f93c98c4841d83230596d6011b74abdecf
George Hugh <gshugh@ncsu.edu>
2019-03-23 23:09:09 -0400

    Remove redundant code in table_header.rb.

--- app/models/table_header.rb
+++ app/models/table_header.rb
@@ -1,13 +1,22 @@
+include ActionView::Helpers
+
 class TableHeader < QuestionnaireHeader
   def complete(_count, _answer = nil)
-    html = '<br/><big><b>' + self.txt + '</b></big><br/>'
-    html += '<table class="general" style="border: 2; text-align: left; width: 100%">'
-    html
+    make_html
   end
 
   def view_completed_question(_count, _answer)
-    html = '<br/><big><b>' + self.txt + '</b></big><br/>'
-    html += '<table class="general" style="border: 2; text-align: left; width: 100%">'
-    html
+    make_html
+  end
+
+  def make_html
+    capture do
+      concat tag("br", nil, false, false)
+      concat content_tag(:big, content_tag(:b, self.txt, {}, false), {}, false)
+      concat tag("br", nil, false, false)
+      concat tag("table", {:class => "general", :style => "border: 2; text-align: left; width: 100%"}, true, false)
+    end
+    # html = '<br/><big><b>' + self.txt + '</b></big><br/>'
+    # html += '<table class="general" style="border: 2; text-align: left; width: 100%">'
   end
 end

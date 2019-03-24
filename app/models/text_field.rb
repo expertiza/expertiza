4c06f83350c62862c8dea73172049ea7de10be9c
George Hugh <gshugh@ncsu.edu>
2019-03-24 13:29:21 -0400

    Use the return of the conditional for variable assignment and comparison.

--- app/models/text_field.rb
+++ app/models/text_field.rb
@@ -9,28 +9,28 @@ class TextField < TextResponse
             ' name="responses[' + count.to_s + '][comment]" style="width: 40%;" size=' + self.size.to_s + ' type="text"'
     html += 'value="' + answer.comments.to_s unless answer.nil?
     html += '">'
-    html += '<BR/><BR/>' if self.type == 'TextField' and self.break_before == false
+    html += '<BR/><BR/>' if self.type == 'TextField' and !self.break_before
+    html
   end
 
   def view_completed_question(count, answer)
-    if self.type == 'TextField' and self.break_before
+    html = if self.type == 'TextField' and self.break_before
       # html = '<b>' + count.to_s + ". " + self.txt + "</b>"
       # html += '&nbsp;&nbsp;&nbsp;&nbsp;'
       # html += answer.comments.to_s
       # html += '<BR/><BR/>' if Question.exists?(answer.question_id + 1) && Question.find(answer.question_id + 1).break_before
-      html = capture do
+      capture do
         concat content_tag(:b, count.to_s + ". " + self.txt, {}, false)
         concat '&nbsp;&nbsp;&nbsp;&nbsp;' + answer.comments.to_s
         concat tag('br') if Question.exists?(answer.question_id + 1) && Question.find(answer.question_id + 1).break_before
         concat tag('br') if Question.exists?(answer.question_id + 1) && Question.find(answer.question_id + 1).break_before
       end
     else
-      html = capture do
+      capture do
         concat self.txt + answer.comments
         concat tag("br")
         concat tag("br")
       end
     end
-    html
   end
 end

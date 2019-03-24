9530edc884aae287d32150dd8d379a6c8c88d098
Expertiza Developer <jfwang2@ncsu.edu>
2019-03-24 14:56:09 -0400

    finished testing all lines dealing with first method

--- spec/controllers/assessment360_controller_spec.rb
+++ spec/controllers/assessment360_controller_spec.rb
@@ -14,8 +14,12 @@ describe Assessment360Controller do
   let(:assignment_with_participants) {build(:assignment, id: 1, name: "test_assignment", instructor_id: 2,
                                             participants: [build(:participant, id: 1, user_id: 1, assignment: assignment)], course_id: 1)}
   let(:assignment_with_participants_list) { [assignment_with_participants] }
-  let(:teammate_review) { build(:review_response_map) }
-  let(:meta_review) { build(:meta_review_response_map) }
+  let(:empty_teammate_review) { [] }
+  let(:empty_meta_review) { [] }
+  let(:review1) { double('Review', average_score: 90) }
+  let(:review2) { double('Review', average_score: 100) }
+  let(:teammate_review) { [review1, review2] }
+  let(:meta_review) { [review2] }
 
   describe '#all_students_all_reviews' do
     context 'when course does not have participants' do
@@ -63,6 +67,10 @@ describe Assessment360Controller do
         get :all_students_all_reviews, params, session
         expect(controller.send(:action_allowed?)).to be true
         expect(response.status).to eq(200)
+        returned_teammate_review = controller.instance_variable_get(:@teammate_review)
+        expect(returned_teammate_review[nil]).to eq({})
+        returned_meta_review = controller.instance_variable_get(:@meta_review)
+        expect(returned_meta_review[nil]).to eq({})
       end
 
       it 'has participants, next assignment participant exists, but there are no reviews' do
@@ -72,15 +80,37 @@ describe Assessment360Controller do
         allow(course).to receive(:get_participants).and_return([course_participant]) #has participants
         allow(StudentTask).to receive(:teamed_students).with(course_participant.user).and_return(student1)
         allow(assignment_with_participants.participants).to receive(:find_by).with({:user_id=>course_participant.user_id}).and_return(course_participant)
-        allow(course_participant).to receive(:teammate_reviews).and_return(teammate_review)
-        allow(course_participant).to receive(:metareviews).and_return(meta_review)
-        allow(teammate_review).to receive(:count).and_return(0)
-        allow(meta_review).to receive(:count).and_return(0)
+        allow(course_participant).to receive(:teammate_reviews).and_return(empty_teammate_review)
+        allow(course_participant).to receive(:metareviews).and_return(empty_meta_review)
         params = {course_id: 1}
         session = {user: instructor}
         get :all_students_all_reviews, params, session
         expect(controller.send(:action_allowed?)).to be true
         expect(response.status).to eq(200)
+        returned_teammate_review = controller.instance_variable_get(:@teammate_review)
+        expect(returned_teammate_review[nil]).to eq({})
+        returned_meta_review = controller.instance_variable_get(:@meta_review)
+        expect(returned_meta_review[nil]).to eq({})
+      end
+
+      it 'has participants, next assignment participant exists, but there are reviews' do
+        allow(Course).to receive(:find).with("1").and_return(course)
+        allow(course).to receive(:assignments).and_return(assignment_with_participants_list)
+        allow(assignment_with_participants_list).to receive(:reject).and_return(assignment_with_participants_list)
+        allow(course).to receive(:get_participants).and_return([course_participant]) #has participants
+        allow(StudentTask).to receive(:teamed_students).with(course_participant.user).and_return(student1)
+        allow(assignment_with_participants.participants).to receive(:find_by).with({:user_id=>course_participant.user_id}).and_return(course_participant)
+        allow(course_participant).to receive(:teammate_reviews).and_return(teammate_review)
+        allow(course_participant).to receive(:metareviews).and_return(meta_review)
+        params = {course_id: 1}
+        session = {user: instructor}
+        get :all_students_all_reviews, params, session
+        expect(controller.send(:action_allowed?)).to be true
+        expect(response.status).to eq(200)
+        returned_teammate_review = controller.instance_variable_get(:@teammate_review)
+        expect(returned_teammate_review[nil][1]).to eq("95%")
+        returned_meta_review = controller.instance_variable_get(:@meta_review)
+        expect(returned_meta_review[nil][1]).to eq("100%")
       end
     end
   end

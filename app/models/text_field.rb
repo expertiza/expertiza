4f53c71a82e9fe726973fc795e30af39d92c3163
George Hugh <gshugh@ncsu.edu>
2019-03-24 13:20:22 -0400

    First commit of test_field.

--- app/models/text_field.rb
+++ app/models/text_field.rb
@@ -1,4 +1,6 @@
 class TextField < TextResponse
+  include ActionView::Helpers
+
   def complete(count, answer = nil)
     html = '<p style="width: 80%;">'
     html += '<label for="responses_' + count.to_s + '" >' + self.txt + '&nbsp;&nbsp;</label>'
@@ -8,19 +10,26 @@ class TextField < TextResponse
     html += 'value="' + answer.comments.to_s unless answer.nil?
     html += '">'
     html += '<BR/><BR/>' if self.type == 'TextField' and self.break_before == false
-    html
   end
 
   def view_completed_question(count, answer)
-    if self.type == 'TextField' and self.break_before == true
-      html = '<b>' + count.to_s + ". " + self.txt + "</b>"
-      html += '&nbsp;&nbsp;&nbsp;&nbsp;'
-      html += answer.comments.to_s
-      html += '<BR/><BR/>' if Question.exists?(answer.question_id + 1) && Question.find(answer.question_id + 1).break_before == true
+    if self.type == 'TextField' and self.break_before
+      # html = '<b>' + count.to_s + ". " + self.txt + "</b>"
+      # html += '&nbsp;&nbsp;&nbsp;&nbsp;'
+      # html += answer.comments.to_s
+      # html += '<BR/><BR/>' if Question.exists?(answer.question_id + 1) && Question.find(answer.question_id + 1).break_before
+      html = capture do
+        concat content_tag(:b, count.to_s + ". " + self.txt, {}, false)
+        concat '&nbsp;&nbsp;&nbsp;&nbsp;' + answer.comments.to_s
+        concat tag('br') if Question.exists?(answer.question_id + 1) && Question.find(answer.question_id + 1).break_before
+        concat tag('br') if Question.exists?(answer.question_id + 1) && Question.find(answer.question_id + 1).break_before
+      end
     else
-      html = self.txt
-      html += answer.comments
-      html += '<BR/><BR/>'
+      html = capture do
+        concat self.txt + answer.comments
+        concat tag("br")
+        concat tag("br")
+      end
     end
     html
   end

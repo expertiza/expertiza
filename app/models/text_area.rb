b8256df4553043a5075fcdbf1631bceedea53251
George Hugh <gshugh@ncsu.edu>
2019-03-24 12:29:40 -0400

    Add content_tag commandss to text_area.

--- app/models/text_area.rb
+++ app/models/text_area.rb
@@ -1,4 +1,6 @@
 class TextArea < TextResponse
+  include ActionView::Helpers
+
   def complete(count, answer = nil)
     if self.size.nil?
       cols = '70'
@@ -7,19 +9,30 @@ class TextArea < TextResponse
       cols = self.size.split(',')[0]
       rows = self.size.split(',')[1]
     end
-    html = '<p><label for="responses_' + count.to_s + '">' + self.txt + '</label></p>'
-    html += '<input id="responses_' + count.to_s + '_score" name="responses[' + count.to_s + '][score]" type="hidden" value="">'
-    html += '<p><textarea cols="' + cols + '" rows="' + rows +
-            '" id="responses_' + count.to_s + '_comments" name="responses[' + count.to_s + '][comment]" class="tinymce">'
-    html += answer.comments unless answer.nil?
-    html += '</textarea>'
-    html += '</p>'
-    html
+    text_area_text = answer.comments unless answer.nil?
+    capture do
+      content_tag(:p, content_tag(:label, self.txt, {for: 'responses_' + count.to_s}, false), {}, false)
+      tag(:input, {id: 'responses_' + count.to_s + '_score', name: 'responses[' + count.to_s + '][score]',
+                   type: "hidden", value: ""}, true, false)
+      content_tag(:p,
+                  content_tag(:textarea, text_area_text, {cols: cols, rows: rows, id: 'responses_' + count.to_s + '_comments',
+                                                          name: 'responses[' + count.to_s + '][comment]', class: "tinymce"}, false),
+                  {}, false)
+    end
   end
 
   def view_completed_question(count, answer)
-    html = '<b>' + count.to_s + ". " + self.txt + "</b><BR/>"
-    html += '&nbsp;' * 8 + answer.comments.gsub('^p', '').gsub(/\n/, '<BR/>') + '<BR/><BR/>'
-    html
+    # html = '<b>' + count.to_s + ". " + self.txt + "</b><BR/>"
+    # html += '&nbsp;' * 8 + answer.comments.gsub('^p', '').gsub(/\n/, '<BR/>') + #'<BR/><BR/>'
+    # html
+
+    answer_comments = '&nbsp;' * 8 + answer.comments.gsub('^p', '').gsub(/\n/, '<BR/>')
+    capture do
+      content_tag(:b, count.ts_s + ". " + self.txt, {}, false)
+      content_tag(nil, answer_comments, {}, false)
+      tag("br")
+      tag("br")
+      tag("br")
+    end
   end
 end

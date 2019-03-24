4da1c7bfb82ac0de2e28c773fcc5d3ecc5960a9b
George Hugh <gshugh@ncsu.edu>
2019-03-24 18:00:26 -0400

    Finish adding content_tag and tag.

--- app/models/text_field.rb
+++ app/models/text_field.rb
@@ -2,15 +2,27 @@ class TextField < TextResponse
   include ActionView::Helpers
 
   def complete(count, answer = nil)
-    html = '<p style="width: 80%;">'
-    html += '<label for="responses_' + count.to_s + '" >' + self.txt + '&nbsp;&nbsp;</label>'
-    html += '<input id="responses_' + count.to_s + '_score" name="responses[' + count.to_s + '][score]" type="hidden" value="" ">'
-    html += '<input id="responses_' + count.to_s + '_comments" label=' + self.txt +
-            ' name="responses[' + count.to_s + '][comment]" style="width: 40%;" size=' + self.size.to_s + ' type="text"'
-    html += 'value="' + answer.comments.to_s unless answer.nil?
-    html += '">'
-    html += '<BR/><BR/>' if self.type == 'TextField' and !self.break_before
-    html
+    # html = '<p style="width: 80%;">'
+    # html += '<label for="responses_' + count.to_s + '" >' + self.txt + '&nbsp;&nbsp;</label>'
+    # html += '<input id="responses_' + count.to_s + '_score" name="responses[' + count.to_s + '][score]" type="hidden" value="" ">'
+    # html += '<input id="responses_' + count.to_s + '_comments" label=' + self.txt +
+    #         ' name="responses[' + count.to_s + '][comment]" style="width: 40%;" size=' + self.size.to_s + ' type="text"'
+    # html += 'value="' + answer.comments.to_s unless answer.nil?
+    # html += '">'
+    # html += '<BR/><BR/>' if self.type == 'TextField' and !self.break_before
+    # html
+    answer_comments = answer.comments.to_s unless answer.nil?
+    capture do
+      concat tag(:p, {style: "width: 80%;"}, true, false)
+      concat content_tag(:label, self.txt + '&nbsp;&nbsp;', {for: 'responses_' + count.to_s}, false)
+      concat tag(:input, {id: 'responses_' + count.to_s + '_score',
+                          name: 'responses[' + count.to_s + '][score]', type: "hidden", value: ""}, true, false)
+      concat tag(:input, {id: 'responses_' + count.to_s + '_comments', label: self.txt,
+                          name: 'responses[' + count.to_s + '][comment]', style: "width: 40%;",
+                          size: self.size.to_s, type: "text", value: answer_comments}, true, false)
+      concat tag("br") if self.type == 'TextField' and !self.break_before
+      concat tag("br") if self.type == 'TextField' and !self.break_before
+    end
   end
 
   def view_completed_question(count, answer)

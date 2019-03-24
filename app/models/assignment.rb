3a41c1c9bc8759e6d6fd9477290f4556d8de4b91
Sushan Basnet <sbasnet2@ncsu.edu>
2019-03-23 20:43:14 -0400

    refacotor stage_deadline to make nil return more explicit

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -332,9 +332,9 @@ class Assignment < ActiveRecord::Base
   end
 
   def stage_deadline(topic_id = nil)
-    return 'Unknown' if topic_missing?(topic_id)
-    due_date = find_current_stage(topic_id)
-    due_date.nil? || due_date == 'Finished' ? due_date : due_date.due_at.to_s
+    return 'Unknown' if topic_id.nil? and self.staggered_deadline?
+    return nil if finished?(topic_id)
+    DueDate.get_next_due_date(topic_id).due_at.to_s
   end
 
   def num_review_rounds

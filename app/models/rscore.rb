e876a300c814376f4e8759c2d38a08075e086952
vijayaganesh <mvijayaganesh94@gmail.com>
2019-03-25 07:22:37 -0400

    Code Climate fixes to app/models/rscore.rb

--- app/models/rscore.rb
+++ app/models/rscore.rb
@@ -1,11 +1,5 @@
 class Rscore
   attr_accessor :my_max, :my_min, :my_avg, :my_type
-  def initialize(max = 0.0, min = 0.0, avg = 0.0, type = nil)
-    @my_max = max == -1 ? "N/A" : max
-    @my_min = min == -1 ? "N/A" : min
-    @my_avg = avg == -1 ? "N/A" : avg
-    @my_type = type
-  end
 
   def initialize(my_score, type)
     @my_max = my_score[type][:scores][:max] == -1 ? "N/A" : my_score[type][:scores][:max]

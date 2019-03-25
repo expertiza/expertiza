1ceec18b5c8fc7200c98d776cce189d5972d68fe
vijayaganesh <mvijayaganesh94@gmail.com>
2019-03-25 06:45:02 -0400

    Code Climate fixes to app/models/questionnaire_header.rb

--- app/models/questionnaire_header.rb
+++ app/models/questionnaire_header.rb
@@ -2,13 +2,16 @@ class QuestionnaireHeader < Question
   # This method returns what to display if an instructor (etc.) is creating or editing a questionnaire (questionnaires_controller.rb)
   def edit(_count)
     html = '<tr>'
-    html += '<td align="center"><a rel="nofollow" data-method="delete" href="/questions/' + self.id.to_s + '">Remove</a></td>'
-    html += '<td><input size="6" value="' + self.seq.to_s + '" name="question[' + self.id.to_s + '][seq]" id="question_' + self.id.to_s + '_seq" type="text"></td>'
-    html += '<td><textarea cols="50" rows="1" name="question[' + self.id.to_s + '][txt]" id="question_' + self.id.to_s + '_txt" placeholder="Edit question content here">' + self.txt + '</textarea></td>'
-    html += '<td><input size="10" disabled="disabled" value="' + self.type + '" name="question[' + self.id.to_s + '][type]" id="question_' + self.id.to_s + '_type" type="text">''</td>'
+    html += '<td align="center"><a rel="nofollow" data-method="delete" href="/questions/' + self.id.to_s +
+            '">Remove</a></td>'
+    html += '<td><input size="6" value="' + self.seq.to_s + '" name="question[' + self.id.to_s +
+            '][seq]" id="question_' + self.id.to_s + '_seq" type="text"></td>'
+    html += '<td><textarea cols="50" rows="1" name="question[' + self.id.to_s + '][txt]" id="question_' +
+            self.id.to_s + '_txt" placeholder="Edit question content here">' + self.txt + '</textarea></td>'
+    html += '<td><input size="10" disabled="disabled" value="' + self.type + '" name="question[' + self.id.to_s +
+            '][type]" id="question_' + self.id.to_s + '_type" type="text"></td>'
     html += '<td><!--placeholder (QuestionnaireHeader does not need weight)--></td>'
     html += '</tr>'
-
     html.html_safe
   end
 

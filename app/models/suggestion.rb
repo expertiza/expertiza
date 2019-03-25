b638f4e54d40bdb976d53fd08b02432a84a237c5
uschatto <uschatto@ncsu.edu>
2019-03-24 22:08:06 -0400

    E1917 : Unprotected mass assignment issue

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -1,4 +1,5 @@
 class Suggestion < ActiveRecord::Base
   validates :title, :description, presence: true
   has_many :suggestion_comments
+  attr_accessible :assignment_id, :title, :description, :status, :unityID, :signup_preference
 end

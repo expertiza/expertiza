c1c8587d973ae8c8cdb41dde3c5c6fcaf4944bd3
Srikar <srikarkrishna95@gmail.com>
2019-03-25 16:55:02 -0400

    Req 9) Renamed participants variable to teams

--- app/controllers/sign_up_sheet_controller.rb
+++ app/controllers/sign_up_sheet_controller.rb
@@ -120,7 +120,7 @@ class SignUpSheetController < ApplicationController
     # to treat all assignments as team assignments
     # Though called participants, @participants are actually records in signed_up_teams table, which
     # is a mapping table between teams and topics (waitlisted recored are also counted)
-    @participants = SignedUpTeam.find_team_participants(assignment_id, session[:ip])
+    @teams = SignedUpTeam.find_team_participants(assignment_id, session[:ip])
   end
 
   def set_values_for_new_topic

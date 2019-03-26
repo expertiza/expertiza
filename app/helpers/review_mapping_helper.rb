b780b3d101a8b8556ba3b695a1096259bf2f2a88
Devang Upadhyay <upadhyaydevang004@gmail.com>
2019-03-25 20:04:49 -0400

    comments added for the refactored methods

--- app/helpers/review_mapping_helper.rb
+++ app/helpers/review_mapping_helper.rb
@@ -313,11 +313,8 @@ module ReviewMappingHelper
     html.html_safe
   end
 
-  #
-  # for author feedback report
-  #
-  #
-  # varying rubric by round
+  
+  # gets review and feedback responses for all rounds for the feedback report 
   def get_each_review_and_feedback_response_map(author)
     @team_id = TeamsUser.team_id(@id.to_i, author.user_id)
     # Calculate how many responses one team received from each round
@@ -337,6 +334,7 @@ module ReviewMappingHelper
     @rspan_round_three = @review_responses_round_three.nil? ? 0 : @review_responses_round_three.length
   end
 
+  # gets review and feedback responses for a certain round for the feedback report 
   def get_certain_review_and_feedback_response_map(author)
     @feedback_response_maps = FeedbackResponseMap.where(["reviewed_object_id IN (?) and reviewer_id = ?", @all_review_response_ids, author.id])
     @team_id = TeamsUser.team_id(@id.to_i, author.user_id)

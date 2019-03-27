1ba86c22e80dc27a2622f64107b489b4156822a8
Yuhan Chen <cyhhanna097@gmail.com>
2019-03-27 10:16:20 -0400

    Update popup_controller_spec.rb

--- spec/controllers/popup_controller_spec.rb
+++ spec/controllers/popup_controller_spec.rb
@@ -9,12 +9,10 @@ describe PopupController do
   let(:ta) { build(:teaching_assistant) }
   let(:participant) { build(:participant, id: 1, user_id: 1, user: student, assignment: assignment) }
   let(:participant2) { build(:participant, id: 2, user: student2, assignment: assignment) }
-
   let(:response) { build(:response, id: 1) }
   let(:questionnaire) { build(:questionnaire, id: 1, max_question_score: 15)}
   let(:question) { build(:question, id: 1, questionnaire_id: questionnaire.id)}
   let(:answer) { build(:answer, id: 1, question_id: question.id, response_id: response.id, answer: 10) }
-
   let(:assignment) { build(:assignment, id: 1) }
   let(:response_map) { build(:review_response_map, id: 1, reviewee_id: assignment_team.id, reviewer_id: participant2.id, response: [response], assignment: assignment) }
   let(:final_versions) {{
@@ -22,24 +20,11 @@ describe PopupController do
       review_round_two: {questionnaire_id: 2, response_ids: [2]},
       review_round_three: {questionnaire_id: 3, response_ids: [3], }
   }}
-
   test_url = "http://peerlogic.csc.ncsu.edu/reviewsentiment/viz/478-5hf542"
   mocked_comments_one = OpenStruct.new(comments: "test comment")
-
-  # let(:questionnaire_id) { {questionnaire: {id: 1, name: "Test questionnaire", instructor_id: 1, private: false,
-  #                          min_question_score: 0, max_question_score: 15, created_at: nil, updated_at: nil, type: "ReviewQuestionnaire",
-  #                                           display_type: "Review", instruction_loc: nil}}}
-
-
-  # WEBSERVICE_CONFIG = {
-  #     'heatmap_url' => 'http://peerlogic.csc.ncsu.edu/reviewsentiment/configure',
-  #     'sentiment_webservice_url' => 'http://peerlogic.csc.ncsu.edu/sentiment/'
-  # }
-
   let(:sentiment_summary) {
     [{"sentiments"=>[{"id"=>0, "neg"=>"0.00", "neu"=>"1.00", "pos"=>"0.00", "sentiment"=>"0.00", "text"=>"N/A"}]}]
   }
-
 
   describe '#action_allowed?' do
     context 'when the role name of current user is super admin or admin' do
@@ -48,22 +33,18 @@ describe PopupController do
         expect(controller.send(:action_allowed?)).to be true
       end
     end
-
-
     context 'when the role current user is super instructor' do
       it 'allows certain action' do
         stub_current_user(instructor, instructor.role.name, instructor.role)
         expect(controller.send(:action_allowed?)).to be true
       end
     end
-
     context 'when the role current user is ta' do
       it 'allows certain action' do
         stub_current_user(ta, ta.role.name, ta.role)
         expect(controller.send(:action_allowed?)).to be true
       end
     end
-
     context 'when the role current user is student' do
       it 'does not allow certain action' do
         stub_current_user(student, student.role.name, student.role)
@@ -103,7 +84,6 @@ describe PopupController do
       it 'calculates response stats for one response' do
         session = {user: instructor}
         params = {id: 1, id2: 1}
-
         allow(ResponseMap).to receive(:where).with("1").and_return(response_map)
         allow(assignment).to receive(:num_review_rounds).and_return(1)
         allow(Answer).to receive(:where).with(any_args).and_return([answer])
@@ -113,7 +93,6 @@ describe PopupController do
         allow(response).to receive(:average_score).and_return(90)
         allow(response).to receive(:total_score).and_return(90)
         allow(response).to receive(:maximum_score).and_return(100)
-
         get :team_users_popup, params, session
         expect(controller.instance_variable_get(:@response_round_1)).to eq response
         expect(controller.instance_variable_get(:@response_id_round_1)).to eq 1
@@ -124,7 +103,6 @@ describe PopupController do
         expect(controller.instance_variable_get(:@total_possible_round_1)).to eq 100
       end
     end
-
     context 'when a response map does not exist' do
       it 'finds team and users' do
         session = {user: instructor}
@@ -139,16 +117,13 @@ describe PopupController do
   end
 
   describe '#participants_popup' do
-
     describe 'called with no response' do
       it 'does not calculate scores' do
         params = {id: 1}
         session = {user: instructor}
-
         allow(Participant).to receive(:find).with("1").and_return(participant)
         allow(participant).to receive(:parent_id).and_return(1)
         allow(User).to receive(:find).with(any_args).and_return(instructor)
-
         get :participants_popup, params, session
         expect(controller.instance_variable_get(:@sum)).to eq 0
         expect(controller.instance_variable_get(:@count)).to eq 0
@@ -173,15 +148,12 @@ describe PopupController do
         allow(ResponseMap).to receive(:find).with(any_args).and_return(response_map)
         allow(Assignment).to receive(:find).with(any_args).and_return(assignment)
       end
-
       context 'assignment has no review questionnaire' do
         it 'calculates the scores without a max' do
           params = {id: 1, id2: 1}
           session = {user: instructor}
-
           allow(AssignmentQuestionnaire).to receive(:where).with(any_args).and_return([])
           allow(Answer).to receive(:where).with(any_args).and_return([answer])
-
           get :participants_popup, params, session
           expect(controller.instance_variable_get(:@maxscore)).to eq 0
           expect(controller.instance_variable_get(:@reviewid)).to eq response_map.id
@@ -201,12 +173,10 @@ describe PopupController do
         it 'calculates the scores with a max' do
           params = {id: 1, id2: 1}
           session = {user: instructor}
-
           allow(AssignmentQuestionnaire).to receive(:where).with(any_args).and_return([questionnaire])
           allow(Answer).to receive(:where).with(any_args).and_return([answer])
           allow(questionnaire).to receive(:questionnaire_id).and_return(1)
           allow(Questionnaire).to receive(:find).with(1).and_return(questionnaire)
-
           get :participants_popup, params, session
           expect(controller.instance_variable_get(:@maxscore)).to eq 15
           expect(controller.instance_variable_get(:@revqids)).to eq [questionnaire]
@@ -217,7 +187,6 @@ describe PopupController do
       end
     end
   end
-
   ######### Tone Analysis Tests ##########
   describe "tone analysis tests" do
     before(:each) do
@@ -226,7 +195,6 @@ describe PopupController do
       allow(ReviewResponseMap).to receive(:final_versions_from_reviewer).with("1").and_return(final_versions)
       allow(Answer).to receive(:where).with(any_args).and_return(mocked_comments_one)
       @request.host = test_url
-
       allow(Questionnaire).to receive(:find).with(any_args).and_return(questionnaire)
       allow(Question).to receive(:where).with(:questionnaire_id => questionnaire.id).and_return([question])
     end
@@ -245,9 +213,7 @@ describe PopupController do
     end
 
     describe '#view_review_scores_popup' do
-
       context 'review tone analysis is calculated' do
-
         it 'prepares tone analysis report for building' do
           params = {reviewer_id: 1, assignment_id: 1}
           session = {user: instructor}
@@ -262,12 +228,10 @@ describe PopupController do
   end
 
   describe '#build_tone_analysis_report' do
-
     before(:each) do
       allow(Questionnaire).to receive(:find).with(any_args).and_return(questionnaire)
       allow(Question).to receive(:where).with(:questionnaire_id => questionnaire.id).and_return([question])
     end
-
       describe 'answer is not provided' do
         it 'build tone analysis report' do
           controller.instance_variable_set(:@review_final_versions, final_versions)
@@ -281,14 +245,9 @@ describe PopupController do
           controller.instance_variable_set(:@review_final_versions, final_versions)
           allow(Answer).to receive(:where).with(any_args).and_return([answer])
           controller.send(:build_tone_analysis_report)
-          # controller.send(:build_tone_analysis_report)
-
-          # controller.send(:build_tone_analysis_report)
-          # expect(controller.instance_variable_get(:@sentiment_summary)).to eq nil
         end
       end
   end
-
 
   describe '#build_tone_analysis_heatmap' do
     describe 'sentiment is empyt' do
@@ -298,20 +257,16 @@ describe PopupController do
         allow(ReviewResponseMap).to receive(:final_versions_from_reviewer).with("1").and_return(final_versions)
         allow(Questionnaire).to receive(:find).with(any_args).and_return(questionnaire)
         allow(Question).to receive(:where).with(:questionnaire_id => questionnaire).and_return([question])
-
         controller.instance_variable_set(:@review_final_versions, final_versions)
         controller.instance_variable_set(:@sentiment_summary, sentiment_summary)
         controller.send(:build_tone_analysis_heatmap)
       end
-
       it 'build tone analysis heatmap' do
       end
     end
   end
 
-
   describe '#reviewer_details_popup' do
-    ## INSERT CONTEXT/DESCRIPTION/CODE HERE
     context 'it will show the reviewer details' do
       it 'it will show the reviewer details' do
         params = {assignment_id: 1, id: 1}
@@ -325,7 +280,6 @@ describe PopupController do
   end
 
   describe '#self_review_popup' do
-    ## INSERT CONTEXT/DESCRIPTION/CODE HERE
     context 'when response_id exists' do
       it 'get the result' do
         params = {response_id: 1, user_fullname: 1}

160735aff08be48eb38b35ee1533eb63a4e1bcce
ashishj9820 <ashishj9820@gmail.com>
2019-03-31 15:31:53 -0700

    added tests file for review_mapping_helper

--- spec/features/helpers/review_mapping_helper_spec.rb
+++ spec/features/helpers/review_mapping_helper_spec.rb
@@ -1,7 +1,5 @@
-require 'rspec'
-
 describe 'My Test Cases' do
-  it "can display review metrics", js: true do
+  before(:each) do
     create(:instructor)
     create(:role_of_student)
     login_as 'instructor6'
@@ -9,28 +7,26 @@ describe 'My Test Cases' do
     click_link 'View reports'
     expect(page).to have_current_path('/reports/response_report')
     click_link 'View'
+  end
+
+  it "can display review metrics", js: true do
     expect(page).to have_content('Metrics')
   end
 
   it "can display review grades of each round", js: true do
-    create(:instructor)
-    create(:role_of_student)
-    login_as 'instructor6'
-    visit '/tree_display/list'
-    click_link 'View reports'
-    expect(page).to have_current_path('/reports/response_report')
-    click_link 'View'
     expect(page).to have_content('Score awarded')
   end
 
   it "can display review summary", js: true do
-    create(:instructor)
-    create(:role_of_student)
-    login_as 'instructor6'
-    visit '/tree_display/list'
-    click_link 'View reports'
-    expect(page).to have_current_path('/reports/response_report')
-    click_link 'View'
     expect(page).to have_content('Reviews done')
   end
+
+  it "can display review summary", js: true do
+    expect(page).to have_content('Reviewer')
+  end
+
+  it "can display review summary", js: true do
+    expect(page).to have_content('Team reviewed')
+  end
 end
+

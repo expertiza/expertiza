c1ce4338d1c1a51d8200bf4de847606782c09f6e
arjunptm <arjunptm@gmail.com>
2019-03-31 22:10:59 -0400

    removed redundant test case

--- spec/models/criterion_spec.rb
+++ spec/models/criterion_spec.rb
@@ -3,13 +3,7 @@ describe "criterion" do
   let(:criterion) { Criterion.new id: 1, type: "Criterion", seq: 1.0, txt: "test txt", weight: 1, questionnaire: questionnaire }
   let(:answer) { Answer.new answer: 8 }
 
-  describe "#complete" do
-    it "returns the html " do
-      html = criterion.complete(0, nil, 0, 5).to_s
-      expect(html).to eq("<div><label for=\"responses_0\">test txt</label></div>")
-    end
-  end
-  #
+  
   describe "#view_completed_question" do
     it "returns the html " do
       html = criterion.view_completed_question(0, answer, 5).to_s

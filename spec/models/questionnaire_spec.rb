8b7684865bf2ea1d0e23b74c7783cd8e09c2a377
Dharmang Bhavsar <dharmangb@gmail.com>
2019-03-31 23:59:43 -0400

    Code Climate changes.

--- spec/models/questionnaire_spec.rb
+++ spec/models/questionnaire_spec.rb
@@ -56,7 +56,7 @@ describe Questionnaire do
     end
   end
 
-  it "allowing calls from copy_questionnaire_details" do 
+  it "allowing calls from copy_questionnaire_details" do
     allow(Questionnaire).to receive(:find).with('1').and_return(questionnaire)
     allow(Question).to receive(:where).with(questionnaire_id: '1').and_return([Question])
     question_advice = build(:question_advice)

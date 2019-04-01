ee1fa7de83ee7b2a04f4b8565303359baa0c88ec
ashishj9820 <ashishj9820@gmail.com>
2019-04-01 13:53:11 -0700

    test spec added for review_mapping_helper.rb

---
+++ spec/features/review_mapping_helper_spec.rb
@@ -0,0 +1,91 @@
+require 'rspec'
+
+describe 'Tests Review report' do
+
+  before(:each) do
+    create(:instructor)
+    create(:assignment, course: nil, name: 'Test Assignment')
+    assignment_id = Assignment.where(name: 'Test Assignment')[0].id
+    login_as 'instructor6'
+    visit "/reports/response_report?id=#{assignment_id}"
+    click_on 'View'
+  end
+
+  it "can display review metrics" do
+    expect(page).to have_content('Metrics')
+  end
+
+  it "can display review grades of each round" do
+    expect(page).to have_content('Score awarded')
+  end
+
+  it "can display reviews done" do
+    expect(page).to have_content('Reviews done')
+  end
+
+  it "can display Reviewer" do
+    expect(page).to have_content('Reviewer')
+  end
+
+  it "can display team reviewed" do
+    expect(page).to have_content('Team reviewed')
+  end
+end
+
+describe "Test Author feedback report" do
+  before(:each) do
+    create(:instructor)
+    create(:assignment, course: nil, name: 'Test Assignment')
+    assignment_id = Assignment.where(name: 'Test Assignment')[0].id
+    login_as 'instructor6'
+    visit "/reports/response_report?id=#{assignment_id}"
+    page.select("Author feedback report", :from => "report[type]")
+    click_button "View"
+  end
+
+  it "can display rejoinder" do
+    expect(page).to have_content('Rejoinder')
+  end
+
+  it "can display author feedbacks done"  do
+    expect(page).to have_content('author feedbacks done' )
+  end
+
+  it "can display Review response rejoined"  do
+    expect(page).to have_content('Review response rejoined ')
+  end
+
+  it "can display Last rejoined at"  do
+    expect(page).to have_content('Last rejoined at')
+  end
+
+end
+
+describe "Test Teammate Review report" do
+  before(:each) do
+    create(:instructor)
+    create(:assignment, course: nil, name: 'Test Assignment')
+    assignment_id = Assignment.where(name: 'Test Assignment')[0].id
+    login_as 'instructor6'
+    visit "/reports/response_report?id=#{assignment_id}"
+    page.select("Teammate review report", :from => "report[type]")
+    click_button "View"
+  end
+
+  it "can display Reviewer" do
+    expect(page).to have_content('Reviewer')
+  end
+
+  it "can display teammate reviews done"  do
+    expect(page).to have_content('teammate reviews done' )
+  end
+
+  it "can display Teammate reviewed"  do
+    expect(page).to have_content('Teammate reviewed')
+  end
+
+  it "can display Last reviewed at"  do
+    expect(page).to have_content('Last reviewed at')
+  end
+
+end

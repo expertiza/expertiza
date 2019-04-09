1885d04e87a16625a5925b51c63b3cb3320a68e2
Ashwin Risbood <ashwinrisbood@gmail.com>
2019-04-09 14:29:54 -0400

    Update axios-instance.js

--- client/src/axios-instance.js
+++ client/src/axios-instance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL : 'http://localhost:3001/api/v1/'
+    baseURL : 'http://152.46.17.203:3001/api/v1/'
 });
 
 export default instance;

93e72e59cdc8a250905ff3453f0e157b2eadf095
Kunal Narang <kunalnarang1994@gmail.com>
2019-04-11 19:43:42 -0400

    Fix error for review_bids migration

--- db/migrate/20190404173615_create_review_bids.rb
+++ db/migrate/20190404173615_create_review_bids.rb
@@ -1,10 +1,9 @@
 class CreateReviewBids < ActiveRecord::Migration
   def change
     create_table :review_bids do |t|
-      t.integer :topic_id
-      t.integer :student_id
-      t.integer :priority, null => false
-
+      t.belongs_to :team
+      t.belongs_to :participant
+      t.integer	   :priority,   :null => false
       t.timestamps null: false
     end
   end

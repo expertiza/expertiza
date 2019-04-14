162a8184dbd95377b66ba03df6c33573f33eedb5
jwboykin <jwboykin@ncsu.edu>
2019-04-14 18:29:06 -0400

    Add assignment finished? helper method.

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -609,4 +609,9 @@ class Assignment < ActiveRecord::Base
   def find_due_dates(type)
     self.due_dates.select {|due_date| due_date.deadline_type_id == DeadlineType.find_by(name: type).id }
   end
+
+  # New function to check if the assignment is finished
+  def finished?( topic_id = nil )
+    DueDate.get_next_due_date(self.id, topic_id).nil?
+  end
 end

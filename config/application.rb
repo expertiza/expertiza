e24d102faa4ef109ad67afac77dde9e702ee1acd
Mandhani <mkushal@ncsu.edu>
2019-04-18 16:38:17 -0400

    changed application.rb

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,12 @@ module Expertiza
       # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
       # config.i18n.default_locale = :de
 
+       # locales available for application
+      config.i18n.available_locales = [:en,:hi_IN]
+
+       # setting default locale to :en(english)
+      config.i18n.default_locale = :en
+
       # Do not swallow errors in after_commit/after_rollback callbacks.
       config.active_record.raise_in_transactional_callbacks = true
     end

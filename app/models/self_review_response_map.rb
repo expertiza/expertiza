e8fc336ac8207dbe9507f08a85722946bb50c8d7
Aurora Tiffany-Davis <aurora.tdavis@gmail.com>
2019-04-19 15:44:39 -0700

    E1936: Extend review response map  model #questionnaire
    - Also accept optional argument topic_id
    - Added to rspec to test topic_id functionality
    - Also handle case when no questionnaire can be found
    - Added to rspec to test case when no questionnaire can be found

--- app/models/self_review_response_map.rb
+++ app/models/self_review_response_map.rb
@@ -4,13 +4,18 @@ class SelfReviewResponseMap < ResponseMap
   belongs_to :reviewee, class_name: 'Team', foreign_key: 'reviewee_id'
   belongs_to :assignment, class_name: 'Assignment', foreign_key: 'reviewed_object_id'
 
-  # This method is used to get questionnaire for self-review to be performed by user
-  def questionnaire(round)
-    if self.assignment.varying_rubrics_by_round?
-      Questionnaire.find(self.assignment.review_questionnaire_id(round))
-    else
-      Questionnaire.find(self.assignment.review_questionnaire_id)
+  # Find a review questionnaire associated with this self-review response map's assignment
+  # For more details please see method description for assignment.review_questionnaire_id()
+  def questionnaire(round_number = nil, topic_id = nil)
+    # Override arguments if they are non-sensical
+    unless self.assignment.varying_rubrics_by_round?
+      round_number = nil
     end
+    unless self.assignment.varying_rubrics_by_topic?
+      topic_id = nil
+    end
+    # Use find_by() instead of find() in case the review questionnaire id is nil
+    Questionnaire.find_by(id: self.assignment.review_questionnaire_id(round_number, topic_id))
   end
 
   # This method helps to find contributor - here Team ID
--- spec/models/self_review_response_map_spec.rb
+++ spec/models/self_review_response_map_spec.rb
@@ -5,20 +5,37 @@ describe SelfReviewResponseMap do
     # This method is little more than a wrapper for assignment.review_questionnaire_id()
     # So it will be tested relatively lightly
     # We want to know how it responds to the combinations of various arguments it could receive
+    # We want to know how it responds if no questionnaire can be found
 
-    it "returns the correct questionnaire when round not given" do
+    it "returns the correct questionnaire when round given, topic given" do
       # create multiple questionnaires and assignment_questionnaires,
       # for confidence that correct questionnaire is returned
       assignment = create(:assignment)
       self_review_response_map = create(:self_review_response_map, assignment: assignment)
       questionnaire_1 = create(:questionnaire)
       questionnaire_2 = create(:questionnaire)
-      create(:assignment_questionnaire, assignment: assignment, questionnaire: questionnaire_1, used_in_round: 1)
-      create(:assignment_questionnaire, assignment: assignment, questionnaire: questionnaire_2, used_in_round: 2)
-      expect(self_review_response_map.questionnaire(nil).id).to eql questionnaire_1.id
+      questionnaire_3 = create(:questionnaire)
+      questionnaire_4 = create(:questionnaire)
+      create(:assignment_questionnaire, assignment: assignment, questionnaire: questionnaire_1, used_in_round: 1, topic_id: 1)
+      create(:assignment_questionnaire, assignment: assignment, questionnaire: questionnaire_2, used_in_round: 1, topic_id: 2)
+      create(:assignment_questionnaire, assignment: assignment, questionnaire: questionnaire_3, used_in_round: 2, topic_id: 1)
+      create(:assignment_questionnaire, assignment: assignment, questionnaire: questionnaire_4, used_in_round: 2, topic_id: 2)
+      expect(self_review_response_map.questionnaire(2, 2).id).to eql questionnaire_4.id
     end
 
-    it "returns the correct questionnaire when round given" do
+    it "returns the correct questionnaire when round not given, topic given" do
+      # create multiple questionnaires and assignment_questionnaires,
+      # for confidence that correct questionnaire is returned
+      assignment = create(:assignment)
+      self_review_response_map = create(:self_review_response_map, assignment: assignment)
+      questionnaire_1 = create(:questionnaire)
+      questionnaire_2 = create(:questionnaire)
+      create(:assignment_questionnaire, assignment: assignment, questionnaire: questionnaire_1, topic_id: 1)
+      create(:assignment_questionnaire, assignment: assignment, questionnaire: questionnaire_2, topic_id: 2)
+      expect(self_review_response_map.questionnaire(nil, 2).id).to eql questionnaire_2.id
+    end
+
+    it "returns the correct questionnaire when round given, topic not given" do
       # create multiple questionnaires and assignment_questionnaires,
       # for confidence that correct questionnaire is returned
       assignment = create(:assignment)
@@ -27,7 +44,25 @@ describe SelfReviewResponseMap do
       questionnaire_2 = create(:questionnaire)
       create(:assignment_questionnaire, assignment: assignment, questionnaire: questionnaire_1, used_in_round: 1)
       create(:assignment_questionnaire, assignment: assignment, questionnaire: questionnaire_2, used_in_round: 2)
-      expect(self_review_response_map.questionnaire(2).id).to eql questionnaire_2.id
+      expect(self_review_response_map.questionnaire(2, nil).id).to eql questionnaire_2.id
+    end
+
+    it "returns the correct questionnaire when round number not given, topic not given" do
+      # create multiple questionnaires and assignment_questionnaires,
+      # for confidence that correct questionnaire is returned
+      assignment = create(:assignment)
+      self_review_response_map = create(:self_review_response_map, assignment: assignment)
+      questionnaire_1 = create(:questionnaire)
+      questionnaire_2 = create(:questionnaire)
+      create(:assignment_questionnaire, assignment: assignment, questionnaire: questionnaire_1, used_in_round: 1)
+      create(:assignment_questionnaire, assignment: assignment, questionnaire: questionnaire_2, used_in_round: 2)
+      expect(self_review_response_map.questionnaire(nil, nil).id).to eql questionnaire_1.id
+    end
+
+    it "returns nil when no questionnaire can be found" do
+      assignment = create(:assignment)
+      self_review_response_map = create(:self_review_response_map, assignment: assignment)
+      expect(self_review_response_map.questionnaire(2, nil)).to be_nil
     end
 
   end

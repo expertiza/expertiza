73ab0edd04f401d74dfd600b38dd5a8dd39f21ea
Pratik Jain <pjain22@ncsu.edu>
2019-04-20 17:53:14 -0400

    Updated the schema to add the sentiment score and the overall sentiment score

--- db/migrate/20190420151900_add_suggestion_chance_percentage_to_responses.rb
+++ db/migrate/20190420151900_add_suggestion_chance_percentage_to_responses.rb
@@ -1,5 +1,7 @@
 class AddSuggestionChancePercentageToResponses < ActiveRecord::Migration
   def change
     add_column :responses, :suggestion_chance_percentage, :integer
+    add_column :responses, :suggestion_sentiment_score, :float
+    add_column :responses, :overall_tone, :string
   end
 end

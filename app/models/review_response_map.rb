8cdb1fe5584c8111bdd3f284b3c607cda022aaf7
Aurora Tiffany-Davis <aurora.tdavis@gmail.com>
2019-04-21 13:45:44 -0700

    E1936: Add "TODO" comment for out of scope needed change
    - IMO there is a downstream effect of #1936 that is just not feasible for us to tackle
    - Commented this in the code
    - Contacted our mentor

--- app/models/review_response_map.rb
+++ app/models/review_response_map.rb
@@ -96,7 +96,7 @@ class ReviewResponseMap < ResponseMap
     responses
   end
 
-  # wrap lastest version of responses in each response map, together withe the questionnaire_id
+  # wrap latest version of responses in each response map, together with the questionnaire_id
   # will be used to display the reviewer summary
   def self.final_versions_from_reviewer(reviewer_id)
     maps = ReviewResponseMap.where(reviewer_id: reviewer_id)
@@ -153,7 +153,13 @@ class ReviewResponseMap < ResponseMap
                ("review round" + round.to_s).to_sym
              end
     review_final_versions[symbol] = {}
-    # TODO E1936 review_questionnaire_id method signature has changed - need to change call to review_questionnaire_id here?
+    # TODO E1936
+    # TODO review_questionnaire_id method signature has changed
+    # TODO need to change call to review_questionnaire_id here
+    # TODO cannot do this as part of this project's scope
+    # TODO the structure of the output (assumes only 1 questionnaire per round) has to change
+    # TODO and this change has to bubble all the way up to tone analysis, heatmaps, and review scores pop-up
+    # TODO this is a vary-by-topic redesign project all on its own
     review_final_versions[symbol][:questionnaire_id] = assignment.review_questionnaire_id(round)
     response_ids = []
     maps.each do |map|

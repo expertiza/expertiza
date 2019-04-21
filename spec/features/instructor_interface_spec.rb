0c931f72c5a8975e2a6502e5ffff10057abd3518
arjun-0896 <asivaga@ncsu.edu>
2019-04-21 14:41:47 -0400

    Feature for saving submission and review grade added

--- spec/features/instructor_interface_spec.rb
+++ spec/features/instructor_interface_spec.rb
@@ -1,3 +1,4 @@
+
 include InstructorInterfaceHelperSpec
 
 describe "Integration tests for instructor interface" do
@@ -36,6 +37,36 @@ describe "Integration tests for instructor interface" do
     end
   end
 
+  describe "View Submision Grading History" do
+    it 'should display submission grading history' do
+      login_as("instructor6")
+      visit '/grades/view_team?id=31971'
+      fill_in "instructor_id", with: ''
+      fill_in "assignment_id", with: ''
+      fill_in "grading_type", with: ''
+      fill_in "grade_receiver_id", with: ''
+      fill_in "grade", with: ''
+      fill_in "comment", with: 'public submission comment for test'
+      click_button "Save"
+      expect(GradingHistory.where(comment: "public submission comment for test")).to exist
+    end
+  end
+
+  describe "View Review Grading History" do
+    it 'should display review grading history' do
+      login_as("instructor6")
+      visit '/grades/view_team?id=31971'
+      fill_in "instructor_id", with: ''
+      fill_in "assignment_id", with: ''
+      fill_in "grading_type", with: ''
+      fill_in "grade_receiver_id", with: ''
+      fill_in "grade", with: ''
+      fill_in "comment", with: 'public review comment for test'
+      click_button "Save"
+      expect(GradingHistory.where(comment:"public review comment for test")).to exist
+    end
+  end
+
   describe "View Publishing Rights" do
     it 'should display teams for assignment without topic' do
       login_as("instructor6")
@@ -45,33 +76,6 @@ describe "Integration tests for instructor interface" do
     end
   end
 
-  # E1776 (Fall 2017)
-  #
-  # The tests below are no longer reflective of the current import process for topics.
-  #
-  # 1. There is now an additional intermediary page during the import process.
-  # 2. There are now checkbox options on the initial import page to specify optional columns.
-  # 3. The intermediary data structures for imports have changed (see the pull request notes).
-  # 4. The new import process expects all rows in a file to have the same number of columns.
-  #    That is, it expects optional columns to be common across all rows within the same file.
-  #
-  # describe "Import tests for assignment topics" do
-  #   it 'should be valid file with 3 columns' do
-  #     validate_login_and_page_content("spec/features/assignment_topic_csvs/3-col-valid_topics_import.csv", %w(expertiza mozilla), true)
-  #   end
-  #
-  #   it 'should be a valid file with 3 or more columns' do
-  #     validate_login_and_page_content("spec/features/assignment_topic_csvs/3or4-col-valid_topics_import.csv", %w(capybara cucumber), true)
-  #   end
-  #
-  #   it 'should be a invalid csv file' do
-  #     validate_login_and_page_content("spec/features/assignment_topic_csvs/invalid_topics_import.csv", %w(airtable devise), false)
-  #   end
-  #
-  #   it 'should be an random text file' do
-  #     validate_login_and_page_content("spec/features/assignment_topic_csvs/random.txt", ['this is a random file which should fail'], false)
-  #   end
-  # end
 
   describe "View assignment scores" do
     it 'is able to view scores' do

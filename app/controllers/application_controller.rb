f29ba4c8053bb79e63c5d04334b0af4e344a8379
Mandhani <mkushal@ncsu.edu>
2019-04-23 17:37:39 -0400

    Fixed missing params issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -62,7 +62,7 @@ class ApplicationController < ActionController::Base
     I18n.locale = params[:locale] || I18n.default_locale
   end
 
-   def default_url_options(options={})
+  def default_url_options(options={})
     {locale: I18n.locale}.merge options
   end
 

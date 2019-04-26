9d533735b8197e56852370e2b0aa4972f8864d00
arjun-0896 <asivaga@ncsu.edu>
2019-04-26 16:07:23 -0400

    tests params need to be fixed

--- spec/controllers/grading_histories_controller_test.rb
+++ spec/controllers/grading_histories_controller_test.rb
@@ -2,7 +2,7 @@ describe GradingHistoriesController do
   describe '#index' do
     context 'when show review grading history for paticipant' do
       it 'gets history for the paticipant and renders grading_history#index page' do
-        allow(GradingHistory).to receive(:where).with(grade_receiver_id: 1)
+        allow(GradingHistory).to receive(:where).with(grade_receiver_id: 772)
         params = {grade_receiver_id: 1, grade_type:"Submission"}
         get :index, params
         expect(response).to redirect_to('grading_histories?grade_receiver_id=1&grade_type=Submission')

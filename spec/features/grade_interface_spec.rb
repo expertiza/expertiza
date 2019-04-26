ee0528a79d3af9f456d734af7d057e13b5d87996
cladkins <cladkins@ncsu.edu>
2019-04-26 19:31:47 -0400

    updated test database to add second assignment

--- spec/features/grade_interface_spec.rb
+++ spec/features/grade_interface_spec.rb
@@ -1,8 +1,6 @@
 require 'database_cleaner'
-require 'byebug'
 
 describe 'Integration tests for viewing grades: ', js: true do
-# describe 'Integration tests for viewing grades: ' do
   before(:context) do
     DatabaseCleaner.strategy = :truncation, {:pre_count => true, :reset_ids => true}
     DatabaseCleaner.start
@@ -29,14 +27,22 @@ describe 'Integration tests for viewing grades: ', js: true do
     end
     }
 
-    #add assignments
-    let!(:assignment1) {create :assignment, name: "final123"}
     let!(:deadline_right1) {
       rights = %w(NO LATE OK)
       rights.each {|right| create :deadline_right, name: right}
     }
-    let!(:assignment_due_date1) {create :assignment_due_date}
+
+
+    #add assignments 1
+    let!(:assignment1) {create :assignment, name: "final123"}
     let!(:assignment_team1) {2.times {create :assignment_team}}
+    let!(:assignment_due_date1) {create :assignment_due_date}
+
+    #add assignments 1
+    let!(:assignment2) {create :assignment, name: "final456"}
+    let!(:assignment_team2) {2.times {create :assignment_team}}
+    let!(:assignment_due_date2) {create(:assignment_due_date, assignment: assignment2)}
+
 
     # add users to teams
     let!(:team_user1) {
@@ -48,20 +54,25 @@ describe 'Integration tests for viewing grades: ', js: true do
         end
       end}
 
+    #add participants
     let!(:participant1) {
-      user_id = 2
-      4.times do
-        create :participant, user_id: user_id
-        user_id += 1
+      2.times do |assignment_id|
+        user_id = 2
+        4.times do
+          create :participant, user_id: user_id, assignment: Assignment.find(assignment_id +1)
+          user_id += 1
+        end
       end}
 
-
+    #add response_maps
     let!(:review_response_map1) {
-      participant_id = 4
-      2.times do |team_id|
-        2.times do
-          create(:review_response_map, reviewee: AssignmentTeam.find(team_id + 1), reviewer: AssignmentParticipant.find(participant_id))
-          participant_id -= 1
+      (1..2).each do |assignment_id|
+        participant_id = 4
+        (1..2).each do |team_id|
+          2.times do
+            create :review_response_map, reviewee: AssignmentTeam.find(team_id), reviewer: AssignmentParticipant.find(participant_id), assignment: Assignment.find(assignment_id)
+            participant_id -= 1
+          end
         end
       end}
 
@@ -69,9 +80,11 @@ describe 'Integration tests for viewing grades: ', js: true do
     let!(:q_aire) {
       2.times {|i| create(:questionnaire, name: "Test questionnarie#{i + 1}")}
     }
-
+    #assignment_questionnaires
     let!(:assignment1_q_aire) {
-      2.times {|round| create(:assignment_questionnaire, used_in_round: round + 1)}
+      (1..2).each do |assignment_id|
+        (1..2).each {|round| create(:assignment_questionnaire, used_in_round: round, assignment: Assignment.find(assignment_id))}
+      end
     }
 
     let!(:questions) {5.times {create :question}}
@@ -81,28 +94,32 @@ describe 'Integration tests for viewing grades: ', js: true do
     let!(:assignment1_tq_aire) {create(:assignment_questionnaire, questionnaire: t_aire)}
     let!(:teammate_questions) {5.times {create :question, questionnaire: t_aire}}
 
-
-
+    #add question_advice
     let!(:q_advice) {create :question_advice}
 
-
+    #add responses
     let!(:data_reponse) {
-      2.times do |round|
-        4.times do |map_id|
-          5.times do
-            create(:response, is_submitted: true, response_map: ReviewResponseMap.find(map_id + 1), round: round + 1)
+      (1..2).each do #assignments
+        response_review_index = 1
+        (1..2).each do |round|
+          (1..4).each do # number of users seeded
+            create :response, is_submitted: true, response_map: ReviewResponseMap.find(response_review_index), round: round
+            response_review_index += 1
           end
         end
       end}
 
     #answers
     let!(:answers1) {
-      response_id = 1
-      2.times do #round
-        5.times do |i|
-          4.times do |ii|
-            create(:answers, question: Question.find(i + 1), answer: 1 + ii, response: Response.find(response_id))
+      (1..2).each do #assignments
+        response_id = 0
+        (1..2).each do #round
+          (1..4).each do |ii| # number of users seeded
             response_id += 1
+            (1..5).each do |i| #number of questions
+              create(:answers, question: Question.find(i), answer: ii, response: Response.find(response_id))
+
+            end
           end
         end
       end}
@@ -110,44 +127,41 @@ describe 'Integration tests for viewing grades: ', js: true do
     #ScoreView
     let!(:score_view1) {
       index = 1
-      2.times do |round|
-        5.times do |question_id|
-          4.times do
-            create(:score_view,
-                   question_weight: 1,
-                   type: "Criterion",
-                   q1_id: 1,
-                   q1_name: "q_aire.first.name",
-                   q1_instructor_id: 2,
-                   q1_private: 0,
-                   q1_min_question_score: 0,
-                   q1_max_question_score: 5,
-                   q1_type: "ReviewResponseMap",
-                   q1_display_type: "Review",
-                   ques_id: question_id + 1,
-                   ques_questionnaire_id: 1,
-                   s_id: index,
-                   s_question_id: question_id + 1,
-                   s_score: Answer.find(index).answer,
-                   s_comments: nil,
-                   s_response_id: index
-            )
-            index += 1
+      (1..2).each do #assignments
+        2.times do |round|
+          5.times do |question_id|
+            4.times do
+              create(:score_view,
+                     question_weight: 1,
+                     type: "Criterion",
+                     q1_id: 1,
+                     q1_name: "q_aire.first.name",
+                     q1_instructor_id: 2,
+                     q1_private: 0,
+                     q1_min_question_score: 0,
+                     q1_max_question_score: 5,
+                     q1_type: "ReviewResponseMap",
+                     q1_display_type: "Review",
+                     ques_id: question_id + 1,
+                     ques_questionnaire_id: 1,
+                     s_id: index,
+                     s_question_id: question_id + 1,
+                     s_score: Answer.find(index).answer,
+                     s_comments: nil,
+                     s_response_id: index
+              )
+              index += 1
+            end
           end
         end
       end}
 
-    # it 'is visible' do
-      it 'is visible', :driver => :selenium_chrome do
-        Capybara.page.driver.browser.manage.window.maximize
-        Capybara.default_max_wait_time = 600
-
-      # byebug
+    it 'is visible', :driver => :selenium_chrome_headless do
+      Capybara.page.driver.browser.manage.window.maximize
+      Capybara.default_max_wait_time = 90
 
       login_as("instructor6")
       visit view_grades_path(:id => assignment1.id)
-
-      # byebug
 
       expect(page).to have_selector('#chart_div', visible: true)
     end

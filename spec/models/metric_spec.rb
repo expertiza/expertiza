e5190d7d0733d710bf939c12ace7a5f1ec1f6e18
Pratik Jain <pjain22@ncsu.edu>
2019-04-26 18:54:00 -0400

    Adding extra mocks for testing average calculation

--- spec/models/metric_spec.rb
+++ spec/models/metric_spec.rb
@@ -1,10 +1,14 @@
 describe Metric do
   let(:participant) { build(:participant, id: 1, user: build(:student, name: 'no name', fullname: 'no one')) } 
   let(:participant2) { build(:participant, id: 2) }
-  let(:assignment) { build(:assignment, id: 1, name: 'Test Assgt') }
+  let(:assignment1) { build(:assignment, id: 1, name: 'Test Assgt') }
+  let(:assignment2) { build(:assignment, id: 2, name: 'Test Assgt Nil') }
   let(:team) { build(:assignment_team) }
-  let(:review_response_map) { build(:review_response_map, assignment: assignment, reviewer: participant, reviewee: team) }
-  let(:response) { build(:response, id: 1, map_id: 1, response_map: review_response_map, scores: [answer]) }
+  let(:review_response_map1) { build(:review_response_map, assignment: assignment1, reviewer: participant, reviewee: team) }
+  let(:review_response_map2) { build(:review_response_map, assignment: assignment2, reviewer: participant, reviewee: team) }
+  let(:response1) { build(:response, id: 2, map_id: 1, response_map: review_response_map1, suggestion_chance_percentage: 0.3) }
+  let(:response2) { build(:response, id: 3, map_id: 1, response_map: review_response_map1, suggestion_chance_percentage: 0.5) }
+  let(:response3) { build(:response, id: 1, map_id: 2, response_map: review_response_map2, suggestion_chance_average: nil) }
 
   describe '#suggestion_chance_average' do
   end

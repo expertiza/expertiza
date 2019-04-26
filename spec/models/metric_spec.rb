20c263d4ec84b577a07d34ecd343834c735dc532
Pratik Jain <pjain22@ncsu.edu>
2019-04-26 18:37:05 -0400

    Added test for snetiment test function

--- spec/models/metric_spec.rb
+++ spec/models/metric_spec.rb
@@ -6,14 +6,27 @@ describe Metric do
   let(:review_response_map) { build(:review_response_map, assignment: assignment, reviewer: participant, reviewee: team) }
   let(:response) { build(:response, id: 1, map_id: 1, response_map: review_response_map, scores: [answer]) }
 
-
-  describe '#update_suggestion_chance' do
-  end
-
   describe '#suggestion_chance_average' do
   end
 
   describe '#get_sentiment_text' do
+    context 'when avg_sentiment_for_response is -0.4' do
+      it 'returns Negative' do
+        expect(metric.get_sentiment_text(-0.4)).to eq("Negative")
+      end
+    end
+      
+    context 'when avg_sentiment_for_response is 0' do
+      it 'returns Neutral' do
+        expect(metric.get_sentiment_text(0)).to eq("Neutral")
+      end
+    end
+
+    context 'when avg_sentiment_for_response is 0.4' do
+      it 'returns Positive' do
+        expect(metric.get_sentiment_text(0.4)).to eq("Positive")
+      end
+    end
   end
 
 end

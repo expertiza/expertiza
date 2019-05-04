c08c65a382bd9cad39f3460b32a3bb70bee8c3f0
Kunal Narang <kunalnarang1994@gmail.com>
2019-05-04 18:16:20 -0400

    Fix end issue

--- app/controllers/student_review_controller.rb
+++ app/controllers/student_review_controller.rb
@@ -75,14 +75,11 @@ class StudentReviewController < ApplicationController
     @bids.each do |bid|
           signed_up_topics << bid
         end
-      end
-    end
     # signed_up_topics &= @bids
     # @bids -= signed_up_topics
     @selected_topics = signed_up_topics
 
   end
-
 
 
   # E1928 Allow reviews to bid on what review.

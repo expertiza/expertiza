aed48b0e86328c83c9a712224724600af59d5e21
Vivek Reddy <vivekreddykarri98@gmail.com>
2019-10-21 17:26:13 -0400

    Refactored Peer_review_strategy by using a helper function gen_random_participant_id

--- app/controllers/review_mapping_controller.rb
+++ app/controllers/review_mapping_controller.rb
@@ -444,6 +444,33 @@ class ReviewMappingController < ApplicationController
                                                last.created_at
   end
 
+
+  ## Helper Method for generating a random participant which is to be used in peer_review_strategy method. 
+  def gen_random_participant_id(iterator, participants_hash, num_participants, participants)
+    if iterator.zero?
+        rand_num = rand(0..num_participants - 1)
+    else
+        min_value = participants_hash.values.min
+        # get the temp array including indices of participants, each participant has minimum review number in hash table.
+        participants_with_min_assigned_reviews = []
+        participants.each do |participant|
+          participants_with_min_assigned_reviews << participants.index(participant) if participants_hash[participant.id] == min_value
+        end
+    # if participants_with_min_assigned_reviews is blank
+    if_condition_1 = participants_with_min_assigned_reviews.empty?
+    # or only one element in participants_with_min_assigned_reviews, prohibit one student to review his/her own artifact
+    if_condition_2 = (participants_with_min_assigned_reviews.size == 1 and TeamsUser.exists?(team_id: team.id, user_id: participants[participants_with_min_assigned_reviews[0]].user_id))
+    rand_num = if if_condition_1 or if_condition_2
+                 # use original method to get random number
+                 rand(0..num_participants - 1)
+               else
+                 # rand_num should be the position of this participant in original array
+                 participants_with_min_assigned_reviews[rand(0..participants_with_min_assigned_reviews.size - 1)]
+               end
+    end
+    return rand_num
+  end 
+
   def peer_review_strategy(assignment_id, review_strategy, participants_hash)
     teams = review_strategy.teams
     participants = review_strategy.participants
@@ -466,27 +493,8 @@ class ReviewMappingController < ApplicationController
           break if selected_participants.size == participants.size - num_participants_this_team
 
           # generate random number
-          if iterator.zero?
-            rand_num = rand(0..num_participants - 1)
-          else
-            min_value = participants_hash.values.min
-            # get the temp array including indices of participants, each participant has minimum review number in hash table.
-            participants_with_min_assigned_reviews = []
-            participants.each do |participant|
-              participants_with_min_assigned_reviews << participants.index(participant) if participants_hash[participant.id] == min_value
-            end
-            # if participants_with_min_assigned_reviews is blank
-            if_condition_1 = participants_with_min_assigned_reviews.empty?
-            # or only one element in participants_with_min_assigned_reviews, prohibit one student to review his/her own artifact
-            if_condition_2 = (participants_with_min_assigned_reviews.size == 1 and TeamsUser.exists?(team_id: team.id, user_id: participants[participants_with_min_assigned_reviews[0]].user_id))
-            rand_num = if if_condition_1 or if_condition_2
-                         # use original method to get random number
-                         rand(0..num_participants - 1)
-                       else
-                         # rand_num should be the position of this participant in original array
-                         participants_with_min_assigned_reviews[rand(0..participants_with_min_assigned_reviews.size - 1)]
-                       end
-          end
+          rand_num = gen_random_participant_id(iterator, participants_hash, num_participants, participants)
+          
           # prohibit one student to review his/her own artifact
           next if TeamsUser.exists?(team_id: team.id, user_id: participants[rand_num].user_id)
 

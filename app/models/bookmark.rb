d7bc4728a72b9848fe676152ca7420ebac407fab
Abhirav Kariya <abhiravredox@Abhiravs-MacBook-Pro.local>
2019-10-23 12:58:48 -0400

    Added url validation for update (bookmark validation bug)

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -6,5 +6,6 @@ class Bookmark < ActiveRecord::Base
   validates :title, presence: true
   validates :description, presence: true
   validates_format_of :url, :multiline => true, :with => /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/, :on => :create
+  validates_format_of :url, :multiline => true, :with => /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/, :on => :update
 
 end

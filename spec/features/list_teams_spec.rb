81a863b8d21019be311c80ac9d7a3dc554e06ab5
LingLi <lli46@ncsu.edu>
2019-10-23 20:13:17 -0400

    issue3 test debug

--- spec/features/list_teams_spec.rb
+++ spec/features/list_teams_spec.rb
@@ -15,12 +15,6 @@ describe 'List Team' do
 end
 
 describe "View users" do
-  before(:each) do
-    @user1 =  User.new name: 'abc', fullname: 'abc xyz', email: 'abcxyz@gmail.com', password: '12345678', password_confirmation: '12345678',
-                       email_on_submission: 1, email_on_review: 1, email_on_review_of_review: 0, copy_of_emails: 1, handle: 'handle'
-    @user1.save
-  end
-
   it "check if instructors show their institutions on the same line as their new feature" do
     require 'pp'
     driver = Selenium::WebDriver.for :firefox
@@ -33,21 +27,6 @@ describe "View users" do
     has_institution = false
     if driver.find_element(xpath: "/html/body/div[1]/div[1]/div/div/div/div/table/tbody/tr[2]/th[3]").text == "Institution"
       has_institution = true
-      driver.find_elements(xpath: "/html/body/div[1]/div[1]/div/div/div/div/table/tbody/tr").each.with_index(3) do |_,index|
-        user_name = driver.find_element(xpath: "/html/body/div[1]/div[1]/div/div/div/div/table/tbody/tr[#{index}]/td[1]").text
-        pp user_name
-        pp User.where(["name LIKE ?", "#{"student"}%"])
-        pp User.find_by_id(index.to_i)
-        # this_ins = driver.find_element(xpath: "/html/body/div[1]/div[1]/div/div/div/div/table/tbody/tr[#{index}]/td[3]").text
-        # that_ins = Institution.find(User.find_by(:name => user_name).institution_id)
-        # pp that_ins
-        # if (this_ins != "" or that_ins != nil) and this_ins != that_ins
-        #   pp user_name
-        #   pp this_ins
-        #   pp that_ins
-        #   has_institution = false
-        # end
-      end
       expect(has_institution).to be(true)
     end
     expect(has_institution).to be(true)

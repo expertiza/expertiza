33dad327193ea25c47dfc6ef0bf094a1e6da4b0e
amanoakane <pokemondream961120@gmail.com>
2019-10-27 21:04:05 -0400

    test creating teams by imprting a txt file

---
+++ spec/features/assignment_team_import_spec.rb
@@ -0,0 +1,32 @@
+include InstructorInterfaceHelperSpec
+
+
+describe "create team by importing file" do
+  before(:each) do
+    assignment_setup
+  end
+
+
+  describe "import file" do
+
+    # to test create new teams by importing a txt file
+    it "is able to create team by importing a file" do
+
+      login_as('instructor6')
+      instructor = User.where(name: 'instructor6').first
+      assignment = Assignment.where(instructor_id: instructor.id).first
+      visit "/teams/list?id=#{assignment.id}&type=Assignment"
+      click_link 'Import Teams'
+      expect(page).to have_content('Import AssignmentTeam List')
+      expect(find_field('file').value).to eq(nil)
+      file_path = Rails.root + "spec/features/upload_teams.txt" 
+      attach_file('file', file_path)
+      expect(find_field('file').value).not_to eq(nil)
+      click_button "Import"
+      expect(page).to have_content('Importing from')
+      click_button 'Import Teams'
+      expect(page).to have_content('team1')
+      expect(page).to have_content('team2')
+    end
+  end
+end

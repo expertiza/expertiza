31d903dc3fb5437e1efad6300b6f3d8062814f70
oskulkar@ncsu.edu <oskulkar@vm16-62.vcl.ncsu.edu>
2019-10-28 13:51:25 -0400

    changes in Rspec

--- spec/models/vm_question_response_spec.rb
+++ spec/models/vm_question_response_spec.rb
@@ -75,9 +75,9 @@ describe VmQuestionResponse  do
       it 'adds reviews' do
         response = VmQuestionResponse.new(author_feedback_questionnaire, assignment, 1)
         #allow(participant).to receive(:feedback).and_return(reviews)
-        allow(FeedbackResponseMap).to receive(:where).with(id: 1).and_return(double('FeedbackResponseMap', reviewer_id: 1))
-        allow(Participant).to recieve(:find).with(1).and_return(participant)
-	      allow(Response).to recieve(:where).with(id: 1).and_return(response)
+        allow(FeedbackResponseMap).to receive(:where).with(reviewer_id: 1).and_return(double('FeedbackResponseMap', reviewee_id: 1))
+        allow(Participant).to receive(:find_by).with(id: 1).and_return(participant)
+	      allow(Response).to receive(:where).with(map_id: 1).and_return(response)
 	      response.add_reviews(participant, team, false)
         expect(response.list_of_reviews.size).to eq(1)
         expect(response.list_of_reviewers.size).to eq(1)

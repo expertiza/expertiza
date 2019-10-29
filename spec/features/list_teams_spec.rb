a241e97e2ce9e0172b8d772318278b829594b74c
Yongjian Zhu <yjzhu.sh@gmail.com>
2019-10-28 23:17:07 -0400

    test: #1188 test

--- spec/features/list_teams_spec.rb
+++ spec/features/list_teams_spec.rb
@@ -18,14 +18,7 @@ describe "View users" do
   it "check if instructors show their institutions on the same line as their new feature" do
     create(:superadmin, name: 'super_administrator2')
     login_as('super_administrator2')
-    visit "/course/new?private=1"
-    page.body.should have_selector("select#course_institutions_id option:nth-of-type(0)", text: 'Institution')
-
-    has_institution = false
-    if driver.find_element(xpath: "/html/body/div[1]/div[1]/div/div/div/div/table/tbody/tr[2]/th[3]").text == "Institution"
-      has_institution = true
-      expect(has_institution).to be(true)
-    end
-    expect(has_institution).to be(true)
+    visit "/users/list"
+    expect(page.has_content?("Institution")).to eq(true)
   end
 end

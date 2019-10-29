1dda65a8b370b88a594d4d3f6618fe806aedadc4
Roshani <rnarasi2@ncsu.edu>
2019-10-28 22:41:53 -0400

    Import assignment participant fixed

--- app/helpers/import_file_helper.rb
+++ app/helpers/import_file_helper.rb
@@ -11,6 +11,7 @@ module ImportFileHelper
     attributes["email_on_submission"] = 1
     attributes["email_on_review"] = 1
     attributes["email_on_review_of_review"] = 1
+    attributes["handle"] = row_hash[:fullname]
     attributes
   end
 

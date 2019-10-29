b695a9e72b6569ff5e3d95434f48a998ac261bb6
Pranav <pmgaikwa@ncsu.edu>
2019-10-28 21:56:37 -0400

    adding missing participant

--- spec/models/vm_question_response_spec.rb
+++ spec/models/vm_question_response_spec.rb
@@ -78,6 +78,7 @@ describe VmQuestionResponse  do
         #allow(participant).to receive(:feedback).and_return(reviews)
         allow(FeedbackResponseMap).to receive(:where).with(reviewer_id: 3).and_return([double(id: 1, reviewee_id: 4, response_id: 1)])
         allow(Participant).to receive(:find_by).with(id: 4).and_return(reviwee)
+        allow(Participant).to receive(:find_by).with(id: 3).and_return(participant)
         allow(Response).to receive_message_chain(:where, :order, :last).and_return(response)
 	      response.add_reviews(participant, team, false)
         expect(response.list_of_reviews.size).to eq(1)

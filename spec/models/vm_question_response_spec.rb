0ed53e9a277a5c01e3b837aa7ecc51da822e1e0b
Pranav <pmgaikwa@ncsu.edu>
2019-10-29 10:54:56 -0400

    investigating side effect in another test due to mocked object [1]

--- spec/models/vm_question_response_spec.rb
+++ spec/models/vm_question_response_spec.rb
@@ -8,7 +8,7 @@ describe VmQuestionResponse  do
   let(:questions) { [question] }
   let(:team) { build(:assignment_team, id: 1, name: 'no team') }
   let(:participant) { build(:participant, id: 3, grade: 100) }
-  let(:reviwee) { build(:participant, id: 4, grade: 100) }
+  let(:reviewee) { build(:participant, id: 4, grade: 100) }
   let(:response) { VmQuestionResponse.new(review_questionnaire, assignment, 1) }
   let(:answer) { double('Answer') }
   let(:reviews) { [double('Response', map_id: 1, response_id: 1)] }
@@ -77,11 +77,11 @@ describe VmQuestionResponse  do
         response = VmQuestionResponse.new(author_feedback_questionnaire, assignment, 1)
         #allow(participant).to receive(:feedback).and_return(reviews)
         allow(FeedbackResponseMap).to receive(:where).with(reviewer_id: 3).and_return([double(id: 1, reviewer_id: 3, reviewee_id: 4, response_id: 1)])
-        allow(Participant).to receive(:find_by).with(id: 4).and_return(reviwee)
+        # allow(Participant).to receive(:find_by).with(id: 4).and_return(reviewee)
 	      response.add_reviews(participant, team, false)
         expect(response.list_of_reviews.size).to eq(0)
         expect(response.list_of_reviewers.size).to eq(1)
-        expect(response.list_of_reviewers.first).to eq(reviwee)
+        # expect(response.list_of_reviewers.first).to eq(reviewee)
       end
     end
 

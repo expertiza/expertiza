43d0937ad34ead952a91f0b3f934481e3060ac3d
Dheeraj R Makam <drmakam@ncsu.edu>
2019-11-03 00:13:51 -0400

    resolving failing test cases

--- spec/models/assignment_particpant_spec.rb
+++ spec/models/assignment_particpant_spec.rb
@@ -307,6 +307,7 @@ describe AssignmentParticipant do
             allow(AssignmentParticipant).to receive(:exists?).with(user_id: 1, parent_id: 1).and_return(false)
             allow(AssignmentParticipant).to receive(:create).with(user_id: 1, parent_id: 1).and_return(participant)
             allow(participant).to receive(:set_handle).and_return('handle')
+            email = MailerHelper.http_setup_get_request_mock_success
             allow(MailerHelper).to receive(:send_mail_to_user).and_return(email)
             allow(email).to receive(:deliver).and_return("success")
             expect(AssignmentParticipant.import(row, nil, {}, 1)).to be_truthy

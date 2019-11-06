753ec64e36aa1e05939e8cb7cdbda76292164235
mliu25 <mliu25@ncsu.edu>
2019-11-06 14:24:39 -0500

    coding climate

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -170,23 +170,23 @@ class GradesController < ApplicationController
   def save_grade_and_comment_for_submission
     @participant = AssignmentParticipant.find_by(id: params[:participant_id])
     @assignment = @participant.assignment
-    if TaMapping.where(ta_id:current_user.id,course_id:@assignment.course.id).empty?&&current_user.role.name !='Instructor'
+    if TaMapping.where(ta_id: current_user.id, course_id: @assignment.course.id).empty? && current_user.role.name != 'Instructor'
       flash[:error] = 'Unauthorized action!'
       redirect_to controller: 'grades', action: 'view_team', id: participant.id
       # if cannot find TA's id attach to this course, he/she could not grade for this course's assignment
     else
 
-    participant = AssignmentParticipant.find_by(id: params[:participant_id])
-    @team = participant.team
-    @team.grade_for_submission = params[:grade_for_submission]
-    @team.comment_for_submission = params[:comment_for_submission]
-    begin
-      @team.save
-      flash[:success] = 'Grade and comment for submission successfully saved.'
-    rescue StandardError
-      flash[:error] = $ERROR_INFO
-    end
-    redirect_to controller: 'grades', action: 'view_team', id: participant.id
+      participant = AssignmentParticipant.find_by(id: params[:participant_id])
+      @team = participant.team
+      @team.grade_for_submission = params[:grade_for_submission]
+      @team.comment_for_submission = params[:comment_for_submission]
+      begin
+        @team.save
+        flash[:success] = 'Grade and comment for submission successfully saved.'
+      rescue StandardError
+        flash[:error] = $ERROR_INFO
+      end
+      redirect_to controller: 'grades', action: 'view_team', id: participant.id
     end
   end
 

b11c53321ab9d5c4919e6bac407e4fe61bdfcb87
sdinaka <sdinaka@ncsu.edu>
2019-11-05 20:44:07 -0500

    removed link for current Stage function

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -351,17 +351,7 @@ class Assignment < ActiveRecord::Base
     AssignmentQuestionnaire.where(assignment_id: self.id, used_in_round: 2).size >= 1
   end
 
-  def link_for_current_stage(topic_id = nil)
-    if self.staggered_deadline?
-      return nil if topic_id.nil?
-    end
-    due_date = next_due_date(topic_id)
-    if finished?(topic_id) or due_date.nil? or due_date.is_a?(TopicDueDate)
-      return nil
-    else
-      due_date.description_url
-    end
-  end
+
 
   def stage_deadline(topic_id = nil)
     return 'Unknown' if topic_missing?(topic_id)

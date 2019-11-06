cb8fbb446110c9bb0e1d174c585a016cdb26844d
apkulka2 <apkulka2@ncsu.edu>
2019-11-06 10:16:19 -0500

    Resolving following Rspec & Code Climate error

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -33,25 +33,25 @@ class AssignmentsController < ApplicationController
       dir_path = assignment_form_params[:assignment][:directory_path]
       exist_directory = Assignment.find_by(directory_path: dir_path, course_id: @assignment_form.assignment.course_id)
       if (!exist_assignment and !exist_directory) and @assignment_form.save
-          @assignment_form.create_assignment_node
-          current_assignment = Assignment.find_by(name: @assignment_form.assignment.name, course_id: @assignment_form.assignment.course_id)
-          assignment_form_params[:assignment][:id] = current_assignment.id.to_s
-          ques_array = assignment_form_params[:assignment_questionnaire]
-          due_array = assignment_form_params[:due_date]
-          ques_array.each do |cur_questionnaire|
-            cur_questionnaire[:assignment_id] = current_assignment.id.to_s
-          end
-          due_array.each do |cur_due|
-            cur_due[:parent_id] = current_assignment.id.to_s
-          end
-          assignment_form_params[:assignment_questionnaire] = ques_array
-          assignment_form_params[:due_date] = due_array
-          @assignment_form.update(assignment_form_params, current_user)
-          aid = Assignment.find_by(name: @assignment_form.assignment.name, course_id: @assignment_form.assignment.course_id).id
-          ExpertizaLogger.info "Assignment created: #{@assignment_form.as_json}"
-          redirect_to edit_assignment_path aid
-          undo_link("Assignment \"#{@assignment_form.assignment.name}\" has been created successfully. ")
-          return
+        @assignment_form.create_assignment_node
+        current_assignment = Assignment.find_by(name: @assignment_form.assignment.name, course_id: @assignment_form.assignment.course_id)
+        assignment_form_params[:assignment][:id] = current_assignment.id.to_s
+        ques_array = assignment_form_params[:assignment_questionnaire]
+        due_array = assignment_form_params[:due_date]
+        ques_array.each do |cur_questionnaire|
+          cur_questionnaire[:assignment_id] = current_assignment.id.to_s
+        end
+        due_array.each do |cur_due|
+          cur_due[:parent_id] = current_assignment.id.to_s
+        end
+        assignment_form_params[:assignment_questionnaire] = ques_array
+        assignment_form_params[:due_date] = due_array
+        @assignment_form.update(assignment_form_params, current_user)
+        aid = Assignment.find_by(name: @assignment_form.assignment.name, course_id: @assignment_form.assignment.course_id).id
+        ExpertizaLogger.info "Assignment created: #{@assignment_form.as_json}"
+        redirect_to edit_assignment_path aid
+        undo_link("Assignment \"#{@assignment_form.assignment.name}\" has been created successfully. ")
+        return
       else
         flash.now[:error] = "Failed to create an assignment. May be since the assignment/directory already exists in the selected course. Kindly rename."
         render 'new'

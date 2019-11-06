ed5e280de7d580a7d865d8154bbebf6a6871d04b
samruddhikhandale <khandale.samruddhi@gmail.com>
2019-11-06 14:02:30 -0500

    Rspec modification

--- spec/controllers/assignments_controller_spec.rb
+++ spec/controllers/assignments_controller_spec.rb
@@ -149,7 +149,7 @@ describe AssignmentsController do
         allow(Assignment).to receive(:find_by).with(name: 'test assignment', course_id: 1).and_return(assignment)
         allow(Assignment).to receive(:find_by).with(directory_path: 'test_assignment', course_id: 1).and_return(assignment)
         post :create, @params
-        expect(flash.now[:error]).to eq("Failed to create an assignment. May be since the assignment/directory already exists in the selected course. Kindly rename.")
+        expect(flash.now[:error]).to eq("Failed to create an assignment. Kindly rename.")
         expect(response).to render_template(:new)
       end
     end

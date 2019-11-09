2952fefecce571bdc4c867c7fa1903242720dd65
Abhishek T <atripat6@ncsu.edu>
2019-11-08 21:14:19 -0500

    Model View and Controller for GradingHistory

--- app/models/grading_history.rb
+++ app/models/grading_history.rb
@@ -1,2 +1,6 @@
-class GradingHistory
+class GradingHistory < ActiveRecord::Base
+  attr_protected
+  # attr_accessible
+  belongs_to :instructor, inverse_of: :instructor_id
+  belongs_to :assignment, inverse_of: :assignment_id
 end

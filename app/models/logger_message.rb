06fa17258c74ab1e73bb96b8db59f023bfa8242b
Abhishek T <atripat6@ncsu.edu>
2019-11-08 21:19:29 -0500

    Changes to Add customized filters to the Log

--- app/models/logger_message.rb
+++ app/models/logger_message.rb
@@ -1,10 +1,11 @@
 class LoggerMessage
-  attr_reader :generator, :unity_id, :message, :oip, :req_id
-  def initialize(generator, unity_id, message, req = nil)
+  attr_reader :generator, :unity_id, :message, :oip, :req_id, :log_map
+  def initialize(generator, unity_id, message, req = nil, log_map = nil)
     @generator = generator
     @unity_id = unity_id
     @message = message
     @oip = req.remote_ip if req
     @req_id = req.uuid if req
+    @log_map = log_map if log_map
   end
 end

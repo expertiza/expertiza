dd38c87727053b77af9d12961eb9e87463bb1d09
nmdzl <namaedzl@google.com>
2019-11-27 10:21:26 -0500

    Supplementary review questionnaire and answers added to view/response/response.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181205201208) do
+ActiveRecord::Schema.define(version: 20191126032312) do
 
   create_table "answer_tags", force: :cascade do |t|
     t.integer  "answer_id",                limit: 4
@@ -112,6 +112,7 @@ ActiveRecord::Schema.define(version: 20181205201208) do
     t.boolean  "is_answer_tagging_allowed"
     t.boolean  "has_badge"
     t.boolean  "allow_selecting_additional_reviews_after_1st_round"
+    t.boolean  "is_supplementary_review_enabled"
   end
 
   add_index "assignments", ["course_id"], name: "fk_assignments_courses", using: :btree
@@ -630,7 +631,6 @@ ActiveRecord::Schema.define(version: 20181205201208) do
     t.string  "status",            limit: 255
     t.string  "unityID",           limit: 255
     t.string  "signup_preference", limit: 255
-    t.string  "feedback",          limit: 255
   end
 
   create_table "survey_deployments", force: :cascade do |t|
@@ -696,15 +696,16 @@ ActiveRecord::Schema.define(version: 20181205201208) do
   end
 
   create_table "teams", force: :cascade do |t|
-    t.string  "name",                       limit: 255
-    t.integer "parent_id",                  limit: 4
-    t.string  "type",                       limit: 255
-    t.text    "comments_for_advertisement", limit: 65535
+    t.string  "name",                                  limit: 255
+    t.integer "parent_id",                             limit: 4
+    t.string  "type",                                  limit: 255
+    t.text    "comments_for_advertisement",            limit: 65535
     t.boolean "advertise_for_partner"
-    t.text    "submitted_hyperlinks",       limit: 65535
-    t.integer "directory_num",              limit: 4
-    t.integer "grade_for_submission",       limit: 4
-    t.text    "comment_for_submission",     limit: 65535
+    t.text    "submitted_hyperlinks",                  limit: 65535
+    t.integer "directory_num",                         limit: 4
+    t.integer "grade_for_submission",                  limit: 4
+    t.text    "comment_for_submission",                limit: 65535
+    t.integer "supplementary_review_questionnaire_id", limit: 4
   end
 
   create_table "teams_users", force: :cascade do |t|
@@ -716,14 +717,14 @@ ActiveRecord::Schema.define(version: 20181205201208) do
   add_index "teams_users", ["user_id"], name: "fk_teams_users", using: :btree
 
   create_table "track_notifications", force: :cascade do |t|
+    t.integer  "notification_id", limit: 4
     t.integer  "user_id",         limit: 4
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "notification_id", limit: 4, null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
-  add_index "track_notifications", ["notification_id"], name: "notification_id", using: :btree
-  add_index "track_notifications", ["user_id"], name: "user_id", using: :btree
+  add_index "track_notifications", ["notification_id"], name: "index_track_notifications_on_notification_id", using: :btree
+  add_index "track_notifications", ["user_id"], name: "index_track_notifications_on_user_id", using: :btree
 
   create_table "tree_folders", force: :cascade do |t|
     t.string  "name",       limit: 255
@@ -817,4 +818,6 @@ ActiveRecord::Schema.define(version: 20181205201208) do
   add_foreign_key "tag_prompt_deployments", "tag_prompts"
   add_foreign_key "teams_users", "teams", name: "fk_users_teams"
   add_foreign_key "teams_users", "users", name: "fk_teams_users"
+  add_foreign_key "track_notifications", "notifications"
+  add_foreign_key "track_notifications", "users"
 end

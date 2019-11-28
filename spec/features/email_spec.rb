deca8167a5bec8ba240874d6dfe02a8fd4b043c3
urvishvasani <ur207vasani@gmail.com>
2019-11-28 14:50:20 -0500

    added test case for importing user from csv file

--- spec/features/email_spec.rb
+++ spec/features/email_spec.rb
@@ -22,4 +22,26 @@ describe 'send email to new user', type: :feature do
     mail = ActionMailer::Base.deliveries.last
     expect(mail.subject).to eq('Your Expertiza password has been created')
   end
+
+  context 'when user will be created using import file' do
+    let(:row) do
+      {name: 'username', fullname: 'full name', email: 'abc@mailinator.com'}
+    end
+    let(:attributes) do
+      {role_id: 1, name: 'username', fullname: 'full name', email: 'abc@mailinator.com', email_on_submission: 'abc@mailinator.com', email_on_review: 'abc@mailinator.com', email_on_review_of_review: 'abc@mailinator.com' }
+    end
+
+    it 'imports one user and number of mail counnt should be one' do
+      ActionMailer::Base.deliveries.clear
+      allow(ImportFileHelper).to receive(:define_attributes).with(row).and_return(attributes)
+      allow(ImportFileHelper).to receive(:create_new_user) do
+        test_user = User.new(name: 'username', fullname: 'full name', email: 'abc@mailinator.com')
+        test_user.id = 123
+        test_user.save!
+        test_user
+      end
+      allow(User).to receive(:exists?).with(name: 'username').and_return(false)
+      expect{(User.import(row, nil, {},1))}.to change{ActionMailer::Base.deliveries.count}.by(2)
+    end
+  end
 end

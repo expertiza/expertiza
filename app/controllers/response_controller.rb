5c090245935e239d1bb9bd82be13c15836f24296
apkulka2 <apkulka2@ncsu.edu>
2019-11-30 00:15:58 -0500

    Proper Review Functionality from List Submissions Page

--- app/controllers/response_controller.rb
+++ app/controllers/response_controller.rb
@@ -107,7 +107,8 @@ class ResponseController < ApplicationController
     # A new response has to be created when there hasn't been any reviews done for the current round,
     # or when there has been a submission after the most recent review in this round.
     @response = Response.where(map_id: @map.id, round: @current_round.to_i).order(updated_at: :desc).first
-    if @response.nil? || AssignmentTeam.find(@map.reviewee_id).most_recent_submission.updated_at > @response.updated_at
+    team_most_recent_submission = AssignmentTeam.find(@map.reviewee_id).most_recent_submission
+    if @response.nil? || (!team_most_recent_submission.nil? && team_most_recent_submission.updated_at > @response.updated_at)
       @response = Response.create(map_id: @map.id, additional_comment: '', round: @current_round, is_submitted: 0)
     end
     questions = sort_questions(@questionnaire.questions)
@@ -340,3 +341,4 @@ class ResponseController < ApplicationController
     end
   end
 end
+

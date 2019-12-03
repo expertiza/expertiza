3e65f099dafa44c1e2bfdf04213571bd80d26ecb
urvishvasani <ur207vasani@gmail.com>
2019-12-03 16:46:17 -0500

    try 5 at passing travis build

--- spec/controllers/response_controller_spec.rb
+++ spec/controllers/response_controller_spec.rb
@@ -1,10 +1,10 @@
 describe ResponseController do
-  let(:assignment) { build(:assignment, instructor_id: 6) }
+  let(:assignment) { create(:assignment, id: 1, instructor_id: 6) }
   let(:instructor) { build(:instructor, id: 6) }
-  let(:participant) { build(:participant, id: 1, user_id: 6, assignment: assignment) }
-  let(:review_response) { build(:response, id: 1, map_id: 1) }
+  let(:participant) { create(:participant, id: 1, user_id: 6, parent_id: 1, assignment: assignment) }
+  let(:review_response) { create(:response, id: 1, map_id: 1) }
   let(:review_response_round1) { build(:response, id: 1, map_id: 1, round: 1, is_submitted: 0) }
-  let(:review_response_map) { build(:review_response_map, id: 1, reviewer: participant) }
+  let(:review_response_map) { create(:review_response_map, id: 1, reviewer: participant) }
   let(:questionnaire) { build(:questionnaire, id: 1, questions: [question]) }
   let(:question) { Criterion.new(id: 1, weight: 2, break_before: true) }
   let(:assignment_questionnaire) { build(:assignment_questionnaire) }

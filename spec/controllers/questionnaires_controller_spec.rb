13682d1d45f92cf4a18cc0ae28f3570ce4915b00
Qingyan Wang <qingyanwang21@gmail.com>
2019-12-04 16:40:00 -0500

    revise setup for tests in questionnares controller

--- spec/controllers/questionnaires_controller_spec.rb
+++ spec/controllers/questionnaires_controller_spec.rb
@@ -10,13 +10,7 @@ describe QuestionnairesController do
   let(:instructor) { build(:instructor, id: 6) }
   let(:instructor2) { build(:instructor, id: 66) }
   let(:ta) { build(:teaching_assistant, id: 8) }
-  let(:assignment) { build(:assignment, id: 1) }
-  let(:assignment_questionnaire1) {build(:assignment_questionnaire, id: 1, assignment_id: 1, questionnaire_id: 1, used_in_round: 1)}
-  let(:due_date1) {build(:due_date, id: 1, due_at: '2019-11-30 23:30:12', deadline_type_id: 1, parent_id: 1, round: 1)}
-  let(:due_date2) {build(:due_date, id: 2, due_at: '2019-12-30 23:30:12', deadline_type_id: 2, parent_id: 1, round: 1)}
-  let(:assignment_questionnaire2) {build(:assignment_questionnaire, id: 2, assignment_id: 1, questionnaire_id: 2, used_in_round: 2)}
-  let(:due_date3) {build(:due_date, id: 3, due_at: '2020-01-30 23:30:12', deadline_type_id: 1, parent_id: 1, round: 2)}
-  let(:due_date4) {build(:due_date, id: 4, due_at: '2020-02-30 23:30:12', deadline_type_id: 2, parent_id: 1, round: 2)}
+
 
   before(:each) do
     allow(Questionnaire).to receive(:find).with('1').and_return(questionnaire)
@@ -231,6 +225,25 @@ describe QuestionnairesController do
       allow(Questionnaire).to receive(:find).with('1').and_return(@questionnaire1)
       @questionnaire2 = double('Questionnaire', id: 2)
       allow(Questionnaire).to receive(:find).with('2').and_return(@questionnaire2)
+      @assignment1 = double('Assignment', id: 1)
+      allow(Assignment).to receive(:find).with('1').and_return(@assignment1)
+      @questionnaire3 = double('Questionnaire', id: 3)
+      allow(Questionnaire).to receive(:find).with('3').and_return(@questionnaire3)
+      @questionnaire4 = double('Questionnaire', id: 4)
+      allow(Questionnaire).to receive(:find).with('4').and_return(@questionnaire4)
+      @duedate1 = double('Duedate', id: 1, due_at: '2019-11-30 23:30:12', deadline_type_id: 1, parent_id: 1, round: 1)
+      allow(DueDate).to receive(:find).with('1').and_return(@duedate1)
+      @duedate2 = double('Duedate', id: 2, due_at: '2019-12-30 23:30:12', deadline_type_id: 2, parent_id: 1, round: 1)
+      allow(DueDate).to receive(:find).with('2').and_return(@duedate2)
+      @duedate3 = double('Duedate', id: 3, due_at: '2019-01-30 23:30:12', deadline_type_id: 1, parent_id: 1, round: 2)
+      allow(DueDate).to receive(:find).with('3').and_return(@duedate3)
+      @duedate4 = double('Duedate', id: 4, due_at: '2019-02-30 23:30:12', deadline_type_id: 2, parent_id: 1, round: 2)
+      allow(DueDate).to receive(:find).with('4').and_return(@duedate4)
+      @assignment_questionnaire1 = double('AssignmentQuestionnaire', id: 1, assignment_id: 1, questionnaire_id: 3, used_in_round: 1)
+      allow(AssignmentQuestionnaire).to receive(:find).with('1').and_return(@assignment_questionnaire1)
+      @assignment_questionnaire2 = double('AssignmentQuestionnaire', id: 2, assignment_id: 1, questionnaire_id: 4, used_in_round: 2)
+      allow(AssignmentQuestionnaire).to receive(:find).with('2').and_return(@assignment_questionnaire2)
+
       @params = {id: 1,
                  questionnaire: {name: 'test questionnaire',
                                  instructor_id: 6,
@@ -308,7 +321,7 @@ describe QuestionnairesController do
 
     context 'when params[:add_new_questions] is not nil and the change is in the period.' do
       it 'redirects to questionnaire#add_new_questions' do
-        params = {id: 1,
+        params = {id: 3,
                   add_new_questions: true,
                   new_question: {total_num: 2,
                                  type: 'TextArea'}}
@@ -320,7 +333,7 @@ describe QuestionnairesController do
 
     context 'when params[:add_new_questions] is not nil and the change is out of the period.' do
       it 'redirects to questionnaire#add_new_questions' do
-        params = {id: 2,
+        params = {id: 4,
                   add_new_questions: true,
                   new_question: {total_num: 2,
                                  type: 'TextArea'}}

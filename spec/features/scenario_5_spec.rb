c758df10c7bb3e84e17dda29c33a9f56b6281b74
Tanvi <tmpandi2@ncsu.edu>
2019-12-04 15:30:01 -0500

    capybara test for reset password mailer feature

--- spec/features/scenario_5_spec.rb
+++ spec/features/scenario_5_spec.rb
@@ -1,40 +1,14 @@
-require( 'spec_helper')
-describe 'password reset request',js: true do
-  before(:each) do
-    #create(:student)
-    #visit '/'
-    #click_link 'Forgot password?'
-    #Selenium::WebDriver.logger.level = :debug
-  end
+require 'rails_helper'
 
-  context 'invalid email' do
-    it 'invalid email', js: true do
-      visit '/'
-      click_link 'Forgot password?'
-      expect(page).to have_current_path('/password_retrieval/forgotten')
-      fill_in 'user_email', with: 'dummy'
-      click_button 'Request password'
-      expect(page).to have_content('No account is associated with the e-mail address ‘dummy’:  Please try again')
-    end
-    end
+  RSpec.feature 'Password Reset', type: :feature do
+    context 'Reset Password' do
+      scenario 'Email not associated with any account' do
 
-  it 'email does not exist'
-  context 'no input' do
-    it 'no input', js: true do
-      visit '/'
-      click_link 'Forgot password?'
-      expect(page).to have_current_path('/password_retrieval/forgotten')
-      click_button 'Request password'
-      expect(page).to have_content('No account is associated with the e-mail address ‘dummy’:  Please try again')
+        visit '/'
+        click_link 'Forgot password?'
+        expect(page).to have_current_path('/password_retrieval/forgotten')
+        fill_in 'user_email', with: 'expertiza@mailinator.com'
+        click_button 'Request password'
+      end
     end
   end
-  it 'works correctly' do
-    visit '/'
-    click_link 'Forgot password?'
-    expect(page).to have_current_path('/password_retrieval/forgotten')
-    fill_in 'user_email', with: 'expertiza@mailinator.com'
-    click_button 'Request password'
-    expect(page).to have_content('A link to reset your password has been sent to your e-mail address.')
-  end
-
-end

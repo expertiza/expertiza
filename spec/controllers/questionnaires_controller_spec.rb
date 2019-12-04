d0de564ef072289fdcdbbdda9ebe5d47fe64f1b7
Yolanda-Ling-Li <lli46@ncsu.edu>
2019-12-04 17:24:36 -0500

    answer helper test

--- spec/controllers/questionnaires_controller_spec.rb
+++ spec/controllers/questionnaires_controller_spec.rb
@@ -263,27 +263,6 @@ describe QuestionnairesController do
     before(:each) do
       @questionnaire1 = double('Questionnaire', id: 1)
       allow(Questionnaire).to receive(:find).with('1').and_return(@questionnaire1)
-      @questionnaire2 = double('Questionnaire', id: 2)
-      allow(Questionnaire).to receive(:find).with('2').and_return(@questionnaire2)
-      @assignment1 = double('Assignment', id: 1)
-      allow(Assignment).to receive(:find).with('1').and_return(@assignment1)
-      @questionnaire3 = double('Questionnaire', id: 3)
-      allow(Questionnaire).to receive(:find).with('3').and_return(@questionnaire3)
-      @questionnaire4 = double('Questionnaire', id: 4)
-      allow(Questionnaire).to receive(:find).with('4').and_return(@questionnaire4)
-      @duedate1 = double('Duedate', id: 1, due_at: '2019-11-30 23:30:12', deadline_type_id: 1, parent_id: 1, round: 1)
-      allow(DueDate).to receive(:find).with('1').and_return(@duedate1)
-      @duedate2 = double('Duedate', id: 2, due_at: '2019-12-30 23:30:12', deadline_type_id: 2, parent_id: 1, round: 1)
-      allow(DueDate).to receive(:find).with('2').and_return(@duedate2)
-      @duedate3 = double('Duedate', id: 3, due_at: '2019-01-30 23:30:12', deadline_type_id: 1, parent_id: 1, round: 2)
-      allow(DueDate).to receive(:find).with('3').and_return(@duedate3)
-      @duedate4 = double('Duedate', id: 4, due_at: '2019-02-30 23:30:12', deadline_type_id: 2, parent_id: 1, round: 2)
-      allow(DueDate).to receive(:find).with('4').and_return(@duedate4)
-      @assignment_questionnaire1 = double('AssignmentQuestionnaire', id: 1, assignment_id: 1, questionnaire_id: 3, used_in_round: 1)
-      allow(AssignmentQuestionnaire).to receive(:find).with('1').and_return(@assignment_questionnaire1)
-      @assignment_questionnaire2 = double('AssignmentQuestionnaire', id: 2, assignment_id: 1, questionnaire_id: 4, used_in_round: 2)
-      allow(AssignmentQuestionnaire).to receive(:find).with('2').and_return(@assignment_questionnaire2)
-
       @params = {id: 1,
                  questionnaire: {name: 'test questionnaire',
                                  instructor_id: 6,
@@ -361,12 +340,12 @@ describe QuestionnairesController do
 
     context 'when params[:add_new_questions] is not nil and the change is in the period.' do
       it 'redirects to questionnaire#add_new_questions' do
-        params = {id: 3,
+        params = {id: 1,
                   add_new_questions: true,
                   new_question: {total_num: 2,
                                  type: 'TextArea'}}
-        allow(AnswerHelper).to receive(:in_active_period).with('3').and_return(true)
-        allow(AnswerHelper).to receive(:delete_existing_responses).with('3')
+        allow(AnswerHelper).to receive(:in_active_period).with('1').and_return(true)
+        allow(AnswerHelper).to receive(:delete_existing_responses).with('1')
         post :update, params
         expect(flash[:success]).to eq('You have successfully added a new question. The existing reviews for the questionnaire have been deleted!')
         expect(response).to redirect_to action: 'add_new_questions', id: params[:id], question: params[:new_question]
@@ -375,11 +354,11 @@ describe QuestionnairesController do
 
     context 'when params[:add_new_questions] is not nil and the change is out of the period.' do
       it 'redirects to questionnaire#add_new_questions' do
-        params = {id: 4,
+        params = {id: 1,
                   add_new_questions: true,  
                   new_question: {total_num: 2,
                                  type: 'TextArea'}}
-        allow(AnswerHelper).to receive(:in_active_period).with('4').and_return(false)
+        allow(AnswerHelper).to receive(:in_active_period).with('1').and_return(false)
         post :update, params
         expect(flash[:success]).to eq('You have successfully added a new question.')
         expect(response).to redirect_to action: 'add_new_questions', id: params[:id], question: params[:new_question]

74ed0a3cd3a9c5e69d3b63a62b56ddfd9d9bffc1
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-12-05 18:15:10 -0500

    added comment

--- app/helpers/report_formatter_helper.rb
+++ app/helpers/report_formatter_helper.rb
@@ -1,6 +1,7 @@
 module ReportFormatterHelper
   def summary_by_reviewee_and_criteria(params, _session = nil)
     assign_basics(params)
+    # E1991 : pass extra session variable to address anonymized view
     sum = SummaryHelper::Summary.new.summarize_reviews_by_reviewees(@assignment, @summary_ws_url, _session)
     @summary = sum.summary
     @reviewers = sum.reviewers

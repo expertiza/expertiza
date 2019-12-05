aeb8252e75a9616e916642ab1377da5046422d6c
rohanpillai20 <rpillai1997@gmail.com>
2019-12-05 12:05:57 -0500

    Updated RSpec Test Cases for submission_viewing_events_controller_spec.rb

--- spec/controllers/submission_viewing_events_controller_spec.rb
+++ spec/controllers/submission_viewing_events_controller_spec.rb
@@ -11,7 +11,16 @@ describe SubmissionViewingEventsController do
       it 'should update time record with end time as current time' do
 	    submission_viewing_event_records=double('SubmissionViewingEvent')
         allow(SubmissionViewingEvent).to receive(:where).with([:map_id,:round,:link]).and_return(submission_viewing_event_records)		
-		 dummy = double('BasicObject')
+		dummy = double('BasicObject')
+        allow(SubmissionViewingEvent).to receive(:end_at).and_return(dummy)
+        allow(dummy).to receive(:nil?).and_return(true)
+		expect(response.body).to be_blank
+      end
+	  
+	  it 'should update time record with end time as current time for an Expertiza Review link' do
+	    submission_viewing_event_records=double('SubmissionViewingEvent')
+        allow(SubmissionViewingEvent).to receive(:where).with([:map_id,:round,link: 'Expertiza Review']).and_return(submission_viewing_event_records)		
+		dummy = double('BasicObject')
         allow(SubmissionViewingEvent).to receive(:end_at).and_return(dummy)
         allow(dummy).to receive(:nil?).and_return(true)
 		expect(response.body).to be_blank
@@ -31,6 +40,20 @@ describe SubmissionViewingEventsController do
         expect(response.body).to be_blank
       end
 	end
+  end
+  
+  describe '#mark_end_time' do
+    context 'end time for all uncommitted files' do
+	  it 'update end time for all the files that are not having an end time' do
+	    submission_viewing_event_records=double('SubmissionViewingEvent')
+        allow(SubmissionViewingEvent).to receive(:where).with([:map_id,:round,:link]).and_return(submission_viewing_event_records)
+		dummy = double('BasicObject')
+        allow(SubmissionViewingEvent).to receive(:end_at).and_return(dummy)
+        allow(dummy).to receive(:nil?).and_return(true)
+		allow(dummy).to receive(:update_attributes).with(:end_at,Time.now.to_date).and_return(submission_viewing_event_records)
+        expect(response.body).to be_blank
+	  end
+	end	
   end
   
 end

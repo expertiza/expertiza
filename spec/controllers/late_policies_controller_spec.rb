e10a27dddf7c1bf00b1b56945e4094f9a9f473e7
nnegi2 <nnegi2@ncsu.edu>
2019-12-06 05:09:28 -0500

    Removed commented code

--- spec/controllers/late_policies_controller_spec.rb
+++ spec/controllers/late_policies_controller_spec.rb
@@ -1,6 +1,4 @@
-require 'rails_helper'
-
-RSpec.describe LatePoliciesController, :type => :controller do
+describe LatePoliciesController do
 
   let(:instructor) { build(:instructor, id: 6) }
 
@@ -9,51 +7,40 @@ RSpec.describe LatePoliciesController, :type => :controller do
   end
 
   describe "GET #index" do
-    it "when index is called" do
-      get :index
-      expect(get: "late_policies/").to route_to("late_policies#index")
-    end
-    it "when redirects to index page" do
-      get :index
-      expect(response).to render_template(:index)
-    end
+    context 'when index is called' do
+      it "routes to index page" do
+        get :index
+        expect(get: "late_policies/").to route_to("late_policies#index")
+      end
+      it "renders the new page" do
+        get :index
+        expect(response).to render_template(:index)
+      end
+     end
   end
 
   describe "GET #show" do
-    it "when show is called" do
-      params = {
-          id: 1
-      }
-      get :show,params
-      expect(get: "late_policies/1").to route_to("late_policies#show",id:"1")
+    context 'when show is called' do
+      it "routes to show page" do
+        params = {
+            id: 1
+        }
+        get :show,params
+        expect(get: "late_policies/1").to route_to("late_policies#show",id:"1")
+      end
     end
-    # it "when show is called" do
-    #   params = {
-    #       id: 1
-    #   }
-    #   get :show,params
-    #   expect(response).to render_template(:show)
-    # end
   end
 
-  # describe "GET #edit" do
-  #   it "when edit is called" do
-  #     params = {
-  #         id: 1
-  #     }
-  #     get :edit,params
-  #     expect(get: "late_policies/1/edit").to route_to("late_policies#edit", id:"1")
-  #   end
-  # end
-
   describe "GET #new" do
-    it "when new is called" do
-      get :new
-      expect(get: "late_policies/new").to route_to("late_policies#new")
-    end
-    it "when new is called" do
-      get :new
-      expect(response).to render_template(:new)
+    context 'when new is called' do
+      it "routes to new page" do
+        get :new
+        expect(get: "late_policies/new").to route_to("late_policies#new")
+      end
+      it "renders the new page" do
+        get :new
+        expect(response).to render_template(:new)
+      end
     end
   end
 
@@ -68,28 +55,23 @@ RSpec.describe LatePoliciesController, :type => :controller do
   end
 
   describe "POST #create" do
-    # before(:each) do
-    #   latePolicy = LatePolicy.new
-    #   allow(latePolicy).to receive(:check_policy_with_same_name).with(any_args).and_return(false)
-    # end
-
     context 'when maximum penalty is less than penalty per unit' do
       before(:each) do
         latePolicy = LatePolicy.new
         allow(latePolicy).to receive(:check_policy_with_same_name).with(any_args).and_return(false)
       end
-    it "throws a flash error " do
-      params = {
-          late_policy: {
-              max_penalty: 10,
-              penalty_per_unit: 30,
-              policy_name: "Policy1"
-          }
-      }
-      post :create,params
-      expect(flash[:error]).to eq("The maximum penalty cannot be less than penalty per unit.")
-      expect(response).to redirect_to('/late_policies/new')
-    end
+      it "throws a flash error " do
+        params = {
+            late_policy: {
+                max_penalty: 10,
+                penalty_per_unit: 30,
+                policy_name: "Policy1"
+            }
+        }
+        post :create,params
+        expect(flash[:error]).to eq("The maximum penalty cannot be less than penalty per unit.")
+        expect(response).to redirect_to('/late_policies/new')
+      end
     end
 
     context 'when policy with same name exists' do
@@ -110,44 +92,44 @@ RSpec.describe LatePoliciesController, :type => :controller do
       end
     end
 
-    context 'when everything is fine' do
+    context 'when the late_policy is not saved' do
       before(:each) do
 
         latePolicy = LatePolicy.new
         latePolicy.policy_name="Policy1"
-        latePolicy.max_penalty=100
+        latePolicy.max_penalty=40
         latePolicy.penalty_per_unit=30
         latePolicy.instructor_id=6
         allow(latePolicy).to receive(:check_policy_with_same_name).with(any_args).and_return(false)
         allow(latePolicy).to receive(:new).with(any_args).and_return(latePolicy)
         allow(latePolicy).to receive(:save!).and_return(false )
       end
-      it "saves " do
+      it "throws a flash error " do
         params = {
             late_policy: {
-                max_penalty: 100,
+                max_penalty: 40,
                 penalty_per_unit: 30,
                 policy_name: "Policy1"
             }
         }
         post :create,params
-        expect(flash[:error]).to eq("The following error occurred while saving the penalty policy: ")
+        expect(flash[:error]).to eq("The following error occurred while saving the late policy: ")
       end
     end
 
 end
 
   describe "POST #update" do
+      before(:each) do
+        latePolicy = LatePolicy.new
+        latePolicy.policy_name="Policy2"
+        latePolicy.max_penalty=40
+        latePolicy.penalty_per_unit=30
+        latePolicy.instructor_id=6
+        allow(LatePolicy).to receive(:find).with("1").and_return(latePolicy)
+        allow(LatePolicy).to receive(:check_policy_with_same_name).with(any_args).and_return(true)
+      end
       context 'when maximum penalty is less than penalty per unit' do
-        before(:each) do
-          latePolicy = LatePolicy.new
-          latePolicy.policy_name="Policy2"
-          latePolicy.max_penalty=100
-          latePolicy.penalty_per_unit=30
-          latePolicy.instructor_id=6
-          allow(LatePolicy).to receive(:find).with("1").and_return(latePolicy)
-          allow(LatePolicy).to receive(:check_policy_with_same_name).with(any_args).and_return(true)
-        end
         it "throws a flash error " do
           params = {
               late_policy: {
@@ -165,12 +147,6 @@ end
 
       context 'when policy with same name exists' do
         before(:each) do
-          latePolicy = LatePolicy.new
-          latePolicy.policy_name="Policy3"
-          latePolicy.max_penalty=30
-          latePolicy.penalty_per_unit=10
-          latePolicy.instructor_id=6
-          allow(LatePolicy).to receive(:find).with("1").and_return(latePolicy)
           allow(LatePolicy).to receive(:check_policy_with_same_name).with(any_args).and_return(false)
         end
         it "throws a flash error " do
@@ -187,18 +163,8 @@ end
         end
       end
 
-      context 'when everything is fine' do
-        before(:each) do
-
-          latePolicy = LatePolicy.new
-          latePolicy.policy_name="Policy1"
-          latePolicy.max_penalty=100
-          latePolicy.penalty_per_unit=30
-          latePolicy.instructor_id=6
-          allow(LatePolicy).to receive(:find).with("1").and_return(latePolicy)
-          allow(LatePolicy).to receive(:check_policy_with_same_name).with(any_args).and_return(true)
-        end
-        it "saves " do
+      context 'when the late_policy is not updated' do
+        it "throws a flash error " do
           params = {
               late_policy: {
                   max_penalty: 30,
@@ -208,7 +174,7 @@ end
               id:1
           }
           post :update,params
-          expect(flash[:error]).to eq("The following error occurred while updating the penalty policy: ")
+          expect(flash[:error]).to eq("The following error occurred while updating the late policy: ")
         end
       end
   end

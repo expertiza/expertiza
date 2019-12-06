7af5f80a5f4aebcefd8da8aa48875d4367c2738e
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-12-06 16:30:32 -0500

    removed unnecsessary session call in the generate team link name

--- app/helpers/review_mapping_helper.rb
+++ app/helpers/review_mapping_helper.rb
@@ -102,8 +102,7 @@ module ReviewMappingHelper
   # For assignments with 1 team member, the following method returns user's fullname else it returns "team name" that a particular reviewee belongs to.
   def get_team_reviewed_link_name(max_team_size, response, reviewee_id)
     team_reviewed_link_name = if max_team_size == 1
-                                # E1991 : check anonymized view here
-                                TeamsUser.where(team_id: reviewee_id).first.user.fullname(session[:ip])
+                                TeamsUser.where(team_id: reviewee_id).first.user.fullname
                               else
                                 # E1991 : check anonymized view here
                                 Team.find(reviewee_id).name(session[:ip])

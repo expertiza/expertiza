e3b6e08b44e8255a2450a558d35c6d841c521e75
piyushtiw <piyush@bigbinary.com>
2019-12-06 16:26:54 -0500

    refactor github metric helper

--- app/helpers/github_metrics_helper.rb
+++ app/helpers/github_metrics_helper.rb
@@ -1,147 +1,32 @@
 module GithubMetricsHelper
   def display_github_metrics(gitVariable, graph_type, timeline_type, due_date)
-    parsed_data = gitVariable[:parsed_data]
-    authors = gitVariable[:authors]
+    @parsed_data = gitVariable[:parsed_data]
+    @authors = gitVariable[:authors]
     dates = gitVariable[:dates]
-    dates_to_week = Set[]
-
-    submission_week = if ['Unknown', 'Finished'].include? due_date
+    @dates_to_week = Set[]
+    @y_axis_label = ''
+    @submission_week = if ['Unknown', 'Finished'].include? due_date
                         nil
                       else
                         DateTime.parse(due_date).strftime('%V')
                       end
 
     dates.each do |date|
-      dates_to_week.add(DateTime.parse(date).strftime('%V'))
+      @dates_to_week.add(DateTime.parse(date).strftime('%V'))
     end
-    @y_axis_label = ''
+    
     if timeline_type == GithubMetric::timeline_types['week'].to_s
-      @y_axis_label = 'Weeks to submission'
-      dates_to_week = dates_to_week.sort
-      parsed_data_by_week = {}
-      parsed_data.each do |author_email, commit_hash|
-        week_commits = {}
-        dates_to_week.each do |key|
-          week_commits[key] = {
-            commits: 0,
-            additions: 0,
-            deletions: 0
-          }
-        end
-        commit_hash.each do |date, commit_object|
-          week_number = DateTime.parse(date).strftime('%V')
-          week_commits[week_number][:commits] += commit_object[:commits]
-          week_commits[week_number][:additions] += commit_object[:additions]
-          week_commits[week_number][:deletions] += commit_object[:deletions]
-        end
-        week_commits = week_commits.sort.to_h
-        parsed_data_by_week[author_email] = week_commits
-      end
-
-      data_array = []
-      color = %w[#ed1c1c99 #ffea0099 #00ff0899 #00e5ff99 #ff007799 #0d00ff99 #7d4a5699 #77c9c899]
-      i = 0
-      authors.each do |author|
-        no_of_commits_data = []
-        no_of_lines_added_data = []
-        no_of_lines_deleted_data = []
-
-        parsed_data_by_week[author].each do |key, commit_object|
-          no_of_commits_data << commit_object[:commits]
-          no_of_lines_added_data << commit_object[:additions]
-          no_of_lines_deleted_data << commit_object[:deletions]
-        end
-
-        data_object = {}
-        data_object['label'] = author
-        data_object['backgroundColor'] = color[i]
-        data_object['borderWidth'] = 1
-        case graph_type
-        when GithubMetric::graph_types['number of commits'].to_s
-          data_object['data'] = no_of_commits_data
-          data_object['stack'] = 1
-        when GithubMetric::graph_types['lines added'].to_s
-          data_object['data'] = no_of_lines_added_data
-          data_object['stack'] = 2
-        when GithubMetric::graph_types['lines deleted'].to_s
-          data_object['data'] = no_of_lines_deleted_data
-          data_object['stack'] = 3
-        else
-          #do nothing
-        end
-        data_array.push(data_object)
-        i += 1
-        i = 0 if i > 7
-      end
-
-      if submission_week.present?
-        dates_to_week = dates_to_week.map { |week| submission_week.to_i - week.to_i }
-      end
+      data_array = get_commits_data_group_by_week(graph_type)
 
       data = {
-        labels: dates_to_week,
+        labels: @dates_to_week,
         datasets: data_array
       }
     else
-      parsed_data_by_week = {}
-      dates_to_week = dates_to_week.sort
-      dates_to_week.each do |week|
-        week_data = {}
-        authors.each do |author_email|
-          week_data[author_email] = {
-            commits: 0,
-            additions: 0,
-            deletions: 0
-          }
-        end
-        parsed_data_by_week[week] = week_data
-      end
-      parsed_data.each do |author_email, commit_hash|
-        commit_hash.each do |date, commit_object|
-          week = DateTime.parse(date).strftime('%V')
-          parsed_data_by_week[week][author_email][:commits] += commit_object[:commits]
-          parsed_data_by_week[week][author_email][:additions] += commit_object[:additions]
-          parsed_data_by_week[week][author_email][:deletions] += commit_object[:deletions]
-        end
-      end
-      data_array = []
-      color = %w[red yellow blue gray green magenta]
-      i = 0
-      dates_to_week.each do |week|
-        no_of_commits_data = []
-        no_of_lines_added_data = []
-        no_of_lines_deleted_data = []
-
-        parsed_data_by_week[week].each do |key, commit_object|
-          no_of_commits_data << commit_object[:commits]
-          no_of_lines_added_data << commit_object[:additions]
-          no_of_lines_deleted_data << commit_object[:deletions]
-        end
-
-        data_object = {}
-        data_object['label'] = 'Week:' + week
-        data_object['backgroundColor'] = color[i]
-        data_object['borderWidth'] = 1
-        case graph_type
-        when GithubMetric::graph_types['number of commits'].to_s
-          data_object['data'] = no_of_commits_data
-          data_object['stack'] = 1
-        when GithubMetric::graph_types['lines added'].to_s
-          data_object['data'] = no_of_lines_added_data
-          data_object['stack'] = 2
-        when GithubMetric::graph_types['lines deleted'].to_s
-          data_object['data'] = no_of_lines_deleted_data
-          data_object['stack'] = 3
-        else
-          #do nothing
-        end
-        data_array.push(data_object)
-        i += 1
-        i = 0 if i > 7
-      end
+      data_array = get_commits_data_group_by_student(graph_type)
 
       data = {
-        labels: authors,
+        labels: @authors,
         datasets: data_array
       }
     end
@@ -208,5 +93,128 @@ module GithubMetricsHelper
     end
     github_metrics_summary
   end
-end
 
+  private
+
+  def get_commits_data_group_by_week(graph_type)
+    @y_axis_label = 'Weeks to submission'
+    @dates_to_week = @dates_to_week.sort
+    parsed_data_by_week = {}
+    @parsed_data.each do |author_email, commit_hash|
+      week_commits = {}
+      @dates_to_week.each do |key|
+        week_commits[key] = {
+          commits: 0,
+          additions: 0,
+          deletions: 0
+        }
+      end
+      commit_hash.each do |date, commit_object|
+        week_number = DateTime.parse(date).strftime('%V')
+        week_commits[week_number][:commits] += commit_object[:commits]
+        week_commits[week_number][:additions] += commit_object[:additions]
+        week_commits[week_number][:deletions] += commit_object[:deletions]
+      end
+      week_commits = week_commits.sort.to_h
+      parsed_data_by_week[author_email] = week_commits
+    end
+
+    data_array = []
+    index = 0
+    @authors.each do |author|
+      @no_of_commits_data = []
+      @no_of_lines_added_data = []
+      @no_of_lines_deleted_data = []
+
+      parsed_data_by_week[author].each do |key, commit_object|
+        @no_of_commits_data << commit_object[:commits]
+        @no_of_lines_added_data << commit_object[:additions]
+        @no_of_lines_deleted_data << commit_object[:deletions]
+      end
+
+      data_object = initialize_data_object(graph_type)
+      data_object['label'] = author
+      data_object['backgroundColor'] = color[index]
+      data_array.push(data_object)
+      index += 1
+      index = 0 if index > 7
+    end
+
+    if @submission_week.present?
+      @dates_to_week = @dates_to_week.map { |week| @submission_week.to_i - week.to_i }
+    end
+
+    data_array
+  end
+
+
+  def get_commits_data_group_by_student(graph_type)
+    parsed_data_by_week = {}
+    @dates_to_week = @dates_to_week.sort
+    @dates_to_week.each do |week|
+      week_data = {}
+      @authors.each do |author_email|
+        week_data[author_email] = {
+          commits: 0,
+          additions: 0,
+          deletions: 0
+        }
+      end
+      parsed_data_by_week[week] = week_data
+    end
+    @parsed_data.each do |author_email, commit_hash|
+      commit_hash.each do |date, commit_object|
+        week = DateTime.parse(date).strftime('%V')
+        parsed_data_by_week[week][author_email][:commits] += commit_object[:commits]
+        parsed_data_by_week[week][author_email][:additions] += commit_object[:additions]
+        parsed_data_by_week[week][author_email][:deletions] += commit_object[:deletions]
+      end
+    end
+    data_array = []
+    index = 0
+    @dates_to_week.each do |week|
+      @no_of_commits_data = []
+      @no_of_lines_added_data = []
+      @no_of_lines_deleted_data = []
+
+      parsed_data_by_week[week].each do |key, commit_object|
+        @no_of_commits_data << commit_object[:commits]
+        @no_of_lines_added_data << commit_object[:additions]
+        @no_of_lines_deleted_data << commit_object[:deletions]
+      end
+
+      data_object = initialize_data_object(graph_type)
+      data_object['label'] = 'Week:' + week
+      data_object['backgroundColor'] = color[index]
+      data_array.push(data_object)
+      index += 1
+      index = 0 if index > 7
+    end
+
+    data_array
+  end
+
+  def initialize_data_object(graph_type)
+    data_object = {}
+    data_object['borderWidth'] = 1
+    case graph_type
+    when GithubMetric::graph_types['number of commits'].to_s
+      data_object['data'] = @no_of_commits_data
+      data_object['stack'] = 1
+    when GithubMetric::graph_types['lines added'].to_s
+      data_object['data'] = @no_of_lines_added_data
+      data_object['stack'] = 2
+    when GithubMetric::graph_types['lines deleted'].to_s
+      data_object['data'] = @no_of_lines_deleted_data
+      data_object['stack'] = 3
+    else
+      #do nothing
+    end
+
+    data_object
+  end
+
+  def color
+    %w[#ed1c1c #ffea00 #00ff08 #00e5ff #ff0077 #0d00ff #7d4a56 #77c9c8]
+  end
+end

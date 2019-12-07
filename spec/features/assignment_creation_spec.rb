3407663a92350b7d7ec4d15951d5382fe2309916
urvishvasani <ur207vasani@gmail.com>
2019-12-06 22:11:35 -0500

    try 23 at passing travis build

--- spec/features/assignment_creation_spec.rb
+++ spec/features/assignment_creation_spec.rb
@@ -1,3 +1,4 @@
+#
 def questionnaire_options(assignment, type, _round = 0)
   questionnaires = Questionnaire.where(['private = 0 or instructor_id = ?', assignment.instructor_id]).order('name')
   options = []

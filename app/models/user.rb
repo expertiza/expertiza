5177bd9cbd8baaae3c58c3b9bf23b7fbdfa46570
steve <stevemenezes70@gmail.com>
2019-12-07 00:14:06 -0500

    error in user.rb evaluated

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ActiveRecord::Base
   validates :name, uniqueness: true
   validates :name, format: {without: /\s/}
 
-  validates :fullname, presence :true
+  validates :fullname, presence: true
 
   validates :email, presence: {message: "can't be blank"}
   validates :email, format: {with: /\A[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\z/i, allow_blank: true}

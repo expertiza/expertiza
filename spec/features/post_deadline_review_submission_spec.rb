5d39af4c4376a832afbe9166167298de3557302f
“pushkardravid” <pushkardravid@github.com>
2019-12-16 23:19:06 -0500

    spec fix

--- spec/features/post_deadline_review_submission_spec.rb
+++ spec/features/post_deadline_review_submission_spec.rb
@@ -35,7 +35,7 @@ describe 'assignment review after deadline' do
     # user = User.find_by(name: "student2065")
     # stub_current_user(user, user.role.name, user.role)
     login_as("student2065")
-    visit '/root/student_task/view?id=1'
+    visit '/student_task/view?id=1'
 
     # the page should have content, but after deadline passes it is displayed as gray
     # but there should not be any link attached to it

97ad05636e778d5c556c9e9afa3ddc7199a2b1c9
Saurabh Shingte <saurabh.shingte@gmail.com>
2020-02-02 21:09:26 -0500

    Fixed #1491 added conditions to validate reviewer

--- app/controllers/review_mapping_controller.rb
+++ app/controllers/review_mapping_controller.rb
@@ -46,33 +46,50 @@ class ReviewMappingController < ApplicationController
   def add_reviewer
     assignment = Assignment.find(params[:id])
     topic_id = params[:topic_id]
-    user_id = User.where(name: params[:user][:name]).first.id
+    if params[:user][:name] == ""
+      msg = 'User field cannot be empty'
+    else
+      user_id = User.where(name: params[:user][:name]).first.id rescue nil
+      msg = "Cannot find user '" + params[:user][:name] + "'" unless user_id
+    end
     # If instructor want to assign one student to review his/her own artifact,
     # it should be counted as “self-review” and we need to make /app/views/submitted_content/_selfreview.html.erb work.
-    if TeamsUser.exists?(team_id: params[:contributor_id], user_id: user_id)
-      flash[:error] = "You cannot assign this student to review his/her own artifact."
-    else
-      # Team lazy initialization
-      SignUpSheet.signup_team(assignment.id, user_id, topic_id)
-      msg = ''
-      begin
-        user = User.from_params(params)
-        # contributor_id is team_id
-        regurl = url_for id: assignment.id,
-                         user_id: user.id,
-                         contributor_id: params[:contributor_id]
 
-        # Get the assignment's participant corresponding to the user
-        reviewer = get_reviewer(user, assignment, regurl)
-        # ACS Removed the if condition(and corressponding else) which differentiate assignments as team and individual assignments
-        # to treat all assignments as team assignments
-        if ReviewResponseMap.where(reviewee_id: params[:contributor_id], reviewer_id: reviewer.id).first.nil?
-          ReviewResponseMap.create(reviewee_id: params[:contributor_id], reviewer_id: reviewer.id, reviewed_object_id: assignment.id)
-        else
-          raise "The reviewer, \"" + reviewer.name + "\", is already assigned to this contributor."
+    participant_temp = Participant.where(user_id: user_id, parent_id: params[:id]).first rescue nil
+    # msg = 'User field cannot be empty'
+
+    # Check for user field is empty or not
+    if user_id
+      if TeamsUser.exists?(team_id: params[:contributor_id], user_id: user_id)
+        flash[:error] = "You cannot assign this student to review his/her own artifact."
+
+        # If the user is not allowed to review this assignment
+      elsif !participant_temp or !participant_temp.can_review
+        flash[:error] = "This user is not authorized to review the assignment."
+        msg = 'This user is not authorized to review the assignment.'
+      else
+        # Team lazy initialization
+        SignUpSheet.signup_team(assignment.id, user_id, topic_id)
+
+        begin
+          user = User.from_params(params)
+          # contributor_id is team_id
+          regurl = url_for id: assignment.id,
+                           user_id: user.id,
+                           contributor_id: params[:contributor_id]
+
+          # Get the assignment's participant corresponding to the user
+          reviewer = get_reviewer(user, assignment, regurl)
+          # ACS Removed the if condition(and corressponding else) which differentiate assignments as team and individual assignments
+          # to treat all assignments as team assignments
+          if ReviewResponseMap.where(reviewee_id: params[:contributor_id], reviewer_id: reviewer.id).first.nil?
+            ReviewResponseMap.create(reviewee_id: params[:contributor_id], reviewer_id: reviewer.id, reviewed_object_id: assignment.id)
+          else
+            raise "The reviewer, \"" + reviewer.name + "\", is already assigned to this contributor."
+          end
+        rescue StandardError => e
+          msg = e.message
         end
-      rescue StandardError => e
-        msg = e.message
       end
     end
     redirect_to action: 'list_mappings', id: assignment.id, msg: msg

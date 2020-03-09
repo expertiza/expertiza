ca6960795ecdcca109b2557c9562b3c793b72e1a
Jamie Gachie <jrgachie@gmail.com>
2020-03-08 22:40:47 -0400

    Implemented tests conditions for when 'red' should and should not be returned

--- spec/helpers/review_mapping_helper_spec.rb
+++ spec/helpers/review_mapping_helper_spec.rb
@@ -1,5 +1,23 @@
-describe ReviewMappingHelper do
+require 'spec_helper'
 
-  
+describe ReviewMappingHelper, type: :helper do
+
+  describe 'get_team_colour' do
+    before(:each) do
+      @assignment = create(:assignment)
+      @response_map = create(:review_response_map)
+    end
+
+    it 'should return \'red\' if response_map does not exist in Responses' do
+      colour = get_team_colour(@response_map)
+      expect(colour).to eq('red')
+    end
+
+    it 'should not return \'red\' if response_map exists in Responses' do
+      create(:response, response_map: @response_map)
+      colour = get_team_colour(@response_map)
+      expect(colour).not_to eq('red')
+    end
+  end
 
 end

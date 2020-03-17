674f2c2f649af361b6968e2b33a91af4171cfc72
Dave <dbell5@ncsu.edu>
2020-03-17 19:36:43 -0400

    Convert `if` nested inside `else` to `elsif` in update_feedback_assignment_form_attributes

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -368,12 +368,10 @@ class AssignmentsController < ApplicationController
   def update_feedback_assignment_form_attributes
     if params[:set_pressed][:bool] == 'false'
       flash[:error] = "There has been some submissions for the rounds of reviews that you're trying to reduce. You can only increase the round of review."
-    else
-      if @assignment_form.update_attributes(assignment_form_params, current_user)
+    elsif @assignment_form.update_attributes(assignment_form_params, current_user)
         flash[:note] = 'The assignment was successfully saved....'
-      else
+    else
         flash[:error] = "Failed to save the assignment: #{@assignment_form.errors.get(:message)}"
-      end
     end
     ExpertizaLogger.info LoggerMessage.new("", session[:user].name, "The assignment was saved: #{@assignment_form.as_json}", request)
   end

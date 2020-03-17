e5dd4730049b347fc80e399261b1648fbf9dc5b8
Sandeep Kundala <sandeepkundala@gmail.com>
2020-03-17 18:22:29 +0530

    REFACTOR summarize_reviews_by_reviewee -4

--- app/helpers/summary_helper.rb
+++ app/helpers/summary_helper.rb
@@ -9,30 +9,35 @@ module SummaryHelper
     attr_accessor :summary, :reviewers, :avg_scores_by_reviewee, :avg_scores_by_round, :avg_scores_by_criterion, :r_id
 
     def summarize_reviews_by_reviewee_questions(questions, round, assignment, summary_ws_url)
-      self.summary[round.to_s] = {}
-      self.avg_scores_by_criterion[round.to_s] = {}
-      self.avg_scores_by_round[round.to_s] = 0.0
       r_id = self.r_id
       questions[round].each do |q|
         next if q.type.eql?("SectionHeader")
+        
         self.summary[round.to_s][q.txt] = ""
         self.avg_scores_by_criterion[round.to_s][q.txt] = 0.0
+        
         question_answers = Answer.answers_by_question_for_reviewee(assignment.id, r_id, q.id)
+        
         max_score = get_max_score_for_question(q)
+        
         comments = break_up_comments_to_sentences(question_answers)
+        
         # get the avg scores for this question
         self.avg_scores_by_criterion[round.to_s][q.txt] = calculate_avg_score_by_criterion(question_answers, max_score)
         # get the summary of answers to this question
         self.summary[round.to_s][q.txt] = summarize_sentences(comments, summary_ws_url)
       end
     end
- 
+
     def summarize_reviews_by_reviewee(questions, assignment, r_id, summary_ws_url)
       self.summary = self.avg_scores_by_round = self.avg_scores_by_criterion = ({})
       self.r_id = r_id
 
       # get all answers for each question and send them to summarization WS
       questions.each_key do |round|
+        self.summary[round.to_s] = {}
+        self.avg_scores_by_criterion[round.to_s] = {}
+        self.avg_scores_by_round[round.to_s] = 0.0
         summarize_reviews_by_reviewee_questions(questions, round, assignment, summary_ws_url)
         self.avg_scores_by_round[round.to_s] = calculate_avg_score_by_round(self.avg_scores_by_criterion[round.to_s], questions[round])
       end

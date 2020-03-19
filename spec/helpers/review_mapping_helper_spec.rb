48a42e24d28ce4aa2014ed0a5089421645dba43b
jandr <jamart24@ncsu.edu>
2020-03-19 14:53:50 -0400

    Added test to check_submission_test method Test pending

--- spec/helpers/review_mapping_helper_spec.rb
+++ spec/helpers/review_mapping_helper_spec.rb
@@ -105,7 +105,54 @@ describe ReviewMappingHelper, type: :helper do
       expect(colour).to eq('purple')
     end
   end
-  
+
+
+describe 'check_submission_state' do
+  before(:each) do
+    @assignment = create(:assignment, name: 'get_team_colour_test', created_at: DateTime.now.in_time_zone - 13.day)
+  end
+
+  it 'should return \'purple\' if the was submitted within the round' do
+    create(:deadline_right, name: 'No')
+    create(:deadline_right, name: 'Late')
+    create(:deadline_right, name: 'OK')
+    reviewer = create(:participant, review_grade: nil)
+    reviewee = create(:assignment_team, assignment: @assignment)
+    response_map = create(:review_response_map, reviewer: reviewer, reviewee: reviewee)
+    create(:submission_record, assignment_id: @assignment.id, team_id: reviewee.id, operation: 'Submit Hyperlink', content: 'https://wiki.archlinux.org/', created_at: DateTime.now.in_time_zone - 7.day)
+    create(:response, response_map: response_map)
+    create(:assignment_due_date, assignment: @assignment, parent_id: @assignment.id, round: 1, due_at: DateTime.now.in_time_zone - 5.day)
+    create(:assignment_due_date, assignment: @assignment, parent_id: @assignment.id, round: 2, due_at: DateTime.now.in_time_zone + 6.day)
+
+    assignment_created = @assignment.created_at
+    assignment_due_dates = DueDate.where(parent_id: response_map.reviewed_object_id)
+    round = 2
+    colour = check_submission_state(response_map, assignment_created, assignment_due_dates, round)
+    expect(colour).to eq('purple')
+  end
+
+  it 'should return \'green\' if the submission link does not exist' do
+    create(:deadline_right, name: 'No')
+    create(:deadline_right, name: 'Late')
+    create(:deadline_right, name: 'OK')
+    reviewer = create(:participant, review_grade: nil)
+    response_map = create(:review_response_map, reviewer: reviewer)
+    create(:response, response_map: response_map)
+    create(:assignment_due_date, assignment: @assignment, parent_id: @assignment.id, round: 1)
+    create(:assignment_due_date, assignment: @assignment, parent_id: @assignment.id, round: 2)
+
+    assignment_created = @assignment.created_at
+    assignment_due_dates = DueDate.where(parent_id: response_map.reviewed_object_id)
+    round = 2
+    colour = check_submission_state(response_map, assignment_created, assignment_due_dates, round)
+    expect(colour).to eq('green')
+  end
+
+
+
+end
+
+
   describe 'link_updated_since_last?' do
     before(:each) do
       @round = 2
@@ -227,7 +274,6 @@ describe ReviewMappingHelper, type: :helper do
     end
 
     end
-
 
   describe 'get_team_reviewed_link_name' do
     before(:each) do

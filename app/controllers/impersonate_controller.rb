d322057a19322abf2c44ace8b30ccb2aae5c4c2f
nradhak2 <nradhak2@ncsu.edu>
2020-03-19 18:25:22 -0400

    bug fix to test build result

--- app/controllers/impersonate_controller.rb
+++ app/controllers/impersonate_controller.rb
@@ -1,4 +1,3 @@
-
 class ImpersonateController < ApplicationController
   include SecurityHelper
 
@@ -21,8 +20,8 @@ class ImpersonateController < ApplicationController
   def start
     flash[:error] = "This page doesn't take any query string." unless request.GET.empty?
   end
+ 
 
-<<<<<<< HEAD
   # method to clear the session 
   def clear_session
     #original_user = session[:super_user] || session[:user]
@@ -112,85 +111,54 @@ class ImpersonateController < ApplicationController
       begin
       @original_user = session[:super_user] || session[:user]
 
-=======
-  def impersonate
-    if params[:user]
-      message = "No user exists with the name '#{params[:user][:name]}'."
-    elsif params[:impersonate]
-      message = "No user exists with the name '#{params[:impersonate][:name]}'."
-    end
-    begin
-      original_user = session[:super_user] || session[:user]
->>>>>>> 13ace7fccaaba2a97058ee6c282225f4e4b40962
       # Impersonate using form on /impersonate/start
       if params[:impersonate].nil?
         # check if special chars /\?<>|&$# are used to avoid html tags or system command
-        if warn_for_special_chars(params[:user][:name], "Username")
-          redirect_back
-          return
-        end
+        check_if_spl_char
+
         user = User.find_by(name: params[:user][:name])
         if user
-          unless original_user.can_impersonate? user
-            flash[:error] = "You cannot impersonate #{params[:user][:name]}."
-            redirect_back
-            return
-          end
+          checkif_user_impersonateable 
           session[:super_user] = session[:user] if session[:super_user].nil?
-          AuthController.clear_user_info(session, nil)
-          session[:original_user] = original_user
-          session[:impersonate] = true
-          session[:user] = user
+	  clear_session
         else
-          flash[:error] = message
-          redirect_back
-          return
+          display_error_msg
         end
       else
+
         # Impersonate a new account
         if !params[:impersonate][:name].empty?
           # check if special chars /\?<>|&$# are used to avoid html tags or system command
-          if warn_for_special_chars(params[:impersonate][:name], "Username")
-            redirect_back
-            return
-          end
+          check_if_spl_char
+
           user = User.find_by(name: params[:impersonate][:name])
           if user
-            unless original_user.can_impersonate? user
-              flash[:error] = "You cannot impersonate #{params[:user][:name]}."
-              redirect_back
-              return
-            end
-            AuthController.clear_user_info(session, nil)
-            session[:user] = user
-            session[:impersonate] =  true
-            session[:original_user] = original_user
+            checkif_user_impersonateable 
+	    clear_session
           else
-            flash[:error] = message
-            redirect_back
-            return
+            display_error_msg
           end
+
           # Revert to original account
         else
           if !session[:super_user].nil?
-            AuthController.clear_user_info(session, nil)
+	    AuthController.clear_user_info(session, nil)
             session[:user] = session[:super_user]
             user = session[:user]
             session[:super_user] = nil
           else
-            flash[:error] = "No original account was found. Please close your browser and start a new session."
-            redirect_back
-            return
+            display_error_msg
           end
         end
       end
       # Navigate to user's home location
       AuthController.set_current_role(user.role_id, session)
       redirect_to action: AuthHelper.get_home_action(session[:user]),
-                  controller: AuthHelper.get_home_controller(session[:user])
+		controller: AuthHelper.get_home_controller(session[:user])
     rescue Exception => e
-      flash[:error] = e.message
+      flash[:error] = @message
       redirect_to :back
+
     end
   end
 end

46fc13df683a836f256a5605132379bc8e3e5b0f
Tyler Sattler <tsattle@ncsu.edu>
2020-03-22 14:09:08 -0400

    Removed questionaire class check in save_new_questions

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -227,9 +227,7 @@ class QuestionnairesController < ApplicationController
         q.questionnaire_id = questionnaire_id
         q.type = params[:question_type][question_key][:type]
         q.seq = question_key.to_i
-        if @questionnaire.type == "QuizQuestionnaire"
-          q.weight = 1 # setting the weight to 1 for quiz questionnaire since the model validates this field
-        end
+        q.weight = 1 # setting the weight to 1 for quiz questionnaire since the model validates this field
         q.save unless q.txt.strip.empty?
       end
     end

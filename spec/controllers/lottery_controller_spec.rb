25e20ef97b19d845161127807ef056ef71d38ca8
Carl Klier <carlanthonyklier@gmail.com>
2020-03-21 21:06:27 -0400

    added match_new_teams_to_topics, construct_team_bidding_info, create_new_teams_for_bidding_response tests

--- spec/controllers/lottery_controller_spec.rb
+++ spec/controllers/lottery_controller_spec.rb
@@ -2,15 +2,21 @@ include AssignmentHelper
 require 'pp'
 
 describe LotteryController do
-  let(:assignment) {build(:assignment, id: 1)}
+  let(:assignment) {build(:assignment, id: 1, is_intelligent: true)}
+  let(:assignment2) {build(:assignment, id: 2)}
   let(:student) {build(:student)}
   let(:ta) {build(:teaching_assistant)}
   let(:instructor) {build(:instructor)}
   let(:admin) {build(:admin)}
   let(:topic1) {build(:topic, assignment: assignment)}
   let(:topic2) {build(:topic, assignment: assignment)}
-  let(:assignment_team1) {build(:assignment_team, assignment: assignment)}
-  let(:assignment_team2) {build(:assignment_team, assignment: assignment)}
+  let(:assignment_team1) {create(:assignment_team, parent_id: assignment.id)}
+  let(:assignment_team2) {create(:assignment_team, parent_id: assignment.id)}
+
+  let(:student1) {build(:student)}
+  let(:student2) {build(:student)}
+  let(:student3) {build(:student)}
+  let(:student5) {build(:student)}
   
   describe "#run_intelligent_assignmnent" do
     it "webservice call should be successful" do
@@ -95,14 +101,57 @@ describe LotteryController do
     end
   end
 
+  describe "#create_new_teams_for_bidding_response" do
+    it "create new Assignment Teams" do
+      user_bidding_info = []
+      teams = [[student1.id, student2.id], [student3.id, student5.id]]
+      expect(AssignmentTeam.count).to eq(0)
+      expect(TeamNode.count).to eq(0)
+      expect(TeamsUser.count).to eq(0)
+      expect(TeamUserNode.count).to eq(0)
+      controller.send(:create_new_teams_for_bidding_response, teams, assignment, user_bidding_info)
+      #expect {create(:AssignmentTeam)}.to change(AssignmentTeam, :count).by(2)
+      expect(AssignmentTeam.count).to eq(2)
+      expect(TeamNode.count).to eq(2)
+      expect(TeamsUser.count).to eq(4)
+      expect(TeamUserNode.count).to eq(4)
+    end
+  end
+
   describe "#run_intelligent_assignment" do
     it "should redirect to list action in tree_display controller" do
       allow(Assignment).to receive(:find_by).with(id: 1).and_return(assignment)
       params = {id: 1}
-      get :run_intelligent_assignment, params
+      user = student
+      stub_current_user(user, user.role.name, user.role)
+      session = {user: student}
+      get :run_intelligent_assignment, params, session
 #      expect(response).to redirect_to('/tree_display/list')
     end
   end
+
+  describe "#construct_team_bidding_info" do
+    it "should generate team bidding info hash based on newly created teams" do
+      unassigned_teams = [assignment_team1, assignment_team2]
+      sign_up_topics = [topic1, topic2]
+      team_bidding_info = controller.send(:construct_team_bidding_info, unassigned_teams, sign_up_topics)
+      expect(team_bidding_info.size).to eq(2)
+    end
+  end
+
+  describe "#match_new_teams_to_topics" do
+    it "assigns topics to teams" do
+      expect(assignment2.is_intelligent).to eq(false)
+      controller.send(:match_new_teams_to_topics, assignment2)
+      expect(assignment2.is_intelligent).to eq(false)
+      expect(assignment.is_intelligent).to eq(true)
+      bid1 = Bid.create(team_id: assignment_team1.id, topic_id: topic1.id)
+      bid2 = Bid.create(team_id: assignment_team2.id, topic_id: topic2.id)
+      controller.send(:match_new_teams_to_topics, assignment)
+      expect(assignment.is_intelligent).to eq(false)
+    end
+  end
+
 
  # describe "#log" do
   #  it "prints a log message through ExpertizaLogger" do

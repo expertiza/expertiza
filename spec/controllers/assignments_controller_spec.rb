79ee5bf0a58012454019623349d249345dbc008d
Christian Morris <cgmorris@ncsu.edu>
2020-03-23 01:59:33 -0400

    removed params with remove assignment

--- spec/controllers/assignments_controller_spec.rb
+++ spec/controllers/assignments_controller_spec.rb
@@ -362,13 +362,10 @@ describe AssignmentsController do
         assignment_form = AssignmentForm.new
         allow(AssignmentForm).to receive(:new).and_return(assignment_form)
         allow(assignment_form).to receive(:remove_assignment_from_course).with('true')
-        params = {
-            id: 1,
-            force: 'true'
-        }
-        session = {user: instructor}
-        get :remove_assignment_from_course, params, session
+        
+        get :remove_assignment_from_course
         expect(flash[:error]).to be nil
+        expect(response).to redirect_to('/tree_display/list')
       end
     end
   end

a4d386c6f3480aa7ddf66e889eff22e568245686
DanielleHancock <daniellenicolehancock@gmail.com>
2020-03-22 21:41:03 -0400

    change MenuItem name for bookmark rating rubrics

--- db/migrate/20200322210139_add_bookmark_questionnaire_to_menu.rb
+++ db/migrate/20200322210139_add_bookmark_questionnaire_to_menu.rb
@@ -8,7 +8,7 @@ class AddBookmarkQuestionnaireToMenu < ActiveRecord::Migration
 
     item = MenuItem.find_by_name('manage/questionnaires')
     maxseq = MenuItem.where(parent_id: item.id).length
-    MenuItem.find_or_create_by(:name => 'manage/questionnaires/bookmark reviews', :label => 'Bookmark Rating rubrics', :parent_id => item.id, :seq => maxseq+1, :controller_action_id => bookmarkreview_rubrics_action.id)
+    MenuItem.find_or_create_by(:name => 'manage/questionnaires/bookmark rating rubrics', :label => 'Bookmark Rating rubrics', :parent_id => item.id, :seq => maxseq+1, :controller_action_id => bookmarkreview_rubrics_action.id)
 
     Role.rebuild_cache
   end

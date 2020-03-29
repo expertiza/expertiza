ec4b1a34f492c84556f37bca29126321d0bb3331
bzhong2 <bzhong2@ncsu.edu>
2020-03-29 15:39:23 -0400

    delete bridge_to_is_available

--- config/routes.rb
+++ config/routes.rb
@@ -445,7 +445,6 @@ resources :institution, except: [:destroy] do
       post :list
       post :children_node_ng
       post :children_node_2_ng
-      post :bridge_to_is_available
       get :session_last_open_tab
       get :set_session_last_open_tab
     end

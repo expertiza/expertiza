2ae7aa1991673559df0cbfd14d8ec06d80c6d801
twinter <thwinter@ncsu.edu>
2020-03-30 17:18:16 -0400

    remove_empty_teams, remove_user_from_previous_team, assign_available_slots tests

--- spec/controllers/lottery_controller_spec.rb
+++ spec/controllers/lottery_controller_spec.rb
@@ -39,6 +39,57 @@ describe LotteryController do
     end
   end
 
+  describe "remove" do
+    before :each do
+      @assignment = create(:assignment, name: "remove_user_from_previous_team")
+      @assignment_team = create(:assignment_team, assignment: @assignment)
+      @team_user1 = create(:team_user, team: @assignment_team, user: create(:student, name: "team_user1"))
+      @team_user2 = create(:team_user, team: @assignment_team, user: create(:student, name: "team_user2"))
+      @team_user3 = create(:team_user, team: @assignment_team, user: create(:student, name: "team_user3"))
+    end
+
+    describe "#remove_user_from_previous_team" do
+      it "should return the team without the removed user" do
+        user_id = @team_user3.user_id
+        assignment_id = @assignment.id
+        number_of_team_users = TeamsUser.count
+        controller.send(:remove_user_from_previous_team, assignment_id, user_id)
+
+        expect(TeamsUser.count).to eq(number_of_team_users - 1)
+        expect(TeamsUser.find_by(user_id: @team_user3.user_id)).to be nil
+        expect(TeamsUser.find_by(user_id: @team_user2.user_id)).to eq @team_user2
+        expect(TeamsUser.find_by(user_id: @team_user1.user_id)).to eq @team_user1
+      end
+    end
+
+    describe "#remove_empty_teams" do
+      before :each do
+        @assignment_team2 = create(:assignment_team, assignment: @assignment, teams_users: [])
+      end
+      
+      it "should reduce the number of teams by the number of empty teams in the assignment" do
+        number_of_teams = AssignmentTeam.count
+        number_of_teams_in_assignment = Assignment.find(@assignment.id).teams.count
+        controller.send(:remove_empty_teams, @assignment)
+        expect(AssignmentTeam.count).to eq(number_of_teams - 1)
+        expect(Assignment.find(@assignment.id).teams.count).to_not eq(number_of_teams_in_assignment)
+      end
+    end
+  end
+
+  describe "#assign_available_slots" do
+    before :each do
+      @sign_up_topic = topic1
+      @topic_bids1 = [{topic_id: @sign_up_topic.id, priority: 1}]
+      @team_bids = [{team_id: assignment_team1.id, bids: @topic_bids1}]
+    end
+    it "should assign topic to team of biggest size" do
+      number_of_signed_up_teams = SignedUpTeam.count
+      controller.send(:assign_available_slots, @team_bids)
+      expect(SignedUpTeam.count).to eq(number_of_signed_up_teams + 1)
+    end
+  end
+
   describe "#auto_merge_teams" do
     it "sorts the unassigned teams" do
       assignment = double("Assignment")

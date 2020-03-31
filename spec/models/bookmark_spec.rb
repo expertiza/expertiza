13956cea1fa5d0c4859a8edfc2b9fb02d5456a86
Eric Peden <empeden@ncsu.edu>
2020-03-30 23:10:28 -0400

    remove unnecessary assertion

--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -7,36 +7,30 @@ describe Bookmark do
 
   describe '#url' do
     it 'should not be blank' do
-      expect(bookmark).to be_valid
       bookmark.url = ""
       expect(bookmark).not_to be_valid
     end
     it 'should not be nil' do
-      expect(bookmark).to be_valid
       bookmark.url = nil
       expect(bookmark).not_to be_valid
     end
   end
   describe '#title' do
     it 'should not be blank' do
-      expect(bookmark).to be_valid
       bookmark.title = ""
       expect(bookmark).not_to be_valid
     end
     it 'should not be nil' do
-      expect(bookmark).to be_valid
       bookmark.title = nil
       expect(bookmark).not_to be_valid
     end
   end
   describe '#description' do
     it 'should not be blank' do
-      expect(bookmark).to be_valid
       bookmark.description = ""
       expect(bookmark).not_to be_valid
     end
     it 'should not be nil' do
-      expect(bookmark).to be_valid
       bookmark.description = nil
       expect(bookmark).not_to be_valid
     end

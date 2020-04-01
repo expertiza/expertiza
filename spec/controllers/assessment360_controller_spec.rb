a699629a1958f80562c7c0beb1e7e2e64f802338
ushvarma <ushvarma@gmail.com>
2020-03-31 21:11:51 -0400

    Added the testing condition for Insuring existence of course participants

--- spec/controllers/assessment360_controller_spec.rb
+++ spec/controllers/assessment360_controller_spec.rb
@@ -289,4 +289,39 @@ describe Assessment360Controller do
       end
     end
   end
+
+  describe '#insure_existence_of' do
+    context 'checking if the course participants are empty' do
+      before(:each) do
+        request.env['HTTP_REFERER'] = 'http://example.com'
+        get "insure_existence_of"
+      end
+
+      it 'redirects to back' do
+        expect(response).to redirect_to(:back)
+      end
+
+      it 'flashes an error' do
+        expect(flash[:error]).to be_present
+      end
+    end
+    context 'method is called' do
+      before(:each) do
+        allow(Course).to receive(:find).with("1").and_return(course)
+        request.env['HTTP_REFERER'] = 'http://example.com'
+      end
+
+      it 'redirects to back and flashes error as there are no participants' do
+        allow(course).to receive(:assignments).and_return([assignment])
+        allow(assignment).to receive(:reject).and_return(assignment)
+        allow(course).to receive(:get_participants).and_return([]) # no participants
+        params = {course_id: 1}
+        session = {user: instructor}
+        get :course_student_grade_summary, params, session
+        expect(controller.send(:action_allowed?)).to be true
+        expect(response).to redirect_to(:back)
+        expect(flash[:error]).to be_present
+      end
+    end
+  end
 end

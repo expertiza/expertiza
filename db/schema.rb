a3a312d9da8fdf5efb89b3a5d70f0656c2cc61a3
SujalAhrodia <sujalahrodia@gmail.com>
2020-04-18 16:51:51 -0400

    Updating value of status field to visibility

--- db/schema.rb
+++ db/schema.rb
@@ -470,7 +470,7 @@ ActiveRecord::Schema.define(version: 20181205201208) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "calibrate_to",                   default: false
-    t.string   "status",             limit: 255, default: "private"
+    t.string   "visibility",             limit: 255, default: "private"
   end
 
   add_index "response_maps", ["reviewer_id"], name: "fk_response_map_reviewer", using: :btree

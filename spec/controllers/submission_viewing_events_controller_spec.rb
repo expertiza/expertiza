85e7bbe0c94c965e6fdb6b8cfd8336828e8e1d83
jamcdon3 <cluckarman@gmail.com>
2020-04-22 20:36:38 -0400

    updated rspec and added record_end_time test

--- spec/controllers/submission_viewing_events_controller_spec.rb
+++ spec/controllers/submission_viewing_events_controller_spec.rb
@@ -28,4 +28,18 @@ describe SubmissionViewingEventsController do
     end
   end
 
+  describe '#record_end_time' do
+    context 'when response does not have a end time' do
+      it 'should update time record with end time as current time' do
+        submission_viewing_event_records=double('SubmissionViewingEvent')
+        allow(SubmissionViewingEvent).to receive(:where).with([:map_id,:round,:link]).and_return(submission_viewing_event_records)
+        dummy = double('BasicObject')
+        allow(SubmissionViewingEvent).to receive(:end_at).and_return(dummy)
+        allow(dummy).to receive(:nil?).and_return(true)
+        allow(SubmissionViewingEvent).to receive(:update_attributes).with(:end_at,Time.now.to_date).and_return(submission_viewing_event_records)
+        expect(response.body).to be_blank
+      end
+    end
+  end
+
 end

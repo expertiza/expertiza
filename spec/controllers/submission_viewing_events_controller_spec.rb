3a080e80a286533d8fb51e5dc0e94f5f2dc73f25
jamcdon3 <cluckarman@gmail.com>
2020-04-22 21:24:14 -0400

    update rspec and added mark_end_time test

--- spec/controllers/submission_viewing_events_controller_spec.rb
+++ spec/controllers/submission_viewing_events_controller_spec.rb
@@ -42,4 +42,18 @@ describe SubmissionViewingEventsController do
     end
   end
 
+  describe '#mark_end_time' do
+    context 'end time for all uncommitted files' do
+      it 'update end time for all the files that are not having an end time' do
+        submission_viewing_event_records=double('SubmissionViewingEvent')
+        allow(SubmissionViewingEvent).to receive(:where).with([:map_id,:round,:link]).and_return(submission_viewing_event_records)
+        dummy = double('BasicObject')
+        allow(SubmissionViewingEvent).to receive(:end_at).and_return(dummy)
+        allow(dummy).to receive(:nil?).and_return(true)
+        allow(dummy).to receive(:update_attributes).with(:end_at,Time.now.to_date).and_return(submission_viewing_event_records)
+        expect(response.body).to be_blank
+      end
+    end
+  end
+
 end

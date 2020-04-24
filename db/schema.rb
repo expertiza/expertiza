4bba81dbeab3eb28079862718d7d7546cdf99ca6
Yulin Zhang <yzhan114@ncsu.edu>
2020-04-23 23:05:18 -0400

    Resolve merge conflict

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181205201208) do
+ActiveRecord::Schema.define(version: 20200419221902) do
 
   create_table "answer_tags", force: :cascade do |t|
     t.integer  "answer_id",                limit: 4
@@ -112,6 +112,7 @@ ActiveRecord::Schema.define(version: 20181205201208) do
     t.boolean  "is_answer_tagging_allowed"
     t.boolean  "has_badge"
     t.boolean  "allow_selecting_additional_reviews_after_1st_round"
+    t.boolean  "is_revision_planning_enabled"
   end
 
   add_index "assignments", ["course_id"], name: "fk_assignments_courses", using: :btree
@@ -440,6 +441,7 @@ ActiveRecord::Schema.define(version: 20181205201208) do
     t.boolean "break_before",                                           default: true
     t.string  "max_label",        limit: 255,                           default: ""
     t.string  "min_label",        limit: 255,                           default: ""
+    t.integer "team_id",          limit: 4
   end
 
   add_index "questions", ["questionnaire_id"], name: "fk_question_questionnaires", using: :btree
@@ -630,7 +632,6 @@ ActiveRecord::Schema.define(version: 20181205201208) do
     t.string  "status",            limit: 255
     t.string  "unityID",           limit: 255
     t.string  "signup_preference", limit: 255
-    t.string  "feedback",          limit: 255
   end
 
   create_table "survey_deployments", force: :cascade do |t|

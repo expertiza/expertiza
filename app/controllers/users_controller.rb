1620047f9fef26c010c8e2fabd26e8d08a2170a1
Saurabh Shingte <saurabh.shingte@gmail.com>
2020-06-27 15:24:31 -0400

    Fixed #964 by adding logic to create new institution if required

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -119,12 +119,21 @@ class UsersController < ApplicationController
     check = User.find_by(name: params[:user][:name])
     params[:user][:name] = params[:user][:email] unless check.nil?
     @user = User.new(user_params)
+    if params[:user][:institution_id].blank?
+      begin
+        params[:user][:institution_id] = (Institution.create(name: params[:institution][:name])).id
+      rescue StandardError
+        flash[:error] = "Institution with same name already exists"
+        redirect_to action: 'new', role: 'Student'
+      end
+      # params[:user][:institution_id] = params[:user][:institution_id]
+    end
     @user.institution_id = params[:user][:institution_id]
     # record the person who created this new user
     @user.parent_id = session[:user].id
     # set the user's timezone to its parent's
     @user.timezonepref = User.find(@user.parent_id).timezonepref
-    if @user.save
+    if @user.save!
       password = @user.reset_password # the password is reset
       prepared_mail = MailerHelper.send_mail_to_user(@user, "Your Expertiza account and password have been created.", "user_welcome", password)
       prepared_mail.deliver

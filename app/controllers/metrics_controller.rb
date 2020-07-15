6bc8ed99450bed6a123eb7a5d084605980200767
koushik1 <kshanka2@ncsu.edu>
2020-07-15 12:26:19 -0400

    Created mock objects for metrics controller

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -1,11 +1,12 @@
 class MetricsController
-  def confidence_metric(reviews)
-    reviews[:reviews].each do |review|
-      output_to_append = {Sentiment: rand,
-                          Suggestions: rand,
-                          Emotion: rand,
-                          Problem: rand }
-      review.merge!(Confidence: output_to_append)
+  def bulk_service_retrival(reviews,metric,confidence)
+    if confidence
+        if %w(problem suggestions sentiment emotions).include?(metric)
+          reviews['reviews'].each do |review|
+            output_to_append = {'confidence': rand }
+            review.merge!(output_to_append)
+          end
+        end
     end
     reviews
   end

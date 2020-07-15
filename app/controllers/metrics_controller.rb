1da445814ea58ad7ea1da8a3be90a0c6fc816cf0
koushik1 <kshanka2@ncsu.edu>
2020-07-15 13:01:52 -0400

    Added difference case for sentiment and emotions

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -1,9 +1,16 @@
 class MetricsController
   def bulk_service_retrival(reviews,metric,type)
     if type == 'confidence'
-        if %w(problem suggestions sentiment emotions).include?(metric)
+        if %w(problem suggestions).include?(metric)
           reviews['reviews'].each do |review|
             output_to_append = {'confidence': rand }
+            review.merge!(output_to_append)
+          end
+
+        elsif %w(sentiment emotions).include?(metric)
+
+          reviews['reviews'].each do |review|
+            output_to_append = {'confidence': rand > 0.5 ? 1 : 0 }
             review.merge!(output_to_append)
           end
         end

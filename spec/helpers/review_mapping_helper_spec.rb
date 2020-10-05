c3ee28b4ec46089383e73e41f7ca5949d70120f3
Prasanth Yadla <prasanthyadla2@gmail.com>
2020-10-05 19:53:21 -0400

    add tests for obtain_team_color

--- spec/helpers/review_mapping_helper_spec.rb
+++ spec/helpers/review_mapping_helper_spec.rb
@@ -5,13 +5,14 @@ describe ReviewMappingHelper, type: :helper do
   let(:response) { build(:response, map_id: 2, visibility: 'public') }
   let(:review_response_map) { build(:review_response_map, id: 2) }
   
-  describe '#visibility_public?' do
-    it 'should return true if visibility is public or published' do
-      allow(Response).to receive(:where).with(map_id: 2, visibility: ['public', 'published']).and_return(response)
-      allow(response).to receive(:exists?).and_return(true)
-      expect(helper.visibility_public?(review_response_map)).to be true
-    end
-  end
+#  describe '#visibility_public?' do
+#
+#    it 'should return true if visibility is public or published' do
+#      allow(Response).to receive(:where).with(map_id: 2, visibility: ['public','published']).and_return(response)
+#      allow(response).to receive(:exists?).and_return(true)
+#      expect(helper.visibility_public?(review_response_map)).to be true
+#    end
+#  end
 
   describe 'check_submission_state' do
     before(:each) do
@@ -121,6 +122,100 @@ describe ReviewMappingHelper, type: :helper do
 
       result = link_updated_since_last?(@round, @due_dates, link_updated_at)
       expect(result).to eq(true)
+    end
+
+  end
+
+  ## rspec test for obtain_team_color method
+
+  describe 'obtain_team_color' do
+
+    before(:each) do
+
+      @assignment = create(:assignment, created_at: DateTime.now.in_time_zone - 13.day)
+      @reviewer = create(:participant, review_grade: nil)
+      @reviewee = create(:assignment_team, assignment: @assignment)
+      @response_map = create(:review_response_map, reviewer: @reviewer, reviewee: @reviewee)
+    end
+
+    it 'should return purple if previous round was not submitted but submitted in current round' do
+      create(:deadline_right, name: 'No')
+      create(:deadline_right, name: 'Late')
+      create(:deadline_right, name: 'OK')
+
+      create(:submission_record, assignment_id: @assignment.id, team_id: @reviewee.id, operation: 'Submit Hyperlink', content: 'https://wiki.archlinux.org/', created_at: DateTime.now.in_time_zone + 5.day)
+      create(:response, response_map: @response_map)
+      create(:assignment_due_date, assignment: @assignment, parent_id: @assignment.id, round: 1, due_at: DateTime.now.in_time_zone - 5.day)
+      create(:assignment_due_date, assignment: @assignment, parent_id: @assignment.id, round: 2, due_at: DateTime.now.in_time_zone + 6.day)
+
+      assignment_created = @assignment.created_at
+      assignment_due_dates = DueDate.where(parent_id: @response_map.reviewed_object_id)
+      
+      
+      last_round_color = obtain_team_colour(@response_map, assignment_created, assignment_due_dates)
+      expect(last_round_color).to eq('purple')
+    end
+
+
+    it 'should return color for 4th round in a 4-round assignment' do
+      create(:deadline_right, name: 'No')
+      create(:deadline_right, name: 'Late')
+      create(:deadline_right, name: 'OK')
+
+      create(:submission_record, assignment_id: @assignment.id, team_id: @reviewee.id, operation: 'Submit Hyperlink', content: 'https://wiki.archlinux.org/', created_at: DateTime.now.in_time_zone + 1.day)
+      create(:submission_record, assignment_id: @assignment.id, team_id: @reviewee.id, operation: 'Submit Hyperlink', content: 'https://wiki.archlinux.org/', created_at: DateTime.now.in_time_zone + 3.day)
+      create(:submission_record, assignment_id: @assignment.id, team_id: @reviewee.id, operation: 'Submit Hyperlink', content: 'https://wiki.archlinux.org/', created_at: DateTime.now.in_time_zone + 5.day)
+      create(:submission_record, assignment_id: @assignment.id, team_id: @reviewee.id, operation: 'Submit Hyperlink', content: 'https://wiki.archlinux.org/', created_at: DateTime.now.in_time_zone + 7.day)
+
+      create(:response, response_map: @response_map)
+      create(:assignment_due_date, assignment: @assignment, parent_id: @assignment.id, round: 1, due_at: DateTime.now.in_time_zone + 2.day)
+      create(:assignment_due_date, assignment: @assignment, parent_id: @assignment.id, round: 2, due_at: DateTime.now.in_time_zone + 4.day)
+      create(:assignment_due_date, assignment: @assignment, parent_id: @assignment.id, round: 2, due_at: DateTime.now.in_time_zone + 6.day)
+      create(:assignment_due_date, assignment: @assignment, parent_id: @assignment.id, round: 2, due_at: DateTime.now.in_time_zone + 8.day)
+
+      assignment_created = @assignment.created_at
+      assignment_due_dates = DueDate.where(parent_id: @response_map.reviewed_object_id)
+	
+      last_round_color = obtain_team_colour(@response_map, assignment_created, assignment_due_dates)
+      expect(last_round_color).to eq('purple')
+	    
+    end
+
+    it 'should return green if there was no assignment submission in any round' do
+      create(:deadline_right, name: 'No')
+      create(:deadline_right, name: 'Late')
+      create(:deadline_right, name: 'OK')
+
+      
+      create(:response, response_map: @response_map)
+      create(:assignment_due_date, assignment: @assignment, parent_id: @assignment.id, round: 1, due_at: DateTime.now.in_time_zone - 5.day)
+      create(:assignment_due_date, assignment: @assignment, parent_id: @assignment.id, round: 2, due_at: DateTime.now.in_time_zone + 6.day)
+
+      assignment_created = @assignment.created_at
+      assignment_due_dates = DueDate.where(parent_id: @response_map.reviewed_object_id)
+      
+      last_round_color = obtain_team_colour(@response_map, assignment_created, assignment_due_dates)
+      expect(last_round_color).to eq('green')
+    end
+
+
+    it 'should return color of 3rd round in a 3-round submitted assignment' do
+      create(:deadline_right, name: 'No')
+      create(:deadline_right, name: 'Late')
+      create(:deadline_right, name: 'OK')
+
+      create(:submission_record, assignment_id: @assignment.id, team_id: @reviewee.id, operation: 'Submit Hyperlink', content: 'https://wiki.archlinux.org/', created_at: DateTime.now.in_time_zone - 7.day)
+      create(:submission_record, assignment_id: @assignment.id, team_id: @reviewee.id, operation: 'Submit Hyperlink', content: 'https://wiki.archlinux.org/', created_at: DateTime.now.in_time_zone + 4.day)
+      create(:response, response_map: @response_map)
+      create(:assignment_due_date, assignment: @assignment, parent_id: @assignment.id, round: 1, due_at: DateTime.now.in_time_zone - 5.day)
+      create(:assignment_due_date, assignment: @assignment, parent_id: @assignment.id, round: 2, due_at: DateTime.now.in_time_zone + 6.day)
+
+      assignment_created = @assignment.created_at
+      assignment_due_dates = DueDate.where(parent_id: @response_map.reviewed_object_id)
+
+      last_round_color = obtain_team_colour(@response_map, assignment_created, assignment_due_dates)
+      expect(last_round_color).to eq('purple')
+
     end
 
   end

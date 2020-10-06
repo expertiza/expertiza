41e88376417172ce29f1d05b00836e42c845433d
Micah <mbgaffor@ncsu.edu>
2020-10-06 15:56:42 -0400

    edited review_mapping_helper.rb

--- app/helpers/review_mapping_helper.rb
+++ app/helpers/review_mapping_helper.rb
@@ -154,7 +154,7 @@ module ReviewMappingHelper
       end
     end
 
-    @num_rounds = @assignment.num_review_rounds
+    @num_rounds = @assignment.num_review_rounds.to_f
     @all_reviewers_avg_vol_per_round = []
     @all_reviewers_overall_avg_vol = @reviewers.inject(0) {|sum, r| sum += r.overall_avg_vol } / (@reviewers.blank? ? 1 : @reviewers.length)
     @num_rounds.each do |round|

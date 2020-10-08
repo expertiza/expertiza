30e2d7340b72ea4446c0bfec7c3499b168a1bf40
snkumar32 <snkumar1912@gmail.com>
2020-10-08 00:42:39 -0400

    Issue #1412: Rspec update

--- spec/controllers/grades_controller_spec.rb
+++ spec/controllers/grades_controller_spec.rb
@@ -12,6 +12,7 @@ describe GradesController do
   let(:student) { build(:student) }
   let(:review_response_map) { build(:review_response_map, id: 1) }
   let(:assignment_due_date) { build(:assignment_due_date) }
+  let(:ta) { build(:teaching_assistant, id: 8) }
 
 
   before(:each) do

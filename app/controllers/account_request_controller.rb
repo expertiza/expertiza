730aaea8f974b5c11b80a4d0b025e45e2317590f
Sumitosh <sith2594@gmail.com>
2020-10-13 16:22:45 -0400

    Checkbox added and replaced radiobuttons to accept/reject a bunch of requests

--- app/controllers/account_request_controller.rb
+++ app/controllers/account_request_controller.rb
@@ -10,59 +10,55 @@ class AccountRequestController < ApplicationController
     case params[:action]
     when 'list_pending_requested'
       current_user_has_admin_privileges?
-      # ['Super-Administrator',
-      #  'Administrator'].include? current_role_name
     when 'request_new'
       true
     when 'create_requested_user_record'
       true
     when 'keys'
       current_user_has_student_privileges?
-      # current_role_name.eql? 'Student'
     else
       current_user_has_ta_privileges?
-      # ['Super-Administrator',
-      #  'Administrator',
-      #  'Instructor',
-      #  'Teaching Assistant'].include? current_role_name
     end
   end
 
   def create_approved_user
-    requested_user = AccountRequest.find_by(id: params[:id])
-    requested_user.status = params[:status]
-    if requested_user.status.nil?
-      flash[:error] = "Please Approve or Reject before submitting"
-    elsif requested_user.update_attributes(params[:user])
-      flash[:success] = "The user \"#{requested_user.name}\" has been successfully updated."
-    end
-    if requested_user.status == "Approved"
-      new_user = User.new
-      new_user.name = requested_user.name
-      new_user.role_id = requested_user.role_id
-      new_user.institution_id = requested_user.institution_id
-      new_user.fullname = requested_user.fullname
-      new_user.email = requested_user.email
-      new_user.parent_id = session[:user].id
-      new_user.timezonepref = User.find_by(id: new_user.parent_id).timezonepref
-      if new_user.save
-        password = new_user.reset_password
-        # Mail is sent to the user with a new password
-        prepared_mail = MailerHelper.send_mail_to_user(new_user, "Your Expertiza account and password have been created.", "user_welcome", password)
-        prepared_mail.deliver
-        flash[:success] = "A new password has been sent to new user's e-mail address."
-        undo_link("The user \"#{requested_user.name}\" has been successfully created. ")
-      else
-        foreign
+    users = params[:selection]
+    users.each do |user|
+      requested_user = AccountRequest.find_by(id: user.first)
+      requested_user.status = params[:commit] 
+      if requested_user.status.nil?
+        flash[:error] = "Please Approve or Reject before submitting"
+      elsif requested_user.update_attributes(params[:user])
+        flash[:success] = flash[:success].to_s + "The user \"#{requested_user.name}\" has been successfully updated.<br/>"
       end
-    elsif requested_user.status == "Rejected"
-      # If the user request has been rejected, a flash message is shown and redirected to review page
-      if requested_user.update_columns(status: params[:status])
-        flash[:success] = "The user \"#{requested_user.name}\" has been Rejected."
-        redirect_to action: 'list_pending_requested'
-        return
-      else
-        flash[:error] = "Error processing request."
+      if requested_user.status == "Approved"
+        new_user = User.new
+        new_user.name = requested_user.name
+        new_user.role_id = requested_user.role_id
+        new_user.institution_id = requested_user.institution_id
+        new_user.fullname = requested_user.fullname
+        new_user.email = requested_user.email
+        new_user.parent_id = session[:user].id
+        new_user.timezonepref = User.find_by(id: new_user.parent_id).timezonepref
+        if new_user.save
+          password = new_user.reset_password
+          # Mail is sent to the user with a new password
+          prepared_mail = MailerHelper.send_mail_to_user(new_user, "Your Expertiza account and password have been created.", "user_welcome", password)
+          prepared_mail.deliver
+          flash[:success] = "A new password has been sent to new user's e-mail address."
+          undo_link("The user \"#{requested_user.name}\" has been successfully created. ")
+        else
+          foreign
+        end
+      elsif requested_user.status == "Rejected"
+        # If the user request has been rejected, a flash message is shown and redirected to review page
+        if requested_user.update_columns(status: params[:status])
+          flash[:success] = "The user \"#{requested_user.name}\" has been Rejected."
+          redirect_to action: 'list_pending_requested'
+          return
+        else
+          flash[:error] = "Error processing request."
+        end
       end
     end
     redirect_to action: 'list_pending_requested'
@@ -81,8 +77,6 @@ class AccountRequestController < ApplicationController
   end
 
   def list_pending_requested
-    # @requested_users = AccountRequest.all
-    # display all requests with pagination and in reverse chronologigal order
     @requested_users = AccountRequest.all.order(created_at: :desc).paginate(page: params[:page], per_page: 10) 
     @roles = Role.all
   end

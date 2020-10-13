15688ab688553a86a6b1956c28effa0d083dbc33
Xiwen Chen <xchen33@ncsu.edu>
2020-10-13 00:04:56 -0400

    delete assignment_task_spec.rb

--- spec/views/student_task/list.html.erb_spec.rb
+++ spec/views/student_task/list.html.erb_spec.rb
@@ -1,18 +1,22 @@
 include InstructorInterfaceHelperSpec
 
-describe "Integration tests for instructor's assignment page" do
+describe "Integration tests for instructor interface" do
   before(:each) do
     assignment_setup
   end
 
-  describe "Instructor login" do
-    it "with valid username and password" do
-      login_as("instructor6")
-      visit '/menu/student_task'
-      expect(page).to have_content("Assignments")
-      expect(page).to have_content("CSC 517 Fall 2009")
+  describe "student_task test" do
+
+    it "how many courses shows how many tables" do
+      user = User.find_by(name: "instructor6")
+      login_as(user.name)
+      stub_current_user(user, user.role.name, user.role)
+
+      visit '/student_task/list'
+
+      numberOfTables = user.course.length
+      # puts "Number Of Tables: " + numberOfTables.to_s
+      expect(page).to have_css('table', count: numberOfTables*2)
     end
-
-
   end
 end

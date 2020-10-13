c69fe40a49e60d5ff978fb5c0776364229603552
Sumitosh <sith2594@gmail.com>
2020-10-13 15:24:42 -0400

    Added statement to include AuthorizationHelper

--- app/controllers/account_request_controller.rb
+++ app/controllers/account_request_controller.rb
@@ -1,5 +1,6 @@
 
 class AccountRequestController < ApplicationController
+  include AuthorizationHelper
   autocomplete :user, :name
   # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
   verify method: :post, only: %i[destroy create update],

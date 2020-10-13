fc5c63e1bc32e9ab5ad048b58e053cde8c915443
Expertiza Developer <xiwenzoeychen@gmail.com>
2020-10-12 22:25:35 -0400

    try the test if multiple courses show multiple tables, failed

--- spec/features/assignment_task_spec.rb
+++ spec/features/assignment_task_spec.rb
@@ -1,12 +1,20 @@
 describe "student_task test" do
 
     it "with valid username and password" do
-      user = User.find_by(name: "instructor6")
-      login_as(user.name)
+      user_name = 'student2066'
+      user = User.find_by(name: user_name)
+      msg = user.to_yaml
+      File.open('log/diagnostic.txt', 'a') {|f| f.write msg }
+
+      visit root_path
+      fill_in 'login_name', with: user_name
+      fill_in 'login_password', with: 'password'
+      click_button 'Sign in'
       stub_current_user(user, user.role.name, user.role)
+
       visit '/student_task/list'
 
-      numberOfTables = student.course.length
+      numberOfTables = user.course.length
       puts "Number Of Tables: " + numberOfTables.to_s
       expect(page).to have_css('table', count: numberOfTables*2)
     end

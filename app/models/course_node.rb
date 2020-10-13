79ccf36b858537b235d3c64e2fb7b83139db0864
Jonathan Nguyen <jhnguye4@ncsu.edu>
2020-10-13 14:54:51 -0400

    adding self to create_course_node so it can be called in controller

--- app/models/course_node.rb
+++ app/models/course_node.rb
@@ -3,7 +3,7 @@ class CourseNode < Node
   belongs_to :node_object, class_name: "Course", foreign_key: "node_object_id"
 
   # Creates a new courese node from the given course
-  def create_course_node(course)
+  def self.create_course_node(course)
     parent_id = CourseNode.get_parent_id
     @course_node = CourseNode.new
     @course_node.node_object_id = course.id

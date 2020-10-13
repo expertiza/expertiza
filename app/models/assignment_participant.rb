4494d647fd1a1ffae3c14e90d4a1e1b503cafba5
Robert Casano <rcasano1@gmail.com>
2020-10-13 19:09:32 -0400

    refactored assign_quiz and copy methods

--- app/models/assignment_participant.rb
+++ app/models/assignment_participant.rb
@@ -27,11 +27,6 @@ class AssignmentParticipant < Participant
     assignment.try :directory_path
   end
 
-  def assign_quiz(contributor, reviewer, _topic = nil)
-    quiz = QuizQuestionnaire.find_by(instructor_id: contributor.id)
-    QuizResponseMap.create(reviewed_object_id: quiz.try(:id), reviewee_id: contributor.id, reviewer_id: reviewer.id)
-  end
-
   # all the participants in this assignment who have reviewed the team where this participant belongs
   def reviewers
     reviewers = []
@@ -144,7 +139,7 @@ class AssignmentParticipant < Participant
   end
 
   # Copy this participant to a course
-  def copy(course_id)
+  def copy_participant(course_id)
     CourseParticipant.find_or_create_by(user_id: self.user_id, parent_id: course_id)
   end
 

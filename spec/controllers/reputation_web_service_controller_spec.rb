68edc71849b78c461e0b4e2131b77ce51eda9d3e
uahamed <uahamed@ncsu.edu>
2020-10-17 16:38:14 -0400

    update

--- spec/controllers/reputation_web_service_controller_spec.rb
+++ spec/controllers/reputation_web_service_controller_spec.rb
@@ -1,5 +1,5 @@
 describe ReputationWebServiceController do
-  let(:review_response) { build(:response) }
+  #let(:review_response) { build(:response) }
 
 
 
@@ -9,28 +9,26 @@ describe ReputationWebServiceController do
   #end
 
   describe '#calculate' do
-    xit 'should calculate peer review grades' do
-      has_topic = false
-      raw_data_array = []
-      #review_responses = controller.get_review_responses(45, 0)
-      review_responses = controller.get_review_responses(55, 0)
-      #review_responses << review_response
-      raw_data_array = controller.calculate_peer_review_grades(has_topic, review_responses, 0)
-      expect(raw_data_array).to be_an_instance_of(Array)
-      expect(raw_data_array).to_not eq(nil)
-    end
+    # xit 'should calculate peer review grades' do
+    #   has_topic = false
+    #   raw_data_array = []
+    #   #review_responses = controller.get_review_responses(45, 0)
+    #   review_responses = controller.get_review_responses(55, 0)
+    #   #review_responses << review_response
+    #   raw_data_array = controller.calculate_peer_review_grades(has_topic, review_responses, 0)
+    #   expect(raw_data_array).to be_an_instance_of(Array)
+    #   expect(raw_data_array).to_not eq(nil)
+    # end
 
     it 'should return review responses' do
       result = controller.get_review_responses(55, 0)
       expect(result).to_not eq(nil)
-      puts result
     end
 
     it 'should calculate quiz scores' do
       result = controller.calculate_quiz_scores(52,0)
       expect(result).to be_an_instance_of(Array)
       expect(result).to_not eq(nil)
-      puts result
     end
   end
 end

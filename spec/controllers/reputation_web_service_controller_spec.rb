f85451f0958a0099903f85bfc238a03ab353d3c8
uahamed <uahamed@ncsu.edu>
2020-10-17 17:01:57 -0400

    Remove comments

--- spec/controllers/reputation_web_service_controller_spec.rb
+++ spec/controllers/reputation_web_service_controller_spec.rb
@@ -1,12 +1,5 @@
 describe ReputationWebServiceController do
-  #let(:review_response) { build(:response) }
 
-
-
-  #before(:each) do
-    #user = build(:instructor)
-    #stub_current_user(user, user.role.name, user.role)
-  #end
 
   describe '#calculate' do
     # xit 'should calculate peer review grades' do
@@ -20,12 +13,12 @@ describe ReputationWebServiceController do
     #   expect(raw_data_array).to_not eq(nil)
     # end
 
-    it 'should return review responses' do
+    it 'should query database and return review responses' do
       result = controller.get_review_responses(55, 0)
       expect(result).to_not eq(nil)
     end
 
-    it 'should calculate quiz scores' do
+    it 'should calculate quiz scores and return them as an array' do
       result = controller.calculate_quiz_scores(52,0)
       expect(result).to be_an_instance_of(Array)
       expect(result).to_not eq(nil)

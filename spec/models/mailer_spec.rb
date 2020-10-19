53ba25b425022813f4224c4b1b6cb221fa817d9c
Expertiza Developer <Anjaney>
2020-10-19 15:32:24 -0400

    Added test for generic_message

--- spec/models/mailer_spec.rb
+++ spec/models/mailer_spec.rb
@@ -3,6 +3,51 @@ describe 'Tests mailer' do
     build(:assignment, name: "test_assignment")
   }
 
+  it 'should be able to pass parameters to generic message' do
+    # Send the email, then test that it got queued
+    email = Mailer.generic_message(
+      to: 'tluo@ncsu.edu',
+      subject: "Test",
+      body: {
+        partial_name: "new_submission",
+        user: "John Doe",
+        first_name: "John",
+        password: "Password",
+        new_pct: 97,
+        avg_pct: 90,
+        assignment: "code assignment"
+      }
+    )
+    expect(email.from[0]).to eq("expertiza.development@gmail.com")
+    expect(email.to[0]).to eq('expertiza.development@gmail.com')
+    expect(email.subject).to eq('Test')
+  end
+
+  it 'should be able to send an email using generic message' do
+    ActionMailer::Base.deliveries.clear
+
+    email = Mailer.generic_message(
+      to: 'tluo@ncsu.edu',
+      subject: "Test",
+      body: {
+        partial_name: "new_submission",
+        user: "John Doe",
+        first_name: "John",
+        password: "Password",
+        new_pct: 97,
+        avg_pct: 90,
+        assignment: "code assignment"
+      }
+    ).deliver_now
+
+    ActionMailer::Base.deliveries.last.tap do |mail|
+      expect(mail.from).to eq(["expertiza.development@gmail.com"])
+      expect(mail.to).to eq(["expertiza.development@gmail.com"])
+      expect(mail.subject).to eq("Test")
+      expect(mail.body).to eq(email.body)
+    end
+  end
+
   it 'should send email to required email address with proper content ' do
     # Send the email, then test that it got queued
     email = Mailer.sync_message(

f375e1495280e0ffb07948ff031394027a3cb711
jbeerel <bikerj15@yahoo.com>
2020-10-19 15:59:51 -0400

    Teammate review response email test added

--- spec/models/teammate_review_response_map_spec.rb
+++ spec/models/teammate_review_response_map_spec.rb
@@ -1,4 +1,4 @@
-describe MetareviewResponseMap do
+describe TeammateReviewResponseMap do
   let(:team) { build(:assignment_team, id: 1, name: 'team no name', assignment: assignment, users: [student], parent_id: 1) }
   let(:team1) { build(:assignment_team, id: 2, name: 'team has name', assignment: assignment, users: [student]) }
   let(:review_response_map) { build(:review_response_map, id: 1, assignment: assignment, reviewer: participant, reviewee: team) }
@@ -20,14 +20,9 @@ describe MetareviewResponseMap do
   let(:response) { build(:response, id: 1, map_id: 1, round: 1, response_map: review_response_map,  is_submitted: true) }
   let(:response1) { build(:response, id: 2, map_id: 1, round: 2, response_map: review_response_map) }
   let(:response2) { build(:response, id: 3, map_id: 1, round: nil, response_map: review_response_map, is_submitted: true) }
-  let(:metareview_response_map) { build(:meta_review_response_map, reviewed_object_id: 1) }
+  let(:teammate_review_response_map) { TeammateReviewResponseMap.new(id: 1, reviewed_object_id: 1, reviewee_id: 1) }
   let(:student) { build(:student, id: 1, name: 'name', fullname: 'no one', email: 'expertiza@mailinator.com') }
   let(:student1) { build(:student, id: 2, name: "name1", fullname: 'no one', email: 'expertiza@mailinator.com') }
-  let(:assignment_questionnaire1) { build(:assignment_questionnaire, id: 1, assignment_id: 1, questionnaire_id: 1) }
-  let(:assignment_questionnaire2) { build(:assignment_questionnaire, id: 2, assignment_id: 1, questionnaire_id: 2) }
-  let(:questionnaire1) { build(:questionnaire, type: 'ReviewQuestionnaire') }
-  let(:questionnaire2) { build(:questionnaire, type: 'MetareviewQuestionnaire') }
-  let(:next_due_date) { build(:assignment_due_date, round: 1) }
 
   before(:each) do
     allow(review_response_map).to receive(:response).and_return(response)
@@ -41,12 +36,11 @@ describe MetareviewResponseMap do
     allow(AssignmentTeam).to receive(:find).with(reviewee_user).and_return(team)
     allow(AssignmentTeam).to receive(:users).and_return(student)
     allow(User).to receive(:find).with(reviewee_user).and_return(student)
-    allow(MetareviewResponseMap).to receive(:where).with(reviewed_object_id: 1).and_return([metareview_response_map])
-    metareview_response_map.reviewee_id = participant.id
-    defn = {body: {type: "Metareview", obj_name: assignment.name, first_name: student.fullname, partial_name: "new_submission"}, to: student.email}
-    
-    expect { metareview_response_map.email(defn, participant, assignment)}
-         .to change { ActionMailer::Base.deliveries.count }.by 1
-  end
+    allow(TeammateReviewResponseMap).to receive(:where).with(reviewed_object_id: 1).and_return([teammate_review_response_map])
+    teammate_review_response_map.reviewee_id = reviewee_user
+    defn = {body: {type: "Teammate Review", obj_name: assignment.name, first_name: student.fullname, partial_name: "new_submission"}, to: student.email}
 
+    expect { teammate_review_response_map.email(defn, participant, assignment)}
+        .to change { ActionMailer::Base.deliveries.count }.by 1
+  end
 end

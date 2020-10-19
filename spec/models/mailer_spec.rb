ea5fe5124296ddd7681ff766498dc70df588dd1f
Joshua Horwitz <jhorwitz@ebsco.com>
2020-10-19 01:39:22 -0500

    Rspec notify grade conflict message test (#5)
    * added test for notify_grade_conflict_message
    
    * fixed failing test for mailer
    
    * added test to response_spec
    
    * added context to new test
    
    * fixed typo in new test
    
    Co-authored-by: Joshua Horwitz <jhorwitz@Joshuas-MacBook-Pro.local>

--- spec/models/mailer_spec.rb
+++ spec/models/mailer_spec.rb
@@ -1,4 +1,8 @@
 describe 'Tests mailer' do
+  let(:assignment) {
+    build(:assignment, name: "test_assignment")
+  }
+
   it 'should send email to required email address with proper content ' do
     # Send the email, then test that it got queued
     email = Mailer.sync_message(
@@ -10,6 +14,28 @@ describe 'Tests mailer' do
         location: '1',
         first_name: 'User',
         partial_name: 'update'
+      }
+    ).deliver_now
+
+    expect(email.from[0]).to eq("expertiza.development@gmail.com")
+    expect(email.to[0]).to eq('expertiza.development@gmail.com')
+    expect(email.subject).to eq('Test')
+  end
+
+  it 'should send email to required email address when score is outside acceptable value ' do
+    # Send the email, then test that it got queued
+    email = Mailer.notify_grade_conflict_message(
+      to: 'tluo@ncsu.edu',
+      subject: "Test",
+      body: {
+        assignment: assignment,
+        type: 'review',
+        reviewer_name: 'Reviewer',
+        reviewee_name: 'Reviewee',
+        new_score: 0.95,
+        conflicting_response_url: 'https://expertiza.ncsu.edu/response/view?id=1',
+        summary_url: 'https://expertiza.ncsu.edu/grades/view_team?id=1',
+        assignment_edit_url: 'https://expertiza.ncsu.edu/assignments/1/edit'
       }
     ).deliver_now
 

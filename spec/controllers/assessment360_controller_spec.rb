a130744048dbf0216bab4153008736b12789e186
jmmacdo4 <jmmacdo4@ncsu.edu>
2020-10-19 20:59:35 -0400

    Added tests for all_students_all_reviews_all_grades

--- spec/controllers/assessment360_controller_spec.rb
+++ spec/controllers/assessment360_controller_spec.rb
@@ -292,6 +292,99 @@ describe Assessment360Controller do
     end
   end
 
+  describe '#all_students_all_reviews_all_grades' do
+    context 'when course does not have participants' do
+      before(:each) do
+        request.env['HTTP_REFERER'] = 'http://example.com'
+        get "all_students_all_reviews_all_grades"
+      end
+
+      it 'redirects to back' do
+        expect(response).to redirect_to(:back)
+      end
+
+      it 'flashes an error' do
+        expect(flash[:error]).to be_present
+      end
+    end
+
+    context 'method is called' do
+      before(:each) do
+        allow(Course).to receive(:find).with("1").and_return(course)
+        request.env['HTTP_REFERER'] = 'http://example.com'
+      end
+
+      it 'redirects to back and flashes error as there are no participants' do
+        allow(course).to receive(:assignments).and_return(assignment_list)
+        allow(assignment_list).to receive(:reject).and_return(assignment_list)
+        allow(course).to receive(:get_participants).and_return([]) # no participants
+        params = {course_id: 1, fields:['TeammateReviewScores','MetaReviewScores','NumberOfTeammates', 'InstructorAssignedScores','PeerGrades','Topics']}
+        session = {user: instructor}
+        get :all_students_all_reviews, params, session
+        expect(controller.send(:action_allowed?)).to be true
+        expect(response).to redirect_to(:back)
+        expect(flash[:error]).to be_present
+      end
+
+      it 'has participants, next assignment participant does not exist, and avoids divide by zero' do
+        allow(course).to receive(:assignments).and_return(assignment_list)
+        allow(assignment_list).to receive(:reject).and_return(assignment_list)
+        allow(course).to receive(:get_participants).and_return([course_participant]) # has participants
+        allow(StudentTask).to receive(:teamed_students).with(course_participant.user).and_return(student1)
+        params = {course_id: 1,fields:['TeammateReviewScores','MetaReviewScores','NumberOfTeammates', 'InstructorAssignedScores','PeerGrades','Topics']}
+        session = {user: instructor}
+        get :all_students_all_reviews, params, session
+        expect(controller.send(:action_allowed?)).to be true
+        expect(response.status).to eq(200)
+        expect(response).to render_template(:all_students_all_reviews)
+        returned_teammate_review = controller.instance_variable_get(:@teammate_review)
+        expect(returned_teammate_review[nil]).to eq({})
+        returned_meta_review = controller.instance_variable_get(:@meta_review)
+        expect(returned_meta_review[nil]).to eq({})
+      end
+
+      it 'has participants, next assignment participant exists, but there are no reviews' do
+        allow(course).to receive(:assignments).and_return(assignment_with_participants_list)
+        allow(assignment_with_participants_list).to receive(:reject).and_return(assignment_with_participants_list)
+        allow(course).to receive(:get_participants).and_return([course_participant]) # has participants
+        allow(StudentTask).to receive(:teamed_students).with(course_participant.user).and_return(student1)
+        allow(assignment_with_participants.participants).to receive(:find_by).with(user_id: course_participant.user_id).and_return(course_participant)
+        allow(course_participant).to receive(:teammate_reviews).and_return(empty_teammate_review)
+        allow(course_participant).to receive(:metareviews).and_return(empty_meta_review)
+        params = {course_id: 1,fields:['TeammateReviewScores','MetaReviewScores','NumberOfTeammates', 'InstructorAssignedScores','PeerGrades','Topics']}
+        session = {user: instructor}
+        get :all_students_all_reviews, params, session
+        expect(controller.send(:action_allowed?)).to be true
+        expect(response.status).to eq(200)
+        expect(response).to render_template(:all_students_all_reviews)
+        returned_teammate_review = controller.instance_variable_get(:@teammate_review)
+        expect(returned_teammate_review[nil]).to eq({})
+        returned_meta_review = controller.instance_variable_get(:@meta_review)
+        expect(returned_meta_review[nil]).to eq({})
+      end
+
+      it 'has participants, next assignment participant exists, but there are reviews' do
+        allow(course).to receive(:assignments).and_return(assignment_with_participants_list)
+        allow(assignment_with_participants_list).to receive(:reject).and_return(assignment_with_participants_list)
+        allow(course).to receive(:get_participants).and_return([course_participant]) # has participants
+        allow(StudentTask).to receive(:teamed_students).with(course_participant.user).and_return(student1)
+        allow(assignment_with_participants.participants).to receive(:find_by).with(user_id: course_participant.user_id).and_return(course_participant)
+        allow(course_participant).to receive(:teammate_reviews).and_return(teammate_review)
+        allow(course_participant).to receive(:metareviews).and_return(meta_review)
+        params = {course_id: 1,fields:['TeammateReviewScores','MetaReviewScores','NumberOfTeammates', 'InstructorAssignedScores','PeerGrades','Topics']}
+        session = {user: instructor}
+        get :all_students_all_reviews, params, session
+        expect(controller.send(:action_allowed?)).to be true
+        expect(response.status).to eq(200)
+        expect(response).to render_template(:all_students_all_reviews)
+        returned_teammate_review = controller.instance_variable_get(:@teammate_review)
+        expect(returned_teammate_review[nil][1]).to eq("95%")
+        returned_meta_review = controller.instance_variable_get(:@meta_review)
+        expect(returned_meta_review[nil][1]).to eq("100%")
+      end
+    end
+  end
+
   describe '#insure_existence_of' do
     context 'checking if the course participants are empty' do
       before(:each) do

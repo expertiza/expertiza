fdec3848823d8670d2d6919e234e63695d710fe8
sushreeta2106 <sushreeta21@gmail.com>
2020-11-12 22:55:22 -0500

    Added RSpec test cases for conference controller and recaptcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,6 @@
 Recaptcha.configure do |config|
-  export RECAPTCHA_SITE_KEY   = '6Lfb_uEZAAAAAOrVHOhLI7i2yewc3Fgr87IohvaH'
-  export RECAPTCHA_SECRET_KEY = '6Lfb_uEZAAAAAPcSk-9fcNh3syzfvfagPeNc8Y_B'
+  # export RECAPTCHA_SITE_KEY   = '6Lfb_uEZAAAAAOrVHOhLI7i2yewc3Fgr87IohvaH'
+  # export RECAPTCHA_SECRET_KEY = '6Lfb_uEZAAAAAPcSk-9fcNh3syzfvfagPeNc8Y_B'
   config.site_key  = '6Lfb_uEZAAAAAOrVHOhLI7i2yewc3Fgr87IohvaH'
   config.secret_key = '6Lfb_uEZAAAAAPcSk-9fcNh3syzfvfagPeNc8Y_B'
   # config.site_key  = '6LcFcRUUAAAAAP0aQmJmT0299FLzCPyVExZYlOUv'

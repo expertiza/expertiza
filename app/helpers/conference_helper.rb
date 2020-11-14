6c5c00d1235beb614094eb320dab4c8ddad20c84
palash03 <thisispalash@gmail.com>
2020-11-13 19:47:24 -0500

    create author for conference helper function

--- app/helpers/conference_helper.rb
+++ app/helpers/conference_helper.rb
@@ -38,4 +38,38 @@ module ConferenceHelper
             return '/'
         end
     end
+
+    def create_author
+        print("/n In create Author/n")
+        params[:user][:name] = params[:user][:email] unless !params[:user][:name].nil? and !params[:user][:name].empty?
+        @user = User.new(user_params)
+        # parent id for a conference user will be conference assignment instructor id
+        @user.parent_id = Assignment.find(params[:user][:assignment]).instructor.id
+        @assignment = Assignment.find(params[:user][:assignment])
+        # set the user's timezone to its parent's
+        @user.timezonepref = User.find(@user.parent_id).timezonepref
+        # set default value for institute
+        @user.institution_id = nil
+        if @user.save
+          password = @user.reset_password # the password is reset
+          #Mail to be sent to Author once the user has been created. New partial is used as content for email is different from normal user
+          print("\\n Create author email "+@user.email)
+          prepared_mail = MailerHelper.send_mail_for_conference_user(@user, "Your Expertiza account and password have been created.", "author_conference_invitation", password, @assignment.name)
+          prepared_mail.deliver_now
+          print('\n ----------------passwored is---------]\n')
+          print(password)
+          print('\n ----------------passwored is---------\n')
+          flash[:success] = "A new password has been sent to new user's e-mail address."
+        else
+          raise "Error occurred while creating expertiza account."
+        end
+    end
+    
+    def user_params
+        params.require(:user).permit(:name,
+                                     :role_id,
+                                     :email,
+                                     :parent_id,
+                                     :institution_id)
+    end
 end

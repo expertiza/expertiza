b94e81a88b1543d2ff30c53a7ff4ede151146413
palash03 <thisispalash@gmail.com>
2020-11-13 19:45:16 -0500

    added create_conference_user in conf controller

--- app/controllers/conference_controller.rb
+++ app/controllers/conference_controller.rb
@@ -38,4 +38,39 @@ class ConferenceController < ApplicationController
             @all_roles = Role.where('id in (?) or id = ?', role.get_available_roles, role.id)
         end
     end
+    def create
+      print('In conference create')
+
+      # Check if user needs to be created as author for conference type assignment and add author to assignment
+      @recaptcha_succeeded = verify_recaptcha secret_key: '6Lfb_uEZAAAAAPcSk-9fcNh3syzfvfagPeNc8Y_B'
+      print("\\n")
+      print(@recaptcha_succeeded)
+      if @recaptcha_succeeded==true && create_conference_user
+        add_conference_user_as_participant
+      else
+        print(@all_roles)
+        print('in else')
+        print(@rolename)
+        redirect_to :controller => 'conference', :action => 'new'
+      end
+  end
+
+  def create_conference_user
+      # check if user is already present with given username in system
+      existing_user = User.find_by(name: params[:user][:name])
+      # if user exist then add user as participant to assignment else create account and then add as participant
+      if existing_user.nil?
+        if (params[:user][:name].nil? or params[:user][:name].empty?)and !User.find_by(name: params[:user][:email]).nil?
+          flash[:error] = "A user with username of this email already exists, Please provide a unique username to continue."
+          redirect_to request.referrer
+          return false
+        end
+        # Create author called from Conference Helper
+        create_author
+      else
+          print("\nIn else of create conf\n")
+        @user = existing_user
+      end
+  end
+
 end

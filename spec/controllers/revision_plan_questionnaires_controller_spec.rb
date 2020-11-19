ef8bf3d7afcd565c1ca67e32b092ca2f371ed10d
vickyyang5454 <vickeyyang5454@gmail.com>
2020-11-19 08:39:23 -0500

    Add controller test for Revision Plan Questionnaire

---
+++ spec/controllers/revision_plan_questionnaires_controller_spec.rb
@@ -0,0 +1,79 @@
+describe RevisionPlanQuestionnairesController do
+    let(:questionnaire) { build(:questionnaire,id:1) }
+    let(:revision_plan_questionnaire) { build(:revision_plan_questionnaire, questionnaire_id: 1, team_id: 1) }
+    let(:team) { create(:assignment_team, id: 1, parent_id: 1) }
+    let(:question) { build(:question, id: 1, user: {}) }
+    let(:admin) { build(:admin) }
+    let(:instructor) { build(:instructor, id: 6) }
+    let(:student) { build(:student, id: 3) }
+    let(:student2) { build(:student, id: 4) }
+    let(:assignment) { build(:assignment, id: 1, instructor_id: 6, microtask: true, staggered_deadline: true) }
+    #let(:due_date) { build(:assignment_due_date, deadline_type_id: 1) }
+    #let(:due_date2) { build(:assignment_due_date, deadline_type_id: 2) }
+    before(:each) do
+      allow(Questionnaire).to receive(:find).with(any_args).and_return(questionnaire)
+      allow(RevisionPlanQuestionnaire).to receive(:find).with(any_args).and_return(questionnaire)
+      allow(User).to receive(:find).with(1).and_return(student)
+      controller.params = {id: 1,team_id: 1}
+      allow(Team).to receive(:find).with(any_args).and_return(team)
+      #allow(TeamsUser).to receive(:where).with(team: Team).and_return([student])
+      allow(TeamsUser).to receive(:where).with(any_args).and_return(student)
+    end
+  
+    def check_access username
+      stub_current_user(username, username.role.name, username.role)
+      expect(controller.send(:action_allowed?))
+    end
+  
+    describe '#action_allowed?' do
+  
+      context 'when params action is edit or update' do
+  
+        before(:each) do
+          controller.params = {id: '1', action: 'edit',team_id: 1}
+          controller.request.session[:user] = student
+        end
+  
+        context 'when the role name of current user is super admin or admin' do
+          it 'allows certain action' do
+            check_access(admin).to be true
+          end
+        end
+  
+        context 'when current user is the student of current questionnaires' do
+          it 'allows certain action' do
+            check_access(student).to be true
+          end
+        end
+  
+        context 'when current user is a student but not of the team' do
+          it 'does not allow certain action' do
+            check_access(student2).to be false
+          end
+        end
+      end
+
+      context 'when params action is not edit and update' do
+        before(:each) do
+          controller.params = {id: '1', action: 'new',team_id: 1}
+        end
+  
+        context 'when the role current user is super admin/admin/instructor/ta' do
+          it 'allows certain action except edit and update' do
+            check_access(admin).to be true
+          end
+        end
+      end
+    end
+
+    describe '#new' do
+
+        it 'creates new questionnaire object and renders questionnaires#new page' do
+            allow(Assignment).to receive(:number_of_current_round).with(topic_id:8 ).and_return(2)    
+            allow(Questionnaire).to receive(:get_questionnaire_for_current_round).with( 1).and_return(2) 
+            params = {id: 1, questionnaire_id: 1, team_id: 1}   
+            get :new, params
+            expect(response).to redirect_to('/questionnaires/1/edit')
+        end
+    end
+end

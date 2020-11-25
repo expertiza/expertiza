b2048f71addfbfc5761758e5d3aa4afa51ba1836
sushreeta2106 <sushreeta21@gmail.com>
2020-11-25 15:27:27 -0500

    Updated assignments schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201125191650) do
+ActiveRecord::Schema.define(version: 20201125202200) do
 
   create_table "account_requests", force: :cascade do |t|
     t.string   "name",              limit: 255

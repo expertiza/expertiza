1f10d4116c87ee3ae5c3a3fe3f0d1c6bf0345bfa
Dhanraj Vedanth Raghunathan <56409601+dhanraj-vedanth@users.noreply.github.com>
2020-11-26 13:17:57 -0500

    Update submission_record.rb

--- app/models/submission_record.rb
+++ app/models/submission_record.rb
@@ -4,4 +4,19 @@ class SubmissionRecord < ActiveRecord::Base
   validates :team_id, presence: true
   validates :user, presence: true
   validates :assignment_id, presence: true
+
+  def self.copycalibratedsubmissions(old_assign, new_assign_id)
+    @prev_submission_record = SubmissionRecord.where(assignment_id: old_assign.id)
+    @prev_submission_record.each do |catt|
+      @new_submission_record = SubmissionRecord.new
+      @new_submission_record.type = catt.type
+      @new_submission_record.content = catt.content
+      @new_submission_record.operation = catt.operation
+      @new_submission_record.team_id = catt.team_id
+      @new_submission_record.user = catt.user
+      @new_submission_record.assignment_id = new_assign_id
+      @new_submission_record.save
+    end
+  end
+
 end

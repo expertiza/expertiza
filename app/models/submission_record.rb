0db622f4c51cad4b70a9d852d275a0de71683e33
Saurabh Shingte <saurabh.shingte@gmail.com>
2020-12-17 09:24:20 -0500

    Fix #1833 by checking submission_records

--- app/models/submission_record.rb
+++ app/models/submission_record.rb
@@ -4,4 +4,8 @@ class SubmissionRecord < ActiveRecord::Base
   validates :team_id, presence: true
   validates :user, presence: true
   validates :assignment_id, presence: true
+
+  def self.file_submissions_present?(assignment_id)
+    SubmissionRecord.where(:assignment_id => assignment_id, :operation => "Submit File").present?
+  end
 end

f3c9e01309d58cdc8b0cc51418e814a0e878519c
John Bumgardner <john.bumgardner@icloud.com>
2021-01-25 09:59:04 -0500

    delete tests from model

--- spec/models/response_spec.rb
+++ spec/models/response_spec.rb
@@ -193,19 +193,4 @@ describe Response do
       end
     end
   end
-
-  describe '#get_sentences' do
-    context 'when the answer is nil' do
-      it 'returns a nil object' do
-        expect(SummaryHelper.get_sentences(nil)).to eq(nil)
-      end
-    end
-    context 'when the comment is two sentences' do
-      it 'returns an array of two sentences' do
-        allow(answer). to receive(:comments).and_return("This is a sentence. This is anohter sentence.")
-        sentences = SummaryHelper.get_sentences(answer)
-        expect(sentences.length).to be(2)
-      end
-    end
-  end
 end

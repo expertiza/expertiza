f82a770460a692148b7ca616eb119849a2e892c8
John Bumgardner <john.bumgardner@icloud.com>
2021-01-25 09:23:09 -0500

    better tests

--- spec/models/response_spec.rb
+++ spec/models/response_spec.rb
@@ -195,7 +195,14 @@ describe Response do
   describe '#get_sentences' do
     context 'when the answer is nil' do
       it 'returns a nil object' do
-        expect(SummaryHelper.get_sentences(nil).to eq(nil))
+        expect(SummaryHelper.get_sentences(nil)).to eq(nil)
+      end
+    end
+    context 'when the comment is two sentences' do
+      it 'returns an array of two sentences' do
+        allow(ans). to receive(:comments).and_return("This is a sentence. This is anohter sentence.")
+        sentences = SummaryHelper.get_sentences(ans)
+        expect(sentences.length).to be(2)
       end
     end
   end

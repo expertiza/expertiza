20170f37cc66b670c0e6fa9e25ca91cfc6d97143
John Bumgardner <john.bumgardner@icloud.com>
2021-01-25 10:10:05 -0500

    fix some syntax

--- spec/helpers/summary_helper_spec.rb
+++ spec/helpers/summary_helper_spec.rb
@@ -1,15 +1,14 @@
 describe "SummaryHelper" do
-	let(:answer) { Answer.new(answer: 1, comments: 'Answer text', question_id: 1) }
+	let(:answer) { Answer.new(answer: 1, comments: 'This is a sentence. This is anohter sentence.', question_id: 1) }
 	describe '#get_sentences' do
   	context 'when the answer is nil' do
       it 'returns a nil object' do
-        expect(SummaryHelper.get_sentences(nil)).to eq(nil)
+        expect(get_sentences(nil)).to eq(nil)
       end
     end
     context 'when the comment is two sentences' do
       it 'returns an array of two sentences' do
-        allow(answer). to receive(:comments).and_return("This is a sentence. This is anohter sentence.")
-        sentences = SummaryHelper.get_sentences(answer)
+        sentences = get_sentences(answer)
         expect(sentences.length).to be(2)
       end
     end

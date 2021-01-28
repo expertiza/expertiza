754986e508e61ec21241d762df961ebad668fbe0
John Bumgardner <john.bumgardner@icloud.com>
2021-01-28 08:22:32 -0500

    remove nil as it will be returned by default

--- app/models/on_the_fly_calc.rb
+++ app/models/on_the_fly_calc.rb
@@ -55,8 +55,6 @@ def peer_review_questions_for_team(team, round_number = nil)
     topic_id = SignedUpTeam.find_by(team_id: team.id).topic_id
     review_questionnaire_id = review_questionnaire_id(round_number, topic_id)
     Question.where(questionnaire_id: review_questionnaire_id)
-  else
-    nil
   end
 end
 

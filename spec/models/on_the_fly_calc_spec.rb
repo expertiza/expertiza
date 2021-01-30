ba34088b38eb741470e10551f02ba0ca83bd8abd
John Bumgardner <john.bumgardner@icloud.com>
2021-01-29 19:12:42 -0500

    use send to access privatemethod

--- spec/models/on_the_fly_calc_spec.rb
+++ spec/models/on_the_fly_calc_spec.rb
@@ -78,7 +78,7 @@ describe OnTheFlyCalc do
   describe '#peer_review_questions_for_team' do
     context 'when there is no signed up team' do
       it 'peer review questions should return nil' do
-        expect(on_the_fly_calc.peer_review_questions_for_team(nil).to be nil)
+        expect(on_the_fly_calc.send(:peer_review_questions_for_team, nil).to be nil)
       end
     end
   end

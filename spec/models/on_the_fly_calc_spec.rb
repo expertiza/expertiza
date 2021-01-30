c6801a69163a15fe434304d72a98848b8b5c196e
John Bumgardner <john.bumgardner@icloud.com>
2021-01-29 19:17:55 -0500

    fix the test

--- spec/models/on_the_fly_calc_spec.rb
+++ spec/models/on_the_fly_calc_spec.rb
@@ -78,8 +78,8 @@ describe OnTheFlyCalc do
   describe '#peer_review_questions_for_team' do
     context 'when there is no signed up team' do
       it 'peer review questions should return nil' do
-        on_the_fly_calc.send(:public, *on_the_fly_calc.private_instance_methods)
-        expect(on_the_fly_calc.peer_review_questions_for_team(nil).to be nil)
+        val = on_the_fly_calc.send(:peer_review_questions_for_team, nil)
+        expect(val) to be nil
       end
     end
   end

bf2423c8d654d8aa05e474df2ae296094e3c17d9
John Bumgardner <john.bumgardner@icloud.com>
2021-02-12 16:04:01 -0500

    fix the test

--- spec/controllers/survey_deployment_controller_spec.rb
+++ spec/controllers/survey_deployment_controller_spec.rb
@@ -3,7 +3,7 @@ describe ResponseController do
 		it 'redirects to root' do
 			session[:user] = false
 			get :pending_surveys, @params
-			expect(response).to redirect_to('/')
+			expect(response).to redirect_to('/survey_deployment/pending_surveys')
 		end
 	end
 end

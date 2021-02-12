92d9a3fe678def77964ad0f93e9e88a17dbed473
John Bumgardner <john.bumgardner@icloud.com>
2021-02-12 16:10:54 -0500

    delete the session

--- spec/controllers/survey_deployment_controller_spec.rb
+++ spec/controllers/survey_deployment_controller_spec.rb
@@ -2,7 +2,6 @@ describe SurveyDeploymentController do
 	describe '#pending_surveys' do
 		context 'when session[:user] is invalid' do
 			it 'redirects to root' do
-				session[:user] = false
 				get :pending_surveys, @params
 				expect(response).to redirect_to('/survey_deployment/pending_surveys')
 			end

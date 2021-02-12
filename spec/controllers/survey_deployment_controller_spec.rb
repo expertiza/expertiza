fdd736c254c05c6d37fadbe5fe16ea2724a16357
John Bumgardner <john.bumgardner@icloud.com>
2021-02-12 16:49:56 -0500

    write test for action allowed with an instructor

--- spec/controllers/survey_deployment_controller_spec.rb
+++ spec/controllers/survey_deployment_controller_spec.rb
@@ -1,4 +1,15 @@
 describe SurveyDeploymentController do
+	let(:instructor) { build(:instructor, id: 6) }
+	describe '#action_allowed?' do
+		context 'when the user is a instructor' do
+			it 'returns true' do
+				session[:user] = instructor
+				expect(controller.send(:action_allowed?)).to be true
+			end
+		end
+	end
+
+
 	describe '#pending_surveys' do
 		context 'when session[:user] is invalid' do
 			it 'redirects to root' do

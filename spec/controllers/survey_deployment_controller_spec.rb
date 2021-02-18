0473c0e9f150fb7325566b21c214d28e58981c18
John Bumgardner <john.bumgardner@icloud.com>
2021-02-18 11:01:01 -0500

    fix syntax

--- spec/controllers/survey_deployment_controller_spec.rb
+++ spec/controllers/survey_deployment_controller_spec.rb
@@ -93,6 +93,7 @@ describe '#param_test' do
 			controller.params = ActionController::Parameters.new(survey_deployment: {questionnaire_id: 1, start_date: 2, end_date: 3, parent_id: 4})
 			get :param_test, controller.params
 			expect(controller.params.permitted?).to be true
+		end
 	end
 end
 

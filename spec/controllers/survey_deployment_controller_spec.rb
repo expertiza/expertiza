b4230b9a781ef9c6597eabface34de4efbe0b650
John Bumgardner <john.bumgardner@icloud.com>
2021-02-18 11:09:57 -0500

    use valid times

--- spec/controllers/survey_deployment_controller_spec.rb
+++ spec/controllers/survey_deployment_controller_spec.rb
@@ -90,7 +90,7 @@ describe '#param_test' do
 	end
 	context 'params is valid' do
 		it 'is now permitted' do
-			controller.params = ActionController::Parameters.new(survey_deployment: {questionnaire_id: 1, start_date: DateTime.new, end_date: DateTime.new, parent_id: 4})
+			controller.params = ActionController::Parameters.new(survey_deployment: {questionnaire_id: Integer.new, start_date: DateTime.now, end_date: DateTime.now.new_offset('+09:00'), parent_id: Integer.new})
 			get :param_test, controller.params
 			expect(controller.params.permitted?).to be true
 		end

ebb37cddf2141a5949aa2a1d06c810fa03c54daf
John Bumgardner <john.bumgardner@icloud.com>
2021-02-18 12:23:51 -0500

    add test for create and fix whitespace issue

--- spec/controllers/survey_deployment_controller_spec.rb
+++ spec/controllers/survey_deployment_controller_spec.rb
@@ -20,90 +20,105 @@ describe SurveyDeploymentController do
 		end
 	end
 
-describe '#survey_deployment_types' do
-	context 'when the function is called' do
-		it 'returns the types of survey deployments' do
-			expect(controller.send(:survey_deployment_types)).to eq %w[AssignmentSurveyDeployment
-       CourseSurveyDeployment]
+	describe '#survey_deployment_types' do
+		context 'when the function is called' do
+			it 'returns the types of survey deployments' do
+				expect(controller.send(:survey_deployment_types)).to eq %w[AssignmentSurveyDeployment
+       	CourseSurveyDeployment]
+			end
 		end
 	end
-end
 
-describe '#survey_deployment_type' do
-	context 'when the assignment is an Assignment Survey Deployment' do
-		it 'a constantized AssignmentSurveyDeployment is returned' do
-			controller.params[:type] = 'AssignmentSurveyDeployment'
-			expect(controller.send(:survey_deployment_type)).to eq controller.params[:type].constantize
+	describe '#survey_deployment_type' do
+		context 'when the assignment is an Assignment Survey Deployment' do
+			it 'a constantized AssignmentSurveyDeployment is returned' do
+				controller.params[:type] = 'AssignmentSurveyDeployment'
+				expect(controller.send(:survey_deployment_type)).to eq controller.params[:type].constantize
+			end
+		end
+		context 'when the assignment is an Course Survey Deployment' do
+			it 'a constantized CourseSurveyDeployment is returned' do
+				controller.params[:type] = 'CourseSurveyDeployment'
+				expect(controller.send(:survey_deployment_type)).to eq controller.params[:type].constantize
+			end
+		end
+		context 'when the assignment is an invalid Survey Deployment' do
+			it 'returns nil' do
+				controller.params[:type] = 'InvalidSurveyDeployment'
+				expect(controller.send(:survey_deployment_type)).to be nil
+			end
 		end
 	end
-	context 'when the assignment is an Course Survey Deployment' do
-		it 'a constantized CourseSurveyDeployment is returned' do
-			controller.params[:type] = 'CourseSurveyDeployment'
-			expect(controller.send(:survey_deployment_type)).to eq controller.params[:type].constantize
-		end
-	end
-	context 'when the assignment is an invalid Survey Deployment' do
-		it 'returns nil' do
-			controller.params[:type] = 'InvalidSurveyDeployment'
-			expect(controller.send(:survey_deployment_type)).to be nil
-		end
-	end
-end
 
-describe '#new' do
-	context 'when you try to make an invalid survey' do
-		it 'generates an error' do
-			controller.params[:type] = 'InvalidSurveyDeployment'
-			get :new, controller.params
-			expect(flash[:error]).to be_present
+	describe '#new' do
+		context 'when you try to make an invalid survey' do
+			it 'generates an error' do
+				controller.params[:type] = 'InvalidSurveyDeployment'
+				get :new, controller.params
+				expect(flash[:error]).to be_present
+			end
+		end
+		context 'when you try to create an Assignment Survey Deployment' do
+			it 'creates an assignment survey deployment' do
+				allow(Assignment).to receive(:find).with('1').and_return(assignment)
+				controller.params[:type] = 'AssignmentSurveyDeployment'
+				controller.params[:id] = 1
+    		session = {user: instructor}
+				get :new, controller.params, session
+				expect(response).to render_template(:new)
+			end
+		end
+		context 'when you try to create an Course Survey Deployment' do
+			it 'creates an course survey deployment' do
+				allow(Course).to receive(:find).with('1').and_return(course)
+				controller.params[:type] = 'CourseSurveyDeployment'
+				controller.params[:id] = 1
+    		session = {user: instructor}
+				get :new, controller.params, session
+				expect(response).to render_template(:new)
+			end
 		end
 	end
-	context 'when you try to create an Assignment Survey Deployment' do
-		it 'creates an assignment survey deployment' do
-			allow(Assignment).to receive(:find).with('1').and_return(assignment)
-			controller.params[:type] = 'AssignmentSurveyDeployment'
-			controller.params[:id] = 1
-    	session = {user: instructor}
-			get :new, controller.params, session
-			expect(response).to render_template(:new)
-		end
-	end
-	context 'when you try to create an Course Survey Deployment' do
-		it 'creates an course survey deployment' do
-			allow(Course).to receive(:find).with('1').and_return(course)
-			controller.params[:type] = 'CourseSurveyDeployment'
-			controller.params[:id] = 1
-    	session = {user: instructor}
-			get :new, controller.params, session
-			expect(response).to render_template(:new)
-		end
-	end
-end
 
-describe '#param_test' do
-	context 'params is nil' do
-		it 'remains unpermitted' do
-			controller.params = ActionController::Parameters.new(survey_deployment: nil)
-			get :param_test, controller.params
-			expect(controller.params.permitted?).to be false
+	describe '#param_test' do
+		context 'params is nil' do
+			it 'remains unpermitted' do
+				controller.params = ActionController::Parameters.new(survey_deployment: nil)
+				get :param_test, controller.params
+				expect(controller.params.permitted?).to be false
+			end
+		end
+		context 'params is valid' do
+			it 'is now permitted' do
+				controller.params = ActionController::Parameters.new(
+					survey_deployment: {
+						questionnaire_id: 1, 
+						start_date: DateTime.now, 
+						end_date: DateTime.now.new_offset('+09:00'), 
+						parent_id: 1
+					}
+				)
+				val = controller.param_test
+				expect(val.permitted?).to be true
+			end
 		end
 	end
-	context 'params is valid' do
-		it 'is now permitted' do
-			controller.params = ActionController::Parameters.new(
-				survey_deployment: {
-					questionnaire_id: 1, 
-					start_date: DateTime.now, 
-					end_date: DateTime.now.new_offset('+09:00'), 
-					parent_id: 1
-				}
-			)
-			val = controller.param_test
-			expect(val.permitted?).to be true
-		end
-	end
-end
 
+	describe '#create' do
+		context "creating a survey deployment" do
+			it 'increments count of survey deployment by one' do
+				controller.params = ActionController::Parameters.new(
+					survey_deployment: {
+						questionnaire_id: 1, 
+						start_date: DateTime.now, 
+						end_date: DateTime.now.new_offset('+09:00'), 
+						parent_id: 1
+					}
+				)
+				expect {post :create, controller.params}. to change(SurveyDeployment, :count).by(1)
+			end
+		end 
+	end
 
 	describe '#pending_surveys' do
 		context 'when session[:user] is invalid' do

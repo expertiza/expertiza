ad94e8311dda24362244a58e0fce10957f6edc50
John Bumgardner <john.bumgardner@icloud.com>
2021-02-19 01:35:31 -0500

    fix spelling error in reviewee_id

--- spec/controllers/survey_deployment_controller_spec.rb
+++ spec/controllers/survey_deployment_controller_spec.rb
@@ -211,7 +211,7 @@ describe SurveyDeploymentController do
 				allow(survey_deployment).to receive(:questionnaire_id).and_return('1')
 				allow(Questionnaire).to receive(:find).with('1').and_return(questionnaire1)
 				allow(Question).to receive(:where).with(questionnaire_id: questionnaire1.id).and_return([question])
-				allow(ResponseMap).to receive(:where).with(eviewee_id: '1').and_return(review_response_map)
+				allow(ResponseMap).to receive(:where).with(reviewee_id: '1').and_return(review_response_map)
 				stub_current_user(instructor, instructor.role.name, instructor.role)
 				session = {user: instructor}
 				get :view_responses, params, session

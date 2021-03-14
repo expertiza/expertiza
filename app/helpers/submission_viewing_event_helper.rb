f318dc29cc24d7042c1b03cb087c8a86e3843d1f
Jordan B <jb@boergerfamily.com>
2021-03-14 19:05:12 -0400

    Adding body to getTotalTime

--- app/helpers/submission_viewing_event_helper.rb
+++ app/helpers/submission_viewing_event_helper.rb
@@ -1,6 +1,16 @@
 module SubmissionViewingEventHelper
+
   def getTotalTime(response_map_id, round)
+
     timeLog = SubmissionViewingEvent.where(map_id: response_map_id, round: round)
+    totalTime = 0;
+
+    timeLog.each do |linkTime|
+      totalTime = totalTime + linkTime.end_at - linkTime.start_at
+    end
+
+    return totalTime
 
   end
+
 end

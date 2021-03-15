eea6c349ba202c6e8478a2bf57017c384a72ff86
Ryan Smith <avlwx2014@gmail.com>
2021-03-15 19:59:58 -0400

    Add default value of 0; add not null constraint

--- db/migrate/20210315184532_update_review_time_schema.rb
+++ db/migrate/20210315184532_update_review_time_schema.rb
@@ -1,5 +1,5 @@
 class UpdateReviewTimeSchema < ActiveRecord::Migration
   def change
-    add_column :submission_viewing_events, :total_time, :integer
+    add_column :submission_viewing_events, :total_time, :integer, :default => 0, column_options: { null: false }
   end
 end

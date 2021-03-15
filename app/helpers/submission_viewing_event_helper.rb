8014807c95216bfec17b91940285bfa178a62a70
Jordan B <jb@boergerfamily.com>
2021-03-15 15:02:11 -0400

    Improved readability

--- app/helpers/submission_viewing_event_helper.rb
+++ app/helpers/submission_viewing_event_helper.rb
@@ -1,15 +1,18 @@
 module SubmissionViewingEventHelper
 
+  # Returns number of seconds the review given at response_map_id took during given round
   def getTotalTime(response_map_id, round)
 
+    # Finds submission_viewing_event rows corresponding to map and round given
     timeLog = SubmissionViewingEvent.where(map_id: response_map_id, round: round)
-    totalTime = 0;
+    totalSeconds = 0; # Storage for number of seconds
 
+    # Subtracts start_time from end_time for each link and adds to total number of seconds
     timeLog.each do |linkTime|
-      totalTime = totalTime + (linkTime.end_at - linkTime.start_at).to_i
+      totalSeconds = totalSeconds + (linkTime.end_at - linkTime.start_at).to_i
     end
 
-    return totalTime
+    return totalSeconds
 
   end
 

e5cb2b3216d29995e620b5c27dc7da5cf7bce8a2
Jordan B <jb@boergerfamily.com>
2021-03-17 11:59:29 -0400

    Refactor getTotalTime to remove local variable

--- app/helpers/submission_viewing_event_helper.rb
+++ app/helpers/submission_viewing_event_helper.rb
@@ -2,14 +2,11 @@ module SubmissionViewingEventHelper
 
   # Returns number of seconds the review given at response_map_id took during given round
   def getTotalTime(response_map_id, round)
-
-    # Finds submission_viewing_event rows corresponding to map and round given
-    timeLog = SubmissionViewingEvent.where(map_id: response_map_id, round: round)
     totalSeconds = 0; # Storage for number of seconds
 
-    # Subtracts start_time from end_time for each link and adds to total number of seconds
-    timeLog.each do |linkTime|
-      totalSeconds = totalSeconds + (linkTime.end_at - linkTime.start_at).to_i
+    # Finds submission_viewing_event rows corresponding to map and round given
+    SubmissionViewingEvent.where(map_id: response_map_id, round: round).each do |linkTime|
+      totalSeconds = totalSeconds + (linkTime.end_at - linkTime.start_at).to_i # accumulates total time for review
     end
 
     return totalSeconds

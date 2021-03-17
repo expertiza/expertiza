2fd0ae346da972196a93d1610f74e9905892a949
Ryan Smith <avlwx2014@gmail.com>
2021-03-17 19:14:21 -0400

    Add entrypoints for new routes in JS

--- app/assets/javascripts/review_time.js
+++ app/assets/javascripts/review_time.js
@@ -65,3 +65,106 @@ function markEndTime(map_id, round) {
         })
     });
 }
+
+/**
+ * Signal the submission viewing even controller to start timing
+ * for a particular review asset.
+ * @param map_id The ID of the review map from Expertiza
+ * @param round The Review Round e.g. 1, 2, etc
+ * @param link The asset being review.
+ */
+function startTime2(map_id, round, link) {
+    $.ajax({
+        type: "POST",
+        url: '/submission_viewing_events/record_start_time2',
+        data: $.param({
+            map_id: map_id,
+            round: round,
+            link: link
+        })
+    });
+}
+
+/**
+ * Signal the submission viewing even controller to stop timing
+ * for a particular review asset.
+ * @param map_id The ID of the review map from Expertiza
+ * @param round The Review Round e.g. 1, 2, etc
+ * @param link The asset being review.
+ */
+function endTime2(map_id, round, link) {
+    var data = {
+        map_id: map_id,
+        round: round
+    };
+
+    if (link) {
+        data.link = link;
+    }
+
+    $.ajax({
+        type: 'POST',
+        dataType: 'json',
+        async: false,
+        url: '/submission_viewing_events/record_end_time',
+        data: $.param(data)
+    });
+}
+
+/**
+ * Signal the submission viewing event controller to stop
+ * timing for all open review assets.
+ * @param map_id The ID of the review map from Expertiza
+ * @param round The Review Round e.g. 1, 2, etc
+ */
+function saveAll(map_id, round) {
+    $.ajax({
+        type: 'POST',
+        dataType: 'json',
+        async: false,
+        url: '/submission_viewing_events/hard_save',
+        data: $.param({
+            map_id: map_id,
+            round: round
+        })
+    });
+}
+
+function endRound(map_id, round) {
+    $.ajax({
+        type: 'POST',
+        dataType: 'json',
+        async: false,
+        url: '/submission_viewing_events/end_timing',
+        data: $.param({
+            map_id: map_id,
+            round: round
+        })
+    });
+}
+
+function saveAndEndRound(map_id, round) {
+    $.ajax({
+        type: 'POST',
+        dataType: 'json',
+        async: false,
+        url: '/submission_viewing_events/end_round_and_save',
+        data: $.param({
+            map_id: map_id,
+            round: round
+        })
+    });
+}
+
+function resetRound(map_id, round) {
+    $.ajax({
+        type: 'POST',
+        dataType: 'json',
+        async: false,
+        url: '/submission_viewing_events/reset_timing',
+        data: $.param({
+            map_id: map_id,
+            round: round
+        })
+    });
+}

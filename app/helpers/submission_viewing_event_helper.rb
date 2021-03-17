95358959376622efde80992188bbbc5a41a1cc14
Jordan B <jb@boergerfamily.com>
2021-03-17 11:49:00 -0400

    Created getAvgClassTime

--- app/helpers/submission_viewing_event_helper.rb
+++ app/helpers/submission_viewing_event_helper.rb
@@ -31,4 +31,26 @@ module SubmissionViewingEventHelper
     # Returns string of readable text
     return  "#{hours} hr #{mins} min #{timeInSec} sec"
   end
+
+  # Returns average time taken for a review of an assignment for a specific round given a response_map_id in that assignment and a round
+  def getAvgRevTime(response_map_id, round)
+    classTimes = [] # holds total review times for each reviewer
+    reviewTotalTime = 0 # used to sum the time taken for one review
+
+    # Gets the assignmentId for the given response_map
+    assignmentId = ResponseMap.find(params[:reponse_map_id]).reviewed_object_id
+
+    # Iterates through the ResponseMap table to get maps pertaining to particular assignment
+    ResponseMap.where(reviewed_object_id: assignmentId).each do |map|
+      # Iterates through SubmissionViewingEvent table to get rows pertaining to the current map.id
+      SubmissionViewingEvent.where(map_id: map.id, round: params[:round]).each do |event|
+        reviewTotalTime = reviewTotalTime + event.total_time # sums the total time spent on that review
+      end
+
+      classTimes.push(reviewTotalTime) # pushes summed review time onto array
+      reviewTotalTime = 0 # resets sum of review time
+    end
+
+    return classTimes.reduce(:+).to_f / classTimes.size # returns average of classTimes array
+  end
 end

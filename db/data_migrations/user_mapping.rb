934143ba13af9d56396a99285dbf228f0f219d1f
John Bumgardner <john.bumgardner@icloud.com>
2021-03-19 16:37:02 -0400

    fix file formatting

--- db/data_migrations/user_mapping.rb
+++ db/data_migrations/user_mapping.rb
@@ -11,7 +11,7 @@ class UserMapping
 			end
 			user_id = user.id
 			scrubbed_name = "#{user.role.name.downcase.gsub(/[- ]/,'_')}#{user.id}"
-			File.write("user_mapping.csv", user_id.to_s + ", " + scrubbed_name, mode: "a")
+			File.write("user_mapping.csv", user_id.to_s + ", " + scrubbed_name + ",\n", mode: "a")
 		end
 	file.close
 	end

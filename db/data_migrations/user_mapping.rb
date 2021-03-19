0a955a13edb96f898b4b16d51143ff87968ef642
John Bumgardner <john.bumgardner@icloud.com>
2021-03-19 16:38:37 -0400

    formatting fix for header

--- db/data_migrations/user_mapping.rb
+++ db/data_migrations/user_mapping.rb
@@ -1,7 +1,7 @@
 class UserMapping
 	def self.run!
 		file = File.open("user_mapping.csv", "w") 
-		File.write("user_mapping.csv", "UserID, ScrubbedUserID", mode: "a")
+		File.write("user_mapping.csv", "UserID, ScrubbedUserID,\n", mode: "a")
 		puts "Writing the mapping of #{User.count} users"
 		User.find_each do |user|
 			user_id = user.id

f516cb2fa1bef83ab1a0f518fabc72b8263bfb96
John Bumgardner <john.bumgardner@icloud.com>
2021-03-19 16:25:20 -0400

    switch syntax to talk to databases

--- db/data_migrations/user_mapping.rb
+++ db/data_migrations/user_mapping.rb
@@ -3,7 +3,7 @@ class UserMapping
 		file = File.open("user_mapping.csv", "w") 
 		File.write("user_mapping.csv", "UserID, ScrubbedUserID", mode: "a")
 		puts "Writing the mapping of #{User.count} users"
-		User.each do |user|
+		User.find_each do |user|
 			user_id = user.id
 			scrubbed_name = "#{user.role.name.downcase.gsub(/[- ]/,'_')}#{user.id}"
 			File.write("user_mapping.csv", user_id.to_s + ", " + scrubbed_name, mode: "a")

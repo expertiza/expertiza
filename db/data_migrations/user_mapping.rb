ee76fed79548308822ccd8c8c4cb3978e5a74fff
John Bumgardner <john.bumgardner@icloud.com>
2021-03-19 15:55:50 -0400

    first iteration of the new rake tool to write user mapping to a file

---
+++ db/data_migrations/user_mapping.rb
@@ -0,0 +1,13 @@
+class UserMapping
+	def self.run!
+		file = File.open("user_mapping.csv", "w") 
+		File.write("user_mapping.csv", "UserID, ScrubbedUserID", mode: "a")
+		puts "Writing the mapping of #{User.count} users"
+		User.each do |user|
+			user_id = user.id
+			scrubbed_name = "#{user.role.name.downcase.gsub(/[- ]/,'_')}#{user.id}"
+			File.write("user_mapping.csv", user_id.to_s + ", " scrubbed_name, mode: "a")
+		end
+	file.close
+	end
+end

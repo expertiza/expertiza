1b1cfb502f8fe0a0e1cbe80f2eef524990ba95f6
Ryan Smith <avlwx2014@gmail.com>
2021-03-21 20:05:18 -0400

    Refactor and Docstrings
    Rename startTime2 and endTime2 to remove the "2" suffix and add docstrings.

--- app/assets/javascripts/review_time.js
+++ app/assets/javascripts/review_time.js
@@ -1,11 +1,12 @@
 /**
  * Signal the submission viewing even controller to start timing
  * for a particular review asset.
+ *
  * @param map_id The ID of the review map from Expertiza
  * @param round The Review Round e.g. 1, 2, etc
  * @param link The asset being review.
  */
-function startTime2(map_id, round, link) {
+function startTime(map_id, round, link) {
     $.ajax({
         type: "POST",
         url: '/submission_viewing_events/start_timing',
@@ -30,7 +31,7 @@ function startTime2(map_id, round, link) {
  * @param round The Review Round e.g. 1, 2, etc
  * @param link The asset being review.
  */
-function endTime2(map_id, round, link) {
+function endTime(map_id, round, link) {
     var data = {
         submission_viewing_event: {
             map_id: map_id,
@@ -55,29 +56,12 @@ function endTime2(map_id, round, link) {
 }
 
 /**
- * Signal the submission viewing event controller to stop
- * timing for all open review assets.
+ * Signal the submission viewing event controller to
+ * stop timing for all open review assets for [round].
+ *
  * @param map_id The ID of the review map from Expertiza
  * @param round The Review Round e.g. 1, 2, etc
  */
-function saveAll(map_id, round) {
-    $.ajax({
-        type: 'POST',
-        dataType: 'json',
-        async: false,
-        url: '/submission_viewing_events/hard_save',
-        data: $.param({
-            submission_viewing_event: {
-                map_id: map_id,
-                round: round
-            }
-        }),
-        error: function (e) {
-            console.log(e);
-        }
-    });
-}
-
 function endRound(map_id, round) {
     $.ajax({
         type: 'POST',
@@ -96,6 +80,14 @@ function endRound(map_id, round) {
     });
 }
 
+/**
+ * Signal the submission viewing event controller to stop
+ * timing for all review assets for [round] and persist
+ * their timing data.
+ *
+ * @param map_id The ID of the review map from Expertiza
+ * @param round The Review Round e.g. 1, 2, etc
+ */
 function endRoundAndSave(map_id, round) {
     $.ajax({
         type: 'POST',
@@ -114,6 +106,13 @@ function endRoundAndSave(map_id, round) {
     });
 }
 
+/**
+ * Signal the submission viewing event controller to
+ * stop and restart timing for all review assets for [round].
+ *
+ * @param map_id The ID of the review map from Expertiza
+ * @param round The Review Round e.g. 1, 2, etc
+ */
 function resetRound(map_id, round) {
     $.ajax({
         type: 'POST',
@@ -129,5 +128,29 @@ function resetRound(map_id, round) {
         error: function (e) {
             console.log(e);
         },
+    });
+}
+
+/**
+ * Signal the submission viewing event controller to persist
+ * timing for all review assets to the database.
+ * @param map_id The ID of the review map from Expertiza
+ * @param round The Review Round e.g. 1, 2, etc
+ */
+function saveAll(map_id, round) {
+    $.ajax({
+        type: 'POST',
+        dataType: 'json',
+        async: false,
+        url: '/submission_viewing_events/hard_save',
+        data: $.param({
+            submission_viewing_event: {
+                map_id: map_id,
+                round: round
+            }
+        }),
+        error: function (e) {
+            console.log(e);
+        }
     });
 }

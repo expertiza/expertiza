d4e96ab70d8414b4b8ded70ff795808b9b9cd2b6
Ryan Smith <avlwx2014@gmail.com>
2021-03-25 12:37:59 -0400

    Add a LocalStorage instance to tests, make @args instance variable

--- spec/controllers/submission_viewing_events_controller_spec.rb
+++ spec/controllers/submission_viewing_events_controller_spec.rb
@@ -1,12 +1,18 @@
 describe SubmissionViewingEventsController do
 
   before :each do
-    args = {
+    @args = {
       submission_viewing_event: {
         :map_id => 123456,
         :round => 1,
         :link => "https://www.github.com" }
     }
+
+    @store = LocalStorage.new
+  end
+
+  after :each do
+    @store.remove_all
   end
 
   describe '#action_allowed?' do
@@ -16,43 +22,33 @@ describe SubmissionViewingEventsController do
   end
 
   describe '#start_timing' do
-    context 'when the link is opened' do
-      it 'should record the start time as the current time and clear the end time' do
-        expect(controller.start_timing(args)).to have_http_status :ok
-      end
+    it 'should record the start time as the current time and clear the end time' do
+      expect(controller.start_timing(@args)).to have_http_status :ok
     end
   end
 
   describe '#end_timing' do
-    context 'when a tab is closed' do
-      it 'should record the end time as the current time and update the total time' do
-        expect(controller.end_timing(args)).to have_http_status :ok
-      end
+    it 'should record the end time as the current time and update the total time' do
+      expect(controller.end_timing(@args)).to have_http_status :ok
     end
-    end
+  end
 
   describe '#reset_timing' do
-    context 'when a reivew is saved or submitted' do
-      it 'should record the end time as the current time and update the total time, and restart timing' do
-        expect(controller.reset_timing(args)).to have_http_status :ok
-      end
+    it 'should record the end time as the current time and update the total time, and restart timing' do
+      expect(controller.reset_timing(@args)).to have_http_status :ok
     end
   end
 
   describe '#hard_save' do
-    context 'when explicitly requested to' do
-      it 'should save all storage proxy records in the database and remove them from the storage proxy' do
-        expect(controller.hard_save(args)).to have_http_status :ok
-      end
+    it 'should save all storage proxy records in the database and remove them from the storage proxy' do
+      expect(controller.hard_save(@args)).to have_http_status :ok
     end
   end
 
   describe '#end_round_and_save' do
-    context 'when explicitly requested to' do
       it 'stop timing for all links for the given round, and save them to the database' do
-        expect(controller.end_round_and_save(args)).to have_http_status :ok
+        expect(controller.end_round_and_save(@args)).to have_http_status :ok
       end
-    end
   end
 
 end

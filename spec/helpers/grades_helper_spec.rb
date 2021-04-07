9e0ce9f7618a48977630c9d914220e136b272704
John Bumgardner <john.bumgardner@icloud.com>
2021-04-07 09:29:57 -0400

    add Rspec test fot type_and_max

--- spec/helpers/grades_helper_spec.rb
+++ spec/helpers/grades_helper_spec.rb
@@ -60,7 +60,25 @@ describe GradesHelper, type: :helper do
       expect(charts(symbol)).to eq(nil)
     end
   end
-    
+
+describe 'type_and_max' do
+  before(:each) do
+    @row = {question_id => 1, question_max_score => 95}
+    allow(Question).to receive(:find).with(1).and_return(question)
+  end
+  context 'when the question is a Checkbox' do
+    it 'returns 10_003'
+      question[type] == 'Checkbox'
+      expect(type_and_max(@row)).to eq(10_003)
+    end
+  end
+  context 'when the question is something else' do
+    it 'returns 9998'
+    question[type] == 'NotACheckbox'
+    expect(type_and_max(@row)).to eq(9998)
+    end
+  end 
+end
 
 
 end

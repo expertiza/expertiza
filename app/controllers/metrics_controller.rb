63920f12c2ffa48ded05dbc79d88e451ae72891c
J <cjl742952519@gmail.com>
2021-04-14 13:15:51 -0400

    new metrics controller on this branch

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -1,4 +1,39 @@
 class MetricsController < ApplicationController
+  helper :file
+  helper :submitted_content
+  helper :penalty
+  include PenaltyHelper
+  include StudentTaskHelper
+  include AssignmentHelper
+  include GradesHelper
+  include AuthorizationHelper
+
+  def view
+    if session["github_access_token"].nil?
+      session["assignment_id"] = params[:id]
+      session["github_view_type"] = "view_scores"
+      return redirect_to authorize_github_grades_path
+    end
+    @assignment = Assignment.find(params[:id])
+    questionnaires = @assignment.questionnaires
+
+    if @assignment.vary_by_round
+      @questions = retrieve_questions questionnaires, @assignment.id
+    else
+      @questions = {}
+      questionnaires.each do |questionnaire|
+        @questions[questionnaire.symbol] = questionnaire.questions
+      end
+    end
+
+    @scores = @assignment.scores(@questions)
+    averages = calculate_average_vector(@assignment.scores(@questions))
+    @average_chart = bar_chart(averages, 300, 100, 5)
+    @avg_of_avg = mean(averages)
+    calculate_all_penalties(@assignment.id)
+    @show_reputation = false
+  end
+
   def show
   end
 end

7e9e2b233d4e02976a222560cd3b9de206767e65
John Bumgardner <john.bumgardner@icloud.com>
2021-04-16 17:23:47 -0400

    create spec file for authority.rb

---
+++ spec/models/authority_spec.rb
@@ -0,0 +1,30 @@
+describe Authority do
+	let(:user1) { create(:student, name: "expertiza user", id: 1) }
+	describe '#initialize' do
+		it 'sets the current user' do
+			authority = Authority.new({current_user: user1})
+			expect(authority.current_user).to be(user1)
+		end
+	end
+
+	describe 'allow?' do
+		context 'the current user is an admin' do
+			it 'returns true' do
+
+			end
+		end
+		context 'you try to use the page controller' do
+			it 'returns true' do
+
+
+			end
+		end
+
+		context 'you are not an admin and not trying to access the page controller' do
+			it 'returns false' do
+
+
+			end
+		end
+	end
+end

9599d23673df8743b93179991aec14ee4f2b25ca
John Bumgardner <john.bumgardner@icloud.com>
2021-04-16 17:29:53 -0400

    add test for pages controller

--- spec/models/authority_spec.rb
+++ spec/models/authority_spec.rb
@@ -1,5 +1,6 @@
 describe Authority do
 	let(:user1) { create(:student, name: "expertizauser", id: 1) }
+	@admin_role = build(:role_of_administrator, id: 3, name: "Administrator_role_test", description: '', parent_id: nil, default_page_id: nil)
 	describe '#initialize' do
 		it 'sets the current user' do
 			authority = Authority.new({current_user: user1})
@@ -15,8 +16,10 @@ describe Authority do
 		end
 		context 'you try to use the page controller' do
 			it 'returns true' do
-
-
+				controller = 'pages'
+				action = nil
+				authority = Authority.new({current_user: user1})
+				expect(authority.allow?(controller, action))).to eq(true)
 			end
 		end
 

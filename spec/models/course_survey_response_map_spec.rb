e5ec1b4ee03256c128253d6b666fb2524381ded3
John Bumgardner <john.bumgardner@icloud.com>
2021-04-17 16:10:02 -0400

    add definitions for next and previous day

--- spec/models/course_survey_response_map_spec.rb
+++ spec/models/course_survey_response_map_spec.rb
@@ -1,6 +1,8 @@
 describe CourseSurveyResponseMap do
 	let(:participant) { build(:participant, user: build(:student, name: "Jane", fullname: "Doe, Jane", id: 1)) }
 	let(:course_survey_questionnaire) { CourseSurveyQuestionnaire.new id: 985, name: "course_survey", private: 0, min_question_score: 0, max_question_score: 10, instructor_id: 1234 }
+	let(:previous_day) { (Time.now.getlocal - 1 * 86_400).strftime("%Y-%m-%d %H:%M:%S") }
+  let(:next_day) { (Time.now.getlocal + 1 * 86_400).strftime("%Y-%m-%d %H:%M:%S") }
 	before(:each) do
     survey_deployment = CourseSurveyDeployment.new questionnaire_id: 985, start_date: previous_day, end_date: next_day, parent_id: "12345678", type: "CourseSurveyDeployment"
   end

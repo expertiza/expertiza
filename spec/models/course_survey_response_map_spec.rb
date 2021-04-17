98b9c3c1b5228fdc47adc998a77074b0c9b6a40a
John Bumgardner <john.bumgardner@icloud.com>
2021-04-17 16:44:35 -0400

    use to eq

--- spec/models/course_survey_response_map_spec.rb
+++ spec/models/course_survey_response_map_spec.rb
@@ -21,7 +21,7 @@ describe CourseSurveyResponseMap do
   describe '#questionnaire' do
   	it 'returns the associated course survey questionnaire' do
   		allow(Questionnaire).to receive(:find_by).with( {:id => 985} ).and_return(:course_survey_questionnaire)
-  		expect(@course_survey_response_map.questionnaire).to_eq(course_survey_questionnaire)
+  		expect(@course_survey_response_map.questionnaire).to eq(course_survey_questionnaire)
   	end
   end
 end

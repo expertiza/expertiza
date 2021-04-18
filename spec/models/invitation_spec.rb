3dbbc9956e637599dbae4078ba3302c4b611d37f
John Bumgardner <john.bumgardner@icloud.com>
2021-04-18 13:56:38 -0400

    create spec file and a test for invitation.rb

---
+++ spec/models/invitation_spec.rb
@@ -0,0 +1,25 @@
+describe Invitation do
+	let(:user2) { build(:student, id: 2) }
+  let(:user3) { build(:student, id: 3) }
+  let(:assignment) { build(:assignment, id: 1) }
+
+  it { should belong_to :to_user }
+  it { should belong_to :from_user }
+
+  describe '#is_invited?' do
+  	context 'an invitation has been sent between user1 and user2' do
+  		it 'returns true' do
+  			allow(Invitation).to_receive(:where).with('from_id = ? and to_id = ? and assignment_id = ? and reply_status = "W"',
+                                       invitee_user_id, invited_user_id, assignment_id).and_return(Invitation.new)
+  			expect(Invitation.is_invited?(user2.id, user3.id, assignment.id)).to eq(true)
+  		end
+  	end
+  	context 'an invitation has not been sent between user1 and user2' do
+  		it 'returns false' do
+  			allow(Invitation).to_receive(:where).with('from_id = ? and to_id = ? and assignment_id = ? and reply_status = "W"',
+                                       invitee_user_id, invited_user_id, assignment_id).and_return(nil)
+  			expect(Invitation.is_invited?(user2.id, user3.id, assignment.id)).to eq(false)
+  		end
+  	end
+  end
+end

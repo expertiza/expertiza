247a9c01a10ba403988c2d3821f211dd9ce2f602
John Bumgardner <john.bumgardner@icloud.com>
2021-04-18 14:02:52 -0400

    make where return an array

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -10,14 +10,14 @@ describe Invitation do
   	context 'an invitation has been sent between user1 and user2' do
   		it 'returns true' do
   			allow(Invitation).to receive(:where).with('from_id = ? and to_id = ? and assignment_id = ? and reply_status = "W"',
-                                       user2.id, user3.id, assignment.id).and_return(Invitation.new)
+                                       user2.id, user3.id, assignment.id).and_return([Invitation.new])
   			expect(Invitation.is_invited?(user2.id, user3.id, assignment.id)).to eq(true)
   		end
   	end
   	context 'an invitation has not been sent between user1 and user2' do
   		it 'returns false' do
   			allow(Invitation).to receive(:where).with('from_id = ? and to_id = ? and assignment_id = ? and reply_status = "W"',
-                                       user2.id, user3.id, assignment.id).and_return(nil)
+                                       user2.id, user3.id, assignment.id).and_return([nil])
   			expect(Invitation.is_invited?(user2.id, user3.id, assignment.id)).to eq(false)
   		end
   	end

fe26e13948531ed22f781ed8c4a0f9cd263d3155
John Bumgardner <john.bumgardner@icloud.com>
2021-04-19 20:38:02 -0400

    add basic tests for calculated penalty and first test for quiz assignment module

---
+++ spec/models/quiz_assignment_spec.rb
@@ -0,0 +1,13 @@
+describe QuizAssignment do
+
+	let(:assignment) { build(:assignment, id: 1)}
+
+	describe '#candidate_topics_for_quiz' do
+		context 'when the assignment does not have topics' do
+			it 'returns nil' do
+				allow(assignment).to receive(:sign_up_topics).and_return([])
+				expect(QuizAssignment.candidate_topics_for_quiz()).to eq(nil)
+			end
+		end
+	end
+end

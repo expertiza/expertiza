611c58eb6a16120a37adcc2f783eb440a28fca36
John Bumgardner <john.bumgardner@icloud.com>
2021-04-19 20:49:04 -0400

    refer to assignment directly

--- spec/models/quiz_assignment_spec.rb
+++ spec/models/quiz_assignment_spec.rb
@@ -7,7 +7,7 @@ describe QuizAssignment do
 	describe '#candidate_topics_for_quiz' do
 		context 'when the assignment does not have topics' do
 			it 'returns nil' do
-				allow(assignment).to receive(:sign_up_topics).and_return([])
+				assignment.sign_up_topics = []
 				expect(candidate_topics_for_quiz()).to eq(nil)
 			end
 		end

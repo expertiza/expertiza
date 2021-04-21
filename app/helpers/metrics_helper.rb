3e99d79ca805def7408f02dba05c0b2050259942
Stevan Dupor <smdupor@gmail.com>
2021-04-21 15:47:39 -0400

    Add Piechart of total commits by author

--- app/helpers/metrics_helper.rb
+++ app/helpers/metrics_helper.rb
@@ -26,6 +26,33 @@ module MetricsHelper
     horizontal_bar_chart data, chart_options
   end
 
+  def display_totals_graph(parsed_data, authors, dates)
+    data_array = []
+    color = %w[ff0000 ffff00 0000ff aaaaaa 00ff00 ff00ff]
+    i = 0
+    authors.each do |author|
+      data_object = []
+      data_object[0] = author
+      data_object[1] = parsed_data[author].values.inject(0) {|sum, value| sum += value}
+      data_object[2] = color[i]
+      #data_object['borderWidth'] = 1
+      data_array.push(data_object)
+      i += 1
+      i = 0 if i > 4
+    end
+
+     link = nil
+    GoogleChart::PieChart.new('600x300', 'Total Commits By Author', false) do |pc|
+      data_array.each do |datapoint|
+        pc.data datapoint[0], datapoint[1], datapoint[2]
+      end
+      link = pc.to_url
+    end
+
+      link
+
+  end
+
   # Fall 2018, E1858
   # Defines the general settings of the github metrics chart
   def chart_options

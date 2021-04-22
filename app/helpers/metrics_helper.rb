d8e1a3fc6cbf0b3d3e5511522dd11911af399f22
Stevan Dupor <smdupor@gmail.com>
2021-04-21 21:02:06 -0400

    Add numbers to piechart and fix layout issue with graphs

--- app/helpers/metrics_helper.rb
+++ app/helpers/metrics_helper.rb
@@ -26,25 +26,25 @@ module MetricsHelper
     horizontal_bar_chart data, chart_options
   end
 
-  def display_totals_graph(parsed_data, authors, dates)
+  def display_totals_piechart(parsed_data, authors, dates)
     data_array = []
     color = %w[ff0000 ffff00 0000ff aaaaaa 00ff00 ff00ff]
     i = 0
     authors.each do |author|
-      data_object = []
-      data_object[0] = author
-      data_object[1] = parsed_data[author].values.inject(0) {|sum, value| sum += value}
-      data_object[2] = color[i]
-      #data_object['borderWidth'] = 1
+      data_object = {}
+      data_object[:author] = author
+      data_object[:commits] = parsed_data[author].values.inject(0) {|sum, value| sum += value}
+      data_object[:color] = color[i]
       data_array.push(data_object)
       i += 1
       i = 0 if i > 4
     end
 
      link = nil
-    GoogleChart::PieChart.new('600x300', 'Total Commits By Author', false) do |pc|
+    GoogleChart::PieChart.new('600x300', '# Commits By Author', false) do |pc|
       data_array.each do |datapoint|
-        pc.data datapoint[0], datapoint[1], datapoint[2]
+        label = datapoint[:author] + " (" + datapoint[:commits].to_s + ")"
+        pc.data label, datapoint[:commits], datapoint[:color]
       end
       link = pc.to_url
     end

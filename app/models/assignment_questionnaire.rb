c41b6c3704c139d4518c28564db1da4968ed4ee6
Nicholas Himes <41524135+nnhimes@users.noreply.github.com>
2021-04-21 21:30:42 -0400

    Create get_latest_assignment method
    Added comment to define method

--- app/models/assignment_questionnaire.rb
+++ app/models/assignment_questionnaire.rb
@@ -7,4 +7,12 @@ class AssignmentQuestionnaire < ActiveRecord::Base
   scope :retrieve_questionnaire_for_assignment, lambda {|assignment_id|
     joins(:questionnaire).where('assignment_questionnaires.assignment_id = ?', assignment_id)
   }
+
+  #Method to find the most recent created_at record and return that record's assignment and round #
+  def self.get_latest_assignment(questionnaire_id)
+    record = self.includes(:assignment).where(questionnaire_id: questionnaire_id).order("assignments.created_at").last
+    unless record.nil?
+      return record.assignment, record.used_in_round
+    end
+  end
 end

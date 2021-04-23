e375314d1136c035b508f7ef106d8696ff985d4f
John Bumgardner <john.bumgardner@icloud.com>
2021-04-23 10:50:57 -0400

    fix naming of the type to my_type to match the attr_accesible

--- spec/models/rscore_spec.rb
+++ spec/models/rscore_spec.rb
@@ -14,7 +14,7 @@ describe Rscore do
   		expect(rscore.my_max).to eq(95)
   		expect(rscore.my_min).to eq(88)
   		expect(rscore.my_avg).to eq(90)
-  		expect(rscore.type).to eq(:quiz)
+  		expect(rscore.my_type).to eq(:quiz)
   	end
   end
 end

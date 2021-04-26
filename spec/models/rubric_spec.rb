0ee4f9c980469fda8c362f3c49a4e6e8a0904d74
John Bumgardner <john.bumgardner@icloud.com>
2021-04-26 11:24:18 -0400

    add parameters

--- spec/models/rubric_spec.rb
+++ spec/models/rubric_spec.rb
@@ -1,6 +1,6 @@
 describe Rubric do
   it 'is valid with valid attributes' do
-    expect(Rubric.new).to be_valid
+    expect(Rubric.new(name: 'Rubric', max_question_score: 100, min_question_score: 95)).to be_valid
   end
   it 'is invalid without the prescence of a name' do
     expect(Rubric.new(name: nil)).to_not be_valid

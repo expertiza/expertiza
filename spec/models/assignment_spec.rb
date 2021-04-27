3875ea88660ba3a0e70fb6377675d084b9ec78c5
John Bumgardner <john.bumgardner@icloud.com>
2021-04-27 17:46:43 -0400

    change test on scores

--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -188,7 +188,7 @@ describe Assignment do
         allow(ReviewResponseMap).to receive(:get_responses_for_team_round).with(team, 1).and_return([response])
         allow(Answer).to receive(:compute_scores).with([response], [question]).and_return(max: 95, min: 88, avg: 90)
         expect(assignment.scores(review1: [question]).inspect).to eq("{:participants=>{:\"1\"=>98}, :teams=>{:\"0\"=>{:team=>#<AssignmentTeam id: 1, "\
-          "name: \"no team\", parent_id: 1, type: \"AssignmentTeam\", comments_for_advertisement: nil, advertise_for_partner: nil, "\
+          "name: \"no team\", parent_id: nil, type: \"AssignmentTeam\", comments_for_advertisement: nil, advertise_for_partner: nil, "\
           "submitted_hyperlinks: \"---\\n- https://www.expertiza.ncsu.edu\", directory_num: 0, grade_for_submission: nil, "\
           "comment_for_submission: nil>, :scores=>{:max=>95, :min=>88, :avg=>90.0}}}}")
       end
@@ -201,7 +201,7 @@ describe Assignment do
         allow(ReviewResponseMap).to receive(:get_assessments_for).with(team).and_return([response])
         allow(Answer).to receive(:compute_scores).with([response], [question]).and_return(max: 95, min: 88, avg: 90)
         expect(assignment.scores(review: [question]).inspect).to eq("{:participants=>{:\"1\"=>98}, :teams=>{:\"0\"=>{:team=>#<AssignmentTeam id: 1, "\
-          "name: \"no team\", parent_id: 1, type: \"AssignmentTeam\", comments_for_advertisement: nil, advertise_for_partner: nil, "\
+          "name: \"no team\", parent_id: nil, type: \"AssignmentTeam\", comments_for_advertisement: nil, advertise_for_partner: nil, "\
           "submitted_hyperlinks: \"---\\n- https://www.expertiza.ncsu.edu\", directory_num: 0, grade_for_submission: nil, "\
           "comment_for_submission: nil>, :scores=>{:max=>95, :min=>88, :avg=>90}}}}")
       end

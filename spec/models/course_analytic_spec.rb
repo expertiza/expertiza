93a02a9faed185795ee761b7cae29426a1271532
John Bumgardner <john.bumgardner@icloud.com>
2021-04-29 10:29:22 -0400

    write test to check number of assignments

--- spec/models/course_analytic_spec.rb
+++ spec/models/course_analytic_spec.rb
@@ -1,18 +1,14 @@
 class DummyClass 
-	attr_accessor :course
-	attr_accessor :participants
+	attr_accessor :course, :participants, :assignments
 	require 'analytic/course_analytic'
 	include CourseAnalytic
 	
-	def initialize(course, participants)
+	def initialize(course, participants, assignments)
 		@course = course
 		@participants = participants
+		@assignments = assignments
 	end
  	
- 	def count_of_parts
-    num_participants
- 	end
-
 end
 
 describe CourseAnalytic do
@@ -20,16 +16,35 @@ describe CourseAnalytic do
 	let(:participant) { build(:participant, user: build(:student, name: "Jane", fullname: "Doe, Jane", id: 1)) }
   let(:participant2) { build(:participant, user: build(:student, name: "John", fullname: "Doe, John", id: 2)) }
   let(:participant3) { build(:participant, can_review: false, user: build(:student, name: "King", fullname: "Titan, King", id: 3)) }
+  let(:assignment) { build(:assignment, id: 1, name: 'no assignment', participants: [participant], teams: [team]) }
+  let(:assignment2) { build(:assignment, id: 2, name: 'no assignment', participants: [participant], teams: [team]) }
+  let(:assignment3) { build(:assignment, id: 3, name: 'no assignment', participants: [participant], teams: [team]) }
+  let(:team) { build(:assignment_team, id: 1, name: 'no team') }
+
 	describe '#num_participants' do
 		context 'when the course has no students in it' do
 			it 'should return zero' do
-				dc = DummyClass.new(course, [])
-				expect(dc.count_of_parts).to eq(0)
+				dc = DummyClass.new(course, [], []])
+				expect(dc.num_participants).to eq(0)
 			end
 		end
 		context 'when the course has three students in it' do
 			it 'should return three' do
-				dc = DummyClass.new(course, [participant, participant2, participant3])
+				dc = DummyClass.new(course, [participant, participant2, participant3], [])
+				expect(dc.num_participants).to eq(3)
+			end
+		end
+	end
+	describe '#num_assignments' do
+		context 'when the course has no assignments in it' do
+			it 'should return zero' do
+				dc = DummyClass.new(course, [], [])
+				expect(dc.num_assignments).to eq(0)
+			end
+		end
+		context 'when the course has three assignments in it' do
+			it 'should return three' do
+				dc = DummyClass.new(course, [], [assignment, assignment2, assignment3])
 				expect(dc.count_of_parts).to eq(3)
 			end
 		end

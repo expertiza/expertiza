6d800c793e7baee881e61d19831c624cc2886a44
John Bumgardner <john.bumgardner@icloud.com>
2021-04-29 12:15:35 -0400

    change name of test class in both analytics files

--- spec/models/question_analytic_spec.rb
+++ spec/models/question_analytic_spec.rb
@@ -1,4 +1,4 @@
-class QuestionAnalyticTestDummyClass 
+class QuestionAnalyticTestQuestionAnalyticTestDummyClass 
 	attr_accessor :txt
 	require 'analytic/question_analytic'
 	include QuestionAnalytic
@@ -24,26 +24,26 @@ describe QuestionAnalytic do
   describe '#unique_character_count' do
   	it 'counts the number of unique characters - case insensitive' do
   		text = 'Aa'
-  		dc = DummyClass.new(text)
+  		dc = QuestionAnalyticTestDummyClass.new(text)
   		expect(dc.uni_character_count).to eq(1)
   	end
   	it 'counts the number of unique characters' do
   		text = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  		dc = DummyClass.new(text)
+  		dc = QuestionAnalyticTestDummyClass.new(text)
   		expect(dc.uni_character_count).to eq(26)
   	end
   end
   describe '#character_count' do
   	it 'counts the number of characters in a string' do
   		text = 'ABCABCabcabc'
-  		dc = DummyClass.new(text)
+  		dc = QuestionAnalyticTestDummyClass.new(text)
   		expect(dc.char_count).to eq(12)
   	end
   end
   describe '#word_count' do
   	it 'counts the words in a string' do
   		text = 'John Bumgardner'
-  		dc = DummyClass.new(text)
+  		dc = QuestionAnalyticTestDummyClass.new(text)
   		expect(dc.wc).to eq(2)
 
   		dc.txt = ''

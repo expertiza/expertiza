b29619fa71343ef600a0f90834d25455b9830e1e
John Bumgardner <john.bumgardner@icloud.com>
2021-04-29 12:03:41 -0400

    fix potential problem of two classes with same name

--- spec/models/course_analytic_spec.rb
+++ spec/models/course_analytic_spec.rb
@@ -1,4 +1,4 @@
-class DummyClass 
+class CourseDummyClass 
 	attr_accessor :course, :participants, :assignments
 	require 'analytic/course_analytic'
 	include CourseAnalytic
@@ -23,13 +23,13 @@ describe CourseAnalytic do
 	describe '#num_participants' do
 		context 'when the course has no students in it' do
 			it 'should return zero' do
-				dc = DummyClass.new(course, [], [])
+				dc = CourseDummyClass.new(course, [], [])
 				expect(dc.num_participants).to eq(0)
 			end
 		end
 		context 'when the course has three students in it' do
 			it 'should return three' do
-				dc = DummyClass.new(course, [participant, participant2, participant3], [])
+				dc = CourseDummyClass.new(course, [participant, participant2, participant3], [])
 				expect(dc.num_participants).to eq(3)
 			end
 		end
@@ -37,13 +37,13 @@ describe CourseAnalytic do
 	describe '#num_assignments' do
 		context 'when the course has no assignments in it' do
 			it 'should return zero' do
-				dc = DummyClass.new(course, [], [])
+				dc = CourseDummyClass.new(course, [], [])
 				expect(dc.num_assignments).to eq(0)
 			end
 		end
 		context 'when the course has three assignments in it' do
 			it 'should return three' do
-				dc = DummyClass.new(course, [], [assignment, assignment2, assignment3])
+				dc = CourseDummyClass.new(course, [], [assignment, assignment2, assignment3])
 				expect(dc.num_assignments).to eq(3)
 			end
 		end
@@ -51,13 +51,13 @@ describe CourseAnalytic do
 	describe '#total_num_assignment_teams' do
 		context 'when there are no assignments' do
 			it 'returns zero' do
-				dc = DummyClass.new(course, [], [])
+				dc = CourseDummyClass.new(course, [], [])
 				expect(dc.total_num_assignment_teams).to eq(0)
 			end
 		end
 		context 'when there are three assignments with each having one team' do
 			it 'returns three' do
-				dc = DummyClass.new(course, [], [assignment, assignment2, assignment3])
+				dc = CourseDummyClass.new(course, [], [assignment, assignment2, assignment3])
 				allow(assignment).to receive(:num_teams).and_return(1)
 				allow(assignment2).to receive(:num_teams).and_return(1)
 				allow(assignment3).to receive(:num_teams).and_return(1)
@@ -68,13 +68,13 @@ describe CourseAnalytic do
 	describe '#average_num_assignment_teams' do
 		context 'when there are no assignments' do
 			it 'returns zero' do
-				dc = DummyClass.new(course, [], [])
+				dc = CourseDummyClass.new(course, [], [])
 				expect(dc.average_num_assignment_teams).to eq(0)
 			end
 		end
 		context 'three assignments have been added to the course, and one has one team, one has two, and one has three' do
 			it 'returns two' do
-				dc = DummyClass.new(course, [], [assignment, assignment2, assignment3])
+				dc = CourseDummyClass.new(course, [], [assignment, assignment2, assignment3])
 				allow(assignment).to receive(:num_teams).and_return(1)
 				allow(assignment2).to receive(:num_teams).and_return(2)
 				allow(assignment3).to receive(:num_teams).and_return(3)
@@ -85,13 +85,13 @@ describe CourseAnalytic do
 	describe '#max_num_assignment_teams' do
 		context 'when there are no assignments' do
 			it 'returns zero' do
-				dc = DummyClass.new(course, [], [])
+				dc = CourseDummyClass.new(course, [], [])
 				expect(dc.max_num_assignment_teams).to eq(0)
 			end
 		end
 		context 'three assignments have been added to the course, and one has one team, one has two, and one has three' do
 			it 'returns three' do
-				dc = DummyClass.new(course, [], [assignment, assignment2, assignment3])
+				dc = CourseDummyClass.new(course, [], [assignment, assignment2, assignment3])
 				allow(assignment).to receive(:num_teams).and_return(1)
 				allow(assignment2).to receive(:num_teams).and_return(2)
 				allow(assignment3).to receive(:num_teams).and_return(3)
@@ -102,13 +102,13 @@ describe CourseAnalytic do
 	describe '#min_num_assignment_teams' do
 		context 'when there are no assignments' do
 			it 'returns zero' do
-				dc = DummyClass.new(course, [], [])
+				dc = CourseDummyClass.new(course, [], [])
 				expect(dc.min_num_assignment_teams).to eq(0)
 			end
 		end
 		context 'three assignments have been added to the course, and one has one team, one has two, and one has three' do
 			it 'returns one' do
-				dc = DummyClass.new(course, [], [assignment, assignment2, assignment3])
+				dc = CourseDummyClass.new(course, [], [assignment, assignment2, assignment3])
 				allow(assignment).to receive(:num_teams).and_return(1)
 				allow(assignment2).to receive(:num_teams).and_return(2)
 				allow(assignment3).to receive(:num_teams).and_return(3)
@@ -119,13 +119,13 @@ describe CourseAnalytic do
 	describe '#average_assignment_score' do
 		context 'when there are no assignments' do
 			it 'returns zero' do
-				dc = DummyClass.new(course, [], [])
+				dc = CourseDummyClass.new(course, [], [])
 				expect(dc.average_assignment_score).to eq(0)
 			end
 		end
 		context 'three assignments have been added to the course, and one has a 90, one has a 95, and one has a 100' do
 			it 'returns 95' do
-				dc = DummyClass.new(course, [], [assignment, assignment2, assignment3])
+				dc = CourseDummyClass.new(course, [], [assignment, assignment2, assignment3])
 				allow(assignment).to receive(:average_team_score).and_return(90)
 				allow(assignment2).to receive(:average_team_score).and_return(95)
 				allow(assignment3).to receive(:average_team_score).and_return(100)
@@ -136,13 +136,13 @@ describe CourseAnalytic do
 	describe '#max_assignment_score' do
 		context 'when there are no assignments' do
 			it 'returns zero' do
-				dc = DummyClass.new(course, [], [])
+				dc = CourseDummyClass.new(course, [], [])
 				expect(dc.max_assignment_score).to eq(0)
 			end
 		end
 		context 'three assignments have been added to the course, and one has a 90, one has a 95, and one has a 100' do
 			it 'returns 100' do
-				dc = DummyClass.new(course, [], [assignment, assignment2, assignment3])
+				dc = CourseDummyClass.new(course, [], [assignment, assignment2, assignment3])
 				allow(assignment).to receive(:max_team_score).and_return(90)
 				allow(assignment2).to receive(:max_team_score).and_return(95)
 				allow(assignment3).to receive(:max_team_score).and_return(100)
@@ -153,13 +153,13 @@ describe CourseAnalytic do
 	describe '#min_assignment_score' do
 		context 'when there are no assignments' do
 			it 'returns zero' do
-				dc = DummyClass.new(course, [], [])
+				dc = CourseDummyClass.new(course, [], [])
 				expect(dc.min_assignment_score).to eq(0)
 			end
 		end
 		context 'three assignments have been added to the course, and one has a 90, one has a 95, and one has a 100' do
 			it 'returns 90' do
-				dc = DummyClass.new(course, [], [assignment, assignment2, assignment3])
+				dc = CourseDummyClass.new(course, [], [assignment, assignment2, assignment3])
 				allow(assignment).to receive(:min_team_score).and_return(90)
 				allow(assignment2).to receive(:min_team_score).and_return(95)
 				allow(assignment3).to receive(:min_team_score).and_return(100)
@@ -170,13 +170,13 @@ describe CourseAnalytic do
 	describe '#assignment_review_counts' do
 		context 'there have been no assignments added to a course' do
 			it 'should return zero' do
-				dc = DummyClass.new(course, [], [])
+				dc = CourseDummyClass.new(course, [], [])
 				expect(dc.assignment_review_counts).to eq([0])
 			end
 		end
 		context 'three assignments have been added to the course, and one has one review, one has two, and one has three' do
 			it 'should return list of [1,2,3]' do
-				dc = DummyClass.new(course, [], [assignment, assignment2, assignment3])
+				dc = CourseDummyClass.new(course, [], [assignment, assignment2, assignment3])
 				allow(assignment).to receive(:total_num_team_reviews).and_return(1)
 				allow(assignment2).to receive(:total_num_team_reviews).and_return(2)
 				allow(assignment3).to receive(:total_num_team_reviews).and_return(3)
@@ -187,13 +187,13 @@ describe CourseAnalytic do
 	describe '#total_num_assignment_reviews' do
 		context 'there have been no assignments added to a course' do
 			it 'should return zero' do
-				dc = DummyClass.new(course, [], [])
+				dc = CourseDummyClass.new(course, [], [])
 				expect(dc.total_num_assignment_reviews).to eq(0)
 			end
 		end
 		context 'three assignments have been added to the course, and one has one review, one has two, and one has three' do
 			it 'should return six' do
-				dc = DummyClass.new(course, [], [assignment, assignment2, assignment3])
+				dc = CourseDummyClass.new(course, [], [assignment, assignment2, assignment3])
 				allow(assignment).to receive(:total_num_team_reviews).and_return(1)
 				allow(assignment2).to receive(:total_num_team_reviews).and_return(2)
 				allow(assignment3).to receive(:total_num_team_reviews).and_return(3)
@@ -204,13 +204,13 @@ describe CourseAnalytic do
 	describe '#average_num_assignment_reviews' do
 		context 'there have been no assignments added to a course' do
 			it 'should return zero' do
-				dc = DummyClass.new(course, [], [])
+				dc = CourseDummyClass.new(course, [], [])
 				expect(dc.average_num_assignment_reviews).to eq(0)
 			end
 		end
 		context 'three assignments have been added to the course, and one has one review, one has two, and one has three' do
 			it 'should return two' do
-				dc = DummyClass.new(course, [], [assignment, assignment2, assignment3])
+				dc = CourseDummyClass.new(course, [], [assignment, assignment2, assignment3])
 				allow(assignment).to receive(:total_num_team_reviews).and_return(1)
 				allow(assignment2).to receive(:total_num_team_reviews).and_return(2)
 				allow(assignment3).to receive(:total_num_team_reviews).and_return(3)
@@ -221,13 +221,13 @@ describe CourseAnalytic do
 	describe '#max_num_assignment_reviews' do
 		context 'there have been no assignments added to a course' do
 			it 'should return zero' do
-				dc = DummyClass.new(course, [], [])
+				dc = CourseDummyClass.new(course, [], [])
 				expect(dc.max_num_assignment_reviews).to eq(0)
 			end
 		end
 		context 'three assignments have been added to the course, and one has one review, one has two, and one has three' do
 			it 'should return three' do
-				dc = DummyClass.new(course, [], [assignment, assignment2, assignment3])
+				dc = CourseDummyClass.new(course, [], [assignment, assignment2, assignment3])
 				allow(assignment).to receive(:total_num_team_reviews).and_return(1)
 				allow(assignment2).to receive(:total_num_team_reviews).and_return(2)
 				allow(assignment3).to receive(:total_num_team_reviews).and_return(3)
@@ -238,13 +238,13 @@ describe CourseAnalytic do
 	describe '#min_num_assignment_reviews' do
 		context 'there have been no assignments added to a course' do
 			it 'should return zero' do
-				dc = DummyClass.new(course, [], [])
+				dc = CourseDummyClass.new(course, [], [])
 				expect(dc.min_num_assignment_reviews).to eq(0)
 			end
 		end
 		context 'three assignments have been added to the course, and one has one review, one has two, and one has three' do
 			it 'should return one' do
-				dc = DummyClass.new(course, [], [assignment, assignment2, assignment3])
+				dc = CourseDummyClass.new(course, [], [assignment, assignment2, assignment3])
 				allow(assignment).to receive(:total_num_team_reviews).and_return(1)
 				allow(assignment2).to receive(:total_num_team_reviews).and_return(2)
 				allow(assignment3).to receive(:total_num_team_reviews).and_return(3)
@@ -255,13 +255,13 @@ describe CourseAnalytic do
 	describe '#assignment_team_count' do
 		context 'there have been no assignments added to a course' do
 			it 'should return zero' do
-				dc = DummyClass.new(course, [], [])
+				dc = CourseDummyClass.new(course, [], [])
 				expect(dc.assignment_team_counts).to eq([0])
 			end
 		end
 		context 'three assignments have been added to the course, and one has one review, one has two, and one has three' do
 			it 'should return [1,2,3]' do
-				dc = DummyClass.new(course, [], [assignment, assignment2, assignment3])
+				dc = CourseDummyClass.new(course, [], [assignment, assignment2, assignment3])
 				allow(assignment).to receive(:num_teams).and_return(1)
 				allow(assignment2).to receive(:num_teams).and_return(2)
 				allow(assignment3).to receive(:num_teams).and_return(3)
@@ -272,13 +272,13 @@ describe CourseAnalytic do
 	describe '#assignment_average_scores' do
 		context 'there have been no assignments added to a course' do
 			it 'should return zero' do
-				dc = DummyClass.new(course, [], [])
+				dc = CourseDummyClass.new(course, [], [])
 				expect(dc.assignment_average_scores).to eq([0])
 			end
 		end
 		context 'three assignments have been added to the course, and one has a 90, one has 95, and one has 100' do
 			it 'should return [90,95,100]' do
-				dc = DummyClass.new(course, [], [assignment, assignment2, assignment3])
+				dc = CourseDummyClass.new(course, [], [assignment, assignment2, assignment3])
 				allow(assignment).to receive(:average_team_score).and_return(90)
 				allow(assignment2).to receive(:average_team_score).and_return(95)
 				allow(assignment3).to receive(:average_team_score).and_return(100)
@@ -289,13 +289,13 @@ describe CourseAnalytic do
 	describe '#assignment_max_scores' do
 		context 'there have been no assignments added to a course' do
 			it 'should return zero' do
-				dc = DummyClass.new(course, [], [])
+				dc = CourseDummyClass.new(course, [], [])
 				expect(dc.assignment_max_scores).to eq([0])
 			end
 		end
 		context 'three assignments have been added to the course, and one has a 90, one has 95, and one has 100' do
 			it 'should return [90,95,100]' do
-				dc = DummyClass.new(course, [], [assignment, assignment2, assignment3])
+				dc = CourseDummyClass.new(course, [], [assignment, assignment2, assignment3])
 				allow(assignment).to receive(:max_team_score).and_return(90)
 				allow(assignment2).to receive(:max_team_score).and_return(95)
 				allow(assignment3).to receive(:max_team_score).and_return(100)
@@ -306,13 +306,13 @@ describe CourseAnalytic do
 	describe '#assignment_min_scores' do
 		context 'there have been no assignments added to a course' do
 			it 'should return zero' do
-				dc = DummyClass.new(course, [], [])
+				dc = CourseDummyClass.new(course, [], [])
 				expect(dc.assignment_min_scores).to eq([0])
 			end
 		end
 		context 'three assignments have been added to the course, and one has a 90, one has 95, and one has 100' do
 			it 'should return [90,95,100]' do
-				dc = DummyClass.new(course, [], [assignment, assignment2, assignment3])
+				dc = CourseDummyClass.new(course, [], [assignment, assignment2, assignment3])
 				allow(assignment).to receive(:min_team_score).and_return(90)
 				allow(assignment2).to receive(:min_team_score).and_return(95)
 				allow(assignment3).to receive(:min_team_score).and_return(100)

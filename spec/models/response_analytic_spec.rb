3ce05790a6ebd1efb42ce32db50b987d6c21b938
John Bumgardner <john.bumgardner@icloud.com>
2021-04-30 13:15:07 -0400

    fix max test number

--- spec/models/response_analytic_spec.rb
+++ spec/models/response_analytic_spec.rb
@@ -207,7 +207,7 @@ describe ResponseAnalytic do
         allow(answer1).to receive(:word_count).and_return(50)
         allow(answer2).to receive(:word_count).and_return(75)
         allow(answer3).to receive(:word_count).and_return(100)
-        expect(dc.max_word_count).to eq(75)
+        expect(dc.max_word_count).to eq(100)
       end
     end
   end
@@ -219,7 +219,7 @@ describe ResponseAnalytic do
       end
     end
     context 'there are three answers associated with the response, with word counts of 50, 75, and 100' do
-      it 'will return 100' do
+      it 'will return 50' do
         dc = ResponseAnalyticTestDummyClass.new(@scores)
         allow(answer1).to receive(:word_count).and_return(50)
         allow(answer2).to receive(:word_count).and_return(75)

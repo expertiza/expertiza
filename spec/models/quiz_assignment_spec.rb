336b4f84b85a19d78453c444cae482e22a7bfe30
John Bumgardner <john.bumgardner@icloud.com>
2021-05-05 11:01:33 -0400

    use response map

--- spec/models/quiz_assignment_spec.rb
+++ spec/models/quiz_assignment_spec.rb
@@ -5,8 +5,7 @@ describe QuizAssignment do
   let(:participant) { build(:participant, id: 1) }
   let(:team) { build(:assignment_team, id: 1, parent_id: 1) }
   let(:questionnaire1) { build(:questionnaire, id: 1, type: 'ReviewQuestionnaire') }
-  let(:quiz_response_map1) { build(:quiz_response_map, id: 1) }
-  let(:quiz_response_map2) { build(:quiz_response_map, id: 2) }
+  let(:teammate_review_response_map) { build(:review_response_map, type: 'TeammateReviewResponseMap') }
   describe '#candidate_topics_for_quiz' do
     context 'when there are no signup topics' do
       it 'returns nil' do
@@ -15,10 +14,10 @@ describe QuizAssignment do
     end
   end
   describe '#quiz_taken_by?' do
-  	context 'when the participant has taken 2 quizzes' do
+  	context 'when the participant has taken one quizzes' do
       it 'returns true' do
         allow(QuizQuestionnaire).to receive(:find_by).with(instructor_id: 6).and_return(questionnaire1)
-        allow(QuizResponseMap).to receive(:where).with('reviewee_id = 6 AND reviewer_id = 1 AND reviewed_object_id = 1').and_return([quiz_response_map1, quiz_response_map2])
+        allow(QuizResponseMap).to receive(:where).with('reviewee_id = 6 AND reviewer_id = 1 AND reviewed_object_id = 1').and_return([teammate_review_response_map])
         expect(assignment.quiz_taken_by?(instructor, participant)).to eq(true)
       end
     end 

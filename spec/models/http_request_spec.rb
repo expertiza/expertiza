7d1444fdcdd04b3f7e1246e7eb239647e127b8dd
John Bumgardner <john.bumgardner@icloud.com>
2021-05-06 09:43:31 -0400

    add tests for HTTP request

--- spec/models/http_request_spec.rb
+++ spec/models/http_request_spec.rb
@@ -14,5 +14,17 @@ describe "HttpRequest" do
     expect(HttpRequest.valid_url?("http://www.google.com/arg1/123/arg2/456")).to be_truthy
   end
 
-  # NOTE: no need to test get() and get_file() since they wrap net/http methods which should have their own tests
+  describe '#get' do
+    context 'when the limit is too small' do
+     it 'returns an empty string' do
+       expect(HttpRequest.get('url', 0)).to eq('')
+     end
+    end
+    context 'when you call a valid url' do
+      it 'the result' do
+        url = 'httpbin.org'
+        expect(HttpRequest.get(url, 5)).to eq('')
+      end
+    end
+  end
 end

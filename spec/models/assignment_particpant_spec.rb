d5e100d23b899043597ceef2cd85b602d15a4689
John Bumgardner <john.bumgardner@icloud.com>
2021-05-09 17:18:56 -0400

    stub

--- spec/models/assignment_particpant_spec.rb
+++ spec/models/assignment_particpant_spec.rb
@@ -3,7 +3,7 @@ describe AssignmentParticipant do
   let(:team) { build(:assignment_team, id: 1) }
   let(:team2) { build(:assignment_team, id: 2) }
   let(:response_map) { build(:review_response_map, reviewer_id: 2, response: [response]) }
-  let(:participant) { build(:participant, id: 1, assignment: assignment, team: team) }
+  let(:participant) { build(:participant, id: 1, assignment: assignment) }
   let(:participant2) { build(:participant, id: 2, grade: 100) }
   let(:assignment) { build(:assignment, id: 1) }
   let(:review_questionnaire) { build(:questionnaire, id: 1) }
@@ -31,6 +31,7 @@ describe AssignmentParticipant do
     context 'when the associated assignment is reviewed by his team' do
       it 'returns the team' do
         allow(assignment).to receive(:reviewer_is_team).and_return(true)
+        allow(participant).to receive(:team).and_return(team)
         expect(participant.get_reviewer).to eq(team)
       end
     end

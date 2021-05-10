39da4392114419d042adff7808f0ea4b118324aa
John Bumgardner <john.bumgardner@icloud.com>
2021-05-10 13:31:23 -0400

    fix the variable name

--- spec/models/questionnaire_spec.rb
+++ spec/models/questionnaire_spec.rb
@@ -71,10 +71,10 @@ describe Questionnaire do
     context 'when there are no rounds' do
      it 'just uses the symbol with no round' do
        allow(AssignmentQuestionnaire).to receive(:find_by).with(assignment_id: 1, questionnaire_id: 2).and_return(assignment_questionnaire1)
-       allow(assignment_questionnaire2).to receive(:used_in_round).and_return(nil)
+       allow(assignment_questionnaire1).to receive(:used_in_round).and_return(nil)
        allow(questionnaire2).to receive(:symbol).and_return('a')
        allow(questionnaire2).to receive(:assignment_questionnaires).and_return(assignment_questionnaire2)
-       allow(assignment_questionnaire2).to receive(:find_by).with(assignment_id: 1).and_return(assignment_questionnaire2)
+       allow(assignment_questionnaire1).to receive(:find_by).with(assignment_id: 1).and_return(assignment_questionnaire2)
        scores = {
          'a': {
            scores: {

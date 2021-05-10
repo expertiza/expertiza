4bce9dec39d5a36445a626c3da7eca4017c7c43f
John Bumgardner <john.bumgardner@icloud.com>
2021-05-10 14:43:42 -0400

    create test code for column header

---
+++ spec/models/column_header_spec.rb
@@ -0,0 +1,16 @@
+describe ColumnHeader do
+  describe '#complete' do
+    it 'returns the header' do
+      ch = ColumnHeader.new
+      ch.txt = 'Question 1'
+      expect(ch.complete(1, nil)).to eq('<tr><th style="width: 15%">Question 1</th>')
+    end
+  end
+  describe '#view_completed_question' do
+    it 'returns the header' do
+      ch = ColumnHeader.new
+      ch.txt = 'Question 1'
+      expect(ch.complete(1, nil)).to eq('<tr><th style="width: 15%">Question 1</th>')
+    end
+  end
+end

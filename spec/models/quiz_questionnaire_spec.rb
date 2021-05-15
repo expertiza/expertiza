15abb5c7232c5361552109c695a8ad79ff85a0cc
John Bumgardner <john.bumgardner@icloud.com>
2021-05-15 19:21:13 -0400

    fix syntax

--- spec/models/quiz_questionnaire_spec.rb
+++ spec/models/quiz_questionnaire_spec.rb
@@ -2,7 +2,9 @@ describe QuizQuestionnaire do
    let(:quiz_questionnaire) {build (:quiz_questionnaire)}
    let(:quiz_response_map) {build (:quiz_response_map)}
    describe '#symbol' do
-     expect(quiz_questionnaire.symbol).to eq("quiz".to_sym)
+     it 'returns the quiz symbol' do
+       expect(quiz_questionnaire.symbol).to eq("quiz".to_sym)
+     end
    end
 
 end

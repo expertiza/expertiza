056faddf8437c923a43e17524d0364ce7279dac4
John Bumgardner <john.bumgardner@icloud.com>
2021-05-15 16:09:09 -0400

    add test for export fields

--- spec/models/multiple_choice_checkbox_spec.rb
+++ spec/models/multiple_choice_checkbox_spec.rb
@@ -70,4 +70,9 @@ describe MultipleChoiceCheckbox do
       expect(Question.compute_question_score).to eq(0)
     end
   end
+  describe '#export_fields' do
+    it 'returns the column headers' do
+      expect(Question.export_fields).to eq(["Seq", "Question", "Type", "Weight", "text area size", "max_label", "min_label"])
+    end
+  end
 end

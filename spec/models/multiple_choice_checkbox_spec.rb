d38b95e20a195481393b29a0bad57a17942727ea
John Bumgardner <john.bumgardner@icloud.com>
2021-05-15 17:04:50 -0400

    fix both tests

--- spec/models/multiple_choice_checkbox_spec.rb
+++ spec/models/multiple_choice_checkbox_spec.rb
@@ -19,21 +19,18 @@ describe MultipleChoiceCheckbox do
     context 'when the question itself does not have txt' do
       it 'returns "Please make sure all questions have text"' do
         allow(multiple_choice_checkbox).to receive(:txt).and_return(nil)
-        questions = {}
+        questions = {"1" => {txt: 'question text', iscorrect: '1'}, "2" => {txt: 'question text', iscorrect: '1'}, "3" => {txt: 'question text', iscorrect: '0'}, "4" => {txt: 'question text', iscorrect: '0'}}
         expect(multiple_choice_checkbox.valid?(questions)).to eq("Please make sure all questions have text")
       end
     end
     context 'when a choice does not have txt' do
       it 'returns "Please make sure every question has text for all options"' do
-        questions = {"1" => {txt: nil}, "2" => {txt: nil}, "3" => {txt: nil}, "4" => {txt: nil}}
+        questions = {"1" => {txt: nil, iscorrect: '1'}, "2" => {txt: nil, iscorrect: '1'}, "3" => {txt: nil, iscorrect: '0'}, "4" => {txt: nil, iscorrect: '0'}}
         expect(multiple_choice_checkbox.valid?(questions)).to eq("Please make sure every question has text for all options")
       end
     end
     context 'when no choices are correct' do
       it 'returns "Please select a correct answer for all questions"' do
-        qc = double('QuizQuestionChoice')
-        allow(qc).to receive(:iscorrect).and_return('0')
-        allow(qc).to receive(:txt).and_return('question text')
         questions = {"1" => {txt: 'question text', iscorrect: '0'}, "2" => {txt: 'question text', iscorrect: '0'}, "3" => {txt: 'question text', iscorrect: '0'}, "4" => {txt: 'question text', iscorrect: '0'}}
         expect(multiple_choice_checkbox.valid?(questions)).to eq("Please select a correct answer for all questions")
       end

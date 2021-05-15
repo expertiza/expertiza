cd0add2ea5c8f466ec265a151b84a92c62af7894
John Bumgardner <john.bumgardner@icloud.com>
2021-05-15 15:09:36 -0400

    stub txt

--- spec/models/multiple_choice_checkbox_spec.rb
+++ spec/models/multiple_choice_checkbox_spec.rb
@@ -5,6 +5,7 @@ describe MultipleChoiceCheckbox do
       qc = double('QuizQuestionChoice')
       allow(QuizQuestionChoice).to receive(:where).with(question_id: 1).and_return([qc, qc, qc, qc])
       allow(qc).to receive(:iscorrect).and_return(true)
+      allow(qc).to receive(:txt).and_return('question text')
       expect(multiple_choice_checkbox.edit).to eq('')
     end
   end

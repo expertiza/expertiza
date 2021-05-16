c23b6ebe10d84e38c44ef5b93104c4deeb191800
John Bumgardner <john.bumgardner@icloud.com>
2021-05-16 12:46:41 -0400

    fix typo

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -4,7 +4,7 @@ describe CourseTeam do
   let(:course_team2) { build(:course_team, id: 2) }
   let(:instructor) { build(:instructor, id: 6) }
   let(:user1) { User.new name: 'abc', fullname: 'abc bbc', email: 'abcbbc@gmail.com', password: '123456789', password_confirmation: '123456789' }
-  let(:user2) { User.new name: 'cbd', fullname: 'cbd ccd', email: 'bcdccd@gmail.com', password: '123456789', password_confirmation: '123456789' }
+  let(:user2) { User.new name: 'bcd', fullname: 'cbd ccd', email: 'bcdccd@gmail.com', password: '123456789', password_confirmation: '123456789' }
   let(:participant) { build(:participant, user: build(:student, name: "Jane", fullname: "Doe, Jane", id: 1)) }
   let(:participant2) { build(:participant, user: build(:student, name: "John", fullname: "Doe, John", id: 2)) }
   let(:assignment) { build(:assignment, id: 1, name: 'no assgt') }

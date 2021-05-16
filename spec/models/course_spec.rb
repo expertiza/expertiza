ffb3fd33a86bc0cafafe3656044c6d2689015730
John Bumgardner <john.bumgardner@icloud.com>
2021-05-16 11:12:57 -0400

    test path

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -2,6 +2,7 @@ describe CourseTeam do
   let(:course) { build(:course, id: 1, name: 'ECE517') }
   let(:course_team1) { build(:course_team, id: 1) }
   let(:course_team2) { build(:course_team, id: 2) }
+  let(:instructor) { build(:instructor, id: 6) }
   describe '#get_teams' do
     it 'returns the associated teams with the course' do
       allow(CourseTeam).to receive(:where).with(parent_id: 1).and_return([course_team1, course_team2])
@@ -14,6 +15,12 @@ describe CourseTeam do
       it 'an error is raised' do
         allow(course).to receive(:instructor_id).and_return(nil)
         expect{course.path}.to raise_error("Path can not be created. The course must be associated with an instructor.")
+      end
+    end
+    context 'when there is no associated instructor' do
+      it 'an error is raised' do
+        allow(course).to receive(:instructor_id).and_return(6)
+        allow(course.path).to eq('')        
       end
     end
   end

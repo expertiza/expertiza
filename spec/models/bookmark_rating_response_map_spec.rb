e3c9cfcf7b4cdbae8e436b063a8ca4fc06ab5961
John Bumgardner <john.bumgardner@icloud.com>
2021-05-16 12:59:33 -0400

    add test for bookmark rating response map

--- spec/models/bookmark_rating_response_map_spec.rb
+++ spec/models/bookmark_rating_response_map_spec.rb
@@ -1,7 +1,9 @@
 describe BookmarkRatingResponseMap do
+  let(:assignment) { build(:assignment, id: 1, name: 'no assgt') }
+  let(:questionnaire1) { build(:questionnaire, id: 1, type: 'MetareviewQuestionnaire') }
+  let(:questionnaire2) { build(:questionnaire, id: 2, type: 'BookmarkRatingQuestionnaire') }
+  let(:model) { BookmarkRatingResponseMap.new }
   describe '#reviewee' do
-    let(:model) { BookmarkRatingResponseMap.new }
-
     # check if class belongs to reviewee
     it { should belong_to :reviewee }
 
@@ -15,5 +17,14 @@ describe BookmarkRatingResponseMap do
     it '#get_title' do
       expect(model.get_title).to eq("Bookmark Review")
     end
+  end
+  describe '#questionnaire' do
+    it 'returns bookmark rating questionnaires associated with the assignment' do
+      model.assignment = assignment
+      questionnaires = [questionnaire1, questionnaire2]
+      allow(assignment).to receive(:questionnaires).and_return(questionnaires)
+      allow(questionnaires).to receive(:find_by).with(type: 'BookmarkRatingQuestionnaire').and_return(questionnaire2)
+      expect(model.questionnaire).to eq(questionnaire2)
+    end  
   end
 end

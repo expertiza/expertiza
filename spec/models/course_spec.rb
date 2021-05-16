5626de4118c0cd716a8cf947ef29a223f18f0396
John Bumgardner <john.bumgardner@icloud.com>
2021-05-16 12:04:11 -0400

    set permission_granted

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -62,7 +62,7 @@ describe CourseTeam do
         allow(User).to receive(:find_by).with(name: 'abc').and_return(user1)
         allow(user1).to receive(:id).and_return(1)
         allow(CourseParticipant).to receive(:where).with(parent_id: 1, user_id: 1).and_return([nil])
-        allow(CourseParticipant).to receive(:create).with(parent_id: 1, user_id: 1, permission_granted: false).and_return(participant)
+        allow(CourseParticipant).to receive(:create).with(parent_id: 1, user_id: 1, permission_granted: 0).and_return(participant)
         expect(course.add_participant('abc')).to eq(participant)
       end
     end

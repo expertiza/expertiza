e6c4506c41a38fd69c510ee1e129016ca7c6403c
John Bumgardner <john.bumgardner@icloud.com>
2021-05-16 11:23:28 -0400

    test get participant

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -35,4 +35,10 @@ describe CourseTeam do
       expect(course.get_participants).to eq([participant, participant2])
     end
   end
+  describe '#get_participant' do
+    it 'returns a specific participant from the user id' do
+      allow(CourseParticipant).to receive(:where).with(parent_id: 1, user_id: 2).and_return([participant2])
+      expect(course.get_participant(2)).to eq([participant2])
+    end
+  end
 end

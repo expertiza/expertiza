811d3d1976052ef2a0b762748073c4ea5ec3b242
John Bumgardner <john.bumgardner@icloud.com>
2021-05-17 13:04:14 -0400

    define ta_mapping

--- spec/models/ta_spec.rb
+++ spec/models/ta_spec.rb
@@ -62,6 +62,7 @@ describe Ta do
     end
     context 'there are  TaMappings for the user' do
       it 'returns an empty array' do
+      	ta_mapping = TaMapping.new
         allow(TaMapping).to receive(:where).with(ta_id: 999).and_return(ta_mapping)
         allow(ta_mapping).to receive(:course_id).and_return(1)
         allow(Course).to receive(:find).with(1).and_return(course1)

f45b753c39141e90a6467072cc6dc0545a060763
John Bumgardner <john.bumgardner@icloud.com>
2021-05-17 12:48:03 -0400

    add test for list_all

--- spec/models/ta_spec.rb
+++ spec/models/ta_spec.rb
@@ -2,6 +2,10 @@ describe Ta do
   let!(:ta) { create(:teaching_assistant, id: 999) }
   let(:course1) { build(:course, id: 1, name: 'ECE517') }
   let(:course2) { build(:course, id: 2, name: 'ECE506') }
+  let(:instructor) { build(:instructor, id: 6) }
+  let(:assignment) { build(:assignment, id: 1, name: 'no assignment', participants: [participant], teams: [team]) }
+  let(:participant) { build(:participant, id: 1) }
+  let(:team) { build(:assignment_team, id: 1, name: 'no team') }
   describe '#teaching_assistant?' do
     it 'returns true for a teaching assistant' do
       expect(ta.teaching_assistant?).to be_truthy
@@ -21,5 +25,9 @@ describe Ta do
       allow(Course).to receive(:find).with(1).and_return(course1)
       expect(ta.courses_assisted_with).to eq([course1])
     end
+  end
+  describe '#list_all' do
+    allow(Assignment).to receive(:where).with(["instructor_id = ? OR private = 0", 6]).and_return([assignment])
+    expect(ta.list_all(Assignment, 6)).to eq([assignment])
   end
 end

a14eca0faf9b809572f811ab5b706d22436ef4da
John Bumgardner <john.bumgardner@icloud.com>
2021-05-17 11:39:33 -0400

    fix globals

--- spec/models/dropdown_spec.rb
+++ spec/models/dropdown_spec.rb
@@ -2,6 +2,8 @@ describe Dropdown do
 	let(:dropdown) {build(:dropdown, id: 1)}
 	let(:questionnaire) { create(:questionnaire, id: 1) }
   let(:question1) { create(:question, questionnaire: questionnaire, weight: 1, id: 1, type: "Criterion") }
+  let(:response_map) { create(:review_response_map, id: 1, reviewed_object_id: 1) }
+  let!(:response_record) { create(:response, id: 1, response_map: response_map) }
   let!(:answer) { create(:answer, question: question1, comments: "test comment", response_id: 1) }
   describe '#view_question_text' do
     it 'returns the html' do

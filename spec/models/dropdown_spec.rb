85b4d866f789bdeb39c25e9ea3e10a5a23e4e2ef
John Bumgardner <john.bumgardner@icloud.com>
2021-05-17 11:41:21 -0400

    fix matcher

--- spec/models/dropdown_spec.rb
+++ spec/models/dropdown_spec.rb
@@ -8,13 +8,13 @@ describe Dropdown do
   describe '#view_question_text' do
     it 'returns the html' do
       html = dropdown.view_question_text
-      expect(html).to eq('<TR><TD align=\"left\"> Test question: </TD><TD align=\"left\">TrueFalse</TD><td align=\"center\">1</TD><TD align=\"center\">&mdash;</TD></TR>')
+      expect(html).to eq('<TR><TD align="left"> Test question: </TD><TD align="left">TrueFalse</TD><td align="center">1</TD><TD align="center">&mdash;</TD></TR>')
     end
   end
   describe '#view_completed_question' do
     it 'returns the html' do
       html = dropdown.view_completed_question(1, answer)
-      expect(html).to eq('')
+      expect(html).to eq('<b>1. Test question:</b><BR>&nbsp&nbsp&nbsp&nbsptest comment')
     end
   end
 end

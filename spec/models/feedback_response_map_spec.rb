ee5288a2b5d5ce02fe41163f3f71c294d175a180
John Bumgardner <john.bumgardner@icloud.com>
2021-05-18 11:47:52 -0400

    add test for get title and questionnaire

--- spec/models/feedback_response_map_spec.rb
+++ spec/models/feedback_response_map_spec.rb
@@ -10,11 +10,15 @@ describe FeedbackResponseMap do
   let(:answer) { Answer.new(answer: 1, comments: 'Answer text', question_id: 1) }
   let(:response) { build(:response, id: 1, map_id: 1, response_map: review_response_map, scores: [answer]) }
   before(:each) do
+  	questionnaires = [questionnaire1, questionnaire2]
     allow(feedback_response_map).to receive(:reviewee).and_return(participant)
     allow(feedback_response_map).to receive(:review).and_return(response)
     allow(feedback_response_map).to receive(:reviewer).and_return(assignment_participant)
     allow(response).to receive(:map).and_return(review_response_map)
     allow(review_response_map).to receive(:assignment).and_return(assignment)
+    allow(feedback_response_map).to receive(:assignment).and_return(assignment)
+    allow(assignment).to receive(:questionnaires).and_return(questionnaires)
+    allow(questionnaires).to receive(:find_by).with(type: 'AuthorFeedbackQuestionnaire').and_return([questionnaire1])
   end
   describe '#assignment' do
     it 'returns the assignment associated with this FeedbackResponseMap' do
@@ -34,5 +38,15 @@ describe FeedbackResponseMap do
         expect(feedback_response_map.show_review).to eq('No review was performed')
       end
     end
+  end
+  describe '#get_title' do
+    it 'returns "Feedback"' do
+      expect(feedback_response_map.get_title).to eq("Feedback")
+    end
   end  
+  describe '#questionnaire' do
+    it 'returns an AuthorFeedbackQuestionnaire' do
+      expect(feedback_response_map.questionnaire.first.type).to eq('AuthorFeedbackQuestionnaire')
+    end
+  end
 end

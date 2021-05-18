473b9e3a5ed15e084e78a9841a39e23784489d29
John Bumgardner <john.bumgardner@icloud.com>
2021-05-18 10:58:13 -0400

    add tests for Markup Style

---
+++ spec/models/markup_style_spec.rb
@@ -0,0 +1,8 @@
+describe MarkupStyle do
+  it "is invalid without valid attributes" do
+    expect(MarkupStyle.new).not_to be_valid
+  end
+  it "is valid with valid attributes" do
+    expect(MarkupStyle.new(name: 'Header for Question')).to be_valid
+  end
+end

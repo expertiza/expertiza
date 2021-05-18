a175377be829d6a36668f715a49ed3fb2bb2fc71
John Bumgardner <john.bumgardner@icloud.com>
2021-05-18 11:07:48 -0400

    access name

--- spec/models/markup_style_spec.rb
+++ spec/models/markup_style_spec.rb
@@ -5,4 +5,8 @@ describe MarkupStyle do
   it "is valid with valid attributes" do
     expect(MarkupStyle.new(name: 'Header for Question')).to be_valid
   end
+  it "can access name" do
+    ms = MarkupStyle.new(name: 'Header for Question')	
+    expect(ms.name).to eq('Header for Question')
+  end
 end

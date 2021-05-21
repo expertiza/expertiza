d84aff25fc04cab7d6e255573c7d285768ba56a3
John Bumgardner <john.bumgardner@icloud.com>
2021-05-21 15:59:00 -0400

    remove describ

--- spec/models/system_settings_spec.rb
+++ spec/models/system_settings_spec.rb
@@ -4,14 +4,10 @@ describe SystemSettings do
   	@student_role = build(:role_of_student, id: 1, name: "Student_role_test", description: '', parent_id: nil, default_page_id: nil)
   	@system_settings.public_role = @student_role
   end
-  describe 'test some basic equalities' do
-    it 'returns table name' do
-      expect(SystemSettings.table_name).to eq('system_settings')
-    end
+  it 'returns table name' do
+    expect(SystemSettings.table_name).to eq('system_settings')
   end
-  describe '#public_role' do
-  	it 'returns role' do
-      expect(@system_settings.public_role).to be(@student_role)
-    end
+  it 'returns role' do
+    expect(@system_settings.public_role).to be(@student_role)
   end
 end

1ed27e7d1817113797af23e1c3fdff47b7b63267
John Bumgardner <john.bumgardner@icloud.com>
2021-05-22 22:40:08 -0400

    test update mapping

--- spec/models/rubric_spec.rb
+++ spec/models/rubric_spec.rb
@@ -5,4 +5,10 @@ describe Rubric do
   it 'is valid with a name, max and min question scores' do
     expect(Rubric.new(name: 'Test Assignment Rubric', max_question_score: 100, min_question_score: 0)).to be_valid
   end
+  describe '#update_mapping' do
+    it 'redirects to action list' do
+      rubric = Rubric.new(name: 'Test Assignment Rubric', max_question_score: 100, min_question_score: 0)
+      expect{rubric.update_mapping}.to redirect_to('/assignments/1/edit')
+    end
+  end
 end

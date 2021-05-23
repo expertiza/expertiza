2f8611f63643faaca654252892d8eea99b781ed2
John Bumgardner <john.bumgardner@icloud.com>
2021-05-22 22:16:35 -0400

    add tests for validations

---
+++ spec/models/rubric_spec.rb
@@ -0,0 +1,8 @@
+describe Rubric do
+  it 'is not valid without a name, max and min question scores' do
+    expect(Rubric.new).to_not be_valid
+  end
+  it 'is valid with a name, max and min question scores' do
+    expect(Rubric.new(name: 'Test Assignment Rubric', max_question_score: 100, min_question_score: 0)).to be_valid
+  end
+end

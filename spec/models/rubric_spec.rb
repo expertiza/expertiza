18f86c689be2ea401ad26e730d4fdcab7e5b050e
John Bumgardner <john.bumgardner@icloud.com>
2021-05-22 22:46:59 -0400

    delete uneeded message

--- spec/models/rubric_spec.rb
+++ spec/models/rubric_spec.rb
@@ -5,11 +5,4 @@ describe Rubric do
   it 'is valid with a name, max and min question scores' do
     expect(Rubric.new(name: 'Test Assignment Rubric', max_question_score: 100, min_question_score: 0)).to be_valid
   end
-  describe '#update_mapping' do
-    it 'redirects to action list' do
-      rubric = Rubric.new(name: 'Test Assignment Rubric', max_question_score: 100, min_question_score: 0)
-      allow(rubric).to receive(:type_id).and_return(1)
-      expect(rubric.update_mapping).to eq('/rubrics/1/list')
-    end
-  end
 end

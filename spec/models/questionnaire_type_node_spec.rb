72e3044298cf2ca27d4f510bcd85b3a82b2f9bef
John Bumgardner <john.bumgardner@icloud.com>
2021-05-27 14:17:21 -0400

    add test for get

--- spec/models/questionnaire_type_node_spec.rb
+++ spec/models/questionnaire_type_node_spec.rb
@@ -6,4 +6,13 @@ describe QuestionnaireTypeNode do
       expect(QuestionnaireTypeNode.table).to eq("tree_folders")
     end
   end
+  describe '#get' do
+    it 'gets nodes associated with the parent' do
+      tree_folder = double('TreeFolder', id: 1)
+      allow(TreeFolder).to receive(:find_by).with(name: 'Questionnaires').and_return(tree_folder)
+      allow(TreeFolder).to receive(:where).with(parent_id: 1).and_return([double('FolderNode', id: 1)])
+      allow(FolderNode).to receive(:find_by).with(node_object_id: 1).and_return(double('QuestionnaireNode'))
+      expect(QuestionnaireTypeNode.get().length).to eq(1)
+    end
+  end
 end

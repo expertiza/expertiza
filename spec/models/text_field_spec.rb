a350608becaebe7295421b6839a93a858e7158ac
John Bumgardner <john.bumgardner@icloud.com>
2021-06-01 11:50:34 -0400

    delete extra quote

--- spec/models/text_field_spec.rb
+++ spec/models/text_field_spec.rb
@@ -6,7 +6,7 @@ describe TextField do
       tf.txt = 'Field Text'
       tf.size = 35
       html = tf.complete(1, answer)
-      expect(html).to eq('<p style="width: 80%;"><label for="responses_1" >Field Text&nbsp;&nbsp;</label><input id="responses_1_score" name="responses[1][score]" type="hidden" value="" "><input id="responses_1_comments" label=Field Text name="responses[1][comment]" style="width: 40%;" size=35 type="text"value="test comment">'')
+      expect(html).to eq('<p style="width: 80%;"><label for="responses_1" >Field Text&nbsp;&nbsp;</label><input id="responses_1_score" name="responses[1][score]" type="hidden" value="" "><input id="responses_1_comments" label=Field Text name="responses[1][comment]" style="width: 40%;" size=35 type="text"value="test comment">')
     end
   end
 end

4774778679c35cc152336d554495d9411d5bda9c
John Bumgardner <john.bumgardner@icloud.com>
2021-06-07 18:12:40 -0400

    add do

--- spec/models/text_area_spec.rb
+++ spec/models/text_area_spec.rb
@@ -1,14 +1,14 @@
 describe TextArea do
   let(:text_area) {create(:text_area)}
   describe '#complete' do
-    context 'when the size is nil'
+    context 'when the size is nil' do
       it 'uses a default size and returns html' do
         allow(text_area).to receive(:size).and_return(nil)
         html = text_area.complete(1,nil)
         expect(html).to eq('')
       end
     end
-    context 'when the size is set'
+    context 'when the size is set' do
       it 'uses that size and returns html' do
         allow(text_area).to receive(:size).and_return('1,1')
         html = text_area.complete(1,nil)

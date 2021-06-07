bbe87053369688e289da41dd2f11188370674246
John Bumgardner <john.bumgardner@icloud.com>
2021-06-07 18:23:25 -0400

    try with blank

--- spec/models/text_area_spec.rb
+++ spec/models/text_area_spec.rb
@@ -1,5 +1,6 @@
 describe TextArea do
   let(:text_area) {create(:text_area)}
+  let!(:answer) { create(:answer, comments: "test comment") }
   describe '#complete' do
     context 'when the size is nil' do
       it 'uses a default size and returns html' do
@@ -14,6 +15,12 @@ describe TextArea do
         html = text_area.complete(1,nil)
         expect(html).to eq('<p><label for="responses_1">Test question:</label></p><input id="responses_1_score" name="responses[1][score]" type="hidden" value=""><p><textarea cols="1" rows="1" id="responses_1_comments" name="responses[1][comment]" class="tinymce"></textarea></p>')
       end
+    end
+  end
+  describe '#view_completed_questions' do
+    it 'return html' do
+      html = text_area.view_completed_question(1,answer)
+      expect(html).to eq('')
     end
   end
 end

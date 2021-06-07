7ad05047dbfe8cbb11fab26c5423698c433fbcf1
John Bumgardner <john.bumgardner@icloud.com>
2021-06-07 18:20:46 -0400

    check with custom var

--- spec/models/text_area_spec.rb
+++ spec/models/text_area_spec.rb
@@ -12,7 +12,7 @@ describe TextArea do
       it 'uses that size and returns html' do
         allow(text_area).to receive(:size).and_return('1,1')
         html = text_area.complete(1,nil)
-        expect(html).to eq('<p><label for="responses_1">Test question:</label></p><input id="responses_1_score" name="responses[1][score]" type="hidden" value=""><p><textarea cols="70" rows="1" id="responses_1_comments" name="responses[1][comment]" class="tinymce"></textarea></p>')
+        expect(html).to eq('<p><label for="responses_1">Test question:</label></p><input id="responses_1_score" name="responses[1][score]" type="hidden" value=""><p><textarea cols="1" rows="1" id="responses_1_comments" name="responses[1][comment]" class="tinymce"></textarea></p>')
       end
     end
   end

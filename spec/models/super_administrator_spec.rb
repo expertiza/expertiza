23c23943d3afacb400df3428d6320b3c943b3016
John Bumgardner <john.bumgardner@icloud.com>
2021-06-08 20:56:56 -0400

    add test for super admin

---
+++ spec/models/super_administrator_spec.rb
@@ -0,0 +1,13 @@
+describe SuperAdministration do
+  let(:user1) { User.new name: 'abc', fullname: 'abc bbc', email: 'abcbbc@gmail.com', password: '123456789', password_confirmation: '123456789' }
+  let(:user2) { User.new name: 'abc', fullname: 'abc bbc', email: 'abcbbe@gmail.com', password: '123456789', password_confirmation: '123456789' }
+  let(:superadmin) {build(:superadmin)}
+  describe '#get_user_list' do
+    it 'should return a list of all users in the system' do
+      obj = [user1, user2]
+      allow(User).to receive(:all).and_return(obj)
+      expect(obj).to receive(:find_each).and_yield(obj.first).and_yield(object.last)
+      expect(superadmin.get_user_list).to eq(obj)
+    end
+  end
+end

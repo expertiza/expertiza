0420bd1fc69b38b1d09091d577fecb315a10e750
John Bumgardner <john.bumgardner@icloud.com>
2021-06-17 00:25:36 -0400

    please work

--- spec/models/late_policy_spec.rb
+++ spec/models/late_policy_spec.rb
@@ -2,7 +2,7 @@ describe LatePolicy do
   describe '#check_policy_with_same_name' do
     it 'returns true when there is a policy with the same name' do
    	  lp = LatePolicy.new(policy_name: 'late_policy_1', instructor_id: 6, max_penalty: 5, penalty_per_unit: 5, penalty_unit: 1)
-      lo.instructor_id = 6
+      lp.instructor_id = 6
       list = [lp]
       allow(LatePolicy).to receive(:where).with(policy_name: 'late_policy_1').and_return(list)
       expect(LatePolicy.check_policy_with_same_name('late_policy_1', 6)).to be_truthy 

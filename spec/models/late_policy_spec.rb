44aaeffe256ad2e9cf8d7cde22c4220395c707af
John Bumgardner <john.bumgardner@icloud.com>
2021-06-17 00:48:39 -0400

    add stubs

--- spec/models/late_policy_spec.rb
+++ spec/models/late_policy_spec.rb
@@ -13,30 +13,43 @@ describe LatePolicy do
   describe '#update_calculated_penalty_objects' do
   	before(:all) do
       lp = LatePolicy.new(policy_name: 'late_policy_1', instructor_id: 6, max_penalty: 5, penalty_per_unit: 5, penalty_unit: 1)
-      cp = nil
-      allow(CalculatedPenalty).to receive(:all).and_return([cp])
-      allow(AssignmentParticipant).to receive(:find).with(2).and_return(participant)
-      allow(participant).to receive(:assignment).and_return(assignment)
-      allow(assignment).to receive(:late_policy_id).and_return(3)
-      allow(lp).to receive(:id).and_return(3)
-      penalties = {submission: 40, review: 30, meta_review: 30}
-      allow(LatePolicy).to receive(:calculate_penalty).with(2).and_return(penalties)
     end
     context 'when it is a submission type penalty' do
       it 'updates the penalty' do
         cp = CalculatedPenalty.create(deadline_type_id: 1, participant_id: 2, penalty_points: nil)
+        allow(CalculatedPenalty).to receive(:all).and_return([cp])
+        allow(AssignmentParticipant).to receive(:find).with(2).and_return(participant)
+        allow(participant).to receive(:assignment).and_return(assignment)
+        allow(assignment).to receive(:late_policy_id).and_return(3)
+        allow(lp).to receive(:id).and_return(3)
+        penalties = {submission: 40, review: 30, meta_review: 30}
+        allow(LatePolicy).to receive(:calculate_penalty).with(2).and_return(penalties)
         expect(cp.penalty_points).to eq(40)
       end
     end
     context 'when it is a review type penalty' do
       it 'updates the penalty' do
         cp = CalculatedPenalty.create(deadline_type_id: 2, participant_id: 2, penalty_points: nil)
+        allow(CalculatedPenalty).to receive(:all).and_return([cp])
+        allow(AssignmentParticipant).to receive(:find).with(2).and_return(participant)
+        allow(participant).to receive(:assignment).and_return(assignment)
+        allow(assignment).to receive(:late_policy_id).and_return(3)
+        allow(lp).to receive(:id).and_return(3)
+        penalties = {submission: 40, review: 30, meta_review: 30}
+        allow(LatePolicy).to receive(:calculate_penalty).with(2).and_return(penalties)
         expect(cp.penalty_points).to eq(30)
       end
     end
     context 'when it is a metareview type penalty' do
       it 'updates the penalty' do
         cp = CalculatedPenalty.create(deadline_type_id: 5, participant_id: 2, penalty_points: nil)
+        allow(CalculatedPenalty).to receive(:all).and_return([cp])
+        allow(AssignmentParticipant).to receive(:find).with(2).and_return(participant)
+        allow(participant).to receive(:assignment).and_return(assignment)
+        allow(assignment).to receive(:late_policy_id).and_return(3)
+        allow(lp).to receive(:id).and_return(3)
+        penalties = {submission: 40, review: 30, meta_review: 30}
+        allow(LatePolicy).to receive(:calculate_penalty).with(2).and_return(penalties)
         expect(cp.penalty_points).to eq(30)
       end
     end

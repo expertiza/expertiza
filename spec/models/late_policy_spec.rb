5662249f51259f58645206cdf199380d80bd74b9
John Bumgardner <john.bumgardner@icloud.com>
2021-06-17 00:05:41 -0400

    fix typo

--- spec/models/late_policy_spec.rb
+++ spec/models/late_policy_spec.rb
@@ -1,4 +1,4 @@
-descibe LatePolicy do
+describe LatePolicy do
   describe '#check_policy_with_same_name' do
     it 'returns true when there is a policy with the same name' do
         allow(LatePolicy).to receive(:where).with(policy_name: 'late_policy_name').and_return([LatePolicy.new(policy_name: 'late_policy_name', instructor_id: 6, max_penalty: 5, penalty_per_unit: 5, penalty_unit: 1)])

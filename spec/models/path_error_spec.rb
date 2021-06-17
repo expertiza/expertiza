e22d6dd5d50945268dfeda109264b3424a8017c6
John Bumgardner <john.bumgardner@icloud.com>
2021-06-17 00:54:48 -0400

    use it

--- spec/models/path_error_spec.rb
+++ spec/models/path_error_spec.rb
@@ -1,5 +1,7 @@
 describe PathError do
   describe 'path_error' do
-    expect{raise PathError.new}.to raise_error(PathError)
+  	it 'raises an error' do
+      expect{raise PathError.new}.to raise_error(PathError)
+    end
   end
 end

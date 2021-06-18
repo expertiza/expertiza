9b4846b5d37500f16c957d5cb2193012bb37817e
John Bumgardner <john.bumgardner@icloud.com>
2021-06-18 12:53:43 -0400

    add tests for CourseNode

---
+++ spec/models/course_node_spec.rb
@@ -0,0 +1,70 @@
+describe CourseNode do
+  let(:course) { build(:course, id: 1, name: 'ECE517') }
+  let(:course_node) { build(:course_node, id: 1) }
+  before(:each) do
+  	course_node.node_object_id = 1
+    allow(Course).to receive(:find_by).with(id: 1).and_return(course)
+    allow(CourseNode).to receive(:get_parent_id).and_return(1)
+  end
+  describe '#create_course_node' do
+    it 'saves a course node with course data' do
+      
+    end
+  end
+  describe '#table' do
+    it 'returns courses' do
+  
+    end
+  end
+  describe '#get_course_query_conditions' do
+    context 'when show and current user are set and you are not a TA' do
+      it 'returns a string of conditions' do
+
+      end
+    end
+    context 'when show and current user are set and you are a TA' do
+      it 'returns a string of conditions' do
+
+      end
+    end
+    context 'when show and current user are not set and you are not a TA' do
+      it 'returns a string of conditions' do
+
+      end
+    end
+  end
+  describe '#get_courses_managed_by_users' do
+    context 'when you arent a TA' do
+      it 'returns the user id' do
+
+      end
+    end
+  end
+  describe '#get_parent_id' do
+    context 'when parent is found' do
+      it 'returns the id of the parent folder' do
+
+      end
+    end
+    context 'when parent is found' do
+      it 'returns nil' do
+
+      end
+    end
+  end
+  describe '#get_children' do
+    it 'returns assignment node' do
+
+    end
+  end
+  describe '#get_private' do
+    it 'returns whether the course is private' do
+
+    end
+  end
+  describe '#get_survey_distribution_id' do
+    it 'returns whether the course is private' do
+
+    end
+  end
+end

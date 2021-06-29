a619da503f370f48ded58855648bcc2ef634a41f
John Bumgardner <john.bumgardner@icloud.com>
2021-06-29 15:44:42 -0400

    add tests for badge

---
+++ spec/models/badge_spec.rb
@@ -0,0 +1,18 @@
+describe Badge do
+  let(:awarded_badge) { build(:awarded_badge) }
+  before(:each) do
+    allow(Badge).to receive(:find_by).and_return(awarded_badge)
+    allow(awarded_badge).to receive(:try).with(:id).and_return(1)
+    allow(awarded_badge).to receive(:try).with(:image_name).and_return('image_name')	
+  end
+  describe '#get_id_from_name' do
+    it 'returns the badge id from the name' do
+      expect(Badge.get_id_from_name('assignment_badge').to eq(1)
+    end
+  end
+  describe '#get_image_name_from_name' do
+    it 'returns the badge id from the name' do
+      expect(Badge.get_image_name_from_name('assignment_badge').to eq('image_name')
+    end
+  end
+end

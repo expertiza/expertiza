c6b6cfffb84d580bfc714ff5d2395f82f774a39a
John Bumgardner <john.bumgardner@icloud.com>
2021-08-08 23:34:58 -0400

    fix maybe

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -511,7 +511,7 @@ class AssignmentsController < ApplicationController
   def query_participants_and_alert
     assignment = Assignment.find(params[:id])
     if assignment.participants.size == 0
-      flash[:alert] = %Q[Saved assignment is missing participants. Add them here <a href="participants/list?id=#{assignment.id}&model=Assignment">#{assignment.id}</a>]
+      flash[:error] = %Q[Saved assignment is missing participants. Add them <a href="participants/list?id=#{assignment.id}&model=Assignment">here</a>]
     end
   end
 

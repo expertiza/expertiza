a9e0a1ff11576af0385b65312f007e8bc57885db
John Bumgardner <john.bumgardner@icloud.com>
2021-08-25 21:55:54 -0400

    rrrr

--- app/models/response.rb
+++ app/models/response.rb
@@ -305,7 +305,7 @@ class Response < ActiveRecord::Base
       row_class = count.even? ? "info" : "warning"
       row_class = "" if question.is_a? QuestionnaireHeader
       code += '<tr class="' + row_class + '"><td>'
-      unless answer.nil? || !question.is_a? QuestionnaireHeader
+      unless answer.nil? && !question.is_a? QuestionnaireHeader
         code += if question.instance_of? Criterion
                   # Answer Tags are enabled only for Criterion questions at the moment.
                   question.view_completed_question(count, answer, questionnaire_max, tag_prompt_deployments, current_user) || ''

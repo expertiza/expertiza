90d0ae39fbffaedad9c1a084633f06b5a7bc36cf
Nicholas Himes <41524135+nnhimes@users.noreply.github.com>
2021-08-27 14:58:14 -0400

    Create admin_controller_spec.rb

---
+++ spec/controllers/admin_controller_spec.rb
@@ -0,0 +1,31 @@
+describe AdminController do
+
+    describe '#action_allowed?' do
+        
+    end
+
+    describe '#list_super_administrators' do
+        
+    end
+
+    describe '#show_super_administrator' do
+        
+    end
+
+    describe '#list_administrators' do
+        
+    end
+
+    describe '#show_administrator' do
+        
+    end
+
+    describe '#list_instructors' do
+        
+    end
+
+    describe '#show_instructor' do
+        
+    end
+
+end

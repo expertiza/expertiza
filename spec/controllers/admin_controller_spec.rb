1bacd0cec047373b27738ca00329bc55c4669ae2
Nicholas Himes <41524135+nnhimes@users.noreply.github.com>
2021-08-28 15:26:41 -0400

    action_allowed draft

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -1,7 +1,20 @@
 describe AdminController do
 
     describe '#action_allowed?' do
-        
+        context 'when the student has admin privileges' do
+            it 'returns true' do 
+                params = {action: 'list_instructors'}
+                session[:user].role.name = 'Administrator'
+                expect(controller.action_allowed?).to eq(true)
+            end
+        end
+        context 'when the student doesnt have admin privileges' do
+            it 'returns false' do
+                params = {action: 'remove_instructor'}
+                session[:user].role.name = 'Student'
+                expect(controller.action_allowed?).to eq(false)
+            end
+        end
     end
 
     describe '#list_super_administrators' do

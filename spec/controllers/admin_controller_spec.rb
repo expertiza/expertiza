2498817180672b0fe2dafd865df33b8abed88233
Nicholas Himes <41524135+nnhimes@users.noreply.github.com>
2021-08-28 15:38:50 -0400

    Add mocked vars

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -1,5 +1,27 @@
 describe AdminController do
-
+    let(:review_response) { build(:response) }
+    let(:assignment) { build(:assignment, id: 1, max_team_size: 2, questionnaires: [review_questionnaire], is_penalty_calculated: true)}
+    let(:assignment_questionnaire) { build(:assignment_questionnaire, used_in_round: 1, assignment: assignment) }
+    let(:participant) { build(:participant, id: 1, assignment: assignment, user_id: 1) }
+    let(:participant2) { build(:participant, id: 2, assignment: assignment, user_id: 1) }
+    let(:review_questionnaire) { build(:questionnaire, id: 1, questions: [question]) }
+    let(:admin) { build(:admin) }
+    let(:instructor) { build(:instructor, id: 6) }
+    let(:question) { build(:question) }
+    let(:team) { build(:assignment_team, id: 1, assignment: assignment, users: [instructor]) }
+    let(:student) { build(:student) }
+    let(:review_response_map) { build(:review_response_map, id: 1) }
+    let(:assignment_due_date) { build(:assignment_due_date) }
+    let(:ta) { build(:teaching_assistant, id: 8) }
+  
+    before(:each) do
+      allow(AssignmentParticipant).to receive(:find).with('1').and_return(participant)
+      allow(participant).to receive(:team).and_return(team)
+      stub_current_user(instructor, instructor.role.name, instructor.role)
+      allow(Assignment).to receive(:find).with('1').and_return(assignment)
+      allow(Assignment).to receive(:find).with(1).and_return(assignment)
+    end
+    
     describe '#action_allowed?' do
         context 'when the student has admin privileges' do
             it 'returns true' do 

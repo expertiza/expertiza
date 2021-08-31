edc174b873a515583c6978adb1cbcf9d1434d653
John Bumgardner <john.bumgardner@icloud.com>
2021-08-30 22:47:38 -0400

    fix syntax

--- app/models/response.rb
+++ app/models/response.rb
@@ -305,7 +305,7 @@ class Response < ActiveRecord::Base
       row_class = count.even? ? "info" : "warning"
       row_class = "" if question.is_a? QuestionnaireHeader
       code += '<tr class="' + row_class + '"><td>'
-      unless answer.nil? && !question.is_a? QuestionnaireHeader
+      if !answer.nil? || question.is_a? QuestionnaireHeader
         code += if question.instance_of? Criterion
                   # Answer Tags are enabled only for Criterion questions at the moment.
                   question.view_completed_question(count, answer, questionnaire_max, tag_prompt_deployments, current_user) || ''
@@ -386,7 +386,7 @@ class Response < ActiveRecord::Base
         end
       end
       max_question_score = questionnaire_data.q1_max_question_score.to_f
-      if sum_of_weights > 0  && max_question_score && !weighted_score.nil?
+      if sum_of_weights > 0 && max_question_score && !weighted_score.nil?
         return (weighted_score / (sum_of_weights * max_question_score)) * 100
       else
         return -1.0 # indicating no score

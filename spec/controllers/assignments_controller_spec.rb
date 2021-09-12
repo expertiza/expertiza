b31f220c871c8f4998b5339842074f154e73c8a3
Nicholas Himes <41524135+nnhimes@users.noreply.github.com>
2021-09-12 17:12:51 -0400

    Add spec case for new error message

--- spec/controllers/assignments_controller_spec.rb
+++ spec/controllers/assignments_controller_spec.rb
@@ -274,6 +274,17 @@ describe AssignmentsController do
           expect(response).to render_template('assignments/edit/_topics')
         end
       end
+
+      context 'when update assignment_form is called on an empty questionnaire of non-zero weight' do
+        it 'shows an error message and redirects to assignments#edit page' do
+          @params[assignment_form][assignment_questionnaire][0]["questionnaire_weight"] = "100"
+          session = {user: instructor}
+          post :update, @params, session
+          expect(flash[:note]).to eq('The assignment was successfully saved....')
+          expect(flash[:error]).to eq("A rubric has no ScoredQuestions, but still has a weight. Please change the weight to 0.")
+          expect(response).to render_template('assignments/edit/_topics')
+        end
+      end
     end
   end
 

69b94995281e4920f46b446e87177e2dc4cbcc6b
Nicholas Himes <41524135+nnhimes@users.noreply.github.com>
2021-09-13 23:38:35 -0400

    Rubrics tab weight check (#2034)
    * Flash error if a rubric is used with a weight but no Scored Questions
    
    * Move init of rubric_weight_error
    
    * Make rubric_weight_error its own var
    
    * Fix forgotten variable rename
    
    * Remove old update note
    
    * Add back elsif for logic fix
    
    * Change location of flash
    
    * Turn rubric_weight_error into method
    
    * Add missing end
    
    * Call id from object
    
    * Question.where with specialized form params
    
    * Fix attributes each error
    
    * Puts for debugging
    
    * Raise true/false from each loop
    
    * Fix logic error
    
    * Remove puts, add flash back
    
    * Fix attempt for errors call
    
    * Fix attempt 2
    
    * Attempt 3
    
    * Attempt 4
    
    * assignments_controller spec fix
    
    * Change rspec questionnaire weight to 0
    
    * Add spec case for new error message
    
    * Change to symbols

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -502,8 +502,11 @@ class AssignmentsController < ApplicationController
       flash[:error] = "There has been some submissions for the rounds of reviews that you're trying to reduce. You can only increase the round of review."
     elsif @assignment_form.update_attributes(assignment_form_params, current_user)
       flash[:note] = 'The assignment was successfully saved....'
+      if @assignment_form.rubric_weight_error(assignment_form_params)
+        flash[:error] = "A rubric has no ScoredQuestions, but still has a weight. Please change the weight to 0."
+      end
     else
-      flash[:error] = "Failed to save the assignment: #{@assignment_form.errors.get(:message)}"
+      flash[:error] = "Failed to save the assignment: #{@assignment_form.errors.to_s}"
     end
     ExpertizaLogger.info LoggerMessage.new("", session[:user].name, "The assignment was saved: #{@assignment_form.as_json}", request)
   end

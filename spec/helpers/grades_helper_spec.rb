e8c49f899d879604fc5e1af479b5ccdecc63cfe9
John Bumgardner <john.bumgardner@icloud.com>
2021-09-27 00:03:20 -0400

    remove helper test

--- spec/helpers/grades_helper_spec.rb
+++ spec/helpers/grades_helper_spec.rb
@@ -1,19 +1,4 @@
 describe GradesHelper, type: :helper do
-  describe 'get_accordion_title' do
-    it 'should render is_first:true if last_topic is nil' do
-      get_accordion_title(nil, 'last question')
-      expect(response).to render_template(partial: 'response/_accordion', locals: {title: 'last question', is_first: true})
-    end
-    it 'should render is_first:false if last_topic is not equal to next_topic' do
-      get_accordion_title('last question', 'next question')
-      expect(response).to render_template(partial: 'response/_accordion', locals: {title: 'next question', is_first: false})
-    end
-    it 'should render nothing if last_topic is equal to next_topic' do
-      get_accordion_title('question', 'question')
-      expect(response).to render_template(nil)
-    end
-  end
-
   describe 'get_css_style_for_X_reputation' do
     hamer_input = [-0.1, 0, 0.5, 1, 1.5, 2, 2.1]
     lauw_input = [-0.1, 0, 0.2, 0.4, 0.6, 0.8, 0.9]

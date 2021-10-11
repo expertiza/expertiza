e7e7658005ee6396989a7a7d975a6aa525d0bfec
MathewAaron <aaron.mathew0217@gmail.com>
2021-10-11 12:09:22 -0400

    E2129: Changes in spec/controller/password_retrieval_controller.rb

--- spec/controllers/password_retrieval_controller_spec.rb
+++ spec/controllers/password_retrieval_controller_spec.rb
@@ -40,7 +40,7 @@ describe PasswordRetrievalController do
       @password_retrival.save!
 
       Timecop.freeze(Time.zone.today + 2.days) do
-        get :check_reset_url, token: local_token
+        get :check_token_validity, token: local_token
         expect(response).to render_template "password_retrieval/forgotten"
       end
     end
@@ -53,7 +53,7 @@ describe PasswordRetrievalController do
       @password_retrival.user_email = "example@example.edu"
       @password_retrival.save!
 
-      get :check_reset_url, token: local_token_sent_as_parameter
+      get :check_token_validity, token: local_token_sent_as_parameter
       expect(response).to render_template "password_retrieval/forgotten"
     end
 
@@ -64,7 +64,7 @@ describe PasswordRetrievalController do
       @password_retrival.user_email = "example@example.edu"
       @password_retrival.save!
       Timecop.freeze(@password_retrival.updated_at + 2.hours) do
-        get :check_reset_url, token: local_token
+        get :check_token_validity, token: local_token
         expect(response).to render_template "password_retrieval/reset_password"
       end
     end

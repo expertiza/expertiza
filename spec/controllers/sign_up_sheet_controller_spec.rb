88ea8b52d783bd2fed5b104ec449388996b9f4d7
ShlokSayani <shloksayani77@gmail.com>
2021-10-10 20:35:22 -0400

    Added Test Case for deletion of multiple topics in private assignment

--- spec/controllers/sign_up_sheet_controller_spec.rb
+++ spec/controllers/sign_up_sheet_controller_spec.rb
@@ -1,6 +1,6 @@
 describe SignUpSheetController do
   let(:assignment) { build(:assignment, id: 1, instructor_id: 6, due_dates: [due_date], microtask: true, staggered_deadline: true) }
-  let(:assignment) { build(:assignment, id: 2, instructor_id: 6, due_dates: [due_date], private: true) }
+  let(:assignment2) { build(:assignment, id: 2, instructor_id: 6, due_dates: [due_date], private: true) }
   let(:instructor) { build(:instructor, id: 6) }
   let(:student) { build(:student, id: 8) }
   let(:participant) { build(:participant, id: 1, user_id: 6, assignment: assignment) }
@@ -148,6 +148,18 @@ describe SignUpSheetController do
     it 'delete_all_selected_topics for a private assignment and redirects to edit assignment page with single topic as input' do
       allow(SignUpTopic).to receive(:find).with(assignment_id: 2,topic_identifier: ['E1732']).and_return(topic)
       params = {assignment_id: 2, topic_ids: ['E1732']}
+      post :delete_all_selected_topics, params
+      expect(flash[:success]).to eq('All selected topics have been deleted successfully.')
+      topics_exist = SignUpTopic.where(assignment_id: 2).count
+      expect(topics_exist).to be_eql 0
+      expect(response).to redirect_to('/assignments/2/edit#tabs-2')
+    end
+  end
+
+  describe '#delete_all_selected_topics' do
+    it 'delete_all_selected_topics for a private assignment and redirects to edit assignment page with multiple topics as input' do
+      allow(SignUpTopic).to receive(:find).with(assignment_id: 2,topic_identifier: ['E1732', 'E1733']).and_return([topic])
+      params = {assignment_id: 2, topic_ids: ['E1732', 'E1733']}
       post :delete_all_selected_topics, params
       expect(flash[:success]).to eq('All selected topics have been deleted successfully.')
       topics_exist = SignUpTopic.where(assignment_id: 2).count

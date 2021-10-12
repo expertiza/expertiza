5813abe3b58a7f6e079996adbad81e11ecd37d0f
Vinay Deshmukh <vinay_deshmukh@outlook.com>
2021-10-11 20:03:00 -0400

    Add test for max penalty should be greater than penalty per point

--- spec/controllers/late_policies_controller_spec.rb
+++ spec/controllers/late_policies_controller_spec.rb
@@ -25,5 +25,20 @@ describe LatePoliciesController do
       expect(flash[:error]).to include("Policy name can't be blank")
       expect(flash[:error]).to include("Penalty per unit can't be blank")
     end
+
+    it "Max penalty should be less than penalty per unit" do
+      get :new
+      params = {
+        late_policy: {
+            policy_name: '',
+            penalty_per_unit: 1,
+            penalty_unit: '',
+            max_penalty: 0,
+        }
+      }
+      post :create, params
+      expect(flash[:error]).to include("The maximum penalty cannot be less than penalty per unit.")
+    end
+
   end
 end

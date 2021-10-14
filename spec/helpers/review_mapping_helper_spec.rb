e484e9bc9b30a9a02cb2d7d954a9cc71101d7992
alex308248 <alex308248@gmail.com>
2021-10-13 23:55:02 -0400

    add test cases of submitted_within_round?

--- spec/helpers/review_mapping_helper_spec.rb
+++ spec/helpers/review_mapping_helper_spec.rb
@@ -227,7 +227,91 @@ describe ReviewMappingHelper, type: :helper do
     end
 
   end
-  #submitted_hyperlink
+
+  #round, response_map, assignment_created, assignment_due_dates
+  describe 'submitted_hyperlink' do
+    before(:each) do
+      @assignment = create(:assignment, name: 'assignment', created_at: DateTime.now.in_time_zone - 13.day)
+      @reviewer = create(:participant, review_grade: nil)
+      @reviewee = create(:assignment_team, assignment: @assignment)
+      @response_map = create(:review_response_map, reviewer: @reviewer, reviewee: @reviewee)
+
+      # create due dates for assignment
+      @round = 2
+    end
+
+    it 'should return https://wiki.archlinux.org/123 if works was submitted on time' do
+      create(:deadline_right, name: 'No')
+      create(:deadline_right, name: 'Late')
+      create(:deadline_right, name: 'OK')
+      create(:submission_record, assignment_id: @assignment.id, team_id: @reviewee.id, operation: 'Submit Hyperlink', content: 'https://wiki.archlinux.org/', created_at: DateTime.now.in_time_zone - 12.day)
+      create(:submission_record, assignment_id: @assignment.id, team_id: @reviewee.id, operation: 'Submit Hyperlink', content: 'https://wiki.archlinux.org/123', created_at: DateTime.now.in_time_zone - 7.day)
+      create(:response, response_map: @response_map)
+      create(:assignment_due_date, assignment: @assignment, parent_id: @assignment.id, round: 1, due_at: DateTime.now.in_time_zone - 10.day)
+      create(:assignment_due_date, assignment: @assignment, parent_id: @assignment.id, round: 2, due_at: DateTime.now.in_time_zone - 5.day)
+
+      assignment_created = @assignment.created_at
+      assignment_due_dates = DueDate.where(parent_id: @response_map.reviewed_object_id)
+
+      hyper_link = submitted_hyperlink(@round, @response_map, assignment_created, assignment_due_dates)
+      expect(hyper_link).to eq('https://wiki.archlinux.org/123')
+    end
+
+    it 'should return https://wiki.archlinux.org/123 if only a work was submitted on time' do
+      create(:deadline_right, name: 'No')
+      create(:deadline_right, name: 'Late')
+      create(:deadline_right, name: 'OK')
+      create(:submission_record, assignment_id: @assignment.id, team_id: @reviewee.id, operation: 'Submit Hyperlink', content: 'https://wiki.archlinux.org/', created_at: DateTime.now.in_time_zone - 8.day)
+      create(:submission_record, assignment_id: @assignment.id, team_id: @reviewee.id, operation: 'Submit Hyperlink', content: 'https://wiki.archlinux.org/123', created_at: DateTime.now.in_time_zone - 6.day)
+      create(:response, response_map: @response_map)
+      create(:assignment_due_date, assignment: @assignment, parent_id: @assignment.id, round: 1, due_at: DateTime.now.in_time_zone - 10.day)
+      create(:assignment_due_date, assignment: @assignment, parent_id: @assignment.id, round: 2, due_at: DateTime.now.in_time_zone - 5.day)
+
+      assignment_created = @assignment.created_at
+      assignment_due_dates = DueDate.where(parent_id: @response_map.reviewed_object_id)
+
+      hyper_link = submitted_hyperlink(@round, @response_map, assignment_created, assignment_due_dates)
+      expect(hyper_link).to eq('https://wiki.archlinux.org/123')
+    end
+
+    it 'should return https://wiki.archlinux.org/ if only a work was submitted on time' do
+      create(:deadline_right, name: 'No')
+      create(:deadline_right, name: 'Late')
+      create(:deadline_right, name: 'OK')
+      create(:submission_record, assignment_id: @assignment.id, team_id: @reviewee.id, operation: 'Submit Hyperlink', content: 'https://wiki.archlinux.org/', created_at: DateTime.now.in_time_zone - 12.day)
+      create(:submission_record, assignment_id: @assignment.id, team_id: @reviewee.id, operation: 'Submit Hyperlink', content: 'https://wiki.archlinux.org/123', created_at: DateTime.now.in_time_zone - 4.day)
+      create(:response, response_map: @response_map)
+      create(:assignment_due_date, assignment: @assignment, parent_id: @assignment.id, round: 1, due_at: DateTime.now.in_time_zone - 10.day)
+      create(:assignment_due_date, assignment: @assignment, parent_id: @assignment.id, round: 2, due_at: DateTime.now.in_time_zone - 5.day)
+
+      assignment_created = @assignment.created_at
+      assignment_due_dates = DueDate.where(parent_id: @response_map.reviewed_object_id)
+
+      hyper_link = submitted_hyperlink(@round, @response_map, assignment_created, assignment_due_dates)
+      expect(hyper_link).to eq('https://wiki.archlinux.org/')
+    end
+
+
+    it 'should return https://wiki.archlinux.org/ if only a work was Submit Hyperlink' do
+      create(:deadline_right, name: 'No')
+      create(:deadline_right, name: 'Late')
+      create(:deadline_right, name: 'OK')
+      create(:submission_record, assignment_id: @assignment.id, team_id: @reviewee.id, operation: 'Submit Hyperlink', content: 'https://wiki.archlinux.org/', created_at: DateTime.now.in_time_zone - 12.day)
+      create(:submission_record, assignment_id: @assignment.id, team_id: @reviewee.id, operation: 'Not Submit Hyperlink', content: 'https://wiki.archlinux.org/123', created_at: DateTime.now.in_time_zone - 6.day)
+      create(:response, response_map: @response_map)
+      create(:assignment_due_date, assignment: @assignment, parent_id: @assignment.id, round: 1, due_at: DateTime.now.in_time_zone - 10.day)
+      create(:assignment_due_date, assignment: @assignment, parent_id: @assignment.id, round: 2, due_at: DateTime.now.in_time_zone - 5.day)
+
+      assignment_created = @assignment.created_at
+      assignment_due_dates = DueDate.where(parent_id: @response_map.reviewed_object_id)
+
+      hyper_link = submitted_hyperlink(@round, @response_map, assignment_created, assignment_due_dates)
+      expect(hyper_link).to eq('https://wiki.archlinux.org/')
+    end
+
+  end
+
+
   #get_link_updated_at
   #get_team_reviewed_link_name
   #initialize_chart_elements

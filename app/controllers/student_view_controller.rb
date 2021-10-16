cd1df04648f46fc5d00b35e0cd76b8bf961eaacd
Expertiza Developer <ggarrido39@gmail.com>
2021-10-16 18:21:33 -0400

    Remove student view feature test, add student view spec test, fix minor nav bar typo

--- app/controllers/student_view_controller.rb
+++ app/controllers/student_view_controller.rb
@@ -1,4 +1,10 @@
 class StudentViewController < ApplicationController
+  include AuthorizationHelper
+
+  def action_allowed?
+    current_user_has_instructor_privileges?
+  end
+
 	def flip_view
 		if(session[:flip_user] == nil || session[:flip_user] == false) then
 			session[:flip_user] = true

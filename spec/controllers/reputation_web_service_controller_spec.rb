c5f5b0650d08f6958cf7a31996754b0c8735fc5e
Henry Chen <hchen34@ncsu.edu>
2021-10-17 17:26:35 -0400

    minor fix

--- spec/controllers/reputation_web_service_controller_spec.rb
+++ spec/controllers/reputation_web_service_controller_spec.rb
@@ -4,15 +4,13 @@ describe ReputationWebServiceController do
     before(:each) do
       @assignment = create(:assignment, created_at: DateTime.now.in_time_zone - 13.day, submitter_count: 0, num_reviews: 3, staggerred_deadline: false, rounds_of_reviews: 2, reputation_algorithm: 'lauw')
 
-      @reviewer_1 = create(:participant, review_grade: nil)
-      @reviewer_2 = create(:participant, review_grade: nil)
-      @reviewer_3 = create(:participant, review_grade: nil)
-      @reviewer_4 = create(:participant, review_grade: nil)
-      @reviewer_5 = create(:participant, review_grade: nil)
+      @reviewer_1 = create(:participant, can_review: 1)
+      @reviewer_2 = create(:participant, can_review: 1)
+      @reviewer_3 = create(:participant, can_review: 1)
+      @reviewer_4 = create(:participant, can_review: 1)
+      @reviewer_5 = create(:participant, can_review: 1)
 
-      @reviewee = create(:assignment_team)
-
-      @reviewee_with_assignment = create(:assignment_team, assignment: @assignment)
+      @reviewee = create(:assignment_team, assignment: @assignment)
 
       @response_map = create(:review_response_map, reviewer: @reviewer)
       @submission_records = create(:submission_records, assignment_id: @assignment.id, team_id: @reviewee.id, operation: 'Submit Hyperlink' content: 'weblink')

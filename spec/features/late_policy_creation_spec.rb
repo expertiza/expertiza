c03ac2b0c51076cf0afa2e4269185a2217d32845
Vinay Deshmukh <vinay_deshmukh@outlook.com>
2021-10-18 18:38:36 -0400

    Add proper test for late policy creation

--- spec/features/late_policy_creation_spec.rb
+++ spec/features/late_policy_creation_spec.rb
@@ -15,18 +15,34 @@ describe "Late Policy Creation" do
       # byebug
     # stub_current_user(instructor, instructor.role.name, instructor.role)
       # login_as("instructor6")
+      login_as("instructor6")
   end
 
   it "create new late policy for assignment" do
 
-      login_as("instructor6")
     visit edit_assignment_path(assignment)
     puts page.body
     click_on "Due dates"
     click_on "New late policy"
-
     expect(page).to have_current_path(new_late_policy_path)
-    expect(true).to eql(true)
+
+    policy_name = "Late policy name"
+    fill_in "late_policy[policy_name]", :with => policy_name
+    fill_in "late_policy[penalty_per_unit]", :with => "1"
+    fill_in "late_policy[max_penalty]", :with => "10"
+
+    click_on "Create"
+
+    expect(page).to have_current_path(late_policies_path)
+    expect(page).to have_content("The penalty policy was successfully created.")
+
+    # Check if database has the new policy
+    policy = LatePolicy.where(policy_name: policy_name).first
+    expect(policy).to have_attributes(
+      penalty_per_unit: 1,
+      max_penalty: 10
+    )
+
   end
 
 end

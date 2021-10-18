62374a39a042747663de3b39a918b2c456f0ee19
Henry Chen <hchen34@ncsu.edu>
2021-10-18 08:09:15 -0400

    modify for object creation

--- spec/controllers/reputation_web_service_controller_spec.rb
+++ spec/controllers/reputation_web_service_controller_spec.rb
@@ -1,12 +1,27 @@
 describe ReputationWebServiceController do
-  let(:team){build(:assignment_team, id:1)}
+  let(:instructor){build(:instructor, id:1)}
+
   describe '' do
     before(:each) do
-      @assignment = create(:assignment, created_at: DateTime.now.in_time_zone - 13.day, submitter_count: 0, num_reviews: 3, rounds_of_reviews: 2, reputation_algorithm: 'lauw')
-      @questionnaire_1 = create(:questionnaire, min_question_score: 0, max_question_score: 10, default_num_choices: 1, type: 'ReviewQuestionnaire')
-      @questionnaire_2 = create(:questionnaire, min_question_score: 0, max_question_score: 10, default_num_choices: 1, type: 'ReviewQuestionnaire')
-      @assignment_questionnaire_1 = create(:assignment_questionnaire, assignment_id: @assignment.id, questionnaire_id: @questionnaire_1.id, used_in_round: 1)
-      @assignment_questionnaire_2 = create(:assignment_questionnaire, assignment_id: @assignment.id, questionnaire_id: @questionnaire_2.id, used_in_round: 2)
+      @assignment_1 = create(:assignment, created_at: DateTime.now.in_time_zone - 13.day, submitter_count: 0, num_reviews: 3, rounds_of_reviews: 2, reputation_algorithm: 'lauw', id: 1)
+      @assignment_2 = create(:assignment, created_at: DateTime.now.in_time_zone - 13.day, submitter_count: 0, num_reviews: 3, rounds_of_reviews: 2, reputation_algorithm: 'hamer', id: 2)
+      @questionnaire_1 = create(:questionnaire, min_question_score: 0, max_question_score: 5, default_num_choices: 1, type: 'ReviewQuestionnaire', id: 1)
+      @questionnaire_2 = create(:questionnaire, min_question_score: 0, max_question_score: 5, default_num_choices: 1, type: 'ReviewQuestionnaire', id: 2)
+      @assignment_questionnaire_1_1 = create(:assignment_questionnaire, assignment_id: @assignment_1.id, questionnaire_id: @questionnaire_1.id, used_in_round: 1, id: 1)
+      @assignment_questionnaire_1_2 = create(:assignment_questionnaire, assignment_id: @assignment_1.id, questionnaire_id: @questionnaire_2.id, used_in_round: 2, id: 2)
+      @assignment_questionnaire_2_1 = create(:assignment_questionnaire, assignment_id: @assignment_2.id, questionnaire_id: @questionnaire_1.id, used_in_round: 1, id: 3)
+      @assignment_questionnaire_2_2 = create(:assignment_questionnaire, assignment_id: @assignment_2.id, questionnaire_id: @questionnaire_2.id, used_in_round: 2, id: 4)
+
+      @question_1_1 = create(:question, questionnaire_id: @questionnaire_1.id, id: 1)
+      @question_1_2 = create(:question, questionnaire_id: @questionnaire_1.id, id: 2)
+      @question_1_3 = create(:question, questionnaire_id: @questionnaire_1.id, id: 3)
+      @question_1_4 = create(:question, questionnaire_id: @questionnaire_1.id, id: 4)
+      @question_1_5 = create(:question, questionnaire_id: @questionnaire_1.id, id: 5)
+      @question_1_1 = create(:question, questionnaire_id: @questionnaire_2.id, id: 6)
+      @question_1_2 = create(:question, questionnaire_id: @questionnaire_2.id, id: 7)
+      @question_1_3 = create(:question, questionnaire_id: @questionnaire_2.id, id: 8)
+      @question_1_4 = create(:question, questionnaire_id: @questionnaire_2.id, id: 9)
+      @question_1_5 = create(:question, questionnaire_id: @questionnaire_2.id, id: 10)
 
       @reviewer_1 = create(:participant, can_review: 1)
       @reviewer_2 = create(:participant, can_review: 1)
@@ -14,36 +29,47 @@ describe ReputationWebServiceController do
       @reviewer_4 = create(:participant, can_review: 1)
       @reviewer_5 = create(:participant, can_review: 1)
 
-      @reviewee = create(:assignment_team, assignment: @assignment)
+      @reviewee_1 = create(:assignment_team, assignment: @assignment)
+      @reviewee_2 = create(:assignment_team, assignment: @assignment)
+      @reviewee_3 = create(:assignment_team, assignment: @assignment)
 
-      @response_map_1 = create(:review_response_map, reviewer: @reviewer_1)
-      @response_map_2 = create(:review_response_map, reviewer: @reviewer_2)
-      @response_map_3 = create(:review_response_map, reviewer: @reviewer_3)
-      @response_map_4 = create(:review_response_map, reviewer: @reviewer_4)
-      @response_map_5 = create(:review_response_map, reviewer: @reviewer_5)
+      @response_map_1_1 = create(:review_response_map, reviewer_id: @reviewer_1.id, reviewee_id: @reviewee_1.id)
+      @response_map_1_2 = create(:review_response_map, reviewer_id: @reviewer_2.id, reviewee_id: @reviewee_1.id)
+      @response_map_1_3 = create(:review_response_map, reviewer_id: @reviewer_3.id, reviewee_id: @reviewee_1.id)
+      @response_map_1_4 = create(:review_response_map, reviewer_id: @reviewer_4.id, reviewee_id: @reviewee_1.id)
+      @response_map_1_5 = create(:review_response_map, reviewer_id: @reviewer_5.id, reviewee_id: @reviewee_1.id)
+      @response_map_2_1 = create(:review_response_map, reviewer_id: @reviewer_1.id, reviewee_id: @reviewee_2.id)
+      @response_map_2_2 = create(:review_response_map, reviewer_id: @reviewer_2.id, reviewee_id: @reviewee_2.id)
+      @response_map_2_3 = create(:review_response_map, reviewer_id: @reviewer_3.id, reviewee_id: @reviewee_2.id)
+      @response_map_2_4 = create(:review_response_map, reviewer_id: @reviewer_4.id, reviewee_id: @reviewee_2.id)
+      @response_map_2_5 = create(:review_response_map, reviewer_id: @reviewer_5.id, reviewee_id: @reviewee_2.id)
+      @response_map_3_1 = create(:review_response_map, reviewer_id: @reviewer_1.id, reviewee_id: @reviewee_3.id)
+      @response_map_3_2 = create(:review_response_map, reviewer_id: @reviewer_2.id, reviewee_id: @reviewee_3.id)
+      @response_map_3_3 = create(:review_response_map, reviewer_id: @reviewer_3.id, reviewee_id: @reviewee_3.id)
+      @response_map_3_4 = create(:review_response_map, reviewer_id: @reviewer_4.id, reviewee_id: @reviewee_3.id)
+      @response_map_3_5 = create(:review_response_map, reviewer_id: @reviewer_5.id, reviewee_id: @reviewee_3.id)
 
+      @response_1_1 = create(:response, is_submitted: true, map_id: @response_map_1_1.id)
+      @response_1_2 = create(:response, is_submitted: true, map_id: @response_map_1_2.id)
+      @response_1_3 = create(:response, is_submitted: true, map_id: @response_map_1_3.id)
+      @response_1_4 = create(:response, is_submitted: true, map_id: @response_map_1_4.id)
+      @response_1_5 = create(:response, is_submitted: true, map_id: @response_map_1_5.id)
+      @response_2_1 = create(:response, is_submitted: true, map_id: @response_map_2_1.id)
+      @response_2_2 = create(:response, is_submitted: true, map_id: @response_map_2_2.id)
+      @response_2_3 = create(:response, is_submitted: true, map_id: @response_map_2_3.id)
+      @response_2_4 = create(:response, is_submitted: true, map_id: @response_map_2_4.id)
+      @response_2_5 = create(:response, is_submitted: true, map_id: @response_map_2_5.id)
+      @response_3_1 = create(:response, is_submitted: true, map_id: @response_map_3_1.id)
+      @response_3_2 = create(:response, is_submitted: true, map_id: @response_map_3_2.id)
+      @response_3_3 = create(:response, is_submitted: true, map_id: @response_map_3_3.id)
+      @response_3_4 = create(:response, is_submitted: true, map_id: @response_map_3_4.id)
+      @response_3_5 = create(:response, is_submitted: true, map_id: @response_map_3_5.id)
     end
 
     it '' do
-      # create()
+       create(:answer, question_id: @question_1_1, response_id: @response_1_1, answer: 5)
+       #repeat for different answers
     end
   end
 
-  describe 'create object answer' do
-    before(:each) do
-      @assignment1 = create(:assignment, name: "name1")
-      @questionnaire = create(:questionnaire)
-      @question = create(:question, questionnaire_id: @questionnaire.id)
-      @user = create(:student, name: "name", fullname: "name")
-      @participant = create(:participant, user_id: @user.id, parent_id: @assignment1.id)
-      @response_map = create(:review_response_map, reviewer: @participant, assignment: @assignment1)
-      @response = create(:response, response_map: @response_map, created_at: "2019-11-01 23:30:00")
-      @answer = create(:answer, response_id: @response.id, question_id: @question.id, comments: "comment")
-      @team = create(:assignment_team)
-    end
-
-    it 'should be used for reputation creation' do
-      # generate reputation
-    end
-  end
 end

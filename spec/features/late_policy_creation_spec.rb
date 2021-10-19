dfa9e14fc183d8253a631367ca6243d478c72328
Akash Sarda <akashsarda3@gmail.com>
2021-10-18 22:08:47 -0400

    added 2 capybara tests for controller

--- spec/features/late_policy_creation_spec.rb
+++ spec/features/late_policy_creation_spec.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 require_relative 'helpers/assignment_creation_helper'
 include AssignmentCreationHelper
 describe "Late Policy Creation" do
@@ -11,7 +12,7 @@ describe "Late Policy Creation" do
   end
 
   it "create new late policy for assignment" do
-
+    # the flow is by editing assignment
     visit edit_assignment_path(assignment)
     click_on "Due dates"
     click_on "New late policy"
@@ -35,7 +36,68 @@ describe "Late Policy Creation" do
       penalty_per_unit: penalty_per_unit,
       max_penalty: max_penalty,
     )
+  end
 
+  it "[negative] create new late policy for assignment with negative penalties" do
+    # the flow is by editing assignment
+    visit edit_assignment_path(assignment)
+    click_on "Due dates"
+    click_on "New late policy"
+    expect(page).to have_current_path(new_late_policy_path)
+
+    policy_name = "Negative penalty points late policy"
+    penalty_per_unit = -1
+    max_penalty = 10
+    fill_in "late_policy[policy_name]", :with => policy_name
+    fill_in "late_policy[penalty_per_unit]", :with => penalty_per_unit
+    fill_in "late_policy[max_penalty]", :with => max_penalty
+
+    click_on "Create"
+    expect(page).to have_current_path(new_late_policy_path)
+    expect(page).to have_content("Penalty per unit must be greater than 0")
+  end
+
+  it "[negative] create new late policy for assignment with blank values" do
+    # the flow is by editing assignment
+    visit edit_assignment_path(assignment)
+    click_on "Due dates"
+    click_on "New late policy"
+    expect(page).to have_current_path(new_late_policy_path)
+
+    policy_name = ""
+    penalty_per_unit = ''
+    max_penalty = ''
+    fill_in "late_policy[policy_name]", :with => policy_name
+    fill_in "late_policy[penalty_per_unit]", :with => penalty_per_unit
+    fill_in "late_policy[max_penalty]", :with => max_penalty
+
+    click_on "Create"
+    expect(page).to have_current_path(new_late_policy_path)
+    expect(page).to have_content("Policy name can't be blank")
+    expect(page).to have_content("Policy name is invalid")
+    expect(page).to have_content("Max penalty can't be blank")
+    expect(page).to have_content("Max penalty is not a number")
+    expect(page).to have_content("Penalty per unit can't be blank")
+    expect(page).to have_content("Penalty per unit is not a number")
+  end
+
+  it "[negative] create new late policy for assignment with blank values" do
+    # the flow is by editing assignment
+    visit edit_assignment_path(assignment)
+    click_on "Due dates"
+    click_on "New late policy"
+    expect(page).to have_current_path(new_late_policy_path)
+
+    policy_name = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    penalty_per_unit = 10
+    max_penalty = 1
+    fill_in "late_policy[policy_name]", :with => policy_name
+    fill_in "late_policy[penalty_per_unit]", :with => penalty_per_unit
+    fill_in "late_policy[max_penalty]", :with => max_penalty
+
+    click_on "Create"
+    expect(page).to have_current_path(new_late_policy_path)
+    expect(page).to have_content("Policy name is invalid")
   end
 
 end

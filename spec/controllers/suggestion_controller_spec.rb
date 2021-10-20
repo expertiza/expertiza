1019dceaf0ba83195babd03fb396529f99da7d6a
yliang123 <yliang24@ncsu.edu>
2021-10-20 00:10:33 -0400

    modify update_suggestion test

--- spec/controllers/suggestion_controller_spec.rb
+++ spec/controllers/suggestion_controller_spec.rb
@@ -14,7 +14,7 @@ describe SuggestionController do
   let(:assignment_questionnaire) { build(:assignment_questionnaire, id: 1, questionnaire: questionnaire) }
 
   before(:each) do
-    allow(assignment).to receive(:find).with('1').and_return(assignment)
+    allow(Assignment).to receive(:find).with('1').and_return(assignment)
     allow(Suggestion).to receive(:find).with('1').and_return(suggestion)
     stub_current_user(student, student.role.name, student.role)
   end
@@ -40,4 +40,37 @@ describe SuggestionController do
       expect(response).to render_template('suggestion/new?id=1')
     end
   end
+
+  describe '#create' do
+    let(:comment) { double('OODD', instructor_id: 2, path: '/cs', name: 'xyz') }
+    before(:each) do
+      allow(Course).to receive(:new).and_return(course_double)
+      allow(course_double).to receive(:save).and_return(true)
+    end
+
+    it "redirects to the correct url" do
+      post :create
+      expect(response).to redirect_to root_url
+    end
+  end
+
+  # describe '#add' do
+  #   it 'adds a participant' do
+  #     allow(Assignment).to receive(:find).with('1').and_return(assignment)
+  #     allow(User).to receive(:find_by).with(name: student.name).and_return(student)
+  #     params = {model: 'Assignment', authorization: 'participant', id: 1, user: {name: student.name}}
+  #     session = {user: instructor}
+  #     xhr :get, :add, params, session
+  #     expect(response).to render_template('add.js.erb')
+  #   end
+  #   it 'does not add a participant for a non-existing user' do
+  #     allow(Assignment).to receive(:find).with('1').and_return(assignment)
+  #     params = {model: 'Assignment', authorization: 'participant', id: 1, user: {name: 'Aaa'}}
+  #     session = {user: instructor}
+  #     xhr :get, :add, params, session
+  #     expect(flash[:error]).to eq 'The user <b>Aaa</b> does not exist or has already been added.'
+  #     expect(response).to render_template('add.js.erb')
+  #   end
+  # end
+
 end

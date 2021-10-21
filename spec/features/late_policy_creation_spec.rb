487fcdde8992617cf35587d4f6266a980614ea04
Akash Sarda <akashsarda3@gmail.com>
2021-10-20 21:29:41 -0400

    Addressed comments

--- spec/features/late_policy_creation_spec.rb
+++ spec/features/late_policy_creation_spec.rb
@@ -1,4 +1,3 @@
-require 'byebug'
 require_relative 'helpers/assignment_creation_helper'
 include AssignmentCreationHelper
 
@@ -19,13 +18,8 @@ describe "Late Policy Creation" do
       login_as("instructor6")
   end
 
-<<<<<<< HEAD
-  it "create new late policy for assignment" do
-    # the flow is by editing assignment
-=======
   it "create new late policy for assignment successfully" do
 
->>>>>>> 112b9c9e36e2797b96d0822a41e7eae6c69412ad
     visit edit_assignment_path(assignment)
     click_on "Due dates"
     click_on "New late policy"
@@ -63,9 +57,11 @@ describe "Late Policy Creation" do
     policy_name = "Negative penalty points late policy"
     penalty_per_unit = -1
     max_penalty = 10
-    fill_in "late_policy[policy_name]", :with => policy_name
-    fill_in "late_policy[penalty_per_unit]", :with => penalty_per_unit
-    fill_in "late_policy[max_penalty]", :with => max_penalty
+    fill_form_fields({
+        :policy_name => policy_name,
+        :penalty_per_unit => penalty_per_unit,
+        :max_penalty => max_penalty,
+      })
 
     click_on "Create"
     expect(page).to have_current_path(new_late_policy_path)
@@ -82,9 +78,11 @@ describe "Late Policy Creation" do
     policy_name = ""
     penalty_per_unit = ''
     max_penalty = ''
-    fill_in "late_policy[policy_name]", :with => policy_name
-    fill_in "late_policy[penalty_per_unit]", :with => penalty_per_unit
-    fill_in "late_policy[max_penalty]", :with => max_penalty
+    fill_form_fields({
+        :policy_name => policy_name,
+        :penalty_per_unit => penalty_per_unit,
+        :max_penalty => max_penalty,
+      })
 
     click_on "Create"
     expect(page).to have_current_path(new_late_policy_path)
@@ -106,9 +104,11 @@ describe "Late Policy Creation" do
     policy_name = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
     penalty_per_unit = 10
     max_penalty = 1
-    fill_in "late_policy[policy_name]", :with => policy_name
-    fill_in "late_policy[penalty_per_unit]", :with => penalty_per_unit
-    fill_in "late_policy[max_penalty]", :with => max_penalty
+    fill_form_fields({
+        :policy_name => policy_name,
+        :penalty_per_unit => penalty_per_unit,
+        :max_penalty => max_penalty,
+      })
 
     click_on "Create"
     expect(page).to have_current_path(new_late_policy_path)

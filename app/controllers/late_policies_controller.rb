218bf7ad819c1b6e3f89c506bb084792e0ac07b1
Renji Joseph Sabu <renjiniravath@gmail.com>
2021-10-20 23:36:31 -0400

    corrected issues in create and update

--- app/controllers/late_policies_controller.rb
+++ app/controllers/late_policies_controller.rb
@@ -52,7 +52,7 @@ class LatePoliciesController < ApplicationController
     invalid_penalty_per_unit = params[:late_policy][:max_penalty].to_i < params[:late_policy][:penalty_per_unit].to_i
     flash[:error] = "The maximum penalty cannot be less than penalty per unit." if invalid_penalty_per_unit
     same_policy_name = false
-    if same_policy_name != LatePolicy.check_policy_with_same_name(params[:late_policy][:policy_name], instructor_id)
+    if LatePolicy.check_policy_with_same_name(params[:late_policy][:policy_name], instructor_id)
       flash[:error] = "A policy with the same name already exists."
       same_policy_name = true
     end
@@ -81,8 +81,9 @@ class LatePoliciesController < ApplicationController
     same_policy_name = false
     # if name has changed then only check for this
     if params[:late_policy][:policy_name] != @penalty_policy.policy_name
-      if same_policy_name == LatePolicy.check_policy_with_same_name(params[:late_policy][:policy_name], instructor_id)
+      if LatePolicy.check_policy_with_same_name(params[:late_policy][:policy_name], instructor_id)
         flash[:error] = "The policy could not be updated because a policy with the same name already exists."
+        same_policy_name = true
       end
     end
     if !same_policy_name && !invalid_penalty_per_unit

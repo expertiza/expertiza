911f7b08638ca664595b9040e0e0e0de9df66381
vvegina <vvegina@ncsu.edu>
2021-10-22 16:54:37 -0400

    commented out unnecessary code in spec

--- spec/controllers/reputation_web_service_controller_spec.rb
+++ spec/controllers/reputation_web_service_controller_spec.rb
@@ -1,12 +1,12 @@
 describe ReputationWebServiceController do
-  let(:response) { build(:response) }
-  let(:response_map) { build(:review_response_map, reviewer_id: 2, response: [response]) }
-  let(:review_response_map) { build(:review_response_map, reviewed_object_id: 1, reviewer_id: 1, reviewee_id: 1) }
-  let(:topic) { build(:topic, id: 1, topic_name: "New Topic") }
-  let(:participant) { build(:participant, id: 1, assignment: assignment) }
-  let(:assignment) { build(:assignment, id: 1) }
-  let(:review_questionnaire) { build(:questionnaire, id: 1) }
-  let(:question) { double('Question') }
+  # let(:response) { build(:response) }
+  # let(:response_map) { build(:review_response_map, reviewer_id: 2, response: [response]) }
+  # let(:review_response_map) { build(:review_response_map, reviewed_object_id: 1, reviewer_id: 1, reviewee_id: 1) }
+  # let(:topic) { build(:topic, id: 1, topic_name: "New Topic") }
+  # let(:participant) { build(:participant, id: 1, assignment: assignment) }
+  # let(:assignment) { build(:assignment, id: 1) }
+  # let(:review_questionnaire) { build(:questionnaire, id: 1) }
+  # let(:question) { double('Question') }
 
   describe '#calculate' do
     # it 'should query database and return review responses' do

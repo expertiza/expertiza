e832e0f4a25f3e96825fd41798f7c66b480333f1
Reuben John <reubenvjohn@gmail.com>
2021-11-10 19:54:31 -0500

    Added hindi translation for assignments title list.

--- config/application.rb
+++ config/application.rb
@@ -55,7 +55,10 @@ module Expertiza
 
       # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
       # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-      # config.i18n.default_locale = :de
+
+      # Languages for the application
+      config.i18n.available_locales = [:en, :hi_IN]
+      config.i18n.default_locale = :hi_IN # english
 
       # Do not swallow errors in after_commit/after_rollback callbacks.
       config.active_record.raise_in_transactional_callbacks = true

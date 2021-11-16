6d4cc8601fc49eed8d2592240ca0c531dae81311
Rageeni Sah <rsah@vclv98-99.hpc.ncsu.edu>
2021-11-16 09:47:09 -0500

    v2

--- spec/models/tag_prompt_deployment_spec.rb
+++ spec/models/tag_prompt_deployment_spec.rb
@@ -1,14 +1,11 @@
 describe TagPromptDeployment do
 	let(:tag_dep) { TagPromptDeployment.new id: 1, tag_prompt: tp, tag_prompt_id: 1, question_type: "Criterion", answer_length_threshold: 5 }
 	let(:tp) { TagPrompt.new(prompt: "test prompt", desc: "test desc", control_type: "Checkbox") }
-
 	let(:team) {Team.new(id: 1, parent_id: 1)}
 	let(:response) {Response.new(id: 1)}
-	# let(:resp) {Response.new(id: 1, round: 1, additional_comment: "improvement scope")}
   let(:responses) {Response.new(id: 1, round: 1, additional_comment: "improvement scope")}
 	let(:assignment) {Assignment.new({assignment: 1})}
 	let(:question) {Question.new(questionnaire_id: 1, type: 'tagging')}
-
 
 	describe '#tag_prompt' do
 		it 'returns the associated tag prompt with the deployment' do
@@ -17,8 +14,6 @@ describe TagPromptDeployment do
 		end
 	end
 
-
-
   # get_number_of_taggable_answers calculates total taggable answers assigned to an user who  participated in "tag review assignment".
 	describe '#get_number_of_taggable_answers' do
     context "when team has no review" do
@@ -26,7 +21,8 @@ describe TagPromptDeployment do
 				allow(Team).to receive(:join).with(:team_users, {:parent_id => 1, :user_id => 1}).and_return(team.id)
 				allow(Response).to receive(:join).with(:response_maps, {:reviewed_object_id => 1, :reviewee_id => team.id}).and_return(response.id)
 				expect(response.id).to be(responses.id)
-			end
+      end
+
 			it "get question for each taggable question" do
 				# questions = Question.where(questionnaire_id: self.questionnaire.id, type: self.question_type)
 				allow(Question).to receive(:where).with({:questionnaire_id => 1, :type => 'tagging'}).and_return(question)
@@ -34,7 +30,4 @@ describe TagPromptDeployment do
       end
     end
   end
-
-
-
 end

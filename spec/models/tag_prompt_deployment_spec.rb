04167706bae8b387d86414d93b57468108f383ec
Rageeni Sah <rsah@vclv98-99.hpc.ncsu.edu>
2021-11-16 21:34:05 -0500

    unit testing

--- spec/models/tag_prompt_deployment_spec.rb
+++ spec/models/tag_prompt_deployment_spec.rb
@@ -23,7 +23,7 @@ describe '#tag_prompt' do
 end
 #get_number_of_taggable_answers calculates total taggable answers assigned to an user who participated in "tag review assignment".
 describe '#get_number_of_taggable_answers' do
-  context "when user_id given" do
+	context "when user_id given" do
 		it 'get team and response info of the participant' do
 			allow(Team).to receive(:join).with(:team_users, {:parent_id => tag_dep.assignment_id, :user_id => 1}).and_return(team)
 			allow(Response).to receive(:join).with(:response_maps, {:reviewed_object_id => 1, :reviewee_id => team.id}).and_return(responses)

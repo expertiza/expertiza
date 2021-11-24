239c521e0eb4febdcafa8eaab1b3cdc69498699d
Expertiza Developer <ggarrido39@gmail.com>
2021-11-24 12:59:32 -0500

    Revise necessity to be on a team to see reviews

--- app/controllers/review_bids_controller.rb
+++ app/controllers/review_bids_controller.rb
@@ -68,6 +68,7 @@ class ReviewBidsController < ApplicationController
     @num_participants = AssignmentParticipant.where(parent_id: @assignment.id).count
     @selected_topics = nil #this is used to list the topics assigned to review. (ie select == assigned i believe)
     #@bids = team_id.nil? ? [] : ReviewBid.where(participant_id:@participant,assignment_id:@assignment.id).order(:priority)
+    @bids = ReviewBid.where(participant_id:@participant,assignment_id:@assignment.id).order(:priority)
     signed_up_topics = []
     @bids.each do |bid|
       sign_up_topic = SignUpTopic.find_by(id: bid.signuptopic_id)

161c9e48dd2d03cb6522ba3810d9988eccc4a498
Aditya Khadse <akk5597@gmail.com>
2021-11-25 20:46:06 -0500

    Add basic test cases and factory methods

--- spec/controllers/answer_tags_controller_spec.rb
+++ spec/controllers/answer_tags_controller_spec.rb
@@ -3,6 +3,10 @@ describe AnswerTagsController do
   #factory objects required for "action_allowed" test cases
   let(:instructor) { build(:instructor, id: 1) }
   let(:student) { build(:student, id: 1) }
+  let(:assignment) { build(:assignment, id: 1) }
+  let(:questionnaire) { build(:questionnaire, id: 1) }
+  let(:tag_prompt_deployment) { build(:tag_prompt_deployment, id: 1, assignment: assignment, questionnaire: questionnaire) }
+  let(:answer_tag) { build(:answer_tag, id: 1, tag_prompt_deployment: tag_prompt_deployment, user: student) }
 
   #To allow the functionality only if the accessing user is having student privileges
   #params: action
@@ -41,4 +45,75 @@ describe AnswerTagsController do
     end
   end
 
+  # Test index method used to return all tag prompt deployments in JSON format
+  describe '#index' do
+    context 'all tag prompt deployments are requested' do
+      before(:each) do
+        controller.request.session[:user] = student
+      end
+
+      it 'when there are no tag prompt deployments' do
+        allow(TagPromptDeployment).to receive(:all).and_return([])
+        get :index
+        output = JSON.parse(response.body)
+        puts output
+        expect(output.length).to eql(0)
+      end
+
+      it 'when there is one tag prompt deployment' do
+        allow(TagPromptDeployment).to receive(:all).and_return([tag_prompt_deployment])
+        get :index
+        output = JSON.parse(response.body)
+        expect(output.length).to eql(1)
+      end
+
+      it 'when there is one tag prompt deployment for given user_id' do
+        allow(TagPromptDeployment).to receive(:all).and_return([tag_prompt_deployment])
+        params = {user_id: student.id}
+        get :index, params
+        output = JSON.parse(response.body)
+        expect(output.length).to eql(1)
+      end
+
+      it 'when there is one tag prompt deployment for given assignment_id' do
+        allow(TagPromptDeployment).to receive(:all).and_return([tag_prompt_deployment])
+        params = {assignment_id: assignment.id}
+        get :index, params
+        output = JSON.parse(response.body)
+        expect(output.length).to eql(1)
+      end
+
+      it 'when there is one tag prompt deployment for given questionnaire_id' do
+        allow(TagPromptDeployment).to receive(:all).and_return([tag_prompt_deployment])
+        params = {questionnaire_id: questionnaire.id}
+        get :index, params
+        output = JSON.parse(response.body)
+        expect(output.length).to eql(1)
+      end
+      
+      it 'when there is one tag prompt deployment for given random user_id' do
+        allow(TagPromptDeployment).to receive(:all).and_return([tag_prompt_deployment])
+        params = {user_id: 42}
+        get :index, params
+        output = JSON.parse(response.body)
+        expect(output.length).to eql(1)
+      end
+
+      it 'when there is one tag prompt deployment for given random assignment_id' do
+        allow(TagPromptDeployment).to receive(:all).and_return([tag_prompt_deployment])
+        params = {assignment_id: 42}
+        get :index, params
+        output = JSON.parse(response.body)
+        expect(output.length).to eql(1)
+      end
+
+      it 'when there is one tag prompt deployment for given random questionnaire_id' do
+        allow(TagPromptDeployment).to receive(:all).and_return([tag_prompt_deployment])
+        params = {questionnaire_id: 42}
+        get :index, params
+        output = JSON.parse(response.body)
+        expect(output.length).to eql(1)
+      end
+    end
+  end
 end

e3bd6a7f41ec52e335a4513c319a8ea957d24ff1
Iamnotsquishy <quinndibble@gmail.com>
2021-11-27 18:20:45 -0500

    comment out unused lines in spec/models/teammate_review_response_map_spec.rb

--- spec/models/teammate_review_response_map_spec.rb
+++ spec/models/teammate_review_response_map_spec.rb
@@ -1,30 +1,30 @@
 describe TeammateReviewResponseMap do
   let(:team) { build(:assignment_team, id: 1, name: 'team no name', assignment: assignment, users: [student], parent_id: 1) }
-  let(:team2) { build(:assignment_team, id: 3, name: 'no team') }
-  let(:team1) { build(:assignment_team, id: 2, name: 'team has name', assignment: assignment, users: [student]) }
-  let(:team3) { build(:assignment_team, id: 4, name: 'team has name1', assignment: assignment, users: [student1]) }
+#  let(:team2) { build(:assignment_team, id: 3, name: 'no team') }
+#  let(:team1) { build(:assignment_team, id: 2, name: 'team has name', assignment: assignment, users: [student]) }
+#  let(:team3) { build(:assignment_team, id: 4, name: 'team has name1', assignment: assignment, users: [student1]) }
   let(:teammate_review_response_map1) { build(:teammate_review_response_map, id: 1, assignment: assignment1, reviewer: participant, reviewee: participant1) }
   let(:review_response_map) { build(:review_response_map, id: 1, assignment: assignment, reviewer: participant, reviewee: team) }
 
-  let(:feedback) { FeedbackResponseMap.new(id: 1, reviewed_object_id: 1, reviewer_id: 1, reviewee_id: 1) }
+#  let(:feedback) { FeedbackResponseMap.new(id: 1, reviewed_object_id: 1, reviewer_id: 1, reviewee_id: 1) }
   let(:participant) { build(:participant, id: 1, parent_id: 1, user: student) }
   let(:participant1) { build(:participant, id: 2, parent_id: 2, user: student1) }
   let(:assignment) { build(:assignment, id: 1, name: 'Test Assgt', rounds_of_reviews: 2) }
   let(:assignment1) { build(:assignment, id: 2, name: 'Test Assgt', rounds_of_reviews: 1) }
   let(:response) { build(:response, id: 1, map_id: 1, round: 1, response_map: teammate_review_response_map1,  is_submitted: true) }
-  let(:response1) { build(:response, id: 2, map_id: 1, round: 2, response_map: teammate_review_response_map1) }
-  let(:response2) { build(:response, id: 3, map_id: 1, round: nil, response_map: teammate_review_response_map1, is_submitted: true) }
-  let(:response3) { build(:response) }
-  let(:metareview_response_map) { build(:meta_review_response_map, reviewed_object_id: 1) }
+#  let(:response1) { build(:response, id: 2, map_id: 1, round: 2, response_map: teammate_review_response_map1) }
+#  let(:response2) { build(:response, id: 3, map_id: 1, round: nil, response_map: teammate_review_response_map1, is_submitted: true) }
+#  let(:response3) { build(:response) }
+#  let(:metareview_response_map) { build(:meta_review_response_map, reviewed_object_id: 1) }
   let(:student) { build(:student, id: 1, name: 'name', fullname: 'no one', email: 'expertiza@mailinator.com') }
   let(:student1) { build(:student, id: 2, name: "name1", fullname: 'no one', email: 'expertiza@mailinator.com') }
-  let(:assignment_teammate_questionnaire1) { build(:assignment_teammate_questionnaire, id: 1, assignment: assignment1, questionnaire: teammate_questionnaire1) }
-  let(:assignment_teammate_questionnaire2) { build(:assignment_teammate_questionnaire, id: 2, assignment_id: 2, questionnaire_id: 2) }
-  let(:teammate_questionnaire1) { build(:teammate_questionnaire, id: 1, type: 'TeammateReviewQuestionnaire') }
-  let(:teammate_questionnaire2) { build(:teammate_questionnaire, id: 2, type: 'TeammateReviewQuestionnaire') }
-  let(:next_due_date) { build(:assignment_due_date, round: 1) }
-  let(:question) { double('Question') }
-  let(:review_questionnaire) { build(:questionnaire, id: 3) }
+#  let(:assignment_teammate_questionnaire1) { build(:assignment_teammate_questionnaire, id: 1, assignment: assignment1, questionnaire: teammate_questionnaire1) }
+#  let(:assignment_teammate_questionnaire2) { build(:assignment_teammate_questionnaire, id: 2, assignment_id: 2, questionnaire_id: 2) }
+#  let(:teammate_questionnaire1) { build(:teammate_questionnaire, id: 1, type: 'TeammateReviewQuestionnaire') }
+#  let(:teammate_questionnaire2) { build(:teammate_questionnaire, id: 2, type: 'TeammateReviewQuestionnaire') }
+#  let(:next_due_date) { build(:assignment_due_date, round: 1) }
+#  let(:question) { double('Question') }
+#  let(:review_questionnaire) { build(:questionnaire, id: 3) }
   let(:response3) { build(:response) }
   let(:response_map) { build(:review_response_map, reviewer_id: 2, response: [response3]) }
   before(:each) do

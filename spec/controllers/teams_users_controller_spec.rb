447140f6b4df043d86f9b44dbdb68fc2643d975e
Subodh Thota <sthota@vm17-225.vcl.ncsu.edu>
2021-11-27 20:47:29 -0500

    undo

--- spec/controllers/teams_users_controller_spec.rb
+++ spec/controllers/teams_users_controller_spec.rb
@@ -5,41 +5,41 @@ describe TeamsUsersController do
   end
   let(:assignment_form) { double('AssignmentForm', assignment: assignment) }
   let(:student) { build(:student) }
-  let(:duty) { build(:duty, id: 1, duty_name: "Role", max_members_for_duty: 2, assignment_id: 1) }
-  let(:teams_user1) {TeamsUser.new id:1, duty_id:1}
- 
+  let(:instructor) { build(:instructor, id: 6) }
+  let(:duty1) { build(:duty, id: 1, duty_name: "Role", max_members_for_duty: 2, assignment_id: 1) }
+  let(:duty2) { build(:duty, id: 2, duty_name: "Role", max_members_for_duty: 2, assignment_id: 1) }
+  let(:team_user) { build(:team_user, id: 1, team_id: 1, user_id: 1, duty_id: 1) }
+
   before(:each) do
     allow(Assignment).to receive(:find).with('1').and_return(assignment)
     stub_current_user(student, student.role.name, student.role)
   end
- 
-  describe '#action_allowed?' do
-    context 'when params action is update duties' do
-      before(:each) do
-        controller.params = {id: '1', action: 'update_duties'}
+
+describe '#action_allowed?' do
+  context 'when params action is update duties' do
+    before(:each) do
+      controller.params = {id: '1', action: 'update_duties'}
+    end
+
+    context 'when the role current user is student' do
+      it 'allows certain action' do
+        stub_current_user(student, student.role.name, student.role)
+        expect(controller.send(:action_allowed?)).to be true
       end
- 
-        context 'when the role current user is student' do
-          it 'allows certain action' do
-            stub_current_user(student, student.role.name, student.role)
-            expect(controller.send(:action_allowed?)).to be true
-          end
-        end
     end
   end
- 
-  describe '#update_duties' do
-      it 'updates the duties for the participant' do
-        allow(TeamsUser).to receive(:find).with('1').and_return(teams_user1)
-        allow(teams_user1).to receive(:update_attribute).with(any_args).and_return('OK')
-        params = {
-            teams_user_id: '1',
-            teams_user: {duty_id: '1'},
-            participant_id: '1'
-        }
-        session = {user: stub_current_user(student, student.role.name, student.role)}
-        get :update_duties, params, session
-        expect(response).to redirect_to('/student_teams/view?student_id=1')
-      end
   end
+
+
+describe '#update_duties' do
+  context 'testing the function to update duty_id' do
+    it 'when duties are updated correctly' do
+      allow(TeamsUser).to receive(:find).with('1').and_return(team_user)
+      params = {teams_user_id: 1, teams_user: { duty_id: 2 }, participant_id: 1 }
+      session = {user: instructor}
+      post :update_duties, params, session
+      expect(response).to redirect_to
+    end
+  end
+end
 end

dc3f80450ee4071baef56c1fa8ae139325af6435
Yi Li <yli273@vm17-120.vcl.ncsu.edu>
2021-11-28 18:49:00 -0500

    heatgrid

--- app/helpers/grades_helper.rb
+++ app/helpers/grades_helper.rb
@@ -158,6 +158,7 @@ module GradesHelper
   def generate_heatgrid(participant, assignment, team, team_id, questionnaires, vmlist)
     # loop through each questionnaire, and populate the view model for all data necessary
     # to render the html tables.
+    @round = nil
     counter_for_revisions = -1
     counter_for_same_rubric = 0
     questionnaires.each do |questionnaire|
@@ -191,6 +192,25 @@ module GradesHelper
     @type = type
     questionnaires = @assignment.questionnaires
     @vmlist = []
+
+    # # loop through each questionnaire, and populate the view model for all data necessary
+    # # to render the html tables.
+    # questionnaires.each do |questionnaire|
+    #   @round = if @assignment.vary_by_round && questionnaire.type == "ReviewQuestionnaire"
+    #              AssignmentQuestionnaire.find_by(assignment_id: @assignment.id, questionnaire_id: questionnaire.id).used_in_round
+    #            else
+    #              nil
+    #            end
+    #   next unless questionnaire.type == type
+    #   vm = VmQuestionResponse.new(questionnaire, @assignment, @round)
+    #   questions = questionnaire.questions
+    #   vm.add_questions(questions)
+    #   vm.add_team_members(@team)
+    #   vm.add_reviews(@participant, @team, @assignment.vary_by_round)
+    #   vm.number_of_comments_greater_than_10_words
+    #   @vmlist << vm
+    # end
+    # # @current_role_name = current_role_name/
 
     generate_heatgrid(@participant, @assignment, @team, @team_id, questionnaires, @vmlist)
 

66c71bd09b737a799609f407b99c1c344a353574
Andrew Shipman <ajshipma@vclvm176-230.vcl.ncsu.edu>
2021-11-27 19:22:35 -0500

    added update_duties test

--- spec/controllers/teams_users_controller_spec.rb
+++ spec/controllers/teams_users_controller_spec.rb
@@ -5,6 +5,10 @@ describe TeamsUsersController do
   end
   let(:assignment_form) { double('AssignmentForm', assignment: assignment) }
   let(:student) { build(:student) }
+  let(:instructor) { build(:instructor, id: 6) }
+  let(:duty1) { build(:duty, id: 1, duty_name: "Role", max_members_for_duty: 2, assignment_id: 1) }
+  let(:duty2) { build(:duty, id: 2, duty_name: "Role", max_members_for_duty: 2, assignment_id: 1) }
+  let(:team_user) { build(:team_user, id: 1, team_id: 1, user_id: 1, duty_id: 1) }
 
   before(:each) do
     allow(Assignment).to receive(:find).with('1').and_return(assignment)
@@ -25,4 +29,17 @@ describe '#action_allowed?' do
     end
   end
   end
+
+
+describe '#update_duties' do
+  context 'testing the function to update duty_id' do
+    it 'when duties are updated correctly' do
+      allow(TeamsUser).to receive(:find).with('1').and_return(team_user)
+      params = {teams_user_id: 1, teams_user: { duty_id: 2 }, participant_id: 1 }
+      session = {user: instructor}
+      post :update_duties, params, session
+      expect(response).to redirect_to
+    end
+  end
+end
 end

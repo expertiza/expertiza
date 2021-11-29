ea833373c68ce6e19befa0c6eb77e3870e017b14
yliang123 <yliang24@ncsu.edu>
2021-11-28 22:32:57 -0500

    update use_github_metric_spec.rb

--- spec/features/use_github_metric_spec.rb
+++ spec/features/use_github_metric_spec.rb
@@ -4,15 +4,14 @@ include AssignmentCreationHelper
 describe "assignment creation due dates", js: true do
 
   before(:each) do
-    create_deadline_types()
-    @assignment = create(:assignment, name: 'public assignment for test')
-    login_as("instructor6")
+     login_as("instructor6")
   end
     it "should edit assignment available to students" do
-         visit "/assignments/834/edit"
+         visit "/assignments/1/edit"
     	 find(:css, "#use_github[value='use_github']").set(true)
-         visit "/assignments/list_submissions?id=834"
+         visit "/assignments/list_submissions?id=1"
          expect(page).to have_content("Github data")
+         #expect(page).to have_no_content('Inherit Teams From Course')
     end
 
   # able to set deadlines for a single round of reviews

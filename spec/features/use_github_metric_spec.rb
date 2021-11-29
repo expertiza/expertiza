ab017de470c266424d7a7fb8f3fe83241e909a7c
yliang123 <yliang24@ncsu.edu>
2021-11-28 22:36:07 -0500

    update

--- spec/features/use_github_metric_spec.rb
+++ spec/features/use_github_metric_spec.rb
@@ -4,21 +4,14 @@ include AssignmentCreationHelper
 describe "assignment creation due dates", js: true do
 
   before(:each) do
+    create_deadline_types()
+    @assignment = create(:assignment, name: 'public assignment for test')
     login_as("instructor6")
-        let(:review_response) { build(:response, id: 1, map_id: 1) }
-        let(:question) { build(:question) }
-        let(:participant) { build(:participant, id: 1, assignment: assignment, user_id: 1) }
-        let(:assignment) { build(:assignment, id: 1, max_team_size: 2, questionnaires: [review_questionnaire], is_penalty_calculated: true)}
-        let(:review_questionnaire) { build(:questionnaire, id: 1, questions: [question]) }
-        let(:assignment) { build(:assignment, id: 1, name: 'no assignment', participants: [participant], teams: [team]) }
-        let(:team) { build(:assignment_team, id: 1, name: 'no team') }
-        let(:metric) { build(:metric, id: 1, metric_source_id: 1, participant_id: participant.id, github_id:"student@ncsu.edu") }
-        let(:assignment_team) {build(:assignment_team, id: 1, name: 'team1', submitted_hyperlinks: ["https://www.github.com/anonymous/expertiza", "https://github.com/expertiza/expertiza/pull/1234"])}
   end
     it "should edit assignment available to students" do
-         visit "/assignments/1/edit"
+         visit "/assignments/834/edit"
     	 find(:css, "#use_github[value='use_github']").set(true)
-         visit "/assignments/list_submissions?id=1"
+         visit "/assignments/list_submissions?id=834"
          expect(page).to have_content("Github data")
     end
 

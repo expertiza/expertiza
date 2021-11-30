3a7db6e3e0df37aa637883f617c7a5e2cf5d516e
neelkanthtripathi <ntripat2@ncsu.edu>
2021-11-29 20:05:30 -0500

    Fixed the list_review_submissions method problem

--- app/helpers/review_mapping_helper.rb
+++ app/helpers/review_mapping_helper.rb
@@ -167,17 +167,7 @@ include ReviewChartHelper
     @reviewers.sort! {|r1, r2| r2.overall_avg_vol <=> r1.overall_avg_vol }
   end
 
-  def list_review_submissions(participant_id, reviewee_team_id, response_map_id)
-    participant = Participant.find(participant_id)
-    team = AssignmentTeam.find(reviewee_team_id)
-    html = ''
-    unless team.nil? || participant.nil?
-      review_submissions_path = team.path + "_review" + "/" + response_map_id.to_s
-      files = team.submitted_files(review_submissions_path)
-      html += display_review_files_directory_tree(participant, files) if files.present?
-    end
-    html.html_safe
-  end
+
 
   # Zhewei - 2016-10-20
   # This is for Dr.Kidd's assignment (806)

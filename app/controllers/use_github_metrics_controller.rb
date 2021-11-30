c487ffb295bbe73d41a1510cd6340b15d5a73847
Robin Piao <lpiao@ncsu.edu>
2021-11-29 19:45:01 -0500

    refactor: rename variables and add comments

--- app/controllers/use_github_metrics_controller.rb
+++ app/controllers/use_github_metrics_controller.rb
@@ -3,44 +3,50 @@ class UseGithubMetricsController < ApplicationController
   skip_before_action :authorize
   helper_method :exist
 
+  #check if assignment_id exists in the table use_github_metrics
   def self.exist(assignment_id)
-    @u = UseGithubMetric.find_by(assignment_id: assignment_id)
-    !@u.nil?
+    use_github = UseGithubMetric.find_by(assignment_id: assignment_id)
+    !use_github.nil?
   end
 
+  #if assignment_id does not exist in the table use_github_metrics, save it
   def self.save(assignment_id)
     unless exist(assignment_id)
-      @u = UseGithubMetric.new(assignment_id)
-      @u.assignment_id = assignment_id
-      @u.save
+      use_github = UseGithubMetric.new(assignment_id)
+      use_github.assignment_id = assignment_id
+      use_github.save
     end
   end
 
+  #if assignment_id exists in the table use_github_metrics, delete it
   def self.delete(assignment_id)
     if exist(assignment_id)
-      @u = UseGithubMetric.find_by(assignment_id: assignment_id)
-      @u.destroy
+      use_github = UseGithubMetric.find_by(assignment_id: assignment_id)
+      use_github.destroy
     end
   end
 
+  #check if assignment_id exists in the table use_github_metrics
   def exist
-    @assignment_id = params[:assignment_id]
-    UseGithubMetricsController.exist(@assignment_id)
+    assignment_id = params[:assignment_id]
+    UseGithubMetricsController.exist(assignment_id)
   end
 
+  #if assignment_id does not exist in the table use_github_metrics, save it
   def save
-    @assignment_id = params[:assignment_id]
-    UseGithubMetricsController.save(@assignment_id)
+    assignment_id = params[:assignment_id]
+    UseGithubMetricsController.save(assignment_id)
     respond_to do |format|
-      format.json { render json: @assignment_id }
+      format.json { render json: assignment_id }
     end
   end
 
+  #if assignment_id exists in the table use_github_metrics, delete it
   def delete
-    @assignment_id = params[:assignment_id]
-    UseGithubMetricsController.delete(@assignment_id)
+    assignment_id = params[:assignment_id]
+    UseGithubMetricsController.delete(assignment_id)
     respond_to do |format|
-      format.json { render json: @assignment_id }
+      format.json { render json: assignment_id }
     end
   end
 

0e7d051cc2024dc18aa0f6ab4e6b8b4b1c640bd6
divyang02 <divyangdoshi02@gmail.com>
2022-01-14 18:23:20 -0500

    added conmment

--- app/models/assignment_form.rb
+++ app/models/assignment_form.rb
@@ -137,6 +137,7 @@ class AssignmentForm
   def update_tag_prompt_deployments(attributes)
     unless attributes.nil?
       attributes.each do |key, value|
+        # We need to use destroy_all to delete all the dependents also. 
         TagPromptDeployment.where(id: value['deleted']).destroy_all if value.key?('deleted')
         # assume if tag_prompt is there, then id, question_type, answer_length_threshold must also be there since the inputs are coupled
         next unless value.key?('tag_prompt')

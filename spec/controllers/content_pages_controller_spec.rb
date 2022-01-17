89547ac2c0f1bcf27fe7e10bdf1d03f877e93289
divyang02 <divyangdoshi02@gmail.com>
2022-01-17 18:27:59 -0500

    added edit, action_allowed? test cases for content_page_controller

--- spec/controllers/content_pages_controller_spec.rb
+++ spec/controllers/content_pages_controller_spec.rb
@@ -1,9 +1,13 @@
 describe ContentPagesController do
     let(:instructor) { build(:instructor, id: 6) }
     let(:content_page_home) {build(:content_page, id: 1)}
+    let(:superadmin) {create(:superadmin)}
     
     before(:each) do
         stub_current_user(instructor, instructor.role.name, instructor.role)
+        @markup_style = MarkupStyle.new(name: 'Markdown')
+        @permission = Permission.new(name: 'administrator')
+
     end
 
     describe '#view' do
@@ -32,6 +36,52 @@ describe ContentPagesController do
                 get :view, params
                 expect(controller.instance_variable_get(:@content_page).content).to eq('(no such page)')
                 expect(response).to render_template(:view)
+            end
+        end
+    end
+
+    describe '#edit' do
+        it "renders grades#edit page" do
+            stub_current_user(superadmin, superadmin.role.name, superadmin.role)
+            allow(ContentPage).to receive(:find).with("1").and_return(content_page_home)
+            allow(MarkupStyle).to receive(:order).with('name').and_return([@markup_style])
+            allow(Permission).to receive(:order).with('name').and_return([@permission])
+            allow(MenuItem).to receive_message_chain(:order, :where).and_return(MenuItem.new(content_page_id: 1))
+            controller.instance_variable_set(:@settings, SystemSettings.new(site_default_page_id: 1))
+            allow(controller.instance_variable_get(:@settings)).to receive(:system_pages).with(1).and_return(["Site default page"])
+            params = {id: 1}
+            get :edit, params
+            expect(response).to render_template(:edit)
+        end
+    end
+
+    describe '#action_allowed' do
+        context 'when user does not have superadmin privileges' do
+            it 'returns false' do
+                params = {action: 'edit'}
+                controller.params = params
+                expect(controller.action_allowed?).to eq(false)
+            end
+
+            it 'return true for view' do
+                params = {action: 'view'}
+                controller.params = params
+                expect(controller.action_allowed?).to eq(true)
+            end
+
+            it 'return true for view_default' do
+                params = {action: 'view_default'}
+                controller.params = params
+                expect(controller.action_allowed?).to eq(true)
+            end
+        end
+
+        context 'when user does have superadmin privileges' do
+            it 'returns true for #edit' do 
+                stub_current_user(superadmin, superadmin.role.name, superadmin.role)
+                params = {action: 'edit'}
+                controller.params = params
+                expect(controller.action_allowed?).to eq(true)
             end
         end
     end

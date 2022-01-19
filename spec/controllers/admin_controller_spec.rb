f8ade7e5e362129e17d14265800943659c843f94
ylo <ylo@ncsu.edu>
2022-01-19 13:24:07 -0500

    E2134. Write unit tests for admin_controller.rb (#2098)
    * rebase branch to beta
    
    * add admin_controller_spec.rb file
    
    * update first template for all controller function
    
    * enable authentication test, disable not working parts
    
    * add test for list_super_administrators
    
    * add test case to admin_controller for show_super_administrator
    
    * add tests for admin_controller with #show functions
    
    * Add test cases to admin_controller with all #list cases
    
    * add comment
    
    Co-authored-by: gggho0806 <gggho0806@gmail.com>
    Co-authored-by: John Bumgardner <38589044+johnbumgardner@users.noreply.github.com>

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -1,74 +1,195 @@
 describe AdminController do
-    let(:review_response) { build(:response) }
-    let(:assignment) { build(:assignment, id: 1, max_team_size: 2, questionnaires: [review_questionnaire], is_penalty_calculated: true)}
-    let(:assignment_questionnaire) { build(:assignment_questionnaire, used_in_round: 1, assignment: assignment) }
-    let(:participant) { build(:participant, id: 1, assignment: assignment, user_id: 1) }
-    let(:participant2) { build(:participant, id: 2, assignment: assignment, user_id: 1) }
-    let(:review_questionnaire) { build(:questionnaire, id: 1, questions: [question]) }
-    let(:admin) { build(:admin) }
-    let(:super_admin_1) { build(:superadmin, id:1)}
-    let(:super_admin_2) { build(:superadmin, id:2)}
-    let(:instructor) { build(:instructor, id: 6) }
-    let(:question) { build(:question) }
-    let(:team) { build(:assignment_team, id: 1, assignment: assignment, users: [instructor]) }
-    let(:student) { build(:student) }
-    let(:review_response_map) { build(:review_response_map, id: 1) }
-    let(:assignment_due_date) { build(:assignment_due_date) }
-    let(:ta) { build(:teaching_assistant, id: 8) }
-  
-    before(:each) do
-      allow(AssignmentParticipant).to receive(:find).with('1').and_return(participant)
-      allow(participant).to receive(:team).and_return(team)
-      stub_current_user(instructor, instructor.role.name, instructor.role)
-      allow(Assignment).to receive(:find).with('1').and_return(assignment)
-      allow(Assignment).to receive(:find).with(1).and_return(assignment)
-    end
+  let(:admin1) { build(:admin, id: 3, role_id: 4, parent_id: 1, name: 'Administrator1') }
+  let(:admin2) { build(:admin, id: 4, role_id: 4, parent_id: 1, name: 'Administrator2') }
+  let(:super_admin) { build(:superadmin, id: 1, role_id: 5) }
+  let(:instructor1) { build(:instructor, id: 10, role_id: 3, parent_id: 3, name: 'Instructor1') }
+  let(:instructor2) { build(:instructor, id: 11, role_id: 3, parent_id: 4, name: 'Instructor2') }
+  let(:student1) { build(:student, id: 21, role_id: 1) }
 
-    describe '#action_allowed?' do
-        context 'when the student doesnt have admin privileges' do
-            it 'returns false' do 
-                params = {action: 'list_instructors'}
-                session[:user].role.name = 'Student'
-                expect(controller.action_allowed?).to eq(false)
-            end
+  # create fake lists
+  let(:admin_list) { [admin1, admin2] }
+  let(:instructor_list) { [instructor1, instructor2] }
+  let(:instructor_list_pid3) { [instructor1] }
+  let(:instructor_list_pid4) { [instructor2] }
 
-            it 'returns false' do
-                params = {action: 'remove_instructor'}
-                session[:user].role.name = 'Student'
-                expect(controller.action_allowed?).to eq(false)
-            end
+  # define default behaviors for each method call
+  before(:each) do
+    allow(User).to receive(:find).with('3').and_return(admin1)
+    allow(User).to receive(:find).with('1').and_return(super_admin)
+    allow(User).to receive(:find).with('10').and_return(instructor1)
+    allow(User).to receive(:where).with(["role_id = ?", super_admin.role_id]).and_return([ super_admin ])
+
+    allow(User).to receive(:where).with(role_id: admin1.role).and_return(admin_list)
+    allow(admin_list).to receive(:order).with(:name).and_return(admin_list)
+    allow(admin_list).to receive(:where).with("parent_id = ?", super_admin.id).and_return(admin_list)
+    allow(admin_list).to receive(:paginate).with(page: '1', per_page: 50).and_return(admin_list)
+
+    allow(User).to receive(:where).with(role_id: instructor1.role).and_return(instructor_list)
+    allow(instructor_list).to receive(:order).with(:name).and_return(instructor_list)
+    allow(instructor_list).to receive(:where).with("parent_id = ?", admin1.id).and_return(instructor_list_pid3)
+    allow(instructor_list_pid3).to receive(:paginate).with(page: '1', per_page: 50).and_return(instructor_list_pid3)
+  end
+
+  describe '#action_allowed?' do
+    context 'when params action is list all instructors' do
+      before(:each) do
+        controller.params = {action: 'list_instructors'}
+      end
+
+      context 'when the role of current user is Admin' do
+        it 'allows certain action' do
+          stub_current_user(admin1, admin1.role.name, admin1.role)
+          (controller.send(:action_allowed?)).should be true
         end
-        context 'when the student has super-admin privileges' do
-            it 'returns true' do
-                params = {action: 'fake_case'}
-                session[:user].role.name = 'Super-Administrator'
-                expect(controller.action_allowed?).to eq(true) 
-            end
+      end
+
+      context 'when the role of current user is Super-Admin' do
+        it 'allows certain action' do
+          stub_current_user(super_admin, super_admin.role.name, super_admin.role)
+          (controller.send(:action_allowed?)).should be true
         end
+      end
+
+      context 'when the role of current user is Instructor' do
+        it 'refuses certain action' do
+          stub_current_user(instructor1, instructor1.role.name, instructor1.role)
+          (controller.send(:action_allowed?)).should be false
+        end
+      end
+
+      context 'when the role of current user is Student' do
+        it 'refuses certain action' do
+          stub_current_user(student1, student1.role.name, student1.role)
+          (controller.send(:action_allowed?)).should be false
+        end
+      end
     end
 
-    describe '#list_super_administrators' do
+    context 'when params action is remove an instructor' do
+      before(:each) do
+        controller.params = {action: 'remove_instructor'}
+      end
 
+      context 'when the role of current user is Admin' do
+        it 'allows certain action' do
+          stub_current_user(admin1, admin1.role.name, admin1.role)
+          (controller.send(:action_allowed?)).should be true
+        end
+      end
+
+      context 'when the role of current user is Super-Admin' do
+        it 'allows certain action' do
+          stub_current_user(super_admin, super_admin.role.name, super_admin.role)
+          (controller.send(:action_allowed?)).should be true
+        end
+      end
+
+      context 'when the role of current user is Instructor' do
+        it 'refuses certain action' do
+          stub_current_user(instructor1, instructor1.role.name, instructor1.role)
+          (controller.send(:action_allowed?)).should be false
+        end
+      end
+
+      context 'when the role of current user is Student' do
+        it 'refuses certain action' do
+          stub_current_user(student1, student1.role.name, student1.role)
+          (controller.send(:action_allowed?)).should be false
+        end
+      end
     end
 
-    describe '#show_super_administrator' do
+    context 'when params action is other than list and remove instructors' do
+      before(:each) do
+        controller.params = {:action => 'remove_administrator'}
+      end
 
+      context 'when the role of current user is Admin' do
+        it 'refuses certain action' do
+          stub_current_user(admin1, admin1.role.name, admin1.role)
+          (controller.send(:action_allowed?)).should be false
+        end
+      end
+
+      context 'when the role of current user is Super-Admin' do
+        it 'allows certain action' do
+          stub_current_user(super_admin, super_admin.role.name, super_admin.role)
+          (controller.send(:action_allowed?)).should be true
+        end
+      end
+
+      context 'when the role of current user is Instructor' do
+        it 'refuses certain action' do
+          stub_current_user(instructor1, instructor1.role.name, instructor1.role)
+          (controller.send(:action_allowed?)).should be false
+        end
+      end
+
+      context 'when the role of current user is Student' do
+        it 'refuses certain action' do
+          stub_current_user(student1, student1.role.name, student1.role)
+          (controller.send(:action_allowed?)).should be false
+        end
+      end
     end
+  end
 
-    describe '#list_administrators' do
-        
+   context '#list_super_administrators' do
+     it 'lists all the Super-Administrators' do
+       stub_current_user(super_admin, super_admin.role.name, super_admin.role)
+       get :list_super_administrators
+       expect(assigns(:users)).to eq([ super_admin ])
+     end
+   end
+
+  context '#show_super_administrator' do
+    it 'find selected Super-Administrator and render #show' do
+      stub_current_user(super_admin, super_admin.role.name, super_admin.role)
+      params = {id: '1'}
+      get :show_super_administrator, params
+      expect(assigns(:user)).to eq(super_admin)
+      expect(assigns(:role)).to eq(super_admin.role)
+      expect(response).to render_template(:show_super_administrator)
     end
+  end
 
-    describe '#show_administrator' do
-        
+  context '#list_administrators' do
+    it 'lists all the admins' do
+      stub_current_user(super_admin, super_admin.role.name, super_admin.role)
+      params = {page: '1'}
+      get :list_administrators, params
+      expect(assigns(:users)).to eq(admin_list)
     end
+  end
 
-    describe '#list_instructors' do
-        
+  context '#show_administrator' do
+    it 'find selected admin and render #show' do
+      stub_current_user(super_admin, super_admin.role.name, super_admin.role)
+      params = {id: '3'}
+      get :show_administrator, params
+      expect(assigns(:user)).to eq(admin1)
+      expect(assigns(:role)).to eq(admin1.role)
+      expect(response).to render_template(:show_administrator)
     end
+  end
 
-    describe '#show_instructor' do
-        
+  context '#list_instructors' do
+    it 'lists all the instructors' do
+      stub_current_user(admin1, admin1.role.name, admin1.role)
+      params = {page: '1'}
+      get :list_instructors, params
+      expect(assigns(:users)).to eq(instructor_list_pid3)
     end
+  end
 
+  context '#show_instructor' do
+    it 'find selected instructor and render #show' do
+      stub_current_user(super_admin, super_admin.role.name, super_admin.role)
+      params = {id: '10'}
+      get :show_instructor, params
+      expect(assigns(:user)).to eq(instructor1)
+      expect(assigns(:role)).to eq(instructor1.role)
+      expect(response).to render_template(:show_instructor)
+    end
+  end
 end

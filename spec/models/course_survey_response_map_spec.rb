8e03388cbd4601f37cbb7a38ac3652bd1f176b01
Nicholas Himes <41524135+nnhimes@users.noreply.github.com>
2022-02-03 21:32:27 -0500

    Update to new email address

--- spec/models/course_survey_response_map_spec.rb
+++ spec/models/course_survey_response_map_spec.rb
@@ -3,7 +3,7 @@ describe CourseSurveyResponseMap do
 	let(:course_survey_questionnaire) { CourseSurveyQuestionnaire.new id: 985, name: "course_survey", private: 0, min_question_score: 0, max_question_score: 10, instructor_id: 1234 }
 	let(:previous_day) { (Time.now.getlocal - 1 * 86_400).strftime("%Y-%m-%d %H:%M:%S") }
   let(:next_day) { (Time.now.getlocal + 1 * 86_400).strftime("%Y-%m-%d %H:%M:%S") }
-  let(:user) { build(:student, email: "expertiza.development@gmail.com", name: "Jane", fullname: "Doe, Jane", id: 1) }
+  let(:user) { build(:student, email: "expertiza.debugging@gmail.com", name: "Jane", fullname: "Doe, Jane", id: 1) }
   let(:course) { build(:course, id: 1, name: 'ECE517')}
 	before(:each) do
     survey_deployment = CourseSurveyDeployment.new questionnaire_id: 985, start_date: previous_day, end_date: next_day, parent_id: "12345678", type: "CourseSurveyDeployment"
@@ -60,10 +60,10 @@ describe CourseSurveyResponseMap do
           first_name: "no one", 
           partial_name: "new_submission"
         }, 
-        to: 'expertiza.development@gmail.com'}
+        to: 'expertiza.debugging@gmail.com'}
       email = @course_survey_response_map.email(defn, participant, course)
-      expect(email.from[0]).to eq("expertiza.development@gmail.com")
-      expect(email.to[0]).to eq('expertiza.development@gmail.com')
+      expect(email.from[0]).to eq("expertiza.debugging@gmail.com")
+      expect(email.to[0]).to eq('expertiza.debugging@gmail.com')
     end
   end
 end

8970be8c161db7c80e85df411ad802edf1fcd1cf
Nicholas Himes <41524135+nnhimes@users.noreply.github.com>
2022-02-16 14:29:13 -0500

    Remove whitespace

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -123,7 +123,7 @@ class ApplicationController < ActionController::Base
   end
 
   alias current_user_role? current_user_role
-  
+
   def logged_in?
     # Recommendation: rename to ever_logged_in? because that's how this actually works
     current_user

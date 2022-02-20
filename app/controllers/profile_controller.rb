d9cacbe899b6826a7e43b6366a2c9fc1c5166e9d
divyang02 <divyangdoshi02@gmail.com>
2022-02-19 19:05:41 -0500

    strong params for content_pages, markup_styles, profile_controller

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -11,14 +11,13 @@ class ProfileController < ApplicationController
   end
 
   def update
-    params.permit!
     @user = session[:user]
 
     unless params[:assignment_questionnaire].nil? || params[:assignment_questionnaire][:notification_limit].blank?
       aq = AssignmentQuestionnaire.where(['user_id = ? and assignment_id is null and questionnaire_id is null', @user.id]).first
       aq.update_attribute('notification_limit', params[:assignment_questionnaire][:notification_limit])
     end
-    if @user.update_attributes(params[:user])
+    if @user.update_attributes(user_params)
       ExpertizaLogger.info LoggerMessage.new(controller_name, @user.name, 'Your profile was successfully updated.', request)
       @user.preference_home_flag = params[:no_show_action] != 'not_show_actions'
       @user.save!
@@ -34,27 +33,16 @@ class ProfileController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name,
-                                 :crypted_password,
-                                 :role_id,
-                                 :password_salt,
-                                 :fullname,
+    params.require(:user).permit(:fullname,
+                                 :password,
+                                 :password_confirmation,
                                  :email,
-                                 :parent_id,
-                                 :private_by_default,
-                                 :mru_directory_path,
+                                 :institution_id,
+                                 :email_on_review_of_review,
                                  :email_on_review,
                                  :email_on_submission,
-                                 :email_on_review_of_review,
-                                 :is_new_user,
-                                 :master_permission_granted,
                                  :handle,
-                                 :digital_certificate,
-                                 :persistence_token,
                                  :timezonepref,
-                                 :public_key,
-                                 :copy_of_emails,
-                                 :institution_id,
-                                 :preference_home_flag)
+                                 :locale)
   end
 end

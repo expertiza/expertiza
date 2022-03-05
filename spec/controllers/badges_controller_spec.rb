e148d4a3d3293056eb6d3dc9b29a8cbbcb430cfe
Atharva Patil <atharvapatil@Atharvas-MacBook-Pro.local>
2022-03-05 16:29:14 -0500

    adding badges admin test

--- spec/controllers/badges_controller_spec.rb
+++ spec/controllers/badges_controller_spec.rb
@@ -1,5 +1,6 @@
 describe BadgesController do
   let(:super_admin) { build(:superadmin, id: 1, role_id: 5) }
+  let(:admin) { build(:admin, id: 3) }
   let(:instructor1) { build(:instructor, id: 10, role_id: 3, parent_id: 3, name: 'Instructor1') }
   let(:student1) { build(:student, id: 21, role_id: 1) }
   let(:ta) { build(:teaching_assistant, id: 6) }
@@ -28,5 +29,14 @@ describe BadgesController do
         expect(controller.send(:action_allowed?)).to be_truthy
       end
     end
+    context 'when the role of current user is Admin' do
+      it 'allows certain action' do
+        stub_current_user(admin, admin.role.name, admin.role)
+        expect(controller.send(:action_allowed?)).to be_truthy
+      end
+    end
+
+
+
   end
 end

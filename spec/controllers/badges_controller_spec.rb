d38eb732b71fdb0bbfcfc113d720674de2ab558a
Atharva Patil <atharvapatil@Atharvas-MacBook-Pro.local>
2022-03-05 16:20:09 -0500

    Added badges test

--- spec/controllers/badges_controller_spec.rb
+++ spec/controllers/badges_controller_spec.rb
@@ -2,6 +2,7 @@ describe BadgesController do
   let(:super_admin) { build(:superadmin, id: 1, role_id: 5) }
   let(:instructor1) { build(:instructor, id: 10, role_id: 3, parent_id: 3, name: 'Instructor1') }
   let(:student1) { build(:student, id: 21, role_id: 1) }
+  let(:ta) { build(:teaching_assistant, id: 6) }
   describe '#action_allowed?' do
     context 'when the role of current user is Super-Admin' do
       it 'allows certain action' do
@@ -19,6 +20,12 @@ describe BadgesController do
       it 'refuses certain action' do
         stub_current_user(student1, student1.role.name, student1.role)
         expect(controller.send(:action_allowed?)).to be_falsey
+      end
+    end
+    context 'when the role of current user is Teaching Assisstant' do
+      it 'allows certain action' do
+        stub_current_user(ta, ta.role.name, ta.role)
+        expect(controller.send(:action_allowed?)).to be_truthy
       end
     end
   end

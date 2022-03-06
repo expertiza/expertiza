ee8abdd5e428523f2f1094bc7503382f20bd2f4f
Nolan Dowdle <njdowdle@vclvm177-212.vcl.ncsu.edu>
2022-03-05 19:50:46 -0500

    added eula_controller_spec.rb

---
+++ spec/controllers/eula_controller_spec.rb
@@ -0,0 +1,17 @@
+describe EulaController do
+  describe '#action_allowed?' do
+    
+  end
+  
+  describe '#display' do
+
+  end
+
+  describe '#accept' do
+    
+  end
+
+  describe '#decline' do
+
+  end
+end

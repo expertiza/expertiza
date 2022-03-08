2066b0c5011efcd727ff5c8f984b4a1d5000612b
Atharva Rajendra Patil <apatil25@vclv99-91.hpc.ncsu.edu>
2022-03-08 14:43:14 -0500

    added boilerplate for publishing_controller_spec.rb

---
+++ spec/controllers/publishing_controller_spec.rb
@@ -0,0 +1,40 @@
+describe PublishingController do
+  let(:super_admin) { build(:superadmin, id: 1, role_id: 5) }
+  let(:admin) { build(:admin, id: 3) }
+  let(:instructor1) { build(:instructor, id: 10, role_id: 3, parent_id: 3, name: 'Instructor1') }
+  let(:student1) { build(:student, id: 21, role_id: 1) }
+  let(:ta) { build(:teaching_assistant, id: 6) }
+	
+	describe '#action_allowed?' do
+    context 'when the role of current user is Super-Admin' do
+      it 'allows certain action' do
+        stub_current_user(super_admin, super_admin.role.name, super_admin.role)
+        expect(controller.send(:action_allowed?)).to be_truthy
+      end
+    end
+    context 'when the role of current user is Instructor' do
+      it 'allows certain action' do
+        stub_current_user(instructor1, instructor1.role.name, instructor1.role)
+        expect(controller.send(:action_allowed?)).to be_truthy
+      end
+    end
+    context 'when the role of current user is Student' do
+      it 'refuses certain action' do
+        stub_current_user(student1, student1.role.name, student1.role)
+        expect(controller.send(:action_allowed?)).to be_truthy
+      end
+    end
+    context 'when the role of current user is Teaching Assisstant' do
+      it 'allows certain action' do
+        stub_current_user(ta, ta.role.name, ta.role)
+        expect(controller.send(:action_allowed?)).to be_truthy
+      end
+    end
+    context 'when the role of current user is Admin' do
+      it 'allows certain action' do
+        stub_current_user(admin, admin.role.name, admin.role)
+        expect(controller.send(:action_allowed?)).to be_truthy
+      end
+    end
+  end
+end

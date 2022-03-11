a2542084931a3b3adcf51dbe27bd688d17c6c248
Evan Brown <89553353+wevanbrown@users.noreply.github.com>
2022-03-10 22:42:32 -0500

    Added constants used by questionnaires_controller

--- app/models/scored_question.rb
+++ app/models/scored_question.rb
@@ -2,6 +2,12 @@ class ScoredQuestion < ChoiceQuestion
   validates :weight, presence: true # user must specify a weight for a question
   validates :weight, numericality: true # the weight must be numeric
 
+  DEFAULT_MAX_AGREEMENT = 'Strongly agree'.freeze # Default maximum agreement for scored questions
+  DEFAULT_MIN_AGREEMENT = 'Strongly disagree'.freeze # Default minimum agreement for scored questions
+  DEFAULT_ALTERNATIVES = '0|1|2|3|4|5'.freeze # Default alternatives for dropdown questions
+  DEFAULT_CRITERION_SIZE = '50, 3'.freeze # Default size for Criterion and Cake questions
+  DEFAULT_TEXT_FIELD_SIZE = '30'.freeze # Default size for a TextField question
+  DEFAULT_TEXT_AREA_SIZE = '60, 5'.freeze # Default size for a TextArea question
   def edit; end
 
   def view_question_text; end

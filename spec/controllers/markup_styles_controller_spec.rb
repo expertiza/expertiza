23f2da7e1409e099d6575e3fff1281b7dbd507ca
Duy Nguyen <duy.nguyen@rewardstyle.com>
2022-03-11 16:13:12 -0500

    Add markup_styles_controller spec test structure

---
+++ spec/controllers/markup_styles_controller_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+describe MarkupStylesController do
+    describe '#action_allowed?' do
+        #Include tests here
+    end
+
+    describe '#index' do
+        #Include tests here
+    end
+
+    describe '#list' do
+        #Include tests here
+    end
+
+    describe '#show' do
+        #Include tests here
+    end
+
+    describe '#new' do
+        #Include tests here
+    end
+
+    describe '#create' do
+        #Include tests here
+    end
+
+    describe '#edit' do
+        #Include tests here
+    end
+
+    describe '#update' do
+        #Include tests here
+    end
+
+    describe '#destroy' do
+        #Include tests here
+    end
+
+end

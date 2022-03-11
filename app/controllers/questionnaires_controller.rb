552730807fa25b3086e8a5810d618eaa9f3f4fad
War-Keeper <87688584+War-Keeper@users.noreply.github.com>
2022-03-10 22:55:31 -0500

    shortened the create method
    Moved the @questionnaire assignments to another method, also moved the questionnaire_node creation to another method. This makes create easier to read

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -59,31 +59,44 @@ class QuestionnairesController < ApplicationController
         flash[:error] = $ERROR_INFO
       end
       begin
-        @questionnaire.private = questionnaire_private
-        @questionnaire.name = params[:questionnaire][:name]
-        @questionnaire.instructor_id = session[:user].id
-        @questionnaire.min_question_score = params[:questionnaire][:min_question_score]
-        @questionnaire.max_question_score = params[:questionnaire][:max_question_score]
-        @questionnaire.type = params[:questionnaire][:type]
+        
         # Zhewei: Right now, the display_type in 'questionnaires' table and name in 'tree_folders' table are not consistent.
         # In the future, we need to write migration files to make them consistency.
         # E1903 : We are not sure of other type of cases, so have added a if statement. If there are only 5 cases, remove the if statement
         if %w[AuthorFeedback CourseSurvey TeammateReview GlobalSurvey AssignmentSurvey BookmarkRating].include?(display_type)
           display_type = (display_type.split /(?=[A-Z])/).join('%')
         end
-        @questionnaire.display_type = display_type
-        @questionnaire.instruction_loc = Questionnaire::DEFAULT_QUESTIONNAIRE_URL
-        @questionnaire.save
-        # Create node
-        tree_folder = TreeFolder.where(['name like ?', @questionnaire.display_type]).first
-        parent = FolderNode.find_by(node_object_id: tree_folder.id)
-        QuestionnaireNode.create(parent_id: parent.id, node_object_id: @questionnaire.id, type: 'QuestionnaireNode')
-        flash[:success] = 'You have successfully created a questionnaire!'
+        # moved all questionnaire assignments to a new method
+        assign_questionnaire_values(questionnaire_private, display_type)
+        
+        
+        # Create node moved to a new method
+        questionnaire_node_creation()
+
       rescue StandardError
         flash[:error] = $ERROR_INFO
       end
       redirect_to controller: 'questionnaires', action: 'edit', id: @questionnaire.id
     end
+  end
+
+  def assign_questionnaire_values(prv, display)
+    @questionnaire.private = prv
+    @questionnaire.name = params[:questionnaire][:name]
+    @questionnaire.instructor_id = session[:user].id
+    @questionnaire.min_question_score = params[:questionnaire][:min_question_score]
+    @questionnaire.max_question_score = params[:questionnaire][:max_question_score]
+    @questionnaire.type = params[:questionnaire][:type]
+    @questionnaire.display_type = display
+    @questionnaire.instruction_loc = Questionnaire::DEFAULT_QUESTIONNAIRE_URL
+    @questionnaire.save
+  end
+  
+  def questionnaire_node_creation()
+    tree_folder = TreeFolder.where(['name like ?', @questionnaire.display_type]).first
+    parent = FolderNode.find_by(node_object_id: tree_folder.id)
+    QuestionnaireNode.create(parent_id: parent.id, node_object_id: @questionnaire.id, type: 'QuestionnaireNode')
+    flash[:success] = 'You have successfully created a questionnaire!'
   end
 
   # Edit a questionnaire

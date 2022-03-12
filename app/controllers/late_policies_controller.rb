3cf3c2c4d057e6938d32d7a10c8a1ad86924dfb3
stumma2 <94282212+sujith3179@users.noreply.github.com>
2022-03-12 15:50:28 -0500

    Added method comments for late_policies_controller
    Added method comments for late_policies_controller

--- app/controllers/late_policies_controller.rb
+++ app/controllers/late_policies_controller.rb
@@ -1,6 +1,9 @@
 class LatePoliciesController < ApplicationController
   include AuthorizationHelper
 
+# This method checks the privileges of current user to perform certain action.Firstly, pulls out action from parameters.
+# If the action is creating a new late policy then verifies the current user has TA privileges or not.
+# If the action is to edit/update or destroy a late policy then verifies if the current user has instructor privileges or not.
   def action_allowed?
     case params[:action]
     when 'new', 'create', 'index'
@@ -11,6 +14,7 @@ class LatePoliciesController < ApplicationController
     end
   end
 
+# This method lists all the late policies records from late_policies table in database.
   def index
     @penalty_policies = LatePolicy.all
     respond_to do |format|
@@ -19,6 +23,7 @@ class LatePoliciesController < ApplicationController
     end
   end
 
+# This method displays a certain record in late_policies table in the database.
   def show
     @penalty_policy = LatePolicy.find(params[:id])
     respond_to do |format|
@@ -27,6 +32,7 @@ class LatePoliciesController < ApplicationController
     end
   end
 
+# New method creates instance of a late policy in the late_policies's table but does not saves in the database.
   def new
     @late_policy = LatePolicy.new
     respond_to do |format|
@@ -35,10 +41,12 @@ class LatePoliciesController < ApplicationController
     end
   end
 
+# This method just fetch a particular record in LatePolicy table.
   def edit
     @penalty_policy = LatePolicy.find(params[:id])
   end
 
+# Create method can create a new late policy. There are few check points before creating a late policy which are written in the if/else statements
   def create
     # penalty per unit cannot be greater than maximum penalty
     invalid_penalty_per_unit = params[:late_policy][:max_penalty].to_i < params[:late_policy][:penalty_per_unit].to_i
@@ -55,7 +63,10 @@ class LatePoliciesController < ApplicationController
       invalid_max_penalty = true
     end
 
-    if !invalid_penalty_per_unit && !same_policy_name && !invalid_max_penalty
+# If penalty  is valid and the penalty name does not already exists then tries to update and save.
+# If something unexpected happens while saving the record in to database then displays a flash notice and redirect to create a new late policy again.
+# If any of above checks fails, then redirect to create a new late policy again.
+if !invalid_penalty_per_unit && !same_policy_name && !invalid_max_penalty
       @late_policy = LatePolicy.new(late_policy_params)
       @late_policy.instructor_id = instructor_id
       begin
@@ -71,9 +82,10 @@ class LatePoliciesController < ApplicationController
     end
   end
 
+# Update method can update late policy. There are few check points before updating a late policy which are written in the if/else statements
   def update
     @penalty_policy = LatePolicy.find(params[:id])
-    # if name has changed then only check for this
+    # if name has changed then need to check if the name already exists or not.
     if params[:late_policy][:policy_name] != @penalty_policy.policy_name &&
        LatePolicy.check_policy_with_same_name(params[:late_policy][:policy_name], instructor_id)
       flash[:error] = 'Cannot edit the policy. A policy with the same name ' + params[:late_policy][:policy_name] + ' already exists.'
@@ -87,6 +99,8 @@ class LatePoliciesController < ApplicationController
       flash[:error] = 'Maximum penalty cannot be greater than or equal to 100'
       redirect_to action: 'edit', id: params[:id]
     else
+      # If the the policy passes all the above checks then controller tries to update.
+      # If something unexpected happens while updating, then redirect to the edit page of that policy again.
       begin
         @penalty_policy.update_attributes(late_policy_params)
         @penalty_policy.save!
@@ -100,6 +114,8 @@ class LatePoliciesController < ApplicationController
     end
   end
 
+# This method fetches a particular record in the late_policy table and try to destroy's it. 
+# If the record cannot be deleted then it displays a flash message.
   def destroy
     @penalty_policy = LatePolicy.find(params[:id])
     begin
@@ -111,16 +127,16 @@ class LatePoliciesController < ApplicationController
   end
 
   private
-
+# This function ensures that a specific parameter is present.If not then throws and error.
   def late_policy_params
     params.require(:late_policy).permit(:policy_name, :penalty_per_unit, :penalty_unit, :max_penalty)
   end
-
+# This function check's if the current user is instructor or not.If not then throws and error.
   def instructor_id
     late_policy.try(:instructor_id) ||
       current_user.instructor_id
   end
-
+# This function checks if the id exists in parameters and assigns it to the instance variable of penalty policy
   def late_policy
     @penalty_policy ||= @late_policy || LatePolicy.find(params[:id]) if params[:id]
   end

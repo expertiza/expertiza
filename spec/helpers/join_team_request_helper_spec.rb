0f126f7c760fc9a17032b3543b5946ff084599eb
mkmcary <mkmcary@gmail.com>
2022-03-15 10:55:36 -0400

    Added tests for JoinTeamRequestsHelper display_request_status()

--- spec/helpers/join_team_request_helper_spec.rb
+++ spec/helpers/join_team_request_helper_spec.rb
@@ -1,9 +1,47 @@
 describe JoinTeamRequestsHelper do
   let(:join_team_request1) { build(:join_team_request) }
   describe '#display_request_status' do
+    # Pending Status
     context 'when the request is pending' do
+      before(:each) do
+        join_team_request1.status = 'P'
+      end
+
       it 'returns the associated status' do
         expect(helper.display_request_status(join_team_request1)).to eq('Pending: A request has been made to join this team.')
+      end
+    end
+
+    # Denied Status
+    context 'when the request is denied' do
+      before(:each) do
+        join_team_request1.status = 'D'
+      end
+
+      it 'returns the associated status' do
+        expect(helper.display_request_status(join_team_request1)).to eq('Pending: A request has been made to join this team.')
+      end
+    end
+
+    # Accepted Status
+    context 'when the request is accepted' do
+      before(:each) do
+        join_team_request1.status = 'A'
+      end
+
+      it 'returns the associated status' do
+        expect(helper.display_request_status(join_team_request1)).to eq('Pending: A request has been made to join this team.')
+      end
+    end
+
+    # Unexpected Status
+    context 'when the request is accepted' do
+      before(:each) do
+        join_team_request1.status = 'a different status'
+      end
+
+      it 'returns the associated status' do
+        expect(helper.display_request_status(join_team_request1)).to eq('a different status')
       end
     end
   end

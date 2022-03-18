c7ef9fc75712ac1ac33a5a87121896d446eb4c69
Duy Nguyen <duy.nguyen@rewardstyle.com>
2022-03-17 21:37:07 -0400

    fix the factory

--- spec/controllers/lock_controller_spec.rb
+++ spec/controllers/lock_controller_spec.rb
@@ -2,7 +2,7 @@ describe LockController do
   let(:super_admin) { build(:superadmin, id: 1, role_id: 5) }
   let(:instructor1) { build(:instructor, id: 10, role_id: 3, parent_id: 3, name: 'Instructor1') }
   let(:student1) { build(:student, id: 21, role_id: 1) }
-  let(:lock1) { build(:lock, id: 1, name: 'test lockable') }
+  let(:lock1) { build(:lock, id: 1, lockable_type: 'test lockable') }
 
   describe '#action_allowed?' do
     context 'when the role of current user is Instructor' do

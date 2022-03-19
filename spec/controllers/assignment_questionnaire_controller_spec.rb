a055eb7c1874eed5980e3a4ca1e989d6552e4341
Jackson Hardy <jmhardy3@vclv99-224.hpc.ncsu.edu>
2022-03-19 16:07:52 -0400

    outline for create testing

--- spec/controllers/assignment_questionnaire_controller_spec.rb
+++ spec/controllers/assignment_questionnaire_controller_spec.rb
@@ -3,6 +3,7 @@ describe AssignmentQuestionnaireController do
   let(:instructor1) { build(:instructor, id: 10, role_id: 3, parent_id: 3, name: 'Instructor1') }
   let(:student1) { build(:student, id: 21, role_id: 1) }
   let(:assignment) { build(:assignment, id: 1) }
+  let(:questionnaire) { build(:questionnaire, id:1) }
   describe '#action_allowed?' do
     context 'when no assignment is associated with the id' do
       it 'refuses certain action' do
@@ -19,4 +20,31 @@ describe AssignmentQuestionnaireController do
       end
     end
   end
+
+  describe '#create' do
+  context 'assignment id is not entered' do
+    it 'flashes a response of missing assignment id' do
+      allow(Assignment).to recieve(new()).and_return(params[:assigment_id])
+      assert_nil(params[:assignment_id])
+    end
+  end
+  context 'questionnaire id is not entered' do
+    it 'flashes a response of missing questionnaire id' do
+      allow(Questionnaire).to recieve(new()).and_return(params[:questionnaire_id])
+      assert_nil(params[:questionnaire_id])
+    end
+  end
+  context 'when no assignment is associated with the id' do
+    it 'flashes no assignment id in the db' do
+      allow(Assignment).to receive(:find).and_return(nil)
+      expect(flash[:error]).to match(/Assignment # [0-9] does not currently exist./) ## check this regex
+    end
+  end
+  context 'when no questionnaire is associated with id' do
+    it 'flashes no questionaire id in the db' do
+      allow(Questionnaire).to receive(:find).and_return(nil)
+      expect(flash[:error]).to match(/Questionnaire # [0-9] does not currently exist./) ## check this regex
+    end
+  end
 end
+

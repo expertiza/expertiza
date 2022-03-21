501c0700dfa6c80e6b513a3fe4aaa06fee3b940a
indu <42580467+indu2407@users.noreply.github.com>
2022-03-21 19:00:27 -0400

    check for quizquestionnaire

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -278,6 +278,10 @@ class QuestionnairesController < ApplicationController
         q.questionnaire_id = questionnaire_id
         q.type = params[:question_type][question_key][:type]
         q.seq = question_key.to_i
+        if @questionnaire.type == 'QuizQuestionnaire'
+          weight_key = "question_#{index + 1}"
+          q.weight = params[:question_weights][weight_key.to_sym]
+        end
         q.save unless q.txt.strip.empty?
       end
     end

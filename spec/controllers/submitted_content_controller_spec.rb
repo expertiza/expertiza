d0bbc9c1d12d851d7450f3561dcdb63dbc328cb4
LFox95 <lmcampb5@ncsu.edu>
2022-03-24 23:53:12 -0400

    Added additional tests for 'submit_file'.  Added two additional files to support new tests

--- spec/controllers/submitted_content_controller_spec.rb
+++ spec/controllers/submitted_content_controller_spec.rb
@@ -13,5 +13,30 @@ describe SubmittedContentController do
         expect(response).to redirect_to(action: :edit, id: 1)
       end
     end
+    context 'user that is participant uploads a file' do
+      before(:each) do
+        allow(AssignmentParticipant).to receive(:find).and_return(participant)
+        stub_current_user(student1, student1.role.name, student1.role)
+      end
+      it 'flashes error for file exceeding size limit' do
+        params = {uploaded_file: Rack::Test::UploadedFile.new("#{Rails.root}/spec/fixtures/files/the-rspec-book_p2_1.pdf"),
+                  id: 1}
+        response = get :submit_file, params
+        expect(response).to redirect_to(action: :edit, id: 1)
+        expect(flash[:error]).to be_present # not checking message content since it uses variable size limit
+      end
+      it 'flashes error for file of unexpected type' do
+        params = {uploaded_file: Rack::Test::UploadedFile.new("#{Rails.root}/spec/fixtures/files/helloworld.c"),
+                  id: 1}
+        response = get :submit_file, params
+        expect(response).to redirect_to(action: :edit, id: 1)
+        expect(flash[:error]).to eq 'File type error'
+      end
+      # it 'creates new directory' do
+      # it 'writes file to server' do
+      # it 'unzips the file if needed' do
+      # it 'creates a submission record' do
+      # it 'notifies all reviewers assigned to reviewee' do
+    end
   end
 end

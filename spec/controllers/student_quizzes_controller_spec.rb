f63821e3e1099fa60bbb2b2cf4a2a82479bcdf7f
Jyothi Sumer <mjyothisumer@gmail.com>
2022-03-27 20:15:28 -0400

    Adding test cases for student_quizzes_controller.rb

---
+++ spec/controllers/student_quizzes_controller_spec.rb
@@ -0,0 +1,51 @@
+describe StudentQuizzesController do
+    let(:assignment_participant) do
+        build(:assignment_participant, id: 36226, can_submit: true, can_review: true, user_id: 5, parent_id: 848)
+    end
+    let(:assignment) do
+        build(:assignment, id: 848, name: "Test 1", directory_path: "Test_1", submitter_count: 0, course_id: 68, instructor_id: 6, private: true, reviews_visible_to_all: false, num_reviewers: 0)
+    end
+    let(:response) do
+        build(:response, id: 96252, map_id: 130389, additional_comment: nil)
+    end
+    let(:question) do
+        build(:question, id: 5841, txt: "Question - 1", weight: 1, questionnaire_id: 572 )
+    end
+    let(:quiz_questionnaire) { QuizQuestionnaire.new }
+    let(:quiz_response_map) { build(:quiz_response_map, id: 130389, quiz_questionnaire: quiz_questionnaire, reviewee_id: 28012, reviewed_object_id: 572) }
+    let(:student) do
+        build(:student, id: 5)
+    end
+    let(:student1) do
+        build(:student, id: 55)
+    end
+    let(:admin) { build(:admin, id: 7) }
+
+    describe "#action_allowed?" do
+        it "when the current user is admin" do
+            # To stub the user into session
+            stub_current_user(admin, admin.role.name, admin.role)
+            expect(controller.send(:action_allowed?)).to be true
+        end
+
+        it "when the current user is student" do
+            # To stub the user into session
+            stub_current_user(student, student.role.name, student.role)
+            expect(controller.send(:action_allowed?)).to be true
+        end
+    end
+
+    describe "GET index" do
+        render_views
+        it "call index method" do
+            stub_current_user(student, student.role.name, student.role)
+            controller.params = { id: '36226'}
+            allow(AssignmentParticipant).to receive(:find).with('36226').and_return assignment_participant
+            allow(Assignment).to receive(:find).with(848).and_return assignment
+            allow(QuizResponseMap).to receive(:where).with(reviewer_id: 36226).and_return([quiz_response_map])
+            result = get :index
+            expect(response).to redirect_to('/')
+            expect(result.status).to eq 302
+        end
+    end
+end

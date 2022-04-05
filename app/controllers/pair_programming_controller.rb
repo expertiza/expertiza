06a77e8f4dc579c39e5acf946edc0e99a7eeb126
Atharva Joshi <amjoshi3@vm19-108.vcl.ncsu.edu>
2022-04-05 18:06:58 -0400

    Pair Programming feature added for students

--- app/controllers/pair_programming_controller.rb
+++ app/controllers/pair_programming_controller.rb
@@ -6,13 +6,27 @@ class PairProgrammingController < ApplicationController
     end
 
     def send_invitations
-        puts "INNNNNNNNNNNNNNNNNNNNNNN"
-        puts params[:team_id]
-
         users = TeamsUser.where(team_id: params[:team_id])
         users.each { |user| user.update_attributes(pair_programming_status: "W")}
+        TeamsUser.find_by(team_id: params[:team_id], user_id: current_user.id).update_attributes(pair_programming_status: "A")
         #ExpertizaLogger.info "Accepting Invitation #{params[:inv_id]}: #{accepted}"
         flash[:success] = "Invitations have been sent successfully!"
+        Team.find(params[:team_id]).update_attributes(pair_programming_request: true)
+        redirect_to view_student_teams_path student_id: params[:student_id]
+    end
+
+    def accept
+        user = TeamsUser.find_by(team_id: params[:team_id], user_id: current_user.id)
+        user.update_attributes(pair_programming_status: "A")
+        flash[:success] = "Pair Programming Request Accepted Successfully!"
+        redirect_to view_student_teams_path student_id: params[:student_id]
+    end
+
+    def decline
+        user = TeamsUser.find_by(team_id: params[:team_id], user_id: current_user.id)
+        user.update_attributes(pair_programming_status: "D")
+        flash[:success] = "Pair Programming Request Declined!"
+        Team.find(params[:team_id]).update_attributes(pair_programming_request: false)
         redirect_to view_student_teams_path student_id: params[:student_id]
     end
 end

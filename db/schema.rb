bd49cbfa1d0f8b8988a0f80e52d463400a649a9f
John Bumgardner <john.bumgardner@icloud.com>
2022-04-14 16:33:34 -0400

    try to fix

--- db/schema.rb
+++ db/schema.rb
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 20220414172528) do
     t.string "value"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.decimal "confidence_level", precision: 10, scale: 5
     t.index ["answer_id"], name: "index_answer_tags_on_answer_id"
     t.index ["tag_prompt_deployment_id"], name: "index_answer_tags_on_tag_prompt_deployment_id"
     t.index ["user_id"], name: "index_answer_tags_on_user_id"
@@ -111,6 +110,7 @@ ActiveRecord::Schema.define(version: 20220414172528) do
     t.integer "simicheck", default: -1
     t.integer "simicheck_threshold", default: 100
     t.boolean "is_answer_tagging_allowed"
+    t.boolean "has_badge"
     t.boolean "allow_selecting_additional_reviews_after_1st_round"
     t.integer "sample_assignment_id"
     t.boolean "vary_by_topic", default: false
@@ -121,7 +121,6 @@ ActiveRecord::Schema.define(version: 20220414172528) do
     t.boolean "auto_assign_mentor", default: false
     t.boolean "duty_based_assignment?"
     t.boolean "questionnaire_varies_by_duty"
-    t.boolean "enable_pair_programming"
     t.index ["course_id"], name: "fk_assignments_courses"
     t.index ["instructor_id"], name: "fk_assignments_instructors"
     t.index ["late_policy_id"], name: "fk_late_policy_id"
@@ -304,7 +303,6 @@ ActiveRecord::Schema.define(version: 20220414172528) do
     t.integer "times_used", default: 0, null: false
     t.integer "instructor_id", null: false
     t.string "policy_name", null: false
-    t.boolean "private", default: true, null: false
     t.index ["instructor_id"], name: "fk_instructor_id"
   end
 
@@ -313,8 +311,8 @@ ActiveRecord::Schema.define(version: 20220414172528) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
-    t.integer "lockable_id"
     t.string "lockable_type"
+    t.integer "lockable_id"
     t.index ["user_id"], name: "fk_rails_426f571216"
   end
 
@@ -681,14 +679,12 @@ ActiveRecord::Schema.define(version: 20220414172528) do
     t.integer "grade_for_submission"
     t.text "comment_for_submission"
     t.boolean "make_public", default: false
-    t.integer "pair_programming_request", limit: 1
   end
 
   create_table "teams_users", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.integer "team_id"
     t.integer "user_id"
     t.integer "duty_id"
-    t.string "pair_programming_status", limit: 1
     t.index ["duty_id"], name: "index_teams_users_on_duty_id"
     t.index ["team_id"], name: "fk_users_teams"
     t.index ["user_id"], name: "fk_teams_users"

cc9f7cc55b73fae356f87ce97f1143df03dfb84c
Vijaya Durga Kona <vkona@vclv99-202.hpc.ncsu.edu>
2022-04-20 19:49:25 -0400

    E2231 -Adds a test case to test the background colour changes based on bid size

--- spec/helpers/review_bids_helper_spec.rb
+++ spec/helpers/review_bids_helper_spec.rb
@@ -8,4 +8,13 @@ describe ReviewBidsHelper do
       end
     end
   end
+  describe '#get_topic_bg_color_review_bids' do
+    context 'when bid size is 0' do
+      it 'changes colour' do
+        allow(ReviewBid).to receive(:where).and_return([])
+        num_bids=0
+        expect(helper.get_topic_bg_color_review_bids(topic,2)).to eq("rgb(47,352,0)")
+      end
+    end
+  end
 end

735ac7f1829d2e2d9c4c6781bbfae385fd7a9af9
War-Keeper <87688584+War-Keeper@users.noreply.github.com>
2022-04-21 23:24:09 -0400

    moved some tests to review_mapping_chart_helper_sp

--- spec/helpers/review_mapping_helper_spec.rb
+++ spec/helpers/review_mapping_helper_spec.rb
@@ -967,26 +967,8 @@ describe ReviewMappingHelper, type: :helper do
       expect(@avg).to eq '9%'
     end
   end
-  describe 'test calculate_key_chart_information' do
-    it 'should return new Hash if intervals are not empty' do
-      intervals = [1.00, 2.00, 3.00, 4.00, 5.00, 6.00]
-      result = helper.calculate_key_chart_information(intervals)
-      expect(result).to be_a_kind_of(Hash)
-      expect(result[:mean]).to eq(3.50)
-      expect(result[:min]).to eq(1.00)
-      expect(result[:max]).to eq(6.00)
-      expect(result[:variance]).to eq(2.92)
-      expect(result[:stand_dev]).to eq(1.71)
-    end
-  end
-
-  describe 'test calculate_key_chart_information' do
-    it 'should return nil if intervals are empty' do
-      intervals = []
-      result = helper.calculate_key_chart_information(intervals)
-      expect(result).to be_nil
-    end
-  end
+  
+  # moved test calculate_key_chart_information tests to the review_mapping_chart_helper_spec file
 
   describe 'test css_style_for_calibration_report' do
     it 'should return correct css class' do

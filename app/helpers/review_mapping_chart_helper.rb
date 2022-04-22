72d9cc3e736fb378a9130f66c5dbfcd28e1a3286
War-Keeper <87688584+War-Keeper@users.noreply.github.com>
2022-04-21 23:36:47 -0400

    rubocop stuff

--- app/helpers/review_mapping_chart_helper.rb
+++ app/helpers/review_mapping_chart_helper.rb
@@ -1,6 +1,5 @@
 module ReviewMappingChartHelper
 
-  
   # moves data of reviews in each round from a current round
   def initialize_chart_elements(reviewer)
     round = 0
@@ -28,59 +27,59 @@ module ReviewMappingChartHelper
   def display_volume_metric_chart(reviewer)
     labels, reviewer_data, all_reviewers_data = initialize_chart_elements(reviewer)
     data = {
-        labels: labels,
-        datasets: [
-            {
-                label: 'vol.',
-                backgroundColor: 'rgba(255,99,132,0.8)',
-                borderWidth: 1,
-                data: reviewer_data,
-                yAxisID: 'bar-y-axis1'
-            },
-            {
-                label: 'avg. vol.',
-                backgroundColor: 'rgba(255,206,86,0.8)',
-                borderWidth: 1,
-                data: all_reviewers_data,
-                yAxisID: 'bar-y-axis2'
-            }
-        ]
+      labels: labels,
+      datasets: [
+        {
+          label: 'vol.',
+          backgroundColor: 'rgba(255,99,132,0.8)',
+          borderWidth: 1,
+          data: reviewer_data,
+          yAxisID: 'bar-y-axis1'
+        },
+        {
+          label: 'avg. vol.',
+          backgroundColor: 'rgba(255,206,86,0.8)',
+          borderWidth: 1,
+          data: all_reviewers_data,
+          yAxisID: 'bar-y-axis2'
+        }
+      ]
     }
     options = {
-        legend: {
-            position: 'top',
-            labels: {
-                usePointStyle: true
-            }
-        },
-        width: '200',
-        height: '125',
-        scales: {
-            yAxes: [{
-                        stacked: true,
-                        id: 'bar-y-axis1',
-                        barThickness: 10
-                    }, {
-                        display: false,
-                        stacked: true,
-                        id: 'bar-y-axis2',
-                        barThickness: 15,
-                        type: 'category',
-                        categoryPercentage: 0.8,
-                        barPercentage: 0.9,
-                        gridLines: {
-                            offsetGridLines: true
-                        }
-                    }],
-            xAxes: [{
-                        stacked: false,
-                        ticks: {
-                            beginAtZero: true,
-                            stepSize: 50,
-                            max: 400
-                        }
-                    }]
+      legend: {
+        position: 'top',
+        labels: {
+          usePointStyle: true
         }
+      },
+      width: '200',
+      height: '125',
+      scales: {
+        yAxes: [{
+          stacked: true,
+          id: 'bar-y-axis1',
+          barThickness: 10
+        }, {
+          display: false,
+          stacked: true,
+          id: 'bar-y-axis2',
+          barThickness: 15,
+          type: 'category',
+          categoryPercentage: 0.8,
+          barPercentage: 0.9,
+          gridLines: {
+            offsetGridLines: true
+          }
+        }],
+        xAxes: [{
+          stacked: false,
+          ticks: {
+            beginAtZero: true,
+            stepSize: 50,
+            max: 400
+          }
+        }]
+      }
     }
     horizontal_bar_chart data, options
   end
@@ -95,35 +94,35 @@ module ReviewMappingChartHelper
     end
     # build the parameters for the chart
     data = {
-        labels: [*1..intervals.length],
-        datasets: [
-            {
-                backgroundColor: 'rgba(255,99,132,0.8)',
-                data: intervals,
-                label: 'time intervals'
-            },
-            unless intervals.empty?
-              {
-                  data: Array.new(intervals.length, interval_mean),
-                  label: 'Mean time spent'
-              }
-            end
-        ]
+      labels: [*1..intervals.length],
+      datasets: [
+        {
+          backgroundColor: 'rgba(255,99,132,0.8)',
+          data: intervals,
+          label: 'time intervals'
+        },
+        unless intervals.empty?
+          {
+            data: Array.new(intervals.length, interval_mean),
+            label: 'Mean time spent'
+          }
+        end
+      ]
     }
     options = {
-        width: '200',
-        height: '125',
-        scales: {
-            yAxes: [{
-                        stacked: false,
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }],
-            xAxes: [{
-                        stacked: false
-                    }]
-        }
+      width: '200',
+      height: '125',
+      scales: {
+        yAxes: [{
+          stacked: false,
+          ticks: {
+            beginAtZero: true
+          }
+        }],
+        xAxes: [{
+          stacked: false
+        }]
+      }
     }
     line_chart data, options
   end
@@ -149,5 +148,4 @@ module ReviewMappingChartHelper
     # if no Hash object is returned, the UI handles it accordingly
   end
   
-
 end

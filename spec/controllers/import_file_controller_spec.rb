8688e20cfe08ec1de824131b4eaf575fa17bb177
Chirrag Nangia <cnangia@vclvm177-129.vcl.ncsu.edu>
2022-04-24 18:45:59 -0400

    added test case for actionallowed, show, import method

--- spec/controllers/import_file_controller_spec.rb
+++ spec/controllers/import_file_controller_spec.rb
@@ -1,3 +1,5 @@
+require 'tempfile'
+
 describe ImportFileController do
   let(:super_admin) { build(:superadmin, id: 1, role_id: 5) }
   let(:instructor1) { build(:instructor, id: 10, role_id: 3, parent_id: 3, name: 'Instructor1') }
@@ -29,4 +31,66 @@ describe ImportFileController do
       end
     end
   end
+
+
+
+  describe '#show' do
+    context 'when trying to display uploaded csv for User model' do
+      it 'renders show template after parsing contents of csv' do
+        stub_current_user(ta, ta.role.name, ta.role)
+        params = {
+          "delim_type"=>"comma", 
+          "has_header"=>"true", 
+          "file"=>Rack::Test::UploadedFile.new("#{Rails.root}/spec/fixtures/User.csv"), 
+          "model"=>"User", 
+          "id"=>""
+        }
+
+        get :show, params
+        # puts controller.instance_variable_get('@contents_grid')
+        # puts controller.instance_variable_get('@contents_hash')
+        expect(response).to render_template(:show)
+      end
+    end
+  end
+
+
+  describe '#import' do
+    context 'when import data for User model succeeds' do
+      it 'redirects to user list page' do
+        stub_current_user(ta, ta.role.name, ta.role)
+        params = {
+          "contents_hash"=>"{:header=>[\"name\", \"full name\", \"email\"], :body=>[[\"super_admin_chi\", \"2, super_administrator\", \"super_admin_chi@mailinator.com\"], [\"instr_chi\", \"3, instructor\", \"instr_chi@mailinator.com\"], [\"stud_chi\", \"8, student\", \"stud_chi@mailinator.com\"]]}", 
+          "has_header"=>"true", 
+          "model"=>"User"
+        }
+
+        allow(User).to receive(:import).with(any_args).and_return(nil)
+        get :import, params, {return_to: list_users_path}
+
+        expect(response).to redirect_to(list_users_path)
+      end
+    end
+
+    context 'when import data for User model fails and raises exception' do
+      it 'redirects to user list page after flashing error message' do
+        stub_current_user(ta, ta.role.name, ta.role)
+        params = {
+          "contents_hash"=>"{:header=>[\"name\", \"full name\", \"email\"], :body=>[[\"super_admin_chi\", \"2, super_administrator\", \"super_admin_chi@mailinator.com\"], [\"instr_chi\", \"3, instructor\", \"instr_chi@mailinator.com\"], [\"stud_chi\", \"8, student\", \"stud_chi@mailinator.com\"]]}", 
+          "has_header"=>"true", 
+          "model"=>"User"
+        }
+
+        expect(User).to receive(:import).and_raise(ActiveRecord::RecordInvalid)
+        get :import, params, {return_to: list_users_path}
+        expect(flash[:error]).to be_present
+        expect(response).to redirect_to(list_users_path)
+      end
+    end
+  end
+
+  
+
+
+
 end

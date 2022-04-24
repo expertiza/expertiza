d70d27f4cd09903cd108fd6eb23a95fd8389cbf4
Rachel Hyo Son <52179017+rachelhson@users.noreply.github.com>
2022-04-24 19:26:46 -0400

    Update review_mapping_helper.rb

--- app/helpers/review_mapping_helper.rb
+++ app/helpers/review_mapping_helper.rb
@@ -235,6 +235,7 @@ module ReviewMappingHelper
   # gets review and feedback responses for a certain round for the feedback report
   def certain_review_and_feedback_response_map?(author)
     # Setting values of instance variables
+    @feedback_response_maps = FeedbackResponseMap.where(['reviewed_object_id IN (?) and reviewer_id = ?', @all_review_response_ids, author.id])
     @team_id = TeamsUser.team_id(@id.to_i, author.user_id)
     @review_response_map_ids = ReviewResponseMap.where(['reviewed_object_id = ? and reviewee_id = ?', @id, @team_id]).pluck('id')
     @review_responses = Response.where(['map_id IN (?)', @review_response_map_ids])

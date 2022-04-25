c4c0429b1ef47c8044f20e1dd345d5fb74fc492e
lfox95 <lmcampb5@ncsu.edu>
2022-04-25 17:58:08 -0400

    Change copy_assignment_submissions to use dup, much shorter

--- app/models/submission_record.rb
+++ app/models/submission_record.rb
@@ -8,12 +8,7 @@ class SubmissionRecord < ApplicationRecord
   def self.copy_assignment_submissions(old_assign, new_assign_id)
     @prev_submission_record = SubmissionRecord.where(assignment_id: old_assign.id)
     @prev_submission_record.each do |catt|
-      @new_submission_record = SubmissionRecord.new
-      @new_submission_record.type = catt.type
-      @new_submission_record.content = catt.content
-      @new_submission_record.operation = catt.operation
-      @new_submission_record.team_id = catt.team_id
-      @new_submission_record.user = catt.user
+      @new_submission_record = catt.dup
       @new_submission_record.assignment_id = new_assign_id
       @new_submission_record.save
     end

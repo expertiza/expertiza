20207e0edbde26912c9d71edea17b2059c0a203a
lpobrien <lpobrien.ncsu.edu>
2022-04-25 16:53:32 -0400

    Corrected mistaken parameter

--- app/helpers/response_helper.rb
+++ app/helpers/response_helper.rb
@@ -14,8 +14,8 @@ module ResponseHelper
 
   # Assigns total contribution for cake question across all reviewers to a hash map
   # Key : question_id, Value : total score for cake question
-  def store_total_cake_score(map_id)
-    reviewee = ResponseMap.select(:reviewee_id, :type).where(id: map_id.to_s).first
+  def store_total_cake_score
+    reviewee = ResponseMap.select(:reviewee_id, :type).where(id: @response.map_id.to_s).first
     @total_score = Cake.get_total_score_for_questions(reviewee.type,
                                                       @review_questions,
                                                       @participant.id,
@@ -39,8 +39,7 @@ module ResponseHelper
     @contributor = @map.contributor
     new_response ? questionnaire_from_response_map : questionnaire_from_response
     set_dropdown_or_scale
-    new_response ? set_questions_for_new_response : set_questions  
-    # @review_questions = sort_questions(@questionnaire.questions)
+    @review_questions = sort_questions(@questionnaire.questions)
     @min = @questionnaire.min_question_score
     @max = @questionnaire.max_question_score
     # The new response is created here so that the controller has access to it in the new method
@@ -49,26 +48,4 @@ module ResponseHelper
       @response = Response.create(map_id: @map.id, additional_comment: '', round: @current_round, is_submitted: 0)
     end
   end
-
-  def set_questions_for_new_response
-    @review_questions = sort_questions(@questionnaire.questions)
-    if(@assignment && @assignment.is_revision_planning_enabled)
-      reviewees_topic = SignedUpTeam.topic_id_by_team_id(@contributor.id)
-      current_round = @assignment.number_of_current_round(reviewees_topic)
-      @revision_plan_questionnaire = RevisionPlanTeamMap.find_by(team_id: @map.reviewee_id, used_in_round: current_round).try(:questionnaire)
-      if(@revision_plan_questionnaire)
-        @review_questions += sort_questions(@revision_plan_questionnaire.questions)
-      end
-    end
-    return @review_questions
-  end
-
-  def set_questions
-    @review_questions = []
-    answers = @response.scores
-    questionnaires = @response.questionnaires_by_answers(answers)
-    questionnaires.each {|questionnaire| @review_questions += sort_questions(questionnaire.questions) }
-    return @review_questions
-  end
-
 end

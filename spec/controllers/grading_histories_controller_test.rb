357d7b1bfe1c1fb7fc92b350a33a0b092389434f
Kelly Fleming <kflemin3@ncsu.edu>
2022-04-25 17:08:44 -0400

    add test for creation of GradingHistory objects and corresponding factory object

--- spec/controllers/grading_histories_controller_test.rb
+++ spec/controllers/grading_histories_controller_test.rb
@@ -1,19 +1,14 @@
 describe GradingHistoriesController do
-  let(:instructor) { build(:instructor, id: 6) }
-  describe '#index' do
-    context 'when show review grading history for paticipant' do
-      it 'gets history for the paticipant and renders grading_history#index page' do
-        session = {user: instructor}
-        allow(GradingHistory).to receive(:create).with(instructor_id: session[:user].id,
-                                                       assignment_id: 1,
-                                                       grading_type: "Submission",
-                                                       grade_receiver_id: 2,
-                                                       grade: 100,
-                                                       comment: 'comment')
-        params = {grade_receiver_id: 2, grade_type:"Submission"}
-        get :index, params,session
-        expect(response).to redirect_to('grading_histories?grade_receiver_id=2&grade_type=Submission')
+  let(:grading_history) { build(:grading_history) }
+  
+  describe ':create' do
+      it 'successfully creates a grading history with valid parameters' do
+        expect(grading_history.grade).to eq(100)
+        expect(grading_history.comment).to eq("Good work!")
+        expect(grading_history.instructor_id).to eq(6)
+        expect(grading_history.assignment_id).to eq(1)
+        expect(grading_history.grade_receiver_id).to eq(1)
+        expect(grading_history.id).to eq(1)
       end
     end
-  end
 end

4d25dee9e137e24d1a65b9af4dbdbf344bea286d
Rachel Hyo Son <52179017+rachelhson@users.noreply.github.com>
2022-04-26 16:41:33 -0400

    Combine To ReviewStrategy

--- app/models/review_strategy.rb
+++ app/models/review_strategy.rb
@@ -1,8 +1,37 @@
 class ReviewStrategy
   attr_accessor :participants, :teams
+
   def initialize(participants, teams, review_num)
     @participants = participants
     @teams = teams
     @review_num = review_num
+  end
+end
+
+class StudentReviewStrategy < ReviewStrategy
+  def reviews_per_team
+    (@participants.size * @review_num * 1.0 / @teams.size).round
+  end
+
+  def reviews_needed
+    @participants.size * @review_num
+  end
+
+  def reviews_per_student
+    @review_num
+  end
+end
+
+class TeamReviewStrategy < ReviewStrategy
+  def reviews_per_team
+    @review_num
+  end
+
+  def reviews_needed
+    @teams.size * @review_num
+  end
+
+  def reviews_per_student
+    (@teams.size * @review_num * 1.0 / @participants.size).round
   end
 end

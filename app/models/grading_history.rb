cbf9e12b209fc31f1006a57e851be1bfc43dae14
gskn98 <97813136+gskn98@users.noreply.github.com>
2022-04-25 22:55:05 -0400

    Update grading_history.rb
    Commented out Attribute Protected because it was throwing an error in Travis Build.

--- app/models/grading_history.rb
+++ app/models/grading_history.rb
@@ -1,5 +1,5 @@
 class GradingHistory < ActiveRecord::Base
-  attr_protected
+  #attr_protected
   belongs_to :instructor, inverse_of: :instructor_id
   belongs_to :assignment, inverse_of: :assignment_id
 end

fde3c8cd3cdb8bff7063325187f7330ceb47584f
Wyatt Plaga <wyattplaga@gmail.com>
2022-04-25 23:10:18 -0400

    Rubocop 2

--- app/controllers/revision_plan_questionnaires_controller.rb
+++ app/controllers/revision_plan_questionnaires_controller.rb
@@ -1,28 +1,27 @@
 class RevisionPlanQuestionnairesController < QuestionnairesController
-    def action_allowed?
-      case params[:action]
-      when 'edit'
-        @team_members = Array.new
-        questionnaire = Questionnaire.find(params[:id])
+  def action_allowed?
+    case params[:action]
+    when 'edit'
+      @team_members = Array.new
+      questionnaire = Questionnaire.find(params[:id])
         
-        TeamsUser.where(["team_id = ?", params[:team_id]]).each do |teamuser|
-          @team_members.push( teamuser.user_id)
-              end
+      TeamsUser.where(["team_id = ?", params[:team_id]]).each do |teamuser|
+        @team_members.push( teamuser.user_id)
+      end
               
-        (user_logged_in? && 
-          @team_members.collect { |u| u.id }.include?(session[:user].id)) || super    
-      else
-        super
-      end
+      (user_logged_in? && 
+      @team_members.collect { |u| u.id }.include?(session[:user].id)) || super    
+    else
+      super
     end
+  end
   
-    def new
-      begin
-        questionnaire = RevisionPlanQuestionnaire.get_questionnaire_for_current_round(params[:team_id])
-        redirect_to action: 'edit', id: questionnaire.id
-      rescue StandardError
-        flash[:error] = $ERROR_INFO
-      end
+  def new
+    begin
+      questionnaire = RevisionPlanQuestionnaire.get_questionnaire_for_current_round(params[:team_id])
+      redirect_to action: 'edit', id: questionnaire.id
+    rescue StandardError
+      flash[:error] = $ERROR_INFO
     end
-  
+  end 
 end

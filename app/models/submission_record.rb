e6b0e17dfe9bfe90d2b311bdb201f0c925951d1d
lfox95 <lmcampb5@ncsu.edu>
2022-04-27 19:08:45 -0400

    Change instance variables in copy_assignment_submissions to local variables

--- app/models/submission_record.rb
+++ app/models/submission_record.rb
@@ -6,11 +6,11 @@ class SubmissionRecord < ApplicationRecord
   validates :assignment_id, presence: true
 
   def self.copy_assignment_submissions(old_assign, new_assign_id)
-    @prev_submission_record = SubmissionRecord.where(assignment_id: old_assign.id)
-    @prev_submission_record.each do |catt|
-      @new_submission_record = catt.dup
-      @new_submission_record.assignment_id = new_assign_id
-      @new_submission_record.save
+    prev_submission_record = SubmissionRecord.where(assignment_id: old_assign.id)
+    prev_submission_record.each do |catt|
+      new_submission_record = catt.dup
+      new_submission_record.assignment_id = new_assign_id
+      new_submission_record.save # should we check if this is successful?
     end
   end
 end

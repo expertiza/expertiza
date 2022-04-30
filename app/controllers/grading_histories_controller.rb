f2c7592c8beef068858c1e0d6ac90022b963c673
Kelly Fleming <kflemin3@ncsu.edu>
2022-04-30 18:38:22 -0400

    Adds AuthenticationHelper method in action_allowed?, adds comments to all methods, renames check_type to assignment_for_history

--- app/controllers/grading_histories_controller.rb
+++ app/controllers/grading_histories_controller.rb
@@ -1,9 +1,14 @@
 class GradingHistoriesController < ApplicationController
+  include AuthorizationHelper
   before_action :set_grading_history, only: %i[show]
 
+  # Checks if user is allowed to view a grading history
   def action_allowed?
-    return true if ['Super-Administrator', 'Administrator'].include? current_role_name
-    check_type(params[:grade_type])
+    # admins and superadmins are always allowed
+    return true if current_user_has_admin_privileges?
+    # populate assignment fields
+    assignment_for_history(params[:grade_type])
+    # if not admin/superadmin, check permissions
     if @assignment.instructor_id == current_user.id
       true
     elsif TaMapping.exists?(ta_id: current_user.id, course_id: @assignment.course_id) &&
@@ -14,12 +19,16 @@ class GradingHistoriesController < ApplicationController
     end
   end
 
-
-  def check_type(type)
+  # populate the assignment fields according to type
+  def assignment_for_history(type)
+    # for a submission, the receiver is an AssignmentTeam
+    # use this AssignmentTeam to find the assignment
     if type.eql? "Submission"
       assignment_team = AssignmentTeam.find(params[:grade_receiver_id])
       @assignment = Assignment.find(assignment_team.parent_id)
     end
+    # for a review, the receiver is an AssignmentParticipant
+    # use this AssignmentParticipant to find the assignment
     if type.eql? "Review"
       participant_id = params[:participant_id]
       grade_receiver = AssignmentParticipant.find(participant_id)
@@ -27,6 +36,8 @@ class GradingHistoriesController < ApplicationController
     end
   end
 
+  # return all grading history entries for the assignment
+  # entries are returned in chronological order
   def index
     @grading_histories = GradingHistory.where(grade_receiver_id: params[:grade_receiver_id], grading_type: params[:grade_type]).reverse_order
   end

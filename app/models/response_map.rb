a991a1df1252e43f1fc70d90bd9bb3ce5e3e1bc7
Nicholas Himes <41524135+nnhimes@users.noreply.github.com>
2022-05-10 12:38:06 -0400

    Try to add belongs_to :assignment

--- app/models/response_map.rb
+++ app/models/response_map.rb
@@ -1,7 +1,8 @@
 class ResponseMap < ApplicationRecord
   extend Scoring
   has_many :response, foreign_key: 'map_id', dependent: :destroy, inverse_of: false
-  class_name: 'Participant', foreign_key: 'reviewer_id', inverse_of: false
+  belongs_to :reviewer, class_name: 'Participant', foreign_key: 'reviewer_id', inverse_of: false
+  belongs_to :assignment, class_name: 'Assignment', foreign_key: 'reviewed_object_id', inverse_of: false
 
   def map_id
     id

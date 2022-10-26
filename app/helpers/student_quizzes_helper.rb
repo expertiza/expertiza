6ee407d30239192d982355fa466dc97094fb5742
subodhgujar <subodhsgujar@gmail.com>
2022-10-26 01:39:31 -0400

    Refactored calculate_score method into smaller methods

--- app/helpers/student_quizzes_helper.rb
+++ app/helpers/student_quizzes_helper.rb
@@ -1,47 +1,73 @@
 module StudentQuizzesHelper
     # the way 'answers' table store the results of quiz
-    def calculate_score(map, response)
-        questionnaire = Questionnaire.find(map.reviewed_object_id)
+    def calculate_score(participant_response, quiz_response)
         scores = []
         valid = true
-        questions = Question.where(questionnaire_id: questionnaire.id)
-        questions.each do |question|
-            score = 0
+        get_all_questions(participant_response).each do |question|
             correct_answers = QuizQuestionChoice.where(question_id: question.id, iscorrect: true)
             ques_type = question.type
             if ques_type.eql? 'MultipleChoiceCheckbox'
-                if params[question.id.to_s].nil?
-                    valid = false
-                else
-                    params[question.id.to_s].each do |choice|
-                        # loop the quiz taker's choices and see if 1)all the correct choice are checked and 2) # of quiz taker's choice matches the # of the correct choices
-                        correct_answers.each do |correct|
-                            score += 1 if choice.eql? correct.txt
-                        end
-                    end
-                    score = score == correct_answers.count && score == params[question.id.to_s].count ? 1 : 0
-                    # for MultipleChoiceCheckbox, score =1 means the quiz taker have done this question correctly, not just make select this choice correctly.
-                    params[question.id.to_s].each do |choice|
-                        new_score = Answer.new comments: choice, question_id: question.id, response_id: response.id, answer: score
-                        valid = false unless new_score.valid?
-                        scores.push(new_score)
-                    end
-                end
+                valid = is_valid_checkbox(params, question, valid)
+                valid = score_multiple_choice_checkbox(correct_answers, question, params, valid, scores, quiz_response)
             else # TrueFalse and MultipleChoiceRadio
-                correct_answer = correct_answers.first
-                score = correct_answer.txt == params[question.id.to_s] ? 1 : 0
-                new_score = Answer.new comments: params[question.id.to_s], question_id: question.id, response_id: response.id, answer: score
-                valid = false if new_score.nil? || new_score.comments.nil? || new_score.comments.empty?
-                scores.push(new_score)
+                valid = score_multiple_choice_radio(correct_answers, params, question, scores, quiz_response, valid)
             end
         end
+        save_scores_if_valid(valid, scores, quiz_response,participant_response)
+    end
+
+    def save_scores_if_valid(valid, scores, quiz_response,participant_response)
         if valid
             scores.each(&:save)
-            redirect_to controller: 'student_quizzes', action: 'finished_quiz', map_id: map.id
+            redirect_to controller: 'student_quizzes', action: 'finished_quiz', map_id: participant_response.id
         else
-            response.destroy
+            quiz_response.destroy
             flash[:error] = 'Please answer every question.'
-            redirect_to action: :take_quiz, assignment_id: params[:assignment_id], questionnaire_id: questionnaire.id, map_id: map.id
+            questionnaire = Questionnaire.find(participant_response.reviewed_object_id)
+            redirect_to action: :take_quiz, assignment_id: params[:assignment_id], questionnaire_id: questionnaire.id, map_id: participant_response.id
         end
+    end
+
+    def get_all_questions(participant_response)
+        questionnaire = Questionnaire.find(participant_response.reviewed_object_id)
+        Question.where(questionnaire_id: questionnaire.id)
+    end
+
+    def is_valid_checkbox(params, question, valid)
+        if params[question.id.to_s].nil?
+            valid = false;
+        end
+        valid
+    end
+
+    def score_multiple_choice_checkbox(correct_answers, question, params, valid, scores, quiz_response)
+        score = 0
+        params[question.id.to_s].each do |choice|
+            # loop the quiz taker's choices and see if 1)all the correct choice are checked and 2) # of quiz taker's choice matches the # of the correct choices
+            correct_answers.each do |correct|
+                score += 1 if choice.eql? correct.txt
+            end
+        end
+        score = score == correct_answers.count && score == params[question.id.to_s].count ? 1 : 0
+        # for MultipleChoiceCheckbox, score =1 means the quiz taker have done this question correctly, not just make select this choice correctly.
+        score_checkbox(scores, params, question, valid, quiz_response, score)
+    end
+
+    def score_checkbox(scores, params, question, valid, quiz_response, score)
+        params[question.id.to_s].each do |choice|
+            new_score = Answer.new comments: choice, question_id: question.id, response_id: quiz_response.id, answer: score
+            valid = false unless new_score.valid?
+            scores.push(new_score)
+        end
+        valid
+    end
+
+    def score_multiple_choice_radio(correct_answers, params, question, scores, quiz_response, valid)
+        correct_answer = correct_answers.first
+        score = correct_answer.txt == params[question.id.to_s] ? 1 : 0
+        new_score = Answer.new comments: params[question.id.to_s], question_id: question.id, response_id: quiz_response.id, answer: score
+        valid = false if new_score.nil? || new_score.comments.nil? || new_score.comments.empty?
+        scores.push(new_score)
+        valid
     end
 end

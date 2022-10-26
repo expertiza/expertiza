a90b1aada9878c7cdf7319dd022432cca8eadd2f
Colin O'Dowd <cdodowdee@gmail.com>
2022-10-26 01:32:51 -0400

    Updating Rspec tests for update_password with PasswordReset deletion checks

--- spec/controllers/password_retrieval_controller_spec.rb
+++ spec/controllers/password_retrieval_controller_spec.rb
@@ -16,11 +16,11 @@ describe PasswordRetrievalController do
       @user.fullname = 'John Bumgardner'
       @user.name = 'Shubham'
       @user.save!
-      @password_retreival = PasswordReset.new
+      @password_retrieval = PasswordReset.new
       @local_token = 'some random string'
-      @password_retreival.token = @local_token
-      @password_retreival.user_email = 'example@example.edu'
-      @password_retreival.save!
+      @password_retrieval.token = @local_token
+      @password_retrieval.user_email = 'example@example.edu'
+      @password_retrieval.save!
       request_params = { user: { email: 'example@example.edu' } }
       post :send_password, params: request_params
       expect(PasswordReset.find_by(user_email: 'example@example.edu').token).not_to eq(@local_token)
@@ -40,10 +40,10 @@ describe PasswordRetrievalController do
   describe 'check if token is expired' do
     it 'checks when token is expired' do
       local_token = 'some random string'
-      @password_retreival = PasswordReset.new
-      @password_retreival.token = Digest::SHA1.hexdigest(local_token)
-      @password_retreival.user_email = 'example@example.edu'
-      @password_retreival.save!
+      @password_retrieval = PasswordReset.new
+      @password_retrieval.token = Digest::SHA1.hexdigest(local_token)
+      @password_retrieval.user_email = 'example@example.edu'
+      @password_retrieval.save!
       request_params = { token: local_token }
       Timecop.freeze(Time.zone.today + 2.days) do
         get :check_token_validity, params: request_params
@@ -54,10 +54,10 @@ describe PasswordRetrievalController do
     it 'checks when token does not exist' do
       local_token = 'some random strin'
       local_token_sent_as_parameter = 'randome some'
-      @password_retreival = PasswordReset.new
-      @password_retreival.token = Digest::SHA1.hexdigest(local_token)
-      @password_retreival.user_email = 'example@example.edu'
-      @password_retreival.save!
+      @password_retrieval = PasswordReset.new
+      @password_retrieval.token = Digest::SHA1.hexdigest(local_token)
+      @password_retrieval.user_email = 'example@example.edu'
+      @password_retrieval.save!
       request_params = { token: local_token_sent_as_parameter }
       get :check_token_validity, params: request_params
       expect(response).to render_template 'password_retrieval/forgotten'
@@ -65,12 +65,12 @@ describe PasswordRetrievalController do
 
     it 'checks when token is not expired' do
       local_token = 'some random string'
-      @password_retreival = PasswordReset.new
-      @password_retreival.token = Digest::SHA1.hexdigest(local_token)
-      @password_retreival.user_email = 'example@example.edu'
-      @password_retreival.save!
+      @password_retrieval = PasswordReset.new
+      @password_retrieval.token = Digest::SHA1.hexdigest(local_token)
+      @password_retrieval.user_email = 'example@example.edu'
+      @password_retrieval.save!
       request_params = { token: local_token }
-      Timecop.freeze(@password_retreival.updated_at + 2.hours) do
+      Timecop.freeze(@password_retrieval.updated_at + 2.hours) do
         get :check_token_validity, params: request_params
         expect(response).to render_template 'password_retrieval/reset_password'
       end
@@ -84,27 +84,15 @@ describe PasswordRetrievalController do
       @user.fullname = 'John Doe'
       @user.name = 'classman'
       @user.save!
-      @password_retreival = PasswordReset.new
-      @password_retreival.user_email = 'example@example.edu'
-      @password_retreival.save!
+      @password_retrieval = PasswordReset.new
+      @password_retrieval.user_email = 'example@example.edu'
+      @password_retrieval.save!
       request_params = { reset: { password: 'AAAAAAAAA123!!', repassword: 'AAAAAAAAA123!!', email: 'example@example.edu' } }
       post :update_password, params: request_params
+      expect(PasswordReset.where(user_email: 'example@example.edu')).not_to exist
       expect(response).to redirect_to '/'
     end
 
-    it 'checks if password reset is deleted' do
-        @user = User.new
-        @user.email = 'example@example.edu'
-        @user.fullname = 'John Doe'
-        @user.name = 'classman'
-        @user.save!
-        @password_retreival = PasswordReset.new
-        @password_retreival.user_email = 'example@example.edu'
-        @password_retreival.save!
-        request_params = { reset: { password: 'AAAAAAAAA123!!', repassword: 'AAAAAAAAA123!!', email: 'example@example.edu' } }
-        post :update_password, params: request_params
-        expect(PasswordReset.where(user_email: 'example@example.edu')).not_to exist
-    end
 
     it 'checks if password and repassword do not match' do
         @user = User.new
@@ -112,11 +100,12 @@ describe PasswordRetrievalController do
         @user.fullname = 'John Doe'
         @user.name = 'classman'
         @user.save!
-        @password_retreival = PasswordReset.new
-        @password_retreival.user_email = 'example@example.edu'
-        @password_retreival.save!
+        @password_retrieval = PasswordReset.new
+        @password_retrieval.user_email = 'example@example.edu'
+        @password_retrieval.save!
         request_params = { reset: { password: 'BAAAAAAAAA123!!', repassword: 'AAAAAAAAA123!!', email: 'example@example.edu' } }
         post :update_password, params: request_params
+        expect(PasswordReset.where(user_email: 'example@example.edu')).to exist
         expect(response).to render_template 'password_retrieval/reset_password'
         expect(flash[:error]).to be_present      
     end

eb9353fccfef245dab13915e80616bc46cca723a
Deep Manoj Mehta <dmmehta2@vclvm176-53.vcl.ncsu.edu>
2022-10-26 18:13:54 -0400

    Remove unused functions for create folder and move, rename file

--- app/controllers/submitted_content_controller.rb
+++ app/controllers/submitted_content_controller.rb
@@ -182,12 +182,6 @@ class SubmittedContentController < ApplicationController
     @current_folder.name = FileHelper.sanitize_folder(params[:current_folder][:name]) if params[:current_folder]
     if params[:faction][:delete]
       delete_selected_files
-    elsif params[:faction][:rename]
-      rename_selected_file
-    elsif params[:faction][:move]
-      move_selected_file
-    elsif params[:faction][:create]
-      create_new_folder
     end
     redirect_to action: 'edit', id: @participant.id
   end
@@ -230,31 +224,6 @@ class SubmittedContentController < ApplicationController
     base.split('.')[base.split('.').size - 1] if base.split('.').size > 1
   end
 
-  def move_selected_file
-    old_filename = params[:directories][params[:chk_files]] + '/' + params[:filenames][params[:chk_files]]
-    newloc = @participant.dir_path
-    newloc += '/'
-    newloc += params[:faction][:move]
-    begin
-        FileHelper.move_file(old_filename, newloc)
-        flash[:note] = "The file was successfully moved from \"/#{params[:filenames][params[:chk_files]]}\" to \"/#{params[:faction][:move]}\""
-    rescue StandardError => e
-      flash[:error] = 'There was a problem moving the file: ' + e.message
-      end
-  end
-
-  def rename_selected_file
-    old_filename = params[:directories][params[:chk_files]] + '/' + params[:filenames][params[:chk_files]]
-    new_filename = params[:directories][params[:chk_files]] + '/' + FileHelper.sanitize_filename(params[:faction][:rename])
-    begin
-      raise "A file already exists in this directory with the name \"#{params[:faction][:rename]}\"" if File.exist?(new_filename)
-
-      File.send('rename', old_filename, new_filename)
-    rescue StandardError => e
-      flash[:error] = 'There was a problem renaming the file: ' + e.message
-    end
-  end
-
   def delete_selected_files
     filename = params[:directories][params[:chk_files]] + '/' + params[:filenames][params[:chk_files]]
     FileUtils.rm_r(filename)
@@ -267,18 +236,6 @@ class SubmittedContentController < ApplicationController
                             assignment_id: assignment.try(:id),
                             operation: 'Remove File')
     ExpertizaLogger.info LoggerMessage.new(controller_name, @participant.name, 'The selected file has been deleted.', request)
-  end
-
-  def create_new_folder
-    newloc = @participant.dir_path
-    newloc += '/'
-    newloc += params[:faction][:create]
-    begin
-      FileHelper.create_directory_from_path(newloc)
-      flash[:note] = "The directory #{params[:faction][:create]} was created."
-    rescue StandardError => e
-      flash[:error] = e.message
-    end
   end
 
   # if one team do not hold a topic (still in waitlist), they cannot submit their work.

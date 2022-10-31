04e90d23146df7ef735a4e9f37152e753cbf1fad
BirdLogics <47367734+BirdLogics@users.noreply.github.com>
2022-10-31 19:34:26 -0400

    Clarify comment

--- app/models/team.rb
+++ app/models/team.rb
@@ -269,9 +269,10 @@ class Team < ApplicationRecord
     end
   end
 
-  # E2254 : This method copies a list of teams to another collection. It is mainly
-  # used in the Team model for copy_to_assignment and bequeath_all for the purpose of copying
-  # teams from assignments to courses and vice versa.
+  # E2254 : This method copies a list of teams to another collection.
+  # It is used in the Team model with copy_to_assignment and bequeath_all
+  # to copy teams from assignments to courses and vice versa.
+  # The teams parameter is either a list of CourseTeams or AssignmentTeams.
   def self.copy_teams_to_collection(teams, recipient_collection_id)
     teams.each do |team|
       team.copy(recipient_collection_id)

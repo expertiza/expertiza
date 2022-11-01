e946533235846a853ee42908b15e75d628552f9e
Colin O'Dowd <cdodowdee@gmail.com>
2022-11-01 11:58:17 -0400

    Added factories to spec test files

--- spec/controllers/password_retrieval_controller_spec.rb
+++ spec/controllers/password_retrieval_controller_spec.rb
@@ -1,39 +1,27 @@
 describe PasswordRetrievalController do
   describe 'password reset' do
     it 'create new entry in password_resets table' do
-      @user = User.new
-      @user.email = 'example@example.edu'
-      @user.fullname = 'John Bumgardner'
-      @user.name = 'ex'
+      @user = FactoryBot.create(:user)
       @user.save!
-      request_params = { user: { email: 'example@example.edu' } }
+      request_params = { user: { email: @user.email } }
       post :send_password, params: request_params
-      expect(PasswordReset.where(user_email: 'example@example.edu')).to exist
+      expect(PasswordReset.where(user_email: @user.email)).to exist
     end
     it 'modifies the token in password_resets_table' do
-      @user = User.new
-      @user.email = 'example@example.edu'
-      @user.fullname = 'John Bumgardner'
-      @user.name = 'Shubham'
+      @user = FactoryBot.create(:user)
       @user.save!
-      @password_retrieval = PasswordReset.new
-      @local_token = 'some random string'
-      @password_retrieval.token = @local_token
-      @password_retrieval.user_email = 'example@example.edu'
+      @password_retrieval = FactoryBot.build(:password_reset, user_email: @user.email)
       @password_retrieval.save!
-      request_params = { user: { email: 'example@example.edu' } }
+      request_params = { user: { email: @user.email } }
       post :send_password, params: request_params
-      expect(PasswordReset.find_by(user_email: 'example@example.edu').token).not_to eq(@local_token)
+      expect(PasswordReset.find_by(user_email: @password_retrieval.user_email).token).not_to eq(@password_retrieval.token)
     end
     it 'if no user no entry is created' do
-      @user = User.new
-      @user.email = 'aexample@example.edu'
-      @user.name = 'Shubham'
-      @user.fullname = 'John Bumgardner'
+      @user = FactoryBot.create(:user)
       @user.save!
-      request_params = { user: { email: 'example@example.edu' } }
+      request_params = { user: { email: 'notafactoryboy@ncsu.edu' } }
       post :send_password, params: request_params
-      expect(PasswordReset.where(user_email: 'example@example.edu')).not_to exist
+      expect(PasswordReset.where(user_email: @user.email)).not_to exist
     end
     it 'if user in request param is nil flash error' do
       request_params = { user: { email: nil } }
@@ -51,12 +39,10 @@ describe PasswordRetrievalController do
 
   describe 'check if token is expired' do
     it 'checks when token is expired' do
-      local_token = 'some random string'
-      @password_retrieval = PasswordReset.new
-      @password_retrieval.token = Digest::SHA1.hexdigest(local_token)
-      @password_retrieval.user_email = 'example@example.edu'
+      non_encrypted_token = 'factory_bot_token'
+      @password_retrieval = FactoryBot.build(:password_reset, token: Digest::SHA1.hexdigest(non_encrypted_token))
       @password_retrieval.save!
-      request_params = { token: local_token }
+      request_params = { token: non_encrypted_token }
       Timecop.freeze(Time.zone.today + 2.days) do
         get :check_token_validity, params: request_params
         expect(response).to render_template 'password_retrieval/forgotten'
@@ -64,25 +50,21 @@ describe PasswordRetrievalController do
     end
 
     it 'checks when token does not exist' do
-      local_token = 'some random strin'
-      local_token_sent_as_parameter = 'randome some'
-      @password_retrieval = PasswordReset.new
-      @password_retrieval.token = Digest::SHA1.hexdigest(local_token)
-      @password_retrieval.user_email = 'example@example.edu'
+      non_encrypted_token = 'factory_bot_token'
+      @password_retrieval = FactoryBot.build(:password_reset, token: Digest::SHA1.hexdigest(non_encrypted_token))
       @password_retrieval.save!
-      request_params = { token: local_token_sent_as_parameter }
+      request_params = { token: nil }
       get :check_token_validity, params: request_params
       expect(response).to render_template 'password_retrieval/forgotten'
     end
 
     it 'checks when token is not expired' do
-      local_token = 'some random string'
-      @password_retrieval = PasswordReset.new
-      @password_retrieval.token = Digest::SHA1.hexdigest(local_token)
-      @password_retrieval.user_email = 'example@example.edu'
+      hours_until_expiration = 2
+      non_encrypted_token = 'factory_bot_token'
+      @password_retrieval = FactoryBot.build(:password_reset, token: Digest::SHA1.hexdigest(non_encrypted_token))
       @password_retrieval.save!
-      request_params = { token: local_token }
-      Timecop.freeze(@password_retrieval.updated_at + 2.hours) do
+      request_params = { token: non_encrypted_token }
+      Timecop.freeze(@password_retrieval.updated_at + hours_until_expiration.hours) do
         get :check_token_validity, params: request_params
         expect(response).to render_template 'password_retrieval/reset_password'
       end
@@ -91,50 +73,39 @@ describe PasswordRetrievalController do
 
   describe 'check if password updated' do
     it 'check if password and repassword do match' do
-      @user = User.new
-      @user.email = 'example@example.edu'
-      @user.fullname = 'John Doe'
-      @user.name = 'classman'
+      password = "factorybotpassword"
+      @user = FactoryBot.create(:user)
       @user.save!
-      @password_retrieval = PasswordReset.new
-      @password_retrieval.user_email = 'example@example.edu'
-      @password_retrieval.token = 'random_token'
+      @password_retrieval = FactoryBot.create(:password_reset)
       @password_retrieval.save!
-      request_params = { reset: { password: 'AAAAAAAAA123!!', repassword: 'AAAAAAAAA123!!', email: 'example@example.edu' } }
+      request_params = { reset: { password: password, repassword: password, email: @user.email } }
       post :update_password, params: request_params
-      expect(PasswordReset.where(user_email: 'example@example.edu')).not_to exist
+      expect(PasswordReset.where(user_email: @password_retrieval.user_email)).not_to exist
       expect(response).to redirect_to '/'
     end
 
     it 'check if password validation fails' do
-      @user = User.new
-      @user.email = 'example@example.edu'
-      @user.fullname = 'John Doe'
-      @user.name = 'classman'
+      invalid_password = "."
+      @user = FactoryBot.create(:user)
       @user.save!
-      @password_retrieval = PasswordReset.new
-      @password_retrieval.user_email = 'example@example.edu'
-      @password_retrieval.token = 'random_token'
+      @password_retrieval = FactoryBot.create(:password_reset)
       @password_retrieval.save!
-      request_params = { reset: { password: 'A', repassword: 'A', email: 'example@example.edu' } }
+      request_params = { reset: { password: invalid_password, repassword: invalid_password, email: @user.email } }
       post :update_password, params: request_params
       expect(response).to redirect_to '/'
       expect(flash[:error]).to be_present
     end
 
     it 'checks if password and repassword do not match' do
-      @user = User.new
-      @user.email = 'example@example.edu'
-      @user.fullname = 'John Doe'
-      @user.name = 'classman'
+      password = "factorybotpassword"
+      repassword = "notafactorybotpassword"
+      @user = FactoryBot.create(:user)
       @user.save!
-      @password_retrieval = PasswordReset.new
-      @password_retrieval.user_email = 'example@example.edu'
-      @password_retrieval.token = 'random_token'
+      @password_retrieval = FactoryBot.create(:password_reset)
       @password_retrieval.save!
-      request_params = { reset: { password: 'BAAAAAAAAA123!!', repassword: 'AAAAAAAAA123!!', email: 'example@example.edu' } }
+      request_params = { reset: { password: password, repassword: repassword, email: @user.email } }
       post :update_password, params: request_params
-      expect(PasswordReset.where(user_email: 'example@example.edu')).to exist
+      expect(PasswordReset.where(user_email: @password_retrieval.user_email)).to exist
       expect(response).to render_template 'password_retrieval/reset_password'
       expect(flash[:error]).to be_present
     end

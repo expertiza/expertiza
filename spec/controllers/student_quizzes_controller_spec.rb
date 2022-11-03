3ffc633fb406e015ba916e2d5a632b160e7c66d9
subodhgujar <subodhsgujar@gmail.com>
2022-11-03 09:12:35 -0400

    Adding comments

--- spec/controllers/student_quizzes_controller_spec.rb
+++ spec/controllers/student_quizzes_controller_spec.rb
@@ -1,25 +1,27 @@
 describe StudentQuizzesController do
-    let(:assignment_participant) do
+    let(:assignment_participant) do  #Creating participants for assignment
         build(:assignment_participant, id: 36226, can_submit: true, can_review: true, user_id: 5, parent_id: 848)
     end
-    let(:assignment) do
+    let(:assignment) do #Creating assignment
         build(:assignment, id: 848, name: "Test 1", directory_path: "Test_1", submitter_count: 0, course_id: 68, instructor_id: 6, private: true, reviews_visible_to_all: false, num_reviewers: 0)
     end
-    let(:response) do
+    let(:response) do  #creating response for assignment
         build(:response, id: 96252, map_id: 130389, additional_comment: nil)
     end
-    let(:question) do
+    let(:question) do  #creating quiz question
         build(:question, id: 5841, txt: "Question - 1", weight: 1, questionnaire_id: 572 )
     end
-    let(:quiz_questionnaire) { QuizQuestionnaire.new }
+    let(:quiz_questionnaire) { QuizQuestionnaire.new }  #creating questionnaire for assignment question
     let(:quiz_response_map) { build(:quiz_response_map, id: 130389, quiz_questionnaire: quiz_questionnaire, reviewee_id: 28012, reviewed_object_id: 572) }
-    let(:student) do
+    let(:student) do  #creating student
         build(:student, id: 5)
     end
-    let(:student1) do
+    let(:student1) do #creating student1
         build(:student, id: 55)
     end
-    let(:admin) { build(:admin, id: 7) }
+    let(:admin) {
+        build(:admin, id: 7) #creating admin
+    }
 
     describe "#action_allowed?" do
         it "when the current user is admin" do

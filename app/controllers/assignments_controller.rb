585a96b2ad5f26a2dffd45ee7f8a170c32c5050d
Vyas Radhakrishnan Cholayil <vrcholay@vclv99-69.hpc.ncsu.edu>
2022-11-09 14:22:04 -0500

    Refactored create method to split tasks into different methods and renamed a variable to correctly reflect its purpose.

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -31,34 +31,44 @@ class AssignmentsController < ApplicationController
       dir_path = assignment_form_params[:assignment][:directory_path]
       find_existing_directory = Assignment.find_by(directory_path: dir_path, course_id: @assignment_form.assignment.course_id)
       if !find_existing_assignment && !find_existing_directory && @assignment_form.save # No existing names/directories
-        @assignment_form.create_assignment_node
-        exist_assignment = Assignment.find(@assignment_form.assignment.id)
-        assignment_form_params[:assignment][:id] = exist_assignment.id.to_s
-        if assignment_form_params[:assignment][:directory_path].blank? # No existing assignment for assignment form
-          assignment_form_params[:assignment][:directory_path] = "assignment_#{assignment_form_params[:assignment][:id]}"
-        end
-        assignment_form_params[:assignment_questionnaire] = assign_questionnaire_array(exist_assignment)
-        assignment_form_params[:due_date] = assign_due_date_array(exist_assignment)
-        @assignment_form.update(assignment_form_params, current_user)
-        aid = Assignment.find(@assignment_form.assignment.id).id
-        ExpertizaLogger.info "Assignment created: #{@assignment_form.as_json}"
-        redirect_to edit_assignment_path aid
-        undo_link("Assignment \"#{@assignment_form.assignment.name}\" has been created successfully. ")
-        return
+        assignment_creation_success
+        # return
       else
-        flash[:error] = 'Failed to create assignment.'
-        if find_existing_assignment
-          flash[:error] << '<br>  ' + @assignment_form.assignment.name + ' already exists as an assignment name'
-        end
-        if find_existing_directory
-          flash[:error] << '<br>  ' + dir_path + ' already exists as a submission directory name'
-        end
-        redirect_to '/assignments/new?private=1'
+        assignemnt_creation_failure(find_existing_assignment, dir_path, find_existing_directory)
       end
     else
       render 'new'
       undo_link("Assignment \"#{@assignment_form.assignment.name}\" has been created successfully. ")
     end
+  end
+
+  # create a new assignment via the assigment form if the new assignment name and directory doesn't exist
+  def assignment_creation_success
+    @assignment_form.create_assignment_node
+    exist_assignment = Assignment.find(@assignment_form.assignment.id)
+    assignment_form_params[:assignment][:id] = exist_assignment.id.to_s
+    if assignment_form_params[:assignment][:directory_path].blank? # No existing assignment for assignment form
+      assignment_form_params[:assignment][:directory_path] = "assignment_#{assignment_form_params[:assignment][:id]}"
+    end
+    assignment_form_params[:assignment_questionnaire] = assign_questionnaire_array(exist_assignment)
+    assignment_form_params[:due_date] = assign_due_date_array(exist_assignment)
+    @assignment_form.update(assignment_form_params, current_user)
+    assignment_id = Assignment.find(@assignment_form.assignment.id).id
+    ExpertizaLogger.info "Assignment created: #{@assignment_form.as_json}"
+    redirect_to edit_assignment_path assignment_id
+    undo_link("Assignment \"#{@assignment_form.assignment.name}\" has been created successfully. ")
+  end
+
+  # raises a flash error messgae when it fails to create an assignment and redirect to assignement/new page 
+  def assignemnt_creation_failure(existing_assignment, dir_path, existing_directory)
+    flash[:error] = 'Failed to create assignment.'
+    if existing_assignment
+      flash[:error] << '<br>  ' + @assignment_form.assignment.name + ' already exists as an assignment name'
+    end
+    if existing_directory
+      flash[:error] << '<br>  ' + dir_path + ' already exists as a submission directory name'
+    end
+    redirect_to '/assignments/new?private=1'
   end
 
   # edits an assignment's deadlines and assigned rubrics

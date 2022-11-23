0161be1f661e086f3241a369369c5cb9b353e0d6
Pradyumna Khawas <pradyumna.khawas@gmail.com>
2022-11-23 18:09:03 -0500

    Added copy_to_team to SubmissionRecord #4

--- app/models/submission_record.rb
+++ app/models/submission_record.rb
@@ -4,4 +4,13 @@ class SubmissionRecord < ApplicationRecord
   validates :team_id, presence: true
   validates :user, presence: true
   validates :assignment_id, presence: true
+
+  # copies submission record to a team
+  def copy_to_team(team)
+    new_submission_record = dup
+    new_submission_record.team_id = team.id
+    new_submission_record.assignment_id = team.parent_id
+    new_submission_record.save
+    new_submission_record
+  end
 end

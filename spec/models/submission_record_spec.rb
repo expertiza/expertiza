bce1c7767ceda464217c0d248b5fd9d70719372a
Pradyumna Khawas <pradyumna.khawas@gmail.com>
2022-11-23 18:11:22 -0500

    Added test for copy_to_team #4

--- spec/models/submission_record_spec.rb
+++ spec/models/submission_record_spec.rb
@@ -1,6 +1,12 @@
 # These rpec will test the validity of the SubmissionRecord model. It tests for
 # valid team_id, operation, user, assignment id.
 describe SubmissionRecord do
+  let(:team) { build(:team, id: 1, name: 'myTeam', parent_id: 1) }
+  let(:team2) { build(:team, id: 2, name: 'myTeam2', parent_id: 2) }
+  let(:assignment) { build(:assignment, id: 1, name: 'no assgt') }
+  let(:assignment2) { build(:assignment, id: 2, name: 'no assgt 2') }
+  let(:submission_record) { build(:submission_record, id: 1, assignment_id: 1, team_id: 1) }
+
   it 'is invalid without a team id' do
     expect(build(:submission_record, team_id: nil)).to_not be_valid
   end
@@ -12,5 +18,15 @@ describe SubmissionRecord do
   end
   it 'is invalid without an assignment id' do
     expect(build(:submission_record, assignment_id: nil)).to_not be_valid
+  end
+
+  describe '#copy_to_team' do
+    it 'should copy submission record to a team' do
+      new_submission_record = submission_record.copy_to_team(team2)
+      puts new_submission_record.team_id
+      puts new_submission_record.assignment_id
+      expect(new_submission_record.assignment_id).to eq(assignment2.id)
+      expect(new_submission_record.team_id).to eq(team2.id)
+    end
   end
 end

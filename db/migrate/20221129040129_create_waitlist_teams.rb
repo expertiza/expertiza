07c39749d80b6749460ac2313fe6c0f48fb2c8ae
nagarajumadamshetti <nagarajumadamshetti@gmail.com>
2022-11-28 23:02:28 -0500

    add migration to add waitlist teams table

---
+++ db/migrate/20221129040129_create_waitlist_teams.rb
@@ -0,0 +1,29 @@
+class CreateWaitlistTeams < ActiveRecord::Migration[5.1]
+
+  def self.up
+    create_table 'waitlist_teams' do |t|
+      t.column 'team_id', :integer
+      t.column 'topic_id', :integer
+      t.column 'created_at', :datetime
+    end
+
+    add_index 'waitlist_teams', ['team_id'], name: 'fk_waitlist_teams'
+
+    execute "alter table waitlist_teams
+                 add constraint fk_waitlist_teams
+                 foreign key (team_id) references teams(id)"
+
+    add_index 'waitlist_teams', ['topic_id'], name: 'fk_waitlist_teams_sign_up_topics'
+
+    execute "alter table waitlist_teams
+               add constraint fk_waitlist_teams_sign_up_topics
+               foreign key (topic_id) references sign_up_topics(id)"
+    
+    add_index 'waitlist_teams', ["team_id", "topic_id"], unique: truehttps://github.com/hss69017/expertiza-E2281.git
+
+  end
+
+  def self.down
+    drop_table 'waitlist_teams'
+  end
+end

7b36c1d824ec6cff47a751602e3a80d1096e3217
Sahithi Ammana <sammana@vclvm176-64.vcl.ncsu.edu>
2022-11-30 01:05:50 -0500

    Adding partials for student_teams view

--- app/models/assignment_team.rb
+++ app/models/assignment_team.rb
@@ -213,7 +213,7 @@ class AssignmentTeam < Team
     return nil if participant.nil?
 
     team = nil
-    teams_users = TeamsUser.where(user_id: participant.user_id)
+    teams_users = TeamsUser.where(user_id: participant.user_id).or(TeamsParticipant.where(participant_id: participant.id))
     return nil unless teams_users
 
     teams_users.each do |teams_user|

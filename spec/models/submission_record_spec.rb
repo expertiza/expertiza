434a12b065bbc4b7fc011bd1c39eebcfdc1cdfe0
Pradyumna Khawas <pradyumna.khawas@gmail.com>
2022-12-01 18:04:05 -0500

    Removed debug print statements

--- spec/models/submission_record_spec.rb
+++ spec/models/submission_record_spec.rb
@@ -23,8 +23,7 @@ describe SubmissionRecord do
   describe '#copy_to_team' do
     it 'should copy submission record to a team' do
       new_submission_record = submission_record.copy_to_team(team2)
-      puts new_submission_record.team_id
-      puts new_submission_record.assignment_id
+
       expect(new_submission_record.assignment_id).to eq(assignment2.id)
       expect(new_submission_record.team_id).to eq(team2.id)
     end

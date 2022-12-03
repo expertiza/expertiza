cdf851076172d09ed332cf636a51c60e3af55a63
subodhgujar <subodhsgujar@gmail.com>
2022-12-03 14:37:58 -0500

    Implemented Chain Of Responsibility pattern to calculate score.

--- app/helpers/student_quizzes_helper.rb
+++ app/helpers/student_quizzes_helper.rb
@@ -5,17 +5,12 @@ module StudentQuizzesHelper
         valid = true
         # Get the entire questionnaire assigned to 'participant_response'
         get_all_questions(participant_response).each do |question|
-            correct_answers = QuizQuestionChoice.where(question_id: question.id, iscorrect: true) # Get correct answer for each question in the questionnaire
-            ques_type = question.type
-            # Update valid flag based on the question type i.e. Radio or Multiple Checkbox or True or False.
-            if ques_type.eql? 'MultipleChoiceCheckbox'
-                valid = is_valid_checkbox(params, question, valid)
-                valid = score_multiple_choice_checkbox(correct_answers, question, params, valid, scores, quiz_response)
-            else # TrueFalse and MultipleChoiceRadio
-                valid = score_multiple_choice_radio(correct_answers, params, question, scores, quiz_response, valid)
-            end
+            # Get correct answer for each question in the questionnaire
+            correct_answers = QuizQuestionChoice.where(question_id: question.id, iscorrect: true)
+
+            #Using Chain Of Responsibility pattern to calculate score
+            valid = score_multiple_choice_checkbox(correct_answers, question, params, valid, scores, quiz_response)
         end
-        #save_scores_if_valid?(valid, scores, quiz_response,participant_response)
         score_response = Hash.new
         score_response["valid"] = valid
         score_response["scores"] = scores
@@ -23,18 +18,6 @@ module StudentQuizzesHelper
         score_response
     end
 
-#     def save_scores_if_valid?(valid, scores, quiz_response,participant_response)
-#         if valid
-#             scores.each(&:save)
-#             redirect_to controller: 'student_quizzes', action: 'finished_quiz', map_id: participant_response.id
-#         else
-#             quiz_response.destroy
-#             flash[:error] = 'Please answer every question.'
-#             questionnaire = Questionnaire.find(participant_response.reviewed_object_id)
-#             redirect_to action: :take_quiz, assignment_id: params[:assignment_id], questionnaire_id: questionnaire.id, map_id: participant_response.id
-#         end
-#     end
-    
     # Return all the questions in the questionnaire which is fetched via the review done by the student
     def get_all_questions(participant_response)
         questionnaire = Questionnaire.find(participant_response.reviewed_object_id)
@@ -50,6 +33,10 @@ module StudentQuizzesHelper
     end
 
     def score_multiple_choice_checkbox(correct_answers, question, params, valid, scores, quiz_response)
+        unless question.type.eql? 'MultipleChoiceCheckbox'
+            return score_single_choice_radio(correct_answers, params, question, scores, quiz_response, valid)
+        end
+        valid = is_valid_checkbox(params, question, valid)
         score = 0
         params[question.id.to_s].each do |choice|
             # loop the quiz taker's choices and see if 1)all the correct choice are checked and 2) # of quiz taker's choice matches the # of the correct choices
@@ -72,7 +59,7 @@ module StudentQuizzesHelper
         valid
     end
 
-    def score_multiple_choice_radio(correct_answers, params, question, scores, quiz_response, valid)
+    def score_single_choice_radio(correct_answers, params, question, scores, quiz_response, valid)
         correct_answer = correct_answers.first
         score = correct_answer.txt == params[question.id.to_s] ? 1 : 0    #checking whether the answser is correct and assigning score 0 or 1
         new_score = Answer.new comments: params[question.id.to_s], question_id: question.id, response_id: quiz_response.id, answer: score

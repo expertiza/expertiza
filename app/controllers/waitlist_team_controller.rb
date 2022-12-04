3b8e3cf6dff1ec6e47227e53a1360da7cfb03c8a
naydt01 <58788092+naydt01@users.noreply.github.com>
2022-12-04 18:34:14 -0500

    Add comments to waitlist team controller

--- app/controllers/waitlist_team_controller.rb
+++ app/controllers/waitlist_team_controller.rb
@@ -1,26 +1,24 @@
 class WaitlistTeamController < ApplicationController
   include AuthorizationHelper
 
-  # belongs_to :topic, class_name: 'SignUpTopic'
-  # belongs_to :team, class_name: 'Team'
-  
-  # validates :topic_id, :team_id, presence: true
-  # validates :topic_id, uniqueness: { scope: :team_id }
-
-
+  # Determines first team on waitlist based on date/time of entry
+  # into the database
   def self.first_team_in_waitlist_for_topic(topic_id)
     waitlisted_team_for_topic = WaitlistTeam.where(topic_id: topic_id).order("created_at ASC").first
     waitlisted_team_for_topic
   end
 
+  # Searches database for waitlist entries for team
   def self.team_has_any_waitlists?(team_id)
     WaitlistTeam.where(team_id: team_id).empty?
   end
 
+  # Searches database for waitlist entries for topic
   def self.topic_has_any_waitlists?(topic_id)
     WaitlistTeam.where(topic_id: topic_id).empty?
   end
 
+  # Deletes all waitlists for a team across all topics
   def self.delete_all_waitlists_for_team(team_id)
     waitlisted_topics_for_team = get_all_waitlists_for_team team_id
     if !waitlisted_topics_for_team.nil?
@@ -31,6 +29,8 @@ class WaitlistTeamController < ApplicationController
     return true
   end
 
+  # Deletes all waitlisted teams on a specific topic
+  # Removing all entries from the database
   def self.delete_all_waitlists_for_topic(topic_id)
     waitlisted_teams_for_topic = get_all_waitlists_for_topic topic_id
     if !waitlisted_teams_for_topic.nil?
@@ -41,14 +41,17 @@ class WaitlistTeamController < ApplicationController
     return true
   end
 
+  # Returns list of all waitlist topics for a team
   def self.get_all_waitlists_for_team(team_id)
     WaitlistTeam.joins(:topic).select('waitlist_teams.id, topic_name, team_id, topic_id, created_at').where(team_id: team_id)
   end
 
+  # Returns list of all waitlisted teams for a topic
   def self.get_all_waitlists_for_topic(topic_id)
     WaitlistTeam.where(topic_id: topic_id)
   end
 
+  # Searches through all topics on an assignment to determine waitlist sizes
   def self.count_all_waitlists_per_topic_per_assignment(assignment_id)
     list_of_topic_waitlist_counts = []
     assignment_topics = Assignment.find(assignment_id).sign_up_topics
@@ -58,6 +61,7 @@ class WaitlistTeamController < ApplicationController
     list_of_topic_waitlist_counts
   end
 
+  # Searches database for all waitlisted teams for specific assignment
   def self.find_waitlisted_teams_for_assignment(assignment_id, ip_address = nil)
     waitlisted_participants = WaitlistTeam.joins('INNER JOIN sign_up_topics ON waitlist_teams.topic_id = sign_up_topics.id')
                                           .select('waitlist_teams.id as id, sign_up_topics.id as topic_id, sign_up_topics.topic_name as name,
@@ -68,10 +72,13 @@ class WaitlistTeamController < ApplicationController
     waitlisted_participants
   end
 
+  # Simple database check to find if waitlisted team exists
   def self.check_team_waitlisted_for_topic(team_id, topic_id)
     WaitlistTeam.exists?(team_id: team_id, topic_id: topic_id)
   end
 
+  # Find team at top of queue and converts to SignedUpTeam
+  # Returns team as a SignedUpTeam so it can be moved in proper database
   def self.signup_first_waitlist_team(topic_id)
     sign_up_waitlist_team = nil
     ApplicationRecord.transaction do

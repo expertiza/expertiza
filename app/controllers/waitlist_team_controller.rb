a25ff6ab7120a2002ef34a85973bac6d92bff14d
naydt01 <58788092+naydt01@users.noreply.github.com>
2022-12-04 16:59:03 -0500

    Remove methods from controller

--- app/controllers/waitlist_team_controller.rb
+++ app/controllers/waitlist_team_controller.rb
@@ -5,29 +5,6 @@ class WaitlistTeamController < ApplicationController
   validates :topic_id, :team_id, presence: true
   validates :topic_id, uniqueness: { scope: :team_id }
 
-  def self.add_team_to_topic_waitlist(team_id, topic_id, user_id)
-    new_waitlist = WaitlistTeam.new
-    new_waitlist.topic_id = topic_id
-    new_waitlist.team_id = team_id
-    if new_waitlist.valid?
-      WaitlistTeam.create(topic_id: topic_id, team_id: team_id)
-    else
-      ExpertizaLogger.info LoggerMessage.new('WaitlistTeam', user_id, "Team #{team_id} cannot be added to waitlist for the topic #{topic_id}")
-      ExpertizaLogger.info LoggerMessage.new('WaitlistTeam', user_id, new_waitlist.errors.full_messages.join(" "))
-      return false
-    end
-    return true
-  end
-
-  def self.remove_team_from_topic_waitlist(team_id, topic_id, user_id)
-    waitlisted_team_for_topic = WaitlistTeam.find_by(topic_id: topic_id, team_id: team_id)
-    unless waitlisted_team_for_topic.nil?
-      waitlisted_team_for_topic.destroy
-    else
-      ExpertizaLogger.info LoggerMessage.new('WaitlistTeam', user_id, "Cannot find Team #{team_id} in waitlist for the topic #{topic_id} to be deleted.")
-    end
-    return true
-  end
 
   def self.first_team_in_waitlist_for_topic(topic_id)
     waitlisted_team_for_topic = WaitlistTeam.where(topic_id: topic_id).order("created_at ASC").first

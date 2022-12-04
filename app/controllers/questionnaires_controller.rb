0ca8f80bfe5890bea9151fc2a714f37164746422
Pujitha Enamandala <pujitha98.e@gmail.com>
2022-12-04 11:30:12 -0500

    Refactor - Factory pattern to create Question objects based on type

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -213,6 +213,33 @@ class QuestionnairesController < ApplicationController
     redirect_to action: 'list', controller: 'tree_display'
   end
 
+  # factory method to create the appropriate question based on the type
+  def question_factory(type, questionnaire_id, seq)
+    if type == 'Criterion'
+      return Criterion.create(txt: '', questionnaire_id: questionnaire_id, seq: seq, type: type, break_before: true)
+    elsif type == 'Scale'
+      return Scale.create(txt: '', questionnaire_id: questionnaire_id, seq: seq, type: type, break_before: true)
+    elsif type == 'Cake'
+      return Cake.create(txt: '', questionnaire_id: questionnaire_id, seq: seq, type: type, break_before: true)
+    elsif type == 'Dropdown'
+      return Dropdown.create(txt: '', questionnaire_id: questionnaire_id, seq: seq, type: type, break_before: true)
+    elsif type == 'Checkbox'
+      return Checkbox.create(txt: '', questionnaire_id: questionnaire_id, seq: seq, type: type, break_before: true)
+    elsif type == 'TextArea'
+      return TextArea.create(txt: '', questionnaire_id: questionnaire_id, seq: seq, type: type, break_before: true)
+    elsif type == 'TextField'
+      return TextField.create(txt: '', questionnaire_id: questionnaire_id, seq: seq, type: type, break_before: true)
+    elsif type == 'UploadFile'
+      return UploadFile.create(txt: '', questionnaire_id: questionnaire_id, seq: seq, type: type, break_before: true)
+    elsif type == 'SectionHeader'
+      return SectionHeader.create(txt: '', questionnaire_id: questionnaire_id, seq: seq, type: type, break_before: true)
+    elsif type == 'TableHeader'
+      return TableHeader.create(txt: '', questionnaire_id: questionnaire_id, seq: seq, type: type, break_before: true)
+    elsif type == 'ColumnHeader'
+      return ColumnHeader.create(txt: '', questionnaire_id: questionnaire_id, seq: seq, type: type, break_before: true)
+    end
+  end
+
   # Zhewei: This method is used to add new questions when editing questionnaire.
   def add_new_questions
     puts "add_new_questions called."
@@ -233,7 +260,7 @@ class QuestionnairesController < ApplicationController
     end
     ((num_of_existed_questions + 1)..(num_of_existed_questions + params[:question][:total_num].to_i)).each do
       max_seq += 1
-      question = Object.const_get(params[:question][:type]).create(txt: '', questionnaire_id: questionnaire_id, seq: max_seq, type: params[:question][:type], break_before: true)
+      question = question_factory(params[:question][:type], questionnaire_id, i)
       if question.is_a? ScoredQuestion
         question.weight = params[:question][:weight]
         question.max_label = 'Strongly agree'

2541ec3de62f9bec66940cf114c6bd8145e0ba0d
Michael <mefergio@ncsu.edu>
2023-03-08 15:04:50 -0500

    Created AssignmentDueDate Mixin, moved requied methods from assignment.rb

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -11,6 +11,7 @@ class Assignment < ApplicationRecord
   include ReviewAssignment
   include QuizAssignment
   include AssignmentHelper
+  include AssignmentDueDate
   has_paper_trail
   # When an assignment is created, it needs to
   # be created as an instance of a subclass of the Assignment (model) class;
@@ -186,36 +187,9 @@ class Assignment < ApplicationRecord
     path_text
   end
 
-  # Check whether review, metareview, etc.. is allowed
-  # The permissions of TopicDueDate is the same as AssignmentDueDate.
-  # Here, column is usually something like 'review_allowed_id'
-  def check_condition(column, topic_id = nil)
-    next_due_date = DueDate.get_next_due_date(id, topic_id)
-    return false if next_due_date.nil?
-
-    right_id = next_due_date.send column
-    right = DeadlineRight.find(right_id)
-    right && (right.name == 'OK' || right.name == 'Late')
-  end
-
-  # Determine if the next due date from now allows for submissions
-  def submission_allowed(topic_id = nil)
-    check_condition('submission_allowed_id', topic_id)
-  end
-
   # Determine if the next due date from now allows to take the quizzes
   def quiz_allowed(topic_id = nil)
     check_condition('quiz_allowed_id', topic_id)
-  end
-
-  # Determine if the next due date from now allows for reviews
-  def can_review(topic_id = nil)
-    check_condition('review_allowed_id', topic_id)
-  end
-
-  # Determine if the next due date from now allows for metareviews
-  def metareview_allowed(topic_id = nil)
-    check_condition('review_of_review_allowed_id', topic_id)
   end
 
   # Deletes all instances created as part of assignment and finally destroys itself.
@@ -294,15 +268,6 @@ class Assignment < ApplicationRecord
     node.save
   end
 
-  # if current  stage is submission or review, find the round number
-  # otherwise, return 0
-  def number_of_current_round(topic_id)
-    next_due_date = DueDate.get_next_due_date(id, topic_id)
-    return 0 if next_due_date.nil?
-
-    next_due_date.round ||= 0
-  end
-
   # For varying rubric feature
   def current_stage_name(topic_id = nil)
     if staggered_deadline?
@@ -323,38 +288,11 @@ class Assignment < ApplicationRecord
     AssignmentQuestionnaire.where(assignment_id: id, used_in_round: 2).size > 1
   end
 
-  def link_for_current_stage(topic_id = nil)
-    return nil if staggered_and_no_topic?(topic_id)
-
-    due_date = find_current_stage(topic_id)
-    if due_date.nil? || (due_date == 'Finished') || due_date.is_a?(TopicDueDate)
-      return nil
-    end
-
-    due_date.description_url
-  end
-
   def stage_deadline(topic_id = nil)
     return 'Unknown' if staggered_and_no_topic?(topic_id)
 
     due_date = find_current_stage(topic_id)
     due_date.nil? || due_date == 'Finished' ? due_date : due_date.due_at.to_s
-  end
-
-  def num_review_rounds
-    due_dates = AssignmentDueDate.where(parent_id: id)
-    rounds = 0
-    due_dates.each do |due_date|
-      rounds = due_date.round if due_date.round > rounds
-    end
-    rounds
-  end
-
-  def find_current_stage(topic_id = nil)
-    next_due_date = DueDate.get_next_due_date(id, topic_id)
-    return 'Finished' if next_due_date.nil?
-
-    next_due_date
   end
 
   # Zhewei: this method is almost the same as 'stage_deadline'
@@ -566,39 +504,6 @@ class Assignment < ApplicationRecord
     fields
   end
 
-  def find_due_dates(type)
-    due_dates.select { |due_date| due_date.deadline_type_id == DeadlineType.find_by(name: type).id }
-  end
-
-  # Method find_review_period is used in answer_helper.rb to get the start and end dates of a round
-  def find_review_period(round)
-    # If round is nil, it means the same questionnaire is used for every round. Thus, we return all periods.
-    # If round is not nil, we return only the period of that round.
-
-    submission_type = DeadlineType.find_by(name: 'submission').id
-    review_type = DeadlineType.find_by(name: 'review').id
-
-    due_dates = []
-    due_dates += find_due_dates('submission')
-    due_dates += find_due_dates('review')
-    due_dates.sort_by!(&:id)
-
-    start_dates = []
-    end_dates = []
-
-    if round.nil?
-      round = 1
-      while self.due_dates.exists?(round: round)
-        start_dates << due_dates.select { |due_date| due_date.deadline_type_id == submission_type && due_date.round == round }.last
-        end_dates << due_dates.select { |due_date| due_date.deadline_type_id == review_type && due_date.round == round }.last
-        round += 1
-      end
-    else
-      start_dates << due_dates.select { |due_date| due_date.deadline_type_id == submission_type && due_date.round == round }.last
-      end_dates << due_dates.select { |due_date| due_date.deadline_type_id == review_type && due_date.round == round }.last
-    end
-    [start_dates, end_dates]
-  end
 
   # for program 1 like assignment, if same rubric is used in both rounds,
   # the 'used_in_round' field in 'assignment_questionnaires' will be null,

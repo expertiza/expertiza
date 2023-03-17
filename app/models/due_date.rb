7ea5dd95f9783f17e622fd84eb7f1e347f97ff60
ShiveshJha12 <shubjha12@gmail.com>
2023-03-17 16:21:21 -0400

    Added helper method for teammate_review_allowed

--- app/models/due_date.rb
+++ app/models/due_date.rb
@@ -8,26 +8,19 @@ class DueDate < ApplicationRecord
     DeadlineRight::DEFAULT_PERMISSION[deadline_type][permission_type]
   end
 
-  def self.current_due_date(due_dates)
-    # Get the current due date from list of due dates
-    due_dates.each do |due_date|
-      if due_date.due_at > Time.now
-        current_due_date = due_date
-        return current_due_date
-      end
-    end
-    # in case current due date not found
-    nil
-  end
+  # def self.current_due_date(due_dates)
+  #   # Get the current due date from list of due dates
+  #   due_dates.each do |due_date|
+  #     if due_date.due_at > Time.now
+  #       current_due_date = due_date
+  #       return current_due_date
+  #     end
+  #   end
+  #   # in case current due date not found
+  #   nil
+  # end
 
-  def self.teammate_review_allowed(student)
-    # time when teammate review is allowed
-    due_date = current_due_date(student.assignment.due_dates)
-    student.assignment.find_current_stage == 'Finished' ||
-      due_date &&
-        (due_date.teammate_review_allowed_id == 3 ||
-        due_date.teammate_review_allowed_id == 2) # late(2) or yes(3)
-  end
+
 
   # def due_at_is_valid_datetime
   #   if due_at.present?

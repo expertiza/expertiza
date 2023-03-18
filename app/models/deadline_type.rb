d5bd7af1d5be970bdd3909ec56289de431bdcd54
kajadhav <kajadhav@ncsu.edu>
2023-03-18 03:44:22 -0400

    Merge main

--- app/models/deadline_type.rb
+++ app/models/deadline_type.rb
@@ -1,5 +1,4 @@
 class DeadlineType < ApplicationRecord
   has_many :assignment_due_dates, class_name: 'AssignmentDueDate', foreign_key: 'deadline_type_id'
-  has_many :topic_due_dates, class_name: 'TopicDueDate', foreign_key: 'deadline_type_id'  
-  end
+  has_many :topic_due_dates, class_name: 'TopicDueDate', foreign_key: 'deadline_type_id'
 end

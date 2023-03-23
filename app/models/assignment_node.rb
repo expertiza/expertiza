1abded6494a914b865cacd3b2892f835a76ba008
Neha Kale <nkale2@vclvm177-125.vcl.ncsu.edu>
2023-03-22 22:14:48 -0400

    final changes

--- app/models/assignment_node.rb
+++ app/models/assignment_node.rb
@@ -60,29 +60,6 @@ class AssignmentNode < Node
   end
 
 
-  # def self.get(sortvar = nil, sortorder = nil, user_id = nil, show = nil, parent_id = nil, _search = nil)
-  #   if show
-  #     conditions = if User.find(user_id).role.name != 'Teaching Assistant'
-  #                    'assignments.instructor_id = ?'
-  #                  else
-  #                    'assignments.course_id in (?)'
-  #                  end
-  #   else
-  #     if User.find(user_id).role.name != 'Teaching Assistant'
-  #       conditions = '(assignments.private = 0 or assignments.instructor_id = ?)'
-  #       values = user_id
-  #     else
-  #       conditions = '(assignments.private = 0 or assignments.course_id in (?))'
-  #       values = Ta.get_mapped_courses(user_id)
-  #     end
-  #   end
-  #   conditions += " and course_id = #{parent_id}" if parent_id
-  #   sortvar ||= 'created_at'
-  #   sortorder ||= 'desc'
-  #   find_conditions = [conditions, values]
-  #   includes(:assignment).where(find_conditions).order("assignments.#{sortvar} #{sortorder}")
-  # end
-
   # Indicates that this object is always a leaf
   def is_leaf
     true

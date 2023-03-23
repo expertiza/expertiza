170b5fe6da7eb508e466c5ed16e3e199ec65f969
ritwik4690 <vaidyaritwik@gmail.com>
2023-03-22 23:09:12 -0400

    Refactored to reduce LOC.

--- app/helpers/review_mapping_helper.rb
+++ app/helpers/review_mapping_helper.rb
@@ -187,26 +187,19 @@ module ReviewMappingHelper
 
   # moves data of reviews in each round from a current round
   def initialize_chart_elements(reviewer)
-    round = 0
-    labels = []
-    reviewer_data = []
-    all_reviewers_data = []
-
-    # display avg volume for all reviewers per round
+    labels, reviewer_data, all_reviewers_data = [], [], []
     @num_rounds.times do |rnd|
-      next unless @all_reviewers_avg_vol_per_round[rnd] > 0
-
-      round += 1
-      labels.push round
-      reviewer_data.push reviewer.avg_vol_per_round[rnd]
-      all_reviewers_data.push @all_reviewers_avg_vol_per_round[rnd]
+      next if @all_reviewers_avg_vol_per_round[rnd] <= 0
+      labels << (rnd + 1)
+      reviewer_data << reviewer.avg_vol_per_round[rnd]
+      all_reviewers_data << @all_reviewers_avg_vol_per_round[rnd]
     end
-
-    labels.push 'Total'
-    reviewer_data.push reviewer.overall_avg_vol
-    all_reviewers_data.push @all_reviewers_overall_avg_vol
+    labels << 'Total'
+    reviewer_data << reviewer.overall_avg_vol
+    all_reviewers_data << @all_reviewers_overall_avg_vol
     [labels, reviewer_data, all_reviewers_data]
   end
+
 
   # The data of all the reviews is displayed in the form of a bar chart
   def display_volume_metric_chart(reviewer)
@@ -231,103 +224,64 @@ module ReviewMappingHelper
       ]
     }
     options = {
-      legend: {
-        position: 'top',
-        labels: {
-          usePointStyle: true
-        }
-      },
-      width: '200',
-      height: '125',
+      legend: { position: 'top', labels: { usePointStyle: true } },
+      width: '200', height: '125',
       scales: {
-        yAxes: [{
-          stacked: true,
-          id: 'bar-y-axis1',
-          barThickness: 10
-        }, {
-          display: false,
-          stacked: true,
-          id: 'bar-y-axis2',
-          barThickness: 15,
-          type: 'category',
-          categoryPercentage: 0.8,
-          barPercentage: 0.9,
-          gridLines: {
-            offsetGridLines: true
+        yAxes: [
+          { stacked: true, id: 'bar-y-axis1', barThickness: 10 },
+          {
+            display: false, stacked: true, id: 'bar-y-axis2',
+            barThickness: 15, type: 'category',
+            categoryPercentage: 0.8, barPercentage: 0.9,
+            gridLines: { offsetGridLines: true }
           }
-        }],
-        xAxes: [{
-          stacked: false,
-          ticks: {
-            beginAtZero: true,
-            stepSize: 50,
-            max: 400
-          }
-        }]
+        ],
+        xAxes: [
+          { stacked: false, ticks: { beginAtZero: true, stepSize: 50, max: 400 } }
+        ]
       }
     }
-    horizontal_bar_chart data, options
+    horizontal_bar_chart(data, options)
   end
+
 
   # E2082 Generate chart for review tagging time intervals
   def display_tagging_interval_chart(intervals)
-    # if someone did not do any tagging in 30 seconds, then ignore this interval
     threshold = 30
     intervals = intervals.select { |v| v < threshold }
-    unless intervals.empty?
-      interval_mean = intervals.reduce(:+) / intervals.size.to_f
-    end
-    # build the parameters for the chart
+    interval_mean = intervals.sum / intervals.size.to_f unless intervals.empty?
     data = {
       labels: [*1..intervals.length],
       datasets: [
-        {
-          backgroundColor: 'rgba(255,99,132,0.8)',
-          data: intervals,
-          label: 'time intervals'
-        },
-        unless intervals.empty?
-          {
-            data: Array.new(intervals.length, interval_mean),
-            label: 'Mean time spent'
-          }
-        end
+        { backgroundColor: 'rgba(255,99,132,0.8)', data: intervals, label: 'time intervals' },
+        *(!intervals.empty? && [{ data: [interval_mean] * intervals.length, label: 'Mean time spent' }])
       ]
     }
     options = {
-      width: '200',
-      height: '125',
-      scales: {
-        yAxes: [{
-          stacked: false,
-          ticks: {
-            beginAtZero: true
-          }
-        }],
-        xAxes: [{
-          stacked: false
-        }]
-      }
+      width: '200', height: '125',
+      scales: { yAxes: [{ stacked: false, ticks: { beginAtZero: true } }], xAxes: [{ stacked: false }] }
     }
-    line_chart data, options
+    line_chart(data, options)
   end
+
 
   # Calculate mean, min, max, variance, and stand deviation for tagging intervals
   def calculate_key_chart_information(intervals)
     # if someone did not do any tagging in 30 seconds, then ignore this interval
     threshold = 30
     interval_precision = 2 # Round to 2 Decimal Places
-    intervals = intervals.select { |v| v < threshold }
-
+    intervals.select! { |v| v < threshold }
     # Get Metrics once tagging intervals are available
     unless intervals.empty?
-      metrics = {}
-      metrics[:mean] = (intervals.reduce(:+) / intervals.size.to_f).round(interval_precision)
-      metrics[:min] = intervals.min
-      metrics[:max] = intervals.max
-      sum = intervals.inject(0) { |accum, i| accum + (i - metrics[:mean])**2 }
-      metrics[:variance] = (sum / intervals.size.to_f).round(interval_precision)
-      metrics[:stand_dev] = Math.sqrt(metrics[:variance]).round(interval_precision)
+      mean = intervals.sum / intervals.size.to_f
+      variance = intervals.map { |v| (v - mean) ** 2 }.sum / intervals.size.to_f
+      metrics = {
+        mean: mean.round(interval_precision),
+        min: intervals.min,
+        max: intervals.max,
+        variance: variance.round(interval_precision),
+        stand_dev: Math.sqrt(variance).round(interval_precision)
+      }
       metrics
     end
     # if no Hash object is returned, the UI handles it accordingly

9b23dfcd05c87f975100e5499c09aee77437f1cf
Tanvi Sinha <tanvi9sinha@gmail.com>
2023-03-26 17:52:11 -0400

    added comments to teams_participants.rb

--- app/models/teams_participant.rb
+++ app/models/teams_participant.rb
@@ -46,6 +46,7 @@ class TeamsParticipant < ApplicationRecord
   def self.add_member_to_invited_team(invitee_user_id, invited_user_id, assignment_id)
     can_add_member = false
     users_teams = TeamsParticipant.where(['user_id = ?', invitee_user_id])
+    #fetching teams and adding members
     users_teams.each do |team|
       new_team = AssignmentTeam.where(['id = ? and parent_id = ?', team.team_id, assignment_id]).first
       unless new_team.nil?

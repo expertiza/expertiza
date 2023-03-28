972c66e47fbc22965490cc4da3ae69a56d076d6b
Ameya Vaichalkar <ameyav1234@gmail.com>
2023-03-27 20:08:34 -0400

    added import functionality according to E1923 for course_team.rb

--- spec/models/course_team_spec.rb
+++ spec/models/course_team_spec.rb
@@ -1,4 +1,5 @@
 describe 'CourseTeam' do
+  let(:course) { build(:course) }
   let(:course_team1) { build(:course_team, id: 1, name: 'no team') }
   let(:user2) { build(:student, id: 2, name: 'no name') }
   let(:participant) { build(:participant, user: user2) }
@@ -17,6 +18,39 @@ describe 'CourseTeam' do
       expect(assignment_team.copy_members(assignment_team.id))
     end
   end
+
+  describe ".import" do
+    let(:row) do
+      { teammembers: 'none' }
+    end
+    context "when a course team does not exist with id" do
+      it "raises ImportError" do
+        course_id = 1
+        allow(Course).to receive(:find).with(course_id).and_return(nil)
+        error_message = "The course with the id \"" + course_id.to_s + "\" was not found. <a href='/course/new'>Create</a> this course?"
+        expect { CourseTeam.import(row, nil, course_id, nil) }.
+          to raise_error(ImportError, error_message)
+      end
+    end
+
+    context "when the course team does not have the required fields" do
+      it "raises ArgumentError" do
+        expect { CourseTeam.import([], nil, 1, nil) }.
+          to raise_error(ArgumentError)
+      end
+    end
+
+    context "when a course team with the same id already exists" do
+      it "gets imported through Team.import" do
+        course_id = 1
+        options = []
+        allow(Course).to receive(:find).with(course_id).and_return(course)
+        expect(Team).to receive(:import_helper).with(row, course_id, options, instance_of(CourseTeam))
+        CourseTeam.import(row, nil, course_id, options)
+      end
+    end
+  end
+
   describe '#parent_model' do
     it 'returns parent_model' do
       course_team = CourseTeam.new
@@ -38,15 +72,19 @@ describe 'CourseTeam' do
   end
   describe '#import' do
     context 'when the course does not exist' do
+      let(:row) do
+        { teamname: 'Ruby', teammembers: 'none' }
+      end
       it 'raises an import error' do
         allow(Course).to receive(:find).with(1).and_return(nil)
-        expect { CourseTeam.import({}, 1, nil) }.to raise_error(ImportError)
+        options = []
+        expect { CourseTeam.import({ "teammembers" => "Team Members" }, nil, 1, options) }.to raise_error(ImportError)
       end
     end
     context 'when the course does exist' do
       it 'raises an error with empty row hash' do
         allow(Course).to receive(:find).with(1).and_return(course)
-        expect { CourseTeam.import({}, 1, nil) }.to raise_error(ArgumentError)
+        expect { CourseTeam.import({}, nil, 1, nil) }.to raise_error(ArgumentError)
       end
     end
   end

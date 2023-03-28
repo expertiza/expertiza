fb251254d4b365f611d40d3c5dc7c9cfed9f90fe
rohitgeddam <rohitgeddam2018@gmail.com>
2023-03-27 23:01:37 -0400

    add required comments for methods in super_administrator.rb

--- app/models/super_administrator.rb
+++ app/models/super_administrator.rb
@@ -1,5 +1,6 @@
 class SuperAdministrator < User
   def self.get_user_list
+    # This function returns a list of all users in the system.
     user_list = []
     User.all.find_each do |user|
       user_list << user

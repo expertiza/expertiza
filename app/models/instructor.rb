b31355be6ba3b01833610b4b1401c9dea915f8d8
rohitgeddam <rohitgeddam2018@gmail.com>
2023-03-27 23:00:53 -0400

    add required commits for instructor.rb

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -12,19 +12,29 @@ class Instructor < User
                 ['All public assignments', 'list_all']].freeze
 
   def list_all(object_type, user_id)
+    # This function takes an object_type (e.g. Assignment, Course) and a user_id as inputs,
+    # and returns all objects of that type where the instructor_id matches the user_id or
+    # the "private" attribute is false.
     object_type.where('instructor_id = ? OR private = 0', user_id)
   end
 
   def list_mine(object_type, user_id)
+    # This function takes an object_type and a user_id as inputs,
+    # and returns all objects of that type where the instructor_id matches the user_id.
     object_type.where('instructor_id = ?', user_id)
   end
 
   def get(object_type, id, user_id)
+    # This function takes an object_type, an id, and a user_id as inputs,
+    # and returns the object of that type with the matching id where the
+    # instructor_id matches the user_id or the "private" attribute is false.
     # object_type.where("id = ? AND (instructor_id = ? OR private = 0)", id, user_id).first
     object_type.find_by('id = ? AND (instructor_id = ? OR private = 0)', id, user_id)
   end
 
   def my_tas
+    # This function returns an array of TA ids associated with courses
+    # taught by the current user (who is assumed to be an instructor).
     courses = Course.where(instructor_id: id)
     ta_ids = []
     courses.each do |course|
@@ -35,12 +45,21 @@ class Instructor < User
   end
 
   def self.get_user_list(user)
-    participants = get_participants(user)
+    # This function takes a user object as an input, and returns
+    # a list of users who have the same role as the input user and are
+    # associated with courses or assignments taught by the input user.
+    # The function first gets all participants associated with courses
+    # taught by the user, then gets all participants associated with
+    # assignments taught by the user, and then filters the participants
+    # to only include those with the same role as the input user.
+
+      participants = get_participants(user)
     user_list = filter_participants_by_role(participants, user.role)
     user_list
   end
 
   def self.get_participants(user)
+    # get all participants
     participants = []
     courses = get_courses_for_user(user)
     assignments = get_assignments_for_user(user)
@@ -50,6 +69,7 @@ class Instructor < User
   end
 
   def self.filter_participants_by_role(participants, role)
+    # return participants filtered by role
     participants
       .reject(&:empty?)
       .flatten
@@ -58,10 +78,12 @@ class Instructor < User
   end
 
   def self.get_courses_for_user(user)
+    # Get courses for user
     Course.where(instructor_id: user.id)
   end
 
   def self.get_assignments_for_user(user)
+    # get the assingment for the user with user.id
     Assignment.includes(:participants).where(instructor_id: user.id)
   end
 end

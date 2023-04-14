24e91f883f5036629b2ca0f16757a2784c81af79
ShreyaReddy17 <43646675+ShreyaReddy17@users.noreply.github.com>
2023-04-14 15:41:00 -0400

    Update grading_histories_controller.rb

--- app/controllers/grading_histories_controller.rb
+++ app/controllers/grading_histories_controller.rb
@@ -2,6 +2,30 @@ class GradingHistoriesController < ApplicationController
   include AuthorizationHelper
   before_action :set_grading_history, only: %i[show]
 
+  def show
+    @grading_histories = GradingHistory.includes(:grade_receiver, :assignment, :instructor).where(id: params[:id]).order(created_at: :desc)
+    set_receiver_and_assignment
+  end
+
+  private
+
+  def set_receiver_and_assignment
+    record = @grading_histories.first
+    if record.nil?
+      @receiver = ""
+      @assignment = ""
+    else
+      if record.grading_type == "Submission"
+        @receiver = "of #{record.grade_receiver.name}"
+        @assignment = "for the submission #{record.assignment.name}"
+      else
+        @receiver = "of #{record.grade_receiver.fullname}"
+        @assignment = "for review in #{record.assignment.name}"
+      end
+    end
+  end
+end
+
   # Checks if user is allowed to view a grading history
   def action_allowed?
   # Admins and superadmins are always allowed

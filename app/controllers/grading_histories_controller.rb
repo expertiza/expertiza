3a07f200cbf3b120fd4cc0c205183cbbf2d7cfbc
Sarika Vishwanatham <vishwanathamsarika@gmail.com>
2023-04-20 03:07:54 -0400

    Fixed grading histories controller

--- app/controllers/grading_histories_controller.rb
+++ app/controllers/grading_histories_controller.rb
@@ -1,6 +1,6 @@
 class GradingHistoriesController < ApplicationController
   before_action :set_grading_history, only: %i[show]
-  
+
   def show
     @grading_histories = GradingHistory.includes(:grade_receiver, :assignment, :instructor).where(id: params[:id]).order(created_at: :desc)
     set_receiver_and_assignment
@@ -24,39 +24,25 @@ class GradingHistoriesController < ApplicationController
     end
   end
 
-  # Checks if user is allowed to view a grading history
   def action_allowed?
-  # Admins and superadmins are always allowed
-  return true if current_user_has_admin_privileges?
-
-  # Populate assignment fields
-  assignment_for_history(params[:grade_type])
-
-  # Check permissions
-  if @assignment.instructor_id == current_user.id # If the current user is the instructor of the assignment
-    true # Return true
-  elsif TaMapping.exists?(ta_id: current_user.id, course_id: @assignment.course_id) && 
-        TaMapping.where(course_id: @assignment.course_id).include?(TaMapping.where(ta_id: current_user.id, course_id: @assignment.course_id).first)
-    true # Return true if the current user is a TA for the course that the assignment belongs to
-  elsif @assignment.course_id && Course.find(@assignment.course_id).instructor_id == current_user.id # If the current user is the instructor of the course that the assignment belongs to
-    true # Return true
+    return true if ['Super-Administrator', 'Administrator'].include? current_role_name
+    check_type(params[:grade_type])
+    if @assignment.instructor_id == current_user.id
+      true
+    elsif TaMapping.exists?(ta_id: current_user.id, course_id: @assignment.course_id) &&
+      (TaMapping.where(course_id: @assignment.course_id).include? TaMapping.where(ta_id: current_user.id, course_id: @assignment.course_id).first)
+      true
+    elsif @assignment.course_id && Course.find(@assignment.course_id).instructor_id == current_user.id
+      true
+    end
   end
 
-  # Return false if none of the above conditions are met
-  false
-end
 
-
-  # populate the assignment fields according to type
-  def assignment_for_history(type)
-    # for a submission, the receiver is an AssignmentTeam
-    # use this AssignmentTeam to find the assignment
+  def check_type(type)
     if type.eql? "Submission"
       assignment_team = AssignmentTeam.find(params[:grade_receiver_id])
       @assignment = Assignment.find(assignment_team.parent_id)
     end
-    # for a review, the receiver is an AssignmentParticipant
-    # use this AssignmentParticipant to find the assignment
     if type.eql? "Review"
       participant_id = params[:participant_id]
       grade_receiver = AssignmentParticipant.find(participant_id)
@@ -64,8 +50,6 @@ end
     end
   end
 
-  # return all grading history entries for the assignment
-  # entries are returned in chronological order
   def index
     @grading_histories = GradingHistory.where(grade_receiver_id: params[:grade_receiver_id], grading_type: params[:grade_type]).reverse_order
   end

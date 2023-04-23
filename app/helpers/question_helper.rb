61b6176dd2384ed31dde001ee6f108c053debe8c
jtdavis9 <jtdavis9@ncsu.edu>
2023-04-23 13:44:49 -0400

    helpers converted if/else to switch statement

--- app/helpers/question_helper.rb
+++ app/helpers/question_helper.rb
@@ -1,28 +1,32 @@
 module QuestionHelper
   # factory method to create the appropriate question based on the type
   def question_factory(type, questionnaire_id, seq)
-    if type == 'Criterion'
+    case type
+    when 'Criterion'
       return Criterion.create(txt: '', questionnaire_id: questionnaire_id, seq: seq, type: type, break_before: true)
-    elsif type == 'Scale'
+    when type == 'Scale'
       return Scale.create(txt: '', questionnaire_id: questionnaire_id, seq: seq, type: type, break_before: true)
-    elsif type == 'Cake'
+    when 'Cake'
       return Cake.create(txt: '', questionnaire_id: questionnaire_id, seq: seq, type: type, break_before: true)
-    elsif type == 'Dropdown'
+    when 'Dropdown'
       return Dropdown.create(txt: '', questionnaire_id: questionnaire_id, seq: seq, type: type, break_before: true)
-    elsif type == 'Checkbox'
+    when 'Checkbox'
       return Checkbox.create(txt: '', questionnaire_id: questionnaire_id, seq: seq, type: type, break_before: true)
-    elsif type == 'TextArea'
+    when 'TextArea'
       return TextArea.create(txt: '', questionnaire_id: questionnaire_id, seq: seq, type: type, break_before: true)
-    elsif type == 'TextField'
+    when  'TextField'
       return TextField.create(txt: '', questionnaire_id: questionnaire_id, seq: seq, type: type, break_before: true)
-    elsif type == 'UploadFile'
+    when 'UploadFile'
       return UploadFile.create(txt: '', questionnaire_id: questionnaire_id, seq: seq, type: type, break_before: true)
-    elsif type == 'SectionHeader'
+    when 'SectionHeader'
       return SectionHeader.create(txt: '', questionnaire_id: questionnaire_id, seq: seq, type: type, break_before: true)
-    elsif type == 'TableHeader'
+    when 'TableHeader'
       return TableHeader.create(txt: '', questionnaire_id: questionnaire_id, seq: seq, type: type, break_before: true)
-    elsif type == 'ColumnHeader'
+    when 'ColumnHeader'
       return ColumnHeader.create(txt: '', questionnaire_id: questionnaire_id, seq: seq, type: type, break_before: true)
+    else
+      flash[:error] = "Error: Undefined Question"
     end
+
   end
 end

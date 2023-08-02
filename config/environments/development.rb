fffc3104a4b91a9aeda0fd672623c8986c048b27
Ameya Vaichalkar <57044378+ameyagv@users.noreply.github.com>
2023-08-02 17:47:13 -0400

    Mailer issue fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,10 +22,10 @@ Expertiza::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
-    port: 25,
+    port: 587,
     domain: 'gmail.com',
-    user_name: 'expertiza.debugging@gmail.com',
-    password: 'dt2DR0Hhf',
+    user_name: 'expertiza.mailer@gmail.com',
+    password: 'xdgmnehqevkevkqy',
     authentication: 'plain',
     enable_starttls_auto: true
   }

0a678a90cae127ee9a5e2c3ef15c251659238f5a
Ameya Vaichalkar <57044378+ameyagv@users.noreply.github.com>
2023-08-22 14:28:03 -0400

    Adjust mailer settings in development.rb
    Tweaked mailer configuration settings in the development.rb to resolve issues with email delivery on the production server.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,7 +20,7 @@ Expertiza::Application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
+  ActionMailer::Base.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'gmail.com',

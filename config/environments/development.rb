f36b700a57801f1771c03d75a4369dbbbc5b187d
Ameya Vaichalkar <57044378+ameyagv@users.noreply.github.com>
2023-08-30 18:40:30 -0400

    removed mailer config from development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,19 +17,6 @@ Expertiza::Application.configure do
   config.action_controller.perform_caching = false
   # config.active_record.whitelist_attributes = false # need protected_attributes gem
 
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    domain: 'gmail.com',
-    user_name: 'expertiza.mailer@gmail.com',
-    password: 'xdgmnehqevkevkqy',
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
   # Print development logs

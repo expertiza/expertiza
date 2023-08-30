3a7cf699db119891680eace733c7d9d753e27bf6
Ameya Vaichalkar <57044378+ameyagv@users.noreply.github.com>
2023-08-30 18:41:54 -0400

    added mailer config to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,19 @@ Expertiza::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: 'expertiza.mailer@gmail.com',
+    password: 'xdgmnehqevkevkqy',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.

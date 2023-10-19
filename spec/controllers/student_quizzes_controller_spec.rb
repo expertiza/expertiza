29b2b1a10212855c06ec9ca2dbebabf00ac7370e
Scott Hardin <shardin2@vclvm178-245.vcl.ncsu.edu>
2023-10-19 19:26:10 -0400

    Create the action_allowed? tests and verify functionality

--- spec/controllers/student_quizzes_controller_spec.rb
+++ spec/controllers/student_quizzes_controller_spec.rb
@@ -1,135 +1,264 @@
 describe StudentQuizzesController do
-describe "#action_allowed?" do
-  context "when current user has student privileges" do
-    it "returns true if action is not 'index'" do
-      # Test scenario: current user is a student and action is 'show'
-      # Expected result: true
+  # Describe block for testing the method `action_allowed?` in the controller.
+  describe "#action_allowed?" do
+
+    # Context for testing scenarios when the current user has student privileges.
+    context "when current user has student privileges" do
+
+      # Use `let` to define a student object for testing purposes.
+      let(:student) { create(:student) }
+
+      # Before each test within this context, set up the session and stub methods.
+      before do
+        # Set the current session's user as the student.
+        session[:user] = student
+
+        # Stub the `current_user` method to always return our test student.
+        allow(controller).to receive(:current_user).and_return(student)
+      end
+
+      # Test to ensure that the controller correctly identifies the current user as a student.
+      it "correctly identifies the current user as a student" do
+        expect(controller.send(:current_user_is_a?, 'Student')).to be_truthy
+      end
+
+      # Test for scenario where the current action is not 'index'.
+      # Given that the current user is a student, this test checks that
+      # the method `action_allowed?` should return true.
+      it "returns true if action is not 'index'" do
+        controller.params = { action: 'show' } # Set the action to 'show'
+        expect(controller.send(:action_allowed?)).to be_truthy
+      end
+
+      # Test for scenario where the current action is 'index', but the student
+      # does not have the required reviewer and submitter authorizations.
+      # The method `action_allowed?` should return false in this scenario.
+      it "returns false if action is 'index' and student does not have reviewer and submitter authorizations" do
+        controller.params = { action: 'index', id: 'some_id' } # Set the action to 'index'
+        allow(controller).to receive(:are_needed_authorizations_present?).and_return(false)
+        allow(controller).to receive(:action_name).and_return('index')
+        expect(controller.send(:action_allowed?)).to be_falsey
+      end
+
+      # Test for scenario where the current action is 'index', and the student
+      # has the required reviewer and submitter authorizations.
+      # The method `action_allowed?` should return true in this scenario.
+      it "returns true if action is 'index' and student has reviewer and submitter authorizations" do
+        controller.params = { action: 'index', id: 'some_id' } # Set the action to 'index'
+        allow(controller).to receive(:are_needed_authorizations_present?).and_return(true)
+        expect(controller.send(:action_allowed?)).to be_truthy
+      end
     end
 
-    it "returns false if action is 'index' and student does not have reviewer and submitter authorizations" do
-      # Test scenario: current user is a student and action is 'index', but student does not have reviewer and submitter authorizations
-      # Expected result: false
-    end
+    # Context for testing scenarios when the current user does not have student privileges.
+    context "when current user does not have student privileges" do
 
-    it "returns true if action is 'index' and student has reviewer and submitter authorizations" do
-      # Test scenario: current user is a student and action is 'index', and student has reviewer and submitter authorizations
-      # Expected result: true
+      # Before each test within this context, stub the method `current_user_is_a?` to return false
+      # for 'Student', indicating that the user is not a student.
+      before do
+        allow(controller).to receive(:current_user_is_a?).with('Student').and_return(false)
+      end
+
+      # Test for scenario where the current user has TA (Teaching Assistant) privileges.
+      # Given that the user is not a student, this test checks that if the user is a TA,
+      # the method `action_allowed?` should return true.
+      it "returns true if current user has TA privileges" do
+        allow(controller).to receive(:current_user_has_ta_privileges?).and_return(true) # Stub to indicate the user has TA privileges
+        expect(controller.send(:action_allowed?)).to be_truthy
+      end
+
+      # Test for scenario where the current user does not have either student or TA privileges.
+      # The method `action_allowed?` should return false in this scenario.
+      it "returns false if current user does not have TA privileges" do
+        allow(controller).to receive(:current_user_has_ta_privileges?).and_return(false) # Stub to indicate the user does not have TA privileges
+        expect(controller.send(:action_allowed?)).to be_falsey
+      end
     end
   end
 
-  context "when current user does not have student privileges" do
-    it "returns true if current user has TA privileges" do
-      # Test scenario: current user is a TA
-      # Expected result: true
+  # Group of tests focusing on the 'index' action of a controller.
+  describe "index" do
+    # Using double to stub/mock the participant with an added stub for user_id.
+    let(:participant) { double(:participant, id: 1, user_id: 123) }
+    # Stub/mock for an assignment.
+    let(:assignment) { double(:assignment) }
+    # Stub/mock for quizzes.
+    let(:quizzes) { double(:quizzes) }
+    # A constant value used to provide expected parent_id for stubbing/mocking.
+    let(:some_parent_id) { 456 }
+
+    # Context for tests where the logged-in user is identified as a participant.
+    context "when the logged in user is a participant" do
+      # Before each test in this context, setup the necessary mocks and stubs.
+      before do
+        allow(controller).to receive(:params).and_return(id: participant.id)
+        allow(AssignmentParticipant).to receive(:find).with(participant.id).and_return(participant)
+        allow(participant).to receive(:parent_id).and_return(some_parent_id)
+        allow(controller).to receive(:current_user_id?).with(participant.user_id).and_return(true)
+        allow(Assignment).to receive(:find).with(some_parent_id).and_return(assignment)
+      end
+
+      # Test that checks if the assignment participant is correctly found.
+      it "finds the assignment participant" do
+        expect(AssignmentParticipant).to receive(:find).with(participant.id).and_return(participant)
+        controller.index
+      end
+
+      # Test to verify the correct assignment is found based on the participant's parent_id.
+      it "finds the assignment" do
+        expect(Assignment).to receive(:find).with(some_parent_id).and_return(assignment)
+        controller.index
+      end
+
+      # Test to ensure that the quizzes meant to be reviewed by the participant are returned.
+      it "returns the quizzes to be reviewed by the participant" do
+        expect(QuizResponseMap).to receive(:mappings_for_reviewer).with(participant.id).and_return(quizzes)
+        controller.index
+      end
     end
 
-    it "returns false if current user does not have TA privileges" do
-      # Test scenario: current user is not a student or a TA
-      # Expected result: false
+    # Group of tests for scenarios where the logged-in user is not recognized as a participant.
+    context "when the logged in user is not a participant" do
+      # Before each test in this context, setup the necessary mocks and stubs.
+      before do
+        allow(controller).to receive(:params).and_return(id: participant.id)
+        allow(AssignmentParticipant).to receive(:find).with(participant.id).and_return(participant)
+        allow(controller).to receive(:current_user_id?).with(participant.user_id).and_return(false)
+      end
+
+      # NOTE: The below test was commented out because the original assumption was that the controller's logic would
+      # exit early if the logged-in user wasn't the participant. However, the controller tries to find an assignment
+      # participant regardless of the user's identity.
+      # it "does not find the assignment participant" do
+      #   # Stub current_user_id? to always return false, simulating a scenario
+      #   # where the logged-in user isn't the participant
+      #   allow(controller).to receive(:current_user_id?).and_return(false)
+      #   # Expect that AssignmentParticipant.find is never called due to the early
+      #   # return in the controller's logic
+      #   expect(AssignmentParticipant).not_to receive(:find)
+      #   controller.index
+      # end
+
+      # This test confirms that even when the logged-in user isn't the participant, the controller will still attempt
+      # to find the assignment participant. This is based on the current behavior of the controller's logic.
+      it "finds the assignment participant" do
+        expect(AssignmentParticipant).to receive(:find).with(participant.id).and_return(participant)
+        controller.index
+      end
+
+      # Test to ensure the controller does not attempt to find the assignment when the logged-in user isn't a participant.
+      it "does not find the assignment" do
+        expect(Assignment).not_to receive(:find)
+        controller.index
+      end
+
+      # Test to verify that the controller does not try to fetch any quizzes to be reviewed when the logged-in user isn't a participant.
+      it "does not return any quizzes to be reviewed" do
+        expect(QuizResponseMap).not_to receive(:mappings_for_reviewer)
+        controller.index
+      end
     end
   end
-end
-describe "index" do
-  context "when the logged in user is a participant" do
-    it "finds the assignment participant" do
-      # Test scenario code
-    end
 
-    it "finds the assignment" do
-      # Test scenario code
-    end
+  describe "#finished_quiz" do
+    context "when a participant has finished a quiz" do
+      it "retrieves the participant's response for the quiz" do
+        # Test code
+      end
 
-    it "returns the quizzes to be reviewed by the participant" do
-      # Test scenario code
+      it "retrieves the quiz response map for the given map_id" do
+        # Test code
+      end
+
+      it "retrieves the quiz questions associated with the quiz response map" do
+        # Test code
+      end
+
+      it "retrieves the participant who attempted the quiz" do
+        # Test code
+      end
+
+      it "retrieves the quiz score for the participant" do
+        # Test code
+      end
     end
   end
 
-  context "when the logged in user is not a participant" do
-    it "does not find the assignment participant" do
-      # Test scenario code
-    end
-
-    it "does not find the assignment" do
-      # Test scenario code
-    end
-
-    it "does not return any quizzes to be reviewed" do
-      # Test scenario code
-    end
-  end
-end
-describe "#finished_quiz" do
-  context "when a participant has finished a quiz" do
-    it "retrieves the participant's response for the quiz" do
-      # Test code
-    end
-
-    it "retrieves the quiz response map for the given map_id" do
-      # Test code
-    end
-
-    it "retrieves the quiz questions associated with the quiz response map" do
-      # Test code
-    end
-
-    it "retrieves the participant who attempted the quiz" do
-      # Test code
-    end
-
-    it "retrieves the quiz score for the participant" do
-      # Test code
-    end
-  end
-end
-describe ".get_quiz_questionnaire" do
-  context "when there are no reviewed_team_response_maps for the reviewer" do
-    it "returns an empty array" do
-      # test scenario
-    end
-  end
-
-  context "when there are reviewed_team_response_maps for the reviewer" do
-    context "when the reviewee team is not associated with the assignment" do
-      it "skips the reviewee team and does not include its quiz questionnaire" do
+  describe ".get_quiz_questionnaire" do
+    context "when there are no reviewed_team_response_maps for the reviewer" do
+      it "returns an empty array" do
         # test scenario
       end
     end
 
-    context "when the reviewee team is associated with the assignment" do
-      context "when the reviewee team has not created a quiz questionnaire" do
+    context "when there are reviewed_team_response_maps for the reviewer" do
+      context "when the reviewee team is not associated with the assignment" do
         it "skips the reviewee team and does not include its quiz questionnaire" do
           # test scenario
         end
       end
 
-      context "when the reviewee team has created a quiz questionnaire" do
-        context "when the quiz questionnaire has not been taken by the reviewer" do
-          it "includes the quiz questionnaire in the result" do
+      context "when the reviewee team is associated with the assignment" do
+        context "when the reviewee team has not created a quiz questionnaire" do
+          it "skips the reviewee team and does not include its quiz questionnaire" do
             # test scenario
           end
         end
 
-        context "when the quiz questionnaire has been taken by the reviewer" do
-          it "skips the quiz questionnaire and does not include it in the result" do
-            # test scenario
+        context "when the reviewee team has created a quiz questionnaire" do
+          context "when the quiz questionnaire has not been taken by the reviewer" do
+            it "includes the quiz questionnaire in the result" do
+              # test scenario
+            end
+          end
+
+          context "when the quiz questionnaire has been taken by the reviewer" do
+            it "skips the quiz questionnaire and does not include it in the result" do
+              # test scenario
+            end
           end
         end
       end
     end
   end
-end
-describe "save_quiz_response" do
-  context "when participant response is empty" do
-    it "creates a new response and saves it" do
-      # Test scenario
+
+  describe "save_quiz_response" do
+    context "when participant response is empty" do
+      it "creates a new response and saves it" do
+        # Test scenario
+      end
+
+      it "calculates the score for the participant response" do
+        # Test scenario
+      end
+
+      context "when the score is valid" do
+        it "saves the scores" do
+          # Test scenario
+        end
+
+        it "redirects to the finished_quiz page" do
+          # Test scenario
+        end
+      end
+
+      context "when the score is invalid" do
+        it "destroys the quiz response" do
+          # Test scenario
+        end
+
+        it "displays an error message" do
+          # Test scenario
+        end
+
+        it "redirects to the get_quiz_questionnaire page" do
+          # Test scenario
+        end
+      end
     end
 
-    it "calculates the score for the participant response" do
-      # Test scenario
-    end
-
-    context "when the score is valid" do
-      it "saves the scores" do
+    context "when participant response is not empty" do
+      it "displays an error message" do
         # Test scenario
       end
 
@@ -137,31 +266,6 @@ describe "save_quiz_response" do
         # Test scenario
       end
     end
-
-    context "when the score is invalid" do
-      it "destroys the quiz response" do
-        # Test scenario
-      end
-
-      it "displays an error message" do
-        # Test scenario
-      end
-
-      it "redirects to the get_quiz_questionnaire page" do
-        # Test scenario
-      end
-    end
-  end
-
-  context "when participant response is not empty" do
-    it "displays an error message" do
-      # Test scenario
-    end
-
-    it "redirects to the finished_quiz page" do
-      # Test scenario
-    end
   end
 end
 
-end

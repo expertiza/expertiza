bcda3be19efde3cef9775dde7d91f6f46ea0538e
yagmurbbayraktar <yagmurbasakbayraktar@gmail.com>
2023-10-25 14:41:19 -0400

    test commit

--- app/controllers/student_quizzes_controller.rb
+++ app/controllers/student_quizzes_controller.rb
@@ -27,7 +27,8 @@ class StudentQuizzesController < ApplicationController
 
 
   def finished_quiz
-    @response = Response.where(map_id: params[:map_id]).first #What is the purpose of @response? I don't see it used anywhere else in this file.
+    @participant_response = Response.where(map_id: params[:map_id]).first #What is the purpose of @response? I don't see it used anywhere else in this file.
+    #@response is used in finished_quiz.html.erb
     @response_map = QuizResponseMap.find(params[:map_id])
     @questions = Question.where(questionnaire_id: @response_map.reviewed_object_id) # The reviewed_object_id is questionnaire_id for quiz response_map
     @map = ResponseMap.find(params[:map_id])

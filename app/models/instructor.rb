85234f90087425fea100cc6898c5ffa9e2417bb3
Sergio Vargas Villar <svargas3@vclvm177-73.vcl.ncsu.edu>
2023-10-29 17:22:11 -0400

    ed map method to transform each TaMapping method into its ta_id

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -26,52 +26,52 @@ class Instructor < User
 
   def my_tas
     courses = Course.where(instructor_id: id)
-    ta_ids = []
-    courses.each do |course|
+    ta_ids = courses.flat_map do |course|
       ta_mappings = TaMapping.where(course_id: course.id)
-      ta_mappings.each { |mapping| ta_ids << mapping.ta_id } unless ta_mappings.empty?
+      ta_mappings.map(&:ta_id)
     end
     ta_ids
   end
+  
 
-# This method retrieves a list of users who are participants in the courses and assignments where the given user is an instructor.
-def self.get_user_list(user)
-  user_list = []
-
-  # Retrieve participants from courses where the user is an instructor
-  user_list.concat(get_participants_from_instructed_entities(Course, user))
-
-  # Retrieve participants from assignments where the user is an instructor
-  user_list.concat(get_participants_from_instructed_entities(Assignment.includes(:participants), user))
-
-  # Return the list of users
-  user_list
-end
-
-# This method retrieves the participants from the entities (either Course or Assignment) where the given user is an instructor.
-def self.get_participants_from_instructed_entities(entity, user)
-  # Get all entities where the user is an instructor
-  entities = entity.where(instructor_id: user.id)
-
-  # For each entity, get its participants and filter them based on the user's privileges
-  entities.flat_map do |entity|
-    # Check if the entity has a get_participants method (as in Course) or directly has a participants association (as in Assignment)
-    participants = entity.respond_to?(:get_participants) ? entity.get_participants : entity.participants
-
-    # Filter the participants based on the user's privileges
-    filter_participants(participants, user)
+  # This method retrieves a list of users who are participants in the courses and assignments where the given user is an instructor.
+  def self.get_user_list(user)
+    user_list = []
+  
+    # Retrieve participants from courses where the user is an instructor
+    user_list.concat(get_participants_from_instructed_entities(Course, user))
+  
+    # Retrieve participants from assignments where the user is an instructor
+    user_list.concat(get_participants_from_instructed_entities(Assignment.includes(:participants), user))
+  
+    # Return the list of users
+    user_list
   end
-end
-
-# This method filters a list of participants based on whether the given user has all privileges of each participant's role.
-def self.filter_participants(participants, user)
-  # Select only those participants whose role's privileges are all included in the user's role's privileges
-  filtered_participants = participants.select do |participant|
-    user.role.has_all_privileges_of?(participant.user.role)
+  
+  # This method retrieves the participants from the entities (either Course or Assignment) where the given user is an instructor.
+  def self.get_participants_from_instructed_entities(entity, user)
+    # Get all entities where the user is an instructor
+    entities = entity.where(instructor_id: user.id)
+  
+    # For each entity, get its participants and filter them based on the user's privileges
+    entities.flat_map do |entity|
+      # Check if the entity has a get_participants method (as in Course) or directly has a participants association (as in Assignment)
+      participants = entity.respond_to?(:get_participants) ? entity.get_participants : entity.participants
+    
+      # Filter the participants based on the user's privileges
+      filter_participants(participants, user)
+    end
   end
-
-  # Map each participant to its associated user and return this list of users
-  filtered_participants.map(&:user)
-end
+  
+  # This method filters a list of participants based on whether the given user has all privileges of each participant's role.
+  def self.filter_participants(participants, user)
+    # Select only those participants whose role's privileges are all included in the user's role's privileges
+    filtered_participants = participants.select do |participant|
+      user.role.has_all_privileges_of?(participant.user.role)
+    end
+  
+    # Map each participant to its associated user and return this list of users
+    filtered_participants.map(&:user)
+  end
 
 end

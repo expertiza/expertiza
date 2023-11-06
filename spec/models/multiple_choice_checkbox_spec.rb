106999282c974d4ba697e9eef5e0dcae747a9b27
opheliasin <76908733+opheliasin@users.noreply.github.com>
2023-11-06 13:29:18 -0500

    removed duplicate test in multiple_choice_checkbox_spec.rb

--- spec/models/multiple_choice_checkbox_spec.rb
+++ spec/models/multiple_choice_checkbox_spec.rb
@@ -13,8 +13,7 @@ describe MultipleChoiceCheckbox do
       allow(QuizQuestionChoice).to receive(:where).with(question_id: 1).and_return([qc, qc, qc, qc])
       allow(qc).to receive(:iscorrect).and_return(true)
       allow(qc).to receive(:txt).and_return('question text')
-      # expect(multiple_choice_checkbox.edit).to eq('<tr><td><textarea cols="100" name="question[1][txt]" id="question_1_txt">Test question:</textarea></td></tr><tr><td>Question Weight: <input type="number" name="question_weights[1][txt]" id="question_wt_1_txt" value="1" min="0" /></td></tr><tr><td><input type="hidden" name="quiz_question_choices[1][MultipleChoiceCheckbox][1][iscorrect]" id="quiz_question_choices_1_MultipleChoiceCheckbox_1_iscorrect" value="0" /><input type="checkbox" name="quiz_question_choices[1][MultipleChoiceCheckbox][1][iscorrect]" id="quiz_question_choices_1_MultipleChoiceCheckbox_1_iscorrect" value="1" checked="checked" /><input type="text" name="quiz_question_choices[1][MultipleChoiceCheckbox][1][txt]" id="quiz_question_choices_1_MultipleChoiceCheckbox_1_txt" value="question text" size="40" /></td></tr><tr><td><input type="hidden" name="quiz_question_choices[1][MultipleChoiceCheckbox][2][iscorrect]" id="quiz_question_choices_1_MultipleChoiceCheckbox_2_iscorrect" value="0" /><input type="checkbox" name="quiz_question_choices[1][MultipleChoiceCheckbox][2][iscorrect]" id="quiz_question_choices_1_MultipleChoiceCheckbox_2_iscorrect" value="1" checked="checked" /><input type="text" name="quiz_question_choices[1][MultipleChoiceCheckbox][2][txt]" id="quiz_question_choices_1_MultipleChoiceCheckbox_2_txt" value="question text" size="40" /></td></tr><tr><td><input type="hidden" name="quiz_question_choices[1][MultipleChoiceCheckbox][3][iscorrect]" id="quiz_question_choices_1_MultipleChoiceCheckbox_3_iscorrect" value="0" /><input type="checkbox" name="quiz_question_choices[1][MultipleChoiceCheckbox][3][iscorrect]" id="quiz_question_choices_1_MultipleChoiceCheckbox_3_iscorrect" value="1" checked="checked" /><input type="text" name="quiz_question_choices[1][MultipleChoiceCheckbox][3][txt]" id="quiz_question_choices_1_MultipleChoiceCheckbox_3_txt" value="question text" size="40" /></td></tr><tr><td><input type="hidden" name="quiz_question_choices[1][MultipleChoiceCheckbox][4][iscorrect]" id="quiz_question_choices_1_MultipleChoiceCheckbox_4_iscorrect" value="0" /><input type="checkbox" name="quiz_question_choices[1][MultipleChoiceCheckbox][4][iscorrect]" id="quiz_question_choices_1_MultipleChoiceCheckbox_4_iscorrect" value="1" checked="checked" /><input type="text" name="quiz_question_choices[1][MultipleChoiceCheckbox][4][txt]" id="quiz_question_choices_1_MultipleChoiceCheckbox_4_txt" value="question text" size="40" /></td></tr>')
-      # assuming `multiple_choice_checkbox.edit` returns an HTML fragment
+
       html = Nokogiri::HTML(multiple_choice_checkbox.edit)
 
       # Test for presence of a text area for the question text
@@ -32,13 +31,6 @@ describe MultipleChoiceCheckbox do
     end
   end
   describe '#isvalid' do
-    # context 'when the question itself does not have txt' do
-     # it 'returns "Please make sure all questions have text"' do
-      #  allow(multiple_choice_checkbox).to receive(:txt).and_return('')
-       # questions = { '1' => { txt: 'question text', iscorrect: '1' }, '2' => { txt: 'question text', iscorrect: '1' }, '3' => { txt: 'question text', iscorrect: '0' }, '4' => { txt: 'question text', iscorrect: '0' } }
-       # expect(multiple_choice_checkbox.isvalid(questions)).to eq('Please make sure all questions have text')
-     # end
-    #end
     context 'when a choice does not have txt' do
       it 'returns "Please make sure every question has text for all options"' do
        choices = { '1' => { txt: '', iscorrect: '1' }, '2' => { txt: '', iscorrect: '1' }, '3' => { txt: '', iscorrect: '0' }, '4' => { txt: '', iscorrect: '0' } }

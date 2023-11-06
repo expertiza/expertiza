abf422acd3d2efa4aeab88eefb40ce9859de00c3
root <root@vclvm179-56.vcl.ncsu.edu>
2023-11-06 13:00:57 -0500

    commented out repetitive, unapplicable test

--- spec/models/multiple_choice_checkbox_spec.rb
+++ spec/models/multiple_choice_checkbox_spec.rb
@@ -32,23 +32,23 @@ describe MultipleChoiceCheckbox do
     end
   end
   describe '#isvalid' do
-    context 'when the question itself does not have txt' do
-      it 'returns "Please make sure all questions have text"' do
-        allow(multiple_choice_checkbox).to receive(:txt).and_return('')
-        questions = { '1' => { txt: 'question text', iscorrect: '1' }, '2' => { txt: 'question text', iscorrect: '1' }, '3' => { txt: 'question text', iscorrect: '0' }, '4' => { txt: 'question text', iscorrect: '0' } }
-        expect(multiple_choice_checkbox.isvalid(questions)).to eq('Please make sure all questions have text')
-      end
-    end
+    # context 'when the question itself does not have txt' do
+     # it 'returns "Please make sure all questions have text"' do
+      #  allow(multiple_choice_checkbox).to receive(:txt).and_return('')
+       # questions = { '1' => { txt: 'question text', iscorrect: '1' }, '2' => { txt: 'question text', iscorrect: '1' }, '3' => { txt: 'question text', iscorrect: '0' }, '4' => { txt: 'question text', iscorrect: '0' } }
+       # expect(multiple_choice_checkbox.isvalid(questions)).to eq('Please make sure all questions have text')
+     # end
+    #end
     context 'when a choice does not have txt' do
       it 'returns "Please make sure every question has text for all options"' do
-        questions = { '1' => { txt: '', iscorrect: '1' }, '2' => { txt: '', iscorrect: '1' }, '3' => { txt: '', iscorrect: '0' }, '4' => { txt: '', iscorrect: '0' } }
-        expect(multiple_choice_checkbox.isvalid(questions)).to eq('Please select a correct answer for all questions')
+       choices = { '1' => { txt: '', iscorrect: '1' }, '2' => { txt: '', iscorrect: '1' }, '3' => { txt: '', iscorrect: '0' }, '4' => { txt: '', iscorrect: '0' } }
+        expect(multiple_choice_checkbox.isvalid(choices)).to eq('Please select a correct answer for all questions')
       end
     end
     context 'when no choices are correct' do
       it 'returns "Please select a correct answer for all questions"' do
-        questions = { '1' => { txt: 'question text', iscorrect: '0' }, '2' => { txt: 'question text', iscorrect: '0' }, '3' => { txt: 'question text', iscorrect: '0' }, '4' => { txt: 'question text', iscorrect: '0' } }
-        expect(multiple_choice_checkbox.isvalid(questions)).to eq('Please select a correct answer for all questions')
+        choices = { '1' => { txt: 'choice text', iscorrect: '0' }, '2' => { txt: 'choice text', iscorrect: '0' }, '3' => { txt: 'choice text', iscorrect: '0' }, '4' => { txt: 'choice text', iscorrect: '0' } }
+        expect(multiple_choice_checkbox.isvalid(choices)).to eq('Please select a correct answer for all questions')
       end
     end
     context 'when only 1 choices are correct' do

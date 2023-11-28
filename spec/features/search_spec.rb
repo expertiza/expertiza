3c5a56159337a43b33a2448dcea77e0c534b1a5f
Nainisha Bhallamudi <nbhalla@ncsu.edu>
2023-11-28 15:30:00 -0500

    Feature Test

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -1,33 +1,26 @@
-# spec/features/search_spec.rb
+describe 'Search functionality' do
+    before do
+      # Create necessary records using FactoryBot
+      @course = create(:course)
+      @student = create(:student)
+      @assignment = create(:assignment, course: @course)
+      @course_participant = create(:course_participant, user: @student, course: @course)
+    end
+  
+    it 'filters students based on search input', js: true do
 
-require 'rails_helper'
-
-RSpec.feature 'Search functionality', type: :feature, js: true do
-  before do
-    # Assuming you have some setup code here, e.g., creating test data
-    # or navigating to the page where the search functionality is present
+      login_as('instructor6')
+  
+      # Visit the page where the search functionality is implemented
+      visit "/assessment360/all_students_all_reviews?course_id=#{@course.id}"
+  
+      # Perform a search with a known student name
+      fill_in 'studentSearch', with: @student.fullname
+      # Add other necessary steps for triggering the search
+  
+      # Validate that the displayed table only contains the searched student's information
+      page.all('#myTable tr').each do |tr|
+        expect(tr).to have_content?(@student.fullname)
+      end
+    end
   end
-
-  scenario 'User can search for a student' do
-    visit '/assessment360/all_students_all_reviews?course_id=243'
-
-    # Assuming you have some code here to wait for the JavaScript to load
-    # If you are using a single-page application framework, you might need
-    # to wait for specific elements to be present or visible
-
-    # Type the search query in the input field
-    fill_in 'studentSearch', with: 'student9163'  # Replace 'John Doe' with a valid student name
-
-    # Optionally, you can add some waiting time if necessary
-    # sleep 1
-
-    # Check if the table rows are updated according to the search query
-    expect(page).to have_css('table#myTable tr', count: expected_row_count)
-
-    # You can add more specific expectations based on your JavaScript logic
-    # For example, you might want to check if certain rows are visible or hidden
-    expect(page).to have_css('table#myTable tr:visible', text: 'student9163')
-
-    # Add more expectations as needed based on your specific JavaScript logic
-  end
-end

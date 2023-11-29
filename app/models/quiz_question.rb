0d7b8b573f8c5a2a2a2c0eea1562c641050ad876
opheliasin <76908733+opheliasin@users.noreply.github.com>
2023-11-29 17:00:56 -0500

    refactored quiz_question.rb

--- app/models/quiz_question.rb
+++ app/models/quiz_question.rb
@@ -2,41 +2,19 @@ class QuizQuestion < Question
   has_many :quiz_question_choices, class_name: 'QuizQuestionChoice', foreign_key: 'question_id', inverse_of: false, dependent: :nullify
   def edit
     @quiz_question_choices = QuizQuestionChoice.where(question_id: id)
-
-    @html = '<tr><td>'
-    @html += '<textarea cols="100" name="question[' + id.to_s + '][txt]" '
-    @html += 'id="question_' + id.to_s + '_txt">' + txt + '</textarea>'
-    @html += '</td></tr>'
-
-    @html += '<tr><td>'
-    @html += 'Question Weight: '
-    @html += '<input type="number" name="question_weights[' + id.to_s + '][txt]" '
-    @html += 'id="question_wt_' + id.to_s + '_txt" '
-    @html += 'value="' + weight.to_s + '" min="0" />'
-    @html += '</td></tr>'
-
+    @html = create_html_content
   end
 
   def complete
     @quiz_question_choices = QuizQuestionChoice.where(question_id: id)
-    @html = '<label for="' + id.to_s + '">' + txt + '</label><br>'
-
+    @html = create_label
   end
 
   def view_question_text
-    @html = '<b>' + txt + '</b><br />'
-    @html += 'Question Type: ' + type + '<br />'
-    @html += 'Question Weight: ' + weight.to_s + '<br />'
-    if quiz_question_choices
-      quiz_question_choices.each do |choices|
-        @html += if choices.iscorrect?
-                  '  - <b>' + choices.txt + '</b><br /> '
-                else
-                  '  - ' + choices.txt + '<br /> '
-                end
-      end
-      @html += '<br />'
-    end
+    @html = "<b>#{txt}</b><br />"
+    @html << 'Question Type: ' + type + '<br />'
+    @html << 'Question Weight: ' + weight.to_s + '<br />'
+    @html << create_choices if quiz_question_choices.present?
     @html.html_safe
   end
 
@@ -47,4 +25,40 @@ class QuizQuestion < Question
     @valid
   end
 
+  private
+
+  def create_html_content
+    html_content = ''
+    html_content << create_textarea_row
+    html_content << create_weight_input_row
+    html_content
+  end
+
+  def create_textarea_row
+    "<tr><td><textarea cols='100' name='question[#{id}][txt]' id='question_#{id}_txt'>#{txt}</textarea></td></tr>"
+  end
+  
+  def create_weight_input_row
+    "<tr><td>Question Weight: <input type='number' name='question_weights[#{id}][txt]' id='question_wt_#{id}_txt' value='#{weight}' min='0' /></td></tr>"
+  end
+
+  def create_label
+    "<label for='#{id}'>#{txt}</label><br>"
+  end
+
+  def build_choices
+    choices_html = ''
+    quiz_question_choices.each do |choice|
+      choices_html << choice_html(choice)
+    end
+    choices_html
+  end
+  
+  def choice_html(choice)
+    if choice.iscorrect?
+      "  - <b>#{choice.txt}</b><br/> "
+    else
+      "  - #{choice.txt}<br/> "
+    end
+  end 
 end

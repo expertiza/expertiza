dc209decc3650dd34c8b3453debdb8d10ef7b125
Harsh Rajesh Kumar Mauny <hrmauny@vclvm177-78.vcl.ncsu.edu>
2023-12-04 14:51:29 -0500

    code climate issues

--- spec/helpers/review_mapping_helper_spec.rb
+++ spec/helpers/review_mapping_helper_spec.rb
@@ -979,21 +979,9 @@ describe ReviewMappingHelper, type: :helper do
       expect(result[:stand_dev]).to eq(1.71)
     end
     it 'returns the mean, min, max, variance, and standard deviation of the intervals' do
-      expect(calculate_key_chart_information([10, 15, 20])).to eq({
-        mean: 15.0,
-        min: 10,
-        max: 20,
-        variance: 16.67,
-        stand_dev: 4.08
-      })
+      expect(calculate_key_chart_information([10, 15, 20])).to eq( mean: 15.0, min: 10, max: 20, variance: 16.67, stand_dev: 4.08)
 
-      expect(calculate_key_chart_information([5, 8, 12, 15, 20])).to eq({
-        mean: 12.0,
-        min: 5,
-        max: 20,
-        variance: 27.6,
-        stand_dev: 5.25
-      })
+      expect(calculate_key_chart_information([5, 8, 12, 15, 20])).to eq(mean: 12.0, min: 5, max: 20, variance: 27.6, stand_dev: 5.25)
     end
     context 'when intervals are empty' do
       it 'returns an empty hash' do
@@ -1009,32 +997,32 @@ describe ReviewMappingHelper, type: :helper do
   end
 
   describe 'test calibration_report_css_class' do
-    context "when the difference is 0" do
-      it 'should return correct css class' do
+    context 'when the difference is 0' do
+      it 'returns c5 as the CSS class' do
         css_class_0 = helper.calibration_report_css_class(0)
         expect(css_class_0). to eq('c5')
       end
     end
-    context "when the difference is 1" do
-      it "returns 'c4' as the CSS class" do
+    context 'when the difference is 1' do
+      it 'returns c4 as the CSS class' do
         css_class_1 = helper.calibration_report_css_class(-1)
         expect(css_class_1). to eq('c4')
       end
     end
-    context "when the difference is 2" do
-      it "returns 'c3' as the CSS class" do
+    context 'when the difference is 2' do
+      it 'returns c3 as the CSS class' do
         css_class_2 = helper.calibration_report_css_class(-2)
         expect(css_class_2). to eq('c3')
       end
     end
-    context "when the difference is 3" do
-      it "returns 'c2' as the CSS class" do
+    context 'when the difference is 3' do
+      it 'returns c2 as the CSS class' do
         css_class_3 = helper.calibration_report_css_class(-3)
         expect(css_class_3). to eq('c2')
       end
     end
-    context "when the difference is greater than 3" do
-      it "returns 'c1' as the CSS class" do
+    context 'when the difference is greater than 3' do
+      it 'returns c1 as the CSS class' do
         css_class_3 = helper.calibration_report_css_class(6)
         expect(css_class_3). to eq('c1')
       end

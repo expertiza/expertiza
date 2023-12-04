f45e8ee6d442d9ed04c54a318b09047371f2080b
Helen Solomon <menberesolo@gmail.com>
2023-12-03 19:20:37 -0500

    Fixing typo in controller file

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,4 +1,4 @@
-ass ParticipantsController < ApplicationController
+class ParticipantsController < ApplicationController
   include AuthorizationHelper
   include ParticipantsHelper
   autocomplete :user, :name

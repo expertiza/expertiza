1d88245a329d2fd7debd82a4c1e58a3a6dfcb2aa
KPichon02 <81699761+KPichon02@users.noreply.github.com>
2023-12-05 18:36:35 -0500

    Update team.rb

--- app/models/team.rb
+++ app/models/team.rb
@@ -126,7 +126,7 @@ class Team < ApplicationRecord
   # Start by adding single members to teams that are one member too small.
   # Add two-member teams to teams that two members too small. etc.
   def self.randomize_all_by_parent(parent, team_type, min_team_size)
-    participants = Participant.where(parent_id: parent.id, type: parent.class.to_s + 'Participant', can_mentor: false)
+    participants = Participant.where(parent_id: parent.id, type: parent.class.to_s + 'Participant', can_mentor: [false, nil])
     participants = participants.sort { rand(-1..1) }
     users = participants.map { |p| User.find(p.user_id) }.to_a
     # find teams still need team members and users who are not in any team

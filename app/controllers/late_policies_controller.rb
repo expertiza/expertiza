64ed44396389eaec036ab665bf7fab92ce791720
alecb100 <aleccb27@gmail.com>
2023-12-05 00:41:07 -0500

    Removed a commented out method

--- app/controllers/late_policies_controller.rb
+++ app/controllers/late_policies_controller.rb
@@ -17,15 +17,6 @@ class LatePoliciesController < ApplicationController
     end
   end
 
-  # # This method lists all the late policies records from late_policies table in database.
-  # def index
-  #   @penalty_policies = LatePolicy.where(['instructor_id = ? OR private = 0', instructor_id])
-  #   respond_to do |format|
-  #     format.html # index.html.erb
-  #     format.xml  { render xml: @penalty_policies }
-  #   end
-  # end
-
   def index
     @penalty_policies = LatePolicy.includes([:user]).where(['instructor_id = ? OR private = 0', instructor_id])
     respond_to do |format|

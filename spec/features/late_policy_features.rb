0e4a2bd3fc52b31054d1510b9af9779fc42e7328
Shaival Pradipbhai Shah <sshah35@vclvm177-158.vcl.ncsu.edu>
2024-03-22 18:27:30 -0400

    Add some Rspec tests

---
+++ spec/features/late_policy_features.rb
@@ -0,0 +1,27 @@
+require_relative 'features/helpers/assignment_creation_helper'
+
+describe 'Assignment creation topics tab', js: true do
+  include AssignmentCreationHelper
+  before(:each) do
+    create_deadline_types
+    (1..3).each do |i|
+      create(:course, name: "Course #{i}")
+    end
+    assignment = create(:assignment, name: 'assignment for late policy test')
+    login_as('instructor6')
+    visit "/assignments/#{assignment.id}/edit"
+    check('assignment_has_due_dates')
+    click_link 'Due Dates'
+  end
+
+  it 'Flash Error Message on New Late Policy Page', js: true do
+    assignment = Assignment.where(name: 'assignment for late policy test').first
+    create(:topic, assignment_id: assignment.id)
+    visit "/assignments/#{assignment.id}/edit"
+    click_link 'Due Dates'
+    click_link 'New Late Policy'
+    fails if 
+      expect(flash[:error]).to be('Failed to save the assignment: ') 
+    end
+  end
+end

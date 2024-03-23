ee93ef53fb352cff07b495d66e92c4c54e2f5667
Daksh Mehta <daksh.mehta235@gmail.com>
2024-03-22 23:05:52 -0400

    Update test cases to use copy_assignment_participants

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -76,7 +76,7 @@ describe CourseTeam do
       end
     end
   end
-  describe '#copy_participants' do
+  describe '#copy_assignment_participants' do
     context 'when there are errors' do
       it 'raises an error to the user' do
         allow(AssignmentParticipant).to receive(:where).with(parent_id: 1).and_return([participant, participant2])
@@ -86,7 +86,7 @@ describe CourseTeam do
         allow(participant2).to receive(:user_id).and_return(2)
         allow(course).to receive(:add_participant).with('abc').and_raise('The user abc is already a participant.', StandardError)
         allow(course).to receive(:add_participant).with('bcd').and_raise('The user bcd is already a participant.', StandardError)
-        expect { course.copy_participants(1) }.to raise_error(TypeError)
+        expect { course.copy_assignment_participants(1) }.to raise_error(TypeError)
       end
     end
     context 'when there are no errors' do
@@ -99,7 +99,7 @@ describe CourseTeam do
         allow(course).to receive(:add_participant).with('abc').and_return(participant)
         allow(course).to receive(:add_participant).with('bcd').and_return(participant2)
         allow(course).to receive(:participants).and_return([participant, participant2])
-        expect(course.copy_participants(1)).to eq(nil)
+        expect(course.copy_assignment_participants(1)).to eq(nil)
         expect(course.participants.length).to eq(2)
       end
     end

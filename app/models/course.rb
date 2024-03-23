5e490a02e926fc13b2a170fd6209bedabc6c6a4f
Daksh Mehta <daksh.mehta235@gmail.com>
2024-03-22 23:14:33 -0400

    Add comments to course.rb

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,8 @@
 class Course < ApplicationRecord
+  # Defines the Course model which represents a course in the application.
   enum locale: Locale.code_name_to_db_encoding
   has_many :ta_mappings, dependent: :destroy
-  has_many :tas, through: :ta_mappings
+  has_many :tas, through: :ta_mappings  # Establishes associations with other models.
   has_many :assignments, dependent: :destroy
   belongs_to :instructor, class_name: 'User', foreign_key: 'instructor_id'
   belongs_to :institution, foreign_key: 'institutions_id'
@@ -10,11 +11,14 @@ class Course < ApplicationRecord
   has_one :course_node, foreign_key: 'node_object_id', dependent: :destroy
   has_many :notifications, dependent: :destroy
   has_paper_trail
+  # Validates presence of name and directory_path.
   validates :name, presence: true
   validates :directory_path, presence: true
   # Return any predefined teams associated with this course
   # Author: ajbudlon
   # Date: 7/21/2008
+
+  # Returns any predefined teams associated with this course.
   def get_teams
     CourseTeam.where(parent_id: id)
   end
@@ -25,11 +29,11 @@ class Course < ApplicationRecord
 
     Rails.root + '/pg_data/' + FileHelper.clean_path(User.find(instructor_id).name) + '/' + FileHelper.clean_path(directory_path) + '/'
   end
-
+# Returns associated participants with the course.
   def get_participants
     CourseParticipant.where(parent_id: id)
   end
-
+# Adds a participant to the course.
   def add_participant(user_name)
     user = User.find_by(name: user_name)
     if user.nil?
@@ -43,7 +47,7 @@ class Course < ApplicationRecord
       CourseParticipant.create(parent_id: id, user_id: user.id, permission_granted: user.master_permission_granted)
     end
   end
-
+# Copies assignment participants to the course.
   def copy_assignment_participants(assignment_id)
     participants = AssignmentParticipant.where(parent_id: assignment_id)
     errors = []
@@ -64,7 +68,7 @@ class Course < ApplicationRecord
       raise error_msg
     end
   end
-
+# Checks if the user is on a team associated with the assignment.
   def user_on_team?(user)
     teams = get_teams
     users = []

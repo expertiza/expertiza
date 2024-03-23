7d77ca8ef695d4379ce4fa0b296410938219951e
root <root@vclvm177-127.vcl.ncsu.edu>
2024-03-23 19:35:33 -0400

    GradingHistory refactor controller function to model

--- app/models/grading_history.rb
+++ app/models/grading_history.rb
@@ -1,4 +1,19 @@
 class GradingHistory < ActiveRecord::Base
   belongs_to :instructor
   belongs_to :assignment
+
+  # populate the assignment fields according to type
+  def assignment_for_history(type, assignment_team, participant_id)
+    # for a submission, the receiver is an AssignmentTeam
+    # use this AssignmentTeam to find the assignment
+    if type.eql? 'Submission'
+      @assignment = Assignment.find(assignment_team.parent_id)
+    end
+    # for a review, the receiver is an AssignmentParticipant
+    # use this AssignmentParticipant to find the assignment
+    if type.eql? 'Review'
+      graded_member = AssignmentParticipant.find(participant_id)
+      @assignment = ReviewGrade.find_graded_member(graded_member)
+    end
+  end
 end
--- app/models/review_grade.rb
+++ app/models/review_grade.rb
@@ -1,3 +1,7 @@
 class ReviewGrade < ApplicationRecord
   belongs_to :participant
+
+  def find_graded_member(graded_member)
+    Assignment.find(graded_member.parent_id)
+  end
 end

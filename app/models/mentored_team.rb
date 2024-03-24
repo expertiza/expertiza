cf4c60ad8deeeadb9416e860534b9a936ccf595b
ExtremeMachine12 <kwiatkowskislmartin@gmail.com>
2024-03-24 16:51:56 -0400

    I went ahead and fixed a lot of the easy to fix code climate issues

--- app/models/mentored_team.rb
+++ app/models/mentored_team.rb
@@ -1,7 +1,7 @@
 class MentoredTeam < AssignmentTeam
     # Class created during refactoring of E2351
     # Overridden method to include the MentorManagement workflow
-    def add_member(user, _assignment_id = nil)
+    def add_member(user, assignment_id = nil)
         raise "The user #{user.name} is already a member of the team #{name}" if user?(user)
     
         can_add_member = false
@@ -12,15 +12,15 @@ class MentoredTeam < AssignmentTeam
           TeamUserNode.create(parent_id: parent.id, node_object_id: t_user.id)
           add_participant(parent_id, user)
           ExpertizaLogger.info LoggerMessage.new('Model:Team', user.name, "Added member to the team #{id}")
-          assignment_name = _assignment_id ? Assignment.find(_assignment_id).name.to_s : ""
+          assignment_name = assignment_id ? Assignment.find(assignment_id).name.to_s : ''
           if MentorManagement.user_a_mentor?(user)
-            MailerHelper.send_team_confirmation_mail_to_user(user, "[Expertiza] Added to a Team", "mentor_added_to_team", "#{name}", assignment_name).deliver
+            MailerHelper.send_team_confirmation_mail_to_user(user, '[Expertiza] Added to a Team', 'mentor_added_to_team', name.to_s, assignment_name).deliver
           elsif !user.is_a?(Participant)
-            MailerHelper.send_team_confirmation_mail_to_user(user, "[Expertiza] Added to a Team", "user_added_to_team", "#{name}", assignment_name).deliver
+            MailerHelper.send_team_confirmation_mail_to_user(user, '[Expertiza] Added to a Team', 'user_added_to_team', name.to_s, assignment_name).deliver
           end
         end
         if can_add_member
-            MentorManagement.assign_mentor(_assignment_id, id)
+            MentorManagement.assign_mentor(assignment_id, id)
         end
         can_add_member
     end

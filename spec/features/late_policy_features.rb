615d7ac13b3cd5a90e486ba73339ea5ad41c0a7f
Shaival Pradipbhai Shah <sshah35@vclvm177-158.vcl.ncsu.edu>
2024-03-24 16:19:18 -0400

    resolve spelling issues

--- spec/features/late_policy_features.rb
+++ spec/features/late_policy_features.rb
@@ -25,7 +25,7 @@ describe 'Assignment creation topics tab', js: true do
     end
   end
 
-it 'Back Button Interation on New Late Policy Page', js: true do
+it 'Back Button Interaction on New Late Policy Page', js: true do
     assignment = Assignment.where(name: 'assignment for late policy test').first
     create(:topic, assignment_id: assignment.id)
     visit "/assignments/#{assignment.id}/edit"

872e1b945e6ee8d72d4c3d50a92de08f20158609
root <root@vclvm178-104.vcl.ncsu.edu>
2024-03-24 17:51:31 -0400

    Revert "Modified"
    This reverts commit ea96f9f7c3297e0451ae473ea4de713f5bb67182.

--- spec/models/course_team_spec.rb
+++ spec/models/course_team_spec.rb
@@ -12,7 +12,7 @@ describe 'CourseTeam' do
       assignment_team = AssignmentTeam.new
       assignment_team.save
       course_team = CourseTeam.new
-      course_team.copy_to_assignment_team(assignment_team.id)
+      course_team.copy(assignment_team.id)
       expect(AssignmentTeam.create_team_and_node(assignment_team.id))
       expect(assignment_team.copy_members(assignment_team.id))
     end
@@ -23,12 +23,12 @@ describe 'CourseTeam' do
       expect(course_team.parent_model).to eq('Course')
     end
   end
-  #describe '#assignment_id' do
-    #it 'returns nil since this team is not an assignment team' do
-      #course_team = CourseTeam.new
-      #expect(course_team.assignment_id).to be_nil
-    #end
-  #end
+  describe '#assignment_id' do
+    it 'returns nil since this team is not an assignment team' do
+      course_team = CourseTeam.new
+      expect(course_team.assignment_id).to be_nil
+    end
+  end
   describe '#prototype' do
     it 'creates a course team' do
       expect(CourseTeam.prototype.class).to eq(CourseTeam)

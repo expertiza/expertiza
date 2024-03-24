6aa9bf9a3b1d8fee0ae9ca42de2931ad48a0c38d
Shaival Pradipbhai Shah <sshah35@vclvm177-158.vcl.ncsu.edu>
2024-03-24 18:25:54 -0400

    fix codeclimate issues

--- spec/features/late_policy_features.rb
+++ spec/features/late_policy_features.rb
@@ -20,8 +20,8 @@ describe 'Assignment creation topics tab', js: true do
   #   visit "/assignments/#{assignment.id}/edit"
   #   click_link 'Due Dates'
   #   click_link 'New Late Policy'
-  #   fails if 
-  #     expect(flash[:error]).to be('Failed to save the assignment: ') 
+  #   fails if
+  #     expect(flash[:error]).to be('Failed to save the assignment: ')
   #   end
   # end
 
@@ -32,7 +32,7 @@ describe 'Assignment creation topics tab', js: true do
     click_button 'Back'
     expect(page).to route_to("/assignments/#{assignment.id}/edit")
   end
-  
+
   it 'Back Button Interaction on New Late Policy Page while creating', js: true do
     assignment = create_assignment_and_topic('assignment for late policy test')
     visit_assignment_due_dates_tab(assignment)
@@ -44,16 +44,14 @@ describe 'Assignment creation topics tab', js: true do
   end
   
   private
-  
   def create_assignment_and_topic(assignment_name)
     assignment = Assignment.where(name: assignment_name).first || create(:assignment, name: assignment_name)
     create(:topic, assignment_id: assignment.id)
     assignment
   end
-  
   def fill_in_late_policy_form(name, penalty_per_unit, max_penalty)
     fill_in "policy_name", with: name
     fill_in "penalty_per_unit", with: penalty_per_unit
     fill_in "max_penalty", with: max_penalty
   end
-end 
+end

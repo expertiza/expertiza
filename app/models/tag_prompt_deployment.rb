dc02ba46179186cba645d0068c628c91fdbc72ae
root <root@vclvm177-36.vcl.ncsu.edu>
2024-04-09 11:16:57 -0400

    Updated tagged answers repor: modified file,s tag_prompt_deployment.rb, _answer_tagging_report.html.erb

--- app/models/tag_prompt_deployment.rb
+++ app/models/tag_prompt_deployment.rb
@@ -5,6 +5,7 @@ class TagPromptDeployment < ApplicationRecord
   has_many :answer_tags, dependent: :destroy
 
   require 'time'
+  include ReviewMappingHelper
 
   def tag_prompt
     TagPrompt.find(tag_prompt_id)
@@ -75,5 +76,116 @@ class TagPromptDeployment < ApplicationRecord
       end
     end
     user_answer_tagging
+  end
+
+  # You can add groups of fields to the hashmap
+  EXPORT_FIELDS = { team_score: ['Username', 'Name', '% of Tagged Answers', 'Tagged Answers', 'Not Tagged Answers', 'Intevals', 'Mean', 'Min', 'Variance', 'Standard', 'Deviation', 'Taggable Answers']}
+  def self.export_fields(options)
+    fields = []
+    EXPORT_FIELDS.each do |key, value|
+      value.each do |f|
+        fields.push(f)
+      end
+    end
+    fields
+  end
+  
+  # This method is used for export contents of answerTag#view
+  def self.export(csv, assignment_id, option)
+    assignment = Assignment.find(assignment_id)
+    tag_prompt_deployments = TagPromptDeployment.where(assignment_id: assignment.id)
+    questionnaire_tagging_report = {}
+    user_tagging_report = {}
+    tag_prompt_deployments.each do |tag_dep|
+      questionnaire_tagging_report[tag_dep] = tag_dep.assignment_tagging_progress
+      questionnaire_tagging_report[tag_dep].each do |line|
+        self.tagged_user_summary(user_tagging_report, line)
+      end
+    end
+    sheets = self.export_data(questionnaire_tagging_report, option)
+
+    puts "----------------sheets: #{sheets.inspect}---------------------------------"
+    sheets.each do |sheet, rows|
+      puts "----------------sheet: #{sheet.inspect}---------------------------------"
+      puts "----------------rows: #{rows.inspect}---------------------------------"
+      csv << [sheet]
+      rows.each do |row|
+        csv << row
+      end
+    end
+    
+  end
+
+  def self.tagged_user_summary(user_tagging_report, line)
+    
+    if user_tagging_report[line.user.name].nil?
+      # E2082 Adding extra field of interval array into data structure
+      user_tagging_report[line.user.name] = VmUserAnswerTagging.new(line.user, line.percentage, line.no_tagged, line.no_not_tagged, line.no_tagable, line.tag_update_intervals)
+    else
+      user_tagging_report[line.user.name].no_tagged += line.no_tagged
+      user_tagging_report[line.user.name].no_not_tagged += line.no_not_tagged
+      user_tagging_report[line.user.name].no_tagable += line.no_tagable
+      user_tagging_report[line.user.name].percentage = self.calculate_formatted_percentage(user_tagging_report[line.user.name])
+    end
+  end
+
+  def self.export_data(questionnaire_tagging_report, options)
+    sheets = {}
+    if questionnaire_tagging_report.nil? || questionnaire_tagging_report.count < 1
+      sheets['sheet 1'] = ['No answer tags are found for this assignment']
+    else
+      questionnaire_tagging_report.each do |tag_dep, report_lines|
+        sheetName = tag_dep.questionnaire.name + " (" + tag_dep.tag_prompt.prompt + ")"
+        sheets[sheetName] = []
+        report_lines.each do |report_line|
+          row = []
+          row.push(report_line.user.name)
+          row.push(report_line.user.fullname)
+          row.push(report_line.percentage.to_s)
+          row.push(report_line.no_tagged.to_s)
+          row.push(report_line.no_not_tagged.to_s)
+          row.push(report_line.tag_update_intervals)
+          key_chart_information = self.calculate_key_chart_information(report_line.tag_update_intervals)
+          puts "----------------line.key_chart_information: #{key_chart_information.inspect}---------------------------------"
+          
+          row.push(key_chart_information.nil? ? nil : key_chart_information[:mean])
+          row.push(key_chart_information.nil? ? nil : key_chart_information[:min])
+          row.push(key_chart_information.nil? ? nil : key_chart_information[:max])
+          row.push(key_chart_information.nil? ? nil : key_chart_information[:variance])
+          row.push(key_chart_information.nil? ? nil : key_chart_information[:stand_dev])
+          
+          row.push(report_line.no_tagable)
+          sheets[sheetName].push(row)
+        end
+      end
+      sheets
+    end
+  end
+
+  def self.calculate_key_chart_information(intervals)
+    # if someone did not do any tagging in 30 seconds, then ignore this interval
+    threshold = 30
+    interval_precision = 2 # Round to 2 Decimal Places
+    intervals = intervals.select { |v| v < threshold }
+
+    # Get Metrics once tagging intervals are available
+    unless intervals.empty?
+      metrics = {}
+      metrics[:mean] = (intervals.reduce(:+) / intervals.size.to_f).round(interval_precision)
+      metrics[:min] = intervals.min
+      metrics[:max] = intervals.max
+      sum = intervals.inject(0) { |accum, i| accum + (i - metrics[:mean])**2 }
+      metrics[:variance] = (sum / intervals.size.to_f).round(interval_precision)
+      metrics[:stand_dev] = Math.sqrt(metrics[:variance]).round(interval_precision)
+      metrics
+    end
+    # if no Hash object is returned, the UI handles it accordingly
+  end
+
+  def self.calculate_formatted_percentage(user_tagging_report)
+    number_tagged = user_tagging_report.no_tagged.to_f
+    number_taggable = user_tagging_report.no_tagable
+    formatted_percentage = format('%.1f', (number_tagged / number_taggable) * 100)
+    user_tagging_report.no_tagable.zero? ? '-' : formatted_percentage
   end
 end

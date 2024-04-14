55b9971d1a5abc444750d9139d0021bd24202c86
Ray Wang <rwang32@vclvm177-50.vcl.ncsu.edu>
2024-04-14 09:53:25 -0400

    Added test cases

--- spec/models/view_translation_substitutor_spec.rb
+++ spec/models/view_translation_substitutor_spec.rb
@@ -1,3 +1,6 @@
+require 'rspec'
+require_relative 'app/models/view_translation_substitutor'
+
 describe 'ViewTranslationSubstitutor' do
     let(:vts) { ViewTranslationSubstitutor.new }
     # The substitute method is currently broken in the actual model, this spec will fail
@@ -7,4 +10,55 @@ describe 'ViewTranslationSubstitutor' do
     #         expect(vts.substitute(locale)).to eq(?)
     #     end
     # end
+    let(:locale) do
+        {
+          'views_directory' => {
+            'example_view' => {
+              'hello' => 'Hello, world!'
+            }
+          }
+        }
+      end
+    
+      describe '#substitute' do
+        context 'when the directory and view files exist' do
+          before do
+            allow(File).to receive(:exist?).and_return(true)
+            allow(File).to receive(:open).with('./views_directory/example_view.html.erb', 'w')
+            allow(File).to receive(:open).with('./views_directory/example_view.html.erb').and_yield(StringIO.new("Hello, world!"))
+          end
+    
+          it 'writes the substituted content to the same file' do
+            expect(File).to receive(:write).with('./views_directory/example_view.html.erb', anything)
+            vts.substitute(locale)
+          end
+    
+          it 'replaces the correct text with translation keys' do
+            expect(File).to receive(:write).with('./views_directory/example_view.html.erb', include("<%=t \".hello\"%>"))
+            vts.substitute(locale)
+          end
+        end
+    
+        context 'when the file does not exist' do
+          it 'handles missing files gracefully' do
+            allow(File).to receive(:exist?).and_return(false)
+            expect { vts.substitute(locale) }.not_to raise_error
+          end
+        end
+    
+        context 'checking the YAML output for stats' do
+          let(:expected_yaml_output) { "---\nviews_directory:\n  example_view:\n    hello:\n      replacements:\n      - Hello, world!\n" }
+    
+          it 'creates a YAML file with translation statistics' do
+            allow(File).to receive(:exist?).and_return(true)
+            allow(File).to receive(:open).with('./views_directory/example_view.html.erb').and_yield(StringIO.new("Hello, world!"))
+            allow(File).to receive(:open).with(instance_of(String), 'w') do |file_name, _, &block|
+              expect(file_name).to match(/translation_stats/)
+              block.call(StringIO.new(''))
+            end
+            expect(File).to receive(:write).with(anything, expected_yaml_output)
+            vts.substitute(locale)
+          end
+        end
+      end
 end

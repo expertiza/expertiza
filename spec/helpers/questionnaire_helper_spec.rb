d1f23e3143ec8b57e5f6973e94d42661c4a106a7
root <root@vclvm177-226.vcl.ncsu.edu>
2024-04-19 23:54:16 -0400

    created spec files for questionnaire and review_bids

---
+++ spec/helpers/questionnaire_helper_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+RSpec.describe QuestionnaireHelper, type: :helper do
+  describe '.adjust_advice_size' do
+    let(:questionnaire) { FactoryBot.create(:questionnaire) }
+    let(:scored_question) { FactoryBot.create(:scored_question) }
+    let(:non_scored_question) { FactoryBot.create(:question) }
+
+    context 'when question is a ScoredQuestion' do
+      it 'adjusts advice size based on questionnaire scores' do
+        FactoryBot.create(:question_advice, question: scored_question, score: 5)
+        questionnaire.min_question_score = 1
+        questionnaire.max_question_score = 10
+        described_class.adjust_advice_size(questionnaire, scored_question)
+        expect(scored_question.question_advices.count).to eq(10)
+      end
+    end
+
+    context 'when question is not a ScoredQuestion' do
+      it 'does not adjust advice size' do
+        FactoryBot.create_list(:question_advice, 6, question: non_scored_question, score: 5)
+        described_class.adjust_advice_size(questionnaire, non_scored_question)
+        expect(non_scored_question.question_advices.count).to eq(6)
+      end
+    end
+  end
+
+  describe '.update_questionnaire_questions' do
+    let(:params) { { question: { '1' => { body: 'Updated body' } } } }
+    let(:question) { FactoryBot.create(:question) }
+
+    before do
+      allow(helper).to receive(:params).and_return(params)
+    end
+
+    it 'updates attributes of questionnaire questions based on form data' do
+      allow(QuestionnaireHelper).to receive(:update_questionnaire_questions)
+      described_class.update_questionnaire_questions
+      question.reload
+      expect(question.body).to eq('Updated body')
+    end
+
+    it 'does not modify unchanged attributes' do
+      allow(QuestionnaireHelper).to receive(:update_questionnaire_questions)
+      described_class.update_questionnaire_questions
+      question.reload
+      expect(question.title).to eq('Original title')
+    end
+  end
+
+  describe '.questionnaire_factory' do
+    it 'returns an instance of the corresponding questionnaire class' do
+      type = 'ReviewQuestionnaire'
+      expect(described_class.questionnaire_factory(type)).to be_an_instance_of(ReviewQuestionnaire)
+    end
+
+    it 'sets an error flash message if type is not found in the map' do
+      type = 'UnknownQuestionnaire'
+      expect { described_class.questionnaire_factory(type) }.to change { flash[:error] }.from(nil).to('Error: Undefined Questionnaire')
+    end
+  end
+end
--- spec/helpers/review_bids_helper_spec.rb
+++ spec/helpers/review_bids_helper_spec.rb
@@ -1,10 +1,73 @@
-describe ReviewBidsHelper do
-  let(:topic) { build(:topic) }
+# spec/helpers/review_bids_helper_spec.rb
+
+require 'rails_helper'
+require 'review_bids_helper'
+
+RSpec.describe ReviewBidsHelper, type: :helper do
   describe '#get_intelligent_topic_row_review_bids' do
-    context 'when no selected topics' do
-      it 'returns a row' do
-        allow(ReviewBid).to receive(:where).and_return([])
-        expect(helper.get_intelligent_topic_row_review_bids(topic, nil, 2)).to eq("<tr id=\"topic_\" style=\"background-color:rgb(47,352,0)\">")
+    let(:topic) { double('Topic', id: 1) }
+    let(:selected_topic) { double('SelectedTopic', topic_id: 1, is_waitlisted: false) }
+    let(:num_participants) { 5 }
+    let(:review_bid) { double('ReviewBid') }
+
+    context 'when selected topics are present' do
+      it 'returns HTML code for topic row with appropriate background color' do
+        selected_topic = instance_double('SelectedTopic', topic_id: 1, is_waitlisted: false)
+        allow(selected_topic).to receive_message_chain(:topic_id, :==).and_return(true)
+        allow(selected_topic).to receive(:is_waitlisted).and_return(false)
+        selected_topics = [selected_topic]
+    
+        expect(helper.get_intelligent_topic_row_review_bids(topic, selected_topics, num_participants)).to include('<tr bgcolor="yellow">')
+      end
+    end
+    
+    context 'when selected topic is waitlisted' do
+      it 'returns HTML code for topic row with appropriate background color' do
+        selected_topic = instance_double('SelectedTopic', topic_id: 1, is_waitlisted: true)
+        allow(selected_topic).to receive_message_chain(:topic_id, :==).and_return(true)
+        allow(selected_topic).to receive(:is_waitlisted).and_return(true)
+        selected_topics = [selected_topic]
+    
+        expect(helper.get_intelligent_topic_row_review_bids(topic, selected_topics, num_participants)).to include('<tr bgcolor="lightgray">')
+      end
+    end
+    
+    
+
+    context 'when selected topics are not present' do
+      it 'returns HTML code for topic row with appropriate background color' do
+        allow(helper).to receive(:get_topic_bg_color_review_bids).and_return('rgb(255,255,255)')
+        selected_topics = []
+
+        expect(helper.get_intelligent_topic_row_review_bids(topic, selected_topics, num_participants)).to include('<tr id="topic_1" style="background-color:rgb(255,255,255)">')
+      end
+    end
+
+    context 'when selected topics are nil' do
+      it 'returns HTML code for topic row with appropriate background color' do
+        selected_topics = nil
+
+        expect(helper.get_intelligent_topic_row_review_bids(topic, selected_topics, num_participants)).to include('<tr id="topic_1" style="background-color:rgb(47,352,0)">')
+      end
+    end
+  end
+
+  describe '#get_topic_bg_color_review_bids' do
+    let(:topic) { double('Topic', id: 1) }
+    let(:review_bid) { double('ReviewBid') }
+    let(:num_participants) { 5 }
+
+    it 'returns RGB color code for topic background color' do
+      allow(ReviewBid).to receive(:where).with(signuptopic_id: topic.id).and_return([review_bid])
+
+      expect(helper.get_topic_bg_color_review_bids(topic, num_participants)).to match(/^rgb\(\d+,\d+,\d+\)$/)
+    end
+
+    context 'when there are no review bids' do
+      it 'returns default RGB color code' do
+        allow(ReviewBid).to receive(:where).with(signuptopic_id: topic.id).and_return([])
+
+        expect(helper.get_topic_bg_color_review_bids(topic, num_participants)).to eq('rgb(47,352,0)')
       end
     end
   end

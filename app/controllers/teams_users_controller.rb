b22c0d9b1ae144e8521197578087df780c45cff3
balassmp@gmail.com <balassmp@gmail.com>
2024-04-20 01:25:52 -0400

    Modified code to add mentor to manually created teams

--- app/controllers/teams_users_controller.rb
+++ app/controllers/teams_users_controller.rb
@@ -54,7 +54,12 @@ class TeamsUsersController < ApplicationController
           flash[:error] = "\"#{user.name}\" is not a participant of the current assignment. Please <a href=\"#{urlAssignmentParticipantList}\">add</a> this user before continuing."
         else
           begin
-            add_member_return = team.add_member(user, team.parent_id)
+            if team.type == "Mentored" or assignment.auto_assign_mentor
+              mentoredTeam = MentoredTeamDecorator.new(team)
+              add_member_return = mentoredTeam.add_member(user, team.parent_id)
+            else
+              add_member_return = team.add_member(user, team.parent_id)
+            end
           rescue
             flash[:error] = "The user #{user.name} is already a member of the team #{team.name}"
             redirect_back fallback_location: root_path

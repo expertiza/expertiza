0f223f5bea8a118d271936f278a8e481acd58760
Shiva Kandhagatla <61696599+shiva1239@users.noreply.github.com>
2024-04-21 03:28:39 -0400

    Update lottery_helper.rb

--- app/helpers/lottery_helper.rb
+++ app/helpers/lottery_helper.rb
@@ -1,2 +1,14 @@
 module LotteryHelper
-end
+    def background_color_by_percentage(percentage)
+      case percentage
+      when 0...33
+        'background-color: #ffcccc;' # Light red for low percentages
+      when 33...66
+        'background-color: #ffcc99;' # Light orange for medium percentages
+      when 66..100
+        'background-color: #ccffcc;' # Light green for high percentages
+      else
+        'background-color: none;' # No background if outside range
+      end
+    end
+  end

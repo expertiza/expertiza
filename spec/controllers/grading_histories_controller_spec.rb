d18df01b8a3fdcdc3b20f180b072253b83a155cb
TylerEnck <tyler.enck@yahoo.com>
2024-04-22 18:30:09 -0400

    Add additional tests

--- spec/controllers/grading_histories_controller_spec.rb
+++ spec/controllers/grading_histories_controller_spec.rb
@@ -1,10 +1,12 @@
 describe GradingHistoriesController do
 
-  let(:grading_history) { build(:grading_history) }
+  let(:grading_history) { build(:grading_history, graded_member_id: 1, graded_item_type: 'Submission') }
+  let(:grading_history_review) { build(:grading_history, graded_member_id: 2, graded_item_type: 'Review') }
   let(:admin) { build(:admin) }
   let(:instructor) { build(:instructor, id: 6) }
   let(:ta) { build(:teaching_assistant, id: 8) }
-  let(:student) { build(:student, id: 2) }
+  let(:student) { build(:student, id: 2, name: 'Joe') }
+  let(:team) { build(:assignment_team, id: 1, assignment: assignment, users: [instructor], name: 'Team 1') }
   let(:assignment) do
     build(:assignment, id: 1, name: 'test assignment', instructor_id: 6, staggered_deadline: true, directory_path: 'test_assignment',
                        participants: [build(:participant)], teams: [build(:assignment_team)], course_id: 1)
@@ -76,12 +78,57 @@ describe GradingHistoriesController do
       it "sets @receiver and @assignment to empty strings" do
         request_params = { graded_member_id: 1, grade_type: 'Submission' }
         allow(GradingHistory).to receive(:assignment_for_history).and_return(assignment)
-        allow(GradingHistory).to receive(:where).and_return(assignment)
-        allow(assignment).to receive(:reverse_order).and_return([])
+        empty_list = []
+        allow(GradingHistory).to receive(:where).and_return(empty_list)
+        allow(empty_list).to receive(:reverse_order).and_return([])
         stub_current_user(instructor, instructor.role.name, instructor.role)
         get :index, params: request_params
         expect(controller.instance_variable_get(:@assignment)).to eq ""
         expect(controller.instance_variable_get(:@receiver)).to eq ""
+      end
+    end
+
+    context "when the most recent grading history is for a submission" do
+      it "sets @receiver to the name of the team and @assignment to the name of the submission" do
+        request_params = { graded_member_id: 1, grade_type: 'Submission' }
+        allow(GradingHistory).to receive(:assignment_for_history).and_return(assignment)
+        grading_history_list = [grading_history]
+        allow(GradingHistory).to receive(:where).and_return(grading_history_list)
+        allow(grading_history_list).to receive(:reverse_order).and_return(grading_history_list)
+        allow(Team).to receive(:where).and_return(team)
+        team_name = team.name
+        allow(team).to receive(:pluck).and_return(team_name)
+        allow(team_name).to receive(:first).and_return(team_name)
+        allow(Assignment).to receive(:where).and_return(assignment)
+        assignment_name = assignment.name
+        allow(assignment).to receive(:pluck).and_return(assignment_name)
+        allow(assignment_name).to receive(:first).and_return(assignment_name)
+        stub_current_user(instructor, instructor.role.name, instructor.role)
+        get :index, params: request_params
+        expect(controller.instance_variable_get(:@assignment)).to eq "test assignment"
+        expect(controller.instance_variable_get(:@receiver)).to eq "Graded Team: Team 1"
+      end
+    end
+  
+    context "when the most recent grading history is for a review" do
+      it "sets @receiver to the name of the user and @assignment to the name of the assignment" do
+        request_params = { graded_member_id: 2, grade_type: 'Review' }
+        allow(GradingHistory).to receive(:assignment_for_history).and_return(assignment)
+        grading_history_list = [grading_history_review]
+        allow(GradingHistory).to receive(:where).and_return(grading_history_list)
+        allow(grading_history_list).to receive(:reverse_order).and_return(grading_history_list)
+        allow(User).to receive(:where).and_return(student)
+        user_name = student.name
+        allow(student).to receive(:pluck).and_return(user_name)
+        allow(user_name).to receive(:first).and_return(user_name)
+        allow(Assignment).to receive(:where).and_return(assignment)
+        assignment_name = assignment.name
+        allow(assignment).to receive(:pluck).and_return(assignment_name)
+        allow(assignment_name).to receive(:first).and_return(assignment_name)
+        stub_current_user(instructor, instructor.role.name, instructor.role)
+        get :index, params: request_params
+        expect(controller.instance_variable_get(:@assignment)).to eq "review for test assignment"
+        expect(controller.instance_variable_get(:@receiver)).to eq "Graded User: Joe"
       end
     end
   end

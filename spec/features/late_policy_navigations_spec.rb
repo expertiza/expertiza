0a75e14928078f836050a6313f991aef12935be2
Neel Shah <54409683+neelshah26@users.noreply.github.com>
2024-04-22 14:56:59 -0400

    Code Climate Fixes

--- spec/features/late_policy_navigations_spec.rb
+++ spec/features/late_policy_navigations_spec.rb
@@ -5,20 +5,20 @@ describe 'Navigation scenarios for Late Policies' do
 
   # This block runs before each test to set up the necessary context
   before(:each) do
-    @course = create(:course, name: "Test Course")
+    @course = create(:course, name: 'Test Course')
     login_as('instructor6')
   end
 
   # Test case for navigating back from "New Late Policy" to the Assignments tab when accessed directly
   it 'navigates to Assignments tab on the home screen from New Late Policy' do
-    visit "/late_policies/new"
+    visit '/late_policies/new'
     click_link 'Back'
     expect(page).to have_current_path('/student_task/list')
   end
 
   # Test case for navigating back from "All Late Policies" to the Assignments tab when accessed directly
   it 'navigates to Assignments tab on the home screen from All Late Policies' do
-    visit "/late_policies"
+    visit '/late_policies'
     click_link 'Back'
     expect(page).to have_current_path('/student_task/list')
   end

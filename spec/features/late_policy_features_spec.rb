cd299890aa9abad8deb4b8fab6b7efec45fb288d
Shaival Pradipbhai Shah <sshah35@vclvm176-51.vcl.ncsu.edu>
2024-04-22 19:06:28 -0400

    undo diff methods in late_policy_features_spec

--- spec/features/late_policy_features_spec.rb
+++ spec/features/late_policy_features_spec.rb
@@ -4,7 +4,13 @@ describe 'Assignment creation topics tab' do
   include AssignmentCreationHelper
   # This block runs before each test to set up the environment
   before(:each) do
-    setup_assignment_test_environment
+    create_deadline_types
+    (1..3).each { |i| create(:course, name: "Course #{i}") }
+    @assignment = create(:assignment, name: 'assignment for late policy test')
+    login_as('instructor6')
+    visit "/assignments/#{@assignment.id}/edit"
+    check('assignment_form_assignment_calculate_penalty', allow_label_click: true)
+    click_link 'Due dates'
   end
 
   # Test to ensure no flash error message is displayed on the New Late Policy Page
@@ -41,17 +47,4 @@ describe 'Assignment creation topics tab' do
     click_link 'Back'
     expect(page).to have_current_path("/assignments/#{@assignment.id}/edit")
   end
-
-  private
-
-  def setup_assignment_test_environment
-    create_deadline_types
-    (1..3).each { |i| create(:course, name: "Course #{i}") }
-    @assignment = create(:assignment, name: 'assignment for late policy test')
-    login_as('instructor6')
-    visit "/assignments/#{@assignment.id}/edit"
-    check('assignment_form_assignment_calculate_penalty', allow_label_click: true)
-    click_link 'Due dates'
-  end
-
 end

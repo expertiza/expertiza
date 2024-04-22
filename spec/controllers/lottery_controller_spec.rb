553f43930516ea50e731ca829bb51879fc77b8a3
SaiSanthoshG <52049230+saisanthoshG@users.noreply.github.com>
2024-04-22 14:30:06 -0400

    Moved Rspec tests for background color helper method to lottery_helper_spec.rb

--- spec/controllers/lottery_controller_spec.rb
+++ spec/controllers/lottery_controller_spec.rb
@@ -109,32 +109,6 @@ describe LotteryController do
       expect(controller.calculate_bidding_summary_based_on_priority).to eq(expected_topic_data)
     end
   end
-
-  # Assuming your helper module is in the helpers folder
-  RSpec.describe LotteryHelper, type: :helper do
-  # Test for low percentage range
-  describe '#background_color_by_percentage' do
-    it 'returns light red for low percentages' do
-      expect(helper.background_color_by_percentage(10)).to eq('background-color: #ffcccc;')
-    end
-
-    # Test for medium percentage range
-    it 'returns light orange for medium percentages' do
-      expect(helper.background_color_by_percentage(50)).to eq('background-color: #ffcc99;')
-    end
-
-    # Test for high percentage range
-    it 'returns light green for high percentages' do
-      expect(helper.background_color_by_percentage(80)).to eq('background-color: #ccffcc;')
-    end
-
-    # Test for percentage out of range
-    it 'returns no background for percentages out of range' do
-      expect(helper.background_color_by_percentage(101)).to eq('background-color: none;')
-      expect(helper.background_color_by_percentage(-1)).to eq('background-color: none;')
-    end
-  end
-end
   
   describe '#construct_users_bidding_info' do
     it 'generate users bidding information hash' do

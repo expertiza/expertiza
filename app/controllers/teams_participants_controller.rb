768f71106ee7ed1828c3d1631abb667f81f784d1
abhigna98 <30664350+abhigna98@users.noreply.github.com>
2024-04-23 18:58:45 -0400

    Removed old commented create method

--- app/controllers/teams_participants_controller.rb
+++ app/controllers/teams_participants_controller.rb
@@ -39,68 +39,6 @@ class TeamsParticipantsController < ApplicationController
     @team = Team.find(params[:id])  # Retrieves the team for which a new participant is to be added.
   end
 
-  # Creates a new team participant entry.
-  # def create
-  #   user = User.find_by(name: params[:user][:name].strip)  # Attempts to find an existing user by name.
-  #   unless user
-  #     urlCreate = url_for(controller: 'users', action: 'new')  # Prepares the URL for user creation.
-  #     flash[:error] = "\"#{params[:user][:name].strip}\" is not defined. Please <a href=\"#{urlCreate}\">create</a> this user before continuing."
-  #     return  # Exits the method early if the user does not exist.
-  #   end
-
-  #   team = Team.find(params[:id])  # Finds the team to add the user to.
-  #   unless user.nil?
-  #     if team.is_a?(AssignmentTeam)  # Differentiates behavior based on whether the team is for an assignment.
-  #       assignment = Assignment.find(team.parent_id)  # Retrieves the associated assignment.
-  #       participant = AssignmentParticipant.find_by(user_id: user.id, parent_id: assignment.id)  # Finds or initializes the participant.
-  #       if assignment.participant_on_team?(participant)  # Checks if the participant is already on a team.
-  #         flash[:error] = "This user is already assigned to a team for this assignment"
-  #         redirect_back fallback_location: root_path
-  #         return
-  #       end
-  #       if participant.nil?  # Checks if the participant does not exist.
-  #         urlAssignmentParticipantList = url_for(controller: 'participants', action: 'list', id: assignment.id, model: 'Assignment', authorization: 'participant')
-  #         flash[:error] = "\"#{user.name}\" is not a participant of the current assignment. Please <a href=\"#{urlAssignmentParticipantList}\">add</a> this user before continuing."
-  #       else
-  #         begin
-  #           add_member_return = team.add_participant_to_team(participant, team.parent_id)  # Tries to add the participant to the team.
-  #         rescue  # Handles exceptions if adding the participant fails (e.g., already a member).
-  #           flash[:error] = "The user #{user.name} is already a member of the team #{team.name}"
-  #           redirect_back fallback_location: root_path
-  #           return
-  #         end
-  #         flash[:error] = 'This team already has the maximum number of members.' if add_member_return == false  # Checks if the team is full.
-  #       end
-  #     else  # Handles the case for course teams similarly.
-  #       course = Course.find(team.parent_id)
-  #       participant = CourseParticipant.find_by(user_id: user.id, parent_id: course.id)
-  #       if course.participant_on_team?(participant)
-  #         flash[:error] = "This user is already assigned to a team for this course"
-  #         redirect_back fallback_location: root_path
-  #         return
-  #       end
-  #       if CourseParticipant.find_by(user_id: user.id, parent_id: course.id).nil?
-  #         urlCourseParticipantList = url_for(controller: 'participants', action: 'list', id: course.id, model: 'Course', authorization: 'participant')
-  #         flash[:error] = "\"#{user.name}\" is not a participant of the current course. Please <a href=\"#{urlCourseParticipantList}\">add</a> this user before continuing."
-  #       else
-  #         begin
-  #           add_member_return = team.add_participant_to_team(participant, team.parent_id)
-  #         rescue
-  #           flash[:error] = "The user #{user.name} is already a member of the team #{team.name}"
-  #           redirect_back fallback_location: root_path
-  #           return
-  #         end
-  #         flash[:error] = 'This team already has the maximum number of members.' if add_member_return == false
-  #         if add_member_return
-  #           @teams_participant = TeamsParticipant.last  # Captures the last added participant for confirmation.
-  #           undo_link("The team user \"#{user.name}\" has been successfully added to \"#{team.name}\".")  # Provides an undo link with a success message.
-  #         end
-  #       end
-  #     end
-  #   end
-
-  #   redirect_to controller: 'teams', action: 'list', id: team.parent_id  # Redirects to the list of teams.
-  # end
 
   # Starts the creation process for adding a user to a team
 def create

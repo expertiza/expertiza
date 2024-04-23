f6a4f953727f9e2e33d04a17add7f576cb9324ba
root <root@vclvm177-226.vcl.ncsu.edu>
2024-04-23 19:06:42 -0400

    tested questionnaire factory method

--- spec/helpers/questionnaire_helper_spec.rb
+++ spec/helpers/questionnaire_helper_spec.rb
@@ -1,65 +1,84 @@
 require 'rails_helper'
-require 'fakeredis/rspec'
+require 'questionnaire_helper'
 
 RSpec.describe QuestionnaireHelper, type: :helper do
   describe '.adjust_advice_size' do
-    let(:questionnaire) { FactoryBot.create(:questionnaire) }
-    let(:scored_question) { FactoryBot.create(:scored_question) }
-    let(:non_scored_question) { FactoryBot.create(:question) }
+    let(:questionnaire) { double('Questionnaire', max_question_score: 10, min_question_score: 1) }
+    let(:scored_question) { double('ScoredQuestion', id: 1) }
+    let(:non_scored_question) { double('Question') }
+    let(:question_advice) { double('QuestionAdvice') }
 
     context 'when question is a ScoredQuestion' do
       it 'adjusts advice size based on questionnaire scores' do
-        scored_question = FactoryBot.create(:scored_question)
-        FactoryBot.create(:question_advice, question: scored_question, score: 5)
-        questionnaire = FactoryBot.create(:questionnaire)
-        questionnaire.min_question_score = 1
-        questionnaire.max_question_score = 10
+        allow(QuestionAdvice).to receive(:where).and_return([])
+        allow(QuestionAdvice).to receive(:new).and_return(question_advice)
+        allow(scored_question).to receive(:is_a?).with(ScoredQuestion).and_return(true)
+        allow(scored_question).to receive(:question_advices).and_return([])
+
         described_class.adjust_advice_size(questionnaire, scored_question)
-        expect(scored_question.question_advices.count).to eq(10)
+
+        expect(scored_question).to have_received(:question_advice).exactly(10).times
+        expect(question_advice).to have_received(:save).exactly(10).times
       end
     end
 
-    # context 'when question is not a ScoredQuestion' do
-    #   it 'does not adjust advice size' do
-    #     FactoryBot.create_list(:question_advice, 6, question: non_scored_question, score: 5)
-    #     described_class.adjust_advice_size(questionnaire, non_scored_question)
-    #     expect(non_scored_question.question_advices.count).to eq(6)
-    #   end
-    # end
+    context 'when question is not a ScoredQuestion' do
+      it 'does not adjust advice size' do
+        allow(non_scored_question).to receive(:is_a?).with(ScoredQuestion).and_return(false)
+
+        described_class.adjust_advice_size(questionnaire, non_scored_question)
+
+        expect(non_scored_question).not_to have_received(:question_advice)
+        expect(QuestionAdvice).not_to have_received(:where)
+        expect(QuestionAdvice).not_to have_received(:new)
+      end
+    end  
+  end
+
+  describe '.questionnaire_factory' do
+    context 'when given a valid type' do
+      it 'returns an instance of the specified questionnaire type' do
+        questionnaire_type = 'ReviewQuestionnaire'
+        expect(helper.questionnaire_factory(questionnaire_type)).to be_an_instance_of(ReviewQuestionnaire)
+      end
+    end
+
+    context 'when given an invalid type' do
+      it 'sets an error flash message' do
+        questionnaire_type = 'UnknownQuestionnaire'
+        expect { helper.questionnaire_factory(questionnaire_type) }.to change { flash[:error] }.from(nil).to('Error: Undefined Questionnaire')
+      end
+    end
   end
 
   describe '.update_questionnaire_questions' do
     let(:params) { { question: { '1' => { body: 'Updated body' } } } }
-    let(:question) { FactoryBot.create(:question) } # Make sure :question factory creates an instance of the Question class
+    let(:question) { double('Question') }
 
     before do
-      allow(helper).to receive(:params).and_return(params)
+      allow(Question).to receive(:find).and_return(question)
+      allow(question).to receive(:save)
     end
 
-    it 'updates attributes of questionnaire questions based on form data' do
-      allow(QuestionnaireHelper).to receive(:update_questionnaire_questions)
-      described_class.update_questionnaire_questions
-      question.reload
-      expect(question.body).to eq('Updated body')
+    context 'when params[:question] is not nil' do
+      it 'updates attributes of questionnaire questions based on form data' do
+        helper.update_questionnaire_questions
+        expect(question).to have_received(:save)
+      end
+
+      it 'does not modify unchanged attributes' do
+        allow(question).to receive(:title).and_return('Original title')
+        helper.update_questionnaire_questions
+        expect(question).not_to have_received(:title=)
+      end
     end
 
-    # it 'does not modify unchanged attributes' do
-    #   allow(QuestionnaireHelper).to receive(:update_questionnaire_questions)
-    #   described_class.update_questionnaire_questions
-    #   question.reload
-    #   expect(question.title).to eq('Original title')
-    # end
+    context 'when params[:question] is nil' do
+      it 'returns without making any changes' do
+        allow(helper).to receive(:params).and_return(nil)
+        helper.update_questionnaire_questions
+        expect(question).not_to have_received(:save)
+      end
+    end
   end
-
-  # describe '.questionnaire_factory' do
-  #   it 'returns an instance of the corresponding questionnaire class' do
-  #     type = 'ReviewQuestionnaire'
-  #     expect(described_class.questionnaire_factory(type)).to be_an_instance_of(ReviewQuestionnaire)
-  #   end
-
-  #   it 'sets an error flash message if type is not found in the map' do
-  #     type = 'UnknownQuestionnaire'
-  #     expect { described_class.questionnaire_factory(type) }.to change { flash[:error] }.from(nil).to('Error: Undefined Questionnaire')
-  #   end
-  # end
 end

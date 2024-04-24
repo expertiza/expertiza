6d5ec36342ae2331aa09f43be29959b5bf103a5e
Meghana Chowdary Ainampudi <33152427+ameghana@users.noreply.github.com>
2024-04-23 21:16:21 -0400

    Refactoring manage_team_helper.rb

--- app/helpers/manage_team_helper.rb
+++ app/helpers/manage_team_helper.rb
@@ -17,7 +17,7 @@ module ManageTeamHelper
 
   # check if the user specified by 'user' already belongs to team specified by 'team_id'
   def user?(user, team_id)
-    if TeamsUser.where(team_id: team_id, user_id: user.id).first
+    if TeamsParticipant.where(team_id: team_id, user_id: user.id).first
       true
     else
       false

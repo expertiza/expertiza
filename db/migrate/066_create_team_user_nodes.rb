5b5e3458f05e28c80e89a6fdf3ae96e88820fca1
Meghana Chowdary Ainampudi <33152427+ameghana@users.noreply.github.com>
2024-04-23 22:12:29 -0400

    Refactoring 066_create_team_user_nodes.rb

--- db/migrate/066_create_team_user_nodes.rb
+++ db/migrate/066_create_team_user_nodes.rb
@@ -1,11 +1,11 @@
 class CreateTeamUserNodes < ActiveRecord::Migration[4.2]
   def self.up
     begin
-      remove_column :teams_users, :assignment_id
+      remove_column :teams_participants, :assignment_id
     rescue StandardError
     end
 
-    teamsusers = TeamsUser.all
+    teamsusers = TeamsParticipant.all
     teamsusers.each  do |user|
       parent = TeamNode.find_by_node_object_id(user.team_id)
       if parent
@@ -15,9 +15,9 @@ class CreateTeamUserNodes < ActiveRecord::Migration[4.2]
   end
 
   def self.down
-    teamsusers = TeamsUser.all
+    teamsusers = TeamsParticipant.all
     teamsusers.each(&:destroy)
 
-    add_column :teams_users, :assignment_id, :integer
+    add_column :teams_participants, :assignment_id, :integer
   end
 end

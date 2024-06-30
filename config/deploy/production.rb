0dd05d30082a23122be8add06aa8a271463c48d0
Ameya Vaichalkar <57044378+ameyagv@users.noreply.github.com>
2024-06-30 19:23:57 -0400

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '10.0.0.1', user: 'expertiza', roles: %w[web app]
+server '152.14.199.156', user: 'expertiza', roles: %w[web app]
 
 set :default_env, 'JAVA_HOME' => '/usr/jdk-11'
 set :branch, 'main'

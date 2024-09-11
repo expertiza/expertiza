df7249bdda463fd746ddf7a7477b4314ef9f78d0
root <root@vclvm177-150.vcl.ncsu.edu>
2024-09-10 22:39:21 -0400

    fixed toggle question list functionality

--- app/assets/javascripts/view_team_in_grades.js
+++ app/assets/javascripts/view_team_in_grades.js
@@ -26,6 +26,16 @@ function onMetricToggle(clicked_metric) {
     }
 }
 
+function toggleQuestionnaire(elementId) {
+    const element = document.getElementById(elementId);
+    
+    if (element.style.display === "none" || element.style.display === "") {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+}
+
 var lesser = false;
 // Function to sort the columns based on the total review score
 function col_sort(m) {

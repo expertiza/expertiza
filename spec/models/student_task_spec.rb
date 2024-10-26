b02f02e78ce4ef6b457e98e152a638d4443e249a
moreharsh <moreharsh2001@gmail.com>
2024-10-26 16:30:29 -0400

    updated code as per the requirement of codeclimate: removed unwanted spaces, and changed code format and style

--- spec/models/student_task_spec.rb
+++ spec/models/student_task_spec.rb
@@ -2,29 +2,37 @@ describe StudentTask do
   # Write your mocked object here!
   let(:participant) { build(:participant, id: 1, user_id: user.id, parent_id: assignment.id) }
   let(:participant2) { build(:participant, id: 2, user_id: user2.id, parent_id: assignment.id) }
+  
   let(:user) { create(:student) }
   let(:user2) { create(:student, name: 'qwertyui', id: 5) }
   let(:user3) { create(:student, name: 'qwertyui1234', id: 6) }
-  let(:course) { build(:course) }
+  
   let(:assignment) { build(:assignment, name: 'assignment', directory_path: 'assignment') }
   let(:assignment2) { build(:assignment, name: 'assignment2', directory_path: 'assignment2') }
+  
   let(:team) { create(:assignment_team, id: 1, name: 'team 1', parent_id: assignment.id, users: [user, user2]) }
   let(:team2) { create(:assignment_team, id: 2, name: 'team 2', parent_id: assignment2.id, users: [user3]) }
   let(:team_user) { create(:team_user, id: 3, team_id: team.id, user_id: user.id) }
   let(:team_user2) { create(:team_user, id: 4, team_id: team.id, user_id: user2.id) }
   let(:team2_user3) { create(:team_user, id: 5, team_id: team2.id, user_id: user3.id) }
+  
+  let(:course) { build(:course) }
   let(:course_team) { create(:course_team, id: 3, name: 'course team 1', parent_id: course.id) }
   let(:cource_team_user) { create(:team_user, id: 6, team_id: course_team.id, user_id: user.id) }
   let(:cource_team_user2) { create(:team_user, id: 7, team_id: course_team.id, user_id: user2.id) }
+  
   let(:topic) { build(:topic) }
   let(:topic2) { create(:topic, topic_name: 'TestReview') }
   let(:topic3) { create(:topic) }
+  
   let(:due_date) { build(:assignment_due_date, deadline_type_id: 1) }
   let(:deadline_type) { build(:deadline_type, id: 1) }
+  
   let(:review_response_map) { build(:review_response_map, assignment: assignment, reviewer: participant, reviewee: team2) }
   let(:metareview_response_map) { build(:meta_review_response_map, reviewed_object_id: 1) }
   let(:response) { build(:response, id: 1, map_id: 1, response_map: review_response_map) }
   let(:response2) { build(:response, id: 2, map_id: 1, response_map: review_response_map) }
+  
   let(:submission_record) { build(:submission_record, id: 1, team_id: 1, assignment_id: 1) }
   let(:student_task) do
     StudentTask.new(
@@ -270,5 +278,4 @@ describe StudentTask do
       end
     end
   end
-
 end

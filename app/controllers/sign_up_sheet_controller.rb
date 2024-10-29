a5400de871e4a7694168e51cf40d274cb41f7c4d
DarthAsh <54763573+DarthAsh@users.noreply.github.com>
2024-10-28 21:35:54 -0400

    Deleting commented method which has been refactored

--- app/controllers/sign_up_sheet_controller.rb
+++ app/controllers/sign_up_sheet_controller.rb
@@ -271,26 +271,6 @@ class SignUpSheetController < ApplicationController
 
   redirect_to controller: 'assignments', action: 'edit', id: params[:assignment_id]
 end
-#  def signup_as_instructor_action
-#    user = User.find_by(name: params[:username])
-#    if user.nil? # validate invalid user
-#      flash[:error] = 'That student does not exist!'
-#    else
-#      if AssignmentParticipant.exists? user_id: user.id, parent_id: params[:assignment_id]
-#        if SignUpSheet.signup_team(params[:assignment_id], user.id, params[:topic_id])
-#          flash[:success] = 'You have successfully signed up the student for the topic!'
-#          ExpertizaLogger.info LoggerMessage.new(controller_name, '', 'Instructor signed up student for topic: ' + params[:topic_id].to_s)
-#        else
-#          flash[:error] = 'The student has already signed up for a topic!'
-#          ExpertizaLogger.info LoggerMessage.new(controller_name, '', 'Instructor is signing up a student who already has a topic')
-#        end
-#      else
-#        flash[:error] = 'The student is not registered for the assignment!'
-#        ExpertizaLogger.info LoggerMessage.new(controller_name, '', 'The student is not registered for the assignment: ' << user.id)
-#      end
-#    end
-#    redirect_to controller: 'assignments', action: 'edit', id: params[:assignment_id]
-#  end
 
   # this function is used to delete a previous signup
 

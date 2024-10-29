f558c1c7fc77e5d5eb403db519fa616cea9f3095
neerua08 <neerua08@gmail.com>
2024-10-29 15:40:25 -0400

    Renamed variables in get_data_for_review_report

--- app/helpers/review_mapping_helper.rb
+++ app/helpers/review_mapping_helper.rb
@@ -7,18 +7,18 @@ module ReviewMappingHelper
   # gets the response map data such as reviewer id, reviewed object id and type for the review report
   #
   def get_data_for_review_report(reviewed_object_id, reviewer_id, type)
-    rspan = 0
+    row_number = 0
     (1..@assignment.num_review_rounds).each { |round| instance_variable_set('@review_in_round_' + round.to_s, 0) }
 
     response_maps = ResponseMap.where(['reviewed_object_id = ? AND reviewer_id = ? AND type = ?', reviewed_object_id, reviewer_id, type])
     response_maps.each do |ri|
-      rspan += 1 if Team.exists?(id: ri.reviewee_id)
+      row_number += 1 if Team.exists?(id: ri.reviewee_id)
       responses = ri.response
       (1..@assignment.num_review_rounds).each do |round|
         instance_variable_set('@review_in_round_' + round.to_s, instance_variable_get('@review_in_round_' + round.to_s) + 1) if responses.exists?(round: round)
       end
     end
-    [response_maps, rspan]
+    [response_maps, row_number]
   end
 
   #

0c056aef493036ac1d1e7e5ef231538a349d84b9
jliu77 <jacckkliu@gmail.com>
2024-10-30 15:37:02 -0400

    Update auth_controller.rb

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -6,7 +6,7 @@ class AuthController < ApplicationController
   verify method: :post, only: %i[login logout],
          redirect_to: { action: :list }
 
-  def action_allowed
+  def action_allowed?
     case params[:action]
     when 'login', 'logout', 'login_failed'
       true

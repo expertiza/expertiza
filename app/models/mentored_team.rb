f64acb5ca9a22e89935d5b0914d7b39f9bdc522c
jliu77 <jacckkliu@gmail.com>
2024-11-03 17:31:19 -0500

    Update mentored_team.rb
    https://chatgpt.com/share/6727f9a6-b164-8003-88ad-f391e85817cb

--- app/models/mentored_team.rb
+++ app/models/mentored_team.rb
@@ -3,19 +3,20 @@ class MentoredTeam < AssignmentTeam
     # Overridden method to include the MentorManagement workflow
     def add_member(user, _assignment_id = nil)
         raise "The user #{user.name} is already a member of the team #{name}" if user?(user)
-    
-        can_add_member = false
-        unless full?
-          can_add_member = true
-          t_user = TeamsUser.create(user_id: user.id, team_id: id)
-          parent = TeamNode.find_by(node_object_id: id)
-          TeamUserNode.create(parent_id: parent.id, node_object_id: t_user.id)
-          add_participant(parent_id, user)
-          ExpertizaLogger.info LoggerMessage.new('Model:Team', user.name, "Added member to the team #{id}")
-        end
-        if can_add_member
-            MentorManagement.assign_mentor(_assignment_id, id)
-        end
-        can_add_member
+        return false unless !full?
+        
+        add_user_to_team(user)
+        ExpertizaLogger.info LoggerMessage.new('Model:Team', user.name, "Added member to the team #{id}")
+        MentorManagement.assign_mentor(_assignment_id, id)
+        true
+    end
+
+    private 
+
+    def add_user_to_team(user)
+        t_user = TeamsUser.create(user_id: user.id, team_id: id)
+        parent = TeamNode.find_by(node_object_id: id)
+        TeamUserNode.create(parent_id: parent.id, node_object_id: t_user.id)
+        add_participant(parent.id, user)
     end
 end

326cf558deec04c679b8710e8fb7d67fe983ec11
Brandon <bswalia@ncsu.edu>
2024-11-16 18:12:08 -0500

    fix: fixed the github_auth file to use the correct key and secret Removed github_metric_uses as it was being used as its only functionality was its existenence being a boolean value

--- db/migrate/20230322193915_create_github_metric_uses.rb
+++
@@ -1,8 +0,0 @@
-class CreateGithubMetricUses < ActiveRecord::Migration[4.2]
-  def change
-    create_table :github_metric_uses do |t|
-      t.integer :assignment_id
-    end
-    add_index :github_metric_uses, :id, unique: true
-  end
-end

63948d4c1ab40002dbf90f95c9ee2b96e736dbd3
root <root@vclvm178-95.vcl.ncsu.edu>
2024-12-01 15:55:23 -0500

    removed user references from comments in teams_participant

--- app/models/teams_participant.rb
+++ app/models/teams_participant.rb
@@ -5,8 +5,8 @@ class TeamsParticipant < ApplicationRecord
   has_one :team_participant_node, foreign_key: 'node_object_id', dependent: :destroy
   has_paper_trail
 
-  # Returns the name of the user associated with this TeamsParticipant.
-  # Appends '(Mentor)' if the user is identified as a mentor to indicate in UI
+  # Returns the name of the participant associated with this TeamsParticipant.
+  # Appends '(Mentor)' if the participant is identified as a mentor to indicate in UI
   def name(ip_address = nil)
     name = user.name(ip_address)
     name += ' (Mentor)' if MentorManagement.user_a_mentor?(user)
@@ -38,7 +38,7 @@ class TeamsParticipant < ApplicationRecord
     destroy
   end
 
-  # Removes the entry in the TeamsParticipant table for the given user and team IDs
+  # Removes the entry in the TeamsParticipant table for the given participant and team IDs
   def self.remove_team_participant(user_id, team_id)
     team_participant = TeamsParticipant.find_by(user_id: user_id, team_id: team_id)
     team_participant&.destroy
@@ -66,9 +66,9 @@ class TeamsParticipant < ApplicationRecord
     can_add_member
   end
 
-  # Finds the team ID for a given assignment and user
+  # Finds the team ID for a given assignment and participant
   def self.find_team_id(assignment_id, user_id)
-    # team_id variable represents the team_id for this user in this assignment
+    # team_id variable represents the team_id for this participant in this assignment
     TeamsParticipant.where(user_id: user_id).find do |participant|
       team = Team.find_by(id: participant.team_id) if participant.team_id # Finds the team associated with the participant
       team&.parent_id == assignment_id # Checks if the team's parent ID matches given assignment ID

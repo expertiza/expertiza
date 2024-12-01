6aaca8603258375f7081174dd76066b45f9eb02e
Kevin Dai <kdai2@vclvm178-99.vcl.ncsu.edu>
2024-11-30 23:38:57 -0500

    Refactor: Rename GitHub metrics methods for clarity

--- app/controllers/github_metrics_controller.rb
+++ app/controllers/github_metrics_controller.rb
@@ -3,6 +3,8 @@ class GithubMetricsController < ApplicationController
     include AssignmentHelper
     include GithubMetricsHelper
   
+    rescue_from StandardError, with: :handle_error
+  
     def action_allowed?
       current_user_has_instructor_privileges?
     end
@@ -12,7 +14,7 @@ class GithubMetricsController < ApplicationController
       teams = @assignment.teams
       teams.each do |team|
         topic_identifier, topic_name, users_for_curr_team, participants = get_data_for_list_submissions(team)
-        GithubMetrics.new(participants.first.id).process_metrics unless participants.first.nil?
+        process_team_metrics(participants.first) unless participants.first.nil?
       end
       redirect_to controller: 'assignments', action: 'list_submissions', id: @assignment.id
     end
@@ -20,11 +22,33 @@ class GithubMetricsController < ApplicationController
     def callback
       auth = request.env['omniauth.auth']
       session["github_access_token"] = auth.credentials.token
-      GithubMetrics.new(session["participant_id"], session["assignment_id"]).process_metrics
+      process_team_metrics(session["participant_id"], session["assignment_id"])
+      redirect_to action: :show, id: session["participant_id"], assignment_id: session["assignment_id"]
     end
   
     def show
-      @metrics = GithubMetrics.new(params[:id], params[:assignment_id])
+      @metrics = GithubMetrics.new(params[:id], params[:assignment_id], session["github_access_token"])
       @metrics.process_metrics
+    rescue StandardError => e
+      handle_missing_token if e.message == "GitHub access token is required"
+      raise
+    end
+  
+    private
+  
+    def process_team_metrics(participant_id, assignment_id = nil)
+      GithubMetrics.new(participant_id, assignment_id, session["github_access_token"]).process_metrics
+    end
+  
+    def handle_missing_token
+      session["participant_id"] = params[:id]
+      session["assignment_id"] = params[:assignment_id]
+      session["github_view_type"] = "view_submissions"
+      redirect_to "https://github.com/login/oauth/authorize?client_id=#{GITHUB_CONFIG['client_key']}"
+    end
+  
+    def handle_error(error)
+      flash[:error] = error.message
+      redirect_to controller: 'assignments', action: 'list_submissions', id: @assignment.id
     end
   end

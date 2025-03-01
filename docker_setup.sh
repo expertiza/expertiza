72ed57992d51773f0377917fb393ce02cf39c016
Chinmay Singhania <csingha@vclvm177-57.vcl.ncsu.edu>
2025-02-28 20:48:32 -0500

    Docker build successful (but app not running)

--- docker_setup.sh
+++ docker_setup.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
 # Update package list
-apt-get update
+apt-get update -qq
 
 # Install required system packages
 apt-get install -y \
+    openjdk-11-jdk \
     curl \
     default-mysql-client \
-    default-libmysqlclient-dev
+    default-libmysqlclient-dev \
+    build-essential \
+    libssl-dev \
+    xz-utils
 
-# Install nvm (Node Version Manager)
-curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+# Install Node.js 14 manually
+NODE_VERSION="14.21.0"  # Last supported version for older glibc
+cd /tmp
+curl -O https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-linux-x64.tar.xz
+tar -xJf node-v${NODE_VERSION}-linux-x64.tar.xz
+rm node-v${NODE_VERSION}-linux-x64.tar.xz
+mv node-v${NODE_VERSION}-linux-x64 /usr/local/lib/nodejs
+ln -s /usr/local/lib/nodejs/bin/node /usr/local/bin/node
+ln -s /usr/local/lib/nodejs/bin/npm /usr/local/bin/npm
 
-# Load nvm for the current shell
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-# Install Node.js using nvm
-nvm install 14  # Install Node.js 14
-nvm use 14      # Use Node.js 14
-nvm alias default 14  # Set Node.js 14 as the default version
-
-# Install global npm packages
+# Install bower
 npm install -g bower
 
 # Install Ruby dependencies
--- local.dockerfile
+++ local.dockerfile
@@ -3,17 +3,14 @@ FROM ruby:2.4.10
 COPY . /app
 WORKDIR /app
 
-# Run the setup script
-RUN /bin/sh docker_setup.sh
+RUN bash docker_setup.sh
 
-# Set JAVA_HOME to match the actual Java installation path
-ENV JAVA_HOME="/usr/lib/jvm/java-8-oracle"
-ENV NVM_DIR="/root/.nvm"
+# Install bower
+RUN npm install -g bower
 
-# Load nvm and install bower
-RUN . "$NVM_DIR/nvm.sh" && npm install -g bower
+ENV JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64"
+ENV PATH="/usr/local/lib/nodejs/bin:${PATH}"
 
-# Install bundler and project dependencies
 RUN bundle install
 RUN bundle config set --local path 'vendor/cache'
 

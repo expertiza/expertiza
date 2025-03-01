7b2476da74e0a17993a605413d4d26528c8a5dba
root <root@vclvm177-54.vcl.ncsu.edu>
2025-03-01 17:24:25 -0500

    Initial scaffolding for Meeting class.

---
+++ test/system/meetings_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class MeetingsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit meetings_url
+  #
+  #   assert_selector "h1", text: "Meeting"
+  # end
+end

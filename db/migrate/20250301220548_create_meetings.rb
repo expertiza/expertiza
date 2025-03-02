b58321f561cf957663c7e4bee15bcb1868b194ff
root <root@vclvm179-45.vcl.ncsu.edu>
2025-03-02 14:40:49 -0500

    changing table columns to snake_case to follow ruby naming guidelines

--- db/migrate/20250301220548_create_meetings.rb
+++ db/migrate/20250301220548_create_meetings.rb
@@ -1,8 +1,8 @@
 class CreateMeetings < ActiveRecord::Migration[5.1]
   def change
     create_table :meetings do |t|
-      t.datetime :Date
-      t.string :TeamID
+      t.datetime :date
+      t.string :team_id
 
       t.timestamps
     end

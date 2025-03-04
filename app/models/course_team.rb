e6a55932660082ee8041b1c57a4d6684d03356b7
Ahmed Safwat Hassan <aohassan@vclvm176-241.vcl.ncsu.edu>
2025-03-03 21:44:47 -0500

    Removed add_member

--- app/models/course_team.rb
+++ app/models/course_team.rb
@@ -1,10 +1,6 @@
 class CourseTeam < Team
   belongs_to :course, class_name: 'Course', foreign_key: 'parent_id'
 
-  # NOTE: inconsistency in naming of users that's in the team
-  #   currently they are being called: member, participant, user, etc...
-  #   suggestion: refactor all to participant
-
   # since this team is not an assignment team, the assignment_id is nil.
   def assignment_id
     nil
@@ -19,35 +15,25 @@ class CourseTeam < Team
     copy_members(new_team)
   end
 
-  # Import from csv
+  # Overwrite Team.import() to Import a course_team from csv
   def self.import(row, course_id, options)
-    raise ImportError, 'The course with the id "' + course_id.to_s + "\" was not found. <a href='/courses/new'>Create</a> this course?" if Course.find(course_id).nil?
+    raise ImportError, "The course with the id \"#{course_id}\" was not found. <a href='/courses/new'>Create</a> this course?" if Course.find(course_id).nil?
 
     @course_team = CourseTeam.new
     Team.import(row, course_id, options, @course_team)
   end
 
-  # Export to csv
+  # Overwrite Team.export() to Export a course_team to csv
   def self.export(csv, parent_id, options)
     @course_team = CourseTeam.new
     Team.export(csv, parent_id, options, @course_team)
   end
 
-  # Export the fields of the csv column
+  # Export the fields of the csv column to be used in export_file_controller.rb
   def self.export_fields(options)
     fields = []
     fields.push('Team Name')
     fields.push('Team members') if options[:team_name] == 'false'
     fields.push('Course Name')
-  end
-
-  # Add member to the course team
-  def add_member(user, _id = nil)
-    raise "The user \"#{user.name}\" is already a member of the team, \"#{name}\"" if user?(user)
-
-    t_user = TeamsUser.create(user_id: user.id, team_id: id)
-    parent = TeamNode.find_by(node_object_id: id)
-    TeamUserNode.create(parent_id: parent.id, node_object_id: t_user.id)
-    add_participant(parent_id, user)
   end
 end

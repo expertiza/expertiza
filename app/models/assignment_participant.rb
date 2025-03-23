79ba202b5385698b9bcdc3ea65f24bedd5a552e8
nmaruva <nmaruva@ncsu.edu>
2025-03-23 19:10:34 -0400

    refactored methods which had more than the expected arguements

--- app/models/assignment_participant.rb
+++ app/models/assignment_participant.rb
@@ -22,6 +22,20 @@ class AssignmentParticipant < Participant
   attr_accessor :avg_vol_per_round
   attr_accessor :overall_avg_vol
 
+  # Nested class to encapsulate selection parameters
+  class SelectionParams
+    attr_reader :team, :iterator, :participants, :participants_hash, :assignment_id, :review_strategy
+
+    def initialize(team:, iterator:, participants:, participants_hash:, assignment_id:, review_strategy:)
+      @team = team
+      @iterator = iterator
+      @participants = participants
+      @participants_hash = participants_hash
+      @assignment_id = assignment_id
+      @review_strategy = review_strategy
+    end
+  end
+
   def dir_path
     assignment.try :directory_path
   end
@@ -209,10 +223,11 @@ class AssignmentParticipant < Participant
   def team_user
     TeamsUser.where(team_id: team.id, user_id: user_id).first if team
   end
+
   # Returns an array of participant IDs who need more reviews
   def self.participants_needing_reviews(participants_hash, review_strategy)
     participants_hash.select { |_, review_num| review_num < review_strategy.reviews_per_student }
-                     .keys
+                    .keys
   end
 
   # Returns true if the participant is a member of the given team
@@ -238,39 +253,48 @@ class AssignmentParticipant < Participant
 
   # Selects participants for a team based on review strategy
   def self.select_participants_for_team(team, iterator, participants, participants_hash, assignment_id, review_strategy)
+    params = SelectionParams.new(
+      team: team,
+      iterator: iterator,
+      participants: participants,
+      participants_hash: participants_hash,
+      assignment_id: assignment_id,
+      review_strategy: review_strategy
+    )
+    
     if team.equal? team.class.last
-      select_participants_for_last_team(team, participants, participants_hash, review_strategy)
+      select_participants_for_last_team(params)
     else
-      select_participants_for_regular_team(team, iterator, participants, participants_hash, assignment_id, review_strategy)
+      select_participants_for_regular_team(params)
     end
   end
 
   # Selects participants for a regular team
-  def self.select_participants_for_regular_team(team, iterator, participants, participants_hash, assignment_id, review_strategy)
+  def self.select_participants_for_regular_team(params)
     selected_participants = []
-    valid_participants_count = ReviewResponseMap.valid_team_participants_count(team.id, assignment_id)
+    valid_participants_count = ReviewResponseMap.valid_team_participants_count(params.team.id, params.assignment_id)
     
-    while selected_participants.size < review_strategy.reviews_per_team
-      break if selected_participants.size == participants.size - valid_participants_count
+    while selected_participants.size < params.review_strategy.reviews_per_team
+      break if selected_participants.size == params.participants.size - valid_participants_count
       
-      participant_index = select_random_participant(iterator, participants, participants_hash, team.id)
+      participant_index = select_random_participant(params.iterator, params.participants, params.participants_hash, params.team.id)
       
-      next unless can_select_participant?(participant_index, participants, participants_hash, team.id, review_strategy, selected_participants)
+      next unless can_select_participant?(participant_index, params, selected_participants)
       
-      selected_participants << participants[participant_index].id
-      participants_hash[participants[participant_index].id] += 1
-      remove_completed_participants(participants, participants_hash, review_strategy, participant_index)
+      selected_participants << params.participants[participant_index].id
+      params.participants_hash[params.participants[participant_index].id] += 1
+      remove_completed_participants(params.participants, params.participants_hash, params.review_strategy, participant_index)
     end
     
     selected_participants
   end
 
   # Selects participants for the last team
-  def self.select_participants_for_last_team(team, participants, participants_hash, review_strategy)
-    participants.select do |participant|
-      if ReviewResponseMap.can_review_team?(participant.user_id, team.id) && 
-         participants_hash[participant.id] < review_strategy.reviews_per_student
-        participants_hash[participant.id] += 1
+  def self.select_participants_for_last_team(params)
+    params.participants.select do |participant|
+      if ReviewResponseMap.can_review_team?(participant.user_id, params.team.id) && 
+         params.participants_hash[participant.id] < params.review_strategy.reviews_per_student
+        params.participants_hash[participant.id] += 1
         true
       else
         false
@@ -279,10 +303,10 @@ class AssignmentParticipant < Participant
   end
 
   # Checks if a participant can be selected for review
-  def self.can_select_participant?(participant_index, participants, participants_hash, team_id, review_strategy, selected_participants)
-    participant = participants[participant_index]
-    return false unless ReviewResponseMap.can_review_team?(participant.user_id, team_id)
-    return false if participants_hash[participant.id] >= review_strategy.reviews_per_student
+  def self.can_select_participant?(participant_index, params, selected_participants)
+    participant = params.participants[participant_index]
+    return false unless ReviewResponseMap.can_review_team?(participant.user_id, params.team.id)
+    return false if params.participants_hash[participant.id] >= params.review_strategy.reviews_per_student
     return false if selected_participants.include?(participant.id)
     
     true

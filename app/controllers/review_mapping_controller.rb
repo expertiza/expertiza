1109fa9766b9c5a680ab66f840877f3bcf34aaa6
Aditya Anand Kulkarni <akulka26@vclvm176-37.vcl.ncsu.edu>
2025-03-24 00:23:52 -0400

    review_mapping_controller modified

--- app/controllers/review_mapping_controller.rb
+++ app/controllers/review_mapping_controller.rb
@@ -160,26 +160,6 @@ class ReviewMappingController < ApplicationController
     reviewer.below_outstanding_reviews_limit?(assignment)
   end
 
-  # assigns the quiz dynamically to the participant
-  # def assign_quiz_dynamically
-  #   begin
-  #     assignment = Assignment.find(params[:assignment_id])
-  #     reviewer = AssignmentParticipant.where(user_id: params[:reviewer_id], parent_id: assignment.id).first
-  #     if ResponseMap.where(reviewed_object_id: params[:questionnaire_id], reviewer_id: params[:participant_id]).first
-  #       flash[:error] = 'You have already taken that quiz.'
-  #     else
-  #       @map = QuizResponseMap.new
-  #       @map.reviewee_id = Questionnaire.find(params[:questionnaire_id]).instructor_id
-  #       @map.reviewer_id = params[:participant_id]
-  #       @map.reviewed_object_id = Questionnaire.find_by(instructor_id: @map.reviewee_id).id
-  #       @map.save
-  #     end
-  #   rescue StandardError => e
-  #     flash[:alert] = e.nil? ? $ERROR_INFO : e
-  #   end
-  #   redirect_to student_quizzes_path(id: reviewer.id)
-  # end
-  
   def assign_quiz_dynamically
     assignment_id = params[:assignment_id].to_i
     reviewer_id = params[:reviewer_id]

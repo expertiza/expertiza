26179cf89e31b26d13790f39a1f67776a30774cc
Ashwin Kumar Muniswamy <ak.muniswamy2023@gmail.com>
2025-03-24 21:30:39 -0400

    Refactored service test code

--- spec/services/review_bidding_algorithm_service_spec.rb
+++ spec/services/review_bidding_algorithm_service_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe ReviewBiddingAlgorithmService do
   include_context 'review bidding helpers'
+
   let(:service) { described_class }
   let(:assignment_id) { 1 }
   let(:reviewer_ids) { %w[45672 45673 45674 45675 45676 45677] }
@@ -10,26 +11,22 @@ describe ReviewBiddingAlgorithmService do
 
   before do
     allow(Rails.application).to receive(:config_for).with(:webservices)
-                                                    .and_return('review_bidding_webservice_url' => webservice_url)
+      .and_return('review_bidding_webservice_url' => webservice_url)
     allow(ReviewBid).to receive(:bidding_data).with(assignment_id, reviewer_ids)
-                                              .and_return(bidding_data)
+      .and_return(bidding_data)
   end
 
   describe '#run_bidding_algorithm' do
     context 'when the web service is available' do
-      let(:expected_response) { { '45672' => [5139, 5140], '45673' => [5141, 5142] } }
-      before { mock_successful_request(expected_response) }
-      it 'sends the request and returns the matched topics' do
-        expect(service.run_bidding_algorithm(bidding_data)).to eq(expected_response)
-      end
+      before { mock_successful_request('45672' => [5139, 5140], '45673' => [5141, 5142]) }
+      it { expect(service.run_bidding_algorithm(bidding_data)).to eq({ '45672' => [5139, 5140], '45673' => [5141, 5142] }) }
     end
 
     context 'when the web service is unavailable' do
       before { mock_failed_request }
-      it 'logs an error and returns false' do
+      it do
         expect(service.run_bidding_algorithm(bidding_data)).to be false
-        expect(Rails.logger).to have_received(:error)
-          .with(/Error in run_bidding_algorithm: Service down/)
+        expect(Rails.logger).to have_received(:error).with(/Error in run_bidding_algorithm: Service down/)
       end
     end
   end
--- spec/support/review_bidding_helpers.rb
+++ spec/support/review_bidding_helpers.rb
@@ -1,32 +1,32 @@
 RSpec.shared_context 'review bidding helpers', shared_context: :metadata do
-    TOPIC_IDS = [5139, 5140, 5141, 5142].freeze
+  TOPIC_IDS = [5139, 5140, 5141, 5142].freeze
 
-    def generate_bidding_data
-      { 'tid' => TOPIC_IDS, 'users' => user_bidding_data, 'max_accepted_proposals' => 3 }
-    end
+  def generate_bidding_data
+    { 'tid' => TOPIC_IDS, 'users' => user_bidding_data, 'max_accepted_proposals' => 3 }
+  end
 
-    def reviewer_ids
-      @reviewer_ids ||= [] # Ensure it's an array
-    end
+  def reviewer_ids
+    @reviewer_ids ||= [] # Ensure it's an array
+  end
 
-    def user_bidding_data
-      reviewer_ids.map { |id| [id, generate_bids(TOPIC_IDS)] }.to_h
-    end
+  def user_bidding_data
+    reviewer_ids.map { |id| [id, generate_bids(TOPIC_IDS)] }.to_h
+  end
 
-    def generate_bids(topic_ids, time = Time.now)
-      { 'bids' => topic_ids.map.with_index { |tid, i| format_bid(tid, i, time) }, 'otid' => nil }
-    end
+  def generate_bids(topic_ids, time = Time.now)
+    { 'bids' => topic_ids.map.with_index { |tid, i| format_bid(tid, i, time) }, 'otid' => nil }
+  end
 
-    def format_bid(topic_id, index, base_time)
-      { 'tid' => topic_id, 'priority' => index + 1, 'timestamp' => (base_time + index * 2).strftime('%a, %d %b %Y %H:%M:%S %Z %:z') }
-    end
+  def format_bid(topic_id, index, base_time)
+    { 'tid' => topic_id, 'priority' => index + 1, 'timestamp' => (base_time + index * 2).strftime('%a, %d %b %Y %H:%M:%S %Z %:z') }
+  end
 
-    def mock_successful_request(response)
-      allow(RestClient).to receive(:post).and_return(double(body: response.to_json))
-    end
+  def mock_successful_request(response)
+    allow(RestClient).to receive(:post).and_return(double(body: response.to_json))
+  end
 
-    def mock_failed_request
-      allow(RestClient).to receive(:post).and_raise(StandardError, 'Service down')
-      allow(Rails.logger).to receive(:error)
-    end
+  def mock_failed_request
+    allow(RestClient).to receive(:post).and_raise(StandardError, 'Service down')
+    allow(Rails.logger).to receive(:error)
+  end
 end

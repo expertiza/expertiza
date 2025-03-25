b79094049c1d78113e36c37c1280b9587777ab18
Ashwin Kumar Muniswamy <ak.muniswamy2023@gmail.com>
2025-03-24 21:37:43 -0400

    Refactored service test code

--- spec/services/review_bidding_algorithm_service_spec.rb
+++ spec/services/review_bidding_algorithm_service_spec.rb
@@ -11,15 +11,15 @@ describe ReviewBiddingAlgorithmService do
 
   before do
     allow(Rails.application).to receive(:config_for).with(:webservices)
-      .and_return('review_bidding_webservice_url' => webservice_url)
+                                                    .and_return('review_bidding_webservice_url' => webservice_url)
     allow(ReviewBid).to receive(:bidding_data).with(assignment_id, reviewer_ids)
-      .and_return(bidding_data)
+                                              .and_return(bidding_data)
   end
 
   describe '#run_bidding_algorithm' do
     context 'when the web service is available' do
       before { mock_successful_request('45672' => [5139, 5140], '45673' => [5141, 5142]) }
-      it { expect(service.run_bidding_algorithm(bidding_data)).to eq({ '45672' => [5139, 5140], '45673' => [5141, 5142] }) }
+      it { expect(service.run_bidding_algorithm(bidding_data)).to eq('45672' => [5139, 5140], '45673' => [5141, 5142]) }
     end
 
     context 'when the web service is unavailable' do

310e9c8a7c624f5937526dd3e0c5bce83e7f5fe1
Nick Winsen <nwinsen@ncsu.edu>
2025-04-21 17:07:41 -0400

    init service specs

---
+++ spec/services/assign_bidding_service_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+describe AssignBiddingService do
+  include_context 'assign bidding helpers'
+
+end
---
+++ spec/services/completed_reviews_counter_service.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+describe CompletedReviewCounterService do
+
+end

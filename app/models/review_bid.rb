e24f441915c2a4a485afccb7e8bc628afa53b9a0
Nick Winsen <nwinsen@ncsu.edu>
2025-04-22 23:37:48 -0400

    revert model away from debug

--- app/models/review_bid.rb
+++ app/models/review_bid.rb
@@ -28,26 +28,9 @@ class ReviewBid < ApplicationRecord
         topics_to_assign = matched_topics[reviewer_id.to_s] || []
         Rails.logger.debug "Assigning topics to reviewer #{reviewer_id} (user #{reviewer_user_id}): #{topics_to_assign.inspect}"
         topics_to_assign.each do |topic_id|
-          team_to_review = SignedUpTeam.find_by(topic_id: topic_id)&.team_id
-          Rails.logger.debug "team_to_review: #{team_to_review.inspect} for topic #{topic_id}"
-          next if team_to_review.nil?
-          next if team_to_review == reviewer_team_id
-          existing_map = ReviewResponseMap.find_by(
-            reviewed_object_id: assignment_id,
-            reviewer_id: reviewer_id,
-            reviewee_id: team_to_review
-          )
-          unless existing_map
-            ReviewResponseMap.create!(
-              reviewed_object_id: assignment_id,
-              reviewer_id: reviewer_id,
-              reviewee_id: team_to_review,
-              type: 'ReviewResponseMap'
-            )
-            Rails.logger.debug "Created ReviewResponseMap for reviewer #{reviewer_id} and team #{team_to_review}"
-          else
-            Rails.logger.debug "Skipping duplicate map for reviewer #{reviewer_id} and team #{team_to_review}"
-          end
+          Rails.logger.debug "Assigning topics to reviewer #{reviewer_id}: #{topics_to_assign.inspect}"
+          topics_to_assign.each do |topic|
+          assign_topic_to_reviewer(assignment_id, reviewer_id, topic)
         end
       end
     end

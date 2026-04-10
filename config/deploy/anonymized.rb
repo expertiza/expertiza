e4350a0b58ea7565cfeced6fe77aaac5017c7a3d
prithishsamantae <prithishsamanta@gmail.com>
2026-04-09 22:51:32 -0400

    Added anonymized.rb and capistrano.yml

---
+++ config/deploy/anonymized.rb
@@ -0,0 +1,16 @@
+# server-based syntax
+# ======================
+# Defines a single server with a list of roles and multiple properties.
+# You can define all roles on a single server, or split them:
+
+server '152.7.179.77', user: 'expertiza', roles: %w[web app]
+
+set :default_env, 'JAVA_HOME' => '/usr/jdk-11'
+set :branch, 'anonymized_server'
+set :rvm_ruby_version, '2.4'
+set :rails_env, 'anonymized'
+
+# role-based syntax
+role :app, %w[expertiza@152.7.179.77]
+role :web, %w[expertiza@152.7.179.77]
+role :db,  %w[expertiza@152.7.179.77]

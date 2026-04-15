dda922fadace97a03ba5695f9b316ce9b12ef47f
Prithish Samanta <prithishsamanta@gmail.com>
2026-04-15 13:29:10 -0400

    Comment out rails_env in anonymized.rb
    Comment out the rails_env setting in deploy configuration.

--- config/deploy/anonymized.rb
+++ config/deploy/anonymized.rb
@@ -8,7 +8,7 @@ server '152.7.179.77', user: 'expertiza', roles: %w[web app]
 set :default_env, 'JAVA_HOME' => '/usr/jdk-11'
 set :branch, 'anonymized_server'
 set :rvm_ruby_version, '2.4'
-set :rails_env, 'anonymized'
+# set :rails_env, 'anonymized'
 
 # role-based syntax
 role :app, %w[expertiza@152.7.179.77]
